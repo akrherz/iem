3d08e8f80aab0a85ac648f92871309fc244f72af
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2005-10-09 03:34:13 +0000

    Remove font references

--- htdocs/plotting/mesonorth/1min_ot.php
+++ htdocs/plotting/mesonorth/1min_ot.php
@@ -78,17 +78,17 @@ $graph->legend->Pos(0.01,0.05);
 //[DMF]$graph->y2axis->scale->ticks->Set(100,25);
 //[DMF]$graph->y2axis->scale->ticks->SetPrecision(0);
 
-$graph->title->SetFont(FF_VERDANA,FS_BOLD,14);
+$graph->title->SetFont(FF_FONT1,FS_BOLD,14);
 $graph->yaxis->SetTitle("Temperature [F]");
 
 //[DMF]$graph->y2axis->SetTitle("Solar Radiation [W m**-2]");
 
-$graph->yaxis->title->SetFont(FF_ARIAL,FS_BOLD,12);
+$graph->yaxis->title->SetFont(FF_FONT1,FS_BOLD,12);
 $graph->xaxis->SetTitle("Valid Local Time");
 $graph->xaxis->SetTitleMargin(30);
 //$graph->yaxis->SetTitleMargin(48);
 $graph->yaxis->SetTitleMargin(40);
-$graph->xaxis->title->SetFont(FF_ARIAL,FS_BOLD,12);
+$graph->xaxis->title->SetFont(FF_FONT1,FS_BOLD,12);
 $graph->xaxis->SetPos("min");
 
 // Create the linear plot
