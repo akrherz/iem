1c1dcea044ae8a67339ebff95af4bd95e48af013
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2005-10-12 15:26:05 +0000

    Fix lots of errors this thing

--- htdocs/COOP/freezing.php
+++ htdocs/COOP/freezing.php
@@ -2,8 +2,12 @@
 include("../../config/settings.inc.php");
 $TITLE = "IEM | COOP | Freezing Dates";
 include("$rootpath/include/header.php");
-include("$rootpath/include/database.inc.php"); ?>
-<?php include("$rootpath/include/COOPstations.php"); ?>
+include("$rootpath/include/database.inc.php"); 
+include("$rootpath/include/COOPstations.php"); 
+
+$sortcol = isset($_GET["sortcol"]) ? $_GET["sortcol"]: "station";
+
+?>
 
 <b>Nav:</b> <a href="<?php echo $rooturl; ?>/COOP/">COOP</a> <b> > </b> Freezing Dates<p>
 
@@ -35,11 +39,13 @@ function aSortBySecondIndex($multiArray, $secondIndex) {
  $connection = iemdb("coop");
 
  $query = "select station, valid, min_low, min_low_yr from climate 
-     WHERE valid > '2000-08-01' and min_low <= 32 ORDER by valid";
+     WHERE valid > '2000-08-01' and min_low <= 32 
+     and station NOT IN ('ia4381', 'ia7842') ORDER by valid";
  $rs = pg_exec($connection, $query);
 
  $query = "select station, valid, low from climate 
-     WHERE valid > '2000-08-01' and low <= 40 ORDER by valid";
+     WHERE valid > '2000-08-01' and low <= 40 
+     and station NOT IN ('ia4381', 'ia7842') ORDER by valid";
  $rs2 = pg_exec($connection, $query);
 
 
@@ -47,13 +53,12 @@ function aSortBySecondIndex($multiArray, $secondIndex) {
  for( $i=0; $row = @pg_fetch_array($rs,$i); $i++)
  {
    $st = $row["station"];
-   if (strlen($data[$st]) == 0){
-     $data[$st] = Array();
-     $data[$st]["station"] = $st;
+   if (! isset($data[$st])){
+     $data[$st] = Array("min_low" => 100, "station" => $st);
      $data[$st]["low"] = $row["min_low"];
      $data[$st]["lowyr"] = $row["min_low_yr"] ."-". substr($row["valid"], 5, 6);
    }
-   if (strlen($data[$st]["low28"]) == 0 ){
+   if (! isset($data[$st]["low28"]) ){
      if (intval($row["min_low"]) < 29 ){
        $data[$st]["low28"] = $row["min_low"];
        $data[$st]["low28yr"] = $row["min_low_yr"] ."-". substr($row["valid"], 5, 6);
@@ -65,17 +70,17 @@ function aSortBySecondIndex($multiArray, $secondIndex) {
  for( $i=0; $row = @pg_fetch_array($rs2,$i); $i++)
  {
    $st = $row["station"];
-   if (strlen($data[$st]["avelow40day"]) == 0){
+   if (! isset($data[$st]["avelow40day"])){
      if ( intval($row["low"]) < 41 ){
        $data[$st]["avelow40day"] = substr($row["valid"], 5, 6);
      }
    }
-   if (strlen($data[$st]["avelow32day"]) == 0){
+   if (! isset($data[$st]["avelow32day"])){
      if ( intval($row["low"]) < 33 ){
        $data[$st]["avelow32day"] = substr($row["valid"], 5, 6);
      }
    }
-   if (strlen($data[$st]["avelow28day"]) == 0){
+   if (! isset($data[$st]["avelow28day"])){
      if ( intval($row["low"]) < 28 ){
        $data[$st]["avelow28day"] = substr($row["valid"], 5, 6);
      }
@@ -84,30 +89,27 @@ function aSortBySecondIndex($multiArray, $secondIndex) {
  }
 
  $finalA = Array();
- if (strlen($sort) == 0){
-   $sort = "station";
- }
- $finalA = aSortBySecondIndex($data, $sort);
+ $finalA = aSortBySecondIndex($data, $sortcol);
 
  //-------------------------------------------------
 
  echo "<table border=1 cellpadding=2>
   <tr>
-    <th rowspan='3'><a href='freezing.php?sort=station'>COOP Site:</a></th>
+    <th rowspan='3'><a href='freezing.php?sortcol=station'>COOP Site:</a></th>
     <th colspan='4'>Record Lows:</th>
     <th colspan='3'>Average Lows:</th>
   </tr>
   <tr>
     <th colspan='2'>Temp <= 32&deg;F</th>
     <th colspan='2'>Temp <= 28&deg;F</th>
-    <td rowspan='2'><a href='freezing.php?sort=avelow40day'>Below 40&deg;F</a></td>
-    <td rowspan='2'><a href='freezing.php?sort=avelow32day'>Below 32&deg;F</a></td>
-    <td rowspan='2'><a href='freezing.php?sort=avelow28day'>Below 28&deg;F</a></td>
+    <td rowspan='2'><a href='freezing.php?sortcol=avelow40day'>Below 40&deg;F</a></td>
+    <td rowspan='2'><a href='freezing.php?sortcol=avelow32day'>Below 32&deg;F</a></td>
+    <td rowspan='2'><a href='freezing.php?sortcol=avelow28day'>Below 28&deg;F</a></td>
   </tr>
     <td>Temp:</td>
-    <td><a href='freezing.php?sort=lowyr'>Date:</a></td>
+    <td><a href='freezing.php?sortcol=lowyr'>Date:</a></td>
     <td>Temp:</td>
-    <td><a href='freezing.php?sort=low28yr'>Date:</a></td>
+    <td><a href='freezing.php?sortcol=low28yr'>Date:</a></td>
   </tr>";
 
  foreach($finalA as $key => $value ){
