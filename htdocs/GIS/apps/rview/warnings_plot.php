1add59096a0584160fe5a49afb486ea95c396607
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2005-12-09 18:16:44 +0000

    Lots of new stuff here.  This is probably when stuff breaks...

--- htdocs/GIS/apps/rview/warnings_plot.php
+++ htdocs/GIS/apps/rview/warnings_plot.php
@@ -1,4 +1,5 @@
 <?php
+include_once("../../../../config/settings.inc.php");
 global $_DATABASES;
 
 if ($isarchive)
@@ -36,7 +37,7 @@ if (strlen($site) == 0){
   $site = "DMX";
 }
 
-$map = ms_newMapObj("mosaic.map");
+$map = ms_newMapObj("$rootpath/data/gis/base4326.map");
 $map->set("width", $width);
 $map->set("height", $height);
 
@@ -61,11 +62,14 @@ $stlayer->set("status", 1);
 $lakes = $map->getlayerbyname("lakes");
 $lakes->set("status", 1);
 
-$clayer = $map->getlayerbyname("counties_unproj");
-$clayer->set("status", 1);
+$clayer = $map->getlayerbyname("uscounties");
+$clayer->set("status", in_array("uscounties", $layers) );
 
 $cwas = $map->getlayerbyname("cwas");
-$cwas->set("status", 1);
+$cwas->set("status", in_array("cwas", $layers) );
+
+$usdm = $map->getlayerbyname("usdm");
+$usdm->set("status", (in_array("usdm", $layers) && ! $isarchive) );
 
 $watches = $map->getlayerbyname("watches");
 $watches->set("connection", $_DATABASES["postgis"] );
@@ -75,20 +79,19 @@ $watches->set("data", "geom from (select type as wtype, geom, oid from watches w
 
 $c0 = $map->getlayerbyname("warnings0_c");
 $c0->set("connection", $_DATABASES["postgis"] );
-$c0->set("status", 1);
+$c0->set("status", in_array("warnings", $layers) );
 if ($isarchive)
 { 
-   $c0->set("data", "geom from (select phenomena, geom, oid from warnings_$year WHERE significance != 'A' and expire > '$db_ts' and issue <= '$db_ts' and gtype = 'C' ORDER by phenomena ASC) as foo using unique oid using SRID=4326");
-  //$c0->set("data", "geom from warnings_$year");
+   $c0->set("data", "geom from (select significance, phenomena, geom, oid from warnings_$year WHERE expire > '$db_ts' and issue <= '$db_ts' and gtype = 'C' ORDER by phenomena ASC) as foo using unique oid using SRID=4326");
 }else {
-  //$c0->setFilter("significance != 'A' and expire > '".$db_ts."' and issue <= '".$db_ts."' and gtype = 'C'");
-   $c0->set("data", "geom from (select phenomena, geom, oid from warnings_$year WHERE significance != 'A' and expire > '$db_ts' and issue <= '$db_ts' and gtype = 'C' ORDER by phenomena ASC) as foo using unique oid using SRID=4326");
+   $sql = "geom from (select significance, phenomena, geom, oid from warnings WHERE expire > '$db_ts' and issue <= '$db_ts' and gtype = 'C' ORDER by phenomena ASC) as foo using unique oid using SRID=4326";
+   $c0->set("data", $sql);
 }
 $q = "expire > '".$db_ts."' and issue <= '".$db_ts."' and gtype = 'C'";
 
 $p0 = $map->getlayerbyname("warnings0_p");
 $p0->set("connection", $_DATABASES["postgis"] );
-$p0->set("status", 1);
+$p0->set("status", in_array("warnings", $layers) );
 if ($isarchive)
 { 
    $p0->set("data", "geom from (select phenomena, geom, oid from warnings_$year WHERE significance != 'A' and expire > '$db_ts' and issue <= '$db_ts' and gtype = 'P') as foo using unique oid using SRID=4326");
@@ -97,9 +100,9 @@ if ($isarchive)
  $p0->set("data", "geom from (select phenomena, geom, oid from warnings WHERE significance != 'A' and expire > '$db_ts' and issue <= '$db_ts' and gtype = 'P') as foo using unique oid using SRID=4326");
 //$p0->setFilter("significance != 'A' and expire > '".$db_ts."' and issue < '". $db_ts."' and gtype = 'P'");
 }
-$radar = $map->getlayerbyname("radar2");
+$radar = $map->getlayerbyname("nexrad_n0r");
 $radar->set("data", $radfile);
-$radar->set("status", 1);
+$radar->set("status", in_array("nexrad", $layers) );
 
 $lsrs = $map->getlayerbyname("lsrs");
 $lsrs->set("connection", $_DATABASES["postgis"] );
@@ -129,13 +132,21 @@ $cwas->draw( $img);
 $watches->draw($img); 
 $c0->draw($img);
 $p0->draw($img);
+$usdm->draw($img);
 if ($lsrwindow != 0)
   $lsrs->draw($img);
 
+$map->embedScalebar($img);
 mktitle($map, $img, "                  IEM NEXRAD composite base reflect valid: $d");
 $map->drawLabelCache($img);
 mklogolocal($map, $img);
 
 $url = $img->saveWebImage();
+
+//$ltmp = $map->drawLegend();
+//$legendsrc = $ltmp->saveWebImage();
+
+//$stmp = $map->drawScaleBar();
+//$scalesrc = $stmp->saveWebImage();
 
 ?>
