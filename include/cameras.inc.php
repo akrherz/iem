56f6790207766775817123849c67ab7dd3d8ce53
akrherz <akrherz@iastate.edu>
2006-06-11 18:23:02 +0000

    Add Chariton

--- include/cameras.inc.php
+++ include/cameras.inc.php
@@ -11,6 +11,10 @@ $cameras = Array(
 		"iservice" => "Dynamic Broadband", 
 		"iserviceurl" => "http://www.dybb.com/",
 		"ip" => "216.203.109.40", "county" => "Carroll"),
+  "SCHI4" => Array("sts" => mktime(14,0,0,6,11,2006), "ets" => time(),
+        "name" => "Chariton", "active" => true, "removed" => false,
+        "ip" => "69.66.60.199", "county" => "Lucas"),
+
   "SCSI4" => Array("sts" => mktime(0,0,0, 9,20,2003), 
 					"ets" => time(),
 		"name" => "Creston", "active" => true,
