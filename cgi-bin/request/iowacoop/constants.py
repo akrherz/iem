f31c11097354ac0d186f418df38cf70c02697a35
akrherz <akrherz@iastate.edu>
2006-06-29 20:57:12 +0000

    Just forward everybody to the new interface.

--- cgi-bin/request/iowacoop/constants.py
+++ cgi-bin/request/iowacoop/constants.py
@@ -1,19 +1,5 @@
+#!/mesonet/python/bin/python
 
-import gd
-
-xwidth = 800
-yheight = 500
-
-im = gd.image((xwidth,yheight))
-
-# Allocate Colors          
-red = im.colorAllocate((255,0,0))
-green = im.colorAllocate((0,255,0))
-blue = im.colorAllocate((0,0,255))
-black = im.colorAllocate((0,0,0))
-white = im.colorAllocate((255,255,255))
-lgreen = im.colorAllocate((127,125,85))
-
-label = gd.gdFontMediumBold
-title = gd.gdFontGiant
-
+print 'Content-type: text/html'
+print 'Location: http://mesonet.agron.iastate.edu/request/coop/fe.phtml'
+print
