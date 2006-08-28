1fcc417451e653f2733c32e8f5ad3134b9c7154d
akrherz <akrherz@iastate.edu>
2006-08-28 15:58:24 +0000

    Finally

--- cgi-bin/afos/retrieve.py
+++ cgi-bin/afos/retrieve.py
@@ -33,15 +33,13 @@ def Main():
   if (len(rs) == 0 and pil[:3] == "MTR"):
     print "%s doesn't exist in AFOS database, looking in IEM's archive\n" % (pil,)
     access = pg.connect('iem', '10.10.10.20', user='nobody')
-    sql = "SELECT raw from current WHERE station = '%s'" % (pil[3:],)
-    print sql
+    sql = "SELECT raw from current WHERE station = '%s'" % (pil[3:].strip(),)
     rs = access.query( sql ).dictresult()
     if (len(rs) == 1):
       print rs[0]['raw']
     
     if (LIMIT > 1):
-      sql = "SELECT raw from current_log WHERE station = '%s' ORDER by valid DESC LIMIT %s" % (pil[3:], LIMIT)
-      print sql
+      sql = "SELECT raw from current_log WHERE station = '%s' ORDER by valid DESC LIMIT %s" % (pil[3:].strip(), LIMIT)
       rs = access.query( sql ).dictresult()
       for i in range(len(rs)):
         print rs[i]['raw']
