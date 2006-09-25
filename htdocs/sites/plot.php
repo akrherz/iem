8eac8e93658a7d2676a9ecab66d71952b6cdb3c0
akrherz <akrherz@iastate.edu>
2006-09-25 21:13:51 +0000

    small link fix

--- htdocs/sites/plot.php
+++ htdocs/sites/plot.php
@@ -6,8 +6,10 @@ include("setup.php");
  $TITLE = "IEM | Sites";
  include("$rootpath/include/header.php");
 
- $current = "7dayhilo"; include("sidebar.php"); 
  $prod = isset($_GET["prod"]) ? $_GET["prod"]: 0;
+ $current = "7dayhilo"; 
+ if ($prod == 1) $current = "monthhilo";
+ include("sidebar.php"); 
  $products = Array(
 0 => "7day_hilo_plot.php",
 1 => "month_hilo_plot.php",
