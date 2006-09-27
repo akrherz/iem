b8dd0ccc02c1d6c266c0389c98f709ed49ec69fc
akrherz <akrherz@iastate.edu>
2006-09-27 05:09:48 +0000

    Bad

--- include/feature.php
+++ include/feature.php
@@ -46,7 +46,7 @@ if (getenv("REMOTE_ADDR") == "206.61.97.204" )
 {
  $s = "<img src=\"images/smokey_1021.jpg\" style=\"float: left; margin: 5px;\">
 Smokey,
-<br /> &nbsp; &nbsp; &nbsp; &nbsp; 52 weeks now! :) Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! I love you and plenty more where that came from :) <br /> &nbsp; &nbsp; &nbsp; &nbsp;   &nbsp; &nbsp; &nbsp; &nbsp;  love, darly";
+<br /> &nbsp; &nbsp; &nbsp; &nbsp; 55 weeks now! :) Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! Muah! I love you and plenty more where that came from :) <br /> &nbsp; &nbsp; &nbsp; &nbsp;   &nbsp; &nbsp; &nbsp; &nbsp;  love, darly";
 }
 
 echo $s;
