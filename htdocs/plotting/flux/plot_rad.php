bf042c685e0042f0b1d67b53624188a672c7048b
akrherz <akrherz@iastate.edu>
2006-12-19 22:58:26 +0000

    Temp fixes to get plots working

--- htdocs/plotting/flux/plot_rad.php
+++ htdocs/plotting/flux/plot_rad.php
@@ -7,17 +7,17 @@ $month = isset($_GET['month']) ? $_GET['month'] : date("m");
 $day = isset($_GET['day']) ? $_GET['day'] : date("d");
 $var = isset($_GET['var']) ? $_GET['var'] : "netrad";
 
-$titles = Array("netrad" => "Net Radiation",
-  "soilheat" => "Soil Heat Flux",
-  "latent" => "Latent Heat Flux",
-  "co2" => "Carbon Dioxide",
-  "sensible" => "Sensible Heat Flux");
+$titles = Array("rn_total_avg" => "Net Radiation",
+  "shf1_avg" => "Soil Heat Flux",
+  "le_wpl" => "Latent Heat Flux",
+  "fc_wpl" => "Carbon Dioxide",
+  "hs" => "Sensible Heat Flux");
 
-$units = Array("netrad" => "Wm^-2",
-  "soilheat" => "Wm^-2",
-  "latent" => "Wm^-2",
-  "co2" => "mg/m^2/s",
-  "sensible" => "Wm^-2");
+$units = Array("rn_total_avg" => "Wm^-2",
+  "shf1_avg" => "Wm^-2",
+  "le_wpl" => "Wm^-2",
+  "fc_wpl" => "mg/m^2/s",
+  "hs" => "Wm^-2");
 
 $pgconn = iemdb("other");
 
@@ -61,8 +61,8 @@ $ts_lbl = date("d F Y", $ts);
 
 pg_close($pgconn);
 
-include ("/mesonet/php/include/jpgraph/jpgraph.php");
-include ("/mesonet/php/include/jpgraph/jpgraph_line.php");
+include ("$rootpath/include/jpgraph/jpgraph.php");
+include ("$rootpath/include/jpgraph/jpgraph_line.php");
 
 // Create the graph. These two calls are always required
 $graph = new Graph(640,350);
@@ -85,35 +85,37 @@ $graph->xaxis->title->SetFont(FF_FONT1,FS_BOLD,12);
 $graph->legend->SetLayout(LEGEND_HOR);
 $graph->legend->Pos(0.10, 0.06, "right", "top");
 
-if ($havedata['nstl11']) {
 // Create the linear plot
 $a = $rad['nstl11'];
 $lineplot=new LinePlot($a);
 $lineplot->SetColor("red");
-$lineplot->SetLegend($nstl11_lbl);
+$lineplot->SetLegend("NSTL11");
 $lineplot->SetWeight(2);
 $graph->Add($lineplot);
-}
 
-if ($havedata['nstl10']) {
-// Create the linear plot
 $b = $rad['nstl10'];
 $lineplot2=new LinePlot($b);
 $lineplot2->SetColor("blue");
-$lineplot2->SetLegend($nstl10_lbl);
+$lineplot2->SetLegend("NSTL10");
 $lineplot2->SetWeight(2);
 $graph->Add($lineplot2);
-}
 
-if ($havedata['nspr']) {
 // Create the linear plot
-$b = $rad['nspr'];
+$b = $rad['nstlnsp'];
 $lineplot3=new LinePlot($b);
 $lineplot3->SetColor("black");
-$lineplot3->SetLegend("Over Prairie");
+$lineplot3->SetLegend("NSPR");
 $lineplot3->SetWeight(2);
 $graph->Add($lineplot3);
-}
+
+// Create the linear plot
+$b = $rad['nstl30ft'];
+$lineplot4=new LinePlot($b);
+$lineplot4->SetColor("green");
+$lineplot4->SetLegend("NSTL30FT");
+$lineplot4->SetWeight(2);
+$graph->Add($lineplot4);
+
 
 // Display the graph
 $graph->Stroke();
