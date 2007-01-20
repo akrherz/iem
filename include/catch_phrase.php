70f364b2d742d50594981e37830cfc16c90e8eca
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2007-01-20 14:19:31 +0000

    New header stuff!

---
+++ include/catch_phrase.php
@@ -0,0 +1,15 @@
+<?php
+/* Catch phrases, hehe */
+$phrases = Array(
+ "Over 1 billion observations collected",
+ "View images from <a href=\"$rooturl/current/camera.phtml\">20+ webcameras</a>",
+ "Plot carbon and radiation <a href=\"$rooturl/nstl_flux/plots.phtml\">flux time series</a>",
+ "<a href=\"$rooturl/climodat/\">Climodat</a> reports are a great source of Iowa climate data",
+ "The <a href=\"$rooturl/GIS/\">GIS</a> page has everything GIS on the IEM",
+ "<a href=\"$rooturl/mailman/listinfo/dailyb\">Sign up</a> for the <a href=\"$rooturl/data/iemdb.txt\">IEM Daily Bulletin</a>",
+ "View live <a href=\"$rooturl/plotting/snet/1station_1min.php\">1 minute timeseries</a> from SchoolNet",
+ "Relax and enjoy some <a href=\"$rooturl/cool/\">Cool Stuff</a>",
+ "Trying to find station locations? Try the <a href=\"$rooturl/sites/networks.php\">network tables</a> app",
+ "View current <a href=\"$rooturl/roads/\">Iowa Road Conditions</a>",
+
+); ?>
