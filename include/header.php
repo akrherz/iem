1d6af3f752b9e55b3bd049cb681cf89b571fbb27
akrherz <akrherz@iastate.edu>
2007-01-20 14:19:31 +0000

    New header stuff!

--- include/header.php
+++ include/header.php
@@ -1,23 +1,22 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+<?php
+  include("$rootpath/include/catch_phrase.php");
+  srand ((float) microtime() * 10000000);
+  $t = array_rand($phrases);
+  $phrase = $phrases[$t];
+
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
 <head>
-<title><?php echo isset($TITLE) ? $TITLE: "Iowa Environmental Mesonet"; ?></title>
- <link rel="stylesheet" type="text/css" href="<?php echo $rooturl; ?>/css/main.css" />
- <link rel="stylesheet" type="text/css" href="<?php echo $rooturl; ?>/css/main-theme.css" />
- <link rel="alternate stylesheet" type="text/css" media="screen" href="<?php echo $rooturl; ?>/css/red.css"
-title="red" />
- <link rel="alternate stylesheet" type="text/css" media="screen" href="<?php echo $rooturl; ?>/css/slashdot.css" title="slashdot" />
- <script type="text/javascript" src="<?php echo $rooturl; ?>/js/styleswitcher.js"></script>
+ <title><?php echo isset($TITLE) ? $TITLE: "Iowa Environmental Mesonet"; ?></title>
+ <link rel="stylesheet" type="text/css" href="<?php echo $rooturl; ?>/css/main.css?v=0.0.1" />
  <?php if (isset($REFRESH)){ echo $REFRESH; } ?>
  <?php if (isset($HEADEXTRA)){ echo $HEADEXTRA;} ?>
 </head>
 <body <?php if (isset($BODYEXTRA)){ echo $BODYEXTRA;} ?>>
 <?php include("$rootpath/include/webring.html"); ?>
 <div id="iem-main">
- 
 <div id="iem-header">
- 
 <div id="iem-header-logo">
 <a href="<?php echo $rooturl; ?>/"><img src="<?php echo $rooturl; ?>/images/logo_small.gif" alt="IEM" /></a>
 </div>
@@ -28,24 +27,20 @@ title="red" />
 </div>
                                                                                 
 <div id="iem-header-items">
-<b>Select Theme:</b>
-<br /><a title="Small Text Size" class="navSmallText" href="#" onclick="setActiveStyleSheet('default'); return false">Default</a>
-| <a title="Small Text Size" class="navSmallText" href="#" onclick="setActiveStyleSheet('red'); return false">Red</a>
-| <a title="Small Text Size" class="navSmallText" href="#" onclick="setActiveStyleSheet('slashdot'); return false">Slashdot</a>
+<i><?php echo $phrase; ?></i>
 </div>
-                                                                                
-<div class="iem-menu">
- <a href="<?php echo $rooturl; ?>/archive/">Archive</a>
- &middot; <a href="<?php echo $rooturl; ?>/current/">Current</a>
- &middot; <a href="<?php echo $rooturl; ?>/climate/">Climatology</a>
- &middot; <a href="<?php echo $rooturl; ?>/climodat/">Climodat</a>
- &middot; <a href="<?php echo $rooturl; ?>/cool/">Cool Stuff</a>
- &middot; <a href="<?php echo $rooturl; ?>/sites/locate.php">IEM Sites</a>
- &middot; <a href="<?php echo $rooturl; ?>/GIS/">GIS</a>
- &middot; <a href="<?php echo $rooturl; ?>/info.php">Info</a>
- &middot; <a href="<?php echo $rooturl; ?>/QC/">Quality Control</a>
-</div>
+
+<ul id="iemmenu">
+<li><a href="<?php echo $rooturl; ?>/archive/">&middot;Archive</a></li>
+<li><a href="<?php echo $rooturl; ?>/current/">&middot;Current</a></li>
+<li><a href="<?php echo $rooturl; ?>/climate/">&middot;Climatology</a></li>
+<li><a href="<?php echo $rooturl; ?>/climodat/">&middot;Climodat</a></li>
+<li><a href="<?php echo $rooturl; ?>/cool/">&middot;Cool Stuff</a></li>
+<li><a href="<?php echo $rooturl; ?>/sites/locate.php">&middot;IEM Sites</a></li>
+<li><a href="<?php echo $rooturl; ?>/GIS/">&middot;GIS</a></li>
+<li><a href="<?php echo $rooturl; ?>/info.php">&middot;Info</a></li>
+<li><a href="<?php echo $rooturl; ?>/QC/">&middot;Quality Control</a></li>
+</ul> 
+ 
 </div><!-- End of iem-header -->
-<?php 
-include("$rootpath/include/warnings/inc.phtml"); ?>
 <div id="iem-content">
