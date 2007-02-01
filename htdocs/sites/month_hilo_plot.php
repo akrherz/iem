c37a913504a7b85cc0812b04d19e69c050d35de2
akrherz <akrherz@iastate.edu>
2007-02-01 12:44:09 +0000

    Hmmm, fix the year!

--- htdocs/sites/month_hilo_plot.php
+++ htdocs/sites/month_hilo_plot.php
@@ -11,6 +11,7 @@
  /* Call with year and month, if not, then current! */
  $month = isset($_GET["month"]) ? intval($_GET["month"]): date("m");
  $year = isset($_GET["year"]) ? intval($_GET["year"]): date("Y");
+$feature = isset($_GET["feature"]);
 
  /* Get high and low temps for the past 7 days */
 $sql = "SELECT day, max_tmpf, min_tmpf from summary_$year WHERE 
@@ -24,8 +25,6 @@ $lows = Array();
 $xlabels = Array();
 
 for( $i=0; $row = @pg_fetch_array($rs,$i); $i++) {
-  $ts = strtotime($row["day"]);
-  $xlabels[] = date("m/d", $ts);
   $highs[] = $row["max_tmpf"];
   $lows[] = $row["min_tmpf"];
 }
@@ -38,7 +37,7 @@ $db = iemdb("coop");
 $sqlDate = sprintf("2000-%s", date("m-d") );
 
 $sql = "SELECT valid, high, low from climate WHERE station = '$climate_site'
-        and extract(month from valid) = $month ORDER by valid ASC";
+        and extract(month from valid) = $month  ORDER by valid ASC";
 
 $rs = pg_query($db, $sql);
 
@@ -46,6 +45,8 @@ $ahighs = Array();
 $alows = Array();
 
 for( $i=0; $row = @pg_fetch_array($rs,$i); $i++) {
+  $ts = strtotime($row["valid"]);
+  $xlabels[] = date("m/d", $ts);
   $ahighs[] = $row["high"];
   $alows[] = $row["low"];
 }
@@ -62,15 +63,23 @@ $a1 = min($alows);
 $a2 = max($highs);
 $a3 = max($ahighs);
 
-$graph = new Graph(640,360,"auto");
+$height = 360;
+$width = 640;
+if ($feature)
+{
+  $height = 200;
+  $width = 320;
+}
+$graph = new Graph($width,$height);
+
 $graph->SetScale("textlin", min($a0,$a1)-4, max($a2,$a3)+2);
 $graph->SetMarginColor('white');
 
 $graph->ygrid->SetFill(true,'#EFEFEF@0.5','#BBCCFF@0.5');
 $graph->xgrid->Show();
 
-$dstr = date("M Y", $ts);
-$graph->title->Set("$dstr Hi/Lo Temps for ". $metadata["city"]);
+$dstr = date("M", $ts);
+$graph->title->Set("$dstr $year Hi/Lo Temps for ". $metadata["city"]);
 $graph->SetMargin(40,5,50,40);
 
 $graph->xaxis->SetTickLabels($xlabels);
@@ -81,12 +90,12 @@ $graph->yscale->SetGrace(5);
 $graph->yaxis->SetTitle("Temperature [F]");
 
 $graph->legend->SetLayout(LEGEND_HOR);
-$graph->legend->Pos(0.01,0.08,"left","top");
+$graph->legend->Pos(0.01,0.09,"left","top");
 
 $bplot1 = new BarPlot($highs);
 $bplot1->SetFillColor('red');
-$bplot1->SetLegend("High Temp");
-$bplot1->value->Show();
+$bplot1->SetLegend("High");
+$bplot1->value->Show(! $feature);
 $bplot1->value->SetFormat('%d');
 $bplot1->value->SetColor('black');
 $bplot1->value->SetAngle(0);
@@ -96,8 +105,8 @@ $bplot1->SetWidth(0.7);
 
 $bplot2 = new BarPlot($lows);
 $bplot2->SetFillColor('blue');
-$bplot2->SetLegend("Low Temp");
-$bplot2->value->Show();
+$bplot2->SetLegend("Low");
+$bplot2->value->Show(! $feature);
 $bplot2->value->SetFormat('%d');
 $bplot2->value->SetColor('black');
 $bplot2->value->SetAngle(0);
