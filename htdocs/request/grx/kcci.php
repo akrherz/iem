e316fc177a3b6cac25601816c393cc4a9e4a146b
akrherz <akrherz@iastate.edu>
2007-07-05 18:35:29 +0000

    babba

--- htdocs/request/grx/kcci.php
+++ htdocs/request/grx/kcci.php
@@ -4,7 +4,7 @@ header("Content-type: text/plain");
  // Inspiration: http://grlevel3.tornadocentral.com/metars.php?state=IA
 
 echo 'Title: KCCI SchoolNet8
-Refresh: 20 
+Refresh: 1 
 Color: 200 200 255
 IconFile: 1, 18, 32, 2, 31, "http://www.tornadocentral.com/grlevel3/windbarbs.png" 
 IconFile: 2, 15, 15, 8, 8, "http://www.tornadocentral.com/grlevel3/cloudcover.png" 
@@ -21,28 +21,28 @@ $snet = $iem->getNetwork("KCCI");
 
 function s2icon($s)
 {
-  if ($s < 2.5) return "1,20";
+  if ($s < 2.5) return "1,22";
   if ($s < 5) return "1,1";
-  if ($s < 7.5) return "1,2";
-  if ($s < 10) return "1,3";
-  if ($s < 12.5) return "1,4";
-  if ($s < 15) return "1,5";
-  if ($s < 17.5) return "1,6";
-  if ($s < 20) return "1,7";
+  if ($s < 10) return "1,2";
+  if ($s < 15) return "1,3";
+  if ($s < 20) return "1,4";
+  if ($s < 25) return "1,5";
+  if ($s < 30) return "1,6";
+  if ($s < 35) return "1,7";
 
-  if ($s < 22.5) return "1,8";
-  if ($s < 25) return "1,9";
-  if ($s < 27.5) return "1,10";
-  if ($s < 30) return "1,11";
-  if ($s < 32.5) return "1,12";
-  if ($s < 35) return "1,13";
-  if ($s < 37.5) return "1,14";
+  if ($s < 40) return "1,8";
+  if ($s < 45) return "1,9";
+  if ($s < 50) return "1,10";
+  if ($s < 55) return "1,11";
+  if ($s < 60) return "1,12";
+  if ($s < 65) return "1,13";
+  if ($s < 70) return "1,14";
 
-  if ($s < 40) return "1,15";
-  if ($s < 42.5) return "1,16";
-  if ($s < 45) return "1,17";
-  if ($s < 47.5) return "1,18";
-  if ($s < 50) return "1,19";
+  if ($s < 75) return "1,15";
+  if ($s < 80) return "1,16";
+  if ($s < 85) return "1,17";
+  if ($s < 80) return "1,18";
+  if ($s < 100) return "1,19";
   return "1,20";
 
 }
