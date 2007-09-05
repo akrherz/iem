47950713fd948f8b228fd0a7a6d3c89cf7bfc88c
akrherz <akrherz@iastate.edu>
2007-09-05 12:06:38 +0000

    104

--- include/feature.php
+++ include/feature.php
@@ -60,7 +60,7 @@ $s .= "</table>";
 if (getenv("REMOTE_ADDR") == "205.241.141.66" )
 {
  $s = "<img src=\"images/smokey_1229.jpg\" style=\"float: left; margin: 5px;\">
-Smokey, muah! <br /> &nbsp; &nbsp; &nbsp; &nbsp; 103 weeks now!!!!  Darly missed a week and missed smokey very much while a way.  I wish we could spend more time together, but its okay.  1 more week to go, but 7 days at a time first.  I love you very much. <br />&nbsp; &nbsp; &nbsp; &nbsp;   &nbsp; &nbsp; &nbsp; &nbsp;  love, darly";
+Smokey, muah! <br /> &nbsp; &nbsp; &nbsp; &nbsp; 104 weeks now!!!!  Hehe, any number divisible by 52 is a good number!  Looking back, sometimes periods of time seem to go by quickly, but I think our time together has gone by at a good pace. I still get as excited to see smokey as the first time I saw you with that fancy shirt one.  I love you very much, Happy Anniversary! <br />&nbsp; &nbsp; &nbsp; &nbsp;   &nbsp; &nbsp; &nbsp; &nbsp;  love, darly";
 }
 
 echo $s;
