4aae651d4ed30317a06427c11860bfa94eaf3b4e
akrherz <akrherz@iastate.edu>
2007-12-05 15:04:15 +0000

    Nov data

--- include/constants.php
+++ include/constants.php
@@ -2,6 +2,6 @@
 
 $coop_archive_end = mktime(0,0,0,10,1,2007);
 $awos_archive_end = mktime(0,0,0,11,1,2007);
-$asos_archive_end = mktime(0,0,0,10,1,2007);
+$asos_archive_end = mktime(0,0,0,12,1,2007);
 
 ?>
