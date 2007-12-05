717cd728f0a88e907fb797c77cdfea5216fe83bc
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2007-12-05 13:05:00 +0000

    117

--- include/feature.php
+++ include/feature.php
@@ -60,7 +60,7 @@ $s .= "</table>";
 if (getenv("REMOTE_ADDR") == "205.241.141.66" )
 {
  $s = "<img src=\"images/smokey_1007.jpg\" style=\"float: left; margin: 5px;\">
-Smokey, muah! <br /> &nbsp; &nbsp; &nbsp; &nbsp; 116 weeks now!!!! I had a great time with you over turkey break smokey!  I hope that there are many more trips to your uncle's house in the future! I love you very much, Happy Anniversary! <br />&nbsp; &nbsp; &nbsp; &nbsp;   &nbsp; &nbsp; &nbsp; &nbsp;  love, darly";
+Smokey, muah! <br /> &nbsp; &nbsp; &nbsp; &nbsp; 117 weeks now!!!!  Smokey be leaving me next wednesday! :(  Hehe, its okay.  I am glad smokey is going to make it thru the semester and be able to spend some time with her family and friends in Verginya. I love you very much, Happy Anniversary! <br />&nbsp; &nbsp; &nbsp; &nbsp;   &nbsp; &nbsp; &nbsp; &nbsp;  love, darly";
 
   $s .= "<br style=\"clear: right;\" /><b>Rate Feature:</b> <a href=\"$rooturl/index.phtml?feature_good\">Good</a> ($good votes) or <a href=\"$rooturl/index.phtml?feature_bad\">Bad</a> ($bad votes) &nbsp; &nbsp;<a href=\"$rooturl/onsite/features/past.php\">Past Features</a>";
 }
