13e678ac08d2140b5f67f0d8c2ed1e2a77a32a10
akrherz <akrherz@iastate.edu>
2007-12-11 14:34:37 +0000

    add normal curves

--- htdocs/plotting/coop/spread.php
+++ htdocs/plotting/coop/spread.php
@@ -1,7 +1,7 @@
 <?php
 include("../../../config/settings.inc.php");
 include("$rootpath/include/database.inc.php");
-$ts = isset($_GET['ts']) ? strtotime($_GET['ts']) : date("%Y-%m-%d");
+$ts = isset($_GET['ts']) ? strtotime($_GET['ts']) : time();
 $dts = strftime("%m%d", $ts);
 $plotts = strftime("%d %b", $ts);
 
@@ -98,6 +98,48 @@ foreach ($xlabel as $key => $value){
   $i++;
 } 
 
+/* Now, lets compute normal curves! */
+function average($array){
+    $sum   = array_sum($array);
+    $count = count($array);
+    return $sum/$count;
+}
+function deviation ($array, $avg){
+    //$avg = average($array);
+    foreach ($array as $value) {
+        $variance[] = pow($value-$avg, 2);
+    }
+    $deviation = sqrt(average($variance));
+    return $deviation;
+}
+function normal_sample($x, $avg, $sd){
+  $expn = (0 - pow(($x - $avg),2)) / (2.0 * $sd * $sd);
+  return 1 / ($sd * sqrt(2 * pi())) * exp($expn);
+}
+
+$query3 = "SELECT high, low from alldata 
+   WHERE sday = '$dts' and high > -50 and high != low";
+$result = pg_exec($connection, $query3);
+$highs = Array();
+$lows = Array();
+for( $i=0; $row = @pg_fetch_array($result,$i); $i++) {
+  $highs[] = $row["high"];
+  $lows[] = $row["low"];
+}
+
+$h_avg = average($highs);
+$cnt = sizeof($highs);
+$l_avg = average($lows);
+$h_std = deviation($highs, $h_avg);
+$l_std = deviation($lows, $l_avg);
+$h_norm = Array();
+$l_norm = Array();
+foreach ($xl as $k => $x){
+  //echo "<br>". $x ."--". $h_avg ."--". $h_std ."--". normal_sample($x, $h_avg, $h_std);
+  $h_norm[$k] = normal_sample($x, $h_avg, $h_std) * $cnt;
+  $l_norm[$k] = normal_sample($x, $l_avg, $l_std) * $cnt;
+}
+
 pg_close($connection);
 
 include ("$rootpath/include/jpgraph/jpgraph.php");
@@ -115,7 +157,7 @@ $graph->img->SetMargin(60,60,65,50);
 $graph->xaxis->SetFont(FF_FONT1,FS_BOLD);
 $graph->xaxis->SetTickLabels($xl);
 $graph->xaxis->SetLabelAngle(90);
-$graph->title->Set("Daily High and Low Temperatures for $plotts");
+$graph->title->Set("Iowa Daily High and Low Temperatures for $plotts");
 
 $graph->title->SetFont(FF_FONT1,FS_BOLD,16);
 
@@ -133,23 +175,19 @@ $graph->xaxis->title->SetFont(FF_FONT1,FS_BOLD,12);
 $graph->xaxis->SetPos("min");
 $graph->xaxis->SetTitleMargin(15);
 
-$graph->legend->Pos(0.2, 0.09);
+$graph->legend->Pos(0.2, 0.08);
 $graph->legend->SetLayout(LEGEND_HOR);
 
-/*
-// Create the linear plot
-$bp0=new BarPlot($y1);
-$bp0->SetFillColor("red");
-$bp0->SetLegend("High Temp (F)");
+$s = sprintf("High Temperature\n  MEAN: %.1f\n  MEDIAN: %.0f\n  STDDEV: %.1f\nLow Temperature\n  MEAN: %.1f\n  MEDIAN: %.0f\n  STDDEV: %.1f\n", $h_avg, $xl[$hm], $h_std, $l_avg, $xl[$lm], $l_std);
 
-// Create the linear plot
-$bp1=new BarPlot($y2);
-$bp1->SetFillColor("blue");
-$bp1->SetLegend("Low Temp (F)");
+$t1 = new Text($s);
+$t1->Pos(75,75);
+$t1->SetFont(FF_FONT1,FS_NORMAL);
+$t1->SetBox("white","black",true);
+$t1->ParagraphAlign("left");
+$t1->SetColor("black");
+$graph->AddText($t1);
 
-$gbplot = new GroupBarPlot(array($bp0,$bp1));
-$gbplot->SetWidth(0.9);
-*/
 
 $l1=new LinePlot($y1);
 $l1->SetColor("red");
@@ -163,26 +201,42 @@ $l2->SetWeight(2);
 $l2->SetLegend("Low Temp");
 $l2->SetStyle('dashed'); 
 
+$nl1=new LinePlot($h_norm);
+$nl1->SetColor("red@0.7");
+$nl1->SetWeight(2);
+//$nl1->SetLegend("High Temp");
+//$nl1->SetStyle('dashed'); 
+//$l1->SetFillColor('red@0.7');
+//$l1->SetColor('red@0.8');
 
+$nl2=new LinePlot($l_norm);
+$nl2->SetColor("blue@0.7");
+$nl2->SetWeight(2);
+//$nl2->SetLegend("Low Temp");
+//$nl2->SetStyle('dashed'); 
+//$l2->SetFillColor('skyblue@0.7');
+//$l2->SetColor('navy@0.8');
 
 $lp1=new LinePlot($cy1);
 $lp1->SetColor("red");
 $lp1->SetLegend("CDF (high temp)");
-$lp1->AddArea($h5,$h95,LP_AREA_NOT_FILLED,"lightred");
-$lp1->AddArea($hm,$hm,LP_AREA_FILLED,"lightred");
+//$lp1->AddArea($h5,$h95,LP_AREA_NOT_FILLED,"lightred");
+//$lp1->AddArea($hm,$hm,LP_AREA_FILLED,"lightred");
 $lp1->SetWeight(2);
 
 $lp2=new LinePlot($cy2);
 $lp2->SetColor("blue");
 $lp2->SetLegend("CDF (low temp)");
-$lp2->AddArea($l5,$l95,LP_AREA_NOT_FILLED,"lightblue");
-$lp2->AddArea($lm,$lm,LP_AREA_FILLED,"lightblue");
+//$lp2->AddArea($l5,$l95,LP_AREA_NOT_FILLED,"lightblue");
+//$lp2->AddArea($lm,$lm,LP_AREA_FILLED,"lightblue");
 $lp2->SetWeight(2);
 
 
 // Add the plot to the graph
 //$graph->Add($gbplot);
 
+$graph->AddY2($nl1);
+$graph->AddY2($nl2);
 $graph->AddY2($l1);
 $graph->AddY2($l2);
 
