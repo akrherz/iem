a76f84501568edfe1dd4dd301353ad89ecdd7743
akrherz <akrherz@iastate.edu>
2008-01-10 15:49:33 +0000

    better logo

--- htdocs/schoolnet/switchbar.php
+++ htdocs/schoolnet/switchbar.php
@@ -3,7 +3,7 @@
 <tr>
    <td class="kcci" align="center"><a href="<?php echo $link; ?>?ntv=KCCI" class="tabcommands"><img src="<?php echo $rooturl; ?>/schoolnet/images/kcci8.jpg" border="0"> &nbsp; <b>SchoolNet8</b></a></td>
 
-    <td class="kelo" align="center"><a href="<?php echo $link; ?>?ntv=KELO" class="tabcommands"><img src="<?php echo $rooturl; ?>/schoolnet/images/kelo.jpg" border="0"> &nbsp; <b>WeatherNet</b></a></td>
+    <td class="kelo" align="center"><a href="<?php echo $link; ?>?ntv=KELO" class="tabcommands"><img src="<?php echo $rooturl; ?>/schoolnet/images/kelo.png" border="0"> &nbsp; <b>WeatherNet</b></a></td>
 
     <td class="kimt" align="center"><a href="<?php echo $link; ?>?ntv=KIMT" class="tabcommands"><img src="<?php echo $rooturl; ?>/schoolnet/images/kimt_logo.png" border="0"> &nbsp; <b>StormNet</b></a></td>
 
