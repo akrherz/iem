133b4b4b28b4c0b463b1bba3993f85c92e241855
akrherz <akrherz@iastate.edu>
2008-01-10 17:02:26 +0000

    Yummy

--- include/constants.php
+++ include/constants.php
@@ -1,7 +1,7 @@
 <?php
 
-$coop_archive_end = mktime(0,0,0,10,1,2007);
-$awos_archive_end = mktime(0,0,0,12,1,2007);
+$coop_archive_end = mktime(0,0,0,11,1,2007);
+$awos_archive_end = mktime(0,0,0,1,1,2008);
 $asos_archive_end = mktime(0,0,0,12,1,2007);
 
 ?>
