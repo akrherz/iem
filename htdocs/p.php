37074b91ae66327f441e738c812516b271313b65
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2008-01-28 00:53:55 +0000

    Add support for product_id stuff

--- htdocs/p.php
+++ htdocs/p.php
@@ -2,18 +2,26 @@
 include("../config/settings.inc.php");
 include("$rootpath/include/database.inc.php");
 
-$id = isset($_GET['id']) ? intval($_GET['id']) : die('no id');
+$id = isset($_GET['id']) ? intval($_GET['id']) : "";
+$pid = isset($_GET['pid']) ? substr($_GET['pid'],0,32) : "";
+if ($id == "" && $pid == "") die();
 
 $conn = iemdb("postgis");
 
-pg_query($conn, "update text_products SET reads = reads + 1 WHERE id = $id");
-$sql = sprintf('SELECT product from text_products WHERE id = %s', $id);
+if ($id != "") {
+ pg_query($conn, "update text_products SET reads = reads + 1 WHERE id = $id");
+ $sql = sprintf('SELECT product from text_products WHERE id = %s', $id);
+} else {
+ pg_query($conn, "update text_products SET reads = reads + 1 WHERE product_id = '$pid'");
+ $sql = sprintf("SELECT product from text_products WHERE product_id = '%s'", $pid);
+}
 
 $rs = pg_query($conn, $sql);
-
-$row = pg_fetch_array($rs, 0);
-
-echo "<pre>". $row["product"] ."</pre>";
+header("Content-type: text/plain");
+for ($i=0; $row = @pg_fetch_array($rs, $i); $i++)
+{
+  echo  $row["product"] ."\n";
+}
 
 ?>
 
