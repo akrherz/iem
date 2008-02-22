be5c86c17c8f040b40f29a15ef1505c21d82085a
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2008-02-22 20:02:06 +0000

    clean up footer

--- include/footer.php
+++ include/footer.php
@@ -1,27 +1,20 @@
 </div><!-- End #iem-content -->
-<br clear="both" />
 <div id="iem-footer">
-<table class="footer"><tr><td>
-                                                                                
-<div class="iem-footer-text">
+
+<div style="float: right;">
+ <form method="get" action="http://www.google.com/search" name="search">
+ <b>Search Site with Google:</b><br />
+ <input value="mesonet.agron.iastate.edu" name="sitesearch" type="hidden" />
+<input type="text" size="15" name="q" /><input type="submit" value="Search" /></p></form>
+</div>
+
 Copyright &copy; 2001-2008, Iowa State University of Science and Technology.
-All rights reserved.
-<br />
-                                                                                
+<br />All rights reserved.
  <a href="<?php echo $rooturl; ?>/help/abbreviations.php">abbreviations</a>
  &middot; <a href="<?php echo $rooturl; ?>/info/contacts.php">contact us</a>
  &middot; <a href="<?php echo $rooturl; ?>/disclaimer.php">disclaimer</a>
  &middot; <a href="<?php echo $rooturl; ?>/siteindex/">site index</a>
- </div></td><td>
-                                                                                
- <form method="get" action="http://www.google.com/search" name="search">
- <p> <b>Search Site with Google:</b><br />
- <input value="mesonet.agron.iastate.edu" name="sitesearch" type="hidden" />
-<input type="text" size="15" name="q" /><input type="submit" value="Search" /></p></form>
-</td></tr></table>
 </div>
-                                                                                
-                                                                                
 </div> <!-- End of iem-main -->
 <script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
 </script>
@@ -31,4 +24,3 @@ urchinTracker();
 </script>
 </body>
 </html>
-
