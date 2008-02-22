63076aac6c5dcc3fd9b8d21f496789f5b07d143a
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2008-02-22 03:25:07 +0000

    new design test

---
+++ htdocs/test.php
@@ -0,0 +1,135 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="http://mesonet.agron.iastate.edu/css/main.css?v=0.0.1" />
+<style>
+#iem_nav {
+    border: 1px solid #ccc;
+    border-top: 2px solid #ccc;
+	height: 24px;
+	clear: both;
+	margin: 0px 0px 0px 0px;
+	padding: 0px;
+    background-color: #f0f4f5;
+	}
+#iem_nav ul {
+	margin: 0px 0px 0px 5px;
+	padding: 0px;
+	list-style-type: none;
+	}
+#iem_nav ul li {
+	margin: 0;
+	display: block;
+	float: left;
+	white-space: nowrap;
+	padding: 4px 12px;
+	background-image: none;
+	}
+#iem_nav ul li a, 
+#iem_nav ul li a:visited  {
+	margin: 0px 0px 0px 0px;
+	color: #f00;
+	text-decoration: none;
+	}
+#iem_nav ul li a:hover, 
+#iem_nav ul li a:active  {
+	text-decoration: underline;
+	}
+#iem_nav ul li.selected {
+	background-color: white;
+    border: 2px solid #ccc;
+	border-bottom: 2px solid #fff;
+	padding: 3px 12px;
+	}
+/* Subnav bar! */
+#iem_subnav {
+	height: 30px;
+	display: block;
+	float: left;
+	clear: both;
+	width: 100%;
+	margin: 0;
+	padding: 0;
+	overflow: hidden;
+    background-color: #fff;
+    border: 1px solid #ccc;
+    border-top: 0px;
+	}
+#iem_subnav ul {
+	margin: -10px 0 0 2px;
+	padding: 0;
+	list-style-type: none;
+	}
+#iem_subnav ul li {
+	float: left;
+	margin: 7px 0 0 0;
+	padding: 10px 2px 10px 0;
+	color: #333;
+	font-size: 9pt;
+	}
+#iem_subnav ul li a, 
+#iem_subnav ul li a:visited  {
+	margin: 0 0 0 0;
+	padding: 10px 11px 10px 11px;
+	color: #333;
+	font-size: 0.9em;
+	text-decoration: none;
+	}
+#iem_subnav ul li a:hover, 
+#iem_subnav ul li a:active  {
+	text-decoration: underline;
+	}
+#iem_subnav ul li.selected {
+	font-weight: bold;
+	}
+#iem_header_logo img {
+  border: 0px;
+}
+</style>
+</head>
+<body>
+
+<div id="iem_header">
+
+<div id="iem_header_logo">
+<a href="http://mesonet.agron.iastate.edu/"><img src="http://mesonet.agron.iastate.edu/images/logo_small.gif" alt="IEM" /></a>
+</div>
+                                                                                
+<div id="iem-header-title">
+<h3>Iowa Environmental Mesonet</h3>
+<h4>Iowa State University Department of Agronomy</h4>
+
+</div>
+                                                                                
+<div id="iem-header-items">
+<i>View live <a href="http://mesonet.agron.iastate.edu/plotting/snet/1station_1min.php">1 minute timeseries</a> from SchoolNet</i>
+</div>
+
+ <div id="iem_nav">
+  <ul>
+   <li id="iem_nav_archive"><a href="">Archive</a></li>
+   <li id="iem_nav_current" class="selected"><a href="">Current</a></li>
+   <li id="iem_nav_climatology"><a href="">Climatology</a></li>
+   <li id="iem_nav_climodat"><a href="">Climodat</a></li>
+   <li id="iem_nav_cool"><a href="">Cool Stuff</a></li>
+   <li id="iem_nav_sites"><a href="">IEM Sites</a></li>
+   <li id="iem_nav_gis"><a href="">GIS</a></li>
+   <li id="iem_nav_info"><a href="">Info</a></li>
+   <li id="iem_nav_quality"><a href="">Quality Control</a></li>
+  </ul>
+ </div>
+ <div id="iem_subnav">
+  <ul>
+    <li id="iem_subnav01" class="selected"><a href="">Bah</a></li>
+    <li id="iem_subnav02"><a href="">Bah2</a></li>
+    <li id="iem_subnav03"><a href="">Bah3</a></li>
+    <li id="iem_subnav04"><a href="">Bah4</a></li>
+  </ul>
+ </div>
+
+</div>
+
+<h3 class="subtitle">Ba ha ha</h3>
+<img src="http://mesonet.agron.iastate.edu/data/mesonet.gif">
+
+</body>
+</html>
