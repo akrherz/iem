01fccbac35ebb9cba2aeddd07af08ec4426db24d
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2008-02-27 23:26:27 +0000

    Ext 2.0.2

--- htdocs/ext/resources/css/core.css
+++ htdocs/ext/resources/css/core.css
@@ -1,6 +1,6 @@
 /*
- * Ext JS Library 2.0
- * Copyright(c) 2006-2007, Ext JS, LLC.
+ * Ext JS Library 2.0.2
+ * Copyright(c) 2006-2008, Ext JS, LLC.
  * licensing@extjs.com
  * 
  * http://extjs.com/license
