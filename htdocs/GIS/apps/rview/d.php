b7e6d62799a9a0bfe3a9d9018e12cc14dee24d6a
akrherz <akrherz@iastate.edu>
2008-02-27 23:20:34 +0000

    hmmm

--- htdocs/GIS/apps/rview/d.php
+++ htdocs/GIS/apps/rview/d.php
@@ -3,15 +3,15 @@
   <script type="text/javascript" src="../../../ext/adapter/ext/ext-base.js"></script>
   <script type="text/javascript" src="../../../ext/ext-all.js"></script>
   <link rel="stylesheet" type="text/css" href="../../../ext/resources/css/ext-all.css" />
-</head>
-<body>
-<pre>
-<?= $_POST["data"]; ?>
-</pre>
 <script>
 Ext.onReady(function(){
   window.print();
 });
 </script>
+</head>
+<body>
+<pre>
+<?= $_POST["data"]; ?>
+</pre>
 </body>
 </html>
