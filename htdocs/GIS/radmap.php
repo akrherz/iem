8f89d1cf5d2901319e0f50e3f6a5d1fe5c49a7ac
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2008-04-15 21:05:42 +0000

    more features

--- htdocs/GIS/radmap.php
+++ htdocs/GIS/radmap.php
@@ -2,15 +2,37 @@
 /* Tis my job to produce pretty maps with lots of options :) */
 include("../../config/settings.inc.php");
 
-$epsg = 2163;
+/* Straight CGI Butter */
+$sector = isset($_GET["sector"]) ? $_GET["sector"] : "iem";
 
+$sectors = Array(
+ "iem" => Array("epsg" => 4326, "ext" => Array(-100.0, 38.5, -88.0, 46.5)),
+ "conus" => Array("epsg" => 2163, 
+         "ext" => Array(-2110437, -2251067, 2548326, 1239063)),
+ "texas" => Array("epsg" => 2163, 
+         "ext" => Array(-532031.375, -2133488,723680.125, -959689.625)),
+);
+
+/* Could define a custom box */
+if (isset($_GET["bbox"]))
+{
+  $sector = "custom";
+  $bbox = isset($_GET["bbox"]) ? explode(",",$_GET["bbox"]) : die("No BBOX");
+  $sectors["custom"] = Array("epsg"=> 4326, "ext" => $bbox);
+}
+
+
+/* Lets Plot stuff already! */
 dl($mapscript);
 
-$map = ms_newMapObj($rootpath."/data/gis/base${epsg}.map");
+$mapFile = $rootpath."/data/gis/base".$sectors[$sector]['epsg'].".map";
+$map = ms_newMapObj($mapFile);
 $map->set("width", 640);
 $map->set("height",480);
-//US $map->setExtent(-2110437, -2251067, 2548326, 1239063);
-$map->setExtent(-632031.375, -2133488,623680.125, -959689.625);
+$map->setExtent($sectors[$sector]['ext'][0],
+                $sectors[$sector]['ext'][1], 
+                $sectors[$sector]['ext'][2],
+                $sectors[$sector]['ext'][3]);
 
 $img = $map->prepareImage();
 
@@ -22,7 +44,7 @@ $lakes = $map->getlayerbyname("lakes");
 $lakes->set("status", MS_ON);
 $lakes->draw($img);
 
-$counties = $map->getlayerbyname("counties");
+$counties = $map->getlayerbyname("uscounties");
 $counties->set("status", MS_ON);
 $counties->draw($img);
 
@@ -31,23 +53,20 @@ $states->set("status", MS_ON);
 $states->draw($img);
 
 /* Draw NEXRAD Layer */
-$radar = $map->getlayerbyname("radar");
+$radar = $map->getlayerbyname("nexrad_n0r");
 $radar->set("status", MS_ON);
 $radar->draw($img);
-$ts = @filemtime("/home/ldm/data/gis/images/4326/USCOMP/n0r_0.png");
-if ($ts == 0 || $ts == "") {
-  sleep(10);
-  $ts = @filemtime("/mesonet/data/gis/images/4326/USCOMP/n0r_0.png");
-}
-$d = strftime("%d %B %Y %-2I:%M %p %Z" ,  $ts + 15);
+
+/* Watch by County */
+$wbc = $map->getlayerbyname("watch_by_county");
+$wbc->set("status", MS_ON);
+$wbc->set("data", "g from (select phenomena, eventid, multi(geomunion(geom)) as g from warningsWHERE significance = 'A' and phenomena IN ('TO','SV') and issue <= now() and expire > now() GROUP by phenomena, eventid ORDER by phenomena ASC) as foo using SRID=4326 using unique phenomena");
+$wbc->draw($img);
+
 
 $sbw = $map->getlayerbyname("sbw");
 $sbw->set("status", MS_ON);
 $sbw->draw($img);
-
-$watches = $map->getlayerbyname("watches");
-$watches->set("status", MS_ON);
-$watches->draw($img);
 
 $bar640t = $map->getLayerByName("bar640t");
 $bar640t->set("status", 1);
@@ -58,8 +77,10 @@ $point = ms_newpointobj();
 $point->setXY(80, 12);
 $point->draw($map, $tlayer, $img, 0,"NEXRAD Base Reflectivity");
 $point->free();
+
 $point = ms_newpointobj();
 $point->setXY(80, 29);
+$d = strftime("%d %B %Y %-2I:%M %p %Z" ,  time());
 $point->draw($map, $tlayer, $img, 1,"$d");
 $point->free();
 
