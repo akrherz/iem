9e1eba38c2e46e6eeb6ba38c2850a7442f849681
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2008-04-17 21:19:31 +0000

    update

--- include/constants.php
+++ include/constants.php
@@ -1,6 +1,6 @@
 <?php
 
-$coop_archive_end = mktime(0,0,0,1,1,2008);
+$coop_archive_end = mktime(0,0,0,2,1,2008);
 $awos_archive_end = mktime(0,0,0,4,1,2008);
 $asos_archive_end = mktime(0,0,0,4,1,2008);
 
