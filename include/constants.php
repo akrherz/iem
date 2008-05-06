273a8329a20ae0c45d24f5cb6ff124240558ccb2
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2008-05-06 20:03:08 +0000

    updates, always updates

--- include/constants.php
+++ include/constants.php
@@ -1,7 +1,7 @@
 <?php
 
 $coop_archive_end = mktime(0,0,0,2,1,2008);
-$awos_archive_end = mktime(0,0,0,4,1,2008);
-$asos_archive_end = mktime(0,0,0,4,1,2008);
+$awos_archive_end = mktime(0,0,0,5,1,2008);
+$asos_archive_end = mktime(0,0,0,5,1,2008);
 
 ?>
