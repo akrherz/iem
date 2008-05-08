9fbbe5c17f16a381b9da384eaf0326d372a8eb06
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2008-05-08 17:45:40 +0000

    add something to manage google map keys

---
+++ include/google_keys.php
@@ -0,0 +1,10 @@
+<?php
+$GOOGLEKEYS = array(
+ "http://localhost/iem" => array(
+    "maplsr" => "ABQIAAAArXt77YptylBNPFEy0AgJxBQEqbOpq-UVZ0D5kHM5vpm679zg5RShssc7g46Xm0eM5Jlmt9GiE90Uxg",
+ ),
+ "http://mesonet.agron.iastate.edu" => array(
+    "maplsr" => "ABQIAAAArXt77YptylBNPFEy0AgJxBRyku_Vi9Erm7X8i1nZlN7yJeRsoxQB0vI1-xYP2QYOsxEhzNiTBaS-wQ",
+ ),
+);
+?>
