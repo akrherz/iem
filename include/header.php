c7b39de34f501f907967c0e87391e0631c23d12d
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2008-05-15 19:21:43 +0000

    help me out IE

--- include/header.php
+++ include/header.php
@@ -6,7 +6,7 @@
 
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html>
+<html xmlns="http://www.w3.org/1999/xhtml" <?php if (isset($GOOGLEKEYS)){echo "xmlns:v=\"urn:schemas-microsoft-com:vml\"";} ?>>
 <head>
  <title><?php echo isset($TITLE) ? $TITLE: "Iowa Environmental Mesonet"; ?></title>
  <link rel="stylesheet" type="text/css" href="<?php echo $rooturl; ?>/css/main.css?v=0.0.2" />
