8e94a51e301bf7fa901bed207388a32322a114ec
akrherz <akrherz@iastate.edu>
2008-05-15 16:16:48 +0000

    add some more

--- include/google_keys.php
+++ include/google_keys.php
@@ -2,9 +2,11 @@
 $GOOGLEKEYS = array(
  "http://localhost/iem" => array(
     "maplsr" => "ABQIAAAArXt77YptylBNPFEy0AgJxBQEqbOpq-UVZ0D5kHM5vpm679zg5RShssc7g46Xm0eM5Jlmt9GiE90Uxg",
+    "rview" => "ABQIAAAArXt77YptylBNPFEy0AgJxBSzesuTo04SfEVAA8aHdKEgKDyBjBQ8UJs1hA3bQvvlrK7hlIpffKLong",
  ),
  "http://mesonet.agron.iastate.edu" => array(
     "maplsr" => "ABQIAAAArXt77YptylBNPFEy0AgJxBRyku_Vi9Erm7X8i1nZlN7yJeRsoxQB0vI1-xYP2QYOsxEhzNiTBaS-wQ",
+    "rview" => "ABQIAAAArXt77YptylBNPFEy0AgJxBQvv4M5oTbRL27Li5BWPOWBa2LVfxRsHZ284Tw0TLrJslWUJWrr1cQSdA",
  ),
 );
 ?>
