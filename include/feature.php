479a92b0da5e11da3bbe5ea7dcdc7aa9b518ef50
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2008-05-21 04:49:46 +0000

    141

--- include/feature.php
+++ include/feature.php
@@ -78,7 +78,7 @@ $s .= "</table>";
 if (getenv("REMOTE_ADDR") == "205.241.141.66" )
 {
  $s = "<img src=\"images/pheasants.jpg\" style=\"float: left; margin: 5px;\">
-Smokey, muah! <br /> &nbsp; &nbsp; &nbsp; &nbsp; one-four-zero weeks now!!!! Hehe, divisible by lots of numbers!  I am so happy and so lucky to get to spend time with you smokey.  I am excited to get to go to the Cub games!  Go Cub and Go Smokey, hehe.  I love you very much. <br />&nbsp; &nbsp; &nbsp; &nbsp;   &nbsp; &nbsp; &nbsp; &nbsp;  love, darly";
+Smokey, muah! <br /> &nbsp; &nbsp; &nbsp; &nbsp; 141 weeks now!!!!  Sorry to hear that Washington did not pick smokey like darly did.  More job chances will come up, darly will put an add on schoolnet8.com for smokey.  It will say, 'please hire my smokey, she is very smart and fancy and will teach the kids many things'.  hehe! I love you very much. <br />&nbsp; &nbsp; &nbsp; &nbsp;   &nbsp; &nbsp; &nbsp; &nbsp;  love, darly";
 
   $s .= "<br style=\"clear: right;\" /><b>Rate Feature:</b> <a href=\"$rooturl/index.phtml?feature_good\">Good</a> ($good votes) or <a href=\"$rooturl/index.phtml?feature_bad\">Bad</a> ($bad votes) &nbsp; &nbsp;<a href=\"$rooturl/onsite/features/past.php\">Past Features</a>";
 }
