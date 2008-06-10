c267dfa15fb2142dc4a0bfe24dbf158fe7f13332
akrherz <akrherz@iastate.edu>
2008-06-10 14:06:34 +0000

    fix 12am on the plot

--- htdocs/plotting/mesoeast/dailyrain.php
+++ htdocs/plotting/mesoeast/dailyrain.php
@@ -97,10 +97,10 @@ while (list ($line_num, $line) = each ($fcontents)) {
 $xpre = array(0 => '12 AM', '1 AM', '2 AM', '3 AM', '4 AM', '5 AM',
         '6 AM', '7 AM', '8 AM', '9 AM', '10 AM', '11 AM', 'Noon',
         '1 PM', '2 PM', '3 PM', '4 PM', '5 PM', '6 PM', '7 PM',
-        '8 PM', '9 PM', '10 PM', '11 PM', 'Midnight');
+        '8 PM', '9 PM', '10 PM', '11 PM', '12 AM');
 
 
-for ($j=0; $j<24; $j++){
+for ($j=0; $j<25; $j++){
   $xlabel[$j*60] = $xpre[$j];
 }
 
