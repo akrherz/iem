fe81fdc50a9a237df07f4c0ecafede800f6656bc
akrherz <akrherz@iastate.edu>
2008-06-23 19:11:05 +0000

    complexity ensues

--- htdocs/p.php
+++ htdocs/p.php
@@ -10,10 +10,10 @@ $conn = iemdb("postgis");
 
 if ($id != "") {
  pg_query($conn, "update text_products SET reads = reads + 1 WHERE id = $id");
- $sql = sprintf('SELECT product from text_products WHERE id = %s', $id);
+ $sql = sprintf('SELECT product,geom from text_products WHERE id = %s', $id);
 } else {
  pg_query($conn, "update text_products SET reads = reads + 1 WHERE product_id = '$pid'");
- $sql = sprintf("SELECT product from text_products WHERE product_id = '%s'", $pid);
+ $sql = sprintf("SELECT product,geom from text_products WHERE product_id = '%s'", $pid);
 }
 
 $rs = pg_query($conn, $sql);
@@ -21,6 +21,7 @@ $rs = pg_query($conn, $sql);
 for ($i=0; $row = @pg_fetch_array($rs, $i); $i++)
 {
   echo "<pre>". $row["product"] ."</pre>\n";
+  if ($row["geom"] != ""){  echo "<p><img src=\"GIS/radmap.php?pid=$pid\">"; }
 }
 
 ?>
