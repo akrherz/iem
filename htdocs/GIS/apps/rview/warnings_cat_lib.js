bddcc5c5b4bf4bd8b07918d15010ac4c8d6650f6
akrherz <akrherz@iastate.edu>
2008-06-24 07:13:47 +0000

    height

--- htdocs/GIS/apps/rview/warnings_cat_lib.js
+++ htdocs/GIS/apps/rview/warnings_cat_lib.js
@@ -1,4 +1,30 @@
 Ext.onReady(function(){
+Ext.namespace('Ext.ux.plugins');
+Ext.ux.plugins.ContainerMask = function(opt) {
+    var options = opt||{};
+
+    return {
+        init: function(c) {
+            Ext.applyIf(c,{
+                showMask : function(msg, msgClass, maskClass){
+                    var el;
+                    if(this.rendered && (el = this[options.el] || Ext.get(options.el) || this.getEl?this.getEl():null)){
+                      el.mask.call(el,msg || options.msg, msgClass || options.msgClass, maskClass || options.maskClass);
+                    }
+                },
+                hideMask : function(){
+                    var el;
+                    if(this.rendered && (el = this[options.el] || Ext.get(options.el) ||  this.getEl?this.getEl():null)){ 
+                      el.unmask.call(el);
+                    }
+                }
+            });
+            if(options.masked){ 
+                c.on('render', c.showMask.createDelegate(c,[null]) ,c, {delay:10, single:true}) ; 
+            }
+        }
+    };
+};
 
    Ext.ux.grid.filter.StringFilter.prototype.icon = 'find.png';
 
@@ -124,6 +150,7 @@ Ext.onReady(function(){
     // create the Grid
     var grid = new Ext.grid.GridPanel({
         id:'lsr-grid',
+        isLoaded:false,
         store: jstore,
         loadMask: {msg:'Loading Data...'},
         cm: new Ext.grid.ColumnModel([
@@ -220,7 +247,7 @@ function mySplitter(val) {
         renderTo: 'displaytabs',
         id: 'top-display',
         width:660,
-        height:520,
+        height:540,
         plain:true,
         enableTabScroll:true,
         items:[
