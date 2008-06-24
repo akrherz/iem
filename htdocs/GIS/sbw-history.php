600bc72797fc832d0236e3da2221f47113647e70
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2008-06-24 00:59:51 +0000

    good good

--- htdocs/GIS/sbw-history.php
+++ htdocs/GIS/sbw-history.php
@@ -2,6 +2,7 @@
 /* Generate an ultra fancy plot of a storm based warning history! */
 include("../../config/settings.inc.php");
 include("$rootpath/include/database.inc.php");
+include("$rootpath/include/vtec.php");
 dl($mapscript);
 $mapFile = $rootpath."/data/gis/base4326.map";
 $postgis = iemdb("postgis");
@@ -25,10 +26,11 @@ $px = Array(160,480,160,480);
 $py = Array(120,120,360,360);
 
 $map2 = ms_newMapObj($mapFile);
+$map2->imagecolor->setrgb(155,155,155);
 $map2->set("width", 640);
 $map2->set("height",480);
 $img2 = $map2->prepareImage();
-$buffer = 0.2;
+$buffer = 0.3;
 
 for ($i=0;$row = @pg_fetch_array($rs, $i); $i++)
 {
@@ -39,8 +41,8 @@ for ($i=0;$row = @pg_fetch_array($rs, $i); $i++)
   } 
 
   $map = ms_newMapObj($mapFile);
-  $map->set("width", 320);
-  $map->set("height",240);
+  $map->set("width", 316);
+  $map->set("height",236);
   $map->setExtent($row["xmin"] - $buffer, $row["ymin"] - $buffer, 
                   $row["xmax"] + $buffer, $row["ymax"] + $buffer);
 
@@ -81,12 +83,13 @@ for ($i=0;$row = @pg_fetch_array($rs, $i); $i++)
   $wc->setProjection("init=epsg:4326");
 
   $wcc0 = ms_newClassObj($wc);
-  $wcc0->set("name", "Product");
+  $wcc0->set("name", $vtec_phenomena[$row["phenomena"]] ." ". $vtec_significance[$row["significance"]] );
   $wcc0s0 = ms_newStyleObj($wcc0);
   $wcc0s0->color->setRGB(255,0,0);
   $wcc0s0->set("size", 3);
   $wcc0s0->set("symbol", 1);
   $wc->draw($img);
+  $map->drawLabelCache($img);
 
   $bar640t = $map->getLayerByName("bar640t");
   $bar640t->set("status", 1);
@@ -104,11 +107,17 @@ for ($i=0;$row = @pg_fetch_array($rs, $i); $i++)
   $point->draw($map, $tlayer, $img, 1,"$d");
   $point->free();
 
+  $layer = $map->getLayerByName("logo");
+  $point = ms_newpointobj();
+  $point->setXY(40, 26);
+  $point->draw($map, $layer, $img, "logo", "");
+  $point->free();
+
   $map->embedLegend($img);
   $map->drawLabelCache($img);
 
 
-  $img2->pasteImage($img, '0x0000000', $px[$i],$py[$i],0);
+  $img2->pasteImage($img, -1, $px[$i],$py[$i],0);
 }
 header ("Content-type: image/png");
 $img2->saveImage('');
