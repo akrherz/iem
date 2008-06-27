769771d725576e19381f45508363d3ea1772e91a
akrherz <akrherz@iastate.edu>
2008-06-27 19:09:57 +0000

    hehe?

--- htdocs/request/grx/webcams.php
+++ htdocs/request/grx/webcams.php
@@ -22,15 +22,27 @@ $sql = "SELECT * from camera_current WHERE valid > (now() - '30 minutes'::interv
 $s2 = "";
 $s3 = "";
 $q = 2;
+$vectors = Array(
+ "E" => Array("ax"=> 320, "ay" => 120),
+ "S" => Array("ax"=> 160, "ay" => 240),
+ "W" => Array("ax"=> 0, "ay" => 120),
+ "N" => Array("ax"=> 160, "ay" => 0));
+
 $rs = pg_exec($conn, $sql);
 for ($i=0;$row=@pg_fetch_array($rs,$i);$i++)
 {
   $key = $row["cam"];
   if (!$overview && $cameras[$key]["network"] != $network){ continue; }
+  $drct = $row["drct"];
+  if ($drct >= 315 || $drct < 45){ $v = $vectors["S"]; }
+  else if ($drct >= 45 && $drct < 135){ $v = $vectors["W"]; }
+  else if ($drct >= 135 && $drct < 225){ $v = $vectors["N"]; }
+  else{ $v = $vectors["E"]; }
+
   if (!$overview)
-  echo sprintf("IconFile: %s, 320, 240, 160, 240,\"http://mesonet.agron.iastate.edu/data/camera/stills/%s.jpg\"\n", $q, $key);
+  echo sprintf("IconFile: %s, 320, 240, %.0f, %.0f,\"http://mesonet.agron.iastate.edu/data/camera/stills/%s.jpg\"\n", $q, $v["ax"], $v["ay"], $key);
   if ($overview)
-  $s2 .= sprintf("Icon: %.4f,%.4f,%s,1,7,\"[%s] %s\"\n", $cameras[$key]['lat'], $cameras[$key]['lon'], $row["drct"], $cameras[$key]["network"], $cameras[$key]["name"]);
+  $s2 .= sprintf("Icon: %.4f,%.4f,%s,1,7,\"[%s] %s\"\n", $cameras[$key]['lat'], $cameras[$key]['lon'], $drct, $cameras[$key]["network"], $cameras[$key]["name"]);
   if (!$overview)
   $s3 .= sprintf("Icon: %.4f,%.4f,000,%s,1\n", $cameras[$key]['lat'], $cameras[$key]['lon'], $q);
   $q += 1;
