48eaf305b765855582ed2a09f1dbb2d693d96117
akrherz <akrherz@iastate.edu>
2008-06-27 21:32:34 +0000

    lets do 30

--- htdocs/request/grx/webcams.php
+++ htdocs/request/grx/webcams.php
@@ -9,7 +9,7 @@ $overview = isset($_GET["overview"]);
 
 $thres = 999;
 $title = "IEM Webcam Overview";
-if (!$overview){ $thres = 15; $title ="$network webcams via IEM";}
+if (!$overview){ $thres = 30; $title ="$network webcams via IEM";}
 header("Content-type: text/plain");
 $r = $camera_refresh / 60;
 echo "Refresh: $r
