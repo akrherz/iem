a00be34496f0dcad201441ef31b13c3af68f0faa
akrherz <akrherz@iastate.edu>
2008-06-30 14:29:44 +0000

    yippeee

---
+++ htdocs/kml/kcci.php
@@ -0,0 +1,45 @@
+<?php
+/* Sucks to render a KML */
+include("../../config/settings.inc.php");
+include("$rootpath/include/database.inc.php");
+$connect = iemdb("access");
+
+/* Now we fetch warning and perhaps polygon */
+$query2 = "SELECT *, askml(geom) as kml
+           from current
+           WHERE network = 'KCCI' and valid > (now() - '30 minutes'::interval)";
+
+$result = pg_exec($connect, $query2);
+
+header("Content-Type:", "application/vnd.google-earth.kml+xml");
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+<kml xmlns=\"http://earth.google.com/kml/2.2\">
+ <Document>
+    <Style id=\"iemstyle\">
+      <LineStyle>
+        <width>1</width>
+        <color>ff000000</color>
+      </LineStyle>
+      <PointStyle>
+        <color>ff000000</color>
+      </PointStyle>
+    </Style>";
+for ($i=0;$row=@pg_fetch_array($result,$i);$i++)
+{
+  echo "<Placemark>
+    <description>
+        <![CDATA[
+  <p><font color=\"red\"><i>Location:</i></font> ". $row["tmpf"] ." ". $row["dwpf"] ." 
+   </p>
+        ]]>
+    </description>
+    <styleUrl>#iemstyle</styleUrl>
+    <name>". $row["tmpf"] ."</name>\n";
+echo $row["kml"];
+echo "</Placemark>";
+}
+echo "
+ </Document>
+</kml>";
+
+?>
