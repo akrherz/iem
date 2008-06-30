4969785e7c8310f6529d0295bb1e1c307119834b
akrherz <akrherz@iastate.edu>
2008-06-30 15:02:06 +0000

    updates

--- htdocs/kml/kcci.php
+++ htdocs/kml/kcci.php
@@ -1,49 +1,17 @@
 <?php
-/* Sucks to render a KML */
-include("../../config/settings.inc.php");
-include("$rootpath/include/database.inc.php");
-$connect = iemdb("access");
-
-/* Now we fetch warning and perhaps polygon */
-$query2 = "SELECT *, askml(geom) as kml
-           from current
-           WHERE network = 'KCCI' and valid > (now() - '30 minutes'::interval)";
-
-$result = pg_exec($connect, $query2);
-
 header("Content-Type:", "application/vnd.google-earth.kml+xml");
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
 <kml xmlns=\"http://earth.google.com/kml/2.2\">
- <Document>
+<Folder>
+   <name>SchoolNet8 Currents</name>
    <NetworkLink>
      <name>SchoolNet8 Currents</name>
      <Link id=\"ID\">
-       <href>http://mesonet.agron.iastate.edu/kml/kcci.php</href>
+       <href>http://mesonet.agron.iastate.edu/kml/sfnetwork.php?network=KCCI</href>
        <refreshInterval>60</refreshInterval>
        <refreshMode>onInterval</refreshMode>
      </Link>
    </NetworkLink>
-   <Style id=\"iemstyle\">
-     <BalloonStyle>
-      <bgColor>ffffffbb</bgColor>
-    </BalloonStyle>
-  </Style>";
-for ($i=0;$row=@pg_fetch_array($result,$i);$i++)
-{
-  echo "<Placemark>
-    <description>
-        <![CDATA[
-  <p><font color=\"red\"><i>Location:</i></font> ". $row["tmpf"] ." ". $row["dwpf"] ." 
-   </p>
-        ]]>
-    </description>
-    <styleUrl>#iemstyle</styleUrl>
-    <name>". $row["tmpf"] ."</name>\n";
-echo $row["kml"];
-echo "</Placemark>";
-}
-echo "
- </Document>
+</Folder>
 </kml>";
-
 ?>
