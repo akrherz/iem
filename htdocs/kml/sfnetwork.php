6d7ab48099b9e96db53a00aa3ff21c0b66a23e94
akrherz <akrherz@iastate.edu>
2008-06-30 16:17:22 +0000

    yawn

--- htdocs/kml/sfnetwork.php
+++ htdocs/kml/sfnetwork.php
@@ -19,8 +19,9 @@ echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
  <Document>
    <Style id=\"iemstyle\">
      <IconStyle>
+      <scale>0.8</scale>
       <Icon>
-        <!-- <href>http://www.schoolnet8.com/favicon.ico</href> -->
+        <href>http://www.schoolnet8.com/favicon.ico</href>
       </Icon>
      </IconStyle>
      <BalloonStyle>
@@ -32,7 +33,8 @@ for ($i=0;$row=@pg_fetch_array($result,$i);$i++)
   echo "<Placemark>
     <description>
         <![CDATA[
-  <p><font color=\"red\"><i>Temperature:</i></font> ". $row["tmpf"] ."
+  <p><font color=\"red\"><i>Site Name:</i></font> ". $row["name"] ."
+   <br /><font color=\"red\"><i>Temperature:</i></font> ". $row["tmpf"] ."
    <br /><font color=\"red\"><i>Dew Point:</i></font> ". $row["dwpf"] ." 
    <br /><font color=\"red\"><i>Today Rainfall:</i></font> ". $row["pday"] ." 
    </p>
