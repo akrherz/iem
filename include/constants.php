2bff832ac2d6d94315526da00fe852f143cbde71
akrherz <akrherz@iastate.edu>
2008-09-05 15:14:29 +0000

    apr

--- include/constants.php
+++ include/constants.php
@@ -1,6 +1,6 @@
 <?php
 
-$coop_archive_end = mktime(0,0,0,4,1,2008);
+$coop_archive_end = mktime(0,0,0,5,1,2008);
 $awos_archive_end = mktime(0,0,0,8,1,2008);
 $asos_archive_end = mktime(0,0,0,9,1,2008);
 $camera_refresh = 300;
