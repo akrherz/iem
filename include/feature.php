37034d804c3fe229069bfb12240383f84e45e585
akrherz <akrherz@iastate.edu>
2008-09-10 11:19:40 +0000

    156

--- include/feature.php
+++ include/feature.php
@@ -78,7 +78,7 @@ $s .= "</table>";
 if (getenv("REMOTE_ADDR") == "12.217.178.106" )
 {
  $s = "<img src=\"images/cardinals.gif\" style=\"float: left; margin: 5px;\">
-Smokey, muah! <br /> &nbsp; &nbsp; &nbsp; &nbsp; 155 weeks now!!!!  Another number divisible by many other numbers, hehe.  We keep plowing ahead, 1 day at a time.  Things will work out for the good, since smokey is a good person.  I love you very much. <br />&nbsp; &nbsp; &nbsp; &nbsp;   &nbsp; &nbsp; &nbsp; &nbsp;  love, darly";
+Smokey, muah! <br /> &nbsp; &nbsp; &nbsp; &nbsp; 156 weeks now!!!!  My goodness, the numbers are getting so big!  Darly very happy and proud of smokey.  Smokey gonna be a great teacher.  She is doing what she loves and  I love you very much. <br />&nbsp; &nbsp; &nbsp; &nbsp;   &nbsp; &nbsp; &nbsp; &nbsp;  love, darly";
 
   $s .= "<br style=\"clear: right;\" /><b>Rate Feature:</b> <a href=\"$rooturl/index.phtml?feature_good\">Good</a> ($good votes) or <a href=\"$rooturl/index.phtml?feature_bad\">Bad</a> ($bad votes) &nbsp; &nbsp;<a href=\"$rooturl/onsite/features/past.php\">Past Features</a>";
 }
