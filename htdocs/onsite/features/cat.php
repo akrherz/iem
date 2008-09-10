4b5a59d367c9258ddbad704d5f98bc131bf3e8b8
akrherz <akrherz@iastate.edu>
2008-09-10 15:18:12 +0000

    yipee, no injection issues

--- htdocs/onsite/features/cat.php
+++ htdocs/onsite/features/cat.php
@@ -3,39 +3,42 @@ include("../../../config/settings.inc.php");
 include("$rootpath/include/database.inc.php");
 $day = isset($_GET["day"]) ? substr($_GET["day"],0,10) : die("No date specified");
 $offset = isset($_GET["offset"]) ? $_GET["offset"] : 0;
-$TITLE = "IEM | Past Feature"; 
-      include("$rootpath/include/header.php"); ?>
 
-<h3>Past IEM Features by Date</h3>
-
-<?php 
-  $connection = iemdb("mesosite");
-  $fdf = "date(valid) = '$day' ";
-  if ($offset == "-1"){
-    $fdf = "valid < '$day' ORDER by valid DESC limit 1";
-  } else if ($offset == "+1") {
-    $fdf = "date(valid) > '$day' ORDER by valid ASC limit 1";
-  }
-  $query1 = "SELECT *, date(valid) as d,
+$dbconn = iemdb("mesosite");
+$rs = pg_prepare($dbconn, "yesterday", "SELECT *, date(valid) as d,
               to_char(valid, 'YYYY/MM/YYMMDD') as imageref, 
               to_char(valid, 'DD Mon YYYY HH:MI AM') as webdate from feature
-              WHERE $fdf ";
-  $result = pg_exec($connection, $query1);
-  $row = @pg_fetch_array($result,0);
-  $day = $row["d"];
-  $thumb = sprintf("http://mesonet.agron.iastate.edu/onsite/features/%s_s.gif", $row["imageref"]);
-  $big = sprintf("http://mesonet.agron.iastate.edu/onsite/features/%s.gif", $row["imageref"]);
+              WHERE valid < $1 ORDER by valid DESC limit 1");
+$rs = pg_prepare($dbconn, "today", "SELECT *, date(valid) as d,
+              to_char(valid, 'YYYY/MM/YYMMDD') as imageref, 
+              to_char(valid, 'DD Mon YYYY HH:MI AM') as webdate from feature
+              WHERE date(valid) = $1");
+$rs = pg_prepare($dbconn, "tomorrow", "SELECT *, date(valid) as d,
+              to_char(valid, 'YYYY/MM/YYMMDD') as imageref, 
+              to_char(valid, 'DD Mon YYYY HH:MI AM') as webdate from feature
+              WHERE valid > $1 ORDER by valid ASC limit 1");
 
-  $fref = "/mesonet/share/features/". $row["imageref"] ."_s.gif";
-  list($width, $height, $type, $attr) = @getimagesize($fref);
-  $width += 20;
+$q = "today";
+if ($offset == "-1"){ $q = "yesterday"; }
+else if ($offset == "+1") {$q = "tomorrow";}
+$result = pg_execute($dbconn, $q, Array($day));
 
+if (pg_num_rows($result) == 0){ die("Feature Not Found"); }
+
+$TITLE = "IEM | Past Feature"; 
+$THISPAGE = "iem-feature";
+include("$rootpath/include/header.php"); 
+
+$row = pg_fetch_array($result,0);
+$day = $row["d"];
+$thumb = sprintf("http://mesonet.agron.iastate.edu/onsite/features/%s_s.gif", $row["imageref"]);
+$big = sprintf("http://mesonet.agron.iastate.edu/onsite/features/%s.gif", $row["imageref"]);
 
 ?>
 <a href="cat.php?day=<?php echo $day; ?>&offset=-1">Previous Feature</a> &nbsp; &nbsp; <a href="cat.php?day=<?php echo $day; ?>&offset=+1">Next Feature</a>
 <hr />
 <div style="width: 640px;">
-<div style="float: left; padding: 5px; width: <?php echo $width; ?>px;">
+<div style="float: left; padding: 5px; ">
 <img src="<?php echo $thumb; ?>" style="margin: 5px;">
 <br /><a href="<?php echo $big; ?>">View larger image</a>
 <br /><?php echo $row["caption"]; ?>
