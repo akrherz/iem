d00c209817817ae463b9c1a3af020b2a6296207e
akrherz <akrherz@iastate.edu>
2008-10-04 13:13:17 +0000

    sep asos

--- include/constants.php
+++ include/constants.php
@@ -2,7 +2,7 @@
 
 $coop_archive_end = mktime(0,0,0,5,1,2008);
 $awos_archive_end = mktime(0,0,0,9,1,2008);
-$asos_archive_end = mktime(0,0,0,9,1,2008);
+$asos_archive_end = mktime(0,0,0,10,1,2008);
 $camera_refresh = 300;
 
 ?>
