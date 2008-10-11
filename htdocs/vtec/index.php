a949385eb525e028041be8fa535a05884f7aa5bc
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2008-10-11 23:06:14 +0000

    intermediate commit to save what I got

---
+++ htdocs/vtec/index.php
@@ -0,0 +1,31 @@
+<?php
+/* Pure IEM 2.0 App to do vtec stuff.  Shorter url please as well */
+
+
+?>
+
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="../ext/resources/css/ext-all.css"/>
+<script type="text/javascript" src="../ext/adapter/ext/ext-base.js"></script>
+<script type="text/javascript" src="../ext/ext-all.js"></script>
+<script type="text/javascript" src="wfos.js"></script>
+<script src="http://maps.google.com/maps?file=api&amp;v=2.x&amp;key=ABQIAAAAJDLv3q8BFBryRorw-851MRT2yXp_ZAY8_ufC3CFXhHIE1NvwkxTyuslsNlFqyphYqv1PCUD8WrZA2A" type="text/javascript"></script>
+<script type="text/javascript" src="Ext.ux.GMapPanel.js"></script>
+<script type="text/javascript" src="RowExpander.js"></script>
+<script type="text/javascript" src="../ext/ux/menu/EditableItem.js"></script>
+<script type="text/javascript" src="../ext/ux/grid/GridFilters.js"></script>
+<script type="text/javascript" src="../ext/ux/grid/filter/Filter.js"></script>
+<script type="text/javascript" src="../ext/ux/grid/filter/StringFilter.js"></script>
+<script type="text/javascript" src="static.js"></script>
+</head>
+<body>
+
+
+<div id="header"></div>
+<div id="help">
+ <h3>Help me!</h3>
+</div>
+<div id="footer"></div>
+
+</body></html>
