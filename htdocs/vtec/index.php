72ce0916e32c82ddeb86469f5ddd5fff9fbef88c
akrherz <akrherz@iastate.edu>
2008-10-12 02:23:55 +0000

    more intermediate commits

--- htdocs/vtec/index.php
+++ htdocs/vtec/index.php
@@ -1,6 +1,15 @@
 <?php
 /* Pure IEM 2.0 App to do vtec stuff.  Shorter url please as well */
-
+$v = isset($_GET["vtec"]) ? $_GET["vtec"] : "2008-O-NEW-KJAX-TO-W-0048";
+$tokens = split("-", $v);
+$year = $tokens[0];
+$operation = $tokens[1];
+$vstatus = $tokens[2];
+$wfo4 = $tokens[3];
+$wfo = substr($wfo4,1,3);
+$phenomena = $tokens[4];
+$significance = $tokens[5];
+$eventid = intval( $tokens[6] );
 
 ?>
 
@@ -24,8 +33,16 @@
 
 <div id="header"></div>
 <div id="help">
- <h3>Help me!</h3>
+ <h3>Help me! <?php echo $v; ?></h3>
 </div>
 <div id="footer"></div>
-
+<script>
+Ext.onReady(function(){
+  Ext.getCmp("wfoselector").setValue("<?php echo $wfo; ?>");
+  Ext.getCmp("phenomenaselector").setValue("<?php echo $phenomena; ?>");
+  Ext.getCmp("significanceselector").setValue("<?php echo $significance; ?>");
+  Ext.getCmp("eventid").setValue("<?php echo $eventid; ?>");
+  Ext.getCmp("yearselector").setValue("<?php echo $year; ?>");
+});
+</script>
 </body></html>
---
+++ htdocs/vtec/json-meta.php
@@ -0,0 +1,37 @@
+<?php
+/* Giveme JSON data listing products */
+require_once 'Zend/Json.php';
+require_once '../../config/settings.inc.php';
+require_once "$rootpath/include/database.inc.php";
+
+$connect = iemdb("postgis");
+pg_exec($connect, "SET TIME ZONE GMT");
+
+$year = isset($_GET["year"]) ? intval($_GET["year"]) : die();
+$wfo = isset($_GET["wfo"]) ? substr($_GET["wfo"],0,3) : die();
+$phenomena = isset($_GET["phenomena"]) ? substr($_GET["phenomena"],0,2) : die();
+$significance = isset($_GET["significance"]) ? substr($_GET["significance"],0,1) : die();
+$eventid = isset($_GET["eventid"]) ? intval($_GET["eventid"]) : die();
+
+$query1 = "SELECT xmax(geom) as x1, xmin(geom) as x0, 
+                  ymin(geom) as y0, ymax(geom) as y1, *
+                  from warnings_$year WHERE wfo = '$wfo' and 
+                  phenomena = '$phenomena' and eventid = $eventid 
+                  and significance = '$significance' 
+                  ORDER by gtype DESC LIMIT 1";
+
+$result = pg_exec($connect, "SET TIME ZONE 'GMT'");
+$result = pg_exec($connect, $query1);
+
+$ar = Array("meta" => Array() );
+for( $i=0; $z = @pg_fetch_array($result,$i); $i++)
+{
+  $z["id"] = $i +1;
+  $z["issue"] = substr($z["issue"],0,16);
+  $z["expire"] = substr($z["expire"],0,16);
+  $ar["meta"][] = $z;
+}
+
+echo Zend_Json::encode($ar);
+
+?>
