9c2390204c7862817ca6a0ee29244d79c9c2147e
akrherz <akrherz@iastate.edu>
2008-10-17 14:10:49 +0000

    add some stuff for prod versus localhost

--- htdocs/vtec/index.php
+++ htdocs/vtec/index.php
@@ -1,4 +1,7 @@
 <?php
+include("../../config/settings.inc.php");
+include("$rootpath/include/google_keys.php");
+
 /* Pure IEM 2.0 App to do vtec stuff.  Shorter url please as well */
 $v = isset($_GET["vtec"]) ? $_GET["vtec"] : "2008-O-NEW-KJAX-TO-W-0048";
 $tokens = split("-", $v);
@@ -11,15 +14,20 @@ $phenomena = $tokens[4];
 $significance = $tokens[5];
 $eventid = intval( $tokens[6] );
 
+$extjs = "ext-all.js";
+if ($rootpath == "http://localhost/iem";){
+  $extjs = "ext-all-debug.js";
+}
+
 ?>
 
 <html>
 <head>
 <link rel="stylesheet" type="text/css" href="../ext/resources/css/ext-all.css"/>
 <script type="text/javascript" src="../ext/adapter/ext/ext-base.js"></script>
-<script type="text/javascript" src="../ext/ext-all-debug.js"></script>
+<script type="text/javascript" src="../ext/<?php echo $extjs; ?>"></script>
 <script type="text/javascript" src="wfos.js"></script>
-<script src="http://maps.google.com/maps?file=api&amp;v=2.x&amp;key=ABQIAAAAJDLv3q8BFBryRorw-851MRT2yXp_ZAY8_ufC3CFXhHIE1NvwkxTyuslsNlFqyphYqv1PCUD8WrZA2A" type="text/javascript"></script>
+<script src="http://maps.google.com/maps?file=api&amp;v=2.x&amp;key=<?php echo $GOOGLEKEYS[$rooturl]["vtec"]; ?>" type="text/javascript"></script>
 <script type="text/javascript" src="Ext.ux.GMapPanel.js"></script>
 <script type="text/javascript" src="RowExpander.js"></script>
 <script type="text/javascript" src="../ext/ux/menu/EditableItem.js"></script>
