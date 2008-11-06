dc19d09e85041c4d04778eb5802056c9268ddfb3
akrherz <akrherz@iastate.edu>
2008-11-06 21:14:45 +0000

    roads

--- include/google_keys.php
+++ include/google_keys.php
@@ -4,11 +4,13 @@ $GOOGLEKEYS = array(
     "maplsr" => "ABQIAAAArXt77YptylBNPFEy0AgJxBQEqbOpq-UVZ0D5kHM5vpm679zg5RShssc7g46Xm0eM5Jlmt9GiE90Uxg",
     "rview" => "ABQIAAAArXt77YptylBNPFEy0AgJxBSzesuTo04SfEVAA8aHdKEgKDyBjBQ8UJs1hA3bQvvlrK7hlIpffKLong",
     "vtec" => "ABQIAAAAJDLv3q8BFBryRorw-851MRT2yXp_ZAY8_ufC3CFXhHIE1NvwkxTyuslsNlFqyphYqv1PCUD8WrZA2A",
+    "roads" => "ABQIAAAArXt77YptylBNPFEy0AgJxBQtmgQCP_2mEDho0V39nlenznERpBSlaoXYPGaJaeFkiTWHFPYSm9hxjQ",
  ),
  "http://mesonet.agron.iastate.edu" => array(
     "maplsr" => "ABQIAAAArXt77YptylBNPFEy0AgJxBRyku_Vi9Erm7X8i1nZlN7yJeRsoxQB0vI1-xYP2QYOsxEhzNiTBaS-wQ",
     "rview" => "ABQIAAAArXt77YptylBNPFEy0AgJxBQvv4M5oTbRL27Li5BWPOWBa2LVfxRsHZ284Tw0TLrJslWUJWrr1cQSdA",
     "vtec" => "ABQIAAAArXt77YptylBNPFEy0AgJxBQKS3Zo0LsVgCUcCGN5hE04uQoUURRrogTCEt6dT4TnelQZalTxMdP_zw",
+    "roads" => "ABQIAAAArXt77YptylBNPFEy0AgJxBTNlYskItsXST8J634CUN-yQ941sBQJFzJ6c6XQpEWm6Aq621XDAurVmg",
  ),
 );
 ?>
