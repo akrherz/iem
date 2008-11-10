91d877fac571523fe4c73536b73f899783844a63
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2008-11-10 15:55:53 +0000

    vrmmm

--- include/constants.php
+++ include/constants.php
@@ -2,7 +2,7 @@
 
 $coop_archive_end = mktime(0,0,0,8,1,2008);
 $awos_archive_end = mktime(0,0,0,11,1,2008);
-$asos_archive_end = mktime(0,0,0,10,1,2008);
+$asos_archive_end = mktime(0,0,0,11,1,2008);
 $camera_refresh = 300;
 
 ?>
