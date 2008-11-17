856e0805f28a8d84dce250ad77197dc906f21d56
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2008-11-17 15:13:26 +0000

    no more sql injection and wrap it with a banner for self promotion please

--- htdocs/p.php
+++ htdocs/p.php
@@ -7,22 +7,30 @@ $pid = isset($_GET['pid']) ? substr($_GET['pid'],0,32) : "";
 if ($id == "" && $pid == "") die();
 
 $conn = iemdb("postgis");
+$rs = pg_prepare($conn, "IDUPDATE", "update text_products 
+                 SET reads = reads + 1 WHERE id = $1");
+$rs = pg_prepare($conn, "PIDUPDATE", "update text_products 
+                 SET reads = reads + 1 WHERE product_id = $1");
+$rs = pg_prepare($conn, "IDSELECT", "SELECT product,geom from text_products 
+                 WHERE id = $1");
+$rs = pg_prepare($conn, "PIDSELECT", "SELECT product,geom from text_products 
+                 WHERE product_id = $1");
 
 if ($id != "") {
- pg_query($conn, "update text_products SET reads = reads + 1 WHERE id = $id");
- $sql = sprintf('SELECT product,geom from text_products WHERE id = %s', $id);
+ pg_execute($conn, "IDUPDATE", Array($id));
+ $rs = pg_execute($conn, "IDSELECT", Array($id));
 } else {
- pg_query($conn, "update text_products SET reads = reads + 1 WHERE product_id = '$pid'");
- $sql = sprintf("SELECT product,geom from text_products WHERE product_id = '%s'", $pid);
+ pg_execute($conn, "PIDUPDATE", Array($pid));
+ $rs = pg_execute($conn, "PIDSELECT", Array($pid));
 }
 
-$rs = pg_query($conn, $sql);
-//header("Content-type: text/plain");
+include("$rootpath/include/header.php");
+
 for ($i=0; $row = @pg_fetch_array($rs, $i); $i++)
 {
   echo "<pre>". $row["product"] ."</pre>\n";
   if ($row["geom"] != ""){  echo "<p><img src=\"GIS/radmap.php?pid=$pid\">"; }
 }
 
+include("$rootpath/include/footer.php");
 ?>
-
