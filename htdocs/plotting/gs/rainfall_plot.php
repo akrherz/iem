308d345e1bb0ba7b0469a22445d17add7767d4d6
akrherz <akrherz@iastate.edu>
2008-12-02 19:00:36 +0000

    bells and whistles

--- htdocs/plotting/gs/rainfall_plot.php
+++ htdocs/plotting/gs/rainfall_plot.php
@@ -3,8 +3,12 @@ include("../../../config/settings.inc.php");
 
 $station = isset($_GET['station']) ? $_GET['station'] : "DSM";
 $year = isset($_GET['year']) ? $_GET['year']: date("Y");
-$sts = mktime(0,0,0,  5, 1, $year);
-$ets = mktime(0,0,0, 10, 1, $year);
+$smonth = isset($_GET['smonth']) ? $_GET['smonth'] : 5;
+$emonth = isset($_GET['emonth']) ? $_GET['emonth'] : 10;
+$sday = isset($_GET['sday']) ? $_GET['sday'] : 1;
+$eday = isset($_GET['eday']) ? $_GET['eday'] : 1;
+$sts = mktime(0,0,0,  $smonth, $sday, $year);
+$ets = mktime(0,0,0, $emonth, $eday, $year);
 $sdate = date("Y-m-d", $sts);
 $edate = date("Y-m-d", $ets);
 $s2date = date("2000-m-d", $sts);
@@ -50,7 +54,7 @@ $cdiff = Array();
 $aclimate = Array();
 $atot = 0;
 $zeros = Array();
-$xlabels = Array();
+$times = Array();
 for( $i=0; $row = @pg_fetch_array($rs,$i); $i++) 
 {
 	$p = $row["precip"];
@@ -61,29 +65,29 @@ for( $i=0; $row = @pg_fetch_array($rs,$i); $i++)
 	$aclimate[$i] = $atot;
 	$zeros[$i] = 0;
 	$xlabels[$i] = "";
+    $times[$i] = strtotime( $row["valid"] );
 }
 
 pg_close($coopdb);
 
-$xlabels[0] = "May 1";
-$xlabels[30] = "Jun 1";
-$xlabels[61] = "Jul 1";
-$xlabels[91] = "Aug 1";
-$xlabels[122] = "Sep 1";
 
 include ("$rootpath/include/jpgraph/jpgraph.php");
 include ("$rootpath/include/jpgraph/jpgraph_line.php");
+include ("$rootpath/include/jpgraph/jpgraph_date.php");
 include ("$rootpath/include/jpgraph/jpgraph_bar.php");
 
 // Create the graph. These two calls are always required
-$graph = new Graph(600,400,"example1");
-$graph->SetScale("textlin");
-$graph->img->SetMargin(45,10,80,30);
+$graph = new Graph(640,480,"example1");
+$graph->SetScale("datlin");
+$graph->img->SetMargin(45,10,80,60);
 $graph->xaxis->SetFont(FF_FONT1,FS_BOLD);
 $graph->yaxis->SetTitleMargin(30);
 $graph->xaxis->SetPos("min");
 //$graph->xaxis->SetTitle("Day of Month");
-$graph->xaxis->SetTickLabels($xlabels);
+//$graph->xaxis->SetTickLabels($xlabels);
+$graph->xaxis->SetLabelFormatString("M d", true);
+$graph->xaxis->SetLabelAngle(90);
+
 
 $graph->yaxis->SetTitle("Precipitation (in)");
 $graph->title->Set( $cities[$station]["name"] ." [$station] Precipitation for ". $year );
@@ -91,34 +95,36 @@ $graph->subtitle->Set("Climate Site: ". $cities[strtoupper($climate_site)]["name
 $graph->legend->SetLayout(LEGEND_HOR);
 $graph->legend->Pos(0.05, 0.1, "right", "top");
 
-$graph->AddLine(new PlotLine(VERTICAL,30,"tan",1));
-$graph->AddLine(new PlotLine(VERTICAL,61,"tan",1));
-$graph->AddLine(new PlotLine(VERTICAL,91,"tan",1));
-$graph->AddLine(new PlotLine(VERTICAL,122,"tan",1));
+reset($times);
+while (list($k,$v) = each($times))
+{
+ if (date("d", $v) == 1)
+   $graph->AddLine(new PlotLine(VERTICAL,$v,"tan",1));
+}
 
 
 // Create the linear plot
-$lp0 =new LinePlot($cdiff);
+$lp0 =new LinePlot($cdiff, $times);
 $lp0->SetColor("green");
 $lp0->SetLegend("Accum Difference");
 
-$b2plot = new BarPlot($obs);
+$b2plot = new BarPlot($obs, $times);
 $b2plot->SetFillColor("blue");
 $b2plot->SetLegend("Obs Rain");
 
 // Create the linear plot
-$lp1=new LinePlot($aobs);
+$lp1=new LinePlot($aobs, $times);
 $lp1->SetLegend("Actual Accum");
 $lp1->SetColor("blue");
-$lp1->SetWeight(2);
+$lp1->SetWeight(3);
 
-$lp2=new LinePlot($aclimate);
+$lp2=new LinePlot($aclimate, $times);
 $lp2->SetLegend("Climate Accum");
 $lp2->SetColor("red");
-$lp2->SetWeight(2);
+$lp2->SetWeight(3);
 
-$z = new LinePlot($zeros);
-$z->SetWeight(2);
+$z = new LinePlot($zeros, $times);
+$z->SetWeight(3);
 
 // Add the plot to the graph
 $graph->Add($lp0);
