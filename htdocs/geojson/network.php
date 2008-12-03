f51689684f076ad73fdca5a00f58ab544aaeec10
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2008-12-03 03:12:41 +0000

    add fancy pants new site selector!

---
+++ htdocs/geojson/network.php
@@ -0,0 +1,30 @@
+<?php
+/* Generate a KML file of a network locations, yummy */
+require_once 'Zend/Json.php';
+include("../../config/settings.inc.php");
+include("$rootpath/include/database.inc.php");
+include("$rootpath/include/network.php");
+$network = isset($_GET["network"]) ? $_GET["network"] : "KCCI"; 
+$nt = new NetworkTable($network);
+
+$ar = Array("type"=>"FeatureCollection",
+      "crs" => Array("type"=>"EPSG", 
+                     "properties" => Array("code"=>4326,
+                                  "coordinate_order" => Array(1,0))),
+      "features" => Array()
+);
+
+while (list($sid,$data) = each($nt->table))
+{
+  $z = Array("type"=>"Feature", "id"=>$sid, 
+             "properties"=>Array(
+                "sname"=> $data["name"],
+                "sid"=> $data["id"],
+              ),
+             "geometry"=>Array("type"=>"Point",
+                         "coordinates"=>Array($data["lon"],$data["lat"])));
+  $ar["features"][] = $z;
+}
+
+echo Zend_Json::encode($ar);
+?>
