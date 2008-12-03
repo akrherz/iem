176e7e2f0d97406a19f817bea3a1feab998256b7
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2008-12-03 01:26:40 +0000

    add overall api key

--- include/google_keys.php
+++ include/google_keys.php
@@ -5,12 +5,14 @@ $GOOGLEKEYS = array(
     "rview" => "ABQIAAAArXt77YptylBNPFEy0AgJxBSzesuTo04SfEVAA8aHdKEgKDyBjBQ8UJs1hA3bQvvlrK7hlIpffKLong",
     "vtec" => "ABQIAAAAJDLv3q8BFBryRorw-851MRT2yXp_ZAY8_ufC3CFXhHIE1NvwkxTyuslsNlFqyphYqv1PCUD8WrZA2A",
     "roads" => "ABQIAAAArXt77YptylBNPFEy0AgJxBQtmgQCP_2mEDho0V39nlenznERpBSlaoXYPGaJaeFkiTWHFPYSm9hxjQ",
+    "any" => "ABQIAAAArXt77YptylBNPFEy0AgJxBT2yXp_ZAY8_ufC3CFXhHIE1NvwkxSWO--kYuoNy3IYAdfhbTLBzJ_wUg",
  ),
  "http://mesonet.agron.iastate.edu" => array(
     "maplsr" => "ABQIAAAArXt77YptylBNPFEy0AgJxBRyku_Vi9Erm7X8i1nZlN7yJeRsoxQB0vI1-xYP2QYOsxEhzNiTBaS-wQ",
     "rview" => "ABQIAAAArXt77YptylBNPFEy0AgJxBQvv4M5oTbRL27Li5BWPOWBa2LVfxRsHZ284Tw0TLrJslWUJWrr1cQSdA",
     "vtec" => "ABQIAAAArXt77YptylBNPFEy0AgJxBQKS3Zo0LsVgCUcCGN5hE04uQoUURRrogTCEt6dT4TnelQZalTxMdP_zw",
     "roads" => "ABQIAAAArXt77YptylBNPFEy0AgJxBTNlYskItsXST8J634CUN-yQ941sBQJFzJ6c6XQpEWm6Aq621XDAurVmg",
+    "any" => "ABQIAAAArXt77YptylBNPFEy0AgJxBQemssV0AF0s1FO3raoWtvCi5VjlBSlLBVTNtvIg7H4w7NCvUPYGyuPDQ",
  ),
 );
 ?>
