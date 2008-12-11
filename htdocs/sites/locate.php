690f8e93a7e6216f4e6a00eff292ef2534388c5b
akrherz <akrherz@iastate.edu>
2008-12-11 15:06:31 +0000

    update to use fancy pants selector, good bye errors from Google Bot

--- htdocs/sites/locate.php
+++ htdocs/sites/locate.php
@@ -1,35 +1,58 @@
 <?php
  include("../../config/settings.inc.php");
  include("$rootpath/include/database.inc.php");
- include("$rootpath/include/selectWidget.php");
+ include("$rootpath/include/google_keys.php");
+ include("$rootpath/include/imagemaps.php");
  $network = isset($_GET["network"]) ? $_GET["network"] : "IA_ASOS";
 
- $sw = new selectWidget("$rooturl/sites/locate.php", "$rooturl/sites/site.php?", $network);
- $swf = Array("network" => $network);
- $sw->setformvars($swf);
- $sw->logic($_GET);
- $ar = Array("IA_ASOS", "AWOS", "IA_RWIS", "IACOCORAHS", "KELO", "KCCI", "KIMT","SD_ASOS", "MN_ASOS", "WI_ASOS", "IL_ASOS", "MO_ASOS", "KS_ASOS", "NE_ASOS");
- $sw->set_networks($ar);
- $swinterface = $sw->printInterface();
-?>
-<?php
 $TITLE = "IEM | Site Locator";
 $THISPAGE = "iem-sites";
+$api = $GOOGLEKEYS[$rooturl]["any"];
+$HEADEXTRA = "<script src='http://maps.google.com/maps?file=api&amp;v=2&amp;key=$api'></script>
+<script src='http://www.openlayers.org/api/OpenLayers.js'></script>
+<script src='${rooturl}/js/olselect.php?network=${network}'></script>";
+$BODYEXTRA = "onload=\"init()\"";
 include("$rootpath/include/header.php"); 
-  include("$rootpath/include/imagemaps.php");
-    ?>
-
+?>
+<style type="text/css">
+        #map {
+            width: 640px;
+            height: 400px;
+            border: 2px solid black;
+        }
+</style>
 <h3 class="heading">IEM Site Information</h3><p>
 <div class="text">
-The IEM site selector allows the user to obtain a details of a given site through selection of the
-site visually through maps of the various station types.  Once a site is selected, the user has 
-access to a site description, current conditions, parameters measured at the site, site neighbors,
-and site photos.<br /><br /></p>
-
+<p>The IEM collects information from many sites.  These sites are organized into
+networks based on their geography and/or the organization who administers the
+network.  This application provides some metadata and site specific applications
+you may find useful.</p>
 
 <div align="center">
 
-<?php echo $swinterface; ?>
+<form name="switcher">
+<table>
+<tr><th>Select By Network:</th>
+<td>
+<?php echo selectNetwork($network); ?>
+</td>
+<td><input type="submit" value="Switch Network"></td>
+</tr></table>
+</form>
+
+<form name="olselect">
+<table>
+<tr><th>Select By Station:</th>
+<td>
+<?php echo networkSelect($network, ""); ?>
+</td>
+<td><input type="submit" value="Select Station"></td>
+</tr></table>
+<br />Or select site from this map by clicking on the black dot....
+<div id="map"></div>
+<div id="sname" unselectable="on">No site selected</div>
+</form>
+
 
 </div>
 <br /><br /></div>
