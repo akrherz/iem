2c19ea81a5bb4990b2a5cdf515585ac03a9057e4
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2008-12-11 19:43:44 +0000

    away with you, coopstations

--- htdocs/COOP/dl/normals_wkr.php
+++ htdocs/COOP/dl/normals_wkr.php
@@ -3,10 +3,13 @@
  /** normals_wkr.php
   *  - Download daily climate values
   */
-include("$rootpath/include/COOPstations.php");
 include("$rootpath/include/database.inc.php");
+include("$rootpath/include/network.php");     
+$nt = new NetworkTable("IACLIMATE");
+$cities = $nt->table;
 
-$station = isset($_GET["station"]) ? substr($_GET["station"],0,6) 
+
+$station = isset($_GET["station"]) ? strtolower(substr($_GET["station"],0,6)) 
                                    : die("No Station Specified");
 $dloption = isset($_GET["dloption"]) ? $_GET["dloption"]
                                    : die("No download option specified");
@@ -34,8 +37,8 @@ $rs = pg_exec($con, "SELECT extract(month from valid) as month,
 $s = "StationID,Name,Latitude,Longitude,Month,Day,Avg_High,Avg_Low,Avg_Precip\n";
 for($i=0; $row = @pg_fetch_array($rs,$i); $i++){
   $sid = $row["station"];
-  $s .= sprintf("%s,%30s,%5.2f,%5.2f,%2d,%2d,%4.1f,%4.1f,%4.2f\n", $sid, $cities[$sid]["city"],
-   $cities[$sid]["lat"], $cities[$sid]["lon"] ,
+  $s .= sprintf("%s,%30s,%5.2f,%5.2f,%2d,%2d,%4.1f,%4.1f,%4.2f\n", $sid, $cities[strtoupper($sid)]["name"],
+   $cities[strtoupper($sid)]["lat"], $cities[strtoupper($sid)]["lon"] ,
    $row["month"], $row["day"], $row["high"], $row["low"], $row["precip"]);
 }
 
