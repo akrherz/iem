5568f2752ee5297c1baca22a431bf83cc1da9315
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2009-02-06 20:05:56 +0000

    asos uploaded

--- include/constants.php
+++ include/constants.php
@@ -2,7 +2,7 @@
 
 $coop_archive_end = mktime(0,0,0,9,1,2008);
 $awos_archive_end = mktime(0,0,0,2,1,2009);
-$asos_archive_end = mktime(0,0,0,1,1,2009);
+$asos_archive_end = mktime(0,0,0,2,1,2009);
 $camera_refresh = 300;
 
 ?>
