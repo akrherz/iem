1b4f876bd26293884edb82a5438a852777ae4c72
akrherz <akrherz@iastate.edu>
2009-03-06 18:01:09 +0000

    asos/awos update!

--- include/constants.php
+++ include/constants.php
@@ -1,8 +1,8 @@
 <?php
 
 $coop_archive_end = mktime(0,0,0,11,1,2008);
-$awos_archive_end = mktime(0,0,0,2,1,2009);
-$asos_archive_end = mktime(0,0,0,2,1,2009);
+$awos_archive_end = mktime(0,0,0,3,1,2009);
+$asos_archive_end = mktime(0,0,0,3,1,2009);
 $camera_refresh = 300;
 
 ?>
