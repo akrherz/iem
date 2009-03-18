26f1c0c5c0afd93852d10044bcc35c3a99f63995
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2009-03-18 12:25:54 +0000

    183

--- include/feature.php
+++ include/feature.php
@@ -98,7 +98,7 @@ $s .= "</table>";
 if (getenv("REMOTE_ADDR") == "173.20.184.18" )
 {
  $s = "<img src=\"images/i3.jpg\" style=\"float: left; margin: 5px;\">
-Smokey, muah! <br /> &nbsp; &nbsp; &nbsp; &nbsp; 182 weeks now!!!! Next week, smokey will be on Bourbon Street and snoopy will be at my house!  All three of us will be having all kinds of fun with darly and snoopy's fun being legal.  Darly also get to see smokey this weekend, everything is fancy these days. I love you very much!<br />&nbsp; &nbsp; &nbsp; &nbsp;   &nbsp; &nbsp; &nbsp; &nbsp;  love, darly";
+Smokey, muah! <br /> &nbsp; &nbsp; &nbsp; &nbsp; 183 weeks now!!!! hehe, smokey on a plane to New Orleans while darly and snoopy are paling around.  Snoopy and darly both miss smokey very much, but hope she is having lots of fun.  I love you very much!<br />&nbsp; &nbsp; &nbsp; &nbsp;   &nbsp; &nbsp; &nbsp; &nbsp;  love, darly";
 
   $s .= "<br style=\"clear: right;\" /><b>Rate Feature:</b> <a href=\"$rooturl/index.phtml?feature_good\">Good</a> ($good votes) or <a href=\"$rooturl/index.phtml?feature_bad\">Bad</a> ($bad votes) &nbsp; &nbsp;<a href=\"$rooturl/onsite/features/past.php\">Past Features</a>";
 }
