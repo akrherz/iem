c6e531f9cdfd1b35704341de7f09aa72c7622230
akrherz <akrherz@iastate.edu>
2009-03-20 13:27:32 +0000

    add interval and monthly based time averaging

--- cgi-bin/wepp/sm2shape.py
+++ cgi-bin/wepp/sm2shape.py
@@ -15,36 +15,67 @@ year = int(form["year"][0])
 month = int(form["month"][0])
 day = int(form["day"][0])
 ts = mx.DateTime.DateTime(year, month, day)
-
 fp = "%s_sm" % (ts.strftime("%Y%m%d"), )
+interval = None
+if form.has_key("interval"):
+    interval = int(form["interval"][0])
+    fp = "%s_%s_sm" % (ts.strftime("%Y%m%d"), interval)
+
+monthly = None
+if form.has_key("monthly"):
+    monthly = 1
+    fp = "%s_sm" % (ts.strftime("%Y%m"), )
+
 
 print "Content-type: application/octet-stream"
 print "Content-Disposition: attachment; filename=%s.zip" % (fp,)
 print
 
-if (os.path.isfile(fp+".zip")):
+# Maybe our data is already cached, lets hope so!
+if os.path.isfile(fp+".zip"):
   print file(fp+".zip", 'r').read(),
   sys.exit(0)
 
 
+# Load up the township GIS stuff
 twp = {}
 rs = mydb.query("SELECT astext(transform(the_geom,4326)) as tg, model_twp from iatwp").dictresult()
 for i in range(len(rs)):
   twp[ rs[i]["model_twp"] ] = rs[i]["tg"]
 
-rs = mydb.query("SELECT * from waterbalance_by_twp \
-  WHERE valid = '%s'" % (ts.strftime("%Y-%m-%d"), ) ).dictresult()
+# Now, lets figure out what data we want
+sql = "SELECT * from waterbalance_by_twp WHERE valid = '%s'" \
+       % (ts.strftime("%Y-%m-%d"), )
+day1 = ts.strftime("%Y%m%d")
+day2 = ts.strftime("%Y%m%d")
+if interval is not None:
+    sql = "SELECT model_twp, avg(vsm) as vsm, -1 as vsm_stddev, \
+      avg(s10cm) as s10cm, avg(s20cm) as s20cm from waterbalance_by_twp \
+      WHERE valid BETWEEN '%s' and ('%s'::date + '%s days'::interval) \
+      GROUP by model_twp" % (ts.strftime("%Y-%m-%d"), ts.strftime("%Y-%m-%d"),\
+      interval)
+    day2 = (ts + mx.DateTime.RelativeDateTime(days=interval)).strftime("%Y%m%d")
+
+if month is not None:
+    sql = "SELECT model_twp, avg(vsm) as vsm, -1 as vsm_stddev, \
+      avg(s10cm) as s10cm, avg(s20cm) as s20cm from waterbalance_by_twp \
+      WHERE valid BETWEEN '%s-01' and ('%s-01'::date + '1 month'::interval) \
+      GROUP by model_twp" % (ts.strftime("%Y-%m"), ts.strftime("%Y-%m") )
+    day1 = (ts + mx.DateTime.RelativeDateTime(day=1)).strftime("%Y%m%d")
+    day2 = (ts + mx.DateTime.RelativeDateTime(day=1,months=1)).strftime("%Y%m%d")
+
+rs = mydb.query(sql).dictresult()
 
 shp = shapelib.create(fp, shapelib.SHPT_POLYGON)
 dbf = dbflib.create(fp)
-dbf.add_field("VALID", dbflib.FTString, 8, 0)
+dbf.add_field("DAY_STA", dbflib.FTString, 8, 0)
+dbf.add_field("DAY_END", dbflib.FTString, 8, 0)
 dbf.add_field("MODL_TWP", dbflib.FTString, 10, 0)
 dbf.add_field("VSM", dbflib.FTDouble, 8, 4)
 dbf.add_field("VSM_STDD", dbflib.FTDouble, 8, 4)
 dbf.add_field("S10CM", dbflib.FTDouble, 8, 4)
 dbf.add_field("S20CM", dbflib.FTDouble, 8, 4)
 
-v = ts.strftime("%Y%m%d")
 for i in range(len(rs)):
   m = rs[i]["model_twp"]
   vsm = float(rs[i]["vsm"])
@@ -55,23 +86,28 @@ for i in range(len(rs)):
   f = wellknowntext.convert_well_known_text( twp[m] )
   obj = shapelib.SHPObject(shapelib.SHPT_POLYGON, 1, f )
   shp.write_object(-1, obj)
-  dbf.write_record(i, (v,m,vsm,vsms,s10,s20) )
+  dbf.write_record(i, (day1,day2,m,vsm,vsms,s10,s20) )
 
 del(dbf)
 del(shp)
 
 o = open(fp+".txt", 'w')
 o.write("""
-IEM Modelled Soil Moisture from the Iowa Daily Erosion Project
+WEPP Modelled Soil Moisture from the Iowa Daily Erosion Project
 http://wepp.mesonet.agron.iastate.edu
 
 DBF Columns are:
+  DAY_STA   Start date of the period data is valid for YYYYMMDD
+  DAY_END   End date of the period data is valid for YYYYMMDD
   MODL_TWP  Model township
-  VALID     Date data is valid for YYYYMMDD
   VSM       Volumetric Soil Moisture [%] over root zone depth
   VSM_STDD  VSM Standard Deviation within model township
   S10CM     0-10 cm depth soil moisture [mm]
   S20CM     10-20 cm depth soil moisture [mm]
+
+If DAY_STA does not equal DAY_END, then the values have been time 
+averaged.  VSM_STDD is set to -1 in this case, since we don't have
+necessary data to produce this statistic.
 
 Data Contact:
   Daryl Herzmann akrherz@iastate.edu  515.294.5978
