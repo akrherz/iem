be4ef480e45ffcfcb3cbcd2436418611c5266ccc
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2009-04-04 12:44:02 +0000

    add march 09

--- include/constants.php
+++ include/constants.php
@@ -2,7 +2,7 @@
 
 $coop_archive_end = mktime(0,0,0,12,1,2008);
 $awos_archive_end = mktime(0,0,0,3,1,2009);
-$asos_archive_end = mktime(0,0,0,3,1,2009);
+$asos_archive_end = mktime(0,0,0,4,1,2009);
 $camera_refresh = 300;
 
 ?>
