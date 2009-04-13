fc390706ab6b1a92a76e7c996756e2f03cbd10e9
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2009-04-13 19:40:43 +0000

    rid ye of cruft

--- htdocs/current/neighbors.php
+++
@@ -1,7 +0,0 @@
-<?php include("/mesonet/php/include/header.php"); ?>
-
-<p>Sorry, this page is depreciated.  Please update your bookmarks to this page:
-
-<a href="http://mesonet.agron.iastate.edu/ASOS/current.phtml">http://mesonet.agron.iastate.edu/ASOS/current.phtml</a>
-
-<?php include("/mesonet/php/include/footer.php"); ?>
