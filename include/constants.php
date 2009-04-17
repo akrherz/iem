07c1c2286c363740f872fb0c31f4cdb2bd0b058b
akrherz <akrherz@iastate.edu>
2009-04-17 19:58:44 +0000

    updates, we should database this at some point :/

--- include/constants.php
+++ include/constants.php
@@ -1,6 +1,6 @@
 <?php
 
-$coop_archive_end = mktime(0,0,0,12,1,2008);
+$coop_archive_end = mktime(0,0,0,1,1,2009);
 $awos_archive_end = mktime(0,0,0,4,1,2009);
 $asos_archive_end = mktime(0,0,0,4,1,2009);
 $camera_refresh = 300;
