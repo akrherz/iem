fe09979dfe05dd1380637f35cd4174a183f33e2e
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2009-04-26 18:42:55 +0000

    60 min refresh

--- include/constants.php
+++ include/constants.php
@@ -3,6 +3,6 @@
 $coop_archive_end = mktime(0,0,0,1,1,2009);
 $awos_archive_end = mktime(0,0,0,4,1,2009);
 $asos_archive_end = mktime(0,0,0,4,1,2009);
-$camera_refresh = 300;
+$camera_refresh = 60;
 
 ?>
