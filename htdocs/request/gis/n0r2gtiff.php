db389bd704b3baf9c4d332684d1ef2f8dfeef1bb
akrherz <akrherz@iastate.edu>
2009-04-28 19:02:45 +0000

    shortcut please

--- htdocs/request/gis/n0r2gtiff.php
+++ htdocs/request/gis/n0r2gtiff.php
@@ -1,4 +1,5 @@
 <?php
+putenv("TZ=GMT");
  /* This bad boy converts a PNG to a geo-tiff */
 
 $dstr = isset($_GET["dstr"]) ? $_GET["dstr"]: die("No dstr set");
@@ -8,14 +9,23 @@ $day = intval( substr($dstr,6,2));
 $hour = intval( substr($dstr,8,2));
 $minute = intval( substr($dstr,10,2));
 $ts = mktime($hour,$minute,0,$month,$day,$year);
+$now = time();
+if ($ts > $now){ die("Request from the future?"); }
 
- @mkdir("/tmp/png2gtiff");
- chdir("/tmp/png2gtiff");
-
-$inFile = sprintf("/mnt/a1/ARCHIVE/data/%s/GIS/uscomp/n0r_%s.png", date("Y/m/d", $ts), date("YmdHi", $ts) );
 $outFile = sprintf("n0r_%s", date("YmdHi", $ts) );
 $zipFile = sprintf("n0r_%s.zip", date("YmdHi", $ts) );
+
+if ($ts > ($now - 360.0)){  
+  $inFile = "/home/ldm/data/gis/images/4326/USCOMP/n0r_0.tif";
+} else {
+  $inFile = sprintf("/mnt/a1/ARCHIVE/data/%s/GIS/uscomp/n0r_%s.png", date("Y/m/d", $ts), date("YmdHi", $ts) );
+
+}
+
 if (! is_file($inFile)) die("No GIS composite found for this time!");
+
+@mkdir("/tmp/png2gtiff");
+chdir("/tmp/png2gtiff");
 
 $cmd = sprintf("/mesonet/local/bin/gdalwarp -t_srs \"EPSG:4326\" -s_srs \"EPSG:4326\" -co \"WORLDFILE=ON\" -of GTIFF %s %s.tif", $inFile, $outFile);
 `$cmd`;
