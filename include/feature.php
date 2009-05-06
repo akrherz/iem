4c60cc3de9eb853826cf345124a211b06f65641a
akrherz <akrherz@iastate.edu>
2009-05-06 11:21:40 +0000

    190

--- include/feature.php
+++ include/feature.php
@@ -98,7 +98,7 @@ $s .= "</table>";
 if (getenv("REMOTE_ADDR") == "173.20.184.18" )
 {
  $s = "<img src=\"images/i3.jpg\" style=\"float: left; margin: 5px;\">
-Smokey, muah! <br /> &nbsp; &nbsp; &nbsp; &nbsp; 189 weeks now!!!! An even bigger number this week smokey.  Darly always excited when he gets to make smokey feature.  The smokey feature is much more special than the regular feature, just like smokey is special.  Next weekend darly get to wear a tux and smokey a dress.  Yippeee.  I love you very much!<br />&nbsp; &nbsp; &nbsp; &nbsp;   &nbsp; &nbsp; &nbsp; &nbsp;  love, darly";
+Smokey, muah! <br /> &nbsp; &nbsp; &nbsp; &nbsp; 190 weeks now!!!!  Wow, what a big week smokey.  We will get to go to a wedding and it is a week with a large and even number divisible by 5, 10, and 95! hehe.  We also get to see a musical, what fun!  I love you very much!<br />&nbsp; &nbsp; &nbsp; &nbsp;   &nbsp; &nbsp; &nbsp; &nbsp;  love, darly";
 
   $s .= "<br style=\"clear: right;\" /><b>Rate Feature:</b> <a href=\"$rooturl/index.phtml?feature_good\">Good</a> ($good votes) or <a href=\"$rooturl/index.phtml?feature_bad\">Bad</a> ($bad votes) &nbsp; &nbsp;<a href=\"$rooturl/onsite/features/past.php\">Past Features</a>";
 }
