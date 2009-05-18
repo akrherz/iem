d9fa46a0ce12445f75d7a0408c2fdae32aa3483b
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2009-05-18 16:06:20 +0000

    updates

--- htdocs/plotting/coop/highlow_scatter.php
+++ htdocs/plotting/coop/highlow_scatter.php
@@ -1,6 +1,7 @@
 <?php 
 include("../../../config/settings.inc.php");
 $TITLE = "IEM | COOP High/Low Scatterplot";
+$THISPAGE = "climatology-today";
 include("$rootpath/include/header.php");
 include("$rootpath/include/imagemaps.php");
 include("$rootpath/include/forms.php");
@@ -8,6 +9,11 @@ $station = isset($_GET["station"]) ? $_GET["station"] : "IA0200";
 $month = isset($_GET["month"]) ? intval($_GET["month"]): date("m");
 $day = isset($_GET["day"]) ? intval($_GET["day"]): date("d");
 ?>
+
+<p>This application generates a scatter plot of daily 
+high versus low temperature for a NWS COOP site for a given date.  The resulting
+plot gives an indication of the spread of temperatures given a high or
+low temperature.
 
 <form method="GET" action="highlow_scatter.php">
 
