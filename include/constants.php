1d364767ece6978cfb40049822830866dac2ae42
akrherz <akrherz@iastate.edu>
2009-06-08 01:58:18 +0000

    back to 300

--- include/constants.php
+++ include/constants.php
@@ -3,6 +3,6 @@
 $coop_archive_end = mktime(0,0,0,1,1,2009);
 $awos_archive_end = mktime(0,0,0,5,1,2009);
 $asos_archive_end = mktime(0,0,0,5,1,2009);
-$camera_refresh = 60;
+$camera_refresh = 300;
 
 ?>
