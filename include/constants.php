cb103e5d187fd84e7fcb14a0d9280ed952e68341
akrherz <akrherz@iastate.edu>
2009-06-08 20:30:22 +0000

    Upload ASOS May

--- include/constants.php
+++ include/constants.php
@@ -2,7 +2,7 @@
 
 $coop_archive_end = mktime(0,0,0,1,1,2009);
 $awos_archive_end = mktime(0,0,0,5,1,2009);
-$asos_archive_end = mktime(0,0,0,5,1,2009);
+$asos_archive_end = mktime(0,0,0,6,1,2009);
 $camera_refresh = 300;
 
 ?>
