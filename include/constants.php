b7df1460d0b55d8b422b3ad550f8969abbf62bf7
akrherz <akrherz@iastate.edu>
2009-06-18 23:33:36 +0000

    bust bust bust

--- include/constants.php
+++ include/constants.php
@@ -3,6 +3,6 @@
 $coop_archive_end = mktime(0,0,0,2,1,2009);
 $awos_archive_end = mktime(0,0,0,6,1,2009);
 $asos_archive_end = mktime(0,0,0,6,1,2009);
-$camera_refresh = 60;
+$camera_refresh = 300;
 
 ?>
