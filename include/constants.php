f81df125073901fce511902aeeba174a137bdc56
akrherz <akrherz@iastate.edu>
2009-06-23 17:18:50 +0000

    back down to 60 we go

--- include/constants.php
+++ include/constants.php
@@ -3,6 +3,6 @@
 $coop_archive_end = mktime(0,0,0,2,1,2009);
 $awos_archive_end = mktime(0,0,0,6,1,2009);
 $asos_archive_end = mktime(0,0,0,6,1,2009);
-$camera_refresh = 300;
+$camera_refresh = 60;
 
 ?>
