60f1bc1df10ce21813fb57a4cc4ff4133b4947e6
akrherz <akrherz@iastate.edu>
2009-06-30 19:02:10 +0000

    get rid of debugging

--- htdocs/current/webcam.php
+++ htdocs/current/webcam.php
@@ -5,7 +5,7 @@ include("../../config/settings.inc.php");
 $HEADEXTRA = "
   <link rel='stylesheet' type='text/css' href='../ext-3.0-rc2/resources/css/ext-all.css'/>
   <script type='text/javascript' src='../ext-3.0-rc2/adapter/ext/ext-base.js'></script>
-  <script type='text/javascript' src='../ext-3.0-rc2/ext-all-debug.js'></script>
+  <script type='text/javascript' src='../ext-3.0-rc2/ext-all.js'></script>
 <script>
 Ext.namespace('cfg');
 cfg.refreshint = ${camera_refresh}000;
