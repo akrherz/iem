db5879737f1f9eb23f95e9d59f7ddd9b9359a811
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2009-08-05 13:08:01 +0000

    use smtplib

--- scripts/alerts/check_db.py
+++ scripts/alerts/check_db.py
@@ -1,7 +1,14 @@
 # Make sure the database server is up!
 
-import pg, os
+import pg, smtplib
+from email.mime.text import MIMEText
+
 try:
   pgconn = pg.connect('postgis', 'iemdb')
 except:
-  os.system("echo 'IEMDB DOWN!' | mail 5154519249@messaging.sprintpcs.com")
+  msg = MIMEText("IEMDB DOWN!")
+  msg['From'] = 'akrherz@iastate.edu'
+  msg['To'] = '5154519249@messaging.sprintpcs.com'
+  s = smtplib.SMTP('mailhub.iastate.edu')
+  s.sendmail(msg['From'], [msg['To']], msg.as_string())
+  s.quit()
