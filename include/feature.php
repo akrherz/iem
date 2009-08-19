dafc18d378341ee6119e1986b10fc6481165c3af
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2009-08-19 11:05:52 +0000

    205

--- include/feature.php
+++ include/feature.php
@@ -98,7 +98,7 @@ $s .= "</table>";
 if (getenv("REMOTE_ADDR") == "173.30.53.40" )
 {
  $s = "<img src=\"images/i3.jpg\" style=\"float: left; margin: 5px;\">
-Smokey, muah! <br /> &nbsp; &nbsp; &nbsp; &nbsp; 204 weeks now!!!!  My goodness, darly no sure if this number is right.  Darly does know that smokey is right for darly, hehe.  Smokey back home and so is snoopy.  Smokey makes darly very proud.  I love you very much!<br />&nbsp; &nbsp; &nbsp; &nbsp;   &nbsp; &nbsp; &nbsp; &nbsp;  love, darly";
+Smokey, muah! <br /> &nbsp; &nbsp; &nbsp; &nbsp; 205 weeks now!!!!  My goodness, a big number divisible by 5 and what else, hmmm I am not sure.  Anyway, smokey very fancy and soon will have many new kids to learn at schoooooool.  Darly very proud of smokey and I hope you have a good time at the triathlon.  I love you very much!<br />&nbsp; &nbsp; &nbsp; &nbsp;   &nbsp; &nbsp; &nbsp; &nbsp;  love, darly";
 
   $s .= "<br style=\"clear: right;\" /><b>Rate Feature:</b> <a href=\"$rooturl/index.phtml?feature_good\">Good</a> ($good votes) or <a href=\"$rooturl/index.phtml?feature_bad\">Bad</a> ($bad votes) &nbsp; &nbsp;<a href=\"$rooturl/onsite/features/past.php\">Past Features</a>";
 }
