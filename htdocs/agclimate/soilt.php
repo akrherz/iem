bf69b9f1da38e08d7a0b65c53ebb41a497bb93e3
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2009-09-03 12:53:46 +0000

    Add a page with the 4 inch soil temperature analysis

---
+++ htdocs/agclimate/soilt.php
@@ -0,0 +1,18 @@
+<?php
+include("../../config/settings.inc.php");
+$THISPAGE="networks-agclimate";
+include("$rootpath/include/header.php");
+?>
+
+<h3>Recent 4 inch Soil Temperatures</h3>
+
+<p>The following images are an interpolated analysis of county-by-county
+average soil temperature at a four inch depth.</p>
+
+<p>
+<img src="../data/soilt_day1.png">
+<img src="../data/soilt_day2.png">
+<img src="../data/soilt_day3.png">
+</p>
+
+<?php include("$rootpath/include/footer.php"); ?>
