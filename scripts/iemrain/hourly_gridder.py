400821c39e26789cbe98d4d4058d80f7613fe2ae
akrherz <akrherz@iastate.edu>
2009-09-11 16:32:40 +0000

    Initial commit of quasi working iemrainfall code! :)

---
+++ scripts/iemrain/hourly_gridder.py
@@ -0,0 +1,92 @@
+# IEMRAIN hourly data gridder
+
+import lib
+import os, mx.DateTime, numpy, Ngl, pg, math
+from osgeo import gdal
+from pyIEM import mesonet
+
+def do(ts):
+    """
+    Grid and save the precipitation for a given hour
+    """
+    nc = lib.load_netcdf(ts)
+    stations = lib.load_stations()
+
+    # Okay, now we are going to step thru the hour
+    ts0 = ts
+    ts1 = ts + mx.DateTime.RelativeDateTime(hours=1)
+    interval = mx.DateTime.RelativeDateTime(minutes=5)
+
+    # Step 1: ________________________
+    # Load up the reflectivities
+    reflect = numpy.zeros((12, len(nc.dimensions['lat']), len(nc.dimensions['lon'])), 'f')
+    i = -1
+    now = ts0 + interval
+    while now <= ts1:
+        i += 1
+        fp = now.strftime("/mnt/a1/ARCHIVE/data/%Y/%m/%d/GIS/uscomp/n0r_%Y%m%d%H%M.png")
+        if not os.path.isfile(fp):
+            continue
+        n0r = gdal.Open(fp, 0)
+        n0rd = n0r.ReadAsArray() # 2600, 6000 --> row, col
+        x0,y0 = lib.composite_lalo2pt(40.37, -96.64)
+        x1,y1 = lib.composite_lalo2pt(43.50, -90.14)
+        d = numpy.flipud( n0rd[y1:y0,x0:x1] )
+        reflect[i,:,:] = ( d - 5.0) * 5.0 # convert to dBZ
+
+        now += interval
+    total_reflect = numpy.sum( reflect, 0)
+    max_reflect = numpy.max( reflect, 0)
+
+    # Step 2: _____________ Create RH, precip mask
+    asos = pg.connect('asos', 'iemdb', user='nobody')
+    rs = asos.query("""SELECT station, max(tmpf) as t, max(dwpf) as d,
+         max(CASE WHEN p01m > 0 THEN p01m ELSE 0 END) as mm from t%s WHERE
+         valid > '%s+00' and valid <= '%s+00' and station in %s
+         and tmpf > -50 and dwpf > -50 GROUP by station""" % (
+         ts.year, ts0.strftime("%Y-%m-%d %H:%M"), 
+         ts1.strftime("%Y-%m-%d %H:%M"), 
+         str(tuple(stations.keys())) ) ).dictresult()
+    lats = []
+    lons = []
+    relh = []
+    p01m = []
+    for i in range(len(rs)):
+        stid = rs[i]['station']
+        if not stations.has_key(stid):
+            continue
+        lats.append( stations[stid]['lat'] )
+        lons.append( stations[stid]['lon'] )
+        relh.append( mesonet.relh( rs[i]['t'], rs[i]['d'] ) )
+        p01m.append( rs[i]['mm'] )
+
+    rh = Ngl.natgrid(lons, lats, relh, nc.variables['lon'][:], 
+         nc.variables['lat'][:])
+    pc = Ngl.natgrid(lons, lats, p01m, nc.variables['lon'][:], 
+         nc.variables['lat'][:])
+
+    # Print some diagnostics
+    print "Mean RH is %.2f" % (numpy.average(rh),)
+
+    # Now for our logic...
+    # z = 300 r^1.4  mm/hr
+    result = numpy.zeros((4, len(nc.dimensions['lat']), len(nc.dimensions['lon'])), 'f')
+    raw_rates = numpy.power(numpy.power(10.0,(reflect/10.0)) / 300.0 , 1.0/1.4) / 12.0  # For a 5min period
+    result[0,:,:] = numpy.sum( raw_rates[0:3,:,:], 0)
+    result[1,:,:] = numpy.sum( raw_rates[3:6,:,:], 0)
+    result[2,:,:] = numpy.sum( raw_rates[6:9,:,:], 0)
+    result[3,:,:] = numpy.sum( raw_rates[9:12,:,:], 0)
+
+    # Need to figure out the time index
+    monstart = ts + mx.DateTime.RelativeDateTime(day=1,hour=0,minute=0)
+    tdx0 = int( (ts-monstart).minutes / 15.0 )
+    tdx1 = int( (ts1-monstart).minutes / 15.0 )
+
+    print tdx0, tdx1, raw_rates[:,100,100], reflect[:,100,100],result[:,100,100]
+
+    nc.variables['precipitation'][tdx0:tdx1,:,:] = result
+    nc.close()
+
+#reflect = numpy.arange(-25,75,5)
+#print numpy.power( numpy.power(10.0,reflect/10.0) / 300.0, 1.0/1.4)
+do(mx.DateTime.DateTime(2009,9,3,9,0))
---
+++ scripts/iemrain/lib.py
@@ -0,0 +1,102 @@
+# Support library for IEMRAIN
+
+import netCDF3, sys, os, mx.DateTime, numpy, pg
+
+basedir = "/mesonet/share/iemrain"
+
+def load_stations():
+    """
+    Load up a dictionary of station metadata that we want for Iowa
+    """
+    mesosite = pg.connect("mesosite", "iemdb", user="nobody")
+    rs = mesosite.query("""SELECT x(geom) as lon, y(geom) as lat, id 
+         from stations WHERE network in ('AWOS','IA_ASOS') and
+         id != 'TVK'""").dictresult()
+    meta = {}
+    for i in range(len(rs)):
+        meta[ rs[i]['id'] ] = rs[i]
+    mesosite.close()
+    return meta
+
+def composite_lalo2pt(lat, lon):
+    """
+    Compute the raster x,y point for a given lat and lon
+    """
+    x = int(( -126.0 - lon ) / - 0.01 )
+    y = int(( 50.0 - lat ) / 0.01 )
+    return x, y
+
+
+def load_netcdf(ts):
+    """
+    Return the netcdf file for this timestamp
+    """
+    fp = "%s/%s/%s.nc" % (basedir, ts.year, ts.strftime("%Y%m"))
+    if not os.path.isfile(fp):
+        create_grid(ts)
+    return netCDF3.Dataset(fp, 'a')
+
+
+def create_grid(ts):
+    """
+    Create the NetCDF grid file that is used for all our work
+    """
+    fp = "%s/%s/%s.nc" % (basedir, ts.year, ts.strftime("%Y%m"))
+    # Create netCDF3 object
+    nc = netCDF3.Dataset(fp, 'w')
+    nc.institution = "Iowa State University, Ames, IA, USA"
+    nc.Conventions = "CF-1.0"
+    nc.contact     = "Daryl Herzmann, akrherz@iastate.edu, 515-294-5978"
+    nc.history     = "%s Generated" % (mx.DateTime.now().strftime("%d %B %Y"),)
+    nc.title       = "IEM Precipitation Analysis"
+
+    # Setup Dimensions
+    day1 = ts + mx.DateTime.RelativeDateTime(day=1,hour=0,minute=0,second=0)
+    day2 = day1 + mx.DateTime.RelativeDateTime(months=1)
+    tsteps = (day2-day1).days * 24 * 4
+    nc.createDimension('time', tsteps)
+    # Iowa Bounds -96.639706 40.375437,-90.140061 43.501196
+    x0 = -96.64
+    y0 =  40.37
+    x1 = -90.14
+    y1 =  43.50
+    nc.createDimension('lat', int((y1-y0) * 100) )
+    nc.createDimension('lon', int((x1-x0) * 100) )
+    nc.createDimension('bnds', 2)
+
+    # Setup Variables
+    tm = nc.createVariable('time', 'd', ('time',) )
+    tm.units = "minutes since %s-01 00:00:00.0" % (day1.strftime("%Y-%m"),)
+    tm.calendar = "gregorian"
+    tm.bounds = "time_bnds"
+    tm.long_name = "time"
+    tm.standard_name = "time"
+    tm[:] = numpy.arange(15, (day2 - day1).minutes + 15, 15)
+
+    tb = nc.createVariable('time_bnds', 'd', ('time','bnds') )
+    val = numpy.zeros( (tsteps,2), 'd' )
+    val[:,0] = numpy.arange(0, (day2 - day1).minutes , 15)
+    val[:,1] = numpy.arange(15, (day2 - day1).minutes + 15, 15)
+    tb[:] = val  
+
+    lat = nc.createVariable('lat', 'd', ('lat',) )
+    lat.units = "degrees_north"
+    lat.long_name = "latitude"
+    lat.standard_name = "latitude"
+    lat[:] = numpy.arange(y0,y1 - 0.01,0.01)
+
+    lon = nc.createVariable('lon', 'd', ('lon',) )
+    lon.units = "degrees_east"
+    lon.long_name = "longitude"
+    lon.standard_name = "longitude"
+    lon[:] = numpy.arange(x0,x1,0.01)
+
+    precip = nc.createVariable('precipitation', 'f', ('time','lat','lon'))
+    precip.units = 'kg m-2' # aka mm
+    precip.standard_name = 'precipitation_flux'
+    precip.long_name = 'Accumulated Precipitation'
+    precip.cell_methods = 'time: sum (interval: 15 minutes)'
+    precip._FillValue = 1.e20
+
+    nc.close()
+    del nc
