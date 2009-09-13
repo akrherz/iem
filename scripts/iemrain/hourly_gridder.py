1a84514013bdb665768e118c182e93f9395d6bb0
akrherz <akrherz@iastate.edu>
2009-09-13 23:45:24 +0000

    Weekend's worth of work, lets run this for a bit on iem10 to gen some more data

--- scripts/iemrain/hourly_gridder.py
+++ scripts/iemrain/hourly_gridder.py
@@ -26,52 +26,61 @@ def do(ts):
         i += 1
         fp = now.strftime("/mnt/a1/ARCHIVE/data/%Y/%m/%d/GIS/uscomp/n0r_%Y%m%d%H%M.png")
         if not os.path.isfile(fp):
+            now += interval
             continue
         n0r = gdal.Open(fp, 0)
         n0rd = n0r.ReadAsArray() # 2600, 6000 --> row, col
         x0,y0 = lib.composite_lalo2pt(40.37, -96.64)
         x1,y1 = lib.composite_lalo2pt(43.50, -90.14)
-        d = numpy.flipud( n0rd[y1:y0,x0:x1] )
-        reflect[i,:,:] = ( d - 5.0) * 5.0 # convert to dBZ
+        d = (numpy.flipud( n0rd[y1:y0,x0:x1] ) - 6.0) * 5.0 # to dBZ
+        # Greater than 50 dbZ causes Z-R to freak
+        d = numpy.where( d > 50, 50, d)
+        reflect[i,:,:] = d
+        del n0rd
+        del n0r 
 
         now += interval
     total_reflect = numpy.sum( reflect, 0)
     max_reflect = numpy.max( reflect, 0)
 
     # Step 2: _____________ Create RH, precip mask
-    asos = pg.connect('asos', 'iemdb', user='nobody')
-    rs = asos.query("""SELECT station, max(tmpf) as t, max(dwpf) as d,
-         max(CASE WHEN p01m > 0 THEN p01m ELSE 0 END) as mm from t%s WHERE
-         valid > '%s+00' and valid <= '%s+00' and station in %s
-         and tmpf > -50 and dwpf > -50 GROUP by station""" % (
-         ts.year, ts0.strftime("%Y-%m-%d %H:%M"), 
-         ts1.strftime("%Y-%m-%d %H:%M"), 
-         str(tuple(stations.keys())) ) ).dictresult()
-    lats = []
-    lons = []
-    relh = []
-    p01m = []
-    for i in range(len(rs)):
-        stid = rs[i]['station']
-        if not stations.has_key(stid):
-            continue
-        lats.append( stations[stid]['lat'] )
-        lons.append( stations[stid]['lon'] )
-        relh.append( mesonet.relh( rs[i]['t'], rs[i]['d'] ) )
-        p01m.append( rs[i]['mm'] )
-
-    rh = Ngl.natgrid(lons, lats, relh, nc.variables['lon'][:], 
-         nc.variables['lat'][:])
-    pc = Ngl.natgrid(lons, lats, p01m, nc.variables['lon'][:], 
-         nc.variables['lat'][:])
+    #asos = pg.connect('asos', 'iemdb', user='nobody')
+    #rs = asos.query("""SELECT station, max(tmpf) as t, max(dwpf) as d,
+    #     max(CASE WHEN p01m > 0 THEN p01m ELSE 0 END) as mm from t%s WHERE
+    #     valid > '%s+00' and valid <= '%s+00' and station in %s
+    #     and tmpf > -50 and dwpf > -50 GROUP by station""" % (
+    #     ts.year, ts0.strftime("%Y-%m-%d %H:%M"), 
+    #     ts1.strftime("%Y-%m-%d %H:%M"), 
+    #     str(tuple(stations.keys())) ) ).dictresult()
+    #lats = []
+    #lons = []
+    #relh = []
+    #p01m = []
+    #for i in range(len(rs)):
+    #    stid = rs[i]['station']
+    #    if not stations.has_key(stid):
+    #        continue
+    #    lats.append( stations[stid]['lat'] )
+    #    lons.append( stations[stid]['lon'] )
+     #   relh.append( mesonet.relh( rs[i]['t'], rs[i]['d'] ) )
+     #   p01m.append( rs[i]['mm'] )
+#
+    #rh = Ngl.natgrid(lons, lats, relh, nc.variables['lon'][:], 
+    #     nc.variables['lat'][:])
+    #pc = Ngl.natgrid(lons, lats, p01m, nc.variables['lon'][:], 
+    #     nc.variables['lat'][:])
 
     # Print some diagnostics
-    print "Mean RH is %.2f" % (numpy.average(rh),)
+    #print "Mean RH is %.2f" % (numpy.average(rh),)
+    #print "Mean PC is %.2f" % (numpy.average(pc),)
+
+    #print "Ob rain cells and < 70% humidity", numpy.sum( 
+    #      numpy.where( pc > 1.0, numpy.where(rh < 70.0,1,0),  0) )
 
     # Now for our logic...
     # z = 300 r^1.4  mm/hr
     result = numpy.zeros((4, len(nc.dimensions['lat']), len(nc.dimensions['lon'])), 'f')
-    raw_rates = numpy.power(numpy.power(10.0,(reflect/10.0)) / 300.0 , 1.0/1.4) / 12.0  # For a 5min period
+    raw_rates = numpy.power(numpy.power(10.0,(reflect/10.0)) / 200.0 , 1.0/1.6) / 12.0  # For a 5min period
     result[0,:,:] = numpy.sum( raw_rates[0:3,:,:], 0)
     result[1,:,:] = numpy.sum( raw_rates[3:6,:,:], 0)
     result[2,:,:] = numpy.sum( raw_rates[6:9,:,:], 0)
@@ -86,7 +95,14 @@ def do(ts):
 
     nc.variables['precipitation'][tdx0:tdx1,:,:] = result
     nc.close()
+    del nc
 
 #reflect = numpy.arange(-25,75,5)
 #print numpy.power( numpy.power(10.0,reflect/10.0) / 300.0, 1.0/1.4)
-do(mx.DateTime.DateTime(2009,9,3,9,0))
+sts = mx.DateTime.DateTime(1997,5,1,0,0)
+ets = mx.DateTime.DateTime(1997,6,1,0,0)
+interval = mx.DateTime.RelativeDateTime(hours=1)
+now = sts
+while now < ets:
+  do(now)
+  now += interval
