4ba205fa22a44969461c8f43e7026e20100634be
akrherz <akrherz@iastate.edu>
2009-09-18 14:18:07 +0000

    need to database these!

--- include/constants.php
+++ include/constants.php
@@ -1,6 +1,6 @@
 <?php
 
-$coop_archive_end = mktime(0,0,0,4,1,2009);
+$coop_archive_end = mktime(0,0,0,5,1,2009);
 $awos_archive_end = mktime(0,0,0,9,1,2009);
 $asos_archive_end = mktime(0,0,0,9,1,2009);
 $camera_refresh = 300;
