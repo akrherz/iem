b5fc1db08a15bbaadc0cd705357db0ceb923b1a7
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2009-09-24 16:06:54 +0000

    migrate to svn

---
+++ scripts/summary/snet_hourly_precip.py
@@ -0,0 +1,19 @@
+
+import mx.DateTime
+from pyIEM import iemdb
+i = iemdb.iemdb()
+iem = i['iem']
+iem.query("SET TIME ZONE 'GMT'")
+
+  # Our period
+t0 = mx.DateTime.utc() + mx.DateTime.RelativeDateTime(hours=-1,minute=0)
+t1 = mx.DateTime.utc() + mx.DateTime.RelativeDateTime(hours=-0,minute=0)
+
+sql = "INSERT into hourly_%s \
+       (SELECT station, network, '%s+00'::timestamp as v, max(phour) as p \
+        from current_log WHERE valid >= '%s' \
+        and valid < '%s' and phour > 0 and \
+        network NOT IN ('KCCI','KELO','KIMT') GROUP by station, network, v)" % \
+       (t0.year, t0.strftime("%Y-%m-%d %H:%M"), t0.strftime("%Y-%m-%d %H:%M"), \
+        t1.strftime("%Y-%m-%d %H:%M") )
+iem.query( sql )
