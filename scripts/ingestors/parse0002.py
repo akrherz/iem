eaf4360204d16c0fb302465287d9936576ae8dd4
akrherz <akrherz@iastate.edu>
2009-09-24 18:22:51 +0000

    migrate

---
+++ scripts/ingestors/parse0002.py
@@ -0,0 +1,49 @@
+#!/mesonet/python/bin/python
+# Quick and Dirty to get the ISUMET station data into the DB
+# Daryl Herzmann 19 Jun 2003
+# 18 Aug 2003	Add in iemAccess support
+
+import mx.DateTime, re, os, sys
+from pyIEM import iemAccessDatabase, iemAccessOb
+try:
+  iemdb = iemAccessDatabase.iemAccessDatabase()
+except:
+  if (mx.DateTime.now().minute == 10):
+    print "DATABASE FAIL"
+  sys.exit(0)
+
+
+now = mx.DateTime.now()
+fp = now.strftime("/mnt/a1/ARCHIVE/data/%Y/%m/%d/text/ot/ot0002.dat")
+
+if (not os.path.isfile(fp)):
+  sys.exit(0)
+
+iem = iemAccessOb.iemAccessOb("OT0002")
+
+lines = open(fp, "r").readlines()
+
+lastLine = lines[-1]
+
+tokens = re.split("[\s+]+", lastLine)
+
+tparts = re.split(":", tokens[4])
+valid = now + mx.DateTime.RelativeDateTime(hour= int(tparts[0]), \
+  minute = int(tparts[1]), second = int(tparts[2]) )
+
+iem.data['ts'] = valid
+iem.data['year'] = valid.year
+
+sped = float(tokens[8])
+sknt = round(sped *  0.868976, 1)
+
+iem.data['sknt'] = sknt
+iem.data['drct'] = tokens[9]
+iem.data['tmpf'] = tokens[7]
+
+iem.updateDatabase(iemdb)
+
+#mydb.query("SELECT zero_record('OT0002')")
+#mydb.query("UPDATE current SET tmpf = %s, indoor_tmpf = %s, valid = '%s', \
+#  sknt = %s, drct = %s WHERE station = '%s'" % (tokens[7], tokens[6], \
+#  valid, tokens[8], tokens[9], "OT0002") )
---
+++ scripts/ingestors/parse0006.py
@@ -0,0 +1,69 @@
+# Ingest the Monkey's data
+
+from pyIEM import iemAccessOb, iemAccess, mesonet, iemAccessDatabase
+import mx.DateTime, sys, os
+
+try:
+  iemdb = iemAccessDatabase.iemAccessDatabase()
+except:
+  if (mx.DateTime.now().minute == 10):
+    print "DATABASE FAIL"
+  sys.exit(0)
+iemob = iemAccessOb.iemAccessOb("OT0006")
+
+fp = "/mnt/a1/ARCHIVE/data/%s/text/ot/ot0006.dat" % (mx.DateTime.now().strftime("%Y/%m/%d"),)
+
+if (not os.path.isfile(fp)):
+  if (mx.DateTime.now().minute == 10 and mx.DateTime.now().hour == 12):
+    print "WHERE IS IEM2's DATA?"
+  sys.exit(0)
+
+lines = open(fp, 'r').readlines()
+
+line = lines[-1]
+if (len(line) < 20):
+  sys.exit(0)
+
+tokens = line.split()
+# ['10', '28', '2004', '00', '38', '53.2', '53.2', '52.8', '94', '2', '176', '6', '12:02AM', '30.08', '0.00', '1.38', '1.38', '75.2', '54']
+ts = mx.DateTime.DateTime( int(tokens[2]), int(tokens[0]), int(tokens[1]), int(tokens[3]), int(tokens[4]))
+
+iemob.setObTime(ts)
+iemob.data['tmpf'] = tokens[5]
+iemob.data['relh'] = tokens[8]
+iemob.data['dwpf'] = mesonet.dwpf(float(iemob.data['tmpf']), float(iemob.data['relh']) )
+iemob.data['sknt'] = tokens[9]
+iemob.data['drct'] = tokens[10]
+iemob.data['pres'] = tokens[13]
+iemob.data['pday'] = tokens[14]
+iemob.data['gust'] = tokens[11]
+iemob.updateDatabase(iemdb)
+
+# Piggy Back
+iemob = iemAccessOb.iemAccessOb("OT0007")
+
+fp = "/mnt/a1/ARCHIVE/data/%s/text/ot/ot0007.dat" % (mx.DateTime.now().strftime("%Y/%m/%d"),)
+
+if (not os.path.isfile(fp)):
+  sys.exit(0)
+
+lines = open(fp, 'r').readlines()
+
+line = lines[-1]
+
+# 114,2006,240,1530,18.17,64.70, 88.9,2.675,21.91,1014.3,0.000
+tokens = line.split(",")
+if (len(tokens) != 11):
+  sys.exit(0)
+hhmm = "%04i" % (int(tokens[3]),)
+ts = mx.DateTime.now() + mx.DateTime.RelativeDateTime(hour= int(hhmm[:2]), minute= int(hhmm[2:]), second = 0 )
+
+iemob.setObTime(ts)
+iemob.data['tmpf'] = tokens[5]
+iemob.data['relh'] = tokens[6]
+iemob.data['dwpf'] = mesonet.dwpf(float(iemob.data['tmpf']), float(iemob.data['relh']) )
+iemob.data['sknt'] = float(tokens[7]) * 1.94
+iemob.data['drct'] = tokens[8]
+iemob.data['pres'] = float(tokens[9]) / 960 * 28.36
+iemob.updateDatabase(iemdb)
+
