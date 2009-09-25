441aaf0fca2ae32d6c260280f0f6fe7ea26b75c0
akrherz <akrherz@iastate.edu>
2009-09-25 13:10:32 +0000

    more migration

--- scripts/RUN_10_AFTER.sh
+++ scripts/RUN_10_AFTER.sh
@@ -5,3 +5,6 @@ cd delta
 cd ../summary
 /mesonet/python/bin/python snet_hourly_precip.py
 /mesonet/python/bin/python update_snet_precip.py
+
+cd ../week
+/mesonet/python/bin/python plot_obs.py
---
+++ scripts/week/plot_obs.py
@@ -0,0 +1,59 @@
+# Generate analysis of precipitation
+
+import sys, os, random
+sys.path.append("../lib/")
+import iemplot
+
+import mx.DateTime
+now = mx.DateTime.now()
+
+from pyIEM import iemdb
+i = iemdb.iemdb()
+iem = i['iem']
+
+# Compute normal from the climate database
+sql = """
+select station, 
+  x(geom) as lon, y(geom) as lat, 
+  sum(pday) as rainfall
+from summary 
+WHERE day > ('TODAY'::date - '7 days'::interval) 
+and network in ('AWOS', 'IA_ASOS')
+and pday > 0 and pday < 30
+GROUP by station, lon, lat
+"""
+
+lats = []
+lons = []
+vals = []
+valmask = []
+rs = iem.query(sql).dictresult()
+for i in range(len(rs)):
+  lats.append( rs[i]['lat'] )
+  lons.append( rs[i]['lon'] + (random.random() * 0.01))
+  vals.append( rs[i]['rainfall'] )
+  valmask.append( True )
+
+cfg = {
+ 'wkColorMap': 'BlAqGrYeOrRe',
+ 'nglSpreadColorStart': 2,
+ 'nglSpreadColorEnd'  : -1,
+ '_valmask'           : valmask,
+ '_format'            : '%.2f',
+ '_showvalues'        : True,
+ '_title'             : "Iowa Past 7 Days Rainfall",
+ '_valid'             : "%s - %s inclusive" % ((now - mx.DateTime.RelativeDateTime(days=6)).strftime("%d %b %Y"), now.strftime("%d %b %Y") ),
+ 'lbTitleString'      : "[inch]",
+ 'pmLabelBarHeightF'  : 0.6,
+ 'pmLabelBarWidthF'   : 0.1,
+ 'lbLabelFontHeightF' : 0.025
+}
+# Generates tmp.ps
+iemplot.simple_contour(lons, lats, vals, cfg)
+
+os.system("convert -rotate -90 -trim -border 5 -bordercolor '#fff' -resize 900x700 -density 120 +repage tmp.ps tmp.png")
+os.system("/home/ldm/bin/pqinsert -p 'plot c 000000000000 summary/7day/ia_precip.png bogus png' tmp.png")
+if os.environ["USER"] == "akrherz":
+  os.system("xv tmp.png")
+os.remove("tmp.png")
+os.remove("tmp.ps")
