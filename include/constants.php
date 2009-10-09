43b4a49ddc5dfb3e2090e62274edc0cce66d1ca6
akrherz <akrherz@iastate.edu>
2009-10-09 20:11:25 +0000

    add awos

--- include/constants.php
+++ include/constants.php
@@ -1,7 +1,7 @@
 <?php
 
 $coop_archive_end = mktime(0,0,0,5,1,2009);
-$awos_archive_end = mktime(0,0,0,9,1,2009);
+$awos_archive_end = mktime(0,0,0,10,1,2009);
 $asos_archive_end = mktime(0,0,0,9,1,2009);
 $camera_refresh = 300;
 
