e8dc3e8c7652532eae58a54778428ced98b546e6
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2009-10-10 12:42:07 +0000

    Update for asos

--- include/constants.php
+++ include/constants.php
@@ -2,7 +2,7 @@
 
 $coop_archive_end = mktime(0,0,0,5,1,2009);
 $awos_archive_end = mktime(0,0,0,10,1,2009);
-$asos_archive_end = mktime(0,0,0,9,1,2009);
+$asos_archive_end = mktime(0,0,0,10,1,2009);
 $camera_refresh = 300;
 
 ?>
