79ebb467c67823ec79526fe01e0e0e7624c2f6ba
akrherz <akrherz@iastate.edu>
2009-10-11 17:41:06 +0000

    tweak a little bit

--- scripts/climodat/genSpringFall.py
+++ scripts/climodat/genSpringFall.py
@@ -1,4 +1,6 @@
-# Growing season stuff. hmmm
+"""
+Generate the length of a growing season given some base threshold
+"""
 
 
 def write(mydb, rs, stationID, base, _REPORTID):
@@ -16,18 +18,15 @@ def write(mydb, rs, stationID, base, _REPORTID):
 
   now = s
   d = {}
-  while ( now <= e):
+  while now <= e:
     ep = now + mx.DateTime.RelativeDateTime(month=12, day=1)
     sp = now + mx.DateTime.RelativeDateTime(month=1, day=1)
     d[int(now.year)] = {'sts': sp, 'ets': ep}
     now += interval
 
   for i in range(len(rs)):
-    try:
-      low = int(rs[i]["low"])
-    except:
-      continue
-    if (low > base):
+    low = int(rs[i]["low"])
+    if low > base:
       continue
     ts = mx.DateTime.strptime(rs[i]["day"], "%Y-%m-%d")
     mp = ts + mx.DateTime.RelativeDateTime(month=7, day=1)
@@ -54,10 +53,12 @@ def write(mydb, rs, stationID, base, _REPORTID):
          int(d[yr]['ets'].strftime("%j") ), \
          (d[yr]['ets'] - d[yr]['sts']).strftime("%d") ) )
 
-
   smean = sjdaytot / yrs
   emean = ejdaytot / yrs
+  sts = mx.DateTime.DateTime(2001,1,1) + mx.DateTime.RelativeDateTime(days=smean)
+  ets = mx.DateTime.DateTime(2001,1,1) + mx.DateTime.RelativeDateTime(days=emean)
 
-  out.write("             MEAN %3i              MEAN %3i     MEAN %3i\n" \
-    % (smean, emean, emean - smean) )
+  out.write("%7s%4i%6i%4i        %4i%6i%4i          %3s\n" \
+    % ("MEAN", sts.month, sts.day, smean, ets.month, ets.day, 
+       emean, emean - smean) )
   out.close()
