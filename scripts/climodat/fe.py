eed947e37c24deb18f68ea0169680fbbc94ed8b9
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2009-10-21 16:05:46 +0000

    print nicely

--- scripts/climodat/fe.py
+++ scripts/climodat/fe.py
@@ -6,6 +6,7 @@ ets = mx.DateTime.DateTime(2009,10,21)
 interval = mx.DateTime.RelativeDateTime(days=1)
 now = sts
 while now < ets:
+  print now
   os.system("/mesonet/python/bin/python daily_estimator.py %s" % (now.strftime("%Y %m %d"),))
   os.system("/mesonet/python/bin/python compute_ia0000.py %s" % (now.strftime("%Y %m %d"),))
   now += interval
