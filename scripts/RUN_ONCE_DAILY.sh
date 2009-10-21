0fc3ef42e19366d1e320ef87b2c0439efccd93ee
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2009-10-21 18:10:02 +0000

    Add climodat daily update fun

--- scripts/RUN_ONCE_DAILY.sh
+++ scripts/RUN_ONCE_DAILY.sh
@@ -10,3 +10,8 @@ cd ../outgoing
 cd ../dbutil 
 /mesonet/python/bin/python ot2archive.py
 /mesonet/python/bin/python hads_delete_dups.py
+
+cd ../climodat
+/mesonet/python/bin/python daily_estimator.py
+/mesonet/python/bin/python compute_ia0000.py
+./run.sh >& a
