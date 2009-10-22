2cb02a625b8f23f9e7b52546135c441788a8699e
akrherz <akrherz@iastate.edu>
2009-10-22 19:55:38 +0000

    add groupname

--- htdocs/json/products.php
+++ htdocs/json/products.php
@@ -7,7 +7,7 @@ require_once "$rootpath/include/database.inc.php";
 $connect = iemdb("mesosite");
 
 /* Standard Issue Products :) */
-$result = pg_exec($connect, sprintf("SELECT * from archive_products"));
+$result = pg_exec($connect, sprintf("SELECT * from archive_products ORDER by groupname, name"));
 
 $ar = Array("products" => Array() );
 for( $i=0; $row = @pg_fetch_array($result,$i); $i++)
@@ -15,21 +15,23 @@ for( $i=0; $row = @pg_fetch_array($result,$i); $i++)
   $z = Array("id" => $row["id"],
         "template" => $row["template"], 
         "name" => $row["name"], 
+        "groupname" => $row["groupname"], 
         "interval" => $row["interval"],
         "sts" => substr($row["sts"],0,10));
   $ar["products"][] = $z;
 }
 
 /* Now, lets get webcam stuff */
-$result = pg_exec($connect, sprintf("SELECT * from webcams"));
+$result = pg_exec($connect, sprintf("SELECT * from webcams ORDER by network, name"));
 
 for( $i=0; $row = @pg_fetch_array($result,$i); $i++)
 {
   $tpl = sprintf("%%Y/%%m/%%d/camera/%s/%s_%%Y%%m%%d%%H%%i.jpg", $row["id"],
          $row["id"]);
   $z = Array("id" => $row["id"],
-        "template" => $tpl, 
-        "name" => $row["name"] ." Webcam", 
+        "template" => $tpl,
+        "groupname" => $row["groupname"] ." Webcams",
+        "name" => $row["name"], 
         "interval" => 5,
         "sts" => $row["sts"]);
   $ar["products"][] = $z;
