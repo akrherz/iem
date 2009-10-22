2a96760a95d85565eaeab9faa5f85c3229a97c0d
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2009-10-22 12:24:03 +0000

    fix this as the precip data is not available

--- scripts/RUN_ONCE_DAILY.sh
+++ scripts/RUN_ONCE_DAILY.sh
@@ -1,5 +1,4 @@
 # Run once daily
-# This runs at 1 AM or so!
 
 cd qc
 /mesonet/python/bin/python dailyVtec.py
@@ -10,8 +9,3 @@ cd ../outgoing
 cd ../dbutil 
 /mesonet/python/bin/python ot2archive.py
 /mesonet/python/bin/python hads_delete_dups.py
-
-cd ../climodat
-/mesonet/python/bin/python daily_estimator.py
-/mesonet/python/bin/python compute_ia0000.py
-./run.sh >& a
