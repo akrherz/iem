8a32eb2ec7fb86f10c88c1ce5269d27bfc0e91ca
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2009-10-30 18:34:39 +0000

    update for united

--- scripts/outgoing/snet_collect.py
+++ scripts/outgoing/snet_collect.py
@@ -18,6 +18,7 @@ st.sts["SMAI4"]["name"] = "M-town"
 st.sts["SBZI4"]["name"] = "Zoo"
 st.sts["SMSI4"]["name"] = "Barnum"
 st.sts["STQI4"]["name"] = "Tama"
+st.sts["SBOI4"]["name"] = "United"
 
 
 def altiTxt(d):
