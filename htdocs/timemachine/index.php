850be5ddcb3009f20f1962cb6ae583423a6fa907
akrherz <akrherz@iastate.edu>
2009-12-04 23:28:49 +0000

    fix bugs, try to do auto advance

--- htdocs/timemachine/index.php
+++ htdocs/timemachine/index.php
@@ -6,7 +6,7 @@ $HEADEXTRA = '
 <link rel="stylesheet" type="text/css" href="http://extjs.cachefly.net/ext-3.0.3/resources/css/ext-all.css"/>
 <script type="text/javascript" src="http://extjs.cachefly.net/ext-3.0.3/adapter/ext/ext-base.js"></script>
 <script type="text/javascript" src="http://extjs.cachefly.net/ext-3.0.3/ext-all.js"></script>
-<script type="text/javascript" src="static.js?v=6"></script>
+<script type="text/javascript" src="static.js?v=7"></script>
 ';
 include("$rootpath/include/header.php"); 
 ?>
