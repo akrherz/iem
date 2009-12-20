4423f622815db5e0da733a355250cc150b8dc020
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2009-12-20 19:17:02 +0000

    auto populate please

---
+++ scripts/ingestors/dotcams/initialize.py
@@ -0,0 +1,28 @@
+
+import os
+from pyIEM import iemdb, mesonet
+i = iemdb.iemdb()
+mesosite = i['mesosite']
+import glob
+
+os.chdir("work")
+files = glob.glob("*640x480.jpg")
+for file in files:
+  cid = file[:11]
+
+  rs = mesosite.query("SELECT * from webcams WHERE id = '%s'" % (cid,)).dictresult()
+  if len(rs) > 0:
+    continue
+
+  rid = cid[6:8]
+  nwsli = mesonet.RWISconvert[ rid ]
+
+  rs = mesosite.query("SELECT name, x(geom) as lon, y(geom) as lat from stations where id = '%s'" % (nwsli,)).dictresult()
+  lon = rs[0]['lon']
+  lat = rs[0]['lat']
+  name = rs[0]['name']
+
+  sql = """insert into webcams (id, name, pan0, online, network, 
+         geom, removed, state) values ('%s', '%s', 0, 't', 'IDOT', 'SRID=4326;POINT(%s %s)', 'f', 'IA')""" % (cid, name, lon, lat)
+  mesosite.query( sql )
+
