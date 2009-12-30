0cc870792400f6de0d13cb2ebcfb8c424e7716c4
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2009-12-30 16:30:42 +0000

    Move GA into the header, where it belongs

--- include/footer.php
+++ include/footer.php
@@ -15,18 +15,5 @@ Copyright &copy; 2001-2009, Iowa State University of Science and Technology.
  &middot; <a href="<?php echo $rooturl; ?>/disclaimer.php">disclaimer</a>
 </div>
 <?php if (! isset($NOCONTENT)) echo "</div> <!-- End of iem-main -->"; ?>
-<script type="text/javascript">
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-784549-2']);
-_gaq.push(['_trackPageview']);
-(function() {
-var ga = document.createElement('script');
-ga.src = ('https:' == document.location.protocol ?
-    'https://ssl' : 'http://www') +
-    '.google-analytics.com/ga.js';
-ga.setAttribute('async', 'true');
-document.documentElement.firstChild.appendChild(ga);
-})();
-</script>
 </body>
 </html>
