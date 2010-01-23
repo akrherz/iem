e12e0200a06ba83bf119a011b027465721c01aba
akrherz <akrherz@iastate.edu>
2010-01-23 03:25:36 +0000

    fix google key?

--- htdocs/lsr/index.php
+++ htdocs/lsr/index.php
@@ -8,7 +8,7 @@ $HEADEXTRA = '<script type="text/javascript"
    href="http://extjs.cachefly.net/ext-2.2.1/resources/css/ext-all.css" />
 <link rel="stylesheet" type="text/css" 
   href="http://extjs.cachefly.net/ext-2.2.1/examples/shared/examples.css" />
-<script src="http://maps.google.com/maps?file=api&amp;v=2.x&amp;key='. $GOOGLEKEYS[$rooturl]["vtec"] .'" type="text/javascript"></script>
+<script src="http://maps.google.com/maps?file=api&amp;v=2.x&amp;key='. $GOOGLEKEYS[$rooturl]["any"] .'" type="text/javascript"></script>
 <script src="http://openlayers.org/api/2.8/OpenLayers.js"></script>
 <script type="text/javascript" src="GeoExt.js"></script>
 <script type="text/javascript" src="RowExpander.js"></script>
@@ -22,6 +22,20 @@ include("$rootpath/include/header.php");
 ?>
 <script>
 Ext.onReady(function(){
+  var tokens = window.location.href.split('#');
+  if (tokens.length == 2){
+    var tokens2 = tokens[1].split("/");
+    if (tokens2.length == 3){
+      Ext.getCmp("wfoselector").setValues( tokens2[0].split(",") );
+      utc_start  = Date.parseDate(tokens2[1], 'YmdHi');
+      Ext.getCmp("datepicker1").setValue( utc_start.fromUTC() );
+      Ext.getCmp("timepicker1").setValue( utc_start.fromUTC() );
+      utc_end    = Date.parseDate(tokens2[2], 'YmdHi');
+      Ext.getCmp("datepicker2").setValue( utc_end.fromUTC() );
+      Ext.getCmp("timepicker2").setValue( utc_end.fromUTC() );
+      Ext.getCmp('refresh').fireEvent('click', {});
+    }
+  }
 });
 </script>
 </body></html>
