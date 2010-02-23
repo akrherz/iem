60d1e74be6e2ad41152fd33aaaf002df7b576ad2
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2010-02-23 00:47:00 +0000

    Support multi-year download and optional wfo specifier

--- cgi-bin/request/gis/lsr.py
+++ cgi-bin/request/gis/lsr.py
@@ -13,7 +13,13 @@ mydb.query("SET TIME ZONE 'GMT'")
 
 # Get CGI vars
 form = cgi.FormContent()
-year = int(form["year"][0])
+
+if form.has_key('year'):
+  year1 = int(form["year"][0])
+  year2 = int(form["year"][0])
+else:
+  year1 = int(form["year1"][0])
+  year2 = int(form["year2"][0])
 month1 = int(form["month1"][0])
 if (not form.has_key("month2")):  sys.exit()
 month2 = int(form["month2"][0])
@@ -24,8 +30,13 @@ hour2 = int(form["hour2"][0])
 minute1 = int(form["minute1"][0])
 minute2 = int(form["minute2"][0])
 
-sTS = mx.DateTime.DateTime(year, month1, day1, hour1, minute1)
-eTS = mx.DateTime.DateTime(year, month2, day2, hour2, minute2)
+wfoLimiter = ""
+if form.has_key('wfo[]'):
+  aWFO = form['wfo[]']
+  wfoLimiter = " and wfo in %s " % ( str( tuple(aWFO) ), )
+
+sTS = mx.DateTime.DateTime(year1, month1, day1, hour1, minute1)
+eTS = mx.DateTime.DateTime(year2, month2, day2, hour2, minute2)
 
 os.chdir("/tmp/")
 fp = "lsr_%s_%s" % (sTS.strftime("%Y%m%d%H%M"), eTS.strftime("%Y%m%d%H%M") )
@@ -44,9 +55,10 @@ dbf.add_field("SOURCE", dbflib.FTString, 40, 0)
 dbf.add_field("REMARK", dbflib.FTString, 400, 0)
 
 
-sql = "SELECT distinct *, astext(geom) as tgeom from lsrs_%s WHERE \
-	valid >= '%s' and valid < '%s' \
-	ORDER by valid ASC" % (sTS.year, sTS.strftime("%Y-%m-%d %H:%M"), eTS.strftime("%Y-%m-%d %H:%M") )
+sql = "SELECT distinct *, astext(geom) as tgeom from lsrs WHERE \
+	valid >= '%s' and valid < '%s' %s \
+	ORDER by valid ASC" % (sTS.strftime("%Y-%m-%d %H:%M"), 
+    eTS.strftime("%Y-%m-%d %H:%M"), wfoLimiter )
 rs = mydb.query(sql).dictresult()
 
 cnt = 0
