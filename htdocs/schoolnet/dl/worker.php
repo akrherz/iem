362dc4fa54151acfa7993fcf6f66a6faee32f18f
akrherz <akrherz@iastate.edu>
2010-03-01 02:04:38 +0000

    support multi-year download please

--- htdocs/schoolnet/dl/worker.php
+++ htdocs/schoolnet/dl/worker.php
@@ -3,21 +3,22 @@ include("../../../config/settings.inc.php");
 // Does the work of processing the download request...
 include("$rootpath/include/database.inc.php");
 
-$s_hour = isset($_GET["s_hour"])? $_GET["s_hour"]: die("No s_hour");
-$e_hour = isset($_GET["e_hour"])? $_GET["e_hour"]: die("No e_hour");
-$s_day = isset($_GET["s_day"])? $_GET["s_day"]: die("No s_day");
-$e_day = isset($_GET["e_day"])? $_GET["e_day"]: die("No e_day");
+$year1 = isset($_GET["year1"])? $_GET["year1"]: die("No year1");
+$year2 = isset($_GET["year2"])? $_GET["year2"]: die("No year2");
+$hour1 = isset($_GET["hour1"])? $_GET["hour1"]: die("No hour1");
+$hour2 = isset($_GET["hour2"])? $_GET["hour2"]: die("No hour2");
+$day1 = isset($_GET["day1"])? $_GET["day1"]: die("No day1");
+$day2 = isset($_GET["day2"])? $_GET["day2"]: die("No day2");
 $month1 = isset($_GET["month1"])? $_GET["month1"]: die("No month1");
 $month2 = isset($_GET["month2"])? $_GET["month2"]: die("No month2");
-$year = isset($_GET["year"])? $_GET["year"]: die("No year");
 $vars = isset($_GET["vars"])? $_GET["vars"]: die("No vars");
 $sample = isset($_GET["sample"])? $_GET["sample"]: die("No Sample");
 $dl_option = isset($_GET["dl_option"])? $_GET["dl_option"]: die("No dl_option");
 $delim = isset($_GET["delim"]) ? $_GET["delim"]: die("No delim");
 
-$ts1 = mktime($s_hour, 0, 0, $month1, $s_day, $year) or 
+$ts1 = mktime($hour1, 0, 0, $month1, $day1, $year1) or 
   die("Invalid Date Format");
-$ts2 = mktime($e_hour, 0, 0, $month2, $e_day, $year) or
+$ts2 = mktime($hour2, 0, 0, $month2, $day2, $year2) or
   die("Invalid Date Format");
 
 if ($ts1 >= $ts2){
@@ -38,7 +39,6 @@ for ($i=0; $i< $num_vars;$i++){
 
 $sqlTS1 = strftime("%Y-%m-%d %H:%M", $ts1);
 $sqlTS2 = strftime("%Y-%m-%d %H:%M", $ts2);
-$table = strftime("t%Y", $ts1);
 $nicedate = strftime("%Y-%m-%d", $ts1);
 
 $sampleStr = Array("1min" => "1",
@@ -61,7 +61,7 @@ $stationString .= ")";
 
 
 
-$sqlStr .= "to_char(valid, 'YYYY-MM-DD HH24:MI') as dvalid from ".$table ;
+$sqlStr .= "to_char(valid, 'YYYY-MM-DD HH24:MI') as dvalid from alldata";
 $sqlStr .= " WHERE valid >= '".$sqlTS1."' and valid <= '".$sqlTS2 ."' ";
 $sqlStr .= " and station IN ". $stationString ." and ";
 $sqlStr .= " extract(minute from valid)::int % ".$sampleStr[$sample] ." = 0 ";
