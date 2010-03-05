d2b37d521aaa02d40edd4d20e695875da7efdf99
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2010-03-05 22:16:25 +0000

    another test refs #1

--- htdocs/GIS/apps/adjust/adjust.php
+++ htdocs/GIS/apps/adjust/adjust.php
@@ -34,19 +34,22 @@ function mktitle($map, $imgObj, $titlet) {
 dl($mapscript);
 
 $map = ms_newMapObj("stations.map");
-$map->setExtent(-98,40.5,-89,43.5);
-$map->set("height", 768);
-$map->set("width",  1024);
+//iowa $map->setExtent(-98,40.5,-89,43.5);
+//$map->setExtent(-110,26,-72,50);
+//CLAYTON $map->setExtent(-92,42.5,-91.1,43.3);
+$map->setExtent(-93.5,42.0,-92.8,42.6);
+$map->set("height", 240);
+$map->set("width",  320);
 $map->selectOutputFormat("PNG24");
 
 $namer = $map->getlayerbyname("namerica");
-$namer->set("status", MS_ON);
+$namer->set("status", MS_OFF);
 
 $lakes = $map->getlayerbyname("lakes");
-$lakes->set("status", MS_ON);
+$lakes->set("status", MS_OFF);
 
 $states = $map->getlayerbyname("states");
-$states->set("status", MS_ON);
+$states->set("status", MS_OFF);
 
 $iembox = $map->getlayerbyname("iembox");
 $iembox->set("status", MS_ON);
@@ -54,21 +57,25 @@ $iembox->set("status", MS_ON);
 $counties = $map->getlayerbyname("counties");
 $counties->set("status", MS_ON);
 
-$aeronet = $map->getlayerbyname("aeronet");
-$aeronet->set("status", MS_ON);
+$terra = $map->getlayerbyname("terra");
+$terra->set("status", MS_ON);
+
+$maxdbz = $map->getlayerbyname("maxdbz");
+$maxdbz->set("status", MS_ON);
+$maxdbz->set("transparency", 70);
 
 $bars = $map->getlayerbyname("bars");
 $bars->set("status", MS_ON);
 
 $dm = $map->getlayerbyname("dm");
-$dm->set("status", MS_ON);
+$dm->set("status", MS_OFF);
 
 $warnings0_c = $map->getlayerbyname("warnings0_c");
-$warnings0_c->set("status", MS_ON);
+$warnings0_c->set("status", MS_OFF);
 $warnings0_c->set("data", "g from (select phenomena, eventid, multi(geomunion(geom)) as g from warnings_2008 WHERE significance = 'A' and phenomena IN ('TO','SV') and issue < '2008-04-10 20:00' and expire > '2008-04-10 20:00' GROUP by phenomena, eventid ORDER by phenomena ASC) as foo using SRID=4326 using unique phenomena");
 
 $cwa = $map->getlayerbyname("cwa");
-$cwa->set("status", MS_ON);
+$cwa->set("status", MS_OFF);
 
 /*
 $cwa->queryByAttributes("WFO", "DMX", MS_SINGLE);
@@ -87,14 +94,14 @@ $rect->project($projin, $projout);
 //  $rect->maxx + 50000, $rect->maxy + 50000);
 
 $locs = $map->getlayerbyname("locs");
-$locs->set("status", MS_ON);
+$locs->set("status", MS_OFF);
 
 $cities = $map->getlayerbyname("sites");
 $cities->set("status", MS_OFF);
 
 $watches = $map->getlayerbyname("watches");
 $watches->set("status", MS_OFF);
-$watches->set("data", "geom from (select type as wtype, geom, oid from watches WHERE extract(year from expired) = 2005 and type = 'TOR' ORDER by type ASC) as foo");
+$watches->set("data", "geom from (select type as wtype, geom, oid from watches WHERE extract(year from expired) = 2009 ORDER by type ASC) as foo");
 
 $iards = $map->getlayerbyname("iards");
 $iards->set("status", MS_OFF);
@@ -108,17 +115,18 @@ $namer->draw($img);
 //$iards_label->draw($img);
 //$dm->draw($img);
 $lakes->draw($img);
-//$watches->draw($img);
-$aeronet->draw($img);
+$watches->draw($img);
+$terra->draw($img);
+$maxdbz->draw($img);
 $counties->draw($img);
 $states->draw($img);
-$locs->draw($img);
+//$locs->draw($img);
 //$iembox->draw($img);
 
 $map->drawLabelCache($img);
 //$bars->draw($img);
 
-mktitle($map, $img, " 6 Apr 2009 - GOES East Visible + High Temps");
+mktitle($map, $img, " 9 Aug Max NEXRAD + 12 Aug Terra MODIS");
 //mkl($map, $img);
 
 $url = $img->saveWebImage();
