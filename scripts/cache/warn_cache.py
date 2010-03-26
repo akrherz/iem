53d56187f50d0f22b396d2ccb7000e27cd0cde53
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2010-03-26 12:32:05 +0000

    Add cache script for the warnings

---
+++ scripts/cache/warn_cache.py
@@ -0,0 +1,16 @@
+# Need something to cache the warnings GIS files, since they are so 
+# huge.
+
+import os
+
+FINAL = "/mesonet/share/pickup/wwa/"
+
+for year in range(1986,2010):
+  cmd = "wget -q -O %s/%s_all.zip http://localhost/cgi-bin/request/gis/watchwarn.py?year1=%s&month1=1&day1=1&hour1=0&minute1=0&year2=%s&month2=1&day2=1&hour2=0&minute2=0" % (FINAL, year,  year, year+1)
+  os.system(cmd)
+  cmd = "wget -q -O %s/%s_tsmf.zip http://localhost/cgi-bin/request/gis/watchwarn.py?year1=%s&month1=1&day1=1&hour1=0&minute1=0&year2=%s&month2=1&day2=1&hour2=0&minute2=0&limite0=yes"  % (FINAL, year,  year, year+1)
+  os.system(cmd)
+  if year > 2001:
+    cmd = "wget -q -O %s/%s_tsmf_sbw.zip http://localhost/cgi-bin/request/gis/watchwarn.py?year1=%s&month1=1&day1=1&hour1=0&minute1=0&year2=%s&month2=1&day2=1&hour2=0&minute2=0&limit0=yes&limit1=yes"  % (FINAL, year,  year, year+1)
+
+    os.system(cmd)
