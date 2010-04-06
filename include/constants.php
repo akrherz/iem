5709569bb78ea7a9965654960411b6a6ba019971
akrherz <akrherz@iastate.edu>
2010-04-06 18:44:39 +0000

    manual still, but that is okay

--- include/constants.php
+++ include/constants.php
@@ -3,6 +3,6 @@
 $coop_archive_end = mktime(0,0,0,10,1,2009);
 $awos_archive_end = mktime(0,0,0,3,1,2010);
 $asos_archive_end = mktime(0,0,0,4,1,2010);
-$camera_refresh = 300;
+$camera_refresh = 60;
 
 ?>
