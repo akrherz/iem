91700737ff6ddb5de62777144b42f0ded20238cd
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2010-04-06 12:48:07 +0000

    bug fixes galore!

--- htdocs/timemachine/index.php
+++ htdocs/timemachine/index.php
@@ -3,14 +3,14 @@ include("../../config/settings.inc.php");
 $TITLE = "IEM | Time Machine";
 $THISPAGE="archive-tm";
 $HEADEXTRA = '
-<link rel="stylesheet" type="text/css" href="http://extjs.cachefly.net/ext-3.1.0/resources/css/ext-all.css"/>
-<script type="text/javascript" src="http://extjs.cachefly.net/ext-3.1.0/adapter/ext/ext-base.js"></script>
-<script type="text/javascript" src="http://extjs.cachefly.net/ext-3.1.0/ext-all.js"></script>
-<script type="text/javascript" src="static.js?v=9"></script>
+<link rel="stylesheet" type="text/css" href="http://extjs.cachefly.net/ext-3.2.0/resources/css/ext-all.css"/>
+<script type="text/javascript" src="http://extjs.cachefly.net/ext-3.2.0/adapter/ext/ext-base.js"></script>
+<script type="text/javascript" src="http://extjs.cachefly.net/ext-3.2.0/ext-all.js"></script>
+<script type="text/javascript" src="static.js?v=11"></script>
 ';
 include("$rootpath/include/header.php"); 
 ?>
 <p style="margin-bottom: 5px;"><strong>IEM Time Machine:</strong> Adjust sliders to select a product of your choice from the archive.</p>
 <div id="theform"></div>
-<img id="imagedisplay" src="../data/mesonet.gif" />
+<img id="imagedisplay" src="timemachine.png" />
 <?php include("$rootpath/include/footer.php"); ?>
