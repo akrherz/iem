a09084c302ac381ff58fd3445e5a67fea255c3f5
akrherz <akrherz@iastate.edu>
2010-04-07 14:46:11 +0000

    oopsy, need to be updating this

--- include/constants.php
+++ include/constants.php
@@ -1,6 +1,6 @@
 <?php
 
-$coop_archive_end = mktime(0,0,0,10,1,2009);
+$coop_archive_end = mktime(0,0,0,12,1,2009);
 $awos_archive_end = mktime(0,0,0,3,1,2010);
 $asos_archive_end = mktime(0,0,0,4,1,2010);
 $camera_refresh = 300;
