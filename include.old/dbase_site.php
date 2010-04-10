f8f29b55b26f15e2497e5c971b7e0fbf349fd4e2
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2010-04-10 21:40:06 +0000

    more cruft

--- include.old/dbase_site.php
+++
@@ -1,6 +0,0 @@
-  $connection = pg_connect("localhost", 5432, "mesosite");
-  $rs = pg_exec($connection, "SELECT *  from stations WHERE id = '". $id ."' ");
-  pg_close($connection);
-
-  $row = @pg_fetch_array($rs,0);
-
