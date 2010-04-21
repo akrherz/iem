b5c0c28ae1c911664f8d1b11292f901fcc230f7b
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2010-04-21 15:28:23 +0000

    migrate rwis to archive script here

--- scripts/RUN_0Z.sh
+++ scripts/RUN_0Z.sh
@@ -1,3 +1,7 @@
 
 cd summary
 /mesonet/python/bin/python max_reflect.py
+
+# Rerun today
+cd ../dbutil
+/mesonet/python/bin/python rwis2archive.py 1
--- scripts/RUN_MIDNIGHT.sh
+++ scripts/RUN_MIDNIGHT.sh
@@ -3,3 +3,6 @@
 
 cd qc
 /mesonet/python/bin/python fixSNETPrecip.py
+
+cd ../dbutil
+/mesonet/python/bin/python rwis2archive.py
