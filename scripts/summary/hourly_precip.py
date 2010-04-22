02a4f4efa2cad835931723826e1a4b2864faaa4a
akrherz <akrherz@iastate.edu>
2010-04-22 20:20:17 +0000

    we want zeros, sigh

--- scripts/summary/hourly_precip.py
+++ scripts/summary/hourly_precip.py
@@ -12,7 +12,7 @@ t1 = mx.DateTime.utc() + mx.DateTime.RelativeDateTime(hours=-0,minute=0)
 sql = "INSERT into hourly_%s \
        (SELECT station, network, '%s+00'::timestamp as v, max(phour) as p \
         from current_log WHERE valid >= '%s' \
-        and valid < '%s' and phour > 0 and \
+        and valid < '%s' and phour >= 0 and \
         network NOT IN ('KCCI','KELO','KIMT') GROUP by station, network, v)" % \
        (t0.year, t0.strftime("%Y-%m-%d %H:%M"), t0.strftime("%Y-%m-%d %H:%M"), \
         t1.strftime("%Y-%m-%d %H:%M") )
