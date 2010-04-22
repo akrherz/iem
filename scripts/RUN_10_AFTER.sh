9a030f8bc31f9fba92aa9dba42eec2a65201307c
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2010-04-22 20:12:47 +0000

    change the name, so that I know what I am doing!

--- scripts/RUN_10_AFTER.sh
+++ scripts/RUN_10_AFTER.sh
@@ -9,7 +9,7 @@ cd ../current
 /mesonet/python/bin/python plot_hilo.py 0
 
 cd ../summary
-/mesonet/python/bin/python snet_hourly_precip.py
+/mesonet/python/bin/python hourly_precip.py
 /mesonet/python/bin/python update_snet_precip.py
 
 cd ../week
