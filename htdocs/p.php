2107a044d11bedaa22f0b22c793b1e51bd20121c
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2010-04-23 00:51:53 +0000

    minor cleanup and double check

--- htdocs/p.php
+++ htdocs/p.php
@@ -1,4 +1,8 @@
 <?php
+/* Simple program to simply print out a product, if the product has a 
+ * geometry, present it in a map as well.  iembot generates links to this
+ * app
+ */
 include("../config/settings.inc.php");
 include("$rootpath/include/database.inc.php");
 
@@ -7,19 +11,19 @@ $pid = isset($_GET['pid']) ? substr($_GET['pid'],0,32) : "";
 if ($id == "" && $pid == "") die();
 
 $conn = iemdb("postgis");
-$rs = pg_prepare($conn, "IDUPDATE", "update text_products 
-                 SET reads = reads + 1 WHERE id = $1");
-$rs = pg_prepare($conn, "PIDUPDATE", "update text_products 
-                 SET reads = reads + 1 WHERE product_id = $1");
-$rs = pg_prepare($conn, "IDSELECT", "SELECT product,geom from text_products 
-                 WHERE id = $1");
-$rs = pg_prepare($conn, "PIDSELECT", "SELECT product,geom from text_products 
-                 WHERE product_id = $1");
 
 if ($id != "") {
+ $rs = pg_prepare($conn, "IDUPDATE", "update text_products 
+                 SET reads = reads + 1 WHERE id = $1");
+ $rs = pg_prepare($conn, "IDSELECT", "SELECT product,geom from text_products 
+                 WHERE id = $1");
  pg_execute($conn, "IDUPDATE", Array($id));
  $rs = pg_execute($conn, "IDSELECT", Array($id));
 } else {
+ $rs = pg_prepare($conn, "PIDUPDATE", "update text_products 
+                 SET reads = reads + 1 WHERE product_id = $1");
+ $rs = pg_prepare($conn, "PIDSELECT", "SELECT product,geom from text_products 
+                 WHERE product_id = $1");
  pg_execute($conn, "PIDUPDATE", Array($pid));
  $rs = pg_execute($conn, "PIDSELECT", Array($pid));
 }
