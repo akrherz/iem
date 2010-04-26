c1e884bd4f3b8a62b8795daf68b96a535a0f0a8b
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2010-04-26 15:00:30 +0000

    cleanups, what a sad state I have things in :(

--- htdocs/schoolnet/KCCI.php
+++ htdocs/schoolnet/KCCI.php
@@ -15,42 +15,10 @@
  <li><a href="<?php echo $rooturl; ?>/GIS/apps/mesoplot/plot.php?zoom=1">Zoomed in on Des Moines</a></li>
  <li><a href="<?php echo $rooturl; ?>/GIS/apps/delta/plot.php?i=15m">15min Pressure Change</a></li>
  <li><a href="<?php echo $rooturl; ?>/GIS/apps/delta/plot.php">1 hour Pressure Change</a></li>
- <li><a href="/data/snet/solarRad.gif">Solar Radiation</a></li>
- <li>Barometer: <a href="/data/snet/snet_altm.gif">millibar</a> or <a href="/data/snet/snet_alti.gif">inches</a></li>
-<!-- Wait until this works
- <li><a href="/data/snet/snetGust.gif">Peak Gust Today</a></li>
--->
- <li><a href="/data/snetRADAR_0.gif">Des Moines NEXRAD Overlay</a>  
-  [<a href="/data/20snetloop.html">Loop</a>]</li>
-</ul>
-
-<p><h3 class="subtitle">Comparisons</h3>
-<ul>
- <li><a href="<?php echo $rooturl; ?>/data/snet/Tcompare.gif">Temperatures</a></li>
- <li><a href="<?php echo $rooturl; ?>/data/snet/Dcompare.gif">Dew Points</a></li>
- <li>Barometer: <a href="<?php echo $rooturl; ?>/data/snet/Pcompare.gif">millibar</a> or <a href="/data/snet/P2compare.gif">inches</a></li>
 </ul>
 
 </td><td valign="top" width="350">
 
-<p>
-  <div class="snet-precip-table">
-     <b>Daily Precipitation Totals</b>
-  <div style="background: white; padding: 3px;">
-  <a href="<?php echo $rooturl; ?>/data/snet/precToday.gif">Today</a> &nbsp; 
-  <?php
-  echo "<a href=\"$rooturl/archive/data/". date("Y/m/d/", (date("U") - 86400 ) )
-    ."snetPrec.gif\">Yesterday</a> &nbsp; \n";
-  for ($i=2;$i<8;$i++){
-    echo "<a href=\"$rooturl/archive/data/". date("Y/m/d/", (date("U") - $i*86400 ) )
-    ."snetPrec.gif\">". date("M d", (date("U") - $i*86400 ) )  ."</a> &nbsp; \n";
-  }
-  ?>
-   <br><a href="<?php echo $rooturl; ?>/data/snet/precMonth.gif">This Month</a>
-
-  </div>
-  </div>
-  
   <p><h3 class="subtitle">Historical Data</h3>
 <ul>
  <li><a href="<?php echo $rooturl; ?>/schoolnet/dl/">Download</a> from the archive!</a></li>
