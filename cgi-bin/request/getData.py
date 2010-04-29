9218580dc25beea416df5b74023e2c3a6af7f85c
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2010-04-29 17:36:41 +0000

    Support downloading the METAR data and clean it up some, geez

--- cgi-bin/request/getData.py
+++ cgi-bin/request/getData.py
@@ -6,11 +6,10 @@ import cgi, re, string, sys, mx.DateTime
 from pyIEM import iemdb
 i = iemdb.iemdb()
 asosdb = i['asos']
-rwisdb = i['rwis']
 mesositedb = i['mesosite']
 
 def Main():
-    print 'Content-type: text/plain \n\n'
+    print "Content-type: text/plain \n\n",
     form = cgi.FormContent()
     if (not form.has_key("station")):
         print "Station Not defined.  Error"
@@ -24,61 +23,54 @@ def Main():
             gisextra.append( "%.4f" % (rs[0]['lon'],) )
             gisextra.append( "%.4f" % (rs[0]['lat'],) )
     dataVars = form["data"]
-    year = int(form["year"][0])
+    if form.has_key("year"):
+      year1 = int(form["year"][0])
+      year2 = int(form["year"][0])
+    else:
+      year1 = int(form["year1"][0])
+      year2 = int(form["year2"][0])
     month1 = int(form["month1"][0])
     month2 = int(form["month2"][0])
     day1 = int(form["day1"][0])
     day2 = int(form["day2"][0])
     try:
-        sTS = mx.DateTime.DateTime(year, month1, day1)
-        eTS = mx.DateTime.DateTime(year, month2, day2)
+        sTS = mx.DateTime.DateTime(year1, month1, day1)
+        eTS = mx.DateTime.DateTime(year2, month2, day2)
     except:
         print "MALFORMED DATE:"
         sys.exit()
 
     delim = form["format"][0]
     tz = form["tz"][0]
-    tableName = "t%s" % ( sTS.year, )
     timeRange = form["timeRange"][0]
-    include20s = form["include20s"][0]
     mydb = asosdb
-    if len(station) > 3:
-        mydb = rwisdb
 
-    timeConstraint = ""
-    if (include20s == "no"):
-        timeConstraint = " and date_part('minute', valid) = 0 "
+    if timeRange == "allYear":
+        sts = sTS + mx.DateTime.RelativeDateTime(month=1,day=1)
+        ets = sTS + mx.DateTime.RelativeDateTime(month=1,day=1,years=1)
+    elif timeRange == "allMonth":
+        sts = sTS + mx.DateTime.RelativeDateTime(day=1)
+        ets = sTS + mx.DateTime.RelativeDateTime(months=1,day=1)
+    elif timeRange == "rangeDays":
+        sts = sTS
+        ets = eTS
 
-    if (timeRange == "allYear"):
-        timeStr = "date_part('year', valid) = %s" % ( sTS.year, )
-    elif (timeRange == "allMonth"):
-        timeStr = "date_part('month', valid) = %s" % (sTS.month, )
-    elif (timeRange == "rangeMonth"):
-        timeStr = "date(valid) >= '%s' and date(valid) <= '%s'" % (sTS.strftime("%Y-%m-01"), (eTS + mx.DateTime.RelativeDateTime(months=+1, days=-1)).strftime("%Y-%m-%d") ) 
-    elif (timeRange == "allDay"):
-        timeStr = "date(valid) = '%s'" % (sTS.strftime("%Y-%m-%d"),)
-    elif (timeRange == "rangeDays"):
-        timeStr = "date(valid) >= '%s' and date(valid) <= '%s' " % (sTS.strftime("%Y-%m-%d"), eTS.strftime("%Y-%m-%d") )
-
-    if (dataVars[0] == "all" and len(station) == 3):
-        queryCols = "tmpf, dwpf, drct, sknt, p01m,alti,vsby"
-        outCols = ['tmpf','dwpf','drct','sknt','p01m','alti','vsby']
-    elif (dataVars[0] == "all"):
-        queryCols = "tmpf, dwpf, drct, sknt"
-        outCols = ['tmpf','dwpf','drct','sknt']
-
+    if dataVars[0] == "all":
+        queryCols = "tmpf, dwpf, drct, sknt, p01m, alti, vsby, gust, skyc1, skyc2, skyc3, skyl1, skyl2, skyl3, metar"
+        outCols = ['tmpf','dwpf','drct','sknt','p01m','alti','vsby', 'gust',
+          'skyc1', 'skyc2', 'skyc3', 'skyl1', 'skyl2', 'skyl3', 'metar']
     else:
         dataVars = tuple(dataVars)
         outCols = dataVars
         dataVars =  str(dataVars)[1:-2]
         queryCols = re.sub("'", " ", dataVars)
 
-    queryStr = "SELECT station, valid, "+ queryCols +" from "+ tableName +" WHERE "+ timeStr +" and station = '"+ station +"' "+ timeConstraint +" ORDER by valid"
+    queryStr = """SELECT station, valid, %s from alldata 
+      WHERE valid >= '%s' and valid < '%s' and station = '%s' 
+      ORDER by valid ASC""" % (queryCols, sts.strftime("%Y-%m-%d"),
+      ets.strftime("%Y-%m-%d"), station)
 
-    if delim == "space":
-        rD = " "
-        queryCols = re.sub("," , " ", queryCols)
-    elif delim == "tab":
+    if delim == "tab":
         rD = "\t"
         queryCols = re.sub("," , "\t", queryCols) 
     else:
@@ -100,13 +92,15 @@ def Main():
     print queryCols
 
     for i in range(len(rs)):
-        print rs[i]["station"] + rD ,
-        print rs[i]["valid"] + rD , 
-        print rD.join( gisextra ),
+        sys.stdout.write( rs[i]["station"] + rD )
+        sys.stdout.write( rs[i]["valid"] + rD )
+        sys.stdout.write( rD.join( gisextra ) )
         for data1 in outCols:
-            if rs[i][ data1 ] is None or rs[i][ data1 ] <= -99.0:
-                print "M%s" % (rD,),
+            if data1 in ["metar","skyc1","skyc2","skyc3"]:
+                sys.stdout.write("%s%s" % (rs[i][data1], rD))
+            elif rs[i][ data1 ] is None or rs[i][ data1 ] <= -99.0:
+                sys.stdout.write("M%s" % (rD,))
             else:  
-                print "%2.2f%s" % (rs[i][ data1 ], rD),
+                sys.stdout.write("%2.2f%s" % (rs[i][ data1 ], rD))
         print
 Main()
