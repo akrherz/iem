f23f9a6915fb751093303f7c3087d299f5ac951d
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2010-05-05 13:38:09 +0000

    update for awos too now

--- include/constants.php
+++ include/constants.php
@@ -1,7 +1,7 @@
 <?php
 
 $coop_archive_end = mktime(0,0,0,12,1,2009);
-$awos_archive_end = mktime(0,0,0,4,1,2010);
+$awos_archive_end = mktime(0,0,0,5,1,2010);
 $asos_archive_end = mktime(0,0,0,5,1,2010);
 $camera_refresh = 300;
 
