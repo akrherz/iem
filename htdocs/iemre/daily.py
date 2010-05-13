93d4e28e818b8083e3af93a88cd0d1ad85b06768
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2010-05-13 16:33:28 +0000

    Add initial iemre web tool for people to test out

---
+++ htdocs/iemre/daily.py
@@ -0,0 +1,35 @@
+#!/mesonet/python/bin/python
+
+import sys
+sys.path.insert(0, "../../scripts/iemre/")
+
+import cgi
+import constants
+import netCDF3
+import mx.DateTime
+import simplejson
+
+form = cgi.FormContent()
+ts = mx.DateTime.strptime( form["date"][0], "%Y-%m-%d")
+lat = float( form["lat"][0] )
+lon = float( form["lon"][0] )
+format = form["format"][0]
+
+i,j = constants.find_ij(lon, lat)
+offset = int((ts - (ts + mx.DateTime.RelativeDateTime(month=1,day=1))).days)
+
+nc = netCDF3.Dataset("/mnt/mesonet/data/iemre/%s_daily.nc" % (ts.year,), 'r')
+
+res = {'data': [], }
+
+res['data'].append({
+  'daily_high_f': "%.1f" % (
+       constants.k2f(nc.variables['high_tmpk'][offset,j,i]),),
+  'daily_low_f': "%.1f" % (
+       constants.k2f(nc.variables['low_tmpk'][offset,j,i]),),
+  'daily_precip_in': "%.2f" % (
+       nc.variables['p01d'][offset,j,i] / 25.4,),
+})
+
+print 'Content-type: text/plain\n'
+print simplejson.dumps( res )
