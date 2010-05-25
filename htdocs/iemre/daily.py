d0677920f04a4692ba8e47f269cd1689c38dda72
akrherz <akrherz@iastate.edu>
2010-05-25 17:52:04 +0000

    add climatology to the JSON output

--- htdocs/iemre/daily.py
+++ htdocs/iemre/daily.py
@@ -24,15 +24,26 @@ fp = "/mnt/mesonet/data/iemre/%s_daily.nc" % (ts.year,)
 if os.path.isfile(fp):
   nc = netCDF3.Dataset("/mnt/mesonet/data/iemre/%s_daily.nc" % (ts.year,), 'r')
 
+  c2000 = ts + mx.DateTime.RelativeDateTime(year=2000)
+  coffset = int((c2000 - (mx.DateTime.DateTime(2000,1,1))).days)
+  cnc = netCDF3.Dataset("/mnt/mesonet/data/iemre/dailyc.nc", 'r')
 
   res['data'].append({
     'daily_high_f': "%.1f" % (
        constants.k2f(nc.variables['high_tmpk'][offset,j,i]),),
+    'climate_daily_high_f': "%.1f" % (
+       constants.k2f(cnc.variables['high_tmpk'][coffset,j,i]),),
     'daily_low_f': "%.1f" % (
        constants.k2f(nc.variables['low_tmpk'][offset,j,i]),),
+    'climate_daily_low_f': "%.1f" % (
+       constants.k2f(cnc.variables['low_tmpk'][coffset,j,i]),),
     'daily_precip_in': "%.2f" % (
        nc.variables['p01d'][offset,j,i] / 25.4,),
+    'climate_daily_precip_in': "%.2f" % (
+       cnc.variables['p01d'][coffset,j,i] / 25.4,),
   })
+  nc.close()
+  cnc.close()
 
 print 'Content-type: text/plain\n'
 print simplejson.dumps( res )
