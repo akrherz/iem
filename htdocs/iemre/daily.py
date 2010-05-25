263c6457ee63cb31fb0689e44c74f2587d16555b
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2010-05-25 17:29:57 +0000

    more documentation and prevent ISE for missing file

--- htdocs/iemre/daily.py
+++ htdocs/iemre/daily.py
@@ -2,7 +2,7 @@
 
 import sys
 sys.path.insert(0, "../../scripts/iemre/")
-
+import os
 import cgi
 import constants
 import netCDF3
@@ -18,18 +18,21 @@ format = form["format"][0]
 i,j = constants.find_ij(lon, lat)
 offset = int((ts - (ts + mx.DateTime.RelativeDateTime(month=1,day=1))).days)
 
-nc = netCDF3.Dataset("/mnt/mesonet/data/iemre/%s_daily.nc" % (ts.year,), 'r')
-
 res = {'data': [], }
 
-res['data'].append({
-  'daily_high_f': "%.1f" % (
+fp = "/mnt/mesonet/data/iemre/%s_daily.nc" % (ts.year,)
+if os.path.isfile(fp):
+  nc = netCDF3.Dataset("/mnt/mesonet/data/iemre/%s_daily.nc" % (ts.year,), 'r')
+
+
+  res['data'].append({
+    'daily_high_f': "%.1f" % (
        constants.k2f(nc.variables['high_tmpk'][offset,j,i]),),
-  'daily_low_f': "%.1f" % (
+    'daily_low_f': "%.1f" % (
        constants.k2f(nc.variables['low_tmpk'][offset,j,i]),),
-  'daily_precip_in': "%.2f" % (
+    'daily_precip_in': "%.2f" % (
        nc.variables['p01d'][offset,j,i] / 25.4,),
-})
+  })
 
 print 'Content-type: text/plain\n'
 print simplejson.dumps( res )
