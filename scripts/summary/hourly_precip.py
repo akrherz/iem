94ae26e9fdc41c3bf5d6ba1f35e8a9b2442071b0
akrherz <akrherz@iastate.edu>
2010-05-26 15:07:44 +0000

    fix DCP issue with the top of the hour value being for the past hour

--- scripts/summary/hourly_precip.py
+++ scripts/summary/hourly_precip.py
@@ -11,8 +11,8 @@ t1 = mx.DateTime.utc() + mx.DateTime.RelativeDateTime(hours=-0,minute=0)
 
 sql = "INSERT into hourly_%s \
        (SELECT station, network, '%s+00'::timestamp as v, max(phour) as p \
-        from current_log WHERE valid >= '%s' \
-        and valid < '%s' and phour >= 0 and \
+        from current_log WHERE (valid - '1 minute'::interval) >= '%s' \
+        and (valid - '1 minute'::interval) < '%s' and phour >= 0 and \
         network NOT IN ('KCCI','KELO','KIMT') GROUP by station, network, v)" % \
        (t0.year, t0.strftime("%Y-%m-%d %H:%M"), t0.strftime("%Y-%m-%d %H:%M"), \
         t1.strftime("%Y-%m-%d %H:%M") )
--- scripts/summary/updateRain.py
+++ scripts/summary/updateRain.py
@@ -1,7 +1,3 @@
-# 9 Oct 2003	Include the other ASOS networks
-# 15 Jun 2004	Local day!
-# 10 Feb 2005	Fix summary table
-
 from pyIEM import iemAccess, iemAccessDatabase
 iemdb = iemAccessDatabase.iemAccessDatabase()
 import mx.DateTime
@@ -11,7 +7,7 @@ yyyy = now.year
 
 rs = iemdb.query("select station, sum(rain) as rain from \
   (SELECT station, max(phour) as rain, \
-  extract(hour from valid) as hour from current_log \
+  extract(hour from (valid - '1 minute'::interval)) as hour from current_log \
   WHERE (network IN ('AWOS','DCP') or network ~* 'ASOS') \
   and date(valid) = 'TODAY'::timestamp \
   and phour > 0 \
