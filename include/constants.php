4b1520049412f1a4b66bd4e7288f011a80df611e
akrherz <akrherz@iastate.edu>
2010-06-11 22:17:31 +0000

    updates while gone

--- include/constants.php
+++ include/constants.php
@@ -1,7 +1,7 @@
 <?php
 
 $coop_archive_end = mktime(0,0,0,1,1,2010);
-$awos_archive_end = mktime(0,0,0,5,1,2010);
+$awos_archive_end = mktime(0,0,0,6,1,2010);
 $asos_archive_end = mktime(0,0,0,6,1,2010);
 $camera_refresh = 300;
 
