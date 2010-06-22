b06cade23ef673058dc77a8e74ea3d1bbed61643
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2010-06-22 14:26:24 +0000

    update to 10 after

--- scripts/qc/correctGusts.py
+++ scripts/qc/correctGusts.py
@@ -5,8 +5,8 @@ from pyIEM import iemAccess
 iemaccess = iemAccess.iemAccess()
 
 today = mx.DateTime.now()
-
-iemaccess.query("""update summary_%s SET max_gust = 0 \
-  WHERE day = 'TODAY' and max_gust_ts < '%s 00:05' and
-  network in ('KCCI','KIMT','KELO')""" % (
-  today.year, today.strftime("%Y-%m-%d"),) )
+if today.hour == 0:
+  iemaccess.query("""update summary_%s SET max_gust = 0 \
+    WHERE day = 'TODAY' and max_gust_ts < '%s 00:05' and
+    network in ('KCCI','KIMT','KELO')""" % (
+    today.year, today.strftime("%Y-%m-%d"),) )
