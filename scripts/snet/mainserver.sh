ccfaf92030f41011df2942542424bffc704c1cc3
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2010-06-29 15:45:29 +0000

    we migrate the last program from IEM10, yipee

---
+++ scripts/snet/mainserver.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#cd /mesonet/scripts/snetPlex/bin
+
+export PATH=/mesonet/python-2.5/bin:$PATH
+
+which twistd
+kill -9 `cat mainserver.pid `
+twistd --pidfile=mainserver.pid --logfile=/mesonet/data/logs/mainserver.log -y mainserver.tac
