7c4f682692d62cbc71ba8f34808e9bbcba925671
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2010-07-09 19:41:44 +0000

    database these daryl!

--- include/constants.php
+++ include/constants.php
@@ -2,7 +2,7 @@
 
 $coop_archive_end = mktime(0,0,0,1,1,2010);
 $awos_archive_end = mktime(0,0,0,7,1,2010);
-$asos_archive_end = mktime(0,0,0,6,1,2010);
+$asos_archive_end = mktime(0,0,0,7,1,2010);
 $camera_refresh = 300;
 
 ?>
