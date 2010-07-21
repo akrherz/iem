27fb77a4e879f6a5c21003d007e187a3cf52476b
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2010-07-21 17:47:43 +0000

    need something to set STS in the database

---
+++ scripts/dbutil/compute_asos_sts.py
@@ -0,0 +1,20 @@
+# Look into the ASOS database and figure out the start time of various 
+# sites for a given network.
+
+import sys
+sys.path.insert(0, '../lib')
+import db, network
+asos = db.connect('asos')
+mesosite = db.connect('mesosite')
+
+net = sys.argv[1]
+
+table = network.Table( net )
+ids = `tuple(table.sts.keys())`
+
+rs = asos.query("SELECT station, min(valid) from alldata WHERE station in %s GROUP by station ORDER by min ASC" % (ids,)).dictresult()
+for i in range(len(rs)):
+  print rs[i]
+  sql = "UPDATE stations SET sts = '%s' WHERE id = '%s' and network = '%s'" % (
+     rs[i]['min'], rs[i]['station'], net)
+  mesosite.query( sql )
