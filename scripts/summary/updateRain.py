9eefbd19672cb064e4446bf6545d2f0c1881d76b
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2010-08-11 17:45:26 +0000

    fix stupid bug with DCP sites overwritting COOP sites

--- scripts/summary/updateRain.py
+++ scripts/summary/updateRain.py
@@ -5,15 +5,16 @@ import mx.DateTime
 now = mx.DateTime.now()
 yyyy = now.year
 
-rs = iemdb.query("select station, sum(rain) as rain from \
-  (SELECT station, max(phour) as rain, \
-  extract(hour from (valid - '1 minute'::interval)) as hour from current_log \
-  WHERE (network IN ('AWOS','DCP') or network ~* 'ASOS') \
-  and date(valid) = 'TODAY'::timestamp \
-  and phour > 0 \
-  GROUP by station,hour) as foo \
-  GROUP by station").dictresult()
+rs = iemdb.query("""select station, network, sum(rain) as rain from 
+  (SELECT station, network, max(phour) as rain, 
+  extract(hour from (valid - '1 minute'::interval)) as hour from current_log 
+  WHERE (network IN ('AWOS','DCP') or network ~* 'ASOS') 
+  and date(valid) = 'TODAY'::timestamp 
+  and phour > 0 
+  GROUP by station, network, hour) as foo 
+  GROUP by station, network""").dictresult()
 
 for i in range(len(rs)):
-	iemdb.query("UPDATE summary_%s SET pday = '%s' WHERE station \
-    = '%s' and day = 'TODAY' " % (yyyy, rs[i]['rain'], rs[i]['station']) )
+	iemdb.query("""UPDATE summary_%s SET pday = '%s' WHERE station 
+    = '%s' and day = 'TODAY' and network = '%s'""" % (
+    yyyy, rs[i]['rain'], rs[i]['station'], rs[i]['network']) )
