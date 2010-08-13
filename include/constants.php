1fd674aaeeb1d9dab1878d941acbed0a3ce0876e
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2010-08-13 15:18:50 +0000

    more cow bell

--- include/constants.php
+++ include/constants.php
@@ -3,6 +3,6 @@
 $coop_archive_end = mktime(0,0,0,1,1,2010);
 $awos_archive_end = mktime(0,0,0,8,1,2010);
 $asos_archive_end = mktime(0,0,0,8,1,2010);
-$camera_refresh = 300;
+$camera_refresh = 60;
 
 ?>
