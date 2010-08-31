4dd5f1dd3002b03e9dd1d268fb73692ff1901474
akrherz <akrherz@iastate.edu>
2010-08-31 17:41:32 +0000

    rwis_subf

--- scripts/dbutil/rwis2archive.py
+++ scripts/dbutil/rwis2archive.py
@@ -100,7 +100,7 @@ rcursor.executemany("""INSERT into t"""+ `ts.year` +""" (station, valid, tmpf,
     dwpf, drct, sknt, tfs0, tfs1, tfs2, tfs3, subf, gust, tfs0_text, 
     tfs1_text, tfs2_text, tfs3_text, pcpn, vsby) VALUES (%(station)s,
     %(valid)s,%(tmpf)s,%(dwpf)s,%(drct)s,%(sknt)s,%(tsf0)s,
-    %(tsf1)s,%(tsf2)s,%(tsf3)s,%(subf)s,%(gust)s,%(scond0)s,
+    %(tsf1)s,%(tsf2)s,%(tsf3)s,%(rwis_subf)s,%(gust)s,%(scond0)s,
     %(scond1)s,%(scond2)s,%(scond3)s,%(pday)s,%(vsby)s)""", rows )
 rcursor.close()
 
