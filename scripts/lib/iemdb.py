4de890f21503fe721c22558e70120caa610d8360
akrherz <akrherz@iastate.edu>
2010-09-02 15:02:51 +0000

    add bypass arg

--- scripts/lib/iemdb.py
+++ scripts/lib/iemdb.py
@@ -2,11 +2,14 @@
 import os
 import psycopg2
 
-def connect(dbname):
+def connect(dbname, bypass=False):
     """
     Generate a psycopg2 database connection
     """
     dbhost = 'iemdb'
-    if os.environ['USER'] == 'akrherz':
+    dbuser = os.environ['USER']
+    if dbuser == 'akrherz' and not bypass:
         dbhost = 'localhost'
-    return psycopg2.connect(database=dbname, host=dbhost)
+    if dbuser == 'akrherz' and bypass:
+        dbuser = 'nobody'
+    return psycopg2.connect(database=dbname, host=dbhost, user=dbuser)
