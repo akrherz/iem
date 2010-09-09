0634de792d1d511263f751050db5c5acdd8a3608
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2010-09-09 17:10:09 +0000

    lame

--- scripts/windrose/drive_network_windrose.py
+++ scripts/windrose/drive_network_windrose.py
@@ -3,6 +3,7 @@ Generate a windrose for each site in the ASOS network, me thinks...
 """
 import network
 import sys
+import os
 nt = network.Table(sys.argv[1])
 for id in nt.sts.keys():
     os.system("/mesonet/python/bin/python make_windrose.py %s" % (id,))
