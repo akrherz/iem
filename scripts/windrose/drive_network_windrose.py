0035fe0cec4e93637a3e295fa1a60706dd3a044c
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2010-09-09 17:09:42 +0000

    lame way to get around memory leaks

--- scripts/windrose/drive_network_windrose.py
+++ scripts/windrose/drive_network_windrose.py
@@ -2,16 +2,7 @@
 Generate a windrose for each site in the ASOS network, me thinks...
 """
 import network
-import iemplot
-import datetime
 import sys
 nt = network.Table(sys.argv[1])
 for id in nt.sts.keys():
-    fp = "/mesonet/share/windrose/climate/yearly/%s_yearly.png" % (id,)
-    print "All Year %s" % (id,)
-    iemplot.windrose(id, fp=fp)
-    for m in range(1,13):
-        fp = "/mesonet/share/windrose/climate/monthly/%02i/%s_%s.png" % (
-            m, id, datetime.datetime(2000,m,1).strftime("%b").lower() )
-        print "Month %s %s" % (m, id)
-        iemplot.windrose(id, fp=fp, months=(m,))
+    os.system("/mesonet/python/bin/python make_windrose.py %s" % (id,))
