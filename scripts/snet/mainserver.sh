5eb69ad0c441d249fa29f11badea4156db217e20
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2010-09-14 12:20:04 +0000

    make sure the mainserver.pid file is readable by hyperic

--- scripts/snet/mainserver.sh
+++ scripts/snet/mainserver.sh
@@ -7,3 +7,4 @@ export PATH=/mesonet/python-2.5/bin:$PATH
 which twistd
 kill -9 `cat mainserver.pid `
 twistd --pidfile=mainserver.pid --logfile=/mesonet/data/logs/mainserver.log -y mainserver.tac
+chmod 644 mainserver.pid
