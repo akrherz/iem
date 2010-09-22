e1009badbb2be961e0c5393d5886b99247199aa0
akrherz <akrherz@iastate.edu>
2010-09-22 20:16:35 +0000

    Merge where's it raining to this app.

---
+++ htdocs/live/js/fullscreen.js
@@ -0,0 +1,12 @@
+Ext.ns('IEMLive');
+
+IEMLive.FullScreen = Ext.extend(Ext.Viewport, {
+  layout: 'border',
+  items:  [{
+    region: 'center',
+    xtype: 'panel',
+    title: 'My first panel'
+  }]
+
+});
+
