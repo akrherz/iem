6f3c3befdd913cd6eb484cae62c96a74034dc217
akrherz <akrherz@iastate.edu>
2010-09-24 13:01:48 +0000

    move script into run at midnight and use psycopg2

--- scripts/qc/correctGusts.py
+++ scripts/qc/correctGusts.py
@@ -1,12 +1,15 @@
 # 11 Feb 2005	Fix update of summary table
 
 import mx.DateTime
-from pyIEM import iemAccess
-iemaccess = iemAccess.iemAccess()
+import iemdb
+IEM = iemdb.connect('iem')
+icursor = IEM.cursor()
 
 today = mx.DateTime.now()
-if today.hour == 0:
-  iemaccess.query("""update summary_%s SET max_gust = 0 \
+sql = """update summary_%s SET max_gust = 0 
     WHERE day = 'TODAY' and max_gust_ts < '%s 00:05' and
     network in ('KCCI','KIMT','KELO')""" % (
-    today.year, today.strftime("%Y-%m-%d"),) )
+    today.year, today.strftime("%Y-%m-%d"),)
+icursor.execute(sql)
+icursor.close()
+IEM.close()
