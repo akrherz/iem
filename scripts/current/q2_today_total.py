43fa5e755994493889a5cd4e26b6a2299f645c07
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2010-09-27 17:17:04 +0000

    switch source of q2 data to mtarchive, as the current source is filling too rapidly!

--- scripts/current/q2_today_total.py
+++ scripts/current/q2_today_total.py
@@ -8,10 +8,14 @@ import iemplot
 import numpy
 import os, sys
 
-def makefp(ts):
-    return "/mnt/a1/ARCHIVE/data/%s/q2/tile2/q2rad_hsr_nc/short_qpe/%s00.nc" % (
-            ts.gmtime().strftime("%Y/%m/%d"), 
-            ts.gmtime().strftime("%Y%m%d%H%M") )
+
+def make_fp(ts):
+    """
+    Return a string for the filename expected for this timestamp
+    """
+    return "/mnt/a4/data/%s/nmq/tile2/data/QPESUMS/grid/q2rad_hsr_nc/short_qpe/%s00.nc" % (
+        ts.strftime("%Y/%m/%d"), 
+        ts.strftime("%Y%m%d-%H%M") )
 
 def doday(ts):
     """
