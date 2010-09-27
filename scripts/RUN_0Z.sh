a4c6a6c041a9a2bf244f40088fc170acf513f434
akrherz <akrherz@iastate.edu>
2010-09-27 14:53:47 +0000

    fix order

--- scripts/RUN_0Z.sh
+++ scripts/RUN_0Z.sh
@@ -1,13 +1,15 @@
 
-cd summary
+cd 00z
+/mesonet/python/bin/python awos_rtp.py
+
+cd ../summary
 /mesonet/python/bin/python max_reflect.py
 
 # Rerun today
 cd ../dbutil
 /mesonet/python/bin/python rwis2archive.py 1
 
-cd ../00z
-/mesonet/python/bin/python awos_rtp.py
+
 
 cd ../qc
 /mesonet/python/bin/python check5day.py
