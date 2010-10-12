24ca0bba4b40aed6eb65cf1684c642b79ffa23dd
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2010-10-12 17:32:43 +0000

    add tool to extract 5 minute precip from Q2

---
+++ htdocs/iemre/q2_5min.py
@@ -0,0 +1,48 @@
+#!/mesonet/python/bin/python
+# Extract the 5 minute interval data from the Q2 netCDF files
+
+import cgi
+import os
+import netCDF3
+import mx.DateTime
+
+form = cgi.FormContent()
+sts = mx.DateTime.strptime( form["date"][0], "%Y-%m-%d")
+ets = sts + mx.DateTime.RelativeDateTime(days=1)
+interval = mx.DateTime.RelativeDateTime(minutes=5)
+# -110 , 55
+lat = float( form["lat"][0] )
+lon = float( form["lon"][0] )
+x = int( (lon - -110.) / 0.01 )
+y = int( (55. - lat )/ 0.01 ) 
+format = form["format"][0]
+
+def make_fp(ts):
+    """
+    Return a string for the filename expected for this timestamp
+    """
+    return "/mnt/a4/data/%s/nmq/tile2/data/QPESUMS/grid/q2rad_hsr_nc/short_qpe/%s00.nc" % (
+        ts.gmtime().strftime("%Y/%m/%d"), 
+        ts.gmtime().strftime("%Y%m%d-%H%M") )
+
+
+# Begin output
+print 'Content-type: text/plain\n'
+print 'DATE,TIME,PRECIP_IN'
+
+now = sts
+while now < ets:
+    # Open NETCDF File
+    fp = make_fp(now)
+    if not os.path.isfile(fp):
+        val = 0
+    else:
+        nc = netCDF3.Dataset(fp)
+        val = nc.variables["preciprate_hsr"][y,x] / 12.0 / 25.4
+        nc.close()
+    # Lat, Lon
+    print "%s,%s,%.3f" % (now.strftime("%Y-%m-%d"),
+                          now.strftime("%H:%M"),
+                          val)
+    
+    now += interval
