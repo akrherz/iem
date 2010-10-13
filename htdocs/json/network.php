a8a0781c86a0c77b57bd5695753a45e81266e063
akrherz <akrherz@iastate.edu>
2010-10-13 14:54:33 +0000

    Add new DCP plotting app refs #14

---
+++ htdocs/json/network.php
@@ -0,0 +1,23 @@
+<?php
+/* Generate a KML file of a network locations, yummy */
+require_once 'Zend/Json.php';
+include("../../config/settings.inc.php");
+include("$rootpath/include/database.inc.php");
+include("$rootpath/include/network.php");
+$network = isset($_REQUEST["network"]) ? $_REQUEST["network"] : "KCCI"; 
+$nt = new NetworkTable($network);
+
+$ar = Array("stations"=> Array());
+
+while (list($sid,$data) = each($nt->table))
+{
+  $z = Array("id"=>$sid, 
+             
+              "name"=> $data["name"],
+              "lon" => $data["lon"],
+              "lat" => $data["lat"]);
+  $ar["stations"][] = $z;
+}
+
+echo Zend_Json::encode($ar);
+?>
