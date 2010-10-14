1d28cb3a869eb1910b65570fe9566f371aad4b0a
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2010-10-14 13:39:19 +0000

    add frontend to check for missing files

---
+++ scripts/current/archive_check_n_run.py
@@ -0,0 +1,24 @@
+# Look into our archive and make sure we have what we need!
+
+import os
+import iemdb
+import datetime
+MESOSITE = iemdb.connect('mesosite', bypass=True)
+mcursor = MESOSITE.cursor()
+
+mcursor.execute("""SELECT sts, template, interval from archive_products
+    WHERE id = 44""")
+row = mcursor.fetchone()
+
+tpl = row[1]
+now = row[0]
+interval = datetime.timedelta(minutes=row[2])
+ets = datetime.datetime.now().replace(tzinfo= now.tzinfo)
+
+while now < ets:
+    fp = now.strftime(tpl)
+    if not os.path.isfile(fp):
+        cmd = "/mesonet/python/bin/python q2_5min_rate.py %s" % (
+                now.strftime("%Y %m %d %H %M"))
+        print 'Missing', now
+    now += interval
