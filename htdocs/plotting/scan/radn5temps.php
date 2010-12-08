dca43f20ffa537c18881e2a23da0e6c8252ea664
akrherz <akrherz@iastate.edu>
2010-12-08 18:01:27 +0000

    update jpgraph, refs #24

--- htdocs/plotting/scan/radn5temps.php
+++ htdocs/plotting/scan/radn5temps.php
@@ -52,7 +52,7 @@ include ("$rootpath/include/jpgraph/jpgraph_date.php");
 $graph = new Graph(640,480,"example1");
 $graph->SetScale("datlin");
 $graph->SetY2Scale("lin", 0, 900);
-$graph->img->SetMargin(45,50,55,90);
+$graph->img->SetMargin(45,50,55,120);
 $graph->xaxis->SetFont(FF_FONT1,FS_BOLD);
 $graph->xaxis->SetLabelFormatString("m/d h A", true);
 //$graph->xaxis->SetTickLabels($xlabel);
@@ -83,23 +83,28 @@ $graph->y2axis->SetColor("red");
 
 // Create the linear plot
 $lineplot=new LinePlot($ydataSR, $times);
+$graph->AddY2($lineplot);
 $lineplot->SetColor("red");
 $lineplot->SetLegend("Solar Rad");
 
+
 // Create the linear plot
 $lineplot1=new LinePlot($ydata1, $times);
+$graph->Add($lineplot1);
 $lineplot1->SetColor("green");
 $lineplot1->SetLegend("2 in");
 $lineplot1->SetWeight(2);
 
 // Create the linear plot
 $lineplot2=new LinePlot($ydata2, $times);
+$graph->Add($lineplot2);
 $lineplot2->SetColor("aquamarine4");
 $lineplot2->SetLegend("4 in");
 $lineplot2->SetWeight(2);
 
 // Create the linear plot
 $lineplot3=new LinePlot($ydata3, $times);
+$graph->Add($lineplot3);
 $lineplot3->SetColor("chocolate4");
 $lineplot3->SetLegend("8 in");
 $lineplot3->SetStyle("dashed");
@@ -107,12 +112,14 @@ $lineplot3->SetWeight(2);
 
 // Create the linear plot
 $lineplot4=new LinePlot($ydata4, $times);
+$graph->Add($lineplot4);
 $lineplot4->SetColor("blue");
 $lineplot4->SetLegend("20 in");
 $lineplot4->SetWeight(2);
 
 // Create the linear plot
 $lineplot5=new LinePlot($ydata5, $times);
+$graph->Add($lineplot5);
 $lineplot5->SetColor("black");
 $lineplot5->SetLegend("40 in");
 $lineplot5->SetStyle("dotted");
@@ -121,14 +128,6 @@ $lineplot5->SetWeight(3);
 $graph->legend->SetLayout(LEGEND_HOR);
 $graph->legend->Pos(0.10, 0.06, "right", "top");
 
-
-// Add the plot to the graph
-$graph->Add($lineplot1);
-$graph->Add($lineplot2);
-$graph->Add($lineplot3);
-$graph->Add($lineplot4);
-$graph->Add($lineplot5);
-$graph->AddY2($lineplot);
 
 // Display the graph
 $graph->Stroke();
