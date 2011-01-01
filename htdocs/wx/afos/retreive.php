fe28c174dfa90e35e4fef620dcc7df605fd9d246
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2011-01-01 22:47:11 +0000

    implement an optimization and have the old interface call the PHP interface

--- htdocs/wx/afos/retreive.php
+++ htdocs/wx/afos/retreive.php
@@ -7,10 +7,29 @@ $conn = iemdb('afos');
 $pil = strtoupper($_REQUEST["pil"]);
 $cnt = $_REQUEST["cnt"];
 
-$rs = pg_prepare($conn, "SELECT", "SELECT * from products WHERE pil = $1
+/*
+ * We need to optimize some things here, first search the current table
+ * and see if we can come up with enough entries.
+ * TODO
+ */
+$table = sprintf("products_%s_", date("Y"));
+if (intval(date("m")) > 6){
+	$table .= "0712";
+} else {
+	$table .= "0106";
+}
+/* Okay, lets try the local table first */
+$rs = pg_prepare($conn, "LSELECT", "SELECT * from $table WHERE pil = $1
+                         ORDER by entered DESC LIMIT $2");
+$rs = pg_execute($conn, "LSELECT", Array($pil, $cnt));
+if (pg_num_rows($rs) != $cnt){
+	/* Our optimization failed, shucks */
+	$rs = pg_prepare($conn, "SELECT", "SELECT * from products WHERE pil = $1
                          ORDER by entered DESC LIMIT $2");
 
-$rs = pg_execute($conn, "SELECT", Array($pil, $cnt));
+	$rs = pg_execute($conn, "SELECT", Array($pil, $cnt));
+}
+
 
 if (pg_numrows($rs) == 0){
  if (substr($pil,0,3) == "MTR"){
