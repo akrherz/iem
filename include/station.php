e66fb212670ef0992df2e6d73a4ce2cef759b8e5
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2011-01-03 17:57:24 +0000

    remove explicit network requirement for now

--- include/station.php
+++ include/station.php
@@ -5,13 +5,19 @@ include_once("$rootpath/include/database.inc.php");
 
 class StationData {
 
-  function StationData($a,$n)
+  function StationData($a,$n="")
   {
     $this->table = Array();
     $this->dbconn = iemdb("mesosite");
-    $rs = pg_prepare($this->dbconn, "SELECT  ST", "SELECT *, " .
+    if ($n != ""){
+    	$rs = pg_prepare($this->dbconn, "SELECT  ST", "SELECT *, " .
     		"x(geom) as lon, y(geom) as lat from stations " .
     		"WHERE id = $1 and network = $2");
+    } else{
+    	$rs = pg_prepare($this->dbconn, "SELECT  ST", "SELECT *, " .
+    		"x(geom) as lon, y(geom) as lat from stations " .
+    		"WHERE id = $1");
+    }
     if (is_string($a)) $this->load_station($a,$n);
     else if (is_array($a)) 
     {
@@ -20,9 +26,13 @@ class StationData {
 
   }
 
-  function load_station($id,$n)
+  function load_station($id,$n="")
   {
-    $rs = pg_execute($this->dbconn, "SELECT  ST", Array($id,$n));
+  	if ($n != ""){ 
+    	$rs = pg_execute($this->dbconn, "SELECT  ST", Array($id,$n));
+  	} else{
+  		$rs = pg_execute($this->dbconn, "SELECT  ST", Array($id));
+  	}
     for( $i=0; $row = @pg_fetch_array($rs,$i); $i++)
     {
       $this->table[ $row["id"] ] = $row;
