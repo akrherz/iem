2114593bd8dd5ab139f58280adfb678c6fc11eb0
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2011-01-13 15:31:14 +0000

    Add a fun 503 page, need to edit http.conf

---
+++ htdocs/error503.php
@@ -0,0 +1,12 @@
+<?php
+include("../config/settings.inc.php");
+include("$rootpath/include/header.php");
+?>
+
+<h3>Requested file was not found</h3>
+
+<img src="images/snoopy503.jpg" />
+
+<?php
+include("$rootpath/include/footer.php");
+?>
