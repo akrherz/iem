3aa8ac156824a055328cae1e3fd7c49188a3962b
akrherz <akrherz@iastate.edu>
2011-01-18 15:45:23 +0000

    make it more robust and regenerate the overviews each time

--- scripts/summary/max_reflect.py
+++ scripts/summary/max_reflect.py
@@ -6,61 +6,87 @@ from osgeo.gdalconst import *
 import numpy, mx.DateTime, os, shutil, urllib2, sys
 
 def run(sts):
-  ets = sts + mx.DateTime.RelativeDateTime(days=1)
-  interval = mx.DateTime.RelativeDateTime(minutes=5)
+    """
+    Generate max dBZ composite for a given UTC date
+    """
+    ets = sts + mx.DateTime.RelativeDateTime(days=1)
+    interval = mx.DateTime.RelativeDateTime(minutes=5)
 
-  n0r = gdal.Open('../../htdocs/docs/nexrad_composites/reflect_ramp.png', 0)
-  n0rct = n0r.GetRasterBand(1).GetRasterColorTable().Clone()
+    # Load the color table
+    n0r = gdal.Open('../../htdocs/docs/nexrad_composites/reflect_ramp.png', 0)
+    n0rct = n0r.GetRasterBand(1).GetRasterColorTable().Clone()
   
+    # Loop over our archived files and do what is necessary
+    maxn0r = None
+    now = sts
+    while now < ets:
+        fp = now.strftime("/mesonet/ARCHIVE/data/%Y/%m/%d/GIS/uscomp/n0r_%Y%m%d%H%M.png")
+        if (not os.path.isfile(fp)):
+            print "MISSING:", fp
+            now += interval
+            continue
+        n0r = gdal.Open(fp, 0)
+        n0rd = numpy.ravel( n0r.ReadAsArray() )
+        if maxn0r is None:
+            maxn0r = n0rd
+        maxn0r = numpy.where( n0rd > maxn0r, n0rd, maxn0r)
 
-  maxn0r = None
-  now = sts
-  while (now < ets):
-    fp = now.strftime("/mesonet/ARCHIVE/data/%Y/%m/%d/GIS/uscomp/n0r_%Y%m%d%H%M.png")
-    if (not os.path.isfile(fp)):
-      print "MISSING:", fp
-      now += interval
-      continue
-    n0r = gdal.Open(fp, 0)
-    n0rd = numpy.ravel( n0r.ReadAsArray() )
-    if (maxn0r is None):
-      maxn0r = n0rd
-    maxn0r = numpy.where( n0rd > maxn0r, n0rd, maxn0r)
-
-    now += interval
+        now += interval
 
 
-  out_driver = gdal.GetDriverByName("gtiff")
-  outdataset = out_driver.Create('max.tiff', n0r.RasterXSize, n0r.RasterYSize, n0r.RasterCount, GDT_Byte)
-  # Set output color table to match input
-  outdataset.GetRasterBand(1).SetRasterColorTable( n0rct )
-  outdataset.GetRasterBand(1).WriteArray( numpy.resize(maxn0r, (n0r.RasterYSize, n0r.RasterXSize))  )
-  del outdataset
+    out_driver = gdal.GetDriverByName("gtiff")
+    outdataset = out_driver.Create('max.tiff', n0r.RasterXSize, n0r.RasterYSize, n0r.RasterCount, GDT_Byte)
+    # Set output color table to match input
+    outdataset.GetRasterBand(1).SetRasterColorTable( n0rct )
+    outdataset.GetRasterBand(1).WriteArray( numpy.resize(maxn0r, (n0r.RasterYSize, n0r.RasterXSize))  )
+    del outdataset
 
-  os.system("convert max.tiff max.png")
-  # Insert into LDM
-  cmd = "/home/ldm/bin/pqinsert -p 'plot a %s0000 bogus GIS/uscomp/max_n0r_0z0z_%s.png png' max.png" % (
+    os.system("convert max.tiff max.png")
+    # Insert into LDM
+    cmd = "/home/ldm/bin/pqinsert -p 'plot a %s0000 bogus GIS/uscomp/max_n0r_0z0z_%s.png png' max.png" % (
                                             sts.strftime("%Y%m%d"), sts.strftime("%Y%m%d") )
-  os.system(cmd)
+    os.system(cmd)
 
-  # Create tmp world file
-  out = open('/tmp/tmpwld%s.wld' % (sts.strftime("%Y%m%d"),), 'w')
-  out.write("""   0.010000000000%s
+    # Create tmp world file
+    out = open('/tmp/tmpwld%s.wld' % (sts.strftime("%Y%m%d"),), 'w')
+    out.write("""   0.010000000000%s
    0.00000
    0.00000
   -0.01000000000000%s
 -126.000000
   50.0000""" % (random.randint(0,1000), random.randint(0,1000) ) )
-  out.close()
+    out.close()
   
-  # Insert world file as well
-  cmd = "/home/ldm/bin/pqinsert -p 'plot a %s0000 bogus GIS/uscomp/max_n0r_0z0z_%s.wld wld' /tmp/tmpwld%s.wld" % (
+    # Insert world file as well
+    cmd = "/home/ldm/bin/pqinsert -p 'plot a %s0000 bogus GIS/uscomp/max_n0r_0z0z_%s.wld wld' /tmp/tmpwld%s.wld" % (
          sts.strftime("%Y%m%d"), sts.strftime("%Y%m%d"),  sts.strftime("%Y%m%d") )
-  os.system(cmd)
+    os.system(cmd)
 
-  os.remove("max.tiff")
-  os.remove("max.png")
-  os.remove( '/tmp/tmpwld%s.wld' % (sts.strftime("%Y%m%d"),) )
+    # cleanup
+    os.remove("max.tiff")
+    os.remove("max.png")
+    os.remove( '/tmp/tmpwld%s.wld' % (sts.strftime("%Y%m%d"),) )
+  
+    # Iowa
+    png = urllib2.urlopen("http://iemvs105.local/GIS/radmap.php?layers[]=uscounties&layers[]=nexrad_tc&ts=%s" % (
+                                                                        sts.strftime("%Y%m%d%H%M"),))
+    o = open('tmp.png', 'wb')
+    o.write( png.read() )
+    o.close()
+    cmd = "/home/ldm/bin/pqinsert -p 'plot ac %s0000 summary/max_n0r_0z0z_comprad.png comprad/max_n0r_0z0z_%s.png png' tmp.png" % (
+                                                                    sts.strftime("%Y%m%d"), sts.strftime("%Y%m%d") )
+    os.system(cmd)
+
+    # US
+    png = urllib2.urlopen("http://iemvs105.local/GIS/radmap.php?sector=conus&layers[]=uscounties&layers[]=nexrad_tc&ts=%s" % (
+                                                                        sts.strftime("%Y%m%d%H%M"),))
+    o = open('tmp.png', 'wb')
+    o.write( png.read() )
+    o.close()
+    cmd = "/home/ldm/bin/pqinsert -p 'plot ac %s0000 summary/max_n0r_0z0z_usrad.png usrad/max_n0r_0z0z_%s.png png' tmp.png" % (
+                                                                        sts.strftime("%Y%m%d"), sts.strftime("%Y%m%d") )
+    os.system(cmd)
+    os.remove("tmp.png")
 
 """
 s = mx.DateTime.DateTime(2008,6,6)
@@ -75,19 +101,4 @@ while (now < e):
 ts = mx.DateTime.now() + mx.DateTime.RelativeDateTime(hour=0,minute=0)
 run( ts )
 
-# Iowa
-png = urllib2.urlopen("http://iemvs105.local/GIS/radmap.php?layers[]=uscounties&layers[]=nexrad_tc&ts=%s" % (ts.strftime("%Y%m%d%H%M"),))
-o = open('tmp.png', 'wb')
-o.write( png.read() )
-o.close()
-cmd = "/home/ldm/bin/pqinsert -p 'plot ac %s0000 summary/max_n0r_0z0z_comprad.png comprad/max_n0r_0z0z_%s.png png' tmp.png" % (ts.strftime("%Y%m%d"), ts.strftime("%Y%m%d") )
-os.system(cmd)
 
-# US
-png = urllib2.urlopen("http://iemvs105.local/GIS/radmap.php?sector=conus&layers[]=uscounties&layers[]=nexrad_tc&ts=%s" % (ts.strftime("%Y%m%d%H%M"),))
-o = open('tmp.png', 'wb')
-o.write( png.read() )
-o.close()
-cmd = "/home/ldm/bin/pqinsert -p 'plot ac %s0000 summary/max_n0r_0z0z_usrad.png usrad/max_n0r_0z0z_%s.png png' tmp.png" % (ts.strftime("%Y%m%d"), ts.strftime("%Y%m%d") )
-os.system(cmd)
-os.remove("tmp.png")
