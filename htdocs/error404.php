7143c3c0d6d02ed2730082d8999d709bb4b8c7f4
akrherz <akrherz@iastate.edu>
2011-01-18 17:20:11 +0000

    need to be more explicit about image path

--- htdocs/error404.php
+++ htdocs/error404.php
@@ -5,7 +5,7 @@ include("$rootpath/include/header.php");
 
 <h3>Requested file was not found</h3>
 
-<img src="images/snoopy503.jpg" />
+<img src="<?php echo $rooturl; ?>/images/snoopy503.jpg" />
 
 <?php
 include("$rootpath/include/footer.php");
