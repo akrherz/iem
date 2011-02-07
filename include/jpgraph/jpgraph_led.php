5375aa29d70f1a0794ed5a3e2cee394f0f34a97c
akrherz <akrherz@iastate.edu>
2011-02-07 02:45:04 +0000

    get rid of mb_

--- include/jpgraph/jpgraph_led.php
+++ include/jpgraph/jpgraph_led.php
@@ -30,7 +30,7 @@ DEFINE('LEDC_INVERTGRAY', 15);
 
 // Check that mb_strlen() is available
 if( ! function_exists('mb_strlen') ) {
-    JpGraphError::RaiseL(25500);
+    //JpGraphError::RaiseL(25500);
     //'Multibyte strings must be enabled in the PHP installation in order to run the LED module
     // so that the function mb_strlen() is available. See PHP documentation for more information.'
 }
@@ -275,13 +275,13 @@ class DigitalLED74
             $aColor = 0;
         }
 
-        if(($n = mb_strlen($aValStr,'utf8')) == 0) {
+        if(($n = strlen($aValStr)) == 0) {
             $aValStr = ' ';
             $n = 1;
         }
 
         for($i = 0; $i < $n; ++$i) {
-            $d = mb_substr($aValStr, $i, 1, 'utf8');
+            $d = substr($aValStr, $i, 1);
             if(  ctype_digit($d) ) {
                 $d = (int)$d;
             }
