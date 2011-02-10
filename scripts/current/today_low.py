db7617129381c09b1524473ef1e4aacd31c19726
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2011-02-10 13:01:55 +0000

    add plot of low temperatures for feature this morning

---
+++ scripts/current/today_low.py
@@ -0,0 +1,54 @@
+# Output the 12z morning low temperature
+
+import os, random
+import iemdb
+import iemplot
+
+import mx.DateTime
+now = mx.DateTime.now()
+
+IEM = iemdb.connect('iem', bypass=True)
+icursor = IEM.cursor()
+
+sql = """
+select station, 
+  x(geom) as lon, y(geom) as lat, 
+  min_low as low, network
+from summary_%s
+WHERE day = 'TODAY' and min_tmpf < 90 
+and network ~* 'ASOS' and network not in ('PO_ASOS','IA_ASOS','AK_ASOS')
+""" % (now.year, )
+
+lats = []
+lons = []
+vals = []
+valmask = []
+labels = []
+icursor.execute(sql)
+for row in icursor:
+  lats.append( row[2] )
+  lons.append( row[1] )
+  vals.append( row[3] )
+  labels.append( row[0] )
+  valmask.append( row[4] in ['AWOS', 'IA_ASOS'] )
+
+
+cfg = {
+ 'wkColorMap': 'BlAqGrYeOrRe',
+# '_showvalues'        : True,
+# '_valuemask'         :   valmask,
+ 'lbTitleString'    : 'F',
+ '_format'            : '%.0f',
+ '_title'             : "US Morning Low Temperature",
+ '_valid'             : "%s" % (now.strftime("%d %b %Y"), ),
+ '_labels'            : labels
+ '_midwest'		: True
+}
+# Generates tmp.ps
+tmpfp = iemplot.simple_contour(lons, lats, vals, cfg)
+
+#pqstr = "plot ac %s summary/iowa_asos_high.png iowa_asos_high.png png" % (
+#        now.strftime("%Y%m%d%H%M"), )
+#iemplot.postprocess(tmpfp, '')
+iemplot.makefeature(tmpfp)
+
