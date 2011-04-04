961f84fbd56c972913a3cfcae3090d14c5f44cf9
akrherz <akrherz@iastate.edu>
2011-04-04 12:19:34 +0000

    reorder and background some processes

--- scripts/RUN_1MIN.sh
+++ scripts/RUN_1MIN.sh
@@ -1,15 +1,17 @@
 # Run every minute!
+/mesonet/www/apps/nwnwebsite/scripts/GEN.csh &
+
 cd GIS
 /mesonet/python/bin/python attribute2shape.py
 
 cd ../ingestors
-/mesonet/python/bin/python parse0006.py
-/mesonet/python/bin/python parse0002.py
-/mesonet/python/bin/python awos_adas.py
+/mesonet/python/bin/python parse0006.py &
+/mesonet/python/bin/python parse0002.py &
+/mesonet/python/bin/python awos_adas.py &
 /mesonet/python/bin/python ctre_bridge.py &
 
 cd ../outgoing
-/mesonet/python/bin/python snet_collect.py
+/mesonet/python/bin/python snet_collect.py &
 
 cd ../ingestors/sn
 /mesonet/python/bin/python parser.py
@@ -17,4 +19,4 @@ cd ../ingestors/sn
 cd ../awos
 /mesonet/python/bin/python dl_parse_iwapi.py
 
-/mesonet/www/apps/nwnwebsite/scripts/GEN.csh
+
