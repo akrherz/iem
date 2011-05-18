fa6674930ef78bef1c5cd266812efa772bebd469
akrherz <akrherz@iastate.edu>
2011-05-18 17:51:31 +0000

    time for some external testing...

--- htdocs/vtec/search.php
+++ htdocs/vtec/search.php
@@ -4,8 +4,9 @@ include("../../config/settings.inc.php");
 $HEADEXTRA = '<link rel="stylesheet" type="text/css" href="http://extjs.cachefly.net/ext-3.3.1/resources/css/ext-all.css"/>
 <script type="text/javascript" src="http://extjs.cachefly.net/ext-3.3.1/adapter/ext/ext-base.js"></script>
 <script type="text/javascript" src="http://extjs.cachefly.net/ext-3.3.1/ext-all.js"></script>
-<script type="text/javascript" src="search.js?v=3" />
-';
+<script type="text/javascript" src="wfos.js?v=3"></script>
+<script type="text/javascript" src="../lsr/Exporter-all.js"></script>
+<script type="text/javascript" src="search.js?v=3"></script>';
 $THISPAGE ="severe-vtec";
 $TITLE = "VTEC Search by County/Zone";
 include("$rootpath/include/header.php");
@@ -16,8 +17,8 @@ include("$rootpath/include/header.php");
 of NWS VTEC products.  These products include most of the watch, warnings,
 and advisories issued.</blockquote>
 
-<div id="myform" style="float: left;"></div>
-
-<div id="mytable" style="float: right;"></div>
+<div id="myform"></div>
+<br />
+<div id="mytable"></div>
 
 <?php include("$rootpath/include/footer.php"); ?>
