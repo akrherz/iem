914dcfa465dc3f67fce0b595174d6a8ed4bbf9d2
akrherz <akrherz@iastate.edu>
2011-05-19 13:25:37 +0000

    further improvements as per requests

--- htdocs/vtec/search.php
+++ htdocs/vtec/search.php
@@ -6,18 +6,26 @@ $HEADEXTRA = '<link rel="stylesheet" type="text/css" href="../ext-3.3.3/resource
 <script type="text/javascript" src="../ext-3.3.3/ext-all.js"></script>
 <script type="text/javascript" src="wfos.js?v=3"></script>
 <script type="text/javascript" src="../lsr/Exporter-all.js"></script>
-<script type="text/javascript" src="search.js?v=3"></script>';
+<script type="text/javascript" src="search.js?v=4"></script>';
 $THISPAGE ="severe-vtec";
 $TITLE = "VTEC Search by County/Zone";
 include("$rootpath/include/header.php");
 ?>
-<h3>Search for NWS VTEC Products by County/Zone</h3>
-
-<blockquote>This application allows you to query the IEM archive
-of NWS VTEC products.  These products include most of the watch, warnings,
-and advisories issued.</blockquote>
+<h3>Search for NWS Watch/Warning/Advisories Products by County/Zone</h3>
+<br />
+<p>The National Weather Service issues watch, warnings, and advisories (WWA) for counties/parishes.  For 
+some products (like winter warnings), they issue for forecast zones.  In many parts of the country, these zones are exactly the 
+same as the counties/parishes.  When you get into regions with topography, then zones will start to 
+differ to the local counties.</p>
+<br />
+<p>This application allows you to search the IEM's archive of NWS WWA products.  Our archive is not complete, but
+there are no known holes since 12 November 2005.  This archive is of those products that contain VTEC codes, which
+are nearly all WWAs that the NWS issues for.</p>
 
 <div id="myform"></div>
+<br />
+<p>The table will automatically populate below once you make your selections above. Click on the linked Event ID
+to get more information on the warning.</p>
 <br />
 <div id="mytable"></div>
 
