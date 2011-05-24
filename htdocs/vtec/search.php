6659185864bb0552839a67e9888d778499e7982d
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2011-05-24 15:16:32 +0000

    test for IE

--- htdocs/vtec/search.php
+++ htdocs/vtec/search.php
@@ -5,7 +5,7 @@ $HEADEXTRA = '<link rel="stylesheet" type="text/css" href="../ext-3.3.3/resource
 <script type="text/javascript" src="../ext-3.3.3/adapter/ext/ext-base.js"></script>
 <script type="text/javascript" src="../ext-3.3.3/ext-all.js"></script>
 <script type="text/javascript" src="wfos.js?v=3"></script>
-<script type="text/javascript" src="Exporter-all.js"></script>
+<script type="text/javascript" src="../lsr/Exporter-all.js"></script>
 <script type="text/javascript" src="search.js?v=4"></script>';
 $THISPAGE ="severe-vtec";
 $TITLE = "VTEC Search by County/Zone";
