4f35a3f94ed3288e0a341adc94d08bd80ac4e82a
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2011-06-14 20:32:04 +0000

    vamoose

--- scripts/RUN_5MIN.sh
+++ scripts/RUN_5MIN.sh
@@ -1,6 +1,6 @@
 # Run every 5 minutes
 cd dl
-/mesonet/python/bin/python radar_composite.py &
+/mesonet/python/bin/python radar_composite.py 
 
 cd ../GIS
 /mesonet/python/bin/python 24h_lsr.py 
@@ -16,13 +16,13 @@ cd ../../ingestors/rwis
 ./download.csh &
 
 cd ../dotcams
-/mesonet/python/bin/python process.py &
+/mesonet/python/bin/python process.py 
 
-cd ../awos
-/mesonet/python/bin/python parse_idot.py &
+#cd ../awos
+#/mesonet/python/bin/python parse_idot.py 
 
 cd ../../q2
-/mesonet/python/bin/python make_raster.py &
+/mesonet/python/bin/python make_raster.py 
 
 # Lets wait a bit now
 sleep 60
