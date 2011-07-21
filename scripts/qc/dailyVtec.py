b6f01caa7e3088ea55774faf1de3b1ea29ccbe6d
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2011-07-21 12:22:00 +0000

    daily quota of known tests

--- scripts/qc/dailyVtec.py
+++ scripts/qc/dailyVtec.py
@@ -15,7 +15,8 @@ tests = [
  'MKX.TO.2', 'MKX.TO.3', 'MEG.SV.449', 'MEG.SV.450', 'MEG.SV.451','MEG.SV.452',
  'MEG.SV.453','MEG.SV.454','MEG.SV.455','MEG.SV.456','MEG.SV.457','MEG.SV.458',
  'MEG.SV.459','MEG.SV.460','MEG.SV.461','CAE.SV.113', 'LUB.SV.56', 'MLB.SV.72',
- 'LBF.SV.186','MQT.MA.52','PIH.SV.29','PIH.SV.30',
+ 'LBF.SV.186','MQT.MA.52','PIH.SV.29','PIH.SV.30', 'DDC.TO.1', 'ARX.TO.1', 'ARX.FF.1',
+ 'CYS.TO.1', 'CYS.TO.2', 'DVN.TO.1', 'DVN.TO.3', 'DVN.TO.2'
 ]
 for i in range(2,69):
     tests.append('VEF.SV.%s' % (i,))
