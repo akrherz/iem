431fedf5ace88c2f79d825f8ed622a3df0f7fbb4
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2011-08-02 14:25:15 +0000

    save my work on this, fancy

--- scripts/feature/ragbrai.py
+++ scripts/feature/ragbrai.py
@@ -1,4 +1,20 @@
+import mx.DateTime
+import iemdb
+import math
+import numpy
+from pyIEM import mesonet
+ASOS = iemdb.connect('asos', bypass=True)
+acursor = ASOS.cursor()
 
+def uv(sped, drct2):
+  dirr = drct2 * math.pi / 180.00
+  s = math.sin(dirr)
+  c = math.cos(dirr)
+  u = round(- sped * s, 2)
+  v = round(- sped * c, 2)
+  return u, v
+
+DATES = [ 
 [mx.DateTime.DateTime(1973,8,26), mx.DateTime.DateTime(1973,8,31)],
 [mx.DateTime.DateTime(1974,8,4), mx.DateTime.DateTime(1974,8,10)],
 [mx.DateTime.DateTime(1975,8,3), mx.DateTime.DateTime(1975,8,9)],
@@ -37,4 +53,62 @@
 [mx.DateTime.DateTime(2008,7,20), mx.DateTime.DateTime(2008,7,26)],
 [mx.DateTime.DateTime(2009,7,19), mx.DateTime.DateTime(2009,7,25)],
 [mx.DateTime.DateTime(2010,7,25), mx.DateTime.DateTime(2010,7,31)],
-[mx.DateTime.DateTime(2011,7,24), mx.DateTime.DateTime(2011,7,30)],
+[mx.DateTime.DateTime(2011,7,24), mx.DateTime.DateTime(2011,7,30)]
+]
+
+hindex = numpy.zeros((2012-1973))
+uwnd = numpy.zeros((2012-1973))
+
+#output = open('ragbrai.dat', 'w')
+for sdate,edate in DATES:
+    acursor.execute("""
+    SELECT tmpf, dwpf, sknt, drct, valid from t%s WHERE station = 'DSM' and valid BETWEEN '%s 00:00' and '%s 23:59' and tmpf > 0
+    and dwpf > 0 and sknt >= 0 and drct >= 0 ORDER by valid ASC
+    """ % (sdate.year, sdate.strftime("%Y-%m-%d"), edate.strftime("%Y-%m-%d") ))
+    cnt = 0
+    tot = 0
+    ttot = 0
+    utot = 0
+    ucnt = 0
+    vtot = 0
+    for row in acursor:
+        ttot += row[0]
+        h = mesonet.heatidx(row[0], mesonet.relh(row[0], row[1]))
+        if row[4].hour > 5 and row[4].hour < 22:
+            u,v = uv(row[2], row[3])
+        #output.write("%s,%s,%s,%.1f,%.2f,%.2f\n" % (row[4].strftime("%Y,%m,%d,%H,%M"), row[0], row[1], h, u, v))
+            utot += u
+            vtot += v
+            ucnt += 1
+        tot += h
+        cnt += 1
+    print "%s %3s %4.1f %4.1f %4.1f %4.1f" % (sdate.year, cnt, ttot / float(cnt), tot / float(cnt), utot / float(ucnt), vtot / float(ucnt))
+    uwnd[sdate.year-1973] = utot / float(ucnt) * 1.15
+    hindex[sdate.year-1973] = tot / float(cnt)
+#output.close()
+
+import matplotlib.pyplot as plt
+fig = plt.figure()
+ax = fig.add_subplot(211)
+ax.set_xlim(1972.5,2011.5)
+ax.set_title("1973-2011 RAGBRAI Weather")
+ax.bar(numpy.arange(1973,2012)-0.4, hindex, color='r')
+ax.set_ylim(60,100)
+ax.set_ylabel("Average Heat Index $^{\circ}\mathrm{F}$")
+ax.grid(True)
+
+ax2 = fig.add_subplot(212)
+ax2.set_xlim(1972.5,2011.5)
+bars = ax2.bar(numpy.arange(1973,2012)-0.4, uwnd, fc='r')
+for bar in bars:
+    if bar.get_xy()[1] == 0:
+        bar.set_facecolor('g')
+ax2.set_ylabel("East/West Daytime\n Average Wind Speed [mph]")
+ax2.text(1990,5, "Tail-winds")
+ax2.text(1990,-5, "Head-winds")
+
+ax2.grid(True)
+
+fig.savefig('test.ps')
+import iemplot
+iemplot.makefeature('test')
