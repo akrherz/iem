0c7f9c878f127c87987a238af05a94f9cbda9fa4
akrherz <akrherz@iastate.edu>
2011-08-08 19:37:11 +0000

    improve this for flexiable use

--- scripts/ingestors/ncdc/parse_coop_stationtable.py
+++ scripts/ingestors/ncdc/parse_coop_stationtable.py
@@ -1,9 +1,10 @@
 # 
-
+import sys
 from pyIEM import iemdb
 i = iemdb.iemdb()
 mesosite = i['mesosite']
 mesosite.query("BEGIN;")
+ST = sys.argv[1]
 
 def toLALO( s ):
   tokens = s.split(":")
@@ -12,19 +13,23 @@ def toLALO2( s ):
   tokens = s.split(":")
   return float(tokens[0]) - (float(tokens[1])/60.0)
 
-o = open('5321234136207stn.txt', 'r')
+o = open('%s_stn.txt' % (ST,), 'r')
 for line in o:
   name = line[17:48].strip()
-  stid = "ms"+ line[2:6]
+  stid = "%s%s" %(ST, line[2:6])
   tokens = line[48:].split()
   elev = tokens[-1]
   lon = toLALO2( tokens[-2] )
   lat = toLALO( tokens[-3] )
-  sql = """INSERT into stations (id, network, geom, state, plot_name,
-        elevation, country, online) VALUES ('%s', 'MSCLIMATE', 
-        'SRID=4326;POINT(%s %s)', 'MS', '%s', %s, 'US','t')""" % (stid,
-        lon, lat, name, elev)
-  print stid
-  mesosite.query( sql )
+  sql = """SELECT * from stations where id = '%s' and network = '%sCLIMATE'
+        """ % (stid, ST)
+  rows = mesosite.query( sql ).dictresult()
+  if len(rows) == 0:
+    sql = """INSERT into stations (id, network, geom, state, plot_name,
+        elevation, country, online) VALUES ('%s', '%sCLIMATE', 
+        'SRID=4326;POINT(%s %s)', '%s', '%s', %s, 'US','t')""" % (stid, ST,
+        lon, lat, ST, name, elev)
+    print 'Adding ID: %s' % (stid,)
+    mesosite.query( sql )
 
 mesosite.query("COMMIT;")
