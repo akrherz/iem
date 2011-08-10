95a972c8f95250cda9b4aa10d1d47c3bf5c80f1c
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2011-08-10 21:31:20 +0000

    add convective sigmets geoJSON

---
+++ htdocs/geojson/convective_sigmet.php
@@ -0,0 +1,47 @@
+<?php
+/* 
+ * Generate GeoJSON Convective Sigmets
+ */
+require_once 'Zend/Json.php';
+include("../../config/settings.inc.php");
+include("$rootpath/include/database.inc.php");
+include("$rootpath/include/vtec.php");
+$postgis = iemdb("postgis");
+
+$rs = pg_query("SET TIME ZONE 'GMT'");
+$rs = pg_prepare($postgis, "SELECT", "SELECT *, 
+      ST_asGeoJson(geom) as geojson
+      FROM sigmets_current WHERE sigmet_type = 'C'");
+
+$rs = pg_execute($postgis, "SELECT", Array());
+
+
+$ar = Array("type"=>"FeatureCollection",
+      "crs" => Array("type"=>"EPSG", 
+                     "properties" => Array("code"=>4326,
+                                  "coordinate_order" => Array(1,0))),
+      "features" => Array()
+);
+
+$reps = Array();
+$subs = Array();
+
+for ($i=0;$row=@pg_fetch_array($rs,$i);$i++)
+{
+  $reps[] = "\"REPLACEME$i\"";
+  $subs[] = $row["geojson"];
+
+
+  $z = Array("type"=>"Feature", "id"=>$i, 
+             "properties"=>Array(
+                "issue"     => substr($row["issue"],0,16),
+                "expire"     => substr($row["expire"],0,16),
+                "label"         => $row["label"]
+  				),
+              
+             "geometry"=> "REPLACEME$i");
+                         
+  $ar["features"][] = $z;
+}
+echo str_replace($reps, $subs, Zend_Json::encode($ar));
+?>
