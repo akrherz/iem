ffacf122786e6fca4041a51c9e4081084895c1bf
akrherz <akrherz@iastate.edu>
2011-08-18 12:24:11 +0000

    try to make this work a bit better

--- scripts/windrose/daily_drive_network.py
+++ scripts/windrose/daily_drive_network.py
@@ -7,16 +7,18 @@ mcursor = MESOSITE.cursor()
 now = mx.DateTime.now()
 
 mcursor.execute("""SELECT max(id), network from stations 
-    WHERE network ~* 'ASOS' GROUP by network""")
+    WHERE network ~* 'ASOS' GROUP by network ORDER by random()""")
 for row in mcursor:
     network = row[1]
     testfp = "/mesonet/share/windrose/climate/yearly/%s_yearly.png" % (row[0],)
     if not os.path.isfile(testfp):
+        print "Driving network %s because no file" % (network,)
         os.system("/mesonet/python/bin/python drive_network_windrose.py %s" % (network,))
         sys.exit()
     else:
         mtime = os.stat(testfp)[stat.ST_MTIME]
         age = float(now) - mtime
         if age > (50*24*60): #50 days
+            print "Driving network %s because of age!" % (network,)
             os.system("/mesonet/python/bin/python drive_network_windrose.py %s" % (network,))
             sys.exit()
