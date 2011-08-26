78c06def1ed675cd3aae7783dbd4a75b9fc86961
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2011-08-26 20:49:23 +0000

    NCDC's station tables are incorrectly formatted, sigh

--- scripts/ingestors/ncdc/parse_coop_stationtable.py
+++ scripts/ingestors/ncdc/parse_coop_stationtable.py
@@ -6,12 +6,12 @@ mesosite = i['mesosite']
 mesosite.query("BEGIN;")
 ST = sys.argv[1]
 
-def toLALO( s ):
-  tokens = s.split(":")
-  return float(tokens[0]) + (float(tokens[1])/60.0)
 def toLALO2( s ):
   tokens = s.split(":")
   return float(tokens[0]) - (float(tokens[1])/60.0)
+def toLALO( s ):
+  s = s.strip()
+  return float(s[:2]) + (float("%s%s" % (s[2],s[4]))/60.0)
 
 o = open('%s_stn.txt' % (ST,), 'r')
 for line in o:
@@ -29,7 +29,7 @@ for line in o:
         elevation, country, online) VALUES ('%s', '%sCLIMATE', 
         'SRID=4326;POINT(%s %s)', '%s', '%s', %s, 'US','t')""" % (stid, ST,
         lon, lat, ST, name, elev)
-    print 'Adding ID: %s' % (stid,)
+    print 'Adding ID: %s %6.2f %6.2f' % (stid,lon, lat)
     mesosite.query( sql )
 
 mesosite.query("COMMIT;")
