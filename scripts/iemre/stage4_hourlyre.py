4999758ce197fefbb1e9357d517525c439e5000a
akrherz <akrherz@iastate.edu>
2011-08-29 18:08:03 +0000

    we don't need so much data

--- scripts/iemre/stage4_hourlyre.py
+++ scripts/iemre/stage4_hourlyre.py
@@ -22,9 +22,9 @@ def merge(ts):
     if os.path.isfile(fp):
         grib = Nio.open_file(fp, 'r')
         # Rough subsample, since the whole enchillata is too much
-        lats = numpy.ravel( grib.variables["g5_lat_0"][200:-100,300:900] )
-        lons = numpy.ravel( grib.variables["g5_lon_1"][200:-100,300:900] )
-        vals = numpy.ravel( grib.variables["A_PCP_GDS5_SFC_acc1h"][200:-100,300:900] )
+        lats = numpy.ravel( grib.variables["g5_lat_0"][200:-100:5,300:900:5] )
+        lons = numpy.ravel( grib.variables["g5_lon_1"][200:-100:5,300:900:5] )
+        vals = numpy.ravel( grib.variables["A_PCP_GDS5_SFC_acc1h"][200:-100:5,300:900:5] )
         # Clip large values
         vals = numpy.where( vals > 250., 0, vals)
         #print 'STAGE4 MIN: %5.2f AVG: %5.2f MAX: %5.2f' % (numpy.min(vals), numpy.average(vals),
