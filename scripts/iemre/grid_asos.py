cd851499ae4acf290cb2bac93ecb7d7f4aca84e2
akrherz <akrherz@iastate.edu>
2011-08-30 18:17:16 +0000

    grid u and v, also update to use psycopg2

--- scripts/iemre/grid_asos.py
+++ scripts/iemre/grid_asos.py
@@ -5,36 +5,61 @@ except:
     import netCDF3
 import numpy
 import mx.DateTime
-from pyIEM import iemdb, mesonet
 import Ngl
 import iemre
+import iemdb
+import mesonet
+import network
+import psycopg2.extras
 
-i = iemdb.iemdb()
-mesosite = i['mesosite']
-asos = i['asos']
-iem = i['iem']
-locs = {}
-
-def load_stationtable():
-    sql = """SELECT id, x(geom) as lon, y(geom) as lat from
-         stations where network IN ('IA_ASOS','MO_ASOS','IL_ASOS',
+nt = network.Table(('IA_ASOS','MO_ASOS','IL_ASOS',
          'WI_ASOS','MN_ASOS', 'SD_ASOS', 'NE_ASOS', 'KS_ASOS', 'AWOS',
-         'IN_ASOS','KY_ASOS','OH_ASOS','MI_ASOS')"""
-    rs = mesosite.query( sql ).dictresult()
-    for i in range(len(rs)):
-        locs[ rs[i]['id'] ] = rs[i]
-    ids = `locs.keys()`
-    ids = "(%s)" % (ids[1:-1],)
+         'IN_ASOS','KY_ASOS','OH_ASOS','MI_ASOS'))
+
+ids = `nt.sts.keys()`
+ids = "(%s)" % (ids[1:-1],)
+
+def grid_wind(rs):
+    """
+    Grid winds based on u and v components
+    @param rs array of dicts
+    @return uwnd, vwnd
+    """
+    lats = []
+    lons = []
+    udata = []
+    vdata = []
+    for row in rs:
+        # mps
+        u,v = mesonet.uv( row['sknt'] / 0.514, row['drct'] )
+        if v is not None:
+            lats.append(  nt.sts[row['station']]['lat'] )
+            lons.append(  nt.sts[row['station']]['lon'] )
+            vdata.append( v )
+            udata.append( u )
+            
+    if len(vdata) < 4:
+        print "No wind data at all for time: %s" % (ts,)   
+        return None
+    
+    ugrid = Ngl.natgrid(lons, lats, udata, iemre.XAXIS, iemre.YAXIS)
+    vgrid = Ngl.natgrid(lons, lats, vdata, iemre.XAXIS, iemre.YAXIS)
+    if ugrid is not None:
+        ugt = ugrid.transpose()
+        vgt = vgrid.transpose()
+        return ugt, vgt
+    else:
+        return None, None
 
 def grid_skyc(rs):
     lats = []
     lons = []
     vals = []
-    for i in range(len(rs)):
-        v =  max(rs[i]['max_skyc1'], rs[i]['max_skyc2'], rs[i]['max_skyc3'])
+    for row in rs:
+        v =  max(row['max_skyc1'], row['max_skyc2'], row['max_skyc3'])
         if v is not None:
-            lats.append(  locs[rs[i]['station']]['lat'] )
-            lons.append(  locs[rs[i]['station']]['lon'] )
+            lats.append(  nt.sts[row['station']]['lat'] )
+            lons.append(  nt.sts[row['station']]['lon'] )
             vals.append( float(v) )
     if len(vals) < 4:
         print "No SKYC data at all for time: %s" % (ts,)   
@@ -54,11 +79,11 @@ def generic_gridder(rs, idx):
     lats = []
     lons = []
     vals = []
-    for i in range(len(rs)):
-        if rs[i][idx] is not None:
-            lats.append(  locs[rs[i]['station']]['lat'] )
-            lons.append(  locs[rs[i]['station']]['lon'] )
-            vals.append( rs[i][idx]  )
+    for row in rs:
+        if row[idx] is not None:
+            lats.append( nt.sts[row['station']]['lat'] )
+            lons.append( nt.sts[row['station']]['lon'] )
+            vals.append( row[idx]  )
     if len(vals) < 4:
         print "Only %s observations found for %s, won't grid" % (len(vals),
                idx)
@@ -75,8 +100,6 @@ def grid_hour(nc, ts):
     I proctor the gridding of data on an hourly basis
     @param ts Timestamp of the analysis, we'll consider a 20 minute window
     """
-    ids = `locs.keys()`
-    ids = "(%s)" % (ids[1:-1],)
     ts0 = ts - mx.DateTime.RelativeDateTime(minutes=10)
     ts1 = ts + mx.DateTime.RelativeDateTime(minutes=10)
 
@@ -84,14 +107,15 @@ def grid_hour(nc, ts):
 
     # If we are near realtime, look in IEMAccess instead of ASOS database
     if (mx.DateTime.gmt() - ts).hours < 36:
-        dbconn = iem
+        dbconn = iemdb.connect('iem', bypass=True)
         table = "current_log"
         pcolumn = "(phour * 25.4)"
     else:
-        dbconn = asos
+        dbconn = iemdb.connect('asos', bypass=True)
         table = "t%s" % (ts.localtime().year,)
         pcolumn = "p01m"
 
+    pcursor = dbconn.cursor(cursor_factory=psycopg2.extras.DictCursor)
     sql = """SELECT station,
          max(case when tmpf > -60 and tmpf < 130 THEN tmpf else null end) as max_tmpf,
          max(case when sknt > 0 and sknt < 100 then sknt else 0 end) as max_sknt,
@@ -99,16 +123,28 @@ def grid_hour(nc, ts):
          max(getskyc(skyc2)) as max_skyc2,
          max(getskyc(skyc3)) as max_skyc3,
          max(case when %s > 0 and %s < 1000 then %s else 0 end) as max_p01m,
-         max(case when dwpf > -60 and dwpf < 100 THEN dwpf else null end) as max_dwpf from %s  
+         max(case when dwpf > -60 and dwpf < 100 THEN dwpf else null end) as max_dwpf,
+         max(case when sknt >= 0 then sknt else 0 end) as sknt, 
+         max(case when sknt >= 0 then drct else 0 end) as drct from %s  
          WHERE station in %s and 
          valid >= '%s+00' and valid < '%s+00' GROUP by station""" % (
          pcolumn, pcolumn, pcolumn, table, ids, 
          ts0.strftime("%Y-%m-%d %H:%M"), ts1.strftime("%Y-%m-%d %H:%M") )
-    rs = dbconn.query( sql ).dictresult()
-    if len(rs) > 4:
+    pcursor.execute( sql )
+    
+    if pcursor.rowcount > 4:
+        rs = []
+        for row in pcursor:
+            rs.append( row )
+        ures, vres = grid_wind(rs)
+        if ures is not None:
+            nc.variables['uwnd'][offset] = ures
+            nc.variables['vwnd'][offset] = vres
+            
         res = generic_gridder(rs, 'max_tmpf')
         if res is not None:
             nc.variables['tmpk'][offset] = iemre.f2k(res)
+
         res = grid_skyc(rs)
         if res is not None:
             nc.variables['skyc'][offset] = res
@@ -117,9 +153,6 @@ def grid_hour(nc, ts):
             len(rs))
 
 def main(ts):
-    # Load up a station table we are interested in
-    load_stationtable()
-
     # Load up our netcdf file!
     nc = netCDF3.Dataset("/mnt/mesonet/data/iemre/%s_mw_hourly.nc" % (ts.year,), 'a')
     grid_hour(nc , ts)
