4caea5c0dbd1883b778817d1a4acb533c1900bb9
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2011-08-30 19:24:32 +0000

    update iemre to use new files?

--- htdocs/iemre/daily.py
+++ htdocs/iemre/daily.py
@@ -1,13 +1,17 @@
 #!/mesonet/python/bin/python
 
 import sys
-sys.path.insert(0, "../../scripts/iemre/")
+sys.path.insert(1, "/mesonet/www/apps/iemwebsite/scripts/lib/")
 import os
 import cgi
-import constants
-import netCDF3
+import iemre
+try:
+    import netCDF4 as netCDF3
+except:
+    import netCDF3
 import mx.DateTime
 import simplejson
+import mesonet
 
 form = cgi.FormContent()
 ts = mx.DateTime.strptime( form["date"][0], "%Y-%m-%d")
@@ -15,7 +19,7 @@ lat = float( form["lat"][0] )
 lon = float( form["lon"][0] )
 format = form["format"][0]
 
-i,j = constants.find_ij(lon, lat)
+i,j = iemre.find_ij(lon, lat)
 offset = int((ts - (ts + mx.DateTime.RelativeDateTime(month=1,day=1))).days)
 
 res = {'data': [], }
@@ -30,13 +34,13 @@ if os.path.isfile(fp):
 
   res['data'].append({
     'daily_high_f': "%.1f" % (
-       constants.k2f(nc.variables['high_tmpk'][offset,j,i]),),
+       mesonet.k2f(nc.variables['high_tmpk'][offset,j,i]),),
     'climate_daily_high_f': "%.1f" % (
-       constants.k2f(cnc.variables['high_tmpk'][coffset,j,i]),),
+       mesonet.k2f(cnc.variables['high_tmpk'][coffset,j,i]),),
     'daily_low_f': "%.1f" % (
-       constants.k2f(nc.variables['low_tmpk'][offset,j,i]),),
+       mesonet.k2f(nc.variables['low_tmpk'][offset,j,i]),),
     'climate_daily_low_f': "%.1f" % (
-       constants.k2f(cnc.variables['low_tmpk'][coffset,j,i]),),
+       mesonet.k2f(cnc.variables['low_tmpk'][coffset,j,i]),),
     'daily_precip_in': "%.2f" % (
        nc.variables['p01d'][offset,j,i] / 25.4,),
     'climate_daily_precip_in': "%.2f" % (
