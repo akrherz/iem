cff5ec07d75f4ee7bfd67e34e1cf1ce9701dc3ba
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2011-08-31 03:36:33 +0000

    don't allow nulls

--- htdocs/json/state_ugc.php
+++ htdocs/json/state_ugc.php
@@ -7,7 +7,7 @@ require_once '../../config/settings.inc.php';
 require_once "$rootpath/include/database.inc.php";
 $dbconn = iemdb('postgis');
 $rs = pg_prepare($dbconn, "SELECT", "SELECT ugc, name " .
-		"from nws_ugc WHERE state = $1 ORDER by name ASC");
+		"from nws_ugc WHERE state = $1 and ugc is not null and name is not null ORDER by name ASC");
 
 $st = isset($_REQUEST["state"]) ? $_REQUEST["state"] : 'IA';
 
