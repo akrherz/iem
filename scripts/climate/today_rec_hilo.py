682191ec84a2e108527c461b44055e98ef668e08
akrherz <akrherz@iastate.edu>
2011-09-01 13:07:42 +0000

    fix some issues with new climate database

--- scripts/climate/today_rec_hilo.py
+++ scripts/climate/today_rec_hilo.py
@@ -1,14 +1,13 @@
 # Generate a map of today's record high and low temperature
 
 import sys, os
-sys.path.append("../lib/")
 import iemplot
 
 import mx.DateTime
 now = mx.DateTime.now()
 
-from pyIEM import  stationTable
-st = stationTable.stationTable("/mesonet/TABLES/coopClimate.stns")
+import network
+nt = network.Table('IACLIMATE')
 st.sts["IA0200"]["lon"] = -93.6
 st.sts["IA5992"]["lat"] = 41.65
 import iemdb
@@ -16,7 +15,8 @@ import psycopg2.extras
 coop = iemdb.connect('coop', bypass=True)
 
 # Compute normal from the climate database
-sql = """SELECT station, max_high, min_low from climate WHERE valid = '2000-%s'""" % (
+sql = """SELECT station, max_high, min_low from climate WHERE valid = '2000-%s'
+    and substr(station,0,3) = 'ia'""" % (
   now.strftime("%m-%d"),)
 
 lats = []
@@ -29,8 +29,8 @@ c.execute(sql)
 for row in c:
   id = row['station'].upper()
   labels.append( id[2:] )
-  lats.append( st.sts[id]['lat'] )
-  lons.append( st.sts[id]['lon'] )
+  lats.append( nt.sts[id]['lat'] )
+  lons.append( nt.sts[id]['lon'] )
   highs.append( row['max_high'] )
   lows.append( row['min_low'] )
 
