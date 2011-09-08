1888228c7b715a6857e14f5881650afbf62b7d5e
akrherz <akrherz@iastate.edu>
2011-09-08 21:47:54 +0000

    clean up and not do DCP data

--- scripts/summary/hourly_precip.py
+++ scripts/summary/hourly_precip.py
@@ -1,19 +1,25 @@
-
+"""
+My purpose in life is to update the hourly_YYYY with observed or
+computed hourly precipitation
+"""
 import mx.DateTime
-from pyIEM import iemdb
-i = iemdb.iemdb()
-iem = i['iem']
-iem.query("SET TIME ZONE 'GMT'")
+import iemdb
+IEM = iemdb.connect('iem')
+icursor = IEM.cursor()
+icursor.execute("SET TIME ZONE 'GMT'")
 
-  # Our period
+# Our period
 t0 = mx.DateTime.utc() + mx.DateTime.RelativeDateTime(hours=-1,minute=0)
 t1 = mx.DateTime.utc() + mx.DateTime.RelativeDateTime(hours=-0,minute=0)
 
-sql = "INSERT into hourly_%s \
-       (SELECT station, network, '%s+00'::timestamp as v, max(phour) as p \
-        from current_log WHERE (valid - '1 minute'::interval) >= '%s' \
-        and (valid - '1 minute'::interval) < '%s' and phour >= 0 and \
-        network NOT IN ('KCCI','KELO','KIMT') GROUP by station, network, v)" % \
-       (t0.year, t0.strftime("%Y-%m-%d %H:%M"), t0.strftime("%Y-%m-%d %H:%M"), \
+sql = """INSERT into hourly_%s 
+       (SELECT station, network, '%s+00'::timestamp as v, max(phour) as p 
+        from current_log WHERE (valid - '1 minute'::interval) >= '%s' 
+        and (valid - '1 minute'::interval) < '%s' and phour >= 0 and 
+        network NOT IN ('KCCI','KELO','KIMT') 
+        and network ~* 'DCP' GROUP by station, network, v)""" % (
+                t0.year, t0.strftime("%Y-%m-%d %H:%M"), t0.strftime("%Y-%m-%d %H:%M"), 
         t1.strftime("%Y-%m-%d %H:%M") )
-iem.query( sql )
+icursor.execute( sql )
+IEM.commit()
+IEM.close()
