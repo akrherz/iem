52b9f741eceb280f2bc27c871a9736a8c601574e
akrherz <akrherz@iastate.edu>
2011-09-10 13:14:57 +0000

    add some more plots

---
+++ htdocs/plotting/isumet/rh.php
@@ -0,0 +1,108 @@
+<?php
+include("../../../config/settings.inc.php");
+
+$year = isset($_GET["year"]) ? $_GET["year"] : date("Y");
+$month = isset($_GET["month"]) ? $_GET["month"] : date("m");
+$day = isset($_GET["day"]) ? $_GET["day"] : date("d");
+$station = isset($_REQUEST['station']) ? $_REQUEST['station']: null;
+
+if (strlen($year) == 4 && strlen($month) > 0 && strlen($day) > 0 ){
+  $myTime = strtotime($year."-".$month."-".$day);
+} else {
+  $myTime = strtotime( date("Y-m-d") );
+}
+
+$wA = mktime(0,0,0, 8, 4, 2002);
+$wLabel = "1min Instantaneous Wind Speed";
+if ($wA > $myTime){
+ $wLabel = "Instant Wind Speed";
+}
+
+$titleDate = strftime("%b %d, %Y", $myTime);
+
+$dirRef = strftime("%Y/%m/%d", $myTime);
+
+$srad = array();
+$irelh = array();
+$orelh = array();
+
+if ($station == null){
+	$fcontents = file("/mesonet/ARCHIVE/data/$dirRef/text/ot/ot0002.dat");
+	while (list ($line_num, $line) = each ($fcontents)) {
+  		$valid[] = strtotime( substr($line, 0, 26) );
+  		$parts = preg_split ("/\s+/", $line);
+		$mph[] = intval($parts[8]);
+		$drct[] = intval($parts[9]);
+		
+	} // End of while
+} else {
+	$fcontents = file("/mesonet/ARCHIVE/data/$dirRef/text/ot/ot0010.dat");
+	/*
+	 * month, day, year, hour, minute, outside temp, hi outside temp, lo outside
+       temp, outside humidity, wind speed, wind direction, wind gust speed, time
+ 		of gust, pressure, daily_precip, monthly_rain, yearly_rain, inside
+ 		temp, inside humidity, solar radiation, UV index
+	 */
+	while (list ($line_num, $line) = each ($fcontents)) {
+		$tokens = explode(' ', $line);
+		if (sizeof($tokens) != 21){
+			continue;
+		}
+  		$tstring = sprintf("%s %s", $dirRef, $tokens[3]);
+  		$valid[] = strtotime($tstring);
+  		$orelh[] = floatval($tokens[8]);
+  		$irelh[] = floatval($tokens[18]);
+ 	} // End of while
+	
+}
+
+include ("$rootpath/include/jpgraph/jpgraph.php");
+include ("$rootpath/include/jpgraph/jpgraph_line.php");
+include ("$rootpath/include/jpgraph/jpgraph_scatter.php");
+include ("$rootpath/include/jpgraph/jpgraph_date.php");
+
+// Create the graph. These two calls are always required
+$graph = new Graph(600,300,"example1");
+$graph->SetScale("datelin");
+$graph->img->SetMargin(55,40,55,60);
+
+//$graph->yaxis->scale->ticks->SetPrecision(1);
+$graph->title->Set("Inside and Outline Relative Humidity");
+$graph->subtitle->Set($titleDate );
+
+$graph->legend->SetLayout(LEGEND_HOR);
+$graph->legend->Pos(0.01,0.08);
+$graph->xaxis->SetLabelAngle(90);
+$graph->yaxis->scale->ticks->Set(90,15);
+//$graph->yaxis->scale->ticks->SetPrecision(0);
+//$graph->yaxis->scale->ticks->SetPrecision(0);
+
+$graph->yaxis->SetColor("blue");
+
+$graph->title->SetFont(FF_FONT1,FS_BOLD,14);
+
+$graph->yaxis->SetTitle("Relative Humidity [%]");
+
+$graph->yaxis->title->SetFont(FF_FONT1,FS_BOLD,12);
+$graph->xaxis->SetTitle("Valid Local Time");
+$graph->xaxis->SetTitleMargin(30);
+$graph->yaxis->SetTitleMargin(30);
+//$graph->y2axis->SetTitleMargin(28);
+$graph->xaxis->title->SetFont(FF_FONT1,FS_BOLD,12);
+$graph->xaxis->SetPos("min");
+
+// Create the linear plot
+$lineplot=new LinePlot($irelh, $valid);
+$lineplot->SetLegend("Inside RH");
+$lineplot->SetColor("blue");
+
+$lineplot2=new LinePlot($orelh, $valid);
+$lineplot2->SetLegend("Outside RH");
+$lineplot2->SetColor("red");
+
+
+$graph->Add($lineplot);
+$graph->Add($lineplot2);
+
+$graph->Stroke();
+?>
