e87d36e39f3033e459a5d31bff671f9c2c4078ae
akrherz <akrherz@iastate.edu>
2011-09-23 10:59:53 +0000

    fix stationID

--- scripts/iemre/rerun_hourly.py
+++ scripts/iemre/rerun_hourly.py
@@ -1,8 +1,8 @@
 import mx.DateTime
 import stage4_hourlyre
 
-sts = mx.DateTime.DateTime(2010,5,1)
-ets = mx.DateTime.DateTime(2010,5,13)
+sts = mx.DateTime.DateTime(2010,11,1)
+ets = mx.DateTime.DateTime(2011,1,1)
 interval = mx.DateTime.RelativeDateTime(hours=1)
 now = sts
 while now < ets:
