9de63b1316a2eeb19d59a10c2fa0d2bee01aad02
akrherz <akrherz@iastate.edu>
2011-10-12 14:42:41 +0000

    add more vars

--- cgi-bin/climate/cplot.py
+++ cgi-bin/climate/cplot.py
@@ -24,6 +24,14 @@ import network
 from scipy import stats
 
 META = {
+ 'rain_days': {
+    'title': 'Station Average Number of Days of 1.25" Precipitation',
+    'ylabel': 'Number of Days',
+    'xlabel': 'Year', 
+    'func': '',
+    'month_bounds': '', 
+    'valid_offset': '',        
+  }, 
  'annual_sum_precip': {
     'title': 'Annual Precipitation (rain + melted snow)',
     'ylabel': 'Precipitation [inches]',
@@ -39,7 +47,71 @@ META = {
     'func': 'avg((high+low)/2.)', 
     'month_bounds': '',   
     'valid_offset': '',     
+  }, 
+  'winter_avg_temp': {
+    'title': 'Winter [DJF] Average Temperature',
+    'ylabel': 'Temperature [F]',
+    'xlabel': 'Year',   
+    'func': 'avg((high+low)/2.)', 
+    'month_bounds': 'and month in (12,1,2)',   
+    'valid_offset': "- '2 month'::interval ",     
+  },   
+ 'winter_avg_low': {
+    'title': 'Winter [DJF] Average Low Temperature',
+    'ylabel': 'Temperature [F]',
+    'xlabel': 'Year',   
+    'func': 'avg(low)', 
+    'month_bounds': 'and month in (12,1,2)',   
+    'valid_offset': "- '2 month'::interval ",     
+  }, 
+'winter_avg_high': {
+    'title': 'Winter [DJF] Average High Temperature',
+    'ylabel': 'Temperature [F]',
+    'xlabel': 'Year',   
+    'func': 'avg(high)', 
+    'month_bounds': 'and month in (12,1,2)',   
+    'valid_offset': "- '2 month'::interval ",     
+  }, 
+ 'summer_avg_temp': {
+    'title': 'Summer [JJA] Average Temperature',
+    'ylabel': 'Temperature [F]',
+    'xlabel': 'Year',   
+    'func': 'avg((high+low)/2.)', 
+    'month_bounds': 'and month in (6,7,8)',   
+    'valid_offset': " ",     
   },  
+'summer_avg_high': {
+    'title': 'Summer [JJA] Average High Temperature',
+    'ylabel': 'Temperature [F]',
+    'xlabel': 'Year',   
+    'func': 'avg(high)', 
+    'month_bounds': 'and month in (6,7,8)',   
+    'valid_offset': " ",     
+  }, 
+  'summer_avg_low': {
+    'title': 'Summer [JJA] Average Low Temperature',
+    'ylabel': 'Temperature [F]',
+    'xlabel': 'Year',   
+    'func': 'avg(low)', 
+    'month_bounds': 'and month in (6,7,8)',   
+    'valid_offset': " ",     
+  }, 
+ 'spring_avg_temp': {
+    'title': 'Spring [MAM] Average Temperature',
+    'ylabel': 'Temperature [F]',
+    'xlabel': 'Year',   
+    'func': 'avg((high+low)/2.)', 
+    'month_bounds': 'and month in (3,4,5)',   
+    'valid_offset': " ",     
+  }, 
+ 'fall_avg_temp': {
+    'title': 'Fall [SON] Average Temperature',
+    'ylabel': 'Temperature [F]',
+    'xlabel': 'Year',   
+    'func': 'avg((high+low)/2.)', 
+    'month_bounds': 'and month in (9,10,11)',   
+    'valid_offset': " ",     
+  },   
  'frost_free': {
     'title': 'Frost Free Days',
     'ylabel': 'Days',
@@ -95,6 +167,16 @@ def yearly_plot(ax, cfg):
            year <= %(last_year)s GROUP by year) as fall 
          WHERE spring.year = fall.year ORDER by fall.year ASC
         """ % cfg )
+    elif cfg['plot_type'] == 'rain_days':
+        cfg['st'] = cfg['station'][:2]
+        ccursor.execute("""
+select year as yr, avg(cnt) 
+from (select station, year, sum(case when precip >= 1.25 then 1 else 0 end) as cnt from 
+alldata_%(st)s WHERE 
+station in (select distinct station from alldata_%(st)s where year = %(first_year)s 
+and precip > 0 and year >= %(first_year)s and  year <= %(last_year)s) 
+GROUP by station, year) as foo GROUP by yr ORDER by yr ASC
+        """ % cfg )
     else:
         ccursor.execute("""
         SELECT extract(year from (day %s)) as yr, %s as data from alldata_%s WHERE station = '%s' 
@@ -102,11 +184,20 @@ def yearly_plot(ax, cfg):
         """ % (META[cfg['plot_type']]['valid_offset'], META[cfg['plot_type']]['func'], cfg['station'][:2], cfg['station'],  
             META[cfg['plot_type']]['month_bounds']))
     ydata = []
+    y50 = []
+    y00 = []
     for row in ccursor:
         if row[0] < cfg['first_year'] or row[0] > cfg['last_year']:
             continue
         ydata.append( float(row[1]) )
+        if row[0] > 1950 and row[0] < 1961:
+            y50.append( float(row[1]) )
+        if row[0] > 2000 and row[0] < 2011:
+            y00.append( float(row[1]) )
+            
     ydata = numpy.array( ydata )
+    y50 = numpy.array( y50 )
+    y00 = numpy.array( y00 )
     #print 'Content-type: text/plain\n'
     #print ydata
     #sys.exit()
@@ -128,7 +219,12 @@ def yearly_plot(ax, cfg):
         slope, intercept, r_value, p_value, std_err = stats.linregress(xaxis, ydata)
         ax.plot(xaxis, slope * xaxis + intercept, color='#CC6633')
         ax.text( cfg['first_year'], max(ydata), '$R^2$=%.2f' % (r_value ** 2,), color='#CC6633')
-    
+        #ypos = max(ydata) - (max(ydata) - min(ydata)) * 0.05
+        #ax.text( cfg['first_year'], ypos, 'slope=%.2f/decade' % (slope*10,), color='#CC6633')
+        #ypos = max(ydata) - (max(ydata) - min(ydata)) * 0.1
+        #ax.text( cfg['first_year'], ypos, '1950 %.2f' % (numpy.average(y50),), color='#CC6633')
+        #ypos = max(ydata) - (max(ydata) - min(ydata)) * 0.15
+        #ax.text( cfg['first_year'], ypos, '2000 %.2f' % (numpy.average(y00),), color='#CC6633')
 
 def process_cgi(form):
     """
