743a7a4f7435dc2f3fd7097f15839e56c5c3a736
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2011-10-18 21:12:19 +0000

    timezone support, I hope

--- scripts/summary/updateRain.py
+++ scripts/summary/updateRain.py
@@ -6,12 +6,12 @@ now = mx.DateTime.now()
 yyyy = now.year
 
 rs = iemdb.query("""select station, network, sum(rain) as rain from 
-  (SELECT station, network, max(phour) as rain, 
-  extract(hour from (valid - '1 minute'::interval)) as hour from current_log 
-  WHERE (network IN ('AWOS') or network ~* 'ASOS') 
-  and date(valid) = 'TODAY'::timestamp 
+  (SELECT station, s.network, max(phour) as rain, 
+  extract(hour from (valid - '1 minute'::interval)) as hour from current_log c, stations s
+  WHERE (c.network IN ('AWOS') or c.network ~* 'ASOS') and c.network = s.network
+  and  date(valid at time zone s.tzname) = date(now() at time zone s.tzname) 
   and phour > 0 
-  GROUP by station, network, hour) as foo 
+  GROUP by station, s.network, hour) as foo 
   GROUP by station, network""").dictresult()
 
 for i in range(len(rs)):
