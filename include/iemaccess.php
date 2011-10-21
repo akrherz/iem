2cc7f792fc29264f6e59bafee1cd3a1bacaae622
akrherz <akrherz@iastate.edu>
2011-10-21 02:19:00 +0000

    more fixes for upcoming iem database change

--- include/iemaccess.php
+++ include/iemaccess.php
@@ -2,7 +2,7 @@
 /**
  * Time for a standard library to access the IEMAccess DB for observations
  *  This could theoretically be distributed to others to access the DB!
- * Daryl Herzmann 18 June 2003
+ * $Id: $:
  */
 
 include_once("$rootpath/include/database.inc.php");
@@ -22,30 +22,32 @@ class IEMAccess {
 
   function getSingleSite($sid) {
     $sid = strtoupper($sid);
-    $rs = pg_exec($this->dbconn, sprintf("select *, x(c.geom) as x, y(c.geom) as y, 
+    $rs = pg_exec($this->dbconn, sprintf("select *, 
+    x(s.geom) as x, y(s.geom) as y, 
     valid at time zone '%s' as lvalid,
     max_gust_ts at time zone '%s' as lmax_gust_ts,
-    max_sknt_ts at time zone '%s' as lmax_sknt_ts from current c LEFT JOIN summary s USING (station, network) WHERE c.station = '$sid' and s.day = 'TODAY'", $this->tzname, $this->tzname,$this->tzname));
+    max_sknt_ts at time zone '%s' as lmax_sknt_ts from 
+    current c2, summary_%s c, stations s WHERE 
+    c.station = '$sid' and c.station = s.id and c.network = s.network
+    and c2.station = s.id and c2.network = s.network 
+    and c.day = date(now() at time zone s.tzname)", 
+    $this->tzname, $this->tzname,$this->tzname, date("Y")));
     return new IEMAccessOb(@pg_fetch_array($rs,0));
-  }
-
-  function getSingleSiteYest($sid) {
-    $rs = pg_exec($this->dbconn, sprintf("select *, valid at time zone '%s' as lvalid,
-    max_gust_ts at time zone '%s' as lmax_gust_ts,
-    max_sknt_ts at time zone '%s' as lmax_sknt_ts from current c LEFT JOIN summary s 
-    USING (station, network) WHERE c.station = '$sid' and s.day = 'YESTERDAY'", $this->tzname, $this->tzname,$this->tzname ));
-    return new IEMAccessOb(pg_fetch_array($rs,0));
   }
 
   function getNetwork($network) {
     $ret = Array();
-    $sql = sprintf("select *, c.pday as ob_pday, x(c.geom) as x, 
-    y(c.geom) as y, valid at time zone '%s' as lvalid,
+    $sql = sprintf("select *, c.pday as ob_pday, x(s.geom) as x, 
+    y(s.geom) as y, valid at time zone '%s' as lvalid,
     max_gust_ts at time zone '%s' as lmax_gust_ts,
     max_sknt_ts at time zone '%s' as lmax_sknt_ts from 
-    current c LEFT JOIN summary s USING (station, network) 
-    WHERE c.network = '$network' and s.day = date(now() at time zone '%s')",
-    $this->tzname, $this->tzname, $this->tzname, $this->tzname);
+    current c2, summary_%s c, stations s  
+    WHERE c.network = '$network' and c.network = c2.network and
+    c.network = s.network and c.station = s.id and 
+    s.id = c2.station and 
+    c.day = date(now() at time zone '%s')",
+    $this->tzname, $this->tzname, $this->tzname, date("Y"), 
+    $this->tzname);
     $rs = pg_exec($this->dbconn, $sql);
     for( $i=0; $row = @pg_fetch_assoc($rs,$i); $i++) {
       $ret[$row["station"]] = new IEMAccessOb($row);
@@ -53,28 +55,24 @@ class IEMAccess {
     return $ret;
   }
 
-  function getAll() {
+  function getIowa() {
     $ret = Array();
-    $rs = pg_exec($this->dbconn, sprintf("select *, x(c.geom) as x, y(c.geom) as y, valid at time zone '%s' as lvalid,
+    $rs = pg_exec($this->dbconn, sprintf("select *, 
+    x(s.geom) as x, y(s.geom) as y, valid at time zone '%s' as lvalid,
     max_gust_ts at time zone '%s' as lmax_gust_ts,
-    max_sknt_ts at time zone '%s' as lmax_sknt_ts from current c LEFT JOIN summary s USING (station, network) WHERE c.network IN ('IA_RWIS', 'IA_ASOS', 'AWOS', 'KCCI', 'KIMT') and s.day = 'TODAY' and c.valid > 'TODAY' ", $this->tzname, $this->tzname, $this->tzname));
+    max_sknt_ts at time zone '%s' as lmax_sknt_ts from 
+    current c2, summary_%s c, stations s WHERE 
+    c.network IN ('IA_RWIS', 'IA_ASOS', 'AWOS', 'KCCI', 'KIMT') 
+    and c.day = 'TODAY' 
+    and c2.valid > 'TODAY' and s.network = c.network and
+    c.network = c2.network and s.id = c.station and 
+    c.station = c2.station", $this->tzname, 
+    $this->tzname, $this->tzname, date("Y")));
     for( $i=0; $row = @pg_fetch_array($rs,$i); $i++) {
       $ret[$row["station"]] = new IEMAccessOb($row);
     }
     return $ret;
   }
-
-
-  function getNetworkSummary($network, $valid) {
-    $ret = Array();
-    $rs = pg_exec($this->dbconn, "select *, x(c.geom) as x, y(c.geom) as y from current c LEFT JOIN summary s USING (station, network) WHERE c.network = '$network' and s.day = '$valid'");
-    for( $i=0; $row = @pg_fetch_array($rs,$i); $i++) {
-      $ret[$row["station"]] = new IEMAccessOb($row);
-      $ret[$row["station"]]->db["obtime"] = $valid;
-    }
-    return $ret;
-  }
-
 
   function ge_max_tmpf($size) {
     $ret = Array();
