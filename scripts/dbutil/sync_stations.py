74de05205d3b884e0f011cab21243db7f6ea77d0
akrherz <akrherz@iastate.edu>
2011-10-21 17:23:16 +0000

    upstream changes to make this slicky

--- scripts/dbutil/sync_stations.py
+++ scripts/dbutil/sync_stations.py
@@ -2,6 +2,7 @@
 # databases.  This will hopefully remove some hackery
 
 import iemdb
+import datetime
 import psycopg2
 import psycopg2.extras
 MESOSITE = iemdb.connect("mesosite")
@@ -14,21 +15,34 @@ def sync(dbname):
     # connect to synced database
     dbconn = iemdb.connect( dbname )
     dbcursor = dbconn.cursor()
-    # query stations from source
+    # Figure out our latest revision
+    dbcursor.execute("""
+    SELECT max(modified), max(iemid) from stations
+    """)
+    row = dbcursor.fetchone()
+    maxTS = (row[0] or datetime.datetime(1980,1,1))
+    maxID = (row[1] or -1)
+    # figure out what has changed!
     cur = MESOSITE.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    cur.execute("""SELECT * from stations""")
-    rows = cur.fetchall()
-    cur.close()
-    # delete current stations in dest
-    dbcursor.execute("""DELETE from stations""")
-    # insert queried stations
-    dbcursor.executemany("""INSERT into stations(id, name, state,
+    cur.execute("""SELECT * from stations WHERE modified > %s or iemid > %s""",
+    (maxTS, maxID) )
+    for row in cur:
+      # delete current stations in dest
+      dbcursor.execute("""DELETE from stations WHERE iemid = %s or 
+       (network = %s and id = %s)""", (row['iemid'], row['network'], row['id']))
+      # insert queried stations
+      dbcursor.execute("""INSERT into stations(id, name, state,
      elevation, network, online, geom, params, county, plot_name, climate_site,
-     wfo, archive_begin, archive_end, remote_id) values (%(id)s,
+     wfo, archive_begin, archive_end, remote_id, tzname, country, iemid,
+     modified) 
+     values (%(id)s,
       %(name)s, %(state)s, %(elevation)s, %(network)s, %(online)s,
      %(geom)s, %(params)s, %(county)s, %(plot_name)s, %(climate_site)s,
-     %(wfo)s, %(archive_begin)s, %(archive_end)s, %(remote_id)s)""",
-     rows)
+     %(wfo)s, %(archive_begin)s, %(archive_end)s, %(remote_id)s,
+     %(tzname)s, %(country)s, %(iemid)s, %(modified)s)""",
+     row)
+    print 'Database: %s Modified %s rows TS: %s IEMID: %s' % (dbname, 
+       cur.rowcount, maxTS, maxID)
     # close connection
     dbcursor.close()
     dbconn.commit()
