ef88209b7a7635ef8c6eeec491c8d45db9074d89
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2011-10-23 16:22:43 +0000

    cleanups, move things to more appropriate places

--- scripts/dbutil/SYNC_STATIONS.sh
+++ scripts/dbutil/SYNC_STATIONS.sh
@@ -2,7 +2,10 @@
 # Need to sync all our station metadata, when we add new sites!
 
 python checkElevation.py
-python assignCounty.py
+python set_county.py
 python assignClimate.py
-python checkWFO.py
+python set_wfo.py
 python set_timezone.py
+
+python sync_stations.py
+python addSiteMesosite.py
