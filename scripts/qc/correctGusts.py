c3c49c70b0c531a1fabb466c4cea4fb49b18694a
akrherz <akrherz@iastate.edu>
2011-10-24 12:13:24 +0000

    typos

--- scripts/qc/correctGusts.py
+++ scripts/qc/correctGusts.py
@@ -7,7 +7,7 @@ icursor = IEM.cursor()
 
 today = mx.DateTime.now()
 sql = """update summary_%s s SET max_gust = 0 
-    FROM stations t WHERE t.iemd = s.iemdb and day = 'TODAY' and max_gust_ts < '%s 00:05' and
+    FROM stations t WHERE t.iemid = s.iemid and day = 'TODAY' and max_gust_ts < '%s 00:05' and
     t.network in ('KCCI','KIMT','KELO')""" % (
     today.year, today.strftime("%Y-%m-%d"),)
 icursor.execute(sql)
