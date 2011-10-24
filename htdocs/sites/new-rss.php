4b0fdc6de94ce4a47d4369a794fd6f7b4bab8147
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2011-10-24 19:06:25 +0000

    compliance checks

--- htdocs/sites/new-rss.php
+++ htdocs/sites/new-rss.php
@@ -18,24 +18,24 @@
  echo "<subtitle>Iowa Environmental Mesonet - New Stations GeoRSS</subtitle>\n";
  echo "<link href=\"http://mesonet.agron.iastate.edu/sites/locate.php\" />\n";
  echo "<updated>". gmdate('Y-m-d\\TH:i:s\\Z') ."</updated>\n";
- echo "<id>iem-new-sites-rss</id>\n";
+ echo "<id>http://mesonet.agron.iastate.edu/sites/locate.php</id>\n";
  echo "<author><name>Daryl Herzmann</name><email>akrherz@iastate.edu</email></author>\n";
  
  for ($i=0; $row = @pg_fetch_array($rs, $i); $i++) {
- 	$cbody = "<pre>\n";
- 	$cbody .= "ID     : ". $row["id"] ."\n";
- 	$cbody .= "Name   : ". $row["name"] ."\n";
-	$cbody .= "Lat    : ". $row["lat"] ."\n";
-    $cbody .= "Lon    : ". $row["lon"] ."\n";
-    $cbody .= "Ele [m]: ". $row["elevation"] ."\n";
-    $cbody .= "Network: ". $row["netname"] ."(". $row["network"] .")\n";
- 	$cbody .= "</pre>\n";
+ 	$cbody = "<p>\n";
+ 	$cbody .= "<br />ID     : ". $row["id"] ."\n";
+ 	$cbody .= "<br />Name   : ". $row["name"] ."\n";
+	$cbody .= "<br />Lat    : ". $row["lat"] ."\n";
+    $cbody .= "<br />Lon    : ". $row["lon"] ."\n";
+    $cbody .= "<br />Ele [m]: ". $row["elevation"] ."\n";
+    $cbody .= "<br />Network: ". $row["netname"] ."(". $row["network"] .")\n";
+ 	$cbody .= "</p>\n";
   echo "<entry>\n";
   echo "<title>". $row["name"] ." [". $row["id"] ."]</title>\n";
-  echo "<author>akrherz@iastate.edu (Daryl Herzmann)</author>\n";
+  echo "<author><name>Daryl Herzmann</name><email>akrherz@iastate.edu</email></author>\n";
   echo "<link href=\"http://mesonet.agron.iastate.edu/sites/site.php?station=". $row["id"] ."&amp;network=". $row["network"] ."\" />\n";
   echo "<content>". $cbody ."</content>\n";
-  echo "<id>". $row["iemid"] ."</id>\n";
+  echo "<id>http://mesonet.agron.iastate.edu/sites/site.php?station=". $row["id"] ."&amp;network=". $row["network"] ."</id>\n";
   echo "<updated>". gmdate('Y-m-d\\TH:i:s\\Z', strtotime($row["modified"])) ."</updated>\n";
   echo "<georss:point>". $row["lat"] ." ". $row["lon"] ."</georss:point>\n";
   echo "</entry>\n";
