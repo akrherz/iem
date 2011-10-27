421f98a2bf628584fef70ab55725178f76b6be45
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2011-10-27 19:07:37 +0000

    cleanup and make it report an error for dbconnection failures, should get in the habit of checking the connection object for validity

--- include/database.inc.php
+++ include/database.inc.php
@@ -1,30 +1,40 @@
 <?php
+/*
+ * Database connection function that most every script uses :)
+ * $Id: $:
+ */
 $iem20 = 'iemdb';
 global $_DATABASES;
 $_DATABASES = Array(
- 'access' => "dbname=iem host=$iem20 user=nobody",
- 'iem' => "dbname=iem host=$iem20 user=nobody",
- 'afos' => "dbname=afos host=$iem20 user=nobody",
- 'hads' => "dbname=hads host=$iem20 user=nobody",
- 'asos' => "dbname=asos host=$iem20 user=nobody",
- 'coop' => "dbname=coop host=$iem20 user=nobody",
- 'awos' => "dbname=awos host=$iem20 user=nobody",
- 'mos' => "dbname=mos host=192.168.0.20 user=nobody",
- 'rwis' => "dbname=rwis host=$iem20 user=nobody",
- 'wepp' => "dbname=wepp host=$iem20 user=nobody",
- 'snet' => "dbname=snet host=$iem20 user=nobody",
- 'mesosite' => "dbname=mesosite host=$iem20 user=nobody",
- 'isuag' => "dbname=isuag host=$iem20 user=nobody",
- 'other' => "dbname=other host=$iem20 user=nobody",
- 'postgis' => "dbname=postgis host=$iem20 user=nobody",
- 'portfolio' => "dbname=portfolio host=meteor.geol.iastate.edu user=mesonet",
- 'scan' => "dbname=scan host=$iem20 user=nobody",
- 'squaw' => "dbname=squaw host=$iem20 user=nobody",
+ 'access' => "dbname=iem host=$iem20 user=nobody connect_timeout=5",
+ 'iem' => "dbname=iem host=$iem20 user=nobody connect_timeout=5",
+ 'afos' => "dbname=afos host=$iem20 user=nobody connect_timeout=5",
+ 'hads' => "dbname=hads host=$iem20 user=nobody connect_timeout=5",
+ 'asos' => "dbname=asos host=$iem20 user=nobody connect_timeout=5",
+ 'coop' => "dbname=coop host=$iem20 user=nobody connect_timeout=5",
+ 'awos' => "dbname=awos host=$iem20 user=nobody connect_timeout=5",
+ 'mos' => "dbname=mos host=$iem20 user=nobody connect_timeout=5",
+ 'rwis' => "dbname=rwis host=$iem20 user=nobody connect_timeout=5",
+ 'wepp' => "dbname=wepp host=$iem20 user=nobody connect_timeout=5",
+ 'snet' => "dbname=snet host=$iem20 user=nobody connect_timeout=5",
+ 'mesosite' => "dbname=mesosite host=$iem20 user=nobody connect_timeout=5",
+ 'isuag' => "dbname=isuag host=$iem20 user=nobody connect_timeout=5",
+ 'other' => "dbname=other host=$iem20 user=nobody connect_timeout=5",
+ 'postgis' => "dbname=postgis host=$iem20 user=nobody connect_timeout=5",
+ 'portfolio' => "dbname=portfolio host=meteor.geol.iastate.edu user=mesonet connect_timeout=5",
+ 'scan' => "dbname=scan host=$iem20 user=nobody connect_timeout=5",
+ 'squaw' => "dbname=squaw host=$iem20 user=nobody connect_timeout=5",
 );
+
+function database_failure($DBKEY)
+{
+	echo sprintf("<div class='warning'>Unable to contact database: %s</div>", $DBKEY);
+}
 
 function iemdb($DBKEY)
 {
   global $_DATABASES;
-  return pg_connect( $_DATABASES[$DBKEY] );
+  $db = pg_connect( $_DATABASES[$DBKEY] ) or database_failure($DBKEY);
+  return $db;
 }
 ?>
