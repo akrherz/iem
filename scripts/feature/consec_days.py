8ed5721c9ff9fdea278c82326fb983eefa96adaa
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2011-12-08 23:35:18 +0000

    oniem

--- scripts/feature/consec_days.py
+++ scripts/feature/consec_days.py
@@ -15,14 +15,20 @@ mindoy = None
 for row in ccursor:
     if mindoy is None and row[2] < 32:
         ryear = row[0].year
-        mindoy = 1
+        mindoy = 0
         doy.append( int(row[0].strftime("%j")) )
         cnts.append( 0 )
+    if row[2] < 32 and mindoy >= 0:
+        mindoy += 1
     if row[2] < 32:
         cnts[-1] += 1
+    else:
+        mindoy = -1
     if ryear != row[0].year:
+        first.append( mindoy )
         mindoy = None
         ryear = row[0].year
+first.append( mindoy )
 
 import matplotlib.pyplot as plt
 import mx.DateTime
@@ -31,35 +37,12 @@ cnts = numpy.array(cnts)
 xticks = [1,32,62,93,124]
 xticklabels = ['1 Sep','1 Oct', '1 Nov', '1 Dec', '1 Jan']
 
-fig = plt.figure()
-ax = fig.add_subplot(311)
-ax.scatter( numpy.arange(1899,2012), doy)
-ax.set_yticks(xticks)
-ax.set_yticklabels(xticklabels)
-ax.set_ylim(32,135)
-ax.set_xlim(1898.5,2011.5)
-ax.set_ylabel("First Day")
-ax.grid(True)
-ax.set_title("Ames [1900-2011] Days Below Freezing")
-
-ax2 = fig.add_subplot(312)
-bars = ax2.bar( numpy.arange(1899,2011)-0.4, cnts[:-1], facecolor='b', 
-        edgecolor='b')
-avgV = numpy.average(cnts)
-for bar in bars:
-  if bar.get_height() < avgV:
-    bar.set_facecolor('r')
-    bar.set_edgecolor('r')
-ax2.set_xlim(1898.5,2010.5)
-ax2.set_ylabel("Frozen Days")
-ax2.grid(True)
-
-ax3 = fig.add_subplot(313)
-ax3.scatter( doy[:-1], cnts[:-1], color='b')
-ax3.set_xticks(xticks)
-ax3.set_xticklabels(xticklabels)
-ax3.set_xlim(32,135)
-ax3.plot([32,135], [avgV,avgV], color='k')
+ax3 = fig.add_subplot(111)
+ax3.scatter( first, cnts[:-1], color='b')
+#ax3.set_xticks(xticks)
+#ax3.set_xticklabels(xticklabels)
+#ax3.set_xlim(32,135)
+#ax3.plot([32,135], [avgV,avgV], color='k')
 ax3.grid(True)     
 ax3.set_xlabel("First Day")
 ax3.set_ylabel("Frozen Days")
