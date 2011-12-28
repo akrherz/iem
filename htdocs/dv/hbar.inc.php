3672c652161efc3f3f886ef4550803980a12968d
akrherz <akrherz@iastate.edu>
2011-12-28 18:32:39 +0000

    remove cruft

--- htdocs/dv/hbar.inc.php
+++
@@ -1,8 +0,0 @@
-<table cellpadding=2 cellspacing=0 border=0>
-<tr>
-  <th>Mode:</th>
-  <td bgcolor="#FFA500"><a href="<?php echo $urlbase ."/current/$valid/$mode/$id/"; ?>">Real-Time</a></td>
-  <td bgcolor="#1E90FF"><a href="<?php echo $urlbase ."/daily/$valid/$mode/$id/"; ?>">Daily Summary</a></td>
-  <td bgcolor="#FFC0CB">Monthly Summary</td>
-  <td width="95%"> &nbsp; </td></tr>
-</table>
