800f510be814ba06317aced827b57e6f53a616fd
akrherz <akrherz@iastate.edu>
2012-01-12 17:54:35 +0000

    fixups and remove pyIEM

--- cgi-bin/geocoder.py
+++ cgi-bin/geocoder.py
@@ -1,6 +1,9 @@
-#!/mesonet/python/bin/python
-# GeoCoder for the NWN website
-# Daryl Herzmann 30 June 2004
+#!/usr/bin/python
+"""
+ Geocoder used by:
+  - IEM Rainfall App
+$Id: $:
+"""
 
 import xmlrpclib, cgi
 p = xmlrpclib.ServerProxy('http://rpc.geocoder.us/service/xmlrpc')
@@ -18,8 +21,9 @@ def main():
 		print "%s,%s" % (res[0]["lat"], res[0]["long"])
 	else:
 		print "ERROR"
+
+print 'Content-type: text/plain \n'
 try:
-	print 'Content-type: text/plain \n\n'
 	main()
 except:
 	print "ERROR"
--- cgi-bin/test.cgi
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-MS_MAPFILE=/mesonet/www/html/GIS/apps/rainfall/wms.map
-export MS_MAPFILE
-
-/mesonet/www/cgi-bin/mapserv/mapserv.460
