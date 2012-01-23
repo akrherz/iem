d217ebe28735b62bfbbf5171fffc0fd2cc889e32
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2012-01-23 04:39:48 +0000

    stop server log

--- include/mobile_device_detect.php
+++ include/mobile_device_detect.php
@@ -68,7 +68,7 @@ bug fixes with many thanks and much credit to http://www.punchkickinteractive.co
 function mobile_device_detect($iphone=true,$ipad=true,$android=true,$opera=true,$blackberry=true,$palm=true,$windows=true,$mobileredirect=false,$desktopredirect=false){
 
   $mobile_browser   = false; // set mobile browser as false till we can prove otherwise
-  $user_agent       = $_SERVER['HTTP_USER_AGENT']; // get the user agent value - this should be cleaned to ensure no nefarious input gets executed
+  $user_agent       = @$_SERVER['HTTP_USER_AGENT']; // get the user agent value - this should be cleaned to ensure no nefarious input gets executed
   $accept           = @$_SERVER['HTTP_ACCEPT']; // get the content accept value - this should be cleaned to ensure no nefarious input gets executed
 
   switch(true){ // using a switch against the following statements which could return true is more efficient than the previous method of using if statements
