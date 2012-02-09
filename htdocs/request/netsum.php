8b2a92658bd8fa9ac54edfcf85a3126c95aa1feb
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2012-02-09 14:28:17 +0000

    remove sql injection and fix it

--- htdocs/request/netsum.php
+++ htdocs/request/netsum.php
@@ -9,11 +9,12 @@ $year = isset($_GET["year"]) ? $_GET["year"] : date("Y");
 $network = isset($_GET["network"]) ? $_GET["network"] : "IA_ASOS";
 $ts = mktime(0,0,0,$month, $day, $year);
 
-$sql = sprintf("SELECT *, x(s.geom) as x, y(s.geom) as y from 
- summary_%s c, stations s WHERE s.network = '%s' and 
- day = '%s' and s.network = c.network and s.id = c.station
- ORDER by station ASC", date("Y", $ts), $network, date("Y-m-d", $ts) );
-$rs = pg_exec($connection, $sql);
+$sql = sprintf("SELECT s.id, c.*, x(s.geom) as x, y(s.geom) as y from 
+ summary_%s c JOIN stations s ON (s.iemid = c.iemid) WHERE s.network = $1 and 
+ day = $2
+ ORDER by s.id ASC", date("Y", $ts) );
+pg_prepare($connection, "SEL", $sql);
+$rs = pg_execute($connection, "SEL", Array($network, date("Y-m-d", $ts) ));
 
 header("Content-type: text/plain");
 echo "station,date,latitide,longitude,max_tmpf,min_tmpf,rain_inch,\n";
@@ -23,7 +24,7 @@ for( $i=0; $row = @pg_fetch_array($rs,$i); $i++)
   $maxt = ($row["max_tmpf"] > -90) ? $row["max_tmpf"] : "M";
   $mint = ($row["min_tmpf"] < 90) ? $row["min_tmpf"] : "M";
   $rain = ($row["pday"] >= 0) ? $row["pday"] : "M";
-  echo sprintf("%s,%s,%s,%s,%s,%s,%s,\n", $row["station"], 
+  echo sprintf("%s,%s,%s,%s,%s,%s,%s,\n", $row["id"], 
   date("Y-m-d", $ts), $row["y"], $row["x"], $maxt, $mint, $rain);  
 }
 ?>
