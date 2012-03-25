e4a022860c427599152838704fae90fa52005b15
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2012-03-25 23:24:36 +0000

    more known knows

--- scripts/qc/dailyVtec.py
+++ scripts/qc/dailyVtec.py
@@ -13,7 +13,8 @@ postgis = i['postgis']
 tests = ['HGX.FF.7', 'GUM.MA.1', 'GUM.MA.2','MRX.SV.18', 'SGF.TO.23',
          'LMK.TO.81', 'LMK.TO.82', 'LSX.TO.11', 'IND.TO.7', 'IND.TO.8',
          'PAH.TO.52', 'PAH.TO.53','PAH.TO.54', 'PAH.TO.55', 'MRX.TO.58',
-         'ILX.TO.2'
+         'ILX.TO.2', 'CHS.TO.6', 'CAE.FF.1', 'CAE.TO.4', 'CAE.TO.5',
+         'RNK.TO.6'
 ]
 #for i in range(27,78):
 #    tests.append('OAX.TO.%s' % (i,))
