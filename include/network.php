9f42a1e1b09897e22989f9a282ecc127e622e222
akrherz <akrherz@iastate.edu>
2012-04-14 18:09:41 +0000

    close db

--- include/network.php
+++ include/network.php
@@ -16,7 +16,7 @@ class NetworkTable {
     {
       foreach($a as $network) { $this->load_network($network); }
     }
-
+	pg_close($this->dbconn);
   }
 
   function load_network($network)
@@ -35,6 +35,10 @@ class NetworkTable {
     {
       $this->table[ $row["id"] ] = $row;
     }
+    if (pg_num_rows($rs) < 1){
+    	return false;
+    }
+    return true;
   }
 
 
