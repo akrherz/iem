0abb50c6cc09add358a6a0e873c86386d7532d9f
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2012-04-20 12:43:29 +0000

    need to migrate this to db access someday...

--- htdocs/plotting/isumet/1min.php
+++ htdocs/plotting/isumet/1min.php
@@ -49,10 +49,11 @@ if ($station == null){
 		if (sizeof($tokens) != 21){
 			continue;
 		}
-  		$tstring = sprintf("%s %s %s %s", $tokens[0], $tokens[1], $tokens[2], $tokens[3]);
+  		$tstring = sprintf("%s %s %s %s", $tokens[0], $tokens[1], $tokens[2], 
+  				$tokens[3]);
   		$v = strtotime($tstring);
   		
-		if ($v < $myTime){
+		if ($v < $myTime || trim($tstring) == ""){
 			continue;
 		}
 		$valid[] = $v;
@@ -60,7 +61,6 @@ if ($station == null){
   		$relh[] = floatval($tokens[8]);
   		$dwpf[] = dwpf($tokens[5], floatval($tokens[8]));
  	} // End of while
-	
 }
 	
 include ("$rootpath/include/jpgraph/jpgraph.php");
