f986482065fdffaaae1e81717b81f43530558819
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2012-05-04 23:21:40 +0000

    support lines

--- htdocs/request/grx/time_mot_loc.php
+++ htdocs/request/grx/time_mot_loc.php
@@ -9,11 +9,11 @@ $plimit = isset($_GET["all"]) ? "('TO','SV')" : "('TO')";
 $title = isset($_GET["all"]) ? "SVR+TOR" : "TOR";
 $pgconn = iemdb('postgis');
 $rs = pg_query($pgconn, "SELECT x(tml_geom) as lon, y(tml_geom) as lat,
+	ST_AsText(tml_geom_line) as line,
 	tml_valid at time zone 'UTC' as tmlv, tml_direction, tml_sknt, 
 	polygon_end at time zone 'UTC' as pe, eventid, wfo, phenomena
 	 from sbw_". date("Y") ." 
-	WHERE polygon_end > now() and phenomena in $plimit and status != 'CAN'
-	and tml_valid is not null");
+	WHERE polygon_end > now() and phenomena in $plimit and status != 'CAN'");
 
 header( 'Content-type: text/plain');
 
@@ -30,31 +30,43 @@ Font: 1, 11, 1, \"Courier New\"
 for ($k=0;$row=@pg_fetch_array($rs,$k);$k++){
 	$valid = strtotime($row["tmlv"]);
 	$pe = strtotime($row["pe"]);
-	
 	$dur = $pe - $valid;
 	$min = $dur/60;
 	$spd = floatval($row["tml_sknt"]) * 0.514444444;
 	$dir = floatval($row["tml_direction"]);
-	$lat1 = floatval($row["lat"]);
-	$lon1 = floatval($row["lon"]);
 	$distance = $spd * $dur;
-	$lon2 = round($lon1 + (($distance * cos(deg2rad(270-$dir)))/(111325 * cos(deg2rad($lat1)))),6);
-	$lat2 = round($lat1 + (($distance * sin(deg2rad(270-$dir)))/111325),6);
-	if ($row["phenomena"] == 'SV'){
-		echo "Color: 255 255 0\n";
-	} else {
-		echo "Color: 255 0 0\n";
+
+	/* Some magic if we have a line or a string! */
+	$lats = Array();
+	$lons = Array();
+	if ($row["lat"]){
+		$lats[] = floatval($row["lat"]);
+		$lons[] = floatval($row["lon"]);
+	} else if ($row["line"]){
+		preg_match_all('/(?P<lon>[0-9\.\-]+) (?P<lat>[0-9\.\-]+)/', 
+					$row["line"], $matches);
+		$lats = $matches["lat"];
+		$lons = $matches["lon"];
 	}
-	
-	echo "Line: 2,0,\"NWS Warning Track (". $row["wfo"] ."-". $row["phenomena"] 
+	for($m=0; $m < sizeof($lats); $m++){
+		$lat1 = $lats[$m];
+		$lon1 = $lons[$m];
+		if ($row["phenomena"] == 'SV'){
+			echo "Color: 255 255 0\n";
+		} else {
+			echo "Color: 255 0 0\n";
+		}
+	  $lon2 = round($lon1 + (($distance * cos(deg2rad(270-$dir)))/(111325 * cos(deg2rad($lat1)))),6);
+	  $lat2 = round($lat1 + (($distance * sin(deg2rad(270-$dir)))/111325),6);
+
+	  echo "Line: 2,0,\"NWS Warning Track (". $row["wfo"] ."-". $row["phenomena"] 
 	."-". $row["eventid"].")\"
   ".$lat1.",".$lon1."
   ".$lat2.",".$lon2."
 End:
 
 ";
-
-	echo "Color: 255 255 255\nThreshold:10\n\n";
+	  echo "Color: 255 255 255\nThreshold:10\n\n";
 
 	for($i=0;$i<=$min;$i++){
 		$now = $valid + ($i * 60);
@@ -69,5 +81,6 @@ End:
 	}
 	
 	echo "Threshold: 999\n\n";
+	}
 }
 ?>
