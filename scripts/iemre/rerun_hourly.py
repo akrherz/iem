758f8021ef4cbbcfe74327690528dbe7a26a4b3c
daryl herzmann <akrherz@iastate.edu>
2012-05-14 16:58:27 -0500

    upstream

--- scripts/iemre/rerun_hourly.py
+++ scripts/iemre/rerun_hourly.py
@@ -1,8 +1,12 @@
 import mx.DateTime
 import stage4_hourlyre
+import sys
 
-sts = mx.DateTime.DateTime(2011,10,21)
-ets = mx.DateTime.DateTime(2011,10,22,12)
+yr = int(sys.argv[1])
+mo = int(sys.argv[2])
+
+sts = mx.DateTime.DateTime(yr,mo,1)
+ets = sts + mx.DateTime.RelativeDateTime(months=1)
 interval = mx.DateTime.RelativeDateTime(hours=1)
 now = sts
 while now < ets:
