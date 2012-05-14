6f56df836a7f326f3372fdf29151bc8ac8fdcb86
daryl herzmann <akrherz@iastate.edu>
2012-05-14 16:26:24 -0500

    updates for 2012 support

--- scripts/climodat/fe.py
+++ scripts/climodat/fe.py
@@ -1,13 +1,13 @@
 import mx.DateTime
 import os
 
-sts = mx.DateTime.DateTime(2010,1,1)
-ets = mx.DateTime.DateTime(2011,1,1)
+sts = mx.DateTime.DateTime(2010,10,1)
+ets = mx.DateTime.DateTime(2010,11,1)
 interval = mx.DateTime.RelativeDateTime(days=1)
 now = sts
 while now < ets:
   print now
   #for st in ['nd','sd','ne','ks','mo','il','wi','mn','mi','oh','in','ky']:
   #os.system("/mesonet/python/bin/python daily_estimator.py %s %s" % (st, now.strftime("%Y %m %d"),))
-  os.system("/mesonet/python/bin/python compute_0000.py %s" % (now.strftime("%Y %m %d"),))
+  os.system("python compute_0000.py %s" % (now.strftime("%Y %m %d"),))
   now += interval
