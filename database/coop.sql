1ed53ddc09d9131faa74d425cae6494572d90b52
akrherz <akrherz@iastate.edu>
2012-05-16 14:45:24 -0500

    move schema.sql into system database

--- database/coop.sql
+++ database/coop.sql
@@ -72,3 +72,34 @@ CREATE FUNCTION gdd48(real, real) RETURNS numeric
  CREATE FUNCTION hdd65(real, real) RETURNS numeric
  	LANGUAGE sql
  	AS $_$select (case when (65 - (( $1 + $2 )/2.)) > 0 then ( $1 + $2 )/2. else 0 end)::numeric$_$;
+ 	
+
+DROP table r_precipevents;
+CREATE table r_precipevents(
+  stationid char(6),
+  climoweek smallint,
+  maxval real,
+  meanval real,
+  cat1e smallint,
+  cat2e smallint,
+  cat3e smallint,
+  cat4e smallint,
+  cat5e smallint,
+  missing smallint 
+);
+grant select on r_precipevents to nobody;
+create unique index r_precipevents_idx 
+ on r_precipevents(stationid, climoweek);
+
+DROP table r_monthly;
+CREATE table r_monthly(
+  stationid char(6),
+  monthdate date,
+  gdd40 smallint,
+  gdd48 smallint,
+  gdd50 smallint,
+  gdd52 smallint
+);
+grant select on r_rmonthly to nobody;
+create unique index r_monthly_idx
+ on r_monthly(stationid, monthdate);
