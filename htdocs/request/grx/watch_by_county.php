3d361b1daa923735b7a62f3ce4a0af11d96f7618
akrherz <akrherz@iastate.edu>
2012-05-24 15:38:16 -0500

    better implementation to split up multi-polygons

--- htdocs/request/grx/watch_by_county.php
+++ htdocs/request/grx/watch_by_county.php
@@ -10,10 +10,11 @@ header("Content-type: text/plain");
 
  
  $rs = pg_query($dbconn, "select phenomena, eventid, 
- 		ST_asText(ST_Simplify(ST_multi(ST_union(geom)),0.01)) as g from warnings 
- 		WHERE significance = 'A' and phenomena IN ('TO','SV')
- 		and issue <= now() and expire > now() 
- 		and ugc ~* 'C' GROUP by phenomena, eventid ORDER by phenomena ASC");
+ 		ST_asText(ST_Multi(ST_union(ST_SnapToGrid(geom,0.0001)))) as g from warnings 
+ 		WHERE significance = 'A' and phenomena IN ('TO','SV') and 
+ 		issue <= now() and expire > now() 
+ 		and substr(ugc,3,1) = 'C' GROUP by phenomena, eventid 
+ 		ORDER by phenomena ASC");
 
 echo "Refresh: 10\n";
 echo "Threshold: 999\n";
@@ -23,29 +24,29 @@ for ($i=0;$row=@pg_fetch_array($rs,$i);$i++){
 	$geom = $row["g"];
 	$geom = str_replace("MULTIPOLYGON(((", "", $geom);
 	$geom = str_replace(")))", "", $geom);
-	$geom = str_replace("))", "", $geom);
-	$geom = str_replace("((", "", $geom);
-	$tokens = preg_split("/,/", $geom);
-    
-    if ($row["phenomena"] == "SV"){
-    	$c = ", 255, 255, 0, 255";
-    } else { 
-    	$c = ", 255, 0, 0, 255";
-    }
-    echo "\n;". $row["phenomena"] ." Watch Number ". $row["eventid"] ."\n";
-	echo "Polygon:\n";
-	$first = true;
-	foreach($tokens as $token){
-	
-		$parts = preg_split("/ /", $token);
-		$extra = "";
-		if ($first){
-			$extra = $c;
-			$first = false;
+	$tokens = preg_split("/\)\),\(\(/", $geom);
+    foreach ($tokens as $token){
+	    if ($row["phenomena"] == "SV"){
+	    	$c = ", 255, 255, 0, 255";
+	    } else { 
+	    	$c = ", 255, 0, 0, 255";
+	    }
+	    echo "\n;". $row["phenomena"] ." Watch Number ". $row["eventid"] ."\n";
+		echo "Polygon:\n";
+		$first = true;
+		$tokens2 = preg_split("/,/", $token);
+		foreach($tokens2 as $token2){
+		
+			$parts = preg_split("/ /", $token2);
+			$extra = "";
+			if ($first){
+				$extra = $c;
+				$first = false;
+			}
+			echo sprintf("%s, %s%s\n", $parts[1], $parts[0], $extra);
 		}
-		echo sprintf("%s, %s%s\n", $parts[1], $parts[0], $extra);
-	}
-	echo "End:\n\n";
+		echo "End:\n\n";
+    }
 	
 }
 
