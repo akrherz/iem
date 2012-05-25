c8bd15506b5fd57489c5181bebcbf2d5b96a1f46
akrherz <akrherz@iastate.edu>
2012-05-25 08:04:52 -0500

    use tostring() for character arrays

--- scripts/ingestors/madis/sync_stations.py
+++ scripts/ingestors/madis/sync_stations.py
@@ -1,6 +1,5 @@
 """
 Extract station data from file and update any new stations we find, please
-
 """
 
 import netCDF4
@@ -39,23 +38,23 @@ MY_PROVIDERS = ["MNDOT", "KSDOT", "WIDOT", "INDOT", "NDDOT",
 
 
 for recnum in range(len(providers)):
-  thisProvider = ''.join(providers[recnum])
-  if not thisProvider in MY_PROVIDERS:
-    continue
-  stid = ''.join(stations[recnum])
-  name = (''.join(names[recnum])).replace("'", "")
-  network = '%s_RWIS' % (thisProvider[:2],)
-  mcursor.execute("""SELECT * from stations where id = %s and network = %s""",
-  (stid, network))
-  if mcursor.rowcount > 0:
-    continue
-  print 'Adding network: %s station: %s' % (network, stid)
-  sql = """INSERT into stations(id, network, synop, country, plot_name,
+    thisProvider = providers[recnum].tostring().strip()
+    if not thisProvider in MY_PROVIDERS:
+        continue
+    stid = stations[recnum].tostring().strip()
+    name = names[recnum].tostring().strip().replace("'", "")
+    network = '%s_RWIS' % (thisProvider[:2],)
+    mcursor.execute("""SELECT * from stations where id = %s and network = %s""",
+                    (stid, network))
+    if mcursor.rowcount > 0:
+        continue
+    print 'Adding network: %s station: %s' % (network, stid)
+    sql = """INSERT into stations(id, network, synop, country, plot_name,
    name, state, elevation, online, geom) VALUES ('%s', '%s', 9999, 'US',
    '%s', '%s', '%s', %s, 't', 'SRID=4326;POINT(%s %s)')""" % (stid,
    network, name, name, network[:2], elevations[recnum][0], longitudes[recnum][0],
    latitudes[recnum][0])
-  mcursor.execute(sql)
+    mcursor.execute(sql)
 nc.close()
 mcursor.close()
 MESOSITE.commit()
