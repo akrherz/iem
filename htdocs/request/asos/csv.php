d7430a074d6bd49bcb0ad29b276e5e2ed03dc5f8
akrherz <akrherz@iastate.edu>
2012-06-19 12:33:10 -0500

    add date support to allow archived queries

--- htdocs/request/asos/csv.php
+++ htdocs/request/asos/csv.php
@@ -4,7 +4,9 @@ include("../../../config/settings.inc.php");
 include("$rootpath/include/database.inc.php");
 $mesosite = iemdb("mesosite");
 $access = iemdb("access");
+$asos = iemdb("asos");
 pg_exec($access, "SET TIME ZONE 'GMT'");
+pg_exec($asos, "SET TIME ZONE 'GMT'");
 
 $stations = Array("AMW");
 
@@ -23,14 +25,31 @@ if (isset($_GET["lat"]) && isset($_GET["lon"]))
 }
 
 $result = "id,valid,tmpf,dwpf,sknt,drct,phour,alti,gust,lon,lat\n";
+$prepared = null;
 while(list($k,$id) = each($stations))
 {
+	if (isset($_REQUEST["date"])){
+		$ts = strtotime($_REQUEST["date"]);
+		if ($prepared == null){
+		$prepared = pg_prepare($asos, "SELECT", sprintf("SELECT station as id, valid,
+		max(tmpf) as tmpf, max(dwpf) as dwpf, max(sknt) as sknt, max(drct) as drct,
+		max(p01i) as phour, max(alti) as alti, max(gust) as gust, 
+		max(x(s.geom)) as lon, max(y(s.geom)) as lat from t%s t, stations s
+		where s.id = $1 and (s.network ~* 'ASOS' or s.network ~* 'AWOS')
+		and t.station = s.id and t.valid BETWEEN '%s'::date 
+		and '%s'::date + '8 days'::interval GROUP by station, valid 
+		ORDER by valid ASC", date("Y", $ts), 
+		date("Y-m-d", $ts), date("Y-m-d", $ts) ));
+		}
+		$rs = pg_execute($asos, "SELECT", Array($id));	
+	} else {
   $rs = pg_exec($access, "SELECT s.id, valid, max(tmpf) as tmpf, max(dwpf) as dwpf, 
   max(sknt) as sknt, max(drct) as drct,
   max(phour) as phour, max(alti) as alti, max(gust) as gust,
   max(x(s.geom)) as lon, max(y(s.geom)) as lat from current_log c, stations s
   WHERE s.id = '$id' and s.iemid = c.iemid
         GROUP by id, valid ORDER by valid ASC");
+	}
   if (pg_num_rows($rs) == 0){ continue; }
   for ($i=0;$row=@pg_fetch_array($rs,$i);$i++)
   {
