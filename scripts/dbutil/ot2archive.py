f3377b9fa404a2abf8bd06dc84a95e8262ec90f3
akrherz <akrherz@iastate.edu>
2012-07-03 10:27:34 -0500

    python2.6

--- scripts/dbutil/ot2archive.py
+++ scripts/dbutil/ot2archive.py
@@ -1,36 +1,43 @@
-
-from pyIEM import iemdb
-import mx.DateTime, sys, traceback
-i = iemdb.iemdb()
-other = i['other']
-iemdb = i['iem']
+"""
+Dump iem database of OT data to archive
+"""
+import mx.DateTime
+import sys
+import traceback
+import iemdb
+import psycopg2.extras
+OTHER = iemdb.connect('other')
+ocursor = OTHER.cursor(cursor_factory=psycopg2.extras.DictCursor)
+IEM = iemdb.connect('iem')
+icursor = IEM.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
 ts = mx.DateTime.now() - mx.DateTime.RelativeDateTime(days=1)
 
 # Delete any obs from yesterday
 sql = "DELETE from t%s WHERE date(valid) = '%s'" % (ts.year, 
       ts.strftime("%Y-%m-%d") )
-other.query(sql)
+ocursor.execute(sql)
 
 # Get obs from Access
-sql = "SELECT c.*, t.id from current_log c JOIN stations t on (t.iemid = c.iemid) WHERE date(valid) = '%s' and t.network = 'OT'" % (ts.strftime("%Y-%m-%d"), )
-rs = iemdb.query(sql).dictresult()
+sql = """SELECT c.*, t.id from current_log c JOIN stations t on 
+    (t.iemid = c.iemid) WHERE date(valid) = '%s' 
+    and t.network = 'OT'""" % (ts.strftime("%Y-%m-%d"), )
+icursor.execute( sql )
 
-for i in range(len(rs)):
-  pday = 0
-  if (rs[i]['pday'] is not None and float(rs[i]['pday']) > 0):
-    pday = float(rs[i]['pday']) 
-  alti = rs[i]['alti']
-  if alti is None and rs[i]['mslp'] is not None:
-    alti = rs[i]['mslp'] * .03 
-  sql = """INSERT into t%s (station, valid, tmpf, dwpf, drct, sknt,  alti, 
-         pday, gust, c1tmpf,srad) values('%s','%s',%s,%s,%s,%s,%s,%s,%s,%s,%s)""" % \
-  (ts.year,rs[i]['id'], rs[i]['valid'], (rs[i]['tmpf'] or "Null"), \
-  (rs[i]['dwpf'] or "Null"), (rs[i]['drct'] or "Null"), (rs[i]['sknt'] or "Null"),\
-  (alti or "Null"), pday, (rs[i]['gust'] or "Null") , 
-   (rs[i]['c1tmpf'] or "Null"), (rs[i]['srad'] or "Null"))
-  try:
-    other.query(sql)
-  except:
-    print sql
-    traceback.print_exc(file=sys.stdout)
+for row in icursor:
+    pday = 0
+    if (row['pday'] is not None and float(row['pday']) > 0):
+        pday = row['pday'] 
+    alti = row['alti']
+    if alti is None and row['mslp'] is not None:
+        alti = row['mslp'] * .03 
+    sql = """INSERT into t%s (station, valid, tmpf, dwpf, drct, sknt,  alti, 
+         pday, gust, c1tmpf,srad) values('%s','%s',%s,%s,%s,%s,%s,%s,%s,%s,%s)
+         """ % (ts.year,row['id'], row['valid'], (row['tmpf'] or "Null"), 
+  (row['dwpf'] or "Null"), (row['drct'] or "Null"), (row['sknt'] or "Null"),
+  (alti or "Null"), pday, (row['gust'] or "Null") , 
+   (row['c1tmpf'] or "Null"), (row['srad'] or "Null"))
+    icursor.execute(sql)
+
+icursor.close()
+IEM.commit()
