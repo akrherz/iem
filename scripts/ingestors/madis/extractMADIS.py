8f2036e47b65ac2a8fec5711743cb2188046d352
akrherz <akrherz@iastate.edu>
2012-07-13 17:24:31 -0500

    update to python2.6, remove IOC test

--- scripts/ingestors/madis/extractMADIS.py
+++ scripts/ingestors/madis/extractMADIS.py
@@ -1,27 +1,27 @@
 #	extractMADIS.py
 #  Get the latest MADIS numbers from the data file!
 
-import string, mx.DateTime, os, sys
-from pyIEM import iemdb, mesonet
-i = iemdb.iemdb()
-iemaccess = i['iem']
-import netCDF3
+import string
+import mx.DateTime
+import os
+import sys
+import iemdb
+import mesonet
+IEM = iemdb.connect('iem')
+icursor = IEM.cursor()
+import netCDF4
 import numpy
 
 now = mx.DateTime.gmt() + mx.DateTime.RelativeDateTime(hours=-3)
+now = mx.DateTime.DateTime(2012, 5, 25, 12, 0)
 
-# IOC Test
-if len(sys.argv) == 2:
-  fp = "/mnt/mesonet/data/madis/mesonet/%s.nc" % (now.strftime("%Y%m%d_%H00"), )
-  table = "current_qc_ioc"
-else:
-  fp = "/mesonet/data/madis/mesonet/%s.nc" % (now.strftime("%Y%m%d_%H00"), )
-  table = "current_qc"
+fp = "/mesonet/data/madis/mesonet/%s.nc" % (now.strftime("%Y%m%d_%H00"), )
+table = "current_qc"
 
 if (not os.path.isfile(fp)):
   sys.exit()
 
-nc = netCDF3.Dataset(fp)
+nc = netCDF4.Dataset(fp)
 
 
 providers = nc.variables["dataProvider"]
@@ -48,31 +48,28 @@ def check(val):
     return 'Null'
   return val
 
-
-i = 0
-found =0
 for p in range(providers.shape[0]):
-  provider = providers[p]
-  if (provider.tostring()[:3] == 'IEM' or provider.tostring()[:5] == 'IADOT' ):
-    found = 1
-    ts = mx.DateTime.gmtime( nc.variables["observationTime"][i] )
+    provider = providers[p]
+    if provider.tostring().replace('\x00','') not in ['IEM', 'IADOT']:
+        continue  
+    ts = mx.DateTime.gmtime( nc.variables["observationTime"][p] )
 
     (tmpf, tmpf_qc_av, tmpf_qc_sc) = ('Null', 'Null', 'Null')
     (dwpf, dwpf_qc_av, dwpf_qc_sc) = ('Null', 'Null', 'Null')
     (alti, alti_qc_av, alti_qc_sc) = ('Null', 'Null', 'Null')
 
-    if not numpy.ma.is_masked( nc_tmpk[i] ):
-      tmpf = check( mesonet.k2f( nc_tmpk[i] ) )
-      tmpf_qc_av = figure(nc_tmpk[i], tmpkQCD[i,0])
-      tmpf_qc_sc = figure(nc_tmpk[i], tmpkQCD[i,6])
-    if not numpy.ma.is_masked( nc_dwpk[i] ):
-      dwpf = check( mesonet.k2f( nc_dwpk[i] ) )
-      dwpf_qc_av = figure(nc_dwpk[i], dwpkQCD[i,0])
-      dwpf_qc_sc = figure(nc_dwpk[i], dwpkQCD[i,6])
-    if not numpy.ma.is_masked( nc_alti[i] ):
-      alti = check( (nc_alti[i][0] / 100.0 ) * 0.0295298)
-      alti_qc_av = figureAlti(alti, altiQCD[i,0] * 0.0295298 )
-      alti_qc_sc = figureAlti(alti, altiQCD[i,6] * 0.0295298 )
+    if not numpy.ma.is_masked( nc_tmpk[p] ):
+        tmpf = check( mesonet.k2f( nc_tmpk[p] ) )
+        tmpf_qc_av = figure(nc_tmpk[p], tmpkQCD[p,0])
+        tmpf_qc_sc = figure(nc_tmpk[p], tmpkQCD[p,6])
+    if not numpy.ma.is_masked( nc_dwpk[p] ):
+        dwpf = check( mesonet.k2f( nc_dwpk[p] ) )
+        dwpf_qc_av = figure(nc_dwpk[p], dwpkQCD[p,0])
+        dwpf_qc_sc = figure(nc_dwpk[p], dwpkQCD[p,6])
+    if not numpy.ma.is_masked( nc_alti[p] ):
+        alti = check( (nc_alti[p] / 100.0 ) * 0.0295298)
+        alti_qc_av = figureAlti(alti, altiQCD[p,0] * 0.0295298 )
+        alti_qc_sc = figureAlti(alti, altiQCD[p,6] * 0.0295298 )
     #print id, tmpf, tmpkQCD[i]
     sql = """UPDATE %s SET tmpf = %s, tmpf_qc_av = %s, 
      tmpf_qc_sc = %s, dwpf = %s, dwpf_qc_av = %s, 
@@ -80,13 +77,11 @@ for p in range(providers.shape[0]):
      alti_qc_sc = %s, valid = '%s' WHERE station = '%s' """ % (table, tmpf, 
      tmpf_qc_av, tmpf_qc_sc, dwpf, dwpf_qc_av, 
      dwpf_qc_sc, alti, alti_qc_av, alti_qc_sc, 
-     ts.strftime("%Y-%m-%d %H:%M"), (nc.variables["stationId"][i]).tostring()[:5] )
+     ts.strftime("%Y-%m-%d %H:%M"), (nc.variables["stationId"][p]).tostring()[:5] )
     sql = sql.replace("--", "Null").replace("nan", "Null")
     try:
-      iemaccess.query(sql)
+        icursor.execute(sql)
     except:
-      print sql
-
-  i += 1
+        print sql
 
 nc.close()
