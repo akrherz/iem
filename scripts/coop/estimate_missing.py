7a119a9626603c645b6d974812ce2d39c6260977
akrherz <akrherz@iastate.edu>
2012-07-20 12:12:51 -0500

    add workflow for estimating missing data for COOP ingest process

--- scripts/coop/estimate_missing.py
+++ scripts/coop/estimate_missing.py
@@ -1,51 +1,87 @@
+"""
+ Crude data estimator!
+"""
 import sys
-from pyIEM import iemdb
-import sys
-state = sys.argv[1]
+import iemdb
+import numpy
 import network
+import psycopg2.extras
+
+# Database Connection
+COOP = iemdb.connect('coop', bypass=True)
+ccursor = COOP.cursor(cursor_factory=psycopg2.extras.DictCursor)
+ccursor2 = COOP.cursor()
+
+state = sys.argv[1]
 nt = network.Table("%sCLIMATE" % (state.upper(),))
-i = iemdb.iemdb()
-coopdb = i['coop']
-mesosite = i['mesosite']
 
-#st.ids = ['IA8266',]
-# First we create a dictionary of sites we want to estimate from
+# We'll care about our nearest 11 stations, arbitrary
 friends = {}
-for id in nt.sts.keys():
-  sql = """select id, distance(geom, 'SRID=4326;POINT(%s %s)') from stations
+weights = {}
+for station in nt.sts.keys():
+    sql = """select id, distance(geom, 'SRID=4326;POINT(%s %s)') from stations
          WHERE network = '%sCLIMATE' and id != '%s' 
-         and archive_begin < '1951-01-01' ORDER by distance
-         ASC LIMIT 11""" % (nt.sts[id]['lon'], nt.sts[id]['lat'], state.upper(), id.upper())
-  rs = mesosite.query(sql).dictresult()
-  friends[id] = "('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s')" \
-          % (rs[0]['id'], rs[1]['id'],\
-                rs[2]['id'], rs[3]['id'], rs[4]['id'],\
-                rs[5]['id'], rs[6]['id'], rs[7]['id'],\
-                rs[8]['id'], rs[9]['id'], rs[10]['id'])
+         and archive_begin < '1951-01-01' and
+         substr(id, 3, 1) != 'C' and substr(id, 3,4) != '0000'
+         ORDER by distance
+         ASC LIMIT 11""" % (nt.sts[station]['lon'], nt.sts[station]['lat'], 
+                            state.upper(), station)
+    ccursor.execute( sql )
+    friends[station] = []
+    weights[station] = []
+    for row in ccursor:
+        friends[station].append( row[0] )
+        weights[station].append( 1.0 / row[1] )
+    weights[station] = numpy.array( weights[station] )
 
-var = sys.argv[2]
-for id in nt.sts.keys():
-  sql = "select day from alldata_%s WHERE station = '%s' and %s IS NULL \
-         and day >= '1893-01-01'" % (state.lower(), id, var)
-  rs = coopdb.query(sql).dictresult()
-  for i in range(len(rs)):
-    if (i > 0 and i % 1000 == 0):
-      print "UPDATE %s" % (i,)
-    d = rs[i]['day']
-    sql="SELECT avg(%s) as h, stddev(%s) as r from alldata_%s WHERE day = '%s'\
-           and station IN %s and %s IS NOT NULL" % (var, var, state.lower(), d, friends[id], var)
-    rs2 = coopdb.query(sql).dictresult()
-    try:
-      sdev = float(rs2[0]['r'])
-    except:
-      print d, 'NDF'
-      continue
-    if (sdev > 39):
-      print d, sdev
-      continue
-    sql = "UPDATE alldata_%s SET estimated = true, %s = %i WHERE station = '%s' and day = '%s'"\
-          %(state.lower(), var, float(rs2[0]['h']), id, d)
-    #print sql, sdev
-    coopdb.query(sql)
-  if (len(rs) > 0):
-    print "Processed %s for %s [%s]" % (len(rs), id, var)
+def do_var(varname):
+    """
+    Run our estimator for a given variable
+    """
+    sql = """select day, station from alldata_%s WHERE %s IS NULL 
+        and day >= '1893-01-01'""" % (state.lower(), varname)
+    ccursor.execute( sql )
+    for row in ccursor:
+        day = row[0]
+        station = row[1]
+        if not nt.sts.has_key(station):
+            continue
+
+        sql = """SELECT station, %s from alldata_%s WHERE %s is not NULL
+            and station in %s and day = '%s'""" % (varname, state, varname,
+                                    tuple(friends[station]), day)
+        ccursor2.execute(sql)
+        weight = []
+        value = []
+        for row2 in ccursor2:
+            idx = friends[station].index(row2[0])
+            weight.append( weights[station][idx] )
+            value.append( row2[1] )
+
+        if len(weight) < 3:
+            print 'Not Enough Data Found station: %s day: %s var: %s' % (
+                                    station, day, varname)
+            continue
+    
+        mass = sum(weight)
+        interp = numpy.sum(numpy.array(weight) * numpy.array(value) / mass)
+
+        format = '%.2f'
+        if varname in ['high', 'low']:
+            format = '%.0f'
+        print 'Set station: %s day: %s varname: %s value: %s' % (station,
+                                    day, varname, format % (interp,))
+        sql = """UPDATE alldata_%s SET estimated = true, %s = %s WHERE 
+            station = '%s' and day = '%s'""" % (state.lower(), varname, 
+                                                interp, station, day)
+        ccursor2.execute( sql )
+
+def main():
+    for varname in ['high', 'low', 'precip']:
+        do_var(varname)    
+
+    ccursor2.close()
+    COOP.commit()
+    
+if __name__ == '__main__':
+    main()
--- scripts/ingestors/harry/DRIVE.sh
+++ scripts/ingestors/harry/DRIVE.sh
@@ -7,9 +7,12 @@ then
 fi
 
 # Process
-/mesonet/python/bin/python step1_texttosql.py $1 $2
+python step1_texttosql.py $1 $2
 # Inject into database
 psql -f /tmp/harry.sql -h iemdb coop
+# Need to produce estimates of temperatures were there are none
+echo "Generating estimates for missing data"
+python ../../coop/estimate_missing.py IA
 echo "Redoing estimates, this will take a bit!"
 # Estimate ia0000
 python ../../climodat/compute_0000.py $1 $2 >& /dev/null
