70b6950e51cac4ab56c801a90bcd0083087cf360
akrherz <akrherz@iastate.edu>
2012-07-26 10:35:42 -0500

    print out some more debugging and use some better logic I hope

--- scripts/cscap/email_daily_changes.py
+++ scripts/cscap/email_daily_changes.py
@@ -13,6 +13,10 @@ import util
 import mx.DateTime
 import smtplib
 
+EMAILS = ['labend@iastate.edu','akrherz@iastate.edu']
+if len(sys.argv) == 2:
+    EMAILS = ['akrherz@iastate.edu',]
+
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
@@ -36,47 +40,57 @@ docs_client = gdata.docs.client.DocsClient()
 token.authorize(docs_client)
 
 changestamp = int(config.get('memory', 'changestamp'))
-feed = docs_client.get_changes(expand_acl=True, changestamp=changestamp)
 
 html = """
 <h3>CSCAP Documents/Spreadsheet Changes</h3>
 <br />
 <p>Period: 7 AM %s - 7 AM %s
+<p>Changestamp In: %s
 
 <p><table border="1" cellpadding="3" cellspacing="0">
 <thead>
 <tr><th>Time</th><th>Author</th><th>Resource</th></tr>
 </thead>
 <tbody>
-""" % (yesterday.strftime("%d %B %Y"), today.strftime("%d %B %Y"))
+""" % (yesterday.strftime("%d %B %Y"), today.strftime("%d %B %Y"), changestamp)
 
-
-for entry in feed.entry:
-    uri = entry.get_html_link().href
-    updated = mx.DateTime.strptime(entry.updated.text[:16], '%Y-%m-%dT%H:%M')
-    changestamp = max(int(entry.changestamp.value), changestamp)
-    if updated < yesterday:
-        continue
-    updated = updated.localtime()
-    author = "N/A"
-    if entry.last_modified_by:
-        author = entry.last_modified_by.email.text
-    html += "<tr><td>%s</td><td>%s</td><td><a href=\"%s\">%s</a></td></tr>" % (
-        updated.strftime("%-I:%M %P"), author, uri, entry.title.text)
-    #if entry.filename:
-    #    print entry.filename.text
-    #print entry.changestamp.value
+loopcount = 0
+while 1:
+    loopcount += 1
+    print '%s Get Feed changestamp: %s' % (loopcount, changestamp)
+    feed = docs_client.get_changes(expand_acl=True, changestamp=changestamp)
+    count = 0
+    for entry in feed.entry:
+        count += 1
+        uri = entry.get_html_link().href
+        updated = mx.DateTime.strptime(entry.updated.text[:16], '%Y-%m-%dT%H:%M')
+        changestamp = max(int(entry.changestamp.value) +1, changestamp)
+        if updated < yesterday:
+            print 'Old changestamp of: %s thres: %s' % (updated, yesterday)
+            continue
+        updated = updated.localtime()
+        author = "N/A"
+        if entry.last_modified_by:
+            author = entry.last_modified_by.email.text
+        html += "<tr><td>%s</td><td>%s</td><td><a href=\"%s\">%s</a></td></tr>" % (
+            updated.strftime("%-I:%M %P"), author, uri, entry.title.text)
+        #if entry.filename:
+        #    print entry.filename.text
+        #print entry.changestamp.value
+    if count == 0 or loopcount == 10:
+        break
 
 config.set('memory', 'changestamp', changestamp +1)
 config.write( open('mytokens.cfg', 'w'))
 
 html += """</tbody></table>
 
-<p>That is all..."""
+<p>Changestamp out: %s
+<p>That is all...""" % (changestamp,)
 msg = MIMEMultipart('alternative')
 msg['Subject'] = "CSCAP Data ChangeLog"
 msg['From'] = 'mesonet@mesonet.agron.iastate.edu'
-msg['To'] = 'labend@iastate.edu,akrherz@iastate.edu'
+msg['To'] = ','.join(EMAILS)
 
 
 # Create the body of the message (a plain-text and an HTML version).
@@ -89,7 +103,7 @@ msg.attach(part1)
 msg.attach(part2)
 
 s = smtplib.SMTP('mailhub.iastate.edu')
-s.sendmail(msg['From'], ['labend@iastate.edu','akrherz@iastate.edu'], msg.as_string())
+s.sendmail(msg['From'], EMAILS, msg.as_string())
 s.quit()
 
 
