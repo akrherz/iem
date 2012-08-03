e3151f900f2004ffdc3dd8ac8b2007147c81faab
akrherz <akrherz@iastate.edu>
2012-08-03 10:02:35 -0500

    python2.6 update

--- scripts/RUN_10_AFTER.sh
+++ scripts/RUN_10_AFTER.sh
@@ -23,12 +23,12 @@ cd ../outgoing
 php wxc_cocorahs.php
 
 cd ../current
-/mesonet/python/bin/python plot_hilo.py 0
+python plot_hilo.py 0
 
 
 cd ../summary
-/mesonet/python/bin/python hourly_precip.py
-/mesonet/python/bin/python update_snet_precip.py
+python hourly_precip.py
+python update_snet_precip.py
 
 cd ../week
 /mesonet/python/bin/python plot_obs.py
