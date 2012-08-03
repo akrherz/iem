e87d769b8396c0b4b150a9d94a55e2b1b03a5263
akrherz <akrherz@iastate.edu>
2012-08-03 09:37:16 -0500

    small cleanup and move to python2.6

--- scripts/ingestors/elnino.py
+++ scripts/ingestors/elnino.py
@@ -18,7 +18,8 @@ for line in data[1:]:
     date = mx.DateTime.DateTime(int(tokens[0]), int(tokens[1]), 1)
     if maxD is None or date > maxD:
         print 'Found ElNino3.4! %s %s' % (date, anom34)
-        mcursor.execute("INSERT into elnino(monthdate, anom_34) values ('%s',%s)" % (date, anom34))
+        mcursor.execute("""INSERT into elnino(monthdate, anom_34) 
+        values ('%s',%s)""" % (date, anom34))
     
 mesosite.commit()
 mesosite.close()
