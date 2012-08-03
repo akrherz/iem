0f1881bd39b057a1ab60de2f3a301143e7197d72
akrherz <akrherz@iastate.edu>
2012-08-03 11:30:40 -0500

    python2.6 update

--- scripts/12z/awos_rtp.py
+++ scripts/12z/awos_rtp.py
@@ -1,10 +1,15 @@
-# Andy Ervin wants something to extract 12z 12 hr hi/lo values
-# Daryl Herzmann 12 May 2004
+"""
+ Andy Ervin wants something to extract 12z 12 hr hi/lo values
+ 12 May 2004
+"""
 
-from pyIEM import iemAccess, stationTable
-iemaccess = iemAccess.iemAccess()
-import mx.DateTime, shutil, string, os
-st = stationTable.stationTable("/mesonet/TABLES/awos.stns")
+import mx.DateTime
+import subprocess
+import network
+nt = network.Table("AWOS")
+import iemdb
+IEM = iemdb.connect('iem', bypass=True)
+icursor = IEM.cursor()
 
 
 ets = mx.DateTime.now() + mx.DateTime.RelativeDateTime(hour=12,minute=0)
@@ -23,32 +28,32 @@ out.write("""
 :   00Z TO 12Z TODAY LOW TEMPERATURE
 :   12Z YESTERDAY TO 12Z TODAY RAINFALL
 :   ...BASED ON REPORTED OBS...
-""" % ( ets.strftime("%m%d"), string.upper(sts.strftime("%d %b %Y")) ) )
+""" % ( ets.strftime("%m%d"), sts.strftime("%d %b %Y").upper() ) )
 
 highs = {}
-rs = iemaccess.query("SELECT id, \
-	round(max(tmpf)::numeric,0) as max_tmpf, \
-	count(tmpf) as obs FROM current_log c, stations t \
-	WHERE t.iemid = c.iemid and t.network = 'AWOS' and date(valid) = '%s' \
-	and tmpf > -99 GROUP by id " % \
-	(sts.strftime("%Y-%m-%d %H:%M"),) ).dictresult()
+icursor.execute("""SELECT id, 
+	round(max(tmpf)::numeric,0) as max_tmpf, 
+	count(tmpf) as obs FROM current_log c, stations t 
+	WHERE t.iemid = c.iemid and t.network = 'AWOS' and date(valid) = '%s' 
+	and tmpf > -99 GROUP by id """ % (sts.strftime("%Y-%m-%d %H:%M"),) )
 
-for i in range(len(rs)):
-	highs[ rs[i]["id"] ] = int(rs[i]["max_tmpf"])
+for row in icursor:
+	highs[ row[0] ] = row[1]
 
-iemaccess.query("SET TIME ZONE 'GMT'")
+icursor.execute("SET TIME ZONE 'GMT'")
 
 # 12z to 12z precip
 pcpn = {}
-rs = iemaccess.query("select id, sum(precip) from \
-		(select id, extract(hour from valid) as hour, \
-		max(phour) as precip from current_log c, stations t WHERE t.network = 'AWOS' and t.iemid = c.iemid \
-		and valid  >= '%s' and valid < '%s' \
-		GROUP by id, hour) as foo \
-	GROUP by id" % (sts.strftime("%Y-%m-%d %H:%M"), \
-		ets.strftime("%Y-%m-%d %H:%M") ) ).dictresult()
-for i in range(len(rs)):
-	pcpn[ rs[i]["id"] ] = "%5.2f" % (float(rs[i]["sum"]),)
+icursor.execute("""select id, sum(precip) from 
+		(select id, extract(hour from valid) as hour, 
+		max(phour) as precip from current_log c, stations t 
+		WHERE t.network = 'AWOS' and t.iemid = c.iemid 
+		and valid  >= '%s' and valid < '%s' 
+		GROUP by id, hour) as foo 
+	GROUP by id""" % (sts.strftime("%Y-%m-%d %H:%M"), 
+		ets.strftime("%Y-%m-%d %H:%M") ) )
+for row in icursor:
+	pcpn[ row[0] ] = "%5.2f" % (row[1],)
 
 #pcpn["DEH"] = "M"
 #pcpn["VTI"] = "M"
@@ -58,17 +63,17 @@ for i in range(len(rs)):
 #pcpn["MPZ"] = "M"
 
 lows = {}
-rs = iemaccess.query("SELECT id, \
-	round(min(tmpf)::numeric,0) as min_tmpf, \
-	count(tmpf) as obs FROM current_log c, stations t \
-	WHERE t.iemid = c.iemid and t.network = 'AWOS' and date(valid) = 'TODAY' and \
-	extract(hour from valid) < 12 and tmpf > -99 GROUP by id").dictresult()
+icursor.execute("""SELECT id, 
+	round(min(tmpf)::numeric,0) as min_tmpf, 
+	count(tmpf) as obs FROM current_log c, stations t 
+	WHERE t.iemid = c.iemid and t.network = 'AWOS' and date(valid) = 'TODAY' and 
+	extract(hour from valid) < 12 and tmpf > -99 GROUP by id""")
 
-for i in range(len(rs)):
-	lows[ rs[i]["id"] ] = int(rs[i]["min_tmpf"])
+for row in icursor:
+	lows[ row[0] ] = row[1]
 
 
-for s in st.ids:
+for s in nt.sts.keys():
 	myP = "M"
 	myH = "M"
 	myL = "M"
@@ -79,12 +84,12 @@ for s in st.ids:
 	if highs.has_key(s):
 		myH = highs[s]
 
-	out.write( fmt % (s, st.sts[s]["name"], myH, \
-		myL, \
+	out.write( fmt % (s, nt.sts[s]["name"], myH, 
+		myL, 
 		myP, "M", "M") )
 
 out.write(".END\n")
 out.close()
 
 cmd = "/home/ldm/bin/pqinsert -p 'plot ac %s0000 awos_rtp.shef awos_rtp.shef shef' /tmp/awos_rtp.shef" % (ets.strftime("%Y%m%d"), )
-os.system(cmd)
+subprocess.call(cmd, shell=True)
