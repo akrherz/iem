106da6b5a7a2fbfaa39475edb180c9078e0add7b
akrherz <akrherz@iastate.edu>
2012-08-03 09:39:33 -0500

    python2.6 update

--- scripts/RUN_0Z.sh
+++ scripts/RUN_0Z.sh
@@ -10,7 +10,7 @@ python max_reflect.py
 
 # Rerun today
 cd ../dbutil
-/mesonet/python/bin/python rwis2archive.py 1
+python rwis2archive.py 1
 
 cd ../iemre
-/mesonet/python/bin/python stage4_12z_adjust.py
+python stage4_12z_adjust.py
