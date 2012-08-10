b6ff72eebf92e541af3db3665a5edbc7ddaa87f5
akrherz <akrherz@iastate.edu>
2012-08-10 06:52:15 -0500

    use tmpfile to hopefully stop collisions

--- scripts/RUN_1MIN.sh
+++ scripts/RUN_1MIN.sh
@@ -5,19 +5,17 @@ cd sbw
 python raccoon_sbw_to_ppt.py &
 
 cd ../GIS
-python attribute2shape.py
+python attribute2shape.py &
 python wwa2shp.py &
 
 cd ../ingestors
-#/mesonet/python/bin/python parse0006.py &
+
 python parse0002.py &
-#/mesonet/python/bin/python awos_adas.py &
+
 python ctre_bridge.py &
 
 cd ../outgoing
 python snet_collect.py &
 
-#cd ../awos
-#/mesonet/python/bin/python dl_parse_iwapi.py
 
 
