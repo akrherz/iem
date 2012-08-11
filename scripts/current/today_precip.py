36981558949247eb7ae0d6e7e887a60f093108c7
akrherz <akrherz@iastate.edu>
2012-08-11 09:51:40 -0500

    less noisey when there is no rainfall

--- scripts/current/today_precip.py
+++ scripts/current/today_precip.py
@@ -24,18 +24,23 @@ select s.id, s.network,
  c2.valid > (now() - '2 hours'::interval)
  and c.day = 'TODAY'
  and s.country = 'US' and (s.network ~* 'ASOS' or s.network = 'AWOS')
+ and s.state in ('IA','MN','WI','IL','MO','NE','KS','SD','ND')
 """ % (now.year, )
 
 lats = []
 lons = []
 vals = []
+iavals = []
 valmask = []
 icursor.execute(sql)
 for row in icursor:
   lats.append( row[3] )
   lons.append( row[2] )
   vals.append( row[4] )
-  valmask.append(  (row[1] in ['AWOS','IA_ASOS']) )
+  iowa = row[1] in ['AWOS','IA_ASOS']
+  valmask.append( iowa )
+  if iowa:
+      iavals.append( row[4] )
 
 if len(lats) < 3:
   sys.exit(0)
@@ -53,7 +58,11 @@ cfg = {
  '_valuemask'         : valmask
 }
 # Generates tmp.ps
-tmpfp = iemplot.simple_contour(lons, lats, vals, cfg)
+if max(iavals) < 0.04:
+    cfg.pop('lbTitleString')
+    tmpfp = iemplot.simple_valplot(lons, lats, vals, cfg)
+else:
+    tmpfp = iemplot.simple_contour(lons, lats, vals, cfg)
 
 pqstr = "plot c 000000000000 summary/today_prec.png bogus png"
 iemplot.postprocess(tmpfp, pqstr)
