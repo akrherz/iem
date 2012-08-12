819d0be967e2ff861084a96cbb91b49017af054b
akrherz <akrherz@iastate.edu>
2012-08-12 07:43:51 -0500

    add AWOS network into queue for potential running

--- scripts/windrose/daily_drive_network.py
+++ scripts/windrose/daily_drive_network.py
@@ -1,5 +1,8 @@
 # We are going to process one network per day every day!
-import iemdb, os, sys
+import iemdb
+import os
+import sys
+import subprocess
 import stat
 import mx.DateTime
 MESOSITE = iemdb.connect('mesosite', bypass=True)
@@ -7,14 +10,16 @@ mcursor = MESOSITE.cursor()
 now = mx.DateTime.now()
 
 mcursor.execute("""SELECT max(id), network from stations 
-    WHERE network ~* 'ASOS' and online = 't' and country = 'US' 
+    WHERE (network ~* 'ASOS' or network = 'AWOS') 
+    and online = 't' and country = 'US' 
     GROUP by network ORDER by random()""")
 for row in mcursor:
     network = row[1]
     testfp = "/mesonet/share/windrose/climate/yearly/%s_yearly.png" % (row[0],)
     if not os.path.isfile(testfp):
         print "Driving network %s because no file" % (network,)
-        os.system("python drive_network_windrose.py %s" % (network,))
+        subprocess.call("python drive_network_windrose.py %s" % (network,),
+                        shell=True)
         sys.exit()
     else:
         mtime = os.stat(testfp)[stat.ST_MTIME]
@@ -22,5 +27,6 @@ for row in mcursor:
         # 55 days in seconds, enough to cover the number of networks running
         if age > (55*24*60*60): 
             print "Driving network %s because of age!" % (network,)
-            os.system("python drive_network_windrose.py %s" % (network,))
+            subprocess.call("python drive_network_windrose.py %s" % (network,),
+                            shell=True)
             sys.exit()
