d9d4e60029bfd14f566716327a2403ed6bb7ebb5
akrherz <akrherz@iastate.edu>
2012-08-16 08:16:35 -0500

    remove ONCE_DAILY run as it goes at midnight away, improve HADS cleaner

--- scripts/dbutil/hads_delete_dups.py
+++ scripts/dbutil/hads_delete_dups.py
@@ -1,42 +1,49 @@
 """
-Our HADS database gets loaded up with duplicates, this cleans it up.
+ Our HADS database gets loaded up with duplicates, this cleans it up.
+ 
+ called from RUN_MIDNIGHT.sh
 """
 import mx.DateTime
 import iemdb
 HADS = iemdb.connect('hads')
-hcursor = HADS.cursor()
-hcursor.execute("SET TIME ZONE 'GMT'")
+
+def query(sql):
+    """
+    Do a query and make it atomic
+    """
+    hcursor = HADS.cursor()
+    hcursor.execute("SET TIME ZONE 'GMT'")
+    hcursor.execute(sql)
+    hcursor.close()
+    HADS.commit()
 
 def do(ts):
-  # Delete schoolnet data, since we created it in the first place!
-  sql = """DELETE from raw%s WHERE station IN 
-            (SELECT id from stations WHERE network in ('KCCI','KELO','KIMT')
-            )""" % (ts.strftime("%Y_%m"),)
-  hcursor.execute(sql)
-
-  # Extract unique obs to special table
-  sql = "CREATE table tmp as select distinct * from raw%s WHERE valid BETWEEN \
-         '%s' and '%s'" % (ts.strftime("%Y_%m"), ts.strftime("%Y-%m-%d"), \
-         (ts + mx.DateTime.RelativeDateTime(days=1)).strftime("%Y-%m-%d"))
-  hcursor.execute(sql)
-
-  # Delete them all!
-  sql = "delete from raw%s WHERE valid BETWEEN \
-         '%s' and '%s'" % (ts.strftime("%Y_%m"), ts.strftime("%Y-%m-%d"), \
-         (ts + mx.DateTime.RelativeDateTime(days=1)).strftime("%Y-%m-%d"))
-  hcursor.execute(sql)
-
-  # Insert from special table
-  sql = "INSERT into raw%s SELECT * from tmp" % (ts.strftime("%Y_%m"),)
-  hcursor.execute(sql)
-
-  # drop special table!
-  hcursor.execute("DROP table tmp")
+    # Delete schoolnet data, since we created it in the first place!
+    sql = """DELETE from raw%s WHERE station IN 
+              (SELECT id from stations WHERE network in ('KCCI','KELO','KIMT')
+              )""" % (ts.strftime("%Y_%m"),)
+    query(sql)
+    
+    # Extract unique obs to special table
+    sql = """CREATE table tmp as select distinct * from raw%s WHERE valid BETWEEN 
+           '%s' and '%s'""" % (ts.strftime("%Y_%m"), ts.strftime("%Y-%m-%d"), 
+           (ts + mx.DateTime.RelativeDateTime(days=1)).strftime("%Y-%m-%d"))
+    query(sql)
+    
+    # Delete them all!
+    sql = """delete from raw%s WHERE valid BETWEEN 
+           '%s' and '%s'""" % (ts.strftime("%Y_%m"), ts.strftime("%Y-%m-%d"), 
+           (ts + mx.DateTime.RelativeDateTime(days=1)).strftime("%Y-%m-%d"))
+    query(sql)
+        
+    # Insert from special table
+    sql = "INSERT into raw%s SELECT * from tmp" % (ts.strftime("%Y_%m"),)
+    query(sql)
+    
+    # drop special table!
+    query(sql)
 
 do( mx.DateTime.now() - mx.DateTime.RelativeDateTime(days=1) )
-hcursor.close()
-HADS.commit()
-HADS.close()
 
 #sts = mx.DateTime.DateTime(2007,12,1)
 #ets = mx.DateTime.DateTime(2008,1,1)
