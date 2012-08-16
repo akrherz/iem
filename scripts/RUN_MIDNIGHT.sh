950563c7b009a0e5bba31294c8ae5ab5b2dc0822
akrherz <akrherz@iastate.edu>
2012-08-16 08:16:35 -0500

    remove ONCE_DAILY run as it goes at midnight away, improve HADS cleaner

--- scripts/RUN_MIDNIGHT.sh
+++ scripts/RUN_MIDNIGHT.sh
@@ -19,5 +19,12 @@ python plot.py 0
 sleep 600
 cd ../qc
 python correctGusts.py
-
 python check_station_geom.py
+python check_vtec_eventids.py
+
+cd ../outgoing
+python wxc_moon.py
+
+cd ../dbutil 
+python ot2archive.py
+python hads_delete_dups.py
