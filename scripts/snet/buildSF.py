5ba3f4f481bac74b1d94d712e76ac520da8e82b4
akrherz <akrherz@iastate.edu>
2012-09-03 15:22:46 -0500

    python 2.6 updates and cleanups

--- scripts/snet/buildSF.py
+++ scripts/snet/buildSF.py
@@ -1,30 +1,34 @@
-#!/mesonet/python/bin/python
-# Need something to build a GEMPAK surface file 
-# Daryl Herzmann 22 Mar 2004
-# 23 Mar 2004	Augh, use GMT!
-# 31 Mar 2004	Caught error when this bad boy runs at 12:03 AM
-
-from pyIEM import iemAccessDatabase
-iemdb = iemAccessDatabase.iemAccessDatabase()
+"""
+ Need something to build a GEMPAK surface file 
+ Daryl Herzmann 22 Mar 2004
+ 23 Mar 2004	Augh, use GMT!
+ 31 Mar 2004	Caught error when this bad boy runs at 12:03 AM
+"""
+import iemdb
+IEM = iemdb.connect('iem', bypass=True)
+import psycopg2.extras
+icursor = IEM.cursor(cursor_factory=psycopg2.extras.DictCursor)
 import mx.DateTime
 
 now = mx.DateTime.gmt() + mx.DateTime.RelativeDateTime(minutes=-3)
 sftime = now.strftime("%y%m%d/%H%M")
 
 def main():
-	rs = iemdb.query("SELECT *, s.pmonth from current c, summary_%s s, stations t \
-		WHERE s.day = 'TODAY' and s.iemid = c.iemid and t.iemid = c.iemid and \
-		t.network IN ('KCCI', 'KELO') and \
-		c.valid > (CURRENT_TIMESTAMP - '40 minutes'::interval)" % (now.year,) ).dictresult()
+	icursor.execute("""SELECT *, s.pmonth from current c, summary_%s s, stations t 
+		WHERE s.day = 'TODAY' and s.iemid = c.iemid and t.iemid = c.iemid and 
+		t.network IN ('KCCI', 'KELO') and 
+		c.valid > (CURRENT_TIMESTAMP - '40 minutes'::interval)""" % (
+													now.year,) )
 
 	out = open("sfedit.fil", 'w')
-	for i in range(len(rs)):
-		rs[i]["smph"] = rs[i]["sknt"] * 1.15
+	for row in icursor:
+		row2 = row.copy()
+		row2["smph"] = row2["sknt"] * 1.15
 		try:
-			out.write("%7s %s %4i %4i %4i %4i %4.1f %4.2f %4i %4.2f %4.2f %3i\n" %\
-		(rs[i]["id"], sftime, rs[i]["tmpf"], rs[i]["relh"], rs[i]["dwpf"],\
-		rs[i]["drct"], rs[i]["smph"], rs[i]["pday"], rs[i]["srad"], \
-		rs[i]["pres"], rs[i]["pmonth"], rs[i]["max_gust"]) )
+			out.write("%7s %s %4i %4i %4i %4i %4.1f %4.2f %4i %4.2f %4.2f %3i\n" % (
+		row2["id"], sftime, row2["tmpf"], row2["relh"], row2["dwpf"],
+		row2["drct"], row2["smph"], row2["pday"], row2["srad"], 
+		row2["pres"], row2["pmonth"], row2["max_gust"]) )
 		except TypeError:
 			continue
 
--- scripts/snet/run2access.sh
+++ scripts/snet/run2access.sh
@@ -2,7 +2,6 @@
 # I run from rc.local and mine the realtime feed of data into iemaccess
 
 export PYTHONPATH=$PYTHONPATH:/mesonet/scripts/snetPlex
-export PATH=/mesonet/python-2.5/bin:$PATH
 
 kill -9 `cat twistd.pid`
 sleep 1
