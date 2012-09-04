0d80dabfc11edfbda4aa0c78891445d32538b209
akrherz <akrherz@iastate.edu>
2012-09-04 09:09:05 -0500

    take 7 DCP sites at a time, make sure COOP IDs are legit

--- scripts/qc/hads_nwsli_squawk.py
+++ scripts/qc/hads_nwsli_squawk.py
@@ -7,8 +7,8 @@ hcursor2 = HADS.cursor()
 print 'Unknown NWSLIs from DCPish sites'
 hcursor.execute("""
  select nwsli, count(*) as tot, max(product), count(distinct substr(product,1,8)), count(distinct product) from unknown 
- where product !~* 'KTIR' and length(nwsli) = 5
- GROUP by nwsli ORDER by tot DESC LIMIT 5
+ where nwsli ~ '[A-Z]{4}[0-9]'
+ GROUP by nwsli ORDER by tot DESC LIMIT 7
 """)
 for row in hcursor:
     print '%7s Tot:%4s Days:%2s Products: %s %s' % (row[0], row[1], row[3], 
@@ -20,7 +20,8 @@ for row in hcursor:
 print 'Unknown NWSLIs from COOPish sites'
 hcursor.execute("""
  select nwsli, count(*) as tot, max(product), count(distinct substr(product,1,8)), count(distinct product) from unknown 
- WHERE network ~* 'COOP' GROUP by nwsli ORDER by tot DESC LIMIT 5
+ WHERE network ~* 'COOP'
+ and nwsli ~ '[A-Z]{4}[0-9]' GROUP by nwsli ORDER by tot DESC LIMIT 5
 """)
 for row in hcursor:
     print 'COOP %7s Tot:%4s Days:%2s Products: %s %s' % (row[0], row[1], 
