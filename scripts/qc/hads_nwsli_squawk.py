27a8616ce3d6e0ef71d73cd50500e7b9910fb943
akrherz <akrherz@iastate.edu>
2012-09-07 07:50:43 -0500

    prioritize Iowa sites first

--- scripts/qc/hads_nwsli_squawk.py
+++ scripts/qc/hads_nwsli_squawk.py
@@ -1,4 +1,8 @@
-# Squawk to daryl about unknown HADS sites
+"""
+ Squawk to daryl about unknown HADS sites
+
+Run from RUN_2AM.sh
+"""
 import iemdb
 HADS = iemdb.connect('hads')
 hcursor = HADS.cursor()
@@ -6,9 +10,13 @@ hcursor2 = HADS.cursor()
 
 print 'Unknown NWSLIs from DCPish sites'
 hcursor.execute("""
- select nwsli, count(*) as tot, max(product), count(distinct substr(product,1,8)), count(distinct product) from unknown 
+ select nwsli, count(*) as tot, max(product), 
+ count(distinct substr(product,1,8)), 
+ count(distinct product),
+ sum(case when substr(nwsli,4,2) = 'I4' then 1 else 0 end) as priority 
+ from unknown 
  where nwsli ~ '[A-Z]{4}[0-9]'
- GROUP by nwsli ORDER by tot DESC LIMIT 7
+ GROUP by nwsli ORDER by priority DESC, tot DESC LIMIT 7
 """)
 for row in hcursor:
     print '%7s Tot:%4s Days:%2s Products: %s %s' % (row[0], row[1], row[3], 
@@ -19,9 +27,13 @@ for row in hcursor:
 
 print 'Unknown NWSLIs from COOPish sites'
 hcursor.execute("""
- select nwsli, count(*) as tot, max(product), count(distinct substr(product,1,8)), count(distinct product) from unknown 
+ select nwsli, count(*) as tot, max(product), 
+ count(distinct substr(product,1,8)), 
+ count(distinct product),
+ sum(case when substr(nwsli,4,2) = 'I4' then 1 else 0 end) as priority
+ from unknown 
  WHERE network ~* 'COOP'
- and nwsli ~ '[A-Z]{4}[0-9]' GROUP by nwsli ORDER by tot DESC LIMIT 5
+ and nwsli ~ '[A-Z]{4}[0-9]' GROUP by nwsli ORDER by priority DESC, tot DESC LIMIT 5
 """)
 for row in hcursor:
     print 'COOP %7s Tot:%4s Days:%2s Products: %s %s' % (row[0], row[1], 
