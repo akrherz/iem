35e684421c54d2420022812fa6a9630a207b444d
akrherz <akrherz@iastate.edu>
2012-09-14 11:27:05 -0500

    add download option for erosion...

--- cgi-bin/wepp/idep2shape.py
+++ cgi-bin/wepp/idep2shape.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-Provide IDEP soil moisture on a per township basis
+Provide IDEP output on a per township basis
 """
 import sys
 sys.path.insert(0, '/mesonet/www/apps/iemwebsite/scripts/lib')
@@ -14,8 +14,9 @@ import mx.DateTime
 import sys
 import zipfile
 import shutil
+import psycopg2.extras
 WEPP = iemdb.connect('wepp', bypass=True)
-wcursor = WEPP.cursor()
+wcursor = WEPP.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
 os.chdir('/tmp/')
 
@@ -25,16 +26,16 @@ year = int(form["year"][0])
 month = int(form["month"][0])
 day = int(form["day"][0])
 ts = mx.DateTime.DateTime(year, month, day)
-fp = "%s_sm" % (ts.strftime("%Y%m%d"), )
+fp = "%s_idep" % (ts.strftime("%Y%m%d"), )
 interval = None
 if form.has_key("interval"):
     interval = int(form["interval"][0])
-    fp = "%s_%s_sm" % (ts.strftime("%Y%m%d"), interval)
+    fp = "%s_%s_idep" % (ts.strftime("%Y%m%d"), interval)
 
 monthly = None
 if form.has_key("monthly"):
     monthly = 1
-    fp = "%s_sm" % (ts.strftime("%Y%m"), )
+    fp = "%s_idep" % (ts.strftime("%Y%m"), )
 
 
 print "Content-type: application/octet-stream"
@@ -56,27 +57,31 @@ if form.has_key("point"):
       from iatwp"""
 wcursor.execute( sql )
 for row in wcursor:
-    twp[ row[1] ] = row[0]
+    twp[ row[1] ] = row['tg']
 
 # Now, lets figure out what data we want
-sql = """SELECT model_twp, vsm, -1, s10cm, s20cm 
-        from waterbalance_by_twp WHERE valid = '%s'""" % (
+sql = """SELECT *
+        from results_by_twp WHERE valid = '%s'""" % (
                                         ts.strftime("%Y-%m-%d"), )
 day1 = ts.strftime("%Y%m%d")
 day2 = ts.strftime("%Y%m%d")
 if interval is not None:
-    sql = "SELECT model_twp, avg(vsm) as vsm, -1 as vsm_stddev, \
-      avg(s10cm) as s10cm, avg(s20cm) as s20cm from waterbalance_by_twp \
-      WHERE valid BETWEEN '%s' and ('%s'::date + '%s days'::interval) \
-      GROUP by model_twp" % (ts.strftime("%Y-%m-%d"), ts.strftime("%Y-%m-%d"),\
+    sql = """SELECT model_twp, 
+      avg(avg_precip) as avg_precip, 
+      avg(avg_loss) as avg_loss, 
+      avg(avg_runoff) as avg_runoff results_by_twp 
+      WHERE valid BETWEEN '%s' and ('%s'::date + '%s days'::interval) 
+      GROUP by model_twp""" % (ts.strftime("%Y-%m-%d"), ts.strftime("%Y-%m-%d"),
       interval)
     day2 = (ts + mx.DateTime.RelativeDateTime(days=interval)).strftime("%Y%m%d")
 
 if monthly is not None:
-    sql = "SELECT model_twp, avg(vsm) as vsm, -1 as vsm_stddev, \
-      avg(s10cm) as s10cm, avg(s20cm) as s20cm from waterbalance_by_twp \
-      WHERE valid BETWEEN '%s-01' and ('%s-01'::date + '1 month'::interval) \
-      GROUP by model_twp" % (ts.strftime("%Y-%m"), ts.strftime("%Y-%m") )
+    sql = """SELECT model_twp, 
+      sum(avg_precip) as avg_precip, 
+      sum(avg_loss) as avg_loss, 
+      sum(avg_runoff) as avg_runoff from results_by_twp 
+      WHERE valid BETWEEN '%s-01' and ('%s-01'::date + '1 month'::interval) 
+      GROUP by model_twp""" % (ts.strftime("%Y-%m"), ts.strftime("%Y-%m") )
     day1 = (ts + mx.DateTime.RelativeDateTime(day=1)).strftime("%Y%m%d")
     day2 = (ts + mx.DateTime.RelativeDateTime(day=1,months=1) - mx.DateTime.RelativeDateTime(days=1) ).strftime("%Y%m%d")
 
@@ -90,26 +95,23 @@ dbf = dbflib.create(fp)
 dbf.add_field("DAY_STA", dbflib.FTString, 8, 0)
 dbf.add_field("DAY_END", dbflib.FTString, 8, 0)
 dbf.add_field("MODL_TWP", dbflib.FTString, 10, 0)
-dbf.add_field("VSM", dbflib.FTDouble, 8, 4)
-dbf.add_field("VSM_STDD", dbflib.FTDouble, 8, 4)
-dbf.add_field("S10CM", dbflib.FTDouble, 8, 4)
-dbf.add_field("S20CM", dbflib.FTDouble, 8, 4)
+dbf.add_field("PRECIP", dbflib.FTDouble, 8, 4)
+dbf.add_field("LOSS", dbflib.FTDouble, 8, 4)
+dbf.add_field("RUNOFF", dbflib.FTDouble, 8, 4)
 
 i = 0
 for row in wcursor:
-  m = row[0]
-  vsm = row[1]
-  vsms = row[2]
-  s10 = row[3]
-  s20 = row[4]
-
+  m = row['model_twp']
+  loss = row['avg_loss']
+  runoff = row['avg_runoff']
+  precip = row['avg_precip']
   f = wellknowntext.convert_well_known_text( twp[m] )
   if form.has_key("point"):
     obj = shapelib.SHPObject(shapelib.SHPT_POINT, 1, [[f]] )
   else:
     obj = shapelib.SHPObject(shapelib.SHPT_POLYGON, 1, f )
   shp.write_object(-1, obj)
-  dbf.write_record(i, (day1,day2,m,vsm,vsms,s10,s20) )
+  dbf.write_record(i, (day1,day2,m,precip,loss,runoff) )
   i += 1
 
 del(dbf)
@@ -118,21 +120,19 @@ WEPP.close()
 
 o = open(fp+".txt", 'w')
 o.write("""
-WEPP Modelled Soil Moisture from the Iowa Daily Erosion Project
+WEPP Modelled Erosion from the Iowa Daily Erosion Project
 http://wepp.mesonet.agron.iastate.edu
 
 DBF Columns are:
   DAY_STA   Start date of the period data is valid for YYYYMMDD
   DAY_END   End date of the period data is valid for YYYYMMDD
   MODL_TWP  Model township
-  VSM       Volumetric Soil Moisture [%] over root zone depth
-  VSM_STDD  VSM Standard Deviation within model township
-  S10CM     0-10 cm depth soil moisture [mm]
-  S20CM     10-20 cm depth soil moisture [mm]
-
-If DAY_STA does not equal DAY_END, then the values have been time 
-averaged.  VSM_STDD is set to -1 in this case, since we don't have
-necessary data to produce this statistic.
+  PRECIP    Township averaged precipitation  [mm]
+  LOSS      Township averaged loss [kg/m^2]
+  RUNOFF    Township averaged runoff [mm]
+  
+If DAY_STA does not equal DAY_END, then the values have been 
+summed over time.
 
 Data Contact:
   Daryl Herzmann akrherz@iastate.edu  515.294.5978
