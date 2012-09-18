5bead67275e4de21e0f9fe1419d8ae6999b8b448
akrherz <akrherz@iastate.edu>
2012-09-18 08:35:34 -0500

    add email ingestor

---
+++ scripts/ingestors/email/colo_tower.py
@@ -0,0 +1,13 @@
+"""
+Ingest the Colo tower data arriving via email!
+"""
+import sys
+import mx.DateTime
+import email.parser
+msg = email.parser.Parser().parse(sys.stdin)
+for part in msg.walk():
+    if part.get_filename() != None:
+        fp = mx.DateTime.now().strftime("/tmp/colo%Y%m%d%H%M%S.txt")
+        o = open(fp, 'a')
+        o.write( part.get_payload() )
+        o.close()
