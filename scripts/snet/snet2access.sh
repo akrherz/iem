6fda8a02ed13f4a206d3e715e862c3778171aaef
akrherz <akrherz@iastate.edu>
2012-09-19 13:46:25 -0500

    update to python2.6 and crontab to start it

--- scripts/snet/snet2access.sh
+++ scripts/snet/snet2access.sh
@@ -5,3 +5,5 @@
 kill -9 `cat snet2access.pid`
 sleep 1
 twistd --pidfile=snet2access.pid --logfile=/mesonet/data/logs/snet2access.log -y snet2access.py
+# Allow nagios to monitor this file
+chmod 644 snet2access.pid
