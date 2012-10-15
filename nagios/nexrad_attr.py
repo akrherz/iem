3951e87834065ad327a996f32d5a8b8a7b548d72
akrherz <akrherz@iastate.edu>
2012-10-15 15:55:54 -0500

    add checker for nexrad attributes

---
+++ nagios/nexrad_attr.py
@@ -0,0 +1,26 @@
+"""
+ Nagios check to make sure we have NEXRAD attribute data
+"""
+import sys
+import os
+import iemdb
+
+POSTGIS = iemdb.connect('postgis', bypass=True)
+pcursor = POSTGIS.cursor()
+
+pcursor.execute("""
+ select count(*) from nexrad_attributes WHERE 
+ valid > now() - '30 minutes'::interval
+""")
+row = pcursor.fetchone()
+count = row[0]
+
+if count > 30:
+    print 'OK - attrs %s |count=%s;15;30;0' % (count, count)
+    sys.exit(0)
+elif count > 15:
+    print 'OK - attrs %s |count=%s;15;30;0' % (count, count)
+    sys.exit(1)
+else:
+    print 'CRITICAL - attrs %s |count=%s;15;30;0' % (count, count)
+    sys.exit(2)
