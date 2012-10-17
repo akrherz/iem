b9cd1adb09718eb65f024ffc0f20456602496913
akrherz <akrherz@iastate.edu>
2012-10-17 11:57:25 -0500

    increase threshold for whining on IDD monitoring

--- nagios/iem_idd.py
+++ nagios/iem_idd.py
@@ -10,7 +10,7 @@ now = datetime.datetime.now()
 mtime = os.stat("/home/ldm/data/mesonet.gif")[stat.ST_MTIME]
 ts = datetime.datetime.fromtimestamp(mtime)
 diff = (now - ts).days * 86400. + (now - ts).seconds
-if diff < 1800:
+if diff < 3300:
     print 'OK - mesonet.gif %s' % (ts,)
     sys.exit(0)
 elif diff < 3600:
