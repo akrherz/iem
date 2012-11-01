99ff96fca19d152a8e9d12ac8d11cd6fed93c09b
akrherz <akrherz@iastate.edu>
2012-11-01 14:46:56 -0500

    add other database script

---
+++ database/other.sql
@@ -0,0 +1,20 @@
+CREATE TABLE alldata (
+    station character varying(6),
+    valid timestamp with time zone,
+    tmpf real,
+    dwpf real,
+    drct real,
+    sknt real,
+    gust real,
+    relh real,
+    alti real,
+    pcpncnt real,
+    pday real,
+    pmonth real,
+    srad real,
+    c1tmpf real
+);
+
+CREATE TABLE t2012 (
+)
+INHERITS (alldata);
