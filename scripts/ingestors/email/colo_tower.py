2212e299667dfd3238fba33c22043760d5124f68
akrherz <akrherz@iastate.edu>
2012-11-05 16:17:22 -0600

    put data in a safer place

--- scripts/ingestors/email/colo_tower.py
+++ scripts/ingestors/email/colo_tower.py
@@ -7,7 +7,7 @@ import email.parser
 msg = email.parser.Parser().parse(sys.stdin)
 for part in msg.walk():
     if part.get_filename() != None:
-        fp = mx.DateTime.now().strftime("/tmp/colo%Y%m%d%H%M%S.txt")
+        fp = mx.DateTime.now().strftime("/home/colo/colo%Y%m%d%H%M%S.txt")
         o = open(fp, 'a')
         o.write( part.get_payload() )
         o.close()
