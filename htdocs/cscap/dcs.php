c3e630f294e8d18bf5559d0d33963894af1685e5
akrherz <akrherz@iastate.edu>
2012-11-09 15:39:38 -0600

    a working version 1.0 is done!

--- htdocs/cscap/dcs.php
+++ htdocs/cscap/dcs.php
@@ -77,8 +77,9 @@ $(function(){
 
 <form name='authform'>
 <div id="needtoauthenticate">
-To use this interface, you need to be authenticated to google.  A popup should
-appear shortly to start that process...
+To use this interface, you need to be authenticated to google.  
+<input type="button" value="Click to Authenticate to Google" 
+onclick="handleAuthClick()" />
 </div>
 <div id="authenticated" style="display: none;">
 You are authenticated to Google and can use this interface!
@@ -91,6 +92,8 @@ Previously entered Farmer Surveys:
 	onChange="javascript: setFarmer(this.options[this.selectedIndex].value);">
 	<option value='invalid'>-- SELECT FROM LIST --</option>
 </select>
+
+<input type="button" value="Refresh List of Surveys" onclick="getSpreadsheets();"/>
 </form>
 
 <form method="POST" name="theform" id="theform">
@@ -112,23 +115,23 @@ Previously entered Farmer Surveys:
 <?php echo tdgen("County", "county", 30); ?>
 </tr>
 <tr>
-<?php echo tdgen("Total Acres Owned", "ta_owned", 6); ?>
-<?php echo tdgen("Acres Rented", "ta_rented", 6); ?>
+<?php echo tdgen("Total Acres Owned", "taowned", 6); ?>
+<?php echo tdgen("Acres Rented", "tarented", 6); ?>
 </tr>
 </table>
 <table>
 <tr>
-<?php echo tdgen("Type of operation (sole proprietor/family/etc.)", "op_type"); ?>
+<?php echo tdgen("Type of operation (sole proprietor/family/etc.)", "optype"); ?>
 </tr>
 </table>
 
 <p><strong>Field location details (Sufficient information for locating on a plat map or Google Earth, e.g. address, Township, Range):</strong>
-<textarea name="field_details" rows="4" cols="72"></textarea>
+<textarea name="fielddetails" rows="4" cols="72"></textarea>
 
 <table>
 <tr>
 <?php echo tdyn("Farm has livestock:", "livestock"); ?>
-<?php echo tdyn("Manure import from other farms:", "inport_manure"); ?>
+<?php echo tdyn("Manure import from other farms:", "importmanure"); ?>
 </tr>
 </table>
 
@@ -141,15 +144,15 @@ Previously entered Farmer Surveys:
 		"6000"=> "6,000",
 		"other"=>"")); ?>
 		</td>
-		<?php echo tdgen("Other", "availability_other", 10); ?>
+		<?php echo tdgen("Other", "availabilityother", 10); ?>
 		</tr>
 </table>
 
 <p>
 <table><tr>
-<?php echo tdgen("Total # of fields", "total_fields", 10); ?>
-<?php echo tdgen("Area of field 1 (acres)", "field1_acres", 10); ?>
-<?php echo tdgen("Area of field 2 (acres)", "field2_acres", 10); ?>
+<?php echo tdgen("Total # of fields", "totalfields", 10); ?>
+<?php echo tdgen("Area of field 1 (acres)", "field1acres", 10); ?>
+<?php echo tdgen("Area of field 2 (acres)", "field2acres", 10); ?>
 </tr></table>
 
 <p><strong>Livestock Information</strong>
@@ -157,13 +160,13 @@ Previously entered Farmer Surveys:
 <table>
 <?php for($i=1;$i<4;$i++){ 
  echo "<tr>";
- echo tdgen("Type of livestock $i", "livestock${i}_type", 30); 
- echo tdgen("Head Count", "livestock${i}_count", 5); 
- echo tdgen("% feed produced on farm", "livestock${i}_feedonfarm", 4);
+ echo tdgen("Type of livestock $i", "livestock${i}type", 30); 
+ echo tdgen("Head Count", "livestock${i}count", 5); 
+ echo tdgen("% feed produced on farm", "livestock${i}feedonfarm", 4);
  echo "</tr>";
 } ?> 
 <tr>
-<?php echo tdgen("Acres in pasture/hay", "pasturehay_acres", 10); ?>
+<?php echo tdgen("Acres in pasture/hay", "pasturehayacres", 10); ?>
 </tr>
 </table>
 
@@ -176,23 +179,23 @@ Previously entered Farmer Surveys:
 <?php for($field=1;$field<3;$field++){
     echo "<div id='tabs-${field}' style='background: url(DataCollectionSheet_files/field${field}.png) repeat-y;'>";
 	echo "<div style='margin-left: 50px;'>
-   <table><tr>". tdgen("Unique Name", "field${field}_name") ."</tr></table>
+   <table><tr>". tdgen("Unique Name", "field${field}name") ."</tr></table>
 		";
-  echo "<p>Cropping system for this field ". radio("field${field}_system", Array(
+  echo "<p>Cropping system for this field ". radio("field${field}system", Array(
 		"single"=> "Single crop per rotation year",
 		"multi"=> "Multiple/Double/Cover crops"));
-  echo "<table><tr><th>Field is</th><td>". radio("field${field}_tile", Array(
+  echo "<table><tr><th>Field is</th><td>". radio("field${field}tile", Array(
 		"tiled"=> "Tiled",
 		"not_tiled"=> "Not tiled")) ."</td>";
-  echo "<th>Field is<th><td>". radio("field${field}_owned", Array(
+  echo "<th>Field is<th><td>". radio("field${field}owned", Array(
 		"owned"=> "Owned",
 		"rented"=> "Rented")) ."</td>";
   echo "</tr></table>";
   
   echo "<table><tr>";
-  echo tdgen("Soil type", "field${field}_soiltype", 30);
-  echo tdgen("Soil test P", "field${field}_soiltest_p", 10);
-  echo tdgen("Soil test type", "field${field}_soiltest_t", 20);
+  echo tdgen("Soil type", "field${field}soiltype", 30);
+  echo tdgen("Soil test P", "field${field}soiltestp", 10);
+  echo tdgen("Soil test type", "field${field}soiltestt", 20);
   echo "</tr></table>";
   
   echo "<div id=\"field${field}_tabs\">
@@ -206,77 +209,77 @@ Previously entered Farmer Surveys:
     echo "<div id='field${field}_tabs${rotation}' style='background: url(DataCollectionSheet_files/rotation${rotation}.png) repeat-y;'>";
     echo "<div style='margin-left: 25px;'>";
     echo "<table><tr>";
-    echo tdgen("Crop Type", "field${field}_rotation${rotation}_croptype", 30);
-    echo tdgen("Year", "field${field}_rotation${rotation}_year", 6);
+    echo tdgen("Crop Type", "field${field}rotation${rotation}croptype", 30);
+    echo tdgen("Year", "field${field}rotation${rotation}year", 6);
     echo "</tr></table>";
 		
 	echo "<table><tr>";
-	echo tdgen("Yield (per acre)", "field${field}_rotation${rotation}_yield", 10);
-	echo tdyn("Irrigated", "field${field}_rotation${rotation}_irrigated");
-	echo "<th>Harvest:</th><td>". radio("field${field}_rotation${rotation}_harvest", Array(
+	echo tdgen("Yield (per acre)", "field${field}rotation${rotation}yield", 10);
+	echo tdyn("Irrigated", "field${field}rotation${rotation}irrigated");
+	echo "<th>Harvest:</th><td>". radio("field${field}rotation${rotation}harvest", Array(
 			"grain_only"=> "Grain only",
 			"grain_and_res"=> "Grain and residue")) ."</td>";
 	echo "</tr></table>";
 	
 	echo "<table><tr>";
-	echo tdgen("Percent residue removed (%)", "field${field}_rotation${rotation}_resremoved", 5);
-	echo tdyn("Shred cornstalks before tillage", "field${field}_rotation${rotation}_shredstalks");
+	echo tdgen("Percent residue removed (%)", "field${field}rotation${rotation}resremoved", 5);
+	echo tdyn("Shred cornstalks before tillage", "field${field}rotation${rotation}shredstalks");
 	echo "</tr></table>";
 	
 	echo "<table><tr>";
-	echo tdgen("Residue removal: (e.g. bale)", "field${field}_rotation${rotation}_resremovalmethod", 30);
+	echo tdgen("Residue removal: (e.g. bale)", "field${field}rotation${rotation}resremovalmethod", 30);
 	echo "</tr></table>";
 	
 	echo "<table>";
 	for ($tillage=1;$tillage<7;$tillage++){
       echo "<tr>";
-      $s = "field${field}_rotation${rotation}_tillage${tillage}";
-      echo tdgen("Tillage equipment", "${s}_equip", 30);
-      echo tdgen("Width", "${s}_width", 5);
-      echo tdgen("Date", "${s}_date", 10);
+      $s = "field${field}rotation${rotation}tillage${tillage}";
+      echo tdgen("Tillage equipment", "${s}equip", 30);
+      echo tdgen("Width", "${s}width", 5);
+      echo tdgen("Date", "${s}date", 10);
       echo "</tr>";
 	} // End of tillage
 	echo "</table>";
 	
 	echo "<table><tr>";
-	echo tdgen("Seeding rate (per acre)", "field${field}_rotation${rotation}_seedrate", 10);
-	echo "<th>Units:</th><td>". radio("field${field}_rotation${rotation}_seedunits", Array(
+	echo tdgen("Seeding rate (per acre)", "field${field}rotation${rotation}seedrate", 10);
+	echo "<th>Units:</th><td>". radio("field${field}rotation${rotation}seedunits", Array(
 			"seeds_per_acre"=> "seeds per acre",
 			"lbs_per_acre"=> "lbs. per acre")) ."</td>";
 	echo "</tr></table>";
 	
 	echo "<table><tr>";
-	echo tdgen("Relative maturity", "field${field}_rotation${rotation}_maturity");
+	echo tdgen("Relative maturity", "field${field}rotation${rotation}maturity");
 	echo "</tr></table>";
 	
 	echo "<table><tr>";
-	echo tdgen("Planting equipment", "field${field}_rotation${rotation}_plantequip", 30);
-	echo tdgen("Date", "field${field}_rotation${rotation}_plantdate", 10);
+	echo tdgen("Planting equipment", "field${field}rotation${rotation}plantequip", 30);
+	echo tdgen("Date", "field${field}rotation${rotation}plantdate", 10);
 	echo "</tr><tr>";
-	echo tdgen("Harvest equipment", "field${field}_rotation${rotation}_harvestequip", 30);
-	echo tdgen("Date", "field${field}_rotation${rotation}_harvestdate", 10);	
+	echo tdgen("Harvest equipment", "field${field}rotation${rotation}harvestequip", 30);
+	echo tdgen("Date", "field${field}rotation${rotation}harvestdate", 10);	
 	echo "</tr></table>";
 	
 	echo "<table>";
 	for($weed=1;$weed<5;$weed++){
-		$s = "field${field}_rotation${rotation}_weed${weed}";
+		$s = "field${field}rotation${rotation}weed${weed}";
 		echo "<tr>";
-		echo tdgen("Weed Control #${weed}", "${s}_control", 20);
-		echo tdgen("Rate", "${s}_rate", 10);
-		echo tdgen("# of passes", "${s}_passes", 4);
-		echo tdgen("Date", "${s}_date", 10);
+		echo tdgen("Weed Control #${weed}", "${s}control", 20);
+		echo tdgen("Rate", "${s}rate", 10);
+		echo tdgen("# of passes", "${s}passes", 4);
+		echo tdgen("Date", "${s}date", 10);
 		echo "</tr>";
 	} // End of weed
 	echo "</table>";
 	
 	echo "<table>";
 	for($fert=1;$fert<5;$fert++){
-		$s = "field${field}_rotation${rotation}_fert${fert}";
+		$s = "field${field}rotation${rotation}fert${fert}";
 		echo "<tr>";
-		echo tdgen("Fertilizer #${fert}", "${s}_fert", 20);
-		echo tdgen("Amount (lbs/acre)", "${s}_amount", 5);
-		echo tdgen("Applicator type", "${s}_apptype", 15);
-		echo tdgen("Date", "${s}_date", 5);
+		echo tdgen("Fertilizer #${fert}", "${s}fert", 20);
+		echo tdgen("Amount (lbs/acre)", "${s}amount", 5);
+		echo tdgen("Applicator type", "${s}apptype", 15);
+		echo tdgen("Date", "${s}date", 5);
 		echo "</tr>";
 	} // End of fert
 	echo "</table>";
@@ -294,25 +297,25 @@ Previously entered Farmer Surveys:
   
   for ($cover=1;$cover<4;$cover++){
 	echo "<div id='field${field}_ctabs${cover}' style='background: url(DataCollectionSheet_files/cover${cover}.png) repeat-y;'>";
-	echo "<div style='margin-left: 25px;'>";
+	echo "<div style='margin: 25px;'>";
   	echo "<table><tr>";
-  	echo tdgen("Cover Crop Type", "field${field}_cover${cover}_type",30);
-  	echo tdgen("Year", "field${field}_cover${cover}_year", 6);
+  	echo tdgen("Cover Crop Type", "field${field}cover${cover}type",30);
+  	echo tdgen("Year", "field${field}cover${cover}year", 6);
   	echo "</tr></table>";
   
   	echo "<table><tr>";
-  	echo tdgen("Seeding rate (per acre)", "field${field}_cover${cover}_seedrate", 10);
-  	echo "<th>Units:</th><td>". radio("field${field}_cover${cover}_seedunits", Array(
+  	echo tdgen("Seeding rate (per acre)", "field${field}cover${cover}seedrate", 10);
+  	echo "<th>Units:</th><td>". radio("field${field}cover${cover}seedunits", Array(
   			"seeds_per_acre"=> "seeds per acre",
   			"lbs_per_acre"=> "lbs. per acre")) ."</td>";
     echo "</tr><tr>";
-	echo tdgen("Planting equipment", "field${field}_cover${cover}_plantequip", 15);	
-	echo tdgen("Date", "field${field}_cover${cover}_plantdate", 10);	
+	echo tdgen("Planting equipment", "field${field}cover${cover}plantequip", 15);	
+	echo tdgen("Date", "field${field}cover${cover}plantdate", 10);	
 	echo "</tr><tr>";
-	echo tdgen("Termination practice (e.g., herbicide)", "field${field}_cover${cover}_termination", 30);
-	echo tdgen("Date", "field${field}_cover${cover}_termdate", 10);
+	echo tdgen("Termination practice (e.g., herbicide)", "field${field}cover${cover}termination", 30);
+	echo tdgen("Date", "field${field}cover${cover}termdate", 10);
 	echo "</tr><tr>";
-	echo tdgen("Yield (if applicable)", "field${field}_cover${cover}_yield", 10);
+	echo tdgen("Yield (if applicable)", "field${field}cover${cover}yield", 10);
 	echo "</tr></table>";
   	echo "</div></div>";
   } // End of cover
@@ -322,21 +325,21 @@ Previously entered Farmer Surveys:
 echo "</div>";
 
 $conservation = Array(
-"grassed_waterways" => "Grassed waterways",
-"contour_buffer_strips" => "Contour buffer strips",
-"filter_strips" => "Filter strips along waterways and/or field borders",
+"grassedwaterways" => "Grassed waterways",
+"contourbufferstrips" => "Contour buffer strips",
+"filterstrips" => "Filter strips along waterways and/or field borders",
 "windbreaks" => "Windbreaks and shelterbelts",
 "terraces" => "Terraces",
-"drainage_structures" => "Drainage/runoff treatment structures (e.g., nutrient removal wetlands, bioreactors)",
+"drainagestructures" => "Drainage/runoff treatment structures (e.g., nutrient removal wetlands, bioreactors)",
 "conversion" => "Whole portions of cropland converted to grass/trees",
-"cover_crops" => "Cover crops",
-"extended_rotations" => "Extended rotations/diversified with other grains/forage",
-"reduced_tillage" => "Reduced tillage (i.e. strip, ridge)",
-"no_till" => "No-till",
+"covercrops" => "Cover crops",
+"extendedrotations" => "Extended rotations/diversified with other grains/forage",
+"reducedtillage" => "Reduced tillage (i.e. strip, ridge)",
+"notill" => "No-till",
 "nutrient" => "Nutrient management (identify practices)", 
-"canopy_sensors" => "Canopy sensors for nitrogen deficiency",
+"canopysensors" => "Canopy sensors for nitrogen deficiency",
 "ipm" => "IPM (identify practices)", 
-"control_structures" => "Use of control structures to drain and store water depending on crop needs and soil conditions (drainage water management, not just tile)",
+"controlstructures" => "Use of control structures to drain and store water depending on crop needs and soil conditions (drainage water management, not just tile)",
 "precision" => "Precision agriculture (identify practices)" 
 );
 
@@ -359,21 +362,21 @@ while (list($k,$v) = each($conservation)){
  if ($k == 'precision' || $k == 'ipm' || $k == 'nutrient'){
   }
   echo sprintf("<tr style='background: ${color}'><th>%s</th>
-	<td><input type='text' name='field1_conserv_${k}'></td>
-	<td><input type='text' name='field2_conserv_${k}'></td>
-	<td><input type='text' name='fieldOther_conserv_${k}'></td>
+	<td><input type='text' name='field1conserv${k}'></td>
+	<td><input type='text' name='field2conserv${k}'></td>
+	<td><input type='text' name='fieldotherconserv${k}'></td>
 	</tr>", $v);
 } // End of conservation
 echo "<tr><th>Other</th>";
-echo "<td><input type='text' name='field1_other_type'></td>";
-echo "<td><input type='text' name='field2_other_type'></td>";
-echo "<td><input type='text' name='fieldOther_other_type'></td>";
+echo "<td><input type='text' name='field1othertype'></td>";
+echo "<td><input type='text' name='field2othertype'></td>";
+echo "<td><input type='text' name='fieldotherothertype'></td>";
 echo "</tr>";
 
-echo "<tr><th>Other</th>";
-echo "<td><input type='text' name='field1_other2_type'></td>";
-echo "<td><input type='text' name='field2_other2_type'></td>";
-echo "<td><input type='text' name='fieldOther_other2_type'></td>";
+echo "<tr style='background: #EEEEEE'><th>Other</th>";
+echo "<td><input type='text' name='field1other2type'></td>";
+echo "<td><input type='text' name='field2other2type'></td>";
+echo "<td><input type='text' name='fieldotherother2type'></td>";
 echo "</tr>";
 echo "</table>";
 ?>
@@ -389,8 +392,23 @@ nitrogen deficiency, diversified income streams?
 
 <textarea name='notes' rows='6' cols='72'></textarea>
 
+<p>You are presented with three options below.
+<ul>
+ <li><strong>Save as New Entry</strong>: This saves the data you entered above
+  as a new entry in the database. </li>
+ <li><strong>Sae as Edit to Previous Entry</strong>: If you loaded a previously
+ entered survey, this option will save this data overtop the old entry in the
+ database.</li>
+ <li><strong>Clear Form for New Entry</strong>: Clears all form values allowing
+ for a new entry to be added.</li>
+</ul>
+
+<p>Please only click the button once and be patient, it may take 10-30 seconds 
+to process!
+
 <p><input type="button" value="Save as New Entry" onclick="addNewEntry();" />
-<input type="button" value="Save as Edit to Previous Entry" onclick="updateRow();" />
+<input type="button" value="Save as Edit to Previous Entry" onclick="updateRow();"/>
+<input type="reset" value="Clear Form for New Entry" />
 
 </form>
 
