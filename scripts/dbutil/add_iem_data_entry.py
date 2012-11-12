5cd893394a25ce0a0f225eb6ba305550b7425ef6
akrherz <akrherz@iastate.edu>
2012-11-12 13:24:06 -0600

    use second cursor for inserts

--- scripts/dbutil/add_iem_data_entry.py
+++ scripts/dbutil/add_iem_data_entry.py
@@ -9,6 +9,7 @@ import psycopg2.extras
 
 IEM = iemdb.connect('iem')
 icursor = IEM.cursor(cursor_factory=psycopg2.extras.DictCursor)
+icursor2 = IEM.cursor()
 
 # Find sites that are online and not metasites that are not in the current
 # table!
@@ -22,23 +23,23 @@ for row in icursor:
     print "Adding station ID: %s NETWORK: %s" % (row['id'], row['network'])
 
     for tbl in ['trend_1h', 'trend_15m']:
-        icursor.execute("""INSERT into %s (iemid)
+        icursor2.execute("""INSERT into %s (iemid)
           VALUES ( %s) """ % (tbl,  row['iemid'] ) )
   
     tbl = 'summary_%s' % (mx.DateTime.now().year,)
-    icursor.execute("""INSERT into %s ( day, iemid)
+    icursor2.execute("""INSERT into %s ( day, iemid)
           VALUES ('TODAY', %s) """ % (tbl, 
           row['iemid'] ) )
     tbl = 'summary_%s' % ((mx.DateTime.now() + mx.DateTime.RelativeDateTime(days=1)).year,)
-    icursor.execute("""INSERT into %s ( day, iemid)
+    icursor2.execute("""INSERT into %s ( day, iemid)
           VALUES ( 'TOMORROW', %s) """ % (tbl,
           row['iemid'] ) )
     tbl = 'current'
-    icursor.execute("""INSERT into %s ( valid, iemid)
+    icursor2.execute("""INSERT into %s ( valid, iemid)
           VALUES ('1980-01-01', %s) """ % (tbl,
           row['iemid'] ) )
 
-icursor.close()
+icursor2.close()
 IEM.commit()
 IEM.close()
 
