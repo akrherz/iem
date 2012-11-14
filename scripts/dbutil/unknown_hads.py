7b872fb4702bc493185dc8068726b1b4a9e09e50
akrherz <akrherz@iastate.edu>
2012-11-14 07:45:50 -0600

    cleanup some and no longer call addMesosite

--- scripts/dbutil/unknown_hads.py
+++ scripts/dbutil/unknown_hads.py
@@ -1,5 +1,9 @@
+"""
+ I look at the unknown HADS table and see if any of these stations exist
+ in the mesosite database, if so, then I set online to true!
+"""
+
 import iemdb
-import os
 
 HADS = iemdb.connect('hads')
 MESOSITE = iemdb.connect('mesosite')
@@ -8,23 +12,22 @@ hcursor2 = HADS.cursor()
 mcursor = MESOSITE.cursor()
 
 # look for unknown
-hcursor.execute("SELECT distinct nwsli, network from unknown WHERE network != '' and network is not null")
+hcursor.execute("""SELECT distinct nwsli, network from unknown 
+    WHERE network != '' and network is not null""")
 for row in hcursor:
-  id = row[0]
-  network = row[1]
-  mcursor.execute("""
-  SELECT * from stations where network = '%s' and id = '%s'
-  """ % (network, id))
-  row = mcursor.fetchone()
-  if row:
-    #mcursor.execute("""
-    #  update stations SET online = 't' where network = '%s' and id = '%s'
-    #""" % (network, id))
-    os.system("python addSiteMesosite.py %s %s" % (network, id))
-    hcursor2.execute("""DELETE from unknown where nwsli = '%s' and network = '%s'""" % (id, network))
-  else:
-    pass
-    #print 'Unknown %s %s' % (network, id)
+    id = row[0]
+    network = row[1]
+    mcursor.execute("""
+  SELECT * from stations where network = '%s' and id = '%s' and online = 'f'
+      """ % (network, id))
+    row = mcursor.fetchone()
+    if row:
+        mcursor.execute("""
+          update stations SET online = 't' where network = '%s' and id = '%s'
+        """ % (network, id))
+        hcursor2.execute("""DELETE from unknown where nwsli = '%s' and network = '%s'""" % (id, network))
+    else:
+        print 'Site %s [%s] was unknown, but online in DB?' % (id, network)
 
 hcursor2.close()
 HADS.commit()
