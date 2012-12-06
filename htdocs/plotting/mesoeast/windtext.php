320f919868a1afb877dd00a26b6c5c9a603032b5
akrherz <akrherz@iastate.edu>
2012-12-06 14:02:49 -0600

    this site discontinued 30 Jun 2011

--- htdocs/plotting/mesoeast/windtext.php
+++ htdocs/plotting/mesoeast/windtext.php
@@ -5,19 +5,22 @@
 $titleDate = strftime("%b %d, %Y", $myTime);
 
 $dirRef = strftime("%Y/%m/%d", $myTime);
-$fcontents = file("/mesonet/ARCHIVE/data/$dirRef/text/ot/ot0006.dat");
-
+$fn = "/mesonet/ARCHIVE/data/$dirRef/text/ot/ot0006.dat";
 $maxGust = 0;
 $gustTime = 0;
 
-while (list ($line_num, $line) = each ($fcontents)) {
-
-  $parts = split (" ", $line);
-  $gust = $parts[11];
-  $gust_time = $parts[12];
-  if ($gust > $maxGust){ $maxGust = $gust; $gustTime = $gust_time;}
-} // End of while
-
+if (file_exists($fn)){
+	$fcontents = file($fn);
+	
+	
+	while (list ($line_num, $line) = each ($fcontents)) {
+	
+	  $parts = split (" ", $line);
+	  $gust = $parts[11];
+	  $gust_time = $parts[12];
+	  if ($gust > $maxGust){ $maxGust = $gust; $gustTime = $gust_time;}
+	} // End of while
+}
 ?>
 
 <div><font>Today's Maximum Wind Gust: <?php echo $maxGust; ?> mph<br>
