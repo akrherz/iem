935dcb9156f757e44ae161fe4fd89b1a6d24f58b
akrherz <akrherz@iastate.edu>
2012-12-06 14:02:49 -0600

    this site discontinued 30 Jun 2011

--- htdocs/plotting/mesoeast/maxmintemp.php
+++ htdocs/plotting/mesoeast/maxmintemp.php
@@ -4,22 +4,25 @@
 $titleDate = strftime("%b %d, %Y", $myTime);
 
 $dirRef = strftime("%Y/%m/%d", $myTime);
-$fcontents = file("/mesonet/ARCHIVE/data/$dirRef/text/ot/ot0006.dat");
+$fn = "/mesonet/ARCHIVE/data/$dirRef/text/ot/ot0006.dat";
+$thisMax = 'Missing';
+$thisMin = 'Missing';
 
-$thisMax = -99;
-$thisMin = 99;
-
-while (list ($line_num, $line) = each ($fcontents)) {
-
-  $parts = split (" ", $line);
-  $tm = $parts[6];
-  $tn = $parts[7];
-  if ($tm > $thisMax){ $thisMax = $tm; }
-  if ($tn < $thisMin){ $thisMin = $tn; }
-
-} // End of while
-
-?>
-
-<div><font>Today's Maximum Temperature: <?php echo $thisMax; ?> F<br>
-Today's Minimum Temperature: <?php echo $thisMin; ?> F<br></font></div> 
+if (file_exists($fn)){
+	$fcontents = file( $fn );
+	
+	
+	while (list ($line_num, $line) = each ($fcontents)) {
+	
+	  $parts = split (" ", $line);
+	  $tm = $parts[6];
+	  $tn = $parts[7];
+	  if ($tm > $thisMax){ $thisMax = $tm; }
+	  if ($tn < $thisMin){ $thisMin = $tn; }
+	
+	} // End of while
+}	
+	?>
+	
+	<div><font>Today's Maximum Temperature: <?php echo $thisMax; ?> F<br>
+	Today's Minimum Temperature: <?php echo $thisMin; ?> F<br></font></div> 
