d64295e747262abc2a0f35bcc444afe6ef85f1b4
akrherz <akrherz@iastate.edu>
2012-12-11 10:36:40 -0600

    add timestamp, add text of stage, add HP and HT sites

--- scripts/outgoing/wxc_iemstage.py
+++ scripts/outgoing/wxc_iemstage.py
@@ -13,9 +13,11 @@ icursor = IEM.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
 o = open('wxc_iemstage.txt', 'w')
 o.write("""Weather Central 001d0300 Surface Data TimeStamp=%s
-  12
+  15
    5 Station
   64 Stage Location Name
+   2 Day
+   4 Hour
    7 Lat
    7 Lon
   10 Current Stage
@@ -26,10 +28,19 @@ o.write("""Weather Central 001d0300 Surface Data TimeStamp=%s
   10 Sig Stage Moderate
   10 Sig Stage Major
   10 Sig Stage Record
+  10 Sig Stage Text
 """ % (mx.DateTime.gmt().strftime("%Y.%m.%d.%H%M"), ) )
 
+def compute_text(row):
+    """ Generate text of what this current stage is """
+    stage = row['value']
+    for s in ['Record', 'Major', 'Moderate', 'Flood', 'Bankfull', 'Action']:
+        if row['ss_'+s.lower()] != 'M' and float(row['ss_'+s.lower()]) < stage:
+            return s    
+    return 'Normal'
+
 icursor.execute("""
- SELECT c.value, x(geom) as lon, y(geom) as lat, name, station,
+ SELECT c.value, x(geom) as lon, y(geom) as lat, name, station, valid,
  case when sigstage_low is null then 'M' else sigstage_low::text end as ss_low,
  case when sigstage_action is null then 'M' else sigstage_action::text end as ss_action,
  case when sigstage_bankfull is null then 'M' else sigstage_bankfull::text end as ss_bankfull,
@@ -39,15 +50,17 @@ icursor.execute("""
  case when sigstage_record is null then 'M' else sigstage_record::text end as ss_record
 from current_shef c JOIN stations s on (c.station = s.id) WHERE
  s.network in ('IA_DCP') and c.valid > now() - '4 hours'::interval
- and c.physical_code = 'HG' and c.duration = 'I' and c.extremum = 'Z'   
+ and c.physical_code in ('HG','HP', 'HT') and c.duration = 'I' and c.extremum = 'Z'   
 """ , () )
 
 for row in icursor:
     nwsli = row['station']
-    o.write("%5s %-64.64s %7.2f %7.2f %10.2f %10.10s %10.10s %10.10s %10.10s %10.10s %10.10s %10.10s\n" % (
-        row['station'], row['name'], row['lat'], row['lon'], row['value'],
+    o.write("%5s %-64.64s %s %s %7.2f %7.2f %10.2f %10.10s %10.10s %10.10s %10.10s %10.10s %10.10s %10.10s %10.10s\n" % (
+        row['station'], row['name'], row['valid'].day, row['valid'].strftime("%H%M"),
+        row['lat'], row['lon'], row['value'],
         row['ss_low'],row['ss_action'],row['ss_bankfull'],row['ss_flood'],
-        row['ss_moderate'],row['ss_major'],row['ss_record'],))
+        row['ss_moderate'],row['ss_major'],row['ss_record'],
+        compute_text(row) ))
 
 o.close()
 
