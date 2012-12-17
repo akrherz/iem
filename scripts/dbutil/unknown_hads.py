02ee96d06c439c46258392ad9e316b39c33e5d45
akrherz <akrherz@iastate.edu>
2012-12-17 08:11:05 -0600

    s/id/nwsli/

--- scripts/dbutil/unknown_hads.py
+++ scripts/dbutil/unknown_hads.py
@@ -15,23 +15,25 @@ mcursor = MESOSITE.cursor()
 hcursor.execute("""SELECT distinct nwsli, network from unknown 
     WHERE network != '' and network is not null""")
 for row in hcursor:
-    id = row[0]
+    nwsli = row[0]
     network = row[1]
     mcursor.execute("""
   SELECT online from stations where network = '%s' and id = '%s'
-      """ % (network, id))
+      """ % (network, nwsli))
     row = mcursor.fetchone()
     if row is None:
         continue
     elif row[0] == False:
-        print 'Site %s [%s] was unknown, but is in mesosite' % (id, network)
+        print 'Site %s [%s] was unknown, but is in mesosite' % (nwsli, network)
         mcursor.execute(""" 
           update stations SET online = 't' where network = '%s' and id = '%s'
-        """ % (network, id))
-        hcursor2.execute("""DELETE from unknown where nwsli = '%s' and network = '%s'""" % (id, network))
+        """ % (network, nwsli))
+        hcursor2.execute("""DELETE from unknown where nwsli = '%s' and network = '%s'""" % (
+                                                            nwsli, network))
     else:
-        print 'Site %s [%s] was unknown, but online in DB?' % (id, network)
-        hcursor2.execute("""DELETE from unknown where nwsli = '%s' and network = '%s'""" % (id, network))
+        print 'Site %s [%s] was unknown, but online in DB?' % (nwsli, network)
+        hcursor2.execute("""DELETE from unknown where nwsli = '%s' and network = '%s'""" % (
+                                                    nwsli, network))
  
 hcursor2.close()
 HADS.commit()
