8f346b03eb13bd5671cf862466657a01304def50
akrherz <akrherz@iastate.edu>
2012-12-24 06:16:53 -0600

    cleanup and close the db cursor

--- scripts/qc/check_vtec_eventids.py
+++ scripts/qc/check_vtec_eventids.py
@@ -2,8 +2,6 @@
 My purpose in life is to daily check the VTEC database and see if there are
 any IDs that are missing.  daryl then follows up with the weather bureau
 reporting anything he finds after an investigation.
-
-$Id: $:
 """
 import mx.DateTime
 import iemdb
@@ -32,57 +30,58 @@ sql = """SELECT wfo, min(eventid), max(eventid), phenomena,
        GROUP by wfo, phenomena, significance""" % (YEAR, )
 pcursor.execute( sql )
 for row in pcursor:
-  sEvent = row[1]
-  eEvent = row[2]
-  phenomena = row[3]
-  wfo = row[0]
-  sig = row[4]
-  for e in range(1,sEvent):
-    lookup = "%s.%s.%s.%s" % (wfo, phenomena, sig, e)
-    if lookup not in missing:
-      add_missing(wfo, phenomena, sig, e)
-      print "Warning Missing WFO: %s PHENOMENA: %s EVENTID: %s" % (wfo, 
+    sEvent = row[1]
+    eEvent = row[2]
+    phenomena = row[3]
+    wfo = row[0]
+    sig = row[4]
+    for e in range(1,sEvent):
+        lookup = "%s.%s.%s.%s" % (wfo, phenomena, sig, e)
+        if lookup not in missing:
+            add_missing(wfo, phenomena, sig, e)
+            print "Warning Missing WFO: %s PHENOMENA: %s EVENTID: %s" % (wfo, 
                                                             phenomena, e)
       
-  for eventid in range(sEvent, eEvent):
-    sql = """SELECT gtype, count(*) as c from warnings_%s WHERE wfo = '%s' 
+    for eventid in range(sEvent, eEvent):
+        sql = """SELECT gtype, count(*) as c from warnings_%s WHERE wfo = '%s' 
            and phenomena = '%s' and eventid = '%s' and significance = '%s' 
            GROUP by gtype""" % (YEAR, wfo, phenomena, eventid, sig)
-    pcursor2.execute( sql )
-    polyCount = 0
-    cntyCount = 0
-    for row2 in pcursor2:
-      if (row2[0] == "P"): polyCount = int(row2[1])
-      if (row2[0] == "C"): cntyCount = int(row2[1])
+        pcursor2.execute( sql )
+        polyCount = 0
+        cntyCount = 0
+        for row2 in pcursor2:
+            if (row2[0] == "P"): polyCount = int(row2[1])
+            if (row2[0] == "C"): cntyCount = int(row2[1])
 
-    if (cntyCount == 0 and polyCount == 0):
-      lookup = "%s.%s.%s.%s" % (wfo, phenomena, sig, eventid)
-      if lookup not in missing:
-        add_missing(wfo, phenomena, sig, eventid)
-        print "Warning Missing WFO: %s PHENOMENA: %s EVENTID: %s" % (wfo, 
+        if (cntyCount == 0 and polyCount == 0):
+            lookup = "%s.%s.%s.%s" % (wfo, phenomena, sig, eventid)
+        if lookup not in missing:
+            add_missing(wfo, phenomena, sig, eventid)
+            print "Warning Missing WFO: %s PHENOMENA: %s EVENTID: %s" % (wfo, 
                                                         phenomena, eventid)
-    elif (polyCount == 0):
-      print "SBW Missing     WFO: %s PHENOMENA: %s EVENTID: %s" % (wfo, 
+        elif (polyCount == 0):
+            print "SBW Missing     WFO: %s PHENOMENA: %s EVENTID: %s" % (wfo, 
                                                         phenomena, eventid)
-    elif (cntyCount == 0):
-      print "County Missing  WFO: %s PHENOMENA: %s EVENTID: %s" % (wfo, 
+        elif (cntyCount == 0):
+            print "County Missing  WFO: %s PHENOMENA: %s EVENTID: %s" % (wfo, 
                                                         phenomena, eventid)
-    elif (polyCount > 1):
-      print "Duplicate SBW   WFO: %s PHENOMENA: %s EVENTID: %s" % (wfo, 
+        elif (polyCount > 1):
+            print "Duplicate SBW   WFO: %s PHENOMENA: %s EVENTID: %s" % (wfo, 
                                                         phenomena, eventid)
-      sql = """DELETE from warnings_%s WHERE oid IN (
+            sql = """DELETE from warnings_%s WHERE oid IN (
          SELECT max(oid) as m from warnings_%s WHERE wfo = '%s' 
          and phenomena = '%s' and eventid = '%s' and significance = '%s' 
          and gtype = 'P')""" % (mx.DateTime.now().year, YEAR,
           wfo, phenomena, eventid, sig)
-      pcursor2.execute( sql )
-      sql = """DELETE from warnings_%s WHERE oid IN (
+            pcursor2.execute( sql )
+            sql = """DELETE from warnings_%s WHERE oid IN (
               select m from (
 select ugc,  max(oid) as m, count(oid) as c from warnings_%s WHERE wfo = '%s' 
 and phenomena = '%s' and eventid = '%s' and significance = '%s' 
 and gtype = 'C' GROUP by ugc) as foo WHERE c > 1)""" % (YEAR, YEAR,
           wfo, phenomena, eventid, sig)
-      pcursor2.execute( sql )
+            pcursor2.execute( sql )
       
+pcursor2.close()
 POSTGIS.commit()
 POSTGIS.close()
