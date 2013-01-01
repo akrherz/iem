a381b948858cd127888993814b86d5c03986f297
akrherz <akrherz@iastate.edu>
2013-01-01 06:20:26 -0600

    account for nulls

--- scripts/current/today_precip.py
+++ scripts/current/today_precip.py
@@ -18,7 +18,7 @@ icursor = IEM.cursor()
 sql = """
 select s.id, s.network,
   x(s.geom) as lon, y(s.geom) as lat, 
-  (case when c.pday < 0 then 0 else c.pday end) as rainfall
+  (case when c.pday < 0 or c.day is null then 0 else c.pday end) as rainfall
  from summary_%s c, current c2, stations s
  WHERE s.iemid = c2.iemid and c2.iemid = c.iemid and 
  c2.valid > (now() - '2 hours'::interval)
