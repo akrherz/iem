40ec91cb21c07800b4c9f95851fc540f2c4b64b2
akrherz <akrherz@iastate.edu>
2013-01-23 11:32:06 -0600

    be specific about timestamps and define gusts

--- include/iemaccessob.php
+++ include/iemaccessob.php
@@ -12,8 +12,11 @@ class IEMAccessOb {
     if ($row["max_gust_ts"] != ""){
       $this->db["gust_ts"] = strtotime(substr($row["max_gust_ts"],0,16));
       $this->db["lgust_ts"] = strtotime(substr($row["lmax_gust_ts"],0,16));
+    } else{
+    	$this->db["gust_ts"] = "";
+    	$this->db["lgust_ts"] = "";
     }
-    $this->db["obtime"] = strftime("%I:%M %p", $this->db["ts"]);
+    $this->db["obtime"] = strftime("%d %b %I:%M %p", $this->db["ts"]);
     if ($this->db["max_tmpf"] == -99){
     	$this->db["max_tmpf"] = null;
     }
