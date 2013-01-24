5685f820e5c6a8429db159dd77857ada5ecb8f25
akrherz <akrherz@iastate.edu>
2013-01-24 13:00:15 -0600

    remove /mesonet/python usage

--- cgi-bin/precip/extractPrecip.py
+++ cgi-bin/precip/extractPrecip.py
@@ -1,10 +1,11 @@
-#!/mesonet/python/bin/python
-# Wiz bang to extract precip for a given hrap_i and year
-# Daryl Herzmann 22 Jun 2004
-# 9 Feb 2004  We need to finish this bad boy!
+#!/usr/bin/env python
 
-from Scientific.IO import NetCDF
-import sys, mx.DateTime, cgi, datetime, os, Numeric
+import netCDF4
+import datetime
+import sys
+import cgi
+import os
+import numpy
 
 print 'Content-type: text/plain \n'
 
@@ -12,41 +13,43 @@ form = cgi.FormContent()
 
 hrap_i = 20147
 if (form.has_key("hrap")):
-  hrap_i = int(form["hrap"][0])
+    hrap_i = int(form["hrap"][0])
 
 if (form.has_key("syear") and form.has_key("smonth") and form.has_key("sday")):
-  sts = mx.DateTime.DateTime(int(form["syear"][0]), int(form["smonth"][0]), \
-    int(form["sday"][0]) )
+    sts = datetime.datetime(int(form["syear"][0]), int(form["smonth"][0]), 
+                            int(form["sday"][0]) )
 else:
-  sts = mx.DateTime.now() + mx.DateTime.RelativeDateTime(days=-1,hour=0,minute=0)
+    sts = datetime.datetime.now() - datetime.timedelta(days=1)
+    sts = sts.replace(hour=0,minute=0)
 
 if (form.has_key("eyear") and form.has_key("emonth") and form.has_key("eday")):
-  ets = mx.DateTime.DateTime(int(form["eyear"][0]), int(form["emonth"][0]), \
-    int(form["eday"][0]) )
+    ets = datetime.datetime(int(form["eyear"][0]), int(form["emonth"][0]), \
+                               int(form["eday"][0]) )
 else:
-  ets = mx.DateTime.now() + mx.DateTime.RelativeDateTime(hour=0,minute=0)
+    ets = datetime.datetime.now()
+    ets = ets.replace(hour=0,minute=0)
 
 gx = (hrap_i - 1) % 173
 gy = (hrap_i - 1) / 173
 year = 2004
 
-interval = mx.DateTime.RelativeDateTime(days=+1)
+interval = datetime.timedelta(days=1)
 
 now = sts 
-while (now < ets):
-  # Get GMT rep
-  gmt = datetime.datetime.utcfromtimestamp( now.ticks() )
-  fp = gmt.strftime("/wepp/data/rainfall/netcdf/daily/%Y/%m/%Y%m%d_rain.nc")
-  if (os.path.isfile(fp)):
-    nc = NetCDF.NetCDFFile(fp, 'r')
-    p = nc.variables["rainfall_15min"]
-  else:
-    p = Numeric.zeros( (96,134,173), Numeric.Float )
-  for i in range(96):
-    minutes = (i + 1) * 15
-    print "%s %s" % \
- ((now + mx.DateTime.RelativeDateTime(minutes=minutes)).strftime("%Y-%m-%d %H:%M"), p[i][gy][gx] )
-  del(p)
-  if (os.path.isfile(fp)):
-    del(nc)
-  now += interval
+while now < ets:
+    # Get GMT rep
+    fp = now.strftime("/wepp/data/rainfall/netcdf/daily/%Y/%m/%Y%m%d_rain.nc")
+    if (os.path.isfile(fp)):
+        nc = netCDF4.Dataset(fp, 'r')
+        p = nc.variables["rainfall_15min"]
+    else:
+        p = numpy.zeros( (96,134,173), 'f' )
+    for i in range(96):
+        minutes = (i + 1) * 15
+        print "%s %s" % (
+                (now + datetime.timedelta(seconds=minutes*60)).strftime(
+                                "%Y-%m-%d %H:%M"), p[i][gy][gx] )
+    del(p)
+    if (os.path.isfile(fp)):
+        del(nc)
+    now += interval
--- cgi-bin/precip/snetRates.py
+++ cgi-bin/precip/snetRates.py
@@ -1,112 +1,119 @@
-#!/mesonet/python/bin/python
+#!/usr/bin/env python
 
-import  mx.DateTime, cgi, sys
-from pyIEM import iemdb
-i = iemdb.iemdb()
-mydb = i['snet']
-import pg
-iem = pg.connect('iem', 'iemdb', user='nobody')
+import datetime
+import cgi
+import sys
+import psycopg2.extras
+import pytz
 
 def diff(nowVal, pastVal, mulli):
-  if (nowVal < 0 or pastVal < 0): return "%5s," % ("M")
-  differ = nowVal - pastVal
-  if differ < 0: return "%5s," % ("M")
+    if nowVal < 0 or pastVal < 0:
+        return "%5s," % ("M")
+    differ = nowVal - pastVal
+    if differ < 0:
+        return "%5s," % ("M")
 
-  return "%5.2f," % (differ * mulli)
+    return "%5.2f," % (differ * mulli)
 
 def Main():
-  form = cgi.FormContent()
-  year = form["year"][0]
-  month = form["month"][0]
-  day = form["day"][0]
-  station = form["station"][0][:5]
-  s = mx.DateTime.DateTime(int(year), int(month), int(day))
-  e = s + mx.DateTime.RelativeDateTime(days=+1)
-  interval = mx.DateTime.RelativeDateTime(minutes=+1)
+    form = cgi.FormContent()
+    year = form["year"][0]
+    month = form["month"][0]
+    day = form["day"][0]
+    station = form["station"][0][:5]
+    s = datetime.datetime(int(year), int(month), int(day))
+    s = s.replace(tzinfo=pytz.timezone("America/Chicago"))
+    e = s + datetime.timedelta(days=1)
+    e = e.replace(tzinfo=pytz.timezone("America/Chicago"))
+    interval = datetime.timedelta(seconds=60)
 
-  print 'Content-type: text/plain\n\n'
-  print "SID  ,  DATE    ,TIME ,PCOUNT,P1MIN ,60min ,30min ,20min ,15min ,10min , 5min , 1min ,"
-  rs = mydb.query("SELECT station, valid, pday from t%s WHERE \
-    station = '%s' and date(valid) = '%s' ORDER by valid ASC" \
-    % (s.strftime("%Y_%m"), station, s.strftime("%Y-%m-%d") ) ).dictresult()
+    print 'Content-type: text/plain\n\n'
+    print "SID  ,  DATE    ,TIME ,PCOUNT,P1MIN ,60min ,30min ,20min ,15min ,10min , 5min , 1min ,"
 
-  if (s.strftime("%Y%m%d") == mx.DateTime.now().strftime("%Y%m%d")):
-    rs = iem.query("SELECT station, valid, pday from current_log WHERE \
-    station = '%s' and date(valid) = '%s' ORDER by valid ASC" \
-    % ( station, s.strftime("%Y-%m-%d") ) ).dictresult()
-  else:
-    rs = mydb.query("SELECT station, valid, pday from t%s WHERE \
-    station = '%s' and date(valid) = '%s' ORDER by valid ASC" \
-    % (s.strftime("%Y_%m"), station, s.strftime("%Y-%m-%d") ) ).dictresult()
-
-
-  pcpn = [-1]*(24*60)
-
-  if (len(rs) == 0):
-    print 'NO RESULTS FOUND FOR THIS DATE!'
-    sys.exit(0)
-
-  lminutes = 0
-  lval = 0
-  for i in range(len(rs)):
-    ts = mx.DateTime.strptime(rs[i]['valid'][:16], "%Y-%m-%d %H:%M")
-    minutes  = int((ts - s).minutes)
-    val = float(rs[i]['pday'])
-    pcpn[minutes] = val
-    if ((val - lval) < 0.02):
-      for b in range(lminutes, minutes):
-        pcpn[b] = val
-    lminutes = minutes
-    lval = val
-
-  for i in range(len(pcpn)):
-    ts = s + (interval * i)
-    print "%s,%s," % (rs[0]['station'], ts.strftime("%Y-%m-%d,%H:%M") ),
-    if (pcpn[i] < 0): 
-      print "%5s," % ("M"),
+    if s.strftime("%Y%m%d") == datetime.datetime.now().strftime("%Y%m%d"):
+        IEM = psycopg2.connect("dbname=iem host=iemdb user=nobody")
+        cursor = IEM.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        cursor.execute("""SELECT s.id as station, valid, pday from current_log c JOIN
+        stations s on (s.iemid = c.iemid) WHERE 
+            s.id = %s and valid >= %s and valid < %s ORDER by valid ASC""",
+             ( station, s, e ) )
     else:
-      print "%5.2f," % (pcpn[i],),
+        SNET = psycopg2.connect("dbname=snet host=iemdb user=nobody")
+        cursor = SNET.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
-    if (i >= 1):
-      print diff(pcpn[i], pcpn[i-1], 1),
-    else:
-      print "%5s," % (" "),
+        cursor.execute("""SELECT station, valid, pday from t"""+
+                       s.strftime("%Y_%m") +""" WHERE 
+            station = %s and valid >= %s and valid < %s ORDER by valid ASC""", 
+            (station, s, e ) )
 
-    if (i >= 60):
-      print diff(pcpn[i], pcpn[i-60], 1),
-    else:
-      print "%5s," % (" "),
 
-    if (i >= 30):
-      print diff(pcpn[i], pcpn[i-30], 2),
-    else:
-      print "%5s," % (" "),
+    pcpn = [-1]*(24*60)
 
-    if (i >= 20):
-      print diff(pcpn[i], pcpn[i-20], 3),
-    else:
-      print "%5s," % (" "),
+    if cursor.rowcount == 0:
+        print 'NO RESULTS FOUND FOR THIS DATE!'
+        sys.exit(0)
 
-    if (i >= 15):
-      print diff(pcpn[i], pcpn[i-15], 4),
-    else:
-      print "%5s," % (" "),
+    lminutes = 0
+    lval = 0
+    for row in cursor:
+        ts = row['valid']
+        minutes  = (ts - s).seconds / 60
+        val = float(row['pday'])
+        pcpn[minutes] = val
+        if ((val - lval) < 0.02):
+            for b in range(lminutes, minutes):
+                pcpn[b] = val
+        lminutes = minutes
+        lval = val
 
-    if (i >= 10):
-      print diff(pcpn[i], pcpn[i-10], 6),
-    else:
-      print "%5s," % (" "),
+    for i in range(len(pcpn)):
+        ts = s + (interval * i)
+        print "%s,%s," % (station, ts.strftime("%Y-%m-%d,%H:%M") ),
+        if (pcpn[i] < 0): 
+            print "%5s," % ("M"),
+        else:
+            print "%5.2f," % (pcpn[i],),
 
-    if (i >= 5):
-      print diff(pcpn[i], pcpn[i-5], 12),
-    else:
-      print "%5s," % (" "),
+        if (i >= 1):
+            print diff(pcpn[i], pcpn[i-1], 1),
+        else:
+            print "%5s," % (" "),
 
-    if (i >= 1):
-      print diff(pcpn[i], pcpn[i-1], 60),
-    else:
-      print "%5s," % (" "),
+        if (i >= 60):
+            print diff(pcpn[i], pcpn[i-60], 1),
+        else:
+            print "%5s," % (" "),
 
-    print
+        if (i >= 30):
+            print diff(pcpn[i], pcpn[i-30], 2),
+        else:
+            print "%5s," % (" "),
+
+        if (i >= 20):
+            print diff(pcpn[i], pcpn[i-20], 3),
+        else:
+            print "%5s," % (" "),
+
+        if (i >= 15):
+            print diff(pcpn[i], pcpn[i-15], 4),
+        else:
+            print "%5s," % (" "),
+
+        if (i >= 10):
+            print diff(pcpn[i], pcpn[i-10], 6),
+        else:
+            print "%5s," % (" "),
+
+        if (i >= 5):
+            print diff(pcpn[i], pcpn[i-5], 12),
+        else:
+            print "%5s," % (" "),
+
+        if (i >= 1):
+            print diff(pcpn[i], pcpn[i-1], 60),
+        else:
+            print "%5s," % (" "),
+
+        print
 
 Main()
