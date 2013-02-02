f578958773bd0ed6e2872502db6024c207ce59d0
akrherz <akrherz@iastate.edu>
2013-02-02 15:12:50 -0600

    cleanups and make precip plot prettier

--- scripts/coop/today_precip.py
+++ scripts/coop/today_precip.py
@@ -1,11 +1,7 @@
 # Plot the COOP Precipitation Reports, don't use lame-o x100
 
-import sys, os, random
-sys.path.append("../lib/")
 import iemplot
-
-import mx.DateTime
-now = mx.DateTime.now()
+import datetime
 
 import iemdb
 import psycopg2.extras
@@ -16,10 +12,17 @@ sql = """
 select id, 
   x(s.geom) as lon, y(s.geom) as lat, 
   pday 
-from summary_%s c, stations s
+from summary c, stations s
 WHERE day = 'TODAY' and pday >= 0 and pday < 20
 and s.network = 'IA_COOP' and s.iemid = c.iemid
-""" % (now.year, )
+"""
+
+def n(val):
+    if val == 0.001:
+        return 'T'
+    if val == 0:
+        return '0'
+    return '%.2f' % (val,)
 
 lats = []
 lons = []
@@ -28,24 +31,25 @@ valmask = []
 labels = []
 icursor.execute(sql)
 for row in icursor:
-  lats.append( row['lat'] )
-  lons.append( row['lon'] )
-  vals.append( row['pday'] )
-  labels.append( row['id'] )
-  valmask.append( True )
+    lats.append( row['lat'] )
+    lons.append( row['lon'] )
+    vals.append( n(row['pday']) )
+    labels.append( row['id'] )
+    valmask.append( True )
 
 
 cfg = {
  'wkColorMap': 'BlAqGrYeOrRe',
  '_showvalues'        : True,
- '_format'            : '%.2f',
+ '_format'            : '%s',
  '_title'             : "Iowa COOP 24 Hour Precipitation",
- '_valid'             : "ending %s 7 AM" % (now.strftime("%d %b %Y"), ),
+ '_valid'             : "ending approximately %s 7 AM" % (
+                            datetime.datetime.now().strftime("%-d %b %Y"), ),
 # '_labels'            : labels
 }
 # Generates tmp.ps
 tmpfp = iemplot.simple_valplot(lons, lats, vals, cfg)
 
 pqstr = "plot ac %s iowa_coop_precip.png iowa_coop_precip.png png" % (
-        now.strftime("%Y%m%d%H%M"), )
+        datetime.datetime.now().strftime("%Y%m%d%H%M"), )
 iemplot.postprocess(tmpfp, pqstr)
