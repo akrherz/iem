39171167cd354197aaa8069e3ae43a52f4ecd286
akrherz <akrherz@iastate.edu>
2013-02-02 19:15:05 -0600

    improve ISUAG precip estimator

--- scripts/isuag/fix_precip.py
+++ scripts/isuag/fix_precip.py
@@ -4,16 +4,15 @@
 
 import sys
 import iemre
-import mx.DateTime
+import datetime
 import netCDF4
 import numpy
+import pytz
 import iemdb
 import psycopg2.extras
 ISUAG = iemdb.connect('isuag', bypass=True)
 icursor = ISUAG.cursor(cursor_factory=psycopg2.extras.DictCursor)
 icursor2 = ISUAG.cursor(cursor_factory=psycopg2.extras.DictCursor)
-MESOSITE = iemdb.connect('mesosite', bypass=True)
-mcursor = MESOSITE.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
 SKIP = ['A134759', # Lewis
         'A133259',
@@ -22,11 +21,8 @@ SKIP = ['A134759', # Lewis
         'A134309', #Kanawha
 ]
 
-sts = {}
-mcursor.execute("""SELECT id, x(geom) as lon, y(geom) as lat, name from 
-    stations WHERE network = 'ISUAG'""")
-for row in mcursor:
-    sts[ row['id'] ] = row
+import network
+nt = network.Table("ISUAG")
 
 def fix_daily(ts):
     """
@@ -39,7 +35,7 @@ def fix_daily(ts):
     for row in icursor:
         stid = row['station']
         if stid in SKIP:
-          continue
+            continue
         rain = row['rain']
         # Fix it
         sql = """UPDATE daily SET c90 = %.2f, c90_f = 'e' WHERE valid = '%s'
@@ -52,18 +48,19 @@ def fix_hourly(ts):
     """
     Fix the hourly precipitation values, just hard code the stupid IEMRE value
     """
-    nc = netCDF4.Dataset("/mnt/mesonet/data/iemre/%s_mw_hourly.nc" % (ts.year,),'r')
+    nc = netCDF4.Dataset("/mnt/mesonet/data/iemre/%s_mw_hourly.nc" % (
+                                                            ts.year,),'r')
     p01m = nc.variables['p01m']
-    offset = int((ts - (ts + mx.DateTime.RelativeDateTime(month=1,day=1,hour=0))).hours)
+    offset = iemre.hour_idx(ts)
     # Find ISUAG Data
     icursor.execute("SELECT * from hourly WHERE valid = '%s+00'" % (
                                                 ts.strftime("%Y-%m-%d"),))
     for row in icursor:
         stid = row['station']
         if stid in SKIP:
-          continue
-        lat = sts[ stid ]['lat']
-        lon = sts[ stid ]['lon']
+            continue
+        lat = nt.sts[ stid ]['lat']
+        lon = nt.sts[ stid ]['lon']
         # Lookup IEMRE data
         ix,jy = iemre.find_ij(lon, lat)
         estimate = 0.
@@ -74,7 +71,7 @@ def fix_hourly(ts):
             print "Missing Estimate", ts
         if row['c900'] > 0 or estimate > 0:
             print "%s %s %-20.20s Ob: %5.2f F: %s E: %5.2f" % (
-                  ts.strftime("%m%d/%H"), stid, sts[stid]['name'],
+                  ts.strftime("%m%d/%H"), stid, nt.sts[stid]['name'],
                   row['c900'], row['c900_f'], estimate)
         # Fix it
         sql = """UPDATE hourly SET c900 = %.2f, c900_f = 'e' 
@@ -86,13 +83,15 @@ def fix_hourly(ts):
 
 if __name__ == "__main__":
     if len(sys.argv) == 4:
-        ts = mx.DateTime.DateTime( int(sys.argv[1]),int(sys.argv[2]),
+        ts = datetime.datetime( int(sys.argv[1]),int(sys.argv[2]),
                            int(sys.argv[3]) )
     else:
-        ts = mx.DateTime.gmt() + mx.DateTime.RelativeDateTime(days=-1,hour=0,minute=0,second=0)
+        gmt = datetime.datetime.utcnow() - datetime.timedelta(days=1)
+        ts = gmt.replace(hour=0,minute=0,second=0,microsecond=0)
+    ts = ts.replace(tzinfo=pytz.timezone("America/Chicago"))
     for hr in range(24):
-        now = ts + mx.DateTime.RelativeDateTime(hours=hr)
-        fix_hourly( now.gmtime() )
+        now = ts.replace(hour=hr)
+        fix_hourly( ts.astimezone( pytz.timezone("UTC") ) )
     fix_daily(ts)
     icursor.close()
     icursor2.close()
