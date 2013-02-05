c9f8b990121e996fd5affdafd25631ddfc4def8d
akrherz <akrherz@iastate.edu>
2013-02-05 14:33:27 -0600

    Update to google maps v3 and customized openlayers install

--- htdocs/vtec/index.php
+++ htdocs/vtec/index.php
@@ -34,7 +34,7 @@ $HEADEXTRA = '<meta property="og:image" content="/images/logo_small.png"/>
 <link rel="stylesheet" type="text/css" href="../ext/ux/form/Spinner.css"/>
 <script type="text/javascript" src="http://extjs.cachefly.net/ext-3.4.0/adapter/ext/ext-base.js"></script>
 <script type="text/javascript" src="http://extjs.cachefly.net/ext-3.4.0/ext-all.js"></script>
-<script src="http://maps.google.com/maps?file=api&amp;v=2.x&amp;key='. $GOOGLEKEYS[$rooturl]["vtec"] .'" type="text/javascript"></script>
+<script src="https://maps.googleapis.com/maps/api/js?sensor=false" type="text/javascript"></script>
 <script src="OpenLayers.js"></script>';
 
 if (isset($_REQUEST["devel"])){
