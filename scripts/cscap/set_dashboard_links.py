3127042beadb4d04f74745118f7521eb4257c152
akrherz <akrherz@iastate.edu>
2013-02-07 08:04:31 -0600

    generalize some more, support SOIL2

--- scripts/cscap/set_dashboard_links.py
+++ scripts/cscap/set_dashboard_links.py
@@ -14,7 +14,7 @@ docs_client = util.get_docs_client(config)
 
 # Go get row 1
 cell_feed = spr_client.get_cells( config.get('cscap', 'dashboard'), 
-                                          'od6', query=gdata.spreadsheets.client.CellQuery(
+                            'od6', query=gdata.spreadsheets.client.CellQuery(
                                                     min_row=1, max_row=1))
 column_ids = [""] * 100
 for entry in cell_feed.entry:
@@ -22,48 +22,62 @@ for entry in cell_feed.entry:
     text = entry.cell.input_value
     column_ids[ int(entry.cell.col) ] = text
 
+lookuprefs = {
+              'soil2': 'Soil Bulk Density and Water Retention Data',
+              }
+varconv = {
+           'soil2': 'waterretentionat0bar',
+           }
+
 
 def do_row(row):
     cell_feed = spr_client.get_cells( config.get('cscap', 'dashboard'), 
-                                              'od6', query=gdata.spreadsheets.client.CellQuery(
-                                                        min_row=row, max_row=row))
+                        'od6', query=gdata.spreadsheets.client.CellQuery(
+                                        min_row=row, max_row=row))
+    firstcolumn = cell_feed.entry[0]
+    varname = firstcolumn.cell.input_value.split()[0].lower()
+    spreadtitle = lookuprefs.get(varname)
+    if spreadtitle is None:
+        print 'ERROR: Do not know how to reference %s in lookuprefs' % (
+                                                                varname,)
+        return
     
-    
-    for entry in cell_feed.entry:
-        if int(entry.cell.col) == 1:
-            varname = entry.cell.input_value.split()[0].lower()
-            print 'Gonna be looking for variable %s row %s' % (varname, row)
-        if int(entry.cell.col) < 2:
-            continue
-        heading = column_ids[ int(entry.cell.col) ]
+    for entry in cell_feed.entry[1:]:
+        siteid = column_ids[ int(entry.cell.col) ]
     
         query = gdata.docs.client.DocsQuery(show_collections='true', 
-                                title='%s Soil Texture Data' % (heading,))
+                                title='%s %s' % (siteid, spreadtitle))
     
     
-        # We need to go search for the spreadsheet?
+        # We need to go search for the spreadsheet
         resources = docs_client.GetAllResources(query=query)
         if len(resources) == 0:
-            print 'No find!', heading
+            print 'Can not find spread title: |%s %s|' % (siteid, spreadtitle,)
             continue
         if len(resources) == 2:
-            print 'Duplicate', heading
+            print 'Duplicate spread title: |%s %s|' % (siteid, spreadtitle,)
             for res in resources:
-                print heading, res.title.text, res.get_html_link().href
+                print siteid, res.title.text, res.get_html_link().href
             continue
     
         
         # Get the list feed for this spreadsheet
-        list_feed = spr_client.get_list_feed( resources[0].get_id().split("/")[-1][14:], 'od7')
+        list_feed = spr_client.get_list_feed( 
+                        resources[0].get_id().split("/")[-1][14:], 'od7')
         misses = 0
         na = False
+        lookupcol = varconv.get(varname, varname)
         for entry2 in list_feed.entry:
             data = entry2.to_dict()
-            if not data.has_key(varname):
+            if not data.has_key(lookupcol):
                 na = True
                 break
-            if data[varname] is None:
+            if data[lookupcol] is None:
                 misses += 1
+    
+        if na:
+            print 'Could not find header: %s in spreadtitle: %s %s' % (lookupcol,
+                                                            siteid, spreadtitle)
     
         uri = resources[0].get_html_link().href
         if na:
@@ -74,5 +88,5 @@ def do_row(row):
             entry.cell.input_value = '=hyperlink("%s", "Entry")' % (uri,)    
         spr_client.update(entry)
         
-for i in range(43,44):
+for i in range(40,41):
     do_row(i)
