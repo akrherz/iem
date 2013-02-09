9012ccda2a9d6d437c5bfda6af319869e3c0204b
akrherz <akrherz@iastate.edu>
2013-02-09 10:49:35 -0600

    refactor and make it process more stored variables

--- scripts/ingestors/asos/wunder_ingest.py
+++ scripts/ingestors/asos/wunder_ingest.py
@@ -1,297 +1,269 @@
-# Leach Wunderground's archive
+"""
+ This script downloads the METAR data provided by wunderground, another script
+ than compares it with my current archive and that provided by NSSL to see 
+ if there are any differences.
 
-import urllib2, cookielib
-from pyIEM import iemdb, mesonet
-i = iemdb.iemdb()
-asos = i['asos']
-mesosite = i['mesosite']
-import mx.DateTime, csv, time
+Arguments
+    python wunder_ingest.py --network=IA_ASOS 1978 1979
+    python wunder_ingest.py --station=AMW 1978 1979
+
+"""
+import urllib2
+import cookielib
+import datetime
+import time
 import sys
 import os
+import pytz
+from optparse import OptionParser
 from metar import Metar
-"""
-    EGI  2008-12-18 21:55
-"""
+import iemdb
+ASOS = iemdb.connect('asos')
+acursor = ASOS.cursor()
 
-cj = cookielib.CookieJar()
-opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-r = opener.open("http://www.wunderground.com/cgi-bin/findweather/getForecast?setpref=SHOWMETAR&value=1").read()
+class OB(object):
+    station = None
+    valid = None
+    tmpf = None
+    dwpf = None
+    drct = None
+    sknt = None
+    alti = None
+    gust = None
+    vsby = None
+    skyc1 = None
+    skyc2 = None
+    skyc3 = None
+    skyc4 = None
+    skyl1 = None
+    skyl2 = None
+    skyl3 = None
+    metar = None
+    skyl4 = None
+    p03i = None 
+    p06i = None
+    p24i = None
+    max_tmpf_6hr = None 
+    min_tmpf_6hr = None 
+    max_tmpf_24hr = None 
+    min_tmpf_24hr = None
+    mslp = None
+    p01i = None 
+    presentwx = None
 
-def doit(stid, now):
-  metarPass = 0
-  metarFail = 0
-  processed = 0
-  sqlFail = 0
-  if len(stid) == 3:
-    faa = "K%s" % (stid,)
-  else:
-    faa = stid
-  url = "http://www.wunderground.com/history/airport/%s/%s/%-i/%-i/DailyHistory.html?req_city=NA&req_state=NA&req_statename=NA&format=1" % (faa, now.year, now.month, now.day)
-  try:
-    data = opener.open(url).read()
-  except:
-    print "Fail STID: %s NOW: %s" % (stid, now)
-    return 0
-  # Save raw data, since I am an idiot have of the time
-  dir = "cache/%s/%s/" % (stid, now.year)
-  if not os.path.isdir(dir):
-    os.makedirs(dir)
-  fp = "%s%s.txt" % (dir, now.strftime("%Y%m%d"), )
-  o = open(fp, 'w')
-  o.write(data)
-  o.close()
-  
-  lines = data.split("\n")
-  headers = {}
-  for line in lines: # Skip header
-    line = line.replace("<br />", "")
-    if line.strip() == "":
-      continue
-    tokens = line.split(",")
-    if len(tokens) < 3:
-      continue
-    if len(headers.keys()) == 0:
-      for i in range(len(tokens)):
-        headers[ tokens[i] ] = i
-      continue
-  
-    mtr = None
-    if headers.has_key("FullMetar"):
-      mstr = (tokens[headers["FullMetar"]]).strip().replace("'","").replace("SPECI ","").replace("METAR ","")
-      try:
-        mtr = Metar.Metar(mstr, now.month, now.year)
-      except:
-        pass
-    sky = {'skyc1': "", 'skyc2': "", 'skyc3': "", 'skyc4': "",
-           'skyl1': "Null", 'skyl2': "Null", 'skyl3': "Null", 'skyl4': "Null"}
-    if mtr is not None and mtr.time is not None:
-      metarPass += 1
-      gts = mx.DateTime.DateTime( mtr.time.year, mtr.time.month, 
-                mtr.time.day, mtr.time.hour, mtr.time.minute)
-      # When processing data on the last day of the month, we get GMT times
-      # for the next month, need to account for that
-      if gts.day == 1 and now.day > 10:
-          gts += mx.DateTime.RelativeDateTime(months=1)
-      #print now, gts
-      tmpf = "Null"
-      if (mtr.temp):
-        tmpf = mtr.temp.value("F")
-      dwpf = "Null"
-      if (mtr.dewpt):
-        dwpf = mtr.dewpt.value("F")
-
-      sknt = "Null"
-      if mtr.wind_speed:
-        sknt = mtr.wind_speed.value("KT")
-      gust = "Null"
-      if mtr.wind_gust:
-        gust = mtr.wind_gust.value("KT")
-
-      drct = "Null"
-      if mtr.wind_dir and mtr.wind_dir.value() != "VRB":
-        drct = mtr.wind_dir.value()
-
-      vsby = "Null"
-      if mtr.vis:
-        vsby = mtr.vis.value("SM")
-
-      alti = "Null"
-      if mtr.press:
-        alti = mtr.press.value("IN")
-
-      p01m = 0
-      if mtr.precip_1hr:
-        p01m = mtr.precip_1hr.value("CM") * 10.0
-
-      # Do something with sky coverage
-      for i in range(len(mtr.sky)):
-        (c,h,b) = mtr.sky[i]
-        sky['skyc%s' % (i+1)] = c
-        if h is not None:
-          sky['skyl%s' % (i+1)] = h.value("FT")
-
+def get_job_list():
+    """ Figure out the days and stations we need to get """
+    days = []
+    stations = []
+    
+    parser = OptionParser()
+    parser.add_option("-n", "--network", dest="network",
+                  help="IEM network", metavar="NETWORK")
+    parser.add_option("-s", "--station", dest="station",
+                  help="IEM station", metavar="STATION")
+    (options, args) = parser.parse_args()
+    now = datetime.date(int(args[0]),1,1)
+    ets = datetime.date(int(args[1]),1,1)
+    while now < ets:
+        days.append( now )
+        now += datetime.timedelta(days=1)
+    if options.network is not None:
+        sql = """SELECT id from stations where network = %s ORDER by id ASC"""
+        acursor.execute(sql, (options.network,) )
+        for row in acursor:
+            stations.append( row[0] )
     else:
-      metarFail += 1
-      tmpf = tokens[ headers['TemperatureF'] ]
-      addon = 0
-      if headers.has_key('TimeCST'):
-        tstr = tokens[ headers['TimeCST'] ]
-      elif headers.has_key('TimeCDT'):
-        tstr = tokens[ headers['TimeCDT'] ]
-      elif headers.has_key('TimeEDT'):
-        tstr = tokens[ headers['TimeEDT'] ]
-        addon = mx.DateTime.RelativeDateTime(hours=-1)
-      elif headers.has_key('TimeEST'):
-        tstr = tokens[ headers['TimeEST'] ]
-        addon = mx.DateTime.RelativeDateTime(hours=-1)
-      elif headers.has_key('TimeMST'):
-        tstr = tokens[ headers['TimeMST'] ]
-        addon = mx.DateTime.RelativeDateTime(hours=1)
-      elif headers.has_key('TimeMDT'):
-        tstr = tokens[ headers['TimeMDT'] ]
-        addon = mx.DateTime.RelativeDateTime(hours=1)
-      elif headers.has_key('TimePST'):
-        tstr = tokens[ headers['TimePST'] ]
-        addon = mx.DateTime.RelativeDateTime(hours=2)
-      elif headers.has_key('TimePDT'):
-        tstr = tokens[ headers['TimePDT'] ]
-        addon = mx.DateTime.RelativeDateTime(hours=2)
-      elif headers.has_key('TimeAKST'):
-        tstr = tokens[ headers['TimeAKST'] ]
-        addon = mx.DateTime.RelativeDateTime(hours=3)
-      elif headers.has_key('TimeAKDT'):
-        tstr = tokens[ headers['TimeAKDT'] ]
-        addon = mx.DateTime.RelativeDateTime(hours=3)
+        stations.append( options.station )
+        
 
-      elif headers.has_key('TimeAHST'):
-        tstr = tokens[ headers['TimeAHST'] ]
-        addon = mx.DateTime.RelativeDateTime(hours=4)
-      elif headers.has_key('TimeAHDT'):
-        tstr = tokens[ headers['TimeAHDT'] ]
-        addon = mx.DateTime.RelativeDateTime(hours=4)
-        
-      elif headers.has_key('TimeCAT'):
-        tstr = tokens[ headers['TimeCAT'] ]
-        addon = mx.DateTime.RelativeDateTime(hours=5)
-      elif headers.has_key('TimeCAT'):
-        tstr = tokens[ headers['TimeCAT'] ]
-        addon = mx.DateTime.RelativeDateTime(hours=5)
-        
-      elif headers.has_key('TimeHADT'):
-        tstr = tokens[ headers['TimeHADT'] ]
-        addon = mx.DateTime.RelativeDateTime(hours=4)
-      elif headers.has_key('TimeYST'):
-        tstr = tokens[ headers['TimeYST'] ]
-        addon = mx.DateTime.RelativeDateTime(hours=3)
-      elif headers.has_key('TimeBST'): # Not sure
-        tstr = tokens[ headers['TimeBST'] ]
-        addon = mx.DateTime.RelativeDateTime(hours=3)
-        
-      else:
-        print 'Unknown TimeZone!', headers.keys()
-        continue
-      ts = mx.DateTime.strptime( '%s %s' % (now.strftime('%Y-%m-%d'), 
-           tstr), '%Y-%m-%d %I:%M %p' ) + addon
-      gts = ts.gmtime()
-      dwpf = tokens[ headers['Dew PointF'] ]
-      alti = tokens[ headers['Sea Level PressureIn'] ]
-      vsby = tokens[ headers['VisibilityMPH'] ]
-      d = tokens[ headers['Wind Direction'] ]
-      if d in ["Calm","N/A","Variable"]:
-        drct = "Null"
-      else:
-        drct = mesonet.txt2drct[ tokens[ headers['Wind Direction'] ] ]
-      s = tokens[ headers['Wind SpeedMPH'] ]
-      if s == "Calm" or s == "N/A":
-        sknt = 0
-      else:
-        sknt = float( s ) / 1.15
-      g = tokens[ headers['Gust SpeedMPH'] ]
-      if g == "-" or g.strip() == "":
-        gust = "Null"
-      else:
-        gust = float(g) * 1.15
-      p = tokens[ headers['PrecipitationIn'] ]
-      if p == "N/A" or p.strip() == "":
-        p01m = "Null"
-      else:
-        p01m = float(p) * 25.4
+    return stations, days
 
-    sql = """INSERT into t%s (station, valid, tmpf, dwpf, vsby, 
-        drct, sknt, gust, p01m, alti, skyc1, skyc2, skyc3, skyc4,
-        skyl1, skyl2, skyl3, skyl4, metar) values ('%s','%s+00', %s, %s, %s, 
-        %s, %s, %s, %s, %s, '%s', '%s', '%s','%s', %s, %s, %s,%s,'%s')""" % (now.year, 
-        stid, gts.strftime("%Y-%m-%d %H:%M"), tmpf, dwpf, vsby, drct, 
-        sknt, gust, p01m, alti, 
-        sky['skyc1'], sky['skyc2'], sky['skyc3'], sky['skyc4'],
-        sky['skyl1'], sky['skyl2'], sky['skyl3'], sky['skyl4'], mstr)
+def workflow():
+    """ Do some work """
+    stations, days = get_job_list()
+
+    # Set the show metar option
+    cj = cookielib.CookieJar()
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+    opener.open("http://www.wunderground.com/cgi-bin/findweather/getForecast?setpref=SHOWMETAR&value=1").read()
+
+    # Iterate 
+    for station in stations:
+        clear_data(station, days[0], days[-1])
+        total = 0
+        for day in days:
+            (processed, usedcache) = doit(opener, station, day)
+            total += processed
+            ASOS.commit()
+            if not usedcache:
+                time.sleep(0.5)
+        print "%s processed %s entries" % (station, total)
+            
+def clear_data(station, sts, ets):
+    acursor.execute("""DELETE from alldata WHERE station = %s and
+    valid BETWEEN %s and %s""", (station, sts, 
+                                 (ets + datetime.timedelta(days=1))))
+    ASOS.commit()
+
+def doit(opener, station, now):
+    """ Fetch! """
+    usedcache = False
+    processed = 0
+    
+    if len(station) == 3:
+        faa = "K%s" % (station,)
+    else:
+        faa = station
+        
+    mydir = "/mesonet/ARCHIVE/wunder/cache/%s/%s/" % (station, now.year)
+    if not os.path.isdir(mydir):
+        os.makedirs(mydir)
+  
+    fn = "%s%s.txt" % (mydir, now.strftime("%Y%m%d"), )
+    if os.path.isfile(fn):
+        usedcache = True
+        data = open(fn).read()
+        if len(data) < 140:
+            usedcache = False
+    if not usedcache:
+        url = "http://www.wunderground.com/history/airport/%s/%s/%-i/%-i/DailyHistory.html?req_city=NA&req_state=NA&req_statename=NA&format=1" % (faa, now.year, now.month, now.day)
+        try:
+            data = opener.open(url).read()
+        except KeyboardInterrupt:
+            sys.exit()
+        except:
+            print "Download Fail STID: %s NOW: %s" % (station, now)
+            return 0, False
+
+    # Save raw data, since I am an idiot have of the time
+    o = open(fn, 'w')
+    o.write(data)
+    o.close()
+  
+    lines = data.split("\n")
+    headers = None
+    for line in lines: 
+        line = line.replace("<br />", "").replace("\xff", "")
+        if line.strip() == "":
+            continue
+        tokens = line.split(",")
+        if len(tokens) < 3:
+            continue
+        if headers is None:
+            headers = {}
+            for i in range(len(tokens)):
+                headers[ tokens[i] ] = i
+            continue
+  
+        if headers.has_key("FullMetar"):
+            mstr = (tokens[headers["FullMetar"]]).strip().replace("'",
+                                "").replace("SPECI ","").replace("METAR ","")
+            ob = process_metar(mstr, now)
+            if ob is None:
+                continue
+
+            sql = """INSERT into t"""+ `ob.valid.year` +""" (station, valid, 
+            tmpf, dwpf, vsby, drct, sknt, gust, p01i, alti, skyc1, skyc2, 
+            skyc3, skyc4, skyl1, skyl2, skyl3, skyl4, metar, mslp, presentwx,
+            p03i, p06i, p24i, max_tmpf_6hr, max_tmpf_24hr, min_tmpf_6hr,
+            min_tmpf_24hr) 
+            values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,%s, 
+            %s, %s, %s,%s,%s, %s, %s, %s, %s, %s, %s, %s, %s)""" 
+            args = (station, ob.valid, ob.tmpf, ob.dwpf, ob.vsby, ob.drct,
+                    ob.sknt, ob.gust, ob.p01i, ob.alti, ob.skyc1, ob.skyc2,
+                    ob.skyc3, ob.skyc4, ob.skyl1, ob.skyl2, ob.skyl3,
+                    ob.skyl4, ob.metar, ob.mslp, ob.presentwx, ob.p03i,
+                    ob.p06i, ob.p24i, ob.max_tmpf_6hr, ob.max_tmpf_24hr,
+                    ob.min_tmpf_6hr, ob.min_tmpf_24hr) 
+
+            acursor.execute(sql, args)
+            processed += 1
+
+    return processed, usedcache
+
+            
+def process_metar(mstr, now):
     try:
-      asos.query(sql)
-      processed += 1
+        mtr = Metar.Metar(mstr, now.month, now.year)
     except:
-      sqlFail += 1
+        return None
+    if mtr is None or mtr.time is None:
+        return None
+    
+    ob = OB()
+    ob.metar = mstr
+    
+    gts = datetime.datetime( mtr.time.year, mtr.time.month, 
+                mtr.time.day, mtr.time.hour, mtr.time.minute)
+    gts = gts.replace(tzinfo=pytz.timezone("UTC"))
+    # When processing data on the last day of the month, we get GMT times
+    # for the first of this month
+    if gts.day == 1 and now.day > 10:
+        tm = now + datetime.timedelta(days=1)
+        gts = gts.replace(year=tm.year, month=tm.month, day=tm.day)
+      
+    ob.valid = gts
+      
+    if mtr.temp:
+        ob.tmpf = mtr.temp.value("F")
+    if (mtr.dewpt):
+        ob.dwpf = mtr.dewpt.value("F")
 
-  #print "%s %s MPASS: %s MFAIL: %s SQLFAIL: %s" % (stid, now, metarPass, 
-  #      metarFail, sqlFail)
-  return processed
+    if mtr.wind_speed:
+        ob.sknt = mtr.wind_speed.value("KT")
+    if mtr.wind_gust:
+        ob.gust = mtr.wind_gust.value("KT")
 
-def recover():
-    """
-    Account for a stupid processing error I did with not accounting for obs
-    at the end of the month
-    """
-    network = sys.argv[1]
-    rs = mesosite.query("SELECT * from stations WHERE network = '%s'  ORDER by id DESC" % (
-                                                                                          network,)).dictresult()
-    for i in range(len(rs)):
-        sid = rs[i]['id']
-        # Delete all obs from the last day of the month, sigh
-        asos.query("""DELETE from alldata where station = '%s' and
-        (
-        (extract(month from valid) = 1 and extract(day from valid) = 31) or
-        (extract(month from valid) = 2 and extract(day from valid) = 28) or
-        (extract(month from valid) = 2 and extract(day from valid) = 29) or
-        (extract(month from valid) = 3 and extract(day from valid) = 31) or
-        (extract(month from valid) = 4 and extract(day from valid) = 30) or
-        (extract(month from valid) = 5 and extract(day from valid) = 31) or
-        (extract(month from valid) = 6 and extract(day from valid) = 30) or
-        (extract(month from valid) = 7 and extract(day from valid) = 31) or
-        (extract(month from valid) = 8 and extract(day from valid) = 31) or
-        (extract(month from valid) = 9 and extract(day from valid) = 30) or
-        (extract(month from valid) = 10 and extract(day from valid) = 31) or
-        (extract(month from valid) = 11 and extract(day from valid) = 30) or
-        (extract(month from valid) = 12 and extract(day from valid) = 31)
-        ) and valid < '2010-02-15'
-        """ % (sid,))
-        now = mx.DateTime.DateTime(1948,1,1)
-        ets = mx.DateTime.DateTime(2010,3,1)
-        interval = mx.DateTime.RelativeDateTime(months=1)
-        while now < ets:
-            ts = now - mx.DateTime.RelativeDateTime(days=1)
-            obs = doit(sid, ts)
-            #print sid, ts, obs
-            now += interval
+    if mtr.wind_dir and mtr.wind_dir.value() != "VRB":
+        ob.drct = mtr.wind_dir.value()
+
+    if mtr.vis:
+        ob.vsby = mtr.vis.value("SM")
+
+    if mtr.press:
+        ob.alti = mtr.press.value("IN")
+      
+    if mtr.press_sea_level:
+        ob.mslp = mtr.press_sea_level.value("MB")
+
+    if mtr.precip_1hr:
+        ob.p01i = mtr.precip_1hr.value("IN")
+
+    # Do something with sky coverage
+    for i in range(len(mtr.sky)):
+        (c,h,b) = mtr.sky[i]
+        setattr(ob, 'skyc%s' % (i+1), c)
+        if h is not None:
+            setattr(ob, 'skyl%s' % (i+1), h.value("FT"))
+
+    if mtr.max_temp_6hr:
+        ob.max_tmpf_6hr = mtr.max_temp_6hr.value("F")
+    if mtr.min_temp_6hr:
+        ob.min_tmpf_6hr = mtr.min_temp_6hr.value("F")
+    if mtr.max_temp_24hr:
+        ob.max_tmpf_24hr = mtr.max_temp_24hr.value("F")
+    if mtr.min_temp_24hr: 
+        ob.min_tmpf_6hr = mtr.min_temp_24hr.value("F")
+    if mtr.precip_3hr:
+        ob.p03i = mtr.precip_3hr.value("IN")
+    if mtr.precip_6hr:
+        ob.p06i = mtr.precip_6hr.value("IN")
+    if mtr.precip_24hr:
+        ob.p24i = mtr.precip_24hr.value("IN")
+
+    # Presentwx
+    if mtr.weather:
+        pwx = []
+        for x in mtr.weather:
+            pwx.append( ("").join([a for a in x if a is not None]) )
+        ob.presentwx = (",".join(pwx))[:24]
         
-def normal():
-    network = sys.argv[1]
-    # 1. Query for a list of stations to iterate over
-    rs = mesosite.query("SELECT * from stations WHERE network = '%s'  ORDER by id DESC" % (
-                                                                                          network,)).dictresult()
-    for i in range(len(rs)):
-        sid = rs[i]['id']
-  # 2. Look in the database for earliest ob with METAR
-        rs2 = asos.query("""SELECT min(valid) from alldata WHERE station = '%s'
-     and metar is not null""" % (sid,)).dictresult()
-        if len(rs2) == 0 or rs2[0]['min'] is None:
-            tend = mx.DateTime.now()
-        else:
-            tend = mx.DateTime.strptime(rs2[0]['min'][:16], '%Y-%m-%d %H:%M')
+    return ob
 
-        # 3. Move old data to old table
-        asos.query("""INSERT into alldata_save SELECT * from alldata WHERE 
-   station = '%s' and valid < '%s'""" % (sid, tend.strftime("%Y-%m-%d %H:%M")))
-        asos.query("""DELETE from alldata WHERE 
-   station = '%s' and valid < '%s'""" % (sid, tend.strftime("%Y-%m-%d %H:%M")))
 
-#     4. Figure out when wunder archive starts and then process till end
-        sts = mx.DateTime.DateTime(1948,1,1)
-        ets = tend
-        now = sts
-        tbegin = None
-        processed = 0
-        while now < ets:
-            interval = mx.DateTime.RelativeDateTime(days=1)
-            time.sleep(0.5)
-            obs = doit(sid, now)
-            if obs == 0 and processed == 0:
-                interval = mx.DateTime.RelativeDateTime(months=1)
-            elif obs > 0 and processed == 0:
-                tbegin = now
-            processed += obs
-            now += interval
-        if tbegin is not None:
-            print "Station %s Begin: %s Obs: %s" % (sid, 
-                                tbegin.strftime("%Y-%m-%d"), processed)
-
-#recover()
-normal()
+if __name__ == "__main__":
+    print 'Starting up...'
+    workflow()
+    ASOS.commit()
+    ASOS.close()
+    print 'Done!'
