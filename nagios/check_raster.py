a5db7af23c5b5c88646ee7da60a90fb91bb67a3e
akrherz <akrherz@iastate.edu>
2013-02-25 07:53:39 -0600

    Add checker of NTP composite

---
+++ nagios/check_raster.py
@@ -0,0 +1,21 @@
+"""
+Check a raster file and count the number of non-zero values
+"""
+from osgeo import gdal
+import numpy
+import sys
+
+ntp = gdal.Open('/home/ldm/data/gis/images/4326/USCOMP/ntp_0.png')
+data = ntp.ReadAsArray()
+count = numpy.sum( numpy.where(data > 0, 1, 0)  )
+sz = data.shape[0] * data.shape[1]
+
+if count > 1000:
+    print 'OK - %s/%s ' % (count, sz)
+    sys.exit(0)
+elif count > 500:
+    print 'WARNING - %s/%s' % (count, sz)
+    sys.exit(1)
+else:
+    print 'CRITICAL - %s/%s' % (count, sz)
+    sys.exit(2)
