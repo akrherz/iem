894238ada7a89a57ec7e7a9657856485adb4a005
akrherz <akrherz@iastate.edu>
2013-03-09 16:26:31 -0600

    fix not closing database and migrate to datetime

--- scripts/ingestors/madis/extractMADIS.py
+++ scripts/ingestors/madis/extractMADIS.py
@@ -1,9 +1,10 @@
-#	extractMADIS.py
-#  Get the latest MADIS numbers from the data file!
+"""	extractMADIS.py
+   Get the latest MADIS numbers from the data file!
+"""
 
-import string
-import mx.DateTime
+import datetime
 import os
+import pytz
 import sys
 import iemdb
 import mesonet
@@ -12,47 +13,49 @@ icursor = IEM.cursor()
 import netCDF4
 import numpy
 
-now = mx.DateTime.gmt() + mx.DateTime.RelativeDateTime(hours=-3)
-#now = mx.DateTime.DateTime(2012, 5, 25, 12, 0)
+utcnow = datetime.datetime.utcnow()
+for i in range(10):
+    now = utcnow - datetime.timedelta(hours=i)
+    fp = "/mesonet/data/madis/mesonet/%s.nc" % (now.strftime("%Y%m%d_%H00"), )
+    if os.path.isfile(fp):
+        break
 
-fp = "/mesonet/data/madis/mesonet/%s.nc" % (now.strftime("%Y%m%d_%H00"), )
-table = "current_qc"
-
-if (not os.path.isfile(fp)):
-  sys.exit()
+if not os.path.isfile(fp):
+    print 'No MADIS Files found at all?'
+    sys.exit()
 
 nc = netCDF4.Dataset(fp)
-
-
-providers = nc.variables["dataProvider"]
-
-nc_tmpk = nc.variables["temperature"]
-nc_dwpk = nc.variables["dewpoint"]
-nc_alti = nc.variables["altimeter"]
-tmpkQCD = nc.variables["temperatureQCD"]
-dwpkQCD = nc.variables["dewpointQCD"]
-altiQCD = nc.variables["altimeterQCD"]
+providers = nc.variables["dataProvider"][:]
+nc_tmpk = nc.variables["temperature"][:]
+nc_dwpk = nc.variables["dewpoint"][:]
+nc_alti = nc.variables["altimeter"][:]
+tmpkQCD = nc.variables["temperatureQCD"][:]
+dwpkQCD = nc.variables["dewpointQCD"][:]
+altiQCD = nc.variables["altimeterQCD"][:]
 
 def figure(val, qcval):
-  if (qcval > 1000):
-    return 'Null'
-  return mesonet.k2f(val + qcval) -  mesonet.k2f(val)
+    if qcval > 1000:
+        return 'Null'
+    return float(mesonet.k2f(val + qcval) -  mesonet.k2f(val))
 
 def figureAlti(val, qcval):
-  if (qcval > 100000.):
-    return 'Null'
-  return qcval / 100.0 
+    if qcval > 100000.:
+        return 'Null'
+    return float(qcval / 100.0) 
 
 def check(val):
-  if val >  1000000.:
-    return 'Null'
-  return val
+    if val >  1000000.:
+        return 'Null'
+    return float(val)
 
 for p in range(providers.shape[0]):
     provider = providers[p]
     if provider.tostring().replace('\x00','') not in ['IEM', 'IADOT']:
-        continue  
-    ts = mx.DateTime.gmtime( nc.variables["observationTime"][p] )
+        continue
+    ticks = nc.variables["observationTime"][p]
+    ts = datetime.datetime(1970, 1, 1) + datetime.timedelta(seconds = ticks)
+    ts = ts.replace(tzinfo=pytz.timezone("UTC"))
+    print ts, ticks
 
     (tmpf, tmpf_qc_av, tmpf_qc_sc) = ('Null', 'Null', 'Null')
     (dwpf, dwpf_qc_av, dwpf_qc_sc) = ('Null', 'Null', 'Null')
@@ -70,18 +73,17 @@ for p in range(providers.shape[0]):
         alti = check( (nc_alti[p] / 100.0 ) * 0.0295298)
         alti_qc_av = figureAlti(alti, altiQCD[p,0] * 0.0295298 )
         alti_qc_sc = figureAlti(alti, altiQCD[p,6] * 0.0295298 )
-    #print id, tmpf, tmpkQCD[i]
-    sql = """UPDATE %s SET tmpf = %s, tmpf_qc_av = %s, 
+    sql = """UPDATE current_qc SET tmpf = %s, tmpf_qc_av = %s, 
      tmpf_qc_sc = %s, dwpf = %s, dwpf_qc_av = %s, 
      dwpf_qc_sc = %s, alti = %s, alti_qc_av = %s, 
-     alti_qc_sc = %s, valid = '%s' WHERE station = '%s' """ % (table, tmpf, 
+     alti_qc_sc = %s, valid = %s WHERE station = %s """ 
+    args = (tmpf, 
      tmpf_qc_av, tmpf_qc_sc, dwpf, dwpf_qc_av, 
      dwpf_qc_sc, alti, alti_qc_av, alti_qc_sc, 
-     ts.strftime("%Y-%m-%d %H:%M"), (nc.variables["stationId"][p]).tostring()[:5] )
-    sql = sql.replace("--", "Null").replace("nan", "Null")
-    try:
-        icursor.execute(sql)
-    except:
-        print sql
-
+     ts, (nc.variables["stationId"][p]).tostring()[:5] )
+    icursor.execute(sql, args)
+    
 nc.close()
+icursor.close()
+IEM.commit()
+IEM.close()
