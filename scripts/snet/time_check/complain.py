87ce337c83ea823625050d4776e53e9e0bce7500
akrherz <akrherz@iastate.edu>
2013-03-12 08:34:22 -0500

    cleanups and make more general

--- scripts/snet/time_check/complain.py
+++ scripts/snet/time_check/complain.py
@@ -1,35 +1,35 @@
-#!/usr/bin/env python
+"""
+ Complain about things, this is what I do best
+"""
+import network
+nt = network.Table("KCCI")
+import psycopg2
+portfolio = psycopg2.connect('dbname=portfolio host=meteor.geol.iastate.edu user=mesonet')
+pcursor = portfolio.cursor()
 
-import re
-from pyIEM import iemdb, mesonet
-i = iemdb.iemdb()
-mydb = i["portfolio"]
 
 lines = open("times.txt", 'r').readlines()
 
 emails = {}
 bad = 0
 for line in lines:
-  t = line.split("|")
-  sid = t[0]
-  #if sid in ('SALI4', 'SAGI4','SLOI4'):
-  #  print "Skip Mallard"
-  #  continue
-  sname = t[1]
-  dur = float(t[2])
-  if (not mesonet.kcciBack.has_key(sid)):
-    continue
-  if (dur < 1000):
-    continue
-  sql = "SELECT email from iem_site_contacts WHERE portfolio = 'kccisnet' \
-         and s_mid = '%s'" % (sid, )
-  rs = mydb.query(sql).dictresult()
-  for i in range(len(rs)):
-    emails[ rs[i]['email'] ] = 1
-  print sname, dur
-  bad += 1
+    t = line.split("|")
+    sid = t[0]
+    if not nt.sts.has_key(sid):
+        continue
+    sname = t[1]
+    dur = float(t[2])
+    if dur < 1000 and dur > -1000:
+        continue
+    sql = """SELECT email from iem_site_contacts WHERE portfolio = 'kccisnet' 
+         and s_mid = %s"""
+    args =  (sid, )
+    pcursor.execute(sql, args)
+    for row in pcursor:
+        emails[ row[0] ] = 1
+    bad += 1
 
 for k in emails.keys():
-  print "%s," % (k,),
+    print "%s," % (k,),
 
 print bad
--- scripts/snet/time_check/sample.py
+++ scripts/snet/time_check/sample.py
@@ -1,27 +1,22 @@
-# Application to save SNET data flow to the IEM Access database!
-# Daryl Herzmann 4 Sept 2003
-# 16 Sep 2003	Must be careful how we import IEMAccessOb
-#		Don't ingest Max and Min lines
+"""
+Samples the SNET feed, so we can complain about sites that have bad clocks!
+"""
 
-from twisted.protocols.basic import LineReceiver
 from twisted.internet import reactor
-from twisted.protocols import policies, basic
-from twisted.internet import protocol
 from twisted.application import service, internet
-from sys import stdout                                                         
-from pyIEM import nwnformat, mesonet,stationTable
+from twisted.python import log
+import nwnformat
 import re
-import mx.DateTime
+import datetime
 
 from nwnserver import hubclient
 
-
-st = stationTable.stationTable("/mesonet/TABLES/snet.stns")
-
-#class NWNClientProtocol(basic.LineReceiver):
-#    def lineReceived(self, line):
-#        #self.resetTimeout()
-#        self.factory.processData(line)
+import network
+nt = network.Table(("KCCI", "KIMT", "KELO"))
+id2nwsli = {}
+for sid in nt.sts.keys():
+    if nt.sts[sid]['nwn_id'] is not None:
+        id2nwsli[ int(nt.sts[sid]['nwn_id']) ] = sid
 
 
                                                                                 
@@ -38,34 +33,39 @@ class NWNClientFactory(hubclient.HubClientProtocolBaseFactory):
 db = {}                 
 
 def ingestData(data):
-  if (data == None or data == ""):
-    return
+    if data is None or data == "":
+        return
 
-  tokens = re.split("\s+", data)
-  if (len(tokens) != 14): 
-    return
+    tokens = re.split("\s+", data)
+    if len(tokens) != 14: 
+        return
 
-  siteID = tokens[1]
-  if (tokens[2] != "Min" and tokens[2] != "Max"):
-    if (not db.has_key(siteID) ):
-      db[siteID] = nwnformat.nwnformat()
-    db[siteID].raw = tokens
-    db[siteID].parseLineRT(tokens)  
+    siteID = tokens[1]
+    if tokens[2] != "Min" and tokens[2] != "Max":
+        if not db.has_key(siteID):
+            db[siteID] = nwnformat.nwnformat()
+        db[siteID].raw = tokens
+        db[siteID].parseLineRT(tokens)  
 
                                                                    
 def saveData():
-  out = open('times.txt', 'w')
-  for key in db.keys():
-    sid = int(key)
-    now = mx.DateTime.now()
-    if (mesonet.snetConv.has_key(sid)):
-      t = db[key].raw[2]
-      d = db[key].raw[3]
-      ts = mx.DateTime.strptime(t +" "+d, "%H:%M %m/%d/%y")
-      out.write("%s|%s|%.1f\n" % (mesonet.snetConv[sid], st.sts[ mesonet.snetConv[sid]]["name"], (now - ts)) )
+    log.msg("saveData() called...")
+    out = open('times.txt', 'w')
+    for key in db.keys():
+        sid = int(key)
+        now = datetime.datetime.now()
+        if id2nwsli.has_key(sid):
+            t = db[key].raw[2]
+            d = db[key].raw[3]
+            ts = datetime.datetime.strptime(t +" "+d, "%H:%M %m/%d/%y")
+            delta = (now - ts).days * 86400.0 + (now -ts).seconds
+            out.write("%s|%s|%.1f\n" % (id2nwsli[sid], 
+                                nt.sts[ id2nwsli[sid]]["name"], delta ))
+        else:
+            log.msg("Unknown site ID of: %s" % (sid,))
 
-  out.close()
-  reactor.stop()
+    out.close()
+    reactor.stop()
 
 application = service.Application("PythonHub")
 serviceCollection = service.IServiceCollection(application)
