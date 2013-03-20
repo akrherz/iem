f710e7fccb56a50949cbca8673be129e72cecd0e
akrherz <akrherz@iastate.edu>
2013-03-20 08:28:02 -0500

    Add NARR ingest into daily routine

--- scripts/RUN_0Z.sh
+++ scripts/RUN_0Z.sh
@@ -15,3 +15,9 @@ python snet2archive.py
 
 cd ../iemre
 python stage4_12z_adjust.py
+
+cd ../cscap
+# at 0z, -5 days is available, hopefully!
+python download_narr.py $(date -u --date '5 days ago' +'%Y %m %d')
+cd ../coop
+python narr_solarrad.py $(date -u --date '5 days ago' +'%Y %m %d')
