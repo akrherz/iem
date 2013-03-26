2ae2a2182e7fef139b856d46d9319ec158408a90
akrherz <akrherz@iastate.edu>
2013-03-26 11:26:37 -0500

    Add clearsky radiation and use units of MJ m-2 dy-1

--- scripts/coop/merra_solarrad.py
+++ scripts/coop/merra_solarrad.py
@@ -5,8 +5,8 @@
  1 langley is 41840.00 J m-2 is 41840.00 W s m-2 is 11.622 W hr m-2
  
  So 1000 W m-2 x 3600 is 3,600,000 W s m-2 is 86 langleys
- 
- 26 Jun 1988 is bad!
+  
+ Dr Arritt wants MJ m-2 dy-1
  
 """
 import netCDF4
@@ -50,6 +50,7 @@ def do( date ):
     fn = sts.strftime("/mesonet/merra/%Y/%Y%m%d.nc")
     nc = netCDF4.Dataset( fn )
     rad = nc.variables['swgdn'][7:,:,:]
+    cs_rad = nc.variables['swgdnclr'][7:,:,:]
     xc = nc.variables['longitude'][:]
     yc = nc.variables['latitude'][:] 
     nc.close()
@@ -57,9 +58,12 @@ def do( date ):
     fn2 = ets.strftime("/mesonet/merra/%Y/%Y%m%d.nc")
     nc = netCDF4.Dataset( fn2 )
     rad2 = nc.variables['swgdn'][:7,:,:]
+    cs_rad2 = nc.variables['swgdnclr'][7:,:,:]
     nc.close()
 
+    # W m-2 -> J m-2 s-1 -> J m-2 dy-1
     total = (numpy.sum(rad, 0) + numpy.sum(rad2, 0)) * 3600.0
+    cs_total = (numpy.sum(cs_rad, 0) + numpy.sum(cs_rad2, 0)) * 3600.0
 
     ccursor.execute("""
         SELECT station, x(geom), y(geom) from alldata a JOIN stations t on 
@@ -77,15 +81,29 @@ def do( date ):
         val = ((z0/distances[0] + z1/distances[1] + z2/distances[2] 
                 + z3/distances[3]) / (1./distances[0] + 1./distances[1] + 
                                       1./distances[2] + 1./distances[3] ))
-        langleys = float(val) / 41840.0
-        if langleys < 0:
-            print 'WHOA! Negative RAD: %.2f, station: %s' % (langleys, row[0])
+        # MJ m-2 dy-1
+        rad_mj = float(val) / 1000000.0
+        
+        z0 = cs_total[gridys[0], gridxs[0]]
+        z1 = cs_total[gridys[1], gridxs[1]]
+        z2 = cs_total[gridys[2], gridxs[2]]
+        z3 = cs_total[gridys[3], gridxs[3]]
+        
+        cs_val = ((z0/distances[0] + z1/distances[1] + z2/distances[2] 
+                + z3/distances[3]) / (1./distances[0] + 1./distances[1] + 
+                                      1./distances[2] + 1./distances[3] ))
+        # MJ m-2 dy-1
+        cs_rad_mj = float(cs_val) / 1000000.0
+        
+        if rad_mj < 0:
+            print 'WHOA! Negative RAD: %.2f, station: %s' % (rad_mj, row[0])
             continue
         #print "station: %s rad: %.1f" % (row[0], langleys)
         ccursor2.execute("""
-        UPDATE alldata_"""+ row[0][:2] +""" SET merra_srad = %s WHERE
+        UPDATE alldata_"""+ row[0][:2] +""" SET merra_srad = %s,
+        merra_srad_cs = %s WHERE
         day = %s and station = %s
-        """, (langleys, date.strftime("%Y-%m-%d"), row[0]))
+        """, (rad_mj, cs_rad_mj, date.strftime("%Y-%m-%d"), row[0]))
     
 
 do( datetime.datetime(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3])) )
