0238cc73183443055a78076782abaa7d27fa7aeb
akrherz <akrherz@iastate.edu>
2013-03-31 07:34:28 -0500

    Ready to release MOM

---
+++ htdocs/mom.php
@@ -0,0 +1,77 @@
+<?php 
+
+// Map without Missouri
+
+// Deanna Troi
+// Drones to "collect air temperatures"
+// Snail mail tornado reports
+// Interpolated Mars data
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Midwest Observational Mesonet</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le styles -->
+    <link href="/css/bootstrap.css" rel="stylesheet">
+    <link href="/css/bootstrap-responsive.css" rel="stylesheet">
+</head>
+
+<body> 
+<div class="container">
+
+<div class="page-header">
+  <h1>Say hello to your MOM!!! 
+  <br /><small>After 12 years, the Iowa Environmental Mesonet
+  finally expands to become the Midwest Observational Mesonet (MOM).</small></h1>
+    <div class="thumbnail">
+      <img src="/images/mom.png" alt="">
+      <p>Figure 1: Domain of data collected. Note: <i>Due to a long standing fued between Iowa and Missoura, Missoura is
+      not included.</i></p>
+    </div>
+  </div>
+
+  <div class="marketing">
+
+    <h1>Our Moto: Don't say anything bad about your mom.</h1>
+    <p class="marketing-byline">We are not just a new name, we are also
+    announcing these new capabilities.</p>
+
+    <div class="row-fluid">
+      <div class="span3">
+        <img class="img-rounded" src="http://upload.wikimedia.org/wikipedia/en/thumb/d/df/DeannaTroi.jpg/250px-DeannaTroi.jpg">
+        <h2>Telepathic Data Collection</h2>
+        <p>Using computers and Al Gore's Internet to collect data is so last decade. Our local betazoid and new employee Deanna Troi, will collect data telepathically.</p>
+      </div>
+      <div class="span3">
+        <img class="img-rounded" src="http://upload.wikimedia.org/wikipedia/commons/thumb/3/33/MQ-9_Reaper_-_090609-F-0000M-777.JPG/300px-MQ-9_Reaper_-_090609-F-0000M-777.JPG">
+        <h2>More webcams!</h2>
+        <p>Our newly acquired fleet of drones will show weather and your illegal backyard swimming pool like never before. When you see one flying, know that your MOM is looking out for you.</p>
+      </div>
+      <div class="span3">
+        <img class="img-rounded" src="/images/mom_extrap.png">
+        <h2>Extrapolated Data for Mars</h2>
+        <p>Other planets have weather data too and our state-of-the-art extrapolation software has generated the last 100 years of data on Mars using only data from Iowa.</p>
+      </div>
+      <div class="span3">
+        <img class="img-rounded" src="/images/mom_tornado_anim.gif">
+        <h2>Report tornados via US Mail</h2>
+        <p>There are many ways to report seeing a tornado, but never before via US Mail. See a tornado?  Send it to us via US Mail and we'll relay to the NWS.</p>
+      </div>
+    </div>
+</div>
+
+<div class="row-fluid">
+<div class="span12">
+<p><a href="/index.php?old" class="btn btn-primary">OK, take me back to the IEM Homepage</a>
+</div>
+</div>
+
+</div>
+</body>
+</html>
