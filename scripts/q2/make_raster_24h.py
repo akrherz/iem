d90582b301d2fe7879dece6f61d7b88f58feaf6b
akrherz <akrherz@iastate.edu>
2013-04-02 13:14:07 -0500

    wrong current fn

--- scripts/q2/make_raster_24h.py
+++ scripts/q2/make_raster_24h.py
@@ -84,7 +84,7 @@ def doit(gts, hr):
     # Now we need to generate the world file
     nmq.write_worldfile('%s.wld' % (tmpfn,))
     # Inject WLD file
-    pqstr = "/home/ldm/bin/pqinsert -p 'plot ac %s gis/images/4326/q2/p%sh.png GIS/q2/p%sh_%s.wld wld' %s.wld" % (
+    pqstr = "/home/ldm/bin/pqinsert -p 'plot ac %s gis/images/4326/q2/p%sh.wld GIS/q2/p%sh_%s.wld wld' %s.wld" % (
                     gts.strftime("%Y%m%d%H%M"),hr, hr, gts.strftime("%Y%m%d%H%M"), tmpfn )
     subprocess.call(pqstr, shell=True)
     # Now we inject into LDM
