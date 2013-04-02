ad9610649beccd532ac8f03b3fc28cecf6a8fb5e
akrherz <akrherz@iastate.edu>
2013-04-02 10:28:59 -0500

    generalized to work automagically

--- cgi-bin/climate/orc.py
+++ cgi-bin/climate/orc.py
@@ -1,121 +1,110 @@
 #!/usr/bin/python
+import sys
+sys.path.insert(0, '/mesonet/www/apps/iemwebsite/scripts/lib')
 
-import mx.DateTime
-#from pyIEM import iemAccess, iemdb
-#iemaccess = iemAccess.iemAccess()
-#i = iemdb.iemdb()
-#climatedb = i["coop"]
-import pg
-climatedb = pg.connect('coop','iemdb', user='nobody')
-iemaccess = pg.connect('iem', 'iemdb', user='nobody')
-asos = pg.connect('asos', 'iemdb', user='nobody')
+import datetime
+import iemdb
+COOP = iemdb.connect('coop', bypass=True)
+ccursor = COOP.cursor()
+IEM = iemdb.connect('iem', bypass=True)
+icursor = IEM.cursor()
+ASOS = iemdb.connect('asos', bypass=True)
+acursor = ASOS.cursor()
 
 ADJUSTMENT = 0
-s = mx.DateTime.DateTime(2012,12,18)
-e = mx.DateTime.DateTime(2013,1,17)
-interval = mx.DateTime.RelativeDateTime(days=+1)
+now = datetime.datetime.now()
+e = now.replace(day=17)
+s = (e - datetime.timedelta(days=31)).replace(day=17)
 
 def averageTemp(db, hi="high", lo="low"):
-  highSum, lowSum = 0, 0
-  for day in db.keys():
-    highSum += db[day][hi] 
-    lowSum += db[day][lo] 
+    highSum, lowSum = 0, 0
+    for day in db.keys():
+        highSum += db[day][hi] 
+        lowSum += db[day][lo] 
 
-  highAvg = highSum / len(db)
-  lowAvg = lowSum / len(db)
+    highAvg = highSum / len(db)
+    lowAvg = lowSum / len(db)
 
-  return highAvg, lowAvg
+    return highAvg, lowAvg
 
 def hdd(db, hi="high", lo="low"):
-  dd = 0
-  for day in db.keys():
-    h = db[day][hi]
-    l = db[day][lo] 
-    a = (h+l) / 2.00
-    if (a < 65):
-      dd += (65.0 - a)
-  return dd
+    dd = 0
+    for day in db.keys():
+        h = db[day][hi]
+        l = db[day][lo] 
+        a = (h+l) / 2.00
+        if a < 65:
+            dd += (65.0 - a)
+    return dd
 
 def cdd(db, hi="high", lo="low"):
-  dd = 0
-  for day in db.keys():
-    h = db[day][hi]
-    l = db[day][lo] 
-    a = (h+l) / 2.00
-    if (a > 65):
-      dd += (a - 65.0)
-  return dd
+    dd = 0
+    for day in db.keys():
+        h = db[day][hi]
+        l = db[day][lo] 
+        a = (h+l) / 2.00
+        if a > 65:
+            dd += (a - 65.0)
+    return dd
 
 def main():
-  db = {}
-  now = s
-  while (now <= e):
-    db[ now.strftime("%Y-%m-%d") ] = {'high': 'M', 'low': 'M',
-        'avg_high': 'M', 'avg_low': 'M'}
-    now += interval
+    db = {}
+    now = s
+    while now <= e:
+        db[ now.strftime("%m%d") ] = {'high': 'M', 'low': 'M',
+                                          'avg_high': 'M', 'avg_low': 'M'}
+        now += datetime.timedelta(days=1)
 
-  rs = iemaccess.query("""SELECT day, max_tmpf, min_tmpf from summary_%s s JOIN stations t
-    ON (t.iemid = s.iemid) WHERE t.id = 'SUX'""" % (now.year -1, ) ).dictresult()
+    # Get Sioux City data
+    icursor.execute("""SELECT day, max_tmpf, min_tmpf from 
+        summary s JOIN stations t ON (t.iemid = s.iemid) 
+        WHERE t.id = 'SUX' and day >= '%s' and 
+        day < '%s' """ % (s.strftime("%Y-%m-%d"), e.strftime("%Y-%m-%d")) )
+    for row in icursor:
+        db[ row[0].strftime("%m%d") ]['high'] = row[1] + ADJUSTMENT
+        db[ row[0].strftime("%m%d") ]['low'] = row[2]  + ADJUSTMENT
 
-  for i in range(len(rs)):
-    if (db.has_key( rs[i]['day'] )):
-      db[ rs[i]['day'] ]['high'] = rs[i]['max_tmpf'] + ADJUSTMENT
-      db[ rs[i]['day'] ]['low'] = rs[i]['min_tmpf']  + ADJUSTMENT
 
-  rs = iemaccess.query("""SELECT day, max_tmpf, min_tmpf from summary_%s s JOIN stations t
-    ON (t.iemid = s.iemid) WHERE t.id = 'SUX'""" % (now.year, ) ).dictresult()
+    # Lemars
+    ccursor.execute("""SELECT high, low, valid  from climate 
+        WHERE station = 'IA4735'""")
+    for row in ccursor:
+        if not db.has_key(row[2].strftime("%m%d")):
+            continue
+        db[ row[2].strftime("%m%d") ]['avg_high'] = row[0] 
+        db[ row[2].strftime("%m%d") ]['avg_low'] = row[1]
 
-  for i in range(len(rs)):
-    if (db.has_key( rs[i]['day'] )):
-      db[ rs[i]['day'] ]['high'] = rs[i]['max_tmpf'] + ADJUSTMENT
-      db[ rs[i]['day'] ]['low'] = rs[i]['min_tmpf']  + ADJUSTMENT
 
-  # Lemars
-  rs = climatedb.query("SELECT high, low, \
-    to_char(valid, '2012-mm-dd') as valid from climate \
-    WHERE station = 'IA4735'").dictresult()
+    # Compute Average wind speed
+    acursor.execute("""
+      SELECT avg(sknt) from alldata where station = 'ORC' and 
+      valid BETWEEN '%s' and '%s' and sknt >= 0
+      """ % (s.strftime("%Y-%m-%d %H:%M"), e.strftime("%Y-%m-%d %H:%M")))
+    row = acursor.fetchone()
+    awind = row[0]
 
-  for i in range(len(rs)):
-    if (db.has_key( rs[i]['valid'] )):
-      db[ rs[i]['valid'] ]['avg_high'] = rs[i]['high'] 
-      db[ rs[i]['valid'] ]['avg_low'] = rs[i]['low']
+    print 'Content-type: text/plain\n\n'
+    print '  Orange City Climate Summary\n'
+    print '%15s %6s %6s' % ("DATE", "HIGH", "LOW")
+    now = s
+    while now <= e:
+        print "%15s %6i %6i %6i %6i" % (now.strftime("%Y-%m-%d"), 
+                db[now.strftime("%m%d")]['high'], 
+                db[now.strftime("%m%d")]['low'],
+                db[now.strftime("%m%d")]['avg_high'], 
+                db[now.strftime("%m%d")]['avg_low'])
+        now += datetime.timedelta(days=1)
 
-  rs = climatedb.query("SELECT high, low, \
-    to_char(valid, '2013-mm-dd') as valid from climate \
-    WHERE station = 'IA4735'").dictresult()
-                                                                                
-  for i in range(len(rs)):
-    if (db.has_key( rs[i]['valid'] )):
-      db[ rs[i]['valid'] ]['avg_high'] = rs[i]['high']
-      db[ rs[i]['valid'] ]['avg_low'] = rs[i]['low']
+    h, l = averageTemp(db)
+    ch, cl = averageTemp(db, "avg_high", "avg_low")
 
-  # Compute Average wind speed
-  rs = asos.query("""
-  SELECT avg(sknt) from alldata where station = 'ORC' and 
-  valid BETWEEN '%s' and '%s' and sknt >= 0
-  """ % (s.strftime("%Y-%m-%d %H:%M"), e.strftime("%Y-%m-%d %H:%M"))).dictresult()
-  awind = rs[0]['avg']
+    l_hdd = hdd(db)
+    c_hdd = hdd(db,"avg_high", "avg_low")
 
-  print 'Content-type: text/plain\n\n'
-  print '  Orange City Climate Summary\n'
-  print '%15s %6s %6s' % ("DATE", "HIGH", "LOW")
-  now = s
-  while (now <= e):
-    print "%15s %6i %6i %6i %6i" % (now.strftime("%Y-%m-%d"), \
-     db[now.strftime("%Y-%m-%d")]['high'], db[now.strftime("%Y-%m-%d")]['low'],\
-     db[now.strftime("%Y-%m-%d")]['avg_high'], db[now.strftime("%Y-%m-%d")]['avg_low'])
-    now += interval
+    l_cdd = cdd(db)
+    c_cdd = cdd(db,"avg_high", "avg_low")
 
-  h, l = averageTemp(db)
-  ch, cl = averageTemp(db, "avg_high", "avg_low")
-
-  l_hdd = hdd(db)
-  c_hdd = hdd(db,"avg_high", "avg_low")
-
-  l_cdd = cdd(db)
-  c_cdd = cdd(db,"avg_high", "avg_low")
-
-  print """
+    print """
 Summary Information [%s - %s]
 -------------------
               Observed     |  Climate  |  Diff  
