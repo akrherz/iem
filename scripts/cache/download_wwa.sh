23abbfcaaafd31d26cc453b225372c357ef0fb94
akrherz <akrherz@iastate.edu>
2013-04-09 09:24:16 -0500

    lets start caching the WWA map

---
+++ scripts/cache/download_wwa.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+DSTAMP=$(date -u +'%Y%m%d%H%M')
+
+wget -q --timeout=60 -O /tmp/wwa_${DSTAMP}.png http://forecast.weather.gov/wwamap/png/US.png
+
+/home/ldm/bin/pqinsert -p "plot a ${DSTAMP} bogus wwa/wwa_${DSTAMP}.png png" /tmp/wwa_${DSTAMP}.png >& /dev/null
+
+exit 0
