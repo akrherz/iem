18de59982512573ad930681ec6c78266e06387ef
akrherz <akrherz@iastate.edu>
2013-04-22 08:06:09 -0500

    try this commit again, without replacing the wrong things!

--- scripts/cache/nesdis_satellite.sh
+++ scripts/cache/nesdis_satellite.sh
@@ -22,8 +22,8 @@ mkdir nesdis.$ftm
 cd nesdis.$ftm
 
 BASE="http://satepsanone.nesdis.noaa.gov/pub/GIS"
-SCP="$SCP -o ConnectTimeout=5"
-SSH="$SSH -o ConnectTimeout=5 -f"
+SCP="scp -o ConnectTimeout=5"
+SSH="ssh -o ConnectTimeout=5 -f"
 
 function getter(){
 	for i in {1..10}
@@ -107,46 +107,46 @@ fi
 
 for mach in $(echo "iemvs100.local iemvs101.local iemvs102.local iemvs103.local iemvs104.local iemvs105.local iemvs106.local iemvs107.local iemvs108.local iem21.local iem50.local"); do
  
- $SCP -q GoesWest1V${tm}.tif ldm@${mach}:/tmp/west1V_0.tif
+ $SCP  -q GoesWest1V${tm}.tif ldm@${mach}:/tmp/west1V_0.tif
   $SSH -q ldm@${mach} "cat /tmp/west1V_0.tif | python ~/pyWWA/rotate.py gis/images/4326/goes/west1V_ tif"
-  $SCP -q GoesWest1V${tm}.tfw ldm@${mach}:/mesonet/data/gis/images/4326/goes/west1V_0.tfw
+  $SCP  -q GoesWest1V${tm}.tfw ldm@${mach}:/mesonet/data/gis/images/4326/goes/west1V_0.tfw
  
 
-  $SCP -q GoesEast1V${tm}.tif ldm@${mach}:/tmp/east1V_0.tif
+  $SCP  -q GoesEast1V${tm}.tif ldm@${mach}:/tmp/east1V_0.tif
   $SSH -q ldm@${mach} "cat /tmp/east1V_0.tif | python ~/pyWWA/rotate.py gis/images/4326/goes/east1V_ tif"
-  $SCP -q GoesEast1V${tm}.tfw ldm@${mach}:/mesonet/data/gis/images/4326/goes/east1V_0.tfw
+  $SCP  -q GoesEast1V${tm}.tfw ldm@${mach}:/mesonet/data/gis/images/4326/goes/east1V_0.tfw
 
-  $SCP -q GoesWest04I4${tm}.tif ldm@${mach}:/tmp/west04I4_0.tif
+  $SCP  -q GoesWest04I4${tm}.tif ldm@${mach}:/tmp/west04I4_0.tif
   $SSH -q ldm@${mach} "cat /tmp/west04I4_0.tif | python ~/pyWWA/rotate.py gis/images/4326/goes/west04I4_ tif"
-  $SCP -q GoesWest04I4${tm}.tfw ldm@${mach}:/mesonet/data/gis/images/4326/goes/west04I4_0.tfw
+  $SCP  -q GoesWest04I4${tm}.tfw ldm@${mach}:/mesonet/data/gis/images/4326/goes/west04I4_0.tfw
 
-  $SCP -q GoesEast04I4${tm}.tif ldm@${mach}:/tmp/east04I4_0.tif
+  $SCP  -q GoesEast04I4${tm}.tif ldm@${mach}:/tmp/east04I4_0.tif
   $SSH -q ldm@${mach} "cat /tmp/east04I4_0.tif | python ~/pyWWA/rotate.py gis/images/4326/goes/east04I4_ tif"
-  $SCP -q GoesEast04I4${tm}.tfw ldm@${mach}:/mesonet/data/gis/images/4326/goes/east04I4_0.tfw
+  $SCP  -q GoesEast04I4${tm}.tfw ldm@${mach}:/mesonet/data/gis/images/4326/goes/east04I4_0.tfw
 
-  $SCP -q GoesWest04I3${tm}.tif ldm@${mach}:/tmp/west04I3_0.tif
+  $SCP  -q GoesWest04I3${tm}.tif ldm@${mach}:/tmp/west04I3_0.tif
   $SSH -q ldm@${mach} "cat /tmp/west04I3_0.tif | python ~/pyWWA/rotate.py gis/images/4326/goes/west04I3_ tif"
-  $SCP -q GoesWest04I3${tm}.tfw ldm@${mach}:/mesonet/data/gis/images/4326/goes/west04I3_0.tfw
+  $SCP  -q GoesWest04I3${tm}.tfw ldm@${mach}:/mesonet/data/gis/images/4326/goes/west04I3_0.tfw
 
-  $SCP -q GoesEast04I3${tm}.tif ldm@${mach}:/tmp/east04I3_0.tif
+  $SCP  -q GoesEast04I3${tm}.tif ldm@${mach}:/tmp/east04I3_0.tif
   $SSH -q ldm@${mach} "cat /tmp/east04I3_0.tif | python ~/pyWWA/rotate.py gis/images/4326/goes/east04I3_ tif"
-  $SCP -q GoesEast04I3${tm}.tfw ldm@${mach}:/mesonet/data/gis/images/4326/goes/east04I3_0.tfw
+  $SCP  -q GoesEast04I3${tm}.tfw ldm@${mach}:/mesonet/data/gis/images/4326/goes/east04I3_0.tfw
 
   if [ -e vis_900913.tif ]
   	then
-    $SCP -q vis_900913.tif ldm@${mach}:/tmp/vis_900913_0.tif
+    $SCP  -q vis_900913.tif ldm@${mach}:/tmp/vis_900913_0.tif
     $SSH -q ldm@${mach} "mv /tmp/vis_900913_0.tif ~/data/gis/images/900913/goes/vis_0.tif"
     fi
   	
   if [ -e ir_900913.tif ]
   	then
-    $SCP -q ir_900913.tif ldm@${mach}:/tmp/ir_900913_0.tif
+    $SCP  -q ir_900913.tif ldm@${mach}:/tmp/ir_900913_0.tif
     $SSH -q ldm@${mach} "mv /tmp/ir_900913_0.tif ~/data/gis/images/900913/goes/ir_0.tif"
     fi
   	
   if [ -e wv_900913.tif ]
   	then
-    $SCP -q wv_900913.tif ldm@${mach}:/tmp/wv_900913_0.tif
+    $SCP  -q wv_900913.tif ldm@${mach}:/tmp/wv_900913_0.tif
     $SSH -q ldm@${mach} "mv /tmp/wv_900913_0.tif ~/data/gis/images/900913/goes/wv_0.tif"
     fi 
 done
