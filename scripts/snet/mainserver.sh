7f42000119292f632b4d1da07829a722a4ab45f0
akrherz <akrherz@iastate.edu>
2013-05-13 16:04:43 -0500

    cleanups

--- scripts/snet/mainserver.sh
+++ scripts/snet/mainserver.sh
@@ -1,7 +1,9 @@
-#!/bin/sh
+# This is the Twisted python process that collects schoolnet data
+# Run via @reboot on mesonet's crontab
 
 if [ -e mainserver.pid ]; then
-	kill -9 `cat mainserver.pid `	
+	kill -9 `cat mainserver.pid `
+	rm -f mainserver.pid
 fi
 twistd --pidfile=mainserver.pid --logfile=/mesonet/data/logs/mainserver.log -y mainserver.py
 sleep 10
