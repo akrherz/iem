28c770fb1a64dc2290e5b7ad82d630fcf5f5e6ce
akrherz <akrherz@iastate.edu>
2013-05-13 15:56:06 -0500

    sync upstream

--- scripts/feature/days_since_T2.py
+++ scripts/feature/days_since_T2.py
@@ -15,19 +15,25 @@ ccursor = COOP.cursor()
 
 # Compute normal from the climate database
 sql = """
- SELECT c.name, c.network, c.id, max_tmpf, x(c.geom) as lon, 
+ SELECT c.name, c.network, c.id, pday, x(c.geom) as lon, 
  y(c.geom) as lat, c.climate_site from summary_2013 s, stations c 
  where s.iemid = c.iemid
- and s.day = '2013-04-08' and c.network in ('IA_ASOS', 'AWOS') 
- and max_tmpf > 0 and id not in ('IFA')
+ and s.day = '2013-04-17' and c.network in ('IA_ASOS', 'AWOS') 
+ and pday > 0 and id not in ('OXV', 'OOA', 'IKV', 'BRL', 'GGI')
  ORDER by id ASC
 """
 
 def compute(ts):
-    now = datetime.date(2013,4,8)
+    now = datetime.date(2013,4,17)
     days = (now - ts).days
     years = int(days / 365)
     months = int((days % 365) / 30)
+    if years == 0 and months == 0 and days == 1:
+        return '-', 0
+    if years == 0 and months == 0:
+        return "%sd" % (days,), days
+    if years == 0:
+        return "%sm" % (months,), days
     return "%sy%sm" % (years, months), days
 
 lats = []
@@ -36,15 +42,15 @@ vals = []
 icursor.execute( sql )
 for row in icursor:
     station = row[2]
-    max_tmpf = int(row[3])
+    max_tmpf = row[3]
     cid = row[6]
     ccursor.execute("""
     SELECT max(day) from alldata_ia where station = '%s' 
-    and high >= %.0f and day < '2013-04-08'
+    and precip >= %.0f and day < '2013-04-17'
     """ % (cid, max_tmpf))
     row2 = ccursor.fetchone()
     lbl, days = compute(row2[0])
-    vals.append( "%.0f~C~%s" % (max_tmpf, days-1) )
+    vals.append( "%.2f~C~%s" % (max_tmpf, lbl) )
     lats.append( row[5] )
     lons.append( row[4] )
     print '%5s %s %s %s' % (station, max_tmpf, lbl, days)
@@ -53,8 +59,8 @@ cfg = {
  'wkColorMap': 'BlAqGrYeOrRe',
  'nglSpreadColorStart': 2,
  'nglSpreadColorEnd'  : -1,
- '_title'             : "8 Apr 2013 High Temperature and Days Since as Warm Temperature",
- '_valid'             : "8 Apr 2013, values in F",
+ '_title'             : "17 Apr 2013 Precipitation and Days Since as High Amount",
+ '_valid'             : "17 Apr 2013, values in inch",
 #'lbTitleString'      : "[days]",
  '_showvalues'        : True,
  '_format'            : '%s',
