d41efa70c51af8e18f26edbf318f0d7662c75ffd
akrherz <akrherz@iastate.edu>
2013-05-13 16:22:05 -0500

    commit some devel stuff with templates for now

---
+++ include/myview.php
@@ -0,0 +1,22 @@
+<?php
+// http://coding.smashingmagazine.com/2011/10/17/getting-started-with-php-templating/
+class MyView {
+    protected $vars = array();
+    public function __construct($template_dir = null) {
+            $this->template_dir =  ROOTPATH .'/include/templates/';
+    }
+    public function render($template_file) {
+        if (file_exists($this->template_dir.$template_file)) {
+            include $this->template_dir.$template_file;
+        } else {
+            throw new Exception('no template file ' . $template_file . ' present in directory ' . $this->template_dir);
+        }
+    }
+    public function __set($name, $value) {
+        $this->vars[$name] = $value;
+    }
+    public function __get($name) {
+        return $this->vars[$name];
+    }
+}
+?>
