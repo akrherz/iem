bd9c46ffe83248815e8259db84e961ad64d425cf
akrherz <akrherz@iastate.edu>
2013-05-15 13:46:48 -0500

    start monitoring the N0Q composite, instead of slower mesonet.gif

--- nagios/iem_idd.py
+++ nagios/iem_idd.py
@@ -7,15 +7,15 @@ import stat
 import datetime
 
 now = datetime.datetime.now()
-mtime = os.stat("/home/ldm/data/mesonet.gif")[stat.ST_MTIME]
+mtime = os.stat("/home/ldm/data/gis/images/4326/USCOMP/n0q_0.png")[stat.ST_MTIME]
 ts = datetime.datetime.fromtimestamp(mtime)
 diff = (now - ts).days * 86400. + (now - ts).seconds
-if diff < 3300:
-    print 'OK - mesonet.gif %s' % (ts,)
+if diff < 600:
+    print 'OK - n0q_0.png %s' % (ts,)
     sys.exit(0)
-elif diff < 3600:
-    print 'WARNING - mesonet.gif %s' % (ts,)
+elif diff < 700:
+    print 'WARNING - n0q_0.png %s' % (ts,)
     sys.exit(1)
 else:
-    print 'CRITICAL - mesonet.gif %s' % (ts,)
+    print 'CRITICAL - n0q_0.png %s' % (ts,)
     sys.exit(2)
