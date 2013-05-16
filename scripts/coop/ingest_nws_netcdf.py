94f8a932a239a05860a6aab04c92decd1455ece4
akrherz <akrherz@iastate.edu>
2013-05-16 15:55:22 -0500

    upstream

--- scripts/coop/ingest_nws_netcdf.py
+++ scripts/coop/ingest_nws_netcdf.py
@@ -1,9 +1,10 @@
-import netCDF3
+import netCDF4
 import iemdb
+import numpy
 COOP = iemdb.connect('coop')
 ccursor = COOP.cursor()
 
-nc = netCDF3.Dataset('DSM.nc')
+nc = netCDF4.Dataset('ALO.nc')
 
 byear = nc.variables['byear'][:]
 maxt = nc.variables['maxt'][:]
@@ -16,26 +17,26 @@ for yr in range(byear, 2011):
   print yr
   for mo in range(12):
     for dy in range(31):
-      high = float(maxt[yr-byear,mo,dy]) / 1.0
-      if high < -100 or high > 150:
+      high = float(maxt[yr-byear,mo,dy])
+      if high < -100 or high > 150 or numpy.isnan(high):
         high = 'null'
-      low = float(mint[yr-byear,mo,dy])  / 1.0
-      if low < -100 or low > 150:
+      low = float(mint[yr-byear,mo,dy])
+      if low < -100 or low > 150 or numpy.isnan(low):
         low = 'null'
-      precip = float(pcpn[yr-byear,mo,dy]) / 100.0
-      if precip < -100 or precip > 150:
+      precip = float(pcpn[yr-byear,mo,dy])
+      if precip < 0 or precip > 150 or numpy.isnan(precip):
         precip = 'null'
-      snowfall = float(snow[yr-byear,mo,dy]) / 10.0
-      if snowfall < -100 or snowfall > 150:
+      snowfall = float(snow[yr-byear,mo,dy])
+      if snowfall < 0 or snowfall > 150 or numpy.isnan(snowfall):
         snowfall = 'null'
-      snowd = float(snwg[yr-byear,mo,dy]) / 1.0
-      if snowd < -100 or snowd > 150:
+      snowd = float(snwg[yr-byear,mo,dy])
+      if snowd < 0 or snowd > 150 or numpy.isnan(snowd):
         snowd = 'null'
       if high == 'null' or low == 'null':
         continue
 
       sql = """INSERT into alldata_tmp(station, day, high, low, precip,
-            snow, sday, year, month, snowd) VALUES ('IA2203', '%s-%s-%s',
+            snow, sday, year, month, snowd) VALUES ('IA8706', '%s-%s-%s',
             %s, %s, %s, %s, '%02i%02i', %s, %s, %s)""" % (yr,
             mo+1, dy+1, high, low, precip, snowfall, mo+1, dy+1, yr, mo+1,
             snowd)
