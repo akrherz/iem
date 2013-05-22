5824f4da84ba2c0796d0144b6b8ae7d8da4f4e34
akrherz <akrherz@iastate.edu>
2013-05-22 11:48:59 -0500

    add landon to daily emails

--- scripts/cscap/email_daily_changes.py
+++ scripts/cscap/email_daily_changes.py
@@ -15,7 +15,7 @@ rtype_xref = {
 'application/vnd.openxmlformats-officedocument.wordprocessingml.document': 'MS Document',
               }
 
-EMAILS = ['labend@iastate.edu','akrherz@iastate.edu']
+EMAILS = ['labend@iastate.edu','akrherz@iastate.edu', 'landon@iastate.edu']
 if len(sys.argv) == 2:
     EMAILS = ['akrherz@iastate.edu',]
 
