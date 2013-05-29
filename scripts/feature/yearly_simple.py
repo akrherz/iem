3bb7e50493d44098180d2472ffe7fd62c0a90650
akrherz <akrherz@iastate.edu>
2013-05-29 17:58:10 -0500

    30 may 2013 feature

--- scripts/feature/yearly_simple.py
+++ scripts/feature/yearly_simple.py
@@ -3,38 +3,38 @@ COOP = iemdb.connect('coop', bypass=True)
 ccursor = COOP.cursor()
 
 ccursor.execute("""
- SELECT extract(year from day) as yr, max(p) from 
-(SELECT foo.day, foo.precip + foo2.precip as p from
- (SELECT day, precip from alldata_ia where station = 'IA0000') as foo,
- (SELECT day + '1 day'::interval as d, precip from alldata_ia where station = 'IA0000') as foo2 WHERE foo.day = foo2.d) as foo3
-  GROUP by yr ORDER by yr ASC""")
+select year, sum(precip) from alldata_ia where station = 'IA0000' and month in (3,4,5) GROUP by year ORDER by year ASC
+""")
 
 years = []
 precip = []
 for row in ccursor:
-  print row
   years.append( row[0] )
   precip.append( row[1] )
 
 import matplotlib.pyplot as plt
 import numpy
 
+precip = numpy.array(precip)
+avg = numpy.average(precip)
+
 (fig, ax) = plt.subplots(1,1)
 years = numpy.array(years)
 bars = ax.bar(years - 0.4, precip, fc='b', ec='b')
+ax.plot([1893,2013],[avg,avg], lw=2.0, color='k', zorder=2)
 bars[-1].set_facecolor('r')
 bars[-1].set_edgecolor('r')
 for i, bar in enumerate(bars):
-  if precip[i] > precip[-1]:
-    ax.text(years[i], precip[i]+0.1, "%.0f" % (years[i],), ha='center') 
+  if precip[i] >= 14:
+    ax.text(years[i], precip[i]+0.1, "%s\n%.02f" % (years[i],precip[i],), ha='center') 
 
-ax.set_xlabel("*2013 thru 21 April")
+ax.set_xlabel("*2013 thru 29 May")
 ax.set_xlim(1892.5, 2013.5)
 ax.grid(True)
-ax.set_ylabel("Maximum Precipitation [inch]")
-ax.set_title("Iowa Yearly Maximum Two Day Areal Averaged Precip")
+ax.set_ylabel("Total Precipitation [inch]")
+ax.set_title("1893-2013 Iowa Areal Averaged Precip (March, April, May)")
 
-fig.savefig('test.ps')
+fig.savefig('test.svg')
 
 import iemplot
 iemplot.makefeature('test')
