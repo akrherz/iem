14277036c8fc08b3a86c78619a6464b772345e12
akrherz <akrherz@iastate.edu>
2013-06-05 18:13:15 -0500

    features

--- scripts/feature/yearly_simple.py
+++ scripts/feature/yearly_simple.py
@@ -1,38 +1,47 @@
 import iemdb
+import matplotlib.patheffects as PathEffects
+
 COOP = iemdb.connect('coop', bypass=True)
 ccursor = COOP.cursor()
 
 ccursor.execute("""
-select year, sum(precip) from alldata_ia where station = 'IA0000' and month in (3,4,5) GROUP by year ORDER by year ASC
+select year, max(high), min(high) from alldata_ia where station = 'IA2203' and 
+ month = 6 and sday < '0606' GROUP by year ORDER by year ASC
 """)
 
 years = []
-precip = []
+highs = []
+lows = []
 for row in ccursor:
   years.append( row[0] )
-  precip.append( row[1] )
+  highs.append( row[1] )
+  lows.append( row[2] )
 
 import matplotlib.pyplot as plt
 import numpy
 
-precip = numpy.array(precip)
-avg = numpy.average(precip)
+highs = numpy.array(highs)
+lows = numpy.array(lows)
+avg = numpy.average(highs)
 
 (fig, ax) = plt.subplots(1,1)
 years = numpy.array(years)
-bars = ax.bar(years - 0.4, precip, fc='b', ec='b')
-ax.plot([1893,2013],[avg,avg], lw=2.0, color='k', zorder=2)
+bars = ax.bar(years - 0.4, highs - lows, bottom=lows, fc='b', ec='b')
+#ax.plot([1893,2013],[avg,avg], lw=2.0, color='k', zorder=2)
 bars[-1].set_facecolor('r')
 bars[-1].set_edgecolor('r')
 for i, bar in enumerate(bars):
-  if precip[i] >= 14:
-    ax.text(years[i], precip[i]+0.1, "%s\n%.02f" % (years[i],precip[i],), ha='center') 
+  if highs[i] <= highs[-1]:
+    bar.set_facecolor('r')
+    bar.set_edgecolor('r')
+    txt= ax.text(years[i], lows[i]-2, "%s\n%.0f" % (years[i],highs[i],), ha='center', va='top') 
+    txt.set_path_effects([PathEffects.withStroke(linewidth=2, foreground="w")])
 
-ax.set_xlabel("*2013 thru 29 May")
-ax.set_xlim(1892.5, 2013.5)
+#ax.set_xlabel("*2013 thru 29 May")
+ax.set_xlim(1877.5, 2013.5)
 ax.grid(True)
-ax.set_ylabel("Total Precipitation [inch]")
-ax.set_title("1893-2013 Iowa Areal Averaged Precip (March, April, May)")
+ax.set_ylabel(r"High Temperature $^\circ$F")
+ax.set_title("1879-2013 1-5 June Des Moines Daily High Temperature Range")
 
 fig.savefig('test.svg')
 
