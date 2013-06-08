2866cd4c02bcb5b1bfa74c3cf3267d0223aacd6a
akrherz <akrherz@iastate.edu>
2013-06-08 10:23:48 -0500

    allow explicit time period specification allow selection of up to 4 products

--- htdocs/nws/wfo_vtec_count.php
+++ htdocs/nws/wfo_vtec_count.php
@@ -3,11 +3,41 @@
  define("IEM_APPID", 144);
  $phenomena = isset($_GET["phenomena"]) ? $_GET["phenomena"] : "TO";
  $significance = isset($_GET["significance"]) ? $_GET["significance"] : "W";
- $year = isset($_REQUEST["year"])? intval($_REQUEST["year"]): date("Y");
+ $phenomena2 = isset($_GET["phenomena2"]) ? $_GET["phenomena2"] : "TO";
+ $significance2 = isset($_GET["significance2"]) ? $_GET["significance2"] : "W";
+ $enabled2 = isset($_REQUEST['enabled2']);
+ $phenomena3 = isset($_GET["phenomena3"]) ? $_GET["phenomena3"] : "TO";
+ $significance3 = isset($_GET["significance3"]) ? $_GET["significance3"] : "W";
+ $enabled3 = isset($_REQUEST['enabled3']);
+ $phenomena4 = isset($_GET["phenomena4"]) ? $_GET["phenomena4"] : "TO";
+ $significance4 = isset($_GET["significance4"]) ? $_GET["significance4"] : "W";
+ $enabled4 = isset($_REQUEST['enabled4']);
  
- $imgurl = sprintf("wfo_vtec_count_%s_%s_%s.png", $year, $phenomena,  
- 		$significance);
+ $year1 = isset($_REQUEST["year1"])? intval($_REQUEST["year1"]): date("Y");
+ $year2 = isset($_REQUEST["year2"])? intval($_REQUEST["year2"]): date("Y");
+ $month1 = isset($_REQUEST["month1"])? intval($_REQUEST["month1"]): 1;
+ $month2 = isset($_REQUEST["month2"])? intval($_REQUEST["month2"]): date("m");
+ $day1 = isset($_REQUEST["day1"])? intval($_REQUEST["day1"]): 1;
+ $day2 = isset($_REQUEST["day2"])? intval($_REQUEST["day2"]): date("d");
  
+ $ts1 = gmmktime(0,0,0, $month1, $day1, $year1);
+ $ts2 = gmmktime(0,0,0, $month2, $day2, $year2);
+ 
+ $imgurl = sprintf("wfo_vtec_count_plot.py?phenomena=%s&significance=%s&",
+ 	$phenomena, $significance);
+ if ($enabled2)
+ 	$imgurl .= sprintf("phenomena2=%s&significance2=%s&",
+ 		$phenomena2, $significance2);
+ if ($enabled3)
+ 	$imgurl .= sprintf("phenomena3=%s&significance3=%s&",
+ 		$phenomena3, $significance3);
+  if ($enabled4)
+ 	$imgurl .= sprintf("phenomena4=%s&significance4=%s&",
+	 	$phenomena4, $significance4);
+
+  $imgurl .= sprintf("sts=%s&ets=%s", gmdate("Ymd", $ts1), 
+  		gmdate("Ymd", $ts2));
+  
  $TITLE = "IEM | NWS WWA Product Counts by Year";
 
  include("../../include/vtec.php"); 
@@ -18,20 +48,21 @@
  <h3>WWA Product Counts by WFO by Year</h3>
  
  <p>This application generates a map of the number of VTEC encoded Watch, Warning, 
- and Advisory (WWA) by NWS Forecast Office for a year.  
- The archive of products goes back to October 2005 and the image is current up 
- until the time reported on the plot.  Note: Not all VTEC products go back to
- 2005.
+ and Advisory (WWA) events by NWS Forecast Office for a time period of your choice.  
+ The archive of products goes back to October 2005.  Note: Not all VTEC products go back to
+ 2005.  You can optionally plot up to 4 different VTEC phenomena and significance 
+ types.
  
  <div class="warning">Please be patient, this app may take 10-30 seconds to
  generate the image!</div>
  
 <form method="GET" name="theform">
 
-<table>
-<tr><th>Year</th><th>Phenomena</th><th>Significance</th></tr>
+<table cellpadding='3' cellspacing='0' border='1'>
+<tr><th>Enabled:?</th><th>Phenomena</th><th>Significance</th></tr>
+
 <tr>
-<td><?php echo yearSelect(2005, $year); ?></td>
+<td></td>
 <td><select name="phenomena">
 <?php 
 reset($vtec_phenomena);
@@ -39,7 +70,6 @@ while (list($key, $value)=each($vtec_phenomena)){
   echo sprintf("<option value='%s'%s>%s</option>\n", $key,
   		($key == $phenomena) ? " SELECTED='SELECTED'": '', $value);
 }
-
 ?>
 </select>
 </td>
@@ -50,13 +80,92 @@ while (list($key, $value)=each($vtec_significance)){
   echo sprintf("<option value='%s'%s>%s</option>\n", $key,
   		($key == $significance) ? " SELECTED='SELECTED'": '', $value);
 }
-
 ?>
 </select>
 </td>
-
 </tr>
-</table>
+<tr>
+<td><input type='checkbox' name='enabled2' <?php if ($enabled2) echo "checked='checked'"; ?>>Include</input></td>
+<td><select name="phenomena2">
+<?php 
+reset($vtec_phenomena);
+while (list($key, $value)=each($vtec_phenomena)){
+  echo sprintf("<option value='%s'%s>%s</option>\n", $key,
+  		($key == $phenomena2) ? " SELECTED='SELECTED'": '', $value);
+}
+?>
+</select>
+</td>
+<td><select name="significance2">
+<?php 
+reset($vtec_significance);
+while (list($key, $value)=each($vtec_significance)){
+  echo sprintf("<option value='%s'%s>%s</option>\n", $key,
+  		($key == $significance2) ? " SELECTED='SELECTED'": '', $value);
+}
+?>
+</select>
+</td>
+</tr>
+<tr>
+<td><input type='checkbox' name='enabled3' <?php if ($enabled3) echo "checked='checked'"; ?>>Include</input></td>
+<td><select name="phenomena3">
+<?php 
+reset($vtec_phenomena);
+while (list($key, $value)=each($vtec_phenomena)){
+  echo sprintf("<option value='%s'%s>%s</option>\n", $key,
+  		($key == $phenomena3) ? " SELECTED='SELECTED'": '', $value);
+}
+?>
+</select>
+</td>
+<td><select name="significance3">
+<?php 
+reset($vtec_significance);
+while (list($key, $value)=each($vtec_significance)){
+  echo sprintf("<option value='%s'%s>%s</option>\n", $key,
+  		($key == $significance3) ? " SELECTED='SELECTED'": '', $value);
+}
+?>
+</select>
+</td>
+</tr>
+
+<tr>
+<td><input type='checkbox' name='enabled4' <?php if ($enabled4) echo "checked='checked'"; ?>>Include</input></td>
+<td><select name="phenomena4">
+<?php 
+reset($vtec_phenomena);
+while (list($key, $value)=each($vtec_phenomena)){
+  echo sprintf("<option value='%s'%s>%s</option>\n", $key,
+  		($key == $phenomena4) ? " SELECTED='SELECTED'": '', $value);
+}
+?>
+</select>
+</td>
+<td><select name="significance4">
+<?php 
+reset($vtec_significance);
+while (list($key, $value)=each($vtec_significance)){
+  echo sprintf("<option value='%s'%s>%s</option>\n", $key,
+  		($key == $significance4) ? " SELECTED='SELECTED'": '', $value);
+}
+?>
+</select>
+</td>
+</tr>
+<tr><th colspan='3'>Time Period (UTC Dates)</th></tr>
+<tr><td colspan='3'><strong>Start Date:</strong>
+  <?php yearSelect2(2005, $year1, 'year1'); ?>
+  <?php monthSelect2($month1, 'month1'); ?>
+  <?php echo daySelect2($day1, 'day1'); ?>  
+  </td></tr>
+<tr><td colspan='3'><strong>End Date:</strong>
+  <?php yearSelect2(2005, $year2, 'year2'); ?>
+  <?php monthSelect2($month2, 'month2'); ?>
+  <?php echo daySelect2($day2, 'day2'); ?> (exclusive)  
+  </td></tr>
+  </table>
 
 <p><input type="submit" value="Generate Map" />
 </form>
--- htdocs/nws/wfo_vtec_count_plot.py
+++ htdocs/nws/wfo_vtec_count_plot.py
@@ -15,27 +15,47 @@ bins = numpy.array([0,1,3,5,10,20,35,50,75,100,150,200,250,500,750,1000])
 POSTGIS = psycopg2.connect(database='postgis', host='iemdb', user='nobody')
 pcursor = POSTGIS.cursor()
 
-def run(phenomena, significance, year, mc, key):
+def run(phenomena, significance, sts, ets, mc, key):
     """ Figure out what the user wanted and do it! """
+    pstr = []
+    subtitle = ""
+    title = ""
+    for p,s in zip(phenomena, significance):
+        pstr.append("(phenomena = '%s' and significance = '%s')" % (p,s))
+        subtitle += "%s.%s " % (p, s)
+        title += "%s %s" % (vtec._phenDict.get(p, p),
+                        vtec._sigDict.get(s, s))
+    if len(phenomena) > 1:
+        title = "VTEC Unique Event"
+    pstr = " or ".join( pstr )
+    pstr = "(%s)" % (pstr,)
 
     pcursor.execute("""
 with total as (
-select distinct wfo, eventid from warnings_"""+`year`+""" 
- where phenomena = %s and significance = %s
+select distinct wfo, extract(year from issue at time zone 'UTC'), phenomena, 
+significance, eventid from warnings 
+ where """+ pstr +""" and 
+ issue >= '%s 00:00+00' and issue < '%s 00:00+00'
 )
-SELECT wfo, count(*) from total GROUP by wfo
+SELECT wfo, count(*) from total GROUP by wfo ORDER by count DESC
 
-    """, (phenomena, significance))
+    """ % (sts.strftime("%Y-%m-%d"), ets.strftime("%Y-%m-%d")))
     data = {}
+    first = True
     for row in pcursor:
+        if first:
+            if row[1] > 1000:
+                global bins
+                bins = numpy.array([0,1,5,10,50,100,150,200,250,500,750,1000,1250,1500,2000])
+                
+            first = False
         data[ row[0]] = row[1]
 
     p = plot.MapPlot(sector='nws',
-                 title='%s %s %s Counts by NWS Office' % (year,
-                        vtec._phenDict.get(phenomena, phenomena),
-                        vtec._sigDict.get(significance, significance)),
-                 subtitle='Valid %s, based on VTEC: %s.%s' % (utc.strftime("%d %b %Y %H%M UTC"),
-                                                phenomena, significance))
+                 title='%s Counts by NWS Office' % (title,),
+                 subtitle='Valid %s - %s UTC, based on VTEC: %s' % (sts.strftime("%d %b %Y"),
+                                                                    ets.strftime("%d %b %Y"),
+                                                subtitle))
     p.fill_cwas(data, bins=bins)
     p.postprocess(web=True, memcache=mc, memcachekey=key, memcacheexpire=1800)
 
@@ -43,13 +63,31 @@ if __name__ == '__main__':
     mc = memcache.Client(['iem-memcached:11211'], debug=0)
     
     form = cgi.FieldStorage()
-    phenomena = form.getfirst('phenomena', 'TO')[:2]
-    significance = form.getfirst('significance', 'W')[0]
-    year = int(form.getfirst('year', 2013))
-    key = "wfo_vtec_count_%s_%s_%s.png" % (year, phenomena, significance)
+    phenomena = []
+    phenomena.append( form.getfirst('phenomena', 'TO')[:2] )
+    phenomena.append( form.getfirst('phenomena2', ' ')[:2] )
+    phenomena.append( form.getfirst('phenomena3', ' ')[:2] )
+    phenomena.append( form.getfirst('phenomena4', ' ')[:2] )
+    while ' ' in phenomena:
+        phenomena.remove(' ')
+    significance = []
+    significance.append( form.getfirst('significance', 'W')[0] )
+    significance.append( form.getfirst('significance2', ' ')[0] )
+    significance.append( form.getfirst('significance3', ' ')[0] )
+    significance.append( form.getfirst('significance4', ' ')[0] )
+    while ' ' in significance:
+        significance.remove(' ')
+    
+    sts = datetime.datetime.strptime(form.getfirst('sts', '20130101'), '%Y%m%d')
+    ets = datetime.datetime.strptime(form.getfirst('ets', '20140101'), '%Y%m%d')
+    
+    key = "wfo_vtec_count_"
+    for p,s in zip(phenomena, significance):
+        key += "%s.%s_" % (p,s)
+    key += "%s_%s.png" % (sts.strftime("%Y%m%d"), ets.strftime("%Y%m%d"))
     res = mc.get(key)
     if res:
         print 'Content-type: image/png\n'
         sys.stdout.write( res )
     else:
-        run(phenomena, significance, year, mc, key)
+        run(phenomena, significance, sts, ets, mc, key)
