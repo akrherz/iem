357e7ef563d0f64e2604818197d532076e764e7e
akrherz <akrherz@iastate.edu>
2013-06-10 12:06:23 -0500

    migration to make script work, this script is unused

--- scripts/iemre/precip_departure.py
+++ scripts/iemre/precip_departure.py
@@ -1,39 +1,47 @@
+"""
+ Precipitation departure from the climatology grid
+"""
 import netCDF4
 import numpy
-import mx.DateTime
+import datetime
+from pyiem import iemre
+from pyiem.plot import MapPlot
+from mpl_toolkits.basemap import maskoceans
 
-offset1 = int((mx.DateTime.DateTime(2012,5,1) - mx.DateTime.DateTime(2012,1,1)).days)
-offset2 = int((mx.DateTime.DateTime(2012,6,6) - mx.DateTime.DateTime(2012,1,1)).days)
+sts = datetime.datetime(2013,5,1)
+ets = datetime.datetime(2013,6,10)
 
-nc = netCDF4.Dataset("/mesonet/data/iemre/2012_mw_daily.nc", 'r')
+offset1 = iemre.daily_offset(sts)
+offset2 = iemre.daily_offset(ets)
+
+nc = netCDF4.Dataset("/mesonet/data/iemre/%s_mw_daily.nc" % (sts.year,), 'r')
 p01d = nc.variables['p01d']
 
 ncc = netCDF4.Dataset("/mesonet/data/iemre/mw_dailyc.nc", 'r')
 cp01d = ncc.variables['p01d']
 for i in range(offset1,offset2):
-  if cp01d[i,5,5] > 20:
-    print i, p01d[i,5,5], cp01d[i,5,5]
+    if cp01d[i,5,5] > 20:
+        print i, p01d[i,5,5], cp01d[i,5,5]
 
 diff = (numpy.sum(p01d[offset1:offset2,:,:],0) -  numpy.sum(cp01d[offset1:offset2,:,:],0)) / 25.4
 print numpy.sum(p01d[offset1:offset2,5,5]), numpy.sum(cp01d[offset1:offset2,5,5])
 print numpy.max(diff), numpy.min(diff)
 
-cfg = {'_title': '1 May - 6 June 2012 Precipitation Departure from Normal',
-       '_valid': '1 May - 6 June 2012',
-       #'cnFillMode': 'CellFill',
-       '_midwest': True,
- 'cnLevelSelectionMode' : 'ManualLevels',
- 'cnLevelSpacingF'      : 1.,
- 'cnMinLevelValF'       : -7.,
- 'cnMaxLevelValF'       : 7.,
-       'wkColorMap' : 'hotcolr_19lev',
-        'nglSpreadColorStart': -1,
-        'nglSpreadColorEnd'  : 2,
-       'lbTitleString': '[inch]'}
+lons = nc.variables['lon'][:]
+lats = nc.variables['lat'][:]
+x,y = numpy.meshgrid(lons, lats)
+diff = maskoceans(x, y, diff)
+extra = lons[-1] + (lons[-1] - lons[-2])
+lons = numpy.concatenate([lons, [extra,]])
 
-import iemplot
-tmpfp = iemplot.simple_grid_fill(nc.variables['lon'][:], nc.variables['lat'][:], diff, cfg)
-iemplot.makefeature(tmpfp)
+extra = lats[-1] + (lats[-1] - lats[-2])
+lats = numpy.concatenate([lats, [extra,]])
+x,y = numpy.meshgrid(lons, lats)
+
+m = MapPlot(sector='midwest')
+m.pcolormesh(x, y, diff, numpy.arange(-8,8.1,1))
+m.postprocess(view=True)
 
 nc.close()
 ncc.close()
+
