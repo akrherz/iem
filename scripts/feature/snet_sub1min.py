d08c0c416aa6a330e581cd75ca9577ceaac7af69
akrherz <akrherz@iastate.edu>
2013-06-25 17:13:06 -0500

    24 jun feature

--- scripts/feature/snet_sub1min.py
+++ scripts/feature/snet_sub1min.py
@@ -12,7 +12,7 @@ tmpf = []
 dwpf = []
 pday = []
 lines = []
-now = mx.DateTime.DateTime(2012,5,3,0,0)
+now = mx.DateTime.DateTime(2013,6,23,0,0)
 txt2drct = {
  'N': '360',
  'North': '360',
@@ -35,7 +35,7 @@ txt2drct = {
  'NW': '315',
  'NNW': '335'}
 
-for line in open('bussey.txt'):
+for line in open('farmhamville.txt'):
   if line.find("Min") > 0 or line.find("Max") > 0:
     continue
   ts = mx.DateTime.strptime(line[7:21], '%H:%M %m/%d/%y')
@@ -51,8 +51,8 @@ for line in open('bussey.txt'):
     ts += mx.DateTime.RelativeDateTime(seconds=delta)
     t = float(line[42:45])
     rh = float(line[47:50])
-    tmpf.append( t - 4 )
-    dwpf.append( mesonet.dwpf(t -4,rh) )
+    tmpf.append( t  )
+    dwpf.append( mesonet.dwpf(t,rh) )
     pres.append( float(line[52:57]) * 33.86375)
     sped.append( float(line[26:28]))
     pday.append( float(line[59:64]))
@@ -60,8 +60,8 @@ for line in open('bussey.txt'):
     valid.append( ts.ticks() )
   lines = []
 
-sts = mx.DateTime.DateTime(2012,5,3,7,30)
-ets = mx.DateTime.DateTime(2012,5,3,9,16)
+sts = mx.DateTime.DateTime(2013,6,23,8,45)
+ets = mx.DateTime.DateTime(2013,6,23,9,46)
 interval = mx.DateTime.RelativeDateTime(minutes=15)
 xticks = []
 xticklabels = []
@@ -83,8 +83,9 @@ fancybox = mpatches.FancyBboxPatch(
         boxstyle='round', alpha=0.2, facecolor='#7EE5DE', edgecolor='#EEEEEE', zorder=1)
 ax[0].add_patch( fancybox)
 ax[0].plot(valid, numpy.array(tmpf) , label='Air')
+print tmpf, max(tmpf)
 ax[0].plot(valid, numpy.array(dwpf), color='g' , label='Dew Point')
-ax[0].set_ylim(40,90)
+ax[0].set_ylim(40,92)
 fancybox = mpatches.FancyBboxPatch(
         [mx.DateTime.DateTime(2012,5,3,8,6).ticks(),
          0], 60*18, 360,
@@ -102,7 +103,7 @@ fancybox = mpatches.FancyBboxPatch(
 zorder=1)
 ax[2].add_patch( fancybox)
 ax[2].plot(valid, pres, 'b', label="Pressure [mb]")
-ax[2].set_ylim(1000,1006)
+ax[2].set_ylim(1006,1014)
 #ax3 = ax[2].twinx()
 #ax3.plot(valid, pday, 'r')
 #ax3.set_ylabel("Daily Precip [inch]")
@@ -113,13 +114,13 @@ ax[0].set_xlim( sts.ticks(), ets.ticks() )
 import matplotlib.font_manager
 prop = matplotlib.font_manager.FontProperties(size=12)
 
-ax[0].legend(loc=1, prop=prop, ncol=2)
+ax[0].legend(loc=2, prop=prop, ncol=2)
 ax[2].legend(loc=1, prop=prop)
 ax[0].grid(True)
 ax[1].grid(True)
 ax[2].grid(True)
-ax[0].set_title("Bussey - Twin Cedars SchoolNet8 Site (~ $\Delta$6 second data)")
-ax[2].set_xlabel("Morning of 3 May 2012, * -4$^{\circ}\mathrm{F}$ temp correction applied")
+ax[0].set_title("Farnhamville SchoolNet8 Site (~ $\Delta$6 second data)")
+ax[2].set_xlabel("Morning of 23 Jun 2013")
 #ax[2].set_ylabel("Pressure [mb]")
 #[t.set_color('red') for t in ax3.yaxis.get_ticklines()]
 #[t.set_color('red') for t in ax3.yaxis.get_ticklabels()]
@@ -127,8 +128,8 @@ ax[2].set_xlabel("Morning of 3 May 2012, * -4$^{\circ}\mathrm{F}$ temp correctio
 [t.set_color('red') for t in ax2.yaxis.get_ticklabels()]
 [t.set_color('b') for t in ax[2].yaxis.get_ticklines()]
 [t.set_color('b') for t in ax[2].yaxis.get_ticklabels()]
-[t.set_color('b') for t in ax[0].yaxis.get_ticklines()]
-[t.set_color('b') for t in ax[0].yaxis.get_ticklabels()]
+#[t.set_color('b') for t in ax[0].yaxis.get_ticklines()]
+#[t.set_color('b') for t in ax[0].yaxis.get_ticklabels()]
 [t.set_color('b') for t in ax[1].yaxis.get_ticklines()]
 [t.set_color('b') for t in ax[1].yaxis.get_ticklabels()]
 ax[1].set_ylabel("Wind Direction")
@@ -136,8 +137,8 @@ ax[0].set_ylabel("Temperature $^{\circ}\mathrm{F}$")
 ax[1].set_yticks( numpy.arange(0,361,90) )
 ax[1].set_ylim(0,361)
 ax[1].set_yticklabels( ['N','E','S','W','N'] )
-ax[2].set_yticks( numpy.arange(1000,1007) )
-ax[2].set_yticklabels( numpy.arange(1000,1007) )
+ax[2].set_yticks( numpy.arange(1006,1014) )
+ax[2].set_yticklabels( numpy.arange(1006,1015) )
 fig.savefig('test.ps')
 import iemplot
 iemplot.makefeature('test')
