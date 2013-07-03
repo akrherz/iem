b4fa422a8a9a306e0c9c5fe667209b99b84910e0
akrherz <akrherz@iastate.edu>
2013-07-03 09:28:40 -0500

    feature script update

--- scripts/feature/warnings/vtec_final_status_by_wfo.py
+++ scripts/feature/warnings/vtec_final_status_by_wfo.py
@@ -5,8 +5,8 @@ dbconn = psycopg2.connect(database='postgis', host='iemdb', user='nobody')
 cursor = dbconn.cursor()
 
 cursor.execute("""
- SELECT wfo, sum(case when status = 'NEW' then 1 else 0 end), count(*) from
- warnings where phenomena = 'SV' and significance = 'W' and gtype = 'P'
+ SELECT wfo, sum(case when expire - issue >= '1 hour'::interval then 1 else 0 end), count(*) from
+ warnings where phenomena = 'TO' and significance = 'W' and gtype = 'P'
  and issue > '2007-10-01' GROUP by wfo
 """)
 
@@ -15,7 +15,7 @@ for row in cursor:
     data[ row[0] ] = row[1] / float(row[2]) * 100.
 
 m = MapPlot(sector='nws',
-            title="Percentage of Severe T'Storm Warnings without SVS Update",
+            title="Percentage of Tornado Warnings AOA 60 minutes long ",
             subtitle='period: 1 Oct 2007 - 21 Jun 2013')
 
 m.fill_cwas(data)
