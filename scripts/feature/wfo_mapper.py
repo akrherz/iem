02315801b6b6df42427027ec1b2f18826c0b98bc
akrherz <akrherz@iastate.edu>
2013-07-05 07:07:21 -0500

    sundry

--- scripts/feature/wfo_mapper.py
+++ scripts/feature/wfo_mapper.py
@@ -3,33 +3,26 @@ import numpy
 from pyiem import plot
 
 #bins = numpy.array([0,1,14,31,91,182,273,365,730,1460,2920,3800])
-bins = numpy.arange(0,110,5)
+bins = numpy.arange(0,12000.1,1000.)
 
-POSTGIS = iemdb.connect('postgis', bypass=True)
+POSTGIS = iemdb.connect('afos', bypass=True)
 pcursor = POSTGIS.cursor()
-
 pcursor.execute("""
-with total as (
-select distinct wfo, eventid from warnings_2013 
- where phenomena = 'FF' and significance = 'W'
-)
-SELECT wfo, count(*) from total GROUP by wfo
+select wfo, avg(length) from (SELECT substr(source,2,3) as wfo, length(data)from products
+  WHERE substr(pil,1,3) = 'AFD' and entered > '2009-01-01' 
+  and source is not null ) as foo GROUP by wfo
 """)
 
-#pcursor.execute("""
-# select (case when source = 'TJSJ' then 'TSJU' else source end) as src, 
-# count(*) from products where 
-# substr(pil,1,3) = 'FWS' and entered > '2009-01-01' 
-# and source is not null group by src order by count
-#""")
 data = {}
 for row in pcursor:
+    
+    print row
     data[ row[0] ] = row[1] 
 
 p = plot.MapPlot(sector='nws',
-                 title='2013 Flash Flood Warnings by NWS Office',
-                 subtitle='based on VTEC (FF.W) products: 1 Jan 2013 - 7 Jun 2013')
-p.fill_cwas(data, bins=bins, lblformat='%.0f')
+                 title='Average Character Count of Area Forecast Discussions per Day',
+                 subtitle='based on IEM archive: 1 Jan 2009 - 3 Jul 2013')
+p.fill_cwas(data, bins=bins, lblformat='%.0f', units='characters')
 p.postprocess(filename='test.png')
 #import iemplot
 #iemplot.makefeature('test')
