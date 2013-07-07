f4132b4704665c55cbc1f27bbc61d58b2560de8a
akrherz <akrherz@iastate.edu>
2013-07-07 18:00:31 -0500

    disable until upstream is fixed

--- scripts/ingestors/rwis/awosMETAR.py
+++ scripts/ingestors/rwis/awosMETAR.py
@@ -12,16 +12,16 @@ data = {}
 for line in open('/mesonet/data/incoming/iaawos_metar.txt', 'r'):
     m = re.match("METAR K(?P<id>[A-Z1-9]{3})", line)
     if not m:
-      continue
+        continue
     d = m.groupdict()
     data[ d['id'] ] = line
     
 fd, path = tempfile.mkstemp()
 #os.write(fd,  '000\r\nNZUS99 KDMX %s\r\n' % (gmt.strftime("%d%H%M"),)) 
-for id in data.keys():
-    os.write(fd, '%s=\r\n' % (data[id].strip().replace("METAR ", ""),))
+for sid in data.keys():
+    os.write(fd, '%s=\r\n' % (data[sid].strip().replace("METAR ", ""),))
 os.close(fd)
-p = subprocess.Popen("/home/ldm/bin/pqinsert -p 'data c 000000000000 LOCDSMMETAR.dat LOCDSMMETAR.dat txt' %s" % (path,),
-                     shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT) 
-os.waitpid(p.pid, 0)
+#p = subprocess.Popen("/home/ldm/bin/pqinsert -p 'data c 000000000000 LOCDSMMETAR.dat LOCDSMMETAR.dat txt' %s" % (path,),
+#                     shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT) 
+#os.waitpid(p.pid, 0)
 os.remove(path) 
