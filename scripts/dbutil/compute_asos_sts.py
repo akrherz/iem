b8d7e459f93de2d9787d14e6882c1378e74fe21a
akrherz <akrherz@iastate.edu>
2013-07-10 15:51:39 -0500

    compute archive_end as well, when necessary

--- scripts/dbutil/compute_asos_sts.py
+++ scripts/dbutil/compute_asos_sts.py
@@ -6,6 +6,12 @@
 import iemdb
 import network
 import sys
+import datetime
+import pytz
+
+basets = datetime.datetime.now()
+basets = basets.replace(tzinfo=pytz.timezone("America/Chicago"))
+
 asos = iemdb.connect('asos', bypass=True)
 acursor = asos.cursor()
 mesosite = iemdb.connect('mesosite')
@@ -20,7 +26,7 @@ if len(keys) > 1:
 else:
     ids = "('%s')" % (keys[0],)
 
-acursor.execute("""SELECT station, min(valid) from alldata 
+acursor.execute("""SELECT station, min(valid), max(valid) from alldata 
   WHERE station in %s GROUP by station 
   ORDER by min ASC""" % (ids,))
 for row in acursor:
@@ -34,6 +40,23 @@ for row in acursor:
     if mcursor.rowcount == 0:
         print 'ERROR: No rows updated'
   
+    # Site without data in past year is offline!
+    if (basets - row[2]).days  > 365:
+        if table.sts[station]['archive_end'] != row[2]:
+            print 'Updated %s ETS WAS: %s NOW: %s' % (station, 
+                    table.sts[station]['archive_end'], row[2])
+  
+            mcursor.execute("""UPDATE stations SET archive_end = %s 
+                 WHERE id = %s and network = %s""" , (row[2], station, net) )
+    # If it was offline and now is on, correct this
+    if ((basets - row[2]).days  < 365 and 
+        table.sts[station]['archive_end'] is not None):
+        print 'Updated %s ETS WAS: %s NOW: None' % (station, 
+                    table.sts[station]['archive_end'])
+  
+        mcursor.execute("""UPDATE stations SET archive_end = null 
+                 WHERE id = %s and network = %s""" , ( station, net) )
+    
 mcursor.close()
 mesosite.commit()
 mesosite.close()
