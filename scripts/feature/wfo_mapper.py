cd5fc7aa37ae3c0f2e02732004cb24970298a9f3
akrherz <akrherz@iastate.edu>
2013-07-12 11:50:23 -0500

    12 jul g+ posts

--- scripts/feature/wfo_mapper.py
+++ scripts/feature/wfo_mapper.py
@@ -3,26 +3,29 @@ import numpy
 from pyiem import plot
 
 #bins = numpy.array([0,1,14,31,91,182,273,365,730,1460,2920,3800])
-bins = numpy.arange(0,12000.1,1000.)
+bins = numpy.array([0,1,14,31,91,135,182,227,273,365,730,1460,])
+#bins = numpy.arange(0,12000.1,1000.)
 
-POSTGIS = iemdb.connect('afos', bypass=True)
+POSTGIS = iemdb.connect('postgis', bypass=True)
 pcursor = POSTGIS.cursor()
 pcursor.execute("""
-select wfo, avg(length) from (SELECT substr(source,2,3) as wfo, length(data)from products
-  WHERE substr(pil,1,3) = 'AFD' and entered > '2009-01-01' 
-  and source is not null ) as foo GROUP by wfo
+ select wfo, max(d) from (select wfo, 
+ issue - lag(issue) over (partition by wfo order by issue ASC) as d 
+ from warnings where significance = 'W' and phenomena = 'TO' and 
+ gtype = 'P') as foo GROUP by wfo ORDER by max ASC
 """)
 
 data = {}
 for row in pcursor:
-    
+    if row[1] is None:
+        continue
     print row
-    data[ row[0] ] = row[1] 
+    data[ row[0] ] = row[1].days
 
 p = plot.MapPlot(sector='nws',
-                 title='Average Character Count of Area Forecast Discussions per Day',
-                 subtitle='based on IEM archive: 1 Jan 2009 - 3 Jul 2013')
-p.fill_cwas(data, bins=bins, lblformat='%.0f', units='characters')
+                 title='Maximum Number of Days in-between issuing Tornado Warning',
+                 subtitle='based on IEM archive: 1 Jan 2002 - 12 Jul 2013')
+p.fill_cwas(data, bins=bins, lblformat='%.0f', units='days')
 p.postprocess(filename='test.png')
 #import iemplot
 #iemplot.makefeature('test')
