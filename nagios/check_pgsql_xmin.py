5eb0dce7f2c287203ace05b6989785b7e4fdac75
akrherz <akrherz@iastate.edu>
2013-07-14 14:09:25 -0500

    add script to track xmin in the database

---
+++ nagios/check_pgsql_xmin.py
@@ -0,0 +1,33 @@
+"""
+Return the maximum xmin in the database 
+"""
+import os
+import sys
+import stat
+import psycopg2
+IEM = psycopg2.connect(database='iem', host='iemdb', user='nobody')
+icursor = IEM.cursor()
+
+def check():
+    icursor.execute("""
+    SELECT datname, age(datfrozenxid) FROM pg_database
+    ORDER by age DESC LIMIT 1
+    """)
+    row = icursor.fetchone()
+
+    return row
+    
+if __name__ == '__main__':
+    dbname, count = check()
+    if count < 200000000:
+        print 'OK - %s %s |count=%s;200000000;215000000;220000000' % (count, 
+                                                            dbname, count)
+        sys.exit(0)
+    elif count < 215000000:
+        print 'WARNING - %s %s |count=%s;200000000;215000000;220000000' % (count, 
+                                                            dbname, count)
+        sys.exit(1)
+    else:
+        print 'CRITICAL - %s %s |count=%s;200000000;215000000;220000000' % (count, 
+                                                            dbname, count)
+        sys.exit(2)
