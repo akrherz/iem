75d7c6138f1fa4a1b9a8a8392a0064e61503ce01
akrherz <akrherz@iastate.edu>
2013-07-15 09:09:59 -0500

    replace pyngl with matplotlib

--- scripts/isuag/fancy_4inch.py
+++ scripts/isuag/fancy_4inch.py
@@ -1,24 +1,24 @@
 """
 Generates the nice 4inch soil temperature plot by county
 """
-
-import Ngl
-import numpy
-import mx.DateTime
+from pyiem.plot import MapPlot
+import numpy as np
+import datetime
 import os
 import sys
-import iemdb
+from scipy.interpolate import NearestNDInterpolator
 import network
+import matplotlib.cm as cm
 import psycopg2.extras
 nt = network.Table("ISUAG")
 
-ISUAG = iemdb.connect('isuag', bypass=True)
+ISUAG = psycopg2.connect(database='isuag', host='iemdb', user='nobody')
 icursor = ISUAG.cursor(cursor_factory=psycopg2.extras.DictCursor)
-POSTGIS = iemdb.connect('postgis', bypass=True)
+POSTGIS = psycopg2.connect(database='postgis', host='iemdb', user='nobody')
 pcursor = POSTGIS.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
 day_ago = int(sys.argv[1])
-ts = mx.DateTime.now() - mx.DateTime.RelativeDateTime(days=day_ago)
+ts = datetime.datetime.now() - datetime.timedelta(days=day_ago)
 
 # Query out the data
 soil_obs = []
@@ -55,106 +55,45 @@ ymax    = max(lats) + 2.
 xc      = (xmax-xmin)/(numxout-1)
 yc      = (ymax-ymin)/(numyout-1)
 
-xo = xmin + xc * numpy.arange(0,numxout)
-yo = ymin + yc * numpy.arange(0,numyout)
+xo = xmin + xc * np.arange(0,numxout)
+yo = ymin + yc * np.arange(0,numyout)
 
-analysis = Ngl.natgrid(lons, lats, soil_obs, list(xo), list(yo))
-
-# Generate plot
-rlist = Ngl.Resources()
-rlist.wkColorMap = "BlAqGrYeOrRe"
-
-wks = Ngl.open_wks( "ps","highs",rlist)
-resources = Ngl.Resources()
-
-resources.nglDraw     = False
-resources.nglFrame    = False
-
-resources.cnFillOn              = True     # Turn on contour fill.
-resources.cnInfoLabelOn         = False    # Turn off info label.
-resources.cnLineLabelsOn        = False    # Turn off line labels.
-resources.cnLinesOn        = False    # Turn off line labels.
-
-resources.lbOrientation         = "Horizontal" 
-resources.tiMainString          = "Iowa 4in Soil Temperatures %s" % (ts.strftime("%b %d %Y"), )
-
-resources.cnMinLevelValF = 10
-resources.cnMaxLevelValF = 100
-resources.cnLevelSpacingF = 5
-resources.cnLevelSelectionMode = "ManualLevels"
-
-
-
-resources.lbTitleString = "[Fahrenheit]"
-resources.lbOrientation = "Horizontal"
-
-# turn off grid
-resources.pmTickMarkDisplayMode = "Never"
-
-resources.nglPaperOrientation = "portrait"
-
-
-resources.sfXCStartV = min(xo)
-resources.sfXCEndV   = max(xo)
-resources.sfYCStartV = min(yo)
-resources.sfYCEndV   = max(yo)
-
-resources.mpProjection = "LambertEqualArea"  # Change the map projection.
-resources.mpCenterLonF = -95.
-resources.mpCenterLatF = 42.
-
-resources.mpLimitMode = "LatLon"    # Limit the map view.
-resources.mpMinLonF   = -96.6
-resources.mpMaxLonF   = -90.
-resources.mpMinLatF   = 40.
-resources.mpMaxLatF   = 44.
-resources.mpPerimOn   = False
-resources.pmTickMarkDisplayMode = "Never"
-resources.mpOutlineBoundarySets     = "AllBoundaries"
-resources.mpDataBaseVersion         = "mediumres"
-resources.mpDataSetName             = "Earth..2"
-resources.mpGridAndLimbOn = False
-resources.mpUSStateLineThicknessF = 3
-
-resources.mpFillOn              = True 
-resources.mpFillAreaSpecifiers  = ["Conterminous US",]
-resources.mpSpecifiedFillColors = [0,]
-resources.mpAreaMaskingOn       = True
-resources.mpMaskAreaSpecifiers = ["Conterminous US : Iowa",]
-resources.cnFillDrawOrder       = "Predraw" 
-
-analysis2 = numpy.transpose(analysis)
-contour = Ngl.contour_map(wks,analysis2,resources)
-
-# Draw text!
-txres               = Ngl.Resources()
-txres.txFontHeightF = 0.02
-#txres.txFontHeightF = 0.03
-txres.txFont        = "helvetica-bold"
+#analysis = griddata((lons, lats), soil_obs, (xo, yo) )
+#rbfi = Rbf(lons, lats, soil_obs, function='cubic')
+#analysis = rbfi(xo, yo)
+nn = NearestNDInterpolator((lons, lats), np.array(soil_obs))
+analysis = nn(xo, yo)
 
 # Query out centroids of counties...
 pcursor.execute("""SELECT x(centroid(the_geom)) as lon, 
   y(centroid(the_geom)) as lat 
  from uscounties WHERE state_name = 'Iowa'""")
+clons = []
+clats = []
 for row in pcursor:
-    lat = row['lat']
-    lon = row['lon']
-    smp = sampler(xo,yo,analysis, lon, lat)
+    clats.append( row['lat'] )
+    clons.append( row['lon'] )
 
-    text = Ngl.add_text(wks,contour,"%.0f" % (smp,),lon,lat,txres)
+cobs = nn(clons, clats)
+#cobs = griddata((lons, lats), soil_obs, (clons, clats))
 
-#Ngl.add_text(wks,contour,"Data Currently in Error", -93,42,txres)
-
-Ngl.panel(wks,[contour,],[1,1])
-
-
- 
-Ngl.end()
+m = MapPlot(sector='iowa',
+    title="Iowa 4in Soil Temperatures %s" % (ts.strftime("%b %d %Y"), ),
+    subtitle='Based on gridded analysis of ISUAG network observations'
+)
+m.contourf(xo, yo, analysis, np.arange(10,101,5), cmap=cm.get_cmap('jet'),
+           units=r'$^\circ$F')
+for lo, la, ob in zip(clons, clats, cobs):
+#for lo, la, ob in zip(lons, lats, soil_obs):
+    xi, yi = m.map(lo, la)
+    txt = m.ax.text(xi, yi, "%.0f" % (ob,))
+m.drawcounties()
+m.postprocess(filename='highs.ps')
 
 os.system("convert -trim  highs.ps obs.png")
 routes = "a"
 if day_ago < 4:
-  routes = "ac" 
+    routes = "ac" 
 os.system("/home/ldm/bin/pqinsert -p 'plot %s %s0000 soilt_day%s.png isuag_county_4inch_soil.png png' obs.png" % (
  routes, ts.strftime("%Y%m%d"), day_ago) )
 os.unlink('highs.ps')
