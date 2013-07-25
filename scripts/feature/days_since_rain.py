6b7de41d1f2b3af884b6e8e98c86815b75a98a17
akrherz <akrherz@iastate.edu>
2013-07-24 21:52:17 -0500

    25 jul 2013 feature

--- scripts/feature/days_since_rain.py
+++ scripts/feature/days_since_rain.py
@@ -1,7 +1,6 @@
 # Number of days since the last 0.25 inch rainfall
 
 import sys, os, random
-sys.path.append("../lib/")
 import iemplot
 
 import mx.DateTime
@@ -10,21 +9,10 @@ now = mx.DateTime.now() + mx.DateTime.RelativeDateTime(days=1)
 import pg
 wepp = pg.connect('wepp', 'iemdb', user='nobody')
 
-# Compute normal from the climate database
-sql = """
-select station, 
-  x(geom) as lon, y(geom) as lat, 
-  max(day) as maxday
-from summary 
-WHERE pday >= 0.25
-and (network ~* 'ASOS' or network = 'AWOS')
-GROUP by station, lon, lat
-"""
-
 sql = """
 SELECT x(transform(centroid(the_geom),4326)) as lon, 
        y(transform(centroid(the_geom),4326)) as lat, maxday from
- (SELECT hrap_i, max(valid) as maxday from daily_rainfall_2012 
+ (SELECT hrap_i, max(valid) as maxday from daily_rainfall_2013 
   WHERE rainfall / 25.4 > 0.50 GROUP by hrap_i) as dr, hrap_polygons h
  WHERE h.hrap_i = dr.hrap_i
 """
@@ -37,19 +25,15 @@ rs = wepp.query(sql).dictresult()
 for i in range(len(rs)):
   ts = mx.DateTime.strptime(rs[i]['maxday'], '%Y-%m-%d')
   lats.append( rs[i]['lat'] )
-  lons.append( rs[i]['lon'] + (random.random() * 0.01))
+  lons.append( rs[i]['lon'] )
   vals.append( (now -ts).days )
 
-cfg = {
- 'wkColorMap': 'BlAqGrYeOrRe',
- 'nglSpreadColorStart': 2,
- 'nglSpreadColorEnd'  : -1,
- '_title'             : "Days since last 0.5in plus Daily Rainfall",
- '_valid'             : "%s" % ( now.strftime("%d %b %Y"), ),
- 'lbTitleString'      : "[days]",
-#'cnFillMode'          : 'CellFill',
-}
-# Generates tmp.ps
-tmpfp = iemplot.simple_contour(lons, lats, vals, cfg)
-
-iemplot.makefeature(tmpfp)
+import numpy as np
+from pyiem.plot import MapPlot
+import matplotlib.cm as cm
+m = MapPlot(sector='iowa', title='Days since last 0.5+ inch calendar day rainfall', subtitle='based on NOAA Stage IV Estimates')
+m.contourf(lons, lats, vals, np.arange(0,61,5), cmap=cm.get_cmap('jet'),
+       units='days')
+m.drawcounties()
+m.postprocess(filename='test.ps')
+iemplot.makefeature('test')
