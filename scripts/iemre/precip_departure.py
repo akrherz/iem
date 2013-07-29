9b4535caf5c06412a011c078d0a285dc45d7c2af
akrherz <akrherz@iastate.edu>
2013-07-29 07:48:49 -0500

    9 jul feature

--- scripts/iemre/precip_departure.py
+++ scripts/iemre/precip_departure.py
@@ -7,9 +7,10 @@ import datetime
 from pyiem import iemre
 from pyiem.plot import MapPlot
 from mpl_toolkits.basemap import maskoceans
+import matplotlib.cm as cm
 
-sts = datetime.datetime(2013,5,1)
-ets = datetime.datetime(2013,6,10)
+sts = datetime.datetime(2013,6,1)
+ets = datetime.datetime(2013,7,9)
 
 offset1 = iemre.daily_offset(sts)
 offset2 = iemre.daily_offset(ets)
@@ -32,15 +33,20 @@ lats = nc.variables['lat'][:]
 x,y = numpy.meshgrid(lons, lats)
 diff = maskoceans(x, y, diff)
 extra = lons[-1] + (lons[-1] - lons[-2])
-lons = numpy.concatenate([lons, [extra,]])
+#lons = numpy.concatenate([lons, [extra,]])
 
 extra = lats[-1] + (lats[-1] - lats[-2])
-lats = numpy.concatenate([lats, [extra,]])
+#lats = numpy.concatenate([lats, [extra,]])
 x,y = numpy.meshgrid(lons, lats)
 
-m = MapPlot(sector='midwest')
-m.pcolormesh(x, y, diff, numpy.arange(-8,8.1,1))
-m.postprocess(view=True)
+m = MapPlot(sector='iowa',
+            title='1 June - 9 July 2013 Precipitation Departure',
+            subtitle='2013 vs 1950-2012 Climatology')
+m.contourf(x, y, diff, numpy.arange(-5.,5.1,1.), cmap=cm.get_cmap('RdYlBu'))
+m.drawcounties()
+m.postprocess(filename='test.ps')
+import iemplot
+iemplot.makefeature('test')
 
 nc.close()
 ncc.close()
