dbc12d4e189d0e360aed6dc359bcce7ca0fa5267
akrherz <akrherz@iastate.edu>
2013-07-31 21:36:13 -0500

    1 Aug feature

--- scripts/feature/daily_month_avg.py
+++ scripts/feature/daily_month_avg.py
@@ -7,8 +7,8 @@ ASOS = iemdb.connect("asos", bypass=True)
 acursor = ASOS.cursor()
 acursor.execute("""SELECT valid, tmpf, dwpf 
 from alldata where station = 'DSM' and dwpf > -50 and dwpf < 90 and tmpf > 50 
-and extract(month from valid) = 3 and 
-extract(minute from valid + '10 minutes'::interval) between 0 and 60""")
+and extract(month from valid) = 7 and 
+extract(minute from valid + '10 minutes'::interval) < 11""")
 
 Tmix = numpy.zeros( (100,), 'f')
 cnts = numpy.zeros( (100,), 'f')
@@ -32,17 +32,17 @@ ax = fig.add_subplot(211)
 
 ax.bar(numpy.arange(1913,2013)-0.4, Tmix / cnts * 1000.0, fc='g')
 
-ax.set_title("Des Moines [KDSM] March Humidity [1933-2012]")
+ax.set_title("Des Moines [KDSM] July Humidity [1933-2013]")
 ax.set_ylabel("Avg Mixing Ratio ($g/kg$)")
-ax.set_xlim(1933,2013)
+ax.set_xlim(1933,2014)
 ax.grid(True)
 
 ax2 = fig.add_subplot(212)
-ax2.bar(numpy.arange(1913,2013)-0.4, maxD, fc='b')
+ax2.bar(numpy.arange(1913,2014)-0.4, maxD, fc='b')
 ax2.set_title("Maximum Dew Point ")
 ax2.set_ylabel("Dew Point $^{\circ}\mathrm{F}$")
-ax2.set_ylim(25,65)
-ax2.set_xlim(1933,2013)
+ax2.set_ylim(45,85)
+ax2.set_xlim(1933,2014)
 ax2.grid(True)
 
 import iemplot
