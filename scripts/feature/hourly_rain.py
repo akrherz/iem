af4d0b6c733f6aea812be512c134373065071a2c
akrherz <akrherz@iastate.edu>
2013-08-11 14:57:40 -0500

    12 aug feature

--- scripts/feature/hourly_rain.py
+++ scripts/feature/hourly_rain.py
@@ -8,7 +8,8 @@ climate = []
 icursor.execute("""
  select hr, avg(p) from (select extract(year from valid) as yr, 
  extract(hour from valid) as hr, sum(phour) as p from hourly 
- where valid < '2013-01-01' and extract(month from valid) in (5) 
+ where valid < '2013-01-01' and extract(month from valid) in (6,7,8)
+ and extract(doy from valid) < 224 
  and station = 'DSM' GROUP by yr, hr) as foo GROUP by hr ORDER by hr
 """)
 for row in icursor:
@@ -19,7 +20,7 @@ dhour = []
 icursor.execute("""
  select hr, p from (select extract(year from valid) as yr, 
  extract(hour from valid) as hr, sum(phour) as p from hourly_2013
- where  extract(month from valid) in (5) 
+ where  extract(month from valid) in (6,7,8) 
  and station = 'DSM' GROUP by yr, hr) as foo ORDER by hr
 """)
 for row in icursor:
@@ -34,17 +35,17 @@ import numpy
 
 ax.bar( dhour, d2012, width=0.3, fc='b', ec='b', label='2013: %.2f in' % (sum(d2012),), zorder=2)
 ax.bar( numpy.arange(0,24)-0.3, climate, width=0.3, fc='g', ec='g', label='Climate: %.2f in' % (sum(climate),), zorder=2)
-rect = Rectangle((-0.5,0), 8, 0.9, facecolor="#aaaaaa", zorder=1)
-ax.add_patch(rect)
+#rect = Rectangle((-0.5,0), 8, 0.9, facecolor="#aaaaaa", zorder=1)
+#ax.add_patch(rect)
 ax.set_xticks( (0,4,8,12,16,20) )
 ax.set_xticklabels( ('Mid','4 AM','8 AM', 'Noon', '4 PM', '8 PM') )
-ax.set_xlabel("2013 Total thru 27 May")
+ax.set_xlabel("2013 Total")
 ax.set_ylabel("Precipitation [inch]")
-ax.set_title("Des Moines May Hourly Precipitation")
+ax.set_title("Des Moines 1 June - 11 August Hourly Precipitation")
 ax.set_xlim(-0.5,23.5)
 ax.grid(True)
-ax.legend()
+ax.legend(loc=2)
 
-fig.savefig('test.svg')
+fig.savefig('test.ps')
 import iemplot
 iemplot.makefeature('test')
