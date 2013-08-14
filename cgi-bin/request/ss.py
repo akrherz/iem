2fa594a59881ec49e3099ce44f58ae5f37068ab7
akrherz <akrherz@iastate.edu>
2013-08-14 09:07:52 -0500

    adjust output format of Staurt Smith data as per reqest

--- cgi-bin/request/ss.py
+++ cgi-bin/request/ss.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python
 """
 Return a simple CSV of stuart smith data
+
+Levelogger Reading (ft)    
+Barologger Reading    
+Temp (C)    
+Barologger Air Temp (C)    
+Conductivity (micro-S)
+7.20473    ch1_data_p
+2.68857     ch2_data_p
+21.1       ch1_data_t
+18.19    ch2_data_t
+48       ch1_data_c
 """
 
 import psycopg2
@@ -8,20 +19,28 @@ import cgi
 import sys
 import datetime
 
+lookup = {9100104: "SSP #6",
+          9100135: "SSP #1",
+          9100131: "SSP #7",
+          9100156: "SSP #2"}
+
 def gage_run(sts, ets):
     """ run() """
     
     dbconn = psycopg2.connect(database='other', host='iemdb', user='nobody')
     cursor = dbconn.cursor()
     cursor.execute("""select site_serial, valid, ch1_data_p, ch2_data_p,
-    ch1_data_t, ch2_data_t, ch1_data_c, ch2_data_c from ss_logger_data
+    ch1_data_t, ch2_data_t, ch1_data_c from ss_logger_data
     WHERE valid between %s and %s ORDER by valid ASC""", (sts, ets))
     
-    res = "valid,site_serial,ch1_data_p,ch2_data_p,ch1_data_t,ch2_data_t,ch1_data_c,ch2_data_c\n"
+    res = ("Date,Time,Site ID,Well,Levelogger Reading (ft),Barologger Reading,"
+           +"Temp (C),Barologger Air Temp (C),Conductivity (micro-S)\n")
     for row in cursor:
-        res += "%s,%s,%s,%s,%s,%s,%s,%s\n" % (row[1].strftime("%Y-%m-%d %H:%M"), row[0],
+        res += "%s,%s,%s,%s,%s,%s,%s,%s,%s\n" % (row[1].strftime("%Y-%m-%d"),
+                                              row[1].strftime("%H:%M"), 
+                                              row[0], lookup.get(row[0], "N/A"),
                                          row[1], row[2], row[3], row[4],
-                                         row[5], row[6])
+                                         row[5])
 
     return res.replace("None", "M")
 
