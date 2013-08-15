89900021e67b063f52c5c3263b8a701a8fef3198
akrherz <akrherz@iastate.edu>
2013-08-14 21:42:12 -0500

    sync iem feature scripts, remove some unuseful old plots

--- scripts/feature/daily_month_avg.py
+++ scripts/feature/daily_month_avg.py
@@ -10,9 +10,9 @@ from alldata where station = 'DSM' and dwpf > -50 and dwpf < 90 and tmpf > 50
 and extract(month from valid) = 7 and 
 extract(minute from valid + '10 minutes'::interval) < 11""")
 
-Tmix = numpy.zeros( (100,), 'f')
-cnts = numpy.zeros( (100,), 'f')
-maxD = numpy.zeros( (100,), 'f')
+Tmix = numpy.zeros( (101,), 'f')
+cnts = numpy.zeros( (101,), 'f')
+maxD = numpy.zeros( (101,), 'f')
 for row in acursor:
   dwpc = mesonet.f2c( row[2] )
   e  = 6.112 * math.exp( (17.67 * dwpc) / (dwpc + 243.5));
@@ -27,22 +27,22 @@ for row in acursor:
 import matplotlib.pyplot as plt
 import matplotlib
 
-fig = plt.figure()
+fig = plt.figure(figsize=(9,7))
 ax = fig.add_subplot(211)
 
-ax.bar(numpy.arange(1913,2013)-0.4, Tmix / cnts * 1000.0, fc='g')
+ax.bar(numpy.arange(1913,2014)-0.4, Tmix / cnts * 1000.0, fc='g', ec='g')
 
-ax.set_title("Des Moines [KDSM] July Humidity [1933-2013]")
+ax.set_title("Des Moines [KDSM] July Humidity [1935-2013]")
 ax.set_ylabel("Avg Mixing Ratio ($g/kg$)")
-ax.set_xlim(1933,2014)
+ax.set_xlim(1934,2014)
 ax.grid(True)
 
 ax2 = fig.add_subplot(212)
-ax2.bar(numpy.arange(1913,2014)-0.4, maxD, fc='b')
+ax2.bar(numpy.arange(1913,2014)-0.4, maxD, fc='b', ec='b')
 ax2.set_title("Maximum Dew Point ")
 ax2.set_ylabel("Dew Point $^{\circ}\mathrm{F}$")
-ax2.set_ylim(45,85)
-ax2.set_xlim(1933,2014)
+ax2.set_ylim(65,85)
+ax2.set_xlim(1934,2014)
 ax2.grid(True)
 
 import iemplot
