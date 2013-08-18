211890986634924be444d1ce2b67fcbc68358cdc
akrherz <akrherz@iastate.edu>
2013-08-18 14:25:20 -0500

    Add to app registry and update to newer extjs

--- htdocs/timemachine/index.php
+++ htdocs/timemachine/index.php
@@ -1,17 +1,18 @@
 <?php
 include("../../config/settings.inc.php");
-$TITLE = "IEM | Time Machine";
+$TITLE = "IEM :: Time Machine";
+define("IEM_APPID", 148);
 $THISPAGE="archive-tm";
 $HEADEXTRA = '
-<link rel="stylesheet" type="text/css" href="http://extjs.cachefly.net/ext-3.4.0/resources/css/ext-all.css"/>
-<script type="text/javascript" src="http://extjs.cachefly.net/ext-3.4.0/adapter/ext/ext-base.js"></script>
-<script type="text/javascript" src="http://extjs.cachefly.net/ext-3.4.0/ext-all.js"></script>
+<link rel="stylesheet" type="text/css" href="http://cdn.sencha.com/ext/gpl/3.4.1.1/resources/css/ext-all.css"/>
+<script type="text/javascript" src="http://cdn.sencha.com/ext/gpl/3.4.1.1//adapter/ext/ext-base.js"></script>
+<script type="text/javascript" src="http://cdn.sencha.com/ext/gpl/3.4.1.1/ext-all.js"></script>
 <script type="text/javascript" src="static.js?v=14"></script>
 ';
-include("$rootpath/include/header.php"); 
+include("../../include/header.php"); 
 ?>
 <p style="margin-bottom: 5px;"><strong>IEM Time Machine:</strong> Adjust sliders 
 to select a product of your choice from the archive.</p>
 <div id="theform"></div>
 <img id="imagedisplay" src="timemachine.png" />
-<?php include("$rootpath/include/footer.php"); ?>
+<?php include("../../include/footer.php"); ?>
