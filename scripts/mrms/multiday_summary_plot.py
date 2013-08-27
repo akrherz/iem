2277b002509ca8f47eee92afd9e823a56921fa55
akrherz <akrherz@iastate.edu>
2013-08-26 20:34:30 -0500

    27 aug feature

--- scripts/mrms/multiday_summary_plot.py
+++ scripts/mrms/multiday_summary_plot.py
@@ -5,8 +5,8 @@ import datetime
 import numpy as np
 import matplotlib.cm as cm
 
-sts = datetime.datetime(2013,7,1)
-ets = datetime.datetime(2013,8,8)
+sts = datetime.datetime(2013,8,1)
+ets = datetime.datetime(2013,8,26)
 
 idx0 = iemre.daily_offset(sts)
 idx1 = iemre.daily_offset(ets)
@@ -19,11 +19,12 @@ p01d = np.sum(nc.variables['p01d'][idx0:idx1,:,:],0) / 24.5
 nc.close()
 
 
-m = MapPlot(sector='midwest', title='MRMS 1 July - 8 August 2013 Total Precipitation',
+m = MapPlot(sector='iowa', title='MRMS 1 August - 26 August 2013 Total Precipitation',
             subtitle='Data from NOAA MRMS Project')
 x,y = np.meshgrid(lons, lats)
-m.pcolormesh(x, y, p01d, np.arange(0,21,2), cmap=cm.get_cmap('RdYlBu'),
+m.pcolormesh(x, y, p01d, [0.01,0.1,0.25,0.5,0.75,1,2,3,4,5,6], cmap=cm.get_cmap('RdYlBu'),
              label='inches')
+m.drawcounties()
 m.postprocess(filename='test.ps')
 import iemplot
 iemplot.makefeature('test')
