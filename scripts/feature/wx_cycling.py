f2086fb0dd22e913fd5a966876a250435e514967
akrherz <akrherz@iastate.edu>
2013-09-03 18:37:11 -0500

    4 sep feature

--- scripts/feature/wx_cycling.py
+++ scripts/feature/wx_cycling.py
@@ -7,8 +7,8 @@ import matplotlib.pyplot as plt
 
 (fig, ax) = plt.subplots(1,1)
 
-sts = datetime.datetime(2012,11,26)
-ets = datetime.datetime(2013,4,23)
+sts = datetime.datetime(2013,4,22)
+ets = datetime.datetime(2013,9,4)
 interval = datetime.timedelta(days=7)
 
 lbls = []
@@ -74,12 +74,16 @@ while now > ets:
 
 ax.plot(tsigma, psigma, zorder=2)
 for l,t,p,a in zip(lbls, tsigma, psigma,aligns):
+    # Manual move label some for readiability
+    if l == '7/15':
+        t = float(t) + 0.1
+        p = float(p) + -0.2
     ax.text(t,p, l, va=a, zorder=2)
 
 ax.set_xlim(-2,2)
 ax.set_ylabel("Precipitation Departure $\sigma$")
 ax.set_xlabel("Temperature Departure $\sigma$")
-ax.set_title("26 Nov 2012 - 22 Apr 2013 Iowa\n 14 Day Trailing Departures plotted every 7 days")
+ax.set_title("22 Apr 2013 - 3 Sep 2013 Iowa\n 14 Day Trailing Departures plotted every 7 days")
 ax.grid(True)
 
 fig.savefig('test.ps')
