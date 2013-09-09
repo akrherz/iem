30e88e851fa232343f9eac357da7bf308413abc8
akrherz <akrherz@iastate.edu>
2013-09-09 18:49:46 -0500

    10 sep feature

--- scripts/feature/days_since_T2.py
+++ scripts/feature/days_since_T2.py
@@ -18,13 +18,13 @@ sql = """
  SELECT c.name, c.network, c.id, max_tmpf, x(c.geom) as lon, 
  y(c.geom) as lat, c.climate_site from summary_2013 s, stations c 
  where s.iemid = c.iemid
- and s.day = '2013-05-14' and c.network in ('IA_ASOS', 'AWOS') 
- and max_tmpf > 0 and id not in ('xxx')
+ and s.day = '2013-09-09' and c.network in ('IA_ASOS', 'AWOS') 
+ and max_tmpf > 0 and id not in ('OTM')
  ORDER by id ASC
 """
 
 def compute(ts):
-    now = datetime.date(2013,4,17)
+    now = datetime.date(2013,9,9)
     days = (now - ts).days
     years = int(days / 365)
     months = int((days % 365) / 30)
@@ -34,7 +34,7 @@ def compute(ts):
         return "%sd" % (days,), days
     if years == 0:
         return "%sm" % (months,), days
-    return "%sy%sm" % (years, months), days
+    return "%sy" % (years,), days
 
 lats = []
 lons = []
@@ -46,11 +46,11 @@ for row in icursor:
     cid = row[6]
     ccursor.execute("""
     SELECT max(day) from alldata_ia where station = '%s' 
-    and high >= %.0f and day < '2013-05-14'
+    and high >= %.0f and day < '2013-09-09' and month = 9
     """ % (cid, max_tmpf))
     row2 = ccursor.fetchone()
     if row2[0] is None:
-       continue
+        continue
     lbl, days = compute(row2[0])
     vals.append( "%.0f~C~%s" % (max_tmpf, lbl) )
     lats.append( row[5] )
@@ -61,8 +61,8 @@ cfg = {
  'wkColorMap': 'BlAqGrYeOrRe',
  'nglSpreadColorStart': 2,
  'nglSpreadColorEnd'  : -1,
- '_title'             : "14 May 2013 High Temperature and Days Since as High Temp",
- '_valid'             : "14 May 2013, values in F",
+ '_title'             : "9 Sep 2013 High Temp and Years Since as Warm High Temp",
+ '_valid'             : "9 Sep 2013, values in F",
 #'lbTitleString'      : "[days]",
  '_showvalues'        : True,
  '_format'            : '%s',
