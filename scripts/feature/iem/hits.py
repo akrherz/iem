e1140dee97a7bbc284ed63f05601bd4494271fb2
akrherz <akrherz@iastate.edu>
2013-09-09 21:26:02 -0500

    make more legible

--- scripts/feature/iem/hits.py
+++ scripts/feature/iem/hits.py
@@ -12,7 +12,7 @@ for line in open('hits.txt'):
 fig = plt.figure()
 ax = fig.add_subplot(111)
 
-ax.semilogy(x, y)
+ax.semilogy(x, y, lw=3)
 #ax.set_xlim( x[0].ticks(), x[-1].ticks() )
 xticks = []
 xticklabels = []
@@ -26,10 +26,13 @@ while now < ts1:
         xticklabels.append( now.strftime("%Y"))
     now += interval
 ax.set_xticks(xticks)
-ax.set_xticklabels(xticklabels)
-ax.set_ylabel("Maximum Daily Web Hits")
-ax.set_title("IEM Website Hits Milestones [17 Jun 2001-2013]")
+ax.set_xticklabels(xticklabels, fontsize=20, rotation=90)
+ax.set_xlabel("Year", fontsize=20)
+ax.set_ylabel("Maximum Daily Web Requests", fontsize=20)
+ax.set_title("IEM Web Requests Milestones [17 Jun 2001-2013]", fontsize=18)
 ax.grid(True)
+plt.setp(ax.get_yticklabels(), fontsize=20)
+fig.tight_layout()
 fig.savefig('test.png')
 #import iemplot
 #iemplot.makefeature('test')
