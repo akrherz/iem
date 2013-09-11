456405312122ccb54b4de84f3ed7410017c643da
akrherz <akrherz@iastate.edu>
2013-09-11 11:57:30 -0500

    fix paths

--- cgi-bin/request/gis/lsr.py
+++ cgi-bin/request/gis/lsr.py
@@ -122,7 +122,7 @@ source.Destroy()
 out_ds.Destroy()
 
 # Create zip file, send it back to the clients
-shutil.copyfile("/mesonet/data/gis/meta/4326.prj", fp+".prj")
+shutil.copyfile("/mesonet/www/apps/iemwebsite/data/gis/meta/4326.prj", fp+".prj")
 z = zipfile.ZipFile(fp+".zip", 'w', zipfile.ZIP_DEFLATED)
 z.write(fp+".shp")
 z.write(fp+".shx")
