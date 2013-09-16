c96056ad5a70e994e80fd63d8034c6ccffb16374
akrherz <akrherz@iastate.edu>
2013-09-16 16:03:14 -0500

    17 sep feature

--- scripts/feature/nws/wfo_mapper.py
+++ scripts/feature/nws/wfo_mapper.py
@@ -5,17 +5,24 @@ from pyiem.plot import MapPlot
 #bins = numpy.array([0,1,14,31,91,182,273,365,730,1460,2920,3800])
 #bins = numpy.array([0,1,14,31,91,135,182,227,273,365,730,1460,])
 #bins = numpy.arange(0,12000.1,1000.)
-bins = numpy.arange(0,101,10)
+bins = numpy.arange(0,201,20)
 
 POSTGIS = psycopg2.connect(database='postgis', host='iemdb', user='nobody')
 pcursor = POSTGIS.cursor()
 pcursor.execute("""
-select wfo, avg(length)  from 
-(select wfo, eventid, issue, expire, array_length(regexp_split_to_array(meat, '\s+'),1) as length 
-from (select wfo, eventid, issue, expire, 
-split_part(split_part(report, 'PREPAREDNESS ACTIONS', 2), '&&', 1) as meat from 
-warnings_2013 where phenomena in ('SV','TO') and significance = 'W' and gtype = 'P') as foo ) 
-as foo2 GROUP by wfo ORDER by avg
+
+select wfo, max(case when year = 2013 then count else 0 end) /
+    (sum(case when year < 2013 then count else 0 end)/10.0) * 100.0 as d,
+     max(case when year = 2013 then count else 0 end),
+    sum(case when year < 2013 then count else 0 end)/10.0
+     from 
+    (select wfo, year, count(*) from 
+        (select extract(year from issue) as year, wfo, phenomena, eventid from 
+        warnings WHERE extract(doy from issue) < 259 and issue > '2003-01-01' 
+        and gtype = 'P' and phenomena in ('SV','TO') and significance = 'W') as foo 
+        GROUP by wfo, year) as foo2 
+GROUP by wfo ORDER by d DESC
+
 """)
 
 data = {}
@@ -23,12 +30,12 @@ for row in pcursor:
     if row[1] is None:
         continue
     print row
-    data[ row[0] ] = row[1]
+    data[ row[0] ] = row[1] 
     
 p = MapPlot(sector='nws',
-                 title='2013 SVR+TOR Warnings Approx. Avg Word Count in Text',
-                 subtitle='based on IEM archive: 1 Jan 2013 - 21 Aug 2013, preparedness actions section only')
-p.fill_cwas(data, bins=bins, lblformat='%.0f', units='count')
-p.postprocess(filename='test.png')
-#import iemplot
-#iemplot.makefeature('test')
+                 title='1 Jan - 16 Sep 2013 SVR+TOR Warning Count Percent of Average',
+                 subtitle='Departure from 10 year average 2003-2012, year to date')
+p.fill_cwas(data, bins=bins, lblformat='%.0f', units='%')
+p.postprocess(filename='test.ps')
+import iemplot
+iemplot.makefeature('test')
