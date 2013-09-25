86b11730645abac03a52a01e1fdd83021b958ddd
akrherz <akrherz@iastate.edu>
2013-09-25 09:11:07 -0500

    improve plotting and update to pyIEM

--- scripts/current/q3_today_total.py
+++ scripts/current/q3_today_total.py
@@ -11,23 +11,26 @@ import util
 import pytz
 import gzip
 
-from iem.plot import MapPlot
+from pyiem.plot import MapPlot
 
-def doday(ts):
+def doday(ts, realtime):
     """
     Create a plot of precipitation stage4 estimates for some day
     """
     # Start at 1 AM
     now = ts.replace(hour=1, minute=0)
+    ets = now + datetime.timedelta(hours=24)
     interval = datetime.timedelta(hours=1)
 
     precip = np.zeros( (3500,7000) )
-    while now < ts:
+    lastts = None
+    while now < ets:
         gmt = now.astimezone(pytz.timezone("UTC"))
         # Only need tile 1 and 2 to sufficiently do Iowa
         for tile in range(1,3):
             fn = util.get_fn('1hrad', gmt, tile)
             if os.path.isfile(fn):
+                lastts = now
                 tilemeta, val = util.reader(fn)
                 ysz, xsz = np.shape(val)
                 if tile == 1:
@@ -41,12 +44,17 @@ def doday(ts):
                 print 'Missing 1HRAD MRMS for q3_today_total', fn
         now += interval
     
-   
-    subtitle = 'Total up to %s' % (ts.strftime("%d %B %Y %I:%M %p %Z"),)
-    pqstr = "plot ac %s00 iowa_q2_1d.png iowa_q2_1d.png png" % (
+    lastts = lastts - datetime.timedelta(minutes=1)
+    subtitle = "Total between 12:00 AM and %s" % (
+                                            lastts.strftime("%H:%M %p %Z"),)
+    routes = 'ac'
+    if not realtime:
+        routes = 'a'
+    pqstr = "plot %s %s00 iowa_q2_1d.png iowa_q2_1d.png png" % (routes,
             ts.strftime("%Y%m%d%H"), )
-    m = MapPlot(title="NMQ Q3 Today's Precipitation [inch]",
-                subtitle=subtitle, pqstr=pqstr)
+    m = MapPlot(title="%s NMQ Q3 Today's Precipitation" % (
+                                                    ts.strftime("%-d %b %Y"),),
+                subtitle=subtitle)
         
     clevs = np.arange(0,0.2,0.05)
     clevs = np.append(clevs, np.arange(0.2, 1.0, 0.1))
@@ -54,18 +62,25 @@ def doday(ts):
     clevs = np.append(clevs, np.arange(5.0, 10.0, 1.0))
     clevs[0] = 0.01
 
-    m.contourf(util.XAXIS, util.YAXIS, precip / 24.5, clevs)
+    x,y = np.meshgrid(util.XAXIS, util.YAXIS)
+    m.pcolormesh(x, y, precip / 24.5, clevs, units='inch')
 
     #map.drawstates(zorder=2)
     m.drawcounties()
-    m.postprocess()    
+    m.postprocess(pqstr=pqstr)    
     
 if __name__ == "__main__":
     ''' This is how we roll '''
     if len(sys.argv) == 4:
-        ts = datetime.datetime(int(sys.argv[1]), 
-                                   int(sys.argv[2]), int(sys.argv[3]), 23, 55)
+        date = datetime.datetime(int(sys.argv[1]), int(sys.argv[2]), 
+                                 int(sys.argv[3]), 12, 0)
+        realtime = False
     else:
-        ts = datetime.datetime.now()
-    ts = pytz.timezone("America/Chicago").localize(ts)
-    doday(ts)
+        date = datetime.datetime.now()
+        date = date - datetime.timedelta(minutes=60)
+        date = date.replace(hour=12, minute=0, second=0, microsecond=0)
+        realtime = True
+    # Stupid pytz timezone dance
+    date = date.replace(tzinfo=pytz.timezone("UTC"))
+    date = date.astimezone(pytz.timezone("America/Chicago"))
+    doday(date, realtime)
