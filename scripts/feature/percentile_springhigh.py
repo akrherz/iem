3141cabb8bc2949e99b68cfef1d0a9b1e605e59d
akrherz <akrherz@iastate.edu>
2013-09-24 19:36:23 -0500

    25 sep feature

--- scripts/feature/percentile_springhigh.py
+++ scripts/feature/percentile_springhigh.py
@@ -1,48 +1,42 @@
 # Month percentile 
 import iemdb, network
 import iemplot
+from pyiem.plot import MapPlot
+import matplotlib.cm as cm
 nt = network.Table("IACLIMATE")
 COOP = iemdb.connect("coop", bypass=True)
 ccursor = COOP.cursor()
 ccursor.execute("""
- ---select foo2.station, max(foo2.min) as maxprev, min(foo.d2013) as thisyear, 
- ---sum(case when foo.d2013> foo2.min then 1 else 0 end), count(*) from 
----
- ---  (select station, extract(year from day + '0 months'::interval) as yr, 
- ---  avg((high+low)/2.0) as min from alldata_ia where sday < '0327' 
- ---  GROUP by station, yr) as foo2, 
----
----   (select station, avg((high+low)/2.0) as d2013 from alldata_ia 
----   where sday < '0327' 
----   and station != 'IA0000' and substring(station,2,1) != 'C'
----   and year = 2013 GROUP  by station) as foo 
-
---- where foo.station = foo2.station GROUP by foo2.station
- SELECT station, max(high) from alldata_ia where year = 2013 and
- station != 'IA0000' and substring(station,2,1) != 'C' GROUP by station
+  SELECT station, max(case when year = 2013 then rank else -1 end) as ma, count(*),
+  max(case when year = 2013 then  m else -1 end) from 
+  (SELECT station, year, m, rank() over (partition by station ORDER by m ASC)
+  from
+    (SELECT station, year, min(low) as m from alldata_ia where
+     month > 6 and sday < '0925' and year > 1950 GROUP by station, year) as foo
+  ) as foo2 WHERE station != 'IA0000' and substr(station,3,1) != 'C' 
+  GROUP by station ORDER by ma DESC
 """)
 lats = []
 lons = []
 vals = []
 for row in ccursor:
-    print row
+
     if not nt.sts.has_key(row[0]):
         continue
+    if row[1] < 0 or row[2] < 60 or row[0] in ['IA2364',]:
+        continue
+    print row
     #vals.append( float(row[3]) / float(row[4]) * 100.0 )
-    vals.append( row[1] )
+    vals.append( row[1] / float(row[2]) * 100.0 )
     lats.append( nt.sts[row[0]]['lat'] )
     lons.append( nt.sts[row[0]]['lon'] )
 
-cfg = {
- 'wkColorMap': 'BlAqGrYeOrRe',
- 'nglSpreadColorStart': 2,
- 'nglSpreadColorEnd'  : -1,
- '_title'             : "2013 Maximum Temperature",
- '_valid'             : "1 Jan 2013 - 27 Mar 2013",
- 'lbTitleString'      : "[F]",
- '_showvalues'        : True,
- '_format'            : '%.0f',
-}
-# Generates tmp.ps
-tmpfp = iemplot.simple_contour(lons, lats, vals, cfg)
-iemplot.makefeature(tmpfp)
+import numpy as np
+m = MapPlot(sector='iowa', title='Fall Minimum Temperature Percentile [100=warmest]',
+            subtitle='thru 24 September, 2013 vs 1951-2012')
+cmap = cm.get_cmap('jet')
+m.contourf(lons, lats, vals, np.arange(0,101,10), cmap=cmap)
+m.plot_values(lons, lats, vals, '%.0f')
+m.drawcounties()
+m.postprocess(filename='test.ps')
+iemplot.makefeature('test')
