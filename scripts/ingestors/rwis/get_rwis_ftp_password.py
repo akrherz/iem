be02e732675c848d079d8450a6e5405d285016d6
akrherz <akrherz@iastate.edu>
2013-10-01 15:05:03 -0500

    migrate rwis ftp setting to database, remove hack fixes #1

---
+++ scripts/ingestors/rwis/get_rwis_ftp_password.py
@@ -0,0 +1,11 @@
+'''
+Get RWIS FTP password from the database settings
+'''
+import psycopg2
+DBCONN = psycopg2.connect(database='mesosite', host='iemdb', user='nobody')
+cursor = DBCONN.cursor()
+
+cursor.execute("""SELECT propvalue from properties 
+    where propname = 'rwis_ftp_password'""")
+row = cursor.fetchone()
+print row[0]
