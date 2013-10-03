bb290a409416930047bfd1f5ca575c9e99c50185
akrherz <akrherz@iastate.edu>
2013-10-03 14:07:24 -0500

    TODO: need to finish this at somebody

---
+++ scripts/outgoing/ndfd_to_wxc.py
@@ -0,0 +1,32 @@
+"""
+
+Weather Central 001d0300 Hourly Forecast
+ 19
+  5 Station
+ 40 City
+  2 State
+  7 Lat
+  8 Lon
+ 15 RunTime
+ 15 ValidTime
+  2 dewpoint
+  2 feelslike
+  3 humidity
+  4 precipamount
+ 10 precipitation
+  1 preciprain
+  1 precipsnow
+ 30 skyconditions
+  7 supercell
+  2 temperature
+  2 winddirection
+  2 windspeed
+ KALO                                 Waterloo IA   42.55    -92.4
+2013.07.11.1300 2013.07.11.1300 57 61  87 0.00            0 0
+clearday ss_none 61  N  2
+ KALO                                 Waterloo IA   42.55    -92.4
+2013.07.11.1300 2013.07.11.1400 59 66  76 0.00            0 0
+clearday ss_none 67 NE  1
+
+
+"""
