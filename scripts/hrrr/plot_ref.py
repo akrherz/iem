dca0f64bf3bbcae7f9b73ac21658915e83983ab0
akrherz <akrherz@iastate.edu>
2013-10-09 13:49:10 -0500

    allow specification of exact time on command line optimize grid subsetting, cut runtime from 4m33s to 3m33s catch upstream error without data

--- scripts/hrrr/plot_ref.py
+++ scripts/hrrr/plot_ref.py
@@ -9,23 +9,36 @@ at 20:20 UTC, the 18z run is in.
 
 import pygrib
 from pyiem.plot import MapPlot
+import pyiem.reference as ref
 import numpy as np
 import datetime
 import pytz
 import subprocess
 import os
 import urllib2
+import sys
 
-def run( utc ):
+def compute_bounds(lons, lats):
+    ''' figure out a minimum box to extract data from, save CPU '''
+    dist = ((lats - ref.MW_SOUTH)**2 + (lons - ref.MW_WEST)**2)**0.5
+    x1, y1 = np.unravel_index(dist.argmin(), dist.shape)
+    dist = ((lats - ref.MW_NORTH)**2 + (lons - ref.MW_EAST)**2)**0.5
+    x2, y2 = np.unravel_index(dist.argmin(), dist.shape)
+    return x1, x2, y1, y2
+
+def run( utc , routes):
     ''' Generate the plot for the given UTC time '''
 
     uri = utc.strftime(("http://hrrr.agron.iastate.edu/data/"
                         +"hrrr_reflectivity/hrrr.ref.%Y%m%d%H00.grib2"))
 
     fn = "/tmp/hrrr.ref.tmp.grib2"
-    fp = open(fn, 'wb')
-    fp.write( urllib2.urlopen(uri).read() )
-    fp.close()
+    try:
+        fp = open(fn, 'wb')
+        fp.write( urllib2.urlopen(uri).read() )
+        fp.close()
+    except urllib2.HTTPError:
+        return
 
     grbs = pygrib.open(fn)
 
@@ -45,8 +58,12 @@ def run( utc ):
         g = gs[0]
         if lats is None:
             lats, lons = g.latlons()
-        ref = g['values']
-
+            x1, x2, y1, y2 = compute_bounds(lons, lats)
+            lats = lats[x1:x2,y1:y2]
+            lons = lons[x1:x2,y1:y2]
+            
+        ref = g['values'][x1:x2,y1:y2]
+        
         m = MapPlot(sector='midwest',
                 title='%s UTC HRRR 1 km AGL Reflectivity' % (
                                             utc.strftime("%-d %b %Y %H"),),
@@ -66,8 +83,8 @@ def run( utc ):
                          +"/tmp/hrrr_ref_???.gif > /tmp/hrrr_ref.gif"),
                 shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
 
-    pqstr = ("plot ac %s model/hrrr/hrrr_1km_ref.gif "
-             +"model/hrrr/hrrr_1km_ref_%02i.gif gif") % (
+    pqstr = ("plot %s %s model/hrrr/hrrr_1km_ref.gif "
+             +"model/hrrr/hrrr_1km_ref_%02i.gif gif") % (routes, 
                             utc.strftime("%Y%m%d%H%M"), utc.hour)
     subprocess.call("/home/ldm/bin/pqinsert -p '%s' /tmp/hrrr_ref.gif" % (
                                                             pqstr,),
@@ -80,8 +97,15 @@ def run( utc ):
 if __name__ == '__main__':
     ''' go go gadget '''
     utcnow = datetime.datetime.utcnow()
+    if len(sys.argv) == 5:
+        utcnow = datetime.datetime( int(sys.argv[1]), int(sys.argv[2]),
+                                    int(sys.argv[3]), int(sys.argv[4]))
+        routes = 'a'
+    else:
+        # Two hours ago
+        utcnow = utcnow - datetime.timedelta(hours=2)
+        routes = 'ac'
     utcnow = utcnow.replace(tzinfo=pytz.timezone("UTC"),minute=0,second=0,
                             microsecond=0)
-    # Two hours ago
-    utcnow = utcnow - datetime.timedelta(hours=2)
-    run( utcnow )
+        
+    run( utcnow , routes)
