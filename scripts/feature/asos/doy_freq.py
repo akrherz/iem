687dc9cbf318cf1eda294e0f070c1860aae9c181
akrherz <akrherz@iastate.edu>
2013-10-08 21:37:55 -0500

    9 Oct feature

--- scripts/feature/asos/doy_freq.py
+++ scripts/feature/asos/doy_freq.py
@@ -1,38 +1,79 @@
 import psycopg2
+import numpy as np
+
+def smooth(x,window_len=11,window='hanning'):
+
+    if x.ndim != 1:
+        raise ValueError, "smooth only accepts 1 dimension arrays."
+
+    if x.size < window_len:
+        raise ValueError, "Input vector needs to be bigger than window size."
+
+
+    if window_len<3:
+        return x
+
+
+    if not window in ['flat', 'hanning', 'hamming', 'bartlett', 'blackman']:
+        raise ValueError, "Window is on of 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'"
+
+
+    s=np.r_[2*x[0]-x[window_len:1:-1],x,2*x[-1]-x[-1:-window_len:-1]]
+    #print(len(s))
+    if window == 'flat': #moving average
+        w=np.ones(window_len,'d')
+    else:
+        w=eval('np.'+window+'(window_len)')
+
+    y=np.convolve(w/w.sum(),s,mode='same')
+    return y[window_len-1:-window_len+1]
+
 
 ASOS = psycopg2.connect(database='asos', host='iemdb', user='nobody')
 cursor = ASOS.cursor()
 
 cursor.execute("""
- SELECT doy, sum(case when t = 0 and d = 0 and c = 0 then 1 else 0 end), count(*)
- from
-  (SELECT extract(year from valid) as yr, extract(doy from valid) as doy,
-  max(case when tmpf < 69.5 or tmpf >= 79.5 then 1 else 0 end) as t,
-  max(case when dwpf >= 59.5 then 1 else 0 end) as d,
-  max(case when 'OVC' in (skyc1,skyc2,skyc3,skyc4) or 
-                'BRK' in (skyc1,skyc2,skyc3,skyc4) then 1 else 0 end) as c
-  from alldata where station = 'DSM' 
-  and extract(hour from valid) between 12 and 17 GROUP by yr, doy) as foo
-  GROUP by doy ORDER by doy ASC
+ SELECT doy, count(*),
+ sum(case when wind >= 10 then 1 else 0 end),
+ sum(case when wind >= 10 and (tmpf - 5.0) > avg then 1 else 0 end),
+ sum(case when (tmpf - 5.0) > avg then 1 else 0 end),
+ sum(case when wind >= 10 and (tmpf + 5.0) < avg then 1 else 0 end),
+ sum(case when (tmpf + 5.0) < avg then 1 else 0 end) from
+ (SELECT extract(doy from valid) as doy, tmpf, sknt * 1.15 as wind,
+ avg(tmpf) over (partition by extract(doy from valid)) from alldata WHERE station = 'DSM'
+ and extract(hour from valid + '10 minutes'::interval) = 15 and 
+ extract(minute from valid + '10 minutes'::interval) <= 10 and
+ tmpf is not null) as foo
+ GROUP by doy ORDER by doy ASC
 """)
 
-doy = []
-freq = []
+allfreq = []
+t5overfreq = []
+t5underfreq = []
 for row in cursor:
-    doy.append( row[0] )
-    freq.append( row[1] / float(row[2]) * 100.0)
-    
+    allfreq.append( row[2] / float(row[1]) * 100.0)
+    t5overfreq.append( row[3] / float(row[4]) * 100.0)
+    t5underfreq.append( row[5] / float(row[6]) * 100.0)
+
+allfreq= np.array(allfreq)
+t5overfreq= np.array(t5overfreq)
+t5underfreq= np.array(t5underfreq)
+
 import matplotlib.pyplot as plt
 
 (fig, ax) = plt.subplots(1,1)
 
-ax.bar(doy, freq, fc='b', ec='b')
+ax.plot(np.arange(1,365), smooth(allfreq[:-2],14), c='g', label='All Cases')
+ax.plot(np.arange(1,365), smooth(t5overfreq[:-2],14), c='r', label='+5 $^\circ$F')
+ax.plot(np.arange(1,365), smooth(t5underfreq[:-2],14), c='b', label='-5 $^\circ$F')
 ax.grid(True)
 ax.set_xticks( (1,32,60,91,121,152,182,213,244,274,305,335,365) )
 ax.set_xticklabels( ('Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec') )
-ax.set_title("1933-2012 Des Moines 12 PM - 5 PM Great Weather\nTemperature in 70s$^\circ$F, mostly clear, Dew Point below 60$^\circ$F")
+ax.set_title("1933-2012 Des Moines 3 PM Temps and Winds\nAir Temperature +/- 5$^\circ$F over average and 10+ mph winds")
 ax.set_ylabel("Frequency [%]")
-ax.set_xlabel("Day of Year")
+ax.set_xlabel("Day of Year, 14 day centered smoothing applied")
+ax.set_xlim(1,366)
+ax.legend(loc=4)
 fig.savefig('test.ps')
 import iemplot
 iemplot.makefeature('test')
