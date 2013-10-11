25d204aeb23e0991cb4828e9f85aa8eb004697b2
akrherz <akrherz@iastate.edu>
2013-10-11 16:12:47 -0500

    WIP commit of bootstrap migration, will go live this weekend

--- htdocs/error404.php
+++ htdocs/error404.php
@@ -1,12 +1,11 @@
 <?php
-include("../config/settings.inc.php");
-include("$rootpath/include/header.php");
-?>
+require '../config/settings.inc.php';
+include_once('../include/myview.php');
 
+$t = new MyView();
+$t->content = <<<EOF
 <h3>Requested file was not found</h3>
-
-<img src="<?php echo $rooturl; ?>/images/snoopy503.jpg" />
-
-<?php
-include("$rootpath/include/footer.php");
+<img src="/images/snoopy503.jpg" class="img-responsive" alt="daryl's Cat" />
+EOF;
+$t->render('single.phtml');
 ?>
--- include/footer.php
+++ include/footer.php
@@ -1,26 +1,2 @@
-<?php if (! isset($NOCONTENT)) echo "</div><!-- End #iem-content -->"; ?>
-<div id="iem-footer">
-
-<div style="float: right;">
- <form method="get" action="http://www.google.com/search" name="search">
- <b>Search Site with Google:</b><br />
- <input value="mesonet.agron.iastate.edu" name="sitesearch" type="hidden" />
-<input type="text" size="15" name="q" /><input type="submit" value="Search" /></form>
-</div>
-
-Copyright &copy; 2001-<?php echo date("Y"); ?>, Iowa State University of Science and Technology.
-<br />All rights reserved.
- <a href="<?php echo $rooturl; ?>/help/abbreviations.php">abbreviations</a>
- &middot; <a href="<?php echo $rooturl; ?>/info/contacts.php">contact us</a>
- &middot; <a href="https://github.com/akrherz/iem">development</a>
- &middot; <a href="<?php echo $rooturl; ?>/disclaimer.php">disclaimer</a>
-</div>
-<?php 
-if (! isset($NOCONTENT)) echo "</div> <!-- End of iem-main -->"; 
-if (defined("FBEXTRA")) {
-	echo '<script src="/js/jquery.js"></script>';
-	echo '<script src="/js/fbcomments.js"></script>';
-}
-?>
-</body>
-</html>
+</div><!--/.container-->
+<?php include dirname(__FILE__) . '/templates/footer.phtml'; ?>
