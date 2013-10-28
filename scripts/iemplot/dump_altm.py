93b7a68a26a08eaa999f8dc8b04e0bf14197a2d7
akrherz <akrherz@iastate.edu>
2013-10-28 14:53:42 -0500

    only dump midwest values, prevent troubles of having potentially foreign sites invade

--- scripts/iemplot/dump_altm.py
+++ scripts/iemplot/dump_altm.py
@@ -1,13 +1,17 @@
-# Dump Altimeter please to a GEMPAK file
-import mx.DateTime
-ts = mx.DateTime.gmt().strftime("%y%m%d/%H00")
-import iemdb
-IEM = iemdb.connect("iem", bypass=True)
-icursor = IEM.cursor()
+"""
+    Dumping altimeter data so that GEMPAK can analyze it
+"""
+import datetime
+import psycopg2
 
-icursor.execute("""
- SELECT t.id, alti from current c, stations t WHERE alti > 0 and t.iemid = c.iemid 
- and valid > (now() - '60 minutes'::interval)
+ts = datetime.datetime.utcnow().strftime("%y%m%d/%H00")
+IEM = psycopg2.connect(database='iem', host='iemdb', user='nobody')
+cursor = IEM.cursor()
+
+cursor.execute("""
+ SELECT t.id, alti from current c JOIN stations t on (t.iemid = c.iemid)
+ WHERE alti > 0 and valid > (now() - '60 minutes'::interval)
+ and t.state in ('IA','MO','IL','WI','IN','OH','KY','MI','SD','ND','NE','KS')
  """)
 
 o = open('altm.txt', 'w')
@@ -16,7 +20,7 @@ o.write(""" PARM = ALTM
     STN    YYMMDD/HHMM      ALTM
 """)
 
-for row in icursor:
-  o.write("   %4s    %s  %8.2f\n" % (row[0], ts, row[1] * 33.8637526))
+for row in cursor:
+    o.write("   %4s    %s  %8.2f\n" % (row[0], ts, row[1] * 33.8637526))
 
 o.close()
