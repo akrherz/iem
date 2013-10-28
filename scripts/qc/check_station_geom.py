1bf4ddab3bca151bf21d3da365f94362513d8475
akrherz <akrherz@iastate.edu>
2013-10-28 07:54:25 -0500

    postgis2

--- scripts/qc/check_station_geom.py
+++ scripts/qc/check_station_geom.py
@@ -8,7 +8,7 @@ mcursor = MESOSITE.cursor()
 
 mcursor.execute("""
  SELECT id, network, ST_x(geom), ST_y(geom), modified from stations WHERE
- x(geom) >= 180 or x(geom) <= -180 or y(geom) > 90 or y(geom) < -90
+ ST_x(geom) >= 180 or ST_x(geom) <= -180 or ST_y(geom) > 90 or ST_y(geom) < -90
 """)
 for row in mcursor:
     print 'LOC QC FAIL', row
