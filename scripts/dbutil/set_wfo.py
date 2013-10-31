582dad12b748434abba4f920671dce6736098b74
akrherz <akrherz@iastate.edu>
2013-10-31 09:41:33 -0500

    cleanups and a postgis2 fix

--- scripts/dbutil/set_wfo.py
+++ scripts/dbutil/set_wfo.py
@@ -1,9 +1,7 @@
 """
  Assign a WFO to sites in the metadata tables that have no WFO set
-$Id: $:
 """
 
-import re
 import iemdb
 MESOSITE = iemdb.connect('mesosite')
 mcursor = MESOSITE.cursor()
@@ -12,22 +10,22 @@ mcursor2 = MESOSITE.cursor()
 # Find sites we need to check on
 mcursor.execute("""select s.id, c.wfo, s.iemid, s.network 
   from stations s, cwa c WHERE 
-  s.geom && c.the_geom and contains(c.the_geom, s.geom) 
+  s.geom && c.the_geom and ST_contains(c.the_geom, s.geom) 
   and (s.wfo IS NULL or s.wfo = '') and s.country = 'US' """)
 
 for row in mcursor:
-  id = row[0]
-  wfo = row[1]
-  iemid = row[2]
-  network = row[3]
-  if wfo is not None:
-    print 'Assinging WFO: %s to IEMID: %s ID: %s NETWORK: %s' % (wfo,
-     iemid, id, network)
-    mcursor2.execute("UPDATE stations SET wfo = '%s' WHERE iemid = %s" % (
+    sid = row[0]
+    wfo = row[1]
+    iemid = row[2]
+    network = row[3]
+    if wfo is not None:
+        print 'Assinging WFO: %s to IEMID: %s ID: %s NETWORK: %s' % (wfo,
+                                                            iemid, sid, network)
+        mcursor2.execute("UPDATE stations SET wfo = '%s' WHERE iemid = %s" % (
          wfo, iemid) )
-  else:
-    print 'ERROR assigning WFO to IEMID: %s ID: %s NETWORK: %s' % (
-     iemid, id, network)
+    else:
+        print 'ERROR assigning WFO to IEMID: %s ID: %s NETWORK: %s' % (
+                                                            iemid, sid, network)
 
 
 mcursor.close()
