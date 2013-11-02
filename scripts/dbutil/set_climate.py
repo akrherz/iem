05db6e81ee6a7b6ef2034b7e4828ad446898207c
akrherz <akrherz@iastate.edu>
2013-11-02 06:06:49 -0500

    cleanups and fix postgis2 issue

--- scripts/dbutil/set_climate.py
+++ scripts/dbutil/set_climate.py
@@ -1,6 +1,5 @@
 """
  Assign a climate site to each site in the mesosite database, within reason
- Daryl Herzmann 5 May 2004
 """
 
 import iemdb
@@ -19,20 +18,20 @@ for row in mcursor:
 	thisID = row[0]
 	thisGEOM = row[1]
 	st = row[2]
-        # Find the closest site
+	# Find the closest site
 	sql = """select id from stations WHERE network = '%sCLIMATE'
                  and substr(id,3,4) != '0000' and substr(id,3,1) != 'C'
-		 ORDER by distance(geom, '%s') ASC LIMIT 1""" % (
+		 ORDER by ST_distance(geom, '%s') ASC LIMIT 1""" % (
                    st, thisGEOM)
 	mcursor2.execute(sql)
-        row = mcursor2.fetchone()
-        if row is None:
-            print 'Could not find Climate Site for: %s' % (thisID,)
-        else:
-	    sql = """UPDATE stations SET climate_site = '%s' WHERE
-             id = '%s'""" % (row[0], thisID)
-            mcursor2.execute(sql)
-            print 'Set Climate: %s for ID: %s' % (row[0], thisID)
+	row2 = mcursor2.fetchone()
+	if row2 is None:
+		print 'Could not find Climate Site for: %s' % (thisID,)
+	else:
+		sql = """UPDATE stations SET climate_site = '%s' WHERE
+             id = '%s'""" % (row2[0], thisID)
+		mcursor2.execute(sql)
+		print 'Set Climate: %s for ID: %s' % (row2[0], thisID)
 
 mcursor2.close()
 MESOSITE.commit()
