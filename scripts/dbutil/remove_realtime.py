eaa220e151b42749ea2b63ede1950022ece54c3d
akrherz <akrherz@iastate.edu>
2013-11-22 09:59:37 -0600

    fix this up so that it works properly

--- scripts/dbutil/remove_realtime.py
+++ scripts/dbutil/remove_realtime.py
@@ -1,17 +1,37 @@
-import iemdb, sys, mx.DateTime
-IEM = iemdb.connect('iem', bypass=True)
+'''
+  Remove a station from realtime data collection and tracking
+'''
+import psycopg2
+import sys
+import datetime
+IEM = psycopg2.connect(database='iem', host='iemdb')
 icursor = IEM.cursor()
-MESOSITE = iemdb.connect('mesosite')
+MESOSITE = psycopg2.connect(database='mesosite', host='iemdb')
 mcursor = MESOSITE.cursor()
-year = mx.DateTime.now().year
+year = datetime.datetime.now().year
+
+if len(sys.argv) != 3:
+    print 'Usage: python remove_realtime.py NETWORK SID'
+    sys.exit()
+
+for table in ['current', 'trend_1h', 'trend_15m']:
+    icursor.execute("""
+     DELETE from %s where 
+     iemid = (select iemid from stations where id = '%s' and network = '%s')
+    """ % (table, sys.argv[2], sys.argv[1]))
+    if icursor.rowcount != 1:
+        print 'Updating table: %s resulted in %s rows modified' % (table,
+                                                        icursor.rowcount)
 
 icursor.execute("""
- DELETE from current where iemid = (select iemid from stations where id = '%s' and network = '%s')
-""" % (sys.argv[2],sys.argv[1]))
+ DELETE from %s where 
+ iemid = (select iemid from stations where id = '%s' and network = '%s')
+ and day in ('TODAY','TOMORROW')
+""" % ('summary_%s' % (year,), sys.argv[2], sys.argv[1]))
+if icursor.rowcount != 2:
+    print 'Updating table: %s resulted in %s rows modified' % ('summary',
+                                                    icursor.rowcount)
 
-icursor.execute("""
- DELETE from summary_%s where iemid = (select iemid from stations where id = '%s' and network = '%s') and day in ('TODAY','TOMORROW')
-""" % (year, sys.argv[2],sys.argv[1]))
 
 icursor.close()
 IEM.commit()
@@ -19,6 +39,8 @@ IEM.commit()
 mcursor.execute("""
 update stations SET online = false where id = '%s' and network = '%s'
 """ % (sys.argv[2],sys.argv[1]))
+if mcursor.rowcount != 1:
+    print 'Updating mesosite resulted in %s rows modified' % (icursor.rowcount,)
 
 mcursor.close()
 MESOSITE.commit()
