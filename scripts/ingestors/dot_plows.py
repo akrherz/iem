4997e67738b99f1fb56a08c5df70878ffd322063
akrherz <akrherz@iastate.edu>
2013-11-22 05:48:32 -0600

    account for corrupted data for the moment

--- scripts/ingestors/dot_plows.py
+++ scripts/ingestors/dot_plows.py
@@ -37,6 +37,8 @@ def workflow():
         valid = valid.replace(year=ts.year, month=ts.month, day=ts.day,
                               hour=ts.hour,minute=ts.minute,second=ts.second)
         label = feat['attributes']['LABEL']
+        if label == '30808':
+            continue
         if current.get(label, None) is None:
             #print 'New IDOT Snowplow #%s "%s" Valid: %s' % (len(current)+1,
             #                                                label, valid)
