70ca412b8427049e908d87b57794e376273096b0
akrherz <akrherz@iastate.edu>
2013-11-24 05:55:28 -0600

    change order logic for fetching MTR products

--- htdocs/wx/afos/retreive.php
+++ htdocs/wx/afos/retreive.php
@@ -10,6 +10,19 @@ $center = isset($_REQUEST["center"]) ? substr($_REQUEST["center"],0,4): "";
 $sdate = isset($_REQUEST["sdate"]) ? strtotime($_REQUEST["sdate"]) : mktime(0,0,0,12, 31,2009);
 $edate = isset($_REQUEST["edate"]) ? strtotime($_REQUEST["edate"]) : time();
 
+
+if (substr($pil,0,3) == "MTR"){
+	$rs = pg_prepare($access, "SELECT2", "SELECT raw from current_log c, stations t WHERE
+         raw != '' and t.id = $1 and t.iemid = c.iemid ORDER by valid DESC LIMIT $2");
+	$rs = pg_execute($access, "SELECT2", Array(substr($pil,3,3), $cnt));
+	for ($i=0;$row=@pg_fetch_array($rs,$i);$i++)
+	{
+	echo "<pre>". $row["raw"] ."</pre><hr />";
+   }
+   if (pg_numrows($rs) > 0){ die(); }
+
+}
+
 /*
  * We need to optimize some things here, first search the current table
  * and see if we can come up with enough entries.
@@ -40,20 +53,6 @@ if (pg_num_rows($rs) != $cnt){
 				date('Y-m-d H:i' , $edate), $cnt));
 }
 
-
-if (pg_numrows($rs) == 0){
- if (substr($pil,0,3) == "MTR"){
-   $rs = pg_prepare($access, "SELECT2", "SELECT raw from current_log c, stations t WHERE
-         raw != '' and t.id = $1 and t.iemid = c.iemid ORDER by valid DESC LIMIT $2");
-   $rs = pg_execute($access, "SELECT2", Array(substr($pil,3,3), $cnt));
-   for ($i=0;$row=@pg_fetch_array($rs,$i);$i++)
-   {
-      echo "<pre>". $row["raw"] ."</pre><hr />";
-   }
-   if (pg_numrows($rs) > 0){ die(); }
-
- }
-}
 if (pg_numrows($rs) == 0){
    echo "<h3>No entries found for ${pil}</h3>";
    die();
