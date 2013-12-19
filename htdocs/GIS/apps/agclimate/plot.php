b4b54301f725eb75a90bbfa6d82c36fd0e8cc007
akrherz <akrherz@iastate.edu>
2013-12-19 11:48:21 -0600

    support ISUSM stations on these plots

--- htdocs/GIS/apps/agclimate/plot.php
+++ htdocs/GIS/apps/agclimate/plot.php
@@ -1,25 +1,33 @@
 <?php
 include_once("../../../../config/settings.inc.php");
-include_once "$rootpath/include/iemmap.php";
-include_once "$rootpath/include/database.inc.php";
-include("$rootpath/include/network.php");
-$nt = new NetworkTable("ISUAG");
-$ISUAGcities = $nt->table;
+include_once "../../../../include/iemmap.php";
+include_once "../../../../include/database.inc.php";
+include("../../../../include/network.php");
 
 $year = isset($_GET["year"]) ? $_GET["year"]: date("Y", time() - 86400 - (7 * 3600) );
 $month = isset($_GET["month"]) ? $_GET["month"]: date("m", time() - 86400 - (7 * 3600) );
 $day = isset($_GET["day"]) ? $_GET["day"]: date("d", time() - 86400 - (7 * 3600) );
 $date = isset($_GET["date"]) ? $_GET["date"]: $year ."-". $month ."-". $day;
-
-$var = (isset($_GET["var"]) && $_GET["var"] != "" ) ? $_GET["var"] : "c11";
-$var2 = (isset($_GET["var2"]) && $_GET["var2"] != "" ) ? $_GET["var2"] : "";
-$direct = isset($_GET["direct"]) ? $_GET['direct']: "";
-
+$pvar = (isset($_GET["pvar"]) && $_GET["pvar"] != "" ) ? $_GET["pvar"] : "c11";
+$network = isset($_REQUEST["network"]) ? strtoupper($_REQUEST["network"]): 'AUTO';
 $ts = strtotime($date);
+
+$networkopts = Array("AUTO", "ISUAG", "ISUSM");
+if (! in_array($network, $networkopts)){
+	die("Invalid network!");
+}
+if ($network == 'AUTO'){
+	$network = 'ISUAG';
+	if ($ts > strtotime("2013-09-01")){
+		$network = 'ISUSM';
+	}
+}
+
+$nt = new NetworkTable($network);
 
 $varDef = Array("c11" => "High Air Temperatures",
   "c12" => "Low Air Temperatures [F]",
-  "c11c12" => "High and Low Air Temperatures [F]",
+  "c11,c12" => "High and Low Air Temperatures [F]",
   "c30" => "Avg 4in Soil Temperatures [F]",
   "c40" => "Avg Wind Velocity [MPH]",
   "c509" => "Peak 1 Minute Gust [MPH]",
@@ -29,28 +37,19 @@ $varDef = Array("c11" => "High Air Temperatures",
   "c20" => "Avg Relative Humidity",
   "c80" => "Solar Radiation [Langleys]",
   "c70" => "Evapotranspiration [inch]",
-  "c300hc300l" => "High and Low 4in Soil Temps [F]",
-  "c529c530" => "Peak 5 Sec Wind Gust [mph] and Time",
-  "dwpfhdwpfl" => "Max and Min Dew Points [F]",
-  "chill" => "Standard Chill Unit"
+  "c300h,c300l" => "High and Low 4in Soil Temps [F]",
+  "c529,c530" => "Peak 5 Sec Wind Gust [mph] and Time",
 );
 
-$rnd = Array("c11" => 0, "c12" => 0, "c30" => 0,"c300h" => 0, "c300l" => 0,
-  "c70" => 2, "c40" => 1 ,"c80" => 0, "dwpfl" => 0, "dwpfh" => 0,
-  "c90" => 2,
-  "c529" => 0,
-  "c530" => 2,
-  "pmonth" => 2,
-  "pday" => 2);
+$rnd = Array("c11,c12" => 0, "c11" => 0, "c12" => 0, "c30" => 0,
+		"c300h,c300l" => 0, "c509" => 1, "c20" => 0,
+  "c70" => 2, "c40" => 1 ,"c80" => 0, "dwpfl,dwpfh" => 0,
+  "c90" => 2, "c529,c530" => 2, "pmonth" => 2, "pday" => 2);
 
 
-$myStations = $ISUAGcities;
-$height = 480;
-$width = 640;
-
-$map = ms_newMapObj("$rootpath/data/gis/base26915.map");
+$map = ms_newMapObj("../../../../data/gis/base26915.map");
 $map->setProjection("init=epsg:26915");
-$map->setsize($width,$height);
+$map->setsize(640, 480);
 $map->setextent(175000, 4440000, 775000, 4890000);
 
 
@@ -81,91 +80,105 @@ $bar640t->draw($img);
 $c = iemdb("isuag");
 $tbl = strftime("t%Y", $ts);
 $dstamp = strftime("%Y-%m-%d", $ts);
-if ($var == 'c300') {
-  $q = "SELECT station, max(c300) as c300h, max(c300_f) as c300h_f,
+if ($network == 'ISUAG'){
+	if ($pvar == 'c300h,c300l') {
+  		$q = "SELECT station, max(c300) as c300h, max(c300_f) as c300h_f,
      min(c300) as c300l, max(c300_f) as c300l_f
      from hourly WHERE date(valid) = '${dstamp}' GROUP by station";
-  $var = 'c300h';
-  $var2 = 'c300l';
-} else if ($var == 'dwpf') {
-  $q = "select MAX( k2f( dewpt( f2k(c100), c200)))::numeric(7,2) as dwpfh,
+	} else if ($pvar == 'dwpfh,dwpf') {
+  		$q = "select MAX( k2f( dewpt( f2k(c100), c200)))::numeric(7,2) as dwpfh,
       station, MIN( k2f( dewpt( f2k(c100), c200)))::numeric(7,2) as dwpfl,
       max(c100_f) as dwpfh_f, max(c100_f) as dwpfl_f
       from hourly WHERE c200 > 0 and date(valid) = '${dstamp}' GROUP by station";
-  $var = 'dwpfh';
-  $var2 = 'dwpfl';
-} else if ($var == "c529") {
-  $q = "SELECT station, c529, c529_f, 
+	} else if ($pvar == "c529,c530") {
+  		$q = "SELECT station, c529, c529_f, 
     substring(text(c530),length(text(c530)) - 3,2) || ':' || 
     substring(text(c530), length(text(c530)) - 1,2) as c530 , 
     c530_f from daily WHERE valid = '${dstamp}' ";
-  $var2 = 'c530';
+	} else {
+  		$q = "SELECT * from daily WHERE valid = '${dstamp}' ";
+	}  
 } else {
-  $q = "SELECT * from daily WHERE valid = '${dstamp}' ";
-}  
+	$hourlyvars = Array("c20", "c300h,c300l");
+	
+	if (in_array($pvar, $hourlyvars)){
+		$q = <<<EOF
+  SELECT station,
+  max(c2f(t06_c_avg)) as c300h, '' as c300h_f,
+  min(c2f(t06_c_avg)) as c300l, '' as c300l_f,
+  avg(rh) as c20, '' as c20_f
+  from sm_hourly
+	WHERE valid BETWEEN '${dstamp} 00:00' and '${dstamp} 23:59'
+			GROUP by station
+EOF;
+	} else {
+		$q = <<<EOF
+  SELECT station, 
+  c2f(tair_c_max) as c11, '' as c11_f,
+  c2f(tair_c_min) as c12, '' as c12_f,
+  c2f(t06_c_avg) as c30, '' as c30_f,
+  ws_mps_s_wvt * 2.236 as c40, '' as c40_f,
+  ws_mps_max * 2.236 as c509, '' as c509_f,
+  ws_mps_max * 2.236 as c529, '' as c529_f,
+  to_char(ws_mps_tmx, 'HH24MI') as c530, '' as c530_f,
+  slrmj_tot as c80, '' as c80_f,
+  dailyet / 24.5 as c70, '' as c70_f,
+  rain_mm_tot * 24.5 as c90, '' as c90_f from sm_daily 
+	WHERE valid = '${dstamp}' 
+EOF;
+	}
+}
 $rs =  pg_exec($c, $q);
 $data = Array();
-for ($i=0; $row = @pg_fetch_array($rs,$i); $i++) {
+for ($i=0; $row = @pg_fetch_assoc($rs,$i); $i++) {
   $key = $row['station'];
   if ($key == "A133259") continue;
-  $data[$key] = Array();
-  $data[$key]['name'] = $ISUAGcities[$key]['name'];
-  $data[$key]['lon'] = $ISUAGcities[$key]['lon'];
-  $data[$key]['lat'] = $ISUAGcities[$key]['lat'];
-  //print_r($row);
-  //echo "::$var::";
-  $data[$key]['var'] = $row[$var];
 
   // Red Dot... 
   $pt = ms_newPointObj();
-  $pt->setXY($ISUAGcities[$key]['lon'], $ISUAGcities[$key]['lat'], 0);
+  $pt->setXY($nt->table[$key]['lon'], $nt->table[$key]['lat'], 0);
   $pt->draw($map, $ponly, $img, 0, ' ' );
 
+  if (strpos($pvar, ',') !== false){
+  	// Value UL
+  	list($p1,$p2) = explode(",", $pvar);
+  	$pt = ms_newPointObj();
+  	$pt->setXY($nt->table[$key]['lon'], $nt->table[$key]['lat'], 0);
+  	$pt->draw($map, $snet, $img, 1, 
+    	 round($row[$p1], $rnd[$pvar]) ." ". $row[$p1 .'_f'] );
 
-  // Value UL
-  $pt = ms_newPointObj();
-  $pt->setXY($ISUAGcities[$key]['lon'], $ISUAGcities[$key]['lat'], 0);
-  $pt->draw($map, $snet, $img, 1, 
-     round($row[$var], $rnd[$var]) ." ". $row[$var .'_f'] );
 
+  	// Value LL
+  	$pt = ms_newPointObj();
+  	$pt->setXY($nt->table[$key]['lon'], $nt->table[$key]['lat'], 0);
+  	$pt->draw($map, $snet, $img, 2, 
+        round($row[$p2], $rnd[$pvar]) ." ". $row[$p2 .'_f'] );
 
-  if (strlen($var2) > 0) {
-    $data[$key]['var2'] = $row[$var2];
-    // Value LL
-    $pt = ms_newPointObj();
-    $pt->setXY($ISUAGcities[$key]['lon'], $ISUAGcities[$key]['lat'], 0);
-    if ($var2 == 'c530'){
-      $pt->draw($map, $snet, $img, 2, 
-        $row[$var2] ." ". $row[$var2 .'_f'] );
-    } else {
-      $pt->draw($map, $snet, $img, 2, 
-        round($row[$var2], $rnd[$var2]) ." ". $row[$var2 .'_f'] );
-    }
-
+  } else {
+  	
+  	// Value UL
+  	$pt = ms_newPointObj();
+  	$pt->setXY($nt->table[$key]['lon'], $nt->table[$key]['lat'], 0);
+  	$pt->draw($map, $snet, $img, 1,
+  			round($row[$pvar], $rnd[$pvar]) ." ". $row[$pvar .'_f'] );
+  	 
   }
-
+  
   // City Name
   $pt = ms_newPointObj();
-  $pt->setXY($ISUAGcities[$key]['lon'], $ISUAGcities[$key]['lat'], 0);
+  $pt->setXY($nt->table[$key]['lon'], $nt->table[$key]['lat'], 0);
   if ($key == "A131909" || $key == "A130209"){
-    $pt->draw($map, $snet, $img, 0, $ISUAGcities[$key]['name'] );
+    $pt->draw($map, $snet, $img, 0, $nt->table[$key]['name'] );
   } else {
-    $pt->draw($map, $snet, $img, 0, $ISUAGcities[$key]['name'] );
+    $pt->draw($map, $snet, $img, 0, $nt->table[$key]['name'] );
   }
 
 }
 
-iemmap_title($map, $img, $varDef[$var . $var2] ." on ". date("d M Y", $ts),
+iemmap_title($map, $img, $varDef[$pvar] ." on ". date("d M Y", $ts),
 	($i == 0) ? 'No Data Found!': null );
 $map->drawLabelCache($img);
 
-$url = $img->saveWebImage();
-
-if (strlen($direct) > 0) { 
-  header("Content-type: image/png");
-  $img->saveImage('');
-} else {
+header("Content-type: image/png");
+$img->saveImage('');
 ?>
-<img src="<?php echo $url; ?>" border=1>
-
-<?php } ?>
