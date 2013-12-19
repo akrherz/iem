645df6bf58d236d4250b0f2805c47f3ce8dba84d
akrherz <akrherz@iastate.edu>
2013-12-19 05:44:30 -0600

    plot lsrs

--- scripts/feature/nws/wfo_mapper.py
+++ scripts/feature/nws/wfo_mapper.py
@@ -1,29 +1,39 @@
 import psycopg2
-import numpy
+#import numpy as np
 from pyiem.plot import MapPlot
 
 #bins = numpy.array([0,1,14,31,91,182,273,365,730,1460,2920,3800])
 #bins = numpy.array([0,1,14,31,91,135,182,227,273,365,730,1460,])
 #bins = numpy.arange(0,12000.1,1000.)
 #bins = numpy.arange(0,201,20)
-bins = [0,20,40,60,80,100,150,200,250,300,500,700,1000]
+#bins = [0,50,100,150,200,250,300,500,750,1000,1500,2000,2500,3000,3500]
+#bins = [0,25,50,75,100,150,200,250,300,400,500,750,1000,1250]
+bins = [0,0.2,0.4,0.6,0.8,1,1.2,1.4,1.6,1.8,2,2.5,3,4,6,]
 
-POSTGIS = psycopg2.connect(database='postgis', host='iemdb', user='nobody')
+POSTGIS = psycopg2.connect(database='postgis', host='mesonet.agron.iastate.edu', user='nobody')
 pcursor = POSTGIS.cursor()
 pcursor.execute("""
+WITH lsrcount as (
+SELECT wfo, count(*) / 6.0 as a from 
 
-select wfo, max(case when year = 2013 then count else 0 end) /
-    (sum(case when year < 2013 then count else 0 end)/10.0) * 100.0 as d,
-     max(case when year = 2013 then count else 0 end),
-    sum(case when year < 2013 then count else 0 end)/10.0
-     from 
-    (select wfo, year, count(*) from 
-        (select distinct extract(year from issue) as year, wfo, phenomena, eventid from 
-        warnings WHERE extract(doy from issue) < 270 and issue > '2003-01-01' 
-        and phenomena in ('FF') and significance = 'W') as foo 
-        GROUP by wfo, year) as foo2 
-GROUP by wfo ORDER by d DESC
+(select distinct wfo, geom, typetext, magnitude, valid from lsrs WHERE
+ valid > '2008-01-01' and type in ('T','H','G','D','M','W','F','x')) as foo
 
+GROUP by wfo),
+    vteccount as (
+    SELECT wfo, count(*) / 6.0 as a from 
+
+(select distinct wfo, phenomena, extract(year from issue), eventid from sbw WHERE
+ issue > '2008-01-01' and significance = 'W' and 
+ phenomena in ('SV','TO','FF','MA')) as foo2
+
+GROUP by wfo
+    
+    )
+
+SELECT lsrcount.wfo, lsrcount.a / vteccount.a as ratio from vteccount JOIN lsrcount on (lsrcount.wfo = vteccount.wfo)
+ORDER by ratio DESC
+    
 """)
 
 data = {}
@@ -34,9 +44,9 @@ for row in pcursor:
     data[ row[0] ] = row[1] 
     
 p = MapPlot(sector='nws',
-                 title='1 Jan - 26 Sep 2013 FFW Warning Count Percent of Average',
-                 subtitle='Departure from 10 year average 2003-2012, year to date')
-p.fill_cwas(data, bins=bins, lblformat='%.0f', units='%')
+                 title='1 Jan 2008 - 18 Dec 2013 Ratio of Unique LSRs to Warnings',
+                 subtitle='ratio of severe/flash flood LSRs to warnings issued')
+p.fill_cwas(data, bins=bins, lblformat='%.1f', units='1')
 p.postprocess(filename='test.png')
 #import iemplot
 #iemplot.makefeature('test')
