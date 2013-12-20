e007c76c5e74279a7bd78fd8e8aaadfe5cac6001
akrherz <akrherz@iastate.edu>
2013-12-20 09:29:07 -0600

    cleanups and don't hold up progress of RUN_2am script

--- scripts/cache/warn_cache.py
+++ scripts/cache/warn_cache.py
@@ -1,28 +1,46 @@
-# Need something to cache the warnings GIS files, since they are so 
-# huge.
+'''
+ Lets build a cache of warning files so to allow easier download
+ - run from RUN_2AM.sh
+'''
 
+import subprocess
+import datetime
+import sys
 import os
-import mx.DateTime
 
 FINAL = "/mesonet/share/pickup/wwa/"
 URL = "http://iem21.local/cgi-bin/request/gis/watchwarn.py"
 
 
-def get_files(sts, ets):
-    year = mx.DateTime.now().year
-    cmd = 'wget --timeout=60000 -q -O %s/%s_all.zip "%s?year1=%s&month1=1&day1=1&hour1=0&minute1=0&year2=%s&month2=%s&day2=%s&hour2=0&minute2=0"' % (FINAL, 
-                            sts.year,  URL, sts.year, ets.year, ets.month, ets.day)
-    os.system(cmd)
-    cmd = 'wget --timeout=60000 -q -O %s/%s_tsmf.zip "%s?year1=%s&month1=1&day1=1&hour1=0&minute1=0&year2=%s&month2=%s&day2=%s&hour2=0&minute2=0&limit0=yes"'  % (FINAL, 
-                            sts.year,  URL, sts.year, ets.year, ets.month, ets.day)
-    os.system(cmd)
+def get_files(year):
+    ''' Go get our files and then cache them! '''
+    common = 'year1=%s&month1=1&day1=1&hour1=0&minute1=0' % (year,)
+    common += '&year2=%s&month2=12&day2=31&hour2=23&minute2=59' % (year,)
+    
+    cmd = 'wget --timeout=60000 -q -O /tmp/%s_all.zip "%s?%s"' % (year,
+                                                            URL, common)
+    subprocess.call(cmd, shell=True)
+    os.rename("/tmp/%s_all.zip" % (year,),
+              "%s/%s_all.zip" % (FINAL, year))
+    
+    cmd = 'wget --timeout=60000 -q -O /tmp/%s_tsmf.zip "%s?%s&limit0=yes"'  % (
+                            year, URL, common)
+    subprocess.call(cmd, shell=True)
+    os.rename("/tmp/%s_tsmf.zip" % (year,),
+              "%s/%s_tsmf.zip" % (FINAL, year))
+
     if year > 2001:
-        cmd = 'wget --timeout=60000 -q -O %s/%s_tsmf_sbw.zip "%s?year1=%s&month1=1&day1=1&hour1=0&minute1=0&year2=%s&month2=%s&day2=%s&hour2=0&minute2=0&limit0=yes&limit1=yes"'  % (FINAL, 
-                 sts.year,  URL, sts.year, ets.year, ets.month, ets.day)
-        os.system(cmd)
-        
+        cmd = ('wget --timeout=60000 -q -O /tmp/%s_tsmf_sbw.zip'
+               +' "%s?%s&limit0=yes&limit1=yes"')  % (year,  URL, common)
+        subprocess.call(cmd, shell=True)
+        os.rename("/tmp/%s_tsmf_sbw.zip" % (year,),
+              "%s/%s_tsmf_sbw.zip" % (FINAL, year))
+
 
 if __name__ == "__main__":
-    sts = mx.DateTime.now() + mx.DateTime.RelativeDateTime(day=1,month=1,hour=0,minute=0)
-    ets = mx.DateTime.now() + mx.DateTime.RelativeDateTime(days=1,hour=0,minute=0)
-    get_files(sts, ets)
+    ''' Lets go! '''
+    if len(sys.argv) == 2:
+        get_files(sys.argv[1])
+    else:
+        yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
+        get_files(yesterday.year)
