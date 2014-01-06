d6d13d300014dc1ac15d8896a3d819bd3906bd9f
akrherz <akrherz@iastate.edu>
2014-01-06 09:27:30 -0600

    hopefully make a bit faster

--- scripts/coop/compute_climate.py
+++ scripts/coop/compute_climate.py
@@ -10,8 +10,7 @@ nt = network.Table(("IACLIMATE", "MNCLIMATE", "NDCLIMATE", "SDCLIMATE",
   "NECLIMATE", "KSCLIMATE", "MOCLIMATE", "ILCLIMATE", "WICLIMATE",
   "MICLIMATE", "INCLIMATE", "OHCLIMATE", "KYCLIMATE"))
 COOP = psycopg2.connect(database='coop', host='iemdb')
-ccursor = COOP.cursor(cursor_factory=psycopg2.extras.DictCursor)
-ccursor2 = COOP.cursor()
+
 THISYEAR = mx.DateTime.now().year
 META = {
     'climate51' : {'sts': mx.DateTime.DateTime(1951,1,1), 
@@ -28,10 +27,21 @@ def daily_averages(table):
     """
     Compute and Save the simple daily averages
     """
-    for st in ['nd','sd','ne','ks','mo','ia','mn','wi','il','in','oh','mi','ky']:
-        print 'Computing Daily Averages for state:', st
+    for st in ['ND', 'SD', 'NE', 'KS', 'MO', 'IA', 'MN', 'WI', 'IL',
+               'IN', 'OH', 'MI', 'KY']:
+        print 'Computing Daily Averages for state: %s' % (st,)
+        ccursor = COOP.cursor()
+        ccursor.execute("""DELETE from %s WHERE substr(station,1,2) = '%s'
+        """ % (table, st))
+        print '    removed %s rows from %s' % (ccursor.rowcount, table)
         sql = """
-    SELECT '2000-'|| to_char(day, 'MM-DD') as d, station, 
+    INSERT into %s (station, valid, high, low, 
+        max_high, min_high,
+        max_low, min_low,
+        max_precip, precip, 
+        snow, years, gdd50, sdd86, hdd65, max_range,
+        min_range)
+    (SELECT station, ('2000-'|| to_char(day, 'MM-DD'))::date as d,  
     avg(high) as avg_high, avg(low) as avg_low,
     max(high) as max_high, min(high) as min_high,
     max(low) as max_low, min(low) as min_low,
@@ -41,41 +51,29 @@ def daily_averages(table):
     avg( hdd65(high,low) ) as hdd65,
     max( high - low) as max_range, min(high - low) as min_range
     from alldata_%s WHERE day >= '%s' and day < '%s' 
-    GROUP by d, station
-    """ % (st, META[table]['sts'].strftime("%Y-%m-%d"), 
+    GROUP by d, station) 
+    """ % (table, st, META[table]['sts'].strftime("%Y-%m-%d"), 
 		META[table]['ets'].strftime("%Y-%m-%d") )
         ccursor.execute(sql)
-        for row in ccursor:
-            sid = row['station']
-            if not sid.upper() in nt.sts.keys():
-                continue
-            sql = """DELETE from %s WHERE station = '%s' and valid = '%s' """ % (
-                        table, sid, row['d'])
-            ccursor2.execute(sql)
-            sql = """ INSERT into """+ table +""" (station, valid, high, low, precip, snow,
-        max_high, max_low, min_high, min_low, max_precip, years, gdd50, sdd86, max_range,
-        min_range, hdd65) VALUES ('%(station)s', '%(d)s', %(avg_high)s, %(avg_low)s, %(precip)s,
-        %(snow)s, %(max_high)s, %(max_low)s, %(min_high)s, %(min_low)s, %(max_precip)s,
-        %(years)s, %(gdd50)s, %(sdd86)s, %(max_range)s, %(min_range)s,
-        %(hdd65)s)""" % row
-            ccursor2.execute(sql)
-
+        print '    added %s rows to %s' % (ccursor.rowcount, table)
+        ccursor.close()
         COOP.commit()
 
-def do_date(table, row, col, agg_col):
+def do_date(ccursor2, table, row, col, agg_col):
     sql = """
     SELECT year from alldata_%s where station = '%s' and %s = %s and sday = '%s'
     and day >= '%s' and day < '%s'
     ORDER by year ASC
-    """ % (row['station'][:2].lower(), row['station'], col, row[agg_col], 
+    """ % (row['station'][:2], row['station'], col, row[agg_col], 
            row['valid'].strftime("%m%d"),
            META[table]['sts'], 
            META[table]['ets'])
     ccursor2.execute(sql)
     row2 = ccursor2.fetchone()
     if row2 is not None:
-        sql = """ UPDATE %s SET %s_yr = %s WHERE station = '%s' and valid = '%s' """ % (
-                    table, agg_col, row2[0], row['station'], row['valid'])
+        sql = """ 
+            UPDATE %s SET %s_yr = %s WHERE station = '%s' and valid = '%s' 
+            """ % (table, agg_col, row2[0], row['station'], row['valid'])
         ccursor2.execute(sql)
         if ccursor2.rowcount != 1:
             print 'Update %s for station %s and date %s failed' % (table,
@@ -85,17 +83,23 @@ def set_daily_extremes(table):
     sql = """
     SELECT * from %s 
     """ % (table,)
+    ccursor = COOP.cursor(cursor_factory=psycopg2.extras.DictCursor)
     ccursor.execute(sql)
+    ccursor2 = COOP.cursor()
+    cnt = 0
+    total = ccursor.rowcount
     for row in ccursor:
-        do_date(table, row, 'high', 'max_high')
-        do_date(table, row, 'high', 'min_high')
-        do_date(table, row, 'low', 'max_low')
-        do_date(table, row, 'low', 'min_low')
-        do_date(table, row, 'precip', 'max_precip')
-        COOP.commit()
+        do_date(ccursor2, table, row, 'high', 'max_high')
+        do_date(ccursor2, table, row, 'high', 'min_high')
+        do_date(ccursor2, table, row, 'low', 'max_low')
+        do_date(ccursor2, table, row, 'low', 'min_low')
+        do_date(ccursor2, table, row, 'precip', 'max_precip')
+        cnt += 1
+        if cnt % 1000 == 0:
+            print 'set_daily_extremes processed %s/%s' % (cnt,total)
+    ccursor2.close()
+    ccursor.close()
+    COOP.commit()
        
 daily_averages(sys.argv[1])
 set_daily_extremes(sys.argv[1])
-COOP.commit()
-ccursor.close()
-ccursor2.close()
