a4275b1245a5f67ce4daa2e39b095993c12f63f5
akrherz <akrherz@iastate.edu>
2014-01-07 13:06:45 -0600

    cleanups and add a stateSelect generator

--- include/forms.php
+++ include/forms.php
@@ -2,6 +2,70 @@
 /**
  * Library for doing repetetive forms stuff
  */
+function stateSelect($selected){
+	// Create pull down for selecting a state
+	$states = Array("AL" => "Alabama",
+	 "AK" => "Alaska",
+	 "AR" => "Arkansas",
+	 "AZ" => "Arizona",
+	 "CA" => ">California",
+	 "CO" => "Colorado",
+	 "CT" => "Connecticut",
+	 "DE" => "Delaware",
+	 "FL" => "Florida",
+	 "GA" => "Georgia",
+	 "HI" => "Hawaii",
+	 "ID" => "Idaho",
+	 "IL" => "Illinois",
+	 "IN" => "Indiana",
+	 "IA" => "Iowa",
+	 "KS" => "Kansas",
+	 "KY" => "Kentucky",
+	 "LA" => "Louisana",
+	 "MN" => "Maine",
+	 "MD" => "Maryland",
+	 "MA" => "Massachusetts",
+	 "MI" => "Michigan",
+	 "MN" => "Minnesota",
+	 "MS" => "Mississippi",
+	 "MO" => "Missouri",
+	 "MT" => "Montana",
+	 "NE" => "Nebraska",
+	 "NH" => "New Hampshire",
+	 "NC" => "North Carolina",
+	 "ND" => "North Dakota",
+	 "NV" => "Nevada",
+	 "NH" => "New Hampshire",
+	 "NJ" => "New Jersey",
+	 "NM" => "New Mexico",
+	 "NY" => "New York",
+	 "OH" => "Ohio",
+	 "OK" => "Oklahoma",
+	 "OR" => "Oregon",
+	 "PA" => "Pennsylvania",
+	 "RI" => "Rhode Island",
+	 "SC" => "South Carolina",
+	 "SD" => "South Dakota",
+	 "TN" => "Tennessee",
+	 "TX" => "Texas",
+	 "UT" => "Utah",
+	 "VT" => "Vermont",
+	 "VA" => "Virginia",
+	 "WA" => "Washington",
+	 "WV" => "West Virginia",
+	 "WI" => "Wisconsin",
+	 "WY" => "Wyoming",
+	 );
+	$s = "<select name=\"state\">";
+	while (list($key,$val) = each($states)){
+		$s .= "<option value=\"$key\"";
+		if ($selected == $key) $s .= " SELECTED";
+		$s .= ">[".$key."] ". $val ."</option>";
+	}
+	$s .= "</select>\n";
+	return $s;
+}
+
 function vtecPhenoSelect($selected)
 {
  global $vtec_phenomena;
