d4deded328897239e71590c6daaf3be62849e0d3
akrherz <akrherz@iastate.edu>
2014-01-08 16:39:50 -0600

    cleanups and ugcs

--- htdocs/vtec/json-list.php
+++ htdocs/vtec/json-list.php
@@ -2,8 +2,8 @@
 /* Giveme JSON data listing products */
 require_once 'Zend/Json.php';
 require_once '../../config/settings.inc.php';
-require_once "$rootpath/include/database.inc.php";
-
+require_once "../../include/database.inc.php";
+header("Content-type: application/json");
 $connect = iemdb("postgis");
 pg_exec($connect, "SET TIME ZONE 'GMT'");
 
@@ -18,26 +18,33 @@ $phenomena = isset($_GET["phenomena"]) ? substr($_GET["phenomena"],0,2) : "SV";
 $significance = isset($_GET["significance"]) ? substr($_GET["significance"],0,1) : "W";
 
 
- $sql = "select round(sum(ST_area(ST_transform(geom,2163)) / 1000000.0)::numeric,0) as area, sumtxt( n.name || ' ['||n.state||'], ') as locations, eventid,
-         min(issue) as issued, max(expire) as expired from
-        (select distinct ugc, eventid, issue, expire from warnings_$year
-         WHERE wfo = '$wfo' and gtype = 'C' and
-         significance = '$significance' and phenomena = '$phenomena' and eventid is not null) as foo,
-         nws_ugc n WHERE n.ugc = foo.ugc GROUP by eventid ORDER by eventid ASC";
+$sql = <<<EOF
+ select round(sum(ST_area(ST_transform(u.geom,2163)) / 1000000.0)::numeric,0) as area, 
+ string_agg( u.name || ' ['||u.state||']', ', ') as locations, eventid,
+ min(issue) as issued, max(expire) as expired from warnings_$year w JOIN ugcs u
+ ON (u.gid = w.gid) WHERE w.wfo = '$wfo' and
+ significance = '$significance' and phenomena = '$phenomena' 
+ and eventid is not null GROUP by eventid ORDER by eventid ASC
+EOF;
 if (($phenomena == "SV" || $phenomena == "TO" || $phenomena == "FF" || $phenomena == "MA") && $significance == "W" && $year > 2003){
 
- $sql = "SELECT round(area::numeric,0) as area, locations, foo2.eventid, issued, expired FROM 
-    (select sumtxt( n.name || ' ['||n.state||'], ') as locations, eventid,
-     min(issue) as issued, max(expire) as expired from
-      (select distinct ugc, eventid, issue, expire from warnings_$year
-       WHERE wfo = '$wfo' and gtype = 'C' and
-       significance = '$significance' and phenomena = '$phenomena' and eventid is not null) as foo,
-   nws_ugc n WHERE n.ugc = foo.ugc GROUP by eventid ORDER by eventid ASC) as foo2, 
-
-(select ST_area(ST_transform(geom,2163)) / 1000000.0 as area, eventid 
-        from warnings_$year WHERE wfo = '$wfo' and gtype = 'P' and
-       significance = '$significance' and phenomena = '$phenomena' and eventid is not null) as foo3  WHERE foo3.eventid = foo2.eventid";
-
+$sql = <<<EOF
+ WITH countybased as (
+ select string_agg( u.name || ' ['||u.state||']', ', ') as locations, eventid,
+ min(issue) as issued, max(expire) as expired from warnings_$year w JOIN ugcs u
+ ON (u.gid = w.gid) WHERE w.wfo = '$wfo' and
+ significance = '$significance' and phenomena = '$phenomena' 
+ and eventid is not null GROUP by eventid ),
+ 
+ stormbased as (
+ 	SELECT round((ST_area(ST_transform(geom,2163)) / 1000000.0)::numeric,0) as area,
+ 	eventid from sbw_$year w WHERE w.wfo = '$wfo' and
+ 	significance = '$significance' and phenomena = '$phenomena' 
+ 	and eventid is not null)
+ 			
+ SELECT s.area, c.locations, c.eventid, issued, expired from 
+ stormbased s JOIN countybased c on (c.eventid = s.eventid)
+EOF;
 }
 
 
