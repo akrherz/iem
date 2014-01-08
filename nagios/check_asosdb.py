92cc107182934833cb121d2e85fc1dd6a8bf1550
akrherz <akrherz@iastate.edu>
2014-01-08 08:01:41 -0600

    add checker of ASOS database query times

--- nagios/check_asosdb.py
+++ nagios/check_asosdb.py
@@ -11,24 +11,32 @@ icursor = IEM.cursor()
 def check():
     year = str(datetime.datetime.now().year)
     icursor.execute("""
-    SELECT count(*) from t"""+ year +""" WHERE station = 
-    (select id from stations where network ~* 'ASOS' 
-    ORDER by random() ASC LIMIT 1)
+    SELECT station, count(*), min(tmpf), max(tmpf) 
+    from t"""+ year +""" WHERE station = 
+    (select id from stations where network ~* 'ASOS' and online and
+    archive_begin < '1980-01-01' 
+    ORDER by random() ASC LIMIT 1) GROUP by station
     """)
     row = icursor.fetchone()
+    if row is None:
+        return 'XXX', 0
+    return row[0], row[1]
 
     
 if __name__ == '__main__':
     t0 = datetime.datetime.now()
-    check()
+    station, count = check()
     t1 = datetime.datetime.now()
     delta = (t1 - t0).seconds + float((t1 - t0).microseconds) / 1000000.0
     if delta < 5:
-        print 'OK - %.3f |qtime=%.3f;5;10;15' % (delta, delta)
+        print 'OK - %.3f %s %s |qtime=%.3f;5;10;15' % (delta, 
+                                                       station, count, delta)
         sys.exit(0)
     elif delta < 10:
-        print 'WARNING - %.3f |qtime=%.3f;5;10;15' % (delta, delta)
+        print 'WARNING - %.3f %s %s |qtime=%.3f;5;10;15' % (delta, 
+                                                       station, count, delta)
         sys.exit(1)
     else:
-        print 'CRITICAL - %.3f |qtime=%.3f;5;10;15' % (delta, delta)
+        print 'CRITICAL - %.3f %s %s |qtime=%.3f;5;10;15' % (delta,  
+                                                       station, count, delta)
         sys.exit(2)
