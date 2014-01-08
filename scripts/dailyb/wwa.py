3b87efe5786a97a047f10015f947fe17f3ea2f89
akrherz <akrherz@iastate.edu>
2014-01-08 08:49:36 -0600

    delete cruft and improve WWA script for IEM Daily Bulletin

--- scripts/dailyb/wwa.py
+++ scripts/dailyb/wwa.py
@@ -1,13 +1,19 @@
+'''
+ Some simple summary stats for the IEM Daily Bulletin...
+'''
+import psycopg2
+import datetime
+import pytz
 
-import pg
-import mx.DateTime
+POSTGIS = psycopg2.connect(database='postgis', host='iemdb', user='nobody')
+cursor = POSTGIS.cursor()
 
 textfmt = """
              Summary +_________ By WFO ____________+  Watches 
 Type         US   IA | ARX   DVN   DMX   OAX   FSD |  US 
-Tornado     %(tu)3s  %(ti)3s | %(ta)3s   %(tv)3s   %(td)3s   %(to)3s   %(tf)3s | %(tw)3s
-Svr Tstorm  %(su)3s  %(si)3s | %(sa)3s   %(sv)3s   %(sd)3s   %(so)3s   %(sf)3s | %(sw)3s
-Fl Flood    %(fu)3s  %(fi)3s | %(fa)3s   %(fv)3s   %(fd)3s   %(fo)3s   %(ff)3s | N/A
+Tornado     %(TOu)3s  %(TOi)3s | %(TOARX)3s   %(TODVN)3s   %(TODMX)3s   %(TOOAX)3s   %(TOFSD)3s | %(TORw)3s
+Svr Tstorm  %(SVu)3s  %(SVi)3s | %(SVARX)3s   %(SVDVN)3s   %(SVDMX)3s   %(SVOAX)3s   %(SVFSD)3s | %(SVRw)3s
+Flash Flood %(FFu)3s  %(FFi)3s | %(FFARX)3s   %(FFDVN)3s   %(FFDMX)3s   %(FFOAX)3s   %(FFFSD)3s | N/A
 
 ARX = LaCrosse, WI  DVN = Davenport, IA    DMX = Des Moines, IA 
 OAX = Omaha, NE     FSD = Sioux Falls, SD
@@ -16,11 +22,33 @@ OAX = Omaha, NE     FSD = Sioux Falls, SD
 
 htmlfmt = """
 <table cellpadding="3" cellspacing="0" border="1">
-<tr><td></td><th colspan="2">Summary</th><th colspan="5">By WFO</th><th>Watches</th></tr>
-<tr><th>Type</th><th>US</th><th>IA</th><th>ARX</th><th>DVN</th><th>DMX</th><th>OAX</th><th>FSD</th><th>US</th></tr>
-<tr><th>Tornado</th><td>%(tu)3s</td><td>%(ti)3s</td><td>%(ta)3s</td><td>%(tv)3s</td><td>%(td)3s</td><td>%(to)3s</td><td>%(tf)3s</td><td>%(tw)3s</td></tr>
-<tr><th>Svr Tstorm</th><td>%(su)3s</td><td>%(si)3s</td><td>%(sa)3s</td><td>%(sv)3s</td><td>%(sd)3s</td><td>%(so)3s</td><td>%(sf)3s</td><td>%(sw)3s</td></tr>
-<tr><th>Fl Flood</th><td>%(fu)3s</td><td>%(fi)3s</td><td>%(fa)3s</td><td>%(fv)3s</td><td>%(fd)3s</td><td>%(fo)3s</td><td>%(ff)3s</td><td>---</td></tr>
+<tr>
+ <td></td>
+ <th colspan="2">Summary</th>
+ <th colspan="5">By WFO</th>
+ <th>Watches</th></tr>
+<tr>
+ <th>Type</th>
+ <th>US</th><th>IA</th>
+ <th>ARX</th><th>DVN</th><th>DMX</th><th>OAX</th><th>FSD</th>
+ <th>US</th></tr>
+<tr>
+ <th>Tornado</th>
+ <td>%(TOu)3s</td><td>%(TOi)3s</td>
+ <td>%(TOARX)3s</td><td>%(TODVN)3s</td><td>%(TODMX)3s</td>
+   <td>%(TOOAX)3s</td><td>%(TOFSD)3s</td>
+ <td>%(TORw)3s</td></tr>
+<tr>
+ <th>Svr Tstorm</th>
+ <td>%(SVu)3s</td><td>%(SVi)3s</td>
+ <td>%(SVARX)3s</td><td>%(SVDVN)3s</td><td>%(SVDMX)3s</td>
+ <td>%(SVOAX)3s</td><td>%(SVFSD)3s</td>
+ <td>%(SVRw)3s</td></tr>
+<tr>
+ <th>Flash Flood</th>
+ <td>%(FFu)3s</td><td>%(FFi)3s</td>
+ <td>%(FFARX)3s</td><td>%(FFDVN)3s</td><td>%(FFDMX)3s</td>
+ <td>%(FFOAX)3s</td><td>%(FFFSD)3s</td><td>---</td></tr>
 </table>
 
 <p>ARX = LaCrosse, WI  DVN = Davenport, IA    DMX = Des Moines, IA 
@@ -28,73 +56,86 @@ OAX = Omaha, NE     FSD = Sioux Falls, SD
 
 """
 
-def run():
+def run(sts=None, ets=None):
     """ Generate listing of warning counts """
-    mydb = pg.connect('postgis', 'iemdb', user='nobody')
-
-    ts = mx.DateTime.now() - mx.DateTime.RelativeDateTime(days=1)
+    if sts is None or ets is None:
+        # We want to run for yesterday!
+        utc = datetime.datetime.utcnow()
+        utc = utc.replace(tzinfo=pytz.timezone("UTC"), second=0,
+                          microsecond=0, minute=0)
+        ts = utc.astimezone(pytz.timezone("America/Chicago"))
+        sts = ts - datetime.timedelta(hours=24)
+        sts = sts.replace(hour=0)
+        ets = ts.replace(hour=0)
 
     d = {}
 
     # Get US states
-    d['tu'] = 0
-    d['su'] = 0
-    d['fu'] = 0
-    rs = mydb.query("SELECT phenomena, count(*) as count from \
-    (select distinct * from warnings_%s WHERE \
-  gtype = 'P' and date(issue) = 'YESTERDAY' and phenomena IN ('TO','SV','FF')) as foo GROUP by phenomena" % (ts.year, ) ).dictresult()
-
-    c = {'SV': 's', 'FF': 'f', 'TO': 't'}
-    for i in range(len(rs)):
-        d['%su' % (c[ rs[i]['phenomena'] ],)] = rs[i]['count']
+    d['TOu'] = 0
+    d['SVu'] = 0
+    d['FFu'] = 0
+    cursor.execute("""select phenomena, count(*) from sbw_%s 
+        WHERE status = 'NEW' and issue >= '%s' and issue < '%s' 
+        and phenomena IN ('TO','SV','FF') GROUP by phenomena""" % (
+                                        sts.year, sts, ets) )
+    for row in cursor:
+        d['%su' % (row[0],)] = row[1]
 
     # Get Iowa
-    d['ti'] = 0
-    d['si'] = 0
-    d['fi'] = 0
-    sql = """
-select phenomena, count(*) as count 
- from warnings_%s w, states s
-WHERE ST_contains(s.the_geom, w.geom) and s.state_name = 'Iowa' 
-      and gtype = 'P' 
-  and date(issue) = 'YESTERDAY' 
-  and phenomena IN ('TO','SV','FF') GROUP by phenomena""" % (
-     ts.year,) 
-    rs = mydb.query(sql).dictresult()
-    for i in range(len(rs)):
-        d['%si' % (c[ rs[i]['phenomena'] ],)] = rs[i]['count']
+    d['TOi'] = 0
+    d['SVi'] = 0
+    d['FFi'] = 0
+    cursor.execute("""
+        select phenomena, count(*) as count 
+        from sbw_%s w, states s
+        WHERE ST_contains(s.the_geom, w.geom) and s.state_name = 'Iowa' 
+        and issue >= '%s' and issue < '%s' 
+        and phenomena IN ('TO','SV','FF') GROUP by phenomena""" % (sts.year,
+                                                                   sts, ets)) 
+    for row in cursor:
+        d['%si' % (row[0],)] = row[1]
 
     # Get per WFO
-    f = {'DMX': 'd', 'DVN': 'v', 'ARX': 'a', 'FSD': 'f', 'OAX': 'o'}
-    for key in f.keys():
-        d['t%s' % (f[key],)] = 0
-        d['s%s' % (f[key],)] = 0
-        d['f%s' % (f[key],)] = 0
-    sql = """
-  SELECT phenomena, wfo, count(*) as count from warnings_%s WHERE 
-  gtype = 'P' and date(issue) = 'YESTERDAY' 
+    for wfo in ['DMX', 'DVN', 'ARX', 'FSD', 'OAX']:
+        d['TO%s' % (wfo,)] = 0
+        d['SV%s' % (wfo,)] = 0
+        d['FF%s' % (wfo,)] = 0
+    cursor.execute("""
+  SELECT phenomena, wfo, count(*) as count from sbw_%s WHERE 
+  issue >= '%s' and issue < '%s' 
   and phenomena IN ('TO','SV','FF') and
-  wfo in ('DMX','FSD','ARX','DVN','OAX') GROUP by wfo, phenomena""" % (ts.year,)
-    rs = mydb.query(sql).dictresult()
-    for i in range(len(rs)):
-        d['%s%s' % (c[ rs[i]['phenomena'] ], f[ rs[i]['wfo'] ])] = rs[i]['count']
+  wfo in ('DMX','FSD','ARX','DVN','OAX') GROUP by wfo, phenomena""" % (sts.year,
+                                                                    sts, ets))
+    for row in cursor:
+        d['%s%s' % (row[0], row[1])] = row[2]
 
     # SPC Watches
-    d['tw'] = 0
-    d['sw'] = 0
+    d['TORw'] = 0
+    d['SVRw'] = 0
 
-    c = {'SVR': 's', 'FFW': 'f', 'TOR': 't'}
-    rs = mydb.query("SELECT type, count(*) as count from watches WHERE \
-      date(issued) = 'YESTERDAY' GROUP by type").dictresult()
-    for i in range(len(rs)):
-        d['%sw' % (c[ rs[i]['type'] ],)] = rs[i]['count']
+    cursor.execute("""SELECT type, count(*) as count from watches WHERE 
+      issued >= '%s' and issued < '%s' GROUP by type""" % (sts, ets))
+    for row in cursor:
+        d['%sw' % (row[0],)] = row[1]
 
-
+    label = "%s - %s" % (sts.strftime("%-d %b %Y"), 
+                         ets.strftime("%-d %b %Y %Z"))
    
-    txt = "> NWS Watch/Warning Summary for %s\n" % (ts.strftime("%d %b %Y"),)
-    html = "<h3>NWS Watch/Warning Summary for %s</h3>" % (ts.strftime("%d %b %Y"),)
+    txt = "> NWS Watch/Warning Summary for %s\n" % (label,)
+    html = "<h3>NWS Watch/Warning Summary for %s</h3>" % (label,)
 
     txt += textfmt % d
     html += htmlfmt % d
 
     return txt, html
+
+if __name__ == '__main__':
+    sts = datetime.datetime(2005,11,12,12)
+    sts = sts.replace(tzinfo=pytz.timezone("UTC"))
+    sts = sts.astimezone(pytz.timezone("America/Chicago"))
+    sts = sts.replace(hour=0)
+    ets = sts + datetime.timedelta(hours=24)
+    txt, html = run(sts, ets)
+    print txt
+    txt, html = run()
+    print txt
