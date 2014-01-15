77026f1fc3f15f7447029c044f013558fb164066
akrherz <akrherz@iastate.edu>
2014-01-15 07:58:04 -0600

    web API uses pvar now instead of var, shrug

--- scripts/isuag/run_plots.sh
+++ scripts/isuag/run_plots.sh
@@ -12,7 +12,7 @@ python fix_precip.py `date --date '3 day ago' +'%Y %m %d'`
 python compute_soil_hilo.py
 
 fp="air-temp-out"
-wget -q http://${WEBHOST}/GIS/apps/agclimate/plot.php\?direct=yes\&var=c11\&var2=c12\&date=${dstr} -O air-temp-out.png
+wget -q http://${WEBHOST}/GIS/apps/agclimate/plot.php\?direct=yes\&pvar=c11\&var2=c12\&date=${dstr} -O air-temp-out.png
 convert air-temp-out.png air-temp-out.jpg
 convert air-temp-out.jpg air-temp-out.gif
 gifsicle -b -O2 air-temp-out.gif
@@ -23,7 +23,7 @@ rm -f ${fp}.jpg ${fp}.png ${fp}.gif
 
 
 fp="4in-temp-out"
-wget -q http://${WEBHOST}/GIS/apps/agclimate/plot.php\?direct=yes\&var=c30\&date=${dstr} -O 4in-temp-out.png
+wget -q http://${WEBHOST}/GIS/apps/agclimate/plot.php\?direct=yes\&pvar=c30\&date=${dstr} -O 4in-temp-out.png
 convert 4in-temp-out.png 4in-temp-out.jpg
 convert 4in-temp-out.jpg 4in-temp-out.gif
 gifsicle -b -O2 4in-temp-out.gif
@@ -33,7 +33,7 @@ gifsicle -b -O2 4in-temp-out.gif
 rm -f ${fp}.jpg ${fp}.png ${fp}.gif
 
 fp="soil-hilo-out"
-wget -q http://${WEBHOST}/GIS/apps/agclimate/plot.php\?direct=yes\&var=c300\&date=${dstr} -O soil-hilo-out.png
+wget -q http://${WEBHOST}/GIS/apps/agclimate/plot.php\?direct=yes\&pvar=c300\&date=${dstr} -O soil-hilo-out.png
 convert soil-hilo-out.png soil-hilo-out.jpg
 convert soil-hilo-out.jpg soil-hilo-out.gif
 gifsicle -b -O2 soil-hilo-out.gif
@@ -44,7 +44,7 @@ rm -f ${fp}.jpg ${fp}.png ${fp}.gif
 
 
 fp="rad-out"
-wget -q http://${WEBHOST}/GIS/apps/agclimate/plot.php\?direct=yes\&var=c80\&date=${dstr} -O rad-out.png
+wget -q http://${WEBHOST}/GIS/apps/agclimate/plot.php\?direct=yes\&pvar=c80\&date=${dstr} -O rad-out.png
 convert rad-out.png rad-out.jpg
 convert rad-out.jpg rad-out.gif
 gifsicle -b -O2 rad-out.gif
@@ -54,7 +54,7 @@ gifsicle -b -O2 rad-out.gif
 rm -f ${fp}.jpg ${fp}.png ${fp}.gif
 
 fp="et-out"
-wget -q http://${WEBHOST}/GIS/apps/agclimate/plot.php\?direct=yes\&var=c70\&date=${dstr} -O et-out.png
+wget -q http://${WEBHOST}/GIS/apps/agclimate/plot.php\?direct=yes\&pvar=c70\&date=${dstr} -O et-out.png
 convert et-out.png et-out.jpg
 convert et-out.jpg et-out.gif
 gifsicle -b -O2 et-out.gif
@@ -64,7 +64,7 @@ gifsicle -b -O2 et-out.gif
 rm -f ${fp}.jpg ${fp}.png ${fp}.gif
 
 fp="prec-out"
-wget -q http://${WEBHOST}/GIS/apps/agclimate/plot.php\?direct=yes\&var=c90\&date=${dstr} -O prec-out.png
+wget -q http://${WEBHOST}/GIS/apps/agclimate/plot.php\?direct=yes\&pvar=c90\&date=${dstr} -O prec-out.png
 convert prec-out.png prec-out.jpg
 convert prec-out.jpg prec-out.gif
 gifsicle -b -O2 prec-out.gif
@@ -74,7 +74,7 @@ gifsicle -b -O2 prec-out.gif
 rm -f ${fp}.jpg ${fp}.png ${fp}.gif
 
 fp="pk-wind-out"
-wget -q http://${WEBHOST}/GIS/apps/agclimate/plot.php\?direct=yes\&var=c529\&date=${dstr} -O ${fp}.png
+wget -q http://${WEBHOST}/GIS/apps/agclimate/plot.php\?direct=yes\&pvar=c529\&date=${dstr} -O ${fp}.png
 convert ${fp}.png ${fp}.jpg
 convert ${fp}.jpg ${fp}.gif
 gifsicle -b -O2 ${fp}.gif
@@ -84,7 +84,7 @@ gifsicle -b -O2 ${fp}.gif
 rm -f ${fp}.jpg ${fp}.png ${fp}.gif
 
 fp="avewind-out"
-wget -q http://${WEBHOST}/GIS/apps/agclimate/plot.php\?direct=yes\&var=c40\&date=${dstr} -O ${fp}.png
+wget -q http://${WEBHOST}/GIS/apps/agclimate/plot.php\?direct=yes\&pvar=c40\&date=${dstr} -O ${fp}.png
 convert ${fp}.png ${fp}.jpg
 convert ${fp}.jpg ${fp}.gif
 gifsicle -b -O2 ${fp}.gif
@@ -94,7 +94,7 @@ gifsicle -b -O2 ${fp}.gif
 rm -f ${fp}.jpg ${fp}.png ${fp}.gif
 
 fp="dwpts"
-wget -q http://${WEBHOST}/GIS/apps/agclimate/plot.php\?direct=yes\&var=dwpf\&date=${dstr} -O ${fp}.png
+wget -q http://${WEBHOST}/GIS/apps/agclimate/plot.php\?direct=yes\&pvar=dwpf\&date=${dstr} -O ${fp}.png
 convert ${fp}.png ${fp}.jpg
 convert ${fp}.jpg ${fp}.gif
 gifsicle -b -O2 ${fp}.gif
