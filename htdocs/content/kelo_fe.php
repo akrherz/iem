ebb4beda94a65881f85c715f1f02d6933d8eea75
akrherz <akrherz@iastate.edu>
2014-01-22 10:16:06 -0600

    protect the CGI vars some

--- htdocs/content/kelo_fe.php
+++ htdocs/content/kelo_fe.php
@@ -2,8 +2,8 @@
 <head>
 <?php
   include("../../config/settings.inc.php");
-  $station = isset($_GET['station']) ? $_GET["station"]: "SSAS2";
-  $min = isset($_GET["min"]) ? $_GET["min"] : 1;
+  $station = isset($_GET['station']) ? substr($_GET["station"],0,5): "SSAS2";
+  $min = isset($_GET["min"]) ? intval($_GET["min"]) : 1;
   include("$rootpath/include/network.php");
 $nt = new NetworkTable("KELO");
 $cities = $nt->table;
