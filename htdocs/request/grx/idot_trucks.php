1016c6b94674dc9b8685ab3e142ec732e38b2939
akrherz <akrherz@iastate.edu>
2014-02-04 11:59:56 -0600

    memcache this and add a test layer

--- htdocs/request/grx/idot_trucks.php
+++ htdocs/request/grx/idot_trucks.php
@@ -4,6 +4,15 @@
  */
 header("Content-type: text/plain");
 
+// Try to get it from memcached
+$memcache = new Memcache;
+$memcache->connect('iem-memcached', 11211);
+$val = $memcache->get("/request/grx/idot_trucks.php");
+if ($val){
+	die($val);
+}
+ob_start();
+
 echo 'Title: IDOT Trucks
 Refresh: 5
 Color: 200 200 255
@@ -13,6 +22,21 @@ Font: 1, 11, 1, "Courier New"
 include("../../../config/settings.inc.php");
 include("../../../include/database.inc.php");
 $dbconn = iemdb("postgis");
+
+$q = 2;
+$rs = pg_query($dbconn, "SELECT label, ST_x(geom) as lon,
+		ST_y(geom) as lat
+		from idot_dashcam_current WHERE valid > (now() - '1 hour'::interval)");
+
+$s3 = "";
+for ($i=0; $row=@pg_fetch_assoc($rs,$i); $i++){
+	echo sprintf("IconFile: %s, 320, 240, %.0f, %.0f,\"http://mesonet.agron.iastate.edu/data/camera/idot_trucks/%s.jpg\"\n", 
+				$q, 0,0, $row["label"]);
+	$s3 .= sprintf("Icon: %.4f,%.4f,000,%s,1\n", 
+			$row['lat'], $row['lon'], $q);
+	$q += 1;
+}
+
 
 $rs = pg_query($dbconn, "SELECT airtemp, valid, ST_x(geom) as lon, 
 		ST_y(geom) as lat 
@@ -27,5 +51,8 @@ for ($i=0; $row=@pg_fetch_assoc($rs,$i); $i++){
 	."\n", $row["lat"], $row["lon"], $row["airtemp"]);
 }
 
+echo $s3;
 
+$memcache->set("/request/grx/idot_trucks.php", ob_get_contents(), false, 300);
+ob_end_flush();
 ?>
