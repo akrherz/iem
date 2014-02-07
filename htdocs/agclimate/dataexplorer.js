9c3c59c725fbfec1e1417daf7fef6fb1d1d54027
akrherz <akrherz@iastate.edu>
2014-02-07 16:49:57 -0600

    WIP, move ISUSM sortables to dedicated page, further improve viewer app

--- htdocs/agclimate/dataexplorer.js
+++ htdocs/agclimate/dataexplorer.js
@@ -1,4 +1,5 @@
 var map, gj, dtpicker, n0q;
+var varname = 'tmpf';
 var currentdt = new Date(defaultdt);
 
 function pad(number) {
@@ -40,9 +41,11 @@ function logic( dstring ){
 	currentdt = dtpicker.datetimepicker('getDate'); // toISOString()
 	updateMap();
 }
-
+function updateTitle(){
+	$('#maptitle').text("The map is displaying "
+			+ $('#varpicker :selected').text() + " valid at "+ currentdt);
+}
 function updateMap(){
-	gj.protocol.params.varname = $('#varpicker').val();
 	if (currentdt && typeof currentdt != "string"){
 		gj.protocol.params.dt = currentdt.toISOString();
 	}
@@ -52,6 +55,7 @@ function updateMap(){
 		n0q.setVisibility(false);
 		n0q.setVisibility(true);		
 	}
+	updateTitle();
 }
 function get_my_url (bounds) {
     var res = this.map.getResolution();
@@ -72,7 +76,22 @@ function get_my_url (bounds) {
     return url + this.service + "/ridge::" + this.radar + "-" + this.radarProduct + "-"
     +  currentdt.toIEMString() + "/" + path;
 }
+function get_my_url2(bounds){
+    var res = this.map.getResolution();
+    var x = Math.round((bounds.left - this.maxExtent.left)
+                    / (res * this.tileSize.w));
+    var y = Math.round((this.maxExtent.top - bounds.top)
+                    / (res * this.tileSize.h));
+    var z = this.map.getZoom();
 
+    var path = z + "/" + x + "/" + y + "." + this.type + "?"
+                    + parseInt(Math.random() * 9999);
+    var url = this.url;
+    if (url instanceof Array) {
+            url = this.selectUrl(path, url);
+    }
+    return url + this.service + "/" + this.layername + "/" + path;
+}
 
 function init(){
 	  // Build Map Object
@@ -108,30 +127,27 @@ function init(){
       );
 
 	  
-	  var styleMap = new OpenLayers.StyleMap({
-	       'default': {
-               strokeColor: "#00FF00",
-               strokeOpacity: 1,
-               strokeWidth: 3,
-               fillColor: "#FF5500",
-               fillOpacity: 0.5,
-               pointRadius: 6,
-               pointerEvents: "visiblePainted",
-               // label with \n linebreaks
-               label : "${value}",
+      var context = {
+    		  getLabel: function(feature){
+    			  return feature.data[varname];
+    		  }
+      };
+      
+	  var template = OpenLayers.Util.extend({
+               label : "${getLabel}",
                fontColor: "black",
-               fontSize: "12px",
-               fontFamily: "Courier New, monospace",
+               fontSize: "14px",
+               fontFamily: "monospace",
                fontWeight: "bold",
-               labelOutlineColor: "white",
-               labelOutlineWidth: 3
-	       }
+               labelOutlineColor: "yellow",
+               labelOutlineWidth: 6  
 	   });
+      var style = new OpenLayers.Style(template, {context: context});
+      var styleMap = new OpenLayers.StyleMap(style);
 
 	  gj = new OpenLayers.Layer.Vector("Data", {
 		  protocol: new OpenLayers.Protocol.HTTP({
-			  params: {dt: currentdt.toISOString(), 
-				  		varname: $('#varpicker').val()},
+			  params: {dt: currentdt.toISOString()},
 	          url: "/geojson/agclimate.py",
 	          format: new OpenLayers.Format.GeoJSON()
 	      }),
@@ -139,7 +155,20 @@ function init(){
 	      styleMap: styleMap,
 	      strategies: [new OpenLayers.Strategy.Fixed()]
 	  });
-	  map.addLayers([googleLayer,gj,n0q]);
+	  
+	  states = new OpenLayers.Layer.TMS('US States',
+              'https://mesonet.agron.iastate.edu/c/c.py/', {
+                      layername : 's-900913',
+                      service : '1.0.0',
+                      type : 'png',
+                      visibility : true,
+                      opacity : 1,
+                      getURL : get_my_url2,
+                      isBaseLayer : false
+              });
+
+	  
+	  map.addLayers([googleLayer, n0q, states, gj]);
 	   
 	  var proj = new OpenLayers.Projection('EPSG:4326');
 	   var proj2 = new OpenLayers.Projection('EPSG:900913');
@@ -157,25 +186,35 @@ function init(){
 		   showMinute: false,
 		   showSecond: false,
 		   onSelect: logic,
-		   minDate: new Date(2013, 1, 1, 0, 0),
-		   timeFormat: 'hh:mm tt'
+		   minDateTime: (new Date(2013, 1, 1, 0, 0)),
+		   maxDateTime: (new Date()),
+		   timeFormat: 'h:mm TT'
 	   });
-	   dtpicker.datetimepicker('setDate', defaultdt);
+	   setDate();
 	   updateMap();
 }
+function setDate(){
+	   dtpicker.datepicker( "disable" )
+	   	.datetimepicker('setDate', currentdt)
+	   	.datepicker( "enable" );
+}
 
-$('#plusonehour').click(function(){
+$('#plusonehour').click(function(e){
+	$(this).removeClass('focus');	
 	currentdt = new Date(currentdt.valueOf() + 3600000);
-	dtpicker.datetimepicker('setDate', currentdt);
+	   setDate();
 	updateMap();
 });
 
-$('#minusonehour').click(function(){
+$('#minusonehour').click(function(e){
+	$(this).removeClass('focus');
 	currentdt = new Date(currentdt - 3600000);
-	dtpicker.datetimepicker('setDate', currentdt);
+	   setDate();
 	updateMap();
 });
 
 $('#varpicker').change(function(){
-	updateMap();
+	varname = $('#varpicker').val();
+	gj.redraw();
+	updateTitle();
 });
--- htdocs/geojson/agclimate.py
+++ htdocs/geojson/agclimate.py
@@ -13,7 +13,45 @@ import json
 ISUAG = psycopg2.connect(database='isuag', host='iemdb', user='nobody')
 cursor = ISUAG.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
-def get_data( ts, varname ):
+def drct2txt(val):
+    ''' Convert val to textual '''
+    if val is None:
+        return "N"
+    if (val >= 350 or val < 13):
+        return "N"
+    elif (val >= 13 and val < 35):
+        return "NNE"
+    elif (val >= 35 and val < 57):
+        return "NE"
+    elif (val >= 57 and val < 80):
+        return "ENE"
+    elif (val >= 80 and val < 102):
+        return "E"
+    elif (val >= 102 and val < 127):
+        return "ESE"
+    elif (val >= 127 and val < 143):
+        return "SE"
+    elif (val >= 143 and val < 166):
+        return "SSE"
+    elif (val >= 166 and val < 190):
+        return "S"
+    elif (val >= 190 and val < 215):
+        return "SSW"
+    elif (val >= 215 and val < 237):
+        return "SW"
+    elif (val >= 237 and val < 260):
+        return "WSW"
+    elif (val >= 260 and val < 281):
+        return "W"
+    elif (val >= 281 and val < 304):
+        return "WNW"
+    elif (val >= 304 and val < 324):
+        return "NW"
+    elif (val >= 324 and val < 350):
+        return "NNW"
+
+
+def get_data( ts ):
     ''' Get the data for this timestamp '''
     nt = network.Table("ISUSM")
     data = {"type": "FeatureCollection", 
@@ -25,22 +63,19 @@ def get_data( ts, varname ):
     SELECT * from sm_hourly where valid = %s
     """, (ts,))
     for i, row in enumerate(cursor):
-        if varname == 'rh':
-            value = "%.0f%%" % (row["rh"],)
-        elif varname == 'tmpf':
-            value = "%.1f" % (temperature(row['tair_c_avg'], 'C').value('F'))
-        elif varname == 'wind':
-            value = "%s@%.0f" % (row['winddir_d1_wvt'], 
-                                 row['ws_mps_s_wvt'] * 2.23)
         lon = nt.sts[row['station']]['lon']
         lat = nt.sts[row['station']]['lat']
         data['features'].append({"type": "Feature", "id": i,
-                                 "properties": {"value": value
-                                                },
-                                 "geometry": {"type": "Point",
-                                              "coordinates": [lon, lat]
-                                              }
-                                 })
+            "properties": {
+                "rh":  "%.0f%%" % (row["rh"],),
+                "tmpf": "%.1f" % (temperature(row['tair_c_avg'], 'C').value('F')),
+                "wind": "%s@%.0f" % (drct2txt(row['winddir_d1_wvt']), 
+                                    row['ws_mps_s_wvt'] * 2.23)
+            },
+            "geometry": {"type": "Point",
+                        "coordinates": [lon, lat]
+            }
+        })
     sys.stdout.write(json.dumps(data))
 
 if __name__ == '__main__':
@@ -50,5 +85,4 @@ if __name__ == '__main__':
     dt = field.getfirst('dt')
     ts = datetime.datetime.strptime(dt, '%Y-%m-%dT%H:%M:%S.000Z')
     ts = ts.replace(tzinfo=pytz.timezone("UTC"))
-    varname = field.getfirst('varname', 'rh')
-    get_data( ts, varname )
+    get_data( ts )
