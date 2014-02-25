5bd400ee4bdb1ae6a9e13cd352b35f85c7832672
akrherz <akrherz@iastate.edu>
2014-02-25 17:03:34 -0600

    remove unused tile.cgi

--- htdocs/cache/tile.py
+++ htdocs/cache/tile.py
@@ -1,5 +1,6 @@
-#import sys
-#sys.exit()
+"""
+ This is mod_python magic with the help of what is in .htaccess
+"""
 from TileCache import Service, cgiHandler, cfgfiles
 
 if __name__ == '__main__':
