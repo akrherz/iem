ef37e14d020432f2800b76e4bd18a7988e151761
akrherz <akrherz@iastate.edu>
2014-02-27 17:22:02 -0600

    cleanups and use template

--- htdocs/info/contacts.php
+++ htdocs/info/contacts.php
@@ -1,48 +1,47 @@
 <?php 
 include("../../config/settings.inc.php");
-$TITLE = "IEM | Contacts";
-include("$rootpath/include/header.php"); ?>
+include("../../include/myview.php");
+$t = new MyView();
+$t->title = "Contacts";
 
-<h3 class="heading">IEM Contacts</font></h3>
 
-<table><tr><td>
+$t->west = <<<EOF
+<h3>IEM Contacts</font></h3>
 
+<p><div class="alert alert-info">We are very responsive to emails!  Please
+ consider emailing us before calling!</div>
+		
 <p><h3 class="subtitle">IEM Coordinator:</h3><br>
-<div style="margin-left: 20px">
-Dr Raymond Arritt
+		
+<p>Dr Raymond Arritt
 <br>3010 Agronomy Hall
 <br>Iowa State University
 <br>Ames, IA 50011
 <br><i>Email:</i> <a href="mailto:rwarritt&#064;bruce&#046;agron&#046;iastate&#046;edu">rwarritt&#064;bruce&#046;agron&#046;iastate&#046;edu</a>
-</div>
 
-<p><h3 class="subtitle">IEM Assistant Scientist:</h3><br>
-<div style="margin-left: 20px">
-Daryl Herzmann
+<h3>IEM Assistant Scientist:</h3><br>
+		
+<p>Daryl Herzmann
 <br>3010 Agronomy Hall
 <br>Iowa State University
 <br>Ames, IA 50011
 <br><i>Email:</i> <a href="mailto:akrherz@iastate.edu">akrherz@iastate.edu</a>
-<br><i>Phone:</i> 515.294.5978
-<br><i>Fax:</i> 515.294.2619
+<br><i>Office Phone:</i> 515.294.5978
+<br /><i>Jabber:</i> akrherz@jabber.org
+<br /><i>Google Talk</i> akrherz@gmail.com
+<br /><i>Twitter</i> <a href="https://twitter.com/akrherz">@akrherz</a> 
 <br><i>Yahoo IM:</i> iowamesonet , <img src="http://opi.yahoo.com/online?u=iowamesonet&m=g&t=1">
-</div>
 
-<p><h3 class="subtitle">24 Hour/Day Technical Support Hotline:</h3><br>
-<div style="margin-left: 20px">
-<i>Phone:</i> 515.451.9249
-</div>
+<p>If all those fail you, try <i>Daryl's Cell Phone! </i> 515.451.9249
 
-</td><td width="50%">
-
+EOF;
+$t->east = <<<EOF
 <a href="http://www.nsf.gov"><img src="/images/nsf.gif" border="0"></a>
 <br clear="all" />This website is based upon work supported by grants from the National Science
 Foundation. Opinions, findings, and conclusions or recommendations
 expressed in this material are those of the author(s) and do not necessarily
 reflect the views of the National Science Foundation.
 
-</td></tr></table>
-
-<!-- Begin the bottom of the page -->
-
-<?php include("$rootpath/include/footer.php"); ?>
+EOF;
+$t->render('single.phtml');
+?>
