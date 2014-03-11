05872289bfbf798934bf755e783c6f896314d083
akrherz <akrherz@iastate.edu>
2014-03-10 21:02:31 -0500

    11 mar feature

--- scripts/feature/high_v_snow.py
+++ scripts/feature/high_v_snow.py
@@ -9,7 +9,7 @@ icursor.execute("""
     SELECT ST_x(geom), ST_y(geom), snowd from summary_2014 s JOIN stations t 
     ON (t.iemid = s.iemid) WHERE
     network in ('IA_COOP') and 
-    snowd >= 0 and day = '2014-02-18'
+    snowd >= 0 and day = '2014-03-10'
 """)
 for row in icursor:
     snowdlats.append( row[1] )
@@ -23,7 +23,7 @@ icursor.execute("""
     SELECT ST_x(geom), ST_y(geom), max_tmpf, id from summary_2014 s JOIN stations t
     on (t.iemid = s.iemid) WHERE
     network in ('IA_ASOS','AWOS') 
-    and max_tmpf > 0 and day = '2014-02-18' ORDER by max_tmpf ASC
+    and max_tmpf > 0 and day = '2014-03-10' ORDER by max_tmpf ASC
 """)
 for row in icursor:
     print row
@@ -44,7 +44,7 @@ ax.scatter( snowd[2:-2,2:-2].flatten(), highs[2:-2,2:-2].flatten() )
 ax.set_xlim(-0.3,30)
 ax.set_xlabel('Morning Snow Depth [inch]')
 ax.set_ylabel('Afternoon High Temperature [F]')
-ax.set_title("18 Feb 2014: High Temperature  vs. Snow Depth\nPoint Comparison of Gridded Analysis over Iowa")
+ax.set_title("10 Mar 2014: High Temperature  vs. Snow Depth\nPoint Comparison of Gridded Analysis over Iowa")
 ax.grid(True)
 
 fig.savefig('test.ps')
