35cac0a08185f91f64dd485452c06135fc5a072e
akrherz <akrherz@iastate.edu>
2014-03-14 07:57:37 -0500

    cleanups

--- htdocs/AWOS/reports/mon_prec.php
+++ htdocs/AWOS/reports/mon_prec.php
@@ -1,10 +1,12 @@
 <?php
 include("../../../config/settings.inc.php");
 define("IEM_APPID", 38);
+include("../../../include/myview.php");
+$t = new MyView();
 $year = isset($_GET["year"]) ? intval($_GET["year"]): date("Y");
-include("$rootpath/include/database.inc.php");
+include("../../../include/database.inc.php");
 $pgconn = iemdb("access");
-include("$rootpath/include/network.php");
+include("../../../include/network.php");
 $nt = new NetworkTable("AWOS");
 $cities = $nt->table;
 
@@ -28,7 +30,7 @@ for($i=0;$row=@pg_fetch_array($rs,$i);$i++)
   }
   $data[$row["id"]][intval($row["month"])-1] = $row["precip"];
 }
-$HEADEXTRA = '<link rel="stylesheet" type="text/css" href="http://extjs.cachefly.net/ext-3.4.0/resources/css/ext-all.css"/>
+$t->headextra = '<link rel="stylesheet" type="text/css" href="http://extjs.cachefly.net/ext-3.4.0/resources/css/ext-all.css"/>
 <script type="text/javascript" src="http://extjs.cachefly.net/ext-3.4.0/adapter/ext/ext-base.js"></script>
 <script type="text/javascript" src="http://extjs.cachefly.net/ext-3.4.0/ext-all.js"></script>
 <script type="text/javascript" src="../../ext/ux/TableGrid.js"></script>
@@ -50,14 +52,37 @@ Ext.onReady(function(){
 });
 </script>
 ';
-$THISPAGE = "networks-awos";
-$TITLE = "IEM Iowa AWOS Monthly Precipitation";
-include("$rootpath/include/header.php"); 
-?>
+$t->thispage = "networks-awos";
+$t->title = "Iowa AWOS Monthly Precipitation";
 
-<h3 class="heading"><?php echo $year; ?> Iowa AWOS Precipitation Report</h3>
+reset($data);
+function friendly($val){
+	if ($val == null) return "M";
+	return sprintf("%.2f", $val);
+}
+$table = "";
+while (list($key,$val) = each($data)){
+	$table .= sprintf("<tr><td>%s</td><td>%s</td>
+  <td>%s</td><td>%s</td><td>%s</td>
+  <td>%s</td><td>%s</td><td>%s</td>
+  <td>%s</td><td>%s</td><td>%s</td>
+  <td>%s</td><td>%s</td><td>%s</td>
+  <td>%.2f</td><td>%.2f</td>
+  </tr>", $key, $cities[$key]["name"],
+			friendly($val[0]), friendly($val[1]), friendly($val[2]),
+			friendly($val[3]), friendly($val[4]), friendly($val[5]),
+			friendly($val[6]), friendly($val[7]), friendly($val[8]),
+			friendly($val[9]), friendly($val[10]), friendly($val[11]),
+			array_sum(array_slice($val,4,4)),
+			array_sum($val)
+	);
+}
 
-<p>This table was generated at <?php echo date("d M Y h a"); ?> and is based
+$d = date("d M Y h a");
+$t->content = <<<EOF
+<h3>{$year} Iowa AWOS Precipitation Report</h3>
+
+<p>This table was generated at {$d} and is based
 on AWOS minute by minute data.  No attempt was made to estimate missing data.</p>
 
 <button id="create-grid" type="button">Interactive Grid</button>
@@ -84,30 +109,9 @@ on AWOS minute by minute data.  No attempt was made to estimate missing data.</p
 </TR>
 </thead>
 <tbody>
-<?php
-reset($data);
-function friendly($val){
- if ($val == null) return "M";
- return sprintf("%.2f", $val); 
-}
-while (list($key,$val) = each($data)){
-  echo sprintf("<tr><td>%s</td><td>%s</td>
-  <td>%s</td><td>%s</td><td>%s</td>
-  <td>%s</td><td>%s</td><td>%s</td>
-  <td>%s</td><td>%s</td><td>%s</td>
-  <td>%s</td><td>%s</td><td>%s</td>
-  <td>%.2f</td><td>%.2f</td>
-  </tr>", $key, $cities[$key]["name"],
-  friendly($val[0]), friendly($val[1]), friendly($val[2]),
-  friendly($val[3]), friendly($val[4]), friendly($val[5]),
-  friendly($val[6]), friendly($val[7]), friendly($val[8]),
-  friendly($val[9]), friendly($val[10]), friendly($val[11]),
-  array_sum(array_slice($val,4,4)),
-  array_sum($val)
-);
-}
-?>
+{$table}
 </tbody>
 </table>
-
-<?php include("$rootpath/include/footer.php"); ?>
+EOF;
+$t->render('single.phtml');
+?>
