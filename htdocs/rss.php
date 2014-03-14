5ef3cb303e35058815a974a4017e91ad5a0ceb83
akrherz <akrherz@iastate.edu>
2014-03-14 08:36:33 -0500

    protect with memcached and use CDATA for description.

--- htdocs/rss.php
+++ htdocs/rss.php
@@ -1,34 +1,47 @@
 <?php
  include("../config/settings.inc.php");
- define("IEM_APPID", 60);
- include("$rootpath/include/database.inc.php");
  header("Content-type: text/xml");
- echo "<?xml version=\"1.0\" encoding=\"iso-8859-1\"?>\n";
- echo "<rss version=\"2.0\" xmlns:atom=\"http://www.w3.org/2005/Atom\">\n";
- echo "<channel>\n";
- echo "<atom:link href=\"http://mesonet.agron.iastate.edu/rss.php\" rel=\"self\" type=\"application/rss+xml\" />\n";
- echo "<title>Iowa Environmental Mesonet</title>\n";
- echo "<link>http://mesonet.agron.iastate.edu</link>\n";
- echo "<description>
-  Iowa Environmental Mesonet News and Notes
-</description>\n";
- echo "<lastBuildDate>". date('D, d M Y H:i:s O')  ."</lastBuildDate>\n";
+ 
+ $memcache = new Memcache;
+ $memcache->connect('iem-memcached', 11211);
+ $val = $memcache->get("/rss.php");
+ if ($val){
+ 	die($val);
+ }
+ // Need to buffer the output so that we can save it to memcached later
+ ob_start();
+ 
+ 
+ define("IEM_APPID", 60);
+ include("../include/database.inc.php");
+ 
+ $bd = date('D, d M Y H:i:s O');
+ echo <<<EOF
+<?xml version="1.0" encoding="iso-8859-1"?>
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+<channel>
+<atom:link href="http://mesonet.agron.iastate.edu/rss.php" rel="self" type="application/rss+xml" />
+<title>Iowa Environmental Mesonet</title>
+<link>http://mesonet.agron.iastate.edu</link>
+<description>Iowa Environmental Mesonet News and Notes</description>
+<lastBuildDate>{$bd}</lastBuildDate>
+EOF;
  $conn = iemdb("mesosite");
  $rs = pg_exec($conn, "SELECT * from news ORDER by entered DESC LIMIT 20");
  pg_close($conn);
- for ($i=0; $row = @pg_fetch_array($rs, $i); $i++) {
+ for ($i=0; $row = @pg_fetch_assoc($rs, $i); $i++) {
   echo "<item>\n";
   echo "<title>". ereg_replace("&","&amp;",$row["title"]) ."</title>\n";
   echo "<author>akrherz@iastate.edu (Daryl Herzmann)</author>\n";
   echo "<link>http://mesonet.agron.iastate.edu/onsite/news.phtml?id=". $row["id"] ."</link>\n";
   echo "<guid>http://mesonet.agron.iastate.edu/onsite/news.phtml?id=". $row["id"] ."</guid>\n";
-  if ( intval($row["id"]) < 830){
-    echo "<description>&lt;pre&gt;". $row["body"] ."&lt;/pre&gt;</description>\n";
-  } else {
-    echo "<description>". htmlentities($row["body"]) ."</description>\n";
-  }
+  echo "<description><![CDATA[". $row["body"] ."]]></description>\n";
   echo "</item>\n";
  }
- echo "</channel>\n";
- echo "</rss>\n";
+echo "</channel>\n";
+echo "</rss>\n";
+
+$memcache->set("/rss.php", ob_get_contents(), false, 600); // ten minutes
+ob_end_flush();
+
 ?>
