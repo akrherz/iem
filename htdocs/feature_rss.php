7ebb305e40058b8b6519691ae0a2f592a4df2364
akrherz <akrherz@iastate.edu>
2014-03-17 07:59:32 -0500

    implement caching and what we learned from news rss

--- htdocs/feature_rss.php
+++ htdocs/feature_rss.php
@@ -1,34 +1,50 @@
 <?php
  include("../config/settings.inc.php");
  define("IEM_APPID", 62);
- include("$rootpath/include/database.inc.php");
- header("Content-type: text/xml");
- echo "<?xml version=\"1.0\" encoding=\"iso-8859-1\"?>\n";
- echo "<rss version=\"2.0\" xmlns:atom=\"http://www.w3.org/2005/Atom\">\n";
- echo "<channel>\n";
- echo "<atom:link href=\"http://mesonet.agron.iastate.edu/feature_rss.php\" rel=\"self\" type=\"application/rss+xml\" />\n";
- echo "<title>Iowa Environmental Mesonet Daily Feature</title>\n";
- echo "<link>http://mesonet.agron.iastate.edu</link>\n";
- echo "<description>
-  Iowa Environmental Mesonet Daily Feature
-</description>\n";
- echo "<lastBuildDate>". date('D, d M Y H:i:s O') ."</lastBuildDate>\n";
+ header("Content-type: text/xml; charset=UTF-8");
+ 
+ $memcache = new Memcache;
+ $memcache->connect('iem-memcached', 11211);
+ $val = $memcache->get("/feature_rss.php");
+ if ($val){
+ 	die($val);
+ }
+ // Need to buffer the output so that we can save it to memcached later
+ ob_start();
+ 
+ include("../include/database.inc.php");
+ $d = date('D, d M Y H:i:s O');
+ echo <<<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+<channel>
+<atom:link href="http://mesonet.agron.iastate.edu/feature_rss.php" rel="self" type="application/rss+xml" />
+<title>Iowa Environmental Mesonet Daily Feature</title>
+<link>http://mesonet.agron.iastate.edu</link>
+<description>Iowa Environmental Mesonet Daily Feature</description>
+<lastBuildDate>{$d}</lastBuildDate>
+EOF;
  $conn = iemdb("mesosite");
  $rs = pg_exec($conn, "SELECT *, to_char(valid, 'YYYY/MM/YYMMDD') as imageref from feature ORDER by valid DESC LIMIT 20");
  pg_close($conn);
- for ($i=0; $row = @pg_fetch_array($rs, $i); $i++) {
+ for ($i=0; $row = @pg_fetch_assoc($rs, $i); $i++) {
   $cbody = "<img src=\"http://mesonet.agron.iastate.edu/onsite/features/". $row["imageref"] ."_s.png\" alt=\"Feature\" style=\"float: left; padding: 5px;\"/>". $row["story"];
-  $cbody = ereg_replace("&","&amp;", $cbody);
-  $cbody = ereg_replace(">","&gt;", $cbody);
-  $cbody = ereg_replace("<","&lt;", $cbody);
-  echo "<item>\n";
-  echo "<title>". ereg_replace("&","&amp;",$row["title"]) ."</title>\n";
-  echo "<author>akrherz@iastate.edu (Daryl Herzmann)</author>\n";
-  echo "<link>http://mesonet.agron.iastate.edu/onsite/features/cat.php?day=". substr($row["valid"],0,10) ."</link>\n";
-  echo "<guid>http://mesonet.agron.iastate.edu/onsite/features/cat.php?day=". substr($row["valid"],0,10) ."</guid>\n";
-  echo "<description>". $cbody ."</description>\n";
-  echo "</item>\n";
+  $t = htmlentities($row["title"]);
+  $v = substr($row["valid"],0,10);
+  echo <<<EOF
+<item>
+<title>{$t}</title>
+<author>akrherz@iastate.edu (Daryl Herzmann)</author>
+<link>http://mesonet.agron.iastate.edu/onsite/features/cat.php?day={$v}</link>
+<guid>http://mesonet.agron.iastate.edu/onsite/features/cat.php?day={$v}</guid>
+<description><![CDATA[{$cbody}]]></description>
+</item>
+EOF;
  }
  echo "</channel>\n";
  echo "</rss>\n";
+ 
+ $memcache->set("/feature_rss.php", ob_get_contents(), false, 3600); // one hour
+ ob_end_flush();
+ 
 ?>
