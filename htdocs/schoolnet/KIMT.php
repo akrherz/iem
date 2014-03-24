6d2296dbdf0ef4145c8ecfee243e08a111dc7e18
akrherz <akrherz@iastate.edu>
2014-03-24 08:51:47 -0500

    remove rooturl usage and fix one link

--- htdocs/schoolnet/KIMT.php
+++ htdocs/schoolnet/KIMT.php
@@ -1,9 +1,9 @@
 
 <ul>
  <li><a href="current.phtml">Current Conditions</a> (sortable)</li>
- <li><a href="<?php echo $rooturl; ?>/GIS/apps/snet/raining.php?tv=KIMT">Where is it raining?</a></li>
- <li><a href="<?php echo $rooturl; ?>/plotting/snet/1station_1min.php">1 minute traces</a></li>
- <li><a href="<?php echo $rooturl; ?>/GIS/apps/mesoplot/plot.php?network=KIMT">Rapid Update Mesoplot</a></li>
+ <li><a href="/GIS/apps/snet/raining.php?tv=KIMT">Where is it raining?</a></li>
+ <li><a href="/plotting/snet/1station_1min.php">1 minute traces</a></li>
+ <li><a href="/GIS/apps/mesoplot/plot.php?network=KIMT">Rapid Update Mesoplot</a></li>
  <li><a href="/plotting/compare/">Generate Interactive Comparisons</a> between two sites of your choice.</li>
  <li><a href="/QC/madis/network.phtml?network=KIMT">MADIS Raw QC</a>.</li>
   <li>MADIS QC Messages:
