329109d7290e3705a8faf3daa3fb9dde24e266ed
akrherz <akrherz@iastate.edu>
2014-03-25 12:37:08 -0500

    my first upgrade of an app to Extjs4 TODO: figure out what to do about responsive small displays

--- htdocs/timemachine/index.php
+++ htdocs/timemachine/index.php
@@ -6,10 +6,9 @@ $t->title = "Time Machine";
 define("IEM_APPID", 148);
 $t->thispage="archive-tm";
 $t->headextra = '
-<link rel="stylesheet" type="text/css" href="http://cdn.sencha.com/ext/gpl/3.4.1.1/resources/css/ext-all.css"/>
-<script type="text/javascript" src="http://cdn.sencha.com/ext/gpl/3.4.1.1/adapter/ext/ext-base.js"></script>
-<script type="text/javascript" src="http://cdn.sencha.com/ext/gpl/3.4.1.1/ext-all.js"></script>
-<script type="text/javascript" src="static.js?v=15"></script>
+<link rel="stylesheet" type="text/css" href="http://cdn.sencha.com/ext/gpl/4.2.1/resources/css/ext-all.css"/>
+<script type="text/javascript" src="http://cdn.sencha.com/ext/gpl/4.2.1/ext-all.js"></script>
+<script type="text/javascript" src="static.js?v=16"></script>
 ';
 
 $t->content = <<<EOF
