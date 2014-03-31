eb4030bfac3d384d32dbca04fd1a6a256d2289f6
akrherz <akrherz@iastate.edu>
2014-03-30 21:48:11 -0500

    27 march feature

--- scripts/feature/warnings/count_by_week.py
+++ scripts/feature/warnings/count_by_week.py
@@ -14,8 +14,8 @@ def get(state):
      SELECT week, count(*) from 
      (SELECT distinct extract(year from issue) as year, 
      round((extract(doy from issue)::numeric - 1.0) / 7.,0) as week from
-     warnings where phenomena = 'TO' and significance = 'W' and substr(ugc,1,2) = %s
-     and issue < '2013-01-01') as foo 
+     warnings where phenomena = 'SV' and significance = 'W' and substr(ugc,1,2) = %s
+     and issue < '2014-01-01') as foo 
      WHERE week < 53 GROUP by week ORDER by week ASC
     """, (state,))
     weeks = []
@@ -36,8 +36,7 @@ for i, state in enumerate(['IA', 'OK', 'AL', 'TX']):
     counts[ws] = cs
     ax.scatter(np.arange(1,54), counts, marker=markers[i], s=75, label=states[i], c=c[i],
                zorder=i+1)
-ax.set_title("1986-2012 Percentage of Years with\n 1+ Tornado Warning for the Week of Year")
-ax.set_ylabel("March Total")
+ax.set_title("1986-2013 Percentage of Years with\n 1+ Severe Thunderstorm Warning for the Week of Year")
 ax.set_xlim(0, 53)
 ax.set_yticks([0,10,25,50,75,90,100])
 ax.set_ylim(-2, 101)
@@ -46,7 +45,7 @@ ax.set_xticks( np.arange(1,56,7) )
 ax.set_xticklabels( ('Jan 1', 'Feb 19', 'Apr 8', 'May 27', 'Jul 15', 'Sep 2', 'Oct 21', 'Dec 9'))
 
 ax.grid(True)
-ax.legend()
+ax.legend(loc=8)
 
 fig.savefig('test.ps')
 import iemplot
