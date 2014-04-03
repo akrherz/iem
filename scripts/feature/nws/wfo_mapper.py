29e49e25f380a67aa3ae704e7a7de68dc4b31cbb
akrherz <akrherz@iastate.edu>
2014-04-02 20:53:35 -0500

    3 april feature

--- scripts/feature/nws/wfo_mapper.py
+++ scripts/feature/nws/wfo_mapper.py
@@ -1,4 +1,5 @@
 import psycopg2
+import datetime
 #import numpy as np
 from pyiem.plot import MapPlot
 
@@ -8,45 +9,35 @@ from pyiem.plot import MapPlot
 #bins = numpy.arange(0,201,20)
 #bins = [0,50,100,150,200,250,300,500,750,1000,1500,2000,2500,3000,3500]
 #bins = [0,25,50,75,100,150,200,250,300,400,500,750,1000,1250]
-bins = [0,0.2,0.4,0.6,0.8,1,1.2,1.4,1.6,1.8,2,2.5,3,4,6,]
+#bins = [0,0.2,0.4,0.6,0.8,1,1.2,1.4,1.6,1.8,2,2.5,3,4,6,]
+bins = [1,32,60,91,121,152,182,213,244,274,305,335,365]
+clabels =['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
 
 POSTGIS = psycopg2.connect(database='postgis', host='mesonet.agron.iastate.edu', user='nobody')
 pcursor = POSTGIS.cursor()
 pcursor.execute("""
-WITH lsrcount as (
-SELECT wfo, count(*) / 6.0 as a from 
-
-(select distinct wfo, geom, typetext, magnitude, valid from lsrs WHERE
- valid > '2008-01-01' and type in ('T','H','G','D','M','W','F','x')) as foo
-
-GROUP by wfo),
-    vteccount as (
-    SELECT wfo, count(*) / 6.0 as a from 
-
-(select distinct wfo, phenomena, extract(year from issue), eventid from sbw WHERE
- issue > '2008-01-01' and significance = 'W' and 
- phenomena in ('SV','TO','FF','MA')) as foo2
-
-GROUP by wfo
-    
-    )
-
-SELECT lsrcount.wfo, lsrcount.a / vteccount.a as ratio from vteccount JOIN lsrcount on (lsrcount.wfo = vteccount.wfo)
-ORDER by ratio DESC
-    
+   SELECT wfo, avg(min) from 
+   (SELECT wfo, extract(year from issue) as yr, min(extract(doy from issue))
+   from warnings where phenomena = 'SV' and significance = 'W' 
+   and issue < '2014-01-01' GROUP 
+   by wfo, yr) as foo
+   GROUP by wfo
 """)
 
 data = {}
+labels = {}
 for row in pcursor:
     if row[1] is None:
         continue
-    print row
+    ts = datetime.datetime(2000,1,1) + datetime.timedelta(days=(row[1]-1))
+    labels[ row[0] ] = "%s %s%s" % (ts.strftime("%-d"), ts.strftime("%b")[0],
+                           ts.strftime("%b")[2])
     data[ row[0] ] = row[1] 
     
 p = MapPlot(sector='nws',
-                 title='1 Jan 2008 - 18 Dec 2013 Ratio of Unique LSRs to Warnings',
-                 subtitle='ratio of severe/flash flood LSRs to warnings issued')
-p.fill_cwas(data, bins=bins, lblformat='%.1f', units='1')
-p.postprocess(filename='test.png')
-#import iemplot
-#iemplot.makefeature('test')
+                 title="Average Date of First Sever T'Storm Warning by NWS Forecast Office",
+                 subtitle='based on data from 1986-2013')
+p.fill_cwas(data, bins=bins, labels=labels, lblformat='%s', clevlabels=clabels)
+p.postprocess(filename='test.ps')
+import iemplot
+iemplot.makefeature('test')
