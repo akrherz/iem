745def684e42a3f254e9d9685d62366a6e0f722f
akrherz <akrherz@iastate.edu>
2014-04-22 18:09:31 -0500

    cleanups

--- scripts/feature/coop/moving.py
+++ scripts/feature/coop/moving.py
@@ -5,8 +5,8 @@ COOP = iemdb.connect('coop', bypass=True)
 ccursor = COOP.cursor()
 
 ccursor.execute("""
-select year, sum(precip) from alldata where stationid = 'ia0000'
- and year < 2011 GROUP by year ORDER by year ASC
+select year, sum(precip) from alldata where station = 'IA0000'
+ and year < 2015 GROUP by year ORDER by year ASC
 """)
 mt = []
 for row in ccursor:
@@ -20,15 +20,14 @@ for i in range(30, len(mt)):
 fig = plt.figure(figsize=(11,6))
 ax = fig.add_subplot(111)
 
-ax.bar( numpy.arange(1893,2011) - 0.4, mt, facecolor='#96A3FF', edgecolor="#96A3FF")
-ax.plot( numpy.arange(1923,2011), ma, c='r', zorder=2, label='30 Year MA')
+ax.bar( numpy.arange(1893,2015) - 0.4, mt, facecolor='#96A3FF', edgecolor="#96A3FF")
+ax.plot( numpy.arange(1923,2015), ma, c='r', zorder=2, label='30 Year MA')
 
-ax.set_xlim(1892.5,2011)
 ax.set_title("Iowa Average Precipitation [1960-2010]")
 ax.set_ylabel("Precipitation [inch]")
 #ax.set_xlabel("2010 December thru 19 Dec")
 ax.set_ylim(15,50)
-ax.set_xlim(1959.5,2010.5)
+ax.set_xlim(1892.5,2014.5)
 
 ax.grid(True)
 ax.legend(loc=2)
---
+++ scripts/feature/warnings/county_mapper.py
@@ -0,0 +1,74 @@
+import matplotlib.colors as mpcolors
+import matplotlib.cm as cm
+import psycopg2
+import datetime
+import subprocess
+
+from pyiem.plot import MapPlot
+POSTGIS = psycopg2.connect(database='postgis', host='iemdb', user='nobody')
+pcursor = POSTGIS.cursor()
+
+opts = {
+        'subtitle': "In Tornado Warning",
+        'sdate': datetime.date(1997, 1, 1),
+        'edate': datetime.date(2014, 1, 1),
+        'normalized': False,
+        'dbcols': ('TO',),
+        }
+opts['title'] = 'Yearly Average Minutes'
+opts['years'] = opts['edate'].year - opts['sdate'].year
+opts['units'] = 'minutes'
+bins = [1,2,5,10,15,20,25,30,45,60,75,90,120,150,180,240]
+#bins = [1,10,30,45,60,90,120,180,240,300,360,480,600,720,840]
+#bins = [1,30,60,120,240,360,480,600,840,1080,1320, 1540, 1760]
+
+if opts['normalized']:
+    opts['title'] += " (Normalized by County Size)"
+    opts['units'] += " / sq km"
+    #bins = [0.001, 0.002, 0.003, 0.005, 0.007, 0.01, 0.02, 0.05, 0.1, 0.2, 0.3, 0.4]
+    bins = [0.001, 0.003, 0.005, 0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.8]
+
+fn = "%s_%s_%s_%s.png" % (opts['sdate'].year, opts['edate'].year,
+                          "-".join( opts['dbcols']),
+                          'normalized' if opts['normalized'] else 'data')
+
+#bins = [0.001, 0.003, 0.005, 0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.8]
+#bins = [1,2,3,4,5,6,7,8,9,10,15,20,30,50,100,125]
+#bins = [0.02,0.5, 1, 2, 4, 5, 6, 7, 8, 9, 10, 13, 16, 20, 24, 30]
+#bins = [1,2,5,10,15,20,25,30,45,60,75,90,120,150,180,240]
+#bins = [1,2,3,4,5,7,10,15,20,25,30,35,40,50,75,100]
+
+
+cmap = cm.get_cmap("jet")
+cmap.set_under("#ffffff")
+cmap.set_over("#000000")
+
+m = MapPlot(sector='nws',
+            title=opts['title'],
+            subtitle='%s, period: %s thru %s' % (opts['subtitle'],
+                                    opts['sdate'].strftime("%-d %b %Y"),
+                                    opts['edate'].strftime("%-d %b %Y")),
+            cwas=True, nostates=True)
+
+norm = mpcolors.BoundaryNorm(bins, cmap.N)
+
+pcursor.execute("""
+ WITH data as (SELECT ugc, count(*) / %s  as data from 
+ (select distinct ugc, generate_series(issue, expire, '1 minute'::interval) 
+ from warnings where phenomena in %s and significance = 'W' 
+ and ugc is not null and (expire - issue) < '1440 minutes'::interval
+ and issue > %s and issue < %s) as foo2
+ GROUP by ugc),
+ u as (SELECT ugc, ST_Area(ST_Transform(geom, 2163)) / 1000000. as area
+ from ugcs where substr(ugc,3,1) = 'C' and end_ts is null)
+ 
+ SELECT data.ugc, data.data, data.data / u.area 
+ from data JOIN u on (u.ugc = data.ugc) 
+""", (opts['years'], opts['dbcols'], opts['sdate'], opts['edate']))
+data = {}
+for row in pcursor:
+    data[ row[0] ] = row[ 2 if opts['normalized'] else 1 ]
+
+m.fill_ugc_counties(data, bins, cmap=cmap, units=opts['units'])
+m.postprocess(filename=fn)
+subprocess.call("xv %s" % (fn,), shell=True)
