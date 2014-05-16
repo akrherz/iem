b06622a2b60bc0bcb24a98fe4dd13e8c94fe6044
akrherz <akrherz@iastate.edu>
2014-05-16 07:27:33 -0500

    16 May feature

--- scripts/feature/asos/hourly_temp_percentiles.py
+++ scripts/feature/asos/hourly_temp_percentiles.py
@@ -1,20 +1,18 @@
 # Generate some comparison data between ASOS sites, tricky, me thinks
 
-import iemdb
-import datetime
-import numpy
-import mx.DateTime
-ASOS = iemdb.connect('asos', bypass=True)
+import psycopg2
+import numpy as np
+ASOS = psycopg2.connect(database='asos', host='iemdb', user='nobody')
 acursor = ASOS.cursor()
 acursor2 = ASOS.cursor()
 
 # Get this year's hourly data
-percentiles = numpy.zeros( (24,) )
-d2011 = numpy.zeros( (24,) )
-climate = numpy.zeros( (24,) )
+percentiles = np.zeros( (24,) )
+today = np.zeros( (24,) )
+climate = np.zeros( (24,) )
 acursor.execute("""
   SELECT extract(hour from valid + '10 minutes'::interval) as hr, avg(tmpf)
-  from t2011 where valid BETWEEN '2011-07-01' and '2011-08-01' and 
+  from t2014 where valid BETWEEN '2014-05-14 23:50' and '2014-05-15 23:50' and 
   (extract(minute from valid) between 50 and 59 or 
         extract(minute from valid) = 0) and
   station = 'DSM' GROUP by hr ORDER by hr ASC
@@ -24,61 +22,54 @@ for row in acursor:
     ob = row[1]
     # Lookup value
   
-    acursor2.execute("""SELECT extract(year from valid) as yr,
-     avg(tmpf), count(*) from alldata where station = 'DSM'
+    acursor2.execute("""SELECT 
+     sum(case when tmpf < %s then 1 else 0 end),
+     sum(case when tmpf > %s then 1 else 0 end)
+     from alldata where station = 'DSM'
      and (extract(minute from valid) between 50 and 59 or 
         extract(minute from valid) = 0)
      and extract(hour from valid + '10 minutes'::interval) = %s
-     and extract(month from valid) = 7 and valid < '2011-01-01'
-     GROUP by yr ORDER by avg ASC
-    """, (hr,))
-    years = acursor2.rowcount + 1 # 2011
-    place = 0
-    for row in acursor2:
-        if ob < row[1]:
-           break
-        place += 1
+     and extract(month from valid) = 5 and 
+     extract(day from valid) between 11 and 19 and valid < '2014-01-01'
+     
+    """, (ob, ob, hr))
+    row2 = acursor2.fetchone()
+    place = row2[0] / float(row2[0] + row2[1]) * 100.
         
-    print hr, ob, place, years
-    percentiles[hr] = float(place) / float(years) * 100.
-    d2011[hr] = ob
+    print hr, ob, place
+    percentiles[hr] = float(place) 
+    today[hr] = ob
 
     acursor2.execute("""SELECT 
      avg(tmpf) from alldata where station = 'DSM'
      and (extract(minute from valid) between 50 and 59 or 
         extract(minute from valid) = 0)
      and extract(hour from valid + '10 minutes'::interval) = %s
-     and extract(month from valid) = 7 and valid < '2011-01-01'
+     and extract(month from valid) = 5 and
+     extract(day from valid) between 11 and 19 and valid < '2014-01-01'
     """, (hr,))
     row = acursor2.fetchone()
     climate[hr] = row[0]
 
 import matplotlib.pyplot as plt
 
-fig = plt.figure()
-ax = fig.add_subplot(211)
+fig, ax = plt.subplots(2,1, sharex=True)
 
-ax.set_title("Des Moines July 2011 Hourly Temperatures (1932-2011)")
+ax[0].set_title("Des Moines 15 May 2014 Hourly Temperatures\nversus 11-18 May 1932-2013 Hourly Climatology")
 
-ax.bar(numpy.arange(0,24)-0.4, d2011-climate, fc='r')
+ax[0].plot(np.arange(0,24), today, label='2014')
+ax[0].plot(np.arange(0,24), climate, label='Climatology')
+ax[0].legend(loc=2)
+ax[0].grid(True)
+ax[0].set_ylabel("Temperature $^\circ$F")
+ax[0].set_xlim(-0.5,23.5)
 
-ax.set_xlim(-0.5,23.5)
-ax.set_xticks((0,4,8,12,16,20))
-ax.set_xticklabels( ('Mid', '4 AM', '8 AM', 'Noon', '4 PM', '8 PM'))
-ax.set_xlabel("All times CDT")
-ax.set_ylabel("2011 Departure $^{\circ}\mathrm{F}$")
-ax.grid(True)
+ax[1].bar(np.arange(0,24)-0.4, percentiles, fc='b', label='vs 11-18 May 1932-2013')
 
-ax = fig.add_subplot(212)
-ax.bar(numpy.arange(0,24)-0.4, percentiles, fc='r')
-
-ax.set_xlim(-0.5,23.5)
-ax.set_xticks((0,4,8,12,16,20))
-ax.set_xticklabels( ('Mid', '4 AM', '8 AM', 'Noon', '4 PM', '8 PM'))
-ax.set_xlabel("All times CDT")
-ax.set_ylabel("Percentile [100 warmest]")
-ax.set_ylim(80,100)
-ax.grid(True)
+ax[1].grid(True)
+ax[1].set_xticklabels( ('Mid', '4 AM', '8 AM', 'Noon', '4 PM', '8 PM', 'Mid'))
+ax[1].set_xticks(np.arange(0,25,4))
+ax[1].set_ylabel("Percentile Rank [0=coldest]")
 
 
 import iemplot
