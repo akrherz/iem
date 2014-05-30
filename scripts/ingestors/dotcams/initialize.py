a35abcc4b0e6704504c006bebb55e2de3f28b213
akrherz <akrherz@iastate.edu>
2014-05-30 07:40:57 -0500

    more cleanups

--- scripts/ingestors/dotcams/initialize.py
+++ scripts/ingestors/dotcams/initialize.py
@@ -7,22 +7,22 @@ import glob
 
 os.chdir("/mesonet/data/dotcams")
 files = glob.glob("*640x480.jpg")
-for file in files:
-  cid = file[:11]
+for fn in files:
+    cid = fn[:11]
 
-  rs = mesosite.query("SELECT * from webcams WHERE id = '%s'" % (cid,)).dictresult()
-  if len(rs) > 0:
-    continue
+    rs = mesosite.query("SELECT * from webcams WHERE id = '%s'" % (cid,)).dictresult()
+    if len(rs) > 0:
+        continue
 
-  rid = cid[6:8]
-  nwsli = mesonet.RWISconvert[ rid ]
+    rid = cid[6:8]
+    nwsli = mesonet.RWISconvert[ rid ]
 
-  rs = mesosite.query("SELECT name, ST_x(geom) as lon, ST_y(geom) as lat from stations where id = '%s'" % (nwsli,)).dictresult()
-  lon = rs[0]['lon']
-  lat = rs[0]['lat']
-  name = rs[0]['name']
-  print 'Adding %s name [%s] lon [%s] lat [%s]' % (cid, name, lon, lat)
-  sql = """insert into webcams (sts, id, name, pan0, online, network, 
+    rs = mesosite.query("SELECT name, ST_x(geom) as lon, ST_y(geom) as lat from stations where id = '%s'" % (nwsli,)).dictresult()
+    lon = rs[0]['lon']
+    lat = rs[0]['lat']
+    name = rs[0]['name']
+    print 'Adding %s name [%s] lon [%s] lat [%s]' % (cid, name, lon, lat)
+    sql = """insert into webcams (sts, id, name, pan0, online, network, 
          geom, removed, state) values (now(), '%s', '%s', 0, 't', 'IDOT', 'SRID=4326;POINT(%s %s)', 'f', 'IA')""" % (cid, name, lon, lat)
-  mesosite.query( sql )
+    mesosite.query( sql )
 
