0d0c16a4b7e9801c039bf0162bc87bd18baca532
akrherz <akrherz@iastate.edu>
2014-06-02 11:24:02 -0500

    iem21 should be included

--- deployment/all_iem_exec.sh
+++ deployment/all_iem_exec.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Exec a script over all the IEM nodes
 
-MACHINES="iem-director0 iem-director1 iemfe iem12 iem19 iem22 iem23 iem30 iem50 iemvs100 iemvs101 iemvs102 iemvs103 iemvs104 iemvs105 iemvs106 iemvs107 iemvs108"
+MACHINES="iem-director0 iem-director1 iemfe iem12 iem19 iem21 iem22 iem23 iem30 iem50 iemvs100 iemvs101 iemvs102 iemvs103 iemvs104 iemvs105 iemvs106 iemvs107 iemvs108"
 for MACH in $MACHINES
 do
 ssh root@$MACH $1
