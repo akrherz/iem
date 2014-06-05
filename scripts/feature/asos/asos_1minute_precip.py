0f5963e34d0ec4d625c59110ca9e4d1a290f73f4
akrherz <akrherz@iastate.edu>
2014-06-04 21:24:34 -0500

    5 June feature

--- scripts/feature/asos/asos_1minute_precip.py
+++ scripts/feature/asos/asos_1minute_precip.py
@@ -15,8 +15,8 @@ sprec = numpy.zeros( (3000,), 'f')
 
 acursor.execute("""
  SELECT valid, tmpf, dwpf, drct, 
-  sknt, pres1, gust_sknt, precip from t2014_1minute WHERE station = 'AMW'
- and valid BETWEEN '2014-05-20 00:00' and '2014-05-20 23:59' 
+  sknt, pres1, gust_sknt, precip from t2014_1minute WHERE station = 'LWD'
+ and valid BETWEEN '2014-06-03 00:00' and '2014-06-03 23:59' 
  ORDER by valid ASC
 """)
 tot = 0
@@ -35,7 +35,7 @@ acc = numpy.zeros( (3000,), 'f')
 rate15 = numpy.zeros( (3000,), 'f')
 rate60 = numpy.zeros( (3000,), 'f')
 svalid = [0]*3000
-basets = datetime.datetime(2014,5,20)
+basets = datetime.datetime(2014,6,3)
 basets = basets.replace(tzinfo=pytz.timezone("America/Chicago"))
 for i in range(3000):
     acc[i] = acc[i-1] + sprec[i]
@@ -50,16 +50,16 @@ for i in range(3000):
 #    print mx.DateTime.DateTime(2012,8,4, 0) + mx.DateTime.RelativeDateTime(minutes=i)
 
 # Figure out ticks
-sts = datetime.datetime(2014,5,20, 19, 0)
+sts = datetime.datetime(2014,6,3, 17, 0)
 sts = sts.replace(tzinfo=pytz.timezone("America/Chicago"))
-ets = sts + datetime.timedelta(minutes=81)
-interval = datetime.timedelta(minutes=15)
+ets = sts + datetime.timedelta(minutes=421)
+interval = datetime.timedelta(minutes=60)
 now = sts
 xticks = []
 xlabels = []
 xlabels2 = []
 while now <= ets:
-    fmt = "%-I:%M %p"
+    fmt = "%-I %p"
     #if now == sts or now.hour == 0:
     #    fmt = "%-I %p\n%-d %B"
     
@@ -85,7 +85,7 @@ ax.bar(svalid, sprec * 60, width=1./1440., fc='b', ec='b', label="Hourly Rate ov
 ax.plot(svalid, acc, color='k', label="Accumulation",lw=2, zorder=2)
 ax.plot(svalid, rate15, color='g', label="Hourly Rate over 15min", linewidth=2)
 ax.plot(svalid, rate60, color='r', label="Actual Hourly Rate", lw=2)
-x0 = 1170
+x0 = 1168
 ax.text(sts + datetime.timedelta(minutes=5), 10.85, "Minute Accums [inch]", va='bottom')
 for i in range(x0,x0+11):
     ax.text( sts + datetime.timedelta(minutes=5), 10.65 + (x0-i)*0.69, "%s %.2f" % (
@@ -98,8 +98,8 @@ ax.grid(True)
 ax.set_xlim(min(xticks), max(xticks))
 ax.legend(loc=1, prop=prop, ncol=1)
 ax.set_ylim(0,12)
-ax.set_xlabel("20 May 2014 (CDT)")
-ax.set_title("20 May 2014 Ames (KAMW) One Minute Rainfall\n3.03 inches total")
+ax.set_xlabel("3 June 2014 (CDT)")
+ax.set_title("3 June 2014 Lamoni (KLWD) One Minute Rainfall\n5.27 inches total")
 #ax.set_ylim(0,361)
 #ax.set_yticks((0,90,180,270,360))
 #ax.set_yticklabels(('North','East','South','West','North'))
