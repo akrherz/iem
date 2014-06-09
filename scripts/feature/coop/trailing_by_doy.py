988953eb5de46290e13ad976d49ee246e470fdf7
akrherz <akrherz@iastate.edu>
2014-06-09 14:21:56 -0500

    add avg line

--- scripts/feature/coop/trailing_by_doy.py
+++ scripts/feature/coop/trailing_by_doy.py
@@ -1,29 +1,30 @@
 import psycopg2
+import numpy as np
 
 PGCONN = psycopg2.connect(database='coop', host='iemdb', user='nobody')
 cursor = PGCONN.cursor()
 
 cursor.execute("""SELECT year, extract(doy from day) as doy, 
  sum(precip) OVER (ORDER by day ASC ROWS BETWEEN 14 PRECEDING and CURRENT ROW)
- from alldata_ia where station = 'IA0000' and year > 2000 ORDER by day ASC""")
+ from alldata_ia where station = 'IA0000' and year > 1892 ORDER by day ASC""")
 
-data = {2011: [], 2012: [], 2013: [], 2014: []} 
+data = np.zeros( (2015-1893, 366), 'f')
 
 for row in cursor:
-    if row[0] < 2011:
-        continue
-    data[row[0]].append( row[2] )
+    data[row[0]-1893, row[1]-1] = row[2]
+
+davg = np.average(data, 0)
     
 import matplotlib.pyplot as plt
-import numpy as np
 from matplotlib.patches import Rectangle
 
 (fig, ax) = plt.subplots(1,1)
 
-ax.plot(np.arange(len(data[2011])), data[2011], zorder=2, c='b', label='2011')
-ax.plot(np.arange(len(data[2012])), data[2012], zorder=2, c='g', label='2012')
-ax.plot(np.arange(len(data[2013])), data[2013], zorder=2, c='r', label='2013')
-ax.plot(np.arange(len(data[2014])), data[2014], zorder=2, lw=2, c='k', label='2014')
+ax.plot(np.arange(366), davg, zorder=3, c='k', lw=2, label='Average')
+ax.plot(np.arange(366), data[2011-1893,:], zorder=2, c='b', label='2011')
+ax.plot(np.arange(366), data[2012-1893,:], zorder=2, c='g', label='2012')
+ax.plot(np.arange(366), data[2013-1893,:], zorder=2, c='r', label='2013')
+ax.plot(np.arange(366), data[2014-1893,:], zorder=2, lw=2, c='purple', label='2014')
 
 r = Rectangle((182,0), 14, 6, fc='yellow')
 ax.add_patch(r)
