f8bd15cdab1411df5bcb5c04430ac3d765616201
akrherz <akrherz@iastate.edu>
2014-07-02 16:29:11 -0500

    12 jun feature

--- scripts/feature/days_this_year.py
+++ scripts/feature/days_this_year.py
@@ -4,27 +4,28 @@ COOP = iemdb.connect('coop', bypass=True)
 ccursor = COOP.cursor()
 ccursor2 = COOP.cursor()
 
-ccursor.execute("""SELECT year, max(high) from alldata_ia WHERE
- station = 'IA2203' and year < 2012 GROUP by year ORDER by year ASC""")
+ccursor.execute("""SELECT year, sum(case when high > 79 then 1 else 0 end) from alldata_ia WHERE
+ station = 'IA0200' and sday < '0612' GROUP by year ORDER by year ASC""")
 
 years = []
 days = []
 for row in ccursor:
     years.append( row[0] )
-    ccursor2.execute("""SELECt * from alldata_ia where
-    station = 'IA2203' and year = 2012 and high > %s""" % (row[1],))
-    days.append( ccursor2.rowcount )
+    days.append( row[1] )
 
+days[-1] += 1
 import matplotlib.pyplot as plt
 
 (fig, ax) = plt.subplots(1,1)
 
-ax.bar(numpy.array(years)-0.5, days, fc='r', ec='r')
-ax.set_xlim(1886,2013)
+bars = ax.bar(numpy.array(years)-0.5, days, fc='r', ec='r')
+bars[-1].set_facecolor('b')
+bars[-1].set_edgecolor('b')
+ax.set_xlim(1892,2015)
 ax.grid(True)
-ax.set_ylabel("2012 Days Warmer than Given Year")
+ax.set_ylabel("Days per Year")
 ax.set_xlabel("Chart average: %.1f days" % (numpy.average(numpy.array(days)),))
-ax.set_title("Des Moines: 1 Jan - 1 Aug 2012 Days with warmer high temperature\nthan warmest daily high temperature for that year (1886-2011)")
+ax.set_title("1893-2014 Ames Days with 80+$^\circ$F High Temperature\nFor period 1 January - 11 June each year")
 
 fig.savefig('test.ps')
 import iemplot
