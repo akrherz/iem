e148561b4d843a16511285b7ed71102696c95cf1
akrherz <akrherz@iastate.edu>
2014-07-16 22:36:11 -0500

    17 jul feature

--- scripts/feature/days_since_T2.py
+++ scripts/feature/days_since_T2.py
@@ -15,16 +15,16 @@ ccursor = COOP.cursor()
 
 # Compute normal from the climate database
 sql = """
- SELECT c.name, c.network, c.id, min_tmpf, ST_x(c.geom) as lon, 
- ST_y(c.geom) as lat, c.climate_site from summary_2013 s, stations c 
+ SELECT c.name, c.network, c.id, min(min_tmpf), ST_x(c.geom) as lon, 
+ ST_y(c.geom) as lat, c.climate_site from summary_2014 s, stations c 
  where s.iemid = c.iemid
- and s.day = '2013-12-24' and c.network in ('IA_ASOS', 'AWOS') 
- and min_tmpf < 10 
+ and s.day in ('2014-07-15', '2014-07-14') and c.network in ('IA_ASOS', 'AWOS') 
+ and min_tmpf < 90 GROUP by name, network, id, lon, lat, climate_site
  ORDER by id ASC
 """
 
 def compute(ts):
-    now = datetime.date(2013,9,9)
+    now = datetime.date(2014,7,15)
     days = (now - ts).days
     years = int(days / 365)
     months = int((days % 365) / 30)
@@ -46,23 +46,24 @@ for row in icursor:
     cid = row[6]
     ccursor.execute("""
     SELECT max(day) from alldata_ia where station = '%s' 
-    and low <= %.0f and day < '2013-12-24'
+    and low <= %.0f and year < 2014 and month = 7
     """ % (cid, max_tmpf))
     row2 = ccursor.fetchone()
     if row2[0] is None:
         continue
     lbl, days = compute(row2[0])
-    vals.append( "%.0f~C~%s" % (max_tmpf, lbl) )
+    #vals.append( "%.0f~C~%s" % (max_tmpf, lbl) )
+    vals.append( "%.0f" % (max_tmpf,) )
     lats.append( row[5] )
     lons.append( row[4] )
-    print '%5s %s %s %s' % (station, max_tmpf, lbl, days)
+    print '%5s %s %s %s %s %s' % (station, max_tmpf, lbl, days, cid, row2[0])
 
 cfg = {
  'wkColorMap': 'BlAqGrYeOrRe',
  'nglSpreadColorStart': 2,
  'nglSpreadColorEnd'  : -1,
- '_title'             : "24 Dec 2013 Low Temp and Years Since as Cold Low Temp",
- '_valid'             : "24 Dec 2013, values in F",
+ '_title'             : "15-16 July 2014 Coldest Temperature",
+ '_valid'             : "based on Iowa ASOS/AWOS Data",
 #'lbTitleString'      : "[days]",
  '_showvalues'        : True,
  '_format'            : '%s',
