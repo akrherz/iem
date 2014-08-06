716b7dc93981022f62bb7fcf00626b61c8ab66aa
akrherz <akrherz@iastate.edu>
2014-08-06 15:51:49 -0500

    generalize monthly plotter, add new form interface type

--- htdocs/COOP/auto/scripts/__init__.py
+++ htdocs/COOP/auto/scripts/__init__.py
@@ -2,7 +2,7 @@
 data = {'plots': {
     "1": "July-August Days Above Temp v. May-June Precip",
     "2": "Month Precipitation v Month Growing Degree Day Departures",
-    "3": "Maximum Temperature for One Month per Year",
+    "3": "Monthly Temperature / Precipitation Statistics by Year",
     "4": "State Areal Coverage of Precip Intensity over X Days",
     }
 }
