ace696997e45359556dfc84434b340a95760d430
akrherz <akrherz@iastate.edu>
2014-08-24 13:44:06 -0500

    more cleanups for location move and add ASOS tool

---
+++ htdocs/plotting/auto/scripts/p11.py
@@ -0,0 +1,59 @@
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import psycopg2.extras
+import numpy as np
+from pyiem.network import Table as NetworkTable
+
+PDICT ={'max-dwpf': 'Maximum Dew Point'}
+
+def get_description():
+    """ Return a dict describing how to call this plotter """
+    d = dict()
+    d['arguments'] = [
+        dict(type='zstation', name='zstation', default='AMW', label='Select Station:'),
+        dict(type='text', name='year', default='2014', label='Enter Year:'),
+        dict(type='select', name='type', default='max-dwpf', label='Which metric to plot?',
+             options=PDICT), 
+    ]
+    return d
+
+def plotter( fdict ):
+    """ Go """
+    IEM = psycopg2.connect(database='iem', host='iemdb', user='nobody')
+    cursor = IEM.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+    station = fdict.get('zstation', 'AMW')
+    network = fdict.get('network', 'IA_ASOS')
+    nt = NetworkTable(network)
+    year = int(fdict.get('year', 2014))
+    plotvar = fdict.get('pvar', 'max-dwpf')
+    
+    table = "summary_%s" % (year,)
+
+    cursor.execute("""
+     select day, 
+     (case when max_dwpf > -90 and max_dwpf < 100 
+             then max_dwpf else null end) as "max-dwpf"
+     from """+table+""" where iemid = (select iemid from stations where 
+     id = %s and network = %s) ORDER by day ASC 
+    """, (station, network))
+    days = []
+    vals = []
+    for row in cursor:
+        if row[plotvar] is None:
+            continue
+        days.append( row['day'] )
+        vals.append( row[plotvar] )
+        
+    vals = np.array(vals)
+        
+    (fig, ax) = plt.subplots(1,1)
+    ax.bar(days, vals, ec='tan', fc='tan', zorder=1)
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%-d\n%b'))
+    ax.grid(True)
+    ax.set_title("%s [%s] Daily Maximum Dew Point" % (nt.sts[station]['name'],
+                                                      station))
+
+    return fig
