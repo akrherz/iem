143046ccdc1d8d53e743f649bf8f264f1e8fd057
akrherz <akrherz@iastate.edu>
2014-08-25 16:23:33 -0500

    26 Aug feature

--- scripts/feature/asos/rain_during_warning.py
+++ scripts/feature/asos/rain_during_warning.py
@@ -1,25 +1,25 @@
 """
 Compute the amount of precipitation that falls during a SVR,TOR warning
 """
-import iemdb
+import psycopg2
 import numpy
-POSTGIS = iemdb.connect('postgis', bypass=True)
+POSTGIS = psycopg2.connect(database='postgis', host='iemdb', user='nobody')
 pcursor = POSTGIS.cursor()
-ASOS = iemdb.connect('asos', bypass=True)
+ASOS = psycopg2.connect(database='asos', host='iemdb', user='nobody')
 acursor = ASOS.cursor()
 
 
-total_in = numpy.zeros((2013-2002), 'f')
-total_out = numpy.zeros((2013-2002), 'f')
-total_arr = numpy.zeros((2013-2002), 'f')
+total_in = numpy.zeros((2015-2002), 'f')
+total_out = numpy.zeros((2015-2002), 'f')
+total_arr = numpy.zeros((2015-2002), 'f')
 
-for year in range(2002,2013):
+for year in range(2002,2015):
     warntimes = []
     around = []
     pcursor.execute("""
-     select generate_series(issue, expire, '1 minute'::interval) from warnings_%s 
+     select generate_series(issue, expire, '1 minute'::interval) from sbw_%s 
      where wfo = 'DMX' and phenomena in ('SV','TO') and significance = 'W' 
-     and gtype = 'P' 
+     and status = 'NEW'
      and ST_Contains(geom, GeomFromEWKT('SRID=4326;POINT(-93.66308 41.53397)'))
     """ % (year,))
     for row in pcursor:
@@ -28,9 +28,9 @@ for year in range(2002,2013):
     
     pcursor.execute("""
      select generate_series(issue - '1 hour'::interval, 
-         expire + '1 hour'::interval, '1 minute'::interval) from warnings_%s 
+         expire + '1 hour'::interval, '1 minute'::interval) from sbw_%s 
      where wfo = 'DMX' and phenomena in ('SV','TO') and significance = 'W' 
-     and gtype = 'P' 
+     and status = 'NEW'
      and ST_Contains(geom, GeomFromEWKT('SRID=4326;POINT(-93.66308 41.53397)'))
     """ % (year,))
     for row in pcursor:
@@ -47,10 +47,8 @@ for year in range(2002,2013):
     for row in acursor:
         if row[0] in warntimes:
             total_in[row[0].year-2002] += row[1]
-            warntimes.remove(row[0])
         elif row[0] in around:
             total_arr[row[0].year-2002] += row[1] 
-            around.remove(row[0])
         else:
             total_out[row[0].year-2002] += row[1]
             
@@ -62,24 +60,24 @@ import matplotlib.pyplot as plt
 
 fig, ax = plt.subplots(2,1, sharex=True)
 
-ax[0].set_xlim(2001.5,2012.5)
-ax[1].set_xlabel("*2012 thru 5 June")
+ax[0].set_xlim(2001.5,2014.5)
+ax[1].set_xlabel("*2014 thru 1 August, yearly totals don't match official data due to missing obs", fontsize=10)
 ax[0].set_ylabel("Yearly Precip [inch]")
 ax[1].set_ylabel("Percentage [%]")
 ax[1].set_ylim(0,100)
-ax[0].set_title("Des Moines Yearly Airport Precipitation [2002-2012]\nContribution during, around, and outside of TOR,SVR warning")
-ax[0].bar(numpy.arange(2002,2013)-0.4, total_in, fc='r')
-ax[0].bar(numpy.arange(2002,2013)-0.4, total_arr, bottom=total_in, fc='b')
-ax[0].bar(numpy.arange(2002,2013)-0.4, total_out, bottom=(total_in+total_arr), fc='g')
+ax[0].set_title("Des Moines Yearly Airport Precipitation [2002-2014]\nContribution during, around, and outside of TOR,SVR warning")
+ax[0].bar(numpy.arange(2002,2015)-0.4, total_in, fc='r')
+ax[0].bar(numpy.arange(2002,2015)-0.4, total_arr, bottom=total_in, fc='b')
+ax[0].bar(numpy.arange(2002,2015)-0.4, total_out, bottom=(total_in+total_arr), fc='g')
 
 p = total_in / (total_in + total_out + total_arr) * 100.0
-ax[1].bar(numpy.arange(2002,2013)-0.4, p, fc='r', label='During')
+ax[1].bar(numpy.arange(2002,2015)-0.4, p, fc='r', label='During')
 
 p2 = total_arr / (total_in + total_out + total_arr) * 100.0
-ax[1].bar(numpy.arange(2002,2013)-0.4, p2, bottom=p, fc='b', label='+/- 1 hour')
+ax[1].bar(numpy.arange(2002,2015)-0.4, p2, bottom=p, fc='b', label='+/- 1 hour')
 
 p3 = total_out / (total_in + total_out + total_arr) * 100.0
-ax[1].bar(numpy.arange(2002,2013)-0.4, p3, bottom=(p+p2), fc='g', label='Outside')
+ax[1].bar(numpy.arange(2002,2015)-0.4, p3, bottom=(p+p2), fc='g', label='Outside')
 
 ax[1].legend(ncol=3, loc=(0.13,1.01))
 ax[1].grid(True)
