de20260377984de49b0e02db45560baead84aaac
akrherz <akrherz@iastate.edu>
2014-08-25 10:35:14 -0500

    Make available plots appear in a nicer order, non-random

--- htdocs/plotting/auto/scripts/__init__.py
+++ htdocs/plotting/auto/scripts/__init__.py
@@ -1,15 +1,20 @@
 # Association of plots
-data = {'plots': {
-    "1": "July-August Days Above Temp v. May-June Precip",
-    "2": "Month Precipitation v Month Growing Degree Day Departures",
-    "3": "Monthly Temperature / Precipitation Statistics by Year",
-    "4": "State Areal Coverage of Precip Intensity over X Days",
-    "5": "Minimum Daily Temperature Range",
-    "6": "Monthly Temperature/Precipitation Distributions",
-    "7": "Growing Degree Day Periods for One Year by Planting Date",
-    "8": "Monthly Precipitation Reliability",
-    "9": "Growing Degree Day Climatology and Daily Values for one Year",
-    "10": "Last Spring and First Fall Date below given threshold",
-    "11": "ASOS/AWOS Daily Maximum Dew Point for a Year",
-    }
-}
+data = {'plots': [
+    {'label': 'Daily', 'options': [
+        {'id': "4", 'label': "State Areal Coverage of Precip Intensity over X Days"},
+        {'id': "5", 'label': "Minimum Daily Temperature Range"},
+        {'id': "7", 'label': "Growing Degree Day Periods for One Year by Planting Date"},
+        {'id': "11", 'label': "ASOS/AWOS Daily Maximum Dew Point for a Year"},
+    ]},
+    {'label': 'Monthly', 'options': [
+        {'id': "1", 'label': "July-August Days Above Temp v. May-June Precip"},
+        {'id': "2", 'label': "Month Precipitation v Month Growing Degree Day Departures"},
+        {'id': "3", 'label': "Monthly Temperature / Precipitation Statistics by Year"},
+        {'id': "6", 'label': "Monthly Temperature/Precipitation Distributions"},
+        {'id': "8", 'label': "Monthly Precipitation Reliability"},
+        {'id': "9", 'label': "Growing Degree Day Climatology and Daily Values for one Year"},
+    ]},
+    {'label': 'Yearly', 'options': [
+        {'id': "10", 'label': "Last Spring and First Fall Date below given threshold"},
+    ]},
+]}
