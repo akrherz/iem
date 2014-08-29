47a91fc20d347a8f5103e63eed29836e5941d7a8
akrherz <akrherz@iastate.edu>
2014-08-29 13:36:44 -0500

    remove any hints of CGI exec'd tile.py here

--- htdocs/cache/tile.py
+++
@@ -1,8 +0,0 @@
-"""
- This is mod_python magic with the help of what is in .htaccess
-"""
-from TileCache import Service, cgiHandler, cfgfiles
-
-if __name__ == '__main__':
-    svc = Service.load(*cfgfiles)
-    cgiHandler(svc)
