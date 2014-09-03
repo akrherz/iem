f1b842c9530e3095dffa2d6b840a2bd1881279e9
akrherz <akrherz@iastate.edu>
2014-09-03 18:02:05 -0500

    4 Sep feature

--- scripts/feature/nlae/plot_flux.py
+++ scripts/feature/nlae/plot_flux.py
@@ -6,7 +6,7 @@ ocursor = OTHER.cursor()
 
 def getsite(station):
     ocursor.execute("""
-    SELECT valid, outgoing_sw, incoming_sw from flux2012 where station = %s
+    SELECT valid, outgoing_sw, incoming_sw from flux2014 where station = %s
     and extract(hour from valid) = 12 and extract(minute from valid) = 0
     and incoming_sw > 0 and outgoing_sw >= 0 ORDER by valid ASC
     """, (station,))
@@ -29,11 +29,11 @@ import matplotlib.dates as mdates
 
 (fig, ax) = plt.subplots(2,1, sharex=True)
 
-ax[0].plot(v1, s1, label='Upwelling')
-ax[0].plot(v1, s2, label='Downwelling')
+ax[0].bar(v1, s2, label='Downwelling', zorder=1, fc='tan', ec='tan')
+ax[0].bar(v1, s1, label='Upwelling', zorder=2, fc='green', ec='green')
 ax[0].legend(loc=2, prop={'size': 9})
 
-ax[0].set_title("NLAE Flux Site 2012 Data over Corn Crop (local noon each day)")
+ax[0].set_title("NLAE Flux Site 2014 Data over Corn Crop (local noon each day)")
 
 albedo = s1 / s2
 ax[1].bar(v1, albedo, fc='g', ec='g')
@@ -42,8 +42,8 @@ ax[1].grid(True)
 ax[0].grid(True)
 
 bbox_props = dict(boxstyle="rarrow,pad=0.3", fc="cyan", ec="b", lw=2)
-t = ax[1].text(v1[-30], albedo[-1], "Snow!", ha="right", va="center", rotation=15,
-            size=10,
+t = ax[1].text(0.1, 0.5, "Snow!", ha="right", va="center", rotation=15,
+            size=10, transform=ax[1].transAxes,
             bbox=bbox_props)
 bb = t.get_bbox_patch()
 bb.set_boxstyle("rarrow", pad=0.6)
