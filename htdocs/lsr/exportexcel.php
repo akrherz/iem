5ed7df3f96f63928ad65572fa7d045e5c154cd5f
akrherz <akrherz@iastate.edu>
2014-09-04 12:42:43 -0500

    Fix Excel export and add local timestamp columns to output

--- htdocs/lsr/exportexcel.php
+++ htdocs/lsr/exportexcel.php
@@ -1,6 +1,6 @@
 <?php
-header("Content-type: application/vnd.ms-excel");
-header("Content-Disposition: attachment; filename=\"lsrdata.xls\"");
+header("Content-type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+header("Content-Disposition: attachment; filename=\"export.xml\"");
 header("Expires: 0");
 header("Cache-Control: must-revalidate, post-check=0,pre-check=0");
 header("Pragma: public");
