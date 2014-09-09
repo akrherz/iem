6a753b4948e9da6b7a9d6ff60d7e9509875146be
akrherz <akrherz@iastate.edu>
2014-09-09 08:39:14 -0500

    remove unused hosts

--- deployment/all_iem_exec.sh
+++ deployment/all_iem_exec.sh
@@ -2,7 +2,7 @@
 # Exec a script over all the IEM nodes
 
 # Run iem21 first as it may need to be aware of changes in hostnames
-MACHINES="iem21 iem-director0 iem-director1 iemfe iem5 iem12 iem19 iem22 iem23 iem30 iem50 iemvs100 iemvs101 iemvs102 iemvs103 iemvs104 iemvs105 iemvs106 iemvs107 iemvs108"
+MACHINES="iem21 iem-director0 iem-director1 iemfe iem5 iem12 iem22 iem30 iem50 iemvs100 iemvs101 iemvs102 iemvs103 iemvs104 iemvs105 iemvs106 iemvs107 iemvs108"
 for MACH in $MACHINES
 do
 ssh root@$MACH $1
