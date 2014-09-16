76636a6fbb0487084b1df25bf9fffcecc3c39c2f
akrherz <akrherz@iastate.edu>
2014-09-15 21:42:41 -0500

    add multi-day timeseries plot, 16 Sep feature

---
+++ htdocs/plotting/auto/scripts/p18.py
@@ -0,0 +1,103 @@
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+import psycopg2.extras
+from pyiem.network import Table as NetworkTable
+import datetime
+import numpy as np
+
+def get_description():
+    """ Return a dict describing how to call this plotter """
+    d = dict()
+    ts = datetime.date.today() - datetime.timedelta(days=365)
+    d['arguments'] = [
+        dict(type='zstation', name='zstation', default='AMW', 
+             label='Select Station:'),
+    dict(type='date', name='sdate', default=ts.strftime("%Y/%m/%d"), 
+         label='Start Date of Plot:',
+         min="1951/01/01"), # Comes back to python as yyyy-mm-dd
+        dict(type='text', name='days', default='365',
+             label='Days to Plot'),
+    ]
+    return d
+
+def plotter( fdict ):
+    """ Go """
+    ASOS = psycopg2.connect(database='asos', host='iemdb', user='nobody')
+    cursor = ASOS.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    COOP = psycopg2.connect(database='coop', host='iemdb', user='nobody')
+    ccursor = COOP.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+    station = fdict.get('zstation', 'AMW')
+    network = fdict.get('network', 'IA_ASOS')
+    nt = NetworkTable(network)
+    sdate = datetime.datetime.strptime( fdict.get('sdate', '2000-01-01'),
+                                        '%Y-%m-%d')
+    days = int(fdict.get('days', 365))
+    edate = sdate + datetime.timedelta(days=days)
+    today = datetime.datetime.today()
+    if edate > today:
+        edate = today
+        days = (edate - sdate).days
+
+    climo = {}
+    ccursor.execute("""
+    SELECT valid, high, low from ncdc_climate81 where station = %s
+    """, (nt.sts[station]['ncdc81'],))
+    for row in ccursor:
+        climo[ row[0].strftime("%m%d") ] = dict(high=row[1], low=row[2])
+        
+    valid = []
+    tmpf = []
+    cursor.execute("""
+     SELECT valid, tmpf from alldata WHERE station = %s
+     and valid > %s and valid < %s ORDER by valid ASC
+    """, (station, sdate, sdate + datetime.timedelta(days=days)))
+    for row in cursor:
+        valid.append( row[0] )
+        tmpf.append( row[1] )
+
+    (fig, ax) = plt.subplots(1, 1)
+
+    xticks = []
+    xticklabels = []
+    now = sdate
+    cdates = []
+    chighs = []
+    clows = []
+    while now <= edate:
+        cdates.append( now )
+        chighs.append( climo[now.strftime("%m%d")]['high'] )
+        clows.append( climo[now.strftime("%m%d")]['low'] )
+        if now.day == 1 or (now.day % 12 == 0 and days < 180):
+            xticks.append( now )
+            fmt = "%-d"
+            if now.day == 1:
+                fmt = "%-d\n%b"
+            xticklabels.append( now.strftime(fmt))
+    
+        now += datetime.timedelta(days=1)
+    
+    chighs = np.array(chighs)
+    clows = np.array(clows)
+    
+    ax.bar(cdates, chighs - clows, bottom=clows, fc='lightblue', ec='lightblue',
+           label="Daily Climatology")
+    ax.plot(valid, tmpf, color='r', label='Hourly Obs')
+    #ax.plot(valid2, tmpf2, color='k', label='2010-11 Hourly Obs')
+    ax.set_ylabel("Temperature $^{\circ}\mathrm{F}$")
+    #ax.set_xlabel("7 September 2011 (EDT)")
+    ax.set_xticks(xticks)
+    ax.set_xticklabels(xticklabels)
+    ax.set_xlim(sdate, edate)
+    ax.set_ylim(top=max(tmpf)+15.)
+    ax.legend(loc=2, ncol=2)
+    ax.axhline(32, linestyle='-.')
+    ax.grid(True)
+    ax.set_title("%s [%s]\nAir Temperature Timeseries %s - %s" % (
+                        nt.sts[station]['name'], station, 
+                        sdate.strftime("%d %b %Y"), 
+                        edate.strftime("%d %b %Y")))
+
+
+    return fig
