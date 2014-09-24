1d405e481dd39b4c555595b98ff3fb22999169f2
akrherz <akrherz@iastate.edu>
2014-09-24 13:18:20 -0500

    add vtec counts by year by office
    As per request for the number of events per WFO per year

--- htdocs/vtec/yearly_counts.php
+++ htdocs/vtec/yearly_counts.php
@@ -5,16 +5,22 @@ include("../../include/database.inc.php");
 include("../../include/myview.php");
 include("../../include/vtec.php");
 include("../../include/network.php");
+include("../../include/forms.php");
 $nt = new NetworkTable("WFO");
 
 $clobber = isset($_REQUEST["clobber"]);
 $wfo = isset($_REQUEST["wfo"]) ? strtoupper(substr($_REQUEST["wfo"],0,3)): null;
+$phenomena = isset($_REQUEST["phenomena"]) ? strtoupper(substr($_REQUEST["phenomena"],0,2)): null;
+$significance = isset($_REQUEST["significance"]) ? strtoupper(substr($_REQUEST["significance"],0,1)): null;
 $wfoLimiter = "";
 $wfoMsg = "Data for all WFOs shown";
 if ($wfo){
 	$wfoLimiter = " and wfo = '{$wfo}' ";
 	$wfoMsg = "Data for only $wfo shown"; 
 }
+
+$p1 = make_select("phenomena", $phenomena, $vtec_phenomena);
+$s1 = make_select("significance", $significance, $vtec_significance);
 
 $t = new MyView();
 $t->thispage = "severe-main";
@@ -46,22 +52,125 @@ function get_data(){
 	}
 	return Array($data, $pcodes);
 }
+
+function get_data2(){
+	global $phenomena, $significance;
+	$dbconn = iemdb("postgis");
+
+	$rs = pg_query("
+		WITH data as (
+			SELECT distinct wfo, eventid, extract(year from issue) as yr
+			from warnings where phenomena = '$phenomena' and
+			significance = '$significance'
+		)
+		SELECT wfo, yr, count(*) from data GROUP by wfo, yr
+	");
+
+	$data = Array();
+	for ($i=0;$row=@pg_fetch_assoc($rs,$i);$i++){
+		$yr = $row["yr"];
+		if (! array_key_exists($yr, $data)){
+			$data[$yr] = Array();
+		}
+		$data[$yr][$row["wfo"]] = $row["count"];
+	}
+	return $data;
+}
+
 /* see if memcache has our data */
 $memcache = new Memcache;
 $memcache->connect('iem-memcached', 11211);
-$data = $memcache->get("vtec_counts_data_$wfo");
-$pcodes = $memcache->get("vtec_counts_pcodes_$wfo");
-$cachedwarning = '<div class="alert alert-warning">This information was cached
-			within the past 24 hours and may not be up to the moment.</div>';
-if ($clobber || !$data){
-	list($data, $pcodes) = get_data();
-	$memcache->set("vtec_counts_data_$wfo", $data, false, 86400);
-	$memcache->set("vtec_counts_pcodes_$wfo", $pcodes, false, 86400);
-	$cachedwarning = '';
+if ($phenomena == null || $significance == null){
+	$data = $memcache->get("vtec_counts_data_$wfo");
+	$pcodes = $memcache->get("vtec_counts_pcodes_$wfo");
+	$cachedwarning = '<div class="alert alert-warning">This information was cached
+				within the past 24 hours and may not be up to the moment.</div>';
+	if ($clobber || !$data){
+		list($data, $pcodes) = get_data();
+		$memcache->set("vtec_counts_data_$wfo", $data, false, 86400);
+		$memcache->set("vtec_counts_pcodes_$wfo", $pcodes, false, 86400);
+		$cachedwarning = '';
+	}
+	
+	$table = <<<EOF
+	<table class="table table-striped">
+	<thead><tr><th>P</th><th>S</th><th>Phenomena</th><th>Significance</th>
+EOF;
+	$years = array_keys($data);
+	asort($years);
+	while( list($i,$yr) = each($years)){
+		$table .= "<th>{$yr}</th>";
+	}
+	$table .= "</tr></thead>\n";
+	
+	$codes = array_keys($pcodes);
+	asort($codes);
+	while( list($j,$code) = each($codes)){
+		list($phenomena, $significance) = explode(".", $code);
+		$table .= sprintf("<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td>",
+				$phenomena, $significance, @$vtec_phenomena[$phenomena],
+				@$vtec_significance[$significance]);
+	
+		reset($years);
+		while( list($i,$yr) = each($years)){
+			if (array_key_exists($code, $data[$yr])){
+				$table .= sprintf("<td>%s</td>", $data[$yr][$code]);
+			} else {
+				$table .= "<td>0</td>";
+			}
+		}
+		$table .= "</tr>";
+	}
+	$table .= "</table>";
+} else {
+	// Logic for by WFO by year
+	$memkey = "vtec_counts_data_{$phenomena}_{$significance}";
+	$data = $memcache->get($memkey);
+	$cachedwarning = '<div class="alert alert-warning">This information was cached
+				within the past 24 hours and may not be up to the moment.</div>';
+	if ($clobber || !$data){
+		$data = get_data2();
+		$memcache->set($memkey, $data, false, 86400);
+		$cachedwarning = '';
+	}
+	
+	$table = <<<EOF
+	<table class="table table-striped">
+	<thead><tr><th>WFO</th><th>WFO Name</th>
+EOF;
+	$years = array_keys($data);
+	asort($years);
+	while( list($i,$yr) = each($years)){
+		$table .= "<th>{$yr}</th>";
+	}
+	$table .= "</tr></thead>\n";
+
+	reset($nt->table);
+	while( list($key,$val) = each($nt->table)){
+		$table .= sprintf("<tr><td>%s</td><td>%s</td>",
+				$key, $val["name"]);
+	
+		reset($years);
+		while( list($i,$yr) = each($years)){
+			if (array_key_exists($key, $data[$yr])){
+				$table .= sprintf("<td>%s</td>", $data[$yr][$key]);
+			} else {
+				$table .= "<td>0</td>";
+			}
+		}
+		$table .= "</tr>";
+	}
+	$table .= "</table>";
+	$wfoMsg = sprintf("%s.%s (%s %s) Event Counts by WFO", 
+			$phenomena, $significance,
+			$vtec_phenomena[$phenomena], $vtec_significance[$significance]);
 }
 
 $content = <<<EOF
-<h3 class="page-header">VTEC Warning Counts by Year</h3>
+<ol class="breadcrumb">
+<li><a href="/nws/">NWS Resources</a></li>
+<li class="active">VTEC Warning Counts by Year/WFO</li>
+</ol>
 
 <p>This page presents the number of VTEC events issued by either all of the
 NWS or a selected WFO.  These numbers are based on the IEM maintained archive
@@ -74,8 +183,11 @@ WFOs would count as four in this summary, when summarizing all WFOs!</p>
 		
 <div class="alert alert-info">Hopefully, you can copy/paste this table into
 your favorite spreadsheet program for further analysis...</div>
-		$cachedwarning
-<p><form method="GET" name="wfo">
+		
+$cachedwarning
+		
+<p><h4>Option 1: All VTEC Events by Year</h4>
+<form method="GET" name="wfo">
 Limit Numbers by WFO: 
 EOF;
 $content .= "<select name=\"wfo\">
@@ -83,44 +195,22 @@ $content .= "<select name=\"wfo\">
 while( list($key, $value) = each($nt->table) ){
 	$content .= "<option value='$key'>[$key] ". $value["name"] ."</option>\n";
 }
-$content .= "
-</select>";
-
 $content .= <<<EOF
+</select>
 	<input type="submit" value="Generate Table">
 </form>
-		
-<h4>$wfoMsg</h4>
-		
-<table class="table table-striped">
-<thead><tr><th>P</th><th>S</th><th>Phenomena</th><th>Significance</th>
-EOF;
-$years = array_keys($data);
-asort($years);
-while( list($i,$yr) = each($years)){
-	$content .= "<th>{$yr}</th>";
-}
-$content .= "</tr></thead>\n";
 
-$codes = array_keys($pcodes);
-asort($codes);
-while( list($j,$code) = each($codes)){
-	list($phenomena, $significance) = explode(".", $code);
-	$content .= sprintf("<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td>", 
-			$phenomena, $significance, @$vtec_phenomena[$phenomena],
-			@$vtec_significance[$significance]);
-	
-	reset($years);
-	while( list($i,$yr) = each($years)){
-		if (array_key_exists($code, $data[$yr])){
-			$content .= sprintf("<td>%s</td>", $data[$yr][$code]);
-		} else {
-			$content .= "<td>0</td>";
-		}
-	}
-	$content .= "</tr>";
-}
-$content .= "</table>";
+<p><h4>Option 2: Count of One VTEC Phenomena and Significance by WFO</h4>
+<form method="GET" name="wfo2">
+{$p1} {$s1}
+<input type="submit" value="Generate Table">
+</form>
+
+<h4>$wfoMsg</h4>
+
+$table 
+
+EOF;
 $t->content = $content;
 $t->render("single.phtml");
 ?>
