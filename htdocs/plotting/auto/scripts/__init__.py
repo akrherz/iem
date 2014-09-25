f1ec46d3434a5e94a890b90c1a888f6f31f33e65
akrherz <akrherz@iastate.edu>
2014-09-25 14:35:16 -0500

    support first spring / last fall query, 26 Sep feature

--- htdocs/plotting/auto/scripts/__init__.py
+++ htdocs/plotting/auto/scripts/__init__.py
@@ -27,7 +27,7 @@ data = {'plots': [
         #{'id': "17", 'label': "Daily Temperatures + Climatology for Year + Month"},
     ]},
     {'label': 'Yearly', 'options': [
-        {'id': "10", 'label': "Last Spring and First Fall Date below given threshold"},
+        {'id': "10", 'label': "Last Spring and First Fall Date above/below given threshold"},
         {'id': "12", 'label': "Days per year and latest date above given threshold"},
         {'id': "13", 'label': "End Date of Summer (warmest 91 day period) per Year"},
         {'id': "14", 'label': "Yearly Precipitation Contributions by Daily Totals"},
