6c807225e5aa1e96f42697e4aa96207715f87bb6
akrherz <akrherz@iastate.edu>
2014-09-24 21:38:07 -0500

    25 Sep feature

--- scripts/feature/model/modelpoint_grid.py
+++ scripts/feature/model/modelpoint_grid.py
@@ -1,15 +1,17 @@
-import iemdb
+import psycopg2
 import numpy.ma
 import datetime
-import iemtz
+import pytz
 
-MOS = iemdb.connect('mos', bypass=True)
+MOS = psycopg2.connect(database='mos', host='iemdb', user='nobody')
 mcursor = MOS.cursor()
+IEM = psycopg2.connect(database='iem', host='iemdb', user='nobody')
+icursor = IEM.cursor()
 
 # GFS has 8 days worth of data
 # NAM has 3.5 days
-PLOTDAYS = 4
-sts = datetime.datetime(2013,2,26,0, tzinfo=iemtz.Central)
+PLOTDAYS = 7
+sts = datetime.datetime(2014,9,20,0, tzinfo=pytz.timezone("America/Chicago"))
 #___________________________
 # No more custom
 MODELDAYS = PLOTDAYS+ 6
@@ -19,23 +21,19 @@ ets = sts + datetime.timedelta(days=PLOTDAYS)
 qpf = numpy.ma.zeros( (MODELDAYS*4+2, PLOTDAYS*8), numpy.float)
 qpf[:] = -1.
 
-obs = numpy.zeros( (PLOTDAYS*8), numpy.float )
-obs[:] = -1
-obs[:8] = [0,0,0.3, 0.02, 0.02, 0.07, 0.06, 0.05]
-obs[8:16] = [0.07, 0.05, 0.01, 0.05, 0.01, 0, 0, 0]
 
-qpf[-1,:] = obs
 
-print qpf[-1:]
-
+xaxis = []
 xlabels = []
-xticks = range(-1,PLOTDAYS*8,4)
-for i in range(-1,PLOTDAYS*8,4):
+xticks = range(-1,PLOTDAYS*8, 4)
+for i in range(-1,PLOTDAYS*8):
     ts = sts + datetime.timedelta(hours=((i+1)*3))
-    fmt = "%-I %p"
+    xaxis.append( ts )
+    fmt = "Noon"
     if ts.hour == 0:
-        fmt += "\n%d %b"
-    xlabels.append( ts.strftime(fmt))
+        fmt = "Mid\n%d %b"
+    if ts.hour in [0,12]:
+        xlabels.append( ts.strftime(fmt))
 
 ylabels = []
 yticks = range(0,MODELDAYS*4,4)
@@ -44,12 +42,22 @@ for i in range(0,MODELDAYS*4,4):
     fmt = "%d %b"
     ylabels.append( ts.strftime(fmt))
     
-yticks.append( 41 )
-ylabels.append("Obs")
+
+obs = numpy.zeros( (PLOTDAYS*8), numpy.float )
+for i,x in enumerate(xaxis[:-1]):
+    icursor.execute("""SELECT sum(phour) from hourly_2014 where
+     station = 'AMW' and valid >= %s and valid < %s """, (x,
+                            x + datetime.timedelta(hours=3)))
+    row = icursor.fetchone()
+    obs[i] = row[0] 
+    print i, x, row[0]
+qpf[-1,:] = obs
+
+
 
 mcursor.execute("""
-select runtime, ftime, precip from model_gridpoint_2013 
-where station = 'KDSM' and ftime > %s and ftime <= %s and model = 'GFS'
+select runtime, ftime, precip from model_gridpoint_2014
+where station = 'KAMW' and ftime > %s and ftime <= %s and model = 'GFS'
 """, (sts, ets))
 
 for row in mcursor:
@@ -95,18 +103,16 @@ ax.set_ylim(0,MODELDAYS*4+2)
 fig.colorbar( res )
 ax.grid(True)
 
-ax.set_title("GFS Grid Point Forecast for Des Moines\n3 Hour Total Precipitation [inch]")
+ax.set_title("GFS Grid Point Forecast for Ames\n3 Hour Total Precipitation [inch]")
 
 ax.set_xticks( numpy.array(xticks) + 0.5 )
-ax.set_xticklabels( xlabels )
+ax.set_xticklabels( xlabels , fontsize=8)
 ax.set_yticks( yticks )
 ax.set_yticklabels( ylabels )
 ax.set_xlabel('Forecast Date')
 ax.set_ylabel('Model Run')
 
-ax.plot([-0.5,32], [40.5,40.5], color='k')
-#ax.plot([1.5,1.5], [1.5,33.5], color='k')
-#ax.plot([2.5,2.5], [1.5,33.5], color='k')
+ax.text(-0.01, 1, "Obs->", ha='right', va='top', transform=ax.transAxes)
 
 import iemplot
 fig.savefig('test.ps')
