642e5bd1edfc0c2b9e703eb7c6fc8ae003cd1356
akrherz <akrherz@iastate.edu>
2014-09-29 09:12:00 -0500

    pylint cleanup

--- scripts/coop/fetch_merra.py
+++ scripts/coop/fetch_merra.py
@@ -1,8 +1,8 @@
-'''
- Monthly download of the MERRA data, website suggests that the previous 
- month is available around the 2-3 week of the next month, so this is run 
+"""
+ Monthly download of the MERRA data, website suggests that the previous
+ month is available around the 2-3 week of the next month, so this is run
  from RUN_MIDNIGHT.sh, but only on the 21rst of each month
-'''
+"""
 import datetime
 import urllib2
 import os
@@ -10,14 +10,15 @@ import sys
 import subprocess
 
 def trans(now):
+    """ Hacky hack hack """
     if now.year < 1993:
         return '101'
     if now.year < 2001:
         return '201'
     return '300'
 
-def do_month( sts ):
-    ''' Run for a given month '''
+def do_month(sts):
+    """ Run for a given month """
 
     ets = sts + datetime.timedelta(days=35)
     ets = ets.replace(day=1)
@@ -25,8 +26,8 @@ def do_month( sts ):
     interval = datetime.timedelta(days=1)
     now = sts
     while now < ets:
-    
-        uri = now.strftime( ("http://goldsmr2.sci.gsfc.nasa.gov/daac-bin/OTF/"
+
+        uri = now.strftime(("http://goldsmr2.sci.gsfc.nasa.gov/daac-bin/OTF/"
             +"HTTP_services.cgi?FILENAME=%%2Fdata%%2Fs4pa%%2FMERRA%%2"
             +"FMAT1NXRAD.5.2.0%%2F%Y%%2F%m%%2FMERRA"+trans(now)+".prod.assim."
             +"tavg1_2d_rad_Nx.%Y%m%d.hdf&FORMAT=TmV0Q0RGL2d6aXAv&"
@@ -34,32 +35,35 @@ def do_month( sts ):
             +trans(now)+".prod.assim.tavg1_2d_rad_Nx.%Y%m%d.SUB.nc.gz&"
             +"FLAGS=&SHORTNAME=MAT1NXRAD&SERVICE=SUBSET_LATS4D&LAYERS=&"
             +"VERSION=1.02&VARIABLES=swtdn,swgdn,swgdnclr,tauhgh,taulow,"
-            +"taumid,tautot,cldhgh,cldlow,cldmid,cldtot") )
-    
+            +"taumid,tautot,cldhgh,cldlow,cldmid,cldtot"))
+
         try:
             req = urllib2.Request(uri)
-            data = urllib2.urlopen(req).read()   
+            data = urllib2.urlopen(req).read()
         except:
             uri = uri.replace("MERRA300", "MERRA301")
             req = urllib2.Request(uri)
-            data = urllib2.urlopen(req).read()   
+            data = urllib2.urlopen(req).read()
         dirname = now.strftime("/mnt/longterm2/merra/%Y")
         if not os.path.isdir(dirname):
             os.makedirs(dirname)
-        fp = open( now.strftime("/mnt/longterm2/merra/%Y/%Y%m%d.nc.gz"), 'w')
-        fp.write( data )
+        fp = open(now.strftime("/mnt/longterm2/merra/%Y/%Y%m%d.nc.gz"), 'w')
+        fp.write(data)
         fp.close()
         now += interval
 
     os.chdir(dirname)
     subprocess.call("gunzip *.gz", shell=True)
 
-if __name__ == '__main__':
-    ''' Run for last month month '''
+def main():
+    """ Run for last month month """
     now = datetime.datetime.now()
     if len(sys.argv) == 3:
-        now =  datetime.datetime(int(sys.argv[1]), int(sys.argv[2]), 1)
+        now = datetime.datetime(int(sys.argv[1]), int(sys.argv[2]), 1)
     else:
         now = now - datetime.timedelta(days=35)
         now = now.replace(day=1)
-    do_month( now )
+    do_month(now)
+
+if __name__ == '__main__':
+    main()
