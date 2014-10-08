ee675b98eae2e45b0b5fee86a6a054911e9022e8
akrherz <akrherz@iastate.edu>
2014-10-08 16:04:04 -0500

    WIP for display of climate data

---
+++ htdocs/api/nwstext.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+"""
+ Return NWS Text
+"""
+import psycopg2
+import cgi
+import sys
+import pytz
+import datetime
+import memcache
+
+def get_data(product):
+    """ Go get this product from the database 
+    201410071957-KDMX-FXUS63-AFDDMX
+    """
+    pgconn = psycopg2.connect(database='afos', host='iemdb', user='nobody')
+    cursor = pgconn.cursor()
+
+    ts = datetime.datetime.strptime(product[:12], "%Y%m%d%H%M")
+    ts = ts.replace(tzinfo=pytz.timezone("UTC"))
+
+    source = product[13:17]
+    pil = product[25:]
+
+    cursor.execute("""SELECT data from products where source = %s
+    and pil = %s and entered = %s""", (source, pil, ts))
+    
+    if cursor.rowcount == 0:
+        return 'Not Found %s %s' % (source, pil)
+    
+    row = cursor.fetchone()
+    return row[0].replace("\r\r\n", "\n")
+
+def main():
+    ''' Do Stuff '''
+    sys.stdout.write("Content-type: text/plain\n\n")
+    field = cgi.FieldStorage()
+    product = field.getfirst('p', None)[:31]
+    cb = field.getfirst('callback', None)
+
+    mckey = "/api/nwstext/%s" % (product,)
+    mc = memcache.Client(['iem-memcached:11211'], debug=0)
+    res = mc.get(mckey)
+    if not res:
+        res = get_data(product)
+        mc.set(mckey, res, 1800)
+    if cb is None:
+        sys.stdout.write( res )
+    else:
+        sys.stdout.write("%s(%s)" % (cb, res))
+
+    
+if __name__ == '__main__':
+    main()
---
+++ htdocs/nws/climap.js
@@ -0,0 +1,106 @@
+var renderattr = "high";
+var vectorLayer;
+var map;
+var element;
+
+function updateMap(){
+	renderattr = $('#renderattr').val();
+	vectorLayer.setStyle( vectorLayer.getStyle() );
+}
+
+function init(){
+
+	$( "#datepicker" ).datepicker();
+	
+vectorLayer = new ol.layer.Vector({
+	source: new ol.source.GeoJSON({
+	  	projection: ol.proj.get('EPSG:4326'),
+	    url: '/geojson/cli.py'
+  	}),
+  	style: function(feature, resolution){
+  		style = [new ol.style.Style({
+  	        fill: new ol.style.Fill({
+  	          color: 'rgba(255, 255, 255, 0.6)'
+  	        }),
+  	        stroke: new ol.style.Stroke({
+  	          color: '#319FD3',
+  	          width: 1
+  	        }),
+  	        text: new ol.style.Text({
+  	          font: '12px Calibri,sans-serif',
+  	          text: feature.get(renderattr),
+  	          fill: new ol.style.Fill({
+  	            color: '#000'
+  	          }),
+  	          stroke: new ol.style.Stroke({
+  	            color: '#fff',
+  	            width: 3
+  	          })
+  	        })
+  	      })];
+  		return style;
+  	}
+});
+
+map = new ol.Map({
+        target: 'map',
+        layers: [
+                 new ol.layer.Tile({
+                     title: "Global Imagery",
+                     source: new ol.source.TileWMS({
+                       url: 'http://maps.opengeo.org/geowebcache/service/wms',
+                       params: {LAYERS: 'bluemarble', VERSION: '1.1.1'}
+                     })
+                 }),
+          vectorLayer
+        ],
+        view: new ol.View({
+            projection: 'EPSG:4326',
+            center: [-95, 42],
+            zoom: 3,
+            maxResolution: 0.703125
+          })
+      });
+
+element = document.getElementById('popup');
+
+var popup = new ol.Overlay({
+	  element: element,
+	  positioning: 'bottom-center',
+	  stopEvent: false
+	});
+map.addOverlay(popup);
+	
+$(element).popover({
+    'placement': 'top',
+    'html': true,
+    content: function() { return $('#popover-content').html(); }
+  });
+
+// display popup on click
+map.on('click', function(evt) {
+  var feature = map.forEachFeatureAtPixel(evt.pixel,
+      function(feature, layer) {
+        return feature;
+      });
+  if (feature) {
+    var geometry = feature.getGeometry();
+    var coord = geometry.getCoordinates();
+    popup.setPosition(coord);
+    var content = "<p><strong>"+ feature.get('name') 
+    	+"<br />High: "+ feature.get('high') +" Norm:"+ feature.get("high_normal") +" Rec:"+ feature.get("high_record")
+    	+"<br />Low: "+ feature.get('low') +" Norm:"+ feature.get("low_normal") +" Rec:"+ feature.get("low_record")
+    	+"</p>";
+    $('#popover-content').html(content);
+    $(element).popover('show');
+    
+    $.get(feature.get('link'), function(data) {
+        $('#clireport').html("<pre>"+ data +"</pre>");
+     });
+    
+  } else {
+    $(element).popover('destroy');
+  }
+});
+
+};
