5edbb199f7bf9207679cf01d25eb516fc8d90e29
akrherz <akrherz@iastate.edu>
2014-10-14 09:04:48 -0500

    685 million hits milestone

--- scripts/feature/iem/hits.py
+++ scripts/feature/iem/hits.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
-import mx.DateTime
+import datetime
 
 x = []
 y = []
 for line in open('hits.txt'):
     tokens = line.split(":")
-    ts = mx.DateTime.strptime(tokens[0], '%d %B %Y')
+    ts = datetime.datetime.strptime(tokens[0], '%d %b %Y')
     x.append( ts )
     y.append( int(tokens[1])  )
 
@@ -16,9 +16,9 @@ ax.semilogy(x, y, lw=3)
 #ax.set_xlim( x[0].ticks(), x[-1].ticks() )
 xticks = []
 xticklabels = []
-ts0 = mx.DateTime.DateTime(2001,1,1)
-ts1 = mx.DateTime.DateTime(2015,1,2)
-interval = mx.DateTime.RelativeDateTime(days=1)
+ts0 = datetime.datetime(2001,1,1)
+ts1 = datetime.datetime(2015,1,2)
+interval = datetime.timedelta(days=1)
 now = ts0
 while now < ts1:
     if  (now.day == 1 and now.month == 1):
@@ -29,10 +29,8 @@ ax.set_xticks(xticks)
 ax.set_xticklabels(xticklabels, fontsize=20, rotation=90)
 ax.set_xlabel("Year", fontsize=20)
 ax.set_ylabel("Maximum Daily Web Requests", fontsize=20)
-ax.set_title("IEM Web Requests Milestones [17 Jun 2001-2013]", fontsize=18)
+ax.set_title("IEM Web Requests Milestones [17 Jun 2001-2014]", fontsize=18)
 ax.grid(True)
 plt.setp(ax.get_yticklabels(), fontsize=20)
 fig.tight_layout()
 fig.savefig('test.png')
-#import iemplot
-#iemplot.makefeature('test')
