cb65c152b095ffd9c72dbcb98c89b5ccdc80876e
akrherz <akrherz@iastate.edu>
2014-10-14 16:05:00 -0500

    pylint fixes and make white land area

--- htdocs/nws/days_since_plot.py
+++ htdocs/nws/days_since_plot.py
@@ -1,36 +1,36 @@
 #!/usr/bin/env python
-'''
+"""
  Generate a plot of the number of days since the last VTEC product issued
  by the WFO, save this in memcache for 30 minutes before regenerating
-'''
+"""
 
 import sys
-import numpy
 from pyiem import plot
 from pyiem.nws import vtec
 import psycopg2
 import cgi
 import datetime
 import memcache
-    
-utc = datetime.datetime.utcnow()
-bins = numpy.array([0,1,14,31,91,182,273,365,730,1460,2920,3800])
 
-POSTGIS = psycopg2.connect(database='postgis', host='iemdb', user='nobody')
-pcursor = POSTGIS.cursor()
+utc = datetime.datetime.utcnow()
+bins = [0, 1, 14, 31, 91, 182, 273, 365, 730, 1460, 2920, 3800]
+
 
 def run(phenomena, significance, mc, key):
     """ Figure out what the user wanted and do it! """
+    POSTGIS = psycopg2.connect(database='postgis', host='iemdb', user='nobody')
+    pcursor = POSTGIS.cursor()
 
     pcursor.execute("""
- select wfo,  extract(days from ('TODAY'::date - max(issue))) as m from warnings 
- where significance = %s and phenomena = %s GROUP by wfo ORDER by m ASC
+     select wfo,  extract(days from ('TODAY'::date - max(issue))) as m 
+     from warnings where significance = %s and phenomena = %s 
+     GROUP by wfo ORDER by m ASC
     """, (significance, phenomena))
     data = {}
     for row in pcursor:
-        data[ row[0]] = max([row[1],0])
+        data[row[0]] = max([row[1], 0])
 
-    p = plot.MapPlot(sector='nws',
+    p = plot.MapPlot(sector='nws', axisbg='white',
                  title='Days since Last %s %s by NWS Office' % (
                         vtec._phenDict.get(phenomena, phenomena),
                         vtec._sigDict.get(significance, significance)),
@@ -38,10 +38,10 @@ def run(phenomena, significance, mc, key):
     p.fill_cwas(data, bins=bins)
     p.postprocess(web=True, memcache=mc, memcachekey=key, memcacheexpire=1800)
 
-if __name__ == '__main__':
-    ''' See how we are called '''
+def main():
+    """ See how we are called """
     mc = memcache.Client(['iem-memcached:11211'], debug=0)
-    
+
     form = cgi.FieldStorage()
     phenomena = form.getfirst('phenomena', 'TO')[:2]
     significance = form.getfirst('significance', 'W')[0]
@@ -49,6 +49,10 @@ if __name__ == '__main__':
     res = mc.get(key)
     if res:
         sys.stdout.write('Content-type: image/png\n\n')
-        sys.stdout.write( res )
+        sys.stdout.write(res)
     else:
         run(phenomena, significance, mc, key)
+
+if __name__ == '__main__':
+    # Go Main
+    main()
