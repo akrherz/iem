c992862c12eb6dbb851b7f73eda075e0bf9ec0bf
akrherz <akrherz@iastate.edu>
2014-11-07 10:29:08 -0600

    Do better job with Stuart Smith Data

--- scripts/ingestors/stuart_smith.py
+++ scripts/ingestors/stuart_smith.py
@@ -1,5 +1,5 @@
 """
-
+ My belief is that the data is always in standard time
 """
 import subprocess
 import psycopg2
@@ -40,31 +40,32 @@ for line in open('/mnt/rootdocs/Bubbler.csv'):
 
 # Do the STS_GOLD file
 maxts = {}
-mcursor.execute("""SELECT max(valid), site_serial from ss_logger_data
-    GROUP by site_serial""")
+mcursor.execute("""SELECT max(valid at time zone 'UTC'), site_serial 
+    from ss_logger_data GROUP by site_serial""")
 for row in mcursor:
-    maxts[ row[1] ] = datetime.datetime.strptime(row[0].strftime('%m/%d/%y %H:%M:%S'), '%m/%d/%y %H:%M:%S')
-
-proc = subprocess.Popen("echo 'select * from logger_data' | mdb-sql -p /mnt/sts_gold/sts_gold.mdb",
-                        shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-
-data = proc.stdout.read()
-for linenum, line in enumerate(data.split("\n")):
-    tokens = line.split("\t")
-    if len(tokens) < 13 or linenum < 2:
-        continue
-    site_serial = int(tokens[1])
-    ts = datetime.datetime.strptime(tokens[2], '%m/%d/%y %H:%M:%S')
-    if ts > now:
-        continue
-    if maxts.get(site_serial) and ts < maxts.get(site_serial):
-        continue
-    mcursor.execute("""INSERT into ss_logger_data values (%s, %s, %s, %s, %s,
-    %s, %s, %s, %s, %s,
-    %s, %s, %s, %s, %s)""", tokens)
-    # This won't work, sadly
-    #ts = ts.replace(tzinfo=pytz.timezone("America/Chicago"))
+    # Max standard time value
+    maxts[ row[1] ] = row[0] - datetime.timedelta(hours=6)
     
+for sid in [9100104, 9100135, 9100131, 9100156]:
+    ts = maxts.get(sid, datetime.datetime(2000,1,1))
+    ticks = int(ts.strftime("%s"))
+    sql = ("SELECT * from logger_data WHERE site_serial = %s"
+           +" and Date_time > %s") % (sid, ticks)
+    proc = subprocess.Popen("echo '%s' | mdb-sql -p /mnt/sts_gold/sts_gold.mdb" % (sql,),
+                shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+    
+    data = proc.stdout.read()
+    for linenum, line in enumerate(data.split("\n")):
+        tokens = line.split("\t")
+        if len(tokens) < 13 or linenum < 2:
+            continue
+        site_serial = int(tokens[1])
+        ts = datetime.datetime.strptime(tokens[2], '%m/%d/%y %H:%M:%S')
+        tokens[2] = ts.strftime("%Y-%m-%d %H:%M:%S-0600")
+        mcursor.execute("""
+            INSERT into ss_logger_data values (%s, %s, %s, %s, %s,
+            %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)""", tokens)
+        
 mcursor.close()
 MESOSITE.commit()
 MESOSITE.close()
