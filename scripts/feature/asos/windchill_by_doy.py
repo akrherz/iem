3ddbb322786c6f1bce9a31b37232700d90c73700
akrherz <akrherz@iastate.edu>
2014-11-18 05:47:47 -0600

    18 nov feature

--- scripts/feature/asos/windchill_by_doy.py
+++ scripts/feature/asos/windchill_by_doy.py
@@ -21,7 +21,7 @@ def get_station(station):
     acursor.execute("""SELECT valid, 
       tmpf, sknt from alldata where station = %s and tmpf < 40 and
       sknt >= 0 and tmpf > -60
-      and valid BETWEEN '1973-01-01' and '2013-01-01' ORDER by valid ASC""", (station,))
+      and valid BETWEEN '1973-01-01' and '2014-01-01' ORDER by valid ASC""", (station,))
 
     last = None
     for row in acursor:
@@ -44,9 +44,9 @@ import matplotlib.pyplot as plt
 
 (fig, ax) = plt.subplots(1,1)
 
-ax.plot(numpy.arange(1,733), counts / 40.0 * 100.0, label='Mason City')
-ax.plot(numpy.arange(1,733), counts2 / 40.0 * 100.0, label='Des Moines')
-ax.plot(numpy.arange(1,733), counts3 / 40.0 * 100.0, label='Sioux Falls, SD')
+ax.plot(numpy.arange(1,733), counts / 41.0 * 100.0, label='Mason City')
+ax.plot(numpy.arange(1,733), counts2 / 41.0 * 100.0, label='Des Moines')
+ax.plot(numpy.arange(1,733), counts3 / 41.0 * 100.0, label='Sioux Falls, SD')
 ax.set_xticks( (305,335,365,365+32,365+60,365+91,365+121) )
 ax.set_xticklabels( ('Nov 1', 'Dec 1', 'Jan 1', 'Feb 1', 'Mar 1', 'Apr 1','May 1'))
 
@@ -54,7 +54,7 @@ ax.set_xlim(290, 470)
 ax.set_ylim(0,100)
 ax.set_ylabel("Percentage of Years [%]")
 ax.grid(True)
-ax.set_title("1973-2012 Daily Frequency of 1+ Sub-Zero $^{\circ}\mathrm{F}$ Wind Chill Ob")
+ax.set_title("1973-2013 Daily Frequency of 1+ Sub-Zero $^{\circ}\mathrm{F}$ Wind Chill Ob")
 ax.legend()
 
 fig.savefig('test.ps')
