943ad6e28f58481e03448081e48508b8a9f2d8aa
akrherz <akrherz@iastate.edu>
2014-12-04 09:20:51 -0600

    improve interaction as per bugs found
    Can now simply call like this to get the last PIL
    /wx/afos/p.php?pil=RRMEWX

--- htdocs/wx/afos/p.php
+++ htdocs/wx/afos/p.php
@@ -7,45 +7,52 @@ $t = new MyView();
 define("IEM_APPID", 47);
 $t->thispage = "archive-afos";
 
-$e = isset($_GET['e']) ? intval($_GET['e']) : "201112151144";
-$pil = isset($_GET['pil']) ? strtoupper(substr($_GET['pil'],0,6)) : "AFDDMX";
+$e = isset($_GET['e']) ? intval($_GET['e']) : null;
+$pil = isset($_GET['pil']) ? strtoupper(substr($_GET['pil'],0,6)) : null;
 $dir = isset($_REQUEST['dir']) ? $_REQUEST['dir'] : null;
+
+if ($pil == null || trim($pil) == ""){
+	die("No 'pil' provided by URL, it is required.");
+}
+
+$conn = iemdb("afos");
+
+// Okay, lets see if we can find the product we are looking for!
+if ($e == null){
+	$rs = pg_prepare($conn, "_LSELECT", "SELECT data, "
+			." entered at time zone 'UTC' as mytime, source from products"
+			." WHERE pil = $1 and entered > (now() - '31 days'::interval)"
+			." ORDER by entered DESC LIMIT 1");
+	$rs = pg_execute($conn, "_LSELECT", array($pil));
+} else {
+	if ($dir == 'next'){
+		$sortdir = "ASC";
+		$offset0 = 60;
+		$offset1 = 5*86400;
+	} else if ($dir == 'prev'){
+		$sortdir = "DESC";
+		$offset0 = -5*86400;
+		$offset1 = -1;
+	} else {
+		$sortdir = "ASC";
+		$offset0 = 0;
+		$offset1 = 60;
+	}
+	$ts = gmmktime( substr($e,8,2), substr($e,10,2), 0,
+			substr($e,4,2), substr($e,6,2), substr($e,0,4) );
+	$rs = pg_prepare($conn, "_LSELECT", "SELECT data,
+			entered at time zone 'UTC' as mytime, source from products
+			WHERE pil = $1 and entered between $2 and $3
+			ORDER by entered $sortdir LIMIT 100");
+	$rs = pg_execute($conn, "_LSELECT", Array($pil,
+			date("Y-m-d H:i", $ts+$offset0),
+			date("Y-m-d H:i", $ts+$offset1)));
+}
 
 $t->title = sprintf("%s from NWS %s", substr($pil,0,3), substr($pil,3,3));
 $t->twitter_image = "http://mesonet.agron.iastate.edu/content/pil_${pil}.png";
-
-$conn = iemdb("afos");
-$ts = gmmktime( substr($e,8,2), substr($e,10,2), 0, 
-      substr($e,4,2), substr($e,6,2), substr($e,0,4) );
-
-$t->twitter_description = sprintf("%s issued by NWS %s at %s UTC", substr($pil,0,3),
-		substr($pil,3,3), gmdate("d M Y H:i", $ts));
-
-
-if ($dir == 'next'){
-	$sortdir = "ASC";
-	$offset0 = 60;
-	$offset1 = 5*86400;
-} else if ($dir == 'prev'){
-	$sortdir = "DESC";
-	$offset0 = -5*86400;
-	$offset1 = -1;
-} else {
-	$sortdir = "ASC";
-	$offset0 = 0;
-	$offset1 = 60;
-}
-
-$rs = pg_prepare($conn, "_LSELECT", "SELECT data, 
-				entered at time zone 'UTC' as mytime, source from products
-                 WHERE pil = $1 and entered between $2 and $3
-                 ORDER by entered $sortdir LIMIT 100");
-$rs = pg_execute($conn, "_LSELECT", Array($pil, 
-	date("Y-m-d H:i", $ts+$offset0),
-	date("Y-m-d H:i", $ts+$offset1)));
-
-
 $content = "<h3>National Weather Service Raw Text Product</h3>";
+
 if (pg_numrows($rs) < 1){
 	$content .= "<div class=\"alert alert-warning\">Sorry, could not find product.</div>";
 }
@@ -54,6 +61,9 @@ for ($i=0; $row = @pg_fetch_assoc($rs, $i); $i++)
 	if ($i == 0){ 
 		$basets = strtotime($row["mytime"]); 
 		$newe = date("YmdHi", $basets);
+		$t->twitter_description = sprintf("%s issued by NWS %s at %s UTC",
+				substr($pil,0,3), substr($pil,3,3), date("d M Y H:i", $basets));
+		
 		$content .= "<p>Displaying AFOS PIL: <strong>$pil</strong> Received: <strong>". date("Y-m-d H:i", $basets) ." UTC</strong>";
 		$content .= "<br /><a rel=\"nofollow\" class=\"btn btn-primary\" href=\"p.php?dir=prev&pil=$pil&e=$newe\"><i class='glyphicon glyphicon-arrow-left'></i> Previous in Time</a>";
 		$content .= sprintf(" <a rel=\"nofollow\" class=\"btn btn-primary\" href=\"list.phtml?source=%s&day=%s&month=%s&year=%s\">View All %s Products for %s</a>", 
