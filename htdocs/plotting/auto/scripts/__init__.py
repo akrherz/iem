8acb3e10b241abfdd32ffa399728c677d4bdb964
akrherz <akrherz@iastate.edu>
2014-12-08 20:48:02 -0600

    move radiation plot to the website, 9 Dec feature

--- htdocs/plotting/auto/scripts/__init__.py
+++ htdocs/plotting/auto/scripts/__init__.py
@@ -19,6 +19,7 @@ data = {'plots': [
         {'id': "26", 'label': "Minimum Daily Low Temperature after 1 July"},
         {'id': "5", 'label': "Minimum Daily Temperature Range"},
         {'id': "22", 'label': "Percentage of Years within Temperature Range from Averages"},
+        {'id': "38", 'label': "Solar Radiation Estimates from NARR"},
         {'id': "25", 'label': "Spread of Daily High and Low Temperatures"},
         {'id': "4", 'label': "State Areal Coverage of Precip Intensity over X Days"},
         {'id': "28", 'label': "Trailing Number of Days Precipitation Total Rank"},
