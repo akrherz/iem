881559e2b8fa77a7043f91910f464d6f3eec89a3
akrherz <akrherz@iastate.edu>
2014-12-12 23:04:19 -0600

    refactor and add unit test

--- scripts/mrms/mrms_raster_pXXh.py
+++ scripts/mrms/mrms_raster_pXXh.py
@@ -1,13 +1,8 @@
 """
- Generate a raster of 24hour precipitation totals from MRMS
+ Generate a raster of XXhour precipitation totals from MRMS
 
  run from RUN_10_AFTER.sh
- 
- 255 levels...  wanna do 0 to 20 inches
- 
- 0-1   -> 100 - 0.01 res
- 1-5   -> 80 - 0.05 res
- 5-20  -> 75 - 0.20 res
+
 """
 
 import numpy as np
@@ -21,6 +16,38 @@ import util
 import json
 import pygrib
 import gzip
+import unittest
+
+def convert_to_image(data):
+    """Convert data with units of mm into image space 
+
+     255 levels...  wanna do 0 to 20 inches
+     index 255 is missing, index 0 is 0
+     0-1   -> 100 - 0.01 res ||  0 - 25   -> 100 - 0.25 mm  0
+     1-5   -> 80 - 0.05 res  ||  25 - 125 ->  80 - 1.25 mm  100
+     5-20  -> 75 - 0.20 res  || 125 - 500  ->  75 - 5 mm    180
+
+    000 -> 099  0.25mm  000.00 to 024.75
+    100 -> 179  1.25mm  025.00 to 123.75
+    180 -> 254  5.00mm  125.00 to 495.00
+    254                 500.00+
+    255  MISSING/BAD DATA
+    """
+    # Values above 500 mm are set to 254
+    imgdata = np.where(data >= 500, 254, 0)
+
+    imgdata = np.where(np.logical_and(data >= 125, data < 500), 
+                       180 + ((data - 125.) / 5.0), imgdata)
+    imgdata = np.where(np.logical_and(data >= 25, data < 125), 
+                       100 + ((data - 25.) / 1.25), imgdata)
+    imgdata = np.where(np.logical_and(data >= 0, data < 25), 
+                        data / 0.25, imgdata)
+    # -3 is no coverage -> 255
+    # -1 is misisng, so zero
+    # Index 255 is missing
+    imgdata = np.where( data < 0, 0, imgdata)
+    imgdata = np.where( data < -1, 255, imgdata)
+    return imgdata
 
 def doit(gts, hr):
     """
@@ -72,18 +99,7 @@ def doit(gts, hr):
     """
     #total = np.flipud(total)
     # Off scale gets index 254
-    imgdata = np.where(total >= 500, 254, 0)
-    imgdata = np.where(np.logical_and(total >= 125, total < 500), 
-                       180 + ((total - 125.) / 5.0), imgdata)
-    imgdata = np.where(np.logical_and(total >= 25, total < 125), 
-                       100 + ((total - 25.) / 1.25), imgdata)
-    imgdata = np.where(np.logical_and(total >= 0, total < 25), 
-                        total / 0.25, imgdata)
-    # -3 is no coverage -> 255
-    # -1 is misisng, so zero
-    # Index 255 is missing
-    imgdata = np.where( total < 0, 0, imgdata)
-    imgdata = np.where( total < -1, 255, imgdata)
+    imgdata = convert_to_image(total)
  
     (tmpfp, tmpfn) = tempfile.mkstemp()
     # Create Image
@@ -121,12 +137,23 @@ def doit(gts, hr):
     os.close(tmpfp)
     os.unlink(tmpfn)
 
-if __name__ == "__main__":
+def main():
+    """ do main stuff """
     if len(sys.argv) == 5:
-        gts = datetime.datetime(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]),
-                                   int(sys.argv[4]), 0)
+        gts = datetime.datetime(int(sys.argv[1]), int(sys.argv[2]), 
+                                int(sys.argv[3]), int(sys.argv[4]), 0)
     else:
         gts = datetime.datetime.utcnow()
         gts = gts.replace(minute=0,second=0,microsecond=0)
     for hr in [24,48,72]:
         doit( gts , hr)
+
+if __name__ == "__main__":
+    main()
+    
+class test(unittest.TestCase):
+    
+    def test_ramp(self):
+        """ Check our work """
+        img = convert_to_image(np.array([25,]))
+        self.assertEquals(img[0], 100)
