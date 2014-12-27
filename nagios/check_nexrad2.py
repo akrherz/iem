67e2dc639f1881256464f387cda2dbfa231f3374
akrherz <akrherz@iastate.edu>
2014-12-26 21:09:58 -0600

    improve log message to be more meaningful

--- nagios/check_nexrad2.py
+++ nagios/check_nexrad2.py
@@ -5,28 +5,30 @@ import os
 import sys
 import stat
 import datetime
-SAMPLES = ['KDMX', 'KAMA', 'KLWX', 'KFFC', 'KBMX']
+SAMPLES = ['KDMX', 'KAMA', 'KLWX', 'KFFC', 'KBMX', 'KBGM', 'KCLE']
 
 def check():
     now = datetime.datetime.now()
-    count = 0
+    count = []
     for nexrad in SAMPLES:
         fn = "/mnt/mtarchive/nexrd2/raw/%s/dir.list" % (nexrad,)
         mtime = os.stat(fn)[stat.ST_MTIME]
         ts = datetime.datetime.fromtimestamp(mtime)
         diff = (now - ts).days * 86400. + (now - ts).seconds
         if diff > 300:
-            count += 1
+            count.append(nexrad)
     return count
     
 if __name__ == '__main__':
     badcount = check()
-    if badcount < 2:
-        print 'OK - %s/%s outage' % (badcount, len(SAMPLES))
+    msg = '%s/%s outage %s' % (len(badcount), len(SAMPLES),
+                                        ','.join(badcount))
+    if len(badcount) < 2:
+        print 'OK - %s' % (msg,)
         sys.exit(0)
-    elif badcount < 3:
-        print 'WARNING - %s/%s outage' % (badcount, len(SAMPLES))
+    elif len(badcount) < 3:
+        print 'WARNING - %s' % (msg,)
         sys.exit(1)
     else:
-        print 'CRITICAL - %s/%s outage' % (badcount, len(SAMPLES))
+        print 'CRITICAL - %s' % (msg,)
         sys.exit(2)
