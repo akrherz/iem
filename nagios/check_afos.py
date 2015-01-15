e07d5e288e07328884eb7047f234590b69ce16bf
akrherz <akrherz@iastate.edu>
2015-01-15 17:39:14 -0600

    Add nagios check of AFOS ingest

---
+++ nagios/check_afos.py
@@ -0,0 +1,28 @@
+"""
+ Check how much AFOS data we are ingesting
+"""
+import os
+import sys
+import stat
+import psycopg2
+IEM = psycopg2.connect(database='afos', host='iemdb', user='nobody')
+icursor = IEM.cursor()
+
+def check():
+    icursor.execute("""SELECT count(*) from products 
+    WHERE entered > now() - '1 hour'::interval""")
+    row = icursor.fetchone()
+
+    return row[0]
+    
+if __name__ == '__main__':
+    count = check()
+    if count > 1000:
+        print 'OK - %s count |count=%s;100;500;1000' % (count, count)
+        sys.exit(0)
+    elif count > 500:
+        print 'WARNING - %s count |count=%s;100;500;1000' % (count, count)
+        sys.exit(1)
+    else:
+        print 'CRITICAL - %s count |count=%s;100;500;1000' % (count, count)
+        sys.exit(2)
