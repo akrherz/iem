248efb975c17c17e872a674691ea125e2891d48a
akrherz <akrherz@iastate.edu>
2015-01-16 13:26:21 -0600

    Add harvest of Management Datastore to schedule

---
+++ database/upgrade/sustainablecorn/0.sql
@@ -0,0 +1,7 @@
+--- Support additional columns in the Management Datastore
+
+ALTER TABLE operations ADD plantryemethod varchar;
+ALTER TABLE operations ADD plantmaturity varchar;
+ALTER TABLE operations ADD growthstage varchar;
+ALTER TABLE operations ADD canopyheight varchar;
+ALTER TABLE operations ADD fertilizercrop varchar;
