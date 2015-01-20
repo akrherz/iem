e90a93dd017424a1c80cd108550277f8e72c981d
akrherz <akrherz@iastate.edu>
2015-01-20 10:45:28 -0600

    change order of CSCAP processing

--- scripts/RUN_12Z.sh
+++ scripts/RUN_12Z.sh
@@ -17,10 +17,11 @@ python harvest_soil_nitrate.py 2012
 python harvest_soil_nitrate.py 2013
 python harvest_soil_nitrate.py 2014
 python harvest_soil_bd.py 2013
-python email_daily_changes.py 
 python set_dashboard_links.py 2012
 python set_dashboard_links.py 2013
 python set_dashboard_links.py 2014
+python set_dashboard_links.py 2015
+python email_daily_changes.py 
 
 
 # Rerun yesterday and today
