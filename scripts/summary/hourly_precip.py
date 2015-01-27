c4a3129d2ec9de9e1b732be8183445dcbf15691e
akrherz <akrherz@iastate.edu>
2015-01-27 09:15:52 -0600

    cleanups, generate N0Q composite as well

--- scripts/summary/hourly_precip.py
+++ scripts/summary/hourly_precip.py
@@ -3,23 +3,28 @@ My purpose in life is to update the hourly_YYYY with observed or
 computed hourly precipitation
 """
 import mx.DateTime
-import iemdb
-IEM = iemdb.connect('iem')
+import psycopg2
+IEM = psycopg2.connect(database='iem', host='iemdb')
 icursor = IEM.cursor()
-icursor.execute("SET TIME ZONE 'GMT'")
+icursor.execute("SET TIME ZONE 'UTC'")
 
 # Our period
 t0 = mx.DateTime.utc() + mx.DateTime.RelativeDateTime(hours=-1,minute=0)
 t1 = mx.DateTime.utc() + mx.DateTime.RelativeDateTime(hours=-0,minute=0)
 
 sql = """INSERT into hourly_%s 
-       (SELECT t.id, t.network, '%s+00'::timestamp as v, max(phour) as p 
-        from current_log c, stations t WHERE (valid - '1 minute'::interval) >= '%s' 
-        and (valid - '1 minute'::interval) < '%s' and phour >= 0 and 
-        t.network NOT IN ('KCCI','KELO','KIMT') and c.iemid = t.iemid
-        and t.network !~* 'DCP' GROUP by t,id, t.network, v)""" % (
-                t0.year, t0.strftime("%Y-%m-%d %H:%M"), t0.strftime("%Y-%m-%d %H:%M"), 
-        t1.strftime("%Y-%m-%d %H:%M") )
+       (SELECT t.id, t.network, '%s+00'::timestamp as v, max(phour) as p, 
+       t.iemid 
+        from current_log c, stations t WHERE 
+        (valid - '1 minute'::interval) >= '%s' 
+        and (valid - '1 minute'::interval) < '%s' 
+        and phour >= 0 and 
+        t.network NOT IN ('KCCI','KELO','KIMT') 
+        and c.iemid = t.iemid
+        and t.network !~* 'DCP' 
+        GROUP by t,id, t.network, v, t.iemid)""" % (
+        t0.year, t0.strftime("%Y-%m-%d %H:%M"), 
+        t0.strftime("%Y-%m-%d %H:%M"), t1.strftime("%Y-%m-%d %H:%M"))
 icursor.execute( sql )
 IEM.commit()
 IEM.close()
--- scripts/summary/max_reflect.py
+++ scripts/summary/max_reflect.py
@@ -1,31 +1,64 @@
+"""Create simple maximum dbz composites for a given UTC date
+"""
 import time
 import osgeo.gdal as gdal
 import random
-import osgeo.gdal_array
-from osgeo.gdalconst import *
-import numpy, mx.DateTime, os, shutil, urllib2, sys
+from osgeo import gdalconst
+import numpy
+import datetime
+import os
+import urllib2
+import pytz
+import subprocess
+import psycopg2
 
-def run(sts):
-    """
-    Generate max dBZ composite for a given UTC date
-    """
-    ets = sts + mx.DateTime.RelativeDateTime(days=1)
-    interval = mx.DateTime.RelativeDateTime(minutes=5)
+PGCONN = psycopg2.connect(database='mesosite', host='iemdb', user='nobody')
+CURSOR = PGCONN.cursor()
+PQINSERT = "/home/ldm/bin/pqinsert"
+URLBASE = "http://iem.local/GIS/radmap.php?width=1280&height=720&"
 
-    # Load the color table
-    n0r = gdal.Open('../../htdocs/docs/nexrad_composites/reflect_ramp.png', 0)
-    n0rct = n0r.GetRasterBand(1).GetRasterColorTable().Clone()
-  
+def get_colortable(prod):
+    """Get the color table for this prod
+    
+    Args:
+      prod (str): product to get the table for
+      
+    Returns:
+      colortable
+      
+    """
+    CURSOR.execute("""
+    select r,g,b from iemrasters_lookup l JOIN iemrasters r on 
+    (r.id = l.iemraster_id) WHERE r.name = %s ORDER by l.coloridx ASC
+    """, ("composite_"+prod,))
+    ct = gdal.ColorTable()
+    for i, row in enumerate(CURSOR):
+        ct.SetColorEntry(i, (row[0], row[1], row[2], 255))
+    return ct
+
+def run(prod, sts):
+    """Create a max dbZ plot
+    
+    Args:
+      prod (str): Product to run for, either n0r or n0q
+      sts (datetime): date to run for
+    """
+    ets = sts + datetime.timedelta(days=1)
+    interval = datetime.timedelta(minutes=5)
+
+    n0rct = get_colortable(prod)
+   
     # Loop over our archived files and do what is necessary
     maxn0r = None
     now = sts
     while now < ets:
-        fp = now.strftime("/mesonet/ARCHIVE/data/%Y/%m/%d/GIS/uscomp/n0r_%Y%m%d%H%M.png")
-        if (not os.path.isfile(fp)):
-            print "MISSING:", fp
+        fn = now.strftime(("/mesonet/ARCHIVE/data/%Y/%m/%d/"
+                           +"GIS/uscomp/"+prod+"_%Y%m%d%H%M.png"))
+        if not os.path.isfile(fn):
+            print "MISSING:", fn
             now += interval
             continue
-        n0r = gdal.Open(fp, 0)
+        n0r = gdal.Open(fn, 0)
         n0rd = numpy.ravel( n0r.ReadAsArray() )
         if maxn0r is None:
             maxn0r = n0rd
@@ -35,73 +68,92 @@ def run(sts):
 
 
     out_driver = gdal.GetDriverByName("gtiff")
-    outdataset = out_driver.Create('max.tiff', n0r.RasterXSize, n0r.RasterYSize, n0r.RasterCount, GDT_Byte)
+    outdataset = out_driver.Create('max.tiff', n0r.RasterXSize, 
+        n0r.RasterYSize, n0r.RasterCount, gdalconst.GDT_Byte)
     # Set output color table to match input
     outdataset.GetRasterBand(1).SetRasterColorTable( n0rct )
-    outdataset.GetRasterBand(1).WriteArray( numpy.resize(maxn0r, (n0r.RasterYSize, n0r.RasterXSize))  )
+    outdataset.GetRasterBand(1).WriteArray( numpy.resize(maxn0r, 
+                                (n0r.RasterYSize, n0r.RasterXSize)))
     del outdataset
 
-    os.system("convert max.tiff max.png")
+    subprocess.call("convert max.tiff max.png", shell=True)
     # Insert into LDM
-    cmd = "/home/ldm/bin/pqinsert -p 'plot a %s0000 bogus GIS/uscomp/max_n0r_0z0z_%s.png png' max.png" % (
-                                            sts.strftime("%Y%m%d"), sts.strftime("%Y%m%d") )
-    os.system(cmd)
+    cmd = ("%s -p 'plot a %s0000 bogus GIS/uscomp/max_%s_0z0z_%s.png "
+           +"png' max.png") % (
+        PQINSERT, sts.strftime("%Y%m%d"), prod, sts.strftime("%Y%m%d") )
+    subprocess.call(cmd, shell=True)
 
     # Create tmp world file
-    out = open('/tmp/tmpwld%s.wld' % (sts.strftime("%Y%m%d"),), 'w')
-    out.write("""   0.010000000000%s
-   0.00000
-   0.00000
-  -0.01000000000000%s
--126.000000
-  50.0000""" % (random.randint(0,1000), random.randint(0,1000) ) )
+    wldfn = '/tmp/tmpwld%s.wld' % (sts.strftime("%Y%m%d"),)
+    out = open(wldfn, 'w')
+    if prod == 'n0r':
+        out.write("""   0.010000000000%s
+       0.00000
+       0.00000
+      -0.01000000000000%s
+    -126.000000
+      50.0000""" % (random.randint(0,1000), random.randint(0,1000) ) )
+    else:
+        out.write("""   0.005000000000%s
+       0.00000
+       0.00000
+      -0.00500000000000%s
+    -126.000000
+      50.0000""" % (random.randint(0,1000), random.randint(0,1000) ) )
+        
     out.close()
   
     # Insert world file as well
-    cmd = "/home/ldm/bin/pqinsert -p 'plot a %s0000 bogus GIS/uscomp/max_n0r_0z0z_%s.wld wld' /tmp/tmpwld%s.wld" % (
-         sts.strftime("%Y%m%d"), sts.strftime("%Y%m%d"),  sts.strftime("%Y%m%d") )
-    os.system(cmd)
+    cmd = ("%s -p 'plot a %s0000 bogus GIS/uscomp/max_%s_0z0z_%s.wld "
+           +"wld' %s") % (PQINSERT, sts.strftime("%Y%m%d"), prod, 
+                          sts.strftime("%Y%m%d"), wldfn)
+    subprocess.call(cmd, shell=True)
 
     # cleanup
     os.remove("max.tiff")
     os.remove("max.png")
-    os.remove( '/tmp/tmpwld%s.wld' % (sts.strftime("%Y%m%d"),) )
+    os.remove(wldfn)
   
     # Sleep for a bit
     time.sleep(60)
   
     # Iowa
-    png = urllib2.urlopen("http://iemvs105.local/GIS/radmap.php?layers[]=uscounties&layers[]=nexrad_tc&ts=%s" % (
-                                                                        sts.strftime("%Y%m%d%H%M"),))
+    png = urllib2.urlopen("%slayers[]=uscounties&layers[]=%s&ts=%s" % (
+        URLBASE, "nexrad_tc" if prod == 'n0r' else 'n0q_tc',
+        sts.strftime("%Y%m%d%H%M"),))
     o = open('tmp.png', 'wb')
     o.write( png.read() )
     o.close()
-    cmd = "/home/ldm/bin/pqinsert -p 'plot ac %s0000 summary/max_n0r_0z0z_comprad.png comprad/max_n0r_0z0z_%s.png png' tmp.png" % (
-                                                                    sts.strftime("%Y%m%d"), sts.strftime("%Y%m%d") )
-    os.system(cmd)
+    cmd = ("%s -p 'plot ac %s0000 summary/max_%s_0z0z_comprad.png "
+           +"comprad/max_%s_0z0z_%s.png png' tmp.png") % (
+                            PQINSERT, sts.strftime("%Y%m%d"), prod, prod,
+                            sts.strftime("%Y%m%d") )
+    subprocess.call(cmd, shell=True)
 
     # US
-    png = urllib2.urlopen("http://iemvs105.local/GIS/radmap.php?sector=conus&layers[]=uscounties&layers[]=nexrad_tc&ts=%s" % (
-                                                                        sts.strftime("%Y%m%d%H%M"),))
+    png = urllib2.urlopen(("%ssector=conus&layers[]=uscounties&"
+                           +"layers[]=%s&ts=%s") % (
+        URLBASE, "nexrad_tc" if prod == 'n0r' else 'n0q_tc',
+        sts.strftime("%Y%m%d%H%M"),))
     o = open('tmp.png', 'wb')
     o.write( png.read() )
     o.close()
-    cmd = "/home/ldm/bin/pqinsert -p 'plot ac %s0000 summary/max_n0r_0z0z_usrad.png usrad/max_n0r_0z0z_%s.png png' tmp.png" % (
-                                                                        sts.strftime("%Y%m%d"), sts.strftime("%Y%m%d") )
-    os.system(cmd)
+    cmd = ("%s -p 'plot ac %s0000 summary/max_%s_0z0z_usrad.png "
+           +"usrad/max_%s_0z0z_%s.png png' tmp.png") % (
+        PQINSERT, sts.strftime("%Y%m%d"), prod, prod, sts.strftime("%Y%m%d") )
+    subprocess.call(cmd, shell=True)
     os.remove("tmp.png")
 
-"""
-s = mx.DateTime.DateTime(2008,6,6)
-e = mx.DateTime.DateTime(2008,6,7)
-now = s
-while (now < e):
-  print now
-  run(now)
-  now += mx.DateTime.RelativeDateTime(days=1)
-"""
+def main():
+    """Run main()"""
+    # Default is to run for yesterday
+    ts = datetime.datetime.utcnow() - datetime.timedelta(days=1)
+    ts = ts.replace(tzinfo=pytz.timezone("UTC"))
+    ts = ts.replace(hour=0, minute=0, second=0, microsecond=0)
+    for prod in ['n0r', 'n0q']:
+        run(prod, ts)
 
-ts = mx.DateTime.now() + mx.DateTime.RelativeDateTime(hour=0,minute=0)
-run( ts )
-
+if __name__ == '__main__':
+    # Do something
+    main()
 
