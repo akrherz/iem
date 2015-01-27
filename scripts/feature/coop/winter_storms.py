79a488a040e3b20edfb9c6287244a4c393c42e68
akrherz <akrherz@iastate.edu>
2015-01-26 21:18:59 -0600

    27 Jan feature, use older version of this again, perhaps

--- scripts/feature/coop/winter_storms.py
+++ scripts/feature/coop/winter_storms.py
@@ -1,6 +1,6 @@
-import iemdb
+import psycopg2
 import numpy
-COOP = iemdb.connect('coop', bypass=True)
+COOP = psycopg2.connect(database='coop', host='iemdb', user='nobody')
 ccursor = COOP.cursor()
 
 ccursor.execute("""
@@ -12,48 +12,45 @@ ccursor.execute("""
 """)
 
 tots = numpy.zeros( (116,260), 'f')
+events = numpy.zeros( (116,260), 'f')
 is_storm = False
 for row in ccursor:
     snow = row[3]
     if snow > 0:
-        #if not is_storm:
-            # We are in a storm
         doy = row[1]
         year = row[0].year
         storm_tot = tots[ year - 1900,-1]
         tots[ year - 1900, doy: ] = storm_tot + snow
+        if not is_storm:
+           # We are in a storm
+           storm_tot = events[ year - 1900,-1]
+           events[ year - 1900, doy: ] = storm_tot + 1
+        is_storm = True
+    else:
+        is_storm = False
 
 import matplotlib.pyplot as plt
 import mx.DateTime
-
-maxtot = (numpy.max( tots, 1 )).tolist()
-the_max = max( maxtot )
-max_year = maxtot.index( the_max ) + 1900
-the_min = min( maxtot[:-1] )
-min_year = maxtot.index( the_min ) + 1900
 
 xticks = [1,32,62,93,124,155, 183, 214, 244]
 xticklabels = ['1 Sep','1 Oct', '1 Nov', '1 Dec', '1 Jan','1 Feb', '1 Mar', '1 Apr', '1 May']
 
 fig = plt.figure()
 ax3 = fig.add_subplot(111)
-ax3.plot( numpy.arange(0,260), numpy.average(tots,0), lw=2, label="Average" )
-ax3.plot( numpy.arange(0,260), tots[max_year-1900,:], lw=2, label="%s - %s (Max)" % (
-  max_year -1, max_year,) )
-ax3.plot( numpy.arange(0,260), tots[min_year-1900,:], lw=2, label="%s - %s (Min)" % (
-  min_year -1, min_year,) )
-ax3.plot( numpy.arange(0,113), tots[2015-1900,:113], lw=2, label="2014 - 2015")
+v = numpy.average(tots,0)
+ax3.plot( numpy.arange(0,260), v / float(max(v)) * 100., lw=2, label="Accumulation %.1f in" % (max(v),) )
+v = numpy.average(events,0)
+ax3.plot( numpy.arange(0,260), v / float(max(v)) * 100., lw=2, label="Events %.1f" % (max(v),) )
 ax3.set_xticks(xticks)
 ax3.set_xticklabels(xticklabels)
 ax3.set_xlim(31,245)
 ax3.set_title("Des Moines Total Snowfall\nEach Winter between 1900-2013")
 #ax3.plot([32,135], [avgV,avgV], color='k')
-ax3.grid(True)   
+ax3.grid(True)
+ax3.set_yticks([0,10,25,50,75,100])
 #ax3.set_ylim(0,31)  
-ax3.set_xlabel("* 2014-2015 thru 22 December")
-ax3.set_ylabel("Snowfall Total [inch]")
+#ax3.set_xlabel("* 2014-2015 thru 22 December")
+ax3.set_ylabel("Percentage of Average Total")
 ax3.legend(loc=2)
 
-fig.savefig('test.ps')
-import iemplot
-iemplot.makefeature('test')
+fig.savefig('test.png')
