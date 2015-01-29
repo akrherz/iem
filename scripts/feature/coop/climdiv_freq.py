1cb796247fc51e833c6a8c0bf6db719e51a8ebb3
akrherz <akrherz@iastate.edu>
2015-01-29 11:51:32 -0600

    climate division plot, 29 Jan feature

---
+++ scripts/feature/coop/climdiv_freq.py
@@ -0,0 +1,28 @@
+
+import psycopg2.extras
+import calendar
+from pyiem.plot import MapPlot
+import matplotlib.cm as cm
+import datetime
+
+pgconn = psycopg2.connect(database='coop', host='iemdb', user='nobody')
+cursor = pgconn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+cursor.execute("""
+        SELECT station, 
+        sum(case when high > 32 then 1 else 0 end) as above,
+        count(*) from alldata
+        WHERE substr(station,3,1) = 'C' and month in (12,1,2) 
+        GROUP by station""")
+data = {}
+for row in cursor:
+    data[row['station']] = row['above'] / float(row['count']) * 100.
+
+m = MapPlot(sector='midwest', axisbg='white',
+                title='Percent of Dec, Jan, Feb Days with High Above 32$^\circ$F by Climate District',
+                subtitle='Based on IEM Estimates')
+cmap = cm.get_cmap("BrBG_r")
+cmap.set_under('white')
+cmap.set_over('black')
+m.fill_climdiv(data, cmap=cmap)
+m.postprocess(filename='test.png')
