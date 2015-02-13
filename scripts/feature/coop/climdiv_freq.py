ea0827925e83251c5f8ec5e68614a01ebadcdcf4
akrherz <akrherz@iastate.edu>
2015-02-12 21:14:27 -0600

    13 Feb feature of winters that did not breach 50 degrees

--- scripts/feature/coop/climdiv_freq.py
+++ scripts/feature/coop/climdiv_freq.py
@@ -9,18 +9,20 @@ pgconn = psycopg2.connect(database='coop', host='iemdb', user='nobody')
 cursor = pgconn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
 cursor.execute("""
-        SELECT station, 
-        sum(case when high > 32 and low < 32 then 1 else 0 end) as above,
-        count(*) from alldata
-        WHERE substr(station,3,1) = 'C' and month in (12,1,2) 
-        GROUP by station""")
+  WITH yearmax as (
+    SELECT station, max(high),
+    extract(year from day + '35 days'::interval) as yr from alldata
+    WHERE substr(station,3,1) = 'C' and month in (12,1,2) GROUP by station, yr
+  )
+  SELECT station, sum(case when max < 50 then 1 else 0 end), count(*)
+  from yearmax GROUP by station""")
 data = {}
 for row in cursor:
-    data[row['station']] = row['above'] / float(row['count']) * 100.
+    data[row['station']] = row['sum'] / float(row['count']) * 100.
 
 m = MapPlot(sector='midwest', axisbg='white',
-                title='Percentage of Dec,Jan,Feb Days with High >32$^\circ$F + Low <32$^\circ$F',
-                subtitle='By Climate District and Based on IEM Estimates')
+            title='Percentage of Winters Seasons Where High failed to Reach 50$^\circ$F',
+            subtitle='Max Daily High Over Dec,Jan,Feb By Climate District; Based on IEM Estimates (1893-2014)')
 cmap = cm.get_cmap("BrBG_r")
 cmap.set_under('black')
 cmap.set_over('black')
