7862d83e5be23509955a6daa9b4d77beabfbb21d
akrherz <akrherz@iastate.edu>
2015-02-18 15:12:25 -0600

    sync small database fixes found with laptop testing

---
+++ database/init/portfolio.sql
@@ -0,0 +1,44 @@
+-- Portfolio database
+
+--
+-- Base of IEM Trouble tickets
+--
+CREATE TABLE tt_base(
+  id SERIAL UNIQUE NOT NULL,
+  portfolio varchar(30),
+  s_mid varchar(10),
+  entered timestamptz DEFAULT now(),
+  last timestamptz DEFAULT now(),
+  closed timestamptz,
+  subject varchar(256),
+  status varchar(30),
+  author varchar(30),
+  sensor varchar);
+CREATE INDEX tt_base_s_mid_idx on tt_base(s_mid);
+GRANT SELECT on tt_base to nobody,apache;
+
+--
+-- IEM Trouble Ticket Log
+--
+CREATE TABLE tt_log(
+  id SERIAL UNIQUE NOT NULL,
+  portfolio varchar(30),
+  s_mid varchar(10),
+  entered timestamptz DEFAULT now(),
+  author varchar(30),
+  status_c varchar(30),
+  comments text,
+  tt_id int REFERENCES tt_base(id));
+CREATE INDEX tt_log_tt_idx on tt_log(tt_id);
+GRANT SELECT on tt_log to nobody,apache;
+
+--
+-- IEM Site Contacts
+--
+CREATE TABLE iem_site_contacts(
+  id SERIAL NOT NULL,
+  portfolio varchar(30),
+  s_mid varchar(10),
+  name varchar(256),
+  phone varchar(20),
+  email varchar(100));
--- database/upgrade/postgis/6.sql
+++ database/upgrade/postgis/6.sql
@@ -4,3 +4,5 @@ ALTER TABLE text_products add expire timestamptz;
 
 CREATE INDEX text_products_issue_idx on text_products(issue);
 CREATE INDEX text_products_expire_idx on text_products(expire);
+
+GRANT SELECT on text_products to nobody,apache;
