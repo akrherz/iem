59a08e57f8dbb3f4d6843dcd92948b0e6f9206dd
akrherz <akrherz@iastate.edu>
2015-02-19 07:45:55 -0600

    pep8 cleanups and use pqinsert -i for all products

--- scripts/mrms/mrms_rainrate_comp.py
+++ scripts/mrms/mrms_rainrate_comp.py
@@ -1,6 +1,4 @@
-'''
-Generate a composite of the MRMS PrecipRate
-'''
+"""Generate a composite of the MRMS PrecipRate"""
 import datetime
 import pytz
 import numpy as np
@@ -14,20 +12,21 @@ import util
 import pygrib
 import gzip
 
-def do(now , realtime, delta):
+
+def do(now, realtime, delta):
     """ Generate for this timestep! """
     szx = 7000
     szy = 3500
     # Create the image data
-    imgdata = np.zeros( (szy, szx), 'u1')
+    imgdata = np.zeros((szy, szx), 'u1')
     sts = now - datetime.timedelta(minutes=2)
     metadata = {'start_valid': sts.strftime("%Y-%m-%dT%H:%M:%SZ"),
                 'end_valid': now.strftime("%Y-%m-%dT%H:%M:%SZ"),
                 'product': 'a2m',
-                'units': '0.1 mm' }
+                'units': '0.1 mm'}
 
     gribfn = now.strftime(("/mnt/a4/data/%Y/%m/%d/mrms/ncep/PrecipRate/"
-                           +"PrecipRate_00.00_%Y%m%d-%H%M00.grib2.gz"))
+                           "PrecipRate_00.00_%Y%m%d-%H%M00.grib2.gz"))
     if not os.path.isfile(gribfn):
         print("mrms_rainrate_comp.py MISSING %s" % (gribfn,))
         return
@@ -35,57 +34,63 @@ def do(now , realtime, delta):
     # http://www.nssl.noaa.gov/projects/mrms/operational/tables.php
     # Says units are mm/hr
     fp = gzip.GzipFile(gribfn, 'rb')
-    (tmpfp, tmpfn) = tempfile.mkstemp()
+    (_, tmpfn) = tempfile.mkstemp()
     tmpfp = open(tmpfn, 'wb')
     tmpfp.write(fp.read())
     tmpfp.close()
     grbs = pygrib.open(tmpfn)
     grb = grbs[1]
     os.unlink(tmpfn)
-    
+
     val = grb['values']
     # Convert into units of 0.1 mm accumulation
     val = val / 60.0 * 2.0 * 10.0
     val = np.where(val < 0., 255., val)
-    imgdata[:,:] = np.flipud(val.astype('int'))
+    imgdata[:, :] = np.flipud(val.astype('int'))
 
     (tmpfp, tmpfn) = tempfile.mkstemp()
-    
+
     # Create Image
-    png = Image.fromarray( np.flipud(imgdata) )
-    png.putpalette( util.make_colorramp() )
+    png = Image.fromarray(np.flipud(imgdata))
+    png.putpalette(util.make_colorramp())
     png.save('%s.png' % (tmpfn,))
 
     util.write_worldfile('%s.wld' % (tmpfn,))
     # Inject WLD file
-    routes = "c" if realtime else "" 
+    routes = "c" if realtime else ""
     prefix = 'a2m'
-    pqstr = "/home/ldm/bin/pqinsert -i -p 'plot a%s %s gis/images/4326/mrms/%s.wld GIS/mrms/%s_%s.wld wld' %s.wld" % (
-                    routes, now.strftime("%Y%m%d%H%M"), prefix, prefix, 
-                    now.strftime("%Y%m%d%H%M"), tmpfn )
+    pqstr = ("/home/ldm/bin/pqinsert -i -p 'plot a%s %s "
+             "gis/images/4326/mrms/%s.wld GIS/mrms/%s_%s.wld wld' %s.wld"
+             "") % (routes, now.strftime("%Y%m%d%H%M"), prefix, prefix,
+                    now.strftime("%Y%m%d%H%M"), tmpfn)
     subprocess.call(pqstr, shell=True)
     # Now we inject into LDM
-    pqstr = "/home/ldm/bin/pqinsert -p 'plot a%s %s gis/images/4326/mrms/%s.png GIS/mrms/%s_%s.png png' %s.png" % (
-                    routes, now.strftime("%Y%m%d%H%M"), prefix, prefix, 
-                    now.strftime("%Y%m%d%H%M"), tmpfn )
+    pqstr = ("/home/ldm/bin/pqinsert -i -p 'plot a%s %s "
+             "gis/images/4326/mrms/%s.png GIS/mrms/%s_%s.png png' %s.png"
+             "") % (routes, now.strftime("%Y%m%d%H%M"), prefix, prefix,
+                    now.strftime("%Y%m%d%H%M"), tmpfn)
     subprocess.call(pqstr, shell=True)
-    
+
     if realtime:
         # Create 900913 image
-        cmd = "gdalwarp -s_srs EPSG:4326 -t_srs EPSG:3857 -q -of GTiff -tr 1000.0 1000.0 %s.png %s.tif" % (tmpfn, tmpfn)
+        cmd = ("gdalwarp -s_srs EPSG:4326 -t_srs EPSG:3857 -q -of GTiff "
+               "-tr 1000.0 1000.0 %s.png %s.tif") % (tmpfn, tmpfn)
         subprocess.call(cmd, shell=True)
         # Insert into LDM
-        pqstr = "/home/ldm/bin/pqinsert -p 'plot c %s gis/images/900913/mrms/%s.tif GIS/mrms/%s_%s.tif tif' %s.tif" % (
-                        now.strftime("%Y%m%d%H%M"), prefix, prefix, 
-                        now.strftime("%Y%m%d%H%M"), tmpfn )
+        pqstr = ("/home/ldm/bin/pqinsert -p 'plot c %s "
+                 "gis/images/900913/mrms/%s.tif GIS/mrms/%s_%s.tif tif' %s.tif"
+                 "") % (now.strftime("%Y%m%d%H%M"), prefix, prefix,
+                        now.strftime("%Y%m%d%H%M"), tmpfn)
         subprocess.call(pqstr, shell=True)
-        
+
         j = open("%s.json" % (tmpfn,), 'w')
-        j.write( json.dumps(dict(meta=metadata)))
+        j.write(json.dumps(dict(meta=metadata)))
         j.close()
         # Insert into LDM
-        pqstr = "/home/ldm/bin/pqinsert -p 'plot c %s gis/images/4326/mrms/%s.json GIS/mrms/%s_%s.json json' %s.json" % (
-                        now.strftime("%Y%m%d%H%M"),prefix, prefix, now.strftime("%Y%m%d%H%M"), tmpfn )
+        pqstr = ("/home/ldm/bin/pqinsert -i -p 'plot c %s "
+                 "gis/images/4326/mrms/%s.json GIS/mrms/%s_%s.json json' "
+                 "%s.json") % (now.strftime("%Y%m%d%H%M"), prefix, prefix,
+                               now.strftime("%Y%m%d%H%M"), tmpfn)
         subprocess.call(pqstr, shell=True)
     for suffix in ['tif', 'json', 'png', 'wld']:
         if os.path.isfile("%s.%s" % (tmpfn, suffix)):
@@ -94,31 +99,30 @@ def do(now , realtime, delta):
     os.close(tmpfp)
     os.unlink(tmpfn)
 
+
 def main():
     """ Go Main Go """
     utcnow = datetime.datetime.utcnow().replace(tzinfo=pytz.timezone("UTC"))
     if len(sys.argv) == 6:
-        utcnow = datetime.datetime( int(sys.argv[1]),
-                                    int(sys.argv[2]),
-                                    int(sys.argv[3]),
-                                    int(sys.argv[4]),
-                                    int(sys.argv[5]) ).replace(
-                                                tzinfo=pytz.timezone("UTC"))
+        utcnow = datetime.datetime(int(sys.argv[1]), int(sys.argv[2]),
+                                   int(sys.argv[3]), int(sys.argv[4]),
+                                   int(sys.argv[5])
+                                   ).replace(tzinfo=pytz.timezone("UTC"))
         do(utcnow, False, 0)
     else:
-        #If our time is an odd time, run 5 minutes ago 
-        utcnow = utcnow.replace(second=0,microsecond=0)
+        # If our time is an odd time, run 5 minutes ago
+        utcnow = utcnow.replace(second=0, microsecond=0)
         if utcnow.minute % 2 != 1:
             return
         utcnow = utcnow - datetime.timedelta(minutes=5)
-        do( utcnow , True, 0)
+        do(utcnow, True, 0)
         # Also check old dates
         for delta in [30, 90, 1440, 2880]:
             ts = utcnow - datetime.timedelta(minutes=delta)
             fn = ts.strftime(("/mesonet/ARCHIVE/data/%Y/%m/%d/"
-                              +"GIS/mrms/a2m_%Y%m%d%H%M.png"))
+                              "GIS/mrms/a2m_%Y%m%d%H%M.png"))
             if not os.path.isfile(fn):
                 do(ts, False, delta)
-    
+
 if __name__ == '__main__':
     main()
