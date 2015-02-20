ecc256c6610e8b8268504e6d6d6a227c24b8d1bc
akrherz <akrherz@iastate.edu>
2015-02-20 09:31:16 -0600

    pep8 fixes and correct bug with grid subsample of MRMS data

--- scripts/iemre/merge_mrms_q3.py
+++ scripts/iemre/merge_mrms_q3.py
@@ -12,12 +12,13 @@ from pyiem import iemre
 import pygrib
 import gzip
 import tempfile
-#from pyiem.plot import MapPlot
+# from pyiem.plot import MapPlot
+
 
 def run(ts):
     """ Actually do the work, please """
-    nc = netCDF4.Dataset('/mesonet/data/iemre/%s_mw_mrms_daily.nc' % (
-                                                            ts.year,), 'a')
+    nc = netCDF4.Dataset(('/mesonet/data/iemre/%s_mw_mrms_daily.nc'
+                          '') % (ts.year,), 'a')
     offset = iemre.daily_offset(ts)
     ncprecip = nc.variables['p01d']
 
@@ -27,14 +28,14 @@ def run(ts):
     gmtts = ts.astimezone(pytz.timezone("UTC"))
 
     gribfn = gmtts.strftime(("/mnt/a4/data/%Y/%m/%d/mrms/ncep/"
-            +"RadarOnly_QPE_24H/"
-            +"RadarOnly_QPE_24H_00.00_%Y%m%d-%H%M00.grib2.gz"))
+                             "RadarOnly_QPE_24H/"
+                             "RadarOnly_QPE_24H_00.00_%Y%m%d-%H%M00.grib2.gz"))
     if not os.path.isfile(gribfn):
         print("merge_mrms_q3.py MISSING %s" % (gribfn,))
         return
 
     fp = gzip.GzipFile(gribfn, 'rb')
-    (tmpfp, tmpfn) = tempfile.mkstemp()
+    (_, tmpfn) = tempfile.mkstemp()
     tmpfp = open(tmpfn, 'wb')
     tmpfp.write(fp.read())
     tmpfp.close()
@@ -42,35 +43,36 @@ def run(ts):
     grb = grbs[1]
     os.unlink(tmpfn)
 
-    mrms = grb['values']
+    val = grb['values']
     # Anything less than zero, we set to zero
-    mrms = np.where(mrms < 0, 0, mrms)
+    val = np.where(val < 0, 0, val)
 
     # Figure out what we wish to subsample
     y0 = int((iemre.SOUTH - mrms.SOUTH) * 100.0)
     y1 = int((iemre.NORTH - mrms.SOUTH) * 100.0)
     x0 = int((iemre.WEST - mrms.WEST) * 100.0)
     x1 = int((iemre.EAST - mrms.WEST) * 100.0)
-    #print 'y0:%s y1:%s x0:%s x1:%s' % (y0, y1, x0, x1)
-    ncprecip[offset, :, :] = mrms[y0:y1, x0:x1]
-    #m = MapPlot(sector='midwest')
-    #x, y = np.meshgrid(nc.variables['lon'][:], nc.variables['lat'][:])
-    #m.pcolormesh(x, y, ncprecip[offset,:,:], range(10), latlon=True)
-    #m.postprocess(filename='test.png')
-    #(fig, ax) = plt.subplots()
-    #ax.imshow(mrms)
-    #fig.savefig('test.png')
-    #(fig, ax) = plt.subplots()
-    #ax.imshow(mrms[y0:y1,x0:x1])
-    #fig.savefig('test2.png')
+    # print 'y0:%s y1:%s x0:%s x1:%s' % (y0, y1, x0, x1)
+    ncprecip[offset, :, :] = val[y0:y1, x0:x1]
+    # m = MapPlot(sector='midwest')
+    # x, y = np.meshgrid(nc.variables['lon'][:], nc.variables['lat'][:])
+    # m.pcolormesh(x, y, ncprecip[offset,:,:], range(10), latlon=True)
+    # m.postprocess(filename='test.png')
+    # (fig, ax) = plt.subplots()
+    # ax.imshow(mrms)
+    # fig.savefig('test.png')
+    # (fig, ax) = plt.subplots()
+    # ax.imshow(mrms[y0:y1,x0:x1])
+    # fig.savefig('test2.png')
     nc.close()
+
 
 def main():
     """ go main go """
     if len(sys.argv) == 4:
         # 12 noon to prevent ugliness with timezones
         ts = datetime.datetime(int(sys.argv[1]), int(sys.argv[2]),
-                                int(sys.argv[3]), 12, 0)
+                               int(sys.argv[3]), 12, 0)
     else:
         ts = datetime.datetime.now() - datetime.timedelta(hours=24)
         ts = ts.replace(hour=12)
