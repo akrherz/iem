c933e2cf65708e734f2b3fb3b3a77d9e1c30217d
akrherz <akrherz@iastate.edu>
2015-02-23 15:02:56 -0600

    don't update station metadata when STS is already right

--- scripts/dbutil/compute_asos_sts.py
+++ scripts/dbutil/compute_asos_sts.py
@@ -23,41 +23,42 @@ net = sys.argv[1]
 table = NetworkTable(net)
 keys = table.sts.keys()
 if len(keys) > 1:
-    ids = `tuple(keys)`
+    ids = repr(tuple(keys))
 else:
     ids = "('%s')" % (keys[0],)
 
-acursor.execute("""SELECT station, min(valid), max(valid) from alldata 
-  WHERE station in %s GROUP by station 
-  ORDER by min ASC""" % (ids,))
+acursor.execute("""SELECT station, min(valid), max(valid) from alldata
+    WHERE station in %s GROUP by station
+    ORDER by min ASC""" % (ids,))
 for row in acursor:
     station = row[0]
     if table.sts[station]['archive_begin'] != row[1]:
-        print 'Updated %s STS WAS: %s NOW: %s' % (station, 
-                    table.sts[station]['archive_begin'], row[1])
-  
-    mcursor.execute("""UPDATE stations SET archive_begin = %s 
-         WHERE id = %s and network = %s""" , (row[1], station, net) )
-    if mcursor.rowcount == 0:
-        print 'ERROR: No rows updated'
-  
+        print(('Updated %s STS WAS: %s NOW: %s'
+               '') % (station, table.sts[station]['archive_begin'], row[1]))
+
+        mcursor.execute("""UPDATE stations SET archive_begin = %s
+             WHERE id = %s and network = %s""", (row[1], station, net))
+        if mcursor.rowcount == 0:
+            print 'ERROR: No rows updated'
+
     # Site without data in past year is offline!
-    if (basets - row[2]).days  > 365:
+    if (basets - row[2]).days > 365:
         if table.sts[station]['archive_end'] != row[2]:
-            print 'Updated %s ETS WAS: %s NOW: %s' % (station, 
-                    table.sts[station]['archive_end'], row[2])
-  
-            mcursor.execute("""UPDATE stations SET archive_end = %s 
-                 WHERE id = %s and network = %s""" , (row[2], station, net) )
+            print(('Updated %s ETS WAS: %s NOW: %s'
+                   '') % (station, table.sts[station]['archive_end'], row[2]))
+
+            mcursor.execute("""UPDATE stations SET archive_end = %s
+                 WHERE id = %s and network = %s""", (row[2], station, net))
+
     # If it was offline and now is on, correct this
-    if ((basets - row[2]).days  < 365 and 
-        table.sts[station]['archive_end'] is not None):
-        print 'Updated %s ETS WAS: %s NOW: None' % (station, 
-                    table.sts[station]['archive_end'])
-  
-        mcursor.execute("""UPDATE stations SET archive_end = null 
-                 WHERE id = %s and network = %s""" , ( station, net) )
-    
+    if ((basets - row[2]).days < 365 and
+            table.sts[station]['archive_end'] is not None):
+        print(('Updated %s ETS WAS: %s NOW: None'
+               '') % (station, table.sts[station]['archive_end']))
+
+        mcursor.execute("""UPDATE stations SET archive_end = null
+                 WHERE id = %s and network = %s""", (station, net))
+
 mcursor.close()
 mesosite.commit()
 mesosite.close()
