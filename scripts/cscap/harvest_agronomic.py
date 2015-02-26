704a2ca22275385c293a3d343b82f0986e3da7a3
akrherz <akrherz@iastate.edu>
2015-02-26 16:58:37 -0600

    [CSCAP] implement deletion of entries no longer in upstream agronomic spreadsheet

--- scripts/cscap/harvest_agronomic.py
+++ scripts/cscap/harvest_agronomic.py
@@ -20,7 +20,7 @@ pcursor = pgconn.cursor()
 spr_client = util.get_spreadsheet_client(config)
 docs_client = util.get_docs_client(config)
 
-query = gdata.docs.client.DocsQuery(show_collections='false', 
+query = gdata.docs.client.DocsQuery(show_collections='false',
                                     title='Agronomic Data')
 feed = docs_client.GetAllResources(query=query)
 
@@ -36,6 +36,15 @@ for entry in feed:
     worksheet.get_cell_feed()
     siteid = spreadsheet.title.split()[0]
     newvals = 0
+
+    # Load up current data, incase we need to do some deleting
+    current = {}
+    pcursor.execute("""SELECT plotid, varname
+    from agronomic_data WHERE site = %s and year = %s""", (siteid, YEAR))
+    for row in pcursor:
+        key = "%s|%s" % row
+        current[key] = True
+    found_vars = []
     for col in range(1, worksheet.cols+1):
         val = worksheet.get_cell_value(1, col)
         if val is None:
@@ -45,14 +54,17 @@ for entry in feed:
         if val.find("AGR") != 0:
             continue
         varname = val
-        for row in range(4,worksheet.rows+1):
+        if varname not in found_vars:
+            found_vars.append(varname)
+        for row in range(4, worksheet.rows+1):
             plotid = worksheet.get_cell_value(row, plotidcol)
             if plotid is None:
                 continue
             val = worksheet.get_cell_value(row, col)
             try:
                 pcursor.execute("""
-                    INSERT into agronomic_data(site, plotid, varname, year, value)
+                    INSERT into agronomic_data
+                    (site, plotid, varname, year, value)
                     values (%s, %s, %s, %s, %s) RETURNING value
                     """, (siteid, plotid, varname, YEAR, val))
                 if pcursor.rowcount == 1:
@@ -60,12 +72,23 @@ for entry in feed:
             except Exception, exp:
                 print 'HARVEST_AGRONOMIC TRACEBACK'
                 print exp
-                print '%s %s %s %s %s' % (YEAR, siteid, plotid, repr(varname), 
+                print '%s %s %s %s %s' % (YEAR, siteid, plotid, repr(varname),
                                           repr(val))
                 sys.exit()
+            key = "%s|%s" % (plotid, varname)
+            if key in current:
+                del(current[key])
+    for key in current:
+        (plotid, varname) = key.split("|")
+        if varname in found_vars:
+            print 'harvest_agronomic REMOVE %s %s %s' % (siteid, plotid,
+                                                         varname)
+            pcursor.execute("""DELETE from agronomic_data where site = %s and
+            plotid = %s and varname = %s and year = %s
+            """, (siteid, plotid, varname, YEAR))
     if newvals > 0:
-        print 'harvest_agronomic year: %s site: %s had %s new values' % (YEAR,
-                                            siteid, newvals)
+        print(('harvest_agronomic year: %s site: %s had %s new values'
+               '') % (YEAR, siteid, newvals))
 
 pcursor.close()
 pgconn.commit()
