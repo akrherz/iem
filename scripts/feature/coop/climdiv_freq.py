f16f99515bc2db2025570739ef4c91432b48b3a7
akrherz <akrherz@iastate.edu>
2015-03-01 21:35:55 -0600

    2 March feature is Feb climate district temp ranks

--- scripts/feature/coop/climdiv_freq.py
+++ scripts/feature/coop/climdiv_freq.py
@@ -9,20 +9,24 @@ pgconn = psycopg2.connect(database='coop', host='iemdb', user='nobody')
 cursor = pgconn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
 cursor.execute("""
-  WITH yearmax as (
-    SELECT station, max(high),
-    extract(year from day + '35 days'::interval) as yr from alldata
-    WHERE substr(station,3,1) = 'C' and month in (12,1,2) GROUP by station, yr
+  with monthly as (
+   SELECT station, year, avg((high+low)/2.) from alldata
+   where month = 2 and substr(station,3,1) = 'C' GROUP by station, year
+   ),
+   nextup as (
+  SELECT station, year, rank() OVER (PARTITION by station ORDER by avg ASC)
+  from monthly
   )
-  SELECT station, sum(case when max < 50 then 1 else 0 end), count(*)
-  from yearmax GROUP by station""")
+
+  SELECT station, rank from nextup where year = 2015
+  """)
 data = {}
 for row in cursor:
-    data[row['station']] = row['sum'] / float(row['count']) * 100.
+    data[row['station']] = row['rank']
 
 m = MapPlot(sector='midwest', axisbg='white',
-            title='Percentage of Winters Seasons Where High failed to Reach 50$^\circ$F',
-            subtitle='Max Daily High Over Dec,Jan,Feb By Climate District; Based on IEM Estimates (1893-2014)')
+            title='February Average Temperature Rank (1 is coldest)',
+            subtitle='Based on IEM Estimates of Climate District Data (1893-2015)')
 cmap = cm.get_cmap("BrBG_r")
 cmap.set_under('black')
 cmap.set_over('black')
