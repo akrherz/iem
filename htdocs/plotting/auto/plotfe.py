711de2b9f63920065c29be6b61dcec2324c72424
akrherz <akrherz@iastate.edu>
2015-03-17 15:31:43 -0500

    cleanups and lazy import matplotlib to make memcached faster

--- htdocs/plotting/auto/plotfe.py
+++ htdocs/plotting/auto/plotfe.py
@@ -1,18 +1,13 @@
 #!/usr/bin/env python
 
 import cgi
-import cStringIO
+import memcache
 import sys
 import os
-import imp
-import memcache
-import datetime
-import matplotlib
-matplotlib.use('agg')
-import matplotlib.pyplot as plt
+
 
 def parser(cgistr):
-    """ Convert a CGI string into a dict that gets passed to the plotting 
+    """ Convert a CGI string into a dict that gets passed to the plotting
     routine """
     d = dict()
     for token in cgistr.split("::"):
@@ -20,13 +15,15 @@ def parser(cgistr):
         if len(token2) != 2:
             continue
         d[token2[0]] = token2[1]
-    
+
     return d
 
-if __name__ == '__main__':
+
+def main():
+    """See how we are called"""
     form = cgi.FieldStorage()
     q = form.getfirst('q', "")
-    fdict = parser( q )
+    fdict = parser(q)
     p = int(form.getfirst('p', 0))
     dpi = int(fdict.get('dpi', 100))
 
@@ -36,19 +33,30 @@ if __name__ == '__main__':
     sys.stdout.write("Content-type: image/png\n\n")
     hostname = os.environ.get("SERVER_NAME", "")
     if not res or hostname == "iem.local":
+        # Lazy import to help speed things up
+        import matplotlib
+        matplotlib.use('agg')
+        import matplotlib.pyplot as plt
+        import cStringIO
+        import datetime
+        import imp
         name = 'scripts/p%s' % (p,)
         fp, pathname, description = imp.find_module(name)
         a = imp.load_module(name, fp, pathname, description)
         meta = a.get_description()
         fig = a.plotter(fdict)
         # Place timestamp on the image
-        fig.text(0.01, 0.01, 'Plot Generated: %s' % (
-                datetime.datetime.now().strftime("%-d %b %Y %-I:%M %p"),), 
-                 va='bottom', ha='left', fontsize=10)
+        fig.text(0.01, 0.01, ('Plot Generated: %s'
+                              ) % (
+            datetime.datetime.now().strftime("%-d %b %Y %-I:%M %p"),),
+            va='bottom', ha='left', fontsize=10)
         ram = cStringIO.StringIO()
         plt.savefig(ram, format='png', dpi=dpi)
         ram.seek(0)
         res = ram.read()
-        sys.stderr.write("Setting cache: %s" % (mckey,) )
+        sys.stderr.write("Setting cache: %s" % (mckey,))
         mc.set(mckey, res, meta.get('cache', 43200))
-    sys.stdout.write( res )
+    sys.stdout.write(res)
+
+if __name__ == '__main__':
+    main()
