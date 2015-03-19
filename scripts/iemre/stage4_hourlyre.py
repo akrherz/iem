7691a698d73df040928dcfe3d6d7e616d5e1c2da
akrherz <akrherz@iastate.edu>
2015-03-19 13:32:27 -0500

    migrate from Nio to pygrib for stage IV regridder

--- scripts/iemre/stage4_hourlyre.py
+++ scripts/iemre/stage4_hourlyre.py
@@ -1,63 +1,66 @@
 # Need something to push the stage4 data into the hourly files
 
-import Nio
+import pygrib
 import datetime
-import Ngl
-import numpy
+import numpy as np
 from pyiem import iemre
 import os
 import sys
 import pytz
 import netCDF4
+from scipy.interpolate import NearestNDInterpolator
+
 
 def merge(ts):
     """
     Process an hour's worth of stage4 data into the hourly RE
     """
 
-    fp = "/mesonet/ARCHIVE/data/%s/stage4/ST4.%s.01h.grib" % (
-      ts.strftime("%Y/%m/%d"), ts.strftime("%Y%m%d%H") )
-    if os.path.isfile(fp):
-        grib = Nio.open_file(fp, 'r')
+    fn = ("/mesonet/ARCHIVE/data/%s/stage4/ST4.%s.01h.grib"
+          ) % (ts.strftime("%Y/%m/%d"), ts.strftime("%Y%m%d%H"))
+    if os.path.isfile(fn):
+        gribs = pygrib.open(fn)
+        grb = gribs[1]
+        val = grb.values
+        lats, lons = grb.latlons()
         # Rough subsample, since the whole enchillata is too much
-        lats = numpy.ravel( grib.variables["g5_lat_0"][200:-100:5,300:900:5] )
-        lons = numpy.ravel( grib.variables["g5_lon_1"][200:-100:5,300:900:5] )
-        vals = numpy.ravel( grib.variables["A_PCP_GDS5_SFC_acc1h"][200:-100:5,300:900:5] )
+        lats = np.ravel(lats[200:-100:5, 300:900:5])
+        lons = np.ravel(lons[200:-100:5, 300:900:5])
+        vals = np.ravel(val[200:-100:5, 300:900:5])
         # Clip large values
-        vals = numpy.where( vals > 250., 0, vals)
-        #print 'STAGE4 MIN: %5.2f AVG: %5.2f MAX: %5.2f' % (numpy.min(vals), numpy.average(vals),
-        #                                           numpy.max(vals))
-        res = Ngl.natgrid(lons, lats, vals, iemre.XAXIS, iemre.YAXIS)
-        grib.close()
-        del grib
+        vals = np.where(vals > 250., 0, vals)
+        nn = NearestNDInterpolator((lons, lats), vals)
+        xi, yi = np.meshgrid(iemre.XAXIS, iemre.YAXIS)
+        res = nn(xi, yi)
     else:
-        print 'Missing stage4 %s' % (fp,)
-        res = numpy.zeros( (iemre.NX, iemre.NY))
+        print 'Missing stage4 %s' % (fn,)
+        res = np.zeros((iemre.NX, iemre.NY))
 
     # Lets clip bad data
-    res = numpy.where(res < 0, 0., res)
+    res = np.where(res < 0, 0., res)
     # 10 inches per hour is bad data
-    res = numpy.where(res > 250., 0., res)
+    res = np.where(res > 250., 0., res)
 
-    # Print out some debugging information for now
-    #print '%s MIN: %5.2f AVG: %5.2f MAX: %5.2f' % (ts, numpy.min(res), numpy.average(res),
-    #                                               numpy.max(res))
     # Open up our RE file
-    nc = netCDF4.Dataset("/mesonet/data/iemre/%s_mw_hourly.nc" % (
-                                                            ts.year,),'a')
+    nc = netCDF4.Dataset("/mesonet/data/iemre/%s_mw_hourly.nc" % (ts.year,),
+                         'a')
     offset = iemre.hourly_offset(ts)
-    nc.variables["p01m"][offset,:,:] = res.transpose()
+    nc.variables["p01m"][offset, :, :] = res
 
     nc.close()
     del nc
 
-if __name__ == "__main__":
+
+def main():
+    """Go Main"""
     if len(sys.argv) == 5:
-        ts = datetime.datetime( int(sys.argv[1]),int(sys.argv[2]),
-                           int(sys.argv[3]), int(sys.argv[4]) )
+        ts = datetime.datetime(int(sys.argv[1]), int(sys.argv[2]),
+                               int(sys.argv[3]), int(sys.argv[4]))
     else:
-        ts = datetime.datetime.utcnow() 
-        ts = ts.replace(minute=0,second=0, microsecond=0)
+        ts = datetime.datetime.utcnow()
+        ts = ts.replace(minute=0, second=0, microsecond=0)
     ts = ts.replace(tzinfo=pytz.timezone("UTC"))
     merge(ts)
 
+if __name__ == "__main__":
+    main()
