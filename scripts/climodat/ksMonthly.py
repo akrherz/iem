3de160ae75c405d0f83a008d97bcb3f64e1f868d
akrherz <akrherz@iastate.edu>
2015-03-20 08:51:52 -0500

    pep8 cleanups and cast Decimal to float for climodat dumper

--- scripts/climodat/ksMonthly.py
+++ scripts/climodat/ksMonthly.py
@@ -6,22 +6,25 @@ import sys
 import datetime
 import numpy as np
 
+
 def setup_csv(yr):
     """ Setup the output file """
     out = open("/mesonet/share/climodat/ks/%s_monthly.csv" % (yr,), 'w')
     out.write("stationID,stationName,Latitude,Longitude,")
-    for i in range(1,13):
-        for v in ["MINT","MAXT","PREC"]:
-            out.write("%02i_%s,C%02i_%s," % (i,v,i,v) )
-    out.write("%i_MINT,CYR_MINT,%i_MAXT,CYR_MAXT,%i_PREC,CYR_PREC,\n" % (yr,
-                                                                yr, yr))
+    for i in range(1, 13):
+        for v in ["MINT", "MAXT", "PREC"]:
+            out.write("%02i_%s,C%02i_%s," % (i, v, i, v))
+    out.write(("%i_MINT,CYR_MINT,%i_MAXT,CYR_MAXT,%i_PREC,CYR_PREC,\n"
+               ) % (yr, yr, yr))
     return out
 
-def metadata(sid,csv):
+
+def metadata(sid, csv):
     """ write metadata to csv file """
-    csv.write("%s,%s,%s,%s," % (sid, constants.nt.sts[sid]["name"], 
-                                constants.nt.sts[sid]["lat"], 
+    csv.write("%s,%s,%s,%s," % (sid, constants.nt.sts[sid]["name"],
+                                constants.nt.sts[sid]["lat"],
                                 constants.nt.sts[sid]["lon"]))
+
 
 def process(sid, csv, yr):
     """ Actually process a station for a csv file and year """
@@ -31,14 +34,14 @@ def process(sid, csv, yr):
     oh = []
     ol = []
     op = []
-    for i in range(1,13):
+    for i in range(1, 13):
         sql = """
         WITH yearly as (
-            SELECT year, avg(high) as ah, avg(low) as al, 
+            SELECT year, avg(high) as ah, avg(low) as al,
             sum(precip) as sp from %s WHERE station = '%s' and month = %s
             GROUP by year)
 
-        SELECT 
+        SELECT
         avg(ah) as avg_high,
         avg(al) as avg_low,
         avg(sp) as avg_rain,
@@ -51,20 +54,20 @@ def process(sid, csv, yr):
         avgHigh = rs[0]["avg_high"]
         avgLow = rs[0]["avg_low"]
         avgRain = rs[0]["avg_rain"]
-        ah.append( avgHigh )
-        al.append( avgLow )
-        ap.append( avgRain )
-        
+        ah.append(float(avgHigh))
+        al.append(float(avgLow))
+        ap.append(float(avgRain))
+
         obHigh = rs[0]["ob_high"]
         obLow = rs[0]["ob_low"]
         obRain = rs[0]["ob_rain"]
         if obHigh is not None:
-            oh.append( obHigh )
-            ol.append( obLow )
-            op.append( obRain )
+            oh.append(float(obHigh))
+            ol.append(float(obLow))
+            op.append(float(obRain))
 
-        csv.write("%s,%s,%s,%s,%s,%s," % (obLow or 'M', avgLow, 
-                                          obHigh or 'M', avgHigh, 
+        csv.write("%s,%s,%s,%s,%s,%s," % (obLow or 'M', avgLow,
+                                          obHigh or 'M', avgHigh,
                                           obRain or 'M', avgRain))
 
     low = np.average(ol)
@@ -74,16 +77,17 @@ def process(sid, csv, yr):
     avg_high = np.average(ah)
     avg_rain = np.sum(ap)
     csv.write("%s,%s,%s,%s,%s,%s," % (low, avg_low, high, avg_high, rain,
-                                      avg_rain) )
+                                      avg_rain))
 
     csv.write("\n")
     csv.flush()
+
 
 def main(yr):
     """ main ! """
     csv = setup_csv(yr)
     for sid in constants.nt.sts.keys():
-        #print "%s processing [%s] %s" % (yr, id, nt.sts[id]["name"])
+        # print "%s processing [%s] %s" % (yr, id, nt.sts[id]["name"])
         metadata(sid, csv)
         process(sid, csv, yr)
 
@@ -95,5 +99,5 @@ if __name__ == "__main__":
         yr = datetime.datetime.now().year
     main(yr)
     main(yr-1)
-    #for yr in range(1893,1951):
+    # for yr in range(1893,1951):
     #  main(yr)
