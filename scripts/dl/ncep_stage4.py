aed4a9260a33dbfe88ec55dcc62b523387015a9d
akrherz <akrherz@iastate.edu>
2015-03-20 10:01:31 -0500

    use HTTP to download NCEP stageIV data

--- scripts/dl/ncep_stage4.py
+++ scripts/dl/ncep_stage4.py
@@ -8,75 +8,77 @@ import urllib2
 import os
 import subprocess
 
-def download( now ):
+
+def download(now):
     """
     Download a given timestamp from NCEP and inject into LDM
     Example:  ftp://ftpprd.ncep.noaa.gov/pub/data/nccf/com/hourly/prod/
               nam_pcpn_anal.20090916/ST4.2009091618.01h.gz
     """
-    hours = [1,]
+    hours = [1, ]
     if now.hour % 6 == 0:
-        hours.append( 6 )
+        hours.append(6)
     if now.hour == 12 and offset != 0:
-        hours.append( 24 )
+        hours.append(24)
     for hr in hours:
-        url = "%s.%02ih.gz" % ( now.strftime("ftp://ftpprd.ncep.noaa.gov/pub/"+
-                                "data/nccf/com/hourly/prod/"+
-                                "nam_pcpn_anal.%Y%m%d/ST4.%Y%m%d%H"), hr)
+        url = "%s.%02ih.gz" % (now.strftime(("http://ftpprd.ncep.noaa.gov/"
+                                             "data/nccf/com/hourly/prod/"
+                                             "nam_pcpn_anal.%Y%m%d/"
+                                             "ST4.%Y%m%d%H")), hr)
         try:
-            data = urllib2.urlopen( url , timeout=60).read()
+            data = urllib2.urlopen(url, timeout=60).read()
         except Exception, exp:
             if hr < 6:
-                print "NCEP stage ST4 dl fail HR: %s TIME: %s %s [%s]" % (hr, 
-                                                now.strftime("%Y-%m-%d %H"),
-                                                url, exp)
+                print(("NCEP stage ST4 dl fail HR: %s TIME: %s %s [%s]"
+                       ) % (hr, now.strftime("%Y-%m-%d %H"), url, exp))
             continue
         # Same temp file
         o = open("tmp.grib.gz", 'wb')
-        o.write( data )
+        o.write(data)
         o.close()
         subprocess.call("gunzip -f tmp.grib.gz", shell=True)
         # Inject into LDM
         cmd = ("/home/ldm/bin/pqinsert -p 'data a %s blah "
-               +"stage4/ST4.%s.%02ih.grib grib' tmp.grib") % (
-                    now.strftime("%Y%m%d%H%M"), now.strftime("%Y%m%d%H"), hr)
-        subprocess.call( cmd, shell=True )
+               "stage4/ST4.%s.%02ih.grib grib' tmp.grib"
+               ) % (now.strftime("%Y%m%d%H%M"), now.strftime("%Y%m%d%H"), hr)
+        subprocess.call(cmd, shell=True)
         os.remove('tmp.grib')
-        
+
         # Do stage2 ml now
         if hr == 1:
-            url = "%s.Grb.gz" % ( now.strftime("ftp://ftpprd.ncep.noaa.gov"+
-                                "/pub/data/nccf/com/hourly/prod/"+
-                                "nam_pcpn_anal.%Y%m%d/ST2ml%Y%m%d%H"), )
+            url = "%s.Grb.gz" % (now.strftime(("http://ftpprd.ncep.noaa.gov"
+                                               "/data/nccf/com/hourly/prod/"
+                                               "nam_pcpn_anal.%Y%m%d/"
+                                               "ST2ml%Y%m%d%H")), )
         else:
-            url = "%s.%02ih.gz" % ( now.strftime("ftp://ftpprd.ncep.noaa.gov/"+
-                                "pub/data/nccf/com/hourly/prod/"+
-                                "nam_pcpn_anal.%Y%m%d/ST2ml%Y%m%d%H"), hr)
+            url = "%s.%02ih.gz" % (now.strftime(("http://ftpprd.ncep.noaa.gov/"
+                                                 "data/nccf/com/hourly/prod/"
+                                                 "nam_pcpn_anal.%Y%m%d/"
+                                                 "ST2ml%Y%m%d%H")), hr)
         try:
-            data = urllib2.urlopen( url , timeout=60).read()
+            data = urllib2.urlopen(url, timeout=60).read()
         except Exception, exp:
             if hr < 6:
-                print "NCEP stage 2ml dl fail HR: %s TIME: %s %s [%s]" % (hr, 
-                                                now.strftime("%Y-%m-%d %H"),
-                                                url, exp)
+                print(("NCEP stage 2ml dl fail HR: %s TIME: %s %s [%s]"
+                       ) % (hr, now.strftime("%Y-%m-%d %H"), url, exp))
             continue
         # Same temp file
         o = open("tmp.grib.gz", 'wb')
-        o.write( data )
+        o.write(data)
         o.close()
         subprocess.call("gunzip -f tmp.grib.gz", shell=True)
         # Inject into LDM
         cmd = ("/home/ldm/bin/pqinsert -p 'data a %s blah "
-               +"stage4/ST2ml.%s.%02ih.grib grib' tmp.grib") % (
-                    now.strftime("%Y%m%d%H%M"), now.strftime("%Y%m%d%H"), hr)
-        subprocess.call( cmd, shell=True )
-        os.remove('tmp.grib')        
+               "stage4/ST2ml.%s.%02ih.grib grib' tmp.grib"
+               ) % (now.strftime("%Y%m%d%H%M"), now.strftime("%Y%m%d%H"), hr)
+        subprocess.call(cmd, shell=True)
+        os.remove('tmp.grib')
 
 if __name__ == "__main__":
     # We want this hour GMT
     utc = datetime.datetime.utcnow()
     utc = utc.replace(tzinfo=pytz.timezone("UTC"), minute=0, second=0,
                       microsecond=0)
-    for offset in [33,9,3,0]:
+    for offset in [33, 9, 3, 0]:
         now = utc - datetime.timedelta(hours=offset)
-        download( now )
+        download(now)
