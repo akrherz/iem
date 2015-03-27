07dbd8b08213b74954c8e81def2a3322f349a0f1
akrherz <akrherz@iastate.edu>
2015-03-27 15:20:27 -0500

    Add util script to repair a color table issue found in N0R composites
    Will repair archive from 1730 19 Mar 2015 -> 1750 27 Mar 2015

---
+++ scripts/util/repair_colortable.py
@@ -0,0 +1,64 @@
+"""Need to fix some corrupted n0r NEXRAD color tables!"""
+import datetime
+import osgeo.gdal as gdal
+from osgeo import gdalconst
+import subprocess
+import psycopg2
+import os
+
+PGCONN = psycopg2.connect(database='mesosite', host='iemdb', user='nobody')
+CURSOR = PGCONN.cursor()
+
+
+def get_colortable(prod):
+    """Get the color table for this prod
+
+    Args:
+      prod (str): product to get the table for
+
+    Returns:
+      colortable
+
+    """
+    CURSOR.execute("""
+    select r,g,b from iemrasters_lookup l JOIN iemrasters r on
+    (r.id = l.iemraster_id) WHERE r.name = %s ORDER by l.coloridx ASC
+    """, ("composite_"+prod,))
+    ct = gdal.ColorTable()
+    for i, row in enumerate(CURSOR):
+        ct.SetColorEntry(i, (row[0], row[1], row[2], 255))
+    return ct
+
+
+n0rct = get_colortable('n0r')
+
+
+def do(ts):
+    """Process!"""
+    fn = ts.strftime("/mesonet/ARCHIVE/data/%Y/%m/%d/GIS/uscomp/n0r_%Y%m%d%H%M.png")
+    print ts.strftime("%Y%m%d %H%M")
+    n0r = gdal.Open(fn)
+    n0rd = n0r.ReadAsArray() + 6.
+    n0rd[0, :6] = range(6)
+
+    out_driver = gdal.GetDriverByName("gtiff")
+    outdataset = out_driver.Create('tmp.tiff', n0r.RasterXSize,
+                                   n0r.RasterYSize, n0r.RasterCount,
+                                   gdalconst.GDT_Byte)
+    # Set output color table to match input
+    outdataset.GetRasterBand(1).SetRasterColorTable(n0rct)
+    outdataset.GetRasterBand(1).WriteArray(n0rd)
+    del outdataset
+    subprocess.call("convert -define PNG:preserve-colormap tmp.tiff tmp.png",
+                    shell=True)
+
+    os.unlink("tmp.tiff")
+    subprocess.call("mv tmp.png %s" % (fn,), shell=True)
+
+sts = datetime.datetime(2015, 3, 19, 17, 30)
+ets = datetime.datetime(2015, 3, 27, 17, 50)
+interval = datetime.timedelta(minutes=5)
+now = sts
+while now < ets:
+    do(now)
+    now += interval
