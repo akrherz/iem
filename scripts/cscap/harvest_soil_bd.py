146abe9c0d032a766642175dc9604c8b89105303
akrherz <akrherz@iastate.edu>
2015-03-31 08:21:14 -0500

    [CSCAP] support DPAC soil BD+WR reality for their location samples
    Load up the location ID into the subsample ID.  This is a hack for now.

--- scripts/cscap/harvest_soil_bd.py
+++ scripts/cscap/harvest_soil_bd.py
@@ -18,8 +18,9 @@ pcursor = pgconn.cursor()
 spr_client = util.get_spreadsheet_client(config)
 docs_client = util.get_docs_client(config)
 
-query = gdata.docs.client.DocsQuery(show_collections='false', 
-                                    title='Soil Bulk Density and Water Retention Data')
+query = gdata.docs.client.DocsQuery(show_collections='false',
+                                    title=('Soil Bulk Density and '
+                                           'Water Retention Data'))
 feed = docs_client.GetAllResources(query=query)
 
 for entry in feed:
@@ -30,23 +31,21 @@ for entry in feed:
     siteid = spreadsheet.title.split()[0]
     worksheet = spreadsheet.worksheets.get(YEAR)
     if worksheet is None:
-        print 'Missing %s sheet for %s' % (YEAR, siteid)
+        print 'Missing Soil BD+WR %s sheet for %s' % (YEAR, siteid)
         continue
     worksheet.get_cell_feed()
-    #if siteid == 'DPAC':
-    #    print 'ERROR: Skipping DPAC Soil Texture sheet as it has subsamples'
-    #    continue
-    #print 'Processing %s Soil BD & Water Retention Year %s' % (siteid, YEAR),
-    if (worksheet.get_cell_value(1, 1) != 'plotid' or
-        worksheet.get_cell_value(1, 2) != 'depth' or
-        worksheet.get_cell_value(1, 3) != 'subsample'):
+    if siteid == 'DPAC':
+        pass
+    elif (worksheet.get_cell_value(1, 1) != 'plotid' or
+            worksheet.get_cell_value(1, 2) != 'depth' or
+            worksheet.get_cell_value(1, 3) != 'subsample'):
         print 'FATAL site: %s(%s) bd & wr has bad header 1:%s 2:%s 3:%s' % (
-            siteid, YEAR, worksheet.get_cell_value(1, 1), 
-            worksheet.get_cell_value(1, 2), 
+            siteid, YEAR, worksheet.get_cell_value(1, 1),
+            worksheet.get_cell_value(1, 2),
             worksheet.get_cell_value(1, 3))
         continue
-    
-    #Load up current data, incase we need to do some deleting
+
+    # Load up current data, incase we need to do some deleting
     current = {}
     pcursor.execute("""SELECT plotid, varname, depth, subsample
     from soil_data WHERE site = %s and year = %s""", (siteid, YEAR))
@@ -54,48 +53,55 @@ for entry in feed:
         key = "%s|%s|%s|%s" % row
         current[key] = True
     found_vars = []
-    for row in range(4,worksheet.rows+1):
+    for row in range(4, worksheet.rows+1):
         plotid = worksheet.get_cell_value(row, 1)
-        depth = worksheet.get_cell_value(row, 2)
-        subsample = worksheet.get_cell_value(row, 3)
+        if siteid == 'DPAC':
+            depth = worksheet.get_cell_value(row, 3)
+            # Combine the location value into the subsample
+            subsample = '%s%s' % (worksheet.get_cell_value(row, 2),
+                                  worksheet.get_cell_value(row, 4))
+        else:
+            depth = worksheet.get_cell_value(row, 2)
+            subsample = worksheet.get_cell_value(row, 3)
         if plotid is None or depth is None:
             continue
         for col in range(4, worksheet.cols+1):
-            varname = worksheet.get_cell_value(1,col).strip().split()[0]
+            varname = worksheet.get_cell_value(1, col).strip().split()[0]
             if varname[:4] != 'SOIL':
-                #print 'Invalid varname: %s site: %s year: %s' % (
+                # print 'Invalid varname: %s site: %s year: %s' % (
                 #                    worksheet.get_cell_value(1,col).strip(),
                 #                    siteid, YEAR)
                 continue
-            if not varname in found_vars:
+            if varname not in found_vars:
                 found_vars.append(varname)
             val = worksheet.get_cell_value(row, col)
             try:
                 pcursor.execute("""
-                    INSERT into soil_data(site, plotid, varname, year, 
+                    INSERT into soil_data(site, plotid, varname, year,
                     depth, value, subsample)
                     values (%s, %s, %s, %s, %s, %s, %s)
-                    """, (siteid, plotid, varname, YEAR, depth, val, subsample))
+                    """, (siteid, plotid, varname, YEAR, depth, val,
+                          subsample))
             except Exception, exp:
                 print 'HARVEST_SOIL_BD TRACEBACK'
                 print exp
-                print '%s %s %s %s %s %s' % (siteid, plotid, varname, depth, 
+                print '%s %s %s %s %s %s' % (siteid, plotid, varname, depth,
                                              val, subsample)
                 sys.exit()
             key = "%s|%s|%s|%s" % (plotid, varname, depth, subsample)
-            if current.has_key(key):
+            if key in current:
                 del(current[key])
     for key in current:
         (plotid, varname, depth, subsample) = key.split("|")
         if varname in found_vars:
-            print 'harvest_soil_bd REMOVE %s %s %s %s %s' % (siteid, plotid, 
-                                                    varname, depth, subsample)
-            pcursor.execute("""DELETE from soil_data where site = %s and 
+            print(('harvest_soil_bd rm %s %s %s %s %s %s'
+                   ) % (YEAR, siteid, plotid, varname, depth, subsample))
+            pcursor.execute("""DELETE from soil_data where site = %s and
             plotid = %s and varname = %s and year = %s and depth = %s and
             subsample = %s""", (siteid, plotid, varname, YEAR, depth,
                                 subsample))
 
-    #print "...done"
+
 pcursor.close()
 pgconn.commit()
 pgconn.close()
