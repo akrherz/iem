9addba88155250d1ec4f1fbaeff4c337a52ca3f0
akrherz <akrherz@iastate.edu>
2015-04-07 12:31:59 -0500

    some non-func cleanups to schoolnet time checker

--- scripts/snet/time_check/complain.py
+++ scripts/snet/time_check/complain.py
@@ -4,36 +4,37 @@
 from pyiem.network import Table as NetworkTable
 nt = NetworkTable("KCCI")
 import psycopg2
-portfolio = psycopg2.connect('dbname=portfolio host=iemdb')
+portfolio = psycopg2.connect('dbname=portfolio host=iemdb user=nobody')
 pcursor = portfolio.cursor()
 
 
 lines = open("times.txt", 'r').readlines()
 
-print '%-5s %-30.30s %s' % ('ID', 'Location Name', 'Time Difference [secs]')
+print '%-5s %-35.35s %s' % ('ID', 'Location Name', 'Time Difference [secs]')
 emails = {}
 bad = 0
 for line in lines:
     t = line.split("|")
     sid = t[0]
-    if not nt.sts.has_key(sid):
+    if sid not in nt.sts:
         continue
     sname = t[1]
-    dur = float(t[2])
+    dur = int(t[2])
     if dur < 1000 and dur > -1000:
         continue
-    print "%s %-30.30s %s" % (sid, sname, dur)
-    sql = """SELECT email from iem_site_contacts WHERE portfolio = 'kccisnet' 
-         and s_mid = %s"""
-    args =  (sid, )
+    print "%s %-35.35s %s" % (sid, sname, dur)
+    sql = """
+        SELECT email from iem_site_contacts WHERE portfolio = 'kccisnet'
+        and s_mid = %s
+    """
+    args = (sid, )
     pcursor.execute(sql, args)
     for row in pcursor:
-        emails[ row[0] ] = 1
+        emails[row[0]] = 1
     bad += 1
 
 print
 print '%s sites with clocks out of bounds' % (bad,)
-print 
+print
 for k in emails.keys():
     print "%s," % (k,),
-
