583999df7d309633bd95cc21c7f3f1778f86969c
akrherz <akrherz@iastate.edu>
2015-04-16 07:40:25 -0500

    non-func cleanup of nexrad l3 attr nagios checker

--- nagios/nexrad_attr.py
+++ nagios/nexrad_attr.py
@@ -1,26 +1,24 @@
-"""
- Nagios check to make sure we have NEXRAD attribute data
-"""
+"""Nagios check to make sure we have NEXRAD attribute data"""
 import sys
-import os
 import psycopg2
 
 POSTGIS = psycopg2.connect(database='postgis', host='iemdb', user='nobody')
 pcursor = POSTGIS.cursor()
 
 pcursor.execute("""
- select count(*) from nexrad_attributes WHERE 
- valid > now() - '30 minutes'::interval
+    select count(*) from nexrad_attributes WHERE
+    valid > now() - '30 minutes'::interval
 """)
 row = pcursor.fetchone()
 count = row[0]
 
+msg = "L3 NEXRAD attr count %s" % (count, )
 if count > 2:
-    print 'OK - attrs %s |count=%s;2;1;0' % (count, count)
+    print 'OK - %s |count=%s;2;1;0' % (msg, count)
     sys.exit(0)
 elif count > 1:
-    print 'OK - attrs %s |count=%s;2;1;0' % (count, count)
+    print 'OK - %s |count=%s;2;1;0' % (msg, count)
     sys.exit(1)
 else:
-    print 'CRITICAL - attrs %s |count=%s;2;1;0' % (count, count)
+    print 'CRITICAL - %s |count=%s;2;1;0' % (msg, count)
     sys.exit(2)
