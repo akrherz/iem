396978815c684b472f42b9affe38dc39d407ded9
akrherz <akrherz@iastate.edu>
2015-04-16 07:26:44 -0500

    use total_seconds for nagios N0R/N0Q latency check
    The check was happening with a product timestamp slightly in the future,
    which generated a false positive!

--- nagios/check_n0q.py
+++ nagios/check_n0q.py
@@ -7,13 +7,14 @@ import sys
 
 prod = sys.argv[1]
 
-j = json.load(open('/home/ldm/data/gis/images/4326/USCOMP/%s_0.json' % (prod,)))
+j = json.load(open('/home/ldm/data/gis/images/4326/USCOMP/%s_0.json' % (prod,
+                                                                        )))
 prodtime = datetime.datetime.strptime(j['meta']['valid'], '%Y-%m-%dT%H:%M:%SZ')
 radarson = int(j['meta']['radar_quorum'].split("/")[0])
 gentime = j['meta']['processing_time_secs']
 
 utcnow = datetime.datetime.utcnow()
-latency = (utcnow - prodtime).seconds
+latency = (utcnow - prodtime).total_seconds()
 
 stats = "gentime=%s;180;240;300 radarson=%s;100;75;50" % (gentime, radarson)
 
@@ -24,7 +25,8 @@ if gentime > 300:
     print 'CRITICAL - gentime %s|%s' % (gentime, stats)
     sys.exit(2)
 if latency > 600:
-    print 'CRITICAL - latency %s|%s' % (prodtime, stats)
+    print('CRITICAL - radtime:%s latency:%ss|%s' % (prodtime, latency,
+                                                    stats))
     sys.exit(2)
 if radarson < 50:
     print 'CRITICAL - radarson %s|%s' % (radarson, stats)
