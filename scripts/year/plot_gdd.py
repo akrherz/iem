683acfbd101be881e27404e460c6f5dc884857e3
akrherz <akrherz@iastate.edu>
2015-04-23 08:49:59 -0500

    cleanup GDD plot to use smoother intervals and draw counties

--- scripts/year/plot_gdd.py
+++ scripts/year/plot_gdd.py
@@ -2,11 +2,11 @@
 from pyiem.plot import MapPlot
 import sys
 import datetime
-now = datetime.datetime.now() - datetime.timedelta(days=1)
 import numpy as np
 from pyiem.network import Table as NetworkTable
-st = NetworkTable('IACLIMATE')
 import psycopg2
+st = NetworkTable('IACLIMATE')
+now = datetime.datetime.now() - datetime.timedelta(days=1)
 COOP = psycopg2.connect(database='coop', host='iemdb', user='nobody')
 ccursor = COOP.cursor()
 
@@ -23,7 +23,8 @@ if len(sys.argv) == 2 and sys.argv[1] == "gdd48":
 # Compute normal from the climate database
 ccursor.execute("""SELECT station,
    sum(%s(high, low)) as gdd
-   from alldata_ia WHERE station != 'IA0000' and year = %s
+   from alldata_ia WHERE station != 'IA0000'
+   and substr(station, 3, 1) != 'C' and year = %s
    GROUP by station""" % (gfunc, now.year))
 
 lats = []
@@ -46,11 +47,14 @@ m = MapPlot(axisbg='white',
             subtitle="1 Jan - %s" % (now.strftime("%d %b %Y"), ))
 minval = min(gdd50)
 rng = max([int(max(gdd50) - minval), 10])
-m.contourf(lons, lats, gdd50, np.linspace(minval, minval+rng, 10))
+ramp = np.linspace(minval, minval+rng, 10, dtype=np.int)
+print ramp
+m.contourf(lons, lats, gdd50, ramp)
 pqstr = "plot c 000000000000 summary/gdd_jan1.png bogus png"
 if gbase == 52:
     pqstr = "plot c 000000000000 summary/gdd52_jan1.png bogus png"
 elif gbase == 48:
     pqstr = "plot c 000000000000 summary/gdd48_jan1.png bogus png"
+m.drawcounties()
 m.postprocess(view=False, pqstr=pqstr)
 m.close()
