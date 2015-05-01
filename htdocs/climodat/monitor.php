3fc98c987415e511b15942c0242ba38d4a00c200
akrherz <akrherz@iastate.edu>
2015-05-01 15:46:05 -0500

    add other climodat states to climodat monitor
    http://mesonet.agron.iastate.edu/climodat/monitor.php

--- htdocs/climodat/monitor.php
+++ htdocs/climodat/monitor.php
@@ -6,8 +6,8 @@ require_once "../../include/myview.php";
 require_once "../../include/imagemaps.php";
 require_once "../../include/database.inc.php";
 require_once "../../include/network.php";
+require_once "../../include/forms.php";
 
-$nt = new NetworkTable("IACLIMATE");
 
 function ss($v){
 	if ($v == '') return '';
@@ -15,10 +15,17 @@ function ss($v){
 }
 
 $sdate = isset($_GET["sdate"]) ? $_GET["sdate"]: "04/01/2015";
+$network = isset($_GET["network"]) ? $_GET["network"] : "IACLIMATE";
 $edate = isset($_GET["edate"]) ? $_GET["edate"]: "12/31/2015";
 $gddbase = isset($_GET["gddbase"]) ? intval($_GET["gddbase"]) : 50;
 $gddfloor = isset($_GET["gddfloor"]) ? ss($_GET["gddfloor"]) : 50;
 $gddceil = isset($_GET["gddceil"]) ? ss($_GET["gddceil"]) : 86;
+
+$nt = new NetworkTable($network);
+$nt2 = new NetworkTable(Array("IACLIMATE", "NDCLIMATE", 'SDCLIMATE',
+		'NECLIMATE', 'KSCLIMATE', 'MOCLIMATE', 'MNCLIMATE', 'WICLIMATE',
+		'ILCLIMATE', 'KYCLIMATE', 'INCLIMATE', 'OHCLIMATE', 'MICLIMATE'));
+
 $hiddendates = <<<EOF
 <input type="hidden" name="sdate" value="{$sdate}">
 <input type="hidden" name="edate" value="{$edate}">
@@ -48,47 +55,53 @@ while (list($k, $v)=each($s)){
 $hiddenstations = "";
 $table = "";
 reset($s);
+$stationgrps = Array();
 while (list($k,$v)=each($s)){
+	$state = substr($v, 0, 2);
+	if (! array_key_exists($state, $stationgrps)){
+		$stationgrps[$state] = Array();
+	}
+	$stationgrps[$state][] = $v;
 	$hiddenstations .= "<input type=\"hidden\" name=\"s[]\" value=\"$v\">";
 }
 
 $sdatestr = date("Y-m-d", $sdate);
 $edatestr = date("Y-m-d", $edate);
-$sstring = "('". implode(",", $s) ."')";
-$sstring = str_replace(",", "','", $sstring);
+
 $gddstr = "gddxx({$gddbase}, {$gddceil}, high, low)";
 if ($gddfloor == '' || $gddceil == ''){
 	$gddstr = "gdd_onlybase({$gddbase}, high, low)";
 }
-$sql = <<<EOF
-WITH climo as (
-  SELECT station, sday, avg(precip) as avg_precip,
-  avg(sdd86(high,low)) as avg_sdd,
-  avg({$gddstr}) as avg_gdd
-  from alldata_ia WHERE station in {$sstring}
-  and year > 1950 GROUP by station, sday)
-  
-select o.station,
-   sum({$gddstr}) as ogdd50,
-   sum(o.precip) as oprecip,
-   sum(c.avg_gdd) as cgdd50, sum(c.avg_precip) as cprecip,
-   max(o.high) as maxtmpf, min(o.low) as mintmpf,
-   avg( (o.high + o.low) / 2.0 ) as avgtmpf,
-   sum(c.avg_sdd) as csdd86, sum(sdd86(o.high, o.low)) as osdd86
-  from alldata_ia o, climo c WHERE
-   o.station in {$sstring}
-   and o.station = c.station
-   and day >= '{$sdatestr}' and day < '{$edatestr}'
-   and o.sday = c.sday  GROUP by o.station
-   ORDER by o.station ASC
-EOF;
+$pgconn = iemdb('coop');
 
-reset($s);
-if (count($s) > 0){
-	$pgconn = iemdb('coop');
+// Loop over station groups
+while (list($state, $stations)=each($stationgrps)){
+	$sstring = "('". implode(",", $stations) ."')";
+	$sstring = str_replace(",", "','", $sstring);
+	$sql = <<<EOF
+	WITH climo as (
+	  SELECT station, sday, avg(precip) as avg_precip,
+	  avg(sdd86(high,low)) as avg_sdd,
+	  avg({$gddstr}) as avg_gdd
+	  from alldata_{$state} WHERE station in {$sstring}
+	  and year > 1950 GROUP by station, sday)
+	  
+	select o.station,
+	   sum({$gddstr}) as ogdd50,
+	   sum(o.precip) as oprecip,
+	   sum(c.avg_gdd) as cgdd50, sum(c.avg_precip) as cprecip,
+	   max(o.high) as maxtmpf, min(o.low) as mintmpf,
+	   avg( (o.high + o.low) / 2.0 ) as avgtmpf,
+	   sum(c.avg_sdd) as csdd86, sum(sdd86(o.high, o.low)) as osdd86
+	  from alldata_{$state} o, climo c WHERE
+	   o.station in {$sstring}
+	   and o.station = c.station
+	   and day >= '{$sdatestr}' and day < '{$edatestr}'
+	   and o.sday = c.sday  GROUP by o.station
+	   ORDER by o.station ASC
+EOF;
 	$rs = pg_query($pgconn, $sql);
-	while (list($k,$v)=each($s)){
-		$row = pg_fetch_assoc($rs, $k);
+	for ($i=0;$row=@pg_fetch_assoc($rs,$i);$i++){
 		$table .= sprintf("<tr><td>"
 				."<input type=\"checkbox\" name=\"r[]\" value=\"%s\" >"
 				." %s</td><td>%s</td>"
@@ -96,7 +109,7 @@ if (count($s) > 0){
 				."<td>%.1f</td><td>%.1f</td><td>%.1f</td>"
 				."<td>%.1f</td><td>%.1f</td><td>%.1f</td>"
 				."<td>%s</td><td>%s</td><td>%.1f</td></tr>", 
-				$v, $v, $nt->table[$v]['name'], $row["oprecip"],
+				$row['station'], $row['station'], $nt2->table[$row['station']]['name'], $row["oprecip"],
 				$row["cprecip"], $row["oprecip"] - $row["cprecip"],
 				$row["ogdd50"],
 				$row["cgdd50"], $row["ogdd50"] - $row["cgdd50"],
@@ -105,8 +118,7 @@ if (count($s) > 0){
 				$row["maxtmpf"], $row["mintmpf"], $row["avgtmpf"]);
 	}
 }
-
-$nselect = networkSelect("IACLIMATE", "IA0000", Array(), "s[]");
+$nselect = networkSelect($network, "IA0000", Array(), "s[]");
 
 $t = new MyView();
 $t->title = "Climodat Station Monitor";
@@ -127,6 +139,22 @@ $(document).ready(function(){
 });
 </script>
 EOF;
+
+$states = Array(
+		'IACLIMATE' => 'Iowa',
+		'ILCLIMATE' => 'Illinois',
+		'KSCLIMATE' => 'Kansas',
+		'KYCLIMATE' => 'Kentucky',
+		'MICLIMATE' => 'Michigan',
+		'MNCLIMATE' => 'Minnesota',
+		'MOCLIMATE' => 'Missouri',
+		'NECLIMATE' => 'Nebraska',
+		'NDCLIMATE' => 'North Dakota',
+		'OHCLIMATE' => 'Ohio',
+		'SDCLIMATE' => 'South Dakota',
+		'WICLIMATE' => 'Wisconsin',
+);
+$sselect = make_select('network', $network, $states);
 
 $snice = date("d M Y", $sdate);
 $today = ($edate > time()) ? time() : $edate;
@@ -154,8 +182,22 @@ is a description of the three options.</p>
  high temperature is set to this value prior to averaging.  Delete the
  entry to remove this calculation.</li>
 </ul>
-		
+
+<hr />
+<h4>Available States with Data</h4>
+
+<form name="switch">
+{$hiddendates}
+{$hiddenstations}
+{$sselect}
+<input type="submit" value="Select State">
+</form>
+
+<hr />
+<h4>Available Stations within Selected State</h4>
+
 <form name="add">
+<input type="hidden" name="network" value="{$network}">
 {$hiddendates}
 {$hiddenstations}
 	{$nselect}
@@ -166,6 +208,7 @@ is a description of the three options.</p>
 <h4>Table Options</h4>
 
 <form name="dates">
+<input type="hidden" name="network" value="{$network}">
 {$hiddenstations}
 <table class="table table-condensed">
 <tr><th>Growing Degree Days</th>
@@ -190,6 +233,7 @@ is a description of the three options.</p>
 1951-2015.</p>
 
 <form name="remove">
+<input type="hidden" name="network" value="{$network}">
 {$hiddenstations}
 {$hiddendates}
 <table class="table table-bordered table-striped table-condensed">
