d52593bffb4a20eee9466ef8e79aa01a5fc23977
akrherz <akrherz@iastate.edu>
2015-05-04 08:28:13 -0500

    cleanup growing season GDD plot

--- scripts/gs/plot_gdd.py
+++ scripts/gs/plot_gdd.py
@@ -1,15 +1,14 @@
-"""
- Generate a plot of GDD 
-"""
+"""Generate a plot of GDD"""
 from pyiem.plot import MapPlot
 import datetime
 import numpy as np
 import psycopg2
+from pyiem.network import Table as NetworkTable
 COOP = psycopg2.connect("dbname=coop host=iemdb user=nobody")
 ccursor = COOP.cursor()
 
-from pyiem.network import Table as NetworkTable
 nt = NetworkTable("IACLIMATE")
+
 
 def run(base, ceil, now, fn):
     """ Generate the plot """
@@ -23,32 +22,33 @@ def run(base, ceil, now, fn):
     lats = []
     lons = []
     gdd50 = []
-    ccursor.execute( sql )
+    ccursor.execute(sql)
     for row in ccursor:
-        if not nt.sts.has_key(row[0]):
+        if row[0] not in nt.sts:
             continue
-        lats.append( nt.sts[row[0]]['lat'] )
-        lons.append( nt.sts[row[0]]['lon'] )
+        lats.append(nt.sts[row[0]]['lat'])
+        lons.append(nt.sts[row[0]]['lon'])
         gdd50.append(float(row[1]))
 
-    
-    m = MapPlot(title="Iowa 1 May - %s GDD Accumulation" % (
-                        now.strftime("%-d %B %Y"), ),
+    m = MapPlot(title=("Iowa 1 May - %s GDD Accumulation"
+                       ) % (now.strftime("%-d %B %Y"), ),
                 subtitle="base %s" % (base,))
-    a = np.arange(int(min(gdd50)-20), int(max(gdd50)+20), 50)
-    m.contourf(lons, lats, gdd50, a)
-    
+    bins = np.linspace(min(gdd50)-1, max(gdd50)+1, num=10, dtype=np.int)
+    m.contourf(lons, lats, gdd50, bins)
+    m.drawcounties()
+
     pqstr = "plot c 000000000000 summary/%s.png bogus png" % (fn,)
     m.postprocess(pqstr=pqstr)
+
 
 def main():
     """Main()"""
     today = datetime.datetime.now()
     if today.month < 5:
         today = today.replace(year=(today.year-1), month=11, day=1)
-    run(50,86, today, 'gdd_may1')
-    run(60,86, today, 'gdd_may1_6086')
-    run(65,86, today, 'gdd_may1_6586')
-    
+    run(50, 86, today, 'gdd_may1')
+    run(60, 86, today, 'gdd_may1_6086')
+    run(65, 86, today, 'gdd_may1_6586')
+
 if __name__ == '__main__':
     main()
