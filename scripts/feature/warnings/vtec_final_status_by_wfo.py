fa3bd6d748544f612aeb50c3ea119acccd6837ba
akrherz <akrherz@iastate.edu>
2015-05-05 13:46:02 -0500

    another request for warning maps
    http://mesonet.agron.iastate.edu/pickup/nws/071001-150501-svr-svs2.png
    http://mesonet.agron.iastate.edu/pickup/nws/071001-150501-tor-svs2.png

--- scripts/feature/warnings/vtec_final_status_by_wfo.py
+++ scripts/feature/warnings/vtec_final_status_by_wfo.py
@@ -5,10 +5,15 @@ dbconn = psycopg2.connect(database='postgis', host='iemdb', user='nobody')
 cursor = dbconn.cursor()
 
 cursor.execute("""
- select wfo, sum(case when polygon_end = init_expire then 1 else 0 end),
- count(*) from sbw WHERE status = 'NEW' and issue > '2007-10-01' and
- issue < '2015-05-01' and phenomena = 'TO' and significance = 'W'
- GROUP by wfo
+ WITh data as (
+ select wfo, phenomena, significance,
+ extract(year from issue) as yr, count(*)
+ from sbw WHERE issue > '2007-10-01' and
+ issue < '2015-05-01' and phenomena = 'SV' and significance = 'W'
+ GROUP by wfo, phenomena, significance, yr, eventid)
+
+ SELECT wfo, sum(case when count > 2 then 1 else 0 end), count(*) from
+ data GROUP by wfo
 """)
 
 data = {}
@@ -16,7 +21,7 @@ for row in cursor:
     data[row[0]] = row[1] / float(row[2]) * 100.
 
 m = MapPlot(sector='nws', axisbg='white',
-            title="Percentage of Tornado Warnings without SVS Update",
+            title="Percentage of Severe T'Storm Warnings with 2 or more SVS Updates",
             subtitle='period: 1 Oct 2007 - 1 May 2015')
 
 m.fill_cwas(data)
