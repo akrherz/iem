72a3f2885599751e186b99294d1a1c1d0c2a8a63
akrherz <akrherz@iastate.edu>
2015-05-05 12:05:13 -0500

    updates to offline script for some warning summaries as per request
    http://mesonet.agron.iastate.edu/pickup/nws/071001-150501-svr-nosvs.png
    http://mesonet.agron.iastate.edu/pickup/nws/071001-150501-tor-nosvs.png

--- scripts/feature/warnings/vtec_final_status_by_wfo.py
+++ scripts/feature/warnings/vtec_final_status_by_wfo.py
@@ -5,18 +5,19 @@ dbconn = psycopg2.connect(database='postgis', host='iemdb', user='nobody')
 cursor = dbconn.cursor()
 
 cursor.execute("""
- SELECT wfo, sum(case when expire - issue >= '1 hour'::interval then 1 else 0 end), count(*) from
- warnings where phenomena = 'TO' and significance = 'W' and gtype = 'P'
- and issue > '2007-10-01' GROUP by wfo
+ select wfo, sum(case when polygon_end = init_expire then 1 else 0 end),
+ count(*) from sbw WHERE status = 'NEW' and issue > '2007-10-01' and
+ issue < '2015-05-01' and phenomena = 'TO' and significance = 'W'
+ GROUP by wfo
 """)
 
 data = {}
 for row in cursor:
-    data[ row[0] ] = row[1] / float(row[2]) * 100.
+    data[row[0]] = row[1] / float(row[2]) * 100.
 
-m = MapPlot(sector='nws',
-            title="Percentage of Tornado Warnings AOA 60 minutes long ",
-            subtitle='period: 1 Oct 2007 - 21 Jun 2013')
+m = MapPlot(sector='nws', axisbg='white',
+            title="Percentage of Tornado Warnings without SVS Update",
+            subtitle='period: 1 Oct 2007 - 1 May 2015')
 
 m.fill_cwas(data)
 
