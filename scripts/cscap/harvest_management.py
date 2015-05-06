47bcfe7ce95923395021bfe4239e15c944e2ff1b
akrherz <akrherz@iastate.edu>
2015-05-06 07:44:44 -0500

    [CSCAP] pep8 fixes and harvest management logic improvements

--- scripts/cscap/harvest_management.py
+++ scripts/cscap/harvest_management.py
@@ -4,7 +4,6 @@
 import util
 import ConfigParser
 import psycopg2
-import sys
 
 config = ConfigParser.ConfigParser()
 config.read('mytokens.cfg')
@@ -16,7 +15,7 @@ pcursor = pgconn.cursor()
 # Get me a client, stat
 spr_client = util.get_spreadsheet_client(config)
 
-spread = util.Spreadsheet(spr_client, 
+spread = util.Spreadsheet(spr_client,
                           config.get('cscap', 'manstore'))
 
 translate = {'date': 'valid'}
@@ -26,17 +25,23 @@ for sheetkey in ['Operations', 'Management', 'Pesticides']:
     current = {}
     found = {}
     if sheetkey == 'Operations':
-        pcursor.execute("""SELECT valid, uniqueid, updated, operation, cropyear, oid 
-        from operations""")
+        pcursor.execute("""
+            SELECT valid, uniqueid, updated, operation, cropyear, oid
+            from operations
+        """)
     elif sheetkey == 'Management':
-        pcursor.execute("""SELECT '', uniqueid,    updated, '',       cropyear, oid
-        from management""")
+        pcursor.execute("""
+            SELECT '', uniqueid,    updated, '',       cropyear, oid
+            from management
+        """)
     else:
-        pcursor.execute("""SELECT valid, uniqueid, updated, crop, cropyear, oid
-        from pesticides""")
+        pcursor.execute("""
+            SELECT valid, uniqueid, updated, crop, cropyear, oid
+            from pesticides
+        """)
     for row in pcursor:
         v = row[0]
-        if v is not None and type(v) != type(''):
+        if v is not None and not isinstance(v, str):
             v = v.strftime("%-m/%-d/%Y")
         key = "%s,%s,%s,%s,%s" % (v, row[1], row[2], row[3], row[4])
         current[key] = row[5]
@@ -48,35 +53,40 @@ for sheetkey in ['Operations', 'Management', 'Pesticides']:
     for entry in sheet.get_list_feed().entry:
         entries += 1
         d = entry.to_dict()
-        key = '%s,%s,%s,%s,%s' % (d.get('date', ''), d.get('uniqueid'),
-                                  d.get('updated'), 
-            d.get('operation' if sheetkey == 'Operations' else 'crop', ''),
-            d.get('cropyear'))
-        if current.has_key(key):
+        # Units row has n/a as the date, so skip it
+        if d.get('date') == 'n/a':
+            continue
+        key = ('%s,%s,%s,%s,%s'
+               ) % (d.get('date', ''), d.get('uniqueid'), d.get('updated'),
+                    d.get('operation' if sheetkey == 'Operations' else 'crop',
+                          ''), d.get('cropyear'))
+        if key in current:
             del(current[key])
             found[key] = True
             dups += 1
             continue
-        if found.has_key(key):
+        if key in found:
             print 'DUP: ', key
             continue
         cols = []
         vals = []
         for key in d.keys():
-            vals.append( d[key] )
-            cols.append( translate.get(key,key) )
-            
-        sql = """INSERT into %s(%s) VALUES (%s)""" % (sheetkey, ",".join(cols),
-                                                        ','.join(["%s"]*len(cols)))
+            vals.append(d[key])
+            cols.append(translate.get(key, key))
+
+        sql = """
+            INSERT into %s(%s) VALUES (%s)
+            """ % (sheetkey, ",".join(cols), ','.join(["%s"]*len(cols)))
         pcursor.execute(sql, vals)
         added += 1
-    
+
     for key in current.keys():
-        pcursor.execute("""DELETE from """+sheetkey+""" WHERE oid = %s""", 
+        pcursor.execute("""DELETE from """+sheetkey+""" WHERE oid = %s""",
                         (current[key],))
-        
+
     print "   harvest_management %s %4s rows %4s dups %4s add %4s del" % (
         sheetkey, entries, dups, added, len(current))
+
 pcursor.close()
 pgconn.commit()
 pgconn.close()
