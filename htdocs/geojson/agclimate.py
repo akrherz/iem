cb75a61e3271ac7f35561a187c34f41595212530
akrherz <akrherz@iastate.edu>
2015-05-06 08:16:01 -0500

    non-func cleanups of ISUSM geojson service

--- htdocs/geojson/agclimate.py
+++ htdocs/geojson/agclimate.py
@@ -53,11 +53,13 @@ def drct2txt(val):
     elif (val >= 324 and val < 350):
         return "NNW"
 
+
 def safe_t(val, units="C"):
     ''' '''
     if val is None:
         return 'M'
     return '%.1f' % (temperature(val, units).value('F'),)
+
 
 def safe_p(val):
     ''' precipitation '''
@@ -65,35 +67,39 @@ def safe_p(val):
         return 'M'
     return '%.2f' % (val / 25.4,)
 
+
 def safe(val, precision):
     ''' safe precision formatter '''
     if val is None or val < 0:
         return 'M'
-    return round(val,precision)
+    return round(val, precision)
+
 
 def safe_m(val):
     '''  '''
     if val is None or val < 0:
         return 'M'
-    return round(val * 100.,0)
+    return round(val * 100., 0)
 
-def get_data( ts ):
+
+def get_data(ts):
     """ Get the data for this timestamp """
     qcdict = loadqc()
     nt = NetworkTable("ISUSM")
-    data = {"type": "FeatureCollection", 
-            "crs": {"type": "EPSG", 
+    data = {"type": "FeatureCollection",
+            "crs": {"type": "EPSG",
                     "properties": {"code": 4326,
-                                   "coordinate_order": [1,0]}},
+                                   "coordinate_order": [1, 0]}},
             "features": []}
     # Fetch the daily values
     iemcursor.execute("""
-    SELECT id, pday, max_tmpf, min_tmpf from summary s JOIN stations t 
+    SELECT id, pday, max_tmpf, min_tmpf from summary s JOIN stations t
     on (t.iemid = s.iemid) WHERE t.network = 'ISUSM' and day = %s
     """, (ts.date(),))
     daily = {}
     for row in iemcursor:
-        daily[row[0]] = {'pday': row[1], 'max_tmpf': row[2], 'min_tmpf': row[3]}
+        daily[row[0]] = {'pday': row[1], 'max_tmpf': row[2],
+                         'min_tmpf': row[3]}
     cursor.execute("""
     SELECT * from sm_hourly where valid = %s
     """, (ts,))
@@ -101,39 +107,43 @@ def get_data( ts ):
         lon = nt.sts[row['station']]['lon']
         lat = nt.sts[row['station']]['lat']
         q = qcdict.get(row['station'], {})
-        data['features'].append({"type": "Feature", "id": i,
-            "properties": {
-                "encrh_avg": "%s%%" % safe(row['encrh_avg'], 1) if row['encrh_avg'] > 5 else "M",
-                "rh":  "%.0f%%" % (row["rh"],),
-                "hrprecip" : safe_p(row['rain_mm_tot']) if not q.get('precip', False) else 'M',
-                "et": safe_p(row['etalfalfa']),
-                "bat": safe(row['battv_min'], 2),
-                "radmj": safe(row['slrmj_tot'], 2),
-                "tmpf": safe_t(row['tair_c_avg']),
-                "high": safe_t(daily.get(row['station'], {}).get('max_tmpf', None), 'F'),
-                "low": safe_t(daily.get(row['station'], {}).get('min_tmpf', None), 'F'),
-                "pday": safe(daily.get(row['station'], {}).get('pday', None), 2) if not q.get('precip', False) else 'M',
-    "soil04t": safe_t(row['tsoil_c_avg']) if not q.get('soil4', False) else 'M',
-                "soil12t": safe_t(row['t12_c_avg']),
-                "soil24t": safe_t(row['t24_c_avg']),
-                "soil50t": safe_t(row['t50_c_avg']),
-                "soil12m": safe_m(row['vwc_12_avg']),
-                "soil24m": safe_m(row['vwc_24_avg']),
-                "soil50m": safe_m(row['vwc_50_avg']),
-                "wind": "%s@%.0f" % (drct2txt(row['winddir_d1_wvt']), 
-                                    row['ws_mps_s_wvt'] * 2.23)
+        data['features'].append({"type": "Feature", "id": i, "properties": {
+            "encrh_avg": "%s%%" % safe(row['encrh_avg'], 1) if row['encrh_avg'] > 5 else "M",
+            "rh":  "%.0f%%" % (row["rh"],),
+            "hrprecip" : safe_p(row['rain_mm_tot']) if not q.get('precip', False) else 'M',
+            "et": safe_p(row['etalfalfa']),
+            "bat": safe(row['battv_min'], 2),
+            "radmj": safe(row['slrmj_tot'], 2),
+            "tmpf": safe_t(row['tair_c_avg']),
+            "high": safe_t(daily.get(row['station'], {}).get('max_tmpf', None), 'F'),
+            "low": safe_t(daily.get(row['station'], {}).get('min_tmpf', None), 'F'),
+            "pday": safe(daily.get(row['station'], {}).get('pday', None), 2) if not q.get('precip', False) else 'M',
+            "soil04t": safe_t(row['tsoil_c_avg']) if not q.get('soil4', False) else 'M',
+            "soil12t": safe_t(row['t12_c_avg']),
+            "soil24t": safe_t(row['t24_c_avg']),
+            "soil50t": safe_t(row['t50_c_avg']),
+            "soil12m": safe_m(row['vwc_12_avg']),
+            "soil24m": safe_m(row['vwc_24_avg']),
+            "soil50m": safe_m(row['vwc_50_avg']),
+            "wind": "%s@%.0f" % (drct2txt(row['winddir_d1_wvt']),
+                                 row['ws_mps_s_wvt'] * 2.23)
             },
             "geometry": {"type": "Point",
-                        "coordinates": [lon, lat]
-            }
+                         "coordinates": [lon, lat]
+                         }
         })
     sys.stdout.write(json.dumps(data))
 
-if __name__ == '__main__':
-    ''' see how we are called '''
+
+def main(argv):
+    """Go Main Go"""
     sys.stdout.write("Content-type: application/vnd.geo+json\n\n")
     field = cgi.FieldStorage()
     dt = field.getfirst('dt')
     ts = datetime.datetime.strptime(dt, '%Y-%m-%dT%H:%M:%S.000Z')
     ts = ts.replace(tzinfo=pytz.timezone("UTC"))
-    get_data( ts )
+    get_data(ts)
+
+if __name__ == '__main__':
+    # see how we are called
+    main(sys.argv)
