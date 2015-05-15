65a123c624cb74665de4fde612758053e93ece14
akrherz <akrherz@iastate.edu>
2015-05-15 10:37:54 -0500

    improvements to the 4 inch county soil temperature maps
    http://mesonet.agron.iastate.edu/agclimate/soilt.php

--- scripts/isuag/fancy_4inch.py
+++ scripts/isuag/fancy_4inch.py
@@ -1,19 +1,17 @@
 """
 Generates the nice 4inch soil temperature plot by county
 """
-#stdlib
+# stdlib
 import datetime
-import os
 import sys
 
-#thirdparty
+# thirdparty
 import numpy as np
 from scipy.interpolate import NearestNDInterpolator
-import psycopg2
 import matplotlib.cm as cm
 import psycopg2.extras
 
-#pyiem
+# pyiem
 from pyiem.plot import MapPlot
 from pyiem.datatypes import temperature
 from pyiem.tracker import loadqc
@@ -35,20 +33,21 @@ soil_obs = []
 lats = []
 lons = []
 icursor.execute("""
-  SELECT station, tsoil_c_avg from sm_daily 
-  where valid = '%s' and tsoil_c_avg > -40 
+    SELECT station, tsoil_c_avg from sm_daily
+    where valid = '%s' and tsoil_c_avg > -40
 """ % (ts.strftime("%Y-%m-%d"), ))
 for row in icursor:
     stid = row['station']
     if qdict.get(stid, {}).get('soil4', False):
-        #print '%s was QCd out' % (stid,)
+        # print '%s was QCd out' % (stid,)
         continue
-    soil_obs.append( temperature(row['tsoil_c_avg'], 'C').value('F') )
-    lats.append( nt.sts[stid]['lat'] )
-    lons.append( nt.sts[stid]['lon'] )
+    soil_obs.append(temperature(row['tsoil_c_avg'], 'C').value('F'))
+    lats.append(nt.sts[stid]['lat'])
+    lons.append(nt.sts[stid]['lon'])
 
 if len(lats) < 5:
     sys.exit()
+
 
 def sampler(xaxis, yaxis, vals, x, y):
     i = 0
@@ -57,58 +56,54 @@ def sampler(xaxis, yaxis, vals, x, y):
     j = 0
     while (yaxis[j] < y):
         j += 1
-    return vals[i,j]
+    return vals[i, j]
 
 # Grid it
 numxout = 40
 numyout = 40
-xmin    = min(lons) - 2.
-ymin    = min(lats) - 2.
-xmax    = max(lons) + 2.
-ymax    = max(lats) + 2.
-xc      = (xmax-xmin)/(numxout-1)
-yc      = (ymax-ymin)/(numyout-1)
+xmin = min(lons) - 2.
+ymin = min(lats) - 2.
+xmax = max(lons) + 2.
+ymax = max(lats) + 2.
+xc = (xmax-xmin)/(numxout-1)
+yc = (ymax-ymin)/(numyout-1)
 
-xo = xmin + xc * np.arange(0,numxout)
-yo = ymin + yc * np.arange(0,numyout)
+xo = xmin + xc * np.arange(0, numxout)
+yo = ymin + yc * np.arange(0, numyout)
 
-#analysis = griddata((lons, lats), soil_obs, (xo, yo) )
-#rbfi = Rbf(lons, lats, soil_obs, function='cubic')
-#analysis = rbfi(xo, yo)
+# analysis = griddata((lons, lats), soil_obs, (xo, yo) )
+# rbfi = Rbf(lons, lats, soil_obs, function='cubic')
+# analysis = rbfi(xo, yo)
 nn = NearestNDInterpolator((lons, lats), np.array(soil_obs))
 analysis = nn(xo, yo)
 
 # Query out centroids of counties...
-pcursor.execute("""SELECT ST_x(ST_centroid(the_geom)) as lon, 
-  ST_y(ST_centroid(the_geom)) as lat 
- from uscounties WHERE state_name = 'Iowa'""")
+pcursor.execute("""SELECT ST_x(ST_centroid(the_geom)) as lon,
+    ST_y(ST_centroid(the_geom)) as lat
+    from uscounties WHERE state_name = 'Iowa'
+""")
 clons = []
 clats = []
 for row in pcursor:
-    clats.append( row['lat'] )
-    clons.append( row['lon'] )
+    clats.append(row['lat'])
+    clons.append(row['lon'])
 
 cobs = nn(clons, clats)
-#cobs = griddata((lons, lats), soil_obs, (clons, clats))
 
 m = MapPlot(sector='iowa',
-    title="Iowa 4in Soil Temperatures %s" % (ts.strftime("%b %d %Y"), ),
-    subtitle='Based on gridded analysis of ISUAG, ISUSM network observations'
-)
-m.contourf(clons, clats, cobs, np.arange(10,101,5), cmap=cm.get_cmap('jet'),
+            title=("Iowa Average 4 inch Soil Temperatures %s"
+                   ) % (ts.strftime("%b %d %Y"), ),
+            subtitle=("Based on gridded analysis (black numbers) of "
+                      "ISUSM network observations (red numbers)"))
+m.contourf(clons, clats, cobs, np.arange(10, 101, 5), cmap=cm.get_cmap('jet'),
            units=r'$^\circ$F')
-for lo, la, ob in zip(clons, clats, cobs):
-#for lo, la, ob in zip(lons, lats, soil_obs):
-    xi, yi = m.map(lo, la)
-    txt = m.ax.text(xi, yi, "%.0f" % (ob,))
+m.plot_values(clons, clats, cobs, fmt='%.0f', textsize=11)
+m.plot_values(lons, lats, soil_obs, fmt='%.0f', color='r')
+# for lo, la, ob in zip(clons, clats, cobs):
+#    xi, yi = m.map(lo, la)
+#    txt = m.ax.text(xi, yi, "%.0f" % (ob,))
 m.drawcounties()
-m.postprocess(filename='highs.ps')
-
-os.system("convert -trim  highs.ps obs.png")
-routes = "a"
-if day_ago < 4:
-    routes = "ac" 
-os.system("/home/ldm/bin/pqinsert -p 'plot %s %s0000 soilt_day%s.png isuag_county_4inch_soil.png png' obs.png" % (
- routes, ts.strftime("%Y%m%d"), day_ago) )
-os.unlink('highs.ps')
-os.unlink('obs.png')
+routes = "a" if day_ago >= 4 else "ac"
+pqstr = ("plot %s %s0000 soilt_day%s.png isuag_county_4inch_soil.png png"
+         ) % (routes, ts.strftime("%Y%m%d"), day_ago)
+m.postprocess(pqstr=pqstr)
