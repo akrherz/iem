e8eec4d39a1b078caee5bd5dc45d8853f1d24874
akrherz <akrherz@iastate.edu>
2015-06-02 12:56:10 -0500

    add http referer to error log

--- htdocs/error404.php
+++ htdocs/error404.php
@@ -9,5 +9,6 @@ $t->content = <<<EOF
 <img src="/images/snoopy503.jpg" class="img-responsive" alt="daryl's Cat" />
 EOF;
 $t->render('single.phtml');
-error_log("404 ". $_SERVER["REQUEST_URI"]);
+$ref = isset($_SERVER["HTTP_REFERER"]) ? $_SERVER["HTTP_REFERER"] : 'none';
+error_log("404 ". $_SERVER["REQUEST_URI"]. ' referer: '. $ref);
 ?>
