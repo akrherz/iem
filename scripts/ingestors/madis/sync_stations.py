d493bf17b8a535def9a492099b6cbcfdebfa1720
akrherz <akrherz@iastate.edu>
2015-06-10 09:26:27 -0500

    hack support addition of additional MADIS network

--- scripts/ingestors/madis/sync_stations.py
+++ scripts/ingestors/madis/sync_stations.py
@@ -40,20 +40,26 @@ MY_PROVIDERS = [
  "WVDOT", 
  "WYDOT",  
  "VADOT", 
- "VTDOT",  
+ "VTDOT",
+ "MesoWest",
 ]
+VTWAC = ['UVM01', 'UVM02', 'UVM03', 'UVM04', 'UVM05', 'VEC01', 'VTW01']
 
 def provider2network(p):
     """ Convert a MADIS network ID to one that I use, here in IEM land"""
     if p in ['KYMN']:
         return p
+    if p == 'MesoWest':
+        return 'VTWAC'
     return '%s_RWIS' % (p[:2],)
 
 for recnum in range(len(providers)):
     thisProvider = providers[recnum].tostring().replace('\x00','')
-    if not thisProvider in MY_PROVIDERS:
+    if thisProvider not in MY_PROVIDERS:
         continue
     stid = stations[recnum].tostring().replace('\x00','')
+    if thisProvider == 'MesoWest' and stid not in VTWAC:
+        continue
     name = names[recnum].tostring().replace("'", "").replace('\x00','').replace('\xa0', ' ')
     network = provider2network(thisProvider)
     mcursor.execute("""SELECT * from stations where id = %s and network = %s""",
