fd4881a7d1a79f93e75689f644c3987fa2ede177
akrherz <akrherz@iastate.edu>
2015-06-25 10:51:51 -0500

    remove custom IEM python memcache implementation
    I don't think this is necessary anymore.  The reason I added it 5-10
    years ago looks suspect now.  Instead, we use the RHEL provided
    python-memcache client, which is already a requirement for installs.

--- include/python/TileCache/Caches/Memcached.py
+++ include/python/TileCache/Caches/Memcached.py
@@ -2,38 +2,40 @@
 
 from TileCache.Cache import Cache
 import time
+import memcache
+
 
 class Memcached(Cache):
-    def __init__ (self, servers = ['127.0.0.1:11211'], **kwargs):
+
+    def __init__(self, servers=['127.0.0.1:11211'], **kwargs):
         Cache.__init__(self, **kwargs)
-        import iemmemcache
-        if type(servers) is str: servers = map(str.strip, servers.split(","))
-        self.cache = iemmemcache.Client(servers, debug=0)
+        if type(servers) is str:
+            servers = map(str.strip, servers.split(","))
+        self.cache = memcache.Client(servers, debug=0)
         self.timeout = int(kwargs.get('timeout', 0))
-  
+
     def getKey(self, tile):
-         return "/".join(map(str, [tile.layer.name, tile.x, tile.y, tile.z]))
-        
+        return "/".join(map(str, [tile.layer.name, tile.x, tile.y, tile.z]))
+
     def get(self, tile):
         key = self.getKey(tile)
         tile.data = self.cache.get(key)
         return tile.data
-    
+
     def set(self, tile, data):
-        if self.readonly: return data
+        if self.readonly:
+            return data
         key = self.getKey(tile)
         self.cache.set(key, data, self.timeout)
         return data
-    
+
     def delete(self, tile):
         key = self.getKey(tile)
         self.cache.delete(key)
 
-    def attemptLock (self, tile):
-        return self.cache.add( self.getLockName(tile), "0", 
-                               time.time() + self.timeout)
-    
-    def unlock (self, tile):
-        self.cache.delete( self.getLockName(tile) )
+    def attemptLock(self, tile):
+        return self.cache.add(self.getLockName(tile), "0",
+                              time.time() + self.timeout)
 
-
+    def unlock(self, tile):
+        self.cache.delete(self.getLockName(tile))
