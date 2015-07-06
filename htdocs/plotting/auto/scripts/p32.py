1015dc081383a47ab972c0b0b1d35fc901f44bd6
akrherz <akrherz@iastate.edu>
2015-07-06 15:56:00 -0500

    7 Jul feature is plot of daily temp departures
    http://mesonet.agron.iastate.edu/plotting/auto/?q=32

--- htdocs/plotting/auto/scripts/p32.py
+++ htdocs/plotting/auto/scripts/p32.py
@@ -1,29 +1,30 @@
-"""
-  Fall Minimum by Date
-"""
 import psycopg2.extras
-import matplotlib
-matplotlib.use('agg')
-import matplotlib.pyplot as plt
-import numpy as np
 import datetime
+import pandas as pd
 import calendar
 from pyiem.network import Table as NetworkTable
+
 
 def get_description():
     """ Return a dict describing how to call this plotter """
     d = dict()
+    d['data'] = True
+    d['description'] = """This plot presents the daily high temperature
+    departure."""
     d['arguments'] = [
-        dict(type='station', name='station', default='IA0200', 
+        dict(type='station', name='station', default='IA0200',
              label='Select Station:'),
-        dict(type='year', name='year', default='2014', 
+        dict(type='year', name='year', default=datetime.date.today().year,
              label='Year to Plot:'),
     ]
     return d
 
 
-def plotter( fdict ):
+def plotter(fdict):
     """ Go """
+    import matplotlib
+    matplotlib.use('agg')
+    import matplotlib.pyplot as plt
     pgconn = psycopg2.connect(database='coop', host='iemdb', user='nobody')
     cursor = pgconn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
@@ -32,7 +33,7 @@ def plotter( fdict ):
 
     table = "alldata_%s" % (station[:2],)
     nt = NetworkTable("%sCLIMATE" % (station[:2],))
-    
+
     cursor.execute("""
     WITH data as (
      select day, high, sday
@@ -41,17 +42,20 @@ def plotter( fdict ):
      SELECT valid, high from ncdc_climate81
      WHERE station = %s
     )
-    SELECT extract(doy from d.day) as doy, d.high - c.high from data d JOIN climo c on 
+    SELECT extract(doy from d.day) as doy, d.high - c.high from
+    data d JOIN climo c on
     (to_char(c.valid, 'mmdd') = d.sday) ORDER by doy ASC
     """, (station, year, nt.sts[station]['ncdc81']))
 
     jdays = []
     diff = []
+    rows = []
     for row in cursor:
         jdays.append(row[0])
         diff.append(row[1])
-    
-    (fig, ax) = plt.subplots(1,1, sharex=True)
+        rows.append(dict(jday=row[0], diff=row[1]))
+    df = pd.DataFrame(rows)
+    (fig, ax) = plt.subplots(1, 1, sharex=True)
     bars = ax.bar(jdays, diff,  fc='b', ec='b')
     for i, bar in enumerate(bars):
         if diff[i] > 0:
@@ -59,12 +63,11 @@ def plotter( fdict ):
             bar.set_edgecolor('r')
     ax.grid(True)
     ax.set_ylabel("Temperature Departure $^\circ$F")
-    ax.set_title("%s %s\nYear %s Daily High Temperature Departure" % (station, 
-                                        nt.sts[station]['name'], year))
+    ax.set_title(("%s %s\nYear %s Daily High Temperature Departure"
+                  ) % (station, nt.sts[station]['name'], year))
     ax.set_xticklabels(calendar.month_abbr[1:])
-    ax.set_xticks( (1,32,60,91,121,152,182,213,244,274,305,335,365) )
+    ax.set_xticks((1, 32, 60, 91, 121, 152, 182, 213, 244, 274,
+                   305, 335, 365))
+    ax.set_xlim(0, 366)
 
-    ax.set_xlim(0,366)
-
-
-    return fig
+    return fig, df
