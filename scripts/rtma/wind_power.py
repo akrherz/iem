1d9ed74f1675ef3948fd6da5c745f422065836ad
akrherz <akrherz@iastate.edu>
2015-07-11 14:27:57 -0500

    some cleanups to wind_power script

--- scripts/rtma/wind_power.py
+++ scripts/rtma/wind_power.py
@@ -19,45 +19,55 @@ import sys
 levels = [0., 0.01, 0.1, 0.25, 0.5, 0.75, 1, 1.25, 1.5, 2, 2.5, 3, 4, 5, 7, 10,
           20, 30, 40, 50]
 
+
 def run(ts, routes):
     """ Run for a given UTC timestamp """
-    fn = ts.strftime("/mesonet/ARCHIVE/data/%Y/%m/%d/model/rtma/%H/rtma.t%Hz.awp2p5f000.grib2")
+    fn = ts.strftime(("/mesonet/ARCHIVE/data/%Y/%m/%d/model/rtma/%H/"
+                      "rtma.t%Hz.awp2p5f000.grib2"))
     if not os.path.isfile(fn):
         print 'wind_power.py missing', fn
         return
 
     grb = pygrib.open(fn)
-    u = grb.select(name='10 metre U wind component')[0]
-    v = grb.select(name='10 metre V wind component')[0]
+    try:
+        u = grb.select(name='10 metre U wind component')[0]
+        v = grb.select(name='10 metre V wind component')[0]
+    except:
+        print('Missing u/v wind for wind_power.py\nFN: %s' % (fn,))
+        return
     mag = (u['values']**2 + v['values']**2)**.5
-    
+
     mag = (mag * 1.35)**3 * 0.002641
-    #0.002641
+    # 0.002641
 
     lats, lons = u.latlons()
     lts = ts.astimezone(pytz.timezone("America/Chicago"))
-    pqstr = "plot %s %s00 midwest/rtma_wind_power.png midwest/rtma_wind_power_%s00.png png" % (
-                routes, ts.strftime("%Y%m%d%H"), ts.strftime("%H"))
-    m = MapPlot(sector='midwest', 
-                title=r'Wind Power Potential :: (speed_mps_10m * 1.35)$^3$ * 0.002641',
-                subtitle='valid: %s based on NOAA Realtime Mesoscale Analysis' % (
-                                lts.strftime("%d %b %Y %I %p")))
+    pqstr = ("plot %s %s00 midwest/rtma_wind_power.png "
+             "midwest/rtma_wind_power_%s00.png png"
+             ) % (routes, ts.strftime("%Y%m%d%H"), ts.strftime("%H"))
+    m = MapPlot(sector='midwest',
+                title=(r'Wind Power Potential :: '
+                       '(speed_mps_10m * 1.35)$^3$ * 0.002641'),
+                subtitle=('valid: %s based on NOAA Realtime '
+                          'Mesoscale Analysis'
+                          ) % (lts.strftime("%d %b %Y %I %p")))
     m.pcolormesh(lons, lats, mag, numpy.array(levels), units='MW')
-    
+
     m.postprocess(pqstr=pqstr)
 
-def main():
+
+def main(argv):
     """Main()"""
     if len(sys.argv) == 5:
-        now = datetime.datetime( int(sys.argv[1]), int(sys.argv[2]), 
-                                 int(sys.argv[3]), int(sys.argv[4]))
+        now = datetime.datetime(int(sys.argv[1]), int(sys.argv[2]),
+                                int(sys.argv[3]), int(sys.argv[4]))
         routes = "a"
     else:
         now = datetime.datetime.utcnow() - datetime.timedelta(hours=1)
         routes = "ac"
     now = now.replace(tzinfo=pytz.timezone("UTC"))
-    
+
     run(now, routes)
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv)
