5193f076cc80fbc6298db9195cd59c5a5f58190e
akrherz <akrherz@iastate.edu>
2015-07-15 10:13:38 -0500

    pep8 cleanups to trailing period precip plot
    http://mesonet.agron.iastate.edu/plotting/auto/?q=28

--- htdocs/plotting/auto/scripts/p28.py
+++ htdocs/plotting/auto/scripts/p28.py
@@ -2,40 +2,41 @@
   Fall Minimum by Date
 """
 import psycopg2.extras
-import matplotlib
-matplotlib.use('agg')
-import matplotlib.pyplot as plt
 import numpy as np
 import datetime
-import sys
 from pyiem.network import Table as NetworkTable
+
 
 def get_description():
     """ Return a dict describing how to call this plotter """
     d = dict()
+    today = datetime.date.today()
     d['arguments'] = [
-        dict(type='station', name='station', default='IA0200', 
+        dict(type='station', name='station', default='IA0200',
              label='Select Station:'),
-        dict(type='date', name='date', default='2014/10/15', 
+        dict(type='date', name='date', default=today.strftime("%Y/%m/%d"),
              label='To Date:',
-             min="1894/01/01"), # Comes back to python as yyyy-mm-dd
+             min="1894/01/01"),  # Comes back to python as yyyy-mm-dd
 
     ]
     return d
 
 
-def plotter( fdict ):
+def plotter(fdict):
     """ Go """
+    import matplotlib
+    matplotlib.use('agg')
+    import matplotlib.pyplot as plt
     pgconn = psycopg2.connect(database='coop', host='iemdb', user='nobody')
     cursor = pgconn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
     station = fdict.get('station', 'IA0200')
     date = datetime.datetime.strptime(fdict.get('date', '2014-10-15'),
-                                       '%Y-%m-%d')
+                                      '%Y-%m-%d')
 
     table = "alldata_%s" % (station[:2],)
     nt = NetworkTable("%sCLIMATE" % (station[:2],))
-    
+
     cursor.execute("""
     SELECT year,  extract(doy from day) as doy, precip
     from """+table+""" where station = %s and precip is not null
@@ -44,22 +45,23 @@ def plotter( fdict ):
     baseyear = nt.sts[station]['archive_begin'].year - 1
     years = (datetime.datetime.now().year - baseyear) + 1
 
-    data = np.zeros( (years, 367*2))
+    data = np.zeros((years, 367*2))
     # 1892 1893
     # 1893 1894
     # 1894 1895
-    
+
     for row in cursor:
-        #left hand
+        # left hand
         data[int(row['year'] - baseyear), int(row['doy'])] = row['precip']
         # right hand
-        data[int(row['year'] - baseyear -1), int(row['doy']) + 366] = row['precip']
+        data[int(row['year'] - baseyear - 1),
+             int(row['doy']) + 366] = row['precip']
 
     _temp = date.replace(year=2000)
     _doy = int(_temp.strftime("%j"))
     xticks = []
     xticklabels = []
-    for i in range(-366,0):
+    for i in range(-366, 0):
         ts = _temp + datetime.timedelta(days=i)
         if ts.day == 1:
             xticks.append(i)
@@ -74,29 +76,31 @@ def plotter( fdict ):
         sums = np.sum(data[:, idx0:idx1], 1)
         thisyear = sums[myyear]
         sums = np.sort(sums)
-        a = np.digitize([thisyear,], sums)
+        a = np.digitize([thisyear, ], sums)
         rank = years - a[0] + 1
-        ranks.append( rank )
-        totals.append( thisyear )
-        maxes.append( sums[-1] )
-    
-    (fig, ax) = plt.subplots(1,1)
+        ranks.append(rank)
+        totals.append(thisyear)
+        maxes.append(sums[-1])
+
+    (fig, ax) = plt.subplots(1, 1)
     ax.bar(np.arange(-365, 0), ranks[::-1], fc='b', ec='b')
     ax.grid(True)
     ax.set_xticks(xticks)
     ax.set_xticklabels(xticklabels)
-    ax.set_title("%s %s\nTrailing Days Precipitation Rank [%s-%s] to %s" % (station, 
-                                        nt.sts[station]['name'], baseyear+2,
-                                        datetime.datetime.now().year,
-                                        date.strftime("%-d %b %Y")))
+    ax.set_title(("%s %s\nTrailing Days Precipitation Rank [%s-%s] to %s"
+                  ) % (station, nt.sts[station]['name'], baseyear+2,
+                       datetime.datetime.now().year,
+                       date.strftime("%-d %b %Y")))
     ax.set_ylabel("Rank [1=wettest] (bars)")
-    ax.set_xlim(-367,0.5)
-    
+    ax.set_xlim(-367, 0.5)
+
     y2 = ax.twinx()
-    y2.plot(np.arange(-365,0), totals[::-1], color='r', lw=2, label='This Period')
-    y2.plot(np.arange(-365,0), maxes[::-1], color='g', lw=2, label='Maximum')
+    y2.plot(np.arange(-365, 0), totals[::-1], color='r', lw=2,
+            label='This Period')
+    y2.plot(np.arange(-365, 0), maxes[::-1], color='g', lw=2,
+            label='Maximum')
     y2.set_ylabel("Precipitation [inch]")
-    
+
     y2.legend()
-    
+
     return fig
