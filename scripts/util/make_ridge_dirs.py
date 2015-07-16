be2c488b869ac45a71ba88a991f371c522c7b429
akrherz <akrherz@iastate.edu>
2015-07-16 08:53:35 -0500

    python os.makedirs umask can not be trusted, be explicit
    the baseline creation of RIDGE folders failed on iem12.  It created a
    bunch of 755 folders owned by mesonet, which ldm could not write to.

--- scripts/util/make_ridge_dirs.py
+++ scripts/util/make_ridge_dirs.py
@@ -5,11 +5,31 @@ from having lots of 404s
 """
 import os
 import sys
+import grp
 from pyiem.network import Table as NetworkTable
 import datetime
 
 PRODS = {'NEXRAD': ['N0Q', 'N0S', 'N0U', 'N0Z', 'NET'],
          'TWDR': ['NET', 'TR0', 'TV0']}
+
+
+def chgrp(filepath, gid):
+    # https://gist.github.com/jmahmood/2505741
+    uid = os.stat(filepath).st_uid
+    os.chown(filepath, uid, gid)
+
+
+def supermakedirs(path, mode, group):
+    # http://stackoverflow.com/questions/5231901
+    if not path or os.path.exists(path):
+        return []
+    (head, _) = os.path.split(path)
+    res = supermakedirs(head, mode, group)
+    os.mkdir(path)
+    os.chmod(path, mode)
+    chgrp(path, group)
+    res += [path]
+    return res
 
 
 def main(argv):
@@ -26,7 +46,7 @@ def main(argv):
             mydir = "%s/%s/%s" % (basedir, sid, prod)
             if os.path.isdir(mydir):
                 continue
-            os.makedirs(mydir, mode=0775)
+            supermakedirs(mydir, 0775, grp.getgrnam('ldm')[2])
 
 if __name__ == '__main__':
     main(sys.argv)
