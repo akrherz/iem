0aea0199173aadc85821b5d1aba42b646154ec5d
akrherz <akrherz@iastate.edu>
2015-07-20 10:10:23 -0500

    pep8 cleanups and remove extraneous debug printout

--- htdocs/json/goes.py
+++ htdocs/json/goes.py
@@ -12,6 +12,7 @@ import datetime
 BIRDS = ['EAST', 'WEST']
 PRODUCTS = ['WV', 'VIS', 'IR']
 
+
 def parse_time(s):
     """
     Convert ISO something into a mx.DateTime
@@ -27,14 +28,14 @@ def parse_time(s):
         date = datetime.datetime.utcnow()
     return date
 
+
 def find_scans(root, bird, product, start_gts, end_gts):
     """ Find GOES SCANs """
     if bird not in BIRDS or product not in PRODUCTS:
         return
-    now = start_gts.replace(hour=0,minute=0,second=0)
+    now = start_gts.replace(hour=0, minute=0, second=0)
     while now < end_gts:
         mydir = now.strftime("/mesonet/ARCHIVE/data/%Y/%m/%d/GIS/sat/awips211")
-        sys.stderr.write(mydir)
         if os.path.isdir(mydir):
             os.chdir(mydir)
             files = glob.glob("GOES_%s_%s_*.wld" % (bird, product))
@@ -46,25 +47,27 @@ def find_scans(root, bird, product, start_gts, end_gts):
                     root['scans'].append(ts.strftime("%Y-%m-%dT%H:%M:00Z"))
         now += datetime.timedelta(hours=24)
 
+
 def list_files(form):
     """
     List available GOES files based on the form request
     """
     bird = form.getvalue('bird', 'EAST').upper()
     product = form.getvalue('product', 'VIS').upper()
-    
-    #default to a four hour period
+
+    # default to a four hour period
     utc0 = datetime.datetime.utcnow()
     utc1 = utc0 - datetime.timedelta(hours=4)
-    
-    start_gts = parse_time( form.getvalue('start', 
-                            utc1.strftime("%Y-%m-%dT%H:%MZ")) )
-    end_gts = parse_time( form.getvalue('end', 
-                            utc0.strftime("%Y-%m-%dT%H:%MZ")) )
+
+    start_gts = parse_time(form.getvalue('start',
+                                         utc1.strftime("%Y-%m-%dT%H:%MZ")))
+    end_gts = parse_time(form.getvalue('end',
+                                       utc0.strftime("%Y-%m-%dT%H:%MZ")))
     root = {'scans': []}
     find_scans(root, bird, product, start_gts, end_gts)
-        
+
     return root
+
 
 def main():
     """
@@ -79,10 +82,9 @@ def main():
     else:
         sys.stdout.write("Content-type: text/plain\n\n")
     if operation == "list":
-        sys.stdout.write(json.dumps( list_files(form) ))
+        sys.stdout.write(json.dumps(list_files(form)))
     if callback is not None:
         sys.stdout.write(')')
 
 if __name__ == "__main__":
     main()
-#END
