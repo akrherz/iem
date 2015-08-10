e1d4522bc1a7cd2c182f0c288d25092b42f55cf2
akrherz <akrherz@iastate.edu>
2015-08-10 16:04:19 -0500

    pep8 cleanups and add docs to coldest low of fall
    http://mesonet.agron.iastate.edu/plotting/auto/?q=33

--- htdocs/plotting/auto/scripts/p33.py
+++ htdocs/plotting/auto/scripts/p33.py
@@ -2,57 +2,71 @@
   Fall Minimum by Date
 """
 import psycopg2.extras
-import matplotlib
-matplotlib.use('agg')
-import matplotlib.pyplot as plt
-import numpy as np
 import datetime
-import calendar
+import pandas as pd
 from pyiem.network import Table as NetworkTable
+
 
 def get_description():
     """ Return a dict describing how to call this plotter """
     d = dict()
+    d['data'] = True
+    d['description'] = """This plot presents the largest drop in low
+    temperature during a fall season.  The drop compares the lowest
+    low previous to the date with the low for that date.  For example,
+    if your coldest low to date was 40, you would not expect to see a
+    low temperature of 20 the next night without first setting colder
+    daily low temperatures."""
     d['arguments'] = [
-        dict(type='station', name='station', default='IA0200', 
+        dict(type='station', name='station', default='IA0200',
              label='Select Station:'),
     ]
     return d
 
 
-def plotter( fdict ):
+def plotter(fdict):
     """ Go """
+    import matplotlib
+    matplotlib.use('agg')
+    import matplotlib.pyplot as plt
     pgconn = psycopg2.connect(database='coop', host='iemdb', user='nobody')
     cursor = pgconn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
     station = fdict.get('station', 'IA0200')
-
+    today = datetime.date.today()
     table = "alldata_%s" % (station[:2],)
     nt = NetworkTable("%sCLIMATE" % (station[:2],))
-    
+
     cursor.execute("""
       with data as (
-        select day, year, month, low, 
-        min(low) OVER (ORDER by day ASC ROWS between 91 PRECEDING and 1 PRECEDING) as p 
-        from """+table+""" where station = %s) 
+        select day, year, month, low,
+        min(low)
+        OVER (ORDER by day ASC ROWS between 91 PRECEDING and 1 PRECEDING) as p
+        from """ + table + """ where station = %s)
 
-      SELECT year, max(p - low) from data 
+      SELECT year, max(p - low) from data
       WHERE month > 6 GROUP by year ORDER by year ASC
     """, (station,))
 
     years = []
     data = []
+    rows = []
     for row in cursor:
+        if row[0] == today.year:
+            continue
         years.append(row[0])
         data.append(0-row[1])
-    
-    (fig, ax) = plt.subplots(1,1, sharex=True)
-    bars = ax.bar(years, data,  fc='b', ec='b')
+        rows.append(dict(year=row[0], drop=row[1]))
+    df = pd.DataFrame(rows)
+
+    (fig, ax) = plt.subplots(1, 1, sharex=True)
+    ax.bar(years, data, fc='b', ec='b')
     ax.grid(True)
     ax.set_ylabel("Largest Low Temp Drop $^\circ$F")
-    ax.set_title("%s %s\nMax Jul-Dec Low Temp Drop Exceeding Previous Min Low for Fall" % (station, 
-                                        nt.sts[station]['name']))
-    ax.set_xlim(min(years)-1,max(years)+1)
+    ax.set_title(("%s %s\n"
+                  "Max Jul-Dec Low Temp Drop Exceeding "
+                  "Previous Min Low for Fall"
+                  ) % (station, nt.sts[station]['name']))
+    ax.set_xlim(min(years)-1, max(years)+1)
 
-
-    return fig
+    return fig, df
