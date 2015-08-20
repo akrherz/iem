e068257b907211b6bfdf6af1ccbcee42949a5009
akrherz <akrherz@iastate.edu>
2015-08-20 13:35:04 -0500

    21 Aug feature is plot of 24h precip bias computation
    http://mesonet.agron.iastate.edu/plotting/auto/?q=96

---
+++ htdocs/plotting/auto/scripts/p96.py
@@ -0,0 +1,104 @@
+import psycopg2.extras
+from pyiem.network import Table as NetworkTable
+import numpy as np
+import pandas as pd
+import datetime
+import pytz
+
+
+def get_description():
+    """ Return a dict describing how to call this plotter """
+    d = dict()
+    d['data'] = True
+    d['desciption'] = """This plot looks at the bias associated with computing
+    24 hour precipitation totals using a given hour of the day as the
+    delimiter."""
+    d['arguments'] = [
+        dict(type='zstation', name='zstation', default='DSM',
+             label='Select Station:'),
+    ]
+    return d
+
+
+def plotter(fdict):
+    """ Go """
+    import matplotlib
+    matplotlib.use('agg')
+    import matplotlib.pyplot as plt
+    pgconn = psycopg2.connect(database='iem', host='iemdb', user='nobody')
+    cursor = pgconn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+    station = fdict.get('zstation', 'DSM')
+    network = fdict.get('network', 'IA_ASOS')
+    nt = NetworkTable(network)
+
+    jan1 = datetime.datetime.now().replace(hour=0, day=1, month=1, minute=0,
+                                           second=0, microsecond=0,
+                                           tzinfo=pytz.timezone("UTC"))
+    ts1973 = datetime.datetime(1973, 1, 1)
+    today = datetime.datetime.now()
+    cursor.execute("""
+    SELECT valid at time zone 'UTC', phour from hourly WHERE
+    station = %s and network = %s and phour >= 0.01 and
+    valid >= '1973-01-01 00:00+00' and valid < %s
+    """, (station, network, jan1))
+
+    days = (jan1.year - 1973) * 366
+    data = np.zeros((days * 24), 'f')
+    minvalid = today
+    for row in cursor:
+        if row[0] < minvalid:
+            minvalid = row[0]
+        data[(row[0] - ts1973).days * 24 + row[0].hour] = row[1]
+
+    lts = jan1.astimezone(pytz.timezone(nt.sts[station]['tzname']))
+    lts = lts.replace(month=7, hour=0)
+    cnts = [0]*24
+    avgv = [0]*24
+    rows = []
+    for hr in range(24):
+        ts = lts.replace(hour=hr)
+        z = ts.astimezone(pytz.timezone("UTC")).hour
+        a = np.reshape(data[z:(0 - 24 + z)], (days-1, 24))
+        tots = np.sum(a, 1)
+        cnts[hr] = np.sum(np.where(tots > 0, 1, 0))
+        avgv[hr] = np.average(tots[tots > 0])
+        rows.append(dict(average_precip=avgv[hr], events=cnts[hr],
+                         zhour=z, localhour=hr))
+
+    df = pd.DataFrame(rows)
+    (fig, ax) = plt.subplots(1, 1)
+    acount = np.average(cnts)
+    years = today.year - minvalid.year
+    arc = (np.array(cnts)-acount) / float(years)
+    maxv = max([0 - np.min(arc), np.max(arc)])
+    l = ax.plot(range(24), arc, color='b', label='Days Bias')
+    ax.set_ylim(0 - maxv - 0.2, maxv + 0.2)
+
+    ax2 = ax.twinx()
+    aavg = np.average(avgv)
+    aavgv = np.array(avgv) - aavg
+    l2 = ax2.plot(range(24), aavgv, color='r')
+    maxv = max([0 - np.min(aavgv), np.max(aavgv)])
+    ax2.set_ylim(0 - maxv - 0.01, maxv + 0.01)
+    ax2.set_ylabel("Bias with Average 24 Hour Precip [in/day]", color='r')
+    ax.set_title(("[%s] %s %s-%s\n"
+                  "Bias of 24 Hour 'Day' Split for Precipitation"
+                  ) % (station, nt.sts[station]['name'],
+                       minvalid.year,
+                       datetime.date.today().year))
+    ax.set_ylabel("Bias of Days per Year with Precip", color='b')
+    ax.set_xlim(0, 24)
+    ax.set_xticks((0, 4, 8, 12, 16, 20, 24))
+    ax.set_xticklabels(('Mid', '4 AM', '8 AM', 'Noon', '4 PM', '8 PM', 'Mid'))
+    ax.grid(True)
+    ax.set_xlabel(("Hour Used for 24 Hour Summary, Timezone: %s"
+                   ) % (nt.sts[station]['tzname'], ))
+    box = ax.get_position()
+    ax.set_position([box.x0, box.y0,
+                     box.width * .95, box.height])
+    ax2.set_position([box.x0, box.y0,
+                     box.width * .95, box.height])
+    ax.legend([l[0], l2[0]], ['Days Bias', 'Avg Precip Bias'], loc='best',
+              fontsize=10)
+    return fig, df
