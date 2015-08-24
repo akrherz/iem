ec54ab9b517f07fc4fb2be777ed2c2fd40ccb9c6
akrherz <akrherz@iastate.edu>
2015-08-24 15:31:46 -0500

    non-func cleanups of dl/ folder

--- scripts/dl/archive_composite.py
+++ scripts/dl/archive_composite.py
@@ -1,15 +1,13 @@
-'''
-  Regenerate composites to fulfill various reasons
-'''
+"""Regenerate composites to fulfill various reasons"""
 
 from radar_composite import save
 import datetime
 import sys
 
 sts = datetime.datetime(int(sys.argv[1]), int(sys.argv[2]),
-            int(sys.argv[3]), int(sys.argv[4]), int(sys.argv[5]))
+                        int(sys.argv[3]), int(sys.argv[4]), int(sys.argv[5]))
 ets = datetime.datetime(int(sys.argv[6]), int(sys.argv[7]),
-            int(sys.argv[8]), int(sys.argv[9]), int(sys.argv[10]))
+                        int(sys.argv[8]), int(sys.argv[9]), int(sys.argv[10]))
 interval = datetime.timedelta(minutes=5)
 now = sts
 while (now < ets):
@@ -17,6 +15,6 @@ while (now < ets):
     s = now.strftime("%Y%m%d%H%M")
     save('conus', 'uscomp.png', 'usrad', s, routes='a')
     save('iem', 'mwcomp.png', 'comprad', s, routes='a')
-    for i in ['lot','ict','sd','hun']:
-        save(i, '%scomp.png'%(i,), '%srad' %(i,), s, routes='a')
+    for i in ['lot', 'ict', 'sd', 'hun']:
+        save(i, '%scomp.png' % (i, ), '%srad' % (i,), s, routes='a')
     now += interval
