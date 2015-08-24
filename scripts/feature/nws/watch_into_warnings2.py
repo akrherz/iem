0cfe3be54b53dd75c8e5fde6e83aeb724ca3cac6
akrherz <akrherz@iastate.edu>
2015-08-24 09:47:14 -0500

    tweaking of offline plot

--- scripts/feature/nws/watch_into_warnings2.py
+++ scripts/feature/nws/watch_into_warnings2.py
@@ -23,18 +23,18 @@ combo2 as (
 
 """
 
-data = """   1 |   1 |    26
-  2 |   3 |    80
-  3 |  16 |   222
-  4 |  17 |   521
-  5 |  18 |   782
-  6 |  37 |  1297
-  7 |  15 |   885
-  8 |  22 |   637
-  9 |   6 |   208
- 10 |   5 |   112
- 11 |   3 |    37
- 12 |   6 |    37"""
+data = """  1 |  6622 |   7453
+  2 |  9850 |  12204
+  3 | 17635 |  24297
+  4 | 43851 |  59998
+  5 | 54773 |  81612
+  6 | 69593 | 120149
+  7 | 51334 |  84414
+  8 | 36807 |  55814
+  9 | 13021 |  20040
+ 10 | 10953 |  13955
+ 11 |  5205 |   6348
+ 12 |  5462 |   6468"""
 
 ratio = []
 h, a = 0, 0
@@ -42,7 +42,10 @@ for line in data.split("\n"):
   tokens = line.split("|")
   h += float(tokens[1])
   a += float(tokens[2])
-  ratio.append( float(tokens[1]) / float(tokens[2]) * 100.)
+  if float(tokens[2]) == 0:
+    ratio.append(0)
+  else:
+    ratio.append( float(tokens[1]) / float(tokens[2]) * 100.)
 
 import matplotlib.pyplot as plt
 import calendar
@@ -52,12 +55,13 @@ import numpy as np
 ax.bar(np.arange(1, 13)-0.4, ratio)
 ax.set_xlim(0.5, 12.5)
 for i, r in enumerate(ratio):
-  ax.text(i+1, r+1, "%.1f%%" % (r, ), ha='center')
-ax.set_ylim(0, 33)
-ax.set_yticks(np.arange(0, 31, 5))
+  if r > 0:
+    ax.text(i+1, r+1, "%.1f%%" % (r, ), ha='center')
+ax.set_ylim(0, 100)
+ax.set_yticks(np.arange(0, 101, 25))
 ax.grid(True)
-ax.set_title(("1 Oct 2005 - 22 Aug 2015 Percentage of SPC Svr Tstorm Watches"
-              "\nthat receive no Svr Tstorm Warnings, Overall (%.0f/%.0f %.1f%%)"
+ax.set_title(("1 Oct 2005 - 22 Aug 2015 Percent of NWS County Svr Tstorm Warnings"
+              "\nthat no SPC Svr Tstorm Watch Active, Overall (%.0f/%.0f %.1f%%)"
               ) % (h, a, h / a * 100.))
 ax.set_xticks(range(1, 13))
 ax.set_xticklabels(calendar.month_abbr[1:])
