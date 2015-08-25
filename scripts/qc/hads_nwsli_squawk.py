3fc538dcbfabea0d3f7eedbc9366081865acef52
akrherz <akrherz@iastate.edu>
2015-08-25 07:28:55 -0500

    include 7 oldest unknown NWSLIs in daily squawking too

--- scripts/qc/hads_nwsli_squawk.py
+++ scripts/qc/hads_nwsli_squawk.py
@@ -10,28 +10,30 @@ hcursor = HADS.cursor()
 hcursor2 = HADS.cursor()
 acursor = ACCESS.cursor()
 
-print '----- Unknown NWSLIs from DCPish sites -----'
-hcursor.execute("""
- select nwsli, count(*) as tot, max(product),
- count(distinct substr(product,1,8)),
- count(distinct product),
- sum(case when substr(nwsli,4,2) = 'I4' then 1 else 0 end) as priority
- from unknown
- where nwsli ~ '^[A-Z]{4}[0-9]$'
- GROUP by nwsli ORDER by priority DESC, tot DESC LIMIT 7
-""")
-for row in hcursor:
-    print '%7s Tot:%4s Days:%2s Products: %s %s' % (row[0], row[1], row[3],
-                                                    row[4], row[2])
-    # Get vars reported for this site
-    acursor.execute("""SELECT valid, physical_code || duration || source ||
-    extremum || probability as p, value from current_shef WHERE station = %s
-    ORDER by p ASC""", (row[0],))
-    for row2 in acursor:
-        print '    %s %s %s' % (row2[0], row2[1], row2[2])
-    hcursor2.execute("""
-    DELETE from unknown where nwsli = %s
-    """, (row[0],))
+print('----- Unknown NWSLIs from DCPish sites -----')
+for priority in ['priority DESC', 'maxp ASC']:
+    hcursor.execute("""
+     select nwsli, count(*) as tot, max(product) as maxp,
+     count(distinct substr(product,1,8)),
+     count(distinct product),
+     sum(case when substr(nwsli,4,2) = 'I4' then 1 else 0 end) as priority
+     from unknown
+     where nwsli ~ '^[A-Z]{4}[0-9]$'
+     GROUP by nwsli ORDER by """ + priority + """, tot DESC LIMIT 7
+    """)
+    for row in hcursor:
+        print '%7s Tot:%4s Days:%2s Products: %s %s' % (row[0], row[1], row[3],
+                                                        row[4], row[2])
+        # Get vars reported for this site
+        acursor.execute("""
+            SELECT valid, physical_code || duration || source ||
+            extremum || probability as p, value from current_shef
+            WHERE station = %s ORDER by p ASC""", (row[0],))
+        for row2 in acursor:
+            print '    %s %s %s' % (row2[0], row2[1], row2[2])
+        hcursor2.execute("""
+        DELETE from unknown where nwsli = %s
+        """, (row[0],))
 
 print '----- Unknown NWSLIs from COOPish sites -----'
 hcursor.execute("""
