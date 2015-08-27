cba579c287a60205ea83c75c7d7661de6ef05e81
akrherz <akrherz@iastate.edu>
2015-08-27 12:30:44 -0500

    drive climodat snow/snowd off of IEMRE grids

--- scripts/iemre/init_daily.py
+++ scripts/iemre/init_daily.py
@@ -3,7 +3,7 @@
 from pyiem import iemre
 import netCDF4
 import datetime
-import numpy
+import numpy as np
 import sys
 
 
@@ -34,44 +34,44 @@ def init_year(ts):
     nc.createDimension('time', int(days))
 
     # Setup Coordinate Variables
-    lat = nc.createVariable('lat', numpy.float, ('lat',))
+    lat = nc.createVariable('lat', np.float, ('lat',))
     lat.units = "degrees_north"
     lat.long_name = "Latitude"
     lat.standard_name = "latitude"
     lat.axis = "Y"
     lat[:] = iemre.YAXIS
 
-    lon = nc.createVariable('lon', numpy.float, ('lon',))
+    lon = nc.createVariable('lon', np.float, ('lon',))
     lon.units = "degrees_east"
     lon.long_name = "Longitude"
     lon.standard_name = "longitude"
     lon.axis = "X"
     lon[:] = iemre.XAXIS
 
-    tm = nc.createVariable('time', numpy.float, ('time',))
+    tm = nc.createVariable('time', np.float, ('time',))
     tm.units = "Days since %s-01-01 00:00:0.0" % (ts.year,)
     tm.long_name = "Time"
     tm.standard_name = "time"
     tm.axis = "T"
     tm.calendar = "gregorian"
-    tm[:] = numpy.arange(0, int(days))
+    tm[:] = np.arange(0, int(days))
 
     # Tracked variables
-    high = nc.createVariable('high_tmpk', numpy.float, ('time', 'lat', 'lon'),
+    high = nc.createVariable('high_tmpk', np.float, ('time', 'lat', 'lon'),
                              fill_value=1.e20)
     high.units = "K"
     high.long_name = "2m Air Temperature Daily High"
     high.standard_name = "2m Air Temperature"
     high.coordinates = "lon lat"
 
-    low = nc.createVariable('low_tmpk', numpy.float, ('time', 'lat', 'lon'),
+    low = nc.createVariable('low_tmpk', np.float, ('time', 'lat', 'lon'),
                             fill_value=1.e20)
     low.units = "K"
     low.long_name = "2m Air Temperature Daily Low"
     low.standard_name = "2m Air Temperature"
     low.coordinates = "lon lat"
 
-    high12 = nc.createVariable('high_tmpk_12z', numpy.float,
+    high12 = nc.createVariable('high_tmpk_12z', np.float,
                                ('time', 'lat', 'lon'),
                                fill_value=1.e20)
     high12.units = "K"
@@ -79,7 +79,7 @@ def init_year(ts):
     high12.standard_name = "2m Air Temperature"
     high12.coordinates = "lon lat"
 
-    low12 = nc.createVariable('low_tmpk_12z', numpy.float,
+    low12 = nc.createVariable('low_tmpk_12z', np.float,
                               ('time', 'lat', 'lon'),
                               fill_value=1.e20)
     low12.units = "K"
@@ -87,7 +87,7 @@ def init_year(ts):
     low12.standard_name = "2m Air Temperature"
     low12.coordinates = "lon lat"
 
-    p01d = nc.createVariable('p01d', numpy.float, ('time', 'lat', 'lon'),
+    p01d = nc.createVariable('p01d', np.float, ('time', 'lat', 'lon'),
                              fill_value=1.e20)
     p01d.units = 'mm'
     p01d.long_name = 'Precipitation'
@@ -95,7 +95,7 @@ def init_year(ts):
     p01d.coordinates = "lon lat"
     p01d.description = "Precipitation accumulation for the day"
 
-    p01d12 = nc.createVariable('p01d_12z', numpy.float, ('time', 'lat', 'lon'),
+    p01d12 = nc.createVariable('p01d_12z', np.float, ('time', 'lat', 'lon'),
                                fill_value=1.e20)
     p01d12.units = 'mm'
     p01d12.long_name = 'Precipitation'
@@ -103,13 +103,29 @@ def init_year(ts):
     p01d12.coordinates = "lon lat"
     p01d12.description = "24 Hour Precipitation Ending 12 UTC"
 
-    rsds = nc.createVariable('rsds', numpy.float, ('time', 'lat', 'lon'),
+    rsds = nc.createVariable('rsds', np.float, ('time', 'lat', 'lon'),
                              fill_value=1.e20)
     rsds.units = "W m-2"
     rsds.long_name = 'surface_downwelling_shortwave_flux_in_air'
     rsds.standard_name = 'surface_downwelling_shortwave_flux_in_air'
     rsds.coordinates = "lon lat"
     rsds.description = "Global Shortwave Irradiance"
+
+    snow = nc.createVariable('snow_12z', np.float, ('time', 'lat', 'lon'),
+                             fill_value=1.e20)
+    snow.units = 'mm'
+    snow.long_name = 'Snowfall'
+    snow.standard_name = 'Snowfall'
+    snow.coordinates = "lon lat"
+    snow.description = "Snowfall accumulation for the day"
+
+    snowd = nc.createVariable('snowd_12z', np.float, ('time', 'lat', 'lon'),
+                              fill_value=1.e20)
+    snowd.units = 'mm'
+    snowd.long_name = 'Snow Depth'
+    snowd.standard_name = 'Snow Depth'
+    snowd.coordinates = "lon lat"
+    snowd.description = "Snow depth at time of observation"
 
     nc.close()
 
