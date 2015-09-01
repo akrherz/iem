03ba32b80f30c8f0e0cd376169847964b91c8320
akrherz <akrherz@iastate.edu>
2015-09-01 08:27:36 -0500

    pep8 fixes and make DOT AWOS QC ingest more forgiving

--- scripts/ingestors/awos/parse_monthly_maint.py
+++ scripts/ingestors/awos/parse_monthly_maint.py
@@ -1,20 +1,23 @@
 """
  Parse the monthly maint file I get from the DOT
- 
-  id         | integer                  | not null default nextval('iem_calibrati
-on_id_seq'::regclass)
- station    | character varying(10)    | 
- portfolio  | character varying(10)    | 
- valid      | timestamp with time zone | 
- parameter  | character varying(10)    | 
- adjustment | real                     | 
- final      | real                     | 
- comments   | text                     | 
 
- 
+  id         | integer | 
+ station    | character varying(10)    |
+ portfolio  | character varying(10)    |
+ valid      | timestamp with time zone |
+ parameter  | character varying(10)    |
+ adjustment | real                     |
+ final      | real                     |
+ comments   | text                     |
+
 """
+import sys
+import re
+import mx.DateTime
+import psycopg2
 
-class bcolors:
+
+class bcolors(object):
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
     OKGREEN = '\033[92m'
@@ -22,15 +25,13 @@ class bcolors:
     FAIL = '\033[91m'
     ENDC = '\033[0m'
 
-import sys
-import re
-import mx.DateTime
-import psycopg2
-# Use SSH proxy 
-PORTFOLIO = psycopg2.connect("dbname=portfolio host=localhost port=5555 user=nobody")
+# Use SSH proxy
+PORTFOLIO = psycopg2.connect(dbname="portfolio", host="localhost",
+                             port=5555, user="nobody")
 pcursor = PORTFOLIO.cursor()
 
-CALINFO = re.compile(".*Calibrated? T/DP: AWOS:?\s*([0-9\-\.]+)/([0-9\-\.]+) Std:?\.? ([0-9\-\.]+)/([0-9\-\.]+)")
+CALINFO = re.compile((".* T/DP: AWOS:?\s*([0-9\-\.]+)/([0-9\-\.]+) "
+                      "Std:?\.? ([0-9\-\.]+)/([0-9\-\.]+)"))
 
 data = sys.stdin.read()
 
@@ -42,12 +43,12 @@ for line in data.split("\n"):
     if len(faa) != 3:
         continue
     date = mx.DateTime.strptime(tokens[1], '%d-%b-%y')
-    
+
     parts = re.findall(CALINFO, tokens[3])
     if len(parts) == 0:
         print bcolors.FAIL + tokens[3] + bcolors.ENDC
         continue
-    
+
     sql = """INSERT into iem_calibration(station, portfolio, valid, parameter,
     adjustment, final, comments) values (%s, 'iaawos', %s, %s, %s, %s, %s)"""
     tempadj = float(parts[0][2]) - float(parts[0][0])
@@ -55,17 +56,18 @@ for line in data.split("\n"):
             parts[0][2], tokens[3].replace('"', ''))
     if len(sys.argv) > 1:
         pcursor.execute(sql, args)
-    
+
     dewpadj = float(parts[0][3]) - float(parts[0][1])
-    args = (faa, date.strftime("%Y-%m-%d"), 'dwpf', float(parts[0][3]) - float(parts[0][1]),
+    args = (faa, date.strftime("%Y-%m-%d"), 'dwpf',
+            float(parts[0][3]) - float(parts[0][1]),
             parts[0][3], tokens[3].replace('"', ''))
     if len(sys.argv) > 1:
         pcursor.execute(sql, args)
-    
-    print '--> %s [%s] TMPF: %s (%s) DWPF: %s (%s)' % (faa, tokens[1],
-                                                   parts[0][2], tempadj,
-                                                   parts[0][3], dewpadj)
-    
+
+    print(('--> %s [%s] TMPF: %s (%s) DWPF: %s (%s)'
+           ) % (faa, tokens[1], parts[0][2], tempadj,
+                parts[0][3], dewpadj))
+
 if len(sys.argv) == 1:
     print 'WARNING: Disabled, call with arbitrary argument to enable'
 else:
