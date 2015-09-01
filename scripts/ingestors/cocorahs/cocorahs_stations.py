2c7216e923a291585bf71f126d342db31e515578
akrherz <akrherz@iastate.edu>
2015-09-01 10:36:34 -0500

    include printout of blank station names as per daily squawking

--- scripts/ingestors/cocorahs/cocorahs_stations.py
+++ scripts/ingestors/cocorahs/cocorahs_stations.py
@@ -1,8 +1,7 @@
-'''
-Hit cocorah's website API for a listing of stations and add entries for anything
-new found
-'''
-
+"""
+Hit cocorah's website API for a listing of stations and add entries for
+anything new found
+"""
 import urllib2
 import sys
 import psycopg2
@@ -40,22 +39,23 @@ for row in data[1:]:
     if sid in stations:
         continue
 
-    name = cols[ header["StationName"] ].strip().replace("'",' ')
-    cnty = cols[ header["County"] ].strip().replace("'",' ')
-    lat = float(cols[ header["Latitude"] ].strip())
-    lon = float(cols[ header["Longitude"] ].strip())
+    name = cols[header["StationName"]].strip().replace("'", ' ')
+    cnty = cols[header["County"]].strip().replace("'", ' ')
+    lat = float(cols[header["Latitude"]].strip())
+    lon = float(cols[header["Longitude"]].strip())
 
     if lat < 10 or lon > -60 or name == '':
         continue
 
-    print "ADD COCORAHS SID:%s Name:%s County:%s %.3f %.3f" %  (
-                                                sid, name, cnty, lat, lon)
-  
-    sql = """INSERT into stations(id, synop, name, state, country, network, 
-        online, geom, county, plot_name , metasite) 
+    print(("ADD COCORAHS SID:%s Name:%s County:%s %.3f %.3f"
+           ) % (sid, name, cnty, lat, lon))
+
+    sql = """
+        INSERT into stations(id, synop, name, state, country, network,
+        online, geom, county, plot_name , metasite)
         VALUES ('%s', 99999, '%s', '%s', 'US', '%sCOCORAHS', 't',
-        'SRID=4326;POINT(%s %s)', '%s', '%s', 'f')""" % (sid, name,
-        state, state, lon, lat, cnty, name)
+        'SRID=4326;POINT(%s %s)', '%s', '%s', 'f')
+        """ % (sid, name, state, state, lon, lat, cnty, name)
     try:
         mcursor = MESOSITE.cursor()
         mcursor.execute(sql)
@@ -65,11 +65,11 @@ for row in data[1:]:
         mcursor.close()
         MESOSITE.commit()
         mcursor = MESOSITE.cursor()
-        sql = """UPDATE stations SET geom = 'SRID=4326;POINT(%s %s)',
+        sql = """
+            UPDATE stations SET geom = 'SRID=4326;POINT(%s %s)',
             name = '%s', plot_name = '%s'
-           WHERE id = '%s' and network = '%sCOCORAHS'""" % (lon, lat,
-           name, name, sid, state)
-        mcursor.execute( sql )
+            WHERE id = '%s' and network = '%sCOCORAHS'
+        """ % (lon, lat, name, name, sid, state)
+        mcursor.execute(sql)
         mcursor.close()
         MESOSITE.commit()
-
