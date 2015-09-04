8846d21ee5131df073f994b301c51bb5201238bf
akrherz <akrherz@iastate.edu>
2015-09-03 21:31:43 -0500

    4 Sep feature is table of last hdx for Sept

--- scripts/feature/asos/list_records.py
+++ scripts/feature/asos/list_records.py
@@ -1,45 +1,50 @@
 from pyiem.network import Table as NetworkTable
 import psycopg2
-from pyiem.datatypes import pressure
-pgconn = psycopg2.connect(database='asos', host='iemdb', user='nobody')
+from pyiem.datatypes import pressure, temperature
+from pyiem.meteorology import heatindex
+pgconn = psycopg2.connect(database='asos', host='localhost', port=5555,
+                          user='nobody')
 cursor = pgconn.cursor()
 
-nt = NetworkTable("IA_ASOS")
+nt = NetworkTable(["IA_ASOS", "AWOS"])
 ids = nt.sts.keys()
 ids.sort()
 
 print """
 <table class="table table-condensed table-striped">
 <thead>
-<tr><th>ID</th><th>Station Name</th><th>Data Since</th>
-<th>Altimeter [inch]</th><th>Sea Level Pressure [mb]</th></tr>
+<tr><th>ID</th><th>Station Name</th><th>3 Sep Peak Heat Index</th>
+<th>Last Highest</th><th>Date</th></tr>
 </thead>
 """
 
+bah = nt.sts.keys()
 
 for sid in ids:
-    if nt.sts[sid]['archive_begin'].year > 1972:
-        continue
     cursor.execute("""
-    select extract(year from valid) as yr, max(dwpf) from alldata where
-    station = %s and extract(month from valid) < 4 
-    and dwpf is not null GROUP by yr 
-    ORDER by max ASC
+    select valid, tmpf, dwpf from alldata where
+    station = %s and extract(month from valid) = 9
+    and dwpf is not null and tmpf > 84 and valid > '1990-01-01'
+    ORDER by valid DESC
     """, (sid,))
-    
-    maxval = None
-    thisyear = None
-    years = []
+
+    thisdate = [0, None, None, None]
     for i, row in enumerate(cursor):
-        val = round(row[1])
-        if i == 0:
-            maxval = val
-        if row[0] == 2015:
-            thisyear = val
-        if val == maxval:
-            years.append("%.0f" % (row[0],))
-        
-    print '%s,%s,%s,%.0f,%.0f,%s' % (sid, nt.sts[sid]['name'], 
-          nt.sts[sid]['archive_begin'].year, thisyear,
-                                  maxval, "-".join(years))   
+        hdx = heatindex(temperature(row[1], 'F'),
+                        temperature(row[2], 'F')).value('F')
+        if row[0].strftime("%Y%m%d") == '20150903':
+            if hdx > thisdate[0]:
+                thisdate = [hdx, row[0], row[1], row[2]]
+            continue
+        if thisdate[1] is None:
+            break
+        if hdx >= thisdate[0]:
+            bah.remove(sid)
+            print(('%s,%s,%.0f,(%.0f/%.0f),%.0f,(%.0f/%.0f),%s'
+                   ) % (sid, nt.sts[sid]['name'],
+                        thisdate[0], thisdate[2], thisdate[3],
+                        hdx, row[1], row[2],
+                        row[0].strftime("%d %b %Y %I:%M %p")))
+            break
+print 'missed', bah
 print "</table>"
