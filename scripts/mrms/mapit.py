0ba970e06275d81ab6596bd180d1fa9943fa5ea0
akrherz <akrherz@iastate.edu>
2015-09-08 05:19:14 -0500

    8 Sep is a plot of 48h MRMS totals

--- scripts/mrms/mapit.py
+++ scripts/mrms/mapit.py
@@ -9,7 +9,7 @@ cursor = pgconn.cursor()
 cursor.execute("""SELECT ST_x(geom) as lon, ST_y(geom) as lat,
 max(magnitude) from lsrs_2015
 where wfo in ('DMX', 'DVN', 'ARX') and typetext = 'HEAVY RAIN' and
-valid > 'TODAY' GROUP by lon, lat ORDER by max DESC""")
+valid > '2016-01-01' GROUP by lon, lat ORDER by max DESC""")
 llons = []
 llats = []
 vals = []
@@ -19,7 +19,7 @@ for row in cursor:
     vals.append("%.2f" % (row[2], ))
 
 
-img = Image.open("p24h_201508290100.png")
+img = Image.open("p48h_201509081000.png")
 data = np.flipud(np.asarray(img))
 # 7000,3500 == -130,-60,55,25 ===  -100 to -90 then 38 to 45
 sample = data[1800:2501, 3000:4501]
@@ -33,11 +33,11 @@ lats = np.arange(38, 45.01, 0.01)
 x, y = np.meshgrid(lons, lats)
 
 buff = 0.5
-m = MapPlot(sector='custom', west=min(llons)-buff,
-            east=max(llons)+buff, south=min(llats)-buff,
-            north=max(llats)+buff,
-            title='NOAA MRMS 24 Hour RADAR-Only Precipitation Estimate',
-            subtitle='Local Storm Reports Overlaid, MRMS valid 8 PM 27 Aug 2015 to 8 PM 27 Aug 2015')
+m = MapPlot(sector='iowa', #west=min(llons)-buff,
+            #east=max(llons)+buff, south=min(llats)-buff,
+            #north=max(llats)+buff,
+            title='NOAA MRMS 48 Hour RADAR-Only Precipitation Estimate',
+            subtitle='MRMS valid 5 AM 6 Sep 2015 to 5 AM 8 Sep 2015')
 clevs = np.arange(0, 0.2, 0.05)
 clevs = np.append(clevs, np.arange(0.2, 1.0, 0.1))
 clevs = np.append(clevs, np.arange(1.0, 5.0, 0.25))
@@ -54,8 +54,8 @@ for sid in nt.sts.keys():
     la.append(nt.sts[sid]['lat'])
     va.append(nt.sts[sid]['name'])
 
-m.plot_values(lo, la, va, fmt='%s', textsize=10, color='black')
-m.plot_values(llons, llats, vals, fmt='%s')
+#m.plot_values(lo, la, va, fmt='%s', textsize=10, color='black')
+#m.plot_values(llons, llats, vals, fmt='%s')
 
 m.map.drawcounties(zorder=10, linewidth=1.)
 
