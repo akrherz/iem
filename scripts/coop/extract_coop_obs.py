5a16fc5d449e6de6baed10947f205220e69814b7
akrherz <akrherz@iastate.edu>
2015-09-12 16:40:29 -0500

    pep8 fixes and better support db nulls, refs #54

--- scripts/coop/extract_coop_obs.py
+++ scripts/coop/extract_coop_obs.py
@@ -3,7 +3,6 @@
 import mx.DateTime
 import shapelib
 import dbflib
-import psycopg2
 import psycopg2.extras
 import subprocess
 import os
@@ -18,56 +17,58 @@ yyyy = now.strftime('%Y')
 
 cob = {}
 
-icursor.execute("""SELECT s.id, c.pday, 
-  coalesce(c.snow,-99) as snow, coalesce(c.snowd,-99) as snowd, 
-  c.max_tmpf, 
-	case when c.min_tmpf < 99 THEN c.min_tmpf ELSE -99 END as min_tmpf, 
-	ST_x(s.geom) as lon, ST_y(s.geom) as lat, s.name, 
-	c2.valid at time zone 'UTC' as vvv, s.elevation
-	from summary_%s c, current c2, stations s WHERE 
-	c.iemid = c2.iemid and s.iemid = c.iemid and 
-    s.network ~* 'COOP' and min_tmpf > -99 and c2.valid > 'TODAY' 
-	and day = 'TODAY'""" % (yyyy,))
+icursor.execute("""SELECT s.id, coalesce(c.pday, 0) as pday,
+    coalesce(c.snow,-99) as snow, coalesce(c.snowd,-99) as snowd,
+    coalesce(c.max_tmpf, -99) as max_tmpf,
+    case when c.min_tmpf < 99 THEN c.min_tmpf ELSE -99 END as min_tmpf,
+    ST_x(s.geom) as lon, ST_y(s.geom) as lat, s.name,
+    c2.valid at time zone 'UTC' as vvv, s.elevation
+    from summary_%s c, current c2, stations s WHERE
+    c.iemid = c2.iemid and s.iemid = c.iemid and
+    s.network ~* 'COOP' and min_tmpf > -99 and c2.valid > 'TODAY'
+    and day = 'TODAY'""" % (yyyy,))
 
 for row in icursor:
     thisStation = row["id"]
-    cob[ thisStation ] = {}
+    cob[thisStation] = {}
     thisPrec = row["pday"]
     thisSnow = row["snow"]
     thisSnowD = row["snowd"]
-    thisHigh  = row["max_tmpf"]
-    thisLow   = row["min_tmpf"]
+    thisHigh = row["max_tmpf"]
+    thisLow = row["min_tmpf"]
 
     # First we update our cobs dictionary
-    cob[ thisStation ]["TMPX"] = float(thisHigh)
-    cob[ thisStation ]["TMPN"] = float(thisLow)
-    cob[ thisStation ]["P24I"] = round(float(thisPrec),2)
-    cob[ thisStation ]["SNOW"] = float(thisSnow)
-    cob[ thisStation ]["SNOD"] = float(thisSnowD)
-    cob[ thisStation ]["LAT"] = row['lat']
-    cob[ thisStation ]["LON"] = row['lon']
-    cob[ thisStation ]["NAME"] = row['name']
-    cob[ thisStation ]['ELEV_M'] = row['elevation']
-    cob[ thisStation ]["PMOI"] = 0.
-    cob[ thisStation ]["SMOI"] = 0.
+    cob[thisStation]["TMPX"] = float(thisHigh)
+    cob[thisStation]["TMPN"] = float(thisLow)
+    cob[thisStation]["P24I"] = round(float(thisPrec), 2)
+    cob[thisStation]["SNOW"] = float(thisSnow)
+    cob[thisStation]["SNOD"] = float(thisSnowD)
+    cob[thisStation]["LAT"] = row['lat']
+    cob[thisStation]["LON"] = row['lon']
+    cob[thisStation]["NAME"] = row['name']
+    cob[thisStation]['ELEV_M'] = row['elevation']
+    cob[thisStation]["PMOI"] = 0.
+    cob[thisStation]["SMOI"] = 0.
     cob[thisStation]['HHMM'] = row['vvv'].strftime('%H%M')
 
-icursor.execute("""SELECT t.id, sum(pday) as tprec, 
-	sum( case when snow > 0 THEN snow ELSE 0 END) as tsnow 
-	from summary_%s s, stations t WHERE 
-  date_part('month', day) = date_part('month', CURRENT_TIMESTAMP::date) 
-	and date_part('year', day) = %s 
-  and pday >= 0.00 and t.network ~* 'COOP' and t.iemid = s.iemid GROUP by id""" % (
-								now.year, now.year) )
+icursor.execute("""
+    SELECT t.id, sum(pday) as tprec,
+    sum( case when snow > 0 THEN snow ELSE 0 END) as tsnow
+    from summary_%s s, stations t WHERE
+    date_part('month', day) = date_part('month', CURRENT_TIMESTAMP::date)
+    and date_part('year', day) = %s
+    and pday >= 0.00 and t.network ~* 'COOP' and t.iemid = s.iemid
+    GROUP by id
+    """ % (now.year, now.year))
 
 for row in icursor:
     thisStation = row["id"]
     thisPrec = row["tprec"]
     thisSnow = row["tsnow"]
-    if not cob.has_key(thisStation):
+    if thisStation not in cob:
         continue
-    cob[ thisStation ]["PMOI"] = round(float(thisPrec),2)
-    cob[ thisStation ]["SMOI"] = round(float(thisSnow),2)
+    cob[thisStation]["PMOI"] = round(float(thisPrec), 2)
+    cob[thisStation]["SMOI"] = round(float(thisSnow), 2)
 
 csv = open('coop.csv', 'w')
 csv.write('nwsli,site_name,longitude,latitude,date,time,high_f,low_f,prec_in,')
@@ -92,10 +93,10 @@ shp = shapelib.create("coop_"+ts, shapelib.SHPT_POINT)
 j = 0
 for sid in cob.keys():
 
-    obj = shapelib.SHPObject(shapelib.SHPT_POINT, j, 
-		[[(cob[sid]["LON"], cob[sid]["LAT"])]] )
+    obj = shapelib.SHPObject(shapelib.SHPT_POINT, j,
+                             [[(cob[sid]["LON"], cob[sid]["LAT"])]])
     shp.write_object(-1, obj)
-    #print id, cob[sid]
+    # print id, cob[sid]
     if cob[sid]["TMPX"] < 0:
         cob[sid]["TMPX"] = -99.
     if cob[sid]["TMPN"] < 0:
@@ -110,18 +111,20 @@ for sid in cob.keys():
         cob[sid]["PMOI"] = -99.
     if cob[sid]["SMOI"] < 0:
         cob[sid]["SMOI"] = -99.
-    #print cob[sid], id
-    dbf.write_record(j, (sid, cob[sid]["NAME"], cob[sid]['ELEV_M'], ts,"1200",
-		cob[sid]["TMPX"], cob[sid]["TMPN"], cob[sid]["P24I"], 
-		cob[sid]["SNOW"], cob[sid]["SNOD"], cob[sid]["PMOI"], 
-		cob[sid]["SMOI"]) )
+    # print cob[sid], id
+    dbf.write_record(j, (sid, cob[sid]["NAME"], cob[sid]['ELEV_M'], ts, "1200",
+                         cob[sid]["TMPX"], cob[sid]["TMPN"], cob[sid]["P24I"],
+                         cob[sid]["SNOW"], cob[sid]["SNOD"], cob[sid]["PMOI"],
+                         cob[sid]["SMOI"]))
 
-    csv.write("%s,%s,%.4f,%.4f,%s,%s,%s,%s,%s,%s,%s,%s,%s,%.1f\n" % (sid, 
-            cob[sid]["NAME"].replace(","," "), cob[sid]['LON'], cob[sid]["LAT"],
-            ts, cob[sid]['HHMM'], cob[sid]["TMPX"], cob[sid]["TMPN"], cob[sid]["P24I"],
-            cob[sid]["SNOW"], cob[sid]["SNOD"], cob[sid]["PMOI"], 
-            cob[sid]["SMOI"], cob[sid]['ELEV_M']))
-    
+    csv.write(("%s,%s,%.4f,%.4f,%s,%s,%s,%s,%s,%s,%s,%s,%s,%.1f\n"
+               ) % (sid, cob[sid]["NAME"].replace(",", " "),
+                    cob[sid]['LON'], cob[sid]["LAT"],
+                    ts, cob[sid]['HHMM'], cob[sid]["TMPX"], cob[sid]["TMPN"],
+                    cob[sid]["P24I"],
+                    cob[sid]["SNOW"], cob[sid]["SNOD"], cob[sid]["PMOI"],
+                    cob[sid]["SMOI"], cob[sid]['ELEV_M']))
+
     del(obj)
     j += 1
 
@@ -133,6 +136,6 @@ IEM.close()
 csv.close()
 pqstr = "plot c %s csv/coop.csv bogus csv" % (
                             mx.DateTime.now().strftime("%Y%m%d%H%M"),)
-subprocess.call("/home/ldm/bin/pqinsert -p '%s' coop.csv" % (pqstr,), 
+subprocess.call("/home/ldm/bin/pqinsert -p '%s' coop.csv" % (pqstr,),
                 shell=True)
 os.unlink('coop.csv')
