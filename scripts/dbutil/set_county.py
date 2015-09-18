eda593aa1d19b370615a666d4e44839610bf9320
akrherz <akrherz@iastate.edu>
2015-09-18 09:21:02 -0500

    use closest county for stations may be slightly offshore

--- scripts/dbutil/set_county.py
+++ scripts/dbutil/set_county.py
@@ -19,6 +19,11 @@ class bcolors:
     ENDC = '\033[0m'
 
 
+def msg(text):
+    if text == 'OK':
+        return "%s[ OK ]%s" % (bcolors.OKGREEN, bcolors.ENDC)
+
+
 # Get listing of stations without a county set
 mcursor.execute("""SELECT iemid, id, name, network, ST_x(geom), ST_y(geom),
     state from stations WHERE (county is null or ugc_county is null
@@ -47,6 +52,23 @@ for row in mcursor:
                "site.php?station=%s&network=%s"
                "") % (bcolors.FAIL, bcolors.ENDC, station, network, state,
                       lon, lat, station, network))
+        # Okay, so perhaps this site is offshore slightly, lets look for the
+        # nearest county
+        pcursor.execute("""
+        SELECT ugc, name,
+        ST_Distance(geom, ST_GeomFromText('Point(%s %s)',4326)) as d
+        from ugcs WHERE substr(ugc,1,3) = '%s' and end_ts is null and
+        ST_Distance(geom, ST_GeomFromText('Point(%s %s)',4326)) < 0.5
+        ORDER by d ASC LIMIT 1
+        """ % (lon, lat, state + 'C', lon, lat))
+        if pcursor.rowcount == 1:
+            (ugc, ugcname, dist) = pcursor.fetchone()
+            print(("%s [cnty_nearest] ID: %s Net: %s: St: %s "
+                   "UGC: %s Dist: %.2f"
+                   ) % (msg('OK'), station, network, state, ugc, dist))
+            mcursor2.execute("""
+            UPDATE stations SET county = %s, ugc_county = %s
+            WHERE iemid = %s""", (ugcname, ugc, iemid))
     else:
         (ugc, ugcname) = pcursor.fetchone()
         print(("%s[ OK ]%s IEMID: %s SID: %s Network: %s to county: %s [%s]"
