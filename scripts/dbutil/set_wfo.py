52fef62459b325430a8ecfe0437961600ee8c5cb
akrherz <akrherz@iastate.edu>
2015-09-23 11:44:07 -0500

    reorg metadata setting of CWA

--- scripts/dbutil/set_wfo.py
+++ scripts/dbutil/set_wfo.py
@@ -7,24 +7,34 @@ mcursor = MESOSITE.cursor()
 mcursor2 = MESOSITE.cursor()
 
 # Find sites we need to check on
-mcursor.execute("""select s.id, c.wfo, s.iemid, s.network 
-  from stations s, cwa c WHERE 
-  s.geom && c.the_geom and ST_contains(c.the_geom, s.geom) 
-  and (s.wfo IS NULL or s.wfo = '') and s.country = 'US' """)
+mcursor.execute("""
+    select s.id, s.iemid, s.network
+    from stations s WHERE
+    (s.wfo IS NULL or s.wfo = '') and s.country = 'US'
+""")
 
 for row in mcursor:
     sid = row[0]
-    wfo = row[1]
-    iemid = row[2]
-    network = row[3]
-    if wfo is not None:
-        print 'Assinging WFO: %s to IEMID: %s ID: %s NETWORK: %s' % (wfo,
-                                                            iemid, sid, network)
-        mcursor2.execute("UPDATE stations SET wfo = '%s' WHERE iemid = %s" % (
-         wfo, iemid) )
+    iemid = row[1]
+    network = row[2]
+    # Look for matching WFO
+    mcursor2.execute("""
+    select c.wfo
+    from stations s, cwa c WHERE
+    s.geom && c.the_geom and ST_contains(c.the_geom, s.geom)
+    and iemid = %s
+    """, (iemid, ))
+    if mcursor2.rowcount > 0:
+        row2 = mcursor2.fetchone()
+        wfo = row2[0]
+        print(('Assinging WFO: %s to IEMID: %s ID: %s NETWORK: %s'
+               ) % (wfo, iemid, sid, network))
+        mcursor2.execute("""
+            UPDATE stations SET wfo = '%s' WHERE iemid = %s
+        """ % (wfo, iemid))
     else:
-        print 'ERROR assigning WFO to IEMID: %s ID: %s NETWORK: %s' % (
-                                                            iemid, sid, network)
+        print(('ERROR assigning WFO to IEMID: %s ID: %s NETWORK: %s'
+               ) % (iemid, sid, network))
 
 
 mcursor.close()
