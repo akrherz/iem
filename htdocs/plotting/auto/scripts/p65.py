bee471b14391c2acc9f593f66c0d600982d96d73
akrherz <akrherz@iastate.edu>
2015-09-23 15:00:08 -0500

    24 Sep feature is plot of coldest day of month
    https://mesonet.agron.iastate.edu/plotting/auto/?q=65

--- htdocs/plotting/auto/scripts/p65.py
+++ htdocs/plotting/auto/scripts/p65.py
@@ -5,6 +5,9 @@ import calendar
 import pandas as pd
 from pyiem.network import Table as NetworkTable
 
+PDICT = {'cold': 'Coldest Temperature',
+         'hot': 'Hottest Temperature'}
+
 
 def get_description():
     """ Return a dict describing how to call this plotter """
@@ -17,7 +20,9 @@ def get_description():
         dict(type='station', name='station', default='IA2203',
              label='Select Station:'),
         dict(type='month', name='month', default='3',
-             label='Select Month:')
+             label='Select Month:'),
+        dict(type="select", name='dir', default='cold',
+             label='Select variable to plot:', options=PDICT),
     ]
     return d
 
@@ -32,6 +37,7 @@ def plotter(fdict):
 
     station = fdict.get('station', 'IA2203')
     month = int(fdict.get('month', 3))
+    mydir = fdict.get('dir', 'cold')
     ts = datetime.datetime(2000, month, 1)
     ets = ts + datetime.timedelta(days=35)
     ets = ets.replace(day=1)
@@ -40,11 +46,12 @@ def plotter(fdict):
     table = "alldata_%s" % (station[:2],)
     nt = NetworkTable("%sCLIMATE" % (station[:2],))
 
+    s = "ASC" if mydir == 'cold' else 'DESC'
     cursor.execute("""
         with ranks as (
             select day, high, low,
-            rank() OVER (PARTITION by year ORDER by high ASC) as high_rank,
-            rank() OVER (PARTITION by year ORDER by low ASC) as low_rank
+    rank() OVER (PARTITION by year ORDER by high """ + s + """) as high_rank,
+    rank() OVER (PARTITION by year ORDER by low """ + s + """) as low_rank
             from """ + table + """ where station = %s and month = %s),
         highs as (
             SELECT extract(day from day) as dom, count(*) from ranks
@@ -67,18 +74,20 @@ def plotter(fdict):
                            low=low_hits[:days]))
 
     fig, ax = plt.subplots(2, 1, sharex=True)
-
+    lbl = 'Coldest' if mydir == 'cold' else 'Hottest'
     ax[0].set_title(("[%s] %s\nFrequency of Day in %s\n"
-                     "Having Coldest High Temperature of March"
+                     "Having %s High Temperature of %s"
                      ) % (station, nt.sts[station]['name'],
+                          calendar.month_name[month], lbl,
                           calendar.month_name[month]), fontsize=10)
     ax[0].set_ylabel("Years (ties split)")
 
     ax[0].grid(True)
     ax[0].bar(np.arange(1, 32) - 0.4, high_hits)
 
-    ax[1].set_title(("Having Coldest Low Temperature of %s"
-                     ) % (calendar.month_name[month], ), fontsize=10)
+    ax[1].set_title(("Having %s Low Temperature of %s"
+                     ) % (lbl, calendar.month_name[month], ),
+                    fontsize=10)
     ax[1].set_ylabel("Years (ties split)")
     ax[1].grid(True)
     ax[1].set_xlabel("Day of %s" % (calendar.month_name[month], ))
