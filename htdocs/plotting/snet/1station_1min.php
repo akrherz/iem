fb3b6035be945b86c4a1c2ed11b7704ff170642c
akrherz <akrherz@iastate.edu>
2015-09-24 10:25:48 -0500

    migrate openlayers 3.8.2 apps to 3.9.0 (no code mods)

--- htdocs/plotting/snet/1station_1min.php
+++ htdocs/plotting/snet/1station_1min.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
+$OL = "3.9.0";
  include("../../../config/settings.inc.php");
  include_once "../../../include/myview.php";
  $t = new MyView();
@@ -14,12 +15,12 @@ $station = isset($_GET['station'] ) ? $_GET['station'] : "";
  
 if (! isset($_GET["station"])){
 $t->headextra = <<<EOF
-<link rel="stylesheet" href="/vendor/openlayers/3.8.2/ol.css" type="text/css">
-<link type="text/css" href="/vendor/openlayers/3.8.2/ol3-layerswitcher.css" rel="stylesheet" />
+<link rel="stylesheet" href="/vendor/openlayers/{$OL}/ol.css" type="text/css">
+<link type="text/css" href="/vendor/openlayers/{$OL}/ol3-layerswitcher.css" rel="stylesheet" />
 EOF;
 $t->jsextra = <<<EOF
-<script src="/vendor/openlayers/3.8.2/ol.js" type="text/javascript"></script>
-<script src='/vendor/openlayers/3.8.2/ol3-layerswitcher.js'></script>
+<script src="/vendor/openlayers/{$OL}/ol.js" type="text/javascript"></script>
+<script src='/vendor/openlayers/{$OL}/ol3-layerswitcher.js'></script>
 <script src="/js/olselect.php?network=${network}"></script>
 EOF;
 }
