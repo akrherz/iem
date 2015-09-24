ae18d1504a7f63409c3d3cd7f6025ab322f3d876
akrherz <akrherz@iastate.edu>
2015-09-24 15:00:16 -0500

    add download for IEM processed PIREPs
    https://mesonet.agron.iastate.edu/request/gis/pireps.php

---
+++ cgi-bin/request/gis/pireps.py
@@ -0,0 +1,130 @@
+#!/usr/bin/python
+"""
+    Dump PIREPs
+"""
+import datetime
+import zipfile
+import sys
+import cgi
+# import cgitb
+import psycopg2
+import shapefile
+import pytz
+import cStringIO
+# cgitb.enable()
+
+
+def get_context():
+    """Figure out the CGI variables passed to this script"""
+    form = cgi.FieldStorage()
+    if 'year' in form:
+        year1 = form.getfirst('year')
+        year2 = year1
+    else:
+        year1 = form.getfirst('year1')
+        year2 = form.getfirst('year2')
+    month1 = form.getfirst('month1')
+    month2 = form.getfirst('month2')
+    day1 = form.getfirst('day1')
+    day2 = form.getfirst('day2')
+    hour1 = form.getfirst('hour1')
+    hour2 = form.getfirst('hour2')
+    minute1 = form.getfirst('minute1')
+    minute2 = form.getfirst('minute2')
+
+    sts = datetime.datetime(int(year1), int(month1), int(day1),
+                            int(hour1), int(minute1))
+    sts = sts.replace(tzinfo=pytz.timezone("UTC"))
+    ets = datetime.datetime(int(year2), int(month2), int(day2),
+                            int(hour2), int(minute2))
+    ets = ets.replace(tzinfo=pytz.timezone("UTC"))
+    if ets < sts:
+        s = ets
+        ets = sts
+        sts = s
+
+    fmt = form.getfirst('fmt', 'shp')
+
+    return dict(sts=sts, ets=ets, fmt=fmt)
+
+
+def run(ctx):
+    pgconn = psycopg2.connect(database='postgis', host='iemdb', user='nobody')
+    cursor = pgconn.cursor()
+
+    if (ctx['ets'] - ctx['sts']).days > 120:
+        ctx['ets'] = ctx['sts'] + datetime.timedelta(days=120)
+
+    sql = """
+        SELECT to_char(valid at time zone 'UTC', 'YYYYMMDDHH24MI') as utctime,
+        case when is_urgent then 'T' else 'F' end,
+        substr(aircraft_type, 0, 40), substr(report, 0, 255),
+        ST_y(geom::geometry) as lat, ST_x(geom::geometry) as lon
+        from pireps WHERE
+        valid >= '%s' and valid < '%s'  ORDER by valid ASC
+        """ % (ctx['sts'].strftime("%Y-%m-%d %H:%M+00"),
+               ctx['ets'].strftime("%Y-%m-%d %H:%M+00"))
+
+    cursor.execute(sql)
+    if cursor.rowcount == 0:
+        sys.stdout.write("Content-type: text/plain\n\n")
+        sys.stdout.write("ERROR: no results found for your query")
+        return
+
+    fn = "stormattr_%s_%s" % (ctx['sts'].strftime("%Y%m%d%H%M"),
+                              ctx['ets'].strftime("%Y%m%d%H%M"))
+
+    # sys.stderr.write("End SQL with rowcount %s" % (cursor.rowcount, ))
+    if ctx['fmt'] == 'csv':
+        sys.stdout.write("Content-type: application/octet-stream\n")
+        sys.stdout.write(("Content-Disposition: attachment; "
+                          "filename=%s.csv\n\n") % (fn,))
+        sys.stdout.write(("VALID,URGENT,AIRCRAFT,REPORT,"
+                          "LAT,LON\n"))
+        for row in cursor:
+            sys.stdout.write(",".join([str(s) for s in row])+"\n")
+        return
+
+    w = shapefile.Writer(shapeType=shapefile.POINT)
+    w.field('VALID', 'C', 12)
+    w.field('URGENT', 'C', 1)
+    w.field('AIRCRAFT', 'C', 40)
+    w.field('REPORT', 'C', 255)  # Max field size is 255
+    w.field('LAT', 'F', 7, 4)
+    w.field('LON', 'F', 9, 4)
+    for row in cursor:
+        w.point(row[-1], row[-2])
+        w.record(*row)
+
+    # sys.stderr.write("End LOOP...")
+
+    shp = cStringIO.StringIO()
+    shx = cStringIO.StringIO()
+    dbf = cStringIO.StringIO()
+
+    w.save(shp=shp, shx=shx, dbf=dbf)
+    # sys.stderr.write("End of w.save()")
+
+    zio = cStringIO.StringIO()
+    zf = zipfile.ZipFile(zio, mode='w',
+                         compression=zipfile.ZIP_DEFLATED)
+    zf.writestr(fn+'.prj',
+                open(('/mesonet/www/apps/iemwebsite/data/gis/meta/4326.prj'
+                      )).read())
+    zf.writestr(fn+".shp", shp.getvalue())
+    zf.writestr(fn+'.shx', shx.getvalue())
+    zf.writestr(fn+'.dbf', dbf.getvalue())
+    zf.close()
+    sys.stdout.write(("Content-Disposition: attachment; "
+                      "filename=%s.zip\n\n") % (fn,))
+    sys.stdout.write(zio.getvalue())
+
+
+def main():
+    """Do something fun!"""
+    ctx = get_context()
+    run(ctx)
+
+if __name__ == '__main__':
+    # Go Main!
+    main()
---
+++ htdocs/request/gis/pireps.php
@@ -0,0 +1,101 @@
+<?php 
+include("../../../config/settings.inc.php");
+include("../../../include/myview.php");
+include("../../../include/forms.php");
+include("../../../include/imagemaps.php");
+define("IEM_APPID", 111);
+
+$t = new MyView();
+$t->thispage = "archive-pireps";
+$t->title = "Download PIREPs";
+$content = <<<EOF
+<h3>Archived Pilot Reports (PIREPs)</h3>
+
+<p>The IEM attempts to process a feed of Pilot Reports (PIREPs). This
+processing is done via the <a href="https://github.com/akrherz/pyIEM/blob/master/pyiem/nws/products/pirep.py">PyIEM Library</a>.
+Sadly, there is not strict coherence to a format specification and so a 
+number of reports are simply unparsable.  This archive should not be
+considered 'complete'.</p>
+
+<p>Due to filesize and speed, you can only request up to 120 days of data
+at a time!</p>
+
+<form method="GET" action="/cgi-bin/request/gis/pireps.py">
+<h4>Select time interval</h4>
+<i>(Times are in UTC.)</i>
+<table class="table">
+<thead><tr><th colspan="6">Time Interval</th>
+		<th>Format</th></tr></thead>
+<tr>
+	<th></th>
+    <th>Year</th><th>Month</th><th>Day</th>
+    <th>Hour</th><th>Minute</th>
+    <td rowspan="3"><select name="fmt">
+		<option value="shp">ESRI Shapefile</option>
+		<option value="csv">Comma Delimited</option>
+		</select></td>
+</tr>
+
+<tr>
+EOF;
+$content .= "
+    <th>Start:</th>
+    <td>
+     ". yearSelect2(2015, date("Y"), "year1") ."
+    </td>
+    <td>
+     ". monthSelect2(0,"month1") ."
+    </td>
+    <td>
+     ". daySelect2(0, "day1") ."
+    </td>
+    <td>
+     ". gmtHourSelect(0, "hour1") ."
+    </td>
+    <td>
+     ". minuteSelect(0, "minute1") ."
+    </td>
+  </tr>
+
+  <tr>
+    <th>End:</th>
+    <td>
+     ". yearSelect2(2015, date("Y"), "year2") ."
+    </td>
+    <td>
+     ". monthSelect2(0,"month2") ."
+    </td>
+    <td>
+     ". daySelect2(0, "day2") ."
+    </td>
+    <td>
+     ". gmtHourSelect(0, "hour2") ."
+    </td>
+    <td>
+     ". minuteSelect(0, "minute2") ."
+    </td>
+  </tr>
+</table>";
+
+$content .= <<<EOF
+<p><input type="submit" value="Giveme data right now!">
+</form>
+
+<h4>Shapefile DBF schema:</h4>
+<pre>
+Field 0: Type=C/String, Title='VALID', Timestamp in UTC
+Field 1: Type=C/String, Title='URGENT', Was this an urgent PIREP
+Field 2: Type=C/String, Title='AIRCRAFT', Reported Aircraft type
+Field 3: Type=C/String, Title='REPORT', The PIREP Report
+Field 4: Type=N/Double, Title='LAT', Latitude
+Field 5: Type=N/Double, Title='LON', Longitude 
+</pre>
+
+<h4>Archive notes:</h4>
+<ul>
+ <li>Archive starts 20 Jan 2015</li>
+ </ul>
+EOF;
+$t->content = $content;
+$t->render('single.phtml');
+?>
