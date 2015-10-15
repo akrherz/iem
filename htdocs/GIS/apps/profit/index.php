26e6d8fa1a80ceb2f36967a70c868435025fb66a
akrherz <akrherz@iastate.edu>
2015-10-15 08:35:12 -0500

    improve UI of profitability app example

--- htdocs/GIS/apps/profit/index.php
+++ htdocs/GIS/apps/profit/index.php
@@ -3,70 +3,37 @@ include_once "../../../../config/settings.inc.php";
 
 include_once "../../../../include/myview.php";
 
+$OL = "3.9.0";
 $t = new MyView();
 $t->title = "Profitability Map";
 $t->headextra = <<<EOF
-<link rel="stylesheet" href="/assets/openlayers/3.4.0/ol.css" type="text/css">
+<link rel="stylesheet" href="/vendor/openlayers/{$OL}/ol.css" type="text/css">
 <link rel="stylesheet" href="/assets/jquery-ui/1.11.2/jquery-ui.min.css" />
-<link type="text/css" href="/assets/openlayers/3.4.0/ol3-layerswitcher.css" rel="stylesheet" />
+<link type="text/css" href="/vendor/openlayers/{$OL}/ol3-layerswitcher.css" rel="stylesheet" />
 EOF;
 $t->jsextra = <<<EOF
-<script src="/assets/openlayers/3.4.0/ol.js" type="text/javascript"></script>
+<script src="/vendor/openlayers/{$OL}/ol.js" type="text/javascript"></script>
 <script src="/assets/jquery-ui/1.11.2/jquery-ui.js"></script>
-<script src='/assets/openlayers/3.4.0/ol3-layerswitcher.js'></script>
+<script src='/vendor/openlayers/{$OL}/ol3-layerswitcher.js'></script>
 <script>
 var map;
+var player;
 $(document).ready(function(){
-
+	player = new ol.layer.Tile({
+                        title: 'Profitability',
+						visible: true,
+                        source: new ol.source.XYZ({
+                                url : '/c/tile.py/1.0.0/profit2015/{z}/{x}/{y}.png'
+                        })
+                });
+		
 		map = new ol.Map({
                 target: 'map',
                 layers: [new ol.layer.Tile({
             title: 'OpenStreetMap',
             visible: true,
             source: new ol.source.OSM()
-                }),
-                new ol.layer.Tile({
-                        title: 'Profit 2015',
-                		visible: true,
-                        source: new ol.source.XYZ({
-                                url : '/c/tile.py/1.0.0/profit2015/{z}/{x}/{y}.png'
-                        })
-                }),
-                new ol.layer.Tile({
-                        title: 'Profit 2014',
-                		visible: false,
-                        source: new ol.source.XYZ({
-                                url : '/c/tile.py/1.0.0/profit2014/{z}/{x}/{y}.png'
-                        })
-                }),
-                new ol.layer.Tile({
-                        title: 'Profit 2013',
-                		visible: false,
-                        source: new ol.source.XYZ({
-                                url : '/c/tile.py/1.0.0/profit2013/{z}/{x}/{y}.png'
-                        })
-                }),
-                new ol.layer.Tile({
-                        title: 'Profit 2012',
-                		visible: false,
-                        source: new ol.source.XYZ({
-                                url : '/c/tile.py/1.0.0/profit2012/{z}/{x}/{y}.png'
-                        })
-                }),
-                new ol.layer.Tile({
-                        title: 'Profit 2011',
-						visible: false,
-                        source: new ol.source.XYZ({
-                                url : '/c/tile.py/1.0.0/profit2011/{z}/{x}/{y}.png'
-                        })
-                }),
-                new ol.layer.Tile({
-                        title: 'Profit 2010',
-						visible: false,
-                        source: new ol.source.XYZ({
-                                url : '/c/tile.py/1.0.0/profit2010/{z}/{x}/{y}.png'
-                        })
-                })],
+                }), player],
 		view: new ol.View({
                         projection: 'EPSG:3857',
 					center: ol.proj.transform([-93.5, 42.1], 'EPSG:4326', 'EPSG:3857'),
@@ -76,23 +43,44 @@ $(document).ready(function(){
 		});
     var layerSwitcher = new ol.control.LayerSwitcher();
     map.addControl(layerSwitcher);
-		
-		
+
+	$("#yearselect").buttonset();
+	$( '#yearselect input[type=radio]').change(function(){
+		player.setSource(new ol.source.XYZ({
+			url : '/c/tile.py/1.0.0/profit'+this.value+'/{z}/{x}/{y}.png'
+		}));
+    });
 });
 </script>
 EOF;
 
 $t->content = <<<EOF
 <style>
-		#legend{
+#legend{
     position:absolute; 
     top:4em; 
     left:10px; 
     z-index:10000; 
     background-color:#FFFFFF;
 }
+#yearselect{
+	position:absolute; 
+    top:0.5em; 
+    left:60px; 
+    z-index:10000; 
+    background-color:#FFFFFF;	
+}
 </style>
 <div style="width:100%; height:100%" id="map">
+<div id="yearselect">
+  <input type="radio" id="y2010" name="whichyear" value="2010"><label for="y2010">2010</label>
+  <input type="radio" id="y2011" name="whichyear" value="2011"><label for="y2011">2011</label>
+  <input type="radio" id="y2012" name="whichyear" value="2012"><label for="y2012">2012</label>
+  <input type="radio" id="y2013" name="whichyear" value="2013"><label for="y2013">2013</label>
+  <input type="radio" id="y2014" name="whichyear" value="2014"><label for="y2014">2014</label>
+  <input type="radio" id="y2015" name="whichyear" value="2015" checked="checked"><label for="y2015">2015</label>
+</div>
+		
 <div id="legend"><img src="profit_legend.png" /></div>		
 </div>
 
