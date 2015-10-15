6f26a0c2f30b20ff297df38a748d7beb041fe7f9
akrherz <akrherz@iastate.edu>
2015-10-14 21:16:46 -0500

    lower size check on N0Q product to 200KB

--- scripts/qc/check_n0q.py
+++ scripts/qc/check_n0q.py
@@ -18,7 +18,7 @@ def run(sts, ets):
         if not os.path.isfile(fn):
             print 'check_n0q.py %s is missing' % (fn.split("/")[-1],)
         else:
-            if os.stat(fn)[6] < 500000:
+            if os.stat(fn)[6] < 200000:
                 print('check_n0q.py %s too small, size: %s'
                       '') % (fn.split("/")[-1], os.stat(fn)[6])
         now += interval
