3ff41dbf42e4e56c15d07020971a5b220c3d981b
akrherz <akrherz@iastate.edu>
2015-10-15 15:42:05 -0500

    add data result to count of VTEC products by year
    https://mesonet.agron.iastate.edu/plotting/auto/?q=73

--- htdocs/plotting/auto/scripts/p73.py
+++ htdocs/plotting/auto/scripts/p73.py
@@ -1,10 +1,8 @@
-import psycopg2.extras
+import psycopg2
 import datetime
-import numpy as np
-import math
 import pyiem.nws.vtec as vtec
 from pyiem.network import Table as NetworkTable
-import calendar
+from pandas.io.sql import read_sql
 
 PDICT = {'yes': "Limit Plot to Year-to-Date",
          'no': 'Plot Entire Year'}
@@ -13,6 +11,7 @@ PDICT = {'yes': "Limit Plot to Year-to-Date",
 def get_description():
     """ Return a dict describing how to call this plotter """
     d = dict()
+    d['data'] = True
     d['cache'] = 86400
     d['description'] = """This chart displays the number of products issued
     by a NWS Office by year for a given watch, warning, or advisory of your
@@ -37,7 +36,6 @@ def plotter(fdict):
     matplotlib.use('agg')
     import matplotlib.pyplot as plt
     pgconn = psycopg2.connect(database='postgis', host='iemdb', user='nobody')
-    cursor = pgconn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
     station = fdict.get('station', 'DMX')
     limit = fdict.get('limit', 'no')
@@ -47,38 +45,30 @@ def plotter(fdict):
     nt = NetworkTable('WFO')
     nt.sts['_ALL'] = {'name': 'All Offices'}
 
-    lastdoy = 367
-    if limit.lower() == 'yes':
-        lastdoy = int(datetime.datetime.today().strftime("%j")) + 1
-
-    wfo_limiter = " and wfo = '%s' " % (station if len(station) == 3 else station[1:],)
+    wfo_limiter = (" and wfo = '%s' "
+                   ) % (station if len(station) == 3 else station[1:],)
     if station == '_ALL':
         wfo_limiter = ''
     doy_limiter = ''
-    if limit == 'yes':
+    if limit.lower() == 'yes':
         doy_limiter = (" and extract(doy from issue) < "
                        "extract(doy from 'TODAY'::date) ")
 
-    cursor.execute("""
+    df = read_sql("""
         with data as (
             SELECT distinct extract(year from issue) as yr, wfo, eventid
             from warnings where phenomena = %s and significance = %s
             """ + wfo_limiter + doy_limiter + """)
 
         SELECT yr, count(*) from data GROUP by yr ORDER by yr ASC
-      """, (phenomena, significance))
+      """, pgconn, params=(phenomena, significance))
 
-    years = []
-    counts = []
-    for row in cursor:
-        years.append(row[0])
-        counts.append(row[1])
-    if len(years) == 0:
+    if len(df.index) == 0:
         return("Sorry, no data found!")
 
     (fig, ax) = plt.subplots(1, 1)
-    ax.bar(np.array(years)-0.4, counts)
-    ax.set_xlim(min(years)-0.5, max(years)+0.5)
+    ax.bar(df['yr']-0.4, df['count'])
+    ax.set_xlim(df['yr'].min()-0.5, df['yr'].max()+0.5)
     ax.grid(True)
     ax.set_ylabel("Yearly Count")
     ax.set_title(("NWS %s\n%s %s (%s.%s) Count"
@@ -88,4 +78,4 @@ def plotter(fdict):
         ax.set_xlabel(("thru approximately %s"
                        ) % (datetime.date.today().strftime("%-d %b"), ))
 
-    return fig
+    return fig, df
