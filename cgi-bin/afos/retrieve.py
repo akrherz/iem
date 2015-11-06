7e4ed735900091c32dc3f996bcf587cd3dcc2a51
akrherz <akrherz@iastate.edu>
2015-11-06 08:19:31 -0600

    improve AFOS retrieve logic some more, with unit tests even

--- cgi-bin/afos/retrieve.py
+++ cgi-bin/afos/retrieve.py
@@ -3,6 +3,35 @@
 import psycopg2
 import cgi
 import sys
+import unittest
+
+
+def pil_logic(s):
+    """Convert the CGI pil value into something we can query
+
+    Args:
+      s (str): The CGI variable wanted
+
+    Returns:
+      list of PILs to send to the databae"""
+    if s == '':
+        return []
+    s = s.upper()
+    pils = []
+    if s.find(",") == -1:
+        pils.append(s)
+    else:
+        pils = s.split(",")
+
+    res = []
+    for pil in pils:
+        if pil[:3] == "WAR":
+            for q in ['FLS', 'FFS', 'AWW', 'TOR', 'SVR', 'FFW', 'SVS',
+                      'LSR', 'SPS', 'WSW', 'FFA', 'WCN']:
+                res.append("%s%s" % (q, pil[3:6]))
+        else:
+            res.append("%6.6s" % (pil.strip() + '      ', ))
+    return res
 
 
 def main():
@@ -11,13 +40,13 @@ def main():
     sys.stdout.write("X-Content-Type-Options: nosniff\n")
     sys.stdout.write("Content-type: text/plain\n\n")
     form = cgi.FieldStorage()
-    pil0 = form.getfirst('pil', '').upper()
+    pils = pil_logic(form.getfirst('pil', ''))
     limit = int(form.getfirst('limit', 1))
     center = form.getfirst('center', '')[:4]
     sdate = form.getfirst('sdate', '')[:10]
     edate = form.getfirst('edate', '')[:10]
     fmt = form.getfirst('fmt', 'text')
-    if pil0 == '':
+    if len(pils) == 0:
         sys.stdout.write("ERROR: No pil specified...")
         return
     centerlimit = '' if center == '' else (" and source = '%s' " % (center, ))
@@ -26,32 +55,14 @@ def main():
         timelimit = (" and entered >= '%s' and entered < '%s' "
                      ) % (sdate, edate)
 
-    pils = pil0.split(",")
-    myPils = []
-    for pil in pils:
-        if len(pil) < 3:
-            print 'Invalid PIL, try again'
-            return
-        if pil[:3] == "WAR":
-            for q in ['FLS', 'FFS', 'AWW', 'TOR', 'SVR', 'FFW', 'SVS',
-                      'LSR', 'SPS', 'WSW', 'FFA', 'WCN']:
-                pils.append('%s%s' % (q, pil[3:]))
-            continue
-        myPils.append("%6s" % (pil + "      ",))
-
-    pilAR = "("
-    for pil in myPils:
-        pilAR += "'%s'," % (pil,)
-    pilAR = pilAR[:-1] + ")"
-
-    if myPils[0][:3] == 'MTR':
+    if pils[0][:3] == 'MTR':
         access = psycopg2.connect(database='iem', host='iemdb', user='nobody')
         cursor = access.cursor()
         sql = """
             SELECT raw from current_log c JOIN stations t
             on (t.iemid = c.iemid)
             WHERE raw != '' and id = '%s' ORDER by valid DESC LIMIT %s
-            """ % (myPils[0][3:].strip(), limit)
+            """ % (pils[0][3:].strip(), limit)
         cursor.execute(sql)
         for row in cursor:
             sys.stdout.write("\001\n")
@@ -59,7 +70,7 @@ def main():
             sys.stdout.write("\n\003")
         if cursor.rowcount == 0:
             sys.stdout.write("ERROR: METAR lookup for %s failed" % (
-                                                myPils[0][3:].strip(), ))
+                                                pils[0][3:].strip(), ))
         return
 
     try:
@@ -70,18 +81,24 @@ def main():
 
     cursor = mydb.cursor()
 
+    if len(pils) == 1:
+        pillimit = " pil = '%s' " % (pils[0], )
+    else:
+        pillimit = " pil in %s" % (tuple(pils), )
+
     # Do optimized query first, see if we can get our limit right away
     sql = """
-        SELECT data from products WHERE pil IN """ + pilAR + """
+        SELECT data from products WHERE %s
         and entered > now() - '2 days'::interval %s %s
-        ORDER by entered DESC LIMIT %s""" % (centerlimit, timelimit, limit)
+        ORDER by entered DESC LIMIT %s""" % (pillimit, centerlimit,
+                                             timelimit, limit)
 
     cursor.execute(sql)
     if cursor.rowcount != limit:
         sql = """
-            SELECT data from products WHERE pil IN """ + pilAR + """ %s %s
-            ORDER by entered DESC LIMIT %s """ % (centerlimit, timelimit,
-                                                  limit)
+            SELECT data from products WHERE %s %s %s
+            ORDER by entered DESC LIMIT %s """ % (pillimit, centerlimit,
+                                                  timelimit, limit)
         cursor.execute(sql)
 
     for row in cursor:
@@ -99,8 +116,22 @@ def main():
             sys.stdout.write("\n\003\n")
 
     if cursor.rowcount == 0:
-        print "Could not Find: "+pil
+        print("ERROR: Could not Find: %s" % (",".join(pils), ))
 
 
 if __name__ == '__main__':
     main()
+
+
+class tester(unittest.TestCase):
+
+    def test_pil_logic(self):
+        """Make sure our pil logic works! """
+        res = pil_logic("AFDDMX")
+        self.assertEquals(len(res), 1)
+        self.assertEquals(res[0], 'AFDDMX')
+        res = pil_logic("WAREWX")
+        self.assertEquals(len(res), 12)
+        res = pil_logic("STOIA,AFDDMX")
+        self.assertEquals(res[0], 'STOIA ')
+        self.assertEquals(res[1], 'AFDDMX')
