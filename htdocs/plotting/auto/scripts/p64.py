f4fd6c6415a4cbcba102f9b4a3c5b2bcb09a3749
akrherz <akrherz@iastate.edu>
2015-11-19 14:24:38 -0600

    20 Nov feature is plot of first measurable snow
    https://mesonet.agron.iastate.edu/plotting/auto/?q=64
    the logic was rewritten to span 1 Jan and add more diagnostics.

--- htdocs/plotting/auto/scripts/p64.py
+++ htdocs/plotting/auto/scripts/p64.py
@@ -1,23 +1,27 @@
 import psycopg2.extras
 import numpy as np
+import calendar
 import datetime
 import pandas as pd
 from pyiem.network import Table as NetworkTable
 
-PDICT = {'first': 'First Snowfall between Jul - Dec',
-         'last': 'Last Snowfall between Jan - Jun'}
+PDICT = {'first': 'First Snowfall after 1 July',
+         'last': 'Last Snowfall before 1 July'}
 
 
 def get_description():
     """ Return a dict describing how to call this plotter """
     d = dict()
     d['data'] = True
-    d['description'] = """This chart plots the last one plus inch snowfall
-    of each winter season and then the number of days the snow persisted as
-    per snowdepth measurements."""
+    d['description'] = """This chart displays either the first or last date
+    of the winter season with a snowfall of a given intensity.  The snowfall
+    and snow depth data is not of great quality, so please be careful with
+    this plot."""
     d['arguments'] = [
         dict(type='station', name='station', default='IA2203',
              label='Select Station:'),
+        dict(type='text', name='threshold', default='1',
+             label='First Snowfall Threshold (T for trace)'),
         dict(type="select", name='dir', default='last', options=PDICT,
              label='Which Variable to Plot?'),
     ]
@@ -34,86 +38,93 @@ def plotter(fdict):
 
     station = fdict.get('station', 'IA2203')
     mydir = fdict.get('dir', 'last')
+    threshold = fdict.get('threshold', 1)
+    threshold = 0.0001 if threshold == 'T' else float(threshold)
 
     table = "alldata_%s" % (station[:2],)
     nt = NetworkTable("%sCLIMATE" % (station[:2],))
     syear = max(1893, nt.sts[station]['archive_begin'].year)
     eyear = datetime.datetime.now().year
 
-    doy = []
-    snow = []
-    days = []
-    cnts = []
-    years = []
-    for year in range(syear, eyear):
-        sts = datetime.date(year, 1 if mydir == 'last' else 7, 1)
-        ets = datetime.date(year, 7 if mydir == 'last' else 12,
-                            1 if mydir == 'last' else 31)
-        cursor.execute("""
-        SELECT extract(doy from day), day, snow, snowd from """ + table + """
-        where station = %s and day BETWEEN
-        %s and %s ORDER by day ASC
-        """, (station, sts, ets))
-        found = False
-        thissnow = 0
-        d = None
-        for row in cursor:
-            if found and mydir == 'first' and row[3] < 0.1:
-                break
-            if row[2] >= 1:
-                found = True
-                d = row[0]
-                thissnow = row[2]
-                cnt = 0
-            if row[3] > 0 and found:
-                cnt += 1
-        if d is None:
-            continue
-        cnts.append(cnt)
-        years.append(year)
-        doy.append(d)
-        snow.append(thissnow)
-        if cnt >= 11:
-            days.append('g')
-        elif cnt >= 3:
-            days.append('b')
-        else:
-            days.append('r')
+    snow = np.zeros((eyear - syear + 1, 366))
+    snowd = np.ones((eyear - syear + 1, 366)) * 99
+    cursor.execute("""
+        SELECT extract(doy from day), year, snow, snowd from """ + table + """
+        where station = %s and year >= %s
+    """, (station, syear))
+    for row in cursor:
+        snow[row[1] - syear, row[0] - 1] = row[2]
+        snowd[row[1] - syear, row[0] - 1] = row[3]
 
-    doy = np.array(doy)
-    p = np.percentile(doy, np.arange(100))
-    df = pd.DataFrame(dict(doy=doy, snow=snow, year=years))
+    rows = []
+    for i, year in enumerate(range(syear, eyear)):
+        s = tuple(snow[i, 183:]) + tuple(snow[i+1, :183])
+        sd = tuple(snowd[i, 183:]) + tuple(snowd[i+1, :183])
+        idx = np.where(np.array(s) >= threshold, 1, 0)
+        if np.max(idx) == 0:
+            continue
+        if mydir == 'first':
+            idx = tuple(idx).index(1)
+        else:
+            idx = 366 - tuple(idx[::-1]).index(1)
+        for sfree in range(idx+1, 366):
+            if sd[sfree] <= 0.01:
+                break
+        cnt = sfree - idx
+        if cnt >= 30:
+            color = 'purple'
+        elif cnt >= 11:
+            color = 'g'
+        elif cnt >= 3:
+            color = 'b'
+        else:
+            color = 'r'
+        # print year, idx, s[idx], sd[idx:sfree]
+        rows.append(dict(year=year, snow_doy=(idx+183), color=color,
+                         snowfree_doy=(sfree+183), snowfall=s[idx]))
+
+    df = pd.DataFrame(rows)
 
     (fig, ax) = plt.subplots(1, 1)
 
-    ax.scatter(doy, snow, facecolor=days, edgecolor=days, s=100)
-    for x, y, l, c in zip(doy, snow, cnts, days):
-        ax.scatter(x+l, y, marker='x', s=100, color=c)
-        ax.plot([x, x+l], [y, y], lw='2', color=c)
-    if mydir == 'last':
-        ax.set_xticks((1, 32, 60, 91, 121, 152))
-        ax.set_xticklabels(('Jan 1', 'Feb 1', 'Mar 1', 'Apr 1', 'May 1',
-                            'Jun 1'))
-    else:
-        ax.set_xticks((244, 274, 305, 335))
-        ax.set_xticklabels(('Sep 1', 'Oct 1', 'Nov 1', 'Dec 1'))
+    ax.scatter(df['snow_doy'], df['snowfall'], facecolor=df['color'],
+               edgecolor=df['color'], s=100)
+    for _, row in df.iterrows():
+        ax.scatter(row['snowfree_doy'], row['snowfall'], marker='x', s=100,
+                   color=row['color'])
+        ax.plot([row['snow_doy'], row['snowfree_doy']],
+                [row['snowfall'], row['snowfall']], lw='2', color=row['color'])
+    ax.set_xticks([1, 32, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335,
+                   1 + 366, 32 + 366, 60 + 366, 91 + 366, 121 + 366,
+                   152 + 366, 182 + 366, 213 + 366, 244 + 366, 274 + 366,
+                   305 + 366, 335 + 366])
+    ax.set_xticklabels(calendar.month_abbr[1:] + calendar.month_abbr[1:])
     ax.grid(True)
     ax.set_ylim(bottom=0)
     ax2 = ax.twinx()
+    p = np.percentile(df['snow_doy'].values, np.arange(100))
     ax2.plot(p, np.arange(100), lw=2, color='k')
     ax2.set_ylabel(("Frequency of %s Date (CDF) [%%]"
                     ) % ('Start' if mydir == 'first' else 'Last', ))
-    ax.set_ylabel('Snowfall [inch]')
-    ax.set_title(('[%s] %s %s 1+ Inch Snowfall\n'
+    ax.set_ylabel('Snowfall [inch], Avg: %.1f inch' % (df['snowfall'].mean(),))
+    ax.set_title(('[%s] %s %s %s Snowfall\n'
                   '(color is how long snow remained)'
-                  '') % (station, nt.sts[station]['name'],
-                         'Last' if mydir == 'last' else 'First'))
+                  '') % (
+        station, nt.sts[station]['name'],
+        'Last' if mydir == 'last' else 'First',
+        'Trace+' if threshold < 0.1 else "%.2f+ Inch" % (threshold,))
+                 )
+    p0 = plt.Rectangle((0, 0), 1, 1, fc="purple")
     p1 = plt.Rectangle((0, 0), 1, 1, fc="g")
     p2 = plt.Rectangle((0, 0), 1, 1, fc="b")
     p3 = plt.Rectangle((0, 0), 1, 1, fc="r")
-    ax.legend((p1, p2, p3), ('> 10 days', '3 - 10', '< 3 days'),
-              ncol=3, fontsize=11, loc=(0., -0.15))
-    ax.set_xlim(min(doy)-5,  max(doy)+5)
+    ax.legend((p0, p1, p2, p3), (
+        '> 31 days [%s]' % (len(df[df['color'] == 'purple'].index), ),
+        '10 - 31 [%s]' % (len(df[df['color'] == 'g'].index), ),
+        '3 - 10 [%s]' % (len(df[df['color'] == 'b'].index), ),
+        '< 3 days [%s]' % (len(df[df['color'] == 'r'].index), )),
+              ncol=4, fontsize=11, loc=(0., -0.15))
+    ax.set_xlim(df['snow_doy'].min()-5,  df['snowfree_doy'].max()+5)
 
     box = ax.get_position()
     ax.set_position([box.x0, box.y0 + box.height * 0.1, box.width,
@@ -122,3 +133,6 @@ def plotter(fdict):
                      box.height * 0.9])
 
     return fig, df
+
+if __name__ == '__main__':
+    plotter(dict(dir='first'))
