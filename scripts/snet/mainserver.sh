85db0c0140bc64c20dd9b2c5ae071ab36c475c96
akrherz <akrherz@iastate.edu>
2015-11-20 14:08:48 -0600

    delete mainserver log file before attempting to start

--- scripts/snet/mainserver.sh
+++ scripts/snet/mainserver.sh
@@ -5,6 +5,9 @@ if [ -e mainserver.pid ]; then
 	kill -9 `cat mainserver.pid `
 	rm -f mainserver.pid
 fi
+# For some reason, this file 'corrupts' at reboot and sometimes is 000 perms
+# delete it each time, otherwise mainserver won't start!
+rm -f /mesonet/data/logs/mainserver.log
 twistd --pidfile=mainserver.pid --logfile=/mesonet/data/logs/mainserver.log -y mainserver.py
 sleep 10
 # Allow nagios to monitor
