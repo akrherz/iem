bfdb55b3f8b59ffb1ae08843d3c072637e7a50fa
akrherz <akrherz@iastate.edu>
2015-11-20 20:26:31 -0600

    improve snow LSR plotting logic some, I hope

--- scripts/current/lsr_snow_mapper.py
+++ scripts/current/lsr_snow_mapper.py
@@ -1,48 +1,44 @@
-"""Create an analysis of LSR snowfall reports
-"""
+"""Create an analysis of LSR snowfall reports"""
 import numpy as np
 from pyiem.plot import MapPlot
 import pyiem.reference as reference
 import datetime
 import unittest
 import psycopg2
+import pandas as pd
+from pandas.io.sql import read_sql
 POSTGIS = psycopg2.connect(database='postgis', host='iemdb', user='nobody')
-pcursor = POSTGIS.cursor()
 
 
 def run(basets, endts, view):
     """Generate this plot for the given basets"""
-    vals = []
-    valmask = []
-    lats = []
-    lons = []
 
-    pcursor.execute("""SELECT state,
+    df = read_sql("""SELECT state,
         max(magnitude) as val, ST_x(geom) as lon, ST_y(geom) as lat
         from lsrs WHERE type in ('S') and magnitude >= 0 and
-        valid > %s and valid < %s GROUP by state, lon, lat""", (basets, endts))
-    for row in pcursor:
-        vals.append(row[1])
-        lats.append(row[3])
-        lons.append(row[2])
-        valmask.append(row[0] in ['IA', ])
+        valid > %s and valid < %s GROUP by state, lon, lat
+        """, POSTGIS, params=(basets, endts), index_col=None)
+    df['used'] = False
+    df['textplot'] = True
+    df.sort_values(by='val', ascending=False, inplace=True)
 
     # Now, we need to add in zeros, lets say we are looking at a .25 degree box
-    mybuffer = 1.0
+    mybuffer = 0.75
+    newrows = []
     for lat in np.arange(reference.MW_SOUTH, reference.MW_NORTH, mybuffer):
         for lon in np.arange(reference.MW_WEST, reference.MW_EAST, mybuffer):
-            found = False
-            for j in range(len(lats)):
-                if (lats[j] > (lat-(mybuffer/2.)) and
-                    lats[j] < (lat+(mybuffer/2.)) and
-                    lons[j] > (lon-(mybuffer/2.)) and
-                    lons[j] < (lon+(mybuffer/2.))):
-                    found = True
-            if not found:
-                lats.append(lat)
-                lons.append(lon)
-                valmask.append(False)
-                vals.append(0)
+            df2 = df[(df['lat'] >= lat) & (df['lat'] < (lat+mybuffer)) &
+                     (df['lon'] >= lon) & (df['lon'] < (lon+mybuffer))]
+            if len(df2.index) == 0:
+                newrows.append(dict(lon=(lon+mybuffer/2.),
+                                    lat=(lat+mybuffer/2.),
+                                    val=0, used=True, textplot=False))
+                continue
+            df.at[df2.index[0], 'used'] = True
+    dfnew = pd.DataFrame(newrows)
+    df = df.append(dfnew)
+    cdf = df[df['used']]
+    tdf = df[df['textplot']]
 
     rng = [0.01, 0.1, 0.25, 0.5, 1, 2, 3, 5, 7, 9, 11, 13, 15, 17]
 
@@ -50,9 +46,10 @@ def run(basets, endts, view):
                 title="Local Storm Report Snowfall Total Analysis",
                 subtitle=("Reports past 12 hours: %s"
                           "" % (endts.strftime("%d %b %Y %I:%M %p"), )))
-    m.contourf(lons, lats, vals, rng)
+    m.contourf(cdf['lon'].values, cdf['lat'].values, cdf['val'].values, rng)
     m.drawcounties()
-    m.plot_values(lons, lats, vals, fmt='%.1f', valmask=valmask)
+    m.plot_values(tdf['lon'].values, tdf['lat'].values, tdf['val'].values,
+                  fmt='%.1f')
     pqstr = "plot c 000000000000 lsr_snowfall.png bogus png"
     m.postprocess(view=view, pqstr=pqstr)
     m.close()
@@ -62,7 +59,7 @@ def run(basets, endts, view):
                 title="Local Storm Report Snowfall Total Analysis",
                 subtitle=("Reports valid over past 12 hours: %s"
                           "" % (endts.strftime("%d %b %Y %I:%M %p"), )))
-    m.contourf(lons, lats, vals, rng)
+    m.contourf(cdf['lon'].values, cdf['lat'].values, cdf['val'].values, rng)
     m.drawcounties()
     pqstr = "plot c 000000000000 lsr_snowfall_nv.png bogus png"
     m.postprocess(view=view, pqstr=pqstr)
@@ -72,7 +69,7 @@ def run(basets, endts, view):
                 title="Local Storm Report Snowfall Total Analysis",
                 subtitle=("Reports past 12 hours: %s"
                           "" % (endts.strftime("%d %b %Y %I:%M %p"), )))
-    m.contourf(lons, lats, vals, rng)
+    m.contourf(cdf['lon'].values, cdf['lat'].values, cdf['val'].values, rng)
     pqstr = "plot c 000000000000 mw_lsr_snowfall.png bogus png"
     m.postprocess(view=view, pqstr=pqstr)
     m.close()
