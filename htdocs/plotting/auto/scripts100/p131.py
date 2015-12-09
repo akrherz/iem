e4a13376078c91606ebe808f5f44f9f0f755fe39
akrherz <akrherz@iastate.edu>
2015-12-08 20:15:23 -0600

    9 Dec feature is plot of cloudiness by temperature
    https://mesonet.agron.iastate.edu/plotting/auto/?q=131

---
+++ htdocs/plotting/auto/scripts100/p131.py
@@ -0,0 +1,106 @@
+import psycopg2
+import datetime
+import numpy as np
+from pyiem.network import Table as NetworkTable
+from pyiem.meteorology import mixing_ratio, dewpoint_from_pq, relh
+from pyiem.datatypes import temperature, pressure, mixingratio
+from pandas.io.sql import read_sql
+
+MDICT = {'all': 'No Month/Time Limit',
+         'spring': 'Spring (MAM)',
+         'fall': 'Fall (SON)',
+         'winter': 'Winter (DJF)',
+         'summer': 'Summer (JJA)',
+         'jan': 'January',
+         'feb': 'February',
+         'mar': 'March',
+         'apr': 'April',
+         'may': 'May',
+         'jun': 'June',
+         'jul': 'July',
+         'aug': 'August',
+         'sep': 'September',
+         'oct': 'October',
+         'nov': 'November',
+         'dec': 'December'
+         }
+
+
+def get_description():
+    """ Return a dict describing how to call this plotter """
+    d = dict()
+    d['data'] = True
+    d['cache'] = 86400
+    d['description'] = """This plot displays the frequency of having overcast
+    conditions reported by air temperature.  More specifically, this script
+    looks for the report of 'OVC' within the METAR sky conditions.  Many
+    caveats apply with the reporting changes of this over the years."""
+    d['arguments'] = [
+        dict(type='zstation', name='zstation', default='DSM',
+             label='Select Station:'),
+        dict(type='select', name='month', default='all',
+             label='Month Limiter', options=MDICT),
+
+    ]
+    return d
+
+
+def plotter(fdict):
+    """ Go """
+    import matplotlib
+    matplotlib.use('agg')
+    import matplotlib.pyplot as plt
+    pgconn = psycopg2.connect(database='asos', host='iemdb', user='nobody')
+
+    station = fdict.get('zstation', 'AMW')
+    network = fdict.get('network', 'IA_ASOS')
+    month = fdict.get('month', 'all')
+
+    nt = NetworkTable(network)
+
+    if month == 'all':
+        months = range(1, 13)
+    elif month == 'fall':
+        months = [9, 10, 11]
+    elif month == 'winter':
+        months = [12, 1, 2]
+    elif month == 'spring':
+        months = [3, 4, 5]
+    elif month == 'summer':
+        months = [6, 7, 8]
+    else:
+        ts = datetime.datetime.strptime("2000-"+month+"-01", '%Y-%b-%d')
+        # make sure it is length two for the trick below in SQL
+        months = [ts.month, 999]
+
+    df = read_sql("""
+        SELECT tmpf::int as t,
+        SUM(case when (skyc1 = 'OVC' or skyc2 = 'OVC' or skyc3 = 'OVC'
+            or skyc4 = 'OVC') then 1 else 0 end) as hits,
+        count(*)
+        from alldata where station = %s
+        and tmpf is not null and extract(month from valid) in %s
+        GROUP by t ORDER by t ASC
+        """, pgconn, params=(station,  tuple(months)), index_col=None)
+    df['freq'] = df['hits'] / df['count'] * 100.
+    df2 = df[df['count'] > 2]
+
+    (fig, ax) = plt.subplots(1, 1)
+    ax.bar(df2['t'], df2['freq'], ec='green', fc='green', width=1)
+    ax.grid(True, zorder=11)
+    ax.set_title(("%s [%s]\nFrequency of Overcast Clouds by "
+                  "Air Temperature (month=%s) "
+                  "(%s-%s)\n"
+                  "(must have 3+ hourly observations at the given temperature)"
+                  ) % (nt.sts[station]['name'], station, month.upper(),
+                       nt.sts[station]['archive_begin'].year,
+                       datetime.datetime.now().year), size=10)
+
+    ax.set_ylabel("Cloudiness Frequency [%]")
+    ax.set_ylim(0, 100)
+    ax.set_xlabel("Air Temperature $^\circ$F")
+    if df2['t'].min() < 30:
+        ax.axvline(32, lw=2, color='k')
+        ax.text(32, -4, "32", ha='center')
+
+    return fig, df
