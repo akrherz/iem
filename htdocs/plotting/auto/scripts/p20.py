f401d0b515f90976840acf5313af1bf99ce1a274
akrherz <akrherz@iastate.edu>
2015-12-23 20:38:33 -0600

    include max and max year on month precip hours plot too
    https://mesonet.agron.iastate.edu/plotting/auto/?q=20

--- htdocs/plotting/auto/scripts/p20.py
+++ htdocs/plotting/auto/scripts/p20.py
@@ -45,11 +45,22 @@ def plotter(fdict):
         SELECT extract(year from t) as year, extract(month from t) as month,
         count(*) from obs GROUP by year, month
     ), averages as (
-        SELECT month, avg(count) from agg1 GROUP by month
+        SELECT month, avg(count), max(count) from agg1 GROUP by month
     ), myyear as (
         SELECT month, count from agg1 where year = %s
+    ), ranks as (
+        SELECT month, year,
+        rank() OVER (PARTITION by month ORDER by count DESC)
+        from agg1
+    ), top as (
+        SELECT month, max(year) as max_year from ranks
+        WHERE rank = 1 GROUP by month
+    ), agg2 as (
+        SELECT t.month, t.max_year, a.avg, a.max from top t JOIN averages a
+        on (t.month = a.month)
     )
-    SELECT a.month, m.count, a.avg from averages a LEFT JOIN myyear m
+    SELECT a.month, m.count, a.avg, a.max, a.max_year from
+    agg2 a LEFT JOIN myyear m
     on (m.month = a.month) ORDER by a.month ASC
     """, pgconn, params=(station, year), index_col=None)
 
@@ -67,18 +78,23 @@ def plotter(fdict):
             ax.text(i+1+0.25, thisyear[i]+1, "%.0f" % (thisyear[i],),
                     ha='center')
 
+    ax.scatter(df['month'], df['max'], marker='s', s=45, label="Max",
+               zorder=2, color='g')
+    for i, row in df.iterrows():
+        ax.text(row['month'], row['max'], '%i\n%i.' % (row['max_year'],
+                                                       row['max']), ha='right')
     ax.set_xticks(range(0, 13))
     ax.set_xticklabels(calendar.month_abbr)
-    ax.set_xlim(0.5, 12.5)
-    ax.set_ylim(0, max([df['count'].max(), df['avg'].max()]) * 1.2)
-    ax.set_yticks(np.arange(0, max(df['count'].max(), df['avg'].max()) + 20,
+    ax.set_xlim(0, 13)
+    ax.set_ylim(0, max([df['count'].max(), df['max'].max()]) * 1.2)
+    ax.set_yticks(np.arange(0, df['max'].max() + 20,
                             12))
     ax.set_ylabel("Hours with 0.01+ inch precip")
     if datetime.date.today().year == year:
         ax.set_xlabel("Valid till %s" % (
             datetime.date.today().strftime("%-d %B"),))
     ax.grid(True)
-    ax.legend(ncol=2)
+    ax.legend(ncol=3)
     ax.set_title(("%s [%s]\n"
                   "Number of Hours with *Measurable* Precipitation Reported"
                   ) % (nt.sts[station]['name'], station))
