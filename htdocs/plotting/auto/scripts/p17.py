c156984e1d0e3248e2af5a04a3be505c5ebc6655
akrherz <akrherz@iastate.edu>
2015-12-26 20:25:45 -0600

    correct plotting bug with monthly temp plot

--- htdocs/plotting/auto/scripts/p17.py
+++ htdocs/plotting/auto/scripts/p17.py
@@ -1,7 +1,7 @@
-import psycopg2.extras
 import numpy as np
 import datetime
-import pandas as pd
+import psycopg2
+from pandas.io.sql import read_sql
 from pyiem.network import Table as NetworkTable
 import warnings
 warnings.simplefilter("ignore", UserWarning)
@@ -35,11 +35,9 @@ def plotter(fdict):
     import matplotlib.pyplot as plt
     import matplotlib.patheffects as PathEffects
     from matplotlib.patches import Rectangle
-    IEM = psycopg2.connect(database='iem', host='iemdb', user='nobody')
-    cursor = IEM.cursor(cursor_factory=psycopg2.extras.DictCursor)
-
-    COOP = psycopg2.connect(database='coop', host='iemdb', user='nobody')
-    ccursor = COOP.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    pgconn_iem = psycopg2.connect(database='iem', host='iemdb', user='nobody')
+    pgconn_coop = psycopg2.connect(database='coop', host='iemdb',
+                                   user='nobody')
 
     station = fdict.get('station', 'AMW')
     network = fdict.get('network', 'IA_ASOS')
@@ -61,45 +59,23 @@ def plotter(fdict):
             weekends.append(now.day)
         now += datetime.timedelta(days=1)
 
-    highs = [-99]*days
-    lows = [99]*days
-
-    cursor.execute("""
-    SELECT day, coalesce(max_tmpf, -99), coalesce(min_tmpf, 99)
+    df = read_sql("""
+    SELECT to_char(day, 'mmdd') as sday, day, max_tmpf, min_tmpf
     from """+table+""" s JOIN stations t
     on (t.iemid = s.iemid) WHERE id = %s and network = %s and
-    day >= %s and day < %s
-    """, (station, network, sts, ets))
-
-    has_data = False
-    rows = []
-    for row in cursor:
-        has_data = True
-        rows.append(dict(day=row[0].day, high=row[1], low=row[2],
-                         climate_high=None, climate_low=None))
-        highs[int(row[0].day) - 1] = row[1]
-        lows[int(row[0].day) - 1] = row[2]
-    df = pd.DataFrame(rows)
-    highs = np.ma.array(highs)
-    highs.mask = np.ma.where(highs < -98, True, False)
-    lows = np.ma.array(lows)
-    lows.mask = np.ma.where(lows > 98, True, False)
+    day >= %s and day < %s ORDER by day ASC
+    """, pgconn_iem,  params=(station, network, sts, ets), index_col='sday')
+    has_data = (len(df.index) > 0)
 
     # Get the normals
-    ccursor.execute("""
-    SELECT valid, high, low from ncdc_climate81 where station = %s
-    and extract(month from valid) = %s
-    """, (nt.sts[station]['ncdc81'], month))
+    cdf = read_sql("""
+    SELECT to_char(valid, 'mmdd') as sday, valid, high, low from
+    ncdc_climate81 where station = %s
+    and extract(month from valid) = %s ORDER by valid ASC
+    """, pgconn_coop, params=(nt.sts[station]['ncdc81'], month),
+                   index_col='sday')
 
-    clhighs = [None]*days
-    cllows = [None]*days
-    for row in ccursor:
-        idx = int(row[0].day) - 1
-        # February leap day
-        if idx >= days:
-            continue
-        clhighs[idx] = row[1]
-        cllows[idx] = row[2]
+    df = cdf.join(df)
 
     (fig, ax) = plt.subplots(1, 1)
 
@@ -108,35 +84,36 @@ def plotter(fdict):
                          edgecolor='None')
         ax.add_patch(rect)
 
-    ax.plot(np.arange(1, days+1), clhighs, zorder=3, marker='o',
+    ax.plot(np.arange(1, days+1), df['high'].values, zorder=3, marker='o',
             color='pink')
-    ax.plot(np.arange(1, days+1), cllows, zorder=3, marker='o',
+    ax.plot(np.arange(1, days+1), df['low'].values, zorder=3, marker='o',
             color='skyblue')
     if has_data:
-        ax.bar(np.arange(1, days+1) - 0.3, highs, fc='r', ec='k', width=0.3,
+        ax.bar(np.arange(1, days+1) - 0.3, df['max_tmpf'].values,
+               fc='r', ec='k', width=0.3,
                linewidth=0.6)
-        ax.bar(np.arange(1, days+1), lows, fc='b', ec='k', width=0.3,
+        ax.bar(np.arange(1, days+1), df['min_tmpf'].values,
+               fc='b', ec='k', width=0.3,
                linewidth=0.6)
     else:
         ax.text(0.5, 0.5, "No Data Found", transform=ax.transAxes)
         ax.set_ylim(0, 1)
 
-    for i in range(days):
-        txt = ax.text(i+1-0.15, highs[i]+0.5, "%.0f" % (highs[i],),
+    i = 0
+    for _, row in df.iterrows():
+        txt = ax.text(i+1-0.15, row['max_tmpf']+0.5,
+                      "%.0f" % (row['max_tmpf'],),
                       fontsize=10, ha='center', va='bottom', color='k')
         txt.set_path_effects([PathEffects.withStroke(linewidth=2,
                                                      foreground="w")])
-        txt = ax.text(i+1+0.15, lows[i]+0.5,
-                      "%.0f" % (lows[i],), fontsize=10,
+        txt = ax.text(i+1+0.15, row['min_tmpf']+0.5,
+                      "%.0f" % (row['min_tmpf'],), fontsize=10,
                       ha='center', va='bottom', color='k')
         txt.set_path_effects([PathEffects.withStroke(linewidth=2,
                                                      foreground="w")])
-    if ccursor.rowcount > 0:
-        ax.set_ylim(min(min(cllows), min(lows))-5,
-                    max(max(clhighs), max(highs))+5)
-    else:
-        if has_data:
-            ax.set_ylim(min(lows)-5, max(highs)+5)
+        i += 1
+    ax.set_ylim(min(df['low'].min(), df['min_tmpf'].min()) - 5,
+                max(df['high'].max(), df['max_tmpf'].max()) + 5)
     ax.set_xlim(0.5, days + 0.5)
     ax.set_xticks(np.arange(1, days+1))
     ax.set_xticklabels(np.arange(1, days+1), fontsize=8)
