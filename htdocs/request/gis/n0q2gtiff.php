d5a8bfeb79e00ddbb088d0804e0d902f0e7e7b9a
akrherz <akrherz@iastate.edu>
2016-01-02 13:44:35 -0600

    Add N0Q variant of PNG 2 GTIFF

---
+++ htdocs/request/gis/n0q2gtiff.php
@@ -0,0 +1,50 @@
+<?php
+date_default_timezone_set('UTC');
+putenv("TZ=GMT");
+ /* This bad boy converts a PNG to a geo-tiff */
+
+$dstr = isset($_GET["dstr"]) ? $_GET["dstr"]: die("No dstr set");
+$year = intval( substr($dstr,0,4));
+$month = intval( substr($dstr,4,2));
+$day = intval( substr($dstr,6,2));
+$hour = intval( substr($dstr,8,2));
+$minute = intval( substr($dstr,10,2));
+$ts = mktime($hour,$minute,0,$month,$day,$year);
+$now = time();
+if ($ts > $now){ die("Request from the future?"); }
+@mkdir("/tmp/png2gtiff");
+chdir("/tmp/png2gtiff");
+
+$outFile = sprintf("n0q_%s", date("YmdHi", $ts) );
+$zipFile = sprintf("n0q_%s.zip", date("YmdHi", $ts) );
+
+if (is_file($zipFile)){
+  header("Content-type: application/octet-stream");
+  header("Content-Disposition: attachment; filename=${zipFile}");
+  readfile($zipFile);
+  die();
+}
+
+if ($ts > ($now - 360.0)){  
+  $inFile = "/home/ldm/data/gis/images/4326/USCOMP/n0q_0.tif";
+} else {
+  $inFile = sprintf("/mesonet/ARCHIVE/data/%s/GIS/uscomp/n0q_%s.png", date("Y/m/d", $ts), date("YmdHi", $ts) );
+
+}
+
+if (! is_file($inFile)) die("No GIS composite found for this time!");
+
+
+$cmd = sprintf("/usr/bin/gdalwarp -t_srs \"EPSG:4326\" -s_srs \"EPSG:4326\" -of GTIFF %s %s.tif", $inFile, $outFile);
+`$cmd`;
+
+$cmd = "zip $zipFile ${outFile}.tif";
+`$cmd`;
+
+header("Content-type: application/octet-stream");
+header("Content-Disposition: attachment; filename=${zipFile}");
+readfile($zipFile);
+
+unlink($zipFile);
+unlink("${outFile}.tif");
+?>
