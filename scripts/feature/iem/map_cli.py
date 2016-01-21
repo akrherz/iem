fc13fa8bc28e6e4484a6c3ecb27948101b4ce94c
akrherz <akrherz@iastate.edu>
2016-01-20 21:14:19 -0600

    21 Jul feature is plot of snow departures

--- scripts/feature/iem/map_cli.py
+++ scripts/feature/iem/map_cli.py
@@ -4,9 +4,9 @@ IEM = psycopg2.connect(database='iem', host='iemdb', user='nobody')
 cursor = IEM.cursor()
 
 cursor.execute("""select station, st_x(geom), st_y(geom),
-    precip_jun1 - precip_jun1_normal from
+    snow_jul1 - snow_jul1_normal from
     cli_data c JOIN stations s on (s.id = c.station)
-    WHERE s.network = 'NWSCLI' and c.valid = '2015-08-12' """)
+    WHERE s.network = 'NWSCLI' and c.valid = '2016-01-20' """)
 
 lats = []
 lons = []
@@ -23,7 +23,7 @@ for row in cursor:
 
 
 m = MapPlot(sector='midwest', axisbg='white',
-            title='NWS 1 June - 12 August Precipitation Departure [inches]',
-            subtitle='12 August 2015 based on NWS issued CLI Reports')
-m.plot_values(lons, lats, vals, fmt='%.2f', textsize=16, color=colors)
-m.postprocess(filename='150813.png')
+            title='NWS 1 Jul 2015 - 20 Jan 2016 Precipitation Departure [inches]',
+            subtitle='20 January 2016 based on NWS issued CLI Reports')
+m.plot_values(lons, lats, vals, fmt='%.1f', textsize=16, color=colors)
+m.postprocess(filename='160121.png')
