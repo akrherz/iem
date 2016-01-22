c0c6ef657dc8ee2ed7bdcaffad5c8210ff0ca870
akrherz <akrherz@iastate.edu>
2016-01-21 20:29:11 -0600

    add highcharts output to trailing precip plot
    https://mesonet.agron.iastate.edu/plotting/auto/?q=28

--- htdocs/plotting/auto/scripts/p28.py
+++ htdocs/plotting/auto/scripts/p28.py
@@ -10,6 +10,7 @@ def get_description():
     d = dict()
     today = datetime.date.today()
     d['data'] = True
+    d['highcharts'] = True
     d['description'] = """This plot presents three metrics for to date
     precipitation accumulation over a given number of trailing days.  The
     lines represent the actual and maximum accumulations for the period.
@@ -25,11 +26,9 @@ def get_description():
     return d
 
 
-def plotter(fdict):
-    """ Go """
-    import matplotlib
-    matplotlib.use('agg')
-    import matplotlib.pyplot as plt
+def get_ctx(fdict):
+    """Get the plotting context """
+    ctx = {}
     pgconn = psycopg2.connect(database='coop', host='iemdb', user='nobody')
     cursor = pgconn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
@@ -72,6 +71,7 @@ def plotter(fdict):
     ranks = []
     totals = []
     maxes = []
+    avgs = []
     myyear = date.year - baseyear - 1
     for days in range(1, 366):
         idx0 = _doy + 366 - days
@@ -84,30 +84,123 @@ def plotter(fdict):
         ranks.append(rank)
         totals.append(thisyear)
         maxes.append(sums[-1])
+        avgs.append(np.nanmean(sums))
+
+    ctx['sdate'] = date.date() - datetime.timedelta(days=360)
+    ctx['title'] = "%s %s" % (station,
+                              nt.sts[station]['name'])
+    ctx['subtitle'] = ("Trailing Days Precipitation Rank [%s-%s] to %s"
+                       ) % (baseyear+2,
+                            datetime.datetime.now().year,
+                            date.strftime("%-d %b %Y"))
+
+    ctx['ranks'] = ranks
+    ctx['totals'] = totals
+    ctx['maxes'] = maxes
+    ctx['avgs'] = avgs
+    ctx['xticks'] = xticks
+    ctx['xticklabels'] = xticklabels
+    ctx['station'] = station
+    return ctx
+
+
+def highcharts(fdict):
+    """ Go """
+    ctx = get_ctx(fdict)
+    dstart = "Date.UTC(%s, %s, %s)" % (ctx['sdate'].year,
+                                       ctx['sdate'].month - 1,
+                                       ctx['sdate'].day)
+    return """
+$("#ap_container").highcharts({
+    title: {text: '""" + ctx['title'] + """'},
+    subtitle: {text: '""" + ctx['subtitle'] + """'},
+    chart: {zoomType: 'x'},
+    yAxis: [{
+            min: 0,
+            title: {
+                text: 'Precipitation [inch]'
+            }
+        }, {
+            min: 0,
+            title: {
+                text: 'Rank [1 is wettest]'
+            },
+            opposite: true
+    }],
+    tooltip: {
+        shared: true,
+        xDateFormat: '%Y-%m-%d'
+    },
+    series : [{
+        name: 'Rank',
+        pointStart: """ + dstart + """,
+        pointInterval: 24 * 3600 * 1000, // one day
+        zIndex: 5,
+        shadow: true,
+        data: """ + str(ctx['ranks'][::-1]) + """,
+        yAxis: 1
+    },{
+        name: 'This Period',
+        tooltip: {valueDecimals: 2},
+        pointStart: """ + dstart + """,
+        pointInterval: 24 * 3600 * 1000, // one day
+        zIndex: 3,
+        data: """ + str(ctx['totals'][::-1]) + """
+    },{
+        name: 'Average',
+        tooltip: {valueDecimals: 2},
+        pointStart: """ + dstart + """,
+        pointInterval: 24 * 3600 * 1000, // one day
+        zIndex: 4,
+        data: """ + str(ctx['avgs'][::-1]) + """
+    }, {
+        name: 'Maximum',
+        zIndex: 2,
+        tooltip: {valueDecimals: 2},
+        pointStart: """ + dstart + """,
+        pointInterval: 24 * 3600 * 1000, // one day
+        data: """ + str(ctx['maxes'][::-1]) + """
+    }],
+    xAxis: {
+        type: 'datetime'
+    }
+
+});
+
+    """
+
+
+def plotter(fdict):
+    """ Go """
+    import matplotlib
+    matplotlib.use('agg')
+    import matplotlib.pyplot as plt
+    ctx = get_ctx(fdict)
 
     (fig, ax) = plt.subplots(1, 1)
-    ax.bar(np.arange(-365, 0), ranks[::-1], fc='b', ec='b')
+    y2 = ax.twinx()
+
+    y2.bar(np.arange(-365, 0), ctx['ranks'][::-1], fc='b', ec='b')
     ax.grid(True)
-    ax.set_xticks(xticks)
-    ax.set_xticklabels(xticklabels)
-    ax.set_title(("%s %s\nTrailing Days Precipitation Rank [%s-%s] to %s"
-                  ) % (station, nt.sts[station]['name'], baseyear+2,
-                       datetime.datetime.now().year,
-                       date.strftime("%-d %b %Y")))
-    ax.set_ylabel("Rank [1=wettest] (bars)", color='b')
+    ax.set_xticks(ctx['xticks'])
+    ax.set_xticklabels(ctx['xticklabels'])
+    ax.set_title(("%s\n%s") % (ctx['title'], ctx['subtitle']))
+    y2.set_ylabel("Rank [1=wettest] (bars)", color='b')
     ax.set_xlim(-367, 0.5)
 
-    y2 = ax.twinx()
-    y2.plot(np.arange(-365, 0), totals[::-1], color='r', lw=2,
+    ax.plot(np.arange(-365, 0), ctx['totals'][::-1], color='r', lw=2,
             label='This Period')
-    y2.plot(np.arange(-365, 0), maxes[::-1], color='g', lw=2,
+    ax.plot(np.arange(-365, 0), ctx['maxes'][::-1], color='g', lw=2,
             label='Maximum')
-    y2.set_ylabel("Precipitation [inch]")
+    ax.set_ylabel("Precipitation [inch]")
 
-    y2.legend()
+    ax.legend()
     df = pd.DataFrame(dict(day=np.arange(-365, 0),
-                           maxaccum=maxes[::-1],
-                           accum=totals[::-1],
-                           rank=ranks[::-1]))
+                           maxaccum=ctx['maxes'][::-1],
+                           accum=ctx['totals'][::-1],
+                           rank=ctx['ranks'][::-1]))
 
     return fig, df
+
+if __name__ == '__main__':
+    highcharts(dict())
