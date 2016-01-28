e7f612e5912403f16b392888f436e30c1fd23aec
akrherz <akrherz@iastate.edu>
2016-01-27 20:58:15 -0600

    add overall distribution to monthly dist plot
    https://mesonet.agron.iastate.edu/plotting/auto/?q=6

--- htdocs/plotting/auto/scripts/p6.py
+++ htdocs/plotting/auto/scripts/p6.py
@@ -3,6 +3,7 @@ from pandas.io.sql import read_sql
 from matplotlib import mlab
 import calendar
 import datetime
+import numpy as np
 
 PDICT = {'sum-precip': 'Total Precipitation [inch]',
          'avg-high': 'Average Daily High [F]',
@@ -30,7 +31,8 @@ def get_description():
     today = datetime.date.today()
     d['description'] = """This application plots out the distribution of
     some monthly metric for single month for all tracked sites within one
-    state."""
+    state.  The plot presents the distribution and normalized frequency
+    for a specific year and for all years combined for the given month."""
     d['data'] = True
     d['arguments'] = [
         dict(type='clstate', name='state', default='IA',
@@ -56,34 +58,63 @@ def plotter(fdict):
     year = int(fdict.get('year', 2014))
     month = int(fdict.get('month', 6))
     ptype = fdict.get('type', 'sum-precip')
+    ptype_climo = "climo_%s" % (ptype.split("-")[1], )
     table = "alldata_%s" % (state,)
 
     df = read_sql("""
-    SELECT station,
-    sum(precip) as "sum-precip",
-    avg(high) as "avg-high",
-    avg(low) as "avg-low",
-    avg((high+low)/2.) as "avg-t"
-    from """+table+""" WHERE year = %s and month = %s GROUP by station
-    """, pgconn, params=(year, month), index_col='station')
+    WITH yearly as (
+        SELECT station, year, sum(precip) as sum_precip,
+        avg(high) as avg_high, avg(low) as avg_low,
+        avg((high+low)/2.) as avg_temp from """ + table + """
+        WHERE month = %s GROUP by station, year),
+    agg1 as (
+        SELECT station, avg(sum_precip) as precip,
+        avg(avg_high) as high, avg(avg_low) as low,
+        avg(avg_temp) as temp from yearly GROUP by station),
+    thisyear as (
+        SELECT station, sum_precip, avg_high, avg_low, avg_temp from yearly
+        WHERE year = %s)
+
+    SELECT a.station, a.precip as climo_precip, a.high as climo_high,
+    a.low as climo_low, a.temp as climo_t,
+    t.sum_precip as "sum-precip", t.avg_high as "avg-high",
+    t.avg_low as "avg-low", t.avg_temp as "avg-t"
+    FROM agg1 a JOIN thisyear t on (a.station = t.station)
+    """, pgconn, params=(month, year), index_col='station')
     stateavg = df.at["%s0000" % (state, ), ptype]
 
     (fig, ax) = plt.subplots(1, 1)
-    _, bins, _ = ax.hist(df[ptype], 20, fc='lightblue', ec='lightblue',
-                         normed=1)
+    _, bins, _ = ax.hist(df[ptype].dropna(), 20, fc='lightblue',
+                         ec='lightblue', normed=1)
     y = mlab.normpdf(bins, df[ptype].mean(), df[ptype].std())
-    ax.plot(bins, y, 'r--', lw=2,
-            label=("Normal Dist.\n$\sigma$=%.2f $\mu$=%.2f"
-                   ) % (df[ptype].std(), df[ptype].mean()))
+    ax.plot(bins, y, 'b--', lw=2,
+            label=("%s Normal Dist. $\sigma$=%.2f $\mu$=%.2f"
+                   ) % (year, df[ptype].std(), df[ptype].mean()))
+
+    _, bins = np.histogram(df[ptype_climo].dropna(), 20, normed=1)
+    y = mlab.normpdf(bins, df[ptype_climo].mean(), df[ptype_climo].std())
+    ax.plot(bins, y, 'g--', lw=2,
+            label=("Climo Normal Dist. $\sigma$=%.2f $\mu$=%.2f"
+                   ) % (df[ptype_climo].std(), df[ptype_climo].mean()))
+
     if stateavg is not None:
-        ax.axvline(stateavg, label='Statewide Avg\n%.2f' % (stateavg,),
+        ax.axvline(stateavg, label='%s Statewide Avg %.2f' % (year, stateavg),
+                   color='b', lw=2)
+    stateavg = df.at["%s0000" % (state, ), ptype_climo]
+    if stateavg is not None:
+        ax.axvline(stateavg, label='Climo Statewide Avg %.2f' % (stateavg,),
                    color='g', lw=2)
-    ax.legend()
     ax.set_xlabel(PDICT[ptype])
     ax.set_ylabel("Normalized Frequency")
     ax.set_title(("%s %s %s %s Distribution\nNumber of stations: %s"
                   ) % (STATES[state], year, calendar.month_name[month],
                        PDICT[ptype], len(df.index)))
     ax.grid(True)
+    box = ax.get_position()
+    ax.set_position([box.x0, 0.25, box.width, 0.65])
+    ax.legend(ncol=2, fontsize=12, loc=(-0.05, -0.3))
 
     return fig, df
+
+if __name__ == '__main__':
+    plotter({'type': 'avg-low', 'year': 2014, 'month': 12})
