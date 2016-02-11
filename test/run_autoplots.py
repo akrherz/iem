a252b02803aaf63dbca20b44c1400809f3e6f71f
akrherz <akrherz@iastate.edu>
2016-02-11 13:04:38 -0600

    for autoplot testing, do timing on individual formats

--- test/run_autoplots.py
+++ test/run_autoplots.py
@@ -4,8 +4,8 @@ import datetime
 import pandas as pd
 
 
-def run_plot(i):
-    """Run this plot"""
+def get_formats(i):
+    """Figure out which formats this script supports"""
     uri = "http://iem.local/plotting/auto/meta/%s.json" % (i, )
     res = requests.get(uri)
     if res.status_code == 404:
@@ -21,13 +21,17 @@ def run_plot(i):
         fmts.append('txt')
     if 'highcharts' in json and json['highcharts']:
         fmts.append('js')
-    for fmt in fmts:
-        uri = "http://iem.local/plotting/auto/plot/%s/dpi:100.%s" % (i, fmt)
-        res = requests.get(uri)
-        if res.status_code != 200:
-            print("%s. %s -> HTTP: %s" % (i, uri, res.status_code))
-            print(res.text)
-            return False
+    return fmts
+
+
+def run_plot(i, fmt):
+    """Run this plot"""
+    uri = "http://iem.local/plotting/auto/plot/%s/dpi:100.%s" % (i, fmt)
+    res = requests.get(uri)
+    if res.status_code != 200:
+        print("%s. %s -> HTTP: %s" % (i, uri, res.status_code))
+        print(res.text)
+        return False
 
     return True
 
@@ -37,11 +41,17 @@ def main():
     timing = []
     i = 1  # autoplot starts at app 1 and not 0
     while True:
-        sts = datetime.datetime.now()
-        if not run_plot(i):
+        fmts = get_formats(i)
+        if not fmts:
             break
-        ets = datetime.datetime.now()
-        timing.append({'i': i, 'secs': (ets - sts).total_seconds()})
+        for fmt in fmts:
+            sts = datetime.datetime.now()
+            res = run_plot(i, fmt)
+            if not res:
+                break
+            ets = datetime.datetime.now()
+            timing.append({'i': i, 'fmt': fmt,
+                           'secs': (ets - sts).total_seconds()})
         i += 1
     df = pd.DataFrame(timing)
     df.set_index('i', inplace=True)
