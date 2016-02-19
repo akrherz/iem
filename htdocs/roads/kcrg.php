c45190fae278a1420b0945d9d1edf8027de8adb7
akrherz <akrherz@iastate.edu>
2016-02-19 14:04:28 -0600

    php mapserver7 fixes

--- htdocs/roads/kcrg.php
+++ htdocs/roads/kcrg.php
@@ -113,7 +113,6 @@ $logokey_c3s0 = ms_newStyleObj($logokey_c3);
 //$logokey_c3s0->set("size", 45);
 $l = $logokey_c3->addLabel(new labelObj());
 $logokey_c3->getLabel(0)->set("buffer", 10);
-$logokey_c3->getLabel(0)->set("type", MS_BITMAP);
 $logokey_c3->getLabel(0)->set("size", MS_MEDIUM);
 $logokey_c3->getLabel(0)->color->setRGB(0,0,0);
 $bpt = ms_newpointobj();
