093310725c525806033c615f1744ab33d163679c
akrherz <akrherz@iastate.edu>
2016-02-23 08:25:27 -0600

    less aggressively download NCEP stage IV data

--- scripts/dl/ncep_stage4.py
+++ scripts/dl/ncep_stage4.py
@@ -4,35 +4,35 @@
 """
 import datetime
 import pytz
-import urllib2
+import requests
 import os
 import subprocess
 from pyiem.util import exponential_backoff
 
 
-def download(now):
+def download(now, offset):
     """
     Download a given timestamp from NCEP and inject into LDM
     Example:  ftp://ftpprd.ncep.noaa.gov/pub/data/nccf/com/hourly/prod/
               nam_pcpn_anal.20090916/ST4.2009091618.01h.gz
     """
     hours = [1, ]
-    if now.hour % 6 == 0 and offset != 0:
+    if now.hour % 6 == 0 and offset > 24:
         hours.append(6)
-    if now.hour == 12 and offset != 0:
+    if now.hour == 12 and offset > 24:
         hours.append(24)
     for hr in hours:
         url = "%s.%02ih.gz" % (now.strftime(("http://ftpprd.ncep.noaa.gov/"
                                              "data/nccf/com/hourly/prod/"
                                              "nam_pcpn_anal.%Y%m%d/"
                                              "ST4.%Y%m%d%H")), hr)
-        data = exponential_backoff(urllib2.urlopen, url, timeout=60)
-        if data is None:
+        response = exponential_backoff(requests.get, url, timeout=60)
+        if response is None or response.status_code != 200:
             print('ncep_stage4.py: dl %s failed' % (url,))
             continue
         # Same temp file
         o = open("tmp.grib.gz", 'wb')
-        o.write(data.read())
+        o.write(response.content)
         o.close()
         subprocess.call("gunzip -f tmp.grib.gz", shell=True)
         # Inject into LDM
@@ -53,13 +53,13 @@ def download(now):
                                                  "data/nccf/com/hourly/prod/"
                                                  "nam_pcpn_anal.%Y%m%d/"
                                                  "ST2ml%Y%m%d%H")), hr)
-        data = exponential_backoff(urllib2.urlopen, url, timeout=60)
-        if data is None:
+        response = exponential_backoff(requests.get, url, timeout=60)
+        if response is None or response.status_code != 200:
             print('ncep_stage4.py: dl %s failed' % (url,))
             continue
         # Same temp file
         o = open("tmp.grib.gz", 'wb')
-        o.write(data.read())
+        o.write(response.content)
         o.close()
         subprocess.call("gunzip -f tmp.grib.gz", shell=True)
         # Inject into LDM
@@ -69,11 +69,16 @@ def download(now):
         subprocess.call(cmd, shell=True)
         os.remove('tmp.grib')
 
-if __name__ == "__main__":
+
+def main():
+    """ Do something """
     # We want this hour GMT
     utc = datetime.datetime.utcnow()
     utc = utc.replace(tzinfo=pytz.timezone("UTC"), minute=0, second=0,
                       microsecond=0)
     for offset in [33, 9, 3, 0]:
         now = utc - datetime.timedelta(hours=offset)
-        download(now)
+        download(now, offset)
+
+if __name__ == "__main__":
+    main()
