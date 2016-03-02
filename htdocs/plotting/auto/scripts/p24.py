cdb880b1a5e72381c48d2f43695a2a12d4a7d3f5
akrherz <akrherz@iastate.edu>
2016-03-01 20:37:38 -0600

    allow month climate district plot to do seasons
    https://mesonet.agron.iastate.edu/plotting/auto/?q=24

--- htdocs/plotting/auto/scripts/p24.py
+++ htdocs/plotting/auto/scripts/p24.py
@@ -1,12 +1,30 @@
-import psycopg2.extras
+import psycopg2
 import calendar
 import matplotlib.cm as cm
 import datetime
 from pandas.io.sql import read_sql
-
+from collections import OrderedDict
 
 PDICT = {'precip': 'Total Precipitation',
          'avgt': 'Average Temperature'}
+
+MDICT = OrderedDict([
+         ('spring', 'Spring (MAM)'),
+         ('fall', 'Fall (SON)'),
+         ('winter', 'Winter (DJF)'),
+         ('summer', 'Summer (JJA)'),
+         ('jan', 'January'),
+         ('feb', 'February'),
+         ('mar', 'March'),
+         ('apr', 'April'),
+         ('may', 'May'),
+         ('jun', 'June'),
+         ('jul', 'July'),
+         ('aug', 'August'),
+         ('sep', 'September'),
+         ('oct', 'October'),
+         ('nov', 'November'),
+         ('dec', 'December')])
 
 
 def get_description():
@@ -17,16 +35,17 @@ def get_description():
     d['description'] = """This map presents IEM computed precipitation
     ranks for a month and year of your choice.  The map unit is climate
     districts for which the IEM generates spatially weighted daily averages
-    for."""
+    for.  For seasonal totals, the year presented is the calendar year of
+    the last month in the three month period."""
     today = datetime.date.today() - datetime.timedelta(days=28)
     d['arguments'] = [
         dict(type='select', name='var', default='precip',
              label='Select Variable', options=PDICT),
         dict(type='year', name='year', default=today.year,
              label='Select Year:',
-             min=1893),  # Comes back to python as yyyy-mm-dd
-        dict(type='month', name='month', default=today.month,
-             label='Select Month:'),
+             min=1893),
+        dict(type='select', name='month', default=today.strftime("%b").lower(),
+             label='Month Limiter', options=MDICT),
     ]
     return d
 
@@ -39,32 +58,52 @@ def plotter(fdict):
     pgconn = psycopg2.connect(database='coop', host='iemdb', user='nobody')
 
     year = int(fdict.get('year', 2014))
-    month = int(fdict.get('month', 9))
+    month = fdict.get('month', 'sep')
     varname = fdict.get('var', 'precip')
+    l = "0 days"
+    if month == 'fall':
+        months = [9, 10, 11]
+        label = "Fall (SON)"
+    elif month == 'winter':
+        months = [12, 1, 2]
+        l = "32 days"
+        label = "Winter (DJF)"
+    elif month == 'spring':
+        months = [3, 4, 5]
+        label = "Spring (MAM)"
+    elif month == 'summer':
+        months = [6, 7, 8]
+        label = "Summer (JJA)"
+    else:
+        ts = datetime.datetime.strptime("2000-"+month+"-01", '%Y-%b-%d')
+        # make sure it is length two for the trick below in SQL
+        months = [ts.month, 999]
+        label = calendar.month_name[ts.month]
 
     lastyear = datetime.date.today().year
     years = lastyear - 1893 + 1
 
     df = read_sql("""
     with monthly as (
-        SELECT year, station,
-        sum(precip) as p,
+        SELECT extract(year from day + '"""+l+"""'::interval) as myyear,
+        station, sum(precip) as p,
         avg((high+low)/2.) as avgt
         from alldata
-        WHERE substr(station,3,1) = 'C' and month = %s
-        GROUP by year, station),
+        WHERE substr(station,3,1) = 'C' and month in %s
+        GROUP by myyear, station),
     ranks as (
-        SELECT station, year,
+        SELECT station, myyear as year,
         rank() OVER (PARTITION by station ORDER by p DESC) as precip_rank,
         rank() OVER (PARTITION by station ORDER by avgt DESC) as avgt_rank
         from monthly)
 
     SELECT station, precip_rank, avgt_rank from ranks
-    where year = %s """, pgconn, params=(month, year), index_col='station')
+    where year = %s """, pgconn, params=(tuple(months), year),
+                  index_col='station')
 
     m = MapPlot(sector='midwest', axisbg='white',
                 title='%s %s %s Ranks by Climate District' % (
-                        year, calendar.month_name[month], PDICT[varname]),
+                        year, label, PDICT[varname]),
                 subtitle=('Based on IEM Estimates, '
                           '1 is %s out of %s total years (1893-%s)'
                           ) % ('wettest' if varname == 'precip' else 'hottest',
