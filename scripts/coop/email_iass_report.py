d9d472797b80d031042a8254b9c9759f3a5d4ce3
akrherz <akrherz@iastate.edu>
2016-04-01 11:02:17 -0500

    prevent None from tripping up IASS Report
    also fix most pep8 formatting, fixes #59

--- scripts/coop/email_iass_report.py
+++ scripts/coop/email_iass_report.py
@@ -1,9 +1,8 @@
-'''
+"""
  Generate and email a report to the IASS folks with summarized IEM estimated
  COOP data included...
-'''
+"""
 import sys
-import psycopg2
 import psycopg2.extras
 from pyiem.network import Table as NetworkTable
 import cStringIO
@@ -16,92 +15,101 @@ from email.mime.multipart import MIMEMultipart
 nt = NetworkTable("IA_COOP")
 
 districts = [
-'North West',
-'North Central',
-'North East',
-'West Central',
-'Central',
-'East Central',
-'South West',
-'South Central',
-'South East',             
-]
+             'North West',
+             'North Central',
+             'North East',
+             'West Central',
+             'Central',
+             'East Central',
+             'South West',
+             'South Central',
+             'South East',
+            ]
 stids = [
 [
-'Rock Rapids    RKRI4',
-'Sheldon    SHDI4',
-'Sibley    SIBI4',
-'Sioux Center    SIXI4',
-'Spirit Lake    VICI4',
-'Storm Lake    SLBI4',
- ],[   
-'Algona    ALGI4',
-'Charles City    CIYI4',
-'Dakota City    DAKI4',
-'Hampton    HPTI4',
-'Mason City    MCWI4',
-'Osage    OSAI4',
- ],[      
-'Cresco    CRCI4',
-'Decorah    DCRI4',
-'Dubuque    DLDI4',
-'Fayette    FYTI4',
-'Manchester    MHRI4',
-'Tripoli    TRPI4',
-     ],[  
-'Audubon    AUDI4',
-'Carroll    CINI4',
-'Jefferson    JFFI4',
-'Logan    LOGI4',
-'Mapleton    MPTI4',
-'Rockwell City    RKWI4',
-     ],[  
-'Boone    BNWI4',
-'Grinnell    GRII4',
-'Grundy Center    GNDI4',
-'Iowa Falls    IWAI4',
-'Marshalltown    MSHI4',
-'Webster City    WEBI4',
-     ],[  
-'Anamosa    AMOI4',
-'Cedar Rapids    CRPI4',
-'Clinton    CLNI4',
-'Lowden    LWDI4',
-'Maquoketa    MKTI4',
-'Vinton    VNTI4',
-     ],[  
-'Atlantic    ATLI4',
-'Clarinda    CLDI4',
-'Glenwood    GLNI4',
-'Oakland    OAKI4',
-'Red Oak    ROKI4',
-'Sidney    SIDI4',
-     ],[  
-'Allerton    ALRI4',
-'Beaconsfield    BCNI4',
-'Centerville    CNTI4',
-'Indianola    IDAI4',
-'Lamoni    3OI',
-'Osceola    OSEI4',
-     ],[  
-'Bloomfield    BLMI4',
-'Donnellson    DNNI4',
-'Fairfield    FRFI4',
-'Keosauqua    KEQI4',
-'Washington    WSHI4',
- ]
+ 'Rock Rapids    RKRI4',
+ 'Sheldon    SHDI4',
+ 'Sibley    SIBI4',
+ 'Sioux Center    SIXI4',
+ 'Spirit Lake    VICI4',
+ 'Storm Lake    SLBI4',
+],
+[
+ 'Algona    ALGI4',
+ 'Charles City    CIYI4',
+ 'Dakota City    DAKI4',
+ 'Hampton    HPTI4',
+ 'Mason City    MCWI4',
+ 'Osage    OSAI4',
+],
+[
+ 'Cresco    CRCI4',
+ 'Decorah    DCRI4',
+ 'Dubuque    DLDI4',
+ 'Fayette    FYTI4',
+ 'Manchester    MHRI4',
+ 'Tripoli    TRPI4',
+],
+[
+ 'Audubon    AUDI4',
+ 'Carroll    CINI4',
+ 'Jefferson    JFFI4',
+ 'Logan    LOGI4',
+ 'Mapleton    MPTI4',
+ 'Rockwell City    RKWI4',
+],
+[
+ 'Boone    BNWI4',
+ 'Grinnell    GRII4',
+ 'Grundy Center    GNDI4',
+ 'Iowa Falls    IWAI4',
+ 'Marshalltown    MSHI4',
+ 'Webster City    WEBI4',
+],
+[
+ 'Anamosa    AMOI4',
+ 'Cedar Rapids    CRPI4',
+ 'Clinton    CLNI4',
+ 'Lowden    LWDI4',
+ 'Maquoketa    MKTI4',
+ 'Vinton    VNTI4',
+],
+[
+ 'Atlantic    ATLI4',
+ 'Clarinda    CLDI4',
+ 'Glenwood    GLNI4',
+ 'Oakland    OAKI4',
+ 'Red Oak    ROKI4',
+ 'Sidney    SIDI4',
+],
+[
+ 'Allerton    ALRI4',
+ 'Beaconsfield    BCNI4',
+ 'Centerville    CNTI4',
+ 'Indianola    IDAI4',
+ 'Lamoni    3OI',
+ 'Osceola    OSEI4',
+],
+[
+ 'Bloomfield    BLMI4',
+ 'Donnellson    DNNI4',
+ 'Fairfield    FRFI4',
+ 'Keosauqua    KEQI4',
+ 'Washington    WSHI4',
 ]
+]
+
 
 def compute_weekly(fp, sts, ets):
     ''' Compute the weekly stats we need '''
     pgconn = psycopg2.connect(database='iem', host='iemdb', user='nobody')
     cursor = pgconn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    
+
     # Max daily high
     # min daily high
-    # average temperature 
+    # average temperature
     # departure from normal for average temp
-    # Total precip 
+    # Total precip
     # precip departure from normal
     # since 1 april precip
     # since 1 april departure
@@ -112,29 +120,29 @@ def compute_weekly(fp, sts, ets):
     apr1 = "%s-04-01" % (sts.year,)
     cursor.execute("""
     WITH obs as (
-        SELECT id as station , climate_site, 
+        SELECT id as station , climate_site,
         max(max_tmpf) as hi,
         min(min_tmpf)  as lo,
         avg((max_tmpf+min_tmpf)/2.) as avg,
         sum(case when pday >= 0.01 then pday else 0 end) as total_p
-        FROM summary s JOIN stations t on (t.iemid = s.iemid) 
-        WHERE day >= %s and day <= %s 
-        and t.network = 'IA_COOP' and max_tmpf > -90 
+        FROM summary s JOIN stations t on (t.iemid = s.iemid)
+        WHERE day >= %s and day <= %s
+        and t.network = 'IA_COOP' and max_tmpf > -90
         and min_tmpf < 90
         GROUP by station, climate_site
     ), april_obs as (
         SELECT id as station,  climate_site,
-        sum(case when pday >= 0.01 then pday else 0 end) as p, 
+        sum(case when pday >= 0.01 then pday else 0 end) as p,
         sum(gddxx(50,86,max_tmpf,min_tmpf)) as gdd
         from summary s JOIN stations t on (t.iemid = s.iemid)
-        WHERE t.network = 'IA_COOP' and 
+        WHERE t.network = 'IA_COOP' and
         day >= %s and day <= %s
         GROUP by station, climate_site
     ), climo as (
         SELECT station,
         avg((high+low)/2.) as avg,
         sum(precip) as avg_p
-        from climate51 
+        from climate51
         WHERE to_char(valid, 'mmdd') >= %s and to_char(valid, 'mmdd') <= %s
         GROUP by station
     ), april_climo as (
@@ -162,37 +170,38 @@ def compute_weekly(fp, sts, ets):
     data = {}
     for row in cursor:
         data[row['station']] = row
-        
+
     for district, sector in zip(districts, stids):
         fp.write("%s District\n" % (district,))
         for sid in sector:
             nwsli = sid[-5:].strip()
             name = sid[:-5].strip()
-            if not data.has_key(nwsli):
+            if nwsli not in data:
                 print 'Missing NWSLI: %s' % (nwsli,)
                 continue
-            fp.write("%-15.15s %3.0f %3.0f %3.0f %3.0f  %5.2f  %5.2f   %5.2f %6.2f %7.0f %5.0f\n" % (name, 
-                                            data[nwsli]['hi'], 
-                                            data[nwsli]['lo'], data[nwsli]['avg'],
-                                            data[nwsli]['temp_dfn'],
-                                            data[nwsli]['total_p'],
-                                            data[nwsli]['precip_dfn'],
-                                            data[nwsli]['april_p'],
-                                            data[nwsli]['april_p_dfn'],
-                                            data[nwsli]['april_gdd'],
-                                            data[nwsli]['april_gdd_dfn']))
+            fp.write(("%-15.15s %3.0f %3.0f %3.0f %3.0f  %5.2f  %5.2f   "
+                      "%5.2f %6.2f %7.0f %5.0f\n"
+                      ) % (name, data[nwsli]['hi'],
+                           data[nwsli]['lo'], data[nwsli]['avg'],
+                           data[nwsli]['temp_dfn'],
+                           data[nwsli]['total_p'],
+                           data[nwsli]['precip_dfn'],
+                           data[nwsli]['april_p'],
+                           data[nwsli]['april_p_dfn'],
+                           data[nwsli]['april_gdd'],
+                           data[nwsli]['april_gdd_dfn']))
 
 
 def compute_monthly(fp, year, month):
     ''' Compute the monthly data we need to compute '''
     pgconn = psycopg2.connect(database='iem', host='iemdb', user='nobody')
     cursor = pgconn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    
+
     # Max daily high
     # min daily high
-    # average temperature 
+    # average temperature
     # departure from normal for average temp
-    # Total precip 
+    # Total precip
     # precip departure from normal
     # days with measurable precip
     # heating degree days
@@ -202,10 +211,10 @@ def compute_monthly(fp, year, month):
     sts = datetime.datetime(year, month, 1)
     ets = sts + datetime.timedelta(days=35)
     ets = ets.replace(day=1)
-    
+
     cursor.execute("""
     WITH obs as (
-        SELECT id as station, climate_site,  
+        SELECT id as station, climate_site,
         max(max_tmpf) as hi,
         min(min_tmpf)  as lo,
         avg((max_tmpf+min_tmpf)/2.) as avg,
@@ -214,7 +223,7 @@ def compute_monthly(fp, year, month):
         sum(hdd65(max_tmpf,min_tmpf)) as hdd,
         sum(case when min_tmpf <= 32 then 1 else 0 end) as days32,
         sum(case when min_tmpf <= 28 then 1 else 0 end) as days28
-        FROM summary s JOIN stations t on (t.iemid = s.iemid) 
+        FROM summary s JOIN stations t on (t.iemid = s.iemid)
         WHERE t.network = 'IA_COOP' and day >= %s and day < %s
         GROUP by station, climate_site
     ), climo as (
@@ -241,39 +250,45 @@ def compute_monthly(fp, year, month):
     data = {}
     for row in cursor:
         data[row['station']] = row
-        
+
     for district, sector in zip(districts, stids):
         fp.write("%s District\n" % (district,))
         for sid in sector:
             nwsli = sid[-5:].strip()
             name = sid[:-5].strip()
-            fp.write("%-15.15s %3.0f %3.0f %3.0f %3.0f  %5.2f  %5.2f   %2s %5.0f %5.0f %3s  %3s\n" % (name, 
-                                            data[nwsli]['hi'], 
-                                            data[nwsli]['lo'], data[nwsli]['avg'],
-                                            data[nwsli]['temp_dfn'],
-                                            data[nwsli]['total_p'],
-                                            data[nwsli]['precip_dfn'],
-                                            data[nwsli]['days'],
-                                            data[nwsli]['hdd'] or 0,
-                                            data[nwsli]['hdd_dfn'] or 0,
-                                            data[nwsli]['days32'],
-                                            data[nwsli]['days28']))
-    
+            if data[nwsli]['hi'] is None:
+                fp.write(("%-15.15s  --- No Observations Reported ---\n"
+                          ) % (name, ))
+                continue
+            fp.write(("%-15.15s %3.0f %3.0f %3.0f %3.0f  %5.2f  %5.2f   "
+                      "%2s %5.0f %5.0f %3s  %3s\n"
+                      ) % (name, data[nwsli]['hi'],
+                           data[nwsli]['lo'], data[nwsli]['avg'],
+                           data[nwsli]['temp_dfn'],
+                           data[nwsli]['total_p'],
+                           data[nwsli]['precip_dfn'],
+                           data[nwsli]['days'],
+                           data[nwsli]['hdd'] or 0,
+                           data[nwsli]['hdd_dfn'] or 0,
+                           data[nwsli]['days32'],
+                           data[nwsli]['days28']))
+
 
 def monthly_header(fp, sts, ets):
     ''' Write the monthly header information '''
     fp.write("""Weather Summary For Iowa Agricultural Statistics Service
 Prepared By Iowa Environmental Mesonet
- 
-For the Period:    %s 
-            To:    %s 
- 
+
+For the Period:    %s
+            To:    %s
+
                                                                DAYS DAYS
-                      AIR                                       OF   OF 
+                      AIR                                       OF   OF
                   TEMPERATURE      PRECIPITATION     HDD   HDD  32   28
 STATION          HI  LO AVG DFN  TOTAL    DFN DAYS   TOT   DFN COLD COLD
--------          --  --  --  --  -----   ----   --   ---   ---  --   -- 
+-------          --  --  --  --  -----   ----   --   ---   ---  --   --
 """ % (sts.strftime("%A %B %d, %Y"), ets.strftime("%A %B %d, %Y")))
+
 
 def weekly_header(fp, sts, ets):
     ''' Write the header information '''
@@ -290,7 +305,8 @@ For the Period:     %s
 STATION          HI  LO AVG DFN  TOTAL    DFN   TOTAL    DFN   TOTAL   DFN
 -------          --  --  --  --  -----   ----   -----   ----   -----  ----
 """ % (sts.strftime("%A %B %d, %Y"), ets.strftime("%A %B %d, %Y")))
-    
+
+
 def email_report(report, subject):
     ''' Actually do the emailing stuff '''
     msg = MIMEMultipart()
@@ -298,10 +314,10 @@ def email_report(report, subject):
     msg['From'] = 'mesonet@mesonet.agron.iastate.edu'
     msg['Cc'] = 'akrherz@iastate.edu'
     msg['To'] = 'NASSRFOUMR@nass.usda.gov'
-    #msg['To'] = 'akrherz@localhost'
+    # msg['To'] = 'akrherz@localhost'
     msg.preamble = 'Report'
 
-    fn = "iem.txt" 
+    fn = "iem.txt"
 
     b = MIMEBase('Text', 'Plain')
     report.seek(0)
@@ -314,15 +330,13 @@ def email_report(report, subject):
     s = smtplib.SMTP('localhost')
     s.sendmail(msg['From'], [msg['To'], msg['Cc']], msg.as_string())
     s.quit()
-    
 
-    
+
 if __name__ == '__main__':
-    ''' If we are in Nov,Dec,Jan,Feb,Mar -> do monthly report 
-     otherwise, do the weekly 
-     This script is run each Monday or the first of the month 
-     from RUN_2AM script
-    '''
+    # If we are in Nov,Dec,Jan,Feb,Mar -> do monthly report
+    # otherwise, do the weekly
+    # This script is run each Monday or the first of the month
+    # from RUN_2AM script
     today = datetime.date.today()
     yesterday = today - datetime.timedelta(days=1)
     rtype = sys.argv[1]
@@ -338,26 +352,26 @@ if __name__ == '__main__':
             report.seek(0)
             print report.read()
         if rtype == 'weekly':
-            sts = datetime.date(int(sys.argv[2]), int(sys.argv[3]), 
+            sts = datetime.date(int(sys.argv[2]), int(sys.argv[3]),
                                 int(sys.argv[4]))
             ets = sts + datetime.timedelta(days=6)
             report = cStringIO.StringIO()
             weekly_header(report, sts, ets)
-            compute_weekly(report, sts, ets)    
+            compute_weekly(report, sts, ets)
             report.seek(0)
-            print report.read()    
+            print report.read()
     else:
-        if rtype == "monthly" and yesterday.month in (11,12,1,2,3):
-            sts = yesterday.replace(day=1) 
+        if rtype == "monthly" and yesterday.month in (11, 12, 1, 2, 3):
+            sts = yesterday.replace(day=1)
             ets = yesterday
             report = cStringIO.StringIO()
             monthly_header(report, sts, ets)
             compute_monthly(report, sts.year, sts.month)
             email_report(report, "IEM Monthly Data Report")
-        if rtype == "weekly" and today.month in range(4,11):        
+        if rtype == "weekly" and today.month in range(4, 11):
             sts = today - datetime.timedelta(days=7)
             ets = yesterday
             report = cStringIO.StringIO()
             weekly_header(report, sts, ets)
-            compute_weekly(report, sts, ets)    
+            compute_weekly(report, sts, ets)
             email_report(report, "IEM Weekly Data Report")
