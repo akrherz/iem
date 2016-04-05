705935bb2785e831d4a22bda16501f00e02f6b40
akrherz <akrherz@iastate.edu>
2016-04-04 20:36:28 -0500

    allow month/season limit for daily temp deltas
    https://mesonet.agron.iastate.edu/plotting/auto/?q=139

--- htdocs/plotting/auto/scripts100/p139.py
+++ htdocs/plotting/auto/scripts100/p139.py
@@ -2,6 +2,26 @@ import psycopg2
 from pyiem.network import Table as NetworkTable
 from pandas.io.sql import read_sql
 import datetime
+from collections import OrderedDict
+
+MDICT = OrderedDict([
+        ('all', 'No Month/Time Limit'),
+        ('spring', 'Spring (MAM)'),
+        ('fall', 'Fall (SON)'),
+        ('winter', 'Winter (DJF)'),
+        ('summer', 'Summer (JJA)'),
+        ('jan', 'January'),
+        ('feb', 'February'),
+        ('mar', 'March'),
+        ('apr', 'April'),
+        ('may', 'May'),
+        ('jun', 'June'),
+        ('jul', 'July'),
+        ('aug', 'August'),
+        ('sep', 'September'),
+        ('oct', 'October'),
+        ('nov', 'November'),
+        ('dec', 'December')])
 
 
 def get_description():
@@ -15,6 +35,8 @@ def get_description():
     d['arguments'] = [
         dict(type='zstation', name='zstation', default='AMW',
              label='Select Station:'),
+        dict(type='select', name='month', default='all',
+             label='Month Limiter', options=MDICT),
     ]
     return d
 
@@ -32,25 +54,45 @@ def plotter(fdict):
 
     station = fdict.get('zstation', 'AMW')
     network = fdict.get('network', 'IA_ASOS')
+    month = fdict.get('month', 'all')
+
+    if month == 'all':
+        months = range(1, 13)
+    elif month == 'fall':
+        months = [9, 10, 11]
+    elif month == 'winter':
+        months = [12, 1, 2]
+    elif month == 'spring':
+        months = [3, 4, 5]
+    elif month == 'summer':
+        months = [6, 7, 8]
+    else:
+        ts = datetime.datetime.strptime("2000-"+month+"-01", '%Y-%b-%d')
+        # make sure it is length two for the trick below in SQL
+        months = [ts.month, 999]
 
     nt = NetworkTable(network)
 
     df = read_sql("""WITH data as (
         SELECT date(valid at time zone %s) as date, max(tmpf), min(tmpf)
         from alldata where station = %s and tmpf between -100 and 150
+        and extract(month from valid at time zone %s) in %s
         GROUP by date)
 
     SELECT date, min, max, max - min as difference from data
     ORDER by difference DESC, date DESC LIMIT 10
-        """, pgconn, params=(nt.sts[station]['tzname'], station),
+        """, pgconn, params=(nt.sts[station]['tzname'], station,
+                             nt.sts[station]['tzname'], tuple(months)),
                   parse_dates=('date',), index_col=None)
     df['rank'] = df['difference'].rank(ascending=False, method='min')
     fig = plt.figure(figsize=(5.5, 4))
     fig.text(0.5, 0.9, ("%s [%s] %s-%s\n"
-                        "Top 10 Local Calendar Day Temperature Differences"
+                        "Top 10 Local Calendar Day [%s] "
+                        "Temperature Differences"
                         ) % (nt.sts[station]['name'], station,
                              nt.sts[station]['archive_begin'].year,
-                             datetime.date.today().year), ha='center')
+                             datetime.date.today().year,
+                             month.capitalize()), ha='center')
     fig.text(0.1, 0.81, " #  Date         Diff   Low High",
              fontproperties=font0)
     y = 0.74
