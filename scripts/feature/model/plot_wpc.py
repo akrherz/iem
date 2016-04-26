33435eba32322bae3b544b2c217f89577dd50967
akrherz <akrherz@iastate.edu>
2016-04-26 12:30:40 -0500

    26 Apr feature of WPC precip

--- scripts/feature/model/plot_wpc.py
+++ scripts/feature/model/plot_wpc.py
@@ -3,17 +3,17 @@ from pyiem.plot import MapPlot
 import numpy as np
 from pyiem.datatypes import distance
 
-g = pygrib.open('p120m_2015061800f120.grb')
+g = pygrib.open('/tmp/p168m_2016042600f168.grb')
 
 grb = g[1]
 lats, lons = grb.latlons()
 
-m = MapPlot(sector='conus',
+m = MapPlot(sector='midwest', axisbg='tan',
             title=("Weather Prediction Center (WPC) "
-                   "5 Day Forecasted Precipitation"),
-            subtitle='7 PM 18 June thru 7 PM 23 June 2015')
+                   "7 Day Forecasted Precipitation"),
+            subtitle='7 PM 25 April thru 7 PM 2 May 2016')
 
 m.contourf(lons, lats, distance(grb['values'], 'MM').value('IN'),
-           np.arange(0, 7.6, 0.25), units='inches')
+           np.arange(0, 4.6, 0.25), units='inches')
 
 m.postprocess(filename='test.png')
