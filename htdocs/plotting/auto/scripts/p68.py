d00ececf5187448bfc94f8949700685f165aca87
akrherz <akrherz@iastate.edu>
2016-05-09 20:58:55 -0500

    improve yearly VTEC counts app to include diagnostic
    https://mesonet.agron.iastate.edu/plotting/auto/?q=68

--- htdocs/plotting/auto/scripts/p68.py
+++ htdocs/plotting/auto/scripts/p68.py
@@ -12,7 +12,9 @@ def get_description():
     significance combinations issued by a NWS Forecast Office for a given year.
     Please note that not all current-day VTEC products were started in 2005,
     some came a few years later.  So numbers in 2005 are not directly
-    comparable to 2015.
+    comparable to 2015.  Here is a
+<a href="http://www.nws.noaa.gov/os/vtec/pdfs/VTEC_explanation6.pdf">handy
+ chart</a> with more details on VTEC and codes used in this graphic.
     """
     d['arguments'] = [
         dict(type='networkselect', name='station', network='WFO',
@@ -33,40 +35,62 @@ def plotter(fdict):
     nt = NetworkTable('WFO')
     nt.sts['_ALL'] = {'name': 'All Offices'}
 
-    (fig, ax) = plt.subplots(1, 1, sharex=True)
+    fig = plt.figure(figsize=(8,
+                              14 if station != '_ALL' else 21))
+    ax = [None, None]
+    ax[0] = plt.axes([0.1, 0.75, 0.85, 0.2])
+    ax[1] = plt.axes([0.1, 0.05, 0.85, 0.65])
 
     if station == '_ALL':
         df = read_sql("""
-            with obs as (
-                SELECT distinct extract(year from issue) as yr,
+            SELECT distinct extract(year from issue) as year,
                 phenomena, significance from warnings WHERE
                 phenomena is not null and significance is not null and
                 issue > '2005-01-01' and issue is not null
-            )
-            SELECT yr as year, count(*) from obs GROUP by yr ORDER by yr ASC
             """, pgconn, index_col=None)
     else:
         df = read_sql("""
-            with obs as (
-                SELECT distinct extract(year from issue) as yr,
-                phenomena, significance from warnings WHERE
-                wfo = %s and phenomena is not null and significance is not null
-                and issue > '2005-01-01' and issue is not null
-            )
-            SELECT yr as year, count(*) from obs GROUP by yr ORDER by yr ASC
+            SELECT distinct extract(year from issue) as year,
+            phenomena, significance from warnings WHERE
+            wfo = %s and phenomena is not null and significance is not null
+            and issue > '2005-01-01' and issue is not null
             """, pgconn, params=(station, ), index_col=None)
 
     df['wfo'] = station
     df['year'] = df['year'].astype('i')
+    gdf = df.groupby('year').count()
 
-    ax.bar(df['year']-0.4, df['count'], width=0.8, fc='b', ec='b')
-    for yr, val in zip(df['year'], df['count']):
-        ax.text(yr, val+1, "%s" % (val,), ha='center')
-    ax.set_title(("[%s] NWS %s\nCount of Distinct VTEC Phenomena/"
-                  "Significance - %i to %i"
-                  ) % (station, nt.sts[station]['name'],
-                       df['year'].min(), df['year'].max()))
-    ax.grid()
-    ax.set_ylabel("Count")
+    ax[0].bar(gdf.index.values, gdf['wfo'], width=0.8, fc='b', ec='b',
+              align='center')
+    for yr, row in gdf.iterrows():
+        ax[0].text(yr, row['wfo'] + 1, "%s" % (row['wfo'],), ha='center')
+    ax[0].set_title(("[%s] NWS %s\nCount of Distinct VTEC Phenomena/"
+                     "Significance - %i to %i"
+                     ) % (station, nt.sts[station]['name'],
+                          df['year'].min(), df['year'].max()))
+    ax[0].grid()
+    ax[0].set_ylabel("Count")
+    ax[0].set_xlim(gdf.index.values.min() - 0.5,
+                   gdf.index.values.max() + 0.5)
 
+    pos = {}
+    i = 1
+    df.sort_values(['phenomena', 'significance'], inplace=True)
+    for _, row in df.iterrows():
+        key = "%s.%s" % (row['phenomena'], row['significance'])
+        if key not in pos:
+            pos[key] = i
+            i += 1
+        ax[1].text(row['year'], pos[key], key, ha='center',
+                   va='center', fontsize=10,
+                   bbox=dict(color='white'))
+
+    ax[1].set_title("VTEC <Phenomena.Significance> Issued by Year")
+    ax[1].set_ylim(0, i)
+    ax[1].grid(True)
+    ax[1].set_xlim(gdf.index.values.min() - 0.5,
+                   gdf.index.values.max() + 0.5)
     return fig, df
+
+if __name__ == '__main__':
+    plotter(dict())
