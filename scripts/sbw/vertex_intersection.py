1bfaa524802edb2c935bbe366d208371e695aab6
akrherz <akrherz@iastate.edu>
2016-05-23 22:18:22 -0500

    save some vertex work

--- scripts/sbw/vertex_intersection.py
+++ scripts/sbw/vertex_intersection.py
@@ -1,25 +1,45 @@
 import psycopg2
+from pyiem.network import Table as NetworkTable
 
 pgconn = psycopg2.connect(database='postgis', host='localhost', port=5555,
                           user='nobody')
 cursor = pgconn.cursor()
 
-cursor.execute("""
+nt = NetworkTable("WFO")
+
+print("WFO,CNTY_HITS,CWA_HITS,ALL")
+for wfo in nt.sts.keys():
+    cursor.execute("""
     WITH dumps as (
-        select (st_dumppoints(geom)).* from sbw_2016 where wfo = 'DMX'
-        and phenomena = 'SV' and status = 'NEW'),
+        select (st_dumppoints(ST_Transform(geom, 2163))).* from sbw
+        where wfo = %(wfo)s
+        and phenomena in ('SV', 'TO')
+        and status = 'NEW' and issue > '2007-10-01'),
     points as (
         SELECT path, geom from dumps),
     cbuf as (
         SELECT ugc, name,
         ST_buffer(ST_exteriorring(ST_geometryn(ST_multi(
-            geom),1)),0.02) as bgeom
-        from ugcs where wfo = 'DMX'
+            st_transform(geom, 2163)),1)),2000.) as bgeom
+        from ugcs where wfo = %(wfo)s
         and substr(ugc, 3, 1) = 'C' and end_ts is null),
+    wbuf as (
+        SELECT wfo,
+        ST_buffer(ST_exteriorring(ST_geometryn(ST_multi(
+            st_transform(the_geom, 2163)),1)),2000.) as wgeom
+        from cwa where wfo = %(wfo)s),
     agg as (
-        select (path)[3] as pt, c.name from points p LEFT JOIN cbuf c ON
-        ST_Within(p.geom, c.bgeom) where (path)[3] != 1)
-    select sum(case when name is null then 1 else 0 end), count(*) from agg
-    """)
+        select (path)[3] as pt, p.geom, c.name
+        from points p LEFT JOIN cbuf c ON
+        ST_Within(p.geom, c.bgeom) where (path)[3] != 1),
+    agg2 as (
+        select a.pt, a.name, c.wfo from agg a LEFT JOIN wbuf c ON
+        ST_Within(a.geom, c.wgeom))
 
-print cursor.fetchall()
+    select sum(case when name is not null then 1 else 0 end),
+    sum(case when wfo is not null then 1 else 0 end), count(*) from agg2
+    """, dict(wfo=wfo))
+
+    if cursor.rowcount > 0:
+        row = cursor.fetchone()
+        print("%s,%s,%s,%s" % (wfo, row[0], row[1], row[2]))
