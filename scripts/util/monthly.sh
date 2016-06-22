42fc903171ebc5d78265326aeac59414e4b97be2
akrherz <akrherz@iastate.edu>
2016-06-22 14:42:49 -0500

    automate monthly.sh script running

--- scripts/util/monthly.sh
+++ scripts/util/monthly.sh
@@ -4,38 +4,28 @@ export MM="${2}"
 export YY="${1}"
 export YYYY="20${YY}"
 
-echo "Lets do SAO data!"
+# echo "Lets do SAO data!"
 cd /mesonet/data/text/sao/
 gzip ${YY}${MM}????.sao
 mkdir -p /mesonet/ARCHIVE/raw/sao/${YYYY}_${MM}
 mv ${YY}${MM}????.sao.gz /mesonet/ARCHIVE/raw/sao/${YYYY}_${MM}/
 
-echo "Lets go ASOS.gem"
+# echo "Lets go ASOS.gem"
 cd /mesonet/data/gempak/asos
 mkdir -p /mesonet/ARCHIVE/gempak/surface/ASOS/${YYYY}_${MM}
 mv ${YY}${MM}??_asos.gem /mesonet/ARCHIVE/gempak/surface/ASOS/${YYYY}_${MM}/
 
-echo "Lets go AWOS.gem"
+# echo "Lets go AWOS.gem"
 cd /mesonet/data/gempak/awos
 mkdir -p /mesonet/ARCHIVE/gempak/surface/AWOS/${YYYY}_${MM}
 mv ${YY}${MM}??_awos.gem /mesonet/ARCHIVE/gempak/surface/AWOS/${YYYY}_${MM}/
 
-echo "Lets go meso.gem"
+# echo "Lets go meso.gem"
 cd /mesonet/data/gempak/meso
 mkdir -p /mesonet/ARCHIVE/gempak/surface/mesonet/${YYYY}_${MM}
 mv ${YY}${MM}??_meso.gem /mesonet/ARCHIVE/gempak/surface/mesonet/${YYYY}_${MM}/
 
-echo "Lets go rwis.gem"
-cd /mesonet/data/gempak/rwis
-mkdir -p /mesonet/ARCHIVE/gempak/surface/RWIS/${YYYY}_${MM}
-mv ${YY}${MM}??_rwis.gem /mesonet/ARCHIVE/gempak/surface/RWIS/${YYYY}_${MM}/
-
-echo "Lets go sao.gem"
+# echo "Lets go sao.gem"
 cd /mesonet/data/gempak/sao
 mkdir -p /mesonet/ARCHIVE/gempak/surface/sao/${YYYY}_${MM}
 mv ${YY}${MM}??_sao.gem /mesonet/ARCHIVE/gempak/surface/sao/${YYYY}_${MM}/
-
-echo "Lets go snet.gem"
-cd /mesonet/data/gempak/snet
-mkdir -p /mesonet/ARCHIVE/gempak/surface/snet/${YYYY}_${MM}
-mv ${YY}${MM}??_snet.gem /mesonet/ARCHIVE/gempak/surface/snet/${YYYY}_${MM}/
