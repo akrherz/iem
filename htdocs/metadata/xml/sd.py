cb31ed0febb6f22272e0a126fbab1e716ed39e0a
akrherz <akrherz@iastate.edu>
2016-08-03 12:46:12 -0500

    iterate on NMP metadata

--- htdocs/metadata/xml/sd.py
+++ htdocs/metadata/xml/sd.py
@@ -4,6 +4,8 @@ import cgi
 import sys
 from pyiem.network import Table as NetworkTable
 
+IEM = "https://mesonet.agron.iastate.edu/metadata/xml"
+
 
 def main():
     """ Do Something"""
@@ -26,7 +28,7 @@ gml:id="sd_%(network)s_%(station)s">
       <swe:value>%(sts)s 9999-12-31T23:59</swe:value>
     </swe:TimeRange>
   </sfl:validTime>
-  <sfl:deployer xlink:href="https://mesonet.agron.iastate.edu/metadata/xml/rp_IEM.xml"/>
+  <sfl:deployer xlink:href="%(iem)s/rp_IEM.xml"/>
   <sfl:deploymentLocation>
     <gml:Point srsName="urn:ogc:def:derivedCRSType:OGC:1.0:engineering">
       <gml:pos dimension="3" uomLabels="m m m"> 0 0 13.1</gml:pos>
@@ -37,13 +39,81 @@ gml:id="sd_%(network)s_%(station)s">
 
   <sfl:deploys>
 
+    <!-- Wind -->
     <sfl:Sensor gml:id="CS_03002">
       <sfl:serialNumber></sfl:serialNumber>
-      <sfl:characteristics xlink:href="https://mesonet.agron.iastate.edu/metadata/xml/sc_CS_03002.xml"/>
+      <sfl:characteristics xlink:href="%(iem)s/sc_CS_03002.xml"/>
       <sfl:senses>
         <sfl:Sensing gml:id="CS_03002_1">
-          <sfl:sensingProcedure xlink:href="https://mesonet.agron.iastate.edu/metadata/xml/sp_CS_03002.xml" />
+    <sfl:sensingProcedure xlink:href="%(iem)s/sp_CS_03002_WindSpeed.xml" />
           <sfl:unitOfMeasure uom="m/s"/>
+          <sfl:active>true</sfl:active>
+        </sfl:Sensing>
+        <sfl:Sensing gml:id="CS_03002_2">
+    <sfl:sensingProcedure xlink:href="%(iem)s/sp_CS_03002_WindDirection.xml" />
+          <sfl:unitOfMeasure uom="deg"/>
+          <sfl:active>true</sfl:active>
+        </sfl:Sensing>
+      </sfl:senses>
+    </sfl:Sensor>
+
+    <!-- Temp + RH -->
+    <sfl:Sensor gml:id="CS_CS215">
+      <sfl:serialNumber></sfl:serialNumber>
+      <sfl:characteristics xlink:href="%(iem)s/sc_CS_CS215.xml"/>
+      <sfl:senses>
+        <sfl:Sensing gml:id="CS_CS215_1">
+          <sfl:sensingProcedure xlink:href="%(iem)s/sp_CS_CS215_Temp.xml" />
+          <sfl:unitOfMeasure uom="C"/>
+          <sfl:active>true</sfl:active>
+        </sfl:Sensing>
+        <sfl:Sensing gml:id="CS_CS215_2">
+          <sfl:sensingProcedure xlink:href="%(iem)s/sp_CS_CS215_RH.xml" />
+          <sfl:unitOfMeasure uom="%%"/>
+          <sfl:active>true</sfl:active>
+        </sfl:Sensing>
+      </sfl:senses>
+    </sfl:Sensor>
+
+    <!-- Solar Rad-->
+    <sfl:Sensor gml:id="CS_CS300">
+      <sfl:serialNumber></sfl:serialNumber>
+      <sfl:characteristics xlink:href="%(iem)s/sc_CS_CS300.xml"/>
+      <sfl:senses>
+        <sfl:Sensing gml:id="CS_CS300_1">
+          <sfl:sensingProcedure xlink:href="%(iem)s/sp_CS_CS300.xml" />
+          <sfl:unitOfMeasure uom="J m-2 s-1"/>
+          <sfl:active>true</sfl:active>
+        </sfl:Sensing>
+      </sfl:senses>
+    </sfl:Sensor>
+
+    <!-- Soil Moisture and Temp -->
+    <sfl:Sensor gml:id="CS_CS655">
+      <sfl:serialNumber></sfl:serialNumber>
+      <sfl:characteristics xlink:href="%(iem)s/sc_CS_CS655.xml"/>
+      <sfl:senses>
+        <sfl:Sensing gml:id="CS_CS655_1">
+    <sfl:sensingProcedure xlink:href="%(iem)s/sp_CS_CS655_Temp.xml" />
+          <sfl:unitOfMeasure uom="K"/>
+          <sfl:active>true</sfl:active>
+        </sfl:Sensing>
+        <sfl:Sensing gml:id="CS_CS655_2">
+    <sfl:sensingProcedure xlink:href="%(iem)s/sp_CS_CS655_Moisture.xml" />
+          <sfl:unitOfMeasure uom="kg kg-1"/>
+          <sfl:active>true</sfl:active>
+        </sfl:Sensing>
+      </sfl:senses>
+    </sfl:Sensor>
+
+    <!-- Precip -->
+    <sfl:Sensor gml:id="CS_TE525">
+      <sfl:serialNumber></sfl:serialNumber>
+      <sfl:characteristics xlink:href="%(iem)s/sc_CS_TE525.xml"/>
+      <sfl:senses>
+        <sfl:Sensing gml:id="CS_TE525_1">
+          <sfl:sensingProcedure xlink:href="%(iem)s/sp_CS_TE525.xml" />
+          <sfl:unitOfMeasure uom="mm"/>
           <sfl:active>true</sfl:active>
         </sfl:Sensing>
       </sfl:senses>
@@ -51,12 +121,13 @@ gml:id="sd_%(network)s_%(station)s">
 
   </sfl:deploys>
 
-  <sfl:boundTo xlink:href="https://mesonet.agron.iastate.edu/metadata/xml/pl_%(network)s_%(station)s.xml" />
+  <sfl:boundTo xlink:href="%(iem)s/pl_%(network)s_%(station)s.xml" />
 
 </sfl:SensorDeployment>
 """ % dict(network=network, station=station,
            name=nt.sts[station]['name'],
            lat=nt.sts[station]['lat'],
+           iem=IEM,
            lon=nt.sts[station]['lon'],
            sts=nt.sts[station]['archive_begin'].strftime("%Y-%m-%dT%H:%M:%SZ"))
 
