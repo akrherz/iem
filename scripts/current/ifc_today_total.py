509c87d122506aa9e871303d614c41c40ddbf396
akrherz <akrherz@iastate.edu>
2016-08-02 20:49:14 -0500

    merge IFC Precip into local netcdf file

--- scripts/current/ifc_today_total.py
+++ scripts/current/ifc_today_total.py
@@ -4,11 +4,10 @@
 
 import datetime
 import numpy as np
-import os
 import sys
-import pytz
-import osgeo.gdal as gdal
 from pyiem.datatypes import distance
+import netCDF4
+from pyiem.iemre import daily_offset
 import matplotlib
 matplotlib.use('agg')
 from pyiem.plot import MapPlot, nwsprecip  # NOPEP8
@@ -18,42 +17,18 @@ def doday(ts, realtime):
     """
     Create a plot of precipitation stage4 estimates for some day
     """
-    # Start at midnight
-    now = ts.replace(hour=0, minute=0)
-    ets = now + datetime.timedelta(hours=24)
-    interval = datetime.timedelta(minutes=5)
-    currenttime = datetime.datetime.utcnow()
-    currenttime = currenttime.replace(tzinfo=pytz.timezone("UTC"))
-
-    total = None
-    lastts = None
-    while now <= ets:
-        gmt = now.astimezone(pytz.timezone("UTC"))
-        if gmt > currenttime:
-            break
-        fn = gmt.strftime(("/mesonet/ARCHIVE/data/%Y/%m/%d/"
-                           "GIS/ifc/p05m_%Y%m%d%H%M.png"))
-        if not os.path.isfile(fn):
-            now += interval
-            continue
-        png = gdal.Open(fn, 0)
-        data = np.flipud(png.ReadAsArray())  # units are mm per 5 minutes
-        data = np.where(data > 254, 0, data) / 10.0
-        if total is None:
-            total = data
-        else:
-            total += data
-
-        lastts = now
-
-        now += interval
-    if lastts is None:
-        print(('No IFC Data found for date: %s'
-               ) % (now.strftime("%d %B %Y"),))
-        return
-    lastts = lastts - datetime.timedelta(minutes=1)
+    nc = netCDF4.Dataset("/mesonet/data/iemre/%s_ifc_daily.nc" % (ts.year,))
+    idx = daily_offset(ts)
+    xaxis = nc.variables['lon'][:]
+    yaxis = nc.variables['lat'][:]
+    total = nc.variables['p01d'][idx, :, :]
+    nc.close()
+    lastts = datetime.datetime(ts.year, ts.month, ts.day, 23, 59)
+    if realtime:
+        now = datetime.datetime.now() - datetime.timedelta(minutes=60)
+        lastts = now.replace(minute=59)
     subtitle = "Total between 12:00 AM and %s" % (
-                                            lastts.strftime("%I:%M %p %Z"),)
+                                                  lastts.strftime("%I:%M %p"),)
     routes = 'ac'
     if not realtime:
         routes = 'a'
@@ -61,15 +36,14 @@ def doday(ts, realtime):
     clevs = [0.01, 0.1, 0.25, 0.5, 0.75, 1, 1.5, 2, 2.5, 3, 3.5, 4, 5, 6, 8,
              10]
 
-    sector = 'iowa'
-    pqstr = ("plot %s %s00 %s_ifc_1d.png %s_ifc_1d.png png"
-             ) % (routes, ts.strftime("%Y%m%d%H"), sector, sector)
+    pqstr = ("plot %s %s00 iowa_ifc_1d.png iowa_ifc_1d.png png"
+             ) % (routes, ts.strftime("%Y%m%d%H"))
     m = MapPlot(title=("%s Iowa Flood Center Today's Precipitation"
                        ) % (ts.strftime("%-d %b %Y"),),
-                subtitle=subtitle, sector=sector)
+                subtitle=subtitle, sector='custom',
+                west=xaxis[0], east=xaxis[-1],
+                south=yaxis[0], north=yaxis[-1])
 
-    xaxis = -97.154167 + np.arange(1741) * 0.004167
-    yaxis = 40.133331 + np.arange(1057) * 0.004167
     (x, y) = np.meshgrid(xaxis, yaxis)
 
     m.pcolormesh(x, y, distance(total, 'MM').value("IN"), clevs,
@@ -79,21 +53,17 @@ def doday(ts, realtime):
     m.close()
 
 
-def main():
-    if len(sys.argv) == 4:
-        date = datetime.datetime(int(sys.argv[1]), int(sys.argv[2]),
-                                 int(sys.argv[3]), 12, 0)
+def main(argv):
+    if len(argv) == 4:
+        date = datetime.date(int(argv[1]), int(argv[2]),
+                             int(argv[3]))
         realtime = False
     else:
-        date = datetime.datetime.now()
-        date = date - datetime.timedelta(minutes=60)
-        date = date.replace(hour=12, minute=0, second=0, microsecond=0)
+        date = datetime.datetime.now() - datetime.timedelta(minutes=60)
+        date = date.date()
         realtime = True
-    # Stupid pytz timezone dance
-    date = date.replace(tzinfo=pytz.timezone("UTC"))
-    date = date.astimezone(pytz.timezone("America/Chicago"))
     doday(date, realtime)
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
