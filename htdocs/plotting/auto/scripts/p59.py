74d08ec57026455374fd40df565d47d831e96b48
akrherz <akrherz@iastate.edu>
2016-08-10 11:20:52 -0500

    improve wind speed climo autoplot
    https://mesonet.agron.iastate.edu/plotting/auto/?q=59

--- htdocs/plotting/auto/scripts/p59.py
+++ htdocs/plotting/auto/scripts/p59.py
@@ -1,24 +1,21 @@
 from pyiem.datatypes import speed
-import psycopg2.extras
+import psycopg2
 import numpy as np
 import datetime
 import calendar
-import math
 import pandas as pd
+from pandas.io.sql import read_sql
 from pyiem.network import Table as NetworkTable
+from pyiem.util import get_autoplot_context
 
 PDICT = {'mps': 'Meters per Second',
-         'kt': 'knots',
+         'kt': 'Knots',
          'kmh': 'Kilometers per Hour',
          'mph': 'Miles per Hour'}
 
 
-def smooth(x, window_len=11, window='hanning'):
-
-    s = np.r_[2*x[0]-x[window_len:1:-1], x, 2*x[-1]-x[-1:-window_len:-1]]
-    w = eval('np.'+window+'(window_len)')
-    y = np.convolve(w/w.sum(), s, mode='same')
-    return y[window_len-1:-window_len+1]
+def smooth(x):
+    return pd.rolling_mean(x, 7, min_periods=1)
 
 
 def uv(sped, drct):
@@ -32,9 +29,9 @@ def uv(sped, drct):
       u (float): u component in mps
       v (float): v component in mps
     """
-    dirr = drct * math.pi / 180.00
-    u = 0. - sped * math.sin(dirr)
-    v = 0. - sped * math.cos(dirr)
+    dirr = drct * np.pi / 180.00
+    u = 0. - sped * np.sin(dirr)
+    v = 0. - sped * np.cos(dirr)
     return u, v
 
 
@@ -43,8 +40,11 @@ def get_description():
     d = dict()
     d['data'] = True
     d['cache'] = 86400
-    d['description'] = """This plot may take a while to appear!  It contains
-    the approximate daily climatology of component wind speed."""
+    d['description'] = """This plot presents a climatology of wind
+    observations.  The top panel presents the u (east/west) and v (north/south)
+    components.  The bottom panel is the simple average of the wind speed
+    magnitude.  The plotted information contains a seven day smoother.  If you
+    download the raw data, it will not contain this smoothing."""
     d['arguments'] = [
         dict(type='zstation', name='station', default='DSM',
              network='IA_ASOS', label='Select Station:'),
@@ -60,56 +60,64 @@ def plotter(fdict):
     import matplotlib
     matplotlib.use('agg')
     import matplotlib.pyplot as plt
-    ASOS = psycopg2.connect(database='asos', host='iemdb', user='nobody')
-    cursor = ASOS.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    asos = psycopg2.connect(database='asos', host='iemdb', user='nobody')
 
-    station = fdict.get('station', 'AMW')
-    units = fdict.get('units', 'mph')
-    network = fdict.get('network', 'IA_ASOS')
+    ctx = get_autoplot_context(fdict, get_description())
+    station = ctx['station']
+    units = ctx['units']
+    network = ctx['network']
 
     nt = NetworkTable(network)
 
-    cursor.execute("""
-    SELECT extract(doy from valid), sknt * 0.514, drct from alldata
-    where station = %s and sknt >= 0 and drct >= 0
-    """, (station, ))
+    df = read_sql("""
+    SELECT extract(doy from valid) as doy, sknt, drct from alldata
+    where station = %s and sknt >= 0 and drct >= 0 and report_type = 2
+    """, asos, params=(station, ), index_col=None)
 
-    uwnd = np.zeros((366,), 'f')
-    vwnd = np.zeros((366,), 'f')
-    cnt = np.zeros((366,), 'f')
-    for row in cursor:
-        u, v = uv(row[1], row[2])
-        uwnd[int(row[0]) - 1] += u
-        vwnd[int(row[0]) - 1] += v
-        cnt[int(row[0]) - 1] += 1
+    # Compute components in MPS
+    (df['u'], df['v']) = uv(speed(df['sknt'].values, 'KT').value('MPS'),
+                            df['drct'].values)
+    gdf = df.groupby(by='doy').mean()
+    u = speed(gdf['u'].values, 'MPS').value(units.upper())
+    v = speed(gdf['v'].values, 'mps').value(units.upper())
+    mag = speed(gdf['sknt'].values, 'KT').value(units.upper())
 
-    u = speed(uwnd / cnt, 'MPS').value(units.upper())
-    v = speed(vwnd / cnt, 'mps').value(units.upper())
+    df2 = pd.DataFrame(dict(u=pd.Series(u),
+                            v=pd.Series(v),
+                            mag=pd.Series(mag),
+                            day_of_year=pd.Series(np.arange(1, 367))))
 
-    df = pd.DataFrame(dict(u=pd.Series(u),
-                           v=pd.Series(v),
-                           day_of_year=pd.Series(np.arange(1, 366))))
-
-    (fig, ax) = plt.subplots(1, 1)
-
-    ax.plot(np.arange(1, 366), smooth(u[:-1], 14, 'hamming'), color='r',
-            label='u, West(+) : East(-) component')
-    ax.plot(np.arange(1, 366), smooth(v[:-1], 14, 'hamming'), color='b',
-            label='v, South(+) : North(-) component')
+    (fig, axes) = plt.subplots(2, 1, figsize=(8, 9))
+    ax = axes[0]
+    ax.plot(np.arange(1, 366), smooth(u[:-1]), color='r',
+            label='u, West(+) : East(-) component', lw=2)
+    ax.plot(np.arange(1, 366), smooth(v[:-1]), color='b',
+            label='v, South(+) : North(-) component', lw=2)
     ax.set_xticks([1, 32, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335, 365])
     ax.set_xticklabels(calendar.month_abbr[1:])
-    ax.legend(ncol=2, fontsize=11, loc=(0., -0.15))
+    ax.legend(ncol=2, fontsize=11, loc=(0., -0.17))
     ax.grid(True)
     ax.set_xlim(0, 366)
     ax.set_title(("[%s] %s Daily Average Component Wind Speed\n"
-                  "[%s-%s] 14 day smooth filter applied, %.0f obs found"
+                  "[%s-%s] 7 day smooth filter applied, %.0f obs found"
                   "") % (station, nt.sts[station]['name'],
                          nt.sts[station]['archive_begin'].year,
-                         datetime.datetime.now().year, np.sum(cnt)))
+                         datetime.datetime.now().year, len(df.index)))
     ax.set_ylabel("Average Wind Speed %s" % (PDICT.get(units), ))
 
     box = ax.get_position()
     ax.set_position([box.x0, box.y0 + box.height * 0.1, box.width,
                      box.height * 0.9])
 
-    return fig, df
+    ax = axes[1]
+    ax.plot(np.arange(1, 366), smooth(mag[:-1]), color='g', lw=2)
+    ax.set_xticks([1, 32, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335, 365])
+    ax.set_xticklabels(calendar.month_abbr[1:])
+    ax.set_ylabel("Average Speed %s" % (PDICT[units],))
+    ax.grid(True)
+    ax.set_xlim(0, 366)
+
+    return fig, df2
+
+if __name__ == '__main__':
+    plotter(dict(station='AMW', network='IA_ASOS'))
