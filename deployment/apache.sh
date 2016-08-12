fc253fc8fe09863fdc9d1dd78f5e07de5df04590
akrherz <akrherz@iastate.edu>
2016-08-12 14:46:02 -0500

    [travis] rearrange the deck chairs on apache config

--- deployment/apache.sh
+++ deployment/apache.sh
@@ -4,7 +4,7 @@ SVNROOT="/mesonet/www/apps/iemwebsite"
 
 cp -f $SVNROOT/config/geoserver.conf /etc/httpd/conf.d/
 cp -f $SVNROOT/config/backend-vhost.conf /etc/httpd/conf.d/backend.conf
-cp -f $SVNROOT/config/apache-vhost.conf /etc/httpd/conf.d/mesonet.inc
+cp -f $SVNROOT/config/mesonet.inc /etc/httpd/conf.d/mesonet.inc
 cp -f $SVNROOT/config/00iem.conf /etc/httpd/conf.d/
 cp -f /mesonet/www/apps/weppwebsite/etc/apache_vhost.conf /etc/httpd/conf.d/wepp.conf
 cp -r $SVNROOT/config/00iem-ssl.conf /etc/httpd/conf.d/
