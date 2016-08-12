ca6ae2b2c74c1b5c8f8ef70dcdb6065b987aa74c
akrherz <akrherz@iastate.edu>
2016-08-12 13:13:45 -0500

    start using travis for CI

---
+++ database/bootstrap.sh
@@ -0,0 +1,16 @@
+# setup databases
+
+psql -c 'create user nobody;' -U postgres
+psql -c 'create user apache;' -U postgres
+psql -c 'create user mesonet;' -U postgres
+
+for db in afos mesosite postgis snet \
+asos  hads      mos        rwis     squaw \
+awos iem       other      scan     wepp \
+coop isuag      portfolio  smos
+do
+  psql -c "create database $db;" -U postgres
+  psql -f init/${db}.sql -U postgres $db
+done
+
+
