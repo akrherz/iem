f7a6a775499ab38db19f2b974cacac54dd794b3e
akrherz <akrherz@iastate.edu>
2016-08-15 22:08:06 -0500

    add soil links to ISUSM County Soil page

--- htdocs/agclimate/soilt.php
+++ htdocs/agclimate/soilt.php
@@ -6,12 +6,18 @@ $t->thispage = "networks-agclimate";
 $nounce = time();
 $t->title = "ISU Soil Moisture County Temperature Estimates";
 $t->content = <<<EOF
-<h3>Recent 4 inch Soil Temperatures</h3>
+<ol class="breadcrumb">
+ <li><a href="/agclimate/">ISU Soil Moisture Network</a></li>
+ <li class="active">Recent 4 inch Soil Temperatures</li>
+</ol>
 
 <p>The following images are an interpolated analysis of county-by-county
-average soil temperature at a four inch depth. You can find an archive
-of these maps <a href="/timemachine/#57.0">here</a>.</p>
+average soil temperature at a four inch depth.</p>
 
+<p><strong>Links:</strong> <a class="btn btn-default" href="/timemachine/#57.0">Archive of This Map</a>
+<a class="btn btn-default" href="/agclimate/hist/daily.php">Observation Download</a></p>
+
+<h3>Past Three Days</h3>
 <p><img src="/data/soilt_day1.png?{$nounce}" class="img img-responsive"></p>
 <p><img src="/data/soilt_day2.png?{$nounce}" class="img img-responsive"></p>
 <p><img src="/data/soilt_day3.png?{$nounce}" class="img img-responsive"></p>
