dc4ee7660bfcb37b05503ad9a190be872357bdc0
akrherz <akrherz@iastate.edu>
2016-08-31 08:29:51 -0500

    some polish to sites pages
    add a helper when a bad station/network combo is discovered

--- htdocs/sites/locate.php
+++ htdocs/sites/locate.php
@@ -24,11 +24,18 @@ $t->thispage = "iem-sites";
 $t->headextra = <<<EOF
 <link rel="stylesheet" href="/vendor/openlayers/{$OL}/ol.css" type="text/css">
 <link type="text/css" href="/vendor/openlayers/{$OL}/ol3-layerswitcher.css" rel="stylesheet" />
+<link rel="stylesheet" type="text/css" href="/vendor/select2/4.0.3/select2.min.css"/ >
 EOF;
 $t->jsextra = <<<EOF
 <script src="/vendor/openlayers/{$OL}/ol.js" type="text/javascript"></script>
 <script src='/vendor/openlayers/{$OL}/ol3-layerswitcher.js'></script>
 <script src="/js/olselect.php?network=${network}"></script>
+<script src="/vendor/select2/4.0.3/select2.min.js"></script>
+<script type="text/javascript">
+$(document).ready(function(){
+	$(".iemselect2").select2();	
+});
+</script>
 EOF;
 
 $nselect = selectNetwork($network);
--- htdocs/sites/setup.php
+++ htdocs/sites/setup.php
@@ -1,28 +1,57 @@
 <?php
 /*
  * I am sourced by all pages on the "sites" website except the 'locate' 
- * frontend.  If station or network are not set, I throw a fit and force the
- * user to the locate page.
+ * frontend.
  */
  include_once("../../config/settings.inc.php");
  include_once("../../include/database.inc.php");
  include_once("../../include/station.php");
  /* Make sure all is well! */
- $station = isset($_GET["station"]) ? substr($_GET["station"],0,12) : "";
+ $station = isset($_GET["station"]) ? substr($_GET["station"], 0, 20) : "";
  $network = isset($_GET["network"]) ? $_GET["network"] : "";
-
+ 
+if ($station == ""){
+ 	header("Location: locate.php");
+ 	die();
+ }
+ 
 $st = new StationData($station, $network);
 $cities = $st->table;
 
- if (strlen($station) == 0)
- {
-    header("Location: locate.php");
-    die();
- }
  if (! array_key_exists($station, $cities))
  {
-    header("Location: locate.php");
-    die();
+ 	// Attempt to help the user find this station
+ 	include_once "../../include/myview.php";
+	$iemdb = iemdb("mesosite");
+	$rs = pg_prepare($iemdb, "FIND", "SELECT id, name, network from stations ".
+			"WHERE id = $1");
+	$rs = pg_execute($iemdb, 'FIND', Array($station));
+	if (pg_num_rows($rs) == 0){
+		header("Location: locate.php");
+		die();
+	}
+	$table = <<<EOF
+<p>Sorry, the requested station identifier and network could not be found
+	within the IEM database.  Here are other network matches for your 
+	identifier provided.</p>
+
+<table class="table table-ruled table-bordered">
+<thead><tr><th>ID</th><th>Name</th><th>Network</th></tr></thead>
+<tbody>
+EOF;
+	for($i=0; $row=@pg_fetch_assoc($rs, $i); $i++){
+		$table .= sprintf("<tr><td>%s</td><td><a href=\"site.php?network=%s".
+				"&amp;station=%s\">%s</a></td><td>%s</td></tr>", $row["id"],
+				$row["network"], $row["id"], $row["name"], $row["network"]);
+	}
+	$table .= "</tbody></table>";
+	$t = new MyView();
+	$t->thispage = "iem-sites";
+ 	$t->title = "Sites";
+ 	$t->content = $table;
+ 	
+ 	$t->render("single.phtml");
+ 	die();
  }
 
  if (! isset($_GET["network"]) )
