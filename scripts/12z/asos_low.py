65bdac3ff065a77775ef9147a755bbd7db846889
akrherz <akrherz@iastate.edu>
2016-09-11 21:39:06 -0500

    more values on 12z ASOS low plot

--- scripts/12z/asos_low.py
+++ scripts/12z/asos_low.py
@@ -1,17 +1,17 @@
 """Generate a map of the 12z UTC low temperature """
 
 import datetime
-now = datetime.datetime.now()
 from pyiem.plot import MapPlot
 import psycopg2
 IEM = psycopg2.connect(database='iem', host='iemdb', user='nobody')
 icursor = IEM.cursor()
+now = datetime.datetime.now()
 
 sql = """
 WITH lows as (
  SELECT c.iemid, min(tmpf) as low12z from current_log c JOIN stations s
  ON (s.iemid = c.iemid)
- WHERE tmpf > -90 and valid > '%s 00:00:00+00' and valid < '%s 12:00:00+00' 
+ WHERE tmpf > -90 and valid > '%s 00:00:00+00' and valid < '%s 12:00:00+00'
  and s.network in ('IA_ASOS', 'AWOS') GROUP by c.iemid
 )
 
@@ -26,17 +26,18 @@ valmask = []
 labels = []
 icursor.execute(sql)
 for row in icursor:
-    lats.append( row[2] )
-    lons.append( row[1] )
+    lats.append(row[2])
+    lons.append(row[1])
     vals.append(float(row[3]))
     labels.append(row[0])
-    valmask.append( True )
+    valmask.append(True)
 
 m = MapPlot(title='Iowa ASOS/AWOS 12Z Morning Low Temperature',
             subtitle="%s" % (now.strftime("%d %b %Y"), ),
             axisbg='white')
 m.drawcounties()
-m.plot_values(lons, lats, vals, fmt='%.0f', labels=labels)
-pqstr = "plot ac %s summary/iowa_asos_12z_low.png iowa_asos_12z_low.png png" % (
-        now.strftime("%Y%m%d%H%M"), )
+m.plot_values(lons, lats, vals, fmt='%.0f', labels=labels, labelbuffer=5)
+pqstr = ("plot ac %s summary/iowa_asos_12z_low.png "
+         "iowa_asos_12z_low.png png"
+         ) % (now.strftime("%Y%m%d%H%M"), )
 m.postprocess(pqstr=pqstr)
