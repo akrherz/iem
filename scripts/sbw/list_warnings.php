1b058fe75b129663b8396beb1bead93436b00ceb
akrherz <akrherz@iastate.edu>
2016-09-22 13:46:11 -0500

    add lsrs to warnings offline dumper

--- scripts/sbw/list_warnings.php
+++ scripts/sbw/list_warnings.php
@@ -7,6 +7,9 @@ include_once "../../include/network.php";
 
 $nt = new NetworkTable("WFO");
 
+$wfp = fopen("warnings.csv", "w");
+$lfp = fopen("lsrs.csv", "w");
+fwrite($lfp, "WFO,UTC_VALID,UTC_HOUR,WARNED,LON,LAT\n");
 
 while (list($key,$val) = each($nt->table)){
  $cow = new Cow(pg_connect("dbname=postgis host=localhost port=5555 user=nobody"));
@@ -20,11 +23,22 @@ while (list($key,$val) = each($nt->table)){
  //Iterate over the warnings
  reset($cow->warnings);
  while( list($k, $warn) = each($cow->warnings)){
- 	echo sprintf("%s,%s,%s,%s,%.2f,%.2f,%.3f\n", $warn["wfo"],
+ 	fwrite($wfp, sprintf("%s,%s,%s,%s,%.2f,%.2f,%.3f\n", $warn["wfo"],
  			date("Y-m-d H:i", $warn["sts"]), date("H", $warn["sts"]),
  			$warn["verify"] ? "Y": "N", $warn["lon0"],
- 			$warn["lat0"], $warn["parea"]);
+ 			$warn["lat0"], $warn["parea"]));
+ }
+
+ reset($cow->lsrs);
+ while(list($k, $lsr) = each($cow->lsrs)){
+ 	fwrite($lfp, sprintf("%s,%s,%s,%s,%.2f,%.2f\n", $lsr["wfo"],
+ 			date("Y-m-d H:i", $lsr["ts"]),
+ 			date("H", $lsr["ts"]),
+ 			$lsr["warned"] ? "Y": "N", $lsr["lon0"],
+ 			$lsr["lat0"]));
  }
 }
 
+fclose($wfp);
+fclose($lfp);
 ?>
