f3fb9aa758c0bd88029ec146068a73bf9e6ec849
akrherz <akrherz@iastate.edu>
2016-10-03 13:04:25 -0500

    sundry

--- nagios/check_ldm_rtstats.py
+++ nagios/check_ldm_rtstats.py
@@ -1,17 +1,24 @@
 """ Parse the files generated from LDM rtstats
 
 
-        unotice("%s %s %s %12.0lf %12.0lf %10.2f %4.0f@%s %s",
+        snprintf(stats_data, sizeof(stats_data),
+                "%14.14s %14.14s %32.*s %7.10s %32.*s %12.0lf %12.0lf %.8g %10.2f %4.0f@%4.4s %20.20s\n",
                 s_time(buf, sizeof(buf), sb->recent.tv_sec),
+                s_time(buf_a, sizeof(buf_a), sb->recent_a.tv_sec),
+                (int)_POSIX_HOST_NAME_MAX,
+                myname,
                 s_feedtypet(sb->feedtype),
+                (int)HOSTNAMESIZE,
                 sb->origin,
                 sb->nprods,
                 sb->nbytes,
-                sb->latency_sum/(sb->nprods == 0 ? 1 : sb->nprods),
+                d_diff_timestamp(&sb->recent_a, &sb->recent),
+                sb->latency_sum/(sb->nprods == 0 ? 1: sb->nprods),
                 sb->max_latency,
                 s_time_abrv(sb->slowest_at),
-                s_time(buf_a, sizeof(buf_a), sb->recent_a.tv_sec)
+                PACKAGE_VERSION
         );
+
 
 20121029204759 20121029204759
 laptop.local IDS|DDPLUS
