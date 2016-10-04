9d76539ba320182ea570a4a2361d44b34a2bd2e8
akrherz <akrherz@iastate.edu>
2016-10-04 12:52:27 -0500

    don't error on no HML fx available
    https://mesonet.agron.iastate.edu/plotting/auto/?q=160

--- htdocs/plotting/auto/scripts100/p160.py
+++ htdocs/plotting/auto/scripts100/p160.py
@@ -67,14 +67,18 @@ def get_context(fdict):
     on (d.hml_forecast_id = f.id) ORDER by f.id ASC, d.valid ASC
     """, pgconn, params=(station, dt - datetime.timedelta(days=3),
                          dt + datetime.timedelta(days=1)), index_col=None)
-    ctx['primary'] = "%s[%s]" % (ctx['df'].iloc[0]['primaryname'],
-                                 ctx['df'].iloc[0]['primaryunits'])
-    ctx['secondary'] = "%s[%s]" % (ctx['df'].iloc[0]['secondaryname'],
-                                   ctx['df'].iloc[0]['secondaryunits'])
+    if len(ctx['df'].index) > 0:
+        ctx['primary'] = "%s[%s]" % (ctx['df'].iloc[0]['primaryname'],
+                                     ctx['df'].iloc[0]['primaryunits'])
+        ctx['secondary'] = "%s[%s]" % (ctx['df'].iloc[0]['secondaryname'],
+                                       ctx['df'].iloc[0]['secondaryunits'])
 
-    # get obs
-    mints = ctx['df']['valid'].min()
-    maxts = ctx['df']['valid'].max()
+        # get obs
+        mints = ctx['df']['valid'].min()
+        maxts = ctx['df']['valid'].max()
+    else:
+        mints = dt - datetime.timedelta(days=3)
+        maxts = dt + datetime.timedelta(days=3)
     df = read_sql("""
     SELECT valid, h.label, value
     from hml_observed_data_""" + str(dt.year) + """ d JOIN hml_observed_keys h
@@ -82,10 +86,14 @@ def get_context(fdict):
     WHERE station = %s and valid between %s and %s ORDER by valid
     """, pgconn, params=(station, mints, maxts), index_col=None)
     ctx['odf'] = df.pivot('valid', 'label', 'value')
-    ctx['df'] = pd.merge(ctx['df'], ctx['odf'], left_on='valid',
-                         right_index=True, how='left', sort=False)
+    if len(ctx['df'].index) > 0:
+        ctx['df'] = pd.merge(ctx['df'], ctx['odf'], left_on='valid',
+                             right_index=True, how='left', sort=False)
     ctx['title'] = "[%s] %s" % (ctx['station'], ctx['name'])
     ctx['subtitle'] = ctx['dt'].strftime("%d %b %Y %H:%M UTC")
+    if len(ctx['df'].index) == 0:
+        ctx['primary'] = ctx['odf'].columns[0]
+        ctx['secondary'] = ctx['odf'].columns[1]
     return ctx
 
 
