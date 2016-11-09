7c06b92832852279ebbd43ac912abe385df73281
akrherz <akrherz@iastate.edu>
2016-11-08 21:26:53 -0600

    logic fixes to sky coverage autoplot
    https://mesonet.agron.iastate.edu/plotting/auto/?q=40

--- htdocs/plotting/auto/scripts/p40.py
+++ htdocs/plotting/auto/scripts/p40.py
@@ -1,13 +1,16 @@
-import psycopg2.extras
+import psycopg2
 import numpy as np
 import datetime
 import pytz
+from pandas.io.sql import read_sql
 from pyiem.network import Table as NetworkTable
+from pyiem.util import get_autoplot_context
 
 
 def get_description():
     """ Return a dict describing how to call this plotter """
     d = dict()
+    d['data'] = True
     d['cache'] = 86400
     d['description'] = """This chart is an attempted illustration of the amount
     of cloudiness that existed at a METAR site for a given month.  The chart
@@ -35,13 +38,12 @@ def plotter(fdict):
     matplotlib.use('agg')
     import matplotlib.pyplot as plt
     import matplotlib.cm as cm
-    ASOS = psycopg2.connect(database='asos', host='iemdb', user='nobody')
-    acursor = ASOS.cursor(cursor_factory=psycopg2.extras.DictCursor)
-
-    station = fdict.get('zstation', 'AMW')
-    network = fdict.get('network', 'IA_ASOS')
-    year = int(fdict.get('year', 2014))
-    month = int(fdict.get('month', 12))
+    pgconn = psycopg2.connect(database='asos', host='iemdb', user='nobody')
+    ctx = get_autoplot_context(fdict, get_description())
+    station = ctx['zstation']
+    network = ctx['network']
+    year = ctx['year']
+    month = ctx['month']
 
     nt = NetworkTable(network)
 
@@ -51,35 +53,34 @@ def plotter(fdict):
     sts = sts.replace(tzinfo=pytz.timezone("UTC"))
     ets = (sts + datetime.timedelta(days=35)).replace(day=1)
     days = (ets-sts).days
-    data = np.zeros((250, days * 24))
+    data = np.ones((250, days * 24)) * -1
 
-    acursor.execute("""
+    df = read_sql("""
     SELECT valid, skyc1, skyc2, skyc3, skyc4, skyl1, skyl2, skyl3, skyl4
     from alldata where station = %s and valid BETWEEN %s and %s
     ORDER by valid ASC
-    """, (station, sts, ets))
+    """, pgconn, params=(station, sts, ets), index_col=None)
 
     lookup = {'CLR': 0, 'FEW': 25, 'SCT': 50, 'BKN': 75, 'OVC': 100}
 
-    if acursor.rowcount == 0:
+    if len(df.index) == 0:
         return "No database entries found for station, sorry!"
 
-    for row in acursor:
-        delta = (row[0] - sts).days * 24.0 + (row[0] - sts).seconds / 3600.
+    for _, row in df.iterrows():
+        delta = (row['valid'] - sts).total_seconds() / 3600 - 1
+        data[:, delta] = 0
         for i in range(1, 5):
-            a = lookup.get(row[i], 0)
-            if a > 0:
-                l = row[i+4]
+            a = lookup.get(row['skyc%s' % (i,)], -1)
+            if a >= 0:
+                l = row['skyl%s' % (i,)]
                 if l is not None and l > 0:
                     l = l / 100
                     if l >= 250:
                         continue
-                    data[l:l+4, int(delta)] = a
-                    data[l+3:, int(delta)] = min(a, 75)
+                    data[l:l+4, delta] = a
+                    data[l+3:, delta] = min(a, 75)
 
-    data[:, delta:] = 15
-
-    data = np.ma.array(data, mask=np.where(data == 0, True, False))
+    data = np.ma.array(data, mask=np.where(data < 0, True, False))
 
     (fig, ax) = plt.subplots(1, 1)
 
@@ -92,8 +93,11 @@ def plotter(fdict):
                   ) % (station, nt.sts[station]['name'],
                        sts.strftime("%b %Y")))
 
+    cmap = cm.get_cmap('gray_r')
+    cmap.set_bad('white')
+    cmap.set_under('skyblue')
     ax.imshow(np.flipud(data), aspect='auto', extent=[0, days*24, 0, 250],
-              cmap=cm.get_cmap('gray_r'))
+              cmap=cmap, vmin=1)
     ax.set_yticks(range(0, 260, 50))
     ax.set_yticklabels(range(0, 25, 5))
     ax.set_ylabel("Cloud Levels [1000s feet]")
@@ -116,4 +120,7 @@ def plotter(fdict):
               loc='upper center', fontsize=14,
               bbox_to_anchor=(0.5, -0.09), fancybox=True, shadow=True, ncol=4)
 
-    return fig
+    return fig, df
+
+if __name__ == '__main__':
+    plotter(dict(station='DSM', year=2016, month=9, network='IA_ASOS'))
