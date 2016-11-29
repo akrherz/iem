65ba05190f6a7377152a4b3f3f90bb6aa5fdaf60
akrherz <akrherz@iastate.edu>
2016-11-29 16:23:35 -0600

    generalize offline weekly dump script

--- scripts/coop/brendan_dunphy.py
+++ scripts/coop/brendan_dunphy.py
@@ -1,9 +1,10 @@
 import datetime
 import psycopg2
+import sys
 pgconn = psycopg2.connect(database='coop', host='iemdb', user='nobody')
 cursor = pgconn.cursor()
 
-sts = datetime.datetime(2016, 5, 2)
+sts = datetime.datetime(2016, 2, 1)
 ets = datetime.datetime(2016, 11, 18)
 interval = datetime.timedelta(days=7)
 
@@ -11,9 +12,8 @@ now = sts
 while now < ets:
     e = now + interval
     sql = """SELECT avg(high) as h, avg(low) as l, sum(precip) as p
-         from alldata_ia WHERe station IN
-        ('IA0200') and day >= '%s' and day < '%s'
-        """ % (now.strftime("%Y-%m-%d"), e.strftime("%Y-%m-%d"))
+         from alldata_ia WHERe station = '%s' and day >= '%s' and day < '%s'
+        """ % (sys.argv[1], now.strftime("%Y-%m-%d"), e.strftime("%Y-%m-%d"))
     cursor.execute(sql)
     row = cursor.fetchone()
     print "%s,%.1f,%.1f,%.2f" % (now.strftime("%Y-%m-%d"), row[0],
