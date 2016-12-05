fdee6863df257c3c3b60b6736b5c72b6ea96476c
akrherz <akrherz@iastate.edu>
2016-12-05 08:07:25 -0600

    fix plotting intervals

--- scripts/month/plot_avgt.py
+++ scripts/month/plot_avgt.py
@@ -8,20 +8,21 @@ now = datetime.datetime.now()
 IEM = psycopg2.connect(database='iem', host='iemdb', user='nobody')
 icursor = IEM.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
-# Compute normal from the climate database
-sql = """SELECT id, s.network, ST_x(s.geom) as lon, ST_y(s.geom) as lat,
-    avg( (max_tmpf + min_tmpf)/2.0 ) as avgt , count(*) as cnt
-    from summary_%s c JOIN stations s ON (s.iemid = c.iemid)
-    WHERE s.network in ('IA_ASOS', 'AWOS') and
-    extract(month from day) = extract(month from now())
-    and max_tmpf > -30 and min_tmpf < 90 GROUP by id, s.network, lon, lat
-""" % (now.year,)
+day1 = datetime.date.today().replace(day=1)
+day2 = (day1 + datetime.timedelta(days=35)).replace(day=1)
 
 lats = []
 lons = []
 vals = []
 valmask = []
-icursor.execute(sql)
+table = "summary_%s" % (day1.year,)
+icursor.execute("""SELECT id, s.network, ST_x(s.geom) as lon, ST_y(s.geom) as lat,
+    avg( (max_tmpf + min_tmpf)/2.0 ) as avgt , count(*) as cnt
+    from """ + table + """ c JOIN stations s ON (s.iemid = c.iemid)
+    WHERE s.network in ('IA_ASOS', 'AWOS') and
+    day >= %s and day < %s
+    and max_tmpf > -30 and min_tmpf < 90 GROUP by id, s.network, lon, lat
+""", (day1, day2))
 for row in icursor:
     if row['cnt'] != now.day:
         continue
@@ -37,8 +38,10 @@ m = MapPlot(axisbg='white',
             title="Iowa %s Average Temperature" % (now.strftime("%Y %B"),),
             subtitle=("Average of the High + Low ending: %s"
                       "") % (now.strftime("%d %B"), ))
-m.contourf(lons, lats, vals, np.linspace(int(min(vals)), int(max(vals)) + 3,
-                                         10, dtype='i'))
+minval = int(min(vals))
+maxval = max([int(max(vals)) + 3, minval + 11])
+clevs = np.linspace(minval, maxval, 10, dtype='i')
+m.contourf(lons, lats, vals, clevs)
 m.drawcounties()
 m.plot_values(lons, lats, vals, '%.1f')
 pqstr = "plot c 000000000000 summary/mon_mean_T.png bogus png"
