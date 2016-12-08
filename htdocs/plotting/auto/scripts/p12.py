87a645e6ef7750c587a32e36b1c031c0326d065d
akrherz <akrherz@iastate.edu>
2016-12-07 22:25:17 -0600

    more options to first/last date + count dates
    https://mesonet.agron.iastate.edu/plotting/auto/?q=12
    
    * Note that the API to this did change :(  Needed to clear up some vague
    terms.  I corrected the previous IEM Features that linked to this.

--- htdocs/plotting/auto/scripts/p12.py
+++ htdocs/plotting/auto/scripts/p12.py
@@ -7,10 +7,18 @@ from collections import OrderedDict
 from pyiem.util import get_autoplot_context
 
 PDICT = OrderedDict([
-    ('last', 'Last Date At or Above'),
-    ('first', 'First Date At or Above'),
-    ('first_low', 'First Date Below (Low Temperature)'),
-    ('last_low', 'Last Date Below (Low Temperature)')
+    ('last_high_above', 'Last Date At or Above (High Temperature)'),
+    ('last_high_below', 'Last Date Below (High Temperature)'),
+    ('first_high_above', 'First Date At or Above (High Temperature)'),
+    ('first_high_below', 'First Date Below (High Temperature)'),
+    ('first_low_above', 'First Date At or Above (Low Temperature)'),
+    ('first_low_below', 'First Date Below (Low Temperature)'),
+    ('last_low_above', 'Last Date At or Above(Low Temperature)'),
+    ('last_low_below', 'Last Date Below (Low Temperature)')
+    ])
+PDICT2 = OrderedDict([
+    ('calendar', 'Compute over Calendar Year'),
+    ('winter', 'Compute over Winter Season')
     ])
 
 
@@ -26,17 +34,38 @@ def get_description():
     represents the start year of the winter season.  Rewording, the year 2016
     would represent the period of 1 July 2016 to 30 Jun 2017.
     """
+    thisyear = datetime.date.today().year
     d['arguments'] = [
         dict(type='station', name='station', default='IA2203',
-             label='Select Station:'),
+             label='Select Station:', network='IACLIMATE'),
         dict(type='int', name='threshold', default='90',
              label='Enter Threshold:'),
-        dict(type='select', name='which', default='last',
+        dict(type='select', name='which', default='last_high_above',
              label='Date Option:', options=PDICT),
-        dict(type='year', name='year', default=datetime.date.today().year,
-             label='Year to Highlight in Chart:'),
+        dict(type='select', name='season', default='calendar',
+             label='How to split the year:', options=PDICT2),
+        dict(type='year', name='year', default=thisyear,
+             max=(thisyear + 1), label='Year to Highlight in Chart:'),
     ]
     return d
+
+
+def do_labels(ax, extrenum, varname, direction, threshold, station, network,
+              season):
+    nt = NetworkTable(network)
+    ax.set_title(("%s [%s] %s Date and Days\n"
+                  "%s %s$^\circ$F"
+                  ) % (nt.sts[station]['name'], station, extrenum.capitalize(),
+                       PDICT["%s_%s_%s" % (extrenum, varname, direction)],
+                       threshold))
+    ax.set_xlabel(("Date of %s Occurrence%s"
+                   ) % (extrenum.capitalize(),
+                        (", Year of December for Winter Season"
+                         if season == 'winter' else '')))
+    ax.set_ylabel(("Days with %s %s %s$^\circ$F"
+                   ) % (varname.capitalize(),
+                        'At or Above' if direction == 'above'
+                        else 'Below', threshold))
 
 
 def plotter(fdict):
@@ -47,15 +76,15 @@ def plotter(fdict):
     pgconn = psycopg2.connect(database='coop', host='iemdb', user='nobody')
     ctx = get_autoplot_context(fdict, get_description())
     station = ctx['station']
-    network = "%sCLIMATE" % (station[:2],)
-    nt = NetworkTable(network)
     threshold = ctx['threshold']
-    which = ctx['which']
+    network = ctx['network']
+    season = ctx['season']
+    (extrenum, varname, direction) = ctx['which'].split("_")
     year = ctx['year']
 
     table = "alldata_%s" % (station[:2],)
 
-    opp = ' low < ' if which.find("_low") > 0 else ' high >= '
+    op = "%s %s" % (varname, ">=" if direction == 'above' else '<')
     df = read_sql("""
         with data as (
             SELECT extract(year from day + '%s months'::interval) as season,
@@ -64,40 +93,33 @@ def plotter(fdict):
         agg1 as (
             SELECT season - %s as season,
             count(*) as obs,
-            min(case when """+opp+""" %s then day else null end) as nday,
-            max(case when """+opp+""" %s then day else null end) as xday,
-            sum(case when """+opp+""" %s then 1 else 0 end) as count
+            min(case when """+op+""" %s then day else null end) as nday,
+            max(case when """+op+""" %s then day else null end) as xday,
+            sum(case when """+op+""" %s then 1 else 0 end) as count
             from data GROUP by season)
     SELECT season::int, count, obs, nday, extract(doy from nday) as nday_doy,
     xday, extract(doy from xday) as xday_doy from agg1
     ORDER by season ASC
-    """, pgconn, params=(6 if which.find('_low') > 0 else 0,
-                         station,
-                         1 if which.find('_low') > 0 else 0,
+    """, pgconn, params=(6 if season == 'winter' else 0,
+                         station, 1 if season == 'winter' else 0,
                          threshold, threshold, threshold),
                   index_col='season')
     # We need to do some magic to julian dates straight
-    if which.find('_low') > 0:
+    if season == 'winter':
         # drop the first row
         df.drop(df.index.values[0], inplace=True)
         df.loc[df['nday_doy'] < 183, 'nday_doy'] += 365.
         df.loc[df['xday_doy'] < 183, 'xday_doy'] += 365.
     # Set NaN where we did not meet conditions
     zeros = df[df['count'] == 0].index.values
-    col = 'xday_doy' if which.find('last') == 0 else 'nday_doy'
+    col = 'xday_doy' if extrenum == 'last' else 'nday_doy'
     df2 = df[df['count'] > 0]
 
     (fig, ax) = plt.subplots(1, 1)
     ax.scatter(df2[col], df2['count'])
     ax.grid(True)
-    ax.set_ylabel("Days %s Temp of %s$^\circ$F" % (
-        ('Below Low' if which.find('_low') > 0 else
-         'At or Above High'), threshold))
-    ax.set_title(("%s [%s] Days per Year %s %s $^\circ$F\nversus "
-                  "%s Date of that Temperature") % (
-                nt.sts[station]['name'], station,
-                ('below' if which.find('_low') > 0 else 'at or above'),
-                threshold, "Latest" if which == 'last' else 'First'))
+    do_labels(ax, extrenum, varname, direction, threshold, station, network,
+              season)
 
     xticks = []
     xticklabels = []
@@ -108,8 +130,6 @@ def plotter(fdict):
             xticklabels.append(ts.strftime("%-d %b"))
     ax.set_xticks(xticks)
     ax.set_xticklabels(xticklabels)
-    ax.set_xlabel(("Date of Last Occurrence" if which == 'last'
-                   else 'Date of First Occurence'))
 
     ax2 = ax.twinx()
     sortvals = np.sort(df2[col].values)
@@ -150,4 +170,5 @@ def plotter(fdict):
 
 
 if __name__ == '__main__':
-    plotter(dict(which='first_low', threshold=32, year=2016))
+    plotter(dict(which='first_high_below', threshold=32, year=2016,
+                 season='winter'))
