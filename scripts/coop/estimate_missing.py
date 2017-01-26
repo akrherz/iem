ff375c86699b96acaa2624f8d20e965f0d7fdaa8
akrherz <akrherz@iastate.edu>
2017-01-25 21:40:45 -0600

    climodat estimator uses IEMRE now
    better documentation of this is coming!

--- scripts/coop/estimate_missing.py
+++ scripts/coop/estimate_missing.py
@@ -3,110 +3,61 @@
 This is only run for the exceptions, when data is marked as missing for some
 reason.  The main data estimator is found at `../climodat/daily_estimator.py`.
 
-This script looks for the 11 nearest neighbors and if that fails, it goes
-looking for IEMRE gridded data.
+This script utilizes the IEMRE web service to provide data.
 """
-import os
 import sys
-import numpy as np
 from pyiem.network import Table as NetworkTable
-import psycopg2.extras
-import netCDF4
-import datetime
-from pyiem import iemre
-from pyiem.datatypes import temperature, distance
+import requests
+import psycopg2
 
 # Database Connection
 COOP = psycopg2.connect(database='coop', host='iemdb')
-ccursor = COOP.cursor(cursor_factory=psycopg2.extras.DictCursor)
+ccursor = COOP.cursor()
 ccursor2 = COOP.cursor()
 
 state = sys.argv[1]
 nt = NetworkTable("%sCLIMATE" % (state.upper(),))
 
-vnameconv = {'high': 'high_tmpk', 'low': 'low_tmpk', 'precip': 'p01d'}
-
-# We'll care about our nearest 11 stations, arbitrary
-friends = {}
-weights = {}
-for station in nt.sts.keys():
-    sql = """
-        select id, ST_distance(geom, 'SRID=4326;POINT(%s %s)') from stations
-         WHERE network ~* 'CLIMATE' and id != '%s'
-         and archive_begin < '1951-01-01' and
-         substr(id, 3, 1) != 'C' and substr(id, 3,4) != '0000'
-         ORDER by st_distance
-         ASC LIMIT 11
-         """ % (nt.sts[station]['lon'], nt.sts[station]['lat'], station)
-    ccursor.execute(sql)
-    friends[station] = []
-    weights[station] = []
-    for row in ccursor:
-        friends[station].append(row[0])
-        weights[station].append(1.0 / row[1])
-    weights[station] = np.array(weights[station])
+URI = ("http://iem.local/iemre/daily/"
+       "%(date)s/%(lat)s/%(lon)s/json")
 
 
 def do_var(varname):
     """Run our estimator for a given variable"""
-    currentnc = None
     sql = """
         select day, station from alldata_%s WHERE %s is null
         and day >= '1893-01-01' ORDER by day ASC
         """ % (state.lower(), varname)
     ccursor.execute(sql)
+    dataformat = '%.0f' if varname in ['high', 'low'] else '%.2f'
+
     for row in ccursor:
         day = row[0]
         station = row[1]
         if station not in nt.sts:
             continue
+        temp24hour = nt.sts[station]['temp24_hour']
+        prefix = "12z_" if temp24hour != 0 else 'daily_'
+        units = '_in' if varname == 'precip' else '_f'
 
-        sql = """
-            SELECT station, %s from alldata WHERE %s is not NULL
-            and station in %s and day = '%s'
-            """ % (varname, varname, tuple(friends[station]), day)
-        ccursor2.execute(sql)
-        weight = []
-        value = []
-        for row2 in ccursor2:
-            idx = friends[station].index(row2[0])
-            weight.append(weights[station][idx])
-            value.append(row2[1])
+        wsuri = URI % {'date': day.strftime("%Y-%m-%d"),
+                       'lon': nt.sts[station]['lon'],
+                       'lat': nt.sts[station]['lat']}
+        req = requests.get(wsuri)
+        try:
+            estimated = req.json()['data'][0]
+        except:
+            print("\n%s Failure:%s\n%s" % (station, req.content, wsuri))
+            continue
+        newvalue = estimated["%s%s%s" % (prefix, varname, units)]
 
-        if len(weight) < 3:
-            # Nearest neighbors failed, so lets look at our grided analysis
-            # and sample from it
-            if currentnc is None or currentnc.title.find(str(day.year)) == -1:
-                ncfn = "/mesonet/data/iemre/%s_mw_daily.nc" % (day.year,)
-                if not os.path.isfile(ncfn):
-                    print("FATAL: estimate_missing missing: %s" % (ncfn,))
-                    sys.exit()
-                currentnc = netCDF4.Dataset(ncfn)
-            tidx = iemre.daily_offset(datetime.datetime(day.year, day.month,
-                                                        day.day))
-            iidx, jidx = iemre.find_ij(nt.sts[station]['lon'],
-                                       nt.sts[station]['lat'])
-            iemreval = currentnc.variables[vnameconv[varname]][tidx, jidx,
-                                                               iidx]
-            if varname in ('high', 'low'):
-                interp = temperature(iemreval, 'K').value('F')
-            else:
-                interp = distance(iemreval, 'MM').value('IN')
-            print '--> Neighbor failure, %s %s %s' % (station, day, varname)
-        else:
-            mass = sum(weight)
-            interp = np.sum(np.array(weight) * np.array(value) / mass)
-
-        dataformat = '%.2f'
-        if varname in ['high', 'low']:
-            dataformat = '%.0f'
         print(('Set station: %s day: %s varname: %s value: %s'
-               ) % (station, day, varname, dataformat % (interp,)))
+               ) % (station, day, varname, dataformat % (newvalue,)))
         sql = """
             UPDATE alldata_%s SET estimated = true, %s = %s WHERE
             station = '%s' and day = '%s'
             """ % (state.lower(), varname,
-                   dataformat % (interp,), station, day)
+                   dataformat % (newvalue,), station, day)
         sql = sql.replace(' nan ', ' null ')
         ccursor2.execute(sql)
 
