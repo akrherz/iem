b5bb72cbad38f64d8e111715dc49c91a059c4467
akrherz <akrherz@iastate.edu>
2017-02-10 05:40:47 -0600

    add start / end year limit to snow plot
    https://mesonet.agron.iastate.edu/plotting/auto/?q=62

--- htdocs/plotting/auto/scripts/p62.py
+++ htdocs/plotting/auto/scripts/p62.py
@@ -2,6 +2,7 @@ import psycopg2.extras
 import numpy as np
 import datetime
 from pyiem.network import Table as NetworkTable
+from pyiem.util import get_autoplot_context
 
 
 def get_description():
@@ -11,9 +12,15 @@ def get_description():
     as a image.  Each box represents an individual day's report with the
     color denoting the amount.  Values in light gray are missing in the
     database."""
+    today = datetime.datetime.today()
+    lyear = today.year if today.month > 8 else (today.year - 1)
     d['arguments'] = [
         dict(type='station', name='station', default='IA2203',
-             label='Select Station:')
+             label='Select Station:', network='IACLIMATE'),
+        dict(type='year', name='syear', default=1893, min=1893,
+             label='Start Year (inclusive):'),
+        dict(type='year', name='eyear', default=lyear,
+             min=1893, label='End Year (inclusive):'),
     ]
     return d
 
@@ -26,8 +33,12 @@ def plotter(fdict):
     import matplotlib.colors as mpcolors
     pgconn = psycopg2.connect(database='coop', host='iemdb', user='nobody')
     cursor = pgconn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-
-    station = fdict.get('station', 'IA2203')
+    ctx = get_autoplot_context(fdict, get_description())
+    station = ctx['station']
+    syear = ctx['syear']
+    eyear = ctx['eyear']
+    sts = datetime.date(syear, 11, 1)
+    ets = datetime.date(eyear + 1, 6, 1)
 
     table = "alldata_%s" % (station[:2],)
     nt = NetworkTable("%sCLIMATE" % (station[:2],))
@@ -36,22 +47,25 @@ def plotter(fdict):
     obs = np.ma.ones((eyear - syear + 1, 153), 'f') * -1
 
     cursor.execute("""
-     SELECT year, extract(doy from day), snowd from """+table+"""
+     SELECT year, extract(doy from day), snowd, day from """+table+"""
      WHERE station = %s and
-     month in (11,12,1,2,3) and snowd >= 0 and day >= '1893-11-01'
-    """, (station,))
-    minyear = 2015
+     month in (11,12,1,2,3) and snowd >= 0 and day between %s and %s
+    """, (station, sts, ets))
+    minyear = 2050
+    maxyear = 1900
     for row in cursor:
         year = row[0]
         if year < minyear:
             minyear = year
+        if row[3].month > 6 and year > maxyear:
+            maxyear = year
         doy = row[1]
         val = row[2]
         if doy > 180:
             doy = doy - 365
         else:
             year -= 1
-        obs[year-syear, doy + 61] = val
+        obs[year-syear, int(doy + 61)] = val
 
     obs.mask = np.where(obs < 0, True, False)
     # obs[obs == 0] = -1
@@ -76,6 +90,9 @@ def plotter(fdict):
                     extent=[0, 152, eyear+1-0.5, syear-0.5])
     fig.colorbar(res)
     ax.grid(True)
-    ax.set_ylim(top=minyear)
+    ax.set_ylim(maxyear + 0.5, minyear - 0.5)
 
     return fig
+
+if __name__ == '__main__':
+    plotter(dict())
