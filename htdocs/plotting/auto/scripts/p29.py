8c8f9e80e45abcdf230fb7ce30bd2d97e8541a66
akrherz <akrherz@iastate.edu>
2017-02-21 12:44:25 -0600

    plot cleanups and label zeros

--- htdocs/plotting/auto/scripts/p29.py
+++ htdocs/plotting/auto/scripts/p29.py
@@ -5,6 +5,7 @@ import datetime
 import calendar
 import numpy as np
 from pandas.io.sql import read_sql
+from pyiem.util import get_autoplot_context
 
 
 def get_description():
@@ -19,12 +20,12 @@ def get_description():
     d['data'] = True
     d['arguments'] = [
         dict(type='zstation', name='zstation', default='AMW',
-             label='Select Station:'),
+             label='Select Station:', network='IA_ASOS'),
         dict(type='zhour', name='hour', default=20,
              label='At Time (UTC):'),
-        dict(type='text', name='t1', default=70,
+        dict(type='int', name='t1', default=70,
              label='Lower Temperature Bound (inclusive):'),
-        dict(type='text', name='t2', default=79,
+        dict(type='int', name='t2', default=79,
              label='Upper Temperature Bound (inclusive):'),
     ]
     return d
@@ -36,12 +37,12 @@ def plotter(fdict):
     matplotlib.use('agg')
     import matplotlib.pyplot as plt
     pgconn = psycopg2.connect(database='asos', host='iemdb', user='nobody')
-
-    station = fdict.get('zstation', 'AMW')
-    network = fdict.get('network', 'IA_ASOS')
-    hour = int(fdict.get('hour', 20))
-    t1 = int(fdict.get('t1', 70))
-    t2 = int(fdict.get('t2', 70))
+    ctx = get_autoplot_context(fdict, get_description())
+    station = ctx['zstation']
+    network = ctx['network']
+    hour = ctx['hour']
+    t1 = ctx['t1']
+    t2 = ctx['t2']
     nt = NetworkTable(network)
 
     df = read_sql("""
@@ -54,7 +55,7 @@ def plotter(fdict):
         GROUP by t
     )
     SELECT extract(month from t) as month,
-    sum(case when tmp >= %s and tmp <= %s then 1 else 0 end) as hits,
+    sum(case when tmp >= %s and tmp <= %s then 1 else 0 end)::int as hits,
     sum(case when tmp > %s then 1 else 0 end) as above,
     sum(case when tmp < %s then 1 else 0 end) as below,
     count(*) from obs GROUP by month ORDER by month ASC
@@ -62,16 +63,20 @@ def plotter(fdict):
     df['freq'] = df['hits'] / df['count'] * 100.
     df['above_freq'] = df['above'] / df['count'] * 100.
     df['below_freq'] = df['below'] / df['count'] * 100.
-    (fig, ax) = plt.subplots(1, 1)
-    bars = ax.bar(np.arange(1, 13) - 0.4, df['freq'], fc='tan',
-                  label='%s - %s' % (t1, t2), zorder=2)
-    ax.scatter(df.index.values, df['above_freq'], marker='s', s=40,
-               label="Above %s" % (t2,), color='r', zorder=3)
+    (fig, ax) = plt.subplots(1, 1, figsize=(8, 6))
     ax.scatter(df.index.values, df['below_freq'], marker='s', s=40,
                label="Below %s" % (t1,), color='b', zorder=3)
+    bars = ax.bar(np.arange(1, 13), df['freq'], fc='tan',
+                  label='%s - %s' % (t1, t2), zorder=2, align='center')
+    ax.scatter(df.index.values, df['above_freq'], marker='s', s=40,
+               label="Above %s" % (t2,), color='r', zorder=3)
     for i, bar in enumerate(bars):
-        ax.text(i+1, bar.get_height()+3, "%.1f%%" % (bar.get_height(),),
-                ha='center', fontsize=12, zorder=4)
+        value = df.loc[i+1, 'hits']
+        label = "%.1f%%" % (bar.get_height(),)
+        if value == 0:
+            label = 'None'
+        ax.text(i+1, bar.get_height()+3, label, ha='center', fontsize=12,
+                zorder=4)
     ax.set_xticks(range(0, 13))
     ax.set_xticklabels(calendar.month_abbr)
     ax.grid(True)
@@ -90,3 +95,6 @@ def plotter(fdict):
     pos = ax.get_position()
     ax.set_position([pos.x0, pos.y0 + 0.07, pos.width, pos.height * 0.93])
     return fig, df
+
+if __name__ == '__main__':
+    plotter(dict())
