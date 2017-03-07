36f9a92fa8ffad06529adad648a4a3b50e931eca
akrherz <akrherz@iastate.edu>
2017-03-07 09:06:29 -0600

    pep8 and allow for larger magnitudes in LSR dump

--- scripts/GIS/24h_lsr.py
+++ scripts/GIS/24h_lsr.py
@@ -13,23 +13,9 @@ import subprocess
 import psycopg2.extras
 POSTGIS = psycopg2.connect(database='postgis', host='iemdb', user='nobody')
 pcursor = POSTGIS.cursor(cursor_factory=psycopg2.extras.DictCursor)
-pcursor.execute("SET TIME ZONE 'GMT'")
+pcursor.execute("SET TIME ZONE 'UTC'")
 
 os.chdir("/tmp/")
-
-"""
- valid     | timestamp with time zone |
- type      | character(1)             |
- magnitude | real                     |
- city      | character varying(32)    |
- county    | character varying(32)    |
- state     | character(2)             |
- source    | character varying(32)    |
- remark    | text                     |
- wfo       | character(3)             |
- geom      | geometry                 |
- typetext  | geometry                 |
-"""
 
 # We set one minute into the future, so to get expiring warnings
 # out of the shapefile
@@ -39,7 +25,7 @@ shp = shapelib.create("lsr_24hour", shapelib.SHPT_POINT)
 
 dbf = dbflib.create("lsr_24hour")
 dbf.add_field("VALID", dbflib.FTString, 12, 0)
-dbf.add_field("MAG", dbflib.FTDouble, 8, 2)
+dbf.add_field("MAG", dbflib.FTDouble, 10, 2)
 dbf.add_field("WFO", dbflib.FTString, 3, 0)
 dbf.add_field("TYPECODE", dbflib.FTString, 1, 0)
 dbf.add_field("TYPETEXT", dbflib.FTString, 40, 0)
@@ -49,34 +35,33 @@ dbf.add_field("SOURCE", dbflib.FTString, 40, 0)
 dbf.add_field("REMARK", dbflib.FTString, 200, 0)
 
 
-#sql = "SELECT *, astext(geom) as tgeom from warnings WHERE issue < '%s' and \
-sql = """SELECT distinct *, ST_astext(geom) as tgeom from lsrs_%s WHERE 
-	valid > (now() -'1 day'::interval) """ % (eTS.year,)
-pcursor.execute(sql)
+pcursor.execute("""
+    SELECT distinct *, ST_astext(geom) as tgeom from lsrs_%s WHERE
+    valid > (now() -'1 day'::interval)
+    """ % (eTS.year,))
 
 cnt = 0
 for row in pcursor:
-	
-	s = row["tgeom"]
-	if s == None or s == "":
-		continue
-	f = wellknowntext.convert_well_known_text(s)
+    s = row["tgeom"]
+    if s is None or s == "":
+        continue
+    f = wellknowntext.convert_well_known_text(s)
 
-	d = {}
-	d["VALID"] = row['valid'].strftime("%Y%m%d%H%M")
-	d["MAG"] = float(row['magnitude'] or 0)
-	d["TYPECODE"] = row['type']
-	d["WFO"] = row['wfo']
-	d["TYPETEXT"] = row['typetext']
-	d["CITY"] = row['city']
-	d["COUNTY"] = row['county']
-	d["SOURCE"] = row['source']
-	d["REMARK"] = row['remark'][:200]
-	obj = shapelib.SHPObject(shapelib.SHPT_POINT, 1, [[f]] )
-	shp.write_object(-1, obj)
-	dbf.write_record(cnt, d)
-	del(obj)
-	cnt += 1
+    d = {}
+    d["VALID"] = row['valid'].strftime("%Y%m%d%H%M")
+    d["MAG"] = float(row['magnitude'] or 0)
+    d["TYPECODE"] = row['type']
+    d["WFO"] = row['wfo']
+    d["TYPETEXT"] = row['typetext']
+    d["CITY"] = row['city']
+    d["COUNTY"] = row['county']
+    d["SOURCE"] = row['source']
+    d["REMARK"] = row['remark'][:200]
+    obj = shapelib.SHPObject(shapelib.SHPT_POINT, 1, [[f]])
+    shp.write_object(-1, obj)
+    dbf.write_record(cnt, d)
+    del(obj)
+    cnt += 1
 
 
 del(shp)
@@ -89,8 +74,9 @@ shutil.copy('/opt/iem/data/gis/meta/4326.prj', 'lsr_24hour.prj')
 z.write("lsr_24hour.prj")
 z.close()
 
-cmd = "/home/ldm/bin/pqinsert -p \"zip c %s gis/shape/4326/us/lsr_24hour.zip bogus zip\" lsr_24hour.zip" % (eTS.strftime("%Y%m%d%H%M"),)
+cmd = ("/home/ldm/bin/pqinsert -p \"zip c %s gis/shape/4326/us/lsr_24hour.zip "
+       "bogus zip\" lsr_24hour.zip") % (eTS.strftime("%Y%m%d%H%M"),)
 subprocess.call(cmd, shell=True)
 
 for suffix in ['shp', 'shx', 'dbf', 'prj', 'zip']:
-	os.remove('lsr_24hour.%s' % (suffix,))
+    os.remove('lsr_24hour.%s' % (suffix,))
