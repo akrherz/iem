a73e062788ba0e95bdc127da452156cadfd8159d
akrherz <akrherz@iastate.edu>
2017-04-02 20:30:27 -0500

    pep8

--- scripts/season/plot_4month_stage4.py
+++ scripts/season/plot_4month_stage4.py
@@ -3,15 +3,13 @@
   1) Last 4 month's precipitation
   2) Normal for past 4 months
   3) Departure for this period
-  
+
   We care about 4 months as it is used in drought analysis
 '''
 
 import numpy as np
-import os
 import datetime
 import netCDF4
-import subprocess
 from pyiem.plot import MapPlot
 from pyiem import iemre
 from pyiem.network import Table as NetworkTable
@@ -27,56 +25,58 @@ lons = cnc.variables['lon'][:]
 lats = cnc.variables['lat'][:]
 index0 = iemre.daily_offset(sts)
 index1 = iemre.daily_offset(ets)
-if index1 < index0: # Uh oh, we are spanning a year
-    clprecip = np.sum(cnc.variables['p01d'][:index1,:,:],0)
-    clprecip = clprecip + np.sum(cnc.variables['p01d'][index0:,:,:],0)    
+if index1 < index0:  # Uh oh, we are spanning a year
+    clprecip = np.sum(cnc.variables['p01d'][:index1, :, :], 0)
+    clprecip = clprecip + np.sum(cnc.variables['p01d'][index0:, :, :], 0)
 else:
-    clprecip = np.sum(cnc.variables['p01d'][index0:index1,:,:],0)
+    clprecip = np.sum(cnc.variables['p01d'][index0:index1, :, :], 0)
 cnc.close()
 
 # Get the observed precip
-if sts.year != ets.year: #spanner, darn
+if sts.year != ets.year:  # spanner, darn
     onc = netCDF4.Dataset("/mesonet/data/iemre/%s_mw_daily.nc" % (sts.year,))
-    obprecip = np.sum(onc.variables['p01d'][index0:,:,:],0)
+    obprecip = np.sum(onc.variables['p01d'][index0:, :, :], 0)
     onc.close()
     onc = netCDF4.Dataset("/mesonet/data/iemre/%s_mw_daily.nc" % (ets.year,))
-    obprecip = obprecip + np.sum(onc.variables['p01d'][:index1,:,:],0)
+    obprecip = obprecip + np.sum(onc.variables['p01d'][:index1, :, :], 0)
     onc.close()
 else:
     ncfn = "/mesonet/data/iemre/%s_mw_daily.nc" % (sts.year,)
     onc = netCDF4.Dataset(ncfn, 'r')
-    obprecip = np.sum(onc.variables['p01d'][index0:index1,:,:],0)
+    obprecip = np.sum(onc.variables['p01d'][index0:index1, :, :], 0)
     onc.close()
 
 lons, lats = np.meshgrid(lons, lats)
 
 # Plot departure from normal
 m = MapPlot(sector='midwest',
-            title='Precipitation Departure %s - %s' % (sts.strftime("%b %d %Y"),
-                                                       ets.strftime("%b %d %Y")),
+            title=('Precipitation Departure %s - %s'
+                   ) % (sts.strftime("%b %d %Y"), ets.strftime("%b %d %Y")),
             subtitle='based on IEM Estimates'
             )
 
-m.pcolormesh(lons, lats, (obprecip - clprecip) / 25.4, np.arange(-10,10,1))
+m.pcolormesh(lons, lats, (obprecip - clprecip) / 25.4, np.arange(-10, 10, 1))
 m.postprocess(pqstr="plot c 000000000000 summary/4mon_diff.png bogus png")
+m.close()
 
 # Plot normals
 m = MapPlot(sector='midwest',
-            title='Normal Precipitation:: %s - %s' % (sts.strftime("%b %d %Y"),
-                                                       ets.strftime("%b %d %Y")),
+            title=('Normal Precipitation:: %s - %s'
+                   ) % (sts.strftime("%b %d %Y"), ets.strftime("%b %d %Y")),
             subtitle='based on IEM Estimates'
             )
 
-m.pcolormesh(lons, lats, (clprecip) / 25.4, np.arange(0,30,2))
+m.pcolormesh(lons, lats, (clprecip) / 25.4, np.arange(0, 30, 2))
 m.postprocess(pqstr="plot c 000000000000 summary/4mon_normals.png bogus png")
+m.close()
 
 # Plot Obs
 m = MapPlot(sector='midwest',
-            title='Estimated Precipitation:: %s - %s' % (sts.strftime("%b %d %Y"),
-                                                       ets.strftime("%b %d %Y")),
+            title=('Estimated Precipitation:: %s - %s'
+                   ) % (sts.strftime("%b %d %Y"), ets.strftime("%b %d %Y")),
             subtitle='based on IEM Estimates'
             )
 
-m.pcolormesh(lons, lats, (obprecip) / 25.4, np.arange(0,30,2))
+m.pcolormesh(lons, lats, (obprecip) / 25.4, np.arange(0, 30, 2))
 m.postprocess(pqstr="plot c 000000000000 summary/4mon_stage4obs.png bogus png")
- 
+m.close()
