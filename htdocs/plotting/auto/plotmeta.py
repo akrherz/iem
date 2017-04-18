339d5478b0a0940e2b8bf997932a661eed20a43d
akrherz <akrherz@iastate.edu>
2017-04-18 10:42:10 -0500

    add moving progress bar to autoplot frontend
    Timing is based on an average of autoplot timings over the past 7 days.

--- htdocs/plotting/auto/plotmeta.py
+++ htdocs/plotting/auto/plotmeta.py
@@ -1,32 +1,53 @@
 #!/usr/bin/env python
+"""Returns the JSON metadata for a particular autoplot app, or all list all"""
 
 import cgi
 import sys
 import os
 import imp
 import json
+import psycopg2
+
+
+def get_timing(pidx):
+    """Return an average plot generation time for this app"""
+    pgconn = psycopg2.connect(database='mesosite', host='iemdb', user='nobody',
+                              timeout=5)
+    cursor = pgconn.cursor()
+    cursor.execute("""
+        SELECT avg(timing) from autoplot_timing where appid = %s
+        and valid > (now() - '7 days'::interval)
+    """, (pidx, ))
+    timing = cursor.fetchone()[0]
+    return timing if timing is not None else -1
 
 
 def main():
+    """Do Something"""
     form = cgi.FieldStorage()
-    p = int(form.getfirst('p', 0))
+    pidx = int(form.getfirst('p', 0))
 
-    if p == 0:
+    if pidx == 0:
         import scripts  # @UnresolvedImport
         data = scripts.data
     else:
-        if p >= 100:
-            name = 'scripts100/p%s' % (p, )
+        if pidx >= 100:
+            name = 'scripts100/p%s' % (pidx, )
         else:
-            name = 'scripts/p%s' % (p,)
+            name = 'scripts/p%s' % (pidx,)
         if not os.path.isfile('%s.py' % (name, )):
             sys.stderr.write("autoplot/meta 404 %s" % (name, ))
             sys.stdout.write("Status: 404\n")
             sys.stdout.write("Content-type: text/plain\n\n")
             return
+        try:
+            timing = get_timing(pidx)
+        except Exception as _:
+            timing = -1
         fp, pathname, description = imp.find_module(name)
-        a = imp.load_module(name, fp, pathname, description)
-        data = a.get_description()
+        app = imp.load_module(name, fp, pathname, description)
+        data = app.get_description()
+        data['timing[secs]'] = timing
 
         # Defaults
         data['arguments'].append(dict(type='text', name='dpi', default='100',
