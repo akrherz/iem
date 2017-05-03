593032eea49dbc22030331a0bbf5de1689196642
akrherz <akrherz@iastate.edu>
2017-05-02 22:57:32 -0500

    update HTTP 404 logging to use syslog

--- htdocs/error404.php
+++ htdocs/error404.php
@@ -10,5 +10,11 @@ $t->content = <<<EOF
 EOF;
 $t->render('single.phtml');
 $ref = isset($_SERVER["HTTP_REFERER"]) ? $_SERVER["HTTP_REFERER"] : 'none';
-error_log("404 ". $_SERVER["REQUEST_URI"]. ' referer: '. $ref);
+
+// Since we are now running with php-fpm, we don't have access to Apache's
+// errorlog, so we now send to syslog, so that we get some denoted error logged
+// of 404s
+openlog("iem", LOG_PID | LOG_PERROR, LOG_LOCAL1);
+syslog(LOG_WARNING, "404 ". $_SERVER["REQUEST_URI"]. ' referer: '. $ref);
+closelog();
 ?>
