faaf76b724e480ba3d963acda77ccc66276f845d
akrherz <akrherz@iastate.edu>
2017-06-30 12:12:22 -0500

    pylint and prevent ldm insert warning

--- scripts/outgoing/madis2csv.py
+++ scripts/outgoing/madis2csv.py
@@ -1,122 +1,130 @@
 """
  Dump a CSV file of the MADIS data, kind of sad that I do this, but alas
 """
-
-import netCDF4
-import mx.DateTime
-import re
+from __future__ import print_function
 import os
-import numpy.ma
 import sys
 import time
-from pyiem.datatypes import temperature
 import subprocess
 import warnings
+import datetime
+
+from netCDF4 import Dataset, chartostring
+import numpy.ma
+from pyiem.datatypes import temperature
 # prevent core.py:931: RuntimeWarning: overflow encountered in multiply
 warnings.simplefilter("ignore", RuntimeWarning)
 # prevent core.py:3785: UserWarning: Warning: converting a masked element
 warnings.simplefilter("ignore", UserWarning)
 
 
-def sanityCheck(val, lower, upper, goodfmt, bv):
-    if (val > lower and val < upper):
+def sanity_check(val, lower, upper, goodfmt, default):
+    """Simple formatter"""
+    if val > lower and val < upper:
         return goodfmt % (val,)
-    return bv
+    return default
 
 
-# Wow, why did I do it this way... better not change it as I bet the
-# other end is ignoring the headers...
-fmt = ("STN,DATE,TIME,T,TD,WCI,RH,THI,DIR,SPD,GST,ALT,SLP,VIS,SKY,CEIL,"
-       "CLD,SKY,CEIL,CLD,SKY,CEIL,CLD,SKY,CEIL,CLD,SKY,CEIL,CLD,SKY,CEIL,"
-       "CLD,SKYSUM,PR6,PR24,WX,MINT6,MAXT6,MINT24,MAXT24,AUTO,PR1,PTMP1,"
-       "PTMP2,PTMP3,PTMP4,SUBS1,SUBS2,SUBS3,SUBS4,")
-format_tokens = fmt.split(",")
+def main():
+    """Go main"""
+    # Wow, why did I do it this way... better not change it as I bet the
+    # other end is ignoring the headers...
+    fmt = ("STN,DATE,TIME,T,TD,WCI,RH,THI,DIR,SPD,GST,ALT,SLP,VIS,SKY,CEIL,"
+           "CLD,SKY,CEIL,CLD,SKY,CEIL,CLD,SKY,CEIL,CLD,SKY,CEIL,CLD,SKY,CEIL,"
+           "CLD,SKYSUM,PR6,PR24,WX,MINT6,MAXT6,MINT24,MAXT24,AUTO,PR1,PTMP1,"
+           "PTMP2,PTMP3,PTMP4,SUBS1,SUBS2,SUBS3,SUBS4,")
+    format_tokens = fmt.split(",")
 
-gmt = mx.DateTime.gmt()
-fn = "/mesonet/data/madis/mesonet1/%s.nc" % (gmt.strftime("%Y%m%d_%H00"), )
-if not os.path.isfile(fn):
-    if gmt.minute > 30:
-        print '%s does not exist' % (fn,)
-    sys.exit()
-attempt = 0
-nc = None
-# Loop in case the file is being written while we attempt to open it
-while attempt < 3:
-    try:
-        nc = netCDF4.Dataset(fn, 'r')
-        attempt = 3
-    except:
-        time.sleep(10)
-        attempt += 1
-if nc is None:
-    print 'Numerous attempts to open MADIS netcdf %s failed!' % (fn,)
-    sys.exit(0)
+    utc = datetime.datetime.utcnow()
+    fn = "/mesonet/data/madis/mesonet1/%s.nc" % (utc.strftime("%Y%m%d_%H00"), )
+    if not os.path.isfile(fn):
+        if utc.minute > 30:
+            print('madis2csv %s does not exist' % (fn,))
+        sys.exit()
+    attempt = 0
+    nc = None
+    # Loop in case the file is being written while we attempt to open it
+    while attempt < 3:
+        try:
+            nc = Dataset(fn, 'r')
+            attempt = 3
+        except:
+            time.sleep(10)
+            attempt += 1
+    if nc is None:
+        print(("madis2csv Numerous attempts to open MADIS netcdf %s failed!"
+               ) % (fn,))
+        sys.exit(0)
 
-stations = nc.variables["stationId"][:]
-tmpf = temperature(nc.variables["temperature"][:], 'K').value('F')
-dwpf = temperature(nc.variables["dewpoint"][:], 'K').value('F')
-drct = nc.variables["windDir"][:]
-smps = nc.variables["windSpeed"][:] * 1.94384449
-alti = nc.variables["altimeter"][:] * 29.9196 / 1013.2  # in hPa
-vsby = nc.variables["visibility"][:] * 0.000621371192  # in hPa
-providers = nc.variables["dataProvider"][:]
-lat = nc.variables["latitude"][:]
-lon = nc.variables["longitude"][:]
-ele = nc.variables["elevation"][:]
-p01m = nc.variables["precipAccum"][:] * 25.4
-ptmp1 = temperature(nc.variables["roadTemperature1"][:], 'K').value('F')
-ptmp2 = temperature(nc.variables["roadTemperature2"][:], 'K').value('F')
-ptmp3 = temperature(nc.variables["roadTemperature3"][:], 'K').value('F')
-ptmp4 = temperature(nc.variables["roadTemperature4"][:], 'K').value('F')
-subs1 = temperature(nc.variables["roadSubsurfaceTemp1"][:], 'K').value('F')
-subs2 = temperature(nc.variables["roadSubsurfaceTemp2"][:], 'K').value('F')
-subs3 = temperature(nc.variables["roadSubsurfaceTemp3"][:], 'K').value('F')
-subs4 = temperature(nc.variables["roadSubsurfaceTemp4"][:], 'K').value('F')
-times = nc.variables["observationTime"][:]
+    stations = chartostring(nc.variables["stationId"][:])
+    tmpf = temperature(nc.variables["temperature"][:], 'K').value('F')
+    dwpf = temperature(nc.variables["dewpoint"][:], 'K').value('F')
+    drct = nc.variables["windDir"][:]
+    smps = nc.variables["windSpeed"][:] * 1.94384449
+    alti = nc.variables["altimeter"][:] * 29.9196 / 1013.2  # in hPa
+    vsby = nc.variables["visibility"][:] * 0.000621371192  # in hPa
+    # providers = nc.variables["dataProvider"][:]
+    # lat = nc.variables["latitude"][:]
+    # lon = nc.variables["longitude"][:]
+    # ele = nc.variables["elevation"][:]
+    p01m = nc.variables["precipAccum"][:] * 25.4
+    ptmp1 = temperature(nc.variables["roadTemperature1"][:], 'K').value('F')
+    ptmp2 = temperature(nc.variables["roadTemperature2"][:], 'K').value('F')
+    ptmp3 = temperature(nc.variables["roadTemperature3"][:], 'K').value('F')
+    ptmp4 = temperature(nc.variables["roadTemperature4"][:], 'K').value('F')
+    subs1 = temperature(nc.variables["roadSubsurfaceTemp1"][:], 'K').value('F')
+    subs2 = temperature(nc.variables["roadSubsurfaceTemp2"][:], 'K').value('F')
+    subs3 = temperature(nc.variables["roadSubsurfaceTemp3"][:], 'K').value('F')
+    subs4 = temperature(nc.variables["roadSubsurfaceTemp4"][:], 'K').value('F')
+    times = nc.variables["observationTime"][:]
+    nc.close()
 
-db = {}
+    db = {}
 
-for recnum in range(len(stations)):
-    thisStation = re.sub('\x00', '', stations[recnum].tostring())
-    ot = times[recnum]
-    if numpy.ma.is_masked(ot) or ot > 2141347600:
-        continue
-    ts = mx.DateTime.gmtime(times[recnum])
-    db[thisStation] = {
-        'STN': thisStation,
-        'DATE': ts.day,
-        'TIME': ts.strftime("%H%M"),
-        'T': sanityCheck(tmpf[recnum], -100, 150, '%.0f', ''),
-        'DIR': sanityCheck(drct[recnum], -1, 361, '%.0f', ''),
-        'TD': sanityCheck(dwpf[recnum], -100, 150, '%.0f', ''),
-        'SPD': sanityCheck(smps[recnum], -1, 150, '%.0f', ''),
-        'ALT': sanityCheck(alti[recnum], 2000, 4000, '%.0f', ''),
-        'VIS': sanityCheck(vsby[recnum], -1, 40, '%.0f', ''),
-        'PR1': sanityCheck(p01m[recnum], -0.01, 10, '%.0f', ''),
-        'SUBS1': sanityCheck(subs1[recnum], -100, 150, '%.0f', ''),
-        'SUBS2': sanityCheck(subs2[recnum], -100, 150, '%.0f', ''),
-        'SUBS3': sanityCheck(subs3[recnum], -100, 150, '%.0f', ''),
-        'SUBS4': sanityCheck(subs4[recnum], -100, 150, '%.0f', ''),
-        'PTMP1': sanityCheck(ptmp1[recnum], -100, 150, '%.0f', ''),
-        'PTMP2': sanityCheck(ptmp2[recnum], -100, 150, '%.0f', ''),
-        'PTMP3': sanityCheck(ptmp3[recnum], -100, 150, '%.0f', ''),
-        'PTMP4': sanityCheck(ptmp4[recnum], -100, 150, '%.0f', '')
-    }
+    for recnum in range(len(stations)-1, -1, -1):
+        station = stations[recnum]
+        if station in db:
+            continue
+        ot = times[recnum]
+        if numpy.ma.is_masked(ot) or ot > 2141347600:
+            continue
+        ts = datetime.datetime.fromtimestamp(ot)
+        db[station] = {
+            'STN': station,
+            'DATE': ts.day,
+            'TIME': ts.strftime("%H%M"),
+            'T': sanity_check(tmpf[recnum], -100, 150, '%.0f', ''),
+            'DIR': sanity_check(drct[recnum], -1, 361, '%.0f', ''),
+            'TD': sanity_check(dwpf[recnum], -100, 150, '%.0f', ''),
+            'SPD': sanity_check(smps[recnum], -1, 150, '%.0f', ''),
+            'ALT': sanity_check(alti[recnum], 2000, 4000, '%.0f', ''),
+            'VIS': sanity_check(vsby[recnum], -1, 40, '%.0f', ''),
+            'PR1': sanity_check(p01m[recnum], -0.01, 10, '%.0f', ''),
+            'SUBS1': sanity_check(subs1[recnum], -100, 150, '%.0f', ''),
+            'SUBS2': sanity_check(subs2[recnum], -100, 150, '%.0f', ''),
+            'SUBS3': sanity_check(subs3[recnum], -100, 150, '%.0f', ''),
+            'SUBS4': sanity_check(subs4[recnum], -100, 150, '%.0f', ''),
+            'PTMP1': sanity_check(ptmp1[recnum], -100, 150, '%.0f', ''),
+            'PTMP2': sanity_check(ptmp2[recnum], -100, 150, '%.0f', ''),
+            'PTMP3': sanity_check(ptmp3[recnum], -100, 150, '%.0f', ''),
+            'PTMP4': sanity_check(ptmp4[recnum], -100, 150, '%.0f', '')
+        }
 
-out = open('/tmp/madis.csv', 'w')
-out.write("%s\n" % (fmt,))
-for stid in db.keys():
-    for key in format_tokens:
-        if key in db[stid]:
-            out.write("%s," % (db[stid][key],))
-        else:
+    out = open('/tmp/madis.csv', 'w')
+    out.write("%s\n" % (fmt,))
+    for stid in db:
+        for key in format_tokens:
+            if key in db[stid]:
+                out.write("%s" % (db[stid][key],))
             out.write(",")
-    out.write("\n")
+        out.write("\n")
+    out.close()
 
-nc.close()
-out.close()
+    pqstr = "data c %s fn/madis.csv bogus csv" % (utc.strftime("%Y%m%d%H%M"),)
+    cmd = "/home/ldm/bin/pqinsert -i -p '%s' /tmp/madis.csv" % (pqstr,)
+    subprocess.call(cmd, shell=True)
+    # os.remove("/tmp/madis.csv")
 
-pqstr = "data c 000000000000 fn/madis.csv bogus csv"
-cmd = "/home/ldm/bin/pqinsert -p '%s' /tmp/madis.csv" % (pqstr,)
-subprocess.call(cmd, shell=True)
-os.remove("/tmp/madis.csv")
+
+if __name__ == '__main__':
+    main()
