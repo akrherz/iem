bfaa0076ed5c6c7664138c50cea9f207153ae9bf
akrherz <akrherz@iastate.edu>
2017-07-03 08:52:33 -0500

    quell nagios check when in summer for IDOT truck feed

--- nagios/check_idot_snowplows.py
+++ nagios/check_idot_snowplows.py
@@ -1,30 +1,44 @@
 """
  Nagios check to see how much snowplow data we are currently ingesting
 """
+from __future__ import print_function
 import sys
+import datetime
+
 import psycopg2
 
-POSTGIS = psycopg2.connect(database='postgis', host='iemdb', user='nobody')
-pcursor = POSTGIS.cursor()
 
-pcursor.execute("""
-select sum(case when valid > now() - '30 minutes'::interval then 1 else 0 end),
- sum(case when valid > now() - '1 day'::interval then 1 else 0 end)
- from idot_snowplow_current
-""")
-row = pcursor.fetchone()
-count = row[0]
-daycount = row[1]
+def main():
+    """Do Great Things"""
+    pgconn = psycopg2.connect(database='postgis', host='iemdb', user='nobody')
+    pcursor = pgconn.cursor()
 
-if daycount > 2:
-    print(('OK - snowplows %s/%s |count=%s;2;1;0 daycount=%s;2;1;0'
-           ) % (count, daycount, count, daycount))
-    sys.exit(0)
-elif daycount > 1:
-    print(('OK - snowplows %s/%s |count=%s;2;1;0 daycount=%s;2;1;0'
-           ) % (count, daycount, count, daycount))
-    sys.exit(1)
-else:
-    print(('CRITICAL - snowplows %s/%s |count=%s;2;1;0 daycount=%s;2;1;0'
-           ) % (count, daycount, count, daycount))
-    sys.exit(2)
+    pcursor.execute("""
+        select sum(case when valid > now() - '30 minutes'::interval
+            then 1 else 0 end),
+         sum(case when valid > now() - '1 day'::interval then 1 else 0 end)
+         from idot_snowplow_current
+    """)
+    row = pcursor.fetchone()
+    count = row[0]
+    daycount = row[1]
+
+    if daycount > 2:
+        print(('OK - snowplows %s/%s |count=%s;2;1;0 daycount=%s;2;1;0'
+               ) % (count, daycount, count, daycount))
+        sys.exit(0)
+    elif daycount > 1:
+        print(('OK - snowplows %s/%s |count=%s;2;1;0 daycount=%s;2;1;0'
+               ) % (count, daycount, count, daycount))
+        sys.exit(1)
+    else:
+        print(('CRITICAL - snowplows %s/%s |count=%s;2;1;0 daycount=%s;2;1;0'
+               ) % (count, daycount, count, daycount))
+        # Don't error in the summer
+        retcode = (2 if datetime.date.today().month not in (5, 6, 7, 8, 9, 10)
+                   else 0)
+        sys.exit(retcode)
+
+
+if __name__ == '__main__':
+    main()
