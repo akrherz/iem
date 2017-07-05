f0c8491cc83709ff44d38f7a4fb19c0785d03f2d
akrherz <akrherz@iastate.edu>
2017-07-05 10:42:27 -0500

    pylint and use pandas

--- scripts/ingestors/awos/parse_monthly_maint.py
+++ scripts/ingestors/awos/parse_monthly_maint.py
@@ -14,17 +14,20 @@
 from __future__ import print_function
 import sys
 import re
-import mx.DateTime
+import datetime
+
 import psycopg2
+import pandas as pd
 
-
-class bcolors(object):
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
+CALINFO = re.compile((r".* AWOS t/d:?\s*([0-9\-\.]+)/([0-9\-\.]+)\s+"
+                      r"Std\.?\s*t?/?d?:?\s*([0-9\-\.]+)/([0-9\-\.]+)"),
+                     re.IGNORECASE)
+HEADER = '\033[95m'
+OKBLUE = '\033[94m'
+OKGREEN = '\033[92m'
+WARNING = '\033[93m'
+FAIL = '\033[91m'
+ENDC = '\033[0m'
 
 
 def main(argv):
@@ -34,48 +37,40 @@ def main(argv):
                               port=5555, user="nobody")
     pcursor = pgconn.cursor()
 
-    CALINFO = re.compile((r".* AWOS t/d:?\s*([0-9\-\.]+)/([0-9\-\.]+) "
-                          r"Std\s*t?/?d?:?\s*([0-9\-\.]+)/([0-9\-\.]+)"),
-                         re.IGNORECASE)
+    df = pd.read_csv(argv[1])
 
-    data = sys.stdin.read()
+    for _, row in df.iterrows():
+        date = datetime.datetime.strptime(row['Visit Date'], '%d-%b-%y')
 
-    for line in data.split("\n"):
-        tokens = line.split(",")
-        if len(tokens) != 6:
+        if row['Description'].startswith('site offline'):
             continue
-        faa = tokens[0]
-        if len(faa) != 3:
-            continue
-        date = mx.DateTime.strptime(tokens[1], '%d-%b-%y')
-
-        parts = re.findall(CALINFO, tokens[3])
+        parts = re.findall(CALINFO, row['Description'])
         if len(parts) == 0:
-            print(bcolors.FAIL + tokens[3] + bcolors.ENDC)
+            print(FAIL + row['Description'] + ENDC)
             continue
-
+        faa = row['FAA Code']
         sql = """
         INSERT into iem_calibration(station, portfolio, valid, parameter,
         adjustment, final, comments) values (%s, 'iaawos', %s, %s, %s, %s, %s)
         """
         tempadj = float(parts[0][2]) - float(parts[0][0])
         args = (faa, date.strftime("%Y-%m-%d"), 'tmpf', tempadj,
-                parts[0][2], tokens[3].replace('"', ''))
+                parts[0][2], row['Description'].replace('"', ''))
         if len(sys.argv) > 1:
             pcursor.execute(sql, args)
 
         dewpadj = float(parts[0][3]) - float(parts[0][1])
         args = (faa, date.strftime("%Y-%m-%d"), 'dwpf',
                 float(parts[0][3]) - float(parts[0][1]),
-                parts[0][3], tokens[3].replace('"', ''))
+                parts[0][3], row['Description'].replace('"', ''))
         if len(sys.argv) > 1:
             pcursor.execute(sql, args)
 
         print(('--> %s [%s] TMPF: %s (%s) DWPF: %s (%s)'
-               ) % (faa, tokens[1], parts[0][2], tempadj,
+               ) % (faa, date, parts[0][2], tempadj,
                     parts[0][3], dewpadj))
 
-    if len(argv) == 1:
+    if len(argv) == 2:
         print('WARNING: Disabled, call with arbitrary argument to enable')
     else:
         pcursor.close()
