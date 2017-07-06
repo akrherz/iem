2f6c8b75e75ad5e0d0a3b349f9441681c8eed454
akrherz <akrherz@iastate.edu>
2017-07-06 15:44:06 -0500

    pylint and less aggressive database purging

--- scripts/GIS/attribute2shape.py
+++ scripts/GIS/attribute2shape.py
@@ -1,144 +1,24 @@
+"""Write current storm attributes to a shapefile...
+
+Run every minute from RUN_1MIN.sh
 """
- Write current storm attributes to a shapefile...
-"""
-import shapefile
-import mx.DateTime
+from __future__ import print_function
 import zipfile
 import os
 import shutil
-from pyiem import wellknowntext
 import subprocess
+import datetime
+
+import pytz
+import shapefile
 import psycopg2.extras
-POSTGIS = psycopg2.connect(database='postgis', host='iemdb')
-pcursor = POSTGIS.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
-"""
- nexrad         | character(3)             |
- storm_id       | character(2)             |
- geom           | geometry                 |
- azimuth        | smallint                 |
- range          | smallint                 |
- tvs            | character varying(10)    |
- meso           | character varying(10)    |
- posh           | smallint                 |
- poh            | smallint                 |
- max_size       | real                     |
- vil            | smallint                 |
- max_dbz        | smallint                 |
- max_dbz_height | real                     |
- top            | real                     |
- drct           | smallint                 |
- sknt           | smallint                 |
- valid          | timestamp with time zone |
-"""
-
-os.chdir("/tmp")
-
-# Delete anything older than 20 minutes
-now = mx.DateTime.gmt()
-eTS = mx.DateTime.gmt() - mx.DateTime.RelativeDateTime(minutes=+20)
-
-"""
-C is ASCII characters
-N is a double precision integer limited to around 18 characters in length
-D is for dates in the YYYYMMDD format,
-     with no spaces or hyphens between the sections
-F is for floating point numbers with the same length limits as N
-L is for logical data which is stored in the shapefile's attribute table as a
-    short integer as a 1 (true) or a 0 (false).
-    The values it can receive are 1, 0, y, n, Y, N, T, F
-    or the python builtins True and False
-"""
-w = shapefile.Writer(shapefile.POINT)
-w.field("VALID", 'C', 12, 0)
-w.field("STORM_ID", 'C', 2, 0)
-w.field("NEXRAD", 'C', 3, 0)
-w.field("AZIMUTH", 'N', 3, 0)
-w.field("RANGE", 'N', 3, 0)
-w.field("TVS", 'C', 10, 0)
-w.field("MESO", 'C', 10, 0)
-w.field("POSH", 'N', 3, 0)
-w.field("POH", 'N', 3, 0)
-w.field("MAX_SIZE", 'F', 5, 2)
-w.field("VIL", 'N', 3, 0)
-w.field("MAX_DBZ", 'N', 3, 0)
-w.field("MAX_DBZ_H", 'F', 5, 2)
-w.field("TOP", 'F', 9, 2)
-w.field("DRCT", 'N', 3, 0)
-w.field("SKNT", 'N', 3, 0)
-w.field("LAT", 'F', 10, 4)
-w.field("LON", 'F', 10, 4)
-
-sql = """
-DELETE from nexrad_attributes WHERE valid < '%s+00'
-""" % (eTS.strftime("%Y-%m-%d %H:%M"), )
-pcursor.execute(sql)
-sql = """SELECT *, valid at time zone 'UTC' as utcvalid,
-    ST_astext(geom) as tgeom, ST_x(geom) as lon,
-       ST_y(geom) as lat from nexrad_attributes """
-pcursor.execute(sql)
-
-for row in pcursor:
-    s = row["tgeom"]
-    if s is None or s == "":
-        continue
-    f = wellknowntext.convert_well_known_text(s)
-    w.point(f[0], f[1])
-    w.record(row['utcvalid'].strftime("%Y%m%d%H%M"),
-             row['storm_id'],
-             row['nexrad'],
-             row['azimuth'],
-             row['range'],
-             row['tvs'],
-             row['meso'],
-             row['posh'],
-             row['poh'],
-             row['max_size'],
-             row['vil'],
-             row['max_dbz'],
-             row['max_dbz_height'],
-             row['top'],
-             row['drct'],
-             row['sknt'],
-             row['lat'],
-             row['lon'])
-
-if pcursor.rowcount == 0:
-    if now.minute == 1:
-        print 'No NEXRAD attributes found, this may be bad!'
-    w.point(0.1, 0.1)
-    w.record("200000000000",
-             "XX",
-             "XXX",
-             0,
-             0,
-             "NONE",
-             "NONE",
-             0,
-             0,
-             0,
-             0,
-             0,
-             0,
-             0,
-             0,
-             0,
-             0,
-             0)
-
-w.save('current_nexattr')
-shutil.copy("/opt/iem/data/gis/meta/4326.prj", "current_nexattr.prj")
-z = zipfile.ZipFile("current_nexattr.zip", 'w', zipfile.ZIP_DEFLATED)
-z.write("current_nexattr.shp")
-z.write("current_nexattr.shx")
-z.write("current_nexattr.dbf")
-z.write("current_nexattr.prj")
-information = """
+INFORMATION = """
    Iowa Environmental Mesonet
    National Weather Service NEXRAD Storm Attributes Shapefile
 
 *** Warning: This information is provided without warranty and its future
-    availability is not gaurenteed by Iowa State University ***
+    availability is not guaranteed by Iowa State University ***
 
 This file contains a current snapshot of NEXRAD storm attributes from the
 last received volume scan from each NWS WSR-88D RADAR.  Any storm
@@ -154,11 +34,11 @@ The DBF Columns are as follows.
   - TVS      Tornado Vortex Signature, available values below
              NONE - No TVS detected
              TVS  - Low Level TVS detected
-             ETVS - Elevated TVS deteched
+             ETVS - Elevated TVS detected
   - MESO     Mesocyclone, available values below
              NONE - No MESO Detected
              3DCO - 3 Dimensional Correlated Shear Detected
-             MESO - Mesocyclone Indentified
+             MESO - Mesocyclone Identified
              UNCO - Uncorrelated 2-D Shear Detected
              1-25 - A number between 1 and 25, not sure what this means yet
   - POSH     Probability of Severe Hail (+0.75" or larger)
@@ -166,26 +46,143 @@ The DBF Columns are as follows.
   - MAX_SIZE Maximum Hail Size in inches
   - VIL      Vertically Integrated Liquid [inches]
   - MAX_DBZ  Maximum dBZ value
-  - MAX_DBZ_ Height of Maxium dBZ in thousands of feet
+  - MAX_DBZ_ Height of Maximum dBZ in thousands of feet
   - TOP      Storm Top in thousands of feet
   - DRCT     Storm movement direction [degrees from north]
   - SKNT     Storm movement velocity [knots]
-  - LAT      Convience replication of the data in the SHP file [deg North]
-  - LON      Convience replication of the data in the SHP file [deg East]
+  - LAT      Convenience replication of the data in the SHP file [deg North]
+  - LON      Convenience replication of the data in the SHP file [deg East]
 
 Contact Info:
   Daryl Herzmann akrherz@iastate.edu 515-294-5978
-
 """
-z.writestr("current_nexattr.txt", information)
-zinfo = z.getinfo('current_nexattr.txt')
-zinfo.external_attr = 0664 << 16
-z.close()
 
-cmd = ("/home/ldm/bin/pqinsert -p \"zip c %s "
-       "gis/shape/4326/us/current_nexattr.zip bogus zip\" "
-       "current_nexattr.zip") % (now.strftime("%Y%m%d%H%M"),)
-subprocess.call(cmd, shell=True)
 
-for suffix in ['zip', 'shp', 'dbf', 'shx', 'prj']:
-    os.unlink('current_nexattr.%s' % (suffix,))
+def shpschema():
+    """Create the schema
+
+    C is ASCII characters
+    N is a double precision integer limited to around 18 characters in length
+    D is for dates in the YYYYMMDD format,
+         with no spaces or hyphens between the sections
+    F is for floating point numbers with the same length limits as N
+    L is for logical data which is stored in the shapefile's attribute table
+        as a short integer as a 1 (true) or a 0 (false).
+        The values it can receive are 1, 0, y, n, Y, N, T, F
+        or the python builtins True and False
+    """
+    shp = shapefile.Writer(shapefile.POINT)
+    shp.field("VALID", 'C', 12, 0)
+    shp.field("STORM_ID", 'C', 2, 0)
+    shp.field("NEXRAD", 'C', 3, 0)
+    shp.field("AZIMUTH", 'N', 3, 0)
+    shp.field("RANGE", 'N', 3, 0)
+    shp.field("TVS", 'C', 10, 0)
+    shp.field("MESO", 'C', 10, 0)
+    shp.field("POSH", 'N', 3, 0)
+    shp.field("POH", 'N', 3, 0)
+    shp.field("MAX_SIZE", 'F', 5, 2)
+    shp.field("VIL", 'N', 3, 0)
+    shp.field("MAX_DBZ", 'N', 3, 0)
+    shp.field("MAX_DBZ_H", 'F', 5, 2)
+    shp.field("TOP", 'F', 9, 2)
+    shp.field("DRCT", 'N', 3, 0)
+    shp.field("SKNT", 'N', 3, 0)
+    shp.field("LAT", 'F', 10, 4)
+    shp.field("LON", 'F', 10, 4)
+    return shp
+
+
+def main():
+    """Go Main Go"""
+    pgconn = psycopg2.connect(database='postgis', host='iemdb')
+    pcursor = pgconn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+    os.chdir("/tmp")
+
+    # Delete anything older than 20 minutes
+    now = datetime.datetime.utcnow()
+    now = now.replace(tzinfo=pytz.utc)
+    ets = now - datetime.timedelta(minutes=20)
+
+    shp = shpschema()
+
+    # Less aggressively delete data
+    if now.minute % 10 == 0:
+        pcursor.execute("""
+            DELETE from nexrad_attributes WHERE valid < %s
+        """, (ets, ))
+    pcursor.execute("""
+        SELECT *, valid at time zone 'UTC' as utcvalid,
+        ST_x(geom) as lon, ST_y(geom) as lat from nexrad_attributes
+        WHERE valid > %s
+    """, (ets, ))
+
+    for row in pcursor:
+        shp.point(row['lon'], row['lat'])
+        shp.record(row['utcvalid'].strftime("%Y%m%d%H%M"),
+                   row['storm_id'],
+                   row['nexrad'],
+                   row['azimuth'],
+                   row['range'],
+                   row['tvs'],
+                   row['meso'],
+                   row['posh'],
+                   row['poh'],
+                   row['max_size'],
+                   row['vil'],
+                   row['max_dbz'],
+                   row['max_dbz_height'],
+                   row['top'],
+                   row['drct'],
+                   row['sknt'],
+                   row['lat'],
+                   row['lon'])
+
+    if pcursor.rowcount == 0:
+        if now.minute == 1:
+            print('No NEXRAD attributes found, this may be bad!')
+        shp.point(0.1, 0.1)
+        shp.record("200000000000",
+                   "XX",
+                   "XXX",
+                   0,
+                   0,
+                   "NONE",
+                   "NONE",
+                   0,
+                   0,
+                   0,
+                   0,
+                   0,
+                   0,
+                   0,
+                   0,
+                   0,
+                   0,
+                   0)
+
+    shp.save('current_nexattr')
+    shutil.copy("/opt/iem/data/gis/meta/4326.prj", "current_nexattr.prj")
+    zfp = zipfile.ZipFile("current_nexattr.zip", 'w', zipfile.ZIP_DEFLATED)
+    zfp.write("current_nexattr.shp")
+    zfp.write("current_nexattr.shx")
+    zfp.write("current_nexattr.dbf")
+    zfp.write("current_nexattr.prj")
+
+    zfp.writestr("current_nexattr.txt", INFORMATION)
+    zinfo = zfp.getinfo('current_nexattr.txt')
+    zinfo.external_attr = 0664 << 16
+    zfp.close()
+
+    cmd = ("/home/ldm/bin/pqinsert -i -p \"zip c %s "
+           "gis/shape/4326/us/current_nexattr.zip bogus zip\" "
+           "current_nexattr.zip") % (now.strftime("%Y%m%d%H%M"),)
+    subprocess.call(cmd, shell=True)
+
+    for suffix in ['zip', 'shp', 'dbf', 'shx', 'prj']:
+        os.unlink('current_nexattr.%s' % (suffix,))
+
+
+if __name__ == '__main__':
+    main()
