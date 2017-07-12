76509134348a06cdb3a1ee1f4415a7080180b8bc
akrherz <akrherz@iastate.edu>
2017-07-11 22:30:41 -0500

    add computation of state weights for MRMS

--- scripts/iemre/compute_state_weights_mrms.py
+++ scripts/iemre/compute_state_weights_mrms.py
@@ -5,7 +5,7 @@ import datetime
 
 import netCDF4
 from pyiem import iemre
-import numpy
+import numpy as np
 import psycopg2
 
 POSTGIS = psycopg2.connect(database='postgis', user='nobody', host='iemdb')
@@ -28,25 +28,36 @@ def create_file(fn):
     nc.comment = "No Comment at this time"
 
     # Setup Dimensions
-    nc.createDimension('lat', iemre.NY)
-    nc.createDimension('lon', iemre.NX)
+    nc.createDimension('lat', (iemre.NORTH - iemre.SOUTH) * 100.0)
+    nc.createDimension('lon', (iemre.EAST - iemre.WEST) * 100.0)
+    nc.createDimension('nv', 2)
 
-    # Setup Coordinate Variables
-    lat = nc.createVariable('lat', numpy.float, ('lat',))
+    lat = nc.createVariable('lat', np.float, ('lat',))
     lat.units = "degrees_north"
     lat.long_name = "Latitude"
     lat.standard_name = "latitude"
+    lat.bounds = "lat_bnds"
     lat.axis = "Y"
-    lat[:] = iemre.YAXIS
+    # Grid centers
+    lat[:] = np.arange(iemre.SOUTH + 0.005, iemre.NORTH, 0.01)
 
-    lon = nc.createVariable('lon', numpy.float, ('lon',))
+    lat_bnds = nc.createVariable('lat_bnds', np.float, ('lat', 'nv'))
+    lat_bnds[:, 0] = np.arange(iemre.SOUTH, iemre.NORTH, 0.01)
+    lat_bnds[:, 1] = np.arange(iemre.SOUTH + 0.01, iemre.NORTH + 0.01, 0.01)
+
+    lon = nc.createVariable('lon', np.float, ('lon',))
     lon.units = "degrees_east"
     lon.long_name = "Longitude"
     lon.standard_name = "longitude"
+    lon.bounds = "lon_bnds"
     lon.axis = "X"
-    lon[:] = iemre.XAXIS
+    lon[:] = np.arange(iemre.WEST, iemre.EAST, 0.01)
 
-    mask = nc.createVariable('domain', numpy.float, ('lat', 'lon'),
+    lon_bnds = nc.createVariable('lon_bnds', np.float, ('lon', 'nv'))
+    lon_bnds[:, 0] = np.arange(iemre.WEST, iemre.EAST, 0.01)
+    lon_bnds[:, 1] = np.arange(iemre.WEST + 0.01, iemre.EAST + 0.01, 0.01)
+
+    mask = nc.createVariable('domain', np.float, ('lat', 'lon'),
                              fill_value=1.e20)
     mask.units = "1"
     mask.long_name = "domain weighting"
@@ -55,7 +66,7 @@ def create_file(fn):
 
     for state in ['IA', 'ND', 'SD', 'KS', 'NE', 'MO', 'IN', 'IL', 'OH', 'MI',
                   'WI', 'MN', 'KY']:
-        st = nc.createVariable(state, numpy.float, ('lat', 'lon'),
+        st = nc.createVariable(state, np.float, ('lat', 'lon'),
                                fill_value=1.e20)
         st.units = "1"
         st.long_name = "%s weighting" % (state,)
@@ -71,7 +82,7 @@ def do_mask(fn):
     mask = nc.variables['domain']
     for state in ['IA', 'ND', 'SD', 'KS', 'NE', 'MO', 'IN', 'IL', 'OH', 'MI',
                   'WI', 'MN', 'KY']:
-        mask[:] = numpy.where(nc.variables[state][:] > 0, 1, mask)
+        mask[:] = np.where(nc.variables[state][:] > 0, 1, mask)
 
     nc.close()
 
@@ -91,7 +102,7 @@ def do_weighting(fn):
         ymin = row[2] - 0.5
         ymax = row[3] + 0.5
         print('Processing State: %s' % (state,))
-        data = numpy.zeros(numpy.shape(nc.variables[state][:]))
+        data = np.zeros(np.shape(nc.variables[state][:]))
         for i, lon in enumerate(nc.variables['lon'][:]):
             for j, lat in enumerate(nc.variables['lat'][:]):
                 # Don't compute 0s, if we don't have to
@@ -103,9 +114,9 @@ def do_weighting(fn):
                 geom = ("MULTIPOLYGON(((%s %s, %s %s, %s %s, %s %s, %s %s"
                         ")))"
                         ) % (lon, lat,
-                             lon, lat + iemre.DY,
-                             lon + iemre.DX, lat + iemre.DY,
-                             lon + iemre.DX, lat,
+                             lon, lat + 0.01,
+                             lon + 0.01, lat + 0.01,
+                             lon + 0.01, lat,
                              lon, lat)
                 pcursor.execute("""
     SELECT ST_Area(ST_Intersection(ST_SetSrid(ST_GeomFromText('%s'),4326),
@@ -121,7 +132,7 @@ def do_weighting(fn):
 
 def main():
     """Go Main"""
-    fn = "/mesonet/data/iemre/state_weights.nc"
+    fn = "/mesonet/data/iemre/state_weights_mrms.nc"
     if not os.path.isfile(fn):
         create_file(fn)
     do_weighting(fn)
