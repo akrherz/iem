9a2df6f14517401e8b5e6e649efb2e7c494e71b9
akrherz <akrherz@iastate.edu>
2017-07-11 21:33:51 -0500

    add climatology for precip on MRMS grid
    Nothing fancy here, just gridding the NCEI climate71 dataset onto the
    MRMS grid.

--- scripts/iemre/grid_climate.py
+++ scripts/iemre/grid_climate.py
@@ -1,22 +1,24 @@
+"""Grid climate for netcdf usage"""
+from __future__ import print_function
 import sys
+import datetime
+
 import netCDF4
 import numpy as np
-import datetime
 import psycopg2
 from pyiem import iemre, datatypes
 from pyiem.network import Table as NetworkTable
 from scipy.interpolate import NearestNDInterpolator
 
-nt = NetworkTable(['IACLIMATE', 'MNCLIMATE', 'NDCLIMATE',
+NT = NetworkTable(['IACLIMATE', 'MNCLIMATE', 'NDCLIMATE',
                    'SDCLIMATE', 'NECLIMATE', 'KSCLIMATE', 'MOCLIMATE',
                    'ILCLIMATE', 'WICLIMATE', 'MICLIMATE', 'INCLIMATE',
                    'OHCLIMATE', 'KYCLIMATE'])
 
 COOP = psycopg2.connect(database='coop', user='nobody', host='iemdb')
-cursor = COOP.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
 
-def generic_gridder(cursor, idx):
+def generic_gridder(nc, cursor, idx):
     """
     Generic gridding algorithm for easy variables
     """
@@ -24,23 +26,23 @@ def generic_gridder(cursor, idx):
     lons = []
     vals = []
     for row in cursor:
-        if row[idx] is not None and row['station'] in nt.sts:
-            lats.append(nt.sts[row['station']]['lat'])
-            lons.append(nt.sts[row['station']]['lon'])
+        if row[idx] is not None and row['station'] in NT.sts:
+            lats.append(NT.sts[row['station']]['lat'])
+            lons.append(NT.sts[row['station']]['lon'])
             vals.append(row[idx])
     if len(vals) < 4:
-        print "Only %s observations found for %s, won't grid" % (len(vals),
-                                                                 idx)
+        print(("Only %s observations found for %s, won't grid"
+               ) % (len(vals), idx))
         return None
 
-    xi, yi = np.meshgrid(iemre.XAXIS, iemre.YAXIS)
+    xi, yi = np.meshgrid(nc.variables['lon'][:], nc.variables['lat'][:])
     nn = NearestNDInterpolator((lons, lats), np.array(vals))
     grid = nn(xi, yi)
-    print cursor.rowcount, idx, np.max(grid), np.min(grid)
+    print(("%s %s %.3f %.3f"
+           ) % (cursor.rowcount, idx, np.max(grid), np.min(grid)))
     if grid is not None:
         return grid
-    else:
-        return None
+    return None
 
 
 def grid_day(nc, ts):
@@ -48,6 +50,7 @@ def grid_day(nc, ts):
     I proctor the gridding of data on an hourly basis
     @param ts Timestamp of the analysis, we'll consider a 20 minute window
     """
+    cursor = COOP.cursor(cursor_factory=psycopg2.extras.DictCursor)
     offset = iemre.daily_offset(ts)
     if ts.day == 29 and ts.month == 2:
         ts = datetime.datetime(2000, 3, 1)
@@ -57,25 +60,28 @@ def grid_day(nc, ts):
              """ % (ts.strftime("%Y-%m-%d"), )
     cursor.execute(sql)
     if cursor.rowcount > 4:
-        res = generic_gridder(cursor, 'high')
-        if res is not None:
-            nc.variables['high_tmpk'][offset] = datatypes.temperature(
-                                                        res, 'F').value('K')
-        cursor.scroll(0, mode='absolute')
-        res = generic_gridder(cursor, 'low')
-        if res is not None:
-            nc.variables['low_tmpk'][offset] = datatypes.temperature(
-                                                        res, 'F').value('K')
-        cursor.scroll(0, mode='absolute')
-        res = generic_gridder(cursor, 'precip')
+        if 'high_tmpk' in nc.variables:
+            res = generic_gridder(nc, cursor, 'high')
+            if res is not None:
+                nc.variables['high_tmpk'][offset] = datatypes.temperature(
+                                                            res,
+                                                            'F').value('K')
+            cursor.scroll(0, mode='absolute')
+            res = generic_gridder(nc, cursor, 'low')
+            if res is not None:
+                nc.variables['low_tmpk'][offset] = datatypes.temperature(
+                                                            res,
+                                                            'F').value('K')
+            cursor.scroll(0, mode='absolute')
+        res = generic_gridder(nc, cursor, 'precip')
         if res is not None:
             nc.variables['p01d'][offset] = res * 25.4
     else:
-        print "%s has %02i entries, FAIL" % (ts.strftime("%Y-%m-%d"),
-                                             cursor.rowcount)
+        print(("%s has %02i entries, FAIL"
+               ) % (ts.strftime("%Y-%m-%d"), cursor.rowcount))
 
 
-def main(ts):
+def workflow(ts):
     # Load up a station table we are interested in
 
     # Load up our netcdf file!
@@ -83,18 +89,27 @@ def main(ts):
     grid_day(nc, ts)
     nc.close()
 
+    nc = netCDF4.Dataset("/mesonet/data/iemre/mw_mrms_dailyc.nc", 'a')
+    grid_day(nc, ts)
+    nc.close()
 
-if __name__ == "__main__":
-    if len(sys.argv) == 4:
-        ts = datetime.datetime(int(sys.argv[1]), int(sys.argv[2]),
-                               int(sys.argv[3]))
-        main(ts)
+
+def main(argv):
+    """Go Main!"""
+    if len(argv) == 4:
+        ts = datetime.datetime(int(argv[1]), int(argv[2]),
+                               int(argv[3]))
+        workflow(ts)
     else:
         sts = datetime.datetime(2000, 1, 1)
         ets = datetime.datetime(2001, 1, 1)
         interval = datetime.timedelta(days=1)
         now = sts
         while now < ets:
-            print now
-            main(now)
+            print(now)
+            workflow(now)
             now += interval
+
+
+if __name__ == "__main__":
+    main(sys.argv)
--- scripts/iemre/init_mrms_dailyc.py
+++ scripts/iemre/init_mrms_dailyc.py
@@ -12,11 +12,11 @@ def init_year(ts):
     Create a new NetCDF file for a year of our specification!
     """
 
-    fp = "/mesonet/data/iemre/mw_dailyc.nc"
+    fp = "/mesonet/data/iemre/mw_mrms_dailyc.nc"
     nc = netCDF4.Dataset(fp, 'w')
     nc.title = "IEM Daily Reanalysis Climatology %s" % (ts.year,)
     nc.platform = "Grided Climatology"
-    nc.description = "IEM daily analysis on a 0.25 degree grid"
+    nc.description = "IEM daily analysis on a 0.01 degree grid"
     nc.institution = "Iowa State University, Ames, IA, USA"
     nc.source = "Iowa Environmental Mesonet"
     nc.project_id = "IEM"
@@ -28,26 +28,38 @@ def init_year(ts):
     nc.comment = "No Comment at this time"
 
     # Setup Dimensions
-    nc.createDimension('lat', iemre.NY)
-    nc.createDimension('lon', iemre.NX)
+    nc.createDimension('lat', (iemre.NORTH - iemre.SOUTH) * 100.0)
+    nc.createDimension('lon', (iemre.EAST - iemre.WEST) * 100.0)
     ts2 = datetime.datetime(ts.year + 1, 1, 1)
     days = (ts2 - ts).days
     nc.createDimension('time', int(days))
+    nc.createDimension('nv', 2)
 
     # Setup Coordinate Variables
     lat = nc.createVariable('lat', np.float, ('lat',))
     lat.units = "degrees_north"
     lat.long_name = "Latitude"
     lat.standard_name = "latitude"
+    lat.bounds = "lat_bnds"
     lat.axis = "Y"
-    lat[:] = iemre.YAXIS
+    # Grid centers
+    lat[:] = np.arange(iemre.SOUTH + 0.005, iemre.NORTH, 0.01)
+
+    lat_bnds = nc.createVariable('lat_bnds', np.float, ('lat', 'nv'))
+    lat_bnds[:, 0] = np.arange(iemre.SOUTH, iemre.NORTH, 0.01)
+    lat_bnds[:, 1] = np.arange(iemre.SOUTH + 0.01, iemre.NORTH + 0.01, 0.01)
 
     lon = nc.createVariable('lon', np.float, ('lon',))
     lon.units = "degrees_east"
     lon.long_name = "Longitude"
     lon.standard_name = "longitude"
+    lon.bounds = "lon_bnds"
     lon.axis = "X"
-    lon[:] = iemre.XAXIS
+    lon[:] = np.arange(iemre.WEST, iemre.EAST, 0.01)
+
+    lon_bnds = nc.createVariable('lon_bnds', np.float, ('lon', 'nv'))
+    lon_bnds[:, 0] = np.arange(iemre.WEST, iemre.EAST, 0.01)
+    lon_bnds[:, 1] = np.arange(iemre.WEST + 0.01, iemre.EAST + 0.01, 0.01)
 
     tm = nc.createVariable('time', np.float, ('time',))
     tm.units = "Days since %s-01-01 00:00:0.0" % (ts.year,)
@@ -56,21 +68,6 @@ def init_year(ts):
     tm.axis = "T"
     tm.calendar = "gregorian"
     tm[:] = np.arange(0, int(days))
-
-    # Tracked variables
-    high = nc.createVariable('high_tmpk', np.float, ('time', 'lat', 'lon'),
-                             fill_value=1.e20)
-    high.units = "K"
-    high.long_name = "2m Air Temperature Daily High"
-    high.standard_name = "2m Air Temperature"
-    high.coordinates = "lon lat"
-
-    low = nc.createVariable('low_tmpk', np.float, ('time', 'lat', 'lon'),
-                            fill_value=1.e20)
-    low.units = "K"
-    low.long_name = "2m Air Temperature Daily Low"
-    low.standard_name = "2m Air Temperature"
-    low.coordinates = "lon lat"
 
     p01d = nc.createVariable('p01d', np.float, ('time', 'lat', 'lon'),
                              fill_value=1.e20)
