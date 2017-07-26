a44bc922a6215192d22ab6287b2a9c401780897c
akrherz <akrherz@iastate.edu>
2017-07-26 14:35:43 -0500

    more intuitive US Drought Monitor timeseries plot
    https://mesonet.agron.iastate.edu/plotting/auto/?q=183

--- htdocs/plotting/auto/scripts100/p183.py
+++ htdocs/plotting/auto/scripts100/p183.py
@@ -60,24 +60,23 @@ def plotter(fdict):
     (fig, ax) = plt.subplots(1, 1)
 
     xs = df['x'].values
-    ax.bar(xs, df['D0'], width=7, fc=COLORS[0], ec='None',
+    bottom = (df['D4'] + df['D3'] + df['D2'] + df['D1']).values
+    ax.bar(xs, df['D0'], width=7, fc=COLORS[0], ec='None', bottom=bottom,
            label='D0 Abnormal')
-    bottom = df['D0'].values
+    bottom = (df['D4'] + df['D3'] + df['D2']).values
     ax.bar(xs, df['D1'], bottom=bottom, width=7, fc=COLORS[1],
            ec='None',
            label='D1 Moderate')
-    bottom = (df['D1'] + df['D0']).values
+    bottom = (df['D4'] + df['D3']).values
     ax.bar(xs, df['D2'], width=7,
            fc=COLORS[2], ec='None', bottom=bottom,
            label='D2 Severe')
-    bottom = (df['D2'] + df['D1'] + df['D0']).values
+    bottom = df['D4'].values
     ax.bar(xs, df['D3'],
            width=7, fc=COLORS[3], ec='None', bottom=bottom,
            label='D3 Extreme')
-    bottom = (df['D3'] + df['D2'] + df['D1'] + df['D0']).values
-    ax.bar(xs,
-           df['D4'],
-           width=7, fc=COLORS[4], ec='None', bottom=bottom,
+    ax.bar(xs, df['D4'],
+           width=7, fc=COLORS[4], ec='None',
            label='D4 Exceptional')
     # Duplicate last row
     new_index = df.index[-1] + datetime.timedelta(days=7)
