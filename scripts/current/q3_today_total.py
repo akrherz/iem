4285217e590924805b4016061203ae90e41392c7
akrherz <akrherz@iastate.edu>
2017-08-28 13:42:47 -0500

    add midwest static MRMS daily plot

--- scripts/current/q3_today_total.py
+++ scripts/current/q3_today_total.py
@@ -2,15 +2,16 @@
  Create a plot of today's estimated precipitation based on the Q3 data
 """
 import datetime
-import numpy as np
 import sys
+
+import numpy as np
 import pyiem.iemre as iemre
 from pyiem.datatypes import distance
 import netCDF4
 import pytz
 import matplotlib
 matplotlib.use('agg')
-from pyiem.plot import MapPlot, nwsprecip  # NOPEP8
+from pyiem.plot import MapPlot, nwsprecip
 
 
 def doday(ts, realtime):
@@ -46,23 +47,24 @@ def doday(ts, realtime):
     clevs = [0.01, 0.1, 0.25, 0.5, 0.75, 1, 1.5, 2, 2.5, 3, 3.5, 4, 5, 6, 8,
              10]
 
-    sector = 'iowa'
-    pqstr = ("plot %s %s00 %s_q2_1d.png %s_q2_1d.png png"
-             ) % (routes, ts.strftime("%Y%m%d%H"), sector, sector)
-    m = MapPlot(title=("%s NCEP MRMS Q3 Today's Precipitation"
-                       ) % (ts.strftime("%-d %b %Y"),),
-                subtitle=subtitle, sector=sector)
+    (xx, yy) = np.meshgrid(lons, lats)
+    for sector in ['iowa', 'midwest']:
+        pqstr = ("plot %s %s00 %s_q2_1d.png %s_q2_1d.png png"
+                 ) % (routes, ts.strftime("%Y%m%d%H"), sector, sector)
+        mp = MapPlot(title=("%s NCEP MRMS Q3 Today's Precipitation"
+                            ) % (ts.strftime("%-d %b %Y"),),
+                     subtitle=subtitle, sector=sector)
 
-    (x, y) = np.meshgrid(lons, lats)
-
-    m.pcolormesh(x, y, distance(precip, 'MM').value('IN'), clevs,
-                 cmap=nwsprecip(), units='inch')
-    m.drawcounties()
-    m.postprocess(pqstr=pqstr, view=False)
-    m.close()
+        mp.pcolormesh(xx, yy, distance(precip, 'MM').value('IN'), clevs,
+                      cmap=nwsprecip(), units='inch')
+        if sector == 'iowa':
+            mp.drawcounties()
+        mp.postprocess(pqstr=pqstr, view=False)
+        mp.close()
 
 
 def main():
+    """Main Method"""
     if len(sys.argv) == 4:
         date = datetime.date(int(sys.argv[1]), int(sys.argv[2]),
                              int(sys.argv[3]))
