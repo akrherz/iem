ff542930cea29d96157a2464fd66f114716abf85
akrherz <akrherz@iastate.edu>
2017-08-31 08:48:44 -0500

    ignore trace values for now
    refs #137

--- htdocs/rainfall/obhour-json.php
+++ htdocs/rainfall/obhour-json.php
@@ -48,7 +48,8 @@ while( list($key,$interval) = each($intervals))
   } else {
     $ts0 = $ts - ($interval * 3600);
   }
-  $sql = sprintf("select station, sum(phour) as p1 from hourly_%s
+  $sql = sprintf("select station,
+    sum(case when phour < 0.01 then 0 else phour end) as p1 from hourly_%s
           WHERE valid >= '%s+00' and valid < '%s+00' and network IN (%s) 
           GROUP by station", strftime("%Y", $ts), strftime("%Y-%m-%d %H:%M", $ts0), 
           strftime("%Y-%m-%d %H:%M", $ts), $networks);
