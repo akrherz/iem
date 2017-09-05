48ddef2f22e51e659d7cb9458bb6ce9bb6ab486e
akrherz <akrherz@iastate.edu>
2017-09-05 13:33:25 -0500

    remove not needed -99/99 checks

--- include/iemaccessob.php
+++ include/iemaccessob.php
@@ -14,19 +14,11 @@ class IEMAccessOb {
       $this->db["gust_ts"] = strtotime(substr($row["max_gust_ts"],0,16));
       $this->db["lgust_ts"] = strtotime(substr($row["lmax_gust_ts"],0,16));
     } else{
-    	$this->db["gust_ts"] = "";
-    	$this->db["lgust_ts"] = "";
+      $this->db["gust_ts"] = "";
+      $this->db["lgust_ts"] = "";
     }
     $this->db["obtime"] = strftime("%d %b %I:%M %p", $this->db["ts"]);
-    if ($this->db["max_tmpf"] == -99){
-    	$this->db["max_tmpf"] = null;
-    }
-  	if ($this->db["min_tmpf"] == 99){
-    	$this->db["min_tmpf"] = null;
-    }
   }
-  
 
 }
-
 ?>
