75da5e681b073c6047f5a2fb76721eaa0964c2ed
akrherz <akrherz@iastate.edu>
2017-09-05 11:34:12 -0500

    travis-ci fix? for postgres?

--- database/bootstrap.sh
+++ database/bootstrap.sh
@@ -1,18 +1,18 @@
 # setup databases
 # we want this script to exit 2 so that travis will report any failures
 
-psql -c 'create user nobody;' -U postgres
-psql -c 'create user apache;' -U postgres
-psql -c 'create user mesonet;' -U postgres
-psql -c 'create user ldm;' -U postgres
-psql -c 'create user apiuser;' -U postgres
+psql -c 'create user nobody;' -h localhost -U postgres
+psql -c 'create user apache;' -h localhost -U postgres
+psql -c 'create user mesonet;' -h localhost -U postgres
+psql -c 'create user ldm;' -h localhost -U postgres
+psql -c 'create user apiuser;' -h localhost -U postgres
 
 for db in afos mesosite postgis snet \
 asos hads  mos        rwis     squaw \
 awos iem   other      scan     wepp \
 coop isuag portfolio  smos
 do
-psql -v "ON_ERROR_STOP=1" -c "create database $db;" -U postgres || exit 2
-psql -v "ON_ERROR_STOP=1" -f init/${db}.sql -U postgres -q $db || exit 2
-psql -v "ON_ERROR_STOP=1" -f functions.sql -U postgres -q $db || exit 2
+psql -v "ON_ERROR_STOP=1" -c "create database $db;" -h localhost -U postgres || exit 2
+psql -v "ON_ERROR_STOP=1" -f init/${db}.sql -h localhost -U postgres -q $db || exit 2
+psql -v "ON_ERROR_STOP=1" -f functions.sql -h localhost -U postgres -q $db || exit 2
 done
