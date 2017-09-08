4b050125e81bfdbb54891c260e745a2494138d13
akrherz <akrherz@iastate.edu>
2017-09-08 08:01:22 -0500

    util script to zero out ISUSM erroneous precipitation

---
+++ scripts/isuag/zero_daily_precip.py
@@ -0,0 +1,72 @@
+"""Sometimes we need to completely zero out precip for a day
+
+Likely due to water being dumped into the tipping bucket to clean it :/
+"""
+from __future__ import print_function
+import sys
+import datetime
+
+import pytz
+import psycopg2
+
+
+def zero_hourly(station, sts, ets):
+    """Zero out the hourly data"""
+    pgconn = psycopg2.connect(database='isuag', host='iemdb')
+    cursor = pgconn.cursor()
+    for table in ['sm_hourly', 'sm_15minute']:
+        cursor.execute("""
+            UPDATE """ + table + """
+            SET rain_mm_tot_qc = 0, rain_mm_tot_f = 'Z', rain_mm_tot = 0
+            WHERE station = %s and valid > %s and valid <= %s
+        """, (station, sts, ets))
+        print("%s updated %s rows" % (table, cursor.rowcount))
+    cursor.close()
+    pgconn.commit()
+
+
+def zero_daily(station, date):
+    """Zero out the daily data"""
+    pgconn = psycopg2.connect(database='isuag', host='iemdb')
+    cursor = pgconn.cursor()
+    cursor.execute("""
+        UPDATE sm_daily
+        SET rain_mm_tot_qc = 0, rain_mm_tot_f = 'Z', rain_mm_tot = 0
+        WHERE station = %s and valid = %s
+    """, (station, date))
+    print("sm_daily updated %s rows" % (cursor.rowcount, ))
+    cursor.close()
+    pgconn.commit()
+
+
+def zero_iem(station, date):
+    """Zero out the hourly data"""
+    pgconn = psycopg2.connect(database='iem', host='iemdb')
+    cursor = pgconn.cursor()
+    cursor.execute("""
+        UPDATE summary s
+        SET pday = 0
+        FROM stations t
+        WHERE s.iemid = t.iemid and t.id = %s and t.network = 'ISUSM'
+        and day = %s
+    """, (station, date))
+    print("summary updated %s rows" % (cursor.rowcount, ))
+    cursor.close()
+    pgconn.commit()
+
+
+def main(argv):
+    """Go Main"""
+    station = argv[1]
+    date = datetime.date(int(argv[2]), int(argv[3]), int(argv[4]))
+    # Our weather stations are in CST, so the 'daily' precip is for a 6z to 6z
+    # period and not calendar day, the hourly values are in the rears
+    sts = datetime.datetime(date.year, date.month, date.day, 6)
+    sts = sts.replace(tzinfo=pytz.utc)
+    ets = sts + datetime.timedelta(hours=24)
+    zero_hourly(station, sts, ets)
+    zero_daily(station, date)
+    zero_iem(station, date)
+
+if __name__ == '__main__':
+    main(sys.argv)
