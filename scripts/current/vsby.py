775f835c6dd37f1e46be7f0ae1610001cd826605
akrherz <akrherz@iastate.edu>
2017-10-17 04:24:58 -0500

    pylint

--- scripts/current/vsby.py
+++ scripts/current/vsby.py
@@ -1,56 +1,64 @@
 """Generate current plot of visibility"""
-from pyiem.plot import MapPlot
-import sys
+from __future__ import print_function
 import datetime
+
+from pyiem.plot import MapPlot
 import psycopg2
 import numpy as np
 import matplotlib.cm as cm
 
-IEM = psycopg2.connect(database='iem', host='iemdb', user='nobody')
-cursor = IEM.cursor()
 
-# Compute normal from the climate database
-sql = """
-SELECT
-  id, network, vsby, ST_x(geom) as lon, ST_y(geom) as lat
-FROM
-  current c JOIN stations s ON (s.iemid = c.iemid)
-WHERE
-  s.network IN ('AWOS', 'IA_ASOS','IL_ASOS','MN_ASOS','WI_ASOS','SD_ASOS',
-              'NE_ASOS','MO_ASOS') and
-  valid + '60 minutes'::interval > now() and
-  vsby >= 0 and vsby <= 10
-"""
+def main():
+    """GO"""
+    pgconn = psycopg2.connect(database='iem', host='iemdb', user='nobody')
+    cursor = pgconn.cursor()
 
-lats = []
-lons = []
-vals = []
-valmask = []
-cursor.execute(sql)
-for row in cursor:
-    lats.append(row[4])
-    lons.append(row[3])
-    vals.append(row[2])
-    valmask.append(row[1] in ['AWOS', 'IA_ASOS'])
+    # Compute normal from the climate database
+    sql = """
+    SELECT
+      id, network, vsby, ST_x(geom) as lon, ST_y(geom) as lat
+    FROM
+      current c JOIN stations s ON (s.iemid = c.iemid)
+    WHERE
+      s.network IN ('AWOS', 'IA_ASOS','IL_ASOS','MN_ASOS','WI_ASOS','SD_ASOS',
+                  'NE_ASOS','MO_ASOS') and
+      valid + '60 minutes'::interval > now() and
+      vsby >= 0 and vsby <= 10
+    """
 
-if len(lats) < 5:
-    sys.exit(0)
+    lats = []
+    lons = []
+    vals = []
+    valmask = []
+    cursor.execute(sql)
+    for row in cursor:
+        lats.append(row[4])
+        lons.append(row[3])
+        vals.append(row[2])
+        valmask.append(row[1] in ['AWOS', 'IA_ASOS'])
 
-now = datetime.datetime.now()
+    if len(lats) < 5:
+        return
 
-m = MapPlot(sector='iowa',
-            title='Iowa Visibility',
-            subtitle='Valid: %s' % (now.strftime("%d %b %Y %-I:%M %p"),))
+    now = datetime.datetime.now()
 
-m.contourf(lons, lats, vals, np.array([0.01, 0.1, 0.25, 0.5, 1, 2, 3,
-                                       5, 8, 9.9]),
-           units='miles', cmap=cm.get_cmap('gray'))
+    mp = MapPlot(sector='iowa',
+                 title='Iowa Visibility',
+                 subtitle='Valid: %s' % (now.strftime("%d %b %Y %-I:%M %p"),))
 
-m.plot_values(lons, lats, vals, '%.1f', valmask)
-m.drawcounties()
+    mp.contourf(lons, lats, vals, np.array([0.01, 0.1, 0.25, 0.5, 1, 2, 3,
+                                            5, 8, 9.9]),
+                units='miles', cmap=cm.get_cmap('gray'))
 
-pqstr = ("plot ac %s00 iowa_vsby.png vsby_contour_%s00.png png"
-         "") % (datetime.datetime.utcnow().strftime("%Y%m%d%H"),
-                datetime.datetime.utcnow().strftime("%H"))
-m.postprocess(pqstr=pqstr)
-m.close()
+    mp.plot_values(lons, lats, vals, '%.1f', valmask)
+    mp.drawcounties()
+
+    pqstr = ("plot ac %s00 iowa_vsby.png vsby_contour_%s00.png png"
+             "") % (datetime.datetime.utcnow().strftime("%Y%m%d%H"),
+                    datetime.datetime.utcnow().strftime("%H"))
+    mp.postprocess(pqstr=pqstr)
+    mp.close()
+
+
+if __name__ == '__main__':
+    main()
