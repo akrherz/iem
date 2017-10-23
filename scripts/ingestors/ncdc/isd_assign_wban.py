ef4410fd822dbf4c467c1bfe0f21d208f68bfa64
akrherz <akrherz@iastate.edu>
2017-10-23 12:39:15 -0500

    merge of NCEI ISD provided WBAN numbers to mesosite database

---
+++ scripts/ingestors/ncdc/isd_assign_wban.py
@@ -0,0 +1,50 @@
+"""Rip the WBAN number out of the ISD station files and add to database
+
+    ftp://ftp.ncdc.noaa.gov/pub/data/noaa/isd-history.txt
+"""
+from __future__ import print_function
+
+import psycopg2
+
+
+def main():
+    """Go"""
+    pgconn = psycopg2.connect(database='mesosite', host='localhost',
+                              port=5555, user='mesonet')
+    cursor = pgconn.cursor()
+    xref = {}
+    for linenum, line in enumerate(open('isd-history.txt')):
+        if linenum < 24:
+            continue
+        wban = int(line[7:12])
+        if wban == 99999:
+            continue
+        faa = line[51:55]
+        if faa.strip() == '':
+            continue
+        if faa[0] == 'K':
+            faa = faa[1:]
+        if xref.get(faa) is not None and xref[faa] != wban:
+            print("!! conflict %s wban: %s old: %s" % (faa, xref[faa], wban))
+            del xref[faa]
+        xref[faa] = wban
+        country = line[43:45]
+        state = line[48:50]
+        cursor.execute("""
+            SELECT iemid, synop from stations where
+            id = %s and (network ~* 'ASOS' or network ~* 'AWOS')
+        """, (faa, ))
+        if cursor.rowcount != 1:
+            continue
+        row = cursor.fetchone()
+        if row[1] != wban:
+            print("%s %s -> %s" % (faa, row[1], wban))
+            cursor.execute("""
+            UPDATE stations SET synop = %s where iemid = %s
+            """, (wban, row[0]))
+    cursor.close()
+    pgconn.commit()
+
+
+if __name__ == '__main__':
+    main()
