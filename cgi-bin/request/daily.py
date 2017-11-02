a04952ffed5b93dbd0432d96fdf5e510dd299076
akrherz <akrherz@iastate.edu>
2017-11-02 09:42:18 -0500

    code cleanup and prevent a traceback in daily climate request

--- cgi-bin/request/daily.py
+++ cgi-bin/request/daily.py
@@ -3,27 +3,33 @@
 import cgi
 import sys
 import datetime
-import psycopg2
+
+from pyiem.util import get_dbconn
 from pyiem.network import Table as NetworkTable
 
 
-def get_climate(network, stations=[]):
+def get_climate(network, stations):
     """Fetch the climatology for these stations"""
     nt = NetworkTable(network)
+    if not nt.sts:
+        sys.stdout.write("ERROR: Invalid network specified")
+        return
     data = dict()
     clisites = []
     cldata = dict()
     for station in stations:
         cldata[nt.sts[station]['ncdc81']] = dict()
         clisites.append(nt.sts[station]['ncdc81'])
-    if len(clisites) == 0:
+    if not clisites:
         return data
     if len(clisites) == 1:
         clisites.append('XX')
-    mesosite = psycopg2.connect(database='coop', host='iemdb', user='nobody')
+    mesosite = get_dbconn('coop')
     cursor = mesosite.cursor()
-    cursor.execute("""SELECT station, valid, high, low, precip
-    from ncdc_climate81 where station in %s""", (tuple(clisites),))
+    cursor.execute("""
+        SELECT station, valid, high, low, precip
+        from ncdc_climate81 where station in %s
+    """, (tuple(clisites),))
     for row in cursor:
         cldata[row[0]][row[1].strftime("%m%d")] = {'high': row[2],
                                                    'low': row[3],
@@ -42,14 +48,16 @@ def get_climate(network, stations=[]):
     return data
 
 
-def get_data(network, sts, ets, stations=[]):
+def get_data(network, sts, ets, stations):
     """Go fetch data please"""
-    IEM = psycopg2.connect(database='iem', host='iemdb', user='nobody')
-    cursor = IEM.cursor()
-    climate = get_climate(network, stations=stations)
-    s = ("station,day,max_temp_f,min_temp_f,max_dewpoint_f,"
-         "min_dewpoint_f,precip_in,avg_wind_speed_kts,avg_wind_drct,"
-         "min_rh,avg_rh,max_rh,climo_high_f,climo_low_f,climo_precip_in\n")
+    pgconn = get_dbconn('iem')
+    cursor = pgconn.cursor()
+    climate = get_climate(network, stations)
+    if climate is None:
+        return ""
+    res = ("station,day,max_temp_f,min_temp_f,max_dewpoint_f,"
+           "min_dewpoint_f,precip_in,avg_wind_speed_kts,avg_wind_drct,"
+           "min_rh,avg_rh,max_rh,climo_high_f,climo_low_f,climo_precip_in\n")
     if len(stations) == 1:
         stations.append('ZZZZZ')
     cursor.execute("""SELECT id, day, max_tmpf, min_tmpf, max_dwpf, min_dwpf,
@@ -59,14 +67,14 @@ def get_data(network, sts, ets, stations=[]):
         s.day >= %s and s.day < %s and t.network = %s and t.id in %s
         ORDER by day ASC""", (sts, ets, network, tuple(stations)))
     for row in cursor:
-        s += ("%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n"
-              ) % (row[0], row[1], row[2], row[3], row[4], row[5], row[6],
-                   row[7], row[8], row[9], row[10], row[11],
-                   climate[row[0]][row[1].strftime("%m%d")]['high'],
-                   climate[row[0]][row[1].strftime("%m%d")]['low'],
-                   climate[row[0]][row[1].strftime("%m%d")]['precip'])
+        res += ("%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n"
+                ) % (row[0], row[1], row[2], row[3], row[4], row[5], row[6],
+                     row[7], row[8], row[9], row[10], row[11],
+                     climate[row[0]][row[1].strftime("%m%d")]['high'],
+                     climate[row[0]][row[1].strftime("%m%d")]['low'],
+                     climate[row[0]][row[1].strftime("%m%d")]['precip'])
 
-    return s
+    return res
 
 
 def main():
@@ -81,13 +89,13 @@ def main():
 
     sys.stdout.write('Content-type: text/plain\n\n')
     stations = form.getlist('stations')
-    if len(stations) == 0:
+    if not stations:
         stations = form.getlist('station')
-    if len(stations) == 0:
+    if not stations:
         sys.stdout.write("ERROR: No stations specified for request")
         return
     network = form.getfirst('network')[:12]
-    sys.stdout.write(get_data(network, sts, ets, stations=stations))
+    sys.stdout.write(get_data(network, sts, ets, stations))
 
 
 if __name__ == '__main__':
