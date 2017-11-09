0c341af2c9204c283eca7a20b3e6550bf00f18e0
akrherz <akrherz@iastate.edu>
2017-11-09 13:17:07 -0600

    tweaks to offline noaaport dump script as per request

--- scripts/util/split_raw_noaaport.py
+++ scripts/util/split_raw_noaaport.py
@@ -3,35 +3,41 @@ from __future__ import print_function
 import os
 import datetime
 import subprocess
+
+import pytz
+import tqdm
 from pyiem.nws.product import TextProduct
 
 
 def main():
     """Go"""
-    os.chdir("/mesonet/tmp")
-    sts = datetime.datetime(2011, 7, 19)
-    ets = datetime.datetime(2011, 8, 3)
+    os.chdir("/mesonet/tmp/noaaport")
+    sts = datetime.datetime(2017, 10, 10)
+    sts = sts.replace(tzinfo=pytz.utc)
+    ets = datetime.datetime(2017, 11, 3)
+    ets = ets.replace(tzinfo=pytz.utc)
     interval = datetime.timedelta(days=1)
 
     now = sts
     while now < ets:
-        out = open('%s.data' % (now.strftime("%Y%m%d"),), 'w')
         subprocess.call(("tar -zxf /mesonet/ARCHIVE/raw/noaaport/%s/%s.tgz"
                          ) % (now.year, now.strftime("%Y%m%d")), shell=True)
-        for q in range(0, 24):
-            print("%s %s" % (now, q))
-            fn = "%s%02i.txt" % (now.strftime("%Y%m%d"), q)
+        out = open("%s.txt" % (now.strftime("%Y%m%d"), ), 'w')
+        for hour in tqdm.tqdm(range(0, 24), desc=now.strftime("%m%d")):
+            fn = "%s%02i.txt" % (now.strftime("%Y%m%d"), hour)
             if not os.path.isfile(fn):
                 print('Missing %s' % (fn,))
                 continue
-            o = open(fn).read()
-            prods = o.split("\003")
+            fp = open(fn).read()
+            prods = fp.split("\003")
             for prod in prods:
-                try:
-                    p = TextProduct(prod)
-                except Exception as exp:
+                if prod.find("RRSTAR") == -1:
                     continue
-                if p.afos is not None and p.afos[:3] in ['HML', ]:
+                try:
+                    tp = TextProduct(prod, utcnow=now)
+                except Exception as _exp:
+                    continue
+                if tp.afos == 'RRSTAR' and tp.source == 'KWOH':
                     out.write(prod + "\003")
             os.unlink(fn)
         out.close()
