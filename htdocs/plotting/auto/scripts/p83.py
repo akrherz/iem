959ebe01dc44e856f4d0cfde06630edc6cde1b01
akrherz <akrherz@iastate.edu>
2017-11-27 14:43:05 -0600

    cleanups to autoplot comparing periods

--- htdocs/plotting/auto/scripts/p83.py
+++ htdocs/plotting/auto/scripts/p83.py
@@ -1,9 +1,8 @@
 """period averages around a date"""
 import datetime
 
-import psycopg2.extras
 import numpy as np
-import pandas as pd
+from pandas.io.sql import read_sql
 from scipy import stats
 from pyiem import network
 from pyiem.util import get_autoplot_context, get_dbconn
@@ -11,8 +10,8 @@ from pyiem.util import get_autoplot_context, get_dbconn
 PDICT = {'high': 'Average High Temperature',
          'low': 'Average Low Temperature',
          'precip': 'Total Precipitation'}
-UNITS = {'high': '$^\circ$F',
-         'low': '$^\circ$F',
+UNITS = {'high': r'$^\circ$F',
+         'low': r'$^\circ$F',
          'precip': 'inch'}
 
 
@@ -20,9 +19,11 @@ def get_description():
     """ Return a dict describing how to call this plotter """
     desc = dict()
     desc['data'] = True
-    desc['description'] = """This chart presents simple yearly statistics for
-    a given day period around a given date.  For example, you can compare
-    the previous 45 days to the next 45 days around 15 July.
+    desc['description'] = """This plot compares a period of days prior to
+    a specified date to the same number of days after a date.  The specified
+    date is not used in either statistical value.  If you select a period that
+    includes leap day, there is likely some small ambiguity with the resulting
+    plot labels.
     """
     desc['arguments'] = [
         dict(type='station', name='station', default='IA2203',
@@ -35,6 +36,8 @@ def get_description():
              label='Select Month:'),
         dict(type='day', name='day', default='15',
              label='Select Day:'),
+        dict(type='year', name='year', default=datetime.date.today().year,
+             label='Year to Highlight in Chart'),
 
     ]
     return desc
@@ -45,8 +48,7 @@ def plotter(fdict):
     import matplotlib
     matplotlib.use('agg')
     import matplotlib.pyplot as plt
-    pgconn = get_dbconn('coop')
-    ccursor = pgconn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    pgconn = get_dbconn('coop', user='nobody')
     ctx = get_autoplot_context(fdict, get_description())
 
     station = ctx['station']
@@ -55,13 +57,20 @@ def plotter(fdict):
     day = ctx['day']
     dt = datetime.date(2000, month, day)
     days = ctx['days']
+    year = ctx['year']
+    ctx['before_period'] = "%s-%s" % (
+        (dt - datetime.timedelta(days=days)).strftime("%-d %b"),
+        (dt - datetime.timedelta(days=1)).strftime("%-d %b"))
+    ctx['after_period'] = "%s-%s" % (
+        (dt + datetime.timedelta(days=1)).strftime("%-d %b"),
+        (dt + datetime.timedelta(days=days)).strftime("%-d %b"))
 
     table = "alldata_%s" % (station[:2],)
     nt = network.Table("%sCLIMATE" % (station[:2],))
 
-    ccursor.execute("""
+    df = read_sql("""
     with data as (
-        SELECT day,
+        SELECT day, year,
         count(*) OVER
             (ORDER by day ASC ROWS BETWEEN %s PRECEDING AND 1 PRECEDING) as cb,
         avg(high) OVER
@@ -74,27 +83,29 @@ def plotter(fdict):
             (ORDER by day ASC ROWS BETWEEN 1 FOLLOWING AND %s FOLLOWING) as ca,
         avg(high) OVER
             (ORDER by day ASC ROWS BETWEEN 1 FOLLOWING AND %s FOLLOWING) as ha,
-        avg(low) OVER
+        avg(low)OVER
             (ORDER by day ASC ROWS BETWEEN 1 FOLLOWING AND %s FOLLOWING) as la,
         sum(precip) OVER
             (ORDER by day ASC ROWS BETWEEN 1 FOLLOWING AND %s FOLLOWING) as pa
         from """+table+""" WHERE station = %s)
 
-    SELECT hb, lb, pb, ha, la, pa from data where cb = ca and
+    SELECT year, hb as high_before, lb as low_before, pb as precip_before,
+    ha as high_after, la as low_after, pa as precip_after from
+    data where cb = ca and
     cb = %s and extract(month from day) = %s and extract(day from day) = %s
-    """, (days, days, days, days, days, days, days, days, station,
-          days, month, day))
-    rows = []
-    for row in ccursor:
-        rows.append(dict(high_before=row[0], high_after=row[3],
-                         low_before=row[1], low_after=row[4],
-                         precip_before=row[2], precip_after=row[5]))
-    df = pd.DataFrame(rows)
+    """, pgconn, params=(days, days, days, days, days, days, days, days,
+                         station, days, month, day), index_col='year')
 
-    x = np.array(df[varname+'_before'], 'f')
-    y = np.array(df[varname+'_after'], 'f')
+    xvals = df[varname+'_before'].values
+    yvals = df[varname+'_after'].values
     fig, ax = plt.subplots(1, 1, sharex=True, figsize=(8, 6))
-    ax.scatter(x, y)
+    ax.scatter(xvals, yvals, zorder=2)
+    if year in df.index:
+        row = df.loc[year]
+        ax.scatter(row[varname+'_before'], row[varname+'_after'], color='r',
+                   zorder=3)
+        ax.text(row[varname+'_before'], row[varname+'_after'],
+                "%s" % (year, ), ha='right', va='bottom', color='r')
     msg = ("[%s] %s %s over %s days prior to and after %s"
            ) % (station, nt.sts[station]['name'], PDICT.get(varname),
                 days, dt.strftime("%-d %B"))
@@ -102,52 +113,50 @@ def plotter(fdict):
     sz = len(tokens) / 2
     ax.set_title(" ".join(tokens[:sz]) + "\n" + " ".join(tokens[sz:]))
 
-    minv = min([min(x), min(y)])
-    maxv = max([max(x), max(y)])
+    minv = min([min(xvals), min(yvals)])
+    maxv = max([max(xvals), max(yvals)])
     ax.plot([minv-5, maxv+5], [minv-5, maxv+5], label='x=y', color='b')
-    yavg = np.average(y)
-    xavg = np.average(x)
+    yavg = np.average(yvals)
+    xavg = np.average(xvals)
     ax.axhline(yavg, label='After Avg: %.2f' % (yavg,),
                color='r', lw=2)
     ax.axvline(xavg, label='Before Avg: %.2f' % (xavg,),
                color='g', lw=2)
-    df2 = df[np.logical_and(df[varname+'_before'] >= xavg,
-                            df[varname+'_after'] >= yavg)]
-    ax.text(0.98, 0.98, "I: %.1f%%" % (len(df2) / float(len(x)) * 100.,),
+    df2 = df[np.logical_and(xvals >= xavg, yvals >= yavg)]
+    ax.text(0.98, 0.98, "I: %.1f%%" % (len(df2) / float(len(xvals)) * 100.,),
             transform=ax.transAxes, bbox=dict(edgecolor='tan',
                                               facecolor='white'),
             va='top', ha='right', zorder=3)
 
-    df2 = df[np.logical_and(df[varname+'_before'] < xavg,
-                            df[varname+'_after'] < yavg)]
-    ax.text(0.02, 0.02, "III: %.1f%%" % (len(df2) / float(len(x)) * 100.,),
+    df2 = df[np.logical_and(xvals < xavg, yvals < yavg)]
+    ax.text(0.02, 0.02, "III: %.1f%%" % (len(df2) / float(len(xvals)) * 100.,),
             transform=ax.transAxes, bbox=dict(edgecolor='tan',
                                               facecolor='white'),
             zorder=3)
 
-    df2 = df[np.logical_and(df[varname+'_before'] >= xavg,
-                            df[varname+'_after'] < yavg)]
-    ax.text(0.98, 0.02, "IV: %.1f%%" % (len(df2) / float(len(x)) * 100.,),
+    df2 = df[np.logical_and(xvals >= xavg, yvals < yavg)]
+    ax.text(0.98, 0.02, "IV: %.1f%%" % (len(df2) / float(len(xvals)) * 100.,),
             transform=ax.transAxes, bbox=dict(edgecolor='tan',
                                               facecolor='white'),
             va='bottom', ha='right', zorder=3)
 
-    df2 = df[np.logical_and(df[varname+'_before'] < xavg,
-                            df[varname+'_after'] >= yavg)]
-    ax.text(0.02, 0.98, "II: %.1f%%" % (len(df2) / float(len(x)) * 100.,),
+    df2 = df[np.logical_and(xvals < xavg, yvals >= yavg)]
+    ax.text(0.02, 0.98, "II: %.1f%%" % (len(df2) / float(len(xvals)) * 100.,),
             transform=ax.transAxes, bbox=dict(edgecolor='tan',
                                               facecolor='white'),
             va='top', zorder=3)
 
-    ax.set_xlabel("%s %s, %s Days Before" % (PDICT.get(varname),
-                                             UNITS.get(varname), days))
-    ax.set_ylabel("%s %s, %s Days After" % (PDICT.get(varname),
-                                            UNITS.get(varname), days))
+    ax.set_xlabel("%s %s, Period: %s" % (PDICT.get(varname),
+                                         UNITS.get(varname),
+                                         ctx['before_period']))
+    ax.set_ylabel("%s %s, Period: %s" % (PDICT.get(varname),
+                                         UNITS.get(varname),
+                                         ctx['after_period']))
     ax.grid(True)
-    ax.set_xlim(minv-2, maxv+2)
-    ax.set_ylim(minv-2, maxv+2)
+    ax.set_xlim(minv - 5, maxv + 5)
+    ax.set_ylim(minv - 5, maxv + 5)
 
-    _, _, r_value, _, _ = stats.linregress(x, y)
+    _, _, r_value, _, _ = stats.linregress(xvals, yvals)
     ax.text(0.65, 0.02, "R$^2$=%.2f bias=%.2f" % (r_value**2, yavg - xavg),
             ha='right', transform=ax.transAxes, bbox=dict(color='white'))
 
