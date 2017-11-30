f948a7a57b09a93a184ceca57134b7b2ba29d06f
akrherz <akrherz@iastate.edu>
2017-11-30 08:47:32 -0600

    defensive programming to catch errors with ingest

--- scripts/model/motherlode_ingest.py
+++ scripts/model/motherlode_ingest.py
@@ -14,8 +14,6 @@ from pyiem.network import Table as NetworkTable
 from pyiem.util import get_dbconn
 
 NT = NetworkTable(['AWOS', 'IA_ASOS'])
-MOS = get_dbconn('mos')
-mcursor = MOS.cursor()
 
 BASE_URL = "http://thredds.ucar.edu/thredds/ncss/grib/NCEP/"
 URLS = {'NAM': ("NAM/CONUS_12km/conduit/"
@@ -47,7 +45,17 @@ VLOOKUP = {
            }
 
 
-def run(model, station, lon, lat, ts):
+def xref(row, varname, model):
+    """Safer lookup"""
+    rowkey = VLOOKUP[varname][model]
+    if rowkey not in row:
+        print(("motherload_ingest failed to find %s in %s\nrow:%s"
+               ) % (varname, model, row))
+        sys.exit()
+    return row[rowkey]
+
+
+def run(mcursor, model, station, lon, lat, ts):
     """
     Ingest the model data for a given Model, stationid and timestamp
     """
@@ -81,21 +89,21 @@ def run(model, station, lon, lat, ts):
                         mcursor.rowcount, ts, station, model))
 
     count = 0
-    r = csv.DictReader(fp)
-    for row in r:
+    csvfp = csv.DictReader(fp)
+    for row in csvfp:
         for k in row.keys():
             row[k[:k.find("[")]] = row[k]
-        sbcape = row[VLOOKUP['sbcape'][model]]
-        sbcin = row[VLOOKUP['sbcin'][model]]
-        pwater = row[VLOOKUP['pwater'][model]]
-        precipcon = row[VLOOKUP['precipcon'][model]]
-        precip = row[VLOOKUP['precip'][model]]
+        sbcape = xref(row, 'sbcape', model)
+        sbcin = xref(row, 'sbcin', model)
+        pwater = xref(row, 'pwater', model)
+        precipcon = xref(row, 'precipcon', model)
+        precip = xref(row, 'precip', model)
         if precip < 0:
             precip = None
         if precipcon < 0:
             precipcon = None
         fts = datetime.datetime.strptime(row['time'], '%Y-%m-%dT%H:%M:%SZ')
-        fts = fts.replace(tzinfo=pytz.timezone("UTC"))
+        fts = fts.replace(tzinfo=pytz.utc)
         sql = """INSERT into """ + table + """ (station, model, runtime,
               ftime, sbcape, sbcin, pwater, precipcon, precip)
               VALUES (%s, %s , %s,
@@ -107,29 +115,32 @@ def run(model, station, lon, lat, ts):
     return count
 
 
-def run_model(model, ts):
+def run_model(mcursor, model, runtime):
     """Actually do a model and timestamp"""
     for sid in NT.sts:
-        cnt = run(model, "K"+sid, NT.sts[sid]['lon'], NT.sts[sid]['lat'], ts)
+        cnt = run(mcursor, model, "K"+sid,
+                  NT.sts[sid]['lon'], NT.sts[sid]['lat'], runtime)
         if cnt == 0:
-            print('No data K%s %s %s' % (sid, ts, model))
+            print('No data K%s %s %s' % (sid, runtime, model))
 
 
-def check_and_run(model, ts):
+def check_and_run(mcursor, model, runtime):
     ''' Check the database for missing data '''
-    table = "model_gridpoint_%s" % (ts.year,)
+    table = "model_gridpoint_%s" % (runtime.year,)
     mcursor.execute("""
         SELECT * from """+table+""" WHERE
         runtime = %s and model = %s
-    """, (ts, model))
+    """, (runtime, model))
     if mcursor.rowcount < 10:
         print(('Rerunning %s [runtime=%s] due to rowcount %s'
-               ) % (model, ts, mcursor.rowcount))
-        run_model(model, ts)
+               ) % (model, runtime, mcursor.rowcount))
+        run_model(mcursor, model, runtime)
 
 
 def main(argv):
     """Do Something"""
+    pgconn = get_dbconn('mos')
+    mcursor = pgconn.cursor()
     gts = datetime.datetime.utcnow()
     if len(argv) == 5:
         gts = datetime.datetime(int(argv[1]), int(argv[2]),
@@ -140,17 +151,17 @@ def main(argv):
     if gts.hour % 6 == 0:
         ts = gts - datetime.timedelta(hours=6)
         for model in ['GFS', 'NAM']:
-            run_model(model, ts)
-            check_and_run(model, ts - datetime.timedelta(days=7))
+            run_model(mcursor, model, ts)
+            check_and_run(mcursor, model, ts - datetime.timedelta(days=7))
 
     ts = gts - datetime.timedelta(hours=2)
-    run_model("RAP", ts)
-    check_and_run('RAP', ts - datetime.timedelta(days=7))
+    run_model(mcursor, "RAP", ts)
+    check_and_run(mcursor, 'RAP', ts - datetime.timedelta(days=7))
+    mcursor.close()
+    pgconn.commit()
+    pgconn.close()
 
 
 if __name__ == '__main__':
     # Go Go gadget
     main(sys.argv)
-    mcursor.close()
-    MOS.commit()
-    MOS.close()
