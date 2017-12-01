4bc4b1942f0199b8ee5ed03e72013dfdc8c9a157
akrherz <akrherz@iastate.edu>
2017-12-01 12:04:08 -0600

    hfmetar storage of presentwx should be comma delimited

--- scripts/ingestors/madis/extract_hfmetar.py
+++ scripts/ingestors/madis/extract_hfmetar.py
@@ -126,7 +126,8 @@ def process(ncfn):
 
         presentwx = tostring(data['presWx'][i])
         if presentwx != '':
-            iem.data['presentwx'] = presentwx
+            # database storage is comma delimited
+            iem.data['presentwx'] = presentwx.replace(" ", ",")[:24]
             mtr += "%s " % (presentwx,)
 
         for _i, (_c, _l) in enumerate(
