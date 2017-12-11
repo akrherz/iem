df246d2f2c6f56d136233ad9822cc3ca621c6a3b
akrherz <akrherz@iastate.edu>
2017-12-11 15:05:34 -0600

    better code logic for first/last snowfall autoplot
    https://mesonet.agron.iastate.edu/plotting/auto/?q=64

--- htdocs/plotting/auto/scripts/p64.py
+++ htdocs/plotting/auto/scripts/p64.py
@@ -7,6 +7,7 @@ import numpy as np
 import pandas as pd
 from pyiem.network import Table as NetworkTable
 from pyiem.util import get_autoplot_context, get_dbconn
+from pyiem.reference import TRACE_VALUE
 
 PDICT = {'first': 'First Snowfall after 1 July',
          'last': 'Last Snowfall before 1 July'}
@@ -23,7 +24,7 @@ def get_description():
     desc['arguments'] = [
         dict(type='station', name='station', default='IA2203',
              label='Select Station:', network='IACLIMATE'),
-        dict(type='text', name='threshold', default='1',
+        dict(type='float', name='threshold', default='1',
              label='First Snowfall Threshold (T for trace)'),
         dict(type="select", name='dir', default='last', options=PDICT,
              label='Which Variable to Plot?'),
@@ -31,26 +32,21 @@ def get_description():
     return desc
 
 
-def plotter(fdict):
-    """ Go """
-    import matplotlib
-    matplotlib.use('agg')
-    import matplotlib.pyplot as plt
-    pgconn = get_dbconn('coop')
+def get_data(ctx):
+    """Get some data please"""
+    pgconn = get_dbconn('coop', user='nobody')
     cursor = pgconn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    ctx = get_autoplot_context(fdict, get_description())
     station = ctx['station']
-    mydir = ctx['dir']
     threshold = ctx['threshold']
-    threshold = 0.0001 if threshold == 'T' else float(threshold)
+    threshold = TRACE_VALUE if threshold == 'T' else float(threshold)
 
     table = "alldata_%s" % (station[:2],)
-    nt = NetworkTable("%sCLIMATE" % (station[:2],))
-    syear = max(1893, nt.sts[station]['archive_begin'].year)
+    ctx['nt'] = NetworkTable("%sCLIMATE" % (station[:2],))
+    syear = max(1893, ctx['nt'].sts[station]['archive_begin'].year)
     eyear = datetime.datetime.now().year
 
     snow = np.zeros((eyear - syear + 1, 366))
-    snowd = np.ones((eyear - syear + 1, 366)) * 99
+    snowd = np.zeros((eyear - syear + 1, 366))
     cursor.execute("""
         SELECT extract(doy from day), year, snow, snowd from """ + table + """
         where station = %s and year >= %s
@@ -58,37 +54,50 @@ def plotter(fdict):
     for row in cursor:
         snow[row[1] - syear, int(row[0] - 1)] = row[2]
         snowd[row[1] - syear, int(row[0] - 1)] = row[3]
+    # reset any nan
+    snow = np.where(np.isnan(snow), 0, snow)
+    snowd = np.where(np.isnan(snowd), 0, snowd)
 
     rows = []
     for i, year in enumerate(range(syear, eyear)):
-        s = tuple(snow[i, 183:]) + tuple(snow[i+1, :183])
-        sd = tuple(snowd[i, 183:]) + tuple(snowd[i+1, :183])
-        idx = np.where(np.array(s) >= threshold, 1, 0)
-        if np.max(idx) == 0:
+        # Slap the years together so we cross over 1 Jan
+        snowfall = tuple(snow[i, 183:]) + tuple(snow[i+1, :183])
+        snowdepth = tuple(snowd[i, 183:]) + tuple(snowd[i+1, :183])
+        hits = np.where(np.array(snowfall) >= threshold)[0]
+        if hits.size == 0:
             continue
-        if mydir == 'first':
-            idx = tuple(idx).index(1)
-        else:
-            idx = 365 - tuple(idx[::-1]).index(1)
+        idx = hits[0] if ctx['dir'] == 'first' else hits[-1]
+        # Compute how long this stuck around
+        sfree = idx + 1
         for sfree in range(idx+1, 366):
-            if sd[sfree] <= 0.01:
+            if snowdepth[sfree] <= 0.01:
                 break
-        cnt = sfree - idx
-        if cnt >= 30:
+        days = sfree - idx
+        if days >= 30:
             color = 'purple'
-        elif cnt >= 11:
+        elif days >= 11:
             color = 'g'
-        elif cnt >= 3:
+        elif days >= 3:
             color = 'b'
         else:
             color = 'r'
-        # print year, idx, s[idx], sd[idx:sfree]
+        # print("year: %s idx: %s days: %s %s" % (year, idx, days, snowdepth))
         dt = datetime.date(year, 1, 1) + datetime.timedelta(days=(idx+183))
         rows.append(dict(year=year, snow_date=dt, snow_doy=(idx+183),
-                         color=color, days=cnt,
-                         snowfree_doy=(sfree+183), snowfall=s[idx]))
+                         color=color, days=days,
+                         snowfree_doy=(sfree+183),
+                         snowfall=snowfall[idx]))
 
-    df = pd.DataFrame(rows)
+    return pd.DataFrame(rows)
+
+
+def plotter(fdict):
+    """ Go """
+    import matplotlib
+    matplotlib.use('agg')
+    import matplotlib.pyplot as plt
+    ctx = get_autoplot_context(fdict, get_description())
+    df = get_data(ctx)
 
     (fig, ax) = plt.subplots(1, 1, figsize=(8, 6))
 
@@ -107,18 +116,17 @@ def plotter(fdict):
     ax.grid(True)
     ax.set_ylim(bottom=0)
     ax2 = ax.twinx()
-    p = np.percentile(df['snow_doy'].values, np.arange(100))
-    ax2.plot(p, np.arange(100), lw=2, color='k')
-    ax2.set_ylabel(("Frequency of %s Date (CDF) [%%]"
-                    ) % ('Start' if mydir == 'first' else 'Last', ))
+    ptile = np.percentile(df['snow_doy'].values, np.arange(100))
+    ax2.plot(ptile, np.arange(100), lw=2, color='k')
+    ax2.set_ylabel(("Frequency of %s Date (CDF) [%%] (black line)"
+                    ) % ('Start' if ctx['dir'] == 'first' else 'Last', ))
     ax.set_ylabel('Snowfall [inch], Avg: %.1f inch' % (df['snowfall'].mean(),))
-    ax.set_title(('[%s] %s %s %s Snowfall\n'
-                  '(color is how long snow remained)'
-                  '') % (
-        station, nt.sts[station]['name'],
-        'Last' if mydir == 'last' else 'First',
-        'Trace+' if threshold < 0.1 else "%.2f+ Inch" % (threshold,))
-                 )
+    ax.set_title(
+        ('[%s] %s %s %s Snowfall\n(color is how long snow remained)'
+         ) % (ctx['station'], ctx['nt'].sts[ctx['station']]['name'],
+              'Last' if ctx['dir'] == 'last' else 'First',
+              ('Trace+' if ctx['threshold'] < 0.1
+               else "%.2f+ Inch" % (ctx['threshold'],))))
     p0 = plt.Rectangle((0, 0), 1, 1, fc="purple")
     p1 = plt.Rectangle((0, 0), 1, 1, fc="g")
     p2 = plt.Rectangle((0, 0), 1, 1, fc="b")
@@ -136,10 +144,12 @@ def plotter(fdict):
                      box.height * 0.9])
     ax2.set_position([box.x0, box.y0 + box.height * 0.1, box.width,
                      box.height * 0.9])
+    ax2.set_yticks([0, 5, 10, 25, 50, 75, 90, 95, 100])
+    ax2.set_ylim(0, 101)
     df.set_index('year', inplace=True)
     del df['color']
     return fig, df
 
 
 if __name__ == '__main__':
-    plotter(dict(dir='last'))
+    plotter(dict(dir='first', station='IA5230', network='IACLIMATE'))
