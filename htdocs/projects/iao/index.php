0069467dc02e442feb0c6f8054c9fc573d1d408e
akrherz <akrherz@iastate.edu>
2018-03-08 10:55:52 -0600

    update IAO website

--- htdocs/projects/iao/index.php
+++ htdocs/projects/iao/index.php
@@ -15,9 +15,14 @@ div.hangs p {
 EOM;
 $t->content = <<<EOF
 
+<div class="row">
+<div class="col-md-9">
+
 <h3>Iowa Atmospheric Observatory</h3>
 
+<section id="overview">
 <h3>Overview</h3>
+</section>
 
 <p>The Iowa Atmospheric Observatory (IAO) was designed and constructed by 
 Iowa State University under funding from the National Science Foundation 
@@ -52,7 +57,9 @@ crushed-rock area in the center of the image that is
 connected by a crushed-rock private road to a grid of public roads.  
 The east-west roads at the top and bottom of the image are 1 mile apart.</p>
 
+<section id="locations">
 <h3>Tower Locations</h3>
+</section>
 
 <p>Story County tower (A1)
 <br />E911: 67527 110th St, Zearing, IA 
@@ -70,7 +77,9 @@ Photo source:  Iowa NAIP 2017 Orthophotos.  USDA-FSA-APFO Aerial Photography Fie
 </div>
 </div>
 
+<section id="instruments">
 <h3>Instruments</h3>
+</section>
 
 <div class="row">
 <div class="col-md-6">
@@ -229,7 +238,9 @@ from June 2016-May 2017. Image courtesy of: D. Rajewski
 at each tower site: A1 within the wind farm (a) and A2 outside of the wind
 farm (b).  Images courtesy of S. Purdy.</p>
 
+<section id="topography">
 <h3>Topography and Tower Mast Influences on IAO Measurements</h3>
+</section>
 
 <p>Terrain for both sites is flat with about 13-m higher elevation at A2 vs. 
 A1 (356.6 m vs. 343.8 m).  Both sites feature slopes radially away from of 
@@ -271,8 +282,9 @@ boom.   Tower
 boom layout diagram denotes usable wind directions from No Wake sector. 
 Image courtesy of: D. Rajewski</p>
 
-
+<section id="data">
 <h3>Tower Data</h3>
+</section>
 
 <p>Two continuous data streams from the towers operate at different sampling 
 rates.  Temperature, relative humidity, horizontal wind speed, wind 
@@ -282,7 +294,6 @@ direction, and pressure are reported and archived a 1-Hz, while
 1-Hz data began in June 2016.  Data were recorded episodically from the 
 sonic anemometers starting November 2016.</p>
 
-
 <p>One-minute average plots from select data streams for last 36 hours are available at:
 <br /><a href="http://talltowers.agron.iastate.edu/plots/">http://talltowers.agron.iastate.edu/plots/</a>
 </p>
@@ -290,7 +301,9 @@ sonic anemometers starting November 2016.</p>
 <p>A simple plotting tool is available to display and download data:
 <br /><a href="/plotting/auto/?_wait=no&q=158&station=ETTI4">here</a></p>
 
+<section id="cwex">
 <h3>Crop/Wind-entry Experiment (CWEX)</h3>
+</section>
 
 <p>A series of field experiments was conducted in advance of construction of 
 the tall towers from 2009-2013 in the vicinity of the tower locations taking 
@@ -299,12 +312,16 @@ More details of the field sites, observing facilities, data archives and
 scientific results can be found in the publications and presentations 
 listed below.</p>
 
+<section id="contact">
 <h3>Contact Information</h3>
+</section>
 
 <p>For information on the IAO facilities contact E. S. Takle (
 <a href="mailto:gstakle@iastate.edu">gstakle@iastate.edu</a>)</p>
 
+<section id="ack">
 <h3>Acknowledgements</h3>
+</section>
 
 <p>The IAO towers and associated instrumentation were funded by an 
 NSF/EPSCoR grant to the state of Iowa (Grant #1101284) and a 
@@ -331,8 +348,9 @@ Crop/Wind-energy Experiments (CWEX-10, CWEX-11, CWEX-12, and CWEX-13)
 that laid the scientific basis for the concept of the IAO.
 </p>
 
-
+<section id="publications">
 <h3>Publications from the Crop/Wind-energy Experiments and the Iowa Atmospheric Observatory</h3>
+</section>
 
 <div class="hangs">
 <p>Takle, E. S., 2017: Climate. In M. Perrow, ed., 2017:  
@@ -380,31 +398,63 @@ boundary-layer and mesoscale interactions with a wind farm.
 Bull. Amer. Meteor. Soc., 94, 655-672.  <a href="http://journals.ametsoc.org/doi/abs/10.1175/BAMS-D-11-00240.1">doi: 10.1175/BAMS-D-11-00240</a></p>
 </div>
 
+<section id="presentations">
 <h3>Presentations from the Crop/Wind-energy Experiments and the Iowa Atmospheric Observatory</h3>
+</section>
 
 <div class="hangs">
-<p>Takle, E. S., R. A. Rajewski, S. L. Purdy, J. Sun, and S. Zilitinkevich, 2017:  Exchanges to surface and boundary layer reconsidered:  Introducing FaNTASTIC-1.  International Conference on Future Technologies for Wind Energy.  Boulder CO, October 2017.
-<p>Rajewski, D. A., E. S. Takle, and S. L. Purdy, 2017:  Tall tower measurements of wake loss characteristics within a low-density wind farm.  International Conference on Future Technologies for Wind Energy.  Boulder CO, October 2017.
-<p>Takle, Eugene S., Daryl Herzmann, and Dan Rajewski, 2016:  Wind-farm power production diagnostic tools with applications to wake steering.  Invited presentation.  National Renewable Energy Laboratory (DOE), Boulder CO
-<p>Takle, Eugene S., Daryl Herzmann, and Dan Rajewski, 2016:  Power-production diagnostic tools for low-density wind farms with applications to wake steering. Amer. Geophys Union Fall Meeting.  San Francisco.   December
-<p>Takle, E. S., 2017:  Wind Energy:  An outdoor laboratory has been established to collect reliable wind speed and turbulence data for use with computational models of wind turbine systems.  Poster displayed at the EPSCoR Legislator Breakfast at the Iowa Statehouse.
-<p>Takle, E. S., D. A. Rajewski, and S. L. Irvin, 2017:  Identical twin towers for studies of natural turbuence and wind farm boundary layers.  8th Conf. on Wea., Climate, Water and the New Energy Economy, 23 Jan., Amer. Meteor. Soc., Seattle, WA.
-<p>Rajewski, Daniel A., Eugene S. Takle, Julie K. Lundquist, Samantha L. Irvin, and Russell K. Doorenbos, 2015:  Spatial characteristics of power variability from a large wind farm in Iowa during the 2013 Crop/Wind Energy Experiment (CWEX-13).  Sixth Conference on Weather, Climate, and the New Energy Economy. American Meteorological Society. Phoenix, AZ
-<p>Lundquist, Julie K., E. S. Takle, M. Boquet, B. Kosovic, M. E. Rhodes, D. A. Rajewski, R. K. Doorenbos, S. Irvin, M. Aitken, K. Friedrich, P. T. Quelet, J. Rana, C. St. Martin, B. J. Vanderwende, and R. Worsnop, 2014:  Lidar observations of the variation of wind turbine wakes with inflow conditions in an onshore wind farm.  21st Symposium on Boundary Layers and Turbulence.  American Meteorological Society.  Leeds, UK.
-<p>Lundquist, J. K., E.S. Takle, M. Boquet, B. Kosovic, M.E. Rhodes, D. Rajewski, R. Doorenboos, S. Irvin, M.L. Aitken, K. Friedrich, P.T. Quelet, J. Rana, C. St. Martin, B. Vanderwende, W. Rochelle, Lidar observations of interacting wind turbine wakes in an onshore wind farm, European Wind Energy Association 2014. (2014) 33–36. 
-<p>Selvaraj, S.,  A. Chaves, E. Takle and A. Sharma, “Numerical Prediction of Surface Flow Convergence Phenomenon in Windfarms” in Proc. of the Conference on Wind Energy Science and Technology, RUZGEM, Ankara, Turkey, Oct 3-4, 2013.
-<p>Rajewski, D. A., E. S. Takle, J. H. Prueger, S. Oncley, J. K. Lundquist, T. W. Horst, M. E. Rhodes, R. L. Pfeiffer, J. L. Hatfield, K. K. Spoth, and R. Doorenbos, 2013:  Wind Turbine Wake Investigation from Surface Measurements during the 2010 and 2011 Crop Wind-Energy EXperiments (CWEX-10/11).  Fourth Conference on Weather, Climate, and the New Energy Economy.  American Meteorological Society.  Austin, TX.  
-<p>Takle, E. S., D. A. Rajewski, J. Prueger, Steven Oncley, J. K. Lundquist, Thomas W. Horst, Michael E. Rhodes, Richard Pfeiffer, Jerry L. Hatfield, Kristopher K. Spoth, Russell K. Doorenbos, 2013:  CWEX-10/11:  Overview of Results From the First Two Crop/Wind-Energy Experiments.   Fourth Conference on Weather, Climate, and the New Energy Economy.  American Meteorological Society.  Austin, TX.  
-<p>Takle, E. S., D. A. Rajewski, J. Prueger, Steven Oncley, J. K. Lundquist, Thomas W. Horst, Michael E. Rhodes, Richard Pfeiffer, Jerry L. Hatfield, Kristopher K. Spoth, Russell K. Doorenbos, 2012:  CWEX-10/11:  Overview of Results From the First Two Crop/Wind-Energy Experiments.  American Geophysical Union Fall Meeting, San Francisco.
-<p>Rajewski, D. A.,  E. S. Takle, J. H. Prueger, S.P. Oncley, T. W. Horst, R. Pfeiffer, J. L. Hatfield, K. K. Spoth, R.K. Doorenbos, 2012:  Evaluation of surface energy and carbon fluxes within a large wind farm during the CWEX-10/11 Crop Wind-Energy eXperiments.  American Geophysical Union Fall Meeting, San Francisco.
-<p>Rajewski, D. R., E. S. Takle, T. W. Horst, S. P. Oncley, J.K. Lundquist, M. E. Rhodes, and K. K. Spoth, 2012:  Crop-Wind-Energy Experiment 2011 (CWEX11).  16th Symposium on Meteorological Observation and Instrumentation.   Amer. Meteorol. Soc. 23-26 Janurary. New Orleans.  
-<p>Takle, E. S., D. A. Rajewski, J. H. Prueger, J. K. Lundquist, M. Aitken, M. E. Rhodes, A. J. Deppe, F. E. Goodman, K. C. Carter, L. Mattison, S. L. Rabideau, A. J. Rosenberg, C. L. Whitfield, and J. Hatfield, 2010:  Wind and flux measurements in a windfarm co-located with agricultural production. Presented at the American Geophysical Union Fall Meeting, San Francisco.  (Invited presentation)
-<p>Rhodes, M. E., , M. L. Aitken, J. K. Lundquist, E. S. Takle, and J. Prueger, 2010:  Effect of wind turbine wakes on cropland surface fluxes in the US Great Plains during a Nocturnal Low Level Jet.  Presented at the American Geophysical Union Fall Meeting, San Francisco.
-<p>Takle, E. S., D. A. Rajewski, M. Segal, R. Elmore, J. Hatfield, J. H. Prueger, and S. E. Taylor, 2009:  Interaction of turbine-generated turbulence with agricultural crops:  conceptual framework and prelimnary results.  Presentation at American Geophysical Union Fall Meeting, San Francisco.
+<p>18. Takle, E. S., R. A. Rajewski, S. L. Purdy, J. Sun, and S. Zilitinkevich, 
+2017:  Exchanges to surface and boundary layer reconsidered:  Introducing 
+FaNTASTIC-1.  International Conference on Future Technologies for 
+Wind Energy.  Boulder CO, October 2017. [<a href="http://windtechconferences.org/wp-content/uploads/2018/01/Takle-Gene-WindTech1017-FaNTASTIC-Takle-04.pdf">Available PDF</a>]</p>
+
+<p>17. Rajewski, D. A., E. S. Takle, and S. L. Purdy, 2017:  Tall tower 
+measurements of wake loss characteristics within a low-density wind farm.  
+International Conference on Future Technologies for Wind Energy.  Boulder CO, 
+October 2017. [<a href="http://windtechconferences.org/wp-content/uploads/2018/01/Rajewski_D_TallTowerMeasurements_PPT.pdf">Available PDF</a>]</p>
+
+<p>16. Takle, Eugene S., Daryl Herzmann, and Dan Rajewski, 2016:  Wind-farm 
+power production diagnostic tools with applications to wake steering.  
+Invited presentation.  National Renewable Energy Laboratory (DOE), 
+Boulder CO. [<a href="/pickup/iao/nrel_wake_tools.pdf">Available PDF</a>]</p>
+
+<p>15. Takle, Eugene S., Daryl Herzmann, and Dan Rajewski, 2016:  Power-production diagnostic tools for low-density wind farms with applications to wake steering. Amer. Geophys Union Fall Meeting.  San Francisco.   December
+<p>14. Takle, E. S., 2017:  Wind Energy:  An outdoor laboratory has been established to collect reliable wind speed and turbulence data for use with computational models of wind turbine systems.  Poster displayed at the EPSCoR Legislator Breakfast at the Iowa Statehouse.
+<p>13. Takle, E. S., D. A. Rajewski, and S. L. Irvin, 2017:  Identical twin towers for studies of natural turbuence and wind farm boundary layers.  8th Conf. on Wea., Climate, Water and the New Energy Economy, 23 Jan., Amer. Meteor. Soc., Seattle, WA.
+<p>12. Rajewski, Daniel A., Eugene S. Takle, Julie K. Lundquist, Samantha L. Irvin, and Russell K. Doorenbos, 2015:  Spatial characteristics of power variability from a large wind farm in Iowa during the 2013 Crop/Wind Energy Experiment (CWEX-13).  Sixth Conference on Weather, Climate, and the New Energy Economy. American Meteorological Society. Phoenix, AZ
+<p>11. Lundquist, Julie K., E. S. Takle, M. Boquet, B. Kosovic, M. E. Rhodes, D. A. Rajewski, R. K. Doorenbos, S. Irvin, M. Aitken, K. Friedrich, P. T. Quelet, J. Rana, C. St. Martin, B. J. Vanderwende, and R. Worsnop, 2014:  Lidar observations of the variation of wind turbine wakes with inflow conditions in an onshore wind farm.  21st Symposium on Boundary Layers and Turbulence.  American Meteorological Society.  Leeds, UK.
+<p>10. Lundquist, J. K., E.S. Takle, M. Boquet, B. Kosovic, M.E. Rhodes, D. Rajewski, R. Doorenboos, S. Irvin, M.L. Aitken, K. Friedrich, P.T. Quelet, J. Rana, C. St. Martin, B. Vanderwende, W. Rochelle, Lidar observations of interacting wind turbine wakes in an onshore wind farm, European Wind Energy Association 2014. (2014) 33–36. 
+<p>9. Selvaraj, S.,  A. Chaves, E. Takle and A. Sharma, “Numerical Prediction of Surface Flow Convergence Phenomenon in Windfarms” in Proc. of the Conference on Wind Energy Science and Technology, RUZGEM, Ankara, Turkey, Oct 3-4, 2013.
+<p>8. Rajewski, D. A., E. S. Takle, J. H. Prueger, S. Oncley, J. K. Lundquist, T. W. Horst, M. E. Rhodes, R. L. Pfeiffer, J. L. Hatfield, K. K. Spoth, and R. Doorenbos, 2013:  Wind Turbine Wake Investigation from Surface Measurements during the 2010 and 2011 Crop Wind-Energy EXperiments (CWEX-10/11).  Fourth Conference on Weather, Climate, and the New Energy Economy.  American Meteorological Society.  Austin, TX.  
+<p>7. Takle, E. S., D. A. Rajewski, J. Prueger, Steven Oncley, J. K. Lundquist, Thomas W. Horst, Michael E. Rhodes, Richard Pfeiffer, Jerry L. Hatfield, Kristopher K. Spoth, Russell K. Doorenbos, 2013:  CWEX-10/11:  Overview of Results From the First Two Crop/Wind-Energy Experiments.   Fourth Conference on Weather, Climate, and the New Energy Economy.  American Meteorological Society.  Austin, TX.  
+<p>6. Takle, E. S., D. A. Rajewski, J. Prueger, Steven Oncley, J. K. Lundquist, Thomas W. Horst, Michael E. Rhodes, Richard Pfeiffer, Jerry L. Hatfield, Kristopher K. Spoth, Russell K. Doorenbos, 2012:  CWEX-10/11:  Overview of Results From the First Two Crop/Wind-Energy Experiments.  American Geophysical Union Fall Meeting, San Francisco.
+<p>5. Rajewski, D. A.,  E. S. Takle, J. H. Prueger, S.P. Oncley, T. W. Horst, R. Pfeiffer, J. L. Hatfield, K. K. Spoth, R.K. Doorenbos, 2012:  Evaluation of surface energy and carbon fluxes within a large wind farm during the CWEX-10/11 Crop Wind-Energy eXperiments.  American Geophysical Union Fall Meeting, San Francisco.
+<p>4. Rajewski, D. R., E. S. Takle, T. W. Horst, S. P. Oncley, J.K. Lundquist, M. E. Rhodes, and K. K. Spoth, 2012:  Crop-Wind-Energy Experiment 2011 (CWEX11).  16th Symposium on Meteorological Observation and Instrumentation.   Amer. Meteorol. Soc. 23-26 Janurary. New Orleans.  
+<p>3. Takle, E. S., D. A. Rajewski, J. H. Prueger, J. K. Lundquist, M. Aitken, M. E. Rhodes, A. J. Deppe, F. E. Goodman, K. C. Carter, L. Mattison, S. L. Rabideau, A. J. Rosenberg, C. L. Whitfield, and J. Hatfield, 2010:  Wind and flux measurements in a windfarm co-located with agricultural production. Presented at the American Geophysical Union Fall Meeting, San Francisco.  (Invited presentation)
+<p>2. Rhodes, M. E., , M. L. Aitken, J. K. Lundquist, E. S. Takle, and J. Prueger, 2010:  Effect of wind turbine wakes on cropland surface fluxes in the US Great Plains during a Nocturnal Low Level Jet.  Presented at the American Geophysical Union Fall Meeting, San Francisco.
+<p>1. Takle, E. S., D. A. Rajewski, M. Segal, R. Elmore, J. Hatfield, J. H. Prueger, and S. E. Taylor, 2009:  Interaction of turbine-generated turbulence with agricultural crops:  conceptual framework and prelimnary results.  Presentation at American Geophysical Union Fall Meeting, San Francisco.
 </div>
 
+</div><!-- ./col-md-9 -->
+<div class="col-md-3" role="complentary">
+<nav id="sidenav" data-spy="affix" data-offset-top="17">
+<ul class="nav">
+ <li><a href="#overview">Overview</a></li>
+ <li><a href="#locations">Tower Locations</a></li>
+ <li><a href="#instruments">Instruments</a></li>
+ <li><a href="#topography">Topography</a></li>
+ <li><a href="#data">Tower Data</a></li>
+ <li><a href="#cwex">CWEX</a></li>
+ <li><a href="#contact">Contacts</a></li>
+ <li><a href="#ack">Acknowledgements</a></li>
+ <li><a href="#publications">Publications</a></li>
+ <li><a href="#presentations">Presentations</a></li>
+</ul>
+</nav>
+</div><!-- ./col-md-3 -->
+</div><!-- ./row -->
 
 EOF;
 
-$t->render('single.phtml');
+$t->render('full.phtml');
 ?>
