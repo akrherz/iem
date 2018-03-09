aa0a882ca0421ddd9944ebe76d8291ef9d9411cb
akrherz <akrherz@iastate.edu>
2018-03-08 19:07:33 -0600

    improve CRH geojson caching and diagnostics

--- cgi-bin/compare_crh.py
+++ cgi-bin/compare_crh.py
@@ -3,7 +3,6 @@
 from __future__ import print_function
 import datetime
 import sys
-import urllib2
 
 import requests
 import pandas as pd
@@ -11,6 +10,8 @@ from pandas.io.sql import read_sql
 from pyiem.nws.vtec import VTEC_SIGNIFICANCE, VTEC_PHENOMENA
 from pyiem.util import get_dbconn
 
+# Akami has this cached, so we shall cache bust it, please
+NOUNCE = "?%s" % (datetime.datetime.now().strftime("%Y%m%d%H%M%S"))
 URI = "https://www.weather.gov/source/crh/allhazard.geojson"
 
 REV_VTEC_SIGNIFIANCE = dict((v, k) for k, v in VTEC_SIGNIFICANCE.items())
@@ -35,48 +36,55 @@ def main():
     sys.stdout.write("Report run at %s\n" % (datetime.datetime.utcnow(), ))
     sys.stdout.write("Comparing %s\n" % (URI, ))
     try:
-        req = requests.get(URI)
+        req = requests.get(URI + NOUNCE)
         jdata = req.json()
-    except urllib2.HTTPError as _exp:
-        sys.stdout.write("Failure to download %s, comparison failed" % (URI, ))
+    except Exception as exp:
+        sys.stdout.write(("Failure to download %s, comparison failed\n"
+                          "%s") % (URI, exp))
         return
+    sys.stdout.write(("geojson generation_time: %s\n"
+                      ) % (jdata.get('generation_time'), ))
     rows = []
     for feature in jdata['features']:
         props = feature['properties']
-        rows.append(dict(wfo=props['office'][1:],
-                         phenomena=get_phenomena(props['phenomenon']),
-                         significance=get_significance(props['significance']),
-                         eventid=int(props['etn'])))
+        for ugc in props.get('ugc', ['UNKNOW']):
+            rows.append(dict(wfo=props['office'][1:], ugc=ugc,
+                             phenomena=get_phenomena(props['phenomenon']),
+                             significance=get_significance(
+                                 props['significance']),
+                             eventid=int(props['etn'])))
     capdf = pd.DataFrame(rows)
 
     iemdf = read_sql("""
-    SELECT distinct wfo, phenomena, significance, eventid from warnings
+    SELECT ugc, wfo, phenomena, significance, eventid from warnings
     where expire > now()
     """, get_dbconn('postgis', user='nobody'), index_col=None)
     for _idx, row in capdf.iterrows():
         df2 = iemdf[((iemdf['phenomena'] == row['phenomena']) &
                      (iemdf['significance'] == row['significance']) &
                      (iemdf['eventid'] == row['eventid']) &
-                     (iemdf['wfo'] == row['wfo']))]
+                     (iemdf['wfo'] == row['wfo']) &
+                     (iemdf['ugc'] == row['ugc']))]
         if df2.empty:
             if len(row['phenomena']) > 2:
-                print(("Indeteriminate %s %s %s %s"
+                print(("Indeteriminate %s %s %s %s %s"
                        ) % (row['wfo'], row['phenomena'], row['significance'],
-                            row['eventid']))
+                            row['eventid'], row['ugc']))
                 continue
-            print(("IEM MISSING (%s %s %s %s)"
+            print(("IEM MISSING (%s %s %s %s %s)"
                    ) % (row['wfo'], row['phenomena'], row['significance'],
-                        row['eventid']))
+                        row['eventid'], row['ugc']))
 
     for _idx, row in iemdf.iterrows():
         df2 = capdf[((capdf['phenomena'] == row['phenomena']) &
                      (capdf['significance'] == row['significance']) &
                      (capdf['eventid'] == row['eventid']) &
-                     (capdf['wfo'] == row['wfo']))]
+                     (capdf['wfo'] == row['wfo']) &
+                     (capdf['ugc'] == row['ugc']))]
         if df2.empty:
-            print(("CRH MISSING (%s %s %s %s)"
+            print(("CRH MISSING (%s %s %s %s %s)"
                    ) % (row['wfo'], row['phenomena'], row['significance'],
-                        row['eventid']))
+                        row['eventid'], row['ugc']))
 
     sys.stdout.write("DONE...\n")
 
