55d44f6eb62fbad3b309a4439f45b4fb3cd08c3b
akrherz <akrherz@iastate.edu>
2018-04-25 13:10:07 -0500

    add more plotting options to climate station autoplot
    https://mesonet.agron.iastate.edu/plotting/auto/?q=97

--- htdocs/plotting/auto/scripts/p97.py
+++ htdocs/plotting/auto/scripts/p97.py
@@ -23,23 +23,30 @@ PDICT = OrderedDict([
 
 PDICT2 = OrderedDict([
     ('max_high_temp', 'Maximum High Temperature'),
+    ('avg_high_temp', 'Average High Temperature'),
     ('avg_temp_depart', 'Average Temperature Departure'),
+    ('avg_temp', 'Average Temperature'),
     ('min_low_temp', 'Minimum Low Temperature'),
+    ('avg_low_temp', 'Average Low Temperature'),
     ('gdd_sum', 'Growing Degree Days (50/86) Total'),
     ('cgdd_sum', 'Growing Degree Days Climatology (50/86)'),
     ('gdd_depart', 'Growing Degree Days (50/86) Departure'),
     ('precip_depart', 'Precipitation Departure'),
+    ('precip_sum', 'Precipitation Total'),
     ])
 PDICT4 = {'yes': 'Yes, overlay Drought Monitor',
           'no': 'No, do not overlay Drought Monitor'}
 UNITS = {
     'precip_depart': 'inch',
     'min_low_temp': 'F',
-    'max_high_temp': 'F',
+    'avg_low_temp': 'F',
+    'avg_high_temp': 'F',
     'gdd_depart': 'F',
     'gdd_sum': 'F',
     'cgdd_sum': 'F',
     'avg_temp_depart': 'F',
+    'avg_temp': 'F',
+    'precip_sum': 'inch',
     }
 
 
@@ -89,7 +96,9 @@ def plotter(fdict):
     df = read_sql("""
     WITH obs as (
         SELECT station, gddxx(50, 86, high, low) as gdd50,
-        sday, high, low, precip from """ + table + """ WHERE
+        sday, high, low, precip,
+        (high + low)/2. as avg_temp
+        from """ + table + """ WHERE
         day >= %s and day < %s and
         substr(station, 3, 1) != 'C' and substr(station, 3, 4) != '0000'),
     climo as (
@@ -98,14 +107,19 @@ def plotter(fdict):
     combo as (
         SELECT o.station, o.precip - c.precip as precip_diff,
         o.precip as precip, c.precip as cprecip,
+        o.avg_temp,
         o.high, o.low, o.gdd50, c.gdd50 as cgdd50,
         o.gdd50 - c.gdd50 as gdd50_diff,
-        (o.high + o.low)/2. - (c.high + c.low)/2. as temp_diff
+        o.avg_temp - (c.high + c.low)/2. as temp_diff
         from obs o JOIN climo c ON
         (o.station = c.station and o.sday = c.sday)),
     agg as (
-        SELECT station, sum(precip_diff) as precip_depart,
+        SELECT station,
+        avg(avg_temp) as avg_temp,
+        sum(precip_diff) as precip_depart,
         sum(precip) as precip, sum(cprecip) as cprecip,
+        avg(high) as avg_high_temp,
+        avg(low) as avg_low_temp,
         max(high) as max_high_temp,
         min(low) as min_low_temp, sum(gdd50_diff) as gdd_depart,
         avg(temp_diff) as avg_temp_depart, sum(gdd50) as gdd_sum,
@@ -113,9 +127,18 @@ def plotter(fdict):
         from combo GROUP by station)
 
     SELECT d.station, t.name,
-    d.precip as precip_sum, d.cprecip as cprecip_sum,
-    d.precip_depart, d.min_low_temp, d.avg_temp_depart,
-    d.gdd_depart, d.gdd_sum, d.cgdd_sum, d.max_high_temp,
+    precip as precip_sum,
+    avg_temp,
+    cprecip as cprecip_sum,
+    precip_depart,
+    min_low_temp,
+    avg_temp_depart,
+    gdd_depart,
+    gdd_sum,
+    cgdd_sum,
+    max_high_temp,
+    avg_high_temp,
+    avg_low_temp,
     ST_x(t.geom) as lon, ST_y(t.geom) as lat
     from agg d JOIN stations t on (d.station = t.id)
     WHERE t.network ~* 'CLIMATE'
@@ -124,29 +147,39 @@ def plotter(fdict):
 
     sector2 = "state" if sector != 'midwest' else 'midwest'
     datefmt = "%d %b %Y" if varname != 'cgdd_sum' else '%d %b'
-    subtitle = ("Climatology is based on data from 1951-%s"
-                ) % (datetime.date.today().year - 1, )
-    if varname != 'cgdd_sum':
+    subtitle = ''
+    if varname.find('depart') > -1:
         subtitle = ('%s is compared with 1951-%s Climatology'
                     ' to compute departures'
                     ) % (date1.year, datetime.date.today().year - 1)
+    elif varname.startswith('c'):
+        subtitle = ("Climatology is based on data from 1951-%s"
+                    ) % (datetime.date.today().year - 1, )
     mp = MapPlot(sector=sector2, state=sector, axisbg='white',
                  title=('%s - %s %s [%s]'
                         ) % (date1.strftime(datefmt),
                              date2.strftime(datefmt), PDICT2.get(varname),
                              UNITS.get(varname)),
                  subtitle=subtitle)
+    fmt = '%.2f'
     if varname in ['precip_depart', 'avg_temp_depart']:
         rng = df[varname].abs().describe(percentiles=[0.95])['95%']
         clevels = np.linspace(0 - rng, rng, 7)
-        fmt = '%.2f'
+        cmap = cm.get_cmap('RdYlBu'
+                           if varname == 'precip_depart' else 'RdYlBu_r')
+    elif varname in ['precip_sum']:
+        rng = df[varname].abs().describe(percentiles=[0.95])['95%']
+        clevels = np.linspace(0, rng, 7)
+        cmap = cm.get_cmap('plasma_r')
+        cmap.set_under('white')
+        cmap.set_over('black')
     else:
         minv = df[varname].min() - 5
         maxv = df[varname].max() + 5
         clevels = np.linspace(minv, maxv, 6, dtype='i')
         fmt = '%.0f'
+        cmap = cm.get_cmap('RdYlBu_r')
     clevlabels = [fmt % x for x in clevels]
-    cmap = cm.get_cmap('RdYlBu' if varname == 'precip_depart' else 'RdYlBu_r')
     cmap.set_bad('white')
     mp.contourf(df['lon'].values, df['lat'].values,
                 df[varname].values, clevels, clevlabels=clevlabels,
