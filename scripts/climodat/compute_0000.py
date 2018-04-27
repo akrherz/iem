ea5deb883ddff3fb1435a9351a1665eff5468b75
akrherz <akrherz@iastate.edu>
2018-04-27 13:22:27 -0500

    let snow and snowdepth come along for climodat climdiv,st averages

--- scripts/climodat/compute_0000.py
+++ scripts/climodat/compute_0000.py
@@ -6,14 +6,14 @@ import datetime
 import netCDF4
 import numpy as np
 from pyiem import iemre
-from pyiem.datatypes import temperature
+from pyiem.datatypes import temperature, distance
 from pyiem.util import get_dbconn
 
 COOP = get_dbconn("coop")
 ccursor = COOP.cursor()
 
 
-def update_database(stid, valid, high, low, precip):
+def update_database(stid, valid, high, low, precip, snow, snowd):
     """Update the database with these newly computed values!"""
     table = "alldata_%s" % (stid[:2], )
     # See if we need to add an entry
@@ -21,16 +21,18 @@ def update_database(stid, valid, high, low, precip):
     and station = %s""", (valid, stid))
     if ccursor.rowcount != 1:
         ccursor.execute("""INSERT into """ + table + """ (station, day,
-        high, low, precip, estimated, year, month, sday) VALUES
-        (%s, %s, %s, %s, %s, 't', %s, %s, %s)
-        """, (stid, valid, high, low, round(precip, 2), valid.year,
+        high, low, precip, snow, snowd, estimated, year, month, sday) VALUES
+        (%s, %s, %s, %s, %s, %s, %s, 't', %s, %s, %s)
+        """, (stid, valid, high, low, round(precip, 2),
+              round(snow, 1), round(snowd, 1), valid.year,
               valid.month, valid.strftime("%m%d")))
     # Now we update
     ccursor.execute("""
         UPDATE """ + table + """
-        SET high = %s, low = %s, precip = %s
+        SET high = %s, low = %s, precip = %s, snow = %s, snowd = %s
         WHERE station = %s and day = %s
-    """, (high, low, round(precip, 2), stid, valid))
+    """, (high, low, round(precip, 2), round(snow, 1), round(snowd, 1),
+          stid, valid))
     if ccursor.rowcount != 1:
         print('compute_0000:update_database updated %s row for %s %s' % (
             ccursor.rowcount, stid, valid))
@@ -45,15 +47,18 @@ def do_day(valid):
                        'K').value('F')
     low = temperature(nc.variables['low_tmpk_12z'][idx, :, :],
                       'K').value('F')
-    precip = nc.variables['p01d_12z'][idx, :, :] / 25.4
+    precip = distance(nc.variables['p01d_12z'][idx, :, :], 'MM').value("IN")
+    snow = distance(nc.variables['snow_12z'][idx, :, :], 'MM').value("IN")
+    snowd = distance(nc.variables['snowd_12z'][idx, :, :], 'MM').value("IN")
     nc.close()
     for state in ('IA', 'NE', 'MN', 'WI', 'MI', 'OH', 'IN', 'IL', 'MO',
                   'KS', 'KY', 'ND', 'SD'):
-        do_state_day(state, valid, high, low, precip)
-        do_climdiv_day(state, valid, high, low, precip)
+        do_state_day(state, valid, high, low, precip, snow, snowd)
+        do_climdiv_day(state, valid, high, low, precip, snow, snowd)
 
 
-def do_climdiv_day(stabbr, valid, highgrid, lowgrid, precipgrid):
+def do_climdiv_day(stabbr, valid, highgrid, lowgrid, precipgrid,
+                   snowgrid, snowdgrid):
     """
     Compute the virtual climate division data as well
     """
@@ -69,16 +74,19 @@ def do_climdiv_day(stabbr, valid, highgrid, lowgrid, precipgrid):
         high = np.average(highgrid[sw > 0])
         low = np.average(lowgrid[sw > 0])
         precip = np.average(precipgrid[sw > 0])
+        snow = np.average(snowgrid[sw > 0])
+        snowd = np.average(snowdgrid[sw > 0])
 
         print(('%s %s-%s-%s High: %5.1f Low: %5.1f Precip: %4.2f'
                ) % (stid, valid.year, valid.month,
                     valid.day, high, low, precip))
-        update_database(stid, valid, high, low, precip)
+        update_database(stid, valid, high, low, precip, snow, snowd)
 
     sw_nc.close()
 
 
-def do_state_day(stabbr, valid, highgrid, lowgrid, precipgrid):
+def do_state_day(stabbr, valid, highgrid, lowgrid, precipgrid,
+                 snowgrid, snowdgrid):
     """
     Create the statewide average value based on averages of the IEMRE
     """
@@ -91,12 +99,14 @@ def do_state_day(stabbr, valid, highgrid, lowgrid, precipgrid):
     high = np.average(highgrid[sw > 0])
     low = np.average(lowgrid[sw > 0])
     precip = np.average(precipgrid[sw > 0])
+    snow = np.average(snowgrid[sw > 0])
+    snowd = np.average(snowdgrid[sw > 0])
 
     print(('%s %s-%s-%s NEW High: %5.1f Low: %5.1f Precip: %4.2f'
            ) % (stabbr, valid.year, valid.month,
                 valid.day, high, low, precip))
 
-    update_database(stabbr+"0000", valid, high, low, precip)
+    update_database(stabbr+"0000", valid, high, low, precip, snow, snowd)
 
 
 def main(argv):
