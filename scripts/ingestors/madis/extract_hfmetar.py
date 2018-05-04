b28de36fa80785b2023cbdd3154b57877887a470
akrherz <akrherz@iastate.edu>
2018-05-04 12:13:16 -0500

    be more forgiving with HFMETAR MADIS ingest

--- scripts/ingestors/madis/extract_hfmetar.py
+++ scripts/ingestors/madis/extract_hfmetar.py
@@ -6,13 +6,14 @@ Run from RUN_40_AFTER.sh for two hours ago
 from __future__ import print_function
 import os
 import sys
-import re
 import datetime
 import warnings
 
 import pytz
 import numpy as np
+from tqdm import tqdm
 from metar import Metar
+from netCDF4 import chartostring
 from pyiem.datatypes import temperature, distance, pressure, speed
 from pyiem.observation import Observation
 from pyiem.util import get_dbconn, ncopen
@@ -44,11 +45,6 @@ def vsbyfmt(val):
     return int(val)
 
 
-def tostring(val):
-    """Save tostring"""
-    return re.sub('\x00', '', val.tostring()).strip()
-
-
 def process(ncfn):
     """Process this file """
     pgconn = get_dbconn('iem')
@@ -69,9 +65,10 @@ def process(ncfn):
                   'precipAccum', 'presWx', 'skyCvr',
                   'skyCovLayerBase', 'autoRemark', 'operatorRemark']:
         data[vname] = nc.variables[vname][:]
-        vname += "QCR"
-        if vname in nc.variables:
-            data[vname] = nc.variables[vname][:]
+        for qc in ['QCR', 'QCD']:
+            vname2 = vname + qc
+            if vname2 in nc.variables:
+                data[vname2] = nc.variables[vname2][:]
     for vname in ['temperature', 'dewpoint']:
         data[vname+"C"] = temperature(data[vname], 'K').value('C')
         data[vname] = temperature(data[vname], 'K').value('F')
@@ -81,77 +78,94 @@ def process(ncfn):
     data['altimeter'] = pressure(data['altimeter'], 'PA').value("IN")
     data['skyCovLayerBase'] = distance(data['skyCovLayerBase'],
                                        'M').value("FT")
-    data['visibility'] = distance(data['visibility'],
-                                  'M').value("MI")
-    data['precipAccum'] = distance(data['precipAccum'],
-                                   'MM').value("IN")
+    data['visibility'] = distance(data['visibility'], 'M').value("MI")
+    data['precipAccum'] = distance(data['precipAccum'], 'MM').value("IN")
+    stations = chartostring(data['stationId'][:])
+    presentwxs = chartostring(data['presWx'][:])
+    skycs = chartostring(data['skyCvr'][:])
+    autoremarks = chartostring(data['autoRemark'][:])
+    opremarks = chartostring(data['operatorRemark'][:])
 
-    for i in range(len(data['stationId'])):
-        sid = tostring(data['stationId'][i])
+    def decision(i, fieldname, tolerance):
+        """Our decision if we are going to take a HFMETAR value or not"""
+        if data[fieldname][i] is np.ma.masked:
+            return None
+        if data["%sQCR" % (fieldname, )][i] == 0:
+            return data[fieldname][i]
+        # Now we have work to do
+        departure = np.ma.max(
+            np.ma.abs(data['%sQCD' % (fieldname, )][i, :]))
+        # print("departure: %s tolerance: %s" % (departure, tolerance))
+        if departure <= tolerance:
+            return data[fieldname][i]
+        return None
+
+    for i, sid in tqdm(enumerate(stations), total=len(stations),
+                       disable=(not sys.stdout.isatty())):
         sid3 = sid[1:] if sid[0] == 'K' else sid
         ts = datetime.datetime(1970, 1, 1) + datetime.timedelta(
                 seconds=data['observationTime'][i])
-        ts = ts.replace(tzinfo=pytz.timezone("UTC"))
+        ts = ts.replace(tzinfo=pytz.utc)
 
         mtr = "%s %sZ AUTO " % (sid, ts.strftime("%d%H%M"))
         network = xref.get(sid3, 'ASOS')
         iem = Observation(sid3, network, ts)
+
         #  06019G23KT
-        if (data['windDirQCR'][i] == 0 and
-                data['windDir'][i] is not np.ma.masked):
-            iem.data['drct'] = int(data['windDir'][i])
+        val = decision(i, 'windDir', 15)
+        if val is not None:
+            iem.data['drct'] = int(val)
             mtr += "%03i" % (iem.data['drct'], )
         else:
             mtr += "///"
 
-        if (data['windSpeedQCR'][i] == 0 and
-                data['windSpeed'][i] is not np.ma.masked):
-            iem.data['sknt'] = int(data['windSpeed'][i])
+        val = decision(i, 'windSpeed', 10)
+        if val is not None:
+            iem.data['sknt'] = int(val)
             mtr += "%02i" % (iem.data['sknt'], )
         else:
             mtr += "//"
 
-        if (data['windGustQCR'][i] == 0 and
-                data['windGust'][i] is not np.ma.masked and
-                data['windGust'][i] > 0):
-            iem.data['gust'] = int(data['windGust'][i])
+        val = decision(i, 'windGust', 10)
+        if val is not None:
+            iem.data['gust'] = int(val)
             mtr += "G%02i" % (iem.data['gust'],)
         mtr += "KT "
 
-        if (data['visibilityQCR'][i] == 0 and
-                data['visibility'][i] is not np.ma.masked):
-            iem.data['vsby'] = float(data['visibility'][i])
+        val = decision(i, 'visibility', 4)
+        if val is not None:
+            iem.data['vsby'] = float(val)
             mtr += "%sSM " % (vsbyfmt(iem.data['vsby']), )
 
-        presentwx = tostring(data['presWx'][i])
+        presentwx = presentwxs[i]
         if presentwx != '':
             # database storage is comma delimited
             iem.data['wxcodes'] = presentwx.split(" ")
             mtr += "%s " % (presentwx,)
 
-        for _i, (_c, _l) in enumerate(
-                        zip(data['skyCvr'][i], data['skyCovLayerBase'][i])):
-            if tostring(_c) != '':
-                skyc = tostring(_c)
+        for _i, (skyc, _l) in enumerate(
+                        zip(skycs[i], data['skyCovLayerBase'][i])):
+            if skyc != '':
                 iem.data['skyc%s' % (_i+1,)] = skyc
                 if skyc != 'CLR':
                     iem.data['skyl%s' % (_i+1,)] = int(_l)
-                    mtr += "%s%03i " % (tostring(_c), int(_l) / 100)
+                    mtr += "%s%03i " % (skyc, int(_l) / 100)
                 else:
                     mtr += "CLR "
 
         t = ""
         tgroup = "T"
-        if (data['temperatureQCR'][i] == 0 and
-                data['temperature'][i] is not np.ma.masked):
+        val = decision(i, 'temperature', 10)
+        if val is not None:
+            # Recall the pain enabling this
             # iem.data['tmpf'] = float(data['temperature'][i])
             tmpc = float(data['temperatureC'][i])
             t = "%s%02i/" % ("M" if tmpc < 0 else "",
                              tmpc if tmpc > 0 else (0 - tmpc))
             tgroup += "%s%03i" % ("1" if tmpc < 0 else "0",
                                   (tmpc if tmpc > 0 else (0 - tmpc)) * 10.)
-        if (data['dewpointQCR'][i] == 0 and
-                data['dewpoint'][i] is not np.ma.masked):
+        val = decision(i, 'dewpoint', 10)
+        if val is not None:
             # iem.data['dwpf'] = float(data['dewpoint'][i])
             tmpc = float(data['dewpointC'][i])
             if t != "":
@@ -161,36 +175,38 @@ def process(ncfn):
                                       (tmpc if tmpc > 0 else (0 - tmpc)) * 10.)
         if len(t) > 4:
             mtr += t
-        if (data['altimeterQCR'][i] == 0 and
-                data['altimeter'][i] is not np.ma.masked):
-            iem.data['alti'] = round(data['altimeter'][i], 2)
+        val = decision(i, 'altimeter', 20)
+        if val is not None:
+            iem.data['alti'] = float(round(val, 2))
             mtr += "A%4i " % (iem.data['alti'] * 100.,)
 
         mtr += "RMK "
-        if (data['precipAccumQCR'][i] == 0 and
-                data['precipAccum'][i] is not np.ma.masked):
-            if data['precipAccum'][i] >= 0.01:
-                iem.data['phour'] = round(data['precipAccum'][i], 2)
+        val = decision(i, 'precipAccum', 25)
+        if val is not None:
+            if val >= 0.01:
+                iem.data['phour'] = float(round(val, 2))
                 mtr += "P%04i " % (iem.data['phour'] * 100.,)
-            elif data['precipAccum'][i] < 0.01 and data['precipAccum'][i] > 0:
+            elif val < 0.01 and val > 0:
                 # Trace
                 mtr += "P0000 "
                 iem.data['phour'] = TRACE_VALUE
 
         if tgroup != "T":
             mtr += "%s " % (tgroup, )
-        autoremark = tostring(data['autoRemark'][i])
-        opremark = tostring(data['operatorRemark'][i])
-        if autoremark != '' or opremark != '':
-            mtr += "%s %s " % (autoremark, opremark)
+
+        if autoremarks[i] != '' or opremarks[i] != '':
+            mtr += "%s %s " % (autoremarks[i], opremarks[i])
         mtr += "MADISHF"
         # Eat our own dogfood
         try:
             Metar.Metar(mtr)
             iem.data['raw'] = mtr
-        except Exception as exp:
+        except:
             pass
 
+        for key in iem.data:
+            if isinstance(iem.data[key], np.float32):
+                print("key: %s type: %s" % (key, type(iem.data[key])))
         icursor = pgconn.cursor()
         if not iem.save(icursor, force_current_log=True,
                         skip_current=True):
