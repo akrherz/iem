e14f484c31ef6f17b2479843c52d8c5ca7caf28c
akrherz <akrherz@iastate.edu>
2018-06-12 13:05:03 -0500

    allow for period of days plotting for climate district autoplot
    https://mesonet.agron.iastate.edu/plotting/auto/?q=24

--- htdocs/plotting/auto/scripts/p24.py
+++ htdocs/plotting/auto/scripts/p24.py
@@ -15,6 +15,10 @@ PDICT = OrderedDict((
     ('low', 'Average Low Temperature'),
     ('precip', 'Total Precipitation'),
     ))
+PDICT2 = OrderedDict((
+    ('month', 'Plot by Month(s) [Option 1]'),
+    ('day', 'Plot by Inclusive Date Period [Option 2]'),
+))
 
 MDICT = OrderedDict([
          ('spring', 'Spring (MAM)'),
@@ -40,57 +44,124 @@ def get_description():
     desc = dict()
     desc['data'] = True
     desc['cache'] = 86400
-    desc['description'] = """This map presents IEM computed precipitation
-    ranks for a month and year of your choice.  The map unit is climate
-    districts for which the IEM generates spatially weighted daily averages
-    for.  For seasonal totals, the year presented is the calendar year of
-    the last month in the three month period."""
-    today = datetime.date.today() - datetime.timedelta(days=28)
+    desc['description'] = """The map presents IEM computed climate district
+    ranks for a metric of your choice.  This map can be generated for a given
+    month and year or period of dates.  If the period of days includes leap
+    day, this day is included and unweighted against years without it.  If the
+    period spans two years, the presented year on the map represents the
+    second year in the selection.  For plotting period Option 2, the time
+    period is limited to 1 year or less.
+
+    <p>The climate district data uses the current NWS COOP nomenclature of
+    reporting each day at approximately 7 AM.  So for example, a plot of June
+    precipitation would stricly include the period of 7 AM 31 May
+    to 7 AM 30 June. Data for the current date is available at approximately
+    noon each day.
+    """
+    today = datetime.date.today()
+    lmonth = today - datetime.timedelta(days=28)
     desc['arguments'] = [
         dict(type='select', name='var', default='precip',
              label='Select Variable', options=PDICT),
-        dict(type='year', name='year', default=today.year,
-             label='Select Year:',
+        dict(type='select', name='p', default='month',
+             label='Plotting Period', options=PDICT2),
+        dict(type='year', name='year', default=lmonth.year,
+             label='[Option 1] Select Year:',
              min=1893),
-        dict(type='select', name='month', default=today.month,
-             label='Month Limiter', options=MDICT),
+        dict(type='select', name='month', default=lmonth.month,
+             label='[Option 1] Month Limiter', options=MDICT),
+        dict(type='date', name='sdate', default=lmonth.strftime("%Y/%m/%d"),
+             label='[Option 2] Start Date', min='1893/01/01'),
+        dict(type='date', name='edate', default=today.strftime("%Y/%m/%d"),
+             label='[Option 2] End Date (inclusive)', min='1893/01/01'),
     ]
     return desc
 
 
-def plotter(fdict):
-    """ Go """
-    import matplotlib
-    matplotlib.use('agg')
-    from pyiem.plot import MapPlot
+def get_daily_data(ctx):
+    """Do the data work"""
     pgconn = get_dbconn('coop')
-    ctx = get_autoplot_context(fdict, get_description())
+    sdate = ctx['sdate']
+    edate = ctx['edate']
+    offset = "0 days"
+    if edate <= sdate:
+        raise ValueError("start date after end date, please correct")
+    if (edate - sdate).days > 366:
+        raise ValueError("Sorry, too long of period selected. < 1 year please")
+    if edate.year != sdate.year:
+        jan1 = (edate + datetime.timedelta(days=365)).replace(day=1, month=1)
+        offset = "%.0f days" % ((jan1 - edate).days + 1, )
+        sday = " sday >= '%s' or sday <= '%s' " % (sdate.strftime("%m%d"),
+                                                   edate.strftime("%m%d"))
+    else:
+        sday = " sday >= '%s' and sday <= '%s' " % (sdate.strftime("%m%d"),
+                                                    edate.strftime("%m%d"))
 
+    ctx['label'] = "%s - %s" % (sdate.strftime("%-d %b %Y"),
+                                edate.strftime("%-d %b %Y"))
+
+    ctx['df'] = read_sql("""
+    with monthly as (
+        SELECT extract(year from day + '""" + offset + """'::interval) as myyear,
+        station,
+        sum(precip) as p,
+        avg((high+low)/2.) as avgt,
+        avg(low) as avglo,
+        avg(high) as avghi
+        from alldata
+        WHERE substr(station,3,1) = 'C' and ( """ + sday + """ )
+        GROUP by myyear, station),
+    ranks as (
+        SELECT station, myyear as year,
+        avg(p) OVER (PARTITION by station) as avg_precip,
+        stddev(p) OVER (PARTITION by station) as std_precip,
+        p as precip,
+        avg(avghi) OVER (PARTITION by station) as avg_high,
+        stddev(avghi) OVER (PARTITION by station) as std_high,
+        avghi as high,
+        avg(avglo) OVER (PARTITION by station) as avg_low,
+        stddev(avglo) OVER (PARTITION by station) as std_low,
+        avglo as low,
+        rank() OVER (PARTITION by station ORDER by p DESC) as precip_rank,
+        rank() OVER (PARTITION by station ORDER by avghi DESC) as high_rank,
+        rank() OVER (PARTITION by station ORDER by avglo DESC) as low_rank,
+        rank() OVER (PARTITION by station ORDER by avgt DESC) as avgt_rank
+        from monthly)
+
+    SELECT station, precip_rank, avgt_rank, high_rank, low_rank,
+    ((high - avg_high) / std_high) - ((precip - avg_precip) / std_precip)
+    as arridity from ranks
+    where year = %s and
+    substr(station, 1, 2) in ('ND', 'SD', 'NE', 'KS', 'MO', 'IA', 'MN', 'WI',
+    'IL', 'IN', 'KY', 'OH', 'MI')
+    """, pgconn, params=(edate.year, ),
+                         index_col='station')
+
+
+def get_monthly_data(ctx):
+    """Do the data work"""
+    pgconn = get_dbconn('coop')
     year = ctx['year']
     month = ctx['month']
-    varname = ctx['var']
     offset = "0 days"
     if month == 'fall':
         months = [9, 10, 11]
-        label = "Fall (SON)"
+        ctx['label'] = "Fall (SON)"
     elif month == 'winter':
         months = [12, 1, 2]
         offset = "32 days"
-        label = "Winter (DJF)"
+        ctx['label'] = "Winter (DJF)"
     elif month == 'spring':
         months = [3, 4, 5]
-        label = "Spring (MAM)"
+        ctx['label'] = "Spring (MAM)"
     elif month == 'summer':
         months = [6, 7, 8]
-        label = "Summer (JJA)"
+        ctx['label'] = "Summer (JJA)"
     else:
         months = [int(month), ]
-        label = calendar.month_name[int(month)]
+        ctx['label'] = "%s %s" % (year, calendar.month_name[int(month)])
 
-    lastyear = datetime.date.today().year
-    years = lastyear - 1893 + 1
-
-    df = read_sql("""
+    ctx['df'] = read_sql("""
     with monthly as (
         SELECT extract(year from day + '""" + offset + """'::interval) as myyear,
         station, sum(precip) as p,
@@ -124,35 +195,49 @@ def plotter(fdict):
     substr(station, 1, 2) in ('ND', 'SD', 'NE', 'KS', 'MO', 'IA', 'MN', 'WI',
     'IL', 'IN', 'KY', 'OH', 'MI')
     """, pgconn, params=(tuple(months), year),
-                  index_col='station')
+                         index_col='station')
+
+
+def plotter(fdict):
+    """ Go """
+    import matplotlib
+    matplotlib.use('agg')
+    from pyiem.plot import MapPlot
+    ctx = get_autoplot_context(fdict, get_description())
+    if ctx['p'] == 'day':
+        get_daily_data(ctx)
+    else:
+        get_monthly_data(ctx)
+    ctx['lastyear'] = datetime.date.today().year
+    ctx['years'] = ctx['lastyear'] - 1893 + 1
 
     subtitle = ('Based on IEM Estimates, '
                 '1 is %s out of %s total years (1893-%s)'
-                ) % ('wettest' if varname == 'precip' else 'hottest',
-                     years, lastyear)
-    if varname == 'arridity':
+                ) % ('wettest' if ctx['var'] == 'precip' else 'hottest',
+                     ctx['years'], ctx['lastyear'])
+    if ctx['var'] == 'arridity':
         subtitle = "Std Average High Temp Departure minus Std Precip Departure"
     mp = MapPlot(sector='midwest', continentalcolor='white',
-                 title='%s %s %s %sby Climate District' % (
-                         year, label, PDICT[varname],
-                         'Ranks ' if varname != 'arridity' else ''),
-                 subtitle=subtitle)
-    cmap = cm.get_cmap("BrBG_r" if varname in ['precip', 'arridity']
+                 title='%s %s %sby Climate District' % (
+                         ctx['label'], PDICT[ctx['var']],
+                         'Ranks ' if ctx['var'] != 'arridity' else ''),
+                 subtitle=subtitle, titlefontsize=14)
+    cmap = cm.get_cmap("BrBG_r" if ctx['var'] in ['precip', 'arridity']
                        else 'BrBG')
     cmap.set_under('white')
     cmap.set_over('black')
-    bins = [1, 5, 10, 25, 50, 75, 100, years-10, years-5, years]
-    pvar = varname+'_rank'
+    bins = [1, 5, 10, 25, 50, 75, 100, ctx['years'] - 10, ctx['years'] - 5,
+            ctx['years']]
+    pvar = ctx['var']+'_rank'
     fmt = '%.0f'
-    if varname == 'arridity':
+    if ctx['var'] == 'arridity':
         bins = np.arange(-4, 4.1, 1)
-        pvar = varname
+        pvar = ctx['var']
         fmt = '%.1f'
-    mp.fill_climdiv(df[pvar], ilabel=True, plotmissing=False, lblformat=fmt,
-                    bins=bins,
-                    cmap=cmap)
+    mp.fill_climdiv(ctx['df'][pvar], ilabel=True, plotmissing=False,
+                    lblformat=fmt, bins=bins, cmap=cmap)
 
-    return mp.fig, df
+    return mp.fig, ctx['df']
 
 
 if __name__ == '__main__':
