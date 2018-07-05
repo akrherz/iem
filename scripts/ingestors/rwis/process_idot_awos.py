1d74fba366efdad7aa0c9455f9b000491af0c909
akrherz <akrherz@iastate.edu>
2018-07-05 07:23:24 -0500

    make DOT AWOS ingest more robust

--- scripts/ingestors/rwis/process_idot_awos.py
+++ scripts/ingestors/rwis/process_idot_awos.py
@@ -1,10 +1,10 @@
 """Process AWOS METAR file"""
 from __future__ import print_function
 import re
-import os
 import datetime
 import ftplib
 import subprocess
+from io import StringIO
 
 import pyiem.util as util
 
@@ -29,28 +29,30 @@ def main():
     fn = fetch_files()
     utc = datetime.datetime.utcnow().strftime("%Y%m%d%H%M")
     data = {}
-    for line in open(fn):
+    # Sometimes, the file gets gobbled it seems
+    for line in open(fn, 'rb'):
+        line = line.decode('utf-8', 'ignore')
         match = re.match("METAR K(?P<id>[A-Z1-9]{3})", line)
         if not match:
             continue
         gd = match.groupdict()
         data[gd['id']] = line
 
-    fd = open('/tmp/awwwsosss', 'w')
-    fd.write("\001\r\r\n")
-    fd.write(("SAUS00 KISU %s\r\r\n"
-              ) % (datetime.datetime.utcnow().strftime("%d%H%M"), ))
-    fd.write("METAR\r\r\n")
+    sio = StringIO()
+    sio.write("\001\r\r\n")
+    sio.write(("SAUS00 KISU %s\r\r\n"
+               ) % (datetime.datetime.utcnow().strftime("%d%H%M"), ))
+    sio.write("METAR\r\r\n")
     for sid in data:
-        fd.write('%s=\r\r\n' % (data[sid].strip().replace("METAR ", ""), ))
-    fd.write("\003")
-    fd.close()
+        sio.write('%s=\r\r\n' % (data[sid].strip().replace("METAR ", ""), ))
+    sio.write("\003")
+    sio.seek(0)
     proc = subprocess.Popen(("/home/ldm/bin/pqinsert -i -p 'data c %s "
                              "LOCDSMMETAR.dat LOCDSMMETAR.dat txt' %s"
                              ) % (utc, '/tmp/awwwsosss'), shell=True,
                             stdin=subprocess.PIPE, stdout=subprocess.PIPE,
                             stderr=subprocess.STDOUT)
-    os.waitpid(proc.pid, 0)
+    proc.communicate(sio.getvalue().encode('utf-8'))
 
 
 if __name__ == '__main__':
