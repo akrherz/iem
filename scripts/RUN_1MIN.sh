53c902c0bc84cab2e06f5a7e35759338a29f9c64
akrherz <akrherz@iastate.edu>
2018-07-26 09:31:53 -0500

    CTRE bridge sensors are disabled UFN

--- scripts/RUN_1MIN.sh
+++ scripts/RUN_1MIN.sh
@@ -14,7 +14,8 @@ cd ../ingestors
 python parse0002.py &
 python parse0007.py &
 python dot_plows.py &
-python ctre_bridge.py &
+# disabled upstream server 26 Jul 2018
+# python ctre_bridge.py &
 
 cd other
 python parse0006.py &
