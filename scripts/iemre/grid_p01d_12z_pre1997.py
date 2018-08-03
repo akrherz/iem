e999cb59cc1e9655db6ffd3abb74ca8008d368ec
akrherz <akrherz@iastate.edu>
2018-08-02 23:42:49 -0500

    use `verde` based gridder for pre-1997 IEMRE precip

--- scripts/iemre/grid_p01d_12z_pre1997.py
+++ scripts/iemre/grid_p01d_12z_pre1997.py
@@ -3,9 +3,10 @@ import sys
 import datetime
 
 import numpy as np
+import verde as vd
+import pyproj
 from metpy.units import units as mpunits
 from metpy.units import masked_array
-from metpy.gridding import interpolate
 from pandas.io.sql import read_sql
 from pyiem.iemre import get_daily_ncname, daily_offset
 from pyiem.util import ncopen, get_dbconn
@@ -15,20 +16,41 @@ def generic_gridder(day, nc, df, idx):
     """
     Generic gridding algorithm for easy variables
     """
-    bounds = dict(
-        west=nc.variables['lon'][0],
-        east=nc.variables['lon'][-1] + 0.125,
-        south=nc.variables['lat'][0],
-        north=nc.variables['lat'][-1] + 0.125,
+    data = df[idx].values
+    coordinates = (df['lon'].values, df['lat'].values)
+    region = [
+        nc.variables['lon'][0],
+        nc.variables['lon'][-1],
+        nc.variables['lat'][0],
+        nc.variables['lat'][-1],
+    ]
+    projection = pyproj.Proj(proj="merc", lat_ts=df['lat'].mean())
+    spacing = 0.5
+    chain = vd.Chain(
+        [
+            ("mean", vd.BlockReduce(np.mean, spacing=spacing * 111e3)),
+            ("spline", vd.Spline(damping=1e-10, mindist=100e3)),
+        ]
     )
-    # do our gridding
-    _gx, _gy, res = interpolate(
-        df['lon'].values, df['lat'].values,
-        df[idx].values, hres=0.125, boundary_coords=bounds
+    train, test = vd.train_test_split(
+        projection(*coordinates), data, random_state=0
     )
-    print(("%s %s rows for %s column min:%.3f max:%.3f"
-           ) % (day, len(df.index), idx, np.nanmin(res), np.nanmax(res)))
-    return masked_array(np.ma.array(res, mask=np.isnan(res)), mpunits('inch'))
+    chain.fit(*train)
+    score = chain.score(*test)
+    shape = (nc.dimensions['lat'].size, nc.dimensions['lon'].size)
+    grid = chain.grid(
+        region=region,
+        shape=shape,
+        projection=projection,
+        dims=["latitude", "longitude"],
+        data_names=["precip"],
+    )
+    res = grid.to_array()
+    res = np.ma.where(res < 0, 0, res)
+    print(("%s %s rows for %s column min:%.3f max:%.3f score: %.3f"
+           ) % (day, len(df.index), idx, np.nanmin(res),
+                np.nanmax(res), score))
+    return masked_array(res, mpunits('inch'))
 
 
 def main(argv):
