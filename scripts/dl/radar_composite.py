ace891795b8c7027017828dccaf4855e3b263ebf
akrherz <akrherz@iastate.edu>
2018-11-02 11:55:54 -0500

    dynamically flip between n0r/n0q for archived composite images

--- scripts/dl/radar_composite.py
+++ scripts/dl/radar_composite.py
@@ -1,22 +1,26 @@
 """Cache NEXRAD composites for the website."""
 import os
 import subprocess
-import datetime
 import sys
 
 import requests
-from pyiem.util import get_dbconn, exponential_backoff
+from pyiem.util import get_dbconn, exponential_backoff, utc
 
 
 TMPFN = '/tmp/radar_composite_tmp.png'
+N0QBASE = utc(2010, 11, 14)
 
 
-def save(sectorName, file_name, dir_name, tstamp, bbox=None, routes='ac'):
+def save(sectorName, file_name, dir_name, ts, bbox=None, routes='ac'):
     ''' Get an image and write it back to LDM for archiving '''
-    layers = ("layers[]=n0q&layers[]=watch_by_county&layers[]=sbw&"
-              "layers[]=uscounties")
+    tstamp = ts.strftime("%Y%m%d%H%M")
+    nexrad = "nexrad" if ts < N0QBASE else "n0q"
+    layers = ("layers[]=%s&layers[]=watch_by_county&layers[]=sbw&"
+              "layers[]=uscounties") % (nexrad, )
     if sectorName == 'conus':
-        layers = "layers[]=n0q&layers[]=watch_by_county&layers[]=uscounties"
+        layers = (
+            "layers[]=%s&layers[]=watch_by_county&layers[]=uscounties"
+        ) % (nexrad, )
     uri = ("http://iem.local/GIS/radmap.php?sector=%s&ts=%s&%s"
            ) % (sectorName, tstamp, layers)
     if bbox is not None:
@@ -44,12 +48,10 @@ def runtime(ts):
     pgconn = get_dbconn('postgis', user='nobody')
     pcursor = pgconn.cursor()
 
-    sts = ts.strftime("%Y%m%d%H%M")
-
-    save('conus', 'uscomp.png', 'usrad', sts)
-    save('iem', 'mwcomp.png', 'comprad', sts)
+    save('conus', 'uscomp.png', 'usrad', ts)
+    save('iem', 'mwcomp.png', 'comprad', ts)
     for i in ['lot', 'ict', 'sd', 'hun']:
-        save(i, '%scomp.png' % (i,), '%srad' % (i,), sts)
+        save(i, '%scomp.png' % (i,), '%srad' % (i,), ts)
 
     # Now, we query for watches.
     pcursor.execute("""
@@ -64,15 +66,17 @@ def runtime(ts):
         ymax = float(row[3]) + 1.5
         bbox = "%s,%s,%s,%s" % (xmin, ymin, xmax, ymax)
         sel = row[0].lower()
-        save('custom', '%scomp.png' % (sel,), '%srad' % (sel,), sts, bbox)
+        save('custom', '%scomp.png' % (sel,), '%srad' % (sel,), ts, bbox)
 
 
 def main(argv):
     """Go Main Go"""
-    ts = datetime.datetime.utcnow()
+    ts = utc()
     if len(argv) == 6:
-        ts = datetime.datetime(int(argv[1]), int(argv[2]),
-                               int(argv[3]), int(argv[4]), int(argv[5]))
+        ts = utc(
+            int(argv[1]), int(argv[2]), int(argv[3]),
+            int(argv[4]), int(argv[5])
+        )
     runtime(ts)
 
 
