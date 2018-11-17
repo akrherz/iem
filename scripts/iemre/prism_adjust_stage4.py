2628c1803122029c46b8ac40f3ad7068e8f80e08
akrherz <akrherz@iastate.edu>
2018-11-16 21:00:43 -0600

    instrument PRISM adjust stageIV for IEMRE

--- scripts/iemre/prism_adjust_stage4.py
+++ scripts/iemre/prism_adjust_stage4.py
@@ -6,11 +6,19 @@ Take the PRISM data, valid at 12z and bias correct the hourly stage IV data
 from __future__ import print_function
 import sys
 import datetime
+import logging
 
 import numpy as np
 from scipy.interpolate import NearestNDInterpolator
 from pyiem.iemre import daily_offset, hourly_offset
-from pyiem.util import utc, ncopen
+from pyiem import prism as prismutil
+from pyiem.util import utc, ncopen, find_ij
+
+DEBUGLON = -93.89
+DEBUGLAT = 42.04
+logging.basicConfig()
+LOG = logging.getLogger()
+LOG.setLevel(logging.DEBUG if sys.stdout.isatty() else logging.INFO)
 
 
 def workflow(valid):
@@ -28,6 +36,8 @@ def workflow(valid):
     lats = nc.variables['lat'][:]
     nc.close()
     (lons, lats) = np.meshgrid(lons, lats)
+    i, j = prismutil.find_ij(DEBUGLON, DEBUGLAT)
+    LOG.debug("prism debug point ppt: %.3f", ppt[j, i])
 
     # Interpolate this onto the stage4 grid
     nc = ncopen(("/mesonet/data/stage4/%s_stage4_hourly.nc"
@@ -36,16 +46,36 @@ def workflow(valid):
     p01m_status = nc.variables['p01m_status']
     s4lons = nc.variables['lon'][:]
     s4lats = nc.variables['lat'][:]
+    i, j = find_ij(s4lons, s4lats, DEBUGLON, DEBUGLAT)
     # Values are in the hourly arrears, so start at -23 and thru current hour
     sts_tidx = hourly_offset(valid - datetime.timedelta(hours=23))
     ets_tidx = hourly_offset(valid + datetime.timedelta(hours=1))
     s4total = np.sum(p01m[sts_tidx:ets_tidx, :, :], axis=0)
+    LOG.debug(
+        "stage4 s4total: %.3f lon: %.2f (%.2f) lat: %.2f (%.2f)",
+        s4total[i, j], s4lons[i, j], DEBUGLON, s4lats[i, j], DEBUGLAT
+    )
     # make sure the s4total does not have zeros
     s4total = np.where(s4total < 0.001, 0.001, s4total)
 
     nn = NearestNDInterpolator((lons.flatten(), lats.flatten()), ppt.flat)
     prism_on_s4grid = nn(s4lons, s4lats)
+    LOG.debug(
+        "shape of prism_on_s4grid: %s s4lons: %s ll: %.2f s4lats: %s ll: %.2f",
+        np.shape(prism_on_s4grid), np.shape(s4lons), s4lons[0, 0],
+        np.shape(s4lats), s4lats[0, 0]
+    )
     multiplier = prism_on_s4grid / s4total
+    LOG.debug(
+        "prism avg: %.3f stageIV avg: %.3f prismons4grid avg: %.3f mul: %.3f",
+        np.mean(ppt), np.mean(s4total), np.mean(prism_on_s4grid),
+        np.mean(multiplier)
+    )
+    LOG.debug(
+        "Boone IA0807 prism: %.3f stageIV: %.4f prismons4grid: %.3f mul: %.3f",
+        ppt[431, 746], s4total[i, j], prism_on_s4grid[i, j],
+        multiplier[i, j]
+    )
 
     # Do the work now, we should not have to worry about the scale factor
     for tidx in range(sts_tidx, ets_tidx):
@@ -53,7 +83,11 @@ def workflow(valid):
         # we threshold the s4total to at least 0.001, so we divide by 24 here
         # and denote that if the multiplier is zero, then we net zero
         newval = np.where(oldval < 0.001, 0.00004, oldval) * multiplier
-        p01m[tidx, :, :] = newval
+        nc.variables['p01m'][tidx, :, :] = newval
+        LOG.debug(
+            "adjust tidx: %s oldval: %.3f newval: %.3f",
+            tidx, oldval[i, j], newval[i, j]
+        )
         # make sure have data
         if np.ma.max(newval) > 0:
             p01m_status[tidx] = 2
@@ -62,19 +96,18 @@ def workflow(valid):
                    ) % ((datetime.datetime(valid.year, 1, 1, 0) +
                          datetime.timedelta(hours=tidx)
                          ).strftime("%Y-%m-%dT%H"), tidx))
-
+    nc.close()
     """
-    s4total_v2 = np.sum(p01m[sts_tidx:ets_tidx, :, :], axis=0)
-
+    # s4total_v2 = np.sum(p01m[sts_tidx:ets_tidx, :, :], axis=0)
     from pyiem.plot.geoplot import MapPlot
     import matplotlib.pyplot as plt
-    mp = MapPlot(sector='iowa')
-    mp.pcolormesh(s4lons, s4lats, s4total_v2,
-                  np.arange(-10, 11, 1), cmap=plt.get_cmap("BrBG"))
+    mp = MapPlot(sector='conus')
+    mp.pcolormesh(s4lons, s4lats, prism_on_s4grid,
+                  np.arange(0, 25, 2), cmap=plt.get_cmap("jet"))
+    mp.drawcounties()
     mp.postprocess(filename='test.png')
     mp.close()
     """
-    nc.close()
 
 
 def main(argv):
