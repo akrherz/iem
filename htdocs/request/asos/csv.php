037d6a17d07482c9ca11a4073e499467a6e151ae
akrherz <akrherz@iastate.edu>
2019-01-02 12:41:30 -0600

    fix year bounds issue in asos csv query

--- htdocs/request/asos/csv.php
+++ htdocs/request/asos/csv.php
@@ -1,7 +1,7 @@
 <?php 
 /* Generate a CSV file based on a request */
-include("../../../config/settings.inc.php");
-include("$rootpath/include/database.inc.php");
+require_once "../../../config/settings.inc.php";
+require_once "../../../include/database.inc.php";
 $mesosite = iemdb("mesosite");
 $access = iemdb("access");
 $asos = iemdb("asos");
@@ -31,24 +31,23 @@ while(list($k,$id) = each($stations))
 	if (isset($_REQUEST["date"])){
 		$ts = strtotime($_REQUEST["date"]);
 		if ($prepared == null){
-		$prepared = pg_prepare($asos, "SELECT", sprintf("SELECT station as id, valid,
-		max(tmpf) as tmpf, max(dwpf) as dwpf, max(sknt) as sknt, max(drct) as drct,
-		max(p01i) as phour, max(alti) as alti, max(gust) as gust, 
-		max(ST_x(s.geom)) as lon, max(ST_y(s.geom)) as lat from t%s t, stations s
-		where s.id = $1 and (s.network ~* 'ASOS' or s.network ~* 'AWOS')
-		and t.station = s.id and t.valid BETWEEN '%s'::date 
-		and '%s'::date + '9 days'::interval GROUP by station, valid 
-		ORDER by valid ASC", date("Y", $ts), 
-		date("Y-m-d", $ts), date("Y-m-d", $ts) ));
+		  $prepared = pg_prepare($asos, "SELECT", sprintf("SELECT station as id, valid,
+		    max(tmpf) as tmpf, max(dwpf) as dwpf, max(sknt) as sknt, max(drct) as drct,
+		    max(p01i) as phour, max(alti) as alti, max(gust) as gust, 
+		    max(ST_x(s.geom)) as lon, max(ST_y(s.geom)) as lat from alldata t, stations s
+		    where s.id = $1 and (s.network ~* 'ASOS' or s.network ~* 'AWOS')
+		    and t.station = s.id and t.valid BETWEEN '%s'::date 
+		    and '%s'::date + '9 days'::interval GROUP by station, valid 
+		    ORDER by valid ASC", date("Y-m-d", $ts), date("Y-m-d", $ts) ));
 		}
 		$rs = pg_execute($asos, "SELECT", Array($id));	
 	} else {
-  $rs = pg_exec($access, "SELECT s.id, valid, max(tmpf) as tmpf, max(dwpf) as dwpf, 
-  max(sknt) as sknt, max(drct) as drct,
-  max(phour) as phour, max(alti) as alti, max(gust) as gust,
-  max(ST_x(s.geom)) as lon, max(ST_y(s.geom)) as lat from current_log c, stations s
-  WHERE s.id = '$id' and s.iemid = c.iemid
-        GROUP by id, valid ORDER by valid ASC");
+    $rs = pg_exec($access, "SELECT s.id, valid, max(tmpf) as tmpf, max(dwpf) as dwpf, 
+      max(sknt) as sknt, max(drct) as drct,
+      max(phour) as phour, max(alti) as alti, max(gust) as gust,
+      max(ST_x(s.geom)) as lon, max(ST_y(s.geom)) as lat from current_log c, stations s
+      WHERE s.id = '$id' and s.iemid = c.iemid
+      GROUP by id, valid ORDER by valid ASC");
 	}
   if (pg_num_rows($rs) == 0){ continue; }
   for ($i=0;$row=@pg_fetch_array($rs,$i);$i++)
