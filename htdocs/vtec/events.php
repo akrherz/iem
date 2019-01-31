f30543e7e64937ea6e2f35fa6082b9eceb6cd07e
akrherz <akrherz@iastate.edu>
2019-01-31 12:35:30 -0600

    make VTEC events table sortable
    https://mesonet.agron.iastate.edu/vtec/events.php

--- htdocs/vtec/events.php
+++ htdocs/vtec/events.php
@@ -1,13 +1,13 @@
 <?php 
-include_once "../../config/settings.inc.php";
+require_once "../../config/settings.inc.php";
 define("IEM_APPID", 85);
 $wfo = isset($_GET["wfo"]) ? substr($_GET["wfo"], 0, 4): 'DMX';
 $year = isset($_GET["year"]) ? intval($_GET["year"]) : intval(date("Y"));
 
-include_once "../../include/myview.php";
-include_once "../../include/vtec.php";
-include_once "../../include/forms.php";
-include_once "../../include/imagemaps.php";
+require_once "../../include/myview.php";
+require_once "../../include/vtec.php";
+require_once "../../include/forms.php";
+require_once "../../include/imagemaps.php";
 
 $uri = sprintf("http://iem.local/json/vtec_events.py?wfo=%s&year=%s", 
 		$wfo, $year);
@@ -26,8 +26,17 @@ while(list($key, $val)=each($json['events'])){
 
 $t = new MyView();
 $t->title = "VTEC Event Listing for $wfo during $year";
-
-
+$t->headextra = <<<EOM
+<link type="text/css" href="/vendor/jquery-datatables/1.10.16/datatables.min.css" rel="stylesheet" />
+EOM;
+$t->jsextra = <<<EOM
+<script src='/vendor/jquery-datatables/1.10.16/datatables.min.js'></script>
+<script>
+$('#makefancy').click(function(){
+    $("#thetable table").DataTable();
+});
+</script>
+EOM;
 $yselect = yearSelect2(2005, $year, 'year');
 $wfoselect = networkSelect("WFO", $wfo, array(), "wfo");
 
@@ -56,13 +65,17 @@ directly access it here:
 		Select Year: $yselect
 		<input type="submit" value="Generate Table">
 </form>
-				
+
+<p><button id="makefancy">Make Table Interactive</button></p>
+
+<div id="thetable">
 <table class="table table-striped table-condensed">
 <thead><tr><th>WFO</th><th>Event ID</th><th>PH</th><th>SIG</th><th>Event</th>
  <th>Issue</th><th>Expire</th></tr>
 </thead>		
 {$table}
 </table>
+</div>
 
 EOF;
 $t->render("single.phtml");
