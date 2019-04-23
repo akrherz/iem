07afbfdebf4c71c3ed72d1808e80b30e31929592
akrherz <akrherz@iastate.edu>
2019-04-23 12:07:31 -0500

    fix HRRR solar rad run ordering

--- scripts/RUN_NOON.sh
+++ scripts/RUN_NOON.sh
@@ -9,9 +9,9 @@ python daily_analysis.py
 
 cd ../climodat
 python daily_estimator.py $(date +'%Y %m %d')
-python hrrr_solarrad.py $(date --date '1 days ago'  +'%Y %m %d')
 python daily_estimator.py $(date --date '1 days ago'  +'%Y %m %d')
 python compute_0000.py
+python hrrr_solarrad.py $(date --date '1 days ago'  +'%Y %m %d')
 
 cd ../prism
 python ingest_prism.py $(date --date '3 days ago' +'%Y %m %d')
