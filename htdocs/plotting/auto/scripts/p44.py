2acc818d561ba1ec7ded08c885dc5598b516a381
akrherz <akrherz@iastate.edu>
2019-05-10 09:43:13 -0500

    allow setting start/end year for acc warnings plot
    https://mesonet.agron.iastate.edu/plotting/auto/?q=44

--- htdocs/plotting/auto/scripts/p44.py
+++ htdocs/plotting/auto/scripts/p44.py
@@ -46,7 +46,11 @@ def get_description():
              default='TO', label='Select Watch/Warning Phenomena Type:'),
         dict(type='significance', name='significance',
              default='W', label='Select Watch/Warning Significance Level:'),
-
+        dict(type='year', name='syear', default=1986, min=1986,
+             label='Inclusive Start Year (if data is available) for plot:'),
+        dict(type='year', name='eyear', default=datetime.date.today().year,
+             min=1986,
+             label='Inclusive End Year (if data is available) for plot:'),
 
     ]
     return desc
@@ -64,6 +68,8 @@ def plotter(fdict):
     significance = ctx['significance'][:2]
     opt = ctx['opt']
     state = ctx['state'][:2]
+    syear = ctx['syear']
+    eyear = ctx['eyear']
 
     nt = NetworkTable('WFO')
     nt.sts['_ALL'] = {'name': 'All Offices'}
@@ -87,6 +93,8 @@ def plotter(fdict):
         SELECT extract(year from issue) as yr,
         issue, phenomena, significance, eventid, wfo from warnings WHERE
         ((phenomena = %s and significance = %s) """ + eventlimiter + """)
+        and extract(year from issue) >= %s and
+        extract(year from issue) <= %s
         and extract(doy from issue) <= %s """ + wfolimiter + """),
     agg1 as (
         SELECT yr, min(issue) as min_issue, eventid, wfo, phenomena,
@@ -97,10 +105,10 @@ def plotter(fdict):
         from agg1 GROUP by yr, doy)
     SELECT yr, doy, sum(count) OVER (PARTITION by yr ORDER by doy ASC)
     from agg2 ORDER by yr ASC, doy ASC
-    """, (phenomena, significance, lastdoy))
+    """, (phenomena, significance, syear, eyear, lastdoy))
 
     data = {}
-    for yr in range(1986, datetime.datetime.now().year + 1):
+    for yr in range(syear, eyear + 1):
         data[yr] = {'doy': [0], 'counts': [0]}
     rows = []
     for row in cursor:
@@ -108,21 +116,21 @@ def plotter(fdict):
         data[row[0]]['counts'].append(row[2])
         rows.append(dict(year=row[0], day_of_year=row[1], count=row[2]))
     # append on a lastdoy value so all the plots go to the end
-    for yr in range(1986, datetime.datetime.now().year):
+    for yr in range(syear, eyear):
         if len(data[yr]['doy']) == 1 or data[yr]['doy'][-1] >= lastdoy:
             continue
         data[yr]['doy'].append(lastdoy)
         data[yr]['counts'].append(data[yr]['counts'][-1])
-    if data[datetime.datetime.now().year]['doy']:
-        data[datetime.datetime.now().year]['doy'].append(
+    if data[eyear]['doy']:
+        data[eyear]['doy'].append(
             int(datetime.datetime.today().strftime("%j")) + 1)
-        data[datetime.datetime.now().year]['counts'].append(
-            data[datetime.datetime.now().year]['counts'][-1])
+        data[eyear]['counts'].append(
+            data[eyear]['counts'][-1])
     df = pd.DataFrame(rows)
 
     (fig, ax) = plt.subplots(1, 1, figsize=(8, 6))
     ann = []
-    for yr in range(1986, datetime.datetime.now().year + 1):
+    for yr in range(syear, eyear + 1):
         if len(data[yr]['doy']) < 2:
             continue
         lp = ax.plot(data[yr]['doy'], data[yr]['counts'], lw=2,
