c6e6baa62be8389f68d37b388d09127fea8f7afb
akrherz <akrherz@iastate.edu>
2019-07-02 21:16:57 -0500

    fix path to new CPC download script

--- scripts/RUN_NOON.sh
+++ scripts/RUN_NOON.sh
@@ -33,7 +33,9 @@ python compute_0000.py $(date --date '4 days ago' +'%Y %m %d')
 
 # CFS workflow, first two are for yesterday and we actually run for two
 # days ago
-cd ../dl
+cd ../cache
 sh download_cpc.sh
+
+cd ../dl
 python download_cfs.py && cd ../yieldfx && python cfs2iemre_netcdf.py && python cfs_tiler.py
 python cfs_tiler_2019.py
