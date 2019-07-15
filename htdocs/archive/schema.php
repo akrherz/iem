a51df18d15c4f2939c7e290eafd4a1111e0046ce
akrherz <akrherz@iastate.edu>
2019-07-15 13:30:46 -0500

    improve archive schema doc page
    https://mesonet.agron.iastate.edu/archive/schema.php

--- htdocs/archive/schema.php
+++ htdocs/archive/schema.php
@@ -1,6 +1,6 @@
 <?php 
-include("../../config/settings.inc.php");
-include("../../include/myview.php");
+require_once "../../config/settings.inc.php";
+require_once "../../include/myview.php";
 $t = new MyView();
 define("IEM_APPID", 20);
 $t->thispage = "archive-schema";
@@ -8,15 +8,24 @@ $t->title = "Archived Data Schema";
 
 $url = sprintf("https://mesonet.agron.iastate.edu/archive/data/%s", gmdate('Y/m/d'));
 $u = sprintf("<a href=\"%s\">%s</a>\n", $url, $url);
-$d = 
+
+$url2 = sprintf("http://mtarchive.geol.iastate.edu/%s", gmdate('Y/m/d'));
+$u2 = sprintf("<a href=\"%s\">%s</a>\n", $url2, $url2);
+
 
 $t->content = <<<EOF
 
 <h3>Archived Data Schema</h3>
 
-<p>The IEM stores
-most of its archive in a web accessible directory tree structure.  Here is
-the storage schema for today's data:</p>
+<p>The IEM maintains two directly web-accessible trees of archived weather
+products.  This page provides a crude schema of what products are archived and
+where within the directory tree you can find them.  The reason there are
+two archives is that the products have different funding / support resources
+and legacy.</p>
+
+<p>The products are generally organized by UTC date, but some of the products
+found are for the local/Iowa calendar date.</p>
+
 <pre>
 ROOT={$u}
 |- base folder full of various images for the date 
@@ -26,12 +35,14 @@ ROOT={$u}
       |- KCCI LiveSuper DopplerHD radar imagery
    |- sat
       |- GOES Conus Satellite imagery TIF
-   |- uscomp
-      |- CONUS NEXRAD base reflectivity ~1km
-|- bufkit
-   |- BUFKIT files for various model runs for many sites in/near Iowa
+   |- {akcomp,hicomp,prcomp,uscomp}
+      |- NEXRAD composites
+   | - hrrr reflectivity imagery
+   | - mrms imagery
+   | - ifc Iowa Flood Center precipitation imagery
+   | - ridge single site NEXRAD RADAR imagery 
 |- camera
-   |- Per webcam site 5minute imagery
+   |- Per webcam site 5 minute interval imagery
 |- comprad
    |- Iowa pre-generated NEXRAD imagery w/ warnings and watches
 |- hunrad
@@ -57,6 +68,17 @@ ROOT={$u}
    |- Pre-generated NEXRAD imagery w/ warnings and watches United States
 |- stage4
    |- Archive of the NCEP Stage4 Precipitation product in Grib format
+</pre>
+
+<pre>
+ROOT={$u2}
+|- base folder full of various images for the date
+   | - bufkit :: Archive of BUFKIT data files for a RAP, GFS, NAM, SREF models
+   | - cod :: GOES 16+17 Imagery from College of Dupage
+   | - gempak :: some NWS/NCEP data in GEMPAK format
+   | - grib2 :: limited assortment of NCEP/NWS grib2 products
+   | - mrms :: Grib2 NCEP MRMS products
+   | - text :: some NWS data in text format
 </pre>
 
 EOF;
