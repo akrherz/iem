5aa1dbcf74605487f1c8f6b6c15422a7a04a8ab9
akrherz <akrherz@iastate.edu>
2019-09-06 15:48:44 -0500

    plot improvements

--- htdocs/GIS/apps/agclimate/month.php
+++ htdocs/GIS/apps/agclimate/month.php
@@ -1,21 +1,22 @@
 <?php
-include("../../../../config/settings.inc.php");
-include_once "../../../../include/iemmap.php";
-include("../../../../include/database.inc.php");
+require_once "../../../../config/settings.inc.php";
+require_once "../../../../include/iemmap.php";
+require_once "../../../../include/database.inc.php";
+require_once "../../../../include/network.php";
 $dbconn = iemdb("isuag");
 $dvar = isset($_GET["dvar"]) ? $_GET["dvar"] : "rain_mm_tot";
 
-$title = Array("rain_mm_tot" => "Rainfall (inches)", 
-		"dailyet" => "Potential Evapotrans. (in)");
+$title = Array(
+    "rain_mm_tot" => "Rainfall (inches)", 
+	"dailyet" => "Potential Evapotrans. (in)");
 if (!array_key_exists($dvar, $title)) die();
 
-$rs = pg_prepare($dbconn, "SELECT", "select station, sum($dvar) as s,
-		min(valid), max(valid) from sm_daily 
-   WHERE extract(month from valid) = $1 and
-         extract(year from valid) = $2 GROUP by station");
+$rs = pg_prepare($dbconn, "SELECT", "select station, sum({$dvar}_qc) as s,
+    min(valid) as min_valid, max(valid) as max_valid from sm_daily 
+    WHERE extract(month from valid) = $1 and
+    extract(year from valid) = $2 GROUP by station");
 
 
-include("../../../../include/network.php");
 $nt = new NetworkTable("ISUSM");
 $ISUAGcities = $nt->table;
 
@@ -25,12 +26,11 @@ $day = isset($_GET["day"]) ? $_GET["day"]: date("d", time() - 86400 - (7 * 3600)
 $date = isset($_GET["date"]) ? $_GET["date"]: $year ."-". $month ."-". $day;
 
 $direct = isset($_GET["direct"]) ? $_GET['direct']: "";
-$ets = strtotime($date);
 $sts = mktime(0,0,0,$month,1,$year);
 
 $myStations = $ISUAGcities;
-$height = 480;
-$width = 640;
+$height = 768;
+$width = 1024;
 
 $map = ms_newMapObj("../../../../data/gis/base26915.map");
 $map->setProjection("init=epsg:26915");
@@ -62,11 +62,18 @@ $iards->draw($img);
 $bar640t->draw($img);
 
 $rs = pg_execute($dbconn, "SELECT", Array($month, $year));
-
+$minvalid = null;
+$maxvalid = null;
 for ($i=0; $row = @pg_fetch_assoc($rs,$i); $i++) {
   $key = $row["station"];
-  if ($key == "A133259" or $key == "A130219") continue;
+  if ($key == "AMFI4" or $key == "AHTI4") continue;
 
+  if ($minvalid == null || $row["min_valid"] < $minvalid){
+      $minvalid = $row["min_valid"];
+  }
+  if ($maxvalid == null || $row["max_valid"] > $maxvalid){
+    $maxvalid = $row["max_valid"];
+}
   $val = round($row["s"] / 25.4,2);
   $sdate = strtotime( $row["min"] );
   $edate = strtotime( $row["max"] );
@@ -85,14 +92,15 @@ for ($i=0; $row = @pg_fetch_assoc($rs,$i); $i++) {
   $pt = ms_newPointObj();
   $pt->setXY($ISUAGcities[$key]['lon'], $ISUAGcities[$key]['lat'], 0);
   if ($key == "A131909" || $key == "A130209"){
-    $pt->draw($map, $snet, $img, 1, $ISUAGcities[$key]['name'] );
+    $pt->draw($map, $snet, $img, 1, $ISUAGcities[$key]['plot_name'] );
   } else {
-    $pt->draw($map, $snet, $img, 1, $ISUAGcities[$key]['name'] );
+    $pt->draw($map, $snet, $img, 1, $ISUAGcities[$key]['plot_name'] );
   }
 }
-
-
-iemmap_title($map, $img, $title[$dvar] ." [ ". date("d M", $sts) ." thru ". date("d M Y", $ets) ." ]");
+$minvalid = strtotime($minvalid);
+$maxvalid = strtotime($maxvalid);
+iemmap_title($map, $img, $title[$dvar] ." [ ".
+    date("d M", $minvalid) ." thru ". date("d M Y", $maxvalid) ." ]");
 $map->drawLabelCache($img);
 
 
