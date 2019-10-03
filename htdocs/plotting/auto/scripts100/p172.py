d97091dc55c9769e11c3849ac528ce070f5c11d6
akrherz <akrherz@iastate.edu>
2019-10-03 08:26:10 -0500

    update ytd precip plot to do arb periods
    https://mesonet.agron.iastate.edu/plotting/auto/?q=172

--- htdocs/plotting/auto/scripts100/p172.py
+++ htdocs/plotting/auto/scripts100/p172.py
@@ -1,5 +1,4 @@
-"""YTD precip"""
-import calendar
+"""accumulated precip."""
 import datetime
 
 from pandas.io.sql import read_sql
@@ -16,7 +15,14 @@ def get_description():
     precipitation for a station of your choice.  The year with the highest and
     lowest accumulation is shown along with the envelop of observations and
     long term average.  You can optionally plot up to three additional years
-    of your choice.
+    of your choice.</p>
+
+    <p>You can specify the start date (ignore the year 2000) for when to start
+    the 365 day accumulation of precipitation.  If you pick a date after
+    1 July, the year plotted will represent the next calendar year.  For
+    example picking the start date of 1 Oct 2000, the year 2020 plotted would
+    represent the period 1 Oct 2019 thru 30 Sep 2020.  This is intended to
+    emulate the water year nomenclature.
     """
     thisyear = datetime.date.today().year
     desc['arguments'] = [
@@ -28,6 +34,11 @@ def get_description():
              label='Additional Year to Plot: (optional)'),
         dict(type='year', name='year3', optional=True, default=(thisyear - 2),
              label='Additional Year to Plot: (optional)'),
+        dict(
+            type='date', name='sdate', default='2000/01/01',
+            min='2000/01/01', max='2000/12/31',
+            label='Start Day of Year for Plot: (ignore year)'
+        )
         ]
     return desc
 
@@ -37,38 +48,53 @@ def plotter(fdict):
     pgconn = get_dbconn('coop')
     ctx = get_autoplot_context(fdict, get_description())
     station = ctx['station']
+    ab = ctx['_nt'].sts[station]['archive_begin']
+    if ab is None:
+        raise NoDataFound("Unknown station metadata.")
     year1 = ctx.get('year1')
     year2 = ctx.get('year2')
     year3 = ctx.get('year3')
+    sdate = ctx['sdate']
     table = "alldata_%s" % (station[:2],)
+    delta = 1 if sdate.month > 6 else 0
     df = read_sql("""
-        WITH years as (SELECT distinct year from """ + table + """
-            WHERE station = %s and sday = '0101')
-        SELECT day, sday, year, precip,
+        WITH years as (
+            SELECT distinct year + %s as myyear from """ + table + """
+            WHERE station = %s and sday = %s),
+        obs as (
+            SELECT day, precip,
+            case when sday >= %s then year + %s else year end as year
+            from """ + table + """ WHERE station = %s and precip is not null
+        )
+        SELECT day, year, precip,
+        row_number() OVER (PARTITION by year ORDER by day ASC) as row,
         sum(precip) OVER (PARTITION by year ORDER by day ASC) as accum from
-        """ + table + """ WHERE station = %s
-        and year in (select year from years)
+        obs WHERE year in (select myyear from years)
         ORDER by day ASC
-    """, pgconn, params=(station, station), index_col='day')
+    """, pgconn, params=(
+        delta, station, sdate.strftime("%m%d"), sdate.strftime("%m%d"), delta,
+        station),
+                  index_col='day')
     if df.empty:
         raise NoDataFound("No data found!")
 
     (fig, ax) = plt.subplots(1, 1)
     # Average
-    jday = df[['sday', 'accum']].groupby('sday').mean()
+    jday = df[['row', 'accum']].groupby('row').mean()
+    jday['accum'].values[-1] = jday['accum'].values[-2]
     ax.plot(range(1, len(jday.index)+1), jday['accum'], lw=2, zorder=5,
             color='k', label='Average - %.2f' % (jday['accum'].iloc[-1],))
 
     # Min and Max
-    jmin = df[['sday', 'accum']].groupby('sday').min()
-    jmax = df[['sday', 'accum']].groupby('sday').max()
+    jmin = df[['row', 'accum']].groupby('row').min()
+    jmax = df[['row', 'accum']].groupby('row').max()
     ax.fill_between(range(1, len(jday.index)+1), jmin['accum'],
                     jmax['accum'], zorder=2, color='tan')
 
     # find max year
     plotted = []
     for year, color in zip([df['accum'].idxmax().year,
-                            df[df['sday'] == '1231']['accum'].idxmin().year,
+                            df[df['row'] == 365]['accum'].idxmin().year,
                             year1, year2, year3],
                            ['b', 'brown', 'r', 'g', 'purple']):
         if year is None or year in plotted:
@@ -79,24 +105,30 @@ def plotter(fdict):
                 label='%s - %.2f' % (year, df2['accum'].iloc[-1]),
                 color=color, lw=2)
 
-    ab = ctx['_nt'].sts[station]['archive_begin']
-    if ab is None:
-        raise NoDataFound("Unknown station metadata.")
-    ax.set_title(("Year to Date Accumulated Precipitation\n"
+    ax.set_title(("Accumulated Precipitation after %s\n"
                   "[%s] %s (%s-%s)"
-                  ) % (station, ctx['_nt'].sts[station]['name'],
+                  ) % (sdate.strftime("%-d %B"), station,
+                       ctx['_nt'].sts[station]['name'],
                        ab.year,
                        datetime.date.today().year))
     ax.set_ylabel("Precipitation [inch]")
     ax.grid(True)
     ax.legend(loc=2)
-    ax.set_xlim(1, 366)
-    ax.set_xticks((1, 32, 60, 91, 121, 152, 182, 213, 244, 274,
-                   305, 335, 365))
-    ax.set_xticklabels(calendar.month_abbr[1:])
+    xticks = []
+    xticklabels = []
+    for i in range(366):
+        date = sdate + datetime.timedelta(days=i)
+        if date.day != 1:
+            continue
+        xticks.append(i)
+        xticklabels.append(date.strftime("%b"))
+    ax.set_xlim(0, 367)
+    ax.set_ylim(bottom=-0.1)
+    ax.set_xticks(xticks)
+    ax.set_xticklabels(xticklabels)
 
     return fig, df
 
 
 if __name__ == '__main__':
-    plotter(dict())
+    plotter(dict(sdate='2000-10-01'))
