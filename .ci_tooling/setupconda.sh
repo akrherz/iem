8cada681a4660c44f5d387adc91bd7b73e7f7fd8
akrherz <akrherz@iastate.edu>
2019-10-22 13:14:08 -0500

    attempt to speed up travis-ci

---
+++ .ci_tooling/setupconda.sh
@@ -0,0 +1,6 @@
+. $HOME/miniconda$MINICONDA/etc/profile.d/conda.sh
+
+conda config --set quiet True --set always_yes yes --set changeps1 no
+conda config --prepend channels conda-forge
+conda config --set channel_priority strict
+conda activate prod
