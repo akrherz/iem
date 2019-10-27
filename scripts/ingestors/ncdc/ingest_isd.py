631c51d6b121677abcbd068e35a2dcd9d1b252c3
akrherz <akrherz@iastate.edu>
2019-10-26 21:00:30 -0500

    improve ISD ingest per #212

--- scripts/ingestors/ncdc/ingest_isd.py
+++ scripts/ingestors/ncdc/ingest_isd.py
@@ -1,20 +1,21 @@
-"""HMMMM
-"""
-from __future__ import print_function
+"""Ingest NCEI's ISH data into the IEM archives."""
 import sys
 import subprocess
 import os
 
 import tqdm
+import requests
 from pyiem.ncei import ds3505
-from pyiem.util import get_dbconn, utc
+from pyiem.util import get_dbconn, utc, exponential_backoff, logger
 
+LOG = logger()
 ADD_ONLY = True
+TMPDIR = "/mesonet/tmp"
 
 
 def main(argv):
     """Go"""
-    pgconn = get_dbconn('asos')
+    pgconn = get_dbconn("asos")
     airforce = int(argv[1])
     wban = int(argv[2])
     faa = argv[3]
@@ -22,63 +23,76 @@ def main(argv):
     year2 = int(argv[5])
     failedyears = []
     msgs = []
-    dbid = faa if len(faa) == 4 and faa[0] != 'K' else faa[1:]
+    dbid = faa if len(faa) == 4 and faa[0] != "K" else faa[1:]
     for year in tqdm.tqdm(range(year, year2)):
         sts = utc(year, 1, 1)
-        ets = sts.replace(year=year+1)
+        ets = sts.replace(year=year + 1)
         cursor = pgconn.cursor()
         lfn = "%06i-%05i-%s" % (airforce, wban, year)
-        if not os.path.isfile(lfn):
-            fn = "ftp://ftp.ncdc.noaa.gov/pub/data/noaa/%s/%s.gz" % (year, lfn)
-            subprocess.call("wget -q -O %s.gz %s" % (lfn, fn),
-                            shell=True)
-            if (not os.path.isfile(lfn+".gz") or
-                    os.path.getsize(lfn+".gz") == 0):
+        if not os.path.isfile("%s/%s" % (TMPDIR, lfn)):
+            uri = "https://www1.ncdc.noaa.gov/pub/data/noaa/%s/%s.gz" % (year, lfn)
+            req = exponential_backoff(requests.get, uri, timeout=30)
+            if req is None or req.status_code != 200:
+                LOG.info("Failed to fetch %s", uri)
                 failedyears.append(year)
                 continue
-            subprocess.call("gunzip %s.gz" % (lfn, ), shell=True,
-                            stderr=subprocess.PIPE)
+            with open("%s/%s.gz" % (TMPDIR, lfn), "wb") as fh:
+                fh.write(req.content)
+            subprocess.call(
+                "gunzip %s/%s.gz" % (TMPDIR, lfn), shell=True, stderr=subprocess.PIPE
+            )
         added = 0
         bad = 0
         removed = 0
         skipped = 0
+        current = []
+        if ADD_ONLY:
+            # build out our current obs
+            cursor.execute(
+                """
+                SELECT valid at time zone 'UTC'
+                from alldata where station = %s and
+                valid >= %s and valid < %s ORDER by valid ASC
+            """,
+                (dbid, sts, ets),
+            )
+            for row in cursor:
+                current.append(row[0].strftime("%Y%m%d%H%M"))
         # ignore any bad bytes, sigh
-        for line in open(lfn, errors='ignore'):
+        for line in open("%s/%s" % (TMPDIR, lfn), errors="ignore"):
             data = ds3505.parser(line.strip(), faa, add_metar=True)
             if data is None:
                 bad += 1
                 continue
             if added == 0 and not ADD_ONLY:
-                cursor.execute("""
+                cursor.execute(
+                    """
                     DELETE from alldata where station = %s
                     and valid >= %s and valid < %s
-                """, (dbid, sts, ets))
+                """,
+                    (dbid, sts, ets),
+                )
                 if cursor.rowcount > 0:
                     print("deleted %s rows for %s" % (cursor.rowcount, dbid))
                 removed = cursor.rowcount
-            if ADD_ONLY:
-                # check for existing ob
-                cursor.execute("""
-                    SELECT valid from alldata where station = %s and valid = %s
-                """, (dbid, data['valid']))
-                if cursor.rowcount > 0:
-                    skipped += 1
-                    continue
+            if ADD_ONLY and data["valid"].strftime("%Y%m%d%H%M") in current:
+                skipped += 1
+                continue
             res = ds3505.sql(cursor, faa, data)
             if res is None:
                 skipped += 1
             else:
                 added += 1
-        msgs.append(("  %s: %s added: %s removed: %s bad: %s"
-                     " skipped: %s") % (year, faa, added, removed, bad,
-                                        skipped))
+        msgs.append(
+            ("  %s: %s added: %s removed: %s bad: %s" " skipped: %s")
+            % (year, faa, added, removed, bad, skipped)
+        )
         cursor.close()
         pgconn.commit()
-        year += 1
-    print(" failed years: %s" % (failedyears, ))
-    print("\n".join(msgs))
+        os.unlink("%s/%s" % (TMPDIR, lfn))
+    LOG.info(" failed years: %s", failedyears)
+    LOG.info("\n".join(msgs))
 
 
-if __name__ == '__main__':
-    os.chdir("/mesonet/tmp")
+if __name__ == "__main__":
     main(sys.argv)
