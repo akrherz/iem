890f1e3da4208f3f71d7eb8c3c4e5d1b084f239f
akrherz <akrherz@iastate.edu>
2019-10-28 08:10:15 -0500

    sundry

--- scripts/ingestors/squaw/ingest_squaw.py
+++ scripts/ingestors/squaw/ingest_squaw.py
@@ -1,9 +1,10 @@
 """Process the Flow data provided by the USGS website"""
-from __future__ import print_function
 import re
 
 import requests
-from pyiem.util import get_dbconn
+from pyiem.util import get_dbconn, exponential_backoff, logger
+
+LOG = logger()
 
 
 def main():
@@ -11,24 +12,35 @@ def main():
     mydb = get_dbconn("squaw")
     cursor = mydb.cursor()
 
-    uri = ("http://waterdata.usgs.gov/ia/nwis/uv?dd_cd=01&format=rdb&"
-           "period=2&site_no=05470500")
+    uri = (
+        "http://waterdata.usgs.gov/ia/nwis/uv?dd_cd=01&format=rdb&"
+        "period=2&site_no=05470500"
+    )
 
-    data = requests.get(uri, timeout=30).text
+    req = exponential_backoff(requests.get, uri, timeout=30)
+    if req is None or req.status_code != 200:
+        LOG.info("failed to fetch %s", uri)
+        return
+    data = req.text
 
     tokens = re.findall(
-        "USGS\t([0-9]*)\t(....-..-.. ..:..)\t([CSDT]+)\t([0-9]*)", data)
+        "USGS\t([0-9]*)\t(....-..-.. ..:..)\t([CSDT]+)\t([0-9]*)", data
+    )
 
     for ob in tokens:
-        sql = "SELECT * from real_flow WHERE valid = '%s'" % (ob[1], )
+        sql = "SELECT * from real_flow WHERE valid = '%s'" % (ob[1],)
         cursor.execute(sql)
         if cursor.rowcount > 0:
             continue
-        if ob[3] != '':
+        if ob[3] != "":
             sql = """
                 INSERT into real_flow(gauge_id, valid, cfs)
                 VALUES (%s, '%s', %s)
-            """ % (ob[0], ob[1], ob[3])
+            """ % (
+                ob[0],
+                ob[1],
+                ob[3],
+            )
             cursor.execute(sql)
 
     cursor.close()
@@ -36,5 +48,5 @@ def main():
     mydb.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
