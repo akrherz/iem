b1ddec5275bb3a191058bdfa25671b8dfc76845c
akrherz <akrherz@iastate.edu>
2019-10-28 20:29:30 -0500

    drop wsgi file endings

--- htdocs/c/tile.py
+++ htdocs/c/tile.py
@@ -2,19 +2,23 @@ import os
 import sys
 
 tilecachepath, wsgi_file = os.path.split(__file__)
-sys.path.insert(0, '/opt/iem/include/python/')
-sys.path.insert(0, '/opt/iem/include/python/TileCache/')
+sys.path.insert(0, "/opt/iem/include/python/")
+sys.path.insert(0, "/opt/iem/include/python/TileCache/")
 
 from TileCache.Service import Service, wsgiHandler
-cfgfiles = (os.path.join(tilecachepath, "tilecache.cfg"))
+
+cfgfiles = os.path.join(tilecachepath, "tilecache.cfg")
 
 theService = {}
-def wsgiApp (environ, start_response):
+
+
+def wsgiApp(environ, start_response):
     global theService
 
-    cfgs  = cfgfiles
+    cfgs = cfgfiles
     if not theService:
         theService = Service.load(cfgs)
     return wsgiHandler(environ, start_response, theService)
+
 
 application = wsgiApp
--- htdocs/plotting/auto/meta.py
+++ htdocs/plotting/auto/meta.py
@@ -14,12 +14,15 @@ if BASEDIR not in sys.path:
 
 def get_timing(pidx):
     """Return an average plot generation time for this app"""
-    pgconn = get_dbconn('mesosite')
+    pgconn = get_dbconn("mesosite")
     cursor = pgconn.cursor()
-    cursor.execute("""
+    cursor.execute(
+        """
         SELECT avg(timing) from autoplot_timing where appid = %s
         and valid > (now() - '7 days'::interval)
-    """, (pidx, ))
+    """,
+        (pidx,),
+    )
     timing = cursor.fetchone()[0]
     return timing if timing is not None else -1
 
@@ -27,17 +30,17 @@ def get_timing(pidx):
 def generate_html(fields, pidx, appdata):
     """Fun to be had here!"""
     html = ""
-    for arg in appdata['arguments']:
-        html += "type: %s" % (arg['type'], )
+    for arg in appdata["arguments"]:
+        html += "type: %s" % (arg["type"],)
     return html
 
 
 def do_html(fields, pidx):
     """Generate the HTML interface for this autoplot."""
-    response_headers = [('Content-type', 'text/html'), ]
+    response_headers = [("Content-type", "text/html")]
     name = get_script_name(pidx)
-    if not os.path.isfile('%s/%s.py' % (BASEDIR, name)):
-        sys.stderr.write("autoplot/meta 404 %s\n" % (name, ))
+    if not os.path.isfile("%s/%s.py" % (BASEDIR, name)):
+        sys.stderr.write("autoplot/meta 404 %s\n" % (name,))
         status = "404 Not Found"
         output = ""
         return output.encode(), status, response_headers
@@ -46,34 +49,37 @@ def do_html(fields, pidx):
     # see how we are called, finally
     appdata = app.get_description()
     html = generate_html(fields, pidx, appdata)
-    return html, '200 OK', response_headers
+    return html, "200 OK", response_headers
 
 
 def get_script_name(pidx):
     """Return where this script resides, so we can load it!"""
     if pidx >= 200:
-        name = 'scripts200/p%s' % (pidx, )
+        name = "scripts200/p%s" % (pidx,)
     elif pidx >= 100:
-        name = 'scripts100/p%s' % (pidx, )
+        name = "scripts100/p%s" % (pidx,)
     else:
-        name = 'scripts/p%s' % (pidx,)
+        name = "scripts/p%s" % (pidx,)
     return name
 
 
 def do_json(pidx):
     """Do what needs to be done for JSON requests."""
-    status = '200 OK'
+    status = "200 OK"
     if pidx == 0:
         import scripts  # @UnresolvedImport
+
         data = scripts.data
     else:
         name = get_script_name(pidx)
-        if not os.path.isfile('%s/%s.py' % (BASEDIR, name)):
-            sys.stderr.write("autoplot/meta 404 %s\n" % (name, ))
+        if not os.path.isfile("%s/%s.py" % (BASEDIR, name)):
+            sys.stderr.write("autoplot/meta 404 %s\n" % (name,))
             status = "404 Not Found"
             output = ""
-            response_headers = [('Content-type', 'application/json'),
-                                ('Content-Length', str(len(output)))]
+            response_headers = [
+                ("Content-type", "application/json"),
+                ("Content-Length", str(len(output))),
+            ]
             return output, status, response_headers
         try:
             timing = get_timing(pidx)
@@ -82,25 +88,31 @@ def do_json(pidx):
         fp, pathname, description = imp.find_module(name)
         app = imp.load_module(name, fp, pathname, description)
         data = app.get_description()
-        data['mapbox'] = hasattr(app, 'mapbox')
-        data['highcharts'] = hasattr(app, 'highcharts')
-        data['timing[secs]'] = timing
+        data["mapbox"] = hasattr(app, "mapbox")
+        data["highcharts"] = hasattr(app, "highcharts")
+        data["timing[secs]"] = timing
 
         # Defaults
-        data['arguments'].append(dict(type='text', name='dpi', default='100',
-                                      label='Image Resolution (DPI)'))
+        data["arguments"].append(
+            dict(
+                type="text",
+                name="dpi",
+                default="100",
+                label="Image Resolution (DPI)",
+            )
+        )
     output = json.dumps(data)
 
-    response_headers = [('Content-type', 'application/json'), ]
+    response_headers = [("Content-type", "application/json")]
     return output, status, response_headers
 
 
 def application(environ, start_response):
     """Our Application!"""
     fields = parse_formvars(environ)
-    pidx = int(fields.get('p', 0))
-    fmt = fields.get('_fmt', 'json')
-    if fmt == 'html':
+    pidx = int(fields.get("p", 0))
+    fmt = fields.get("_fmt", "json")
+    if fmt == "html":
         output, status, response_headers = do_html(fields, pidx)
     else:
         output, status, response_headers = do_json(pidx)
@@ -108,6 +120,7 @@ def application(environ, start_response):
     start_response(status, response_headers)
     # json.dumps returns str, we need bytes here
     return [output.encode()]
+
 
 # from paste.exceptions.errormiddleware import ErrorMiddleware
 # application = ErrorMiddleware(application, debug=True)
