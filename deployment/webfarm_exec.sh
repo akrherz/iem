b5c311f63eb730c20f1a106c85c26633e3c7d50b
akrherz <akrherz@iastate.edu>
2020-02-04 11:57:58 -0600

    roll with python=3.8

--- deployment/webfarm_exec.sh
+++ deployment/webfarm_exec.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # Exec a script over the webfarm nodes
 
-MACHINES="iem12 iemvs100 iemvs101 iemvs102 iemvs103 iemvs104 iemvs105 iemvs106 iemvs107 iemvs108 iemvs109"
+MACHINES="iem14 iemvs100 iemvs101 iemvs102 iemvs103 iemvs104 iemvs105 iemvs106 iemvs107 iemvs108 iemvs109"
 for MACH in $MACHINES
 do
 	echo "--------------------- $MACH -----------------------------"
-	ssh root@$MACH $1
+	ssh root@${MACH}.local $1
 	echo
 done
