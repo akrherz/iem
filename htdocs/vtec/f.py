667fd0e648d67573126baee2891e97c476943c34
akrherz <akrherz@iastate.edu>
2020-02-18 21:08:35 -0600

    polish parsing

--- htdocs/vtec/f.py
+++ htdocs/vtec/f.py
@@ -1,15 +1,16 @@
 """Generate a web crawler friendly page."""
 import re
-import datetime
 
-import pytz
-from pyiem.util import get_dbconn, html_escape
+from pyiem.util import get_dbconn, html_escape, utc
 from pyiem.nws import vtec
 
 # sadly, I have a lot of links in the wild without a status?
 VTEC_RE = re.compile(
     r"^(?P<year>\d+)-(?P<op>O)-(?P<status>[A-Z]{3})?-?(?P<wfo4>[A-Z]{4})-"
     r"(?P<phenomena>[A-Z]{2})-(?P<significance>[A-Z])-(?P<eventid>\d+)$"
+)
+TIME_RE = re.compile(
+    r"(?P<year>\d+)-(?P<month>\d+)-(?P<day>\d+)T(?P<hour>\d+):(?P<minute>\d+)"
 )
 
 
@@ -37,8 +38,10 @@ def get_data(ctx):
         ),
     )
     row = cursor.fetchone()
-    ctx["report"] = html_escape(row[0])
-    ctx["desc"] = html_escape(row[1][:-2])
+    ctx["report"] = (
+        "" if row[0] is None else html_escape(row[0].replace("\001", ""))
+    )
+    ctx["desc"] = "" if row[1] is None else html_escape(row[1][:-2])
 
 
 def as_html(ctx):
@@ -78,7 +81,7 @@ def as_html(ctx):
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta http-equiv="refresh" content="0; /vtec/#%(v)s">
+<meta http-equiv="refresh" content="0; URL=/vtec/#%(v)s">
 <title>%(ogtitle)s</title>
 <meta property="og:title" content="%(ogtitle)s">
 <meta property="og:description" content="%(desc)s">
@@ -115,8 +118,16 @@ def get_context(url):
     get_data(ctx)
     ctx["valid"] = None
     if len(tokens) > 1:
-        valid = datetime.datetime.strptime(tokens[1][:16], "%Y-%m-%dT%H:%M")
-        ctx["valid"] = valid.replace(tzinfo=pytz.UTC)
+        m = TIME_RE.match(tokens[1])
+        if m:
+            d = m.groupdict()
+            ctx["valid"] = utc(
+                int(d["year"]),
+                int(d["month"]),
+                int(d["day"]),
+                int(d["hour"]),
+                int(d["minute"]),
+            )
     return ctx
 
 
