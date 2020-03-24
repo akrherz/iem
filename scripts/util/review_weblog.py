0f8167d88d5cdf7352433c645e2a676f2fa4e9a8
akrherz <akrherz@iastate.edu>
2020-03-24 14:49:30 -0500

    run for IPV4 and IPV6

--- scripts/util/review_weblog.py
+++ scripts/util/review_weblog.py
@@ -2,20 +2,19 @@
 
 Run every minute, sigh.
 """
-import re
+import sys
 import subprocess
 
 import psycopg2
 
-IPV4 = re.compile(r"^\d+\.\d+\.\d+\.\d+$")
 
-
-def logic(counts):
+def logic(counts, family):
     """Should we or should we not, that is the question."""
+    exe = "iptables" if family == 4 else "ip6tables"
     for addr, hits in counts.items():
         if len(hits) < 30:
             continue
-        cmd = "iptables -A INPUT -s %s -j DROP" % (addr,)
+        cmd = "%s -A INPUT -s %s -j DROP" % (exe, addr)
         print(cmd)
         for hit in hits[:10]:
             print("%s uri:|%s| ref:|%s|" % (hit[0], hit[2], hit[3]))
@@ -23,8 +22,9 @@ def logic(counts):
         subprocess.call(cmd, shell=True)
 
 
-def main():
+def main(argv):
     """Go Main Go."""
+    family = int(argv[1])  # either 4 or 6
     pgconn = psycopg2.connect(
         database="mesosite",
         host="iemdb-mesosite.local",
@@ -36,26 +36,30 @@ def main():
     cursor.execute(
         """
         SELECT valid, client_addr, uri, referer from weblog
-        WHERE http_status = 404 ORDER by valid ASC
-    """
+        WHERE http_status = 404 and family(client_addr) = %s
+        ORDER by valid ASC
+    """,
+        (family,),
     )
     valid = None
     counts = {}
     for row in cursor:
-        m = IPV4.match(row[1])
-        if not m:
-            continue
         d = counts.setdefault(row[1], [])
         d.append(row)
         valid = row[0]
 
     if valid is None:
         return
-    cursor.execute("""DELETE from weblog where valid <= %s""", (valid,))
+    cursor.execute(
+        """
+        DELETE from weblog where valid <= %s and family(client_addr) = %s
+        """,
+        (valid, family),
+    )
     cursor.close()
     pgconn.commit()
-    logic(counts)
+    logic(counts, family)
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
