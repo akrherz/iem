6fb3f1260fe448b608ef6c0335c994b7edbf6625
akrherz <akrherz@iastate.edu>
2020-04-20 08:13:29 -0500

    adjust LSR app export Excel file suffix

--- htdocs/lsr/exportexcel.php
+++ htdocs/lsr/exportexcel.php
@@ -1,6 +1,6 @@
 <?php
 header("Content-type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
-header("Content-Disposition: attachment; filename=\"export.xml\"");
+header("Content-Disposition: attachment; filename=\"export.xlsx\"");
 header("Expires: 0");
 header("Cache-Control: must-revalidate, post-check=0,pre-check=0");
 header("Pragma: public");
