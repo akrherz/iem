ea3fb429f87595f5f393995c2d1505b25a240db9
akrherz <akrherz@iastate.edu>
2020-05-12 14:34:47 -0500

    don't QC out my entire iemre data

--- scripts/iemre/daily_analysis.py
+++ scripts/iemre/daily_analysis.py
@@ -239,7 +239,7 @@ def grid_day12(ts, ds):
         LOG.debug("loaded %s rows from climodat database", len(df.index))
     # Require that high > low before any gridding, accounts for some COOP
     # sites that only report TOB and not 24 hour high/low
-    df.at[df["highdata"] > df["lowdata"], ["highdata", "lowdata"]] = None
+    df.at[df["highdata"] <= df["lowdata"], ["highdata", "lowdata"]] = None
     # plot(df)
 
     if len(df.index) > 4:
