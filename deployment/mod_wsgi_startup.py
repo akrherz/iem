0afaec2d25264a0894ddff16601f9e44650765df
akrherz <akrherz@iastate.edu>
2020-07-24 06:12:06 -0500

    set MPLCONFIGDIR to make matplotlib 3.3 happier

--- deployment/mod_wsgi_startup.py
+++ deployment/mod_wsgi_startup.py
@@ -3,6 +3,7 @@
 import os
 
 os.environ["PROJ_LIB"] = "/opt/miniconda3/envs/prod/share/proj"
+os.environ["MPLCONFIGDIR"] = "/var/cache/matplotlib"
 
 from pyiem.plot.use_agg import plt
 import pandas
