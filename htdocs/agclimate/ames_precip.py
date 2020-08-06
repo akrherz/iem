809aeec951d2a0342160a20d8fba1791ec9f15db
akrherz <akrherz@iastate.edu>
2020-08-06 08:55:02 -0500

    fix Ames dual tipping bucket diagnostic display
    https://mesonet.agron.iastate.edu/agclimate/ames_precip.phtml

--- htdocs/agclimate/ames_precip.py
+++ htdocs/agclimate/ames_precip.py
@@ -1,6 +1,7 @@
 """Report on our second tipping bucket"""
 from io import StringIO
 
+import pandas as pd
 from pandas.io.sql import read_sql
 from pyiem.util import get_dbconn
 
@@ -10,8 +11,8 @@ def application(_environ, start_response):
     pgconn = get_dbconn("iem")
     amsi4 = read_sql(
         """
-    SELECT to_char(day + '16 hours'::interval, 'YYYY-MM-DD HH:MI AM') as valid,
-    pday as coop
+    SELECT to_char(day + '16 hours'::interval, 'YYYY-MM-DD HH24:MI') as valid,
+    pday as coop, day
     from summary s JOIN stations t on (s.iemid = t.iemid)
     where t.id = 'AMSI4' and day > '2017-07-25' and pday >= 0
     ORDER by day ASC
@@ -23,19 +24,44 @@ def application(_environ, start_response):
     pgconn = get_dbconn("isuag")
     df = read_sql(
         """
-    SELECT to_char(valid, 'YYYY-MM-DD HH:MI AM') as valid,
-    rain_mm_tot / 25.4 as bucket1,
-    rain_mm_2_tot / 25.4 as bucket2 from sm_hourly where station = 'BOOI4'
+    SELECT to_char(valid, 'YYYY-MM-DD HH24:MI') as valid,
+    case when extract(hour from valid) > 16 then
+        date(valid + '8 hours'::interval) else date(valid) end as coop_date,
+    rain_mm_tot / 25.4 as bucket1_hourly,
+    coalesce(rain_in_2_tot, rain_mm_2_tot / 25.4) as bucket2_hourly
+    from sm_hourly where station = 'BOOI4'
     and valid > '2017-07-25' and (rain_mm_tot > 0 or rain_mm_2_tot > 0)
     ORDER by valid ASC
     """,
         pgconn,
         index_col="valid",
     )
+    aggobs = df.groupby("coop_date").sum(numeric_only=True)
+    aggobs.columns = [
+        s.replace("_hourly", "") + "_total" for s in aggobs.columns
+    ]
+    df = df.drop("coop_date", axis=1)
+
     df2 = amsi4.join(df, how="outer")
+    df2 = df2.merge(aggobs, how="outer", left_on="day", right_index=True)
+    df2 = df2.drop("day", axis=1)
+    df2 = df2.sort_index(ascending=True).reset_index()
+    df2["valid"] = pd.to_datetime(df2["index"]).dt.strftime("%Y-%m-%d %-I %p")
     sio = StringIO()
     df2.to_html(
-        sio, classes="table table-striped", na_rep="-", float_format="%.2f"
+        sio,
+        columns=[
+            "valid",
+            "coop",
+            "bucket1_total",
+            "bucket2_total",
+            "bucket1_hourly",
+            "bucket2_hourly",
+        ],
+        classes="table table-striped tableFixHead",
+        na_rep="-",
+        float_format="%.2f",
+        index=False,
     )
     sio.seek(0)
     headers = [("Content-type", "text/html")]
