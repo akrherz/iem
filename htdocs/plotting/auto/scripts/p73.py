8345af5ceb736ddb705ae32b848a4c61fa8d38f9
akrherz <akrherz@iastate.edu>
2020-08-21 11:32:54 -0500

    support state summaries for VTEC yearly counts

--- htdocs/plotting/auto/scripts/p73.py
+++ htdocs/plotting/auto/scripts/p73.py
@@ -7,8 +7,13 @@ import pyiem.nws.vtec as vtec
 from pyiem.plot.use_agg import plt
 from pyiem.util import get_autoplot_context, get_dbconn
 from pyiem.exceptions import NoDataFound
+from pyiem.reference import state_names
 
 PDICT = {"yes": "Limit Plot to Year-to-Date", "no": "Plot Entire Year"}
+PDICT2 = {
+    "wfo": "View by Single NWS Forecast Office",
+    "state": "View by State",
+}
 
 
 def get_description():
@@ -19,9 +24,24 @@ def get_description():
     desc[
         "description"
     ] = """This chart displays the number of products issued
-    by a NWS Office by year for a given watch, warning, or advisory of your
-    choice.  These numbers are based on IEM archives and are not official!"""
+    by a NWS Office  or state by year for a given watch, warning,
+    or advisory of your choice.  These numbers are based on IEM archives and
+    are not official!  The counting is summing up distinct events.  If one
+    tornado watch covered 40 counties, this would only count as 1 for this
+    plot.
+
+    <p>Since the year 2005 and 2008 are common start years for VTEC tracking of
+    various phenomena, when this app encounters those years as the starting
+    point of the plot, they are droppped from the display.
+    """
     desc["arguments"] = [
+        dict(
+            type="select",
+            name="opt",
+            default="wfo",
+            options=PDICT2,
+            label="View by WFO or State?",
+        ),
         dict(
             type="networkselect",
             name="station",
@@ -30,6 +50,7 @@ def get_description():
             label="Select WFO:",
             all=True,
         ),
+        dict(type="state", default="IA", name="state", label="Select State:"),
         dict(
             type="select",
             name="limit",
@@ -61,14 +82,21 @@ def plotter(fdict):
     limit = ctx["limit"]
     phenomena = ctx["phenomena"]
     significance = ctx["significance"]
+    opt = ctx["opt"]
+    state = ctx["state"]
 
     ctx["_nt"].sts["_ALL"] = {"name": "All Offices"}
 
-    wfo_limiter = (" and wfo = '%s' ") % (
-        station if len(station) == 3 else station[1:],
-    )
-    if station == "_ALL":
-        wfo_limiter = ""
+    if opt == "wfo":
+        wfo_limiter = (" and wfo = '%s' ") % (
+            station if len(station) == 3 else station[1:],
+        )
+        if station == "_ALL":
+            wfo_limiter = ""
+        title1 = "NWS %s" % (ctx["_nt"].sts[station]["name"],)
+    else:
+        wfo_limiter = f" and substr(ugc, 1, 2) = '{state}' "
+        title1 = state_names[state]
     doy_limiter = ""
     title = "Entire Year"
     if limit.lower() == "yes":
@@ -78,15 +106,17 @@ def plotter(fdict):
             "extract(doy from 'TODAY'::date) "
         )
 
+    desc = "wfo, "
+    if phenomena in ["TR", "HU"]:
+        desc = ""
+    if phenomena in ["SV", "TO"] and significance == "A":
+        desc = ""
     df = read_sql(
-        """
+        f"""
         with data as (
-            SELECT distinct extract(year from issue)::int as yr, wfo, eventid
+            SELECT distinct extract(year from issue)::int as yr, {desc} eventid
             from warnings where phenomena = %s and significance = %s
-            """
-        + wfo_limiter
-        + doy_limiter
-        + """)
+            {wfo_limiter} {doy_limiter})
 
         SELECT yr, count(*) from data GROUP by yr ORDER by yr ASC
       """,
@@ -96,15 +126,30 @@ def plotter(fdict):
     if df.empty:
         raise NoDataFound("Sorry, no data found!")
 
+    # Drop 2005 or 2008 if they are start years
+    if df["yr"].min() == 2005:
+        df = df[df["yr"] > 2005]
+    elif df["yr"].min() == 2008:
+        df = df[df["yr"] > 2008]
     (fig, ax) = plt.subplots(1, 1)
     ax.bar(df["yr"], df["count"], align="center")
     ax.set_xlim(df["yr"].min() - 0.5, df["yr"].max() + 0.5)
+    ymax = df["count"].max()
+    ax.set_ylim(top=(ymax * 1.2))
+    for _, row in df.iterrows():
+        ax.text(
+            row["yr"],
+            row["count"] + (ymax * 0.05),
+            str(row["count"]),
+            rotation=90,
+            ha="center",
+        )
     ax.grid(True)
     ax.set_ylabel("Yearly Count")
     ax.set_title(
-        ("NWS %s [%s]\n%s (%s.%s) Count")
+        ("%s [%s]\n%s (%s.%s) Count")
         % (
-            ctx["_nt"].sts[station]["name"],
+            title1,
             title,
             vtec.get_ps_string(phenomena, significance),
             phenomena,
