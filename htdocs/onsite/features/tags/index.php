63fd5537dfd6cdaf20060c1541eece9679ecd9ee
akrherz <akrherz@iastate.edu>
2020-10-19 07:49:13 -0500

    winter2021 is a thing now

--- htdocs/onsite/features/tags/index.php
+++ htdocs/onsite/features/tags/index.php
@@ -76,6 +76,7 @@ previous winter seasons that these maps are available for:
 	<li><a href="winter1718.html">Winter 2017-2018</a></li>
 	<li><a href="winter1819.html">Winter 2018-2019</a></li>
 	<li><a href="winter1920.html">Winter 2019-2020</a></li>
+	<li><a href="winter2021.html">Winter 2020-2021</a></li>
 </ul></p>
 
 EOM;
