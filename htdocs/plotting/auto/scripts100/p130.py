a8b4f3703ad8182830d5ad9ff347cffce1970fd6
akrherz <akrherz@iastate.edu>
2020-12-15 21:58:19 -0600

    retool as violin plots
    https://mesonet.agron.iastate.edu/plotting/auto/?q=130

--- htdocs/plotting/auto/scripts100/p130.py
+++ htdocs/plotting/auto/scripts100/p130.py
@@ -1,6 +1,8 @@
 """temps vs high and low"""
+import calendar
 
 import numpy as np
+import pandas as pd
 from pandas.io.sql import read_sql
 from pyiem.plot.use_agg import plt
 from pyiem.util import get_autoplot_context, get_dbconn
@@ -13,8 +15,10 @@ def get_description():
     desc["data"] = True
     desc[
         "description"
-    ] = """This chart displays the average high and low
-    temperature by month for days with or without snowcover reported.  There
+    ] = """This chart presents violin plots of the distribution of daily
+    high and low temperatures on dates with and without snow cover by month.
+    A given month is plotted when at least 1% of the days on record for the
+    month had snowcover.  There
     are a number of caveats due to the timing of the daily temperature and
     snow cover report.  Also with the quality of the snow cover data."""
     desc["arguments"] = [
@@ -35,17 +39,10 @@ def plotter(fdict):
     ctx = get_autoplot_context(fdict, get_description())
     station = ctx["station"].upper()
     table = "alldata_%s" % (station[:2],)
+    # Load all available data
     df = read_sql(
-        f"""
-        SELECT year, month,
-        avg(high) as avg_high_all, avg(low) as avg_low_all,
-        avg(case when snowd > 0 then high else null end) as avg_high_snow,
-        avg(case when snowd > 0 then low else null end) as avg_low_snow,
-        avg(case when snowd = 0 then high else null end) as avg_high_nosnow,
-        avg(case when snowd = 0 then low else null end) as avg_low_nosnow,
-        sum(case when snowd > 0 then 1 else 0 end) as coverdays
-        from {table} WHERE station = %s GROUP by year, month
-    """,
+        f"SELECT year, month, high, low, snowd from {table} WHERE "
+        "station = %s and snowd is not null",
         pgconn,
         params=(station,),
         index_col=None,
@@ -53,82 +50,97 @@ def plotter(fdict):
     if df.empty:
         raise NoDataFound("No data found.")
 
-    # Only use months that had at least one day of snowcover
-    df2 = df[df["coverdays"] > 0]
-    df3 = df2.groupby("month").mean()
+    # Consider months that have at least 3% of days with snowcover
+    total = df.groupby("month").count()
+    coverdf = df[df["snowd"] > 0]
+    nocoverdf = df[df["snowd"] == 0]
+    snowcover = coverdf.groupby("month").count()
+    freq = snowcover["snowd"] / total["snowd"]
+    monthsall = freq[freq > 0.03].index.values.tolist()
+    # Sort the months to place fall first
+    months = [x for x in monthsall if x > 6]
+    months2 = [x for x in monthsall if x < 7]
+    months.extend(months2)
 
     (fig, ax) = plt.subplots(2, 1)
+    fig = plt.figure()
+    ax = [
+        fig.add_axes([0.12, 0.56, 0.83, 0.32]),
+        fig.add_axes([0.12, 0.1, 0.83, 0.32]),
+    ]
+    colors = ["r", "b"]
+    res = []
     for i, lbl in enumerate(["high", "low"]):
-        ys = df3.loc[[11, 12, 1, 2, 3], "avg_%s_nosnow" % (lbl,)]
-        ax[i].bar(
-            np.arange(5) - 0.2,
-            ys.values,
-            width=0.4,
-            align="center",
-            label="Without Snowcover",
-            fc="brown",
-            zorder=4,
-        )
-        for x, y in enumerate(ys):
+        for j, month in enumerate(months):
+            vopts = dict(
+                positions=[j], showmeans=False, showextrema=False, widths=0.8
+            )
+            # LHS
+            vals = nocoverdf[nocoverdf["month"] == month][lbl]
+            v = ax[i].violinplot(vals, **vopts)
+            meanval = vals.mean()
+            ax[i].plot([j - 0.4, j], [meanval, meanval], c=colors[0])
             ax[i].text(
-                x - 0.2, y + 2, "%.0f" % (y,), ha="center", color="brown"
+                j, meanval, rf"{meanval:.0f}$^\circ$", c=colors[0], ha="right"
+            )
+            b = v["bodies"][0]
+            m = np.mean(b.get_paths()[0].vertices[:, 0])
+            b.get_paths()[0].vertices[:, 0] = np.clip(
+                b.get_paths()[0].vertices[:, 0], -np.inf, m
+            )
+            b.set_color(colors[0])
+
+            # RHS
+            vals = coverdf[coverdf["month"] == month][lbl]
+            v = ax[i].violinplot(vals, **vopts)
+            meanval2 = vals.mean()
+            ax[i].plot([j, j + 0.4], [meanval2, meanval2], c=colors[1])
+            ax[i].text(
+                j, meanval2, rf"{meanval:.0f}$^\circ$", c=colors[1], ha="left"
+            )
+            b = v["bodies"][0]
+            m = np.mean(b.get_paths()[0].vertices[:, 0])
+            b.get_paths()[0].vertices[:, 0] = np.clip(
+                b.get_paths()[0].vertices[:, 0], m, np.inf
+            )
+            b.set_color(colors[1])
+            res.append(
+                {
+                    "month": month,
+                    f"{lbl}_withoutsnow": meanval,
+                    f"{lbl}_withsnow": meanval2,
+                }
             )
 
-        ys2 = df3.loc[[11, 12, 1, 2, 3], "avg_%s_snow" % (lbl,)]
-        ax[i].bar(
-            np.arange(5) + 0.2,
-            ys2.values,
-            width=0.4,
-            align="center",
-            label="With Snowcover",
-            fc="blue",
-            zorder=4,
-        )
-        for x, y in enumerate(ys2):
-            ax[i].text(
-                x + 0.2, y + 2, "%.0f" % (y,), ha="center", color="blue"
-            )
+        ax[i].set_xticks(range(len(months)))
+        ax[i].set_xticklabels([calendar.month_abbr[m] for m in months])
+        ax[i].grid(axis="y")
+        ax[i].axhline(32, ls="--", color="purple", alpha=0.8, lw=0.5)
+        ax[i].text(len(months) - 0.3, 32, r"32$^\circ$", color="purple")
 
-        ys3 = df3.loc[[11, 12, 1, 2, 3], "avg_%s_all" % (lbl,)]
-        ax[i].scatter(
-            np.arange(5),
-            ys3.values,
-            marker="s",
-            s=50,
-            zorder=5,
-            label="Overall",
-            c="yellow",
-        )
-        for x, y in enumerate(ys3):
-            ax[i].text(
-                x - 0.05,
-                y,
-                "%.0f" % (y,),
-                ha="right",
-                zorder=6,
-                va="top",
-                color="yellow",
-            )
-        ax[i].set_xticks(range(5))
-        ax[i].set_xticklabels(["Nov", "Dec", "Jan", "Feb", "Mar"])
-        ax[i].legend(ncol=3, fontsize=10)
-        ax[i].grid(True)
-        ax[i].set_ylim([(ys2.min() - 10), (ys.max() + 20)])
+    pr0 = plt.Rectangle((0, 0), 1, 1, fc="r")
+    pr1 = plt.Rectangle((0, 0), 1, 1, fc="b")
+    ax[0].legend(
+        (pr0, pr1),
+        ("Without Snow Cover", "With Snow Cover"),
+        ncol=2,
+        loc=(0.1, -0.35),
+    )
 
     ax[0].set_title(
-        ("%s [%s]\nSnow Cover Impact on Average Temp [%s-%s]")
+        ("%s [%s]\nDaily Temp Distributions by Month by Snow Cover[%s-%s]")
         % (
             ctx["_nt"].sts[station]["name"],
             station,
-            df2["year"].min(),
-            df2["year"].max(),
+            df["year"].min(),
+            df["year"].max(),
         )
     )
-    ax[0].set_ylabel(r"Avg High Temp $^\circ$F")
-    ax[1].set_ylabel(r"Avg Low Temp $^\circ$F")
+    ax[0].set_ylabel(r"Daily High Temp $^\circ$F")
+    ax[1].set_ylabel(r"Daily Low Temp $^\circ$F")
 
-    return fig, df
+    return fig, pd.DataFrame(res)
 
 
 if __name__ == "__main__":
-    plotter(dict())
+    plotter(dict(station="IATDSM", network="IACLIMATE"))
