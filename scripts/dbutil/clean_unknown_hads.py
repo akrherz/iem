66d9d704c726dbb5bde061213c30565332368955
akrherz <akrherz@iastate.edu>
2021-01-28 12:50:25 -0600

    add product_id to debug report

--- scripts/dbutil/clean_unknown_hads.py
+++ scripts/dbutil/clean_unknown_hads.py
@@ -1,8 +1,8 @@
 """
- I look at the unknown HADS table and see if any of these stations exist
- in the mesosite database, if so, then I set online to true!
+I look at the unknown HADS table and see if any of these stations exist
+in the mesosite database, if so, then I set online to true!
 
- Run from RUN_2AM.sh
+Run from RUN_2AM.sh
 """
 
 from pyiem.util import get_dbconn
@@ -18,32 +18,32 @@ def main():
 
     # look for unknown
     hcursor.execute(
-        "SELECT distinct nwsli, network from unknown WHERE length(nwsli) = 5"
+        "SELECT nwsli, network, max(product) from unknown "
+        "WHERE length(nwsli) = 5 GROUP by nwsli, network ORDER by nwsli ASC"
     )
     for row in hcursor:
         nwsli = row[0]
         network = row[1]
         mcursor.execute("SELECT online from stations where id = %s", (nwsli,))
-        row = mcursor.fetchone()
-        if row is None:
+        row2 = mcursor.fetchone()
+        if row2 is None:
             continue
-        elif not row[0]:
+        if not row2[0]:
             print(
-                ("Site %s [%s] was unknown, but is in mesosite")
-                % (nwsli, network)
+                ("Site %s [%s] %s was unknown, but is in mesosite")
+                % (nwsli, network, row[2])
             )
             mcursor.execute(
                 "update stations SET online = 't' where id = %s "
                 "and online = 'f'",
                 (nwsli,),
             )
-            hcursor2.execute("DELETE from unknown where nwsli = %s", (nwsli,))
         else:
             print(
-                ("Site %s [%s] was unknown, but online in DB?")
-                % (nwsli, network)
+                ("Site %s [%s] %s was unknown, but online in DB?")
+                % (nwsli, network, row[2])
             )
-            hcursor2.execute("DELETE from unknown where nwsli = %s", (nwsli,))
+        hcursor2.execute("DELETE from unknown where nwsli = %s", (nwsli,))
 
     hcursor2.close()
     hads.commit()
