d1081733e61f20f9a7e98af9aeaec1eb0153b52f
akrherz <akrherz@iastate.edu>
2021-03-04 11:59:44 -0600

    backend IEM River by web service
    https://mesonet.agron.iastate.edu/river/

--- htdocs/river/index.php
+++ htdocs/river/index.php
@@ -1,7 +1,6 @@
 <?php
 require_once "../../config/settings.inc.php";
 define("IEM_APPID", 71);
-require_once "../../include/database.inc.php";
 require_once "../../include/forms.php";
 require_once "../../include/imagemaps.php";
 require_once "../../include/myview.php";
@@ -14,7 +13,6 @@ $content = "";
 $wfo = isset($_GET["wfo"]) ? substr($_GET["wfo"],0,3): "DMX";
 $state = isset($_GET["state"]) ? substr($_GET["state"],0,2): "IA";
 
-$postgis = iemdb("postgis");
 $sevcol = Array(
  "N" =>"#0f0",
  "0" =>"#ff0",
@@ -23,62 +21,29 @@ $sevcol = Array(
  "3" =>"#f0f",
  "U" =>"#fff");
 
-$nwsli_limiter = "";
-if (isset($_GET["dvn"])){
- $nwsli_limiter = "and hvtec_nwsli IN ('FLTI2', 'CMMI4', 'LECI4', 'RCKI2', 'ILNI2', 
- 'MUSI4', 'NBOI2', 'KHBI2', 'DEWI4', 'CNEI4', 'CJTI4', 'WAPI4','LNTI4', 'CMOI2', 
- 'JOSI2', 'MLII2','GENI2')";
- $content .= "<style>
-body {
-  background: #fff;
-  font-size: smaller;
-}
-</style>";
-}
-
-$thisyear = date("Y");
-$statelimiter = "";
-$wfolimiter = "";
+$url = "https://mesonet.agron.iastate.edu/api/1/nws/current_flood_warnings.json";
 $c1 = "";
 $c2 = "";
 $c3 = "";
 if (isset($_REQUEST["state"])){
     $c2 = " well";
-    $statelimiter = sprintf(" and substr(ugc, 1, 2) = '%s' ", $state);
+    $url .= "?state={$state}";
     $ptitle = "<h3>River Forecast Point Monitor by State</h3>";
 } else if (isset($_REQUEST["all"])){
     $c3 = " well";
     $ptitle = "<h3>River Forecast Point Monitor (view all)</h3>";
 } else {
     $c1 = " well";
-    $wfolimiter = sprintf(" and wfo = '%s' ", $wfo);
+    $url .= "?wfo={$wfo}";
     $ptitle = "<h3>River Forecast Point Monitor by NWS WFO</h3>";
 }
 
-$sql = <<<EOM
-WITH warns as (
-    SELECT w.hvtec_nwsli, w.ugc, w.gid, w.eventid, w.phenomena, w.wfo,
-    h.name, h.river_name
-    from warnings_{$thisyear} w JOIN hvtec_nwsli h
-        on (w.hvtec_nwsli = h.nwsli) WHERE
-    status NOT IN ('EXP','CAN') and phenomena = 'FL' and
-    significance = 'W' and  expire > now() $nwsli_limiter $statelimiter
-    $wfolimiter
-), names as (
-    SELECT hvtec_nwsli, sumtxt(u.name || ', ') as counties
-    from warns w, ugcs u WHERE w.gid = u.gid GROUP by hvtec_nwsli
-), agg as (
-    SELECT w.*, n.counties
-    from warns w JOIN names n on (w.hvtec_nwsli = n.hvtec_nwsli)
-)
-SELECT r.*, a.* from riverpro r JOIN agg a on (r.nwsli = a.hvtec_nwsli)
-ORDER by a.name ASC
-EOM;
-$rs = pg_query($postgis, $sql);
+$jdata = file_get_contents($url);
+$jobj = json_decode($jdata, $assoc=TRUE);
 
 $rivers = Array();
 $used = Array();
-for($i=0;$row=pg_fetch_array($rs);$i++)
+foreach($jobj["data"] as $bogus => $row)
 {
     if (in_array($row["nwsli"], $used)) {
         continue;
@@ -110,6 +75,11 @@ processes the flood warning products and attempts to extract the important
 details regarding flood state, severity, forecasted stage and impact. By clicking
 on the graph icon near the location identfier, you are taken to an IEM Autoplot
 which shows forecasted stage and observations.</p>
+
+<p>Data presented here was provided by this <a href="{$url}">JSON Web Service</a>.
+Documentation on this webservice is
+<a href="/api/1/docs#/default/service_nws_current_flood_warnings__fmt__get">here</a>.</p>
+
 
 <h3>Three Ways to View Forecasts</h3>
 <div class="row">
