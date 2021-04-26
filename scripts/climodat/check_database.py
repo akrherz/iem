18b21803e89b769b9a0892526c8139b65fbf6128
akrherz <akrherz@iastate.edu>
2021-04-26 17:55:21 -0500

    drop set offline logic

--- scripts/climodat/check_database.py
+++ scripts/climodat/check_database.py
@@ -10,18 +10,6 @@ from pyiem.util import get_dbconn, logger
 LOG = logger()
 
 
-def set_offline(iemid):
-    """Set this station to offline stations."""
-    pgconn = get_dbconn("mesosite")
-    cursor = pgconn.cursor()
-    cursor.execute(
-        "UPDATE stations SET online = 'f' where iemid = %s",
-        (iemid,),
-    )
-    cursor.close()
-    pgconn.commit()
-
-
 def main(argv):
     """Go Main"""
     state = argv[1]
@@ -48,25 +36,13 @@ def main(argv):
         if missing.empty:
             continue
         LOG.info(
-            "station: %s [%s - %s], missing %s/%s days",
+            "station: %s [%s - %s], missing: %s has:%s days",
             station,
             gdf["day"].min(),
             gdf["day"].max(),
             len(missing),
             len(gdf.index),
         )
-        missing_ratio = len(missing) / float(len(gdf.index))
-        if (
-            len(gdf.index) > 3000
-            and missing_ratio > 0.33
-            and nt.sts[station]["online"]
-        ):
-            LOG.info(
-                "Online %s missing %.2f data, setting offline",
-                station,
-                missing_ratio,
-            )
-            set_offline(nt.sts[station]["iemid"])
 
         sio = StringIO()
         for day in missing:
