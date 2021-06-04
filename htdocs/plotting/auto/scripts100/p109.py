2a9a0b114ffc985ef225a6c3d4e1482478429be0
akrherz <akrherz@iastate.edu>
2021-06-04 10:37:01 -0500

    support count departures, ranks, and std departure
    https://mesonet.agron.iastate.edu/plotting/auto/?q=109

--- htdocs/plotting/auto/scripts100/p109.py
+++ htdocs/plotting/auto/scripts100/p109.py
@@ -1,22 +1,22 @@
 """WFO VTEC counts on a map."""
-from collections import OrderedDict
 import datetime
 
+import pandas as pd
 from pandas.io.sql import read_sql
 import numpy as np
 import pytz
 from pyiem.nws import vtec
 from pyiem.plot import MapPlot, get_cmap
-from pyiem.network import Table as NetworkTable  # This is needed.
 from pyiem.util import get_autoplot_context, get_dbconn
 
-PDICT = OrderedDict(
-    (
-        ("count", "Event Count"),
-        ("days", "Days with 1+ Events"),
-        ("tpercent", "Percent of Time"),
-    )
-)
+PDICT = {
+    "count": "Event Count",
+    "days": "Days with 1+ Events",
+    "tpercent": "Percent of Time",
+    "count_rank": "Rank of Event Count (1=lowest)",
+    "count_departure": "Departure from Average of Event Count",
+    "count_standard": "Standardized Departure from Average of Event Count",
+}
 
 
 def get_description():
@@ -37,7 +37,27 @@ def get_description():
         <li><strong>Percent of Time</strong>: This is the temporal coverage
         percentage within the period of interest.  Rewording, what percentage
         of the time was at least one event active.</li>
+        <li><strong>Rank of Event Count</strong>: The ranking from least to
+        greatest of the given period's event count vs period of record
+        climatology.</li>
+        <li><strong>Departure from Average of Event Count</strong>: The number
+        of events that the given period differs from the period of record
+        climatology.</li>
+        <li>
+        <strong>Standardized Departure from Average of Event Count</strong>:
+        The departure expressed in sigma units of this periods event total
+        vs the period of record climatology.</li>
     </ul></p>
+
+    <p><strong>PLEASE USE CAUTION</strong> with the departure from average
+    plots as they are not exactly straight forward to compute.  If you are
+    plotting small periods of time, individual events will heavily skew the
+    averages.  The exact start date of each event type is not an exact
+    science due to the implementation of some products and other complexities.
+    For Severe Thunderstorm, Tornado, Flash Flood, and Marine Warnings, the
+    IEM has a special accounting of these back to 2002.  For other event
+    types, the archive starts with the VTEC implementation, which varies by
+    product.</p>
 
     <p>Note that various VTEC events have differenting start periods of record.
     Most products go back to October 2005.</p>
@@ -98,63 +118,88 @@ def get_description():
     return desc
 
 
-def plotter(fdict):
-    """Go"""
-    pgconn = get_dbconn("postgis")
-    ctx = get_autoplot_context(fdict, get_description())
-    sts = ctx["sdate"]
-    sts = sts.replace(tzinfo=pytz.UTC)
-    ets = ctx["edate"]
-    ets = ets.replace(tzinfo=pytz.UTC)
-    p1 = ctx["phenomenav1"]
-    p2 = ctx["phenomenav2"]
-    p3 = ctx["phenomenav3"]
-    p4 = ctx["phenomenav4"]
-    varname = ctx["var"]
-    phenomena = []
-    for p in [p1, p2, p3, p4]:
-        if p is not None:
-            phenomena.append(p[:2])
-    s1 = ctx["significancev1"]
-    s2 = ctx["significancev2"]
-    s3 = ctx["significancev3"]
-    s4 = ctx["significancev4"]
-    significance = []
-    for s in [s1, s2, s3, s4]:
-        if s is not None:
-            significance.append(s[0])
+def get_count_df(ctx, pgconn, varname, pstr, sts, ets):
+    """Oh boy, do complex things."""
 
-    pstr = []
-    subtitle = ""
-    title = ""
-    for p, s in zip(phenomena, significance):
-        pstr.append("(phenomena = '%s' and significance = '%s')" % (p, s))
-        subtitle += "%s.%s " % (p, s)
-        title += vtec.get_ps_string(p, s)
-    if len(phenomena) > 1:
-        title = "VTEC Unique Event"
-    pstr = " or ".join(pstr)
-    pstr = "(%s)" % (pstr,)
-    cmap = get_cmap(ctx["cmap"])
+    if varname.startswith("count_"):
+        if (ets - sts).days > 366:
+            raise ValueError("Can't compute over period > 366 days")
+        sday = sts.strftime("%m%d")
+        eday = ets.strftime("%m%d")
+        slimiter = (
+            f" (to_char(issue, 'mmdd') >= '{sday}' and "
+            f"to_char(issue, 'mmdd') <= '{eday}' ) "
+        )
+        yearcol = "extract(year from issue)"
+        if eday <= sday:
+            slimiter = slimiter.replace(" and ", " or ")
+            yearcol = (
+                f"case when to_char(issue, 'mmdd') <= '{eday}' then "
+                "extract(year from issue)::int - 1 else "
+                "extract(year from issue) end"
+            )
 
-    if varname == "count":
+        # compute all the things.
         df = read_sql(
             f"""
-    with total as (
-    select distinct wfo, extract(year from issue at time zone 'UTC') as year,
-    phenomena, significance, eventid from warnings
-    where {pstr} and issue >= %s and issue < %s
-    )
+            with events as (
+                select distinct wfo, {yearcol} as year,
+                phenomena, eventid from warnings where {pstr} and
+                {slimiter} and issue > '2002-01-01')
+            select wfo, year::int as year, count(*) from events
+            group by wfo, year
+            """,
+            pgconn,
+            index_col=None,
+        )
+        # enlarge by wfo and year cartesian product
+        ctx["_subtitle"] = ", Period of Record: %.0f-%.0f" % (
+            df["year"].min(),
+            df["year"].max(),
+        )
+        idx = pd.MultiIndex.from_product(
+            [df["wfo"].unique(), df["year"].unique()],
+            names=["wfo", "year"],
+        )
+        df = df.set_index(["wfo", "year"]).reindex(idx).fillna(0).reset_index()
+        df["rank"] = df.groupby("wfo")["count"].rank(
+            method="min", ascending=True
+        )
+        thisyear = (
+            df[df["year"] == sts.year].set_index("wfo").drop("year", axis=1)
+        )
+        # Ready to construct final df.
+        df = df[["wfo", "count"]].groupby("wfo").agg(["mean", "std"]).copy()
+        df.columns = ["_".join(a) for a in df.columns.to_flat_index()]
+        df[["count", "count_rank"]] = thisyear[["count", "rank"]]
+        df["count_departure"] = df["count"] - df["count_mean"]
+        df["count_standard"] = df["count_departure"] / df["count_std"]
+    else:
+        df = read_sql(
+            f"""
+            with total as (
+            select distinct wfo,
+            extract(year from issue at time zone 'UTC') as year,
+            phenomena, significance, eventid from warnings
+            where {pstr} and issue >= %s and issue < %s
+            )
 
-    SELECT wfo, phenomena, significance, year, count(*) from total
-    GROUP by wfo, phenomena, significance, year
-        """,
+            SELECT wfo, year, count(*) from total
+            GROUP by wfo, year
+            """,
             pgconn,
             params=(sts, ets),
         )
+    return df
 
-        df2 = df.groupby("wfo")["count"].sum()
-        maxv = df2.max()
+
+def get_count_bins(df, varname):
+    """Figure out sensible bins."""
+    minv = df[varname].min()
+    maxv = df[varname].max()
+    if varname == "count_rank":
+        bins = np.arange(1, maxv + 2)
+    elif varname == "count":
         bins = [0, 1, 2, 3, 5, 10, 15, 20, 25, 30, 40, 50, 75, 100, 200]
         if maxv > 5000:
             bins = [
@@ -211,8 +256,72 @@ def plotter(fdict):
                 750,
                 1000,
             ]
-        units = "Count"
+    elif max([abs(minv), abs(maxv)]) > 100:
+        bins = [-200, -150, -100, -50, -25, -10, 0, 10, 25, 50, 100, 150, 200]
+    elif max([abs(minv), abs(maxv)]) > 10:
+        bins = [-100, -50, -25, -10, -5, 0, 5, 10, 25, 50, 100]
+    else:
+        bins = np.arange(-3, 3.1, 0.5)
+    return bins
+
+
+def plotter(fdict):
+    """Go"""
+    pgconn = get_dbconn("postgis")
+    ctx = get_autoplot_context(fdict, get_description())
+    sts = ctx["sdate"]
+    sts = sts.replace(tzinfo=pytz.UTC)
+    ets = ctx["edate"]
+    ets = ets.replace(tzinfo=pytz.UTC)
+    p1 = ctx["phenomenav1"]
+    p2 = ctx["phenomenav2"]
+    p3 = ctx["phenomenav3"]
+    p4 = ctx["phenomenav4"]
+    varname = ctx["var"]
+    phenomena = []
+    for p in [p1, p2, p3, p4]:
+        if p is not None:
+            phenomena.append(p[:2])
+    s1 = ctx["significancev1"]
+    s2 = ctx["significancev2"]
+    s3 = ctx["significancev3"]
+    s4 = ctx["significancev4"]
+    significance = []
+    for s in [s1, s2, s3, s4]:
+        if s is not None:
+            significance.append(s[0])
+
+    pstr = []
+    subtitle = ""
+    title = ""
+    for p, s in zip(phenomena, significance):
+        pstr.append("(phenomena = '%s' and significance = '%s')" % (p, s))
+        subtitle += "%s.%s " % (p, s)
+        title += vtec.get_ps_string(p, s)
+    if len(phenomena) > 1:
+        title = "VTEC Unique Event"
+    pstr = " or ".join(pstr)
+    pstr = "(%s)" % (pstr,)
+    cmap = get_cmap(ctx["cmap"])
+
+    extend = "neither"
+    if varname.startswith("count"):
+        df = get_count_df(ctx, pgconn, varname, pstr, sts, ets)
+
+        bins = get_count_bins(df, varname)
         lformat = "%.0f"
+        units = "Count"
+        if varname == "count":
+            extend = "max"
+        elif varname == "count_rank":
+            extend = "neither"
+            units = "Rank"
+        else:
+            if varname == "count_standard":
+                lformat = "%.1f"
+                units = "sigma"
+            extend = "both"
+        df2 = df[varname]
     elif varname == "days":
         df = read_sql(
             f"""
@@ -277,29 +386,40 @@ def plotter(fdict):
         units = "Percent"
         lformat = "%.1f"
 
-    nt = NetworkTable("WFO")
-    for sid in nt.sts:
-        sid = sid[-3:]
-        if sid not in df2:
-            df2[sid] = 0
-
     mp = MapPlot(
         sector="nws",
         axisbg="white",
+        twitter=True,
         title="%s %s by NWS Office" % (title, PDICT[varname]),
-        subtitle=("Valid %s - %s UTC, based on VTEC: %s")
+        subtitle=("Valid %s - %s UTC, based on VTEC: %s %s")
         % (
             sts.strftime("%d %b %Y %H:%M"),
             ets.strftime("%d %b %Y %H:%M"),
             subtitle,
+            ctx.get("_subtitle", ""),
         ),
     )
     mp.fill_cwas(
-        df2, bins=bins, ilabel=True, units=units, lblformat=lformat, cmap=cmap
+        df2,
+        bins=bins,
+        ilabel=True,
+        units=units,
+        lblformat=lformat,
+        cmap=cmap,
+        extend=extend,
+        labelbuffer=0,
     )
 
     return mp.fig, df
 
 
 if __name__ == "__main__":
-    plotter({"var": "tpercent", "phenomenav1": "SV", "significancev1": "W"})
+    plotter(
+        {
+            "sdate": "2021-05-01 0000",
+            "edate": "2021-06-01 0000",
+            "var": "count_rank",
+            "phenomenav1": "SV",
+            "significancev1": "W",
+        }
+    )
