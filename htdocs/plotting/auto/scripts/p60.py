766c31f5bfd5702e83c78cf038a6d3a02d7133af
akrherz <akrherz@iastate.edu>
2021-11-29 12:51:25 -0600

    enhancements and allow for arb date periods
    https://mesonet.agron.iastate.edu/plotting/auto/?q=60

--- htdocs/plotting/auto/scripts/p60.py
+++ htdocs/plotting/auto/scripts/p60.py
@@ -11,14 +11,12 @@ from pyiem.util import get_autoplot_context, get_dbconn
 from pyiem.exceptions import NoDataFound
 
 PDICT = {"above": "At or Above Threshold", "below": "Below Threshold"}
-PDICT2 = dict(
-    (
-        ("tmpf", "Air Temperature"),
-        ("dwpf", "Dew Point Temp"),
-        ("feel", "Feels Like Temp"),
-        ("relh", "Relative Humidity"),
-    )
-)
+PDICT2 = {
+    "tmpf": "Air Temperature",
+    "dwpf": "Dew Point Temp",
+    "feel": "Feels Like Temp",
+    "relh": "Relative Humidity",
+}
 
 
 def get_description():
@@ -30,7 +28,11 @@ def get_description():
     ] = """This plot presents the hourly frequency of having
     a certain temperature above or below a given threshold.  Values are
     partitioned by week of the year to smooth out some of the day to day
-    variation."""
+    variation.
+
+    <p><strong>Updated 29 Nov 2021</strong>: This plot was updated to always
+    plot any non-zero frequency.  Any zeros are defaulted to a white color.
+    """
     desc["data"] = True
     desc["arguments"] = [
         dict(
@@ -60,6 +62,20 @@ def get_description():
             label="Threshold direction:",
             options=PDICT,
         ),
+        dict(
+            optional=True,
+            type="date",
+            name="sdate",
+            default="1900/01/01",
+            label="Inclusive Start Local Date (optional):",
+        ),
+        dict(
+            optional=True,
+            type="date",
+            name="edate",
+            default=datetime.date.today().strftime("%Y/%m/%d"),
+            label="Exclusive End Local Date (optional):",
+        ),
         dict(type="cmap", name="cmap", default="jet", label="Color Ramp:"),
     ]
     return desc
@@ -76,39 +92,59 @@ def plotter(fdict):
     varname = ctx["var"]
 
     mydir = "<" if direction == "below" else ">="
+    timelimiter = ""
+    if station not in ctx["_nt"].sts:
+        raise NoDataFound("Unknown station metadata.")
+    tzname = ctx["_nt"].sts[station]["tzname"]
+    if ctx.get("sdate"):
+        timelimiter += (
+            f" and valid at time zone '{tzname}' >= '{ctx['sdate']}'"
+        )
+    if ctx.get("edate"):
+        timelimiter += f" and valid at time zone '{tzname}' < '{ctx['edate']}'"
 
     df = read_sql(
         f"""
         WITH data as (
             SELECT extract(week from valid) as week,
             extract(hour from (valid + '10 minutes'::interval)
-              at time zone %s) as hour, {varname} as d from alldata where
-            station = %s and {varname} between -70 and 140
+              at time zone %s) as hour, {varname} as d,
+            valid at time zone %s as local_valid from alldata where
+            station = %s and {varname} between -70 and 140 {timelimiter}
         )
         SELECT week::int, hour::int,
         sum(case when d {mydir} %s then 1 else 0 end),
-        count(*) from data GROUP by week, hour
+        count(*),
+        min(local_valid)::date as min_valid,
+        max(local_valid)::date as max_valid
+        from data GROUP by week, hour
         """,
         pgconn,
-        params=(ctx["_nt"].sts[station]["tzname"], station, threshold),
+        params=(tzname, tzname, station, threshold),
         index_col=None,
     )
-    data = np.zeros((24, 53), "f")
+    data = np.ones((24, 53), "f") * -1
     df["freq[%]"] = df["sum"] / df["count"] * 100.0
     for _, row in df.iterrows():
         data[int(row["hour"]), int(row["week"]) - 1] = row["freq[%]"]
-
+    data = np.ma.masked_where(data <= 0, data)
     sts = datetime.datetime(2012, 1, 1)
     xticks = []
     for i in range(1, 13):
         ts = sts.replace(month=i)
         xticks.append(float(ts.strftime("%j")) / 7.0)
 
-    (fig, ax) = figure_axes(apctx=ctx)
+    units = r"$^\circ$F" if varname != "relh" else "%"
+    title = (
+        f"{ctx['_nt'].sts[station]['name']} [{station}]\n"
+        f"Hourly {PDICT2[varname]} {PDICT[direction]} {threshold}{units} "
+        f"({df['min_valid'].min():%-d %b %Y}-"
+        f"{df['max_valid'].max():%-d %b %Y})"
+    )
+    (fig, ax) = figure_axes(title=title, apctx=ctx)
     cmap = get_cmap(ctx["cmap"])
-    cmap.set_under("white")
-    bins = np.arange(0, 101, 5)
-    bins[0] = 1
+    cmap.set_bad("white")
+    bins = np.arange(0, 101, 5, dtype="f")
     norm = mpcolors.BoundaryNorm(bins, cmap.N)
     res = ax.imshow(
         data,
@@ -118,29 +154,10 @@ def plotter(fdict):
         cmap=cmap,
         norm=norm,
     )
-    fig.colorbar(res, label="%", extend="min")
+    fig.colorbar(res, label="%", extend="neither")
     ax.grid(True, zorder=11)
-    units = r"$^\circ$F" if varname != "relh" else "%"
-    ab = ctx["_nt"].sts[station]["archive_begin"]
-    if ab is None:
-        raise NoDataFound("Unknown station metadata.")
-    ax.set_title(
-        ("%s [%s]\n" "Hourly %s %s %s%s (%s-%s)")
-        % (
-            ctx["_nt"].sts[station]["name"],
-            station,
-            PDICT2[varname],
-            PDICT[direction],
-            threshold,
-            units,
-            ab.year,
-            datetime.datetime.now().year,
-        ),
-        size=12,
-    )
-
     ax.set_xticks(xticks)
-    ax.set_ylabel("%s Timezone" % (ctx["_nt"].sts[station]["tzname"],))
+    ax.set_ylabel(f"{ctx['_nt'].sts[station]['tzname']} Timezone")
     ax.set_xticklabels(calendar.month_abbr[1:])
     ax.set_xlim(0, 53)
     ax.set_ylim(0, 24)
