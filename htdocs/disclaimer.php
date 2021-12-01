e5debf8236648033a002f8e31b2c9e21d02d9169
akrherz <akrherz@iastate.edu>
2021-12-01 09:31:26 -0600

    update disclaimer

--- htdocs/disclaimer.php
+++ htdocs/disclaimer.php
@@ -12,10 +12,16 @@ information without any warranty of accuracy. Users of this weather/climate data
 do so at their own risk, and are advised to use independent judgement as to 
 whether to verify the data presented.</p>
 
+<h3>Usage of IEM Products</h3>
+
+<p>The materials found on this website are in the public domain and may be used
+freely by anyone for any lawful purpose.  Attributing the Iowa Environmental
+Mesonet of Iowa State University would be appreciated.</p>
+
 <p>The IEM is a volunteer effort and receives no funds for facilities or
 staff from Iowa State University or the State of Iowa.  Users of the IEM
 must therefore recognize that the IEM may be discontinued at any time
-with little or no notice.
+with little or no notice.</p>
 
 EOF;
 $t->render('single.phtml');
