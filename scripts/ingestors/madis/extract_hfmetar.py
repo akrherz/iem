d1231b33feaca725824bf04052576c5db6accf05
akrherz <akrherz@iastate.edu>
2022-02-02 20:47:20 -0600

    fix sky level rounding in HFMETAR extraction #287

--- scripts/ingestors/madis/extract_hfmetar.py
+++ scripts/ingestors/madis/extract_hfmetar.py
@@ -29,10 +29,8 @@ def vsbyfmt(val):
     if val == 0:
         return 0
     if val <= 0.07:
-        LOG.debug("found 1/16 mile vis %s", val)
         return "1/16"
     if val <= 0.13:
-        LOG.debug("found 1/8 mile vis %s", val)
         return "1/8"
     if val <= 0.26:
         return "1/4"
@@ -49,6 +47,21 @@ def vsbyfmt(val):
     if val <= 2.6:
         return "2 1/2"
     return int(val)
+
+
+def process_sky(data, skycs, skyls):
+    """Process the sky cover"""
+    mtr = ""
+    for i, (skyc, skyl) in enumerate(zip(skycs, skyls)):
+        if skyc == "":
+            continue
+        data[f"skyc{i + 1}"] = skyc
+        if skyc != "CLR":
+            data[f"skyl{i + 1}"] = float(np.round(skyl, 0))  # GH287
+            mtr += f"{skyc}{(skyl / 100.):03.0f} "
+        else:
+            mtr += "CLR "
+    return mtr
 
 
 def process(ncfn):
@@ -113,10 +126,10 @@ def process(ncfn):
         """Our decision if we are going to take a HFMETAR value or not"""
         if data[fieldname][i] is np.ma.masked:
             return None
-        if data["%sQCR" % (fieldname,)][i] == 0:
+        if data[f"{fieldname}QCR"][i] == 0:
             return data[fieldname][i]
         # Now we have work to do
-        departure = np.ma.max(np.ma.abs(data["%sQCD" % (fieldname,)][i, :]))
+        departure = np.ma.max(np.ma.abs(data[f"{fieldname}QCD"][i, :]))
         # print("departure: %s tolerance: %s" % (departure, tolerance))
         if departure <= tolerance:
             return data[fieldname][i]
@@ -135,7 +148,7 @@ def process(ncfn):
         )
         ts = ts.replace(tzinfo=pytz.UTC)
 
-        mtr = "%s %sZ AUTO " % (sid, ts.strftime("%d%H%M"))
+        mtr = f"{sid} {ts:%d%H%M}Z AUTO "
         network = xref.get(sid3, "ASOS")
         iem = Observation(sid3, network, ts)
 
@@ -143,44 +156,35 @@ def process(ncfn):
         val = decision(i, "windDir", 15)
         if val is not None:
             iem.data["drct"] = int(val)
-            mtr += "%03i" % (iem.data["drct"],)
+            mtr += f"{iem.data['drct']:03.0f}"
         else:
             mtr += "///"
 
         val = decision(i, "windSpeed", 10)
         if val is not None:
             iem.data["sknt"] = int(val)
-            mtr += "%02i" % (iem.data["sknt"],)
+            mtr += f"{iem.data['sknt']:02.0f}"
         else:
             mtr += "//"
 
         val = decision(i, "windGust", 10)
         if val is not None and val > 0:
             iem.data["gust"] = int(val)
-            mtr += "G%02i" % (iem.data["gust"],)
+            mtr += f"G{iem.data['gust']:02.0f}"
         mtr += "KT "
 
         val = decision(i, "visibility", 4)
         if val is not None:
             iem.data["vsby"] = float(val)
-            mtr += "%sSM " % (vsbyfmt(iem.data["vsby"]),)
+            mtr += f"{vsbyfmt(iem.data['vsby'])}SM "
 
         presentwx = presentwxs[i]
         if presentwx != "":
             # database storage is comma delimited
             iem.data["wxcodes"] = presentwx.split(" ")
-            mtr += "%s " % (presentwx,)
+            mtr += f"{presentwx} "
 
-        for _i, (skyc, _l) in enumerate(
-            zip(skycs[i], data["skyCovLayerBase"][i])
-        ):
-            if skyc != "":
-                iem.data["skyc%s" % (_i + 1,)] = skyc
-                if skyc != "CLR":
-                    iem.data["skyl%s" % (_i + 1,)] = int(_l)
-                    mtr += "%s%03i " % (skyc, int(_l) / 100)
-                else:
-                    mtr += "CLR "
+        mtr += process_sky(iem.data, skycs[i], data["skyCovLayerBase"][i])
 
         t = ""
         tgroup = "T"
@@ -189,68 +193,60 @@ def process(ncfn):
             # Recall the pain enabling this
             # iem.data['tmpf'] = float(data['temperature'][i])
             tmpc = float(data["temperatureC"][i])
-            t = "%s%02i/" % (
-                "M" if tmpc < 0 else "",
-                tmpc if tmpc > 0 else (0 - tmpc),
-            )
-            tgroup += "%s%03i" % (
-                "1" if tmpc < 0 else "0",
-                (tmpc if tmpc > 0 else (0 - tmpc)) * 10.0,
-            )
+            tc = tmpc if tmpc > 0 else (0 - tmpc)
+            tf = "M" if tmpc < 0 else ""
+            t = f"{tf}{tc:02.0f}/"
+            tf = "1" if tmpc < 0 else "0"
+            tgroup += f"{tf}{(tc * 10.):03.0f}"
         val = decision(i, "dewpoint", 10)
-        if val is not None:
+        if t != "" and val is not None:
             # iem.data['dwpf'] = float(data['dewpoint'][i])
             tmpc = float(data["dewpointC"][i])
-            if t != "":
-                t = "%s%s%02i " % (
-                    t,
-                    "M" if tmpc < 0 else "",
-                    tmpc if tmpc > 0 else 0 - tmpc,
-                )
-                tgroup += "%s%03i" % (
-                    "1" if tmpc < 0 else "0",
-                    (tmpc if tmpc > 0 else (0 - tmpc)) * 10.0,
-                )
+            tc = tmpc if tmpc > 0 else (0 - tmpc)
+            tf = "M" if tmpc < 0 else ""
+            t = f"{t}{tf}{tc:02.0f} "
+            tf = "1" if tmpc < 0 else "0"
+            tgroup += f"{tf}{(tc * 10.):03.0f}"
         if len(t) > 4:
             mtr += t
         val = decision(i, "altimeter", 20)
         if val is not None:
             iem.data["alti"] = float(round(val, 2))
-            mtr += "A%4i " % (iem.data["alti"] * 100.0,)
+            mtr += f"A{(iem.data['alti'] * 100.0):.0f} "
 
         mtr += "RMK "
         val = decision(i, "precipAccum", 25)
         if val is not None:
             if val > 0.009:
                 iem.data["phour"] = float(round(val, 2))
-                mtr += "P%04i " % (iem.data["phour"] * 100.0,)
+                mtr += f"P{(iem.data['phour'] * 100.0):04.0f} "
             elif val > 0:
                 # Trace
                 mtr += "P0000 "
                 iem.data["phour"] = TRACE_VALUE
 
         if tgroup != "T":
-            mtr += "%s " % (tgroup,)
+            mtr += f"{tgroup} "
 
         if autoremarks[i] != "" or opremarks[i] != "":
-            mtr += "%s %s " % (autoremarks[i], opremarks[i])
+            mtr += f"{autoremarks[i]} {opremarks[i]} "
         mtr += "MADISHF"
         # Eat our own dogfood
         try:
             Metar.Metar(mtr)
             iem.data["raw"] = mtr
         except Exception as exp:
-            print("dogfooding extract_hfmetar %s resulted in %s" % (mtr, exp))
+            print(f"dogfooding extract_hfmetar {mtr} resulted in {exp}")
             continue
 
         for key in iem.data:
             if isinstance(iem.data[key], np.float32):
-                print("key: %s type: %s" % (key, type(iem.data[key])))
+                print(f"key: {key} type: {type(iem.data[key])}")
         icursor = pgconn.cursor()
         if not iem.save(icursor, force_current_log=True, skip_current=True):
             print(
-                ("extract_hfmetar: unknown station? %s %s %s\n%s")
-                % (sid3, network, ts, mtr)
+                f"extract_hfmetar: unknown station? {sid3} {network} {ts}\n"
+                f"{mtr}"
             )
 
         icursor.close()
@@ -272,6 +268,7 @@ def find_fn(argv):
             fn = ts.strftime("/mesonet/data/madis/hfmetar/%Y%m%d_%H00.nc")
             if os.path.isfile(fn):
                 return fn
+        LOG.warning("no MADIS HFMETAR file found!")
         sys.exit()
 
 
@@ -283,3 +280,17 @@ def main(argv):
 
 if __name__ == "__main__":
     main(sys.argv)
+
+
+def test_sky():
+    """Test that we get good numbers with sky coverage."""
+    res = process_sky(
+        {},
+        [
+            "BRK",
+        ],
+        [
+            7999.9,
+        ],
+    )
+    assert res == "BRK080 "
