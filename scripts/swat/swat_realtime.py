f3514a63e2248df35879d3c47b2c17973d60ced3
akrherz <akrherz@iastate.edu>
2022-02-07 11:07:36 -0600

    rework to address arcswat format
    closes #285

--- scripts/swat/swat_realtime.py
+++ scripts/swat/swat_realtime.py
@@ -2,6 +2,11 @@
 
 See akrherz/iem#285.
 
+We are generating the format directly used by the distrubuted SWAT. Notes:
+
+- 1800 HUCs per file, we generate four total files to cover the 5729 HUC12s
+- While the SWATID column is unique, the HUC12 column is not.
+
 Run from RUN_2AM.sh
 """
 # stdlib
@@ -22,47 +27,41 @@ from tqdm import tqdm
 
 LOG = logger()
 WORKDIR = "/mesonet/tmp/umrb_swat_realtime"
+DATADIR = "/mesonet/share/pickup/swat/umrb_realtime"
 
 
-def init_fps(huc12s):
-    """Generate file handles."""
-    for huc12 in huc12s.index.values:
-        tfp = open(
-            os.path.join(WORKDIR, "temperature", f"T{huc12}.txt"),
-            "a",
-            encoding="utf-8",
-        )
-        pfp = open(
-            os.path.join(WORKDIR, "precipitation", f"P{huc12}.txt"),
-            "a",
-            encoding="utf-8",
-        )
-        yield [tfp, pfp]
-
-
-def init_files(huc12s, now):
+def init_files(page, huc12s):
     """Create a bunch of files and fps."""
-    for huc12 in huc12s.index.values:
-        tfp = open(
-            os.path.join(WORKDIR, "temperature", f"T{huc12}.txt"),
-            "w",
-            encoding="utf-8",
-        )
-        pfp = open(
-            os.path.join(WORKDIR, "precipitation", f"P{huc12}.txt"),
-            "w",
-            encoding="utf-8",
-        )
-        tfp.write(f"{now:%Y%m%d}\n")
-        pfp.write(f"{now:%Y%m%d}\n")
-        yield [tfp, pfp]
+    for typ in ["pcp", "tmp"]:
+        fn = os.path.join(WORKDIR, f"{typ}{page}.{typ}")
+        with open(fn, "w", encoding="utf-8") as fh:
+            # Line one has all the SWATIDs
+            fh.write("       ")
+            minv = huc12s["swat"].min()
+            maxv = huc12s["swat"].max()
+            assert ((maxv - minv) + 1) == len(huc12s.index)
+            for i in range(minv, maxv + 1):
+                fh.write(f"{i:>5}")
+            fh.write("\n")
+            # two empty lines
+            fh.write("\n\n")
+            # 0 eelvation
+            fh.write("Elev   ")
+            for _i in range(minv, maxv + 1):
+                fh.write(f"{0:>5}")
+            fh.write("\n")
 
 
-def workflow(huc12s):
+def workflow(page, huc12s):
     """Generate the files as we need."""
+    if not os.path.isfile(os.path.join(WORKDIR, f"tmp{page}.tmp")):
+        init_files(page, huc12s)
+        now = date(2019, 1, 1)
+    else:
+        now = date.today() - timedelta(days=1)
     # default time domain, only run for one date if the files exist.
-    now = date(2019, 1, 1)
     ets = date.today() - timedelta(days=1)
+    LOG.info("Running for time domain %s to %s", now, ets)
     mrmsaffine = Affine(0.01, 0.0, mrms.WEST, 0.0, -0.01, mrms.NORTH)
     mrms_czs = CachingZonalStats(
         mrmsaffine,
@@ -73,79 +72,88 @@ def workflow(huc12s):
     iemre_czs = CachingZonalStats(
         iemreaffine,
     )
-    if os.listdir(os.path.join(WORKDIR, "temperature")):
-        now = ets
-    LOG.info("Running for time domain %s to %s", now, ets)
+
     mrms_nchandles = {}
     iemre_nchandles = {}
-    # Gonna be opening 10k files here...
-    swathandles = []
     dates = pd.date_range(now, ets)
     progress = tqdm(dates, disable=not sys.stdout.isatty())
-    for now in progress:
-        progress.set_description(f"{now:%Y%m%d}")
-        offset = iemre.daily_offset(now)
-        if now.strftime("%Y%m%d") == "20190101":
-            LOG.info("Generating new files")
-            swathandles.extend(init_files(huc12s, now))
-        elif not swathandles:
-            swathandles.extend(init_fps(huc12s))
-        # get mrms netcdf handle
-        if now.year not in mrms_nchandles:
-            mrms_nchandles[now.year] = ncopen(
-                iemre.get_daily_mrms_ncname(now.year), "r"
+    # Open our output page files
+    tmpfn = os.path.join(WORKDIR, f"tmp{page}.tmp")
+    pcpfn = os.path.join(WORKDIR, f"pcp{page}.pcp")
+    with (
+        open(tmpfn, "a", encoding="utf-8") as tfh,
+        open(pcpfn, "a", encoding="utf-8") as pfh,
+    ):
+        for now in progress:
+            progress.set_description(f"{now:%Y%m%d}")
+            offset = iemre.daily_offset(now)
+            # get mrms netcdf handle
+            if now.year not in mrms_nchandles:
+                mrms_nchandles[now.year] = ncopen(
+                    iemre.get_daily_mrms_ncname(now.year), "r"
+                )
+            # get iemre netcdf handle
+            if now.year not in iemre_nchandles:
+                iemre_nchandles[now.year] = ncopen(
+                    iemre.get_daily_ncname(now.year), "r"
+                )
+            # mrms precip
+            precip = np.flipud(mrms_nchandles[now.year]["p01d"][offset])
+            pdata = mrms_czs.gen_stats(precip, huc12s["simple_geom"])
+            # IEMRE high and low
+            highk = np.flipud(iemre_nchandles[now.year]["high_tmpk"][offset])
+            hdata = convert_value(
+                iemre_czs.gen_stats(highk, huc12s["simple_geom"]),
+                "degK",
+                "degC",
             )
-        # get iemre netcdf handle
-        if now.year not in iemre_nchandles:
-            iemre_nchandles[now.year] = ncopen(
-                iemre.get_daily_ncname(now.year), "r"
+            lowk = np.flipud(iemre_nchandles[now.year]["low_tmpk"][offset])
+            ldata = convert_value(
+                iemre_czs.gen_stats(lowk, huc12s["simple_geom"]),
+                "degK",
+                "degC",
             )
-        # mrms precip
-        precip = np.flipud(mrms_nchandles[now.year]["p01d"][offset])
-        pdata = mrms_czs.gen_stats(precip, huc12s["simple_geom"])
-        # IEMRE high and low
-        highk = np.flipud(iemre_nchandles[now.year]["high_tmpk"][offset])
-        hdata = convert_value(
-            iemre_czs.gen_stats(highk, huc12s["simple_geom"]), "degK", "degC"
-        )
-        lowk = np.flipud(iemre_nchandles[now.year]["low_tmpk"][offset])
-        ldata = convert_value(
-            iemre_czs.gen_stats(lowk, huc12s["simple_geom"]), "degK", "degC"
-        )
-        for i, (p, h, l) in enumerate(zip(pdata, hdata, ldata)):
-            swathandles[i][0].write(f"{h:.2f},{l:.2f}\n")
-            swathandles[i][1].write(f"{p:.1f}\n")
-
-    for swath in swathandles:
-        swath[0].close()
-        swath[1].close()
+            # write the year, date
+            tfh.write(f"{now.year:>4}{int(now.strftime('%j')):>3}")
+            pfh.write(f"{now.year:>4}{int(now.strftime('%j')):>3}")
+            for (p, h, l) in zip(pdata, hdata, ldata):
+                pfh.write(f"{p:5.1f}")
+                tfh.write(f"{h:5.1f}{l:5.1f}")
+            tfh.write("\n")
+            pfh.write("\n")
 
 
 def main():
     """Do things."""
-    # 1. Create the work directory
-    for subdir in ("temperature", "precipitation"):
-        os.makedirs(os.path.join(WORKDIR, subdir), exist_ok=True)
-    # 2. Load up HUC12 geometries
+    # Load the cross reference
+    xref = pd.read_csv(
+        os.path.join(DATADIR, "xref.csv"),
+        dtype={"huc12": str},
+    )
+    # Load up HUC12 geometries
     huc12s = read_postgis(
         "SELECT simple_geom, huc12 from wbd_huc12 where umrb_realtime_swat",
         get_dbconnstr("idep"),
         geom_col="simple_geom",
-        index_col="huc12",
+        index_col=None,
     )
+    # The xref HUC12s are not unique, so we need to do a join
+    huc12s = pd.merge(xref, huc12s, how="left", on="huc12")
+    huc12s["page"] = huc12s["swat"] // 1800 + 1
+    # Need to order by the id
+    huc12s = huc12s.sort_values("swat", ascending=True)
     LOG.info("Loaded %s HUC12s", len(huc12s.index))
-
-    # 3. Iterate over what we need to iterate over
-    workflow(huc12s)
+    # Loop over the pages
+    for page, df in huc12s.groupby("page"):
+        workflow(page, df)
     # 4. Zip up the files
     os.chdir(WORKDIR)
     yesterday = date.today() - timedelta(days=1)
     zipfn = f"umrb_realtime_{yesterday:%Y%m%d}.zip"
-    subprocess.call(["zip", "-q", "-r", zipfn, "temperature", "precipitation"])
+    # unsure why the wildcard doesn't work here for non-shell
+    subprocess.call(f"zip -q {zipfn} tmp?.tmp pcp?.pcp", shell=True)
     # 5. Copy the zip file to the resulting folder
-    subprocess.call(
-        ["mv", zipfn, "/mesonet/share/pickup/swat/umrb_realtime/"],
-    )
+    subprocess.call(["mv", zipfn, DATADIR])
 
 
 if __name__ == "__main__":
