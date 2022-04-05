416de1fb8a7d702ca63b15d04c13fa6797a3ee42
akrherz <akrherz@iastate.edu>
2022-04-05 11:20:18 -0500

    tweaks per season roads update

--- scripts/roads/check_roads_geom.py
+++ scripts/roads/check_roads_geom.py
@@ -1,6 +1,6 @@
 """Use the REST service to setup any new segments for the new season
 
- * JSON data is in Google 3857
+ * JSON data is in 3857
 """
 import sys
 import json
@@ -10,8 +10,20 @@ import requests
 from pyiem.util import get_dbconn
 
 URI = (
-    "https://iowadot.maps.arcgis.com/sharing/rest/content/items/"
-    "5d6c7d6963e549539ead6e50d89bdd08/data"
+    "https://services.arcgis.com/8lRhdTsQyJpO52F1/ArcGIS/rest/services/"
+    "511_IA_Road_Conditions_View/FeatureServer/0/query?"
+    "where=1%3D1&objectIds=&time=&"
+    "geometry=&geometryType=esriGeometryEnvelope&inSR=&"
+    "spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&"
+    "units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&"
+    "returnGeometry=true&multipatchOption=xyFootprint&maxAllowableOffset="
+    "&geometryPrecision=&outSR=&datumTransformation=&"
+    "applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&"
+    "returnCountOnly=false&returnExtentOnly=false&returnDistinctValues=false&"
+    "orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&"
+    "resultOffset=&resultRecordCount=&returnZ=false&returnM=false&"
+    "returnExceededLimitFeatures=true&quantizationParameters=&"
+    "sqlFormat=none&f=pjson&token="
 )
 
 
@@ -21,47 +33,40 @@ def main():
     cursor = pgconn.cursor()
     req = requests.get(URI, timeout=30)
     jobj = req.json()
-    featureset = jobj["layers"][0]["featureSet"]
-    for feat in featureset["features"]:
+    queue = []
+    for feat in jobj["features"]:
         props = feat["attributes"]
         # Geometry is [[pt]] and we only have single segments
         path = MultiLineString([LineString(feat["geometry"]["paths"][0])])
         # segid is defined by the database insert
-        major = props["ROUTE_NAME"]
-        (typ, _num) = major.replace("-", " ").split()
-        if typ not in ["I", "US", "IA"]:
-            print("Totally unknown, abort")
-            print(json.dumps(jobj, indent=4))
-            sys.exit()
-        geom = (
-            "ST_Transform(ST_SetSrid(ST_GeomFromText('%s'), 3857), 26915)"
-        ) % (path.wkt)
+        geom = f"ST_Transform(ST_GeomFromText('{path.wkt}', 3857), 26915)"
         idot_id = props["SEGMENT_ID"]
         cursor.execute(
-            """
+            f"""
         SELECT st_length(geom),
-        st_length("""
-            + geom
-            + """), archive_begin from roads_base
-        where idot_id = %s and archive_begin > '2017-01-01'::date
+        st_length({geom}), archive_begin from roads_base
+        where idot_id = %s and archive_end > '2022-01-01'::date
         ORDER by archive_begin DESC
         """,
             (idot_id,),
         )
         row = cursor.fetchone()
+        if row is None:
+            print(f"{idot_id} is unknown to database!")
+            queue.append(idot_id)
+            continue
         if abs(row[1] - row[0]) > 1:
-            print("%s, %s" % (str(row), cursor.rowcount))
+            print(str(row), cursor.rowcount)
             cursor.execute(
-                """
-            UPDATE roads_base SET geom = """
-                + geom
-                + """
-            WHERE idot_id = %s and archive_begin > '2017-01-01'::date
+                f"""
+            UPDATE roads_base SET geom = {geom}
+            WHERE idot_id = %s and archive_end > '2022-01-01'::date
             """,
                 (idot_id,),
             )
-            print("updated %s rows" % (cursor.rowcount,))
+            print(f"updated {cursor.rowcount} rows")
 
+    print(queue)
     cursor.close()
     pgconn.commit()
 
