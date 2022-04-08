9483129fc03d5a8ddd1b23ab5e2b54605b9b9687
akrherz <akrherz@iastate.edu>
2022-04-07 22:03:20 -0500

    set timeout on download

--- scripts/hrrr/dl_hrrrref.py
+++ scripts/hrrr/dl_hrrrref.py
@@ -29,7 +29,7 @@ def run(valid):
     """run for this valid time!"""
     service = get_service(valid)
     should_throttle = service.find("noaa.gov") > -1
-    LOG.debug("using %s as dl service", service)
+    LOG.info("using %s as dl service", service)
     gribfn = valid.strftime(
         "/mesonet/ARCHIVE/data/%Y/%m/%d/model/hrrr/%H/hrrr.t%Hz.refd.grib2"
     )
@@ -43,69 +43,76 @@ def run(valid):
         except Exception as exp:
             logging.debug(exp)
     tmpfn = f"/tmp/{valid:%Y%m%d%H}.grib2"
-    output = open(tmpfn, "wb")
-    for hr in range(0, min([39, HOURS[valid.hour]]) + 1):
-        if should_throttle and hr > 30:
-            # Add some delays to work around upstream connection throttling
-            time.sleep(60)
-        shr = "%02i" % (hr,)
-        if hr <= 18:
-            uri = valid.strftime(
-                f"{service}hrrr.%Y%m%d/conus/hrrr.t%Hz.wrfsubhf{shr}.grib2.idx"
-            )
-        else:
-            uri = valid.strftime(
-                f"{service}hrrr.%Y%m%d/conus/hrrr.t%Hz.wrfsfcf{shr}.grib2.idx"
-            )
-        LOG.debug(uri)
-        req = exponential_backoff(requests.get, uri, timeout=30)
-        if req is None or req.status_code != 200:
-            LOG.info("failed to fetch %s", uri)
-            if hr > 18:
-                continue
-            LOG.info("ABORT")
-            return
-        data = req.text
+    with open(tmpfn, "wb") as output:
+        for hr in range(0, min([39, HOURS[valid.hour]]) + 1):
+            if should_throttle and hr > 30:
+                # Add some delays to work around upstream connection throttling
+                time.sleep(60)
+            shr = f"{hr:02.0f}"
+            if hr <= 18:
+                uri = valid.strftime(
+                    f"{service}hrrr.%Y%m%d/conus/"
+                    f"hrrr.t%Hz.wrfsubhf{shr}.grib2.idx"
+                )
+            else:
+                uri = valid.strftime(
+                    f"{service}hrrr.%Y%m%d/conus/"
+                    f"hrrr.t%Hz.wrfsfcf{shr}.grib2.idx"
+                )
+            LOG.info(uri)
+            req = exponential_backoff(requests.get, uri, timeout=30)
+            if req is None or req.status_code != 200:
+                LOG.info("failed to fetch %s", uri)
+                if hr > 18:
+                    continue
+                LOG.info("ABORT")
+                return
+            data = req.text
 
-        offsets = []
-        neednext = False
-        for line in data.split("\n"):
-            if line.strip() == "":
-                continue
-            tokens = line.split(":")
-            if neednext:
-                offsets[-1].append(int(tokens[1]))
-                neednext = False
-            if tokens[3] == "REFD" and tokens[4] == "1000 m above ground":
-                offsets.append([int(tokens[1])])
-                neednext = True
+            offsets = []
+            neednext = False
+            for line in data.split("\n"):
+                if line.strip() == "":
+                    continue
+                tokens = line.split(":")
+                if neednext:
+                    offsets[-1].append(int(tokens[1]))
+                    neednext = False
+                if tokens[3] == "REFD" and tokens[4] == "1000 m above ground":
+                    offsets.append([int(tokens[1])])
+                    neednext = True
 
-        if 0 < hr < 19 and len(offsets) != 4:
-            LOG.info(
-                "[%s] hr: %s offsets: %s",
-                valid.strftime("%Y%m%d%H"),
-                hr,
-                offsets,
-            )
-        for pr in offsets:
-            headers = {"Range": "bytes=%s-%s" % (pr[0], pr[1])}
-            req = exponential_backoff(requests.get, uri[:-4], headers=headers)
-            if req is None:
-                LOG.info("dl_hrrrref FAIL %s %s", uri[:-4], headers)
-                continue
-            output.write(req.content)
+            if 0 < hr < 19 and len(offsets) != 4:
+                LOG.info(
+                    "[%s] hr: %s offsets: %s",
+                    valid.strftime("%Y%m%d%H"),
+                    hr,
+                    offsets,
+                )
+            for pr in offsets:
+                headers = {"Range": f"bytes={pr[0]}-{pr[1]}"}
+                req = exponential_backoff(
+                    requests.get,
+                    uri[:-4],
+                    headers=headers,
+                    timeout=30,
+                )
+                if req is None:
+                    LOG.info("FAIL %s %s", uri[:-4], headers)
+                    continue
+                output.write(req.content)
 
-    output.close()
     # insert into LDM Please
     pqstr = (
-        "data a %s bogus model/hrrr/%02i/hrrr.t%02iz.refd.grib2 grib2"
-    ) % (valid.strftime("%Y%m%d%H%M"), valid.hour, valid.hour)
-    subprocess.call(
-        "pqinsert -p '%s' %s" % (pqstr, tmpfn),
-        shell=True,
+        f"data a {valid:%Y%m%d%H%M} bogus model/hrrr/{valid.hour:02.0f}/"
+        f"hrrr.t{valid.hour:02.0f}z.refd.grib2 grib2"
+    )
+    with subprocess.Popen(
+        ["pqinsert", "-p", pqstr, tmpfn],
         stderr=subprocess.PIPE,
         stdout=subprocess.PIPE,
-    )
+    ) as proc:
+        proc.communicate()
     os.unlink(tmpfn)
 
 
