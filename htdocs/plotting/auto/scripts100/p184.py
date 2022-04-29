06b251d316fed7cd3cbf1f8b31fe4a8889e62ece
akrherz <akrherz@iastate.edu>
2022-04-28 21:04:47 -0500

    add min,mean value

--- htdocs/plotting/auto/scripts100/p184.py
+++ htdocs/plotting/auto/scripts100/p184.py
@@ -3,9 +3,10 @@ import datetime
 
 import pandas as pd
 from pandas.io.sql import read_sql
-from pyiem.util import get_autoplot_context, get_dbconn
+from pyiem.util import get_autoplot_context, get_sqlalchemy_conn
 from pyiem.plot import figure_axes
 from pyiem.exceptions import NoDataFound
+from sqlalchemy import text
 
 PDICT = {
     "full": "Show Full Year Totals",
@@ -21,7 +22,8 @@ def get_description():
         "description"
     ] = """This plot shows the number of days with a high
     temperature at or above a given threshold.  You can optionally generate
-    this plot for the year to date period.
+    this plot for the year to date period.  The present year is not used for
+    the computation of the average nor minimum value.
     """
     today = datetime.date.today()
     desc["arguments"] = [
@@ -57,28 +59,47 @@ def plotter(fdict):
     limit = ctx["limit"]
     limitsql = ""
     limittitle = ""
-    today = datetime.date.today()
+    today = datetime.date.today() - datetime.timedelta(days=1)
+    params = {"station": station}
     if limit == "ytd":
-        limittitle = "(Jan 1 - %s)" % (today.strftime("%b %-d"),)
-        limitsql = " and extract(doy from day) <= %s" % (today.strftime("%j"),)
+        limittitle = f"(Jan 1 - {today:%b %-d})"
+        params["doy"] = int(today.strftime("%j"))
+        limitsql = " and extract(doy from day) <= :doy "
 
-    dbconn = get_dbconn("coop")
-
-    table = "alldata_%s" % (station[:2],)
-    df = read_sql(
-        f"SELECT year, day, high from {table} WHERE station = %s and "
-        f"high is not null {limitsql} ORDER by day ASC",
-        dbconn,
-        params=(station,),
-        index_col="day",
-    )
+    ab = ctx["_nt"].sts[station]["archive_begin"]
+    if ab is None:
+        raise NoDataFound(f"{station} has no archive_begin")
+    params["sts"] = ab.strftime("%Y-%m-%d")
+    if ab.strftime("%m%d") != "0101":
+        # Truncate up to the next year
+        params["sts"] = f"{ab.year + 1}-01-01"
+    with get_sqlalchemy_conn("coop") as conn:
+        df = read_sql(
+            text(
+                "SELECT year, day, high from alldata WHERE "
+                "station = :station and day >= :sts and "
+                f"high is not null {limitsql} ORDER by day ASC"
+            ),
+            conn,
+            params=params,
+            index_col="day",
+        )
     if df.empty:
         raise NoDataFound("No Data Found.")
     res = []
     maxval2 = int(df["high"].max())
+    years = df["year"].unique()
     for level in range(maxval2 - 30, maxval2 + 2):
-        gdf = df[df["high"] >= level].groupby("year").count()
+        gdf = (
+            df[df["high"] >= level]
+            .groupby("year")
+            .count()
+            .reindex(years)
+            .fillna(0)
+        )
         maxval = gdf.max()[0]
+        if maxval == 0:
+            continue
         label = ",".join(
             [str(s) for s in list(gdf[gdf["high"] == maxval].index.values)]
         )
@@ -86,39 +107,58 @@ def plotter(fdict):
         if year in gdf.index.values:
             thisyear = gdf.at[year, "high"]
         res.append(
-            dict(level=level, label=label, max=maxval, thisyear=thisyear)
+            dict(
+                avgval=gdf.iloc[:-1].mean(),
+                minval=gdf.iloc[:-1].min()[0],
+                level=level,
+                label=label,
+                max=maxval,
+                thisyear=thisyear,
+            )
         )
 
     df = pd.DataFrame(res)
     df = df.set_index("level")
 
-    (fig, ax) = figure_axes(apctx=ctx)
+    title = (
+        f"{ctx['_sname']} :: Max Days per Year {limittitle}\n"
+        "at or above given high temperature threshold"
+    )
+
+    (fig, ax) = figure_axes(apctx=ctx, title=title)
     ax.barh(df.index.values, df["max"].values, label="Max", zorder=2)
-    ax.barh(
-        df.index.values, df["thisyear"].values, label="%s" % (year,), zorder=3
+    ax.barh(df.index.values, df["thisyear"].values, label=f"{year}", zorder=3)
+    ax.scatter(
+        df["minval"].values,
+        df.index.values,
+        label="Min",
+        marker="^",
+        zorder=4,
+        color="black",
+    )
+    ax.scatter(
+        df["avgval"].values,
+        df.index.values,
+        label="Avg",
+        marker="o",
+        zorder=5,
+        color="yellow",
     )
     for level, row in df.iterrows():
         ax.text(
             row["max"] + 1,
             level,
-            "%.0f - %s" % (row["max"], row["label"]),
+            f"{row['max']:.0f} - {row['label']}",
             va="center",
         )
 
     ax.grid(True, color="#EEEEEE", linewidth=1)
-    ax.legend(loc="best")
+    ax.legend(loc=1)
     ax.set_xlim(0, df["max"].max() * 1.2)
     ax.set_ylim(maxval2 - 31, maxval2 + 2)
-    ax.set_title(
-        (
-            "%s Max Days per Year %s\n"
-            "at or above given high temperature threshold"
-        )
-        % (ctx["_nt"].sts[station]["name"], limittitle)
-    )
     ax.set_ylabel(r"High Temperature $^\circ$F")
     if year == datetime.date.today().year:
-        ax.set_xlabel(("Days, %s data through %s") % (year, today))
+        ax.set_xlabel(f"Days, {year} data till {today}")
     else:
         ax.set_xlabel("Days")
 
