8fab26382f65ed4096ee7e8c827c0c4a0296a5de
akrherz <akrherz@iastate.edu>
2022-05-14 08:30:26 -0500

    more ISUSM variable madness (#243)

--- scripts/isusm/agg_1minute.py
+++ scripts/isusm/agg_1minute.py
@@ -5,153 +5,119 @@ import datetime
 
 import numpy as np
 import pandas as pd
-from pyiem.util import get_dbconn, get_dbconnstr, utc, logger
+from pyiem.util import get_dbconn, get_sqlalchemy_conn, utc, logger
 
 LOG = logger()
 TIME_FORMAT = "%Y-%m-%d %H:%M-06"
 
 
-def do_updates(cursor, station, row):
-    """Make the updates happen, captain."""
-    row = row.replace({np.nan: None})
+def hourly_process(cursor, station, hour, mdf, hdf):
+    """Merge this row information into the database."""
+    # The totals for this hour are stored for the next hour
+    row = pd.Series(
+        {"station": station, "valid": hour + datetime.timedelta(hours=1)}
+    )
+    if row["valid"] not in hdf.index:
+        return
+    lastob = mdf.iloc[-1]
+    sumdf = mdf.sum(numeric_only=True)
+    row["obs_count"] = len(mdf.index)
+    current = hdf.loc[row["valid"]]
+    if current["obs_count"] == row["obs_count"]:
+        return
+    row["slrkj_tot"] = sumdf["slrkj_tot"]
+    # Take last ob
+    for colname in (
+        "t4_c_avg t12_c_avg t24_c_avg t50_c_avg calcvwc12_avg "
+        "calcvwc24_avg calcvwc50_avg rh_avg"
+    ).split():
+        row[colname] = float(lastob[f"{colname}_qc"])
+    # Soil Vue Madness
+    for depth in [2, 4, 8, 12, 14, 16, 20, 24, 28, 30, 32, 36, 40, 42, 52]:
+        for col in ["t", "vwc", "ec"]:
+            row[f"sv_{col}{depth}"] = float(lastob[f"sv_{col}{depth}_qc"])
+    tokens = []
+    sigh = {
+        "calcvwc12_avg": "calc_vwc_12_avg",
+        "calcvwc24_avg": "calc_vwc_24_avg",
+        "calcvwc50_avg": "calc_vwc_50_avg",
+    }
+    for colname in row.index:
+        if colname in ["station", "valid"]:
+            continue
+        if colname == "obs_count":
+            tokens.append(f"{colname} = {row[colname]}")
+            continue
+        cname = sigh.get(colname, colname)
+        tokens.append(f"{cname} = coalesce({cname}, %({colname})s)")
+        tokens.append(f"{cname}_qc = coalesce({cname}_qc, %({colname})s)")
+    LOG.info("updating sm_hourly %s %s", station, row["valid"])
     cursor.execute(
-        """
-        UPDATE sm_hourly SET
-        slrkj_tot = coalesce(slrkj_tot, %(slrkj_tot_sum)s, 0),
-        slrkj_tot_qc = coalesce(slrkj_tot_qc, %(slrkj_tot_sum)s, 0),
-        tair_c_avg = %(tair_c_avg)s,
-        tair_c_avg_qc = %(tair_c_avg)s,
-        rh_avg = %(rh_avg)s,
-        rh_avg_qc = %(rh_avg)s,
-        t4_c_avg = %(t4_c_avg)s,
-        t4_c_avg_qc = %(t4_c_avg)s,
-        calc_vwc_12_avg = %(calc_vwc_12_avg)s,
-        calc_vwc_12_avg_qc = %(calc_vwc_12_avg)s,
-        calc_vwc_24_avg = %(calc_vwc_24_avg)s,
-        calc_vwc_24_avg_qc = %(calc_vwc_24_avg)s,
-        calc_vwc_50_avg = %(calc_vwc_50_avg)s,
-        calc_vwc_50_avg_qc = %(calc_vwc_50_avg)s,
-        t12_c_avg = %(t12_c_avg)s,
-        t12_c_avg_qc = %(t12_c_avg)s,
-        t24_c_avg = %(t24_c_avg)s,
-        t24_c_avg_qc = %(t24_c_avg)s,
-        t50_c_avg = %(t50_c_avg)s,
-        t50_c_avg_qc = %(t50_c_avg)s,
-        obs_count = %(obs_count)s
-        WHERE station = %(station)s and valid = %(valid_cst)s
-    """,
+        f"UPDATE sm_hourly SET {','.join(tokens)} "
+        "WHERE station = %(station)s and valid = %(valid)s",
         row,
     )
-    if cursor.rowcount == 0:
-        LOG.debug(
-            "Updating %s %s resulted in 0 rows updated",
-            station,
-            row["valid_cst"],
-        )
 
 
-def hourly_process(cursor, row):
-    """Merge this row information into the database."""
-    # Data for this 'hour' is stored at the top of the next hour in the
-    # database
-    valid_cst = row["hour"] + datetime.timedelta(hours=1)
-    row["valid_cst"] = valid_cst.strftime(TIME_FORMAT)
-    station = row["station"]
-    cursor.execute(
-        "SELECT obs_count from sm_hourly where station = %s and valid = %s",
-        (station, row["valid_cst"]),
-    )
-    if cursor.rowcount == 1:
-        current = cursor.fetchone()
-        if current[0] == row["obs_count"]:
-            LOG.debug(
-                "%s %s obs_count %s matches",
-                row["valid_cst"],
-                station,
-                row["obs_count"],
-            )
-            return
-    elif cursor.rowcount == 0:
-        # Need to create an entry
-        cursor.execute(
-            "INSERT into sm_hourly(station, valid, obs_count) "
-            "values (%s, %s, 0)",
-            (station, row["valid_cst"]),
-        )
-    do_updates(cursor, station, row)
-
-
-def daily_process(cursor, station, date, df):
+def daily_process(cursor, station, date, df, ddf):
     """Process this date's dataframe."""
-    sumdf = df.sum(numeric_only=True)
     mindf = df.min(numeric_only=True)
     maxdf = df.max(numeric_only=True)
-    row = {"station": station, "date": date}
-    row["obs_count"] = float(sumdf["obs_count"])
-    row["tair_c_max"] = maxdf["tair_c_max"]
-    row["tair_c_min"] = mindf["tair_c_min"]
-    for colname in [
-        "t4_c_avg",
-        "t12_c_avg",
-        "t24_c_avg",
-        "t50_c_avg",
-        "calc_vwc_12_avg",
-        "calc_vwc_24_avg",
-        "calc_vwc_50_avg",
-        "rh_avg",
-    ]:
-        row[colname] = float(df[colname].mean())
-    cursor.execute(
-        "SELECT obs_count from sm_daily where station = %s and valid = %s",
-        (station, date),
-    )
-    if cursor.rowcount == 0:
-        LOG.debug("no database entry found %s %s", date, station)
+    avgdf = df.mean(numeric_only=True)
+    row = pd.Series({"station": station, "date": date})
+    row["obs_count"] = df["tair_c_avg_qc"].size
+    row["tair_c_max"] = maxdf["tair_c_avg_qc"]
+    row["tair_c_min"] = mindf["tair_c_avg_qc"]
+    # For daily, we want the average for some of these columns
+    for colname in (
+        "t4_c_avg t12_c_avg t24_c_avg t50_c_avg calcvwc12_avg "
+        "calcvwc24_avg calcvwc50_avg rh_avg"
+    ).split():
+        row[colname] = float(avgdf[f"{colname}_qc"])
+    # Soil Vue Madness
+    for depth in [2, 4, 8, 12, 14, 16, 20, 24, 28, 30, 32, 36, 40, 42, 52]:
+        for col in ["t", "vwc", "ec"]:
+            row[f"sv_{col}{depth}"] = float(avgdf[f"sv_{col}{depth}_qc"])
+    current = ddf.loc[(station, date)]
+    if current["obs_count"] == row["obs_count"]:
+        LOG.info("%s %s obs_count %s matches", date, station, row["obs_count"])
         return
-    current = cursor.fetchone()
-    if current[0] == row["obs_count"]:
-        LOG.debug(
-            "%s %s obs_count %s matches", date, station, row["obs_count"]
-        )
-        return
-    for col in row:
-        if pd.isna(row[col]):
-            row[col] = None
+    # Ensure database gets nulls and not nan
+    row = row.replace({np.nan: None})
+    # build up SQL statement
+    tokens = []
+    sigh = {
+        "calcvwc12_avg": "calc_vwc_12_avg",
+        "calcvwc24_avg": "calc_vwc_24_avg",
+        "calcvwc50_avg": "calc_vwc_50_avg",
+    }
+    for colname in row.index:
+        if colname in ["station", "date"]:
+            continue
+        if colname == "obs_count":
+            tokens.append(f"{colname} = {row[colname]}")
+            continue
+        cname = sigh.get(colname, colname)
+        tokens.append(f"{cname} = coalesce({cname}, %({colname})s)")
+        tokens.append(f"{cname}_qc = coalesce({cname}_qc, %({colname})s)")
+    LOG.info("updating sm_daily %s %s", station, date)
     cursor.execute(
-        """
-        UPDATE sm_daily SET
-        tair_c_max_qc = coalesce(tair_c_max_qc, %(tair_c_max)s),
-        tair_c_min_qc = coalesce(tair_c_min_qc, %(tair_c_min)s),
-        t4_c_avg = %(t4_c_avg)s,
-        t4_c_avg_qc = %(t4_c_avg)s,
-        t12_c_avg = %(t12_c_avg)s,
-        t12_c_avg_qc = %(t12_c_avg)s,
-        rh_avg = %(rh_avg)s,
-        rh_avg_qc = %(rh_avg)s,
-        t24_c_avg = %(t24_c_avg)s,
-        t24_c_avg_qc = %(t24_c_avg)s,
-        t50_c_avg = %(t50_c_avg)s,
-        t50_c_avg_qc = %(t50_c_avg)s,
-        calc_vwc_12_avg = %(calc_vwc_12_avg)s,
-        calc_vwc_12_avg_qc = %(calc_vwc_12_avg)s,
-        calc_vwc_24_avg = %(calc_vwc_24_avg)s,
-        calc_vwc_24_avg_qc = %(calc_vwc_24_avg)s,
-        calc_vwc_50_avg = %(calc_vwc_50_avg)s,
-        calc_vwc_50_avg_qc = %(calc_vwc_50_avg)s,
-        vwc_12_avg = %(calc_vwc_12_avg)s,
-        vwc_12_avg_qc = %(calc_vwc_12_avg)s,
-        vwc_24_avg = %(calc_vwc_24_avg)s,
-        vwc_24_avg_qc = %(calc_vwc_24_avg)s,
-        vwc_50_avg = %(calc_vwc_50_avg)s,
-        vwc_50_avg_qc = %(calc_vwc_50_avg)s,
-        obs_count = %(obs_count)s
-        WHERE station = %(station)s and valid = %(date)s
-    """,
+        f"UPDATE sm_daily SET {','.join(tokens)} "
+        "WHERE station = %(station)s and valid = %(date)s",
         row,
     )
 
 
-def workflow():
+def do_date_ops(df):
+    """Common things."""
+    df["utc_valid"] = df["utc_valid"].dt.tz_localize("UTC")
+    df = df.drop(columns="valid")
+    # Compute a LST date via UTC-6
+    df["date"] = (df["utc_valid"] - datetime.timedelta(hours=6)).dt.date
+    return df
+
+
+def main():
     """Do things."""
     pgconn = get_dbconn("isuag")
     # We need to collect up data for periods representing CST dates, this
@@ -159,78 +125,66 @@ def workflow():
     date = datetime.date.today() - datetime.timedelta(days=7)
     # 6z is the start of such a date
     sts = utc(date.year, date.month, date.day, 6)
-    df = pd.read_sql(
-        """
-    WITH data as (
-        SELECT *,
-        date_trunc('hour',
-            (valid - '1 minute'::interval) at time zone 'UTC+6') as hour,
-        row_number() OVER (
-            PARTITION by station,
-            date_trunc('hour',
-            (valid - '1 minute'::interval) at time zone 'UTC+6')
-            ORDER by valid DESC) as rn
-        from sm_minute where valid >= %s
-    )
-    SELECT
-    station, hour,
-    count(*) as obs_count,
-    sum(slrkj_tot_qc) as slrkj_tot_sum,
-    min(tair_c_avg_qc) as tair_c_min,
-    max(tair_c_avg_qc) as tair_c_max,
-    max(case when rn = 1 then rh_avg_qc else null end) as rh_avg,
-    max(case when rn = 1 then tair_c_avg_qc else null end) as tair_c_avg,
-    max(case when rn = 1 then t4_c_avg_qc else null end) as t4_c_avg,
-    max(case when rn = 1 then t12_c_avg_qc else null end) as t12_c_avg,
-    max(case when rn = 1 then t24_c_avg_qc else null end) as t24_c_avg,
-    max(case when rn = 1 then t50_c_avg_qc else null end) as t50_c_avg,
-    max(case when rn = 1 then calcVWC12_Avg_qc else null end)
-        as calc_vwc_12_avg,
-    max(case when rn = 1 then calcVWC24_Avg_qc else null end)
-        as calc_vwc_24_avg,
-    max(case when rn = 1 then calcVWC50_Avg_qc else null end)
-        as calc_vwc_50_avg
-    from data GROUP by station, hour
-    """,
-        get_dbconnstr("isuag"),
-        params=(sts,),
-        index_col=None,
-    )
-    # Compute the "date"
-    df["date"] = df["hour"].dt.date
+    with get_sqlalchemy_conn("isuag") as conn:
+        # Get the minute data
+        mdf = pd.read_sql(
+            "SELECT *, valid at time zone 'UTC' as utc_valid from sm_minute "
+            "where valid >= %s ORDER by valid ASC",
+            conn,
+            params=(sts,),
+            index_col=None,
+        ).fillna(np.nan)
+        # Get the hourly data
+        hdf = pd.read_sql(
+            "SELECT *, valid at time zone 'UTC' as utc_valid from sm_hourly "
+            "where valid >= %s ORDER by valid ASC",
+            conn,
+            params=(sts,),
+            index_col=None,
+        )
+        # Get the daily data
+        ddf = pd.read_sql(
+            "SELECT * from sm_daily where valid >= %s ORDER by valid ASC",
+            conn,
+            params=(sts.date(),),
+            index_col=["station", "valid"],
+        )
+
+    # Do date operations
+    mdf = do_date_ops(mdf)
+    hdf = do_date_ops(hdf).set_index(["station", "utc_valid"])
 
     # Daily work
     cursor = pgconn.cursor()
-    for (station, date), gdf in df.groupby(["station", "date"]):
-        daily_process(cursor, station, date, gdf)
+    for (station, date), gdf in mdf.groupby(["station", "date"]):
+        if (station, date) not in ddf.index:
+            LOG.info("%s %s not in daily, skipping", station, date)
+            continue
+        daily_process(cursor, station, date, gdf, ddf)
     cursor.close()
     pgconn.commit()
 
     # Hourly work
     cursor = pgconn.cursor()
-    for _, row in df.iterrows():
-        hourly_process(cursor, row)
+    for station, gdf in mdf.groupby("station"):
+        for hour, hgdf in mdf.set_index("utc_valid").resample("H"):
+            hourly_process(cursor, station, hour, hgdf, hdf.loc[station, :])
     cursor.close()
     pgconn.commit()
 
     # Special case of PET
     cursor = pgconn.cursor()
     cursor.execute(
-        "WITH data as ( "
-        "SELECT station, date(valid), "
-        "sum(etalfalfa_qc) as dailyet from sm_hourly WHERE "
-        "valid >= %s and etalfalfa_qc is not null GROUP by station, date) "
-        "UPDATE sm_daily s SET dailyet_qc = d.dailyet, dailyet = d.dailyet "
-        "FROM data d WHERE s.station = d.station and s.valid = d.date",
+        """WITH data as (
+        SELECT station, date(valid),
+        sum(etalfalfa_qc) as dailyet from sm_hourly WHERE
+        valid >= %s and etalfalfa_qc is not null GROUP by station, date)
+        UPDATE sm_daily s SET dailyet_qc = d.dailyet, dailyet = d.dailyet
+        FROM data d WHERE s.station = d.station and s.valid = d.date""",
         (sts,),
     )
     cursor.close()
     pgconn.commit()
-
-
-def main():
-    """Go Main Go."""
-    workflow()
 
 
 if __name__ == "__main__":
