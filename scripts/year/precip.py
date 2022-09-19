a836fadb3418e6b381d8d06542a6b060b08ea401
akrherz <akrherz@iastate.edu>
2022-09-19 09:39:49 -0500

    cleanup yearly precip plot

--- scripts/year/precip.py
+++ scripts/year/precip.py
@@ -1,7 +1,11 @@
 """yearly precip"""
 import datetime
+import os
+import subprocess
+import tempfile
 
 import numpy as np
+import requests
 from pyiem.plot import MapPlot
 from pyiem.network import Table as NetworkTable
 from pyiem.util import get_dbconn
@@ -10,7 +14,7 @@ from pyiem.util import get_dbconn
 def main():
     """Go Main Go"""
     st = NetworkTable("IACLIMATE")
-    pgconn = get_dbconn("coop", user="nobody")
+    pgconn = get_dbconn("coop")
     ccursor = pgconn.cursor()
 
     ts = datetime.datetime.now() - datetime.timedelta(days=1)
@@ -50,41 +54,22 @@ def main():
     mp.close()
 
     # ----------------------------------
-    # - Compute departures
-    nrain = []
-    lats = []
-    lons = []
-    ccursor.execute(
-        """select climate.station, norm, obs from
-        (select c.station, sum(c.precip) as norm from climate51 c
-         where c.valid < '2000-%s' and substr(station,0,3) = 'IA'
-         GROUP by c.station) as climate,
-        (select a.station, sum(a.precip) as obs from alldata a
-         WHERE a.year = %s and substr(a.station,0,3) = 'IA'
-         GROUP by station) as obs
-      WHERE obs.station = climate.station"""
-        % (ts.strftime("%m-%d"), ts.year)
+    # - Departures
+    service = (
+        "http://iem.local/plotting/auto/plot/97/d:sector::sector:IA::"
+        f"var:precip_departdate1:{ts.year}-01-01::usdm:no::"
+        f"date2:{ts:%Y-%m-%d}::p:contour::cmap:RdYlBu::c:yes::"
+        "ct:ncei_climate91::_r:43::_cb:1.png"
     )
-    for row in ccursor:
-        station = row[0]
-        if station not in st.sts:
-            continue
-        nrain.append(row[2] - row[1])
-        lats.append(st.sts[station]["lat"])
-        lons.append(st.sts[station]["lon"])
 
-    mp = MapPlot(
-        axisbg="white",
-        title=("Iowa %s Precipitation Depature") % (ts.strftime("%Y"),),
-        subtitle="1 Jan - %s" % (ts.strftime("%d %b %Y"),),
-    )
-    rng = np.arange(int(min(nrain)) - 1, int(max(nrain)) + 1)
-    if max(nrain) < 10:
-        rng = np.arange(0, 10)
-    mp.contourf(lons, lats, nrain, rng, units="inch")
-    pqstr = "plot c 000000000000 summary/year/diff.png bogus png"
-    mp.postprocess(view=False, pqstr=pqstr)
-    mp.close()
+    req = requests.get(service, timeout=120)
+    tmpfd = tempfile.NamedTemporaryFile(delete=False)
+    tmpfd.write(req.content)
+    tmpfd.close()
+
+    pqstr = f"plot c {ts:%Y%m%d%H%M} summary/year/diff.png bogus png"
+    subprocess.call(f"pqinsert -i -p '{pqstr}' {tmpfd.name}", shell=True)
+    os.unlink(tmpfd.name)
 
 
 if __name__ == "__main__":
