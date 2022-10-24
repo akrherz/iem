fdcbc9ef7479dc66eeef4632aa8df261344ceddb
akrherz <akrherz@iastate.edu>
2022-10-24 17:37:23 -0500

    fix log level

--- scripts/cache/warn_cache.py
+++ scripts/cache/warn_cache.py
@@ -18,9 +18,9 @@ URL = "http://iem.local/cgi-bin/request/gis/watchwarn.py"
 
 def get_uri(uri, localfn):
     """Fetch the remote resource to a local file"""
-    req = requests.get(uri)
+    req = requests.get(uri, timeout=1200)
     if req.status_code != 200:
-        LOG.info(
+        LOG.warning(
             "warn_cache[%s] failed with status: %s\n%s",
             req.status_code,
             uri,
@@ -34,19 +34,19 @@ def get_uri(uri, localfn):
 def get_files(year):
     """Go get our files and then cache them!"""
     myuri = (
-        "%s?simple=yes&year1=%s&month1=1&day1=1&hour1=0&minute1=0"
-        "&year2=%s&month2=12&day2=31&hour2=23&minute2=59"
-    ) % (URL, year, year)
-    get_uri(myuri, "%s/%s_all.zip" % (FINAL, year))
+        f"{URL}?simple=yes&year1={year}&month1=1&day1=1&hour1=0&minute1=0"
+        f"&year2={year}&month2=12&day2=31&hour2=23&minute2=59"
+    )
+    get_uri(myuri, f"{FINAL}/{year}_all.zip")
 
     # Now do SBW variant
-    myuri = "%s&limit0=yes" % (myuri,)
-    get_uri(myuri, "%s/%s_tsmf.zip" % (FINAL, year))
+    myuri = f"{myuri}&limit0=yes"
+    get_uri(myuri, f"{FINAL}/{year}_tsmf.zip")
 
     if year > 2001:
         # Do SBW
-        myuri = "%s&limit1=yes" % (myuri,)
-        get_uri(myuri, "%s/%s_tsmf_sbw.zip" % (FINAL, year))
+        myuri = f"{myuri}&limit1=yes"
+        get_uri(myuri, f"{FINAL}/{year}_tsmf_sbw.zip")
 
 
 def main(argv):
