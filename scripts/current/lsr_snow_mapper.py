0f1bef4faf7c3904e0c28cc9255a93557275fec4
akrherz <akrherz@iastate.edu>
2022-11-29 09:15:29 -0600

    add property update with snow LSR receipt

--- scripts/current/lsr_snow_mapper.py
+++ scripts/current/lsr_snow_mapper.py
@@ -4,9 +4,41 @@ import os
 import subprocess
 
 import requests
-from pyiem.util import logger, utc, exponential_backoff
+from pyiem.util import (
+    exponential_backoff,
+    get_properties,
+    get_dbconn,
+    logger,
+    utc,
+)
 
 LOG = logger()
+
+
+def website_enable_check():
+    """See if we should turn on the homepage graphic."""
+    # See what our current status is
+    status = get_properties().get("homepage.lsrmap.on") == "true"
+    # See if we have any recent LSRs
+    postgis = get_dbconn("postgis")
+    cursor = postgis.cursor()
+    cursor.execute(
+        "SELECT valid from lsrs WHERE valid > now() - '12 hours'::interval "
+        "and state = 'IA' and type = 'S'"
+    )
+    has_lsrs = cursor.rowcount > 0
+    postgis.close()
+    if status == has_lsrs:
+        return
+    LOG.warning("Setting homepage.lsrmap.on to %s", has_lsrs)
+    mesosite = get_dbconn("mesosite")
+    cursor = mesosite.cursor()
+    cursor.execute(
+        "UPDATE properties SET propvalue = %s where propname = %s",
+        ("true" if has_lsrs else "false", "homepage.lsrmap.on"),
+    )
+    cursor.close()
+    mesosite.commit()
 
 
 def do(url, fn):
@@ -45,6 +77,8 @@ def main():
         "::p:contour::hours:12::sz:25.png"
     )
     do(url, "mw_lsr_snowfall.png")
+
+    website_enable_check()
 
 
 if __name__ == "__main__":
