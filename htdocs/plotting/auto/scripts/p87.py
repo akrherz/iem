58ce077a9f41bba8188747b613fcae026e5552b0
akrherz <akrherz@iastate.edu>
2022-12-06 09:16:58 -0600

    add thundersnow option
    https://mesonet.agron.iastate.edu/plotting/auto/?q=87

--- htdocs/plotting/auto/scripts/p87.py
+++ htdocs/plotting/auto/scripts/p87.py
@@ -9,17 +9,16 @@ from pyiem.plot import figure
 from pyiem.util import get_autoplot_context, get_sqlalchemy_conn
 from pyiem.exceptions import NoDataFound
 
-PDICT = dict(
-    (
-        ("BLSN", "Blowing Snow (BLSN)"),
-        ("FG", "Fog (FG)"),
-        ("FZFG", "Freezing Fog (FZFG)"),
-        ("FZRA", "Freezing Rain (FZRA)"),
-        ("PSN", "Heavy Snow (+SN)"),  # +SN causes CGI issues
-        ("-SN", "Light Snow (-SN)"),
-        ("TS", "Thunder (TS)"),
-    )
-)
+PDICT = {
+    "BLSN": "Blowing Snow (BLSN)",
+    "FG": "Fog (FG)",
+    "FZFG": "Freezing Fog (FZFG)",
+    "FZRA": "Freezing Rain (FZRA)",
+    "PSN": "Heavy Snow (+SN)",  # +SN causes CGI issues
+    "-SN": "Light Snow (-SN)",
+    "TS": "Thunder (TS)",
+    "TSSN": "Thundersnow (TSSN)",
+}
 PDICT2 = {"week": "group by week of year", "doy": "group by day of year"}
 
 
