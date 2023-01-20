2445d7c7e4f425abf8dcd76938c534118ce473db
akrherz <akrherz@iastate.edu>
2023-01-19 21:47:42 -0600

    fix bugs/lint in pests app

--- htdocs/topics/pests/main.js
+++ htdocs/topics/pests/main.js
@@ -6,10 +6,15 @@ pestData.soybean_aphid = { 'gddbase': 50, 'gddceil': 90 };
 pestData.common_stalk_borer = { 'gddbase': 41, 'gddceil': 90 };
 pestData.japanese_beetle = { 'gddbase': 50, 'gddceil': 90 };
 
+function text(str) {
+    // XSS
+    return $("<p>").text(str).html();
+}
+
 function hideImageLoad() {
     {
         $('#willload').css('display', 'none');
-        var url = $("#theimage").attr("src").replace(".png", "");
+        const url = $("#theimage").attr("src").replace(".png", "");
         $("#thedata").html(
             '<p>Download point data: <a href="' + url + '.txt" class="btn btn-primary">' +
             '<i class="fa fa-table"></i> As CSV</a> &nbsp;' +
@@ -19,11 +24,11 @@ function hideImageLoad() {
 }
 
 function updateStationForecast() {
-    var station = $("select[name='station']").val();
-    var pest = $("select[name='pest']").val()
-    var opts = pestData[pest];
-    var sdate = $("#sdate").val();
-    var edate = $("#edate").val();
+    const station = text($("select[name='station']").val());
+    const pest = text($("select[name='pest']").val());
+    const opts = pestData[pest];
+    const sdate = text($("#sdate").val());
+    const edate = text($("#edate").val());
     var url = "/json/climodat_dd.py?station=" + station + "&gddbase=" + opts.gddbase +
         "&gddceil=" + opts.gddceil + "&sdate=" + sdate + "&edate=" + edate;
     $.get(url, function (data) {
@@ -43,26 +48,26 @@ function updateStationForecast() {
 function updateImage() {
     showProgressBar();
     $("#theimage").attr("src", "/images/pixel.gif");
-    var station = $("select[name='station']").val();
-    var pest = $("select[name='pest']").val();
+    const station = text($("select[name='station']").val());
+    const pest = text($("select[name='pest']").val());
 
     // Hide all the pinfo containers
     $('.pinfo').css('display', 'none');
 
     // Show this pest's pinfo container
     $('#' + pest).css('display', 'block');
-    var opts = pestData[pest];
-    var sdate = $("#sdate").val();
-    var edate = $("#edate").val();
-    var state = $("select[name='state']").val();
+    const opts = pestData[pest];
+    const sdate = text($("#sdate").val());
+    const edate = text($("#edate").val());
+    let state = text($("select[name='network']").val());
     state = (state !== undefined) ? state.substring(0, 2) : "IA";
-    var imgurl = "/plotting/auto/plot/97/d:sector::sector:" + state + "::var:gdd_sum::" +
+    const imgurl = "/plotting/auto/plot/97/d:sector::sector:" + state + "::var:gdd_sum::" +
         "gddbase:" + opts.gddbase + "::gddceil:" + opts.gddceil + "::date1:" + sdate + "::usdm:no::" +
         "date2:" + edate + "::p:contour::cmap:RdYlBu_r::c:yes::_r:43.png";
     $("#theimage").attr("src", imgurl);
 
     // Update the web browser URL
-    var url = "/topics/pests/?state=" + state + "&pest=" + pest + "&sdate="
+    let url = "/topics/pests/?state=" + state + "&pest=" + pest + "&sdate="
         + sdate + "&station=" + station;
     // is edate_off checked?
     if (!$("#edate_off").is(':checked')) {
@@ -74,14 +79,14 @@ function updateImage() {
 
 function showProgressBar() {
     $('#willload').css('display', 'block');
-    var timing = 0;
-    var progressBar = setInterval(function () {
+    let timing = 0;
+    var progressBar = setInterval(() => {
         {
             if (timing >= 10 ||
                 $('#willload').css('display') == 'none') {
-                    {
-                        clearInterval(progressBar);
-                    }
+                {
+                    clearInterval(progressBar);
+                }
             }
             var width = (timing / 10) * 100.;
             $("#timingbar").css('width', width + '%').attr('aria-valuenow', width);
@@ -91,16 +96,16 @@ function showProgressBar() {
 }
 
 function setupUI() {
-    $('#theimage').on('load', function () {
-        {
-            hideImageLoad();
-        }
-    });
-    $('#theimage').on('error', function () {
-        {
-            hideImageLoad();
-        }
-    });
+    $('#theimage').on('load', () => {
+            {
+                hideImageLoad();
+            }
+        });
+    $('#theimage').on('error', () => {
+            {
+                hideImageLoad();
+            }
+        });
     // The image may be cached and return to the user before this javascript
     // is hit, so we do a check to see if it is indeed loaded now
     if ($("#theimage").get(0) && $("#theimage").get(0).complete) {
@@ -113,7 +118,7 @@ function setupUI() {
         dateFormat: 'yy-mm-dd',
         changeMonth: true,
         changeYear: true,
-        onSelect: function (dateText, inst) {
+        onSelect(_dateText, _inst) {
             updateImage();
         }
     });
@@ -121,12 +126,12 @@ function setupUI() {
         dateFormat: 'yy-mm-dd',
         changeMonth: true,
         changeYear: true,
-        onSelect: function (dateText, inst) {
+        onSelect(_dateText, _inst) {
             updateImage();
         }
     });
 
-    $("select[name='station']").change(function () {
+    $("select[name='station']").change(() => {
         updateStationForecast();
     });
 }
