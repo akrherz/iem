8a2238ae86cb64632b93df802866c7732b9bc6bc
akrherz <akrherz@iastate.edu>
2023-02-16 09:42:27 -0600

    add KML export option to GIS LSR download
    https://mesonet.agron.iastate.edu/request/gis/lsrs.phtml

--- cgi-bin/request/gis/lsr.py
+++ cgi-bin/request/gis/lsr.py
@@ -1,14 +1,16 @@
 """Dumps Local Storm Reports on-demand for web requests."""
 import datetime
 import zipfile
-import os
 from io import BytesIO, StringIO
 
+import fiona
 import shapefile
 import pandas as pd
+import geopandas as gpd
 from paste.request import parse_formvars
 from pyiem.util import get_dbconn, utc, get_sqlalchemy_conn
 
+fiona.supported_drivers["KML"] = "rw"
 EXL = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
 ISO8660 = "%Y-%m-%dT%H:%M"
 
@@ -49,10 +51,10 @@ def get_time_domain(form):
     return sts, ets
 
 
-def do_excel(sts, ets, wfolimiter, statelimiter):
-    """Export as Excel."""
+def do_excel_kml(fmt, sts, ets, wfolimiter, statelimiter):
+    """Export as Excel or KML."""
     with get_sqlalchemy_conn("postgis") as conn:
-        df = pd.read_sql(
+        df = gpd.read_postgis(
             f"""
             WITH wfos as (
                 select case when length(id) = 4 then substr(id, 1, 3)
@@ -60,7 +62,8 @@ def do_excel(sts, ets, wfolimiter, statelimiter):
             ), reports as (
                 select distinct l.wfo, valid, county, city, l.state, typetext,
                 magnitude, l.source, ST_y(l.geom) as lat, ST_x(l.geom) as lon,
-                coalesce(remark, '') as remark, u.ugc, u.name as ugcname
+                coalesce(remark, '') as remark, u.ugc, u.name as ugcname,
+                l.geom
                 from lsrs l LEFT JOIN ugcs u on (l.gid = u.gid) WHERE
                 valid >= %s and valid < %s {wfolimiter} {statelimiter}
             )
@@ -70,11 +73,12 @@ def do_excel(sts, ets, wfolimiter, statelimiter):
             to_char(valid at time zone 'UTC',
                 'YYYY/MM/DD HH24:MI') as utcvalid,
             county, city, state, typetext, magnitude, source, lat, lon,
-            remark, ugc, ugcname
+            remark, ugc, ugcname, geom
             from reports l JOIN wfos w on (l.wfo = w.cwa)
             ORDER by utcvalid ASC""",
             conn,
             params=(sts, ets),
+            geom_col="geom",
         )
     df = df.rename(
         {
@@ -93,19 +97,27 @@ def do_excel(sts, ets, wfolimiter, statelimiter):
         },
         axis=1,
     )
-    bio = BytesIO()
-    # pylint: disable=abstract-class-instantiated
-    writer = pd.ExcelWriter(bio, engine="xlsxwriter")
-    df.to_excel(writer, "Local Storm Reports", index=False)
-    worksheet = writer.sheets["Local Storm Reports"]
-    worksheet.set_column("B:C", 36)
-    worksheet.set_column("D:E", 24)
-    worksheet.set_column("G:G", 24)
-    worksheet.set_column("I:I", 24)
-    worksheet.set_column("L:L", 100)
-    worksheet.freeze_panes(1, 0)
-    writer.close()
-    return bio.getvalue()
+    if fmt == "excel":
+        df = df.drop(columns="geom")
+        bio = BytesIO()
+        # pylint: disable=abstract-class-instantiated
+        writer = pd.ExcelWriter(bio, engine="xlsxwriter")
+        df.to_excel(writer, "Local Storm Reports", index=False)
+        worksheet = writer.sheets["Local Storm Reports"]
+        worksheet.set_column("B:C", 36)
+        worksheet.set_column("D:E", 24)
+        worksheet.set_column("G:G", 24)
+        worksheet.set_column("I:I", 24)
+        worksheet.set_column("L:L", 100)
+        worksheet.freeze_panes(1, 0)
+        writer.close()
+        return bio.getvalue()
+    # KML
+    df["name"] = df["Location"] + ": " + df["Event Type"]
+    fp = BytesIO()
+    with fiona.drivers():
+        df.to_file(fp, driver="KML", NameField="name")
+    return fp.getvalue()
 
 
 def application(environ, start_response):
@@ -136,14 +148,14 @@ def application(environ, start_response):
         aStates = form.getall("state")
         aStates.append("XX")
         if "_ALL" not in aStates:
-            statelimiter = " and l.state in %s " % (str(tuple(aStates)),)
+            statelimiter = f" and l.state in {tuple(aStates)} "
 
     wfoLimiter = ""
     if "wfo[]" in form:
         aWFO = form.getall("wfo[]")
         aWFO.append("XXX")  # Hack to make next section work
         if "ALL" not in aWFO:
-            wfoLimiter = " and l.wfo in %s " % (str(tuple(aWFO)),)
+            wfoLimiter = f" and l.wfo in {tuple(aWFO)} "
 
     fn = f"lsr_{sts:%Y%m%d%H%M}_{ets:%Y%m%d%H%M}"
     if form.get("fmt", "") == "excel":
@@ -152,12 +164,15 @@ def application(environ, start_response):
             ("Content-disposition", f"attachment; Filename={fn}.xlsx"),
         ]
         start_response("200 OK", headers)
-        return [do_excel(sts, ets, wfoLimiter, statelimiter)]
+        return [do_excel_kml("excel", sts, ets, wfoLimiter, statelimiter)]
 
-    os.chdir("/tmp")
-    for suffix in ["shp", "shx", "dbf", "csv"]:
-        if os.path.isfile("%s.%s" % (fn, suffix)):
-            os.remove("%s.%s" % (fn, suffix))
+    if form.get("fmt", "") == "kml":
+        headers = [
+            ("Content-type", "application/octet-stream"),
+            ("Content-disposition", f"attachment; Filename={fn}.kml"),
+        ]
+        start_response("200 OK", headers)
+        return [do_excel_kml("kml", sts, ets, wfoLimiter, statelimiter)]
 
     csv = StringIO()
     csv.write(
@@ -225,25 +240,12 @@ def application(environ, start_response):
                     .replace(",", "_")
                 )
             shp.record(*row[:-1])
+            row5 = row[5].encode("utf-8", "ignore").decode("ascii", "ignore")
+            row9 = row[9] if row[9] is not None else ""
             csv.write(
-                ("%s,%s,%.2f,%.2f,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n")
-                % (
-                    row[0],
-                    row[12],
-                    row[10],
-                    row[11],
-                    row[1],
-                    row[2],
-                    row[3],
-                    row[4],
-                    row[5].encode("utf-8", "ignore").decode("ascii", "ignore"),
-                    row[6],
-                    row[7],
-                    row[8],
-                    row[9] if row[9] is not None else "",
-                    row[13],
-                    row[14],
-                )
+                f"{row[0]},{row[12]},{row[10]:.2f},{row[11]:.2f},{row[1]},"
+                f"{row[2]},{row[3]},{row[4]},{row5},{row[6]},{row[7]},"
+                f"{row[8]},{row9},{row[13]},{row[14]}\n"
             )
 
     if "justcsv" in form or form.get("fmt", "") == "csv":
