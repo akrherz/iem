ffdbfbd9dbd08873d76ea22f35299b572f2d4f2f
akrherz <akrherz@iastate.edu>
2023-02-25 06:54:35 -0600

    correct NASS ingest logic

--- scripts/ingestors/nass_quickstats.py
+++ scripts/ingestors/nass_quickstats.py
@@ -67,11 +67,16 @@ def process(df):
         # Ignore duplicated data like CONDITION, 5 YEAR AVG
         if row["statisticcat_desc"].find("YEAR") > 0:
             continue
+        # fix SQL comparator
+        cc = "is" if row["week_ending"] is None else "="
+        cf = ""
+        if row["county_ansi"] is not None:
+            cf = f"and county_ansi = '{row['county_ansi']}'"
         # Uniqueness by short_desc, year, state_alpha, week_ending
         cursor.execute(
             "SELECT load_time from nass_quickstats where year = %s "
-            "and short_desc = %s and state_alpha = %s and week_ending = %s "
-            "and freq_desc = %s",
+            "and short_desc = %s and state_alpha = %s "
+            f"and week_ending {cc} %s and freq_desc = %s {cf}",
             (
                 row["year"],
                 row["short_desc"],
@@ -88,7 +93,8 @@ def process(df):
             cursor.execute(
                 "DELETE from nass_quickstats where year = %s "
                 "and short_desc = %s and "
-                "state_alpha = %s and week_ending = %s and freq_desc = %s",
+                f"state_alpha = %s and week_ending {cc} %s and freq_desc = %s "
+                f"{cf}",
                 (
                     row["year"],
                     row["short_desc"],
@@ -163,7 +169,7 @@ def main(argv):
         years = range(1980, datetime.now().year + 1)
     else:
         years = [None]
-        sts = (datetime.now() - timedelta(days=1)).replace(
+        sts = (datetime.now() - timedelta(days=2)).replace(
             hour=0, minute=0, second=0
         )
 
