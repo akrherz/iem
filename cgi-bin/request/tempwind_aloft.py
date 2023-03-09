75c300ef95cb17ebe9393bc5d613546846de4ba6
akrherz <akrherz@iastate.edu>
2023-03-09 09:54:44 -0600

    add firstpass download of temps / winds aloft data
    https://mesonet.agron.iastate.edu/request/tempwind_aloft.php
    refs akrherz/pyIEM#713

---
+++ cgi-bin/request/tempwind_aloft.py
@@ -0,0 +1,93 @@
+"""Download tempwinds_aloft dataset"""
+from io import StringIO, BytesIO
+import datetime
+
+import pytz
+import pandas as pd
+from paste.request import parse_formvars
+from sqlalchemy import text
+from pyiem.util import get_sqlalchemy_conn
+
+EXL = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+
+
+def get_data(station, sts, ets, tz, na, fmt):
+    """Go fetch data please"""
+    with get_sqlalchemy_conn("asos") as conn:
+        df = pd.read_sql(
+            text(
+                """
+            SELECT *,
+            to_char(obtime at time zone :tz, 'YYYY/MM/DD HH24:MI')
+                as obtime2,
+            to_char(ftime at time zone :tz, 'YYYY/MM/DD HH24:MI')
+                as ftime2
+            from alldata_tempwind_aloft WHERE ftime >= :sts and
+            ftime <= :ets and station = :station ORDER by obtime, ftime"""
+            ),
+            conn,
+            params={"sts": sts, "ets": ets, "station": station, "tz": tz},
+        )
+    df = df.drop(columns=["obtime", "ftime"]).rename(
+        columns={"obtime2": "obtime", "ftime2": "ftime"}
+    )
+    cols = df.columns.values.tolist()
+    cols.remove("ftime")
+    cols.remove("obtime")
+    cols.insert(1, "obtime")
+    cols.insert(2, "ftime")
+    df = df[cols].dropna(axis=1, how="all")
+    if na != "blank":
+        df = df.fillna(na)
+    if fmt == "json":
+        return df.to_json(orient="records")
+    if fmt == "excel":
+        bio = BytesIO()
+        with pd.ExcelWriter(bio, engine="xlsxwriter") as writer:
+            df.to_excel(writer, "Data", index=False)
+        return bio.getvalue()
+
+    sio = StringIO()
+    df.to_csv(sio, index=False)
+    return sio.getvalue()
+
+
+def parse_dates(form):
+    """Nicely resolve dates please."""
+    sts = datetime.date(
+        int(form.get("year1")), int(form.get("month1")), int(form.get("day1"))
+    )
+    ets = datetime.date(
+        int(form.get("year2")), int(form.get("month2")), int(form.get("day2"))
+    )
+    return sts, ets
+
+
+def application(environ, start_response):
+    """See how we are called"""
+    form = parse_formvars(environ)
+    try:
+        sts, ets = parse_dates(form)
+    except Exception:
+        start_response(
+            "500 Internal Server Error", [("Content-type", "text/plain")]
+        )
+        return [b"Error while parsing provided dates, ensure they are valid."]
+
+    fmt = form.get("format", "csv")
+    tz = form.get("tz", "UTC")
+    pytz.timezone(tz)
+    station = form.get("station")[:4]
+    na = form.get("na", "M")
+    if na not in ["M", "None", "blank"]:
+        start_response("200 OK", [("Content-type", "text/plain")])
+        return [b"ERROR: Invalid `na` value provided. {M, None, blank}"]
+    if fmt != "excel":
+        start_response("200 OK", [("Content-type", "text/plain")])
+        return [get_data(station, sts, ets, tz, na, fmt).encode("ascii")]
+    headers = [
+        ("Content-type", EXL),
+        ("Content-disposition", f"attachment; Filename={station}.xlsx"),
+    ]
+    start_response("200 OK", headers)
+    return [get_data(station, sts, ets, tz, na, fmt)]
--- htdocs/request/tempwind_aloft.php
+++ htdocs/request/tempwind_aloft.php
@@ -6,58 +6,79 @@ require_once "../../include/forms.php";
 require_once "../../include/iemprop.php";
 require_once "../../include/database.inc.php";
 
-define("IEM_APPID", 139);
+define("IEM_APPID", 30);
 $t = new MyView();
 $t->iemss = True;
-$t->title = "Terminal Aerodome Forecast (TAF) Data Download";
+$t->title = "Temps and Winds Aloft (FD) Data Download";
 
 $bogus = 0;
-$y1select = yearSelect2(1996, date("Y"), "year1");
+$y1select = yearSelect2(2004, date("Y"), "year1");
 $m1select = monthSelect(1, "month1");
 $d1select = daySelect2(1, "day1");
 
-$y2select = yearSelect2(1996, date("Y"), "year2");
+$y2select = yearSelect2(2004, date("Y"), "year2");
 $m2select = monthSelect(date("m"), "month2");
 $d2select = daySelect2(date("d"), "day2");
 
-$t->headextra = <<<EOM
-<style type="text/css">
-#map {
-    width: 100%;
-    height: 450px;
-    border: 2px solid black;
-}
-</style>
+$pgconn = iemdb("asos");
+$sql = <<<EOM
+with data as (
+    select distinct station from alldata_tempwind_aloft WHERE
+    ftime > (now() - '5 days'::interval)
+    )
+select station, name from data d LEFT JOIN stations t on
+((case when substr(d.station, 1, 1) = 'K' then substr(d.station, 2, 3)
+ else d.station end) = t.id) ORDER by station ASC
 EOM;
+$rs = pg_query($pgconn, $sql);
+$sids = Array();
+for ($i = 0; $row = pg_fetch_array($rs); $i++) {
+    $name = is_null($row["name"]) ? "((Unknown))": $row["name"];
+    $sids[$row["station"]] = $name;
+}
+$sselect = make_select("station", "KDSM", $sids, '', '', FALSE, TRUE);
+
 $t->content = <<<EOF
 <ol class="breadcrumb">
  <li><a href="/nws/">NWS Mainpage</a></li>
- <li class="active">Download TAF data</li>
+ <li class="active">Download Temps and Winds Aloft (FD) Data</li>
 </ol>
 
-<p>The IEM attempts a high fidelity processing and archival of Terminal
-Aerodome Forecasts (TAF)s.  This page allows an atomic data download of the
-processed data.  If you are wishing to download the raw NWS text TAF data,
-try looking <a href="/wx/afos/p.php?pil=TAFDSM">here</a> as a starting point.</p>
+<p>This page presents data found in the NWS text products like
+try looking <a href="/wx/afos/p.php?pil=FD1US1">FD1US1</a>.  They contain
+near term forecasts of </p>
 
-<p>The TAF archive dates back to 1 January 1996.</p>
+<p>The archive dates back to 23 September 2004.</p>
 
 <p><strong>Related:</strong>
 <a class="btn btn-primary" href="/request/gis/cwas.phtml">CWSU Center Weather Advisories</a>
 <a class="btn btn-primary" href="/request/gis/awc_gairmets.phtml">Graphical AIRMETs</a>
 <a class="btn btn-primary" href="/request/gis/pireps.php">PIREPs</a>
 <a class="btn btn-primary" href="/request/gis/awc_sigmets.phtml">SIGMETs</a>
-<a class="btn btn-primary" href="/request/tempwind_aloft.php">Temp/Winds Aloft</a>
 </p>
 
-<form method="GET" action="/cgi-bin/request/taf.py" name="dl" target="_blank">
+<form method="GET" action="/cgi-bin/request/tempwind_aloft.py" name="dl" target="_blank">
 
 <div class="row">
 <div class="col-md-6">
 
 <p><h3>1. Select Station:</h3><br>
-<br />
-<div id="iemss" data-network="TAF" data-name="station" data-supports-all="0"></div>
+{$sselect}
+
+<p><strong>Returned Columns</strong>
+
+<table>
+<thead><tr><th>column</th><th>Description</th></tr></thead>
+<tbody>
+<tr><td>obtime</td><td>The timestamp within the FD product by which the forecast
+is said to be based off.</td></tr>
+<tr><td>ftime</td><td>The timestamp the values are valid for / forecast timestamp</td></tr>
+<tr><td>tmpc#####</td><td>Air Temperature (&deg;C) at given altitude</td></tr> 
+<tr><td>sknt#####</td><td>Wind Speed (kts) at given altitude</td></tr> 
+<tr><td>drct#####</td><td>Wind Direction (&deg;) at given altitude</td></tr> 
+</tbody>
+</table>
+</p>
 
 </div>
 <div class="col-md-6">
@@ -74,7 +95,8 @@ timezone you specify.</i>
 </SELECT>
 
 <h3>3. Select Start/End Time:</h3><br>
-<i>The end date is not inclusive.</i>
+<i>This limits the data returned for forecast times between the start
+and end date.</i>
 <table class="table table-condensed">
 <thead>
   <tr>
@@ -96,7 +118,7 @@ timezone you specify.</i>
 </table>
 
 <h3>4. Data Format:</h3>
-<select name="fmt">
+<select name="format">
     <option value="comma">Comma</option>
     <option value="excel">Excel</option>
 </select>
@@ -104,29 +126,6 @@ timezone you specify.</i>
 <h3>Submit Form:</h3><br>
 <input type="submit" value="Process Data Request">
 <input type="reset">
-
-<p><strong>Data Columns</strong>
-<table class="table table-striped">
-<thead><tr><th>Name</th><th>Description</th></tr></thead>
-<tbody>
-<tr><th>station</th><td>4 character station identifier</td></tr>
-<tr><th>valid</th><td>TAF issuance timestamp</td></tr>
-<tr><th>fx_valid</th><td>Forecast Timestamp</td></tr>
-<tr><th>raw</th><td>Raw TAF string for this forecast time</td></tr>
-<tr><th>is_tempo</th><td>Is Tempo set?</td></tr>
-<tr><th>fx_end_valid</th><td>Forecast timestamp end time (when set).</td></tr>
-<tr><th>sknt</th><td>Wind Speed (kts)</td></tr>
-<tr><th>drct</th><td>Wind Direction (deg)</td></tr>
-<tr><th>gust</th><td>Wind Gust (kts)</td></tr>
-<tr><th>visibility</th><td>Horizontal Visibility (miles). <strong>Note</strong> 
-Greater than 6 miles is encoded as 6.01</td></tr>
-<tr><th>skyc</th><td>Sky Coverages</td></tr>
-<tr><th>skyl</th><td>Sky Coverage Levels (feet)</td></tr>
-<tr><th>ws_drct</th><td>Wind Shift Direction (deg)</td></tr>
-<tr><th>ws_sknt</th><td>Wind Shift Wind Speed (kts)</td></tr>
-<tr><th>product_id</th><td>IEM NWS Text Product Identifier</td></tr>
-</tbody>
-</table>
 
 </div></div>
 
