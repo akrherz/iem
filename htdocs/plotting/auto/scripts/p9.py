716a1633ffb7d79b05e070991cc7f2c9d7c96ccd
akrherz <akrherz@iastate.edu>
2023-04-25 11:34:16 -0500

    improve returned df, allow isolated dates

--- htdocs/plotting/auto/scripts/p9.py
+++ htdocs/plotting/auto/scripts/p9.py
@@ -1,9 +1,13 @@
-"""GDD climo"""
-import calendar
+"""
+This chart produces the daily climatology of
+Degree Days for a location of your choice. Please note that
+Feb 29 is not considered for this analysis.
+"""
 import datetime
 
-import numpy as np
+import matplotlib.dates as mdates
 import pandas as pd
+from sqlalchemy import text
 from pyiem.plot import figure_axes
 from pyiem.util import get_autoplot_context, get_sqlalchemy_conn
 from pyiem.exceptions import NoDataFound
@@ -14,17 +18,15 @@ PDICT = {
     "hdd": "Heating Degree Days",
     "sdd": "Stress Degree Days",
 }
+PDICT2 = {
+    "daily": "Daily Accumulated",
+    "ytd": "Year to Date Accumulation",
+}
 
 
 def get_description():
     """Return a dict describing how to call this plotter"""
-    desc = {}
-    desc["data"] = True
-    desc[
-        "description"
-    ] = """This chart produces the daily climatology of
-    Degree Days for a location of your choice. Please note that
-    Feb 29 is not considered for this analysis."""
+    desc = {"description": __doc__, "data": True}
     thisyear = datetime.date.today().year
     desc["arguments"] = [
         dict(
@@ -41,6 +43,18 @@ def get_description():
             min=1893,
             label="Select Year:",
         ),
+        {
+            "type": "sday",
+            "default": "0101",
+            "name": "sday",
+            "label": "Start Day of the Year for Plot",
+        },
+        {
+            "type": "sday",
+            "default": "1231",
+            "name": "eday",
+            "label": "Inclusive End Day of the Year for Plot",
+        },
         dict(
             type="select",
             options=PDICT,
@@ -48,6 +62,13 @@ def get_description():
             name="var",
             label="Which variable to plot:",
         ),
+        {
+            "type": "select",
+            "options": PDICT2,
+            "label": "Plot Daily or YTD Accumulated?",
+            "name": "w",
+            "default": "daily",
+        },
         dict(
             type="int",
             name="base",
@@ -90,28 +111,45 @@ def plotter(fdict):
         title = f"base={ceiling}"
     with get_sqlalchemy_conn("coop") as conn:
         df = pd.read_sql(
-            f"SELECT year, sday, {gfunc} as {glabel} from alldata WHERE "
-            "station = %s and year > 1892 and sday != '0229'",
+            text(
+                f"""SELECT year, sday, {gfunc} as {glabel} from alldata WHERE
+            station = :sid and year > 1892 and sday != '0229'
+            and sday >= :sday and sday <= :eday ORDER by day ASC
+            """
+            ),
             conn,
-            params=(station,),
+            params={
+                "sid": station,
+                "sday": f"{ctx['sday']:%m%d}",
+                "eday": f"{ctx['eday']:%m%d}",
+            },
         )
     if df.empty:
         raise NoDataFound("No data Found.")
 
-    # Do some magic!
+    if ctx["w"] == "ytd":
+        df["accum"] = df[["year", glabel]].groupby("year").cumsum()
+        glabel = "accum"
     df2 = (
         df[["sday", glabel]]
         .groupby("sday")
         .describe(percentiles=[0.05, 0.25, 0.75, 0.95])
     )
     df2 = df2.unstack(level=-1)
+    tt = f"{ctx['sday']:%-d %b} thru {ctx['eday']:%-d %b}"
     title = (
-        f"({syear}-{thisyear}) {ctx['_sname']}\n"
+        f"({syear}-{thisyear}) [{tt}] {ctx['_sname']}\n"
         f"{year} {PDICT[varname]} ({title})"
+    )
+    # collapse the multi-index for columns
+    retdf = df2.reset_index().pivot(index="sday", columns="level_1", values=0)
+    # avoid leap year
+    xaxis = pd.to_datetime(
+        "2001" + retdf.reset_index()["sday"], format="%Y%m%d"
     )
     (fig, ax) = figure_axes(title=title, apctx=ctx)
     ax.plot(
-        np.arange(1, 366),
+        xaxis,
         df2[(glabel, "mean")],
         color="r",
         zorder=2,
@@ -120,14 +158,14 @@ def plotter(fdict):
     )
     _data = df[df["year"] == year][[glabel, "sday"]].sort_values(by="sday")
     ax.scatter(
-        np.arange(1, _data[glabel].shape[0] + 1),
+        pd.to_datetime("2001" + _data["sday"], format="%Y%m%d"),
         _data[glabel],
         color="b",
         zorder=2,
         label=f"{year}",
     )
     ax.bar(
-        np.arange(1, 366),
+        xaxis,
         df2[(glabel, "95%")] - df2[(glabel, "5%")],
         bottom=df2[(glabel, "5%")],
         ec="tan",
@@ -136,7 +174,7 @@ def plotter(fdict):
         label="5-95 Percentile",
     )
     ax.bar(
-        np.arange(1, 366),
+        xaxis,
         df2[(glabel, "75%")] - df2[(glabel, "25%")],
         bottom=df2[(glabel, "25%")],
         ec="lightblue",
@@ -144,19 +182,18 @@ def plotter(fdict):
         zorder=1,
         label="25-75 Percentile",
     )
-    ax.set_xlim(0, 367)
-    if varname == "gdd":
+    ax.set_xlim(xaxis.values[0], xaxis.values[-1])
+    if varname == "gdd" and ctx["w"] == "daily":
         ax.set_ylim(-0.25, 40)
     ax.grid(True)
-    ax.set_ylabel(r"Daily Accumulation $^{\circ}\mathrm{F}$")
-    ax.set_xticks((1, 32, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335))
-    ax.set_xticklabels(calendar.month_abbr[1:])
+    ax.set_ylabel(f"{PDICT2[ctx['w']]}" r" $^{\circ}\mathrm{F}$")
+    ax.xaxis.set_major_formatter(
+        mdates.DateFormatter("%-d %b"),
+    )
     ax.legend(ncol=2)
 
-    # collapse the multi-index for columns
-    df = pd.DataFrame(df2)
-    return fig, df
+    return fig, retdf
 
 
 if __name__ == "__main__":
-    plotter({})
+    plotter({"w": "ytd"})
