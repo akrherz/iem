609c3636387c63180d0a98a5ddd85444dd3a5e6b
akrherz <akrherz@iastate.edu>
2023-05-22 17:15:18 -0500

    print top10 daily vtec totals too
    https://mesonet.agron.iastate.edu/plotting/auto/?q=171

--- htdocs/plotting/auto/scripts100/p171.py
+++ htdocs/plotting/auto/scripts100/p171.py
@@ -1,4 +1,18 @@
-"""Distinct VTEC"""
+"""
+This chart displays the monthly number of distinct
+NWS Office issued watch / warning / advisory product. For example, a
+single watch for a dozen counties would only count 1 in this chart. These
+values are based on unofficial archives maintained by the IEM.
+
+<p>The chart summarizes by the month of issuance, so long fuse products like
+Flood Warnings that cover multiple months in time would only appear once for
+the month of issuance.</p>
+
+<p>If you select the state wide option, the totaling is a bit different. A
+single watch issued by multiple WFOs would potentially count as more than
+one event in this listing.  Sorry, tough issue to get around.  In the case
+of warnings and advisories, the totals should be good.</p>
+"""
 import calendar
 
 import numpy as np
@@ -16,25 +30,16 @@ PDICT = {
     "state": "Select by State",
     "ugc": "Select by NWS County/Forecast Zone",
 }
+PDICT2 = {
+    "single": "Total for Single Selected Phenomena / Significance",
+    "svrtor": "Severe T'Storm + Tornado Warnings",
+    "svrtorffw": "Severe T'Storm + Tornado + Flash Flood Warnings",
+}
 
 
 def get_description():
     """Return a dict describing how to call this plotter"""
-    desc = {}
-    desc["data"] = True
-    desc["cache"] = 86400
-    desc[
-        "description"
-    ] = """This chart displays the monthly number of distinct
-    NWS Office issued watch / warning / advisory product. For example, a
-    single watch for a dozen counties would only count 1 in this chart. These
-    values are based on unofficial archives maintained by the IEM.
-
-    <p>If you select the state wide option, the totaling is a bit different. A
-    single watch issued by multiple WFOs would potentially count as more than
-    one event in this listing.  Sorry, tough issue to get around.  In the case
-    of warnings and advisories, the totals should be good.</p>
-    """
+    desc = {"description": __doc__, "data": True, "cache": 86400}
     desc["arguments"] = [
         dict(
             type="select",
@@ -58,6 +63,13 @@ def get_description():
             default="IAC169",
             label="Select UGC Zone/County:",
         ),
+        {
+            "type": "select",
+            "options": PDICT2,
+            "name": "c",
+            "default": "single",
+            "label": "Total by Selected Phenomena/Significance or Combo",
+        },
         dict(
             type="phenomena",
             name="phenomena",
@@ -92,11 +104,15 @@ def plotter(fdict):
     significance = ctx["significance"]
     opt = ctx["opt"]
     state = ctx["state"]
-    ctx["_nt"].sts["_ALL"] = {"name": "All Offices"}
+    ctx["_nt"].sts["_ALL"] = {
+        "name": "All Offices",
+        "tzname": "America/Chicago",
+    }
 
     params = {
-        "ph": phenomena,
+        "ph": (phenomena,),
         "sig": significance,
+        "tzname": ctx["_nt"].sts[station]["tzname"],
     }
     wfo_limiter = " and wfo = :wfo "
     params["wfo"] = station if len(station) == 3 else station[1:]
@@ -110,22 +126,38 @@ def plotter(fdict):
         wfo_limiter = " and ugc = :ugc "
         params["ugc"] = ctx["ugc"]
 
-    # NB we added a hack here that may lead to some false positives when events
-    # cross over months, sigh, recall the 2017 eventid pain
+    subtitle = (
+        f"{vtec.get_ps_string(phenomena, significance)} "
+        f"({phenomena}.{significance}) Issued by Year, Month"
+    )
+    if ctx["c"] == "svrtor":
+        params["ph"] = ("SV", "TO")
+        params["sig"] = "W"
+        subtitle = "Severe T'Storm + Tornado Warnings Issued by Year, Month"
+    elif ctx["c"] == "svrtorffw":
+        params["ph"] = ("SV", "TO", "FF")
+        params["sig"] = "W"
+        subtitle = (
+            "Svr T'Storm + Tornado + Flash Flood Warnings "
+            "Issued by Year, Month"
+        )
+
     with get_sqlalchemy_conn("postgis") as conn:
-        df = pd.read_sql(
+        # NB quasi hack here as we have some redundant ETNs for a given year
+        # so the groupby helps some.
+        daily = pd.read_sql(
             text(
                 f"""
-            with data as (
-                SELECT distinct
+                SELECT
                 extract(year from issue)::int as yr,
-                extract(month from issue)::int as mo, wfo, eventid
-                from warnings where phenomena = :ph and significance = :sig
+                extract(month from issue)::int as mo,
+                min(date(issue at time zone :tzname)) as min_date,
+                wfo,
+                phenomena, significance, eventid
+                from warnings where phenomena in :ph and significance = :sig
                 {wfo_limiter}
-                GROUP by yr, mo, wfo, eventid)
-
-            SELECT yr, mo, count(*) from data GROUP by yr, mo
-            ORDER by yr, mo ASC
+                GROUP by yr, mo, wfo, phenomena, significance, eventid
+                ORDER by yr asc, mo asc
         """
             ),
             conn,
@@ -133,13 +165,20 @@ def plotter(fdict):
             index_col=None,
         )
 
-    if df.empty:
+    if daily.empty:
         if opt == "ugc":
             raise NoDataFound(
                 "No events were found for this UGC + VTEC Phenomena\n"
                 "combination, try flipping between county/zone"
             )
         raise NoDataFound("Sorry, no data found!")
+    df = (
+        daily[["yr", "mo", "eventid"]]
+        .groupby(["yr", "mo"])
+        .count()
+        .reset_index()
+        .rename(columns={"eventid": "count"})
+    )
 
     df2 = df.pivot(index="yr", columns="mo", values="count").reindex(
         index=range(df["yr"].min(), df["yr"].max() + 1),
@@ -158,11 +197,15 @@ def plotter(fdict):
             f"NWS [{wfo}] {ctx['_nt'].sts[wfo]['name']} Issued for "
             f"[{ctx['ugc']}] {name}"
         )
-    subtitle = (
-        f"{vtec.get_ps_string(phenomena, significance)} "
-        f"({phenomena}.{significance}) Issued by Year, Month"
-    )
     (fig, ax) = figure_axes(title=title, subtitle=subtitle, apctx=ctx)
+    # Print the top 10 days
+    ypos = 0.9
+    fig.text(0.84, ypos, "Top 10 Dates")
+    gdf = daily.groupby("min_date").count().sort_values("yr", ascending=False)
+    for dt, row in gdf.head(10).iterrows():
+        ypos -= 0.04
+        fig.text(0.84, ypos, f"{dt} {row['yr']:3.0f}")
+    ax.set_position([0.1, 0.1, 0.75, 0.8])
     sns.heatmap(
         df2,
         annot=True,
@@ -198,4 +241,11 @@ def plotter(fdict):
 
 
 if __name__ == "__main__":
-    plotter(dict(wfo="OUN", network="WFO", phenomena="FG", significance="Y"))
+    plotter(
+        {
+            "wfo": "BOX",
+            "network": "WFO",
+            "phenomena": "SV",
+            "significance": "W",
+        }
+    )
