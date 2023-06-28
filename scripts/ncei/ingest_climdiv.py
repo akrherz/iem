33392cd76c09e9754669b5b1a9b9c3cd95214f16
akrherz <akrherz@iastate.edu>
2023-06-28 13:13:20 -0500

    add NCEI climdiv ingest (statewide for now)

---
+++ scripts/ncei/ingest_climdiv.py
@@ -0,0 +1,132 @@
+"""Ingest what NCEI has for its climdiv dataset.
+
+Run from RUN_0Z.sh
+"""
+
+import pandas as pd
+import requests
+from pyiem.reference import ncei_state_codes
+from pyiem.util import get_dbconn, get_properties, logger
+
+LOG = logger()
+ELEMENT2IEM = {
+    "pcpn": "precip",
+    "tmin": "low",
+    "tmax": "high",
+}
+
+
+def process(procdate):
+    """Process the climdiv data."""
+    ncei2state = {}
+    for key, val in ncei_state_codes.items():
+        ncei2state[f"{int(val):03.0f}"] = key
+    alldf = None
+    for element, iemvar in ELEMENT2IEM.items():
+        LOG.info("Fetching %s", element)
+        df = (
+            pd.read_csv(
+                "https://www.ncei.noaa.gov/pub/data/cirs/climdiv/"
+                f"climdiv-{element}st-v1.0.0-{procdate}",
+                names="stcode 1 2 3 4 5 6 7 8 9 10 11 12".split(),
+                dtype={"stcode": str},
+                sep="\s+",
+                na_values=["-9.99", "-99.90"],
+            )
+            .assign(
+                state=lambda df_: df_["stcode"].str.slice(0, 3),
+                divnum=lambda df_: df_["stcode"].str.slice(3, 4),
+                year=lambda df_: pd.to_numeric(df_["stcode"].str.slice(6, 10)),
+            )
+            .assign(
+                state=lambda df_: df_["state"].map(ncei2state),
+                station=lambda df_: (
+                    df_["state"] + df_["divnum"].str.pad(4, fillchar="0")
+                ),
+            )
+            .dropna(subset=["state"])
+            .drop(columns=["stcode", "state", "divnum"])
+            .melt(
+                id_vars=["station", "year"],
+                value_vars="1 2 3 4 5 6 7 8 9 10 11 12".split(),
+                var_name="month",
+                value_name=iemvar,
+            )
+            .assign(
+                day=1,
+                date=lambda df_: pd.to_datetime(df_[["year", "month", "day"]]),
+            )
+            .drop(columns=["year", "month", "day"])
+            .set_index(["station", "date"])
+        )
+        if alldf is None:
+            alldf = df
+        else:
+            alldf[iemvar] = df[iemvar]
+    return alldf.reset_index()
+
+
+def update(cursor, row) -> int:
+    """Do update."""
+    cursor.execute(
+        """
+        update ncei_climdiv SET precip = %s, high = %s, low = %s
+        WHERE station = %s and day = %s
+        """,
+        (
+            row["precip"],
+            row["high"],
+            row["low"],
+            row["station"],
+            row["date"],
+        ),
+    )
+    return cursor.rowcount
+
+
+def dbsave(df):
+    """Upsert data into database."""
+    conn = get_dbconn("coop")
+    cursor = conn.cursor()
+    for _, row in df.iterrows():
+        if update(cursor, row) != 1:
+            cursor.execute(
+                "INSERT into ncei_climdiv(station, day) VALUES (%s, %s)",
+                (row["station"], row["date"]),
+            )
+            update(cursor, row)
+
+    cursor.close()
+    conn.commit()
+
+
+def set_procdate(procdate):
+    """Update properties."""
+    conn = get_dbconn("mesosite")
+    cursor = conn.cursor()
+    cursor.execute(
+        "UPDATE properties SET propvalue = %s where propname = %s",
+        (procdate, "ncei.climdiv.procdate"),
+    )
+    cursor.close()
+    conn.commit()
+
+
+def main():
+    """Go Main Go."""
+    procdate = requests.get(
+        "https://www.ncei.noaa.gov/pub/data/cirs/climdiv/procdate.txt",
+        timeout=30,
+    ).text.strip()
+    iemprocdate = get_properties().get("ncei.climdiv.procdate")
+    if iemprocdate is not None and iemprocdate == procdate:
+        LOG.info("Nothing to do, procdate %s", procdate)
+        return
+    LOG.warning("Found new %s to process", procdate)
+    df = process(procdate)
+    dbsave(df)
+    set_procdate(procdate)
+
+
+if __name__ == "__main__":
+    main()
