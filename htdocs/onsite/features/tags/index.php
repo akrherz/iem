5f3c909445b2cfacb88cf881e62a303577aea7af
akrherz <akrherz@iastate.edu>
2023-10-29 11:21:21 -0500

    feat: ride with winter 23-24

--- htdocs/onsite/features/tags/index.php
+++ htdocs/onsite/features/tags/index.php
@@ -79,6 +79,7 @@ previous winter seasons that these maps are available for:
     <li><a href="winter2021.html">Winter 2020-2021</a></li>
     <li><a href="winter2122.html">Winter 2021-2022</a></li>
     <li><a href="winter2223.html">Winter 2022-2023</a></li>
+    <li><a href="winter2324.html">Winter 2023-2024</a></li>
 </ul></p>
 
 EOM;
