97ad51eac7b84a50c28e2b1a9eff6609ee86de08
akrherz <akrherz@iastate.edu>
2023-10-29 14:03:12 -0500

    feat: add COOP counts by WFO per day
    https://mesonet.agron.iastate.edu/nws/coop-cnts.php

--- htdocs/nws/coop-cnts.php
+++ htdocs/nws/coop-cnts.php
@@ -1,24 +1,34 @@
 <?php
-/*
- * Print out a listing of COOP sites and observation frequency
- */
+// Print out a listing of COOP sites and observation frequency
 require_once "../../config/settings.inc.php";
 define("IEM_APPID", 113);
 require_once "../../include/myview.php";
 require_once "../../include/database.inc.php";
-require_once "../../include/network.php";
 require_once "../../include/forms.php";
+require_once "../../include/imagemaps.php";
 $t = new MyView();
 
-$nt = new NetworkTable("WFO");
 $dbconn = iemdb("iem");
 
 $wfo = isset($_REQUEST['wfo']) ? xssafe($_REQUEST['wfo']) : 'DMX';
+$by = isset($_REQUEST['by']) ? xssafe($_REQUEST['by']) : 'station';
 $year = get_int404("year", date("Y"));
 $month = get_int404("month", date("m"));
 
-$rs = pg_prepare($dbconn, "MYSELECT", "select id, name,
- count(*) as total, 
+if ($by == "station") {
+    $rs = pg_prepare($dbconn, "MYSELECT", "select id, name,
+    count(*) as total,
+    sum(case when pday >= 0 then 1 else 0 end) as pobs, 
+    sum(case when snow >= 0 then 1 else 0 end) as sobs, 
+    sum(case when snowd >= 0 then 1 else 0 end) as sdobs, 
+    sum(case when max_tmpf > -60 then 1 else 0 end) as tobs 
+    from summary_$year s JOIN stations t on (t.iemid = s.iemid) 
+    WHERE day >= $1 and day < ($1::date + '1 month'::interval) 
+    and day < 'TOMORROW'::date
+    and t.wfo = $2 and t.network ~* 'COOP' GROUP by id, name ORDER by id ASC");
+} else {
+    $rs = pg_prepare($dbconn, "MYSELECT", "select day,
+ count(*) as total,
  sum(case when pday >= 0 then 1 else 0 end) as pobs, 
  sum(case when snow >= 0 then 1 else 0 end) as sobs, 
  sum(case when snowd >= 0 then 1 else 0 end) as sdobs, 
@@ -26,38 +36,52 @@ $rs = pg_prepare($dbconn, "MYSELECT", "select id, name,
  from summary_$year s JOIN stations t on (t.iemid = s.iemid) 
  WHERE day >= $1 and day < ($1::date + '1 month'::interval) 
  and day < 'TOMORROW'::date
- and t.wfo = $2 and t.network ~* 'COOP' GROUP by id, name ORDER by id ASC");
+ and t.wfo = $2 and t.network ~* 'COOP' GROUP by day ORDER by day ASC");
+}
 
+$bselect = make_select("by", $by, array("station" => "Station", "day" => "Day"));
 
 $tstring = sprintf("%s-%02d-01", $year, intval($month));
 $data = pg_execute($dbconn, "MYSELECT", array($tstring, $wfo));
 
 $t->title = "NWS COOP Obs per month per WFO";
 
-$wselect = "";
-foreach ($nt->table as $key => $value) {
-    $wselect .= "<option value=\"$key\" ";
-    if ($wfo == $key) $wselect .= "SELECTED";
-    $wselect .= ">[" . $key . "] " . $nt->table[$key]["name"] . "\n";
-}
+$wselect = networkSelect("WFO", $wfo, array(), "wfo");
 
 $ys = yearSelect("2010", $year);
 $ms = monthSelect($month);
 
 $table = "";
 for ($i = 0; $row = pg_fetch_assoc($data); $i++) {
-    $table .= sprintf(
-        "<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>",
-        $row["id"],
-        $row["name"],
-        $row["total"],
-        $row["pobs"],
-        $row["tobs"],
-        $row["sobs"],
-        $row["sdobs"]
-    );
+    if ($by == "station") {
+        $table .= sprintf(
+            "<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td>" .
+                "<td>%s</td><td>%s</td></tr>",
+            $row["id"],
+            $row["name"],
+            $row["total"],
+            $row["pobs"],
+            $row["tobs"],
+            $row["sobs"],
+            $row["sdobs"]
+        );
+    } else {
+        $table .= sprintf(
+            "<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td>" .
+                "<td>%s</td></tr>",
+            $row["day"],
+            $row["total"],
+            $row["pobs"],
+            $row["tobs"],
+            $row["sobs"],
+            $row["sdobs"]
+        );
+    }
 }
-
+$header = "<th>NWSLI</th><th>Name</th>";
+if ($by == "day") {
+    $header = "<th>Day</th>";
+}
 
 $t->content = <<<EOF
 <ol class="breadcrumb">
@@ -72,9 +96,8 @@ is "missing" data from sites.  Please <a href="/info/contacts.php">let us know</
 <form method="GET" name="changeme">
 <table class="table table-condensed">
 <tr>
-<td><strong>Select WFO:</strong><select name="wfo">
-{$wselect}
-</select></td>
+<td><strong>Select WFO:</strong> {$wselect} </td>
+<td><strong>Aggregate By:</strong> {$bselect} </td>
 <td><strong>Select Year:</strong>{$ys}</td>
 <td><strong>Select Month:</strong>{$ms}</td>
 </tr>
@@ -86,7 +109,7 @@ is "missing" data from sites.  Please <a href="/info/contacts.php">let us know</
 
 <table class="table table-striped table-condensed table-bordered">
 <thead class="sticky">
-<tr><th>NWSLI</th><th>Name</th><th>Possible</th>
+<tr>{$header}<th>Possible</th>
 <th>Precip Obs</th><th>Temperature Obs</th><th>Snowfall Obs</th><th>Snowdepth Obs</th></tr>
 </thead>
 <tbody>
