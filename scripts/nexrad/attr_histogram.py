901fbffd28d70d0b47899f58a2a94a7ce1664ac7
akrherz <akrherz@iastate.edu>
2023-11-07 12:02:41 -0600

    fix: xticks properly set

--- scripts/nexrad/attr_histogram.py
+++ scripts/nexrad/attr_histogram.py
@@ -73,9 +73,7 @@ def run(nexrad, name, network, cname):
     ax[1].set_ylabel("Movement Direction (from)")
     ax[1].set_yticks((0, 90, 180, 270, 360))
     ax[1].set_yticklabels(("N", "E", "S", "W", "N"))
-    ax[1].set_xticks(
-        (1, 32, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335, 365)
-    )
+    ax[1].set_xticks((1, 32, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335))
     ax[1].set_xticklabels(calendar.month_abbr[1:])
     ax[1].set_xlim(0, 365)
     ax[1].grid(True)
