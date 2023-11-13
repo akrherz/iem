1727a569042a582b7698aea33afa58c99941e737
akrherz <akrherz@iastate.edu>
2023-11-13 11:30:56 -0600

    sundry tinkering

---
+++ htdocs/sites/test.py
@@ -0,0 +1,12 @@
+"""placeholder for future work."""
+
+from pyiem.webutil import iemapp
+
+
+@iemapp()
+def application(environ, start_response):
+    """dev."""
+    # We'd want to enforce that this works, so we are getting called rightly
+    (_, _, network, station, _) = environ["SCRIPT_URL"].split("/")
+    start_response("200 OK", [("Content-Type", "text/html")])
+    return [f"Hello World {network} {station}".encode("ascii")]
