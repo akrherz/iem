91b0351ca9618fc52fcc89fb8e020433f1432050
akrherz <akrherz@iastate.edu>
2023-11-14 13:33:32 -0600

    fix: account for CI call

--- htdocs/sites/test.py
+++ htdocs/sites/test.py
@@ -7,6 +7,10 @@ from pyiem.webutil import iemapp
 def application(environ, start_response):
     """dev."""
     # We'd want to enforce that this works, so we are getting called rightly
-    (_, _, network, station, _) = environ["SCRIPT_URL"].split("/")
+    try:
+        (_, _, network, station, _) = environ["SCRIPT_URL"].split("/")
+    except Exception:
+        network = "IA_ASOS"
+        station = "DSM"
     start_response("200 OK", [("Content-Type", "text/html")])
     return [f"Hello World {network} {station}".encode("ascii")]
