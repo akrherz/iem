e513f97b264d943a117adec4eb466e4212532e2d
akrherz <akrherz@iastate.edu>
2024-02-14 22:19:04 -0600

    feat: enhance daily climo plotting
    https://mesonet.agron.iastate.edu/plotting/auto/?q=113
    closes #679

--- htdocs/plotting/auto/scripts100/p113.py
+++ htdocs/plotting/auto/scripts100/p113.py
@@ -3,13 +3,15 @@ Presents the simple daily climatology as
 computed by period of record data.
 """
 import datetime
-from calendar import month_abbr
 
 import pandas as pd
 import requests
+from matplotlib.dates import DateFormatter, DayLocator
+from pyiem.database import get_sqlalchemy_conn
 from pyiem.exceptions import NoDataFound
-from pyiem.plot import figure_axes
+from pyiem.plot import figure
 from pyiem.util import get_autoplot_context
+from sqlalchemy import text
 
 PDICT = {
     "maxmin": "Daily Maximum / Minimums",
@@ -35,8 +37,34 @@ def get_description():
             name="var",
             default="maxmin",
             options=PDICT,
-            label="Which variable?",
+            label="Which variable? (only Daily Max/Min is plottable attm)",
         ),
+        {
+            "type": "year",
+            "optional": True,
+            "name": "year1",
+            "default": datetime.date.today().year,
+            "label": "Show observed values for year:",
+        },
+        {
+            "type": "year",
+            "optional": True,
+            "name": "year2",
+            "default": datetime.date.today().year - 1,
+            "label": "Show observed values for year:",
+        },
+        {
+            "type": "sday",
+            "default": "0101",
+            "name": "sday",
+            "label": "Start Plot on Day of Year:",
+        },
+        {
+            "type": "sday",
+            "default": "1231",
+            "name": "eday",
+            "label": "End Plot on Day of Year:",
+        },
     ]
     return desc
 
@@ -134,19 +162,102 @@ def plotter(fdict):
                 res += f"{row['max_precip']:6.2f}"
         res += "\n"
 
-    title = f"[{station}] {ctx['_nt'].sts[station]['name']} Daily Climatology"
-    (fig, ax) = figure_axes(title=title, apctx=ctx)
-    x = df.index.values
-    ax.plot(range(len(x)), df["avg_high"].values, color="r", label="High")
-    ax.plot(range(len(x)), df["avg_low"].values, color="b", label="Low")
+    title = f"{ctx['_sname']}:: Daily High/Low Temperature Climatology"
+    subtitle = ""
+    if ctx.get("year1") is not None:
+        subtitle = f"Observed High/Low for {ctx['year1']}"
+        if ctx.get("year2") is not None:
+            subtitle += f" and {ctx['year2']}"
+    fig = figure(title=title, subtitle=subtitle, apctx=ctx)
+    ax = fig.add_axes([0.08, 0.1, 0.9, 0.8])
+    x = df.index.date
+    ax.fill_between(
+        x,
+        df["avg_high"].values,
+        df["max_high"].values,
+        color="#f2c2a7",
+        step="post",
+    )
+    ax.fill_between(
+        x,
+        df["avg_low"].values,
+        df["min_low"].values,
+        color="#bbbbe2",
+        step="post",
+    )
+    ax.fill_between(
+        x,
+        df["avg_low"].values,
+        df["avg_high"].values,
+        color="#b4dab3",
+        step="post",
+        label="Range of Ave High/Low",
+    )
+    ax.plot(
+        x,
+        df["max_high"].values,
+        color="#dc300b",
+        label="Record High",
+        lw=1,
+        drawstyle="steps-post",
+    )
+    ax.plot(
+        x,
+        df["min_low"].values,
+        color="#444e9f",
+        label="Record Low",
+        lw=1,
+        drawstyle="steps-post",
+    )
+
+    if ctx.get("year1") is not None:
+        years = [ctx["year1"]]
+        if ctx.get("year2") is not None:
+            years.append(ctx["year2"])
+        with get_sqlalchemy_conn("coop") as conn:
+            obs = pd.read_sql(
+                text(
+                    """select day, year, high, low from alldata
+                WHERE station = :station and year = ANY(:years)
+                     order by day ASC"""
+                ),
+                conn,
+                parse_dates="day",
+                params={"station": station, "years": years},
+            )
+        if not obs.empty:
+            obs["day"] = obs["day"].map(lambda x: x.replace(year=2000))
+            colors = ["#593700", "#49aaf0"]
+            for i, year in enumerate(years):
+                obs2 = obs[obs["year"] == year]
+                ax.bar(
+                    obs2["day"].values,
+                    obs2["high"] - obs2["low"],
+                    bottom=obs2["low"],
+                    color=colors[i],
+                    align="edge",
+                    label=f"Observed {year}",
+                    width=0.8,
+                    alpha=0.8 if len(years) > 1 else 1.0,
+                )
+
     ax.grid(True)
-    ax.legend()
-    ax.set_xticks((1, 32, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335))
-    ax.set_xticklabels(month_abbr[1:])
-    ax.set_xlim(0, 366)
+    ax.legend(ncol=5)
+    ax.set_ylabel(r"Temperature $^\circ$F")
+    ax.set_xlim(
+        ctx["sday"] - datetime.timedelta(days=1),
+        ctx["eday"] + datetime.timedelta(days=1),
+    )
+    days = 1
+    if ctx["eday"] - ctx["sday"] < datetime.timedelta(days=71):
+        days = [1, 8, 15, 22, 29]
+    elif ctx["eday"] - ctx["sday"] < datetime.timedelta(days=121):
+        days = [1, 15]
+    ax.xaxis.set_major_locator(DayLocator(bymonthday=days))
+    ax.xaxis.set_major_formatter(DateFormatter("%b %-d"))
 
     return fig, df, res
 
 
 if __name__ == "__main__":
-    plotter({"var": "range"})
+    plotter({})
