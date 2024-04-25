cf81b05aca3d3ab8db8d32072caf1dfcafab8afc
akrherz <akrherz@iastate.edu>
2024-04-25 10:25:42 -0500

    mnt: update per log review

--- htdocs/plotting/auto/scripts100/p159.py
+++ htdocs/plotting/auto/scripts100/p159.py
@@ -15,9 +15,10 @@ import datetime
 
 import pandas as pd
 from matplotlib.ticker import MaxNLocator
+from pyiem.database import get_sqlalchemy_conn
 from pyiem.exceptions import NoDataFound
 from pyiem.plot import figure
-from pyiem.util import get_autoplot_context, get_sqlalchemy_conn
+from pyiem.util import get_autoplot_context
 from sqlalchemy import text
 
 MDICT = {
