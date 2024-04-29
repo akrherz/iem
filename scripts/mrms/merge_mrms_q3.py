302d5f41d7c4f128fdd203cefc7b84800b15b85c
akrherz <akrherz@iastate.edu>
2024-04-29 13:30:33 -0500

    mnt: improve when MRMS merge runs

--- scripts/mrms/merge_mrms_q3.py
+++ scripts/mrms/merge_mrms_q3.py
@@ -100,11 +100,10 @@ def run(dt: datetime.date, for_dep: bool):
 
 @click.command()
 @click.option("--date", "dt", type=click.DateTime(), help="Date to process")
-def main(dt):
+@click.option("--for-dep", is_flag=True, help="For Daily Erosion Project")
+def main(dt, for_dep):
     """go main go"""
-    run(dt.date(), False)
-    if "0415" <= f"{dt:%m%d}" <= "0531" and dt.year > 2014:
-        run(dt.date(), True)
+    run(dt.date(), for_dep)
 
 
 if __name__ == "__main__":
