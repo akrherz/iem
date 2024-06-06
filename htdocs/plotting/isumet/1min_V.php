70819378b7284952cab053562e309b8e2904c7e6
akrherz <akrherz@iastate.edu>
2024-06-06 17:26:40 -0500

    mnt: address lint

--- htdocs/plotting/isumet/1min_V.php
+++ htdocs/plotting/isumet/1min_V.php
@@ -10,37 +10,35 @@ require_once "../../../include/jpgraph/jpgraph_date.php";
 $year = get_int404("year", date("Y"));
 $month = get_int404("month", date("m"));
 $day = get_int404("day", date("d"));
-$station = isset($_REQUEST['station']) ? $_REQUEST['station']: null;
+$station = isset($_REQUEST['station']) ? $_REQUEST['station'] : null;
 
-if (strlen($year) == 4 && strlen($month) > 0 && strlen($day) > 0 ){
-  $myTime = strtotime($year."-".$month."-".$day);
+if (strlen($year) == 4 && strlen($month) > 0 && strlen($day) > 0) {
+    $myTime = strtotime($year . "-" . $month . "-" . $day);
 } else {
-  $myTime = strtotime( date("Y-m-d") );
+    $myTime = strtotime(date("Y-m-d"));
 }
 
-$wA = mktime(0,0,0, 8, 4, 2002);
+$wA = mktime(0, 0, 0, 8, 4, 2002);
 $wLabel = "1min Instantaneous Wind Speed";
-if ($wA > $myTime){
- $wLabel = "Instant Wind Speed";
+if ($wA > $myTime) {
+    $wLabel = "Instant Wind Speed";
 }
 
 $titleDate = date("M d, Y", $myTime);
 $dirRef = date("Y/m/d", $myTime);
-
 
 $mph = array();
 $drct = array();
 $gust = array();
 $valid = array();
 
-if ($station == null){
+if ($station == null) {
     $fcontents = file("/mesonet/ARCHIVE/data/$dirRef/text/ot/ot0002.dat");
-    foreach($fcontents as $line_num => $line){
-          $valid[] = strtotime( substr($line, 0, 26) );
-          $parts = preg_split ("/\s+/", $line);
+    foreach ($fcontents as $line_num => $line) {
+        $valid[] = strtotime(substr($line, 0, 26));
+        $parts = preg_split("/\s+/", $line);
         $mph[] = intval($parts[8]);
         $drct[] = intval($parts[9]);
-        
     } // End of while
 } else {
     $fcontents = file("/mesonet/ARCHIVE/data/$dirRef/text/ot/ot0010.dat");
@@ -50,69 +48,69 @@ if ($station == null){
          of gust, pressure, daily_precip, monthly_rain, yearly_rain, inside
          temp, inside humidity, solar radiation, UV index
      */
-    foreach($fcontents as $line_num => $line){
+    foreach ($fcontents as $line_num => $line) {
         $tokens = explode(' ', $line);
-        if (sizeof($tokens) != 21){
+        if (sizeof($tokens) != 21) {
             continue;
         }
-                    $tstring = sprintf("%s %s %s %s", $tokens[0], $tokens[1], $tokens[2], $tokens[3]);
-          $v = strtotime($tstring);
-          
-        if ($v < $myTime || trim($tstring) == ""){
+        $tstring = sprintf("%s %s %s %s", $tokens[0], $tokens[1], $tokens[2], $tokens[3]);
+        $v = strtotime($tstring);
+
+        if ($v < $myTime || trim($tstring) == "") {
             continue;
         }
         $speed = floatval($tokens[9]);
-        if ($speed < 0 || $speed > 100){
+        if ($speed < 0 || $speed > 100) {
             continue;
         }
         $valid[] = $v;
         $mph[] = $speed;
         $drct[] = $tokens[10];
-     } // End of while
-    
+    } // End of while
+
 }
 
 
 // Create the graph. These two calls are always required
-$graph = new Graph(600,400,"example1");
-$graph->SetScale("datelin",0, 360);
+$graph = new Graph(600, 400, "example1");
+$graph->SetScale("datelin", 0, 360);
 $graph->SetY2Scale("lin");
-$graph->img->SetMargin(55,40,55,60);
+$graph->img->SetMargin(55, 40, 55, 60);
 
 //$graph->yaxis->scale->ticks->SetPrecision(1);
 $graph->title->Set(" Time Series");
-$graph->subtitle->Set($titleDate );
+$graph->subtitle->Set($titleDate);
 
 $graph->legend->SetLayout(LEGEND_HOR);
-$graph->legend->Pos(0.01,0.08);
+$graph->legend->Pos(0.01, 0.08);
 $graph->xaxis->SetLabelAngle(90);
-$graph->yaxis->scale->ticks->Set(90,15);
+$graph->yaxis->scale->ticks->Set(90, 15);
 //$graph->yaxis->scale->ticks->SetPrecision(0);
 //$graph->yaxis->scale->ticks->SetPrecision(0);
 
 $graph->yaxis->SetColor("blue");
 $graph->y2axis->SetColor("red");
 
-$graph->title->SetFont(FF_FONT1,FS_BOLD,14);
+$graph->title->SetFont(FF_FONT1, FS_BOLD, 14);
 
 $graph->yaxis->SetTitle("Wind Direction");
 $graph->y2axis->SetTitle("Wind Speed [MPH]");
 
-$graph->yaxis->title->SetFont(FF_FONT1,FS_BOLD,12);
+$graph->yaxis->title->SetFont(FF_FONT1, FS_BOLD, 12);
 $graph->xaxis->SetTitle("Valid Local Time");
 $graph->xaxis->SetTitleMargin(30);
 $graph->yaxis->SetTitleMargin(30);
 //$graph->y2axis->SetTitleMargin(28);
-$graph->xaxis->title->SetFont(FF_FONT1,FS_BOLD,12);
+$graph->xaxis->title->SetFont(FF_FONT1, FS_BOLD, 12);
 $graph->xaxis->SetPos("min");
 
 // Create the linear plot
-$lineplot=new LinePlot($mph, $valid);
+$lineplot = new LinePlot($mph, $valid);
 $lineplot->SetLegend($wLabel);
 $lineplot->SetColor("red");
 
 // Create the linear plot
-$sp1=new ScatterPlot($drct, $valid);
+$sp1 = new ScatterPlot($drct, $valid);
 $sp1->mark->SetType(MARK_FILLEDCIRCLE);
 $sp1->mark->SetFillColor("blue");
 $sp1->mark->SetWidth(3);
@@ -122,4 +120,3 @@ $graph->Add($sp1);
 $graph->AddY2($lineplot);
 
 $graph->Stroke();
-?>
--- htdocs/plotting/isumet/1min_inside.php
+++ htdocs/plotting/isumet/1min_inside.php
@@ -1,18 +1,19 @@
 <?php
 include("../../../config/settings.inc.php");
 include_once "../../../include/mlib.php";
+include_once "../../../include/form.php";
 //  1 minute data plotter 
 
-$year = isset($_GET["year"]) ? $_GET["year"] : date("Y");
-$month = isset($_GET["month"]) ? $_GET["month"] : date("m");
-$day = isset($_GET["day"]) ? $_GET["day"] : date("d");
-$station = isset($_REQUEST['station']) ? $_REQUEST['station']: null;
+$year = get_int404("year", date("Y"));
+$month = get_int404("month", date("m"));
+$day = get_int404("day", date("d"));
+$station = isset($_REQUEST['station']) ? $_REQUEST['station'] : null;
 
 
-if (strlen($year) == 4 && strlen($month) > 0 && strlen($day) > 0 ){
-  $myTime = strtotime($year."-".$month."-".$day);
+if (strlen($year) == 4 && strlen($month) > 0 && strlen($day) > 0) {
+    $myTime = strtotime($year . "-" . $month . "-" . $day);
 } else {
-  $myTime = strtotime(date("Y-m-d"));
+    $myTime = strtotime(date("Y-m-d"));
 }
 
 
@@ -25,13 +26,12 @@ $tmpf = array();
 $dwpf = array();
 $relh = array();
 
-if ($station == null){
+if ($station == null) {
     $fcontents = file("/mesonet/ARCHIVE/data/$dirRef/text/ot/ot0002.dat");
-    foreach($fcontents as $line_num => $line){
-        $parts = preg_split ("/\s+/", $line);
-          $valid[] = strtotime( substr($line, 0, 26) );
-          $tmpf[] = round (substr($line, 31, 5),2);
-        
+    foreach ($fcontents as $line_num => $line) {
+        $parts = preg_split("/\s+/", $line);
+        $valid[] = strtotime(substr($line, 0, 26));
+        $tmpf[] = round(substr($line, 31, 5), 2);
     } // End of while
 } else {
     $fcontents = file("/mesonet/ARCHIVE/data/$dirRef/text/ot/ot0010.dat");
@@ -41,66 +41,66 @@ if ($station == null){
          of gust, pressure, daily_precip, monthly_rain, yearly_rain, inside
          temp, inside humidity, solar radiation, UV index
      */
-    foreach($fcontents as $line_num => $line){
+    foreach ($fcontents as $line_num => $line) {
         $tokens = explode(' ', $line);
-        if (sizeof($tokens) != 21){
+        if (sizeof($tokens) != 21) {
             continue;
         }
-           $tstring = sprintf("%s %s %s %s", $tokens[0], $tokens[1], $tokens[2], $tokens[3]);
-          $v = strtotime($tstring);
-          
-        if ($v < $myTime || trim($tstring) == ""){
+        $tstring = sprintf("%s %s %s %s", $tokens[0], $tokens[1], $tokens[2], $tokens[3]);
+        $v = strtotime($tstring);
+
+        if ($v < $myTime || trim($tstring) == "") {
             continue;
         }
         $valid[] = $v;
-          $tmpf[] = $tokens[17];
-          $relh[] = $tokens[18];
-          $dwpf[] = dwpf($tokens[17], $tokens[18]);
-     } // End of while
-    
+        $tmpf[] = $tokens[17];
+        $relh[] = $tokens[18];
+        $dwpf[] = dwpf($tokens[17], $tokens[18]);
+    } // End of while
+
 }
 
 
-include ("../../../include/jpgraph/jpgraph.php");
-include ("../../../include/jpgraph/jpgraph_line.php");
-include ("../../../include/jpgraph/jpgraph_date.php");
+include("../../../include/jpgraph/jpgraph.php");
+include("../../../include/jpgraph/jpgraph_line.php");
+include("../../../include/jpgraph/jpgraph_date.php");
 
 // Create the graph. These two calls are always required
-$graph = new Graph(600,400,"example1");
+$graph = new Graph(600, 400, "example1");
 $graph->SetScale("datelin");
 //$graph->SetY2Scale("lin",0,100);
-$graph->img->SetMargin(65,40,55,70);
+$graph->img->SetMargin(65, 40, 55, 70);
 
 $graph->xaxis->SetLabelAngle(90);
 //$graph->yaxis->scale->ticks->SetPrecision(1);
-$graph->yaxis->scale->ticks->Set(1,0.5);
+$graph->yaxis->scale->ticks->Set(1, 0.5);
 //$graph->yscale->SetGrace(10);
 $graph->title->Set("$titleDate Map Room Temperature & Dew Point");
 
 $graph->legend->SetLayout(LEGEND_HOR);
-$graph->legend->Pos(0.2,0.09);
+$graph->legend->Pos(0.2, 0.09);
 
-$graph->title->SetFont(FF_FONT1,FS_BOLD,14);
+$graph->title->SetFont(FF_FONT1, FS_BOLD, 14);
 $graph->yaxis->SetTitle("Temperature [F]");
 $graph->xaxis->SetLabelFormatString("h:i A", true);
 //$graph->y2axis->SetTitle("Relative Humidity [%]");
 //$graph->y2axis->title->SetFont(FF_FONT1,FS_BOLD,12);
-$graph->yaxis->title->SetFont(FF_FONT1,FS_BOLD,12);
+$graph->yaxis->title->SetFont(FF_FONT1, FS_BOLD, 12);
 $graph->xaxis->SetTitle("Valid Local Time");
 //$graph->yaxis->SetTitleMargin(48);
 $graph->yaxis->SetTitleMargin(40);
-$graph->xaxis->title->SetFont(FF_FONT1,FS_BOLD,12);
+$graph->xaxis->title->SetFont(FF_FONT1, FS_BOLD, 12);
 $graph->xaxis->SetPos("min");
 $graph->xaxis->SetTitleMargin(40);
 
 // Create the linear plot
-$lineplot=new LinePlot($tmpf, $valid);
+$lineplot = new LinePlot($tmpf, $valid);
 $lineplot->SetLegend("Temperature");
 $lineplot->SetColor("red");
 $lineplot->SetWeight(3.0);
 
 // Create the linear plot
-$lineplot2=new LinePlot($dwpf, $valid);
+$lineplot2 = new LinePlot($dwpf, $valid);
 $lineplot2->SetLegend("Dew Point");
 $lineplot2->SetColor("blue");
 $lineplot2->SetWeight(3.0);
