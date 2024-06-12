59e0c6f627dad5c52db8a076fbce209a6de93d86
akrherz <akrherz@iastate.edu>
2024-06-12 13:24:04 -0500

    mnt: migrate some json services to iemapp

--- htdocs/geojson/winter_roads.py
+++ htdocs/geojson/winter_roads.py
@@ -1,68 +1,66 @@
 """Generate a GeoJSON of current storm based warnings"""
 
-import datetime
 import json
 
+from pydantic import Field
+from pyiem.database import get_sqlalchemy_conn
 from pyiem.reference import ISO8601
-from pyiem.util import get_dbconn, html_escape
-from pyiem.webutil import iemapp
-from pymemcache.client import Client
+from pyiem.util import utc
+from pyiem.webutil import CGIModel, iemapp
+from sqlalchemy import text
+
+
+class Schema(CGIModel):
+    """See how we are called."""
+
+    callback: str = Field(None, title="JSONP Callback")
 
 
 def run():
     """Actually do the hard work of getting the current SBW in geojson"""
-    utcnow = datetime.datetime.utcnow()
-    pgconn = get_dbconn("postgis")
-    cursor = pgconn.cursor()
 
     # Look for polygons into the future as well as we now have Flood products
     # with a start time in the future
-    cursor.execute(
-        """
-        SELECT ST_asGeoJson(ST_Transform(simple_geom, 4326)) as geojson,
-        cond_code, c.segid from
-        roads_current c JOIN roads_base b on (c.segid = b.segid)
-        WHERE c.valid > now() - '1000 hours'::interval
-        and cond_code is not null
-    """
-    )
-
-    res = {
-        "type": "FeatureCollection",
-        "features": [],
-        "generation_time": utcnow.strftime(ISO8601),
-        "count": cursor.rowcount,
-    }
-    for row in cursor:
-        res["features"].append(
-            dict(
-                type="Feature",
-                id=row[2],
-                properties=dict(code=row[1]),
-                geometry=json.loads(row[0]),
-            )
+    with get_sqlalchemy_conn("postgis") as conn:
+        res = conn.execute(
+            text("""
+            SELECT ST_asGeoJson(ST_Transform(simple_geom, 4326)) as geojson,
+            cond_code, c.segid from
+            roads_current c JOIN roads_base b on (c.segid = b.segid)
+            WHERE c.valid > now() - '1000 hours'::interval
+            and cond_code is not null
+        """)
         )
 
-    return json.dumps(res)
+        data = {
+            "type": "FeatureCollection",
+            "features": [],
+            "generated_at": utc().strftime(ISO8601),
+            "count": res.rowcount,
+        }
+        for row in res:
+            data["features"].append(
+                dict(
+                    type="Feature",
+                    id=row[2],
+                    properties=dict(code=row[1]),
+                    geometry=json.loads(row[0]),
+                )
+            )
+
+    return json.dumps(data)
 
 
-@iemapp()
+@iemapp(
+    help=__doc__,
+    schema=Schema,
+    memcachekey="/geojson/winter_roads.geojson",
+    memcacheexpire=120,
+    content_type="application/vnd.geo+json",
+)
 def application(environ, start_response):
     """Main Workflow"""
     headers = [("Content-type", "application/vnd.geo+json")]
-    cb = environ.get("callback", None)
-
-    mckey = "/geojson/winter_roads.geojson"
-    mc = Client("iem-memcached:11211")
-    res = mc.get(mckey)
-    if not res:
-        res = run()
-        mc.set(mckey, res, 120)
-    else:
-        res = res.decode("utf-8")
-    mc.close()
-    if cb is not None:
-        res = f"{html_escape(cb)}({res})"
-
+    res = run()
     start_response("200 OK", headers)
-    return [res.encode("ascii")]
+    return res
