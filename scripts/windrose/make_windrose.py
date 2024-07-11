a6fd0db8adf522c8a995368a9b5cf389cbbc468f
akrherz <akrherz@iastate.edu>
2024-07-11 16:47:41 -0500

    fix: RWIS locating bug with windrose generation

--- scripts/windrose/make_windrose.py
+++ scripts/windrose/make_windrose.py
@@ -3,9 +3,9 @@ Drive a windrose for a given network and site
 """
 
 import os
-import sys
 from calendar import month_abbr
 
+import click
 from pyiem.network import Table as NetworkTable
 from pyiem.plot.use_agg import plt
 from pyiem.windrose_utils import windrose
@@ -13,44 +13,45 @@ from pyiem.windrose_utils import windrose
 CACHE_DIR = "/mesonet/share/windrose"
 
 
-def main():
+@click.command()
+@click.option("--network", required=True, help="Network Identifier")
+@click.option("--station", required=True, help="Station Identifier")
+def main(network, station):
     """Go Main"""
-    net = sys.argv[1]
-    nt = NetworkTable(net)
-    sid = sys.argv[2]
+    nt = NetworkTable(network, only_online=False)
 
     database = "asos"
-    if net in ("KCCI", "KELO", "KIMT"):
+    if network in ("KCCI", "KELO", "KIMT"):
         database = "snet"
-    elif net in ("IA_RWIS",):
+    elif network.find("_RWIS") > 0:
         database = "rwis"
-    elif net in ("ISUSM",):
+    elif network in ("ISUSM",):
         database = "isuag"
-    elif net.find("_DCP") > 0:
+    elif network.find("_DCP") > 0:
         database = "hads"
 
-    mydir = os.path.join(CACHE_DIR, net, sid)
+    mydir = os.path.join(CACHE_DIR, network, station)
     if not os.path.isdir(mydir):
         os.makedirs(mydir)
-    fn = f"{mydir}/{sid}_yearly.png"
+    fn = f"{mydir}/{station}_yearly.png"
     res = windrose(
-        sid,
+        station,
         database=database,
-        sname=nt.sts[sid]["name"],
-        tzname=nt.sts[sid]["tzname"],
+        sname=nt.sts[station]["name"],
+        tzname=nt.sts[station]["tzname"],
     )
     res.savefig(fn)
     plt.close()
     for month in range(1, 13):
-        fn = f"{mydir}/{sid}_{month_abbr[month].lower()}.png"
+        fn = f"{mydir}/{station}_{month_abbr[month].lower()}.png"
         res = windrose(
-            sid,
+            station,
             months=[
                 month,
             ],
             database=database,
-            sname=nt.sts[sid]["name"],
-            tzname=nt.sts[sid]["tzname"],
+            sname=nt.sts[station]["name"],
+            tzname=nt.sts[station]["tzname"],
         )
         res.savefig(fn)
         plt.close()
