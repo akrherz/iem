b0e0ce83a318675934d2f932927b42d421497f2a
akrherz <akrherz@iastate.edu>
2024-07-18 17:04:07 -0500

    mnt: use pydantic and improve coverage

--- pylib/iemweb/json/vtec_events_byugc.py
+++ pylib/iemweb/json/vtec_events_byugc.py
@@ -1,16 +1,57 @@
-"""Find VTEC events by a given UGC code."""
+""".. title:: VTEC Events by UGC
+
+Return to `JSON Services</json/>`_.
+
+Documentation for /json/vtec_events_byugc.json
+----------------------------------------------
+
+This metadata service returns VTEC events for the given UGC.
+
+Changelog
+---------
+
+- 2024-07-18: Initial documentation release and migration to pydantic.
+
+"""
 
 import datetime
 import json
 from io import BytesIO, StringIO
 
 import pandas as pd
+from pydantic import Field
 from pyiem.exceptions import IncompleteWebRequest
 from pyiem.nws.vtec import VTEC_PHENOMENA, VTEC_SIGNIFICANCE, get_ps_string
-from pyiem.util import get_sqlalchemy_conn, html_escape
-from pyiem.webutil import iemapp
+from pyiem.util import get_sqlalchemy_conn, utc
+from pyiem.webutil import CGIModel, iemapp
 
 EXL = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+
+
+class Schema(CGIModel):
+    """See how we are called."""
+
+    callback: str = Field(default=None, description="Optional JSON-P Callback")
+    fmt: str = Field(
+        default="json",
+        description="Response format, either json, csv, or excel",
+        pattern="^(csv|json|excel|xlsx)$",
+    )
+    edate: str = Field(
+        default=f"{utc().year}-12-31",
+        description="End Date (@00Z) to end query for issuance",
+    )
+    sdate: str = Field(
+        default="1986-01-01",
+        description="Start Date (@00Z) to start query for issuance",
+    )
+    ugc: str = Field(
+        default="IAC001",
+        description="6-character NWS Universal Geographic Code",
+        pattern=r"^[A-Z][A-Z][CZ]\d\d\d$",
+        max_length=6,
+        min_length=6,
+    )
 
 
 def make_url(row):
@@ -35,7 +76,8 @@ def get_df(ugc, sdate, edate):
                 'YYYY-MM-DDThh24:MI:SSZ') as iso_expired,
             to_char(expire at time zone 'UTC',
                 'YYYY-MM-DD hh24:MI') as expired,
-            eventid, phenomena, significance, hvtec_nwsli, wfo, ugc
+            eventid, phenomena, significance, hvtec_nwsli, wfo, ugc,
+            product_ids[1] as product_id
             from warnings WHERE ugc = %s and issue > %s
             and issue < %s ORDER by issue ASC
             """,
@@ -76,6 +118,7 @@ def as_json(df):
                 "ph_name": row["ph_name"],
                 "sig_name": row["sig_name"],
                 "ugc": row["ugc"],
+                "product_id": row["product_id"],
             }
         )
 
@@ -88,20 +131,29 @@ def parse_date(val):
     return datetime.datetime.strptime(val, fmt)
 
 
-@iemapp()
+def get_mckey(environ):
+    """Compute the key."""
+    if environ["fmt"] != "json":
+        return None
+    return (
+        f"/json/vtec_events_byugc.json|{environ['ugc']}|"
+        f"{environ['sdate']}|{environ['edate']}"
+    )
+
+
+@iemapp(help=__doc__, schema=Schema, memcachekey=get_mckey, memcacheexpire=600)
 def application(environ, start_response):
     """Answer request."""
-    ugc = environ.get("ugc", "IAC001")[:6]
+    ugc = environ["ugc"]
     try:
-        sdate = parse_date(environ.get("sdate", "1986-01-01"))
-        edate = parse_date(environ.get("edate", "2099-01-01"))
+        sdate = parse_date(environ["sdate"])
+        edate = parse_date(environ["edate"])
     except Exception as exp:
         raise IncompleteWebRequest(str(exp)) from exp
-    cb = environ.get("callback", None)
-    fmt = environ.get("fmt", "json")
+    fmt = environ["fmt"]
 
     df = get_df(ugc, sdate, edate)
-    if fmt == "xlsx":
+    if fmt in ["xlsx", "excel"]:
         fn = f"vtec_{ugc}_{sdate:%Y%m%d}_{edate:%Y%m%d}.xlsx"
         headers = [
             ("Content-type", EXL),
@@ -123,9 +175,7 @@ def application(environ, start_response):
         return [bio.getvalue().encode("utf-8")]
 
     res = as_json(df)
-    if cb is not None:
-        res = f"{html_escape(cb)}({res})"
 
     headers = [("Content-type", "application/json")]
     start_response("200 OK", headers)
-    return [res.encode("ascii")]
+    return res.encode("ascii")
