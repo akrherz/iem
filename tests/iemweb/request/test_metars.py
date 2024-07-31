18c5301952690c3c7fb81cfc9ace43618672b140
akrherz <akrherz@iastate.edu>
2024-07-31 09:27:36 -0500

    fix: logged capacity error msg

---
+++ tests/iemweb/request/test_metars.py
@@ -0,0 +1,12 @@
+"""Test iemweb.request.metars module."""
+
+from iemweb.request import metars
+from werkzeug.test import Client
+
+
+def test_load():
+    """Test the requests."""
+    c = Client(metars.application)
+    metars.SIMULTANEOUS_REQUESTS = -1
+    resp = c.get("/request/metars.py?valid=2020070100")
+    assert resp.status_code == 503
