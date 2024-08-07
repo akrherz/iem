f71910883d29c009f385ab9c3db016c4c62f02cb
akrherz <akrherz@iastate.edu>
2024-08-06 21:17:59 -0500

    fix: improve histogram logic for x-hour changes
    https://mesonet.agron.iastate.edu/plotting/auto/?q=35

--- pylib/iemweb/autoplot/scripts/p35.py
+++ pylib/iemweb/autoplot/scripts/p35.py
@@ -1,16 +1,22 @@
 """
 This plot presents a histogram of the change
-in some observed variable over a given number of hours.
+in some observed variable over a given number of hours.  The histogram is
+constructed by computing the 99.9th percentile of the absolute value of the
+change in the variable over the given number of hours.  This max value is
+then used to create a histogram with bins of a given width.  The histogram
+is then normalized by the number of years of data available.
 """
 
 import calendar
 import datetime
 
 import numpy as np
-from pyiem.database import get_dbconn
+import pandas as pd
+from pyiem.database import get_sqlalchemy_conn
 from pyiem.exceptions import NoDataFound
 from pyiem.plot import figure_axes
 from pyiem.util import get_autoplot_context
+from sqlalchemy import text
 
 PDICT = {
     "tmpf": "Air Temp (F)",
@@ -22,18 +28,9 @@ PDICT = {
 }
 
 
-def compute_bins(interval):
-    """Make even intervals centered around zero"""
-    halfsz = interval / 2.0
-    multi = int(80.0 / interval)
-    v = halfsz + interval * multi
-    # print halfsz, multi, v
-    return np.arange(0 - v, v + 0.1, interval)
-
-
 def get_description():
     """Return a dict describing how to call this plotter"""
-    desc = {"description": __doc__, "cache": 86400}
+    desc = {"description": __doc__, "cache": 86400, "data": True}
     desc["arguments"] = [
         dict(
             type="zstation",
@@ -62,8 +59,6 @@ def get_description():
 
 def plotter(fdict):
     """Go"""
-    pgconn = get_dbconn("asos")
-    cursor = pgconn.cursor()
     ctx = get_autoplot_context(fdict, get_description())
     station = ctx["zstation"]
     hours = ctx["hours"]
@@ -74,27 +69,29 @@ def plotter(fdict):
             "Invalid interval provided, positive number less than 10"
         )
 
-    cursor.execute(
-        f"""
-    WITH one as (
-        select valid, {varname} as t from alldata where
-        station = %s and {varname} is not null
-        ),
-        two as (SELECT valid + '%s hours'::interval as v, t from one
-        )
+    with get_sqlalchemy_conn("asos") as conn:
+        obs = pd.read_sql(
+            text(f"""
+        WITH one as (
+            select valid, {varname} as t from alldata where
+            station = :station and {varname} is not null and report_type = 3
+            ),
+            two as (SELECT valid + :hours as v, t from one
+            )
 
-    SELECT extract(week from one.valid), two.t - one.t
-    from one JOIN two on (one.valid = two.v)
-    """,
-        (station, hours),
-    )
-    if cursor.rowcount == 0:
+        SELECT extract(week from one.valid) as week, two.t - one.t as delta,
+        valid
+        from one JOIN two on (one.valid = two.v)
+        """),
+            conn,
+            params={
+                "station": station,
+                "hours": datetime.timedelta(hours=hours),
+            },
+            parse_dates="valid",
+        )
+    if obs.empty:
         raise NoDataFound("No non-null data found")
-    weeks = []
-    deltas = []
-    for row in cursor:
-        weeks.append(row[0])
-        deltas.append(float(row[1]))
 
     sts = datetime.datetime(2012, 1, 1)
     xticks = []
@@ -103,21 +100,36 @@ def plotter(fdict):
         xticks.append(int(ts.strftime("%j")))
 
     # We want bins centered on zero
-    bins = compute_bins(interval)
+    p99 = obs["delta"].abs().quantile(0.999)
+    bins = np.arange(0 - p99, p99, interval)
 
-    hist, xedges, yedges = np.histogram2d(weeks, deltas, [range(54), bins])
-    ab = ctx["_nt"].sts[station]["archive_begin"]
-    if ab is None:
-        raise NoDataFound("Unknown station metadata.")
-    years = float(datetime.datetime.now().year - ab.year)
-    hist = np.ma.array(hist / years / 7.0)
-    hist.mask = np.where(hist < (1.0 / years), True, False)
+    hist, xedges, yedges = np.histogram2d(
+        obs["week"].values, obs["delta"].values, [range(54), bins]
+    )
+    # create a dataframe from this 2d histogram
+    x, y = np.meshgrid(xedges[:-1], yedges[:-1])
+    resultdf = pd.DataFrame(
+        {
+            "week": np.ravel(x),
+            "delta": np.ravel(y),
+            "count": np.ravel(hist),
+        }
+    )
+    years = obs["valid"].dt.year.nunique()
+    hist = np.ma.array(hist / years)
+    hist.mask = np.where(hist < (1.0 / years / 7.0 / 24.0), True, False)
 
-    title = f"{ctx['_sname']} :: Histogram"
-    subtitle = f"(bin={interval}) of {hours} Hour {PDICT[varname]} Change"
+    title = (
+        f"{ctx['_sname']} ({obs['valid'].min():%Y}-"
+        f"{obs['valid'].max():%Y}):: Histogram"
+    )
+    subtitle = (
+        f"(bin={interval}) of {hours} Hour {PDICT[varname]} Change, "
+        f"99.9th percentile: {p99:.1f}, n={len(obs.index)}, years={years}"
+    )
     (fig, ax) = figure_axes(title=title, subtitle=subtitle, apctx=ctx)
     res = ax.pcolormesh((xedges - 1) * 7, yedges, hist.transpose())
-    fig.colorbar(res, label="Hours per Day")
+    fig.colorbar(res, label="Hours per Week")
     ax.grid(True)
     ax.set_ylabel(f"{PDICT[varname]} Change")
 
@@ -125,7 +137,6 @@ def plotter(fdict):
     ax.set_xticklabels(calendar.month_abbr[1:])
     ax.set_xlim(0, 366)
 
-    rng = max([max(deltas), 0 - min(deltas)])
-    ax.set_ylim(0 - rng * 1.3, rng * 1.3)
+    ax.set_ylim(0 - p99, p99)
 
-    return fig
+    return fig, resultdf
