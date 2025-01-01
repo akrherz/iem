3ecaad67bebfc8885c0dfc00eca3dd542cc9c742
akrherz <akrherz@iastate.edu>
2025-01-01 10:59:41 -0600

    📝 Cleanup MRMS merge

--- scripts/mrms/merge_mrms_q3.py
+++ scripts/mrms/merge_mrms_q3.py
@@ -3,16 +3,18 @@
 called from RUN_10_AFTER.sh
 """
 
-import datetime
 import gzip
 import os
 import tempfile
+from datetime import date, datetime, timedelta
 from zoneinfo import ZoneInfo
 
 import click
+import netCDF4
 import numpy as np
 import pygrib
-from pyiem import iemre, mrms
+from pyiem.iemre import daily_offset, get_daily_mrms_ncname
+from pyiem.mrms import fetch as mrms_fetch
 from pyiem.util import logger, ncopen, utc
 
 LOG = logger()
@@ -22,32 +24,27 @@ TMP = "/mesonet/tmp"
 PRODFLIP = utc(2020, 10, 13, 19)
 
 
-def run(dt: datetime.date, for_dep: bool):
+def run(nc: netCDF4.Dataset, dt: date, for_dep: bool):
     """Update netcdf file with the MRMS data
 
     Args:
-      dt (datetime.date): date to process
+      dt (date): date to process
       for_dep (bool): for Daily Erosion Project
     """
-    ncfn = iemre.get_daily_mrms_ncname(dt.year)
+    offset = daily_offset(dt)
     if for_dep:
-        ncfn = ncfn.replace("daily", "dep")
-    nc = ncopen(ncfn, "a", timeout=300)
-    offset = iemre.daily_offset(dt)
-    if for_dep:
-        offset = offset - iemre.daily_offset(datetime.date(dt.year, 4, 11))
+        offset = offset - daily_offset(date(dt.year, 4, 11))
     ncprecip = nc.variables["p01d"]
 
-    midnight = datetime.datetime(
+    midnight = datetime(
         dt.year, dt.month, dt.day, tzinfo=ZoneInfo("America/Chicago")
     )
     utcnow = utc()
     total = None
-    lats = None
     UTC = ZoneInfo("UTC")
     # Stop at 6 PM if we are using DEP
     for hr in range(1, 18 if for_dep else 25):
-        utcdt = (midnight + datetime.timedelta(hours=hr)).astimezone(UTC)
+        utcdt = (midnight + timedelta(hours=hr)).astimezone(UTC)
         if utcdt > utcnow:
             continue
         gribfn = None
@@ -55,7 +52,7 @@ def run(dt: datetime.date, for_dep: bool):
         if utcdt < PRODFLIP:
             qcprod = "GaugeCorr_QPE_01H"
         for prefix in [qcprod, "RadarOnly_QPE_01H"]:
-            fn = mrms.fetch(prefix, utcdt)
+            fn = mrms_fetch(prefix, utcdt)
             if fn is None:
                 continue
             LOG.info("Processing %s %s", utcdt, prefix)
@@ -70,9 +67,7 @@ def run(dt: datetime.date, for_dep: bool):
             continue
         with pygrib.open(gribfn) as grbs:
             grb = grbs[1]
-            if lats is None:
-                lats, _ = grb.latlons()
-            val = grb["values"]
+            val: np.ndarray = grb["values"]
         os.unlink(gribfn)
 
         # Anything less than zero, we set to zero
@@ -82,31 +77,45 @@ def run(dt: datetime.date, for_dep: bool):
         else:
             total += val
 
-    if lats is None:
+    if total is None:
         LOG.warning("nodata for %s, using zeros", dt)
-        ncprecip[offset, :, :] = 0
-        nc.close()
+        ncprecip[offset] = 0
         return
+    # To save some space, we store a tigher CONUS domain
+    # total.shape is 3500 x 7000, our 2700 x 6100
+    # The upper left corner grib is -129.995, 54.995
+    # The upper left corner netcdf is -125.995, 49.995
     # CAREFUL HERE!  The MRMS grid is North to South
-    # set top (smallest y)
-    y0 = int((lats[0, 0] - nc.variables["lat"][-1]) * 100.0)
-    y1 = int((lats[0, 0] - nc.variables["lat"][0]) * 100.0)
-    x0 = int((nc.variables["lon"][0] - mrms.WEST) * 100.0)
-    x1 = int((nc.variables["lon"][-1] - mrms.WEST) * 100.0) + 1  # Stinks
+    if total.shape != (3500, 7000):
+        LOG.warning("total shape is %s, skipping", total.shape)
+        return
+    x0 = 400
+    x1 = 400 + 6100
+    y0 = 500
+    y1 = 500 + 2700
     LOG.info("Writing precip at offset: %s [for_dep:%s]", offset, for_dep)
     ncprecip[offset, :, :] = np.flipud(total[y0:y1, x0:x1])
-    nc.close()
 
 
 @click.command()
-@click.option("--date", "dt", type=click.DateTime(), help="Date to process")
+@click.option(
+    "--date",
+    "dt",
+    type=click.DateTime(),
+    help="Date to process",
+    required=True,
+)
 @click.option("--for-dep", is_flag=True, help="For Daily Erosion Project")
-def main(dt, for_dep):
+def main(dt: datetime, for_dep):
     """go main go"""
     if for_dep and (f"{dt:%m%d}" < "0411" or f"{dt:%m%d}" > "0615"):
         LOG.info("DEP not needed for %s", dt)
         return
-    run(dt.date(), for_dep)
+    ncfn = get_daily_mrms_ncname(dt.year)
+    if for_dep:
+        ncfn = ncfn.replace("daily", "dep")
+    with ncopen(ncfn, "a", timeout=300) as nc:
+        run(nc, dt.date(), for_dep)
 
 
 if __name__ == "__main__":
