814dcb278993e8ed7e6b61f617e31d147ddd9297
akrherz <akrherz@iastate.edu>
2025-01-31 11:04:03 -0600

    🔥 Remove mod_wsgi startup stuff in iemwebfarm repo

--- deployment/mod_wsgi_startup_tc.py
+++
@@ -1,7 +0,0 @@
-"""Invoked at mod-wsgi startup for tc services."""
-
-import sys
-
-# Have our local pylib module as the first place to look
-if "/opt/iem/pylib" not in sys.path:
-    sys.path.insert(0, "/opt/iem/pylib")
