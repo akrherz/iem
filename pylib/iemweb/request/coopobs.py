7824e4dba217e5124b9bb22384fdbd6ba7b75c20
akrherz <akrherz@iastate.edu>
2025-02-22 17:19:17 -0600

    ✨ Migrate COOP obs dl to python
    https://mesonet.agron.iastate.edu/cgi-bin/request/coopobs.py?help

---
+++ pylib/iemweb/request/coopobs.py
@@ -0,0 +1,190 @@
+""".. title:: NWS COOP Observations Download
+
+Return to `API Services </api/#cgi>`_.  This service is the backend for the
+`COOP Obs Download </request/coop/obs-fe.phtml>`_ frontend.
+
+Documentation for /cgi-bin/request/coopobs.py
+---------------------------------------------
+
+This service emits the raw COOP observations without much IEM processing.
+
+Changelog
+---------
+
+- 2025-02-22: Initial implementation
+
+Example Usage
+-------------
+
+Fetch the COOP observations for Iowa on 1 January 2022:
+
+https://mesonet.agron.iastate.edu/cgi-bin/request/coopobs.py?\
+network=IA_COOP&stations=_ALL&sts=2025-01-01\
+&ets=2025-01-01&what=download&delim=comma
+
+"""
+
+from datetime import date, timedelta
+from io import StringIO
+
+from pydantic import Field
+from pyiem.database import sql_helper, with_sqlalchemy_conn
+from pyiem.exceptions import IncompleteWebRequest
+from pyiem.network import Table as NetworkTable
+from pyiem.webutil import CGIModel, ListOrCSVType, iemapp
+
+
+class Schema(CGIModel):
+    """See how we are called."""
+
+    delim: str = Field(
+        "comma",
+        description=(
+            "The delimiter to use in the output file.  "
+            "Options: comma, tab, space"
+        ),
+        pattern="^(comma|tab|space)$",
+    )
+    network: str = Field(
+        "IA_COOP", description="The network to use for station lookups."
+    )
+    stations: ListOrCSVType = Field(
+        [],
+        description=(
+            "List of stations to include in the output. Legacy variable name."
+        ),
+    )
+    what: str = Field("view", description="The type of output to generate.")
+    sts: date = Field(
+        None,
+        description="The starting date for the data request.",
+    )
+    ets: date = Field(
+        None,
+        description="The ending date for the data request.",
+    )
+    year1: int = Field(
+        None,
+        description="The starting year for the data request.",
+    )
+    month1: int = Field(
+        None,
+        description="The starting month for the data request.",
+    )
+    day1: int = Field(
+        None,
+        description="The starting day for the data request.",
+    )
+    year2: int = Field(
+        None,
+        description="The ending year for the data request.",
+    )
+    month2: int = Field(
+        None,
+        description="The ending month for the data request.",
+    )
+    day2: int = Field(
+        None,
+        description="The ending day for the data request.",
+    )
+
+
+def sane_date(year, month, day):
+    """Attempt to account for usage of days outside of the bounds for
+    a given month"""
+    # Calculate the last date of the given month
+    nextmonth = date(year, month, 1) + timedelta(days=35)
+    lastday = nextmonth.replace(day=1) - timedelta(days=1)
+    return date(year, month, min(day, lastday.day))
+
+
+def get_cgi_dates(environ):
+    """Figure out which dates are requested via the form, we shall attempt
+    to account for invalid dates provided!"""
+
+    ets = min(
+        sane_date(environ["year2"], environ["month2"], environ["day2"]),
+        date.today() - timedelta(days=1),
+    )
+
+    return [
+        sane_date(environ["year1"], environ["month1"], environ["day1"]),
+        ets,
+    ]
+
+
+def get_cgi_stations(environ):
+    """Figure out which stations the user wants, return a list of them"""
+    reqlist = environ["stations"]
+    if "_ALL" in reqlist:
+        nt = NetworkTable(environ["network"], only_online=False)
+        return list(nt.sts.keys())
+
+    return reqlist
+
+
+@with_sqlalchemy_conn("iem")
+def do_simple(ctx, conn=None):
+    """Generate Simple output"""
+    limitrowcount = "LIMIT 1048000" if ctx["what"] == "excel" else ""
+    res = conn.execute(
+        sql_helper(
+            """
+ SELECT s.*,t.id, day,
+ coalesce(to_char(coop_valid at time zone t.tzname, 'HH PM'), '') as cv
+ from summary s JOIN stations t on (t.iemid = s.iemid)
+ WHERE day >= :sts and day <= :ets
+ and id = ANY(:stations) and network = :network ORDER by s.day ASC
+    {limitrowcount}""",
+            limitrowcount=limitrowcount,
+        ),
+        {
+            "stations": ctx["stations"],
+            "sts": ctx["sts"],
+            "ets": ctx["ets"],
+            "network": ctx["network"],
+        },
+    )
+    p = {"comma": ",", "tab": "\t", "space": " "}
+    d = p[ctx["delim"]]
+    cols = [
+        "nwsli",
+        "date",
+        "time",
+        "high_F",
+        "low_F",
+        "precip",
+        "snow_inch",
+        "snowd_inch",
+    ]
+    with StringIO() as sio:
+        sio.write(d.join(cols) + "\n")
+        for row in res.mappings():
+            sio.write(row["id"] + d)
+            sio.write(row["day"].strftime("%Y-%m-%d") + d)
+            sio.write(row["cv"] + d)
+            sio.write(f"{row['max_tmpf']}{d}")
+            sio.write(f"{row['min_tmpf']}{d}")
+            sio.write(f"{row['pday']}{d}")
+            sio.write(f"{row['snow']}{d}")
+            sio.write(f"{row['snowd']}\n")
+        return sio.getvalue().replace("None", "").encode("ascii")
+
+
+@iemapp(help=__doc__, schema=Schema)
+def application(environ, start_response):
+    """go main go"""
+    environ["stations"] = get_cgi_stations(environ)
+    if not environ["stations"]:
+        raise IncompleteWebRequest("No stations were specified.")
+    environ["sts"], environ["ets"] = get_cgi_dates(environ)
+
+    headers = [("Content-type", "text/plain")]
+    if environ["what"] == "download":
+        headers.append(
+            ("Content-Disposition", "attachment; filename=coop.txt")
+        )
+
+    start_response("200 OK", headers)
+    res = do_simple(environ)
+    return [res]
