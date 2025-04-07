ccb21dc811a0ea947b4785cd41724d4e46b3bf71
akrherz <akrherz@iastate.edu>
2025-04-07 15:38:01 -0500

    ✨ Add top10 smallest SPC outlooks
    https://mesonet.agron.iastate.edu/nws/spc_top10.phtml

--- pylib/iemweb/json/spc_bysize.py
+++ pylib/iemweb/json/spc_bysize.py
@@ -12,6 +12,7 @@ are made to generate links to the SPC website, to confirm the entries listed.
 Changelog
 ---------
 
+- 2025-04-07: Added sort={asc|desc} to control sort order.
 - 2024-08-14: Documentation Update
 
 Example Requests
@@ -29,11 +30,12 @@ import time
 from datetime import date, timedelta
 
 from pydantic import Field
-from pyiem.database import get_dbconnc
+from pyiem.database import sql_helper, with_sqlalchemy_conn
 from pyiem.nws.products.spcpts import imgsrc_from_row
 from pyiem.reference import ISO8601
 from pyiem.util import utc
 from pyiem.webutil import CGIModel, iemapp
+from sqlalchemy.engine import Connection
 
 
 class Schema(CGIModel):
@@ -42,38 +44,46 @@ class Schema(CGIModel):
     callback: str = Field(None, description="JSONP callback function")
     category: str = Field("CATEGORICAL", description="SPC outlook category")
     day: int = Field(..., description="SPC outlook day to query")
+    sort: str = Field(
+        default="desc",
+        description="how to sort the results,",
+        pattern="^(asc|desc|ASC|DESC)$",
+    )
     syear: int = Field(1987, description="Inclusive start year to query")
     threshold: str = Field(..., description="SPC outlook threshold to query")
 
 
-def dowork(environ):
+@with_sqlalchemy_conn("postgis")
+def dowork(environ, conn: Connection = None) -> str:
     """Actually do stuff"""
 
     data = {"generated_at": f"{utc():%Y-%m-%dT%H:%M:%SZ}", "outlooks": []}
     hits = []
-    dbconn, cursor = get_dbconnc("postgis")
     sts = time.perf_counter()
-    cursor.execute(
-        """
+    res = conn.execute(
+        sql_helper(
+            """
         SELECT product_issue at time zone 'UTC' as i,
         expire at time zone 'UTC' as e,
         ST_Area(st_transform(geom, 9311)) / 1000000. as area_sqkm, cycle,
         product_issue at time zone 'UTC' as product_issue, category, day
-        from spc_outlooks WHERE outlook_type = %(ot)s and
-        day = %(day)s and threshold = %(threshold)s
-        and category = %(category)s and issue > %(sdate)s and geom is not null
-        ORDER by area_sqkm DESC NULLS LAST LIMIT 100
+        from spc_outlooks WHERE outlook_type = :ot and
+        day = :day and threshold = :threshold
+        and category = :cat and issue > :sdate and geom is not null
+        ORDER by area_sqkm {order} NULLS LAST LIMIT 100
     """,
+            order="ASC" if environ["sort"].lower() == "asc" else "DESC",
+        ),
         {
             "day": environ["day"],
             "threshold": environ["threshold"],
-            "category": environ["category"],
+            "cat": environ["category"],
             "ot": "F" if environ["category"].startswith("F") else "C",
             "sdate": date(environ["syear"], 1, 1),
         },
     )
     data["query_time[s]"] = round(time.perf_counter() - sts, 2)
-    for row in cursor:
+    for row in res.mappings():
         key = f"{row['e']:%Y%m%d}"
         if key in hits:
             continue
@@ -89,7 +99,6 @@ def dowork(environ):
         )
         if len(data["outlooks"]) >= 10:
             break
-    dbconn.close()
     return json.dumps(data)
 
 
@@ -97,7 +106,7 @@ def get_mckey(environ: dict) -> str:
     """Figure out the key for this request."""
     return (
         f"/json/spc_bysize.json|{environ['category']}|{environ['day']}|"
-        f"{environ['syear']}|{environ['threshold']}"
+        f"{environ['syear']}|{environ['threshold']}|{environ['sort']}"
     ).replace(" ", "")
 
 
