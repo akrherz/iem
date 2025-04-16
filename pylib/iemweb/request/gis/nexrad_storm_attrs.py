67b891d82c488d4a4a2bf2aeff44004a4da4d777
akrherz <akrherz@iastate.edu>
2025-04-16 09:46:43 -0500

    ✨ Add `min_hail_size` filter to NEXRAD attrs dl
    https://mesonet.agron.iastate.edu/cgi-bin/request/gis/nexrad_storm_attrs.py?help

--- pylib/iemweb/request/gis/nexrad_storm_attrs.py
+++ pylib/iemweb/request/gis/nexrad_storm_attrs.py
@@ -13,8 +13,9 @@ request more than two radar sites, the time span is limited to 7 days.
 Changelog
 ---------
 
+- 2025-04-16: Added `min_hail_size` filter parameter.
 - 2025-02-13: Requests are limited to 1000 (RADARs * days), which is
-    effectively all RADARs for about a week.
+  effectively all RADARs for about a week.
 - 2024-06-11: Initial documentation release
 
 Example Usage
@@ -33,6 +34,11 @@ Provide all attributes for the Tallahassee, FL radar site for Aug 10, 2024 UTC.
 
 https://mesonet.agron.iastate.edu/cgi-bin/request/gis/nexrad_storm_attrs.py?\
 fmt=shp&sts=2024-08-10T00:00:00Z&ets=2024-08-11T00:00:00Z&radar=TLH
+
+Return all attributes on 10 Aug 2024 with at least 0.5 inch hail.
+
+https://mesonet.agron.iastate.edu/cgi-bin/request/gis/nexrad_storm_attrs.py?\
+fmt=shp&sts=2024-08-10T00:00:00Z&ets=2024-08-11T00:00:00Z&min_hail_size=0.5
 
 """
 
@@ -77,6 +83,12 @@ class Schema(CGIModel):
     minute2: int = Field(
         None, description="Minute for end of time if ets not set"
     )
+    min_hail_size: float = Field(
+        None,
+        description="Minimum hail size (inch) to include in the results.",
+        ge=0,
+        le=10,
+    )
 
 
 def run(environ, start_response):
@@ -98,6 +110,9 @@ def run(environ, start_response):
             "Request is too large, please limit to (radars * days) < 1500"
         )
     fn = f"stormattr_{environ['sts']:%Y%m%d%H%M}_{environ['ets']:%Y%m%d%H%M}"
+    filters = ""
+    if environ["min_hail_size"] is not None:
+        filters = " and max_size >= :min_hail_size "
 
     with get_sqlalchemy_conn("radar") as conn:
         res = conn.execute(
@@ -109,14 +124,17 @@ def run(environ, start_response):
             vil, max_dbz, max_dbz_height, top, drct, sknt,
             ST_y(geom) as lat, ST_x(geom) as lon
             from nexrad_attributes_log WHERE
-            valid >= :sts and valid < :ets {radarlimit} ORDER by valid ASC
+            valid >= :sts and valid < :ets {radarlimit} {filters}
+            ORDER by valid ASC
             """,
                 radarlimit=radarlimit,
+                filters=filters,
             ),
             {
                 "sts": environ["sts"],
                 "ets": environ["ets"],
                 "radar": environ["radar"],
+                "min_hail_size": environ["min_hail_size"],
             },
         )
         if res.rowcount == 0:
