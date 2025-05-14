9191368a5c611b70299fac39bc3abb44ad1eeb22
akrherz <akrherz@iastate.edu>
2025-05-14 12:51:04 -0500

    📝 Sundry updates per review

--- scripts/dbutil/clean_afos.py
+++ scripts/dbutil/clean_afos.py
@@ -9,26 +9,26 @@ from pyiem.util import logger
 
 LOG = logger()
 
+# reflect changes to docs/datasets/afos.md
+PIL3_IGNORE = (
+    "RR1 RR2 RR3 RR4 RR5 RR6 RR7 RR8 RR9 RRA RRS RRZ ECM ECS ECX LAV "
+    "LEV MAV MET MTR MEX NBE NBH NBP NBS NBX OSO RSD RWR STO HML WRK "
+    "SCV LLL GSM"
+).split()
+PIL_IGNORE = (
+    "HPTNCF WTSNCF TSTNCF HD3RSA XF03DY XOBUS ECMNC1 SYNBOU MISWTM MISWTX "
+    "MISMA1 MISAM1 XF07DY"
+).split()
+for abbr in state_names:
+    # SCNWSH, le sigh
+    PIL_IGNORE.append(f"SCN{abbr}")
+
 
 def main():
     """Clean AFOS and friends"""
     pgconn = get_dbconn("afos")
     acursor = pgconn.cursor()
 
-    # reflect changes to docs/datasets/afos.md
-    # RRM removed due to request
-    pil3 = (
-        "RR1 RR2 RR3 RR4 RR5 RR6 RR7 RR8 RR9 RRA RRS RRZ ECM ECS ECX LAV "
-        "LEV MAV MET MTR MEX NBE NBH NBP NBS NBX OSO RSD RWR STO HML WRK "
-        "SCV LLL"
-    ).split()
-    pils = (
-        "HPTNCF WTSNCF TSTNCF HD3RSA XF03DY XOBUS ECMNC1 SYNBOU MISWTM MISWTX "
-        "MISMA1 MISAM1"
-    ).split()
-    for abbr in state_names:
-        # SCNWSH, le sigh
-        pils.append(f"SCN{abbr}")
     acursor.execute(
         """
         delete from products WHERE
@@ -36,7 +36,7 @@ def main():
         entered > ('YESTERDAY'::date - '31 days'::interval) and
         (substr(pil, 1, 3) = ANY(%s) or pil = ANY(%s))
         """,
-        (pil3, pils),
+        (PIL3_IGNORE, PIL_IGNORE),
     )
     LOG.info("deleted %s rows", acursor.rowcount)
     if acursor.rowcount == 0:
