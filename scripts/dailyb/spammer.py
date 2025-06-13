f3b1020ca254cb736fe4f6421014f00538fab6a2
akrherz <akrherz@iastate.edu>
2025-06-12 22:56:22 -0500

    📝 Add iemvtec github app to dailyb email report

--- scripts/dailyb/spammer.py
+++ scripts/dailyb/spammer.py
@@ -18,7 +18,9 @@ from pyiem.reference import ISO8601
 from pyiem.util import exponential_backoff, logger, utc
 
 LOG = logger()
-REPOS = "iem pyiem iemone iemlsr pywwa iem-database iem-web-services".split()
+REPOS = (
+    "iem pyiem iemone iemlsr iemvtec pywwa iem-database iem-web-services"
+).split()
 GITHUB_API_BASE = "https://api.github.com/repos/akrherz"
 URLS = re.compile(r"(https?://[\w\d:#@%/;$()~_?\+-=\\\.&]*)", re.MULTILINE)
 
