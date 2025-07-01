5b53f3190ac9ebe01a4953d410600213928eaeb3
akrherz <akrherz@iastate.edu>
2025-07-01 13:18:46 -0500

    🐛 Set maxOptions to 1000 for TomSelect

--- htdocs/js/select2.js
+++ htdocs/js/select2.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('.iemselect2').forEach((el) => {
         window.ts = new TomSelect(el, {
             searchField: ['text', 'value'],
+	    maxOptions: 1000,
             allowEmptyOption: false
         });
     });
