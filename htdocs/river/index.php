2d4921777f3c00afcb2f48fe7febab2825f19e7a
akrherz <akrherz@iastate.edu>
2025-07-30 12:19:53 -0500

    ✨ Add Flood Watches to IEM River app
    https://mesonet.agron.iastate.edu/river/

--- htdocs/river/index.php
+++ htdocs/river/index.php
@@ -20,70 +20,79 @@ $sevcol = array(
     "3" => "#f0f",
     "U" => "#fff"
 );
-
-$url = "{$EXTERNAL_BASEURL}/api/1/nws/current_flood_warnings.json";
+$jobs = Array(
+    "W" => "warnings",
+    "A" => "watches",
+);
 $c1 = "";
 $c2 = "";
 $c3 = "";
-if (isset($_REQUEST["state"])) {
-    $c2 = " bg-light border rounded p-3";
-    $url .= "?state={$state}";
-    $ptitle = "<h3>River Forecast Point Monitor by State</h3>";
-} else if (isset($_REQUEST["all"])) {
-    $c3 = " bg-light border rounded p-3";
-    $ptitle = "<h3>River Forecast Point Monitor (view all)</h3>";
-} else {
-    $c1 = " bg-light border rounded p-3";
-    $url .= "?wfo={$wfo}";
-    $ptitle = "<h3>River Forecast Point Monitor by NWS WFO</h3>";
-}
-
-$jdata = file_get_contents($url);
-$jobj = json_decode($jdata, $assoc = TRUE);
-
 $rivers = array();
 $used = array();
-foreach ($jobj["data"] as $bogus => $row) {
-    if (in_array($row["nwsli"], $used)) {
-        continue;
-    }
-    $used[] = $row["nwsli"];
-    $river = $row["river_name"];
-    $uri = sprintf(
-        "/vtec/?year=%s&wfo=%s&phenomena=%s&significance=W&eventid=%s",
-        date("Y"),
-        rectify_wfo($row["wfo"]),
-        $row["phenomena"],
-        $row["eventid"]
-    );
-    if (!array_key_exists($river, $rivers)) {
-        $rivers[$river] = "";
+foreach ($jobs as $key => $val) {
+    $url = "{$EXTERNAL_BASEURL}/api/1/nws/current_flood_{$val}.json";
+    if (isset($_REQUEST["state"])) {
+        $c2 = " bg-light border rounded p-3";
+        $url .= "?state={$state}";
+        $ptitle = "<h3>River Forecast Point Monitor by State</h3>";
+    } else if (isset($_REQUEST["all"])) {
+        $c3 = " bg-light border rounded p-3";
+        $ptitle = "<h3>River Forecast Point Monitor (view all)</h3>";
+    } else {
+        $c1 = " bg-light border rounded p-3";
+        $url .= "?wfo={$wfo}";
+        $ptitle = "<h3>River Forecast Point Monitor by NWS WFO</h3>";
     }
 
-    $rivers[$river] .= sprintf(
-        "<tr><td style='background: %s;'>&nbsp;&nbsp;</td>" .
-            "<th>%s<br />" .
-            "<a href=\"/plotting/auto/?q=160&amp;station=%s\"><i class=\"fa fa-signal\"></i> %s</a></th><td><a href='%s'>%s</a></td><td>%s</td>" .
-            "<td>%s</td><td>%s</td><td><strong>Impact...</strong> %s</td></tr>",
-        $sevcol[$row["severity"]],
-        $row["name"],
-        $row["nwsli"],
-        $row["nwsli"],
-        $uri,
-        $row["counties"],
-        $row["stage_text"],
-        $row["flood_text"],
-        $row["forecast_text"],
-        $row["impact_text"]
-    );
+    $jdata = file_get_contents($url);
+    $jobj = json_decode($jdata, $assoc = TRUE);
+
+    foreach ($jobj["data"] as $bogus => $row) {
+        if (in_array($row["nwsli"], $used)) {
+            continue;
+        }
+        $used[] = $row["nwsli"];
+        $river = $row["river_name"];
+        $uri = sprintf(
+            "/vtec/?year=%s&wfo=%s&phenomena=%s&significance=%s&eventid=%s",
+            date("Y"),
+            rectify_wfo($row["wfo"]),
+            $row["phenomena"],
+            $row["significance"],
+            $row["eventid"]
+        );
+        if (!array_key_exists($river, $rivers)) {
+            $rivers[$river] = "";
+        }
+
+        $rivers[$river] .= sprintf(
+            "<tr><td style='background: %s;'>&nbsp;&nbsp;</td>" .
+            "<td>%s</td>" .
+                "<th>%s<br />" .
+                "<a href=\"/plotting/auto/?q=160&amp;station=%s\"><i class=\"fa fa-signal\"></i> %s</a></th><td><a href='%s'>%s</a></td><td>%s</td>" .
+                "<td>%s</td><td>%s</td><td><strong>Impact...</strong> %s</td></tr>",
+            $sevcol[$row["severity"]],
+            ($key == "W" ? "Flood Warning" : "Flood Watch"),
+            $row["name"],
+            $row["nwsli"],
+            $row["nwsli"],
+            $uri,
+            $row["counties"],
+            $row["stage_text"],
+            $row["flood_text"],
+            $row["forecast_text"],
+            $row["impact_text"]
+        );
+    }
 }
 $content .= $ptitle;
 $nselect = networkSelect("WFO", $wfo, array(), 'wfo');
 $sselect = stateSelect($state);
 $content .= <<<EOM
 <p>This page produces a summary listing for National Weather Service Flood 
-Forecast Points when the point is currently in a flood warning state.  The IEM
-processes the flood warning products and attempts to extract the important 
+Forecast Points when the point is currently in a flood watch or warning state.
+The IEM
+processes the flood products and attempts to extract the important 
 details regarding flood state, severity, forecasted stage and impact. By clicking
 on the graph icon near the location identfier, you are taken to an IEM Autoplot
 which shows forecasted stage and observations.</p>
@@ -132,11 +141,11 @@ $rvs = array_keys($rivers);
 asort($rvs);
 
 foreach ($rvs as $idx => $key) {
-    $content .=  "<tr><th colspan=\"7\" style='background: #eee; text-align: left;'>$key</th></tr>";
+    $content .=  "<tr><th colspan=\"8\" style='background: #eee; text-align: left;'>$key</th></tr>";
     $content .=  $rivers[$key];
 }
 if (sizeof($rvs) == 0) {
-    $content .= "<tr><td colspan=\"7\">No Entries Found</td></tr>";
+    $content .= "<tr><td colspan=\"8\">No Entries Found</td></tr>";
 }
 
 $content .=  "</table>";
