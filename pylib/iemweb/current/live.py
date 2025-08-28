29037ca592a490a607f6eac3ec10aaf4d29c5c16
akrherz <akrherz@iastate.edu>
2025-08-28 10:18:50 -0500

    💚 Improve coverage

--- pylib/iemweb/current/live.py
+++ pylib/iemweb/current/live.py
@@ -1,8 +1,13 @@
-"""Generate a 'Live' webcam image on demand
+""".. title:: A Live Webcam Image
 
-URIs look like so:
-    /live.py?id=KCRG-006
-    /live/KCRG-006.jpg
+This service is a proxy to get a current webcam image.  There is about 15
+seconds of caching to keep the remote webcam from getting runover.
+
+Changelog
+---------
+
+- 2025-08-28: Implement pydantic validation
+
 """
 
 from datetime import datetime
@@ -10,51 +15,57 @@ from io import BytesIO
 
 import httpx
 from PIL import Image, ImageDraw
-from pyiem.database import get_dbconn
+from pydantic import Field
+from pyiem.database import sql_helper, with_sqlalchemy_conn
 from pyiem.util import LOG, get_properties
-from pyiem.webutil import iemapp
-from pymemcache.client import Client
+from pyiem.webutil import CGIModel, iemapp
+from sqlalchemy.engine import Connection
 
 
-def fetch(cid):
+class MySchema(CGIModel):
+    """See how we are called."""
+
+    id: str = Field(
+        "KCCI-027", description="The ID of the webcam", max_length=10
+    )
+
+
+@with_sqlalchemy_conn("mesosite")
+def fetch(cid: str, conn: Connection | None = None):
     """Do work to get the content"""
     # Get camera metadata
-    pgconn = get_dbconn("mesosite")
-    cursor = pgconn.cursor()
-    cursor.execute(
-        "SELECT ip, fqdn, online, name, port, is_vapix, scrape_url, network "
-        "from webcams WHERE id = %s",
-        (cid,),
+    res = conn.execute(
+        sql_helper("""
+    SELECT ip, fqdn, online, name, port, is_vapix, scrape_url, network
+    FROM webcams WHERE id = :cid
+    """),
+        {"cid": cid},
     )
-    if cursor.rowcount != 1:
+    meta = None
+    for row in res.mappings():
+        meta = row
+    if meta is None:
         return None
-    (
-        ip,
-        fqdn,
-        online,
-        name,
-        port,
-        is_vapix,
-        scrape_url,
-        network,
-    ) = cursor.fetchone()
-    pgconn.close()
-    if scrape_url is not None or not online:
+    if meta["scrape_url"] is not None or not meta["online"]:
         return None
     # Get IEM properties
     iemprops = get_properties()
-    user = iemprops.get(f"webcam.{network.lower()}.user")
-    passwd = iemprops.get(f"webcam.{network.lower()}.pass")
+    user = iemprops.get(f"webcam.{meta['network'].lower()}.user")
+    passwd = iemprops.get(f"webcam.{meta['network'].lower()}.pass")
     # Construct URI
     uribase = "http://%s:%s/-wvhttp-01-/GetOneShot"
-    if is_vapix:
+    if meta["is_vapix"]:
         uribase = "http://%s:%s/axis-cgi/jpg/image.cgi"
-    uri = uribase % (ip if ip is not None else fqdn, port)
+    uri = uribase % (
+        meta["ip"] if meta["ip"] is not None else meta["fqdn"],
+        meta["port"],
+    )
     ham = (
         httpx.BasicAuth
         if cid
         in [
             "KCRG-010",
+            "KCCI-027",
         ]
         else httpx.DigestAuth
     )
@@ -70,38 +81,24 @@ def fetch(cid):
     draw = ImageDraw.Draw(image)
     draw.rectangle([0, height - 12, width, height], fill="#000000")
     stamp = datetime.now().strftime("%d %b %Y %I:%M:%S %P")
-    title = f"{name} - {network} Webcam Live Image at {stamp}"
+    title = f"{meta['name']} - {meta['network']} Webcam Live Image at {stamp}"
     draw.text((5, height - 12), title)
     buf = BytesIO()
     image.save(buf, format="JPEG")
     return buf.getvalue()
 
 
-def workflow(cid):
-    """The necessary workflow for this camera ID"""
-    mckey = f"/current/live/{cid}.jpg"
-    mc = Client("iem-memcached:11211")
-    res = mc.get(mckey)
-    if res:
-        return res
-    try:
-        res = fetch(cid)
-    except Exception:
-        return None
-    if res is not None:
-        # Set for 15 seconds
-        mc.set(mckey, res, 15)
-    mc.close()
-    return res
-
-
-@iemapp()
+@iemapp(
+    content_type="image/jpeg",
+    memcachekey=lambda environ: f"/current/live/{environ['id']}.jpg",
+    memcacheexpire=15,
+    help=__doc__,
+    schema=MySchema,
+)
 def application(environ, start_response):
     """Do Fun Things"""
-    cid = environ.get("id", "KCCI-027")[:10]  # Default to ISU AgFarm
-    imagedata = workflow(cid)
+    imagedata = fetch(environ["id"])
     if imagedata is None:
-        # TOOD: make a sorry image
         image = Image.new("RGB", (640, 480))
         draw = ImageDraw.Draw(image)
         draw.text((320, 240), "Sorry, failed to generate image :(")
@@ -110,4 +107,4 @@ def application(environ, start_response):
         imagedata = buf.getvalue()
 
     start_response("200 OK", [("Content-type", "image/jpeg")])
-    return [imagedata]
+    return imagedata
---
+++ tests/iemweb/current/test_live.py
@@ -0,0 +1,40 @@
+"""Test the current/live.py service."""
+
+from iemweb.current import live
+from PIL import Image
+from pytest_httpx import HTTPXMock
+from werkzeug.test import Client
+
+
+def test_offline():
+    """Test for a webcam that is offline."""
+    client = Client(live.application)
+    response = client.get("/current/live.py?id=KCRG-002")
+    assert response.status_code == 200
+
+
+def test_workflow(httpx_mock: HTTPXMock):
+    """Test we can fetch an image."""
+    image = Image.new("RGB", (320, 240), (73, 109, 137))
+    httpx_mock.add_response(
+        status_code=200,
+        content=image,
+    )
+    client = Client(live.application)
+    response = client.get("/current/live.py?id=KCRG-032")  # is_vapix
+    assert response.status_code == 200
+
+
+def test_badauth(httpx_mock: HTTPXMock):
+    """Test that we handle bad auth."""
+    httpx_mock.add_response(status_code=401)
+    client = Client(live.application)
+    response = client.get("/current/live.py?id=KCRG-003")
+    assert response.status_code == 200
+
+
+def test_live_webcam_image():
+    client = Client(live.application)
+    response = client.get("/current/live.py?id=KCCI-099")
+    assert response.status_code == 200
+    assert response.content_type == "image/jpeg"
