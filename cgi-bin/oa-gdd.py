4136c68b40f03e4de4a3c2cd0ceb18ca55559b9e
akrherz <akrherz@iastate.edu>
2025-09-11 10:07:23 -0500

    🔥 Drop unused oa-gdd script

--- cgi-bin/oa-gdd.py
+++
@@ -1,6 +0,0 @@
-"""Implementation at https://github.com/akrherz/iem/blob/main/pylib/iemweb/oa_gdd.py
-User documentation available at https://mesonet.agron.iastate.edu/cgi-bin/oa-gdd.py?help"""
-
-from iemweb.oa_gdd import application
-
-__all__ = ["application"]
