0b6c1b7a6b200de677b52cb68a7c89dbffd24dce
akrherz <akrherz@iastate.edu>
2025-09-24 14:58:10 -0500

    📝 Tweaks per archive updates per akrherz/infra#7

--- htdocs/projects/webcam.php
+++ htdocs/projects/webcam.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 require_once "../../config/settings.inc.php";
 define("IEM_APPID", 98);
 require_once "../../include/myview.php";
@@ -24,8 +24,8 @@ images saved every 5 minutes for that period!</p>
 <object width="100%" height="350">
 <param name="movie" value="https://youtube.com/v/yXnkzeCU3bE"></param>
 <param name="wmode" value="transparent"></param>
-<embed src="https://youtube.com/v/yXnkzeCU3bE" 
-type="application/x-shockwave-flash" wmode="transparent" width="425" 
+<embed src="https://youtube.com/v/yXnkzeCU3bE"
+type="application/x-shockwave-flash" wmode="transparent" width="425"
 height="350"></embed></object>
 
 <br />An awesome gravity wave lapse from near Tama, IA.  This lapse has over
@@ -64,13 +64,6 @@ webcam images. Here are a few options though:</p>
 <li>Hit the <a href="/json/#IEM+Webcam+Availability">Webcam JSON API</a>
 without providing a network parameter and get all imagery close to a given
 timestamp.  The JSON metadata will contain the URL to the image.</li>
-<li><strong>Blunt Force Method</strong>: The IEM creates per UTC date tarballs
-of the archive/data folder.  Inside of these will be a <code>camera</code>
-folder with all the images for that day.  You can download these tarballs
-<a href="https://iastate.box.com/s/f8pnccjpedqmd4jnppafnkbjxjgxgqwj">here</a>,
-but box.com does not allow this to be very programatic.  But if you share with
-us your box.com account, there is then a means to share that folder with you
-and you can then automate a download.</li>
 </ol>
 
 <h3>Frequently Asked Questions</h3>
@@ -90,7 +83,7 @@ and you can then automate a download.</li>
         have traditionally seen sites use.  The power usage is neglegible
         and usually donated by the webcam's host.  The Internet requirements
         are for fast upload speeds as the webcam needs to serve out its
-        imagery.  Having 1 megabit upload speed will provide a workable 
+        imagery.  Having 1 megabit upload speed will provide a workable
         video stream.  The IEM provides collection and archival services
         at no cost and without warranty.  There is no lock-in with our webcam
         collection software and so other tools can access the webcams at
@@ -112,7 +105,7 @@ and you can then automate a download.</li>
         ethernet networks can be difficult to work with.  We have had luck
         with municipal locations, like town squares, clock towers, and
         private downtown buildings.</p>
-        
+
 <p><strong>Which directions are the most important to see?</strong>
 <br />Nearly all webcam houses create a blind spot whereby the webcam
         can not see past its housing mount.  Having this blind spot be to
