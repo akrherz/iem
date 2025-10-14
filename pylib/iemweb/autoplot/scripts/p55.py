31feee7d8872f0b1fe6cc31abd8b741f2542e492
akrherz <akrherz@iastate.edu>
2025-10-14 11:44:33 -0500

    ✨ Plot ncei71-00 climatology, if it exists within IEM
    https://mesonet.agron.iastate.edu/plotting/auto/?q=55

--- pylib/iemweb/autoplot/scripts/p55.py
+++ pylib/iemweb/autoplot/scripts/p55.py
@@ -11,7 +11,7 @@ import calendar
 from datetime import date
 
 import pandas as pd
-from pyiem.database import get_sqlalchemy_conn
+from pyiem.database import get_sqlalchemy_conn, sql_helper
 from pyiem.exceptions import NoDataFound
 from pyiem.plot import figure
 
@@ -52,46 +52,57 @@ def plotter(ctx: dict):
     station = ctx["station"]
     month = ctx["month"]
 
-    # beat month
+    ncei91 = ctx["_nt"].sts[station]["ncei91"]
+    ncei81 = ctx["_nt"].sts[station]["ncdc81"]
     with get_sqlalchemy_conn("coop") as conn:
         df = pd.read_sql(
-            """
+            sql_helper("""
         with obs as (
-        SELECT sday, avg(high) as avgh, avg(low) as avgl,
-        avg((high+low)/2.) as avgt, min(year) as min_year,
-        max(year) as max_year from alldata
-        WHERE station = %s and month = %s and year >= %s and year <= %s
-        GROUP by sday
+            SELECT sday, avg(high) as avgh, avg(low) as avgl,
+            avg((high+low)/2.) as avgt, min(year) as min_year,
+            max(year) as max_year from alldata
+            WHERE station = :station and month = :month
+            and year >= :syear and year <= :eyear
+            GROUP by sday
         ), c91 as (
         SELECT to_char(valid, 'mmdd') as sday, high, low, (high+low)/2. as avgt
-        from ncei_climate91 where station = %s
+        from ncei_climate91 where station = :ncei91
         ), c81 as (
         SELECT to_char(valid, 'mmdd') as sday, high, low, (high+low)/2. as avgt
-        from ncdc_climate81 where station = %s
+        from ncdc_climate81 where station = :ncei81
+        ), c71 as (
+        select to_char(valid, 'mmdd') as sday, high, low, (high+low)/2. as avgt
+        from ncdc_climate71 where station = :climate_site
         )
 
         SELECT o.sday, o.min_year, o.max_year,
         o.avgh as iem_avgh,
         c91.high as ncei91_avgh,
         c81.high as ncei81_avgh,
+        c71.high as ncei71_avgh,
         o.avgl as iem_avgl,
         c91.low as ncei91_avgl,
         c81.low as ncei81_avgl,
+        c71.low as ncei71_avgl,
         o.avgt as iem_avgt,
         c91.avgt as ncei91_avgt,
-        c81.avgt as ncei81_avgt
-        from obs o, c91, c81 where o.sday = c81.sday
-        and o.sday = c91.sday ORDER by o.sday ASC
-        """,
+        c81.avgt as ncei81_avgt,
+        c71.avgt as ncei71_avgt
+        from obs o LEFT JOIN c91 ON o.sday = c91.sday
+        LEFT JOIN c81 ON o.sday = c81.sday
+        LEFT JOIN c71 ON o.sday = c71.sday
+        ORDER by o.sday ASC
+        """),
             conn,
-            params=(
-                station,
-                month,
-                ctx["syear"],
-                ctx["eyear"],
-                ctx["_nt"].sts[station]["ncei91"],
-                ctx["_nt"].sts[station]["ncdc81"],
-            ),
+            params={
+                "station": station,
+                "month": month,
+                "syear": ctx["syear"],
+                "eyear": ctx["eyear"],
+                "ncei91": ctx["_nt"].sts[station]["ncei91"],
+                "ncei81": ctx["_nt"].sts[station]["ncdc81"],
+                "climate_site": ctx["_nt"].sts[station]["climate_site"],
+            },
             index_col="sday",
         )
     if df.empty:
@@ -101,7 +112,14 @@ def plotter(ctx: dict):
         f"{ctx['_sname']} :: Daily "
         f"Climate Comparison for {calendar.month_name[month]}"
     )
-    fig = figure(apctx=ctx, title=title)
+    fig = figure(
+        apctx=ctx,
+        title=title,
+        subtitle=(
+            f"NCEI91 Station: {ncei91} NCEI81 Station: {ncei81} "
+            f"NCEI71 Station: {ctx['_nt'].sts[station]['climate_site']}"
+        ),
+    )
     height = 0.21
     ax = [
         fig.add_axes((0.1, 0.08 + height * 2 + 0.18, 0.85, height)),
@@ -121,24 +139,28 @@ def plotter(ctx: dict):
                 "Obs Avg"
             ),
         )
-        ax[i].plot(
-            x,
-            df[f"ncei91_{c}"],
-            lw=2,
-            zorder=2,
-            color="b",
-            label="NCEI 1991-2020",
-        )
-        ax[i].plot(
-            x,
-            df[f"ncei81_{c}"],
-            lw=2,
-            zorder=2,
-            color="g",
-            label="NCEI 1981-2010",
-        )
+        for prefix, label, color in zip(
+            ["ncei91", "ncei81", "ncei71"],
+            ["NCEI 1991-2020", "NCEI 1981-2010", "NCEI 1971-2000"],
+            ["red", "blue", "green"],
+            strict=True,
+        ):
+            if df[f"{prefix}_{c}"].isna().all():
+                continue
+            ax[i].plot(
+                x,
+                df[f"{prefix}_{c}"],
+                lw=2,
+                zorder=2,
+                color=color,
+                label=label,
+            )
         ax[i].grid(True)
-        ymin = df[[f"iem_{c}", f"ncei91_{c}", f"ncei81_{c}"]].min().min()
+        ymin = (
+            df[[f"iem_{c}", f"ncei91_{c}", f"ncei81_{c}", f"ncei71_{c}"]]
+            .min()
+            .min()
+        )
         ax[i].set_ylim(bottom=ymin - 2)
 
     ax[0].set_ylabel("High Temp °F")
--- scripts/dbutil/set_climate.py
+++ scripts/dbutil/set_climate.py
@@ -2,78 +2,82 @@
 Assign a climate site to each site in the mesosite database, within reason
 """
 
-from pyiem.database import get_dbconn
+import pandas as pd
+from pyiem.database import sql_helper, with_sqlalchemy_conn
 from pyiem.util import logger
+from sqlalchemy.engine import Connection
 
 LOG = logger()
 
 
-def workflow(col):
+@with_sqlalchemy_conn("mesosite")
+def workflow(col: str, conn: Connection | None = None):
     """Query out all sites with a null climate_site"""
-    pgconn = get_dbconn("mesosite")
-    mcursor = pgconn.cursor()
-    mcursor2 = pgconn.cursor()
-    if col == "climate_site":
-        # Update when sites come offline and online
-        mcursor.execute(
-            """
-            update stations SET climate_site = null where
-            climate_site not in (select id from stations where
-            network ~* 'CLIMATE' and online and archive_begin < '1990-01-01')
-            """
-        )
-        if mcursor.rowcount > 0:
-            LOG.info(
-                "Found %s entries with now offline climate stations",
-                mcursor.rowcount,
-            )
-    mcursor.execute(
-        f"SELECT id, geom, state, iemid, network from stations WHERE {col} "
-        "IS NULL and country = 'US' and state is not null"
+    sl = (
+        "state is not null"
+        if col != "climate_site"
+        else "state != ANY(:states)"
     )
-    for row in mcursor:
-        sid = row[0]
-        geom = row[1]
-        st = row[2]
-        iemid = row[3]
-        network = row[4]
-        # Don't attempt to assign a climate_site for places we have no data
-        if col == "climate_site" and st in [
-            "PU",
-            "P1",
-            "P2",
-            "P3",
-            "P4",
-            "P5",
-        ]:
-            continue
+    updates = pd.read_sql(
+        sql_helper(
+            """
+        SELECT id, geom, state, iemid, network,
+        ST_x(geom) as lon, ST_y(geom) as lat
+        from stations WHERE {col}
+        IS NULL and country = 'US' and {sl}
+        """,
+            col=col,
+            sl=sl,
+        ),
+        conn,
+        params={
+            "states": "PU P1 P2 P3 P4 P5".split(),
+        },
+        index_col="iemid",
+    )
+    LOG.info("Found %s stations needing update for %s", len(updates), col)
+    for iemid, row in updates.iterrows():
+        params = {
+            "lon": row["lon"],
+            "lat": row["lat"],
+            "clnetwork": "NCDC81" if col == "ncdc81" else "NCEI91",
+        }
         # Find the closest site
         if col == "climate_site":
-            sql = f"""
-                select id from stations WHERE network = '{st}CLIMATE'
-                and substr(id,3,4) != '0000' and
-                substr(id,3,1) not in ('C', 'D')
-                and online and archive_begin < '1990-01-01'
-                ORDER by ST_distance(geom, %s) ASC LIMIT 1
-                """
+            # Find the closest site that we have data in ncdc_climate71
+            # which implies a valid climate_site entry already, chicken/egg
+            sql = sql_helper("""
+                select id,
+                ST_Distance(geom, ST_Point(:lon, :lat, 4326)) as dist
+                from stations where
+                id in (select distinct climate_site from stations)
+                ORDER by dist asc
+                """)
         else:
-            clnetwork = "NCDC81" if col == "ncdc81" else "NCEI91"
-            sql = (
-                f"select id from stations WHERE network = '{clnetwork}' "
-                "ORDER by ST_distance(geom, %s) ASC LIMIT 1"
+            sql = sql_helper(
+                "select id from stations WHERE network = :clnetwork "
+                "ORDER by ST_distance(geom, ST_Point(:lon, :lat, 4326)) "
+                "ASC LIMIT 1"
             )
-        mcursor2.execute(sql, (geom,))
-        row2 = mcursor2.fetchone()
-        if row2 is None:
-            LOG.info("Could not find %s site for: %s", col, sid)
-        else:
-            mcursor2.execute(
-                f"UPDATE stations SET {col} = %s WHERE iemid = %s",
-                (row2[0], iemid),
-            )
-            LOG.info("Set %s: %s for ID: %s[%s]", col, row2[0], sid, network)
-    mcursor2.close()
-    pgconn.commit()
+        res = conn.execute(sql, params)
+        if res.rowcount == 0:
+            LOG.info("Could not find any %s sites for: %s", col, iemid)
+            continue
+        row2 = res.mappings().fetchone()
+        conn.execute(
+            sql_helper(
+                "UPDATE stations SET {col} = :id WHERE iemid = :iemid", col=col
+            ),
+            {"id": row2["id"], "iemid": iemid},
+        )
+        LOG.info(
+            "Set %s: %s for ID: %s[%s]",
+            col,
+            row2["id"],
+            row["id"],
+            row["network"],
+        )
+    conn.commit()
 
 
 def main():
