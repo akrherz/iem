7377ddfef43cdcd0daa984817423838e2e233817
akrherz <akrherz@iastate.edu>
2025-11-09 10:40:59 -0600

    📝 Add winter2526 as a thing

--- htdocs/onsite/features/tags/index.php
+++ htdocs/onsite/features/tags/index.php
@@ -81,6 +81,7 @@ previous winter seasons that these maps are available for:
     <li><a href="winter2223.html">Winter 2022-2023</a></li>
     <li><a href="winter2324.html">Winter 2023-2024</a></li>
     <li><a href="winter2425.html">Winter 2024-2025</a></li>
+    <li><a href="winter2526.html">Winter 2025-2026</a></li>
 </ul></p>
 
 EOM;
