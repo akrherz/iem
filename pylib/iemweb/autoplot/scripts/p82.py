ac783d8eec13fabddcfabf29dc8a477bf9e0ae59
akrherz <akrherz@iastate.edu>
2026-01-05 13:09:50 -0600

    ✨ Add column calendar layout to obs autoplot
    https://mesonet.agron.iastate.edu/plotting/auto/?q=82
    closes #180

--- pylib/iemweb/autoplot/scripts/p82.py
+++ pylib/iemweb/autoplot/scripts/p82.py
@@ -5,34 +5,65 @@ weather station.  The climatology is based on the nearest NCEI 1991-2020
 climate station, which in most cases is the same station.  Climatology
 values are rounded to the nearest whole degree Fahrenheit and then compared
 against the observed value to compute a departure.
+
+<p>A current limitation is that no more than 12 months of data can be plotted
+at a single time.  The image shape of the column plot is also hard coded as
+well.
 """
 
 from datetime import date, timedelta
 
 import numpy as np
 import pandas as pd
+from matplotlib.cm import ScalarMappable
+from matplotlib.colors import BoundaryNorm
+from matplotlib.figure import Figure
 from matplotlib.patches import Rectangle
-from pyiem.database import get_dbconnc, get_sqlalchemy_conn, sql_helper
+from pyiem.database import get_sqlalchemy_conn, sql_helper
 from pyiem.exceptions import NoDataFound
-from pyiem.plot import calendar_plot
-from pyiem.reference import TRACE_VALUE
+from pyiem.plot import calendar_plot, figure, get_cmap
+from pyiem.reference import TRACE_VALUE, Z_OVERLAY2, Z_OVERLAY2_LABEL
 from pyiem.util import convert_value
 
 PDICT = {
-    "max_tmpf": "High Temperature",
-    "high_departure": "High Temperature Departure",
-    "min_tmpf": "Low Temperature",
-    "low_departure": "Low Temperature Departure",
-    "avg_tmpf": "Average Temperature",
-    "avg_departure": "Average Temperature Departure",
-    "max_dwpf": "Highest Dew Point Temperature",
-    "min_dwpf": "Lowest Dew Point Temperature",
-    "max_feel": "Maximum Feels Like Temperature",
-    "min_feel": "Minimum Feels Like Temperature",
+    "max_tmpf": "High Temperature (°F)",
+    "high_departure": "High Temperature Departure (°F)",
+    "min_tmpf": "Low Temperature (°F)",
+    "low_departure": "Low Temperature Departure (°F)",
+    "avg_tmpf": "Average Temperature (°F)",
+    "avg_departure": "Average Temperature Departure (°F)",
+    "max_dwpf": "Highest Dew Point Temperature (°F)",
+    "min_dwpf": "Lowest Dew Point Temperature (°F)",
+    "max_feel": "Maximum Feels Like Temperature (°F)",
+    "min_feel": "Minimum Feels Like Temperature (°F)",
     "avg_smph": "Average Wind Speed [mph]",
     "max_smph": "Maximum Wind Speed/Gust [mph]",
-    "pday": "Precipitation",
+    "pday": "Precipitation [inch]",
     "max_rstage": "Maximum Water Stage [ft]",
+}
+BINS = {
+    "max_tmpf": [-40, 121, 10],
+    "high_departure": [-50, 51, 5],
+    "min_tmpf": [-40, 91, 10],
+    "low_departure": [-50, 51, 5],
+    "avg_tmpf": [-20, 101, 5],
+    "avg_departure": [-30, 31, 3],
+    "max_dwpf": [-40, 91, 10],
+    "min_dwpf": [-40, 91, 10],
+    "max_feel": [-40, 121, 10],
+    "min_feel": [-40, 121, 10],
+    "avg_smph": [0, 51, 5],
+    "max_smph": [0, 71, 5],
+    "pday": [0, 3, 0.25],
+    "max_rstage": [0, 51, 5],
+}
+LAYOUT = {
+    "calendar": "Calendar Layout",
+    "column": "Months as Columns Layout",
+}
+COLORIZE = {
+    "yes": "Yes, colorize cells based on values",
+    "no": "No, do not colorize cells",
 }
 STAGES = "action flood moderate major".split()
 COLORS = "white #ffff72 #ffc672 #ff7272 #e28eff".split()
@@ -73,8 +104,100 @@ def get_description():
             label="End Date:",
             min="1900/01/01",
         ),
+        {
+            "type": "select",
+            "options": LAYOUT,
+            "default": "calendar",
+            "label": "Select calendar layout",
+            "name": "layout",
+        },
+        {
+            "type": "select",
+            "options": COLORIZE,
+            "default": "yes",
+            "label": "Colorize cells?",
+            "name": "colorize",
+        },
+        {
+            "type": "cmap",
+            "default": "jet",
+            "name": "cmap",
+            "label": "Select color ramp:",
+        },
+        {
+            "type": "text",
+            "label": (
+                "Specify minimum, maximum, interval for color ramp bins "
+                "as comma separated values (e.g. 0,101,10)"
+            ),
+            "default": "0,101,10",
+            "optional": True,
+            "name": "interval",
+        },
     ]
     return desc
+
+
+def column_plot(fig: Figure, sdate: date, edate: date, data: dict[date, dict]):
+    """Create a specialized figure."""
+    # Create an axes object to manually plot things onto
+    ax = fig.add_axes((0.1, 0.1, 0.75, 0.8), frameon=False)
+    ax.set_xlim(-0.5, 11.5)
+    ax.set_xticks([])
+    ax.set_yticks(np.arange(1, 32, 1))
+    ax.set_yticklabels(np.arange(1, 32, 1))
+    ax.set_ylim(31.5, -0.5)  # Invert y axis
+    ax.set_ylabel("Day of Month")
+    xpos = -1
+    current_xmonth = -1
+    for pdt in pd.date_range(sdate, edate):
+        dt = pdt.date()
+        if dt.month != current_xmonth:
+            xpos += 1
+            current_xmonth = dt.month
+            ax.add_patch(
+                Rectangle(
+                    (xpos - 0.5, -0.5),
+                    1,
+                    1,
+                    facecolor="white",
+                    edgecolor="k",
+                    lw=2,
+                    zorder=Z_OVERLAY2_LABEL,  # Put overtop the days
+                )
+            )
+            ax.annotate(
+                dt.strftime("%b"),
+                xy=(xpos, 0),
+                ha="center",
+                va="center",
+                fontsize=12,
+                zorder=Z_OVERLAY2_LABEL,
+            )
+        if dt not in data:
+            continue
+        val = data[dt]["val"]
+        color = data[dt].get("color", "k")
+        cellcolor = data[dt].get("cellcolor", "white")
+        ax.add_patch(
+            Rectangle(
+                (xpos - 0.5, dt.day - 0.5),
+                1,
+                1,
+                facecolor=cellcolor,
+                edgecolor="lightgray",
+                zorder=Z_OVERLAY2,
+            )
+        )
+        ax.annotate(
+            val,
+            (xpos, dt.day),
+            color=color,
+            ha="center",
+            va="center",
+            fontsize=12,
+            zorder=Z_OVERLAY2_LABEL,
+        )
 
 
 def safe(row, varname):
@@ -121,6 +244,9 @@ def plotter(ctx: dict):
     varname = ctx["var"]
     sdate = ctx["sdate"]
     edate = ctx["edate"]
+    # Check for more than 12 months of data
+    if pd.date_range(sdate, edate).strftime("%Y%m").nunique() > 12:
+        raise NoDataFound("Date range cannot exceed 12 months.")
 
     # Get Climatology
     with get_sqlalchemy_conn("coop") as conn:
@@ -140,45 +266,62 @@ def plotter(ctx: dict):
         raise NoDataFound("No Data Found.")
 
     if ctx["network"].find("CLIMATE") > -1:
-        pgconn, cursor = get_dbconnc("coop")
-        cursor.execute(
-            """
-            SELECT
-            day,
-            high as max_tmpf,
-            low as min_tmpf,
-            null as max_dwpf,
-            null as min_dwpf,
-            null as max_feel,
-            null as min_feel,
-            (high + low) / 2. as avg_tmpf,
-            precip as pday,
-            null as avg_sknt, null as peak_wind,
-            null as max_rstage
-            from alldata
-            WHERE day >= %s and day <= %s and
-            station = %s ORDER by day ASC
-        """,
-            (sdate, edate, station),
-        )
+        with get_sqlalchemy_conn("coop") as conn:
+            res = conn.execute(
+                sql_helper("""
+                SELECT
+                day,
+                high as max_tmpf,
+                low as min_tmpf,
+                null as max_dwpf,
+                null as min_dwpf,
+                null as max_feel,
+                null as min_feel,
+                (high + low) / 2. as avg_tmpf,
+                precip as pday,
+                null as avg_sknt, null as peak_wind,
+                null as max_rstage
+                from alldata
+                WHERE day >= :sdate and day <= :edate and
+                station = :station ORDER by day ASC
+            """),
+                {"sdate": sdate, "edate": edate, "station": station},
+            )
     else:
-        pgconn, cursor = get_dbconnc("iem")
-        cursor.execute(
-            """
-            SELECT day, max_tmpf, min_tmpf, max_dwpf, min_dwpf,
-            (max_tmpf + min_tmpf) / 2. as avg_tmpf,
-            pday, avg_sknt, coalesce(max_gust, max_sknt) as peak_wind,
-            max_rstage, max_feel, min_feel
+        with get_sqlalchemy_conn("iem") as conn:
+            res = conn.execute(
+                sql_helper("""
+                SELECT day, max_tmpf, min_tmpf, max_dwpf, min_dwpf,
+                (max_tmpf + min_tmpf) / 2. as avg_tmpf,
+                pday, avg_sknt, coalesce(max_gust, max_sknt) as peak_wind,
+                max_rstage, max_feel, min_feel
             from summary s JOIN stations t
-            on (t.iemid = s.iemid) WHERE s.day >= %s and s.day <= %s and
-            t.id = %s and t.network = %s ORDER by day ASC
-        """,
-            (sdate, edate, station, ctx["network"]),
-        )
+            on (t.iemid = s.iemid) WHERE s.day >= :sdate and s.day <= :edate
+            and
+            t.id = :station and t.network = :network ORDER by day ASC
+        """),
+                {
+                    "sdate": sdate,
+                    "edate": edate,
+                    "station": station,
+                    "network": ctx["network"],
+                },
+            )
     stagevals = []
     rows = []
     data = {}
-    for row in cursor:
+    cmap = get_cmap(ctx["cmap"])
+    if ctx.get("interval", "") != "":
+        norm = BoundaryNorm(
+            np.arange(*map(float, ctx["interval"].split(","))),
+            cmap.N,
+        )
+    else:
+        norm = BoundaryNorm(
+            np.arange(*BINS[varname]),
+            cmap.N,
+        )
+    for row in res.mappings():
         hd = diff(row["max_tmpf"], cdf.at[row["day"].strftime("%m%d"), "high"])
         ld = diff(row["min_tmpf"], cdf.at[row["day"].strftime("%m%d"), "low"])
         ad = diff(row["avg_tmpf"], cdf.at[row["day"].strftime("%m%d"), "avg"])
@@ -228,7 +371,21 @@ def plotter(ctx: dict):
             if not pd.isna(row["max_rstage"]):
                 idx = np.digitize(row["max_rstage"], stagevals)
                 data[row["day"]]["cellcolor"] = COLORS[idx]
-    pgconn.close()
+        if ctx["colorize"] == "yes":
+            rawval = rows[-1][varname]
+            if rawval is not None:
+                color = cmap(norm([rawval]))[0]
+                hexcolor = (
+                    f"#{int(color[0] * 255):02x}{int(color[1] * 255):02x}"
+                    f"{int(color[2] * 255):02x}"
+                )
+                data[row["day"]]["cellcolor"] = hexcolor
+                # The text now needs to be in an opposite color, so to read!
+                brightness = (
+                    0.299 * color[0] + 0.587 * color[1] + 0.114 * color[2]
+                )
+                data[row["day"]]["color"] = "k" if brightness > 0.5 else "w"
+
     df = pd.DataFrame(rows)
 
     title = (
@@ -237,14 +394,33 @@ def plotter(ctx: dict):
     )
     subtitle = f"{sdate:%-d %b %Y} thru {edate:%-d %b %Y}"
 
-    fig = calendar_plot(
-        sdate,
-        edate,
-        data,
-        title=title,
-        subtitle=subtitle,
-        apctx=ctx,
-    )
-    if varname == "max_rstage":
-        add_stages_legend(fig, stagevals)
+    if ctx["layout"] == "calendar":
+        fig = calendar_plot(
+            sdate,
+            edate,
+            data,
+            title=title,
+            subtitle=subtitle,
+            apctx=ctx,
+        )
+        if varname == "max_rstage":
+            add_stages_legend(fig, stagevals)
+    else:
+        fig = figure(
+            title=title,
+            subtitle=subtitle,
+            figsize=(8, 8),
+        )
+        column_plot(fig, sdate, edate, data)
+        if ctx["colorize"] == "yes":
+            # Create a ScalarMappable for the colorbar
+            sm = ScalarMappable(cmap=cmap, norm=norm)
+            sm.set_array([])  # Required for ScalarMappable
+            cax = fig.add_axes((0.9, 0.15, 0.02, 0.7))
+            fig.colorbar(
+                sm,
+                cax=cax,
+                orientation="vertical",
+                label=PDICT.get(varname),
+            )
     return fig, df
