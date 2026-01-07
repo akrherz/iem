37747938b4155d0b91e43067be3179f9894719bc
akrherz <akrherz@iastate.edu>
2026-01-07 14:23:46 -0600

    ✨ Add GeoJSON service of NWS COOP Obs for a date
    https://mesonet.agron.iastate.edu/geojson/coopobs.py?help

---
+++ htdocs/geojson/coopobs.py
@@ -0,0 +1,6 @@
+"""Implementation at https://github.com/akrherz/iem/blob/main/pylib/iemweb/geojson/coopobs.py
+User documentation available at https://mesonet.agron.iastate.edu/geojson/coopobs.py?help"""
+
+from iemweb.geojson.coopobs import application
+
+__all__ = ["application"]
