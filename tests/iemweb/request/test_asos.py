1d6447e45d6ae89139765ba0bc7e45c52a197dc7
akrherz <akrherz@iastate.edu>
2026-01-23 07:56:57 -0600

    🎨 Document and test a 1,000 station+year limit
    https://mesonet.agron.iastate.edu/cgi-bin/request/asos.py?help

---
+++ tests/iemweb/request/test_asos.py
@@ -0,0 +1,24 @@
+"""Test iemweb.request.asos module."""
+
+from iemweb.request.asos import application
+from werkzeug.test import Client
+
+
+def test_asos_request():
+    """Test too much data with the request."""
+    client = Client(application, None)
+    resp = client.get(
+        "?sts=1900-01-01T00:00Z"
+        "&station=AWM,DSM,ALO,OTM,BRL,DBQ,CID,IOW,MCW,MIW"
+        "&ets=2026-01-01T00:00Z&format=comma"
+    )
+    assert resp.status_code == 400
+
+
+def test_asos_alldata_greater_than_24hours():
+    """Test that we don't allow this request either."""
+    client = Client(application, None)
+    resp = client.get(
+        "?sts=1900-01-01T00:00Z&ets=2026-01-01T00:00Z&format=comma"
+    )
+    assert resp.status_code == 400
