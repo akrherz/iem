b1b44886cf5eacb6bd20c7161edd83cbf019de3d
akrherz <akrherz@iastate.edu>
2026-03-09 08:14:19 -0500

    🎨 Eat dogfood for Iowa vsby plot

--- scripts/current/vsby.py
+++ scripts/current/vsby.py
@@ -1,75 +1,19 @@
-"""Generate current plot of visibility"""
+"""Generate analysis of visibility."""
 
-import warnings
-from datetime import datetime
-
-import numpy as np
-from pyiem.database import get_dbconn
-from pyiem.plot import MapPlot, get_cmap
-from pyiem.util import utc
-
-# Stop whining about missing data for contourf
-warnings.filterwarnings("ignore")
+from pyiem.util import utc, web2ldm
 
 
 def main():
-    """GO"""
-    pgconn = get_dbconn("iem")
-    cursor = pgconn.cursor()
-
-    # Compute normal from the climate database
-    sql = """
-    SELECT
-      id, network, vsby, ST_x(geom) as lon, ST_y(geom) as lat
-    FROM
-      current c JOIN stations s ON (s.iemid = c.iemid)
-    WHERE
-      s.network IN ('IA_ASOS','IL_ASOS','MN_ASOS','WI_ASOS','SD_ASOS',
-                  'NE_ASOS','MO_ASOS') and
-      valid + '60 minutes'::interval > now() and
-      vsby >= 0 and vsby <= 10
-    """
-
-    lats = []
-    lons = []
-    vals = []
-    valmask = []
-    cursor.execute(sql)
-    for row in cursor:
-        lats.append(row[4])
-        lons.append(row[3])
-        vals.append(row[2])
-        valmask.append(row[1] == "IA_ASOS")
-
-    if len(lats) < 5:
-        return
-
-    now = datetime.now()
-
-    mp = MapPlot(
-        sector="iowa",
-        title="Iowa Visibility",
-        subtitle=f"Valid: {now:%d %b %Y %-I:%M %p}",
-    )
-
-    mp.contourf(
-        lons,
-        lats,
-        vals,
-        np.array([0.01, 0.1, 0.25, 0.5, 1, 2, 3, 5, 8, 9.9]),
-        units="miles",
-        cmap=get_cmap("gray"),
-    )
-
-    mp.plot_values(lons, lats, vals, "%.1f", valmask)
-    mp.drawcounties()
-
+    """GO!"""
     pqstr = (
         f"plot ac {utc():%Y%m%d%H}00 iowa_vsby.png "
-        f"vsby_contour_{utc():%H}00.png png"
+        f"iowa_vsby_{utc():%H}.png png"
     )
-    mp.postprocess(pqstr=pqstr)
-    mp.close()
+    web2ldm(
+        "https://mesonet.agron.iastate.edu/plotting/auto/plot/192/"
+        "t:state::state:IA::v:vsby::cmap:gray::_cb:1.png",
+        pqstr,
+    )
 
 
 if __name__ == "__main__":
