d1ca4bfc0879f6f96ee0bac84ee071d86fb4184e
akrherz <akrherz@iastate.edu>
2026-04-14 09:59:56 -0500

    📝 Sundry updates per review

--- htdocs/info.php
+++ htdocs/info.php
@@ -6,43 +6,68 @@ define("IEM_APPID", 61);
 $t->title = "Information Mainpage";
 
 $t->content = <<<EOM
-<h3>Information/Documents</h3><p>
+<div class="container-fluid py-3">
+  <h2 class="h3 mb-4">Information/Documents</h2>
 
-<div class="row">
-<div class="col-md-6 col-sm-6">
+  <div class="row g-4">
+    <div class="col-12 col-md-6">
+      <div class="card h-100">
+        <div class="card-body">
+          <h3 class="h5 card-title">Quick Links</h3>
+          <ul class="list-group list-group-flush mb-3">
+            <li class="list-group-item"><a href="/info/iem.php">IEM Info/Background</a></li>
+            <li class="list-group-item">
+            <i class="bi bi-book"></i> "Mastering the Mesonet" documentation kindly written by Jonathan Sailor, March 2026.
+            <a href="/pickup/mastering_mesonet/Mastering-the-Mesonet.pdf">PDF</a> or
+            <a href="/pickup/mastering_mesonet/Mastering-the-Mesonet.epub">ePub</a>
+            </li>
+            <li class="list-group-item"><a href="/info/links.php">Links</a></li>
+            <li class="list-group-item"><a href="/info/variables.phtml">Variables Collected</a></li>
+          </ul>
 
-<h3>Quick Links:</h3></p>
-<ul>
-<li><a href="/info/iem.php">IEM Info/Background</a></li>
-<li><a href="/info/links.php">Links</a></li>
-<li><a href="/info/variables.phtml">Variables Collected</a></li></ul>
+          <p class="mb-0">Information about requesting a <a href="/request/ldm.php">real-time data feed</a>.</p>
+        </div>
+      </div>
+    </div>
 
-<p>Information about requesting a <a href="/request/ldm.php">real-time data feed</a>
-<p>
-<h3>Station Locations: (graphical)</h3>
-<ul>
-    <li><a href="/sites/locate.php?network=IA_ASOS">ASOS Locations</a></li>
-    <li><a href="/sites/locate.php?network=IA_RWIS">RWIS Locations</a></li>
-    <li><a href="/sites/locate.php?network=IA_COOP">COOP Locations</a></li>
-    <li><a href="/sites/locate.php?network=ISUSM">ISU Soil Moisture Locations</a></li>
-</ul>
+    <div class="col-12 col-md-6">
+      <div class="card h-100">
+        <div class="card-body">
+          <h3 class="h5 card-title">Station Locations (graphical)</h3>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item"><a href="/sites/locate.php?network=IA_ASOS">ASOS Locations</a></li>
+            <li class="list-group-item"><a href="/sites/locate.php?network=IA_RWIS">RWIS Locations</a></li>
+            <li class="list-group-item"><a href="/sites/locate.php?network=IA_COOP">COOP Locations</a></li>
+            <li class="list-group-item"><a href="/sites/locate.php?network=ISUSM">ISU Soil Moisture Locations</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
 
+    <div class="col-12 col-md-6">
+      <div class="card h-100">
+        <div class="card-body">
+          <h3 class="h5 card-title">IEM Server Information</h3>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item"><a href="/usage/">Webfarm Statistics</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-12 col-md-6">
+      <div class="card h-100">
+        <div class="card-body">
+          <h3 class="h5 card-title">Papers/Presentations</h3>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item"><a href="/present/">IEM Presentation Archive</a></li>
+            <li class="list-group-item"><a href="/docs/unidata2006/">2006 Unidata Equipment Grant Report</a></li>
+            <li class="list-group-item"><a href="/docs/unidata2021/">2021 Unidata Equipment Grant Report</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
-<div class="col-md-6 col-sm-6">
-
-<h3>IEM Server Information:</h3>
-<ul>
-    <li><a href="/usage/">Webfarm Statistics</a></li>
-</ul>
-
-<h3>Papers/Presentations</h3>
-<ul>
-  <li><a href="/present/">IEM Presentation Archive</a></li>
-  <li><a href="/docs/unidata2006/">2006 Unidata Equipment Grant Report</a></li>
-  <li><a href="/docs/unidata2021/">2021 Unidata Equipment Grant Report</a></li>
-</ul>
-
-</div></div>
-
 EOM;
 $t->render('single.phtml');
