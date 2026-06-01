c9a7e35990cdaa316062998dc0cc43edc622f96f
akrherz <akrherz@iastate.edu>
2026-06-01 10:00:46 -0500

    ♻️ Refactor {n0r,n0q}2gtiff processing

--- htdocs/request/gis/n0q2gtiff.php
+++ htdocs/request/gis/n0q2gtiff.php
@@ -4,51 +4,30 @@ ignore_user_abort(true);
 
 require_once "../../../include/throttle.php";
 require_once "../../../include/forms.php";
+require_once "../../../include/mlib.php";
 
-date_default_timezone_set('UTC');
-putenv("TZ=GMT");
-/* This bad boy converts a PNG to a geo-tiff */
+$dt = dstr2dt(get_str404("dstr"));
 
-$dstr = get_str404("dstr", null);
-if (is_null($dstr)) {
-    http_response_code(422);
-    die("Invalid date format");
-}
-// Validate dstr format (12 digits: YYYYMMDDHHMM)
-if (!preg_match('/^\d{12}$/', $dstr)) {
-    http_response_code(422);
-    die("Invalid date format");
-}
 $sector = substr(get_str404("sector", "us"), 0, 2);
 // Validate sector contains only letters
 if (!preg_match('/^[a-zA-Z]+$/', $sector)) {
     http_response_code(422);
     die("Invalid sector format");
 }
-$year = intval(substr($dstr, 0, 4));
-$month = intval(substr($dstr, 4, 2));
-$day = intval(substr($dstr, 6, 2));
-$hour = intval(substr($dstr, 8, 2));
-$minute = intval(substr($dstr, 10, 2));
-$ts = mktime($hour, $minute, 0, $month, $day, $year);
-$now = time();
-if ($ts > $now) {
-    http_response_code(422);
-    die("Request from the future?");
-}
-$tmpdirname = sys_get_temp_dir() ."/png2gtiff_". bin2hex(random_bytes(8));
 
-mkdir($tmpdirname, 0755);
-chdir($tmpdirname);
+$tempDir = sys_get_temp_dir() . DIRECTORY_SEPARATOR . uniqid('php_tmp_', true);
+mkdir($tempDir, 0700, true);
+chdir($tempDir);
 
-$outFile = sprintf("n0q_%s", date("YmdHi", $ts));
-$zipFile = sprintf("n0q_%s.zip", date("YmdHi", $ts));
+$outFile = sprintf("n0q_%s", $dt->format("YmdHi"));
+$zipFile = sprintf("n0q_%s.zip", $dt->format("YmdHi"));
 
 $S = strtoupper($sector);
-if ($ts > ($now - 360.0)) {
+$now = new DateTimeImmutable("now", new DateTimeZone("UTC"));
+if ($dt > ($now->sub(new DateInterval('PT6M')))) {
     $inFile = "/mesonet/ldmdata/gis/images/4326/{$S}COMP/n0q_0.tif";
 } else {
-    $inFile = sprintf("/mesonet/ARCHIVE/data/%s/GIS/{$sector}comp/n0q_%s.png", date("Y/m/d", $ts), date("YmdHi", $ts));
+    $inFile = sprintf("/mesonet/ARCHIVE/data/%s/GIS/{$sector}comp/n0q_%s.png", $dt->format("Y/m/d"), $dt->format("YmdHi"));
 }
 
 if (!is_file($inFile)) die("No GIS composite $inFile found for this time!");
@@ -59,12 +38,12 @@ $cmd = sprintf("gdalwarp -t_srs %s -s_srs %s -of GTIFF %s %s.tif",
     escapeshellarg("EPSG:4326"),
     escapeshellarg($inFile),
     escapeshellarg($outFile));
-`$cmd`;
+shell_exec($cmd);
 
 $cmd = sprintf("zip %s %s.tif",
     escapeshellarg($zipFile),
     escapeshellarg($outFile));
-`$cmd`;
+shell_exec($cmd);
 
 header("Content-type: application/octet-stream");
 header("Content-Disposition: attachment; filename={$zipFile}");
@@ -74,4 +53,4 @@ unlink($zipFile);
 unlink("{$outFile}.tif");
 
 chdir("..");
-rmdir($tmpdirname);
+shell_exec("rm -rf $tempDir");
--- include/mlib.php
+++ include/mlib.php
@@ -1,6 +1,48 @@
 <?php
 
 /**
+ * Common logic for converting a hacky 12 char time string in UTC
+ *
+ * @param string $dstr The date string in the format YYYYMMDDHHMM
+ * @param int $minute_modulo The minute must be a multiple of this value (default is 5)
+ * @return DateTimeImmutable The corresponding DateTimeImmutable object in UTC
+ * @throws Exception If the date string is invalid or represents a future time
+ */
+function dstr2dt($dstr, $minute_modulo = 5) {
+    if (is_null($dstr)) {
+        http_response_code(422);
+        die("Invalid date format");
+    }
+    // Validate dstr format (12 digits: YYYYMMDDHHMM)
+    if (!preg_match('/^\d{12}$/', $dstr)) {
+        http_response_code(422);
+        die("Invalid date format");
+    }
+    $year = intval(substr($dstr, 0, 4));
+    $month = intval(substr($dstr, 4, 2));
+    $day = intval(substr($dstr, 6, 2));
+    $hour = intval(substr($dstr, 8, 2));
+    $minute = intval(substr($dstr, 10, 2));
+
+    // Ensure the minute is modulo the specified value
+    if ($minute % $minute_modulo != 0) {
+        http_response_code(422);
+        die("Minute must be a multiple of $minute_modulo");
+    }
+
+    $dt = new DateTimeImmutable(
+        sprintf("%04d-%02d-%02d %02d:%02d:00", $year, $month, $day, $hour, $minute),
+        new DateTimeZone("UTC"),
+    );
+    $now = new DateTimeImmutable("now", new DateTimeZone("UTC"));
+    if ($dt > $now) {
+        http_response_code(422);
+        die("Request from the future?");
+    }
+    return $dt;
+}
+
+/**
  * Convert a vague 3 character WFO identifier to a 4 character one
  * @param string $wfo3 the 3 character WFO identifier
  * @return string the 4 character WFO identifier
