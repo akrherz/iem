fef7e5784a831e485654ccbb3f22b15fe4b2d834
akrherz <akrherz@iastate.edu>
2026-09-15 22:30:05 -0500

    ✏️ Fix spelling

--- pylib/iemweb/autoplot/scripts100/p170.py
+++ pylib/iemweb/autoplot/scripts100/p170.py
@@ -9,7 +9,7 @@ the years and there is some non-standard nomenclature used by some sites.
 The thunder (TS) reports are delineated into three categories here to
 hopefully allow more accurate statistics.
 <ul>
-    <li><strong>All Rain Reports (RA)</strong> incldues any mention of
+    <li><strong>All Rain Reports (RA)</strong> includes any mention of
     <code>RA</code> in any present weather code</li>
     <li><strong>All Thunder Reports (TS)</strong> includes any
     <code>TS</code> mention in any present weather code</li>
